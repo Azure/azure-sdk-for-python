3bf69fba4eb981e6ddf8920994b79314eff208bd
shipunyc <mr.pu.shi@gmail.com>
2014-12-09 10:49:53 -0800

    Implements fetch_next_block.

--- pydocumentdb/query_iterable.py
+++ pydocumentdb/query_iterable.py
@@ -39,6 +39,9 @@ class QueryIterable(object):
             self._iterable = iterable
             self._finished = False
             self._current = -1
+            self._iterable._results = []
+            self._iterable._continuation = None
+            self._iterable._has_started = False
 
         def __iter__(self):
             # Always returns self
@@ -53,7 +56,8 @@ class QueryIterable(object):
             if self._current < len(self._iterable._results):
                 return self._iterable._results[self._current]
             else:
-                if self._iterable._fetch_next_block():
+                if self._iterable.fetch_next_block():
+                    self._current = 0
                     return self._iterable._results[self._current]
             self._finished = True
             raise StopIteration
@@ -61,11 +65,11 @@ class QueryIterable(object):
         # Also support Python 3.x iteration
         __next__ = next
 
-    def _fetch_next_block(self):
+    def fetch_next_block(self):
         """Fetches more items.
 
         :Returns:
-            int, number of fetched items.
+            list of fetched items.
 
         """
         fetched_items = []
@@ -74,9 +78,9 @@ class QueryIterable(object):
                 self._has_started = True
             self._options['continuation'] = self._continuation
             (fetched_items, response_headers) = self._fetch_function(self._options)
-            self._results.extend(fetched_items)
+            self._results = fetched_items
             self._continuation = response_headers.get(
                 http_constants.HttpHeaders.Continuation)
             if fetched_items:
                 break
-        return len(fetched_items)
+        return fetched_items
