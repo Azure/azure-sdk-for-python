895d20cfd0cc2149ce5c8a0b3b9d415e6577be5e
huguesv <huvalo@microsoft.com>
2015-04-23 17:17:34 -0700

    Code review feedback applied and refactoring of common code in authentication classes.

--- azure/storage/__init__.py
+++ azure/storage/__init__.py
@@ -1154,58 +1154,78 @@ def _storage_error_handler(http_error):
     return _general_error_handler(http_error)
 
 
-class StorageSASAuthentication:
+class StorageSASAuthentication(object):
     def __init__(self, sas_token):
         self.sas_token = sas_token
 
     def sign_request(self, request):
-        if request.path.find('?') == -1:
-            request.path += '?'
-        else:
+        if '?' in request.path:
             request.path += '&'
+        else:
+            request.path += '?'
 
         request.path += self.sas_token
 
 
-class StorageSharedKeyAuthentication:
+class _StorageSharedKeyAuthentication(object):
     def __init__(self, account_name, account_key):
         self.account_name = account_name
         self.account_key = account_key
 
-    def sign_request(self, request):
+    def _get_headers(self, request, headers_to_sign):
+        headers = {
+            name.lower() : value for name, value in request.headers if value
+        }
+        return '\n'.join(headers.get(x, '') for x in headers_to_sign) + '\n'
+
+    def _get_verb(self, request):
+        return request.method + '\n'
+
+    def _get_canonicalized_resource(self, request):
         uri_path = request.path.split('?')[0]
+        return '/' + self.account_name + uri_path
 
-        # method to sign
-        string_to_sign = request.method + '\n'
-
-        # get headers to sign
-        headers_to_sign = [
-            'content-encoding', 'content-language', 'content-length',
-            'content-md5', 'content-type', 'date', 'if-modified-since',
-            'if-match', 'if-none-match', 'if-unmodified-since', 'range']
-
-        request_header_dict = dict((name.lower(), value)
-                                   for name, value in request.headers if value)
-        string_to_sign += '\n'.join(request_header_dict.get(x, '')
-                                    for x in headers_to_sign) + '\n'
-
-        # get x-ms header to sign
+    def _get_canonicalized_headers(self, request):
+        string_to_sign = ''
         x_ms_headers = []
         for name, value in request.headers:
-            if 'x-ms' in name:
+            if name.startswith('x-ms-'):
                 x_ms_headers.append((name.lower(), value))
         x_ms_headers.sort()
         for name, value in x_ms_headers:
             if value:
                 string_to_sign += ''.join([name, ':', value, '\n'])
+        return string_to_sign
 
-        # get account_name and uri path to sign
-        string_to_sign += '/' + self.account_name + uri_path
+    def _add_authorization_header(self, request, string_to_sign):
+        signature = _sign_string(self.account_key, string_to_sign)
+        auth_string = 'SharedKey ' + self.account_name + ':' + signature
+        request.headers.append(('Authorization', auth_string))
 
-        # get query string to sign if it is not table service
+
+class StorageSharedKeyAuthentication(_StorageSharedKeyAuthentication):
+    def sign_request(self, request):
+        string_to_sign = \
+            self._get_verb(request) + \
+            self._get_headers(
+                request,
+                [
+                    'content-encoding', 'content-language', 'content-length',
+                    'content-md5', 'content-type', 'date', 'if-modified-since',
+                    'if-match', 'if-none-match', 'if-unmodified-since', 'range'
+                ]
+            ) + \
+            self._get_canonicalized_headers(request) + \
+            self._get_canonicalized_resource(request) + \
+            self._get_canonicalized_resource_query(request)
+
+        self._add_authorization_header(request, string_to_sign)
+
+    def _get_canonicalized_resource_query(self, request):
         query_to_sign = request.query
         query_to_sign.sort()
 
+        string_to_sign = ''
         current_name = ''
         for name, value in query_to_sign:
             if value:
@@ -1215,44 +1235,30 @@ class StorageSharedKeyAuthentication:
                 else:
                     string_to_sign += '\n' + ',' + value
 
-        # sign the request
-        signature = _sign_string(self.account_key, string_to_sign)
-        auth_string = 'SharedKey ' + self.account_name + ':' + signature
-
-        request.headers.append(('Authorization', auth_string))
+        return string_to_sign
 
 
-class StorageTableSharedKeyAuthentication:
-    def __init__(self, account_name, account_key):
-        self.account_name = account_name
-        self.account_key = account_key
-
+class StorageTableSharedKeyAuthentication(_StorageSharedKeyAuthentication):
     def sign_request(self, request):
-        uri_path = request.path.split('?')[0]
+        string_to_sign = \
+            self._get_verb(request) + \
+            self._get_headers(
+                request,
+                ['content-md5', 'content-type', 'date'],
+            ) + \
+            self._get_canonicalized_resource(request) + \
+            self._get_canonicalized_resource_query(request)
 
-        string_to_sign = request.method + '\n'
-        headers_to_sign = ['content-md5', 'content-type', 'date']
-        request_header_dict = dict((name.lower(), value)
-                                   for name, value in request.headers if value)
-        string_to_sign += '\n'.join(request_header_dict.get(x, '')
-                                    for x in headers_to_sign) + '\n'
+        self._add_authorization_header(request, string_to_sign)
 
-        # get account_name and uri path to sign
-        string_to_sign += ''.join(['/', self.account_name, uri_path])
-
+    def _get_canonicalized_resource_query(self, request):
         for name, value in request.query:
             if name == 'comp':
-                string_to_sign += '?comp=' + value
-                break
-
-        # sign the request
-        auth_string = 'SharedKey ' + self.account_name + ':' + \
-            _sign_string(self.account_key, string_to_sign)
-
-        request.headers.append(('Authorization', auth_string))
+                return '?comp=' + value
+        return ''
 
 
-class StorageNoAuthentication:
+class StorageNoAuthentication(object):
     def sign_request(self, request):
         pass
 
