9d3e499990b3b97b56ea3878ccab1064c6191ca6
huguesv <huvalo@microsoft.com>
2015-05-04 15:59:46 -0700

    Fixes to schedule management: - Fixes unicode/str issue on Python 3. - Make put/delete operations return AsynchronousOperationResult so we can wait for them to finish. - Some refactoring of code in tests.

--- tests/test_schedulermanagementservice.py
+++ tests/test_schedulermanagementservice.py
@@ -39,19 +39,39 @@ class SchedulerManagementServiceTest(AzureTestCase):
 
     def setUp(self):
         self.ss = create_service_management(SchedulerManagementService)
-        self.cloud_service_id = getUniqueName('cloud_service_')
+        self.service_id = getUniqueName('cloud_service_')
+        self.coll_id = getUniqueName('job_collection_')
+        self.job_id = 'job_id'
 
     def tearDown(self):
         self.cleanup()
         return super(SchedulerManagementServiceTest, self).tearDown()
 
     def cleanup(self):
-        self.ss.delete_cloud_service(self.cloud_service_id)
+        self.ss.delete_cloud_service(self.service_id)
         pass
 
     def _create_cloud_service(self):
-        self.ss.create_cloud_service(
-            self.cloud_service_id, "label", "description", "West Europe")
+        result = self.ss.create_cloud_service(
+            self.service_id,
+            "label",
+            "description",
+            "West Europe",
+        )
+        self.ss.wait_for_operation_status(result.request_id)
+
+    def _create_job_collection(self):
+        result = self.ss.create_job_collection(self.service_id, self.coll_id)
+        self.ss.wait_for_operation_status(result.request_id)
+
+    def _create_job(self):
+        result = self.ss.create_job(
+            self.service_id,
+            self.coll_id,
+            self.job_id,
+            self._create_job_dict(),
+        )
+        self.ss.wait_for_operation_status(result.request_id)
 
     def _create_job_dict(self):
         return {
@@ -99,11 +119,11 @@ class SchedulerManagementServiceTest(AzureTestCase):
         self._create_cloud_service()
 
         # Act
-        result = self.ss.get_cloud_service(self.cloud_service_id)
+        result = self.ss.get_cloud_service(self.service_id)
 
         # Assert
         self.assertIsNotNone(result)
-        self.assertEqual(result.name, self.cloud_service_id)
+        self.assertEqual(result.name, self.service_id)
         self.assertEqual(result.label, "label")
         self.assertEqual(result.geo_region, "West Europe")
 
@@ -112,10 +132,15 @@ class SchedulerManagementServiceTest(AzureTestCase):
 
         # Act
         result = self.ss.create_cloud_service(
-            self.cloud_service_id, "label", "description", "West Europe")
+            self.service_id,
+            "label",
+            "description",
+            "West Europe",
+        )
+        self.ss.wait_for_operation_status(result.request_id)
 
         # Assert
-        self.assertIsNone(result)
+        self.assertIsNotNone(result)
 
     @unittest.skip("functionality not working, haven't had a chance to debug")
     def test_check_name_availability(self):
@@ -123,8 +148,7 @@ class SchedulerManagementServiceTest(AzureTestCase):
         self._create_cloud_service()
 
         # Act
-        result = self.ss.check_job_collection_name(
-            self.cloud_service_id, "BOB")
+        result = self.ss.check_job_collection_name(self.service_id, "BOB")
 
         # Assert
         self.assertIsNotNone(result)
@@ -134,110 +158,87 @@ class SchedulerManagementServiceTest(AzureTestCase):
         self._create_cloud_service()
 
         # Act
-        result = self.ss.create_job_collection(
-            self.cloud_service_id, getUniqueName('job_collection_'))
+        result = self.ss.create_job_collection(self.service_id, self.coll_id)
+        self.ss.wait_for_operation_status(result.request_id)
 
         # Assert
-        self.assertIsNone(result)
+        self.assertIsNotNone(result)
 
     def test_delete_job_collection(self):
         # Arrange
-        job_collection_id = getUniqueName('job_collection_')
         self._create_cloud_service()
-        self.ss.create_job_collection(self.cloud_service_id, job_collection_id)
+        self._create_job_collection()
 
         # Act
-        result = self.ss.delete_job_collection(
-            self.cloud_service_id, job_collection_id)
+        result = self.ss.delete_job_collection(self.service_id, self.coll_id)
+        self.ss.wait_for_operation_status(result.request_id)
 
         # Assert
-        self.assertIsNone(result)
+        self.assertIsNotNone(result)
 
     def test_get_job_collection(self):
         # Arrange
         self._create_cloud_service()
-        job_collection_id = getUniqueName('job_collection_')
-        self.ss.create_job_collection(self.cloud_service_id, job_collection_id)
+        self._create_job_collection()
 
         # Act
-        result = self.ss.get_job_collection(
-            self.cloud_service_id, job_collection_id)
+        result = self.ss.get_job_collection(self.service_id, self.coll_id)
 
         # Assert
         self.assertIsNotNone(result)
-        self.assertEqual(result.name, job_collection_id)
+        self.assertEqual(result.name, self.coll_id)
 
     def test_create_job(self):
         # Arrange
-        job_collection_id = getUniqueName('job_collection_')
         self._create_cloud_service()
-        self.ss.create_job_collection(self.cloud_service_id, job_collection_id)
+        self._create_job_collection()
 
         # Act
         job = self._create_job_dict()
-
         result = self.ss.create_job(
-            self.cloud_service_id, job_collection_id, "job_id", job)
+            self.service_id,
+            self.coll_id,
+            self.job_id,
+            job,
+        )
+        self.ss.wait_for_operation_status(result.request_id)
 
         # Assert
-        self.assertIsNone(result)
+        self.assertIsNotNone(result)
 
     def test_delete_job(self):
         # Arrange
-        job_collection_id = getUniqueName('job_collection_')
         self._create_cloud_service()
-        self.ss.create_job_collection(self.cloud_service_id, job_collection_id)
-        self.ss.create_job(
-            self.cloud_service_id,
-            job_collection_id,
-            "job_id",
-            self._create_job_dict()
-        )
+        self._create_job_collection()
+        self._create_job()
 
         # Act
-        result = self.ss.delete_job(
-            self.cloud_service_id, job_collection_id, "job_id")
+        result = self.ss.delete_job(self.service_id, self.coll_id, self.job_id)
+        self.ss.wait_for_operation_status(result.request_id)
 
         # Assert
-        self.assertIsNone(result)
+        self.assertIsNotNone(result)
 
     def test_get_job(self):
-        job_collection_id = getUniqueName('job_collection_')
         self._create_cloud_service()
-        self.ss.create_job_collection(self.cloud_service_id, job_collection_id)
-        self.ss.create_job(
-            self.cloud_service_id,
-            job_collection_id,
-            "job_id",
-            self._create_job_dict()
-        )
+        self._create_job_collection()
+        self._create_job()
 
         # Act
-        result = self.ss.get_job(
-            self.cloud_service_id,
-            job_collection_id,
-            "job_id"
-        )
+        result = self.ss.get_job(self.service_id, self.coll_id, self.job_id)
+
         # Assert
         self.assertIsNotNone(result)
         self.assertEqual(result["state"], "enabled")
 
     def test_get_all_jobs(self):
-        job_collection_id = getUniqueName('job_collection_')
         self._create_cloud_service()
-        self.ss.create_job_collection(self.cloud_service_id, job_collection_id)
-        self.ss.create_job(
-            self.cloud_service_id,
-            job_collection_id,
-            "job_id",
-            self._create_job_dict()
-        )
+        self._create_job_collection()
+        self._create_job()
 
         # Act
-        result = self.ss.get_all_jobs(
-            self.cloud_service_id,
-            job_collection_id
-        )
+        result = self.ss.get_all_jobs(self.service_id, self.coll_id)
+
         # Assert
         self.assertIsNotNone(result)
         self.assertEqual(len(result), 1)
