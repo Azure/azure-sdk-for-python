7fa132c8ad9fe905069ec11a8c80cf84bb38b382
huguesv <huvalo@microsoft.com>
2015-08-27 14:46:06 -0700

    Update version to rc2.

--- azure-common/azure/common/__init__.py
+++ azure-common/azure/common/__init__.py
@@ -14,7 +14,7 @@
 #--------------------------------------------------------------------------
 
 __author__ = 'Microsoft Corp. <ptvshelp@microsoft.com>'
-__version__ = '1.0.0rc1'
+__version__ = '1.0.0rc2'
 
 
 class AzureException(Exception):
--- azure-mgmt-compute/azure/mgmt/compute/__init__.py
+++ azure-mgmt-compute/azure/mgmt/compute/__init__.py
@@ -14,6 +14,6 @@
 #--------------------------------------------------------------------------
 
 __author__ = 'Microsoft Corp. <ptvshelp@microsoft.com>'
-__version__ = '0.20.0rc1'
+__version__ = '0.20.0rc2'
 
 from .computemanagement import *
