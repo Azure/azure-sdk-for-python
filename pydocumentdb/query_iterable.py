a9d2ce723c1207d0bef082dcb4312b0233773624
Rajesh Nagpal <rnagpal@microsoft.com>
2016-01-02 12:44:32 -0800

    Python SDK 1.5.0 release
    Python SDK 1.5.0 release

--- pydocumentdb/query_iterable.py
+++ pydocumentdb/query_iterable.py
@@ -1,8 +1,9 @@
-# Copyright (c) Microsoft Corporation.  All rights reserved.
+﻿# Copyright (c) Microsoft Corporation.  All rights reserved.
 
 """Iterable query results.
 """
 
+import pydocumentdb.base as base
 import pydocumentdb.backoff_retry_utility as backoff_retry_utility
 import pydocumentdb.errors as errors
 import pydocumentdb.http_constants as http_constants
@@ -11,13 +12,13 @@ import pydocumentdb.http_constants as http_constants
 class QueryIterable(object):
     """Represents an iterable object of the query results.
     """
-
+    
     def __init__(self, options, retry_policy, fetch_function):
         """
         :Parameters:
-            - `options`: dict
-            - `retry_policy`: documents.RetryPolicy
-            - `fetch_function`: function
+            - `options`: dict, the request options for the request.
+            - `retry_policy`: documents.RetryPolicy, retry policy to be used
+            - `fetch_function`: function, for executing the feed query
 
         Example of `fetch_function`:
 
@@ -31,6 +32,67 @@ class QueryIterable(object):
         self._continuation = None
         self._has_started = False
         self._retry_policy = retry_policy
+        
+        self._client = None
+        self._current_collection_index = 0
+        self._collection_links = []
+        self._collection_links_length = 0
+
+
+    @classmethod
+    def PartitioningQueryIterable(cls, client, options, retry_policy, database_link, query, partition_key):
+        """
+        :Parameters:
+            - `client`: DocumentClient, instance of document client
+            - `options`: dict, the request options for the request.
+            - `retry_policy`: documents.RetryPolicy, retry policy to be used
+            - `database_link`: str, database self link or ID based link
+            - `query`: str or dict
+            - `partition_key`: str, partition key for the query
+
+        Example of `fetch_function`:
+
+        >>> def result_fn(result):
+        >>>     return result['Databases']
+
+        """
+        
+        # This will call the base constructor(__init__ method above)
+        self = cls(options, retry_policy, None)
+
+        self._query = query
+        self._client = client
+
+        partition_resolver = client.GetPartitionResolver(database_link)
+        
+        if(partition_resolver is None):
+            raise ValueError(client.PartitionResolverErrorMessage)
+        else:
+            self._collection_links = partition_resolver.ResolveForRead(partition_key)
+
+        self._collection_links_length = len(self._collection_links)
+
+        if self._collection_links is None:
+            raise ValueError("_collection_links is None.")
+
+        if self._collection_links_length <= 0:
+            raise ValueError("_collection_links_length is not greater than 0.")
+
+        # Creating the QueryFeed for the first collection
+        path = base.GetPathFromLink(self._collection_links[self._current_collection_index], 'docs')
+        collection_id = base.GetResourceIdOrFullNameFromLink(self._collection_links[self._current_collection_index])
+        
+        self._current_collection_index += 1
+
+        def fetch_fn(options):
+            return client.QueryFeed(path,
+                                    collection_id,
+                                    query,
+                                    options)
+
+        self._fetch_function = fetch_fn
+        
+        return self
 
     def __iter__(self):
         """Makes this class iterable.
@@ -79,6 +141,42 @@ class QueryIterable(object):
         __next__ = next
 
     def fetch_next_block(self):
+        """Fetches the next block of query results.
+
+        :Returns:
+            list of fetched items.
+
+        """
+        # Fetch next block of results by executing the query against the current document collection
+        fetched_items = self.fetch_items()
+
+        # If there are multiple document collections to query for(in case of partitioning), keep looping through each one of them,
+        # creating separate feed queries for each collection and fetching the items
+        while not fetched_items:
+            if self._collection_links and self._current_collection_index < self._collection_links_length:
+                path = base.GetPathFromLink(self._collection_links[self._current_collection_index], 'docs')
+                collection_id = base.GetResourceIdOrFullNameFromLink(self._collection_links[self._current_collection_index])
+                
+                self._continuation = None
+                self._has_started = False
+
+                def fetch_fn(options):
+                    return self._client.QueryFeed(path,
+                                            collection_id,
+                                            self._query,
+                                            options)
+
+                self._fetch_function = fetch_fn
+
+                fetched_items = self.fetch_items()
+                self._current_collection_index += 1
+            else:
+                break
+
+        return fetched_items
+        
+
+    def fetch_items(self):
         """Fetches more items.
 
         :Returns:
---
+++ test/test_partition_resolver.py
@@ -0,0 +1,58 @@
+﻿
+"""Test Partition Resolver.
+"""
+
+class TestPartitionResolver(object):
+    """This is a test PartitionResolver used for resolving the collection(s) based on the partitionKey.
+    """
+
+    def __init__(self, collection_links):
+        """Initializes the TestPartitionResolver with the list of collections participating in partitioning
+
+        :Parameters:
+            - `collection_links`: list, Self Links or ID based Links for the collections participating in partitioning
+        """
+        self.collection_links = collection_links
+    
+    def ResolveForCreate(self, document):
+        """Resolves the collection for creating the document based on the partition key
+
+        :Parameters:
+            - `document`: dict, document resource
+
+        :Returns:
+            str, collection Self link or Name based link which should handle the Create operation
+        """
+        # For this TestPartitionResolver, Id property of the document is the partition key
+        partition_key = document['id']
+        return self._GetDocumentCollection(self._GetHashCode(partition_key))
+
+    def ResolveForRead(self, partition_key):
+        """Resolves the collection for reading/querying the document based on the partition key
+
+        :Parameters:
+            - `partition_key`: str, partition key
+
+        :Returns:
+            str, collection Self link(s) or Name based link(s) which should handle the Read operation
+        """
+        # For Read operations, partitionKey can be None in which case we return all collection links
+        if partition_key is None:
+            return self.collection_links
+        else:
+            return [self._GetDocumentCollection(self._GetHashCode(partition_key))]
+
+    # Calculates the hashCode from the partition key 
+    def _GetHashCode(self, partition_key):
+        return int(partition_key)
+
+    # Gets the Document Collection from the hash code 
+    def _GetDocumentCollection(self, hashCode):
+        return self.collection_links[abs(hashCode) % len(self.collection_links)]
+
+
+        
+
+
+
+
