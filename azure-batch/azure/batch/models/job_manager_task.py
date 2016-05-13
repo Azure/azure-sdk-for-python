c111fba069a70c4cced682a2047783d816365c77
SwaggerToSDK Automation <autorestci@microsoft.com>
2016-05-13 16:09:33 +0000

    Generated from 670b053534498bb231015e2af637b5113068fc61

--- azure-batch/azure/batch/models/job_manager_task.py
+++ azure-batch/azure/batch/models/job_manager_task.py
@@ -54,7 +54,7 @@ class JobManagerTask(Model):
      of the compute node where it runs. If true, no other tasks will run on
      the same compute node for as long as the Job Manager is running. If
      false, other tasks can run simultaneously with the Job Manager on a
-     compute node. (The Job Manager task counts normally against the node’s
+     compute node. (The Job Manager task counts normally against the node's
      concurrent task limit, so this is only relevant if the node allows
      multiple concurrent tasks.)
     :type run_exclusive: bool
