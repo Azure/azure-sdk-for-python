bddebc253c9b19272183a7695b7a745b6082ab35
Laurent Mazuel <lmazuel@microsoft.com>
2016-06-22 13:27:04 -0700

    Setup.py extract version from Autorest generated code

--- azure-mgmt-resource/azure/mgmt/resource/__init__.py
+++ azure-mgmt-resource/azure/mgmt/resource/__init__.py
@@ -3,3 +3,8 @@ from .locks import ManagementLockClient
 from .policy import PolicyClient
 from .resources import ResourceManagementClient
 from .subscriptions import SubscriptionClient
+
+from .version import VERSION
+
+__version__ = VERSION
+
