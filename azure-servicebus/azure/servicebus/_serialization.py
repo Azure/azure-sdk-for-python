97f97ce35dc92bdd5394d983a7b19ec84205898e
Bart Verstrynge <bart.verstrynge@ugentec.com>
2016-06-28 10:28:02 +0200

    Added the exclusion of 'strict-transport-security' in header parsing

--- azure-servicebus/azure/servicebus/_serialization.py
+++ azure-servicebus/azure/servicebus/_serialization.py
@@ -83,7 +83,8 @@ def _create_message(response, service_instance):
                                   'transfer-encoding',
                                   'server',
                                   'location',
-                                  'date']:
+                                  'date'
+                                  'strict-transport-security']:
             if '"' in value:
                 value = value[1:-1]
                 try:
