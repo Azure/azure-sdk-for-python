2c67ccde6bfd237b9db0b41883e81445518e3129
Laurent Mazuel <lmazuel@microsoft.com>
2016-07-14 12:41:43 -0700

    Fix and Test Compute.Container

--- azure-mgmt/tests/test_mgmt_compute.py
+++ azure-mgmt/tests/test_mgmt_compute.py
@@ -475,6 +475,57 @@ class MgmtComputeTest(AzureMgmtTestCase):
         virtual_machine_sizes = list(virtual_machine_sizes)
         self.assertGreater(len(virtual_machine_sizes), 0)
 
+    @record
+    def test_container(self):
+        self.create_resource_group()
+
+        container_name = self.get_resource_name('pycontainer')
+        
+        # https://msdn.microsoft.com/en-us/library/azure/mt711471.aspx
+        async_create = self.compute_client.container_service.create_or_update(
+            self.group_name,
+            container_name,
+            {
+                'location': self.region,
+                "master_profile": {
+                    "count": 1,
+                    "dns_prefix": "MasterPrefixTest"
+                },
+                "agent_pool_profiles": [{
+                    "name": "AgentPool1",
+                    "count": 3,
+                    "vm_size": "Standard_A1",
+                        "dns_prefix": "AgentPrefixTest"
+                }],
+                "linux_profile": {
+                    "admin_username": "acsLinuxAdmin",
+                    "ssh": {
+                       "public_keys": [{
+                            "key_data": "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAlj9UC6+57XWVu0fd6zqXa256EU9EZdoLGE3TqdZqu9fvUvLQOX2G0d5DmFhDCyTmWLQUx3/ONQ9RotYmHGymBIPQcpx43nnxsuihAILcpGZ5NjCj4IOYnmhdULxN4ti7k00S+udqokrRYpmwt0N4NA4VT9cN+7uJDL8Opqa1FYu0CT/RqSW+3aoQ0nfGj11axoxM37FuOMZ/c7mBSxvuI9NsDmcDQOUmPXjlgNlxrLzf6VcjxnJh4AO83zbyLok37mW/C7CuNK4WowjPO1Ix2kqRHRxBrzxYZ9xqZPc8GpFTw/dxJEYdJ3xlitbOoBoDgrL5gSITv6ESlNqjPk6kHQ== azureuser@linuxvm"
+                       }]
+                    }
+                },
+            }
+        )
+        container = async_create.result()
+
+        container = self.compute_client.container_service.get(
+            self.group_name,
+            container.name
+        )
+
+        containers = list(self.compute_client.container_service.list(
+            self.group_name
+        ))
+        self.assertEqual(len(containers), 1)
+
+        async_delete = self.compute_client.container_service.delete(
+            self.group_name,
+            container.name
+        )
+        async_delete.wait()
+
+
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
     unittest.main()
