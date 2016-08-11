0661bc721d2d8a542641c70c1e6ccf0fc6c12aa1
Laurent Mazuel <lmazuel@microsoft.com>
2016-08-11 16:44:37 -0700

    Fix #513 - Add renew lock feature

--- azure-servicebus/tests/test_servicebus_servicebus.py
+++ azure-servicebus/tests/test_servicebus_servicebus.py
@@ -399,6 +399,8 @@ class ServiceBusServiceBusTest(ServiceBusTestCase):
         # Act
         received_msg = self.sbs.receive_queue_message(self.queue_name, False)
         with self.assertRaises(AzureServiceBusPeekLockError):
+            received_msg.renew_lock()
+        with self.assertRaises(AzureServiceBusPeekLockError):
             received_msg.unlock()
 
         # Assert
@@ -453,6 +455,7 @@ class ServiceBusServiceBusTest(ServiceBusTestCase):
 
         # Act
         received_msg = self.sbs.receive_queue_message(self.queue_name, True)
+        received_msg.renew_lock()
         received_msg.unlock()
 
         # Assert
@@ -1267,6 +1270,8 @@ class ServiceBusServiceBusTest(ServiceBusTestCase):
         received_msg = self.sbs.receive_subscription_message(
             self.topic_name, 'MySubscription', False)
         with self.assertRaises(AzureServiceBusPeekLockError):
+            received_msg.renew_lock()
+        with self.assertRaises(AzureServiceBusPeekLockError):
             received_msg.unlock()
 
         # Assert
@@ -1329,6 +1334,7 @@ class ServiceBusServiceBusTest(ServiceBusTestCase):
         # Act
         received_msg = self.sbs.receive_subscription_message(
             self.topic_name, 'MySubscription', True)
+        received_msg.renew_lock()
         received_msg.unlock()
 
         # Assert
