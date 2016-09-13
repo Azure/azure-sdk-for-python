56b5a65838fbbf9d2c5f612df8dc01aa1b49a1de
Laurent Mazuel <lmazuel@microsoft.com>
2016-09-13 22:24:18 +0000

    First generation of EventHub

--- unreleased/azure-mgmt-eventhub/azure/mgmt/eventhub/models/event_hub_resource_paged.py
+++ unreleased/azure-mgmt-eventhub/azure/mgmt/eventhub/models/event_hub_resource_paged.py
@@ -9,10 +9,19 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from .event_hub_management_client import EventHubManagementClient
-from .version import VERSION
+from msrest.paging import Paged
 
-__all__ = ['EventHubManagementClient']
 
-__version__ = VERSION
+class EventHubResourcePaged(Paged):
+    """
+    A paging container for iterating over a list of EventHubResource object
+    """
 
+    _attribute_map = {
+        'next_link': {'key': 'nextLink', 'type': 'str'},
+        'current_page': {'key': 'value', 'type': '[EventHubResource]'}
+    }
+
+    def __init__(self, *args, **kwargs):
+
+        super(EventHubResourcePaged, self).__init__(*args, **kwargs)
