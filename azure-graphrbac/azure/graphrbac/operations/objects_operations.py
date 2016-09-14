faa98953b2d16fff89e176b72e8d45b3e640626a
SwaggerToSDK Automation <autorestci@microsoft.com>
2016-09-14 14:47:25 +0000

    Generated from 2f6c69d5c33608cf3640b5c4275a990c38965dc6

--- azure-graphrbac/azure/graphrbac/operations/objects_operations.py
+++ azure-graphrbac/azure/graphrbac/operations/objects_operations.py
@@ -119,8 +119,14 @@ class ObjectsOperations(object):
                 query_parameters['api-version'] = self._serialize.query("api_version", api_version, 'str')
 
             else:
-                url = next_link
+                url = '/{tenantID}/{nextLink}'
+                path_format_arguments = {
+                    'nextLink': self._serialize.url("next_link", next_link, 'str', skip_quote=True),
+                    'tenantID': self._serialize.url("self.config.tenant_id", self.config.tenant_id, 'str')
+                }
+                url = self._client.format_url(url, **path_format_arguments)
                 query_parameters = {}
+                query_parameters['api-version'] = self._serialize.query("api_version", api_version, 'str')
 
             # Construct headers
             header_parameters = {}
@@ -139,73 +145,6 @@ class ObjectsOperations(object):
             request = self._client.post(url, query_parameters)
             response = self._client.send(
                 request, header_parameters, body_content, **operation_config)
-
-            if response.status_code not in [200]:
-                exp = CloudError(response)
-                exp.request_id = response.headers.get('x-ms-request-id')
-                raise exp
-
-            return response
-
-        # Deserialize response
-        deserialized = models.AADObjectPaged(internal_paging, self._deserialize.dependencies)
-
-        if raw:
-            header_dict = {}
-            client_raw_response = models.AADObjectPaged(internal_paging, self._deserialize.dependencies, header_dict)
-            return client_raw_response
-
-        return deserialized
-
-    def get_objects_by_object_ids_next(
-            self, next_link, custom_headers=None, raw=False, **operation_config):
-        """Gets AD group membership by provided AD object Ids.
-
-        :param next_link: Next link for list operation.
-        :type next_link: str
-        :param dict custom_headers: headers that will be added to the request
-        :param bool raw: returns the direct response alongside the
-         deserialized response
-        :param operation_config: :ref:`Operation configuration
-         overrides<msrest:optionsforoperations>`.
-        :rtype: :class:`AADObjectPaged
-         <azure.graphrbac.models.AADObjectPaged>`
-        """
-        api_version = "1.6-internal"
-
-        def internal_paging(next_link=None, raw=False):
-
-            if not next_link:
-                # Construct URL
-                url = '/{tenantID}/{nextLink}'
-                path_format_arguments = {
-                    'nextLink': self._serialize.url("next_link", next_link, 'str', skip_quote=True),
-                    'tenantID': self._serialize.url("self.config.tenant_id", self.config.tenant_id, 'str')
-                }
-                url = self._client.format_url(url, **path_format_arguments)
-
-                # Construct parameters
-                query_parameters = {}
-                query_parameters['api-version'] = self._serialize.query("api_version", api_version, 'str')
-
-            else:
-                url = next_link
-                query_parameters = {}
-
-            # Construct headers
-            header_parameters = {}
-            header_parameters['Content-Type'] = 'application/json; charset=utf-8'
-            if self.config.generate_client_request_id:
-                header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
-            if custom_headers:
-                header_parameters.update(custom_headers)
-            if self.config.accept_language is not None:
-                header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
-
-            # Construct and send request
-            request = self._client.post(url, query_parameters)
-            response = self._client.send(
-                request, header_parameters, **operation_config)
 
             if response.status_code not in [200]:
                 exp = CloudError(response)
