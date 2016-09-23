093d99b6050928a85211abf4845d24985d1d6862
Laurent Mazuel <lmazuel@microsoft.com>
2016-09-23 18:13:38 +0000

    Add resourceId path to resource spec

--- azure-mgmt-resource/azure/mgmt/resource/resources/models/deployment_operation.py
+++ azure-mgmt-resource/azure/mgmt/resource/resources/models/deployment_operation.py
@@ -15,14 +15,22 @@ from msrest.serialization import Model
 class DeploymentOperation(Model):
     """Deployment operation information.
 
-    :param id: Full deployment operation id.
-    :type id: str
-    :param operation_id: Deployment operation id.
-    :type operation_id: str
+    Variables are only populated by the server, and will be ignored when
+    sending a request.
+
+    :ivar id: Full deployment operation id.
+    :vartype id: str
+    :ivar operation_id: Deployment operation id.
+    :vartype operation_id: str
     :param properties: Deployment properties.
     :type properties: :class:`DeploymentOperationProperties
      <azure.mgmt.resource.resources.models.DeploymentOperationProperties>`
     """ 
+
+    _validation = {
+        'id': {'readonly': True},
+        'operation_id': {'readonly': True},
+    }
 
     _attribute_map = {
         'id': {'key': 'id', 'type': 'str'},
@@ -30,7 +38,7 @@ class DeploymentOperation(Model):
         'properties': {'key': 'properties', 'type': 'DeploymentOperationProperties'},
     }
 
-    def __init__(self, id=None, operation_id=None, properties=None):
-        self.id = id
-        self.operation_id = operation_id
+    def __init__(self, properties=None):
+        self.id = None
+        self.operation_id = None
         self.properties = properties
