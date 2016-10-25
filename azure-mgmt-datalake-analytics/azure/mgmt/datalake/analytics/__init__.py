84edff41af55bf0778272f22e164a0af12a0ed93
Laurent Mazuel <lmazuel@microsoft.com>
2016-10-25 11:09:43 -0700

    azure-mgmt-datalake-analytics packaging

--- azure-mgmt-datalake-analytics/azure/mgmt/datalake/analytics/__init__.py
+++ azure-mgmt-datalake-analytics/azure/mgmt/datalake/analytics/__init__.py
@@ -1,1 +1,8 @@
-﻿__import__('pkg_resources').declare_namespace(__name__)
+﻿from .account import DataLakeAnalyticsAccountManagementClient
+from .catalog import DataLakeAnalyticsCatalogManagementClient
+from .job import DataLakeAnalyticsJobManagementClient
+
+from .version import VERSION
+
+__version__ = VERSION
+
