77135c4f32f99f911bf85e57c0421b629d904382
Laurent Mazuel <lmazuel@microsoft.com>
2016-11-03 17:32:16 +0000

    Generated from d76988cd8ae9dfbfb5441dd34eba662360089314

--- azure-mgmt-containerregistry/azure/mgmt/containerregistry/models/registry_paged.py
+++ azure-mgmt-containerregistry/azure/mgmt/containerregistry/models/registry_paged.py
@@ -9,10 +9,19 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from .container_registry_management_client import ContainerRegistryManagementClient
-from .version import VERSION
+from msrest.paging import Paged
 
-__all__ = ['ContainerRegistryManagementClient']
 
-__version__ = VERSION
+class RegistryPaged(Paged):
+    """
+    A paging container for iterating over a list of Registry object
+    """
 
+    _attribute_map = {
+        'next_link': {'key': 'nextLink', 'type': 'str'},
+        'current_page': {'key': 'value', 'type': '[Registry]'}
+    }
+
+    def __init__(self, *args, **kwargs):
+
+        super(RegistryPaged, self).__init__(*args, **kwargs)
