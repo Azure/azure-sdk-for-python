76b97dce8cefc2104dd9016c6ceb503d1cd92387
SwaggerToSDK Automation <autorestci@microsoft.com>
2016-11-12 08:03:46 +0000

    Generated from fead5550011d48b5eafe761a6434fc9f7e08b13a

--- azure-mgmt-sql/azure/mgmt/sql/models/server_metric.py
+++ azure-mgmt-sql/azure/mgmt/sql/models/server_metric.py
@@ -13,7 +13,7 @@ from msrest.serialization import Model
 
 
 class ServerMetric(Model):
-    """Represents Azure SQL Server metrics.
+    """Represents Azure SQL server metrics.
 
     Variables are only populated by the server, and will be ignored when
     sending a request.
