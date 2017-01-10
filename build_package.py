957d2a2e9d32a0608a562c6698f66950755a89e4
Laurent Mazuel <lmazuel@microsoft.com>
2017-01-09 17:57:22 -0800

    Create the build_package.py script

---
+++ build_package.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+#--------------------------------------------------------------------------
+
+import argparse
+import os
+from subprocess import check_call
+
+DEFAULT_DEST_FOLDER = "./dist"
+
+def create_package(name, dest_folder=DEFAULT_DEST_FOLDER):
+    os.chdir(name) # Will raise if not exists
+    check_call(['python', 'setup.py', 'bdist_wheel', '-d', dest_folder])
+    check_call(['python', 'setup.py', "sdist", "--format", "zip", '-d', dest_folder])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Build Azure package.')
+    parser.add_argument('name', help='The package name')
+    parser.add_argument('--dest', '-d', default=DEFAULT_DEST_FOLDER,
+                        help='Destination folder. Relative to the package dir. [default: %(default)s]')
+
+    args = parser.parse_args()
+    create_package(args.name, args.dest)
