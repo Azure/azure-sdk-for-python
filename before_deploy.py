4ee47cd0ad6d766b830f584af1bf10e97b914a53
Laurent Mazuel <lmazuel@microsoft.com>
2017-01-12 14:49:18 -0800

    Improve deploy

--- before_deploy.py
+++ before_deploy.py
@@ -17,9 +17,9 @@ if not travis_tag:
     print("TRAVIS_TAG environment variable is not present")
     sys.exit()
 
-matching = re.match(r"^(?P<name>azure[a-z\-]*) (?P<version>[.0-9]+[rc0-9]*)$", travis_tag)
+matching = re.match(r"^(?P<name>azure[a-z\-]*)_(?P<version>[.0-9]+[rc0-9]*)$", travis_tag)
 if not matching:
-    print("TRAVIS_TAG is not '<package_name> <version>'")
+    print("TRAVIS_TAG is not '<package_name> <version>' (tag is: {})".format(travis_tag))
     sys.exit()
 
 name, version = matching.groups()
