c07a841498cc0142d38f3f1197abd19872beba31
Laurent Mazuel <lmazuel@microsoft.com>
2017-01-31 20:16:21 +0000

    Generated from 870f542162609f7a91f0eee14cf10f02a7d12e73

--- azure-mgmt-web/azure/mgmt/web/models/backup_schedule.py
+++ azure-mgmt-web/azure/mgmt/web/models/backup_schedule.py
@@ -21,19 +21,20 @@ class BackupSchedule(Model):
 
     :param frequency_interval: How often should be the backup executed (e.g.
      for weekly backup, this should be set to 7 and FrequencyUnit should be set
-     to Day)
+     to Day). Default value: 7 .
     :type frequency_interval: int
-    :param frequency_unit: How often should be the backup executed (e.g. for
-     weekly backup, this should be set to Day and FrequencyInterval should be
-     set to 7). Possible values include: 'Day', 'Hour'
+    :param frequency_unit: The unit of time for how often should be the backup
+     executed (e.g. for weekly backup, this should be set to Day and
+     FrequencyInterval should be set to 7). Possible values include: 'Day',
+     'Hour'. Default value: "Day" .
     :type frequency_unit: str or :class:`FrequencyUnit
      <azure.mgmt.web.models.FrequencyUnit>`
     :param keep_at_least_one_backup: True if the retention policy should
      always keep at least one backup in the storage account, regardless how old
-     it is; false otherwise.
+     it is; false otherwise. Default value: True .
     :type keep_at_least_one_backup: bool
     :param retention_period_in_days: After how many days backups should be
-     deleted.
+     deleted. Default value: 90 .
     :type retention_period_in_days: int
     :param start_time: When the schedule should start working.
     :type start_time: datetime
@@ -58,7 +59,7 @@ class BackupSchedule(Model):
         'last_execution_time': {'key': 'lastExecutionTime', 'type': 'iso-8601'},
     }
 
-    def __init__(self, frequency_interval, frequency_unit, keep_at_least_one_backup, retention_period_in_days, start_time=None):
+    def __init__(self, frequency_interval=7, frequency_unit="Day", keep_at_least_one_backup=True, retention_period_in_days=90, start_time=None):
         self.frequency_interval = frequency_interval
         self.frequency_unit = frequency_unit
         self.keep_at_least_one_backup = keep_at_least_one_backup
