502b2496650be1316c0141b0d6e6fb7607d73016
Laurent Mazuel <lmazuel@microsoft.com>
2017-03-30 11:59:22 -0700

    New version of bdist_wheel

--- azure-mgmt-compute/azure_wheel.py
+++ azure-mgmt-compute/azure_wheel.py
@@ -8,19 +8,45 @@ from distutils import log as logger
 import os.path
 try:
     from wheel.bdist_wheel import bdist_wheel as original_bdist_wheel
-    class bdist_wheel(original_bdist_wheel):
+    class azure_bdist_wheel(original_bdist_wheel):
+
+        description = "Create an Azure wheel distribution"
+
+        user_options = original_bdist_wheel.user_options + \
+            [('azure-namespace-package=', None,
+              "Name of the deepest nspkg used")]
+
+        def initialize_options(self):
+            original_bdist_wheel.initialize_options(self)
+            self.azure_namespace_package = None
+
+        def finalize_options(self):
+            original_bdist_wheel.finalize_options(self)
+            if self.azure_namespace_package and not self.azure_namespace_package.endswith("-nspkg"):
+                raise ValueError("azure_namespace_package must finish by -nspkg")
+
+        def run(self):
+            self.distribution.install_requires.append(
+                "{}>=2.0.0".format(self.azure_namespace_package))
+            original_bdist_wheel.run(self)
+
         def write_record(self, bdist_dir, distinfo_dir):
             # following check could be improved, by parsing the package name
             # package_name = self.distribution.get_name()
-            possible_init_py = ['azure-', 'azure-mgmt-', 'azure-mgmt-datalake-']
-            for azure_sub_package in possible_init_py:
-                init_file = os.path.join(bdist_dir, azure_sub_package.replace('-','/'), '__init__.py')
+            if self.azure_namespace_package:
+                # Split and remove last part, assuming it's "nspkg"
+                subparts = self.azure_namespace_package.split('-')[0:-1]
+            folder_with_init = [os.path.join(*subparts[0:i+1]) for i in range(len(subparts))]
+            for azure_sub_package in folder_with_init:
+                init_file = os.path.join(bdist_dir, azure_sub_package, '__init__.py')
                 if os.path.isfile(init_file):
                     logger.info("manually remove {} while building the wheel".format(init_file))
                     os.remove(init_file)
+                else:
+                    raise ValueError("Unable to find {}. Are you sure of your namespace package?".format(init_file))
             original_bdist_wheel.write_record(self, bdist_dir, distinfo_dir)
     cmdclass = {
-        'bdist_wheel': bdist_wheel,
+        'azure_bdist_wheel': azure_bdist_wheel,
     }
 except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
