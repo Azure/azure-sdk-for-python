53e8c14d774131503dbdefe6528cd1e26adbf30b
Laurent Mazuel <lmazuel@microsoft.com>
2017-04-11 10:20:04 -0700

    Allow Travis to load tests and use azure-storage installed from pip at the same time

--- azure_nosetests.py
+++ azure_nosetests.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-import os.path, nose, glob, sys
+import os.path, nose, glob, sys, pkg_resources
 packages = [os.path.dirname(p) for p in glob.glob('azure*/setup.py')]
 sys.path += packages
+# Declare it manually, because "azure-storage" is probably installed with pip
+pkg_resources.declare_namespace('azure')
 
 nose.main()
