a67d974f81a4126d6003fe616c771e0056b82588
Laurent Mazuel <lmazuel@microsoft.com>
2017-04-20 15:46:44 -0700

    Fix StrPaged bug with local built Autorest

--- azure-mgmt-iothub/azure/mgmt/iothub/operations/iot_hub_resource_operations.py
+++ azure-mgmt-iothub/azure/mgmt/iothub/operations/iot_hub_resource_operations.py
@@ -623,11 +623,11 @@ class IotHubResourceOperations(object):
             return response
 
         # Deserialize response
-        deserialized = models.strPaged(internal_paging, self._deserialize.dependencies)
+        deserialized = models.StrPaged(internal_paging, self._deserialize.dependencies)
 
         if raw:
             header_dict = {}
-            client_raw_response = models.strPaged(internal_paging, self._deserialize.dependencies, header_dict)
+            client_raw_response = models.StrPaged(internal_paging, self._deserialize.dependencies, header_dict)
             return client_raw_response
 
         return deserialized
