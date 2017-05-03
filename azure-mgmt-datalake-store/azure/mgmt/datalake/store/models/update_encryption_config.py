df417e0a62ac9a655d96c6a0d4657b6319fe3301
SwaggerToSDK Automation <autorestci@microsoft.com>
2017-05-03 20:58:58 +0000

    Generated from 156cd442edf6badb940149878cecfba2d3198776

--- azure-mgmt-datalake-store/azure/mgmt/datalake/store/models/update_encryption_config.py
+++ azure-mgmt-datalake-store/azure/mgmt/datalake/store/models/update_encryption_config.py
@@ -12,16 +12,18 @@
 from msrest.serialization import Model
 
 
-class AddDataLakeStoreParameters(Model):
-    """Additional Data Lake Store parameters.
+class UpdateEncryptionConfig(Model):
+    """The encryption configuration used to update a user managed Key Vault key.
 
-    :param suffix: the optional suffix for the Data Lake Store account.
-    :type suffix: str
+    :param key_vault_meta_info: The updated Key Vault key to use in user
+     managed key rotation.
+    :type key_vault_meta_info: :class:`UpdateKeyVaultMetaInfo
+     <azure.mgmt.datalake.store.models.UpdateKeyVaultMetaInfo>`
     """
 
     _attribute_map = {
-        'suffix': {'key': 'properties.suffix', 'type': 'str'},
+        'key_vault_meta_info': {'key': 'keyVaultMetaInfo', 'type': 'UpdateKeyVaultMetaInfo'},
     }
 
-    def __init__(self, suffix=None):
-        self.suffix = suffix
+    def __init__(self, key_vault_meta_info=None):
+        self.key_vault_meta_info = key_vault_meta_info
