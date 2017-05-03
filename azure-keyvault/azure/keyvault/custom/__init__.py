ee5d6303099d9baff8c6b06cbb4b9cfd67c50146
Scott Schaab <sschaab@microsoft.com>
2017-05-02 17:29:59 -0700

    removing classes from ./custom __init__.py to avoid confusion from exposing in two namespaces

--- azure-keyvault/azure/keyvault/custom/__init__.py
+++ azure-keyvault/azure/keyvault/custom/__init__.py
@@ -3,15 +3,9 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # ---------------------------------------------------------------------------------------------
 
-from azure.keyvault import http_bearer_challenge_cache as HttpBearerChallengeCache
-from .http_bearer_challenge import HttpBearerChallenge
-from .key_vault_authentication import KeyVaultAuthentication
 from ..version import VERSION
 
 __all__ = [
-    'HttpBearerChallengeCache',
-    'HttpBearerChallenge',
-    'KeyVaultAuthentication'
 ]
 
 __version__ = VERSION
