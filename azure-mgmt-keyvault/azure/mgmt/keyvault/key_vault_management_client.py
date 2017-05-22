8ec20cf899473c80644bdd6935cba1241f63a4c5
Laurent Mazuel <lmazuel@microsoft.com>
2017-05-22 21:39:39 +0000

    Generated from 4284d7e6519882405013bc70a306fe460f0dd54d

--- azure-mgmt-keyvault/azure/mgmt/keyvault/key_vault_management_client.py
+++ azure-mgmt-keyvault/azure/mgmt/keyvault/key_vault_management_client.py
@@ -60,7 +60,7 @@ class KeyVaultManagementClient(object):
     :vartype config: KeyVaultManagementClientConfiguration
 
     :ivar vaults: Vaults operations
-    :vartype vaults: .operations.VaultsOperations
+    :vartype vaults: azure.mgmt.keyvault.operations.VaultsOperations
 
     :param credentials: Credentials needed for the client to connect to Azure.
     :type credentials: :mod:`A msrestazure Credentials
@@ -79,7 +79,7 @@ class KeyVaultManagementClient(object):
         self._client = ServiceClient(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self.api_version = '2015-06-01'
+        self.api_version = '2016-10-01'
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
