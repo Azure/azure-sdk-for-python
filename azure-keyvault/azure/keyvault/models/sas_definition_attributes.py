8322d5eaaaf31693d5d3de554dee353152556add
SwaggerToSDK Automation <autorestci@microsoft.com>
2017-05-24 23:19:52 +0000

    Generated from f1c9d6628d71fcc2be98c9064698eb3f98287ba4

--- azure-keyvault/azure/keyvault/models/sas_definition_attributes.py
+++ azure-keyvault/azure/keyvault/models/sas_definition_attributes.py
@@ -12,13 +12,13 @@
 from msrest.serialization import Model
 
 
-class IssuerAttributes(Model):
-    """The attributes of an issuer managed by the Key Vault service.
+class SasDefinitionAttributes(Model):
+    """The SAS definition management attributes.
 
     Variables are only populated by the server, and will be ignored when
     sending a request.
 
-    :param enabled: Determines whether the issuer is enabled.
+    :param enabled: the enabled state of the object.
     :type enabled: bool
     :ivar created: Creation time in UTC.
     :vartype created: datetime
