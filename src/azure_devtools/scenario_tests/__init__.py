b90edaf69f4f9af34809e26bd770ca190511ca1a
Ian McCowan <v-iam@microsoft.com>
2017-06-05 09:19:20 -0700

    Remove checkers

--- src/azure_devtools/scenario_tests/__init__.py
+++ src/azure_devtools/scenario_tests/__init__.py
@@ -5,10 +5,8 @@
 
 from .base import ScenarioTest, LiveTest
 from .exceptions import AzureTestError
-from .checkers import JMESPathCheck, JMESPathCheckExists, NoneCheck, StringCheck, StringContainCheck
 from .decorators import live_only, record_only
 from .utilities import get_sha1_hash
 
-__all__ = ['ScenarioTest', 'LiveTest', 'AzureTestError', 'JMESPathCheck', 'JMESPathCheckExists', 'NoneCheck',
-           'live_only', 'record_only', 'StringCheck', 'StringContainCheck', 'get_sha1_hash']
+__all__ = ['ScenarioTest', 'LiveTest', 'AzureTestError', 'live_only', 'record_only', 'get_sha1_hash']
 __version__ = '0.1.0+dev'
