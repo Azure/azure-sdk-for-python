c67f4b1050273ee1c69cbd245d523431dfc380ae
Ian McCowan <v-iam@microsoft.com>
2017-06-06 09:52:06 -0700

    Remove unnecessary import

--- src/azure_devtools/scenario_tests/patches.py
+++ src/azure_devtools/scenario_tests/patches.py
@@ -4,7 +4,6 @@
 # --------------------------------------------------------------------------------------------
 
 from .exceptions import AzureTestError
-from .const import MOCKED_SUBSCRIPTION_ID, MOCKED_TENANT_ID
 
 
 def patch_time_sleep_api(unit_test):
