7ccce1a0eec178d816beaf2c299cd50a9c150935
Ian McCowan <v-iam@microsoft.com>
2017-06-06 09:52:55 -0700

    Expose most everything in top-level namespace

--- src/azure_devtools/scenario_tests/__init__.py
+++ src/azure_devtools/scenario_tests/__init__.py
@@ -3,11 +3,25 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from .base import ScenarioTest, LiveTest
+from .base import IntegrationTestBase, ScenarioTest, LiveTest
 from .exceptions import AzureTestError
 from .decorators import live_only, record_only
-from .utilities import get_sha1_hash
+from .patches import patch_time_sleep_api, patch_long_run_operation_delay
+from .preparers import AbstractPreparer, SingleValueReplacer
+from .recording_processors import (
+    RecordingProcessor, SubscriptionRecordingProcessor,
+    LargeRequestBodyProcessor, LargeResponseBodyProcessor, LargeResponseBodyReplacer,
+    OAuthRequestResponsesFilter, DeploymentNameReplacer, GeneralNameReplacer,
+)
+from .utilities import create_random_name, get_sha1_hash
 
-__all__ = ['ScenarioTest', 'LiveTest', 'AzureTestError', 'get_sha1_hash',
-           'live_only', 'record_only']
+__all__ = ['IntegrationTestBase', 'ScenarioTest', 'LiveTest',
+           'AzureTestError',
+           'patch_time_sleep_api', 'patch_long_run_operation_delay',
+           'AbstractPreparer', 'SingleValueReplacer',
+           'RecordingProcessor', 'SubscriptionRecordingProcessor',
+           'LargeRequestBodyProcessor', 'LargeResponseBodyProcessor', 'LargeResponseBodyReplacer',
+           'OAuthRequestResponsesFilter', 'DeploymentNameReplacer', 'GeneralNameReplacer',
+           'live_only', 'record_only',
+           'create_random_name', 'get_sha1_hash']
 __version__ = '0.1.0+dev'
