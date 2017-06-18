863f9ee3dc43541d3c702ffa46217ef90ad385df
Troy Dai <troy.dai@outlook.com>
2017-06-18 12:29:37 -0700

    Add test covering get_sha1_hash

--- src/azure_devtools/scenario_tests/tests/test_utilities.py
+++ src/azure_devtools/scenario_tests/tests/test_utilities.py
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------------------------
 
 import unittest
-from azure_devtools.scenario_tests.utilities import create_random_name
+from azure_devtools.scenario_tests.utilities import create_random_name, get_sha1_hash
 
 
 class TestUtilityFunctions(unittest.TestCase):
@@ -41,4 +41,42 @@ class TestUtilityFunctions(unittest.TestCase):
             self.assertEqual(str(cm.exception), 'The randomized part of the name is shorter than 4, which may not be '
                                                 'able to offer enough randomness')
 
+    def test_get_sha1_hash(self):
+        import tempfile
+        with tempfile.NamedTemporaryFile() as f:
+            content = b"""
+All the world's a stage,
+And all the men and women merely players;
+They have their exits and their entrances,
+And one man in his time plays many parts,
+His acts being seven ages. At first, the infant,
+Mewling and puking in the nurse's arms.
+Then the whining schoolboy, with his satchel
+And shining morning face, creeping like snail
+Unwillingly to school. And then the lover,
+Sighing like furnace, with a woeful ballad
+Made to his mistress' eyebrow. Then a soldier,
+Full of strange oaths and bearded like the pard,
+Jealous in honor, sudden and quick in quarrel,
+Seeking the bubble reputation
+Even in the cannon's mouth. And then the justice,
+In fair round belly with good capon lined,
+With eyes severe and beard of formal cut,
+Full of wise saws and modern instances;
+And so he plays his part. The sixth age shifts
+Into the lean and slippered pantaloon,
+With spectacles on nose and pouch on side;
+His youthful hose, well saved, a world too wide
+For his shrunk shank, and his big manly voice,
+Turning again toward childish treble, pipes
+And whistles in his sound. Last scene of all,
+That ends this strange eventful history,
+Is second childishness and mere oblivion,
+Sans teeth, sans eyes, sans taste, sans everything. 
 
+William Shakespeare
+            """
+            f.write(content)
+            f.seek(0)
+            hash_value = get_sha1_hash(f.name)
+            self.assertEqual('6487bbdbd848686338d729e6076da1a795d1ae747642bf906469c6ccd9e642f9', hash_value)
