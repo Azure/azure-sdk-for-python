52a8204e444d4b0847979bac79c162c6e4c60d51
Scott Schaab <sschaab@microsoft.com>
2017-06-20 14:07:50 -0700

    adding KeyVaultIdentifier and object specific id classes

--- azure-keyvault/azure/keyvault/custom/key_vault_id.py
+++ azure-keyvault/azure/keyvault/custom/key_vault_id.py
@@ -219,6 +219,117 @@ class KeyVaultId(object):
         return KeyVaultId.parse_object_id(KeyVaultCollectionType.certificate_issuers.value, id)
 
 
+class KeyVaultIdentifier(object):
+    _id_format = '{vault}/{collection}/{name}/{version?}'
+    _validation = {}
+    version_none = ''
+
+    def __init__(self, uri=None, **kwargs):
+        self.version = KeyVaultIdentifier.version_none
+
+        for key, value in self._validation.items():
+            self.__dict__[key] = value
+
+        for key, value in kwargs.items():
+            self.__dict__[key] = value
+        if uri:
+            self._parse(uri)
+
+    @property
+    def id(self):
+        return self._format()
+
+    @property
+    def base_id(self):
+        return self._format(fmt=self._id_format.replace('/{version?}', ''))
+
+    def _format(self, fmt=None):
+        fmt = fmt or self._id_format
+        segments = []
+
+        for fmt_seg in fmt.split('/'):
+            if fmt_seg.startswith('{') and fmt_seg.endswith('}'):
+                fmt_seg = fmt_seg[1:-1]
+                fmt_prop = fmt_seg.rstrip('?')
+                seg_val = getattr(self, fmt_prop)
+                if not seg_val:
+                    if not fmt_seg.endswith('?'):
+                        raise ValueError('invalid id: No value specified for the required segment "{}"'.format(fmt_prop))
+                else:
+                    segments.append(seg_val.strip('/'))
+            else:
+                segments.append(fmt_seg)
+
+        return '/'.join(segments)
+
+    def _parse(self, uri):
+        def format_error():
+            ValueError('invalid id: The specified uri "{}", does to match the specified format "{}"'.format(uri, self._id_format))
+
+        uri = _validate_string_argument(uri, 'uri')
+        parsed_uri = _parse_uri_argument(uri)
+
+        id_segs = list(filter(None, parsed_uri.path.split('/')))
+        id_segs.insert(0, '{}://{}'.format(parsed_uri.scheme, parsed_uri.hostname))
+
+        fmt_segs = list(filter(None, self._id_format.split('/')))
+
+        for ix in range(len(fmt_segs)):
+            fmt_seg = fmt_segs[ix]
+            id_seg = id_segs[ix] if ix < len(id_segs) else ''
+
+            if fmt_seg.startswith('{') and fmt_seg.endswith('}'):
+                prop = fmt_seg[1:-1]
+                if not id_seg and not prop.endswith('?'):
+                    raise format_error()
+                prop = prop.rstrip('?')
+                if prop in self._validation and self._validation[prop] != id_seg:
+                    raise ValueError('invalid id: The {} "{}" does not match the expected "{}"'.format(prop, id_seg, self._validation[prop]))
+                self.__dict__[prop] = id_seg
+            else:
+                if not fmt_seg == id_seg:
+                    raise format_error()
+
+
+    def _build(self, **kwargs):
+        pass
+
+
+class KeyId(KeyVaultIdentifier):
+    _id_format = '{vault}/{collection}/{name}/{version?}'
+    _validation = {'collection': 'keys'}
+
+
+class SecretId(KeyVaultIdentifier):
+    _id_format = '{vault}/{collection}/{name}/{version?}'
+    _validation = {'collection': 'secrets'}
+
+
+class CertificateId(KeyVaultIdentifier):
+    _id_format = '{vault}/{collection}/{name}/{version?}'
+    _validation = {'collection': 'certificates'}
+
+
+class CertificateOperationId(KeyVaultIdentifier):
+    _id_format = '{vault}/{collection}/{name}/pending'
+    _validation = {'collection': 'certificates'}
+
+
+class CertificateIssuerId(KeyVaultIdentifier):
+    _id_format = '{vault}/{collection}/issuers/{name}'
+    _validation = {'collection': 'certificates'}
+
+
+class StorageAccountId(KeyVaultIdentifier):
+    _id_format = '{vault}/{collection}/{name}'
+    _validation = {'collection': 'storage'}
+
+
+class StorageSasDefinitionId(KeyVaultIdentifier):
+    _id_format = '{vault}/{collection}/{account_name}/sas/{sas_definition}'
+    _validation = {'collection': 'storage'}
+
+
 def _validate_string_argument(prop, name, nullable=False):
     try:
         prop = prop.strip()
