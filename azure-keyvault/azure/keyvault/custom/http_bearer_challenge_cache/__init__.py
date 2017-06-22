0b9f4684127f4ff49d1d4d720f22c1a6a0c9f286
Scott Schaab <sschaab@microsoft.com>
2017-06-22 12:57:11 -0700

    updating KeyVaultAuthBase to reuse original request connection

--- azure-keyvault/azure/keyvault/custom/http_bearer_challenge_cache/__init__.py
+++ azure-keyvault/azure/keyvault/custom/http_bearer_challenge_cache/__init__.py
@@ -3,12 +3,15 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 #---------------------------------------------------------------------------------------------
 
+from threading import Lock
+
 try:
     import urllib.parse as parse
 except ImportError:
     import urlparse as parse # pylint: disable=import-error
 
 _cache = {}
+_lock = Lock()
 
 def get_challenge_for_url(url):
     """ Gets the challenge for the cached URL.
@@ -18,7 +21,15 @@ def get_challenge_for_url(url):
         raise ValueError('URL cannot be None')
 
     url = parse.urlparse(url)
-    return _cache.get(url.netloc)
+
+    _lock.acquire()
+
+    val = _cache.get(url.netloc)
+
+    _lock.release()
+
+    return val
+
 
 def remove_challenge_for_url(url):
     """ Removes the cached challenge for the specified URL.
@@ -27,7 +38,13 @@ def remove_challenge_for_url(url):
         raise ValueError('URL cannot be empty')
 
     url = parse.urlparse(url)
+
+    _lock.acquire()
+
     del _cache[url.netloc]
+
+    _lock.release()
+
 
 def set_challenge_for_url(url, challenge):
     """ Caches the challenge for the specified URL.
@@ -43,8 +60,18 @@ def set_challenge_for_url(url, challenge):
     if src_url.netloc != challenge.source_authority:
         raise ValueError('Source URL and Challenge URL do not match')
 
+    _lock.acquire()
+
     _cache[src_url.netloc] = challenge
+
+    _lock.release()
+
 
 def clear():
     """ Clears the cache. """
+
+    _lock.acquire()
+
     _cache.clear() # pylint: disable=redefined-outer-name,unused-variable
+
+    _lock.release()
