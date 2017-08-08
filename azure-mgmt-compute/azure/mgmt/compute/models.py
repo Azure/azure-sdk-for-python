1b312a25b47be9d4e9adf7f88a3b1fd65466211c
Laurent Mazuel <laurent.mazuel@gmail.com>
2017-08-08 14:52:35 -0700

    Remove ACS, simplify compute hiearchy

--- azure-mgmt-compute/azure/mgmt/compute/models.py
+++ azure-mgmt-compute/azure/mgmt/compute/models.py
@@ -4,4 +4,4 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from .compute.models import *
+from .v2017_03_30.models import *
