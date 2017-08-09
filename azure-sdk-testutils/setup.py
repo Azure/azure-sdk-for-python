7e532ed3ba4420e40c9c387750a805b57e70870b
Laurent Mazuel <laurent.mazuel@gmail.com>
2017-08-09 12:09:13 -0700

    Executable azure-sdk-testutils/setup.py file

--- azure-sdk-testutils/setup.py
+++ azure-sdk-testutils/setup.py
@@ -1,1 +1,11 @@
-# Blank setup.py so this directory will be added to PYTHONPATH by azure_nosetests.py
+import os
+from setuptools import setup, find_packages
+
+# This is a "fake" package, meaning it's not supposed to be released but used
+# locally with "pip install -e"
+
+setup(
+    name = "azure-sdk-testutils",
+    version = "0.0.0",
+    packages=find_packages(),
+)
