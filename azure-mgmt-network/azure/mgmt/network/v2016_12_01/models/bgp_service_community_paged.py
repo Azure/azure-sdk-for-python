bb7031b867a66b8cf9a650882cb1758ff66b568e
SwaggerToSDK Automation <autorestci@microsoft.com>
2017-08-16 18:26:57 +0000

    Generated from da940d51435e75535994f1c215b9504349af6d24

--- azure-mgmt-network/azure/mgmt/network/v2016_12_01/models/bgp_service_community_paged.py
+++ azure-mgmt-network/azure/mgmt/network/v2016_12_01/models/bgp_service_community_paged.py
@@ -14,7 +14,7 @@ from msrest.paging import Paged
 
 class BgpServiceCommunityPaged(Paged):
     """
-    A paging container for iterating over a list of BgpServiceCommunity object
+    A paging container for iterating over a list of :class:`BgpServiceCommunity <azure.mgmt.network.v2016_12_01.models.BgpServiceCommunity>` object
     """
 
     _attribute_map = {
