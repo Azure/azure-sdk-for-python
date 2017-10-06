2032f15820b345332dad38f5301acb6c6810ba50
SwaggerToSDK Automation <azpysdk@microsoft.com>
2017-10-06 22:06:40 +0000

    Generated from ed9f9e46a4686b481c37a88a3ef88a43742bc10f

--- azure-mgmt-resource/azure/mgmt/resource/policy/v2017_06_01_preview/operations/policy_definitions_operations.py
+++ azure-mgmt-resource/azure/mgmt/resource/policy/v2017_06_01_preview/operations/policy_definitions_operations.py
@@ -268,7 +268,7 @@ class PolicyDefinitionsOperations(object):
         request = self._client.get(url, query_parameters)
         response = self._client.send(request, header_parameters, **operation_config)
 
-        if response.status_code not in [200, 404]:
+        if response.status_code not in [200]:
             exp = CloudError(response)
             exp.request_id = response.headers.get('x-ms-request-id')
             raise exp
