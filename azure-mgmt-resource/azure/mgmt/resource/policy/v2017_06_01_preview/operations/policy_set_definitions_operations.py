2cdb752f4b77825ff3b6845d807b4c203d4a8272
SwaggerToSDK Automation <azpysdk@microsoft.com>
2017-10-06 18:30:23 +0000

    Generated from 07e3b7dabf4d8587930915a7cf926b7bbffd9818

--- azure-mgmt-resource/azure/mgmt/resource/policy/v2017_06_01_preview/operations/policy_set_definitions_operations.py
+++ azure-mgmt-resource/azure/mgmt/resource/policy/v2017_06_01_preview/operations/policy_set_definitions_operations.py
@@ -583,7 +583,7 @@ class PolicySetDefinitionsOperations(object):
         request = self._client.get(url, query_parameters)
         response = self._client.send(request, header_parameters, **operation_config)
 
-        if response.status_code not in [200]:
+        if response.status_code not in [200, 404]:
             raise models.ErrorResponseException(self._deserialize, response)
 
         deserialized = None
