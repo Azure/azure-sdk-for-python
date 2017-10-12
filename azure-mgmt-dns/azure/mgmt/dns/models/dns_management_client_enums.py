ff6e9182592da485bd7fbcecd1805b30629859f2
SwaggerToSDK Automation <azpysdk@microsoft.com>
2017-10-12 17:56:36 +0000

    Generated from 11d3fee4d0450f3b2a0a1f2943ce8b21d22edb6a

--- azure-mgmt-dns/azure/mgmt/dns/models/dns_management_client_enums.py
+++ azure-mgmt-dns/azure/mgmt/dns/models/dns_management_client_enums.py
@@ -12,68 +12,11 @@
 from enum import Enum
 
 
-class OperationStatus(Enum):
-
-    in_progress = "InProgress"
-    succeeded = "Succeeded"
-    failed = "Failed"
-
-
-class HttpStatusCode(Enum):
-
-    continue_enum = "Continue"
-    switching_protocols = "SwitchingProtocols"
-    ok = "OK"
-    created = "Created"
-    accepted = "Accepted"
-    non_authoritative_information = "NonAuthoritativeInformation"
-    no_content = "NoContent"
-    reset_content = "ResetContent"
-    partial_content = "PartialContent"
-    multiple_choices = "MultipleChoices"
-    ambiguous = "Ambiguous"
-    moved_permanently = "MovedPermanently"
-    moved = "Moved"
-    found = "Found"
-    redirect = "Redirect"
-    see_other = "SeeOther"
-    redirect_method = "RedirectMethod"
-    not_modified = "NotModified"
-    use_proxy = "UseProxy"
-    unused = "Unused"
-    temporary_redirect = "TemporaryRedirect"
-    redirect_keep_verb = "RedirectKeepVerb"
-    bad_request = "BadRequest"
-    unauthorized = "Unauthorized"
-    payment_required = "PaymentRequired"
-    forbidden = "Forbidden"
-    not_found = "NotFound"
-    method_not_allowed = "MethodNotAllowed"
-    not_acceptable = "NotAcceptable"
-    proxy_authentication_required = "ProxyAuthenticationRequired"
-    request_timeout = "RequestTimeout"
-    conflict = "Conflict"
-    gone = "Gone"
-    length_required = "LengthRequired"
-    precondition_failed = "PreconditionFailed"
-    request_entity_too_large = "RequestEntityTooLarge"
-    request_uri_too_long = "RequestUriTooLong"
-    unsupported_media_type = "UnsupportedMediaType"
-    requested_range_not_satisfiable = "RequestedRangeNotSatisfiable"
-    expectation_failed = "ExpectationFailed"
-    upgrade_required = "UpgradeRequired"
-    internal_server_error = "InternalServerError"
-    not_implemented = "NotImplemented"
-    bad_gateway = "BadGateway"
-    service_unavailable = "ServiceUnavailable"
-    gateway_timeout = "GatewayTimeout"
-    http_version_not_supported = "HttpVersionNotSupported"
-
-
 class RecordType(Enum):
 
     a = "A"
     aaaa = "AAAA"
+    caa = "CAA"
     cname = "CNAME"
     mx = "MX"
     ns = "NS"
