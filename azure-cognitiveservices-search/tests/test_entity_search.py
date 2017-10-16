b2328b510f668a0566a365135ed9f86dbb8dc0b0
Laurent Mazuel <lmazuel@microsoft.com>
2017-10-16 16:01:56 -0700

    Test EntitySearch

--- azure-cognitiveservices-search/tests/test_entity_search.py
+++ azure-cognitiveservices-search/tests/test_entity_search.py
@@ -9,26 +9,56 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from .generated.entity_search_api import EntitySearchAPI
-from .custom import ApiServicePrincipalCredentials
+from azure.cognitiveservices.search.entitysearch import EntitySearchAPI
+from msrest.authentication import CognitiveServicesAuthentication
+
+from azure_devtools.scenario_tests import ReplayableTest, AzureTestError
+
+from devtools_testutils import mgmt_settings_fake as fake_settings
 
 
-def test_search():
-    query = 'seahawks'
-    market = 'en-us'
+class EntitySearchTest(ReplayableTest):
+    FILTER_HEADERS = ReplayableTest.FILTER_HEADERS + ['Ocp-Apim-Subscription-Key']
 
-    credentials = ApiServicePrincipalCredentials('abf45b6290b34e739e8cc63da460e99b')
-    entity_search_api = EntitySearchAPI(credentials)
-    response = entity_search_api.entities.search(query=query, market=market)
+    def __init__(self, method_name):
+        self._fake_settings, self._real_settings = self._load_settings()
+        super(EntitySearchTest, self).__init__(method_name)
 
-    assert response is not None
-    assert response._type is not None
+    @property
+    def settings(self):
+        if self.is_live:
+            if self._real_settings:
+                return self._real_settings
+            else:
+                raise AzureTestError('Need a mgmt_settings_real.py file to run tests live.')
+        else:
+            return self._fake_settings
 
-    assert response.query_context is not None
-    assert response.query_context.original_query == query
+    def _load_settings(self):
+        try:
+            from devtools_testutils import mgmt_settings_real as real_settings
+            return fake_settings, real_settings
+        except ImportError:
+            return fake_settings, None
 
-    assert response.entities is not None
-    assert response.entities.value is not None
-    assert len(response.entities.value) == 1
-    assert response.entities.value[0].contractual_rules is not None
+    def test_search(self):
+        query = 'seahawks'
+        market = 'en-us'
+
+        credentials = CognitiveServicesAuthentication(
+            self.settings.CS_SUBSCRIPTION_KEY
+        )
+        entity_search_api = EntitySearchAPI(credentials)
+        response = entity_search_api.entities.search(query=query, market=market)
+
+        assert response is not None
+        assert response._type is not None
+
+        assert response.query_context is not None
+        assert response.query_context.original_query == query
+
+        assert response.entities is not None
+        assert response.entities.value is not None
+        assert len(response.entities.value) == 1
+        assert response.entities.value[0].contractual_rules is not None
 
