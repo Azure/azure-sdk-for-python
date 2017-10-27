bee53b5ac45fc8f25b52c8a7dbe273cc8d1a994f
xinchen <xinchen@microsoft.com>
2017-10-27 11:14:56 -0700

    Workaround for stuck async receiver

--- eventhubs/async/__init__.py
+++ eventhubs/async/__init__.py
@@ -17,13 +17,14 @@ class AsyncReceiver(Receiver):
     """
     Implements the async API of a L{Receiver}.
     """
-    def __init__(self):
+    def __init__(self, prefetch=300):
         super(AsyncReceiver, self).__init__(False)
         self.loop = asyncio.get_event_loop()
         self.messages = queue.Queue()
         self.lock = Lock()
         self.link = None
         self.waiter = None
+        self.credit = prefetch
         self.delivered = 0
 
     def on_start(self, link):
@@ -34,19 +35,28 @@ class AsyncReceiver(Receiver):
         """ Handle message received event """
         _message = event.message
         _event = EventData.create(_message)
+        self.offset = _event.offset
         _waiter = None
         with self.lock:
+            self.credit -= 1
             if self.waiter is None:
                 self.messages.put(_event)
+                if self.credit == 0:
+                    event.reactor.schedule(0.1, self)
                 return
             _waiter = self.waiter
             self.waiter = None
             self._on_delivered(1)
-        self.offset = _event.offset
-        self.loop.call_soon_threadsafe(_waiter.set_result, [_event])
+        self.loop.call_soon_threadsafe(_waiter.set_result, None)
 
     def on_event_data(self, event_data):
         pass
+
+    def on_timer_task(self, event):
+        with self.lock:
+            self._on_delivered(0)
+            if self.credit == 0 and not self.messages.empty():
+                event.reactor.schedule(0.1, self)
 
     async def receive(self, count):
         """
@@ -57,21 +67,21 @@ class AsyncReceiver(Receiver):
         """
         _waiter = None
         _list = []
-        with self.lock:
-            _deq = 0
-            while not self.messages.empty() and _deq < count:
-                _list.append(self.messages.get())
-                _deq = _deq + 1
-            if _list:
-                self._on_delivered(_deq)
-                return _list
-            self.waiter = self.loop.create_future()
-            _waiter = self.waiter
-        await _waiter
-        return _waiter.result()
+        while True:
+            with self.lock:
+                while not self.messages.empty() and count > 0:
+                    _list.append(self.messages.get())
+                    count -= 1
+                    self.delivered += 1
+                if _list:
+                    return _list
+                self.waiter = self.loop.create_future()
+                _waiter = self.waiter
+            await _waiter
 
     def _on_delivered(self, count):
         self.delivered = self.delivered + count
         if self.delivered >= 100:
             self.link.flow(self.delivered)
+            self.credit += self.delivered
             self.delivered = 0
