2f945eb36d01ef0925829e0c6f47c74626bd95b5
Sandeep Nuggehalli Lakshminarayana <sandeepn@microsoft.com>
2017-11-13 12:39:56 -0800

    New SDK for Consumption based on new api-version 2017-11-30

--- azure-mgmt-consumption/azure/mgmt/consumption/consumption_management_client.py
+++ azure-mgmt-consumption/azure/mgmt/consumption/consumption_management_client.py
@@ -45,7 +45,7 @@ class ConsumptionManagementClientConfiguration(AzureConfiguration):
 
         super(ConsumptionManagementClientConfiguration, self).__init__(base_url)
 
-        self.add_user_agent('consumptionmanagementclient/{}'.format(VERSION))
+        self.add_user_agent('azure-mgmt-consumption/{}'.format(VERSION))
         self.add_user_agent('Azure-SDK-For-Python')
 
         self.credentials = credentials
@@ -53,7 +53,7 @@ class ConsumptionManagementClientConfiguration(AzureConfiguration):
 
 
 class ConsumptionManagementClient(object):
-    """Consumption management client provides access to consumption resources for Azure Web-Direct subscriptions. Other subscription types which were not purchased directly through the Azure web portal are not supported through this preview API.
+    """Consumption management client provides access to consumption resources for Azure Enterprise Subscriptions.
 
     :ivar config: Configuration for client.
     :vartype config: ConsumptionManagementClientConfiguration
@@ -78,7 +78,7 @@ class ConsumptionManagementClient(object):
         self._client = ServiceClient(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self.api_version = '2017-04-24-preview'
+        self.api_version = '2017-11-30'
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
