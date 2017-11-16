08fb039f90c63eb5838982b9d5c2d0c56c3c361c
Laurent Mazuel <laurent.mazuel@gmail.com>
2017-11-16 10:57:37 -0800

    Split CS Search

--- azure-cognitiveservices-search-websearch/setup.py
+++ azure-cognitiveservices-search-websearch/setup.py
@@ -18,8 +18,8 @@ except ImportError:
     cmdclass = {}
 
 # Change the PACKAGE_NAME only to change folder and different name
-PACKAGE_NAME = "azure-cognitiveservices-search"
-PACKAGE_PPRINT_NAME = "Cognitive Services Search"
+PACKAGE_NAME = "azure-cognitiveservices-search-websearch"
+PACKAGE_PPRINT_NAME = "Cognitive Services WebSearch"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
