c8a62fc2a499917f2ce21884f014199ec9ab1dc5
Hussain Mir <huazam@microsoft.com>
2017-12-13 01:28:40 +0530

    HTTP status codes as described in documentation. (https://docs.microsoft.com/en-us/rest/api/documentdb/http-status-codes-for-documentdb)

--- pydocumentdb/http_constants.py
+++ pydocumentdb/http_constants.py
@@ -266,3 +266,25 @@ class HttpContextProperties:
     """Constants of http context properties.
     """
     SubscriptionId = 'SubscriptionId'
+
+
+class HttpStatusCodes:
+    """HTTP status codes returned by the REST operations
+    """
+    OK = 200
+    CREATED = 201
+    NO_CONTENT = 204
+
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    REQUEST_TIMEOUT = 408
+    CONFLICT = 409
+    PRECONDITION_FAILURE = 412
+    ENTITY_TOO_LARGE = 413
+    TOO_MANY_REQUESTS = 429
+    RETRY_WITH = 449
+
+    INTERNAL_SERVER_ERROR = 500
+    SERVICE_UNAVAILABLE = 503
