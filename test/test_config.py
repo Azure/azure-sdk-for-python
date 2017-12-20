53577e8fd46f609b288287262fb2dc50e59d5e65
Srinath Narayanan <srnara@microsoft.com>
2017-12-20 14:04:33 -0800

    minor change to test_config file

--- test/test_config.py
+++ test/test_config.py
@@ -28,7 +28,7 @@ except:
 
 class _test_config(object):
 
-    host = os.getenv('ACCOUNT_HOST', 'https://localhost:8081')
+    host = os.getenv('ACCOUNT_HOST', 'https://localhost:443')
     masterKey = os.getenv('ACCOUNT_KEY', 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==')
 
     global_host = '[YOUR_GLOBAL_ENDPOINT_HERE]'
