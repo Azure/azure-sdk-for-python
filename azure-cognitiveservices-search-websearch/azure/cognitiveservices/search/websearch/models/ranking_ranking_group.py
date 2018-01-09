841ae37bae8629897dbb90d513ee9ae2915350b9
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-01-09 11:42:14 -0800

    azure-cognitiveservices-search-websearch with Autorest.Python 2.1.34

--- azure-cognitiveservices-search-websearch/azure/cognitiveservices/search/websearch/models/ranking_ranking_group.py
+++ azure-cognitiveservices-search-websearch/azure/cognitiveservices/search/websearch/models/ranking_ranking_group.py
@@ -29,4 +29,5 @@ class RankingRankingGroup(Model):
     }
 
     def __init__(self, items):
+        super(RankingRankingGroup, self).__init__()
         self.items = items
--- azure-cognitiveservices-search-websearch/azure/cognitiveservices/search/websearch/operations/web_operations.py
+++ azure-cognitiveservices-search-websearch/azure/cognitiveservices/search/websearch/operations/web_operations.py
@@ -359,7 +359,7 @@ class WebOperations(object):
 
         # Construct and send request
         request = self._client.get(url, query_parameters)
-        response = self._client.send(request, header_parameters, **operation_config)
+        response = self._client.send(request, header_parameters, stream=False, **operation_config)
 
         if response.status_code not in [200]:
             raise models.ErrorResponseException(self._deserialize, response)
