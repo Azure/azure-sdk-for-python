b7242ebd069b55f1e5de90624711d389dbede2e1
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-01-31 12:10:59 -0800

    Scope change in ManagementGroups

--- azure-mgmt-resource/azure/mgmt/resource/managementgroups/management_groups_api.py
+++ azure-mgmt-resource/azure/mgmt/resource/managementgroups/management_groups_api.py
@@ -27,18 +27,14 @@ class ManagementGroupsAPIConfiguration(AzureConfiguration):
     :param credentials: Credentials needed for the client to connect to Azure.
     :type credentials: :mod:`A msrestazure Credentials
      object<msrestazure.azure_active_directory>`
-    :param subscription_id: Subscription ID.
-    :type subscription_id: str
     :param str base_url: Service URL
     """
 
     def __init__(
-            self, credentials, subscription_id, base_url=None):
+            self, credentials, base_url=None):
 
         if credentials is None:
             raise ValueError("Parameter 'credentials' must not be None.")
-        if subscription_id is None:
-            raise ValueError("Parameter 'subscription_id' must not be None.")
         if not base_url:
             base_url = 'https://management.azure.com'
 
@@ -48,7 +44,6 @@ class ManagementGroupsAPIConfiguration(AzureConfiguration):
         self.add_user_agent('Azure-SDK-For-Python')
 
         self.credentials = credentials
-        self.subscription_id = subscription_id
 
 
 class ManagementGroupsAPI(object):
@@ -69,15 +64,13 @@ class ManagementGroupsAPI(object):
     :param credentials: Credentials needed for the client to connect to Azure.
     :type credentials: :mod:`A msrestazure Credentials
      object<msrestazure.azure_active_directory>`
-    :param subscription_id: Subscription ID.
-    :type subscription_id: str
     :param str base_url: Service URL
     """
 
     def __init__(
-            self, credentials, subscription_id, base_url=None):
+            self, credentials, base_url=None):
 
-        self.config = ManagementGroupsAPIConfiguration(credentials, subscription_id, base_url)
+        self.config = ManagementGroupsAPIConfiguration(credentials, base_url)
         self._client = ServiceClient(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
