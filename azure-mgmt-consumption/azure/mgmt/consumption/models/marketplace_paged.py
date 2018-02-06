f0c4e2b3b7e5e0c86a9914fb380f860f53674b62
kjeur <35118371+kjeur@users.noreply.github.com>
2018-02-06 12:01:26 -0800

    Consumption 2018-01-31 for marketplace, usagedetails, pricesheet, reservation details, summaries and budgets (#1896)
    * New SDK for version 2018-01-31 and modifying tests for usage details and reservation Summary/details and adding new tests for marketplace and pricesheet
    
    * Tests for version 2018-01-31 for marketplace, usagedetails, pricesheet, reservation details, summaries and budgets
    
    * Reverting mgmt_testcase.py - was included by mistake
    
    * updating sdk as asked by @lmazuel autorest specification/consumption/resource-manager/readme.md --python --python-sdks-folder={localpath} --python-mode=update
    
    * uNWANTED CHAR AT THE BEGINNING OF FILE
    
    * Removing code for datetime.now() to avoid recording map failure
    
    * Clean tests
    
    * Scrub tests
    
    * renaming test and genrating new recording
    
    * updating history file and version

--- azure-mgmt-consumption/azure/mgmt/consumption/models/marketplace_paged.py
+++ azure-mgmt-consumption/azure/mgmt/consumption/models/marketplace_paged.py
@@ -9,5 +9,19 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "2.0.0"
+from msrest.paging import Paged
 
+
+class MarketplacePaged(Paged):
+    """
+    A paging container for iterating over a list of :class:`Marketplace <azure.mgmt.consumption.models.Marketplace>` object
+    """
+
+    _attribute_map = {
+        'next_link': {'key': 'nextLink', 'type': 'str'},
+        'current_page': {'key': 'value', 'type': '[Marketplace]'}
+    }
+
+    def __init__(self, *args, **kwargs):
+
+        super(MarketplacePaged, self).__init__(*args, **kwargs)
