36c3fa43e0f77872c37d32d0e02eb624efde5b7a
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-02-08 11:18:33 -0800

    Migrate Redis to new test framework

--- azure-mgmt-redis/tests/test_mgmt_redis.py
+++ azure-mgmt-redis/tests/test_mgmt_redis.py
@@ -8,9 +8,7 @@
 import unittest
 
 import azure.mgmt.redis
-from testutils.common_recordingtestcase import record
-from tests.mgmt_testcase import HttpStatusCode, AzureMgmtTestCase
-
+from devtools_testutils import AzureMgmtTestCase, ResourceGroupPreparer
 
 class MgmtRedisTest(AzureMgmtTestCase):
 
@@ -19,27 +17,25 @@ class MgmtRedisTest(AzureMgmtTestCase):
         self.redis_client = self.create_mgmt_client(
             azure.mgmt.redis.RedisManagementClient
         )
-        if not self.is_playback():
-            self.create_resource_group()
 
-    @record
-    def test_redis(self):
+    @ResourceGroupPreparer()
+    def test_redis(self, resource_group, location):
         account_name = self.get_resource_name('pyarmredis')
 
         cache_name = 'mycachename'
         redis_async_create = self.redis_client.redis.create(
-            self.group_name, 
+            resource_group.name,
             cache_name,
             azure.mgmt.redis.models.RedisCreateParameters( 
                 sku = azure.mgmt.redis.models.Sku(name = 'Basic', family = 'C', capacity = '1'),
-                location = self.region
+                location = location
             )
         ) 
-        redis_cache =redis_async_create.result()
+        redis_cache = redis_async_create.result()
         self.assertEqual(redis_cache.name, cache_name)
 
         result = self.redis_client.redis.get(
-            self.group_name, 
+            resource_group.name,
             cache_name,
         )
         self.assertEqual(result.name, cache_name)
