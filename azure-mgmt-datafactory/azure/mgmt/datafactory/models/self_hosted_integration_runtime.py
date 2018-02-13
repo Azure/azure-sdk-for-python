344416255f4a425c96581075ae2256d8c28de524
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-02-13 08:01:17 -0800

    ADF after https://github.com/Azure/azure-rest-api-specs/pull/2473

--- azure-mgmt-datafactory/azure/mgmt/datafactory/models/self_hosted_integration_runtime.py
+++ azure-mgmt-datafactory/azure/mgmt/datafactory/models/self_hosted_integration_runtime.py
@@ -22,12 +22,23 @@ class SelfHostedIntegrationRuntime(IntegrationRuntime):
     :type description: str
     :param type: Constant filled by server.
     :type type: str
+    :param linked_info:
+    :type linked_info:
+     ~azure.mgmt.datafactory.models.LinkedIntegrationRuntimeProperties
     """
 
     _validation = {
         'type': {'required': True},
     }
 
-    def __init__(self, additional_properties=None, description=None):
+    _attribute_map = {
+        'additional_properties': {'key': '', 'type': '{object}'},
+        'description': {'key': 'description', 'type': 'str'},
+        'type': {'key': 'type', 'type': 'str'},
+        'linked_info': {'key': 'typeProperties.linkedInfo', 'type': 'LinkedIntegrationRuntimeProperties'},
+    }
+
+    def __init__(self, additional_properties=None, description=None, linked_info=None):
         super(SelfHostedIntegrationRuntime, self).__init__(additional_properties=additional_properties, description=description)
+        self.linked_info = linked_info
         self.type = 'SelfHosted'
