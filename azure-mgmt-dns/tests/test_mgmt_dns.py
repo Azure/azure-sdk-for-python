35fe9056a783e4db0d68a8ba8a3d9110c6826774
Muhammad Waqar <muwaqar@microsoft.com>
2018-03-05 15:48:58 -0800

    Fixing public zone test. Adding private zone test.

--- azure-mgmt-dns/tests/test_mgmt_dns.py
+++ azure-mgmt-dns/tests/test_mgmt_dns.py
@@ -8,6 +8,7 @@
 import unittest
 
 import azure.mgmt.dns
+import azure.mgmt.network
 from devtools_testutils import AzureMgmtTestCase, ResourceGroupPreparer
 
 class MgmtDnsTest(AzureMgmtTestCase):
@@ -15,41 +16,42 @@ class MgmtDnsTest(AzureMgmtTestCase):
     def setUp(self):
         super(MgmtDnsTest, self).setUp()
         self.dns_client = self.create_mgmt_client(
-            azure.mgmt.dns.DnsManagementClient
+            azure.mgmt.dns.DnsManagementClient,
+            base_url='https://api-dogfood.resources.windows-int.net/'
         )
 
-    @ResourceGroupPreparer()
-    def test_dns(self, resource_group, location):
-        account_name = self.get_resource_name('pydns.com')
+    @ResourceGroupPreparer(client_kwargs={'base_url':'https://api-dogfood.resources.windows-int.net/'})
+    def test_public_zone(self, resource_group, location):
+        zone_name = self.get_resource_name('pydns.com')
 
-        # The only valid value is 'global', otherwise you will get a:
-        # The subscription is not registered for the resource type 'dnszones' in the location 'westus'.
+        # Zones are a 'global' resource.
         zone = self.dns_client.zones.create_or_update(
             resource_group.name,
-            account_name,
+            zone_name,
             {
+                'zone_type': 'Public',
                 'location': 'global'
             }
         )
-        self.assertEqual(zone.name, account_name)
+        self.assertEqual(zone.name, zone_name)
 
         zone = self.dns_client.zones.get(
             resource_group.name,
             zone.name
         )
-        self.assertEqual(zone.name, account_name)
+        self.assertEqual(zone.name, zone_name)
 
         zones = list(self.dns_client.zones.list_by_resource_group(
             resource_group.name
         ))
-        self.assertEqual(len(zones), 1)
+        self.assertGreaterEqual(len(zones), 1)
 
         zones = list(self.dns_client.zones.list())
-        self.assertEqual(len(zones), 1)
+        self.assertGreaterEqual(len(zones), 1)
 
         # Record set
         record_set_name = self.get_resource_name('record_set')
-        record_set = self.dns_client.record_sets.create_or_update(
+        self.dns_client.record_sets.create_or_update(
             resource_group.name,
             zone.name,
             record_set_name,
@@ -59,15 +61,12 @@ class MgmtDnsTest(AzureMgmtTestCase):
                  "arecords": [
                      {
                         "ipv4_address": "1.2.3.4"
-                     },
-                     {
-                        "ipv4_address": "1.2.3.5"
                      }
                  ]
             }
         )
 
-        record_set = self.dns_client.record_sets.update(
+        self.dns_client.record_sets.update(
             resource_group.name,
             zone.name,
             record_set_name,
@@ -79,7 +78,7 @@ class MgmtDnsTest(AzureMgmtTestCase):
                         "ipv4_address": "1.2.3.4"
                      },
                      {
-                        "ipv4_address": "1.2.3.5"
+                        "ipv4_address": "5.6.7.8"
                      }
                  ]
             }
@@ -91,6 +90,7 @@ class MgmtDnsTest(AzureMgmtTestCase):
             record_set_name,
             'A'
         )
+        self.assertIsNotNone(record_set)
 
         record_sets = list(self.dns_client.record_sets.list_by_type(
             resource_group.name,
@@ -99,11 +99,155 @@ class MgmtDnsTest(AzureMgmtTestCase):
         ))
         self.assertEqual(len(record_sets), 1)
 
+        # NS and SOA records are created by default in public zones.
         record_sets = list(self.dns_client.record_sets.list_by_dns_zone(
             resource_group.name,
             zone.name
         ))
         self.assertEqual(len(record_sets), 3)
+
+        self.dns_client.record_sets.delete(
+            resource_group.name,
+            zone.name,
+            record_set_name,
+            'A'
+        )
+
+        async_delete = self.dns_client.zones.delete(
+            resource_group.name,
+            zone.name
+        )
+        async_delete.wait()
+
+    @ResourceGroupPreparer(client_kwargs={'base_url':'https://api-dogfood.resources.windows-int.net/'})
+    def test_private_zone(self, resource_group, location):
+        zone_name = self.get_resource_name('pydns.com')
+
+        network_client = self.create_mgmt_client(
+            azure.mgmt.network.NetworkManagementClient,
+            api_version='2017-09-01',
+            base_url='https://api-dogfood.resources.windows-int.net/'
+        )
+
+        registration_virtual_network_name = self.get_resource_name('pvtzoneregvnet')
+        registration_network = network_client.virtual_networks.create_or_update(
+            resource_group.name,
+            registration_virtual_network_name,
+            {
+                'location': location,
+                'address_space': {
+                    'address_prefixes': ['10.0.0.0/8']
+                },
+                'subnets': [
+                    {
+                        'name': 'default',
+                        'address_prefix': '10.0.0.0/24'
+                    }
+                ]
+            }
+        ).result()
+
+        resolution_virtual_network_name = self.get_resource_name('pvtzoneresvnet')
+        resolution_network = network_client.virtual_networks.create_or_update(
+            resource_group.name,
+            resolution_virtual_network_name,
+            {
+                'location': location,
+                'address_space': {
+                    'address_prefixes': ['10.0.0.0/8']
+                },
+                'subnets': [
+                    {
+                        'name': 'default',
+                        'address_prefix': '10.0.0.0/24'
+                    }
+                ]
+            }
+        ).result()
+
+        # Zones are a 'global' resource.
+        zone = self.dns_client.zones.create_or_update(
+            resource_group.name,
+            zone_name,
+            {
+                'zone_type': 'Private',
+                'location': 'global',
+                'registration_virtual_networks': [ { 'id': registration_network.id } ],
+                'resolution_virtual_networks': [ { 'id': resolution_network.id } ]
+            }
+        )
+        self.assertEqual(zone.name, zone_name)
+
+        zone = self.dns_client.zones.get(
+            resource_group.name,
+            zone.name
+        )
+        self.assertEqual(zone.name, zone_name)
+
+        zones = list(self.dns_client.zones.list_by_resource_group(
+            resource_group.name
+        ))
+        self.assertGreaterEqual(len(zones), 1)
+
+        zones = list(self.dns_client.zones.list())
+        self.assertGreaterEqual(len(zones), 1)
+
+        # Record set
+        record_set_name = self.get_resource_name('record_set')
+        self.dns_client.record_sets.create_or_update(
+            resource_group.name,
+            zone.name,
+            record_set_name,
+            'A',
+            {
+                 "ttl": 300,
+                 "arecords": [
+                     {
+                        "ipv4_address": "1.2.3.4"
+                     }
+                 ]
+            }
+        )
+
+        self.dns_client.record_sets.update(
+            resource_group.name,
+            zone.name,
+            record_set_name,
+            'A',
+            {
+                 "ttl": 300,
+                 "arecords": [
+                     {
+                        "ipv4_address": "1.2.3.4"
+                     },
+                     {
+                        "ipv4_address": "5.6.7.8"
+                     }
+                 ]
+            }
+        )
+
+        record_set = self.dns_client.record_sets.get(
+            resource_group.name,
+            zone.name,
+            record_set_name,
+            'A'
+        )
+        self.assertIsNotNone(record_set)
+
+        record_sets = list(self.dns_client.record_sets.list_by_type(
+            resource_group.name,
+            zone.name,
+            'A'
+        ))
+        self.assertEqual(len(record_sets), 1)
+
+        # SOA record is created by default in private zones.
+        record_sets = list(self.dns_client.record_sets.list_by_dns_zone(
+            resource_group.name,
+            zone.name
+        ))
+        self.assertEqual(len(record_sets), 2)
 
         self.dns_client.record_sets.delete(
             resource_group.name,
