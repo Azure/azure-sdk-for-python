9da39990fa4545a4f170e765295bc2338e74c1fa
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-03-22 14:13:44 -0700

    azure-mgmt-sql 0.8.6 from https://github.com/Azure/azure-rest-api-specs/blob/71b71ffd57e4faf0f0a429dc4f6f6695cc976e1f/specification/sql/resource-manager/readme.md

--- azure-mgmt-sql/azure/mgmt/sql/models/__init__.py
+++ azure-mgmt-sql/azure/mgmt/sql/models/__init__.py
@@ -14,7 +14,6 @@ from .proxy_resource import ProxyResource
 from .backup_long_term_retention_policy import BackupLongTermRetentionPolicy
 from .backup_long_term_retention_vault import BackupLongTermRetentionVault
 from .tracked_resource import TrackedResource
-from .restore_point import RestorePoint
 from .recoverable_database import RecoverableDatabase
 from .restorable_dropped_database import RestorableDroppedDatabase
 from .max_size_capability import MaxSizeCapability
@@ -26,10 +25,26 @@ from .elastic_pool_dtu_capability import ElasticPoolDtuCapability
 from .elastic_pool_edition_capability import ElasticPoolEditionCapability
 from .server_version_capability import ServerVersionCapability
 from .location_capabilities import LocationCapabilities
+from .check_name_availability_request import CheckNameAvailabilityRequest
+from .check_name_availability_response import CheckNameAvailabilityResponse
 from .server_connection_policy import ServerConnectionPolicy
+from .slo_usage_metric import SloUsageMetric
+from .service_tier_advisor import ServiceTierAdvisor
+from .transparent_data_encryption import TransparentDataEncryption
+from .operation_impact import OperationImpact
+from .recommended_index import RecommendedIndex
+from .database import Database
+from .database_update import DatabaseUpdate
+from .transparent_data_encryption_activity import TransparentDataEncryptionActivity
+from .elastic_pool_database_activity import ElasticPoolDatabaseActivity
+from .elastic_pool_activity import ElasticPoolActivity
+from .recommended_elastic_pool_metric import RecommendedElasticPoolMetric
+from .recommended_elastic_pool import RecommendedElasticPool
 from .database_security_alert_policy import DatabaseSecurityAlertPolicy
 from .data_masking_policy import DataMaskingPolicy
 from .data_masking_rule import DataMaskingRule
+from .elastic_pool import ElasticPool
+from .elastic_pool_update import ElasticPoolUpdate
 from .firewall_rule import FirewallRule
 from .geo_backup_policy import GeoBackupPolicy
 from .import_extension_request import ImportExtensionRequest
@@ -45,25 +60,11 @@ from .replication_link import ReplicationLink
 from .server_azure_ad_administrator import ServerAzureADAdministrator
 from .server_communication_link import ServerCommunicationLink
 from .service_objective import ServiceObjective
-from .check_name_availability_request import CheckNameAvailabilityRequest
-from .check_name_availability_response import CheckNameAvailabilityResponse
-from .recommended_elastic_pool_metric import RecommendedElasticPoolMetric
-from .slo_usage_metric import SloUsageMetric
-from .service_tier_advisor import ServiceTierAdvisor
-from .transparent_data_encryption import TransparentDataEncryption
-from .operation_impact import OperationImpact
-from .recommended_index import RecommendedIndex
-from .database import Database
-from .recommended_elastic_pool import RecommendedElasticPool
-from .elastic_pool import ElasticPool
-from .elastic_pool_update import ElasticPoolUpdate
-from .elastic_pool_activity import ElasticPoolActivity
-from .elastic_pool_database_activity import ElasticPoolDatabaseActivity
-from .database_update import DatabaseUpdate
-from .transparent_data_encryption_activity import TransparentDataEncryptionActivity
 from .server_usage import ServerUsage
 from .database_usage import DatabaseUsage
 from .database_blob_auditing_policy import DatabaseBlobAuditingPolicy
+from .automatic_tuning_options import AutomaticTuningOptions
+from .database_automatic_tuning import DatabaseAutomaticTuning
 from .encryption_protector import EncryptionProtector
 from .failover_group_read_write_endpoint import FailoverGroupReadWriteEndpoint
 from .failover_group_read_only_endpoint import FailoverGroupReadOnlyEndpoint
@@ -91,27 +92,31 @@ from .sync_group import SyncGroup
 from .sync_member import SyncMember
 from .subscription_usage import SubscriptionUsage
 from .virtual_network_rule import VirtualNetworkRule
-from .database_operation import DatabaseOperation
 from .resource_move_definition import ResourceMoveDefinition
+from .automatic_tuning_server_options import AutomaticTuningServerOptions
+from .server_automatic_tuning import ServerAutomaticTuning
 from .server_dns_alias import ServerDnsAlias
 from .server_dns_alias_acquisition import ServerDnsAliasAcquisition
+from .restore_point import RestorePoint
+from .create_database_restore_point_definition import CreateDatabaseRestorePointDefinition
+from .database_operation import DatabaseOperation
+from .elastic_pool_operation import ElasticPoolOperation
 from .backup_long_term_retention_policy_paged import BackupLongTermRetentionPolicyPaged
 from .backup_long_term_retention_vault_paged import BackupLongTermRetentionVaultPaged
-from .restore_point_paged import RestorePointPaged
 from .recoverable_database_paged import RecoverableDatabasePaged
 from .restorable_dropped_database_paged import RestorableDroppedDatabasePaged
-from .data_masking_rule_paged import DataMaskingRulePaged
-from .firewall_rule_paged import FirewallRulePaged
-from .geo_backup_policy_paged import GeoBackupPolicyPaged
+from .server_paged import ServerPaged
+from .database_paged import DatabasePaged
 from .metric_paged import MetricPaged
 from .metric_definition_paged import MetricDefinitionPaged
-from .database_paged import DatabasePaged
+from .data_masking_rule_paged import DataMaskingRulePaged
 from .elastic_pool_paged import ElasticPoolPaged
+from .firewall_rule_paged import FirewallRulePaged
+from .geo_backup_policy_paged import GeoBackupPolicyPaged
 from .replication_link_paged import ReplicationLinkPaged
 from .server_azure_ad_administrator_paged import ServerAzureADAdministratorPaged
 from .server_communication_link_paged import ServerCommunicationLinkPaged
 from .service_objective_paged import ServiceObjectivePaged
-from .server_paged import ServerPaged
 from .elastic_pool_activity_paged import ElasticPoolActivityPaged
 from .elastic_pool_database_activity_paged import ElasticPoolDatabaseActivityPaged
 from .recommended_elastic_pool_paged import RecommendedElasticPoolPaged
@@ -133,24 +138,36 @@ from .sync_group_paged import SyncGroupPaged
 from .sync_member_paged import SyncMemberPaged
 from .subscription_usage_paged import SubscriptionUsagePaged
 from .virtual_network_rule_paged import VirtualNetworkRulePaged
-from .database_operation_paged import DatabaseOperationPaged
 from .server_dns_alias_paged import ServerDnsAliasPaged
+from .restore_point_paged import RestorePointPaged
+from .database_operation_paged import DatabaseOperationPaged
+from .elastic_pool_operation_paged import ElasticPoolOperationPaged
 from .sql_management_client_enums import (
     BackupLongTermRetentionPolicyState,
-    RestorePointType,
     CapabilityStatus,
     MaxSizeUnits,
     PerformanceLevelUnit,
+    CheckNameAvailabilityReason,
     ServerConnectionType,
+    CreateMode,
+    DatabaseEdition,
+    ServiceObjectiveName,
+    TransparentDataEncryptionStatus,
+    RecommendedIndexAction,
+    RecommendedIndexState,
+    RecommendedIndexType,
+    ReadScale,
+    SampleName,
+    TransparentDataEncryptionActivityStatus,
+    ElasticPoolEdition,
     SecurityAlertPolicyState,
     SecurityAlertPolicyEmailAccountAdmins,
     SecurityAlertPolicyUseServerDefault,
     DataMaskingState,
     DataMaskingRuleState,
     DataMaskingFunction,
+    ElasticPoolState,
     GeoBackupPolicyState,
-    DatabaseEdition,
-    ServiceObjectiveName,
     StorageKeyType,
     AuthenticationType,
     UnitType,
@@ -158,18 +175,11 @@ from .sql_management_client_enums import (
     UnitDefinitionType,
     ReplicationRole,
     ReplicationState,
-    CheckNameAvailabilityReason,
-    ElasticPoolEdition,
-    CreateMode,
-    TransparentDataEncryptionStatus,
-    RecommendedIndexAction,
-    RecommendedIndexState,
-    RecommendedIndexType,
-    ReadScale,
-    SampleName,
-    ElasticPoolState,
-    TransparentDataEncryptionActivityStatus,
     BlobAuditingPolicyState,
+    AutomaticTuningMode,
+    AutomaticTuningOptionModeDesired,
+    AutomaticTuningOptionModeActual,
+    AutomaticTuningDisabledReason,
     ServerKeyType,
     ReadWriteEndpointFailoverPolicy,
     ReadOnlyEndpointFailoverPolicy,
@@ -184,6 +194,9 @@ from .sql_management_client_enums import (
     SyncDirection,
     SyncMemberState,
     VirtualNetworkRuleState,
+    AutomaticTuningServerMode,
+    AutomaticTuningServerReason,
+    RestorePointType,
     ManagementOperationState,
 )
 
@@ -193,7 +206,6 @@ __all__ = [
     'BackupLongTermRetentionPolicy',
     'BackupLongTermRetentionVault',
     'TrackedResource',
-    'RestorePoint',
     'RecoverableDatabase',
     'RestorableDroppedDatabase',
     'MaxSizeCapability',
@@ -205,10 +217,26 @@ __all__ = [
     'ElasticPoolEditionCapability',
     'ServerVersionCapability',
     'LocationCapabilities',
+    'CheckNameAvailabilityRequest',
+    'CheckNameAvailabilityResponse',
     'ServerConnectionPolicy',
+    'SloUsageMetric',
+    'ServiceTierAdvisor',
+    'TransparentDataEncryption',
+    'OperationImpact',
+    'RecommendedIndex',
+    'Database',
+    'DatabaseUpdate',
+    'TransparentDataEncryptionActivity',
+    'ElasticPoolDatabaseActivity',
+    'ElasticPoolActivity',
+    'RecommendedElasticPoolMetric',
+    'RecommendedElasticPool',
     'DatabaseSecurityAlertPolicy',
     'DataMaskingPolicy',
     'DataMaskingRule',
+    'ElasticPool',
+    'ElasticPoolUpdate',
     'FirewallRule',
     'GeoBackupPolicy',
     'ImportExtensionRequest',
@@ -224,25 +252,11 @@ __all__ = [
     'ServerAzureADAdministrator',
     'ServerCommunicationLink',
     'ServiceObjective',
-    'CheckNameAvailabilityRequest',
-    'CheckNameAvailabilityResponse',
-    'RecommendedElasticPoolMetric',
-    'SloUsageMetric',
-    'ServiceTierAdvisor',
-    'TransparentDataEncryption',
-    'OperationImpact',
-    'RecommendedIndex',
-    'Database',
-    'RecommendedElasticPool',
-    'ElasticPool',
-    'ElasticPoolUpdate',
-    'ElasticPoolActivity',
-    'ElasticPoolDatabaseActivity',
-    'DatabaseUpdate',
-    'TransparentDataEncryptionActivity',
     'ServerUsage',
     'DatabaseUsage',
     'DatabaseBlobAuditingPolicy',
+    'AutomaticTuningOptions',
+    'DatabaseAutomaticTuning',
     'EncryptionProtector',
     'FailoverGroupReadWriteEndpoint',
     'FailoverGroupReadOnlyEndpoint',
@@ -270,27 +284,31 @@ __all__ = [
     'SyncMember',
     'SubscriptionUsage',
     'VirtualNetworkRule',
-    'DatabaseOperation',
     'ResourceMoveDefinition',
+    'AutomaticTuningServerOptions',
+    'ServerAutomaticTuning',
     'ServerDnsAlias',
     'ServerDnsAliasAcquisition',
+    'RestorePoint',
+    'CreateDatabaseRestorePointDefinition',
+    'DatabaseOperation',
+    'ElasticPoolOperation',
     'BackupLongTermRetentionPolicyPaged',
     'BackupLongTermRetentionVaultPaged',
-    'RestorePointPaged',
     'RecoverableDatabasePaged',
     'RestorableDroppedDatabasePaged',
-    'DataMaskingRulePaged',
-    'FirewallRulePaged',
-    'GeoBackupPolicyPaged',
+    'ServerPaged',
+    'DatabasePaged',
     'MetricPaged',
     'MetricDefinitionPaged',
-    'DatabasePaged',
+    'DataMaskingRulePaged',
     'ElasticPoolPaged',
+    'FirewallRulePaged',
+    'GeoBackupPolicyPaged',
     'ReplicationLinkPaged',
     'ServerAzureADAdministratorPaged',
     'ServerCommunicationLinkPaged',
     'ServiceObjectivePaged',
-    'ServerPaged',
     'ElasticPoolActivityPaged',
     'ElasticPoolDatabaseActivityPaged',
     'RecommendedElasticPoolPaged',
@@ -312,23 +330,35 @@ __all__ = [
     'SyncMemberPaged',
     'SubscriptionUsagePaged',
     'VirtualNetworkRulePaged',
-    'DatabaseOperationPaged',
     'ServerDnsAliasPaged',
+    'RestorePointPaged',
+    'DatabaseOperationPaged',
+    'ElasticPoolOperationPaged',
     'BackupLongTermRetentionPolicyState',
-    'RestorePointType',
     'CapabilityStatus',
     'MaxSizeUnits',
     'PerformanceLevelUnit',
+    'CheckNameAvailabilityReason',
     'ServerConnectionType',
+    'CreateMode',
+    'DatabaseEdition',
+    'ServiceObjectiveName',
+    'TransparentDataEncryptionStatus',
+    'RecommendedIndexAction',
+    'RecommendedIndexState',
+    'RecommendedIndexType',
+    'ReadScale',
+    'SampleName',
+    'TransparentDataEncryptionActivityStatus',
+    'ElasticPoolEdition',
     'SecurityAlertPolicyState',
     'SecurityAlertPolicyEmailAccountAdmins',
     'SecurityAlertPolicyUseServerDefault',
     'DataMaskingState',
     'DataMaskingRuleState',
     'DataMaskingFunction',
+    'ElasticPoolState',
     'GeoBackupPolicyState',
-    'DatabaseEdition',
-    'ServiceObjectiveName',
     'StorageKeyType',
     'AuthenticationType',
     'UnitType',
@@ -336,18 +366,11 @@ __all__ = [
     'UnitDefinitionType',
     'ReplicationRole',
     'ReplicationState',
-    'CheckNameAvailabilityReason',
-    'ElasticPoolEdition',
-    'CreateMode',
-    'TransparentDataEncryptionStatus',
-    'RecommendedIndexAction',
-    'RecommendedIndexState',
-    'RecommendedIndexType',
-    'ReadScale',
-    'SampleName',
-    'ElasticPoolState',
-    'TransparentDataEncryptionActivityStatus',
     'BlobAuditingPolicyState',
+    'AutomaticTuningMode',
+    'AutomaticTuningOptionModeDesired',
+    'AutomaticTuningOptionModeActual',
+    'AutomaticTuningDisabledReason',
     'ServerKeyType',
     'ReadWriteEndpointFailoverPolicy',
     'ReadOnlyEndpointFailoverPolicy',
@@ -362,5 +385,8 @@ __all__ = [
     'SyncDirection',
     'SyncMemberState',
     'VirtualNetworkRuleState',
+    'AutomaticTuningServerMode',
+    'AutomaticTuningServerReason',
+    'RestorePointType',
     'ManagementOperationState',
 ]
--- azure-mgmt-sql/azure/mgmt/sql/operations/database_automatic_tuning_operations.py
+++ azure-mgmt-sql/azure/mgmt/sql/operations/database_automatic_tuning_operations.py
@@ -16,14 +16,14 @@ from msrestazure.azure_exceptions import CloudError
 from .. import models
 
 
-class RecoverableDatabasesOperations(object):
-    """RecoverableDatabasesOperations operations.
+class DatabaseAutomaticTuningOperations(object):
+    """DatabaseAutomaticTuningOperations operations.
 
     :param client: Client for service requests.
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
-    :ivar api_version: The API version to use for the request. Constant value: "2014-04-01".
+    :ivar api_version: The API version to use for the request. Constant value: "2015-05-01-preview".
     """
 
     models = models
@@ -33,14 +33,13 @@ class RecoverableDatabasesOperations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
-        self.api_version = "2014-04-01"
+        self.api_version = "2015-05-01-preview"
 
         self.config = config
 
     def get(
             self, resource_group_name, server_name, database_name, custom_headers=None, raw=False, **operation_config):
-        """Gets a recoverable database, which is a resource representing a
-        database's geo backup.
+        """Gets a database's automatic tuning.
 
         :param resource_group_name: The name of the resource group that
          contains the resource. You can obtain this value from the Azure
@@ -48,25 +47,25 @@ class RecoverableDatabasesOperations(object):
         :type resource_group_name: str
         :param server_name: The name of the server.
         :type server_name: str
-        :param database_name: The name of the database
+        :param database_name: The name of the database.
         :type database_name: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: RecoverableDatabase or ClientRawResponse if raw=true
-        :rtype: ~azure.mgmt.sql.models.RecoverableDatabase or
+        :return: DatabaseAutomaticTuning or ClientRawResponse if raw=true
+        :rtype: ~azure.mgmt.sql.models.DatabaseAutomaticTuning or
          ~msrest.pipeline.ClientRawResponse
         :raises: :class:`CloudError<msrestazure.azure_exceptions.CloudError>`
         """
         # Construct URL
         url = self.get.metadata['url']
         path_format_arguments = {
-            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str'),
             'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
             'serverName': self._serialize.url("server_name", server_name, 'str'),
-            'databaseName': self._serialize.url("database_name", database_name, 'str')
+            'databaseName': self._serialize.url("database_name", database_name, 'str'),
+            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str')
         }
         url = self._client.format_url(url, **path_format_arguments)
 
@@ -96,18 +95,18 @@ class RecoverableDatabasesOperations(object):
         deserialized = None
 
         if response.status_code == 200:
-            deserialized = self._deserialize('RecoverableDatabase', response)
+            deserialized = self._deserialize('DatabaseAutomaticTuning', response)
 
         if raw:
             client_raw_response = ClientRawResponse(deserialized, response)
             return client_raw_response
 
         return deserialized
-    get.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/recoverableDatabases/{databaseName}'}
+    get.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/automaticTuning/current'}
 
-    def list_by_server(
-            self, resource_group_name, server_name, custom_headers=None, raw=False, **operation_config):
-        """Gets a list of recoverable databases.
+    def update(
+            self, resource_group_name, server_name, database_name, desired_state=None, options=None, custom_headers=None, raw=False, **operation_config):
+        """Update automatic tuning properties for target database.
 
         :param resource_group_name: The name of the resource group that
          contains the resource. You can obtain this value from the Azure
@@ -115,65 +114,71 @@ class RecoverableDatabasesOperations(object):
         :type resource_group_name: str
         :param server_name: The name of the server.
         :type server_name: str
+        :param database_name: The name of the database.
+        :type database_name: str
+        :param desired_state: Automatic tuning desired state. Possible values
+         include: 'Inherit', 'Custom', 'Auto', 'Unspecified'
+        :type desired_state: str or ~azure.mgmt.sql.models.AutomaticTuningMode
+        :param options: Automatic tuning options definition.
+        :type options: dict[str,
+         ~azure.mgmt.sql.models.AutomaticTuningOptions]
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: An iterator like instance of RecoverableDatabase
-        :rtype:
-         ~azure.mgmt.sql.models.RecoverableDatabasePaged[~azure.mgmt.sql.models.RecoverableDatabase]
+        :return: DatabaseAutomaticTuning or ClientRawResponse if raw=true
+        :rtype: ~azure.mgmt.sql.models.DatabaseAutomaticTuning or
+         ~msrest.pipeline.ClientRawResponse
         :raises: :class:`CloudError<msrestazure.azure_exceptions.CloudError>`
         """
-        def internal_paging(next_link=None, raw=False):
+        parameters = models.DatabaseAutomaticTuning(desired_state=desired_state, options=options)
 
-            if not next_link:
-                # Construct URL
-                url = self.list_by_server.metadata['url']
-                path_format_arguments = {
-                    'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str'),
-                    'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
-                    'serverName': self._serialize.url("server_name", server_name, 'str')
-                }
-                url = self._client.format_url(url, **path_format_arguments)
+        # Construct URL
+        url = self.update.metadata['url']
+        path_format_arguments = {
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
+            'serverName': self._serialize.url("server_name", server_name, 'str'),
+            'databaseName': self._serialize.url("database_name", database_name, 'str'),
+            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str')
+        }
+        url = self._client.format_url(url, **path_format_arguments)
 
-                # Construct parameters
-                query_parameters = {}
-                query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
+        # Construct parameters
+        query_parameters = {}
+        query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
 
-            else:
-                url = next_link
-                query_parameters = {}
+        # Construct headers
+        header_parameters = {}
+        header_parameters['Content-Type'] = 'application/json; charset=utf-8'
+        if self.config.generate_client_request_id:
+            header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
+        if custom_headers:
+            header_parameters.update(custom_headers)
+        if self.config.accept_language is not None:
+            header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
 
-            # Construct headers
-            header_parameters = {}
-            header_parameters['Content-Type'] = 'application/json; charset=utf-8'
-            if self.config.generate_client_request_id:
-                header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
-            if custom_headers:
-                header_parameters.update(custom_headers)
-            if self.config.accept_language is not None:
-                header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
+        # Construct body
+        body_content = self._serialize.body(parameters, 'DatabaseAutomaticTuning')
 
-            # Construct and send request
-            request = self._client.get(url, query_parameters)
-            response = self._client.send(
-                request, header_parameters, stream=False, **operation_config)
+        # Construct and send request
+        request = self._client.patch(url, query_parameters)
+        response = self._client.send(
+            request, header_parameters, body_content, stream=False, **operation_config)
 
-            if response.status_code not in [200]:
-                exp = CloudError(response)
-                exp.request_id = response.headers.get('x-ms-request-id')
-                raise exp
+        if response.status_code not in [200]:
+            exp = CloudError(response)
+            exp.request_id = response.headers.get('x-ms-request-id')
+            raise exp
 
-            return response
+        deserialized = None
 
-        # Deserialize response
-        deserialized = models.RecoverableDatabasePaged(internal_paging, self._deserialize.dependencies)
+        if response.status_code == 200:
+            deserialized = self._deserialize('DatabaseAutomaticTuning', response)
 
         if raw:
-            header_dict = {}
-            client_raw_response = models.RecoverableDatabasePaged(internal_paging, self._deserialize.dependencies, header_dict)
+            client_raw_response = ClientRawResponse(deserialized, response)
             return client_raw_response
 
         return deserialized
-    list_by_server.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/recoverableDatabases'}
+    update.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/automaticTuning/current'}
