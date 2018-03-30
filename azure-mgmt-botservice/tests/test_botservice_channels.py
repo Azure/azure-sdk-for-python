c65e6d70d5a62050e0e82cba6cf3c83916fa1bba
Swagat Mishra <swagatm@microsoft.com>
2018-03-30 15:24:18 -0700

    first commit of swagger and tests

---
+++ azure-mgmt-botservice/tests/test_botservice_channels.py
@@ -0,0 +1,216 @@
+from devtools_testutils import AzureMgmtTestCase
+from azure.mgmt.botservice import AzureBotService
+from azure.mgmt.botservice.models import Bot, BotProperties,sku,BotChannel,ErrorException
+import pdb
+
+class BotServiceChannelsTestCase(AzureMgmtTestCase):
+    def setUp(self):
+        super(BotServiceChannelsTestCase, self).setUp()
+        #create a bot here
+        self.client = self.create_mgmt_client(AzureBotService)
+        self.resource_group_name = 'testpythonrg'
+        self.resource_name = 'testpythonbot11'
+        location = 'global'
+        sku_name = 'Free'
+        kind= 'Bot'
+        display_name = "this is a test bot"
+        description= "this is a description for a test bot"
+        endpoint = "https://bing.com/messages/"
+        msa_app_id = "41a220b9-6571-4f0b-bbd2-43f1c1d82f52"
+        developer_app_insight_key = '59513bad-10a7-4d41-b4d0-b1c34c6af52a'
+        developer_app_insights_api_key = 'w24iw5ocbhcig71su7ibaj63hey5ieaozeuwdv2r'
+        developer_app_insights_application_id = 'cf03484e-3fdb-4b5e-9ad7-94bde32e5a2b'
+        bot = self.client.bots.create(
+            resource_group_name = self.resource_group_name,
+            resource_name = self.resource_name,
+            parameters = Bot(
+                location= location,
+                sku = sku.Sku(sku_name),
+                kind= kind,
+                properties= BotProperties(
+                    display_name = display_name,
+                    description= description,
+                    endpoint = endpoint,
+                    msa_app_id = msa_app_id,
+                    developer_app_insight_key = developer_app_insight_key,
+                    developer_app_insights_api_key = developer_app_insights_api_key,
+                    developer_app_insights_application_id = developer_app_insights_application_id,
+                )
+            )
+        )
+    
+    def tearDown(self):
+        super(BotServiceChannelsTestCase, self).tearDown()
+        self.client.bots.delete(
+            resource_group_name = self.resource_group_name,
+            resource_name = self.resource_name
+        )
+
+    def validateGetAndDeleteChannel(self, channel_name, channel_properties):
+        botChannel = BotChannel(
+            location = 'global',
+            properties = channel_properties
+        )
+
+        self.client.channels.create(
+            resource_group_name = self.resource_group_name,
+            resource_name = self.resource_name,
+            channel_name = 'EmailChannel',
+            parameters = botChannel 
+        )
+
+        channel = self.client.channels.get(
+            resource_group_name = self.resource_group_name,
+            resource_name = self.resource_name,
+            channel_name = channel_name
+        )
+        
+        self.assertIsNotNone(channel)
+        #is_enabled being true means that the service has managed to get the channel working fine.
+        self.assertTrue(channel.properties.properties.is_enabled)
+
+        channel = self.client.channels.delete(
+            resource_group_name = self.resource_group_name,
+            resource_name = self.resource_name,
+            channel_name = channel_name
+        )
+        with self.assertRaises(ErrorException):
+            channel = self.client.channels.get(
+                resource_group_name = self.resource_group_name,
+                resource_name = self.resource_name,
+                channel_name = channel_name
+            )
+
+    def test_email_channel(self):
+        from azure.mgmt.botservice.models import EmailChannel,EmailChannelProperties
+        channel = EmailChannel(
+            properties = EmailChannelProperties(
+                email_address = 'swagatm2@outlook.com',
+                password = 'Redmond1!',
+                is_enabled = True
+            )
+        )
+
+        self.validateGetAndDeleteChannel(
+            channel_name = 'EmailChannel',
+            channel_properties = channel
+        )
+
+    # def test_msteams_channel(self):
+    #     from azure.mgmt.botservice.models import MsTeamsChannel,MsTeamsChannelProperties
+    #     channel = MsTeamsChannel(
+    #         properties = MsTeamsChannelProperties(
+    #             is_enabled = True, 
+    #             enable_messaging = True, 
+    #         )
+    #     )
+    #     botChannel = BotChannel(
+    #         location = 'global',
+    #         properties = channel
+    #     )
+    #     self.client.channels.create(
+    #         resource_group_name = self.resource_group_name,
+    #         resource_name = self.resource_name,
+    #         # resource_group_name = 'swagatmwebappbottest',
+    #         # resource_name = 'swagatmwebappbottest',
+    #         channel_name = 'MsTeamsChannel',
+    #         parameters = botChannel 
+    #     )
+        
+    #     self.validateGetAndDeleteChannel(
+    #         channel_name = 'MsTeamsChannel'
+    #     )
+    
+    # def test_skype_channel(self):
+    #     from azure.mgmt.botservice.models import SkypeChannel,SkypeChannelProperties
+    #     channel = SkypeChannel(
+    #         properties = SkypeChannelProperties(
+    #             is_enabled = is_enabled,
+    #             enable_messaging = enable_messaging,
+    #         )
+    #     )
+    #     botChannel = BotChannel(
+    #         location = 'global',
+    #         properties = channel
+    #     )
+    #     #todo: change client factor in init file or make this a command
+    #     return client.channels.create(
+    #         resource_group_name = resource_group_name,
+    #         resource_name = resource_name,
+    #         channel_name = 'SkypeChannel',
+    #         parameters = botChannel 
+    #     )
+
+    # def test_telegram_channel(self):
+    #     from azure.mgmt.botservice.models import TelegramChannel,TelegramChannelProperties
+    #     channel = TelegramChannel(
+    #         properties = TelegramChannelProperties(
+    #             access_token = '520413022:AAF12lBf6s4tSqntaXEZnvrn6XOVrjQ6YN4',
+    #             is_enabled = True,
+    #         )
+    #     )
+    #     botChannel = BotChannel(
+    #         location = 'global',
+    #         properties = channel
+    #     )
+    #     self.client.channels.create(
+    #         resource_group_name = self.resource_group_name,
+    #         resource_name = self.resource_name,
+    #         channel_name = 'TelegramChannel',
+    #         parameters = botChannel 
+    #     )
+    #     self.validateGetAndDeleteChannel(
+    #         channel_name = 'TelegramChannel'
+    #     )
+
+    # def test_sms_channel(self):
+    #     from azure.mgmt.botservice.models import SmsChannel,SmsChannelProperties
+    #     channel = SmsChannel(
+    #         properties = SmsChannelProperties(
+    #             phone = '+15153258725',
+    #             account_sid = 'AC421cab6999e0c8c0d1a90c6643db8f05',
+    #             auth_token = '507d2f4f9a832fdd042d05c500b3a88f',
+    #             is_enabled = True,
+    #             is_validated = False 
+    #         )
+    #     )
+    #     botChannel = BotChannel(
+    #         location = 'global',
+    #         properties = channel
+    #     )
+    #     #todo: change client factor in init file or make this a command
+    #     self.client.channels.create(
+    #         resource_group_name = self.resource_group_name,
+    #         resource_name = self.resource_name,
+    #         channel_name = 'SmsChannel',
+    #         parameters = botChannel 
+    #     )    
+    
+    #     self.validateGetAndDeleteChannel(
+    #         channel_name = 'SmsChannel'
+    #     )
+
+    # def test_slack_channel(self):
+    #     from azure.mgmt.botservice.models import SlackChannel,SlackChannelProperties
+    #     channel = SlackChannel(
+    #         properties = SlackChannelProperties(
+    #             client_id = '130828831840.338719758214',
+    #             client_secret = 'f8bbe76c52f8e339622b5c17e5004d26',
+    #             verification_token = 'AhWzLaEjsy8uVgIsxFTZhDwd',
+    #             is_enabled = True
+    #         )
+    #     )
+    #     botChannel = BotChannel(
+    #         location = 'global',
+    #         properties = channel
+    #     )
+    #     self.client.channels.create(
+    #         resource_group_name = self.resource_group_name,
+    #         resource_name = self.resource_name,
+    #         channel_name = 'SlackChannel',
+    #         parameters = botChannel 
+    #     )
+    
+    #     self.validateGetAndDeleteChannel(
+    #         channel_name = 'SlackChannel'
+    #     )
---
+++ azure-mgmt-botservice/tests/test_mgmt_botservice.py
@@ -0,0 +1,83 @@
+from devtools_testutils import AzureMgmtTestCase
+from azure.mgmt.botservice import AzureBotService
+from azure.mgmt.botservice.models import Bot, BotProperties,sku
+import json
+
+class CoreBotServiceTestCase(AzureMgmtTestCase):
+    def setUp(self):
+        super(CoreBotServiceTestCase, self).setUp()
+        self.client = self.create_mgmt_client(AzureBotService)
+        self.resource_group_name = 'testpythonrg'
+        self.resource_name = 'testpythonbot12'
+        self.location = 'global'
+        self.sku_name = 'F0'
+        self.kind= 'Bot'
+        self.display_name = "this is a test bot"
+        self.description= "this is a description for a test bot"
+        self.endpoint = "https://bing.com/messages/"
+        self.msa_app_id = "41a220b9-6571-4f0b-bbd2-43f1c1d82f51"
+        self.developer_app_insight_key = '59513bad-10a7-4d41-b4d0-b1c34c6af52a'
+        self.developer_app_insights_api_key = 'w24iw5ocbhcig71su7ibaj63hey5ieaozeuwdv2r'
+        self.developer_app_insights_application_id = 'cf03484e-3fdb-4b5e-9ad7-94bde32e5a2b'
+
+    def validate_bot_properties(self, bot):
+        self.assertEqual(bot.id, f'/subscriptions/{self.client.config.subscription_id}/resourceGroups/{self.resource_group_name}/providers/Microsoft.BotService/botServices/{self.resource_name}')
+        self.assertEqual(bot.name, self.resource_name)
+        self.assertEqual(bot.location, self.location)
+        self.assertEqual(bot.sku.name, self.sku_name)
+        self.assertEqual(bot.kind, self.kind)
+        self.assertEqual(bot.properties.display_name, self.display_name)
+        self.assertEqual(bot.properties.description, self.description)
+        self.assertEqual(bot.properties.endpoint, self.endpoint)
+        self.assertEqual(bot.properties.msa_app_id, self.msa_app_id)
+        self.assertEqual(bot.properties.developer_app_insight_key, self.developer_app_insight_key)
+        self.assertEqual(bot.properties.developer_app_insights_api_key, None) #this password should not be returned in the response
+        self.assertEqual(bot.properties.developer_app_insights_application_id, self.developer_app_insights_application_id)
+
+    def test_bot_operations(self):
+        bot = self.client.bots.create(
+            resource_group_name = self.resource_group_name,
+            resource_name = self.resource_name,
+            parameters = Bot(
+                location= self.location,
+                sku = sku.Sku(self.sku_name),
+                kind= self.kind,
+                properties= BotProperties(
+                    display_name = self.display_name,
+                    description= self.description,
+                    endpoint = self.endpoint,
+                    msa_app_id = self.msa_app_id,
+                    developer_app_insight_key = self.developer_app_insight_key,
+                    developer_app_insights_api_key = self.developer_app_insights_api_key,
+                    developer_app_insights_application_id = self.developer_app_insights_application_id,
+                )
+            )
+        )
+        self.validate_bot_properties(bot)
+
+        bot = self.client.bots.get(
+            resource_group_name = self.resource_group_name,
+            resource_name = self.resource_name,
+        )
+        self.validate_bot_properties(bot)
+
+        bot.properties.description = 'this is another description'
+        self.description = bot.properties.description
+        bot = self.client.bots.update(
+            resource_group_name = self.resource_group_name,
+            resource_name = self.resource_name,
+            properties = bot.properties
+        )
+        self.validate_bot_properties(bot)
+        bot = self.client.bots.delete(
+            resource_group_name = self.resource_group_name,
+            resource_name = self.resource_name,
+        )
+        validate_bot_properties(self)
+
+        #ensure that the bot was not found with a get
+        with self.assertRaises(ErrorException):
+            bot = self.client.bots.get(
+                resource_group_name = self.resource_group_name,
+                resource_name = self.resource_name
+            )
