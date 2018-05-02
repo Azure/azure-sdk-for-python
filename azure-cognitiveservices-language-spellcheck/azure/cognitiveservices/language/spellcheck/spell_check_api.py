f82666ffc548c2e8a11d9212aaa257ae20a1724f
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-05-02 11:10:11 -0700

    CS with Autorest 3.0.50

--- azure-cognitiveservices-language-spellcheck/azure/cognitiveservices/language/spellcheck/spell_check_api.py
+++ azure-cognitiveservices-language-spellcheck/azure/cognitiveservices/language/spellcheck/spell_check_api.py
@@ -9,7 +9,7 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from msrest.service_client import ServiceClient
+from msrest.service_client import SDKClient
 from msrest import Configuration, Serializer, Deserializer
 from .version import VERSION
 from msrest.pipeline import ClientRawResponse
@@ -42,7 +42,7 @@ class SpellCheckAPIConfiguration(Configuration):
         self.credentials = credentials
 
 
-class SpellCheckAPI(object):
+class SpellCheckAPI(SDKClient):
     """The Spell Check API - V7 lets you check a text string for spelling and grammar errors.
 
     :ivar config: Configuration for client.
@@ -58,7 +58,7 @@ class SpellCheckAPI(object):
             self, credentials, base_url=None):
 
         self.config = SpellCheckAPIConfiguration(credentials, base_url)
-        self._client = ServiceClient(self.config.credentials, self.config)
+        super(SpellCheckAPI, self).__init__(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self.api_version = '1.0'
