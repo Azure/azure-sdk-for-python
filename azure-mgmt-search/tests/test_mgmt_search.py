9b8ab3d5612b817035dc4727118b5d7ea11a2a8d
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-05-08 10:10:16 -0700

    Migrate Search to new test framework

--- azure-mgmt-search/tests/test_mgmt_search.py
+++ azure-mgmt-search/tests/test_mgmt_search.py
@@ -9,9 +9,7 @@ import unittest
 
 import azure.mgmt.search
 from datetime import date, timedelta
-from testutils.common_recordingtestcase import record
-from tests.mgmt_testcase import HttpStatusCode, AzureMgmtTestCase
-
+from devtools_testutils import AzureMgmtTestCase, ResourceGroupPreparer
 
 class MgmtSearchTest(AzureMgmtTestCase):
 
@@ -20,21 +18,19 @@ class MgmtSearchTest(AzureMgmtTestCase):
         self.client = self.create_mgmt_client(
             azure.mgmt.search.SearchManagementClient
         )
-        if not self.is_playback():
-            self.create_resource_group()
 
-    @record
-    def test_search_services(self):
+    @ResourceGroupPreparer()
+    def test_search_services(self, resource_group, location):
         account_name = 'ptvstestsearch'
 
         availability = self.client.services.check_name_availability(account_name)
         self.assertTrue(availability.is_name_available)
 
         service = self.client.services.create_or_update(
-            self.group_name,
+            resource_group.name,
             account_name,
             {
-                'location': self.region,
+                'location': location,
                 'replica_count': 1,
                 'partition_count': 1,
                 'hosting_mode': 'Default',
@@ -49,30 +45,30 @@ class MgmtSearchTest(AzureMgmtTestCase):
         self.assertEquals(availability.reason, "AlreadyExists")
 
         service = self.client.services.get(
-            self.group_name,
+            resource_group.name,
             service.name
         )
         self.assertEquals(service.name, account_name)        
 
-        services = list(self.client.services.list_by_resource_group(self.group_name))
+        services = list(self.client.services.list_by_resource_group(resource_group.name))
         self.assertEquals(len(services), 1)
         self.assertEquals(services[0].name, account_name)
 
         self.client.services.delete(
-            self.group_name,
+            resource_group.name,
             service.name
         )    
 
-    @record
-    def test_search_query_keys(self):
+    @ResourceGroupPreparer()
+    def test_search_query_keys(self, resource_group, location):
         account_name = 'ptvstestquerykeys'
         key_name = 'testkey'
 
         service = self.client.services.create_or_update(
-            self.group_name,
+            resource_group.name,
             account_name,
             {
-                'location': self.region,
+                'location': location,
                 'replica_count': 1,
                 'partition_count': 1,
                 'hosting_mode': 'Default',
@@ -83,14 +79,14 @@ class MgmtSearchTest(AzureMgmtTestCase):
         )
 
         key = self.client.query_keys.create(
-            self.group_name,
+            resource_group.name,
             account_name,
             key_name,
         )
         self.assertEquals(key.name, key_name)
 
         keys = list(self.client.query_keys.list_by_search_service(
-            self.group_name,
+            resource_group.name,
             account_name
         ))
         self.assertEquals(len(keys), 2) # default key and mine
@@ -98,20 +94,20 @@ class MgmtSearchTest(AzureMgmtTestCase):
         self.assertEquals(keys[1].name, key_name)
 
         self.client.query_keys.delete(
-            self.group_name,
+            resource_group.name,
             account_name,
             key_name
         )
 
-    @record
-    def test_search_admin_keys(self):
+    @ResourceGroupPreparer()
+    def test_search_admin_keys(self, resource_group, location):
         account_name = 'ptvstestquerykeys'
 
         service = self.client.services.create_or_update(
-            self.group_name,
+            resource_group.name,
             account_name,
             {
-                'location': self.region,
+                'location': location,
                 'replica_count': 1,
                 'partition_count': 1,
                 'hosting_mode': 'Default',
@@ -122,14 +118,14 @@ class MgmtSearchTest(AzureMgmtTestCase):
         )
 
         admin_keys = self.client.admin_keys.get(
-            self.group_name,
+            resource_group.name,
             account_name
         )
         self.assertIsNotNone(admin_keys.primary_key)
         self.assertIsNotNone(admin_keys.secondary_key)
 
         regenerated_admin_keys = self.client.admin_keys.regenerate(
-            self.group_name,
+            resource_group.name,
             account_name,
             'primary'
         )
