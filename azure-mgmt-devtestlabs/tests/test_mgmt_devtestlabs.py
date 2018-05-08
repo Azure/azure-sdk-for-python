7c8a6bb6ea2aa841ad6fb960ecfe4fe096dee5f3
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-05-08 10:07:01 -0700

    Migrate DevTestLabs to new test framework

--- azure-mgmt-devtestlabs/tests/test_mgmt_devtestlabs.py
+++ azure-mgmt-devtestlabs/tests/test_mgmt_devtestlabs.py
@@ -8,9 +8,7 @@
 import unittest
 
 import azure.mgmt.devtestlabs
-from testutils.common_recordingtestcase import record
-from tests.mgmt_testcase import HttpStatusCode, AzureMgmtTestCase
-
+from devtools_testutils import AzureMgmtTestCase, ResourceGroupPreparer
 
 class MgmtDevTestLabsTest(AzureMgmtTestCase):
 
@@ -19,24 +17,18 @@ class MgmtDevTestLabsTest(AzureMgmtTestCase):
         self.client = self.create_mgmt_client(
             azure.mgmt.devtestlabs.DevTestLabsClient
         )
-        if not self.is_playback():
-            self.create_resource_group()
 
-    @record
-    def test_devtestlabs(self):
+    @ResourceGroupPreparer()
+    def test_devtestlabs(self, resource_group, location):
         lab_name = self.get_resource_name('pylab')
 
         async_lab = self.client.labs.create_or_update(
-            self.group_name,
+            resource_group.name,
             lab_name,
-            {'location': self.region}
+            {'location': location}
         )
         lab = async_lab.result()
         self.assertEqual(lab.name, lab_name)
-            
-            
-
-
 
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
