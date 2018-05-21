87706f26ab88efec6e584d7e3367effb51416d0f
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-05-21 14:06:27 -0700

    Latest Autorest

--- azure-mgmt-cognitiveservices/azure/mgmt/cognitiveservices/cognitive_services_management_client.py
+++ azure-mgmt-cognitiveservices/azure/mgmt/cognitiveservices/cognitive_services_management_client.py
@@ -9,7 +9,7 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from msrest.service_client import ServiceClient
+from msrest.service_client import SDKClient
 from msrest import Serializer, Deserializer
 from msrestazure import AzureConfiguration
 from .version import VERSION
@@ -52,7 +52,7 @@ class CognitiveServicesManagementClientConfiguration(AzureConfiguration):
         self.subscription_id = subscription_id
 
 
-class CognitiveServicesManagementClient(object):
+class CognitiveServicesManagementClient(SDKClient):
     """Cognitive Services Management Client
 
     :ivar config: Configuration for client.
@@ -79,7 +79,7 @@ class CognitiveServicesManagementClient(object):
             self, credentials, subscription_id, base_url=None):
 
         self.config = CognitiveServicesManagementClientConfiguration(credentials, subscription_id, base_url)
-        self._client = ServiceClient(self.config.credentials, self.config)
+        super(CognitiveServicesManagementClient, self).__init__(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self.api_version = '2017-04-18'
