45ef30a63ceb10178ab3dafd0f28adbb692e5110
Laurent Mazuel <lmazuel@microsoft.com>
2018-05-21 16:01:50 -0700

    azure-mgmt-search 2.0.0 (#1958)
    * Automatic PR of restapi_auto_brjohnstmsft_update-search-mgmt-spec into master (#1757)
    
    * Generated from 6a31c5b2391c99143fb762d48d4ebc5120df358d
    
    * Update version.py
    
    * Latest Autorest.Python 3.x
    
    * Adapt Search testing
    
    * azure-mgmt-search 2.0.0 packaging

--- azure-mgmt-search/azure/mgmt/search/models/operation_paged.py
+++ azure-mgmt-search/azure/mgmt/search/models/operation_paged.py
@@ -9,14 +9,19 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from .operations import Operations
-from .admin_keys_operations import AdminKeysOperations
-from .query_keys_operations import QueryKeysOperations
-from .services_operations import ServicesOperations
+from msrest.paging import Paged
 
-__all__ = [
-    'Operations',
-    'AdminKeysOperations',
-    'QueryKeysOperations',
-    'ServicesOperations',
-]
+
+class OperationPaged(Paged):
+    """
+    A paging container for iterating over a list of :class:`Operation <azure.mgmt.search.models.Operation>` object
+    """
+
+    _attribute_map = {
+        'next_link': {'key': 'nextLink', 'type': 'str'},
+        'current_page': {'key': 'value', 'type': '[Operation]'}
+    }
+
+    def __init__(self, *args, **kwargs):
+
+        super(OperationPaged, self).__init__(*args, **kwargs)
