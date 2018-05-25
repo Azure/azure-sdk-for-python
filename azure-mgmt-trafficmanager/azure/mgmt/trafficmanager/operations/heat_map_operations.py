a68d8672060b0be9eb00cbed233deae1956f11ec
Laurent Mazuel <lmazuel@microsoft.com>
2018-05-25 13:45:27 -0700

    azure-mgmt-trafficmanager 0.50.0 (#2038)
    * [TrafficManager] Update (#1597)
    
    * Generated from 342d0f9c112e798f81e6ba344d9d598badba49d4
    
    * Rebuild by https://github.com/Azure/azure-sdk-for-python/pull/1597
    
    * Generated from 522734738313c7d5b08453d1edc8427d89ce2810 (#2479)
    
    Introduce Azure Traffic Manager 2018-03-01 API version with custom headers and status code ranges options.
    
    * Generated from 0a167ffd54481f34ac2101552224462d416857f1 (#2564)
    
    Adding 2018-02-01 Traffic Manager API version.
    
    * Rebuild by https://github.com/Azure/azure-sdk-for-python/pull/2038
    
    * azure-mgmt-trafficmanager packaging
    
    * azure-mgmt-trafficmanager 0.50.0

--- azure-mgmt-trafficmanager/azure/mgmt/trafficmanager/operations/heat_map_operations.py
+++ azure-mgmt-trafficmanager/azure/mgmt/trafficmanager/operations/heat_map_operations.py
@@ -16,13 +16,14 @@ from msrestazure.azure_exceptions import CloudError
 from .. import models
 
 
-class GeographicHierarchiesOperations(object):
-    """GeographicHierarchiesOperations operations.
+class HeatMapOperations(object):
+    """HeatMapOperations operations.
 
     :param client: Client for service requests.
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
+    :ivar heat_map_type: The type of HeatMap for the Traffic Manager profile. Constant value: "default".
     :ivar api_version: Client Api Version. Constant value: "2018-03-01".
     """
 
@@ -33,32 +34,52 @@ class GeographicHierarchiesOperations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
+        self.heat_map_type = "default"
         self.api_version = "2018-03-01"
 
         self.config = config
 
-    def get_default(
-            self, custom_headers=None, raw=False, **operation_config):
-        """Gets the default Geographic Hierarchy used by the Geographic traffic
-        routing method.
+    def get(
+            self, resource_group_name, profile_name, top_left=None, bot_right=None, custom_headers=None, raw=False, **operation_config):
+        """Gets latest heatmap for Traffic Manager profile.
 
+        :param resource_group_name: The name of the resource group containing
+         the Traffic Manager endpoint.
+        :type resource_group_name: str
+        :param profile_name: The name of the Traffic Manager profile.
+        :type profile_name: str
+        :param top_left: The top left latitude,longitude pair of the
+         rectangular viewport to query for.
+        :type top_left: list[float]
+        :param bot_right: The bottom right latitude,longitude pair of the
+         rectangular viewport to query for.
+        :type bot_right: list[float]
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: TrafficManagerGeographicHierarchy or ClientRawResponse if
-         raw=true
-        :rtype:
-         ~azure.mgmt.trafficmanager.models.TrafficManagerGeographicHierarchy or
+        :return: HeatMapModel or ClientRawResponse if raw=true
+        :rtype: ~azure.mgmt.trafficmanager.models.HeatMapModel or
          ~msrest.pipeline.ClientRawResponse
         :raises: :class:`CloudError<msrestazure.azure_exceptions.CloudError>`
         """
         # Construct URL
-        url = self.get_default.metadata['url']
+        url = self.get.metadata['url']
+        path_format_arguments = {
+            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
+            'profileName': self._serialize.url("profile_name", profile_name, 'str'),
+            'heatMapType': self._serialize.url("self.heat_map_type", self.heat_map_type, 'str')
+        }
+        url = self._client.format_url(url, **path_format_arguments)
 
         # Construct parameters
         query_parameters = {}
+        if top_left is not None:
+            query_parameters['topLeft'] = self._serialize.query("top_left", top_left, '[float]', div=',', max_items=2, min_items=2)
+        if bot_right is not None:
+            query_parameters['botRight'] = self._serialize.query("bot_right", bot_right, '[float]', div=',', max_items=2, min_items=2)
         query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
 
         # Construct headers
@@ -83,11 +104,11 @@ class GeographicHierarchiesOperations(object):
         deserialized = None
 
         if response.status_code == 200:
-            deserialized = self._deserialize('TrafficManagerGeographicHierarchy', response)
+            deserialized = self._deserialize('HeatMapModel', response)
 
         if raw:
             client_raw_response = ClientRawResponse(deserialized, response)
             return client_raw_response
 
         return deserialized
-    get_default.metadata = {'url': '/providers/Microsoft.Network/trafficManagerGeographicHierarchies/default'}
+    get.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/trafficmanagerprofiles/{profileName}/heatMaps/{heatMapType}'}
