ed2df7b24c47f83d986bc759155e06473f2b1c71
annatisch <antisch@microsoft.com>
2018-06-25 08:15:20 -0700

    Started adding scenario tests

---
+++ features/steps/eventhub.py
@@ -0,0 +1,13 @@
+from behave import *
+
+@given('we have behave installed')
+def step_impl(context):
+    pass
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
