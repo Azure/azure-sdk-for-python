4f999cfc8c4ebe7ed9c6925cf2baa5ff90036228
Ace Eldeib <aleldeib@microsoft.com>
2018-07-02 14:49:14 -0700

    add tests to validate types

--- azure-loganalytics/tests/test_query.py
+++ azure-loganalytics/tests/test_query.py
@@ -8,15 +8,20 @@ class LogAnalyticsQueryTest(AzureMgmtTestCase):
         self.client = self.create_basic_client(LogAnalyticsDataClient) 
 
     def test_query(self):
-        query = 'AzureActivity | take 10'
+        query = 'Heartbeat | take 10'
         workspace = 'DEMO_WORKSPACE'
         result = self.client.query(workspace, QueryBody(query))
-        
+        print(result.tables[0].columns)
+        print(result.tables[0].rows[0])
         # All queries should return at least a table.
         self.assertGreaterEqual(len(result.tables), 1)
 
-        # AzureActivity table schema has 23 columns.
-        self.assertEqual(len(result.tables[0].columns), 23)
+        # Heartbeat table schema has 29 columns.
+        self.assertEqual(len(result.tables[0].columns), 29)
 
         # The workspace should contain enough data to retrieve 10 rows, as asked
         self.assertEqual(len(result.tables[0].rows), 10)
+
+        # Validate deserialization
+        self.assertIs(type(result.tables[0].rows[0][16]), float)
+        self.assertIs(type(result.tables[0].rows[0][15]), bool)
