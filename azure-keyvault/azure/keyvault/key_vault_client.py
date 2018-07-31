bca73e4ff1b0e82ba9d31fda848d2d1b16634eb5
Scott Schaab <sschaab@microsoft.com>
2018-07-31 10:23:46 -0700

    updates for DP multi-api client

--- azure-keyvault/azure/keyvault/key_vault_client.py
+++ azure-keyvault/azure/keyvault/key_vault_client.py
@@ -8,14 +8,18 @@
 # Changes may cause incorrect behavior and will be lost if the code is
 # regenerated.
 # --------------------------------------------------------------------------
-
-from msrest.service_client import SDKClient
-from msrest import Serializer, Deserializer
+import uuid
+from msrest.pipeline import ClientRawResponse
 from msrestazure import AzureConfiguration
+from msrestazure.azure_active_directory import BasicTokenAuthentication
 
 from azure.profiles import KnownProfiles, ProfileDefinition
 from azure.profiles.multiapiclient import MultiApiClientMixin
 from .version import VERSION
+from . import KeyVaultAuthentication
+
+from azure.keyvault.v7_0.version import VERSION as v7_0_VERSION
+from azure.keyvault.v2016_10_01.version import VERSION as v2016_10_01_VERSION
 
 
 class KeyVaultClientConfiguration(AzureConfiguration):
@@ -43,7 +47,7 @@ class KeyVaultClientConfiguration(AzureConfiguration):
         self.credentials = credentials
 
 
-class KeyVaultClient(MultiApiClientMixin, SDKClient):
+class KeyVaultClient(MultiApiClientMixin):
     """The key vault client performs cryptographic key operations and vault operations against the Key Vault service.
 
     :ivar config: Configuration for client.
@@ -68,13 +72,137 @@ class KeyVaultClient(MultiApiClientMixin, SDKClient):
         _PROFILE_TAG + " latest"
     )
 
+    _init_complete = False
     def __init__(self, credentials, api_version=None, profile=KnownProfiles.default):
         self.config = KeyVaultClientConfiguration(credentials)
-        super(ComputeManagementClient, self).__init__(
-            credentials,
+        self._client_impls = {}
+        super(KeyVaultClient, self).__init__(
             api_version=api_version,
             profile=profile
         )
 
-############ Generated from here ############
+        # if the supplied credentials instance is not derived from KeyVaultAuthBase but is an AAD credential type
+        if not isinstance(credentials, KeyVaultAuthentication) and isinstance(credentials, BasicTokenAuthentication):
+
+            # wrap the supplied credentials with a KeyVaultAuthentication instance. Use that for the credentials
+            # supplied to the base client
+            credentials = KeyVaultAuthentication(credentials=credentials)
+
+        self._credentials = credentials
+        self._init_complete = True
+
+    @property
+    def models(self):
+        api_version = self._get_api_version(None)
+
+        if api_version == v7_0_VERSION:
+            from azure.keyvault.v7_0 import models as implModels
+        elif api_version == v2016_10_01_VERSION:
+            from azure.keyvault.v2016_10_01 import models as implModels
+        else:
+            raise ValueError('unrecognized api_version')
+        return implModels
+
+    def _get_client_impl(self):
+        api_version = self._get_api_version(None)
+        if api_version not in self._client_impls:
+            self._client_impls[api_version] = self._create_client_impl(api_version)
+        return self._client_impls[api_version]
+
+    def _create_client_impl(self, api_version):
+        if api_version == v7_0_VERSION:
+            from azure.keyvault.v7_0 import KeyVaultClient as ImplClient
+        elif api_version == v2016_10_01_VERSION:
+            from azure.keyvault.v2016_10_01 import KeyVaultClient as ImplClient
+        else:
+            raise ValueError('unrecognized api_version')
+
+        impl = ImplClient(credentials=self._credentials)
+        if hasattr(impl, '__enter__'):
+            impl.__enter__()
+        impl.config = self.config
+
+        return impl
+
+    def __enter__(self, *args, **kwargs):
+        for _, impl in self._client_impls.items():
+            if hasattr(impl, '__enter__'):
+                impl.__enter__(*args, **kwargs)
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        for _, impl in self._client_impls.items():
+            if hasattr(impl, '__exit__'):
+                impl.__exit__(*args, **kwargs)
+        return self
+
+    def __getattr__(self, name, *args, **kwargs):
+        impl = self._get_client_impl()
+        return getattr(impl, name)
+
+    def __setattr__(self, name, attr):
+        if self._init_complete and not hasattr(self, name):
+            impl = self._get_client_impl()
+            setattr(impl, name, attr)
+        else:
+            super(KeyVaultClient, self).__setattr__(name, attr)
+
+    def get_pending_certificate_signing_request(self, vault_base_url, certificate_name, custom_headers=None, raw=False, **operation_config):
+        """Gets the Base64 pending certificate signing request (PKCS-10).
+
+        :param vault_base_url: The vault name, e.g.
+         https://myvault.vault.azure.net
+        :type vault_base_url: str
+        :param certificate_name: The name of the certificate
+        :type certificate_name: str
+        :param dict custom_headers: headers that will be added to the request
+        :param bool raw: returns the direct response alongside the
+         deserialized response
+        :param operation_config: :ref:`Operation configuration
+         overrides<msrest:optionsforoperations>`.
+        :return: Base64 encoded pending certificate signing request (PKCS-10).
+        :rtype: str
+        :rtype: :class:`ClientRawResponse<msrest.pipeline.ClientRawResponse>`
+         if raw=true
+        """
+        # Construct URL
+        url = '/certificates/{certificate-name}/pending'
+        path_format_arguments = {
+            'vaultBaseUrl': self._serialize.url("vault_base_url", vault_base_url, 'str', skip_quote=True),
+            'certificate-name': self._serialize.url("certificate_name", certificate_name, 'str')
+        }
+        url = self._client.format_url(url, **path_format_arguments)
+
+        # Construct parameters
+        query_parameters = {}
+        query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
+
+        # Construct headers
+        header_parameters = {}
+        header_parameters['Content-Type'] = 'application/json; charset=utf-8'
+        header_parameters['Accept'] = 'application/pkcs10'
+        if self.config.generate_client_request_id:
+            header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
+        if custom_headers:
+            header_parameters.update(custom_headers)
+        if self.config.accept_language is not None:
+            header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
+
+        # Construct and send request
+        request = self._client.get(url, query_parameters)
+        response = self._client.send(request, header_parameters, **operation_config)
+
+        if response.status_code not in [200]:
+            raise self.models.KeyVaultErrorException(self._deserialize, response)
+
+        deserialized = None
+
+        if response.status_code == 200:
+            deserialized = response.content
+
+        if raw:
+            client_raw_response = ClientRawResponse(deserialized, response)
+            return client_raw_response
+
+        return deserialized
 
