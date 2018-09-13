adca4f4b4c89182632868376e7e8305d769317c0
Laurent Mazuel <lmazuel@microsoft.com>
2018-09-13 15:46:17 -0700

    Rdbms1.3.0 (#3340)
    * Generated from 3ef310e770e8238acf53b192ef2a19295a7e3302 (#3150) (#3182)
    
    Add serverSecurityAlertPolicies APIs for MySQL servers
    
    * Generated from 16bfdf572dc08bae96c0b6ee521c1524b19254d7 (#2875) (#2876)
    
    Add support for PG 10
    
    * [AutoPR] mariadb/resource-manager (#3332)
    
    * [AutoPR mariadb/resource-manager] Add mariadb swagger spec (#3257)
    
    * Generated from d76559bb32dbc6be98d3f9cc774377be21694188
    
    Add mariadb swagger spec
    
    * Generated from 74e04a40cc63c0e6634cce5256bc660b8986132e
    
    Fix readme and set properties
    
    * Packaging update of azure-mgmt-rdbms
    
    * Update version.py
    
    * 1.3.0

--- azure-mgmt-rdbms/azure/mgmt/rdbms/mysql/my_sql_management_client.py
+++ azure-mgmt-rdbms/azure/mgmt/rdbms/mysql/my_sql_management_client.py
@@ -21,6 +21,7 @@ from .operations.configurations_operations import ConfigurationsOperations
 from .operations.log_files_operations import LogFilesOperations
 from .operations.location_based_performance_tier_operations import LocationBasedPerformanceTierOperations
 from .operations.check_name_availability_operations import CheckNameAvailabilityOperations
+from .operations.server_security_alert_policies_operations import ServerSecurityAlertPoliciesOperations
 from .operations.operations import Operations
 from . import models
 
@@ -80,6 +81,8 @@ class MySQLManagementClient(SDKClient):
     :vartype location_based_performance_tier: azure.mgmt.rdbms.mysql.operations.LocationBasedPerformanceTierOperations
     :ivar check_name_availability: CheckNameAvailability operations
     :vartype check_name_availability: azure.mgmt.rdbms.mysql.operations.CheckNameAvailabilityOperations
+    :ivar server_security_alert_policies: ServerSecurityAlertPolicies operations
+    :vartype server_security_alert_policies: azure.mgmt.rdbms.mysql.operations.ServerSecurityAlertPoliciesOperations
     :ivar operations: Operations operations
     :vartype operations: azure.mgmt.rdbms.mysql.operations.Operations
 
@@ -118,6 +121,8 @@ class MySQLManagementClient(SDKClient):
         self.location_based_performance_tier = LocationBasedPerformanceTierOperations(
             self._client, self.config, self._serialize, self._deserialize)
         self.check_name_availability = CheckNameAvailabilityOperations(
+            self._client, self.config, self._serialize, self._deserialize)
+        self.server_security_alert_policies = ServerSecurityAlertPoliciesOperations(
             self._client, self.config, self._serialize, self._deserialize)
         self.operations = Operations(
             self._client, self.config, self._serialize, self._deserialize)
