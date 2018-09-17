cd91466c06ced3064a3eaae1dde8210dc282876c
azuresdkci <azuresdkcheckins@gmail.com>
2018-09-17 19:56:28 +0000

    Packaging update of azure-mgmt-alertsmanagement

--- azure-mgmt-alertsmanagement/setup.py
+++ azure-mgmt-alertsmanagement/setup.py
@@ -19,7 +19,7 @@ except ImportError:
 
 # Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-mgmt-alertsmanagement"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_PPRINT_NAME = "Alerts Management"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
