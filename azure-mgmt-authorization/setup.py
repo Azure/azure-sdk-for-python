0d543500b0a5e5346de734dd845c90570cacea45
azuresdkci <azuresdkcheckins@gmail.com>
2018-09-18 20:08:33 +0000

    Packaging update of azure-mgmt-authorization

--- azure-mgmt-authorization/setup.py
+++ azure-mgmt-authorization/setup.py
@@ -72,13 +72,15 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
     ],
     zip_safe=False,
     packages=find_packages(exclude=["tests"]),
     install_requires=[
-        'msrestazure>=0.4.27,<2.0.0',
-        'azure-common~=1.1,>=1.1.12',
+        'msrest>=0.5.0',
+        'msrestazure>=0.4.32,<2.0.0',
+        'azure-common~=1.1',
     ],
     cmdclass=cmdclass
 )
