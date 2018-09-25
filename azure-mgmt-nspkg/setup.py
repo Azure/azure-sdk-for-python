44e09c3da888e96096771596018740590a17cdfd
Laurent Mazuel <lmazuel@microsoft.com>
2018-09-25 10:05:10 -0700

    azure nspkg 3.0 (#3412)
    * Main azure nspkg 3.0
    
    * Adapt dev_setup to new nspkg
    
    * New nspkg system
    
    * Auto-update parameter for toml
    
    * Common with new packaging style
    
    * Kill install old nspkg
    
    * Ignore packaging for bundle package
    
    * Don't auto-update Cognitive Services nspkg
    
    * azure-nspkg no auto-update
    
    * Cognitive Services new packaging
    
    * Dataplane new packaging
    
    * Never auto-update the nspkg
    
    * All packages which didn't have toml file
    
    * Manual pkgutil for not auto-update
    
    * Batch commit all package with already a toml
    
    * azure-nspkg 3.0.0 and dev_setup for easy backward compat
    
    * Python ignore requires on Py3
    
    * Ignore required for all packages
    
    * Uninstall azure-nspkg
    
    * Install nspkg in wheel mode, not editable
    
    * Improve gitignore
    
    * Cognitive Services new nspkg
    
    * Improve azure-mgmt-nspkg

--- azure-mgmt-nspkg/setup.py
+++ azure-mgmt-nspkg/setup.py
@@ -25,31 +25,26 @@ except ImportError:
 
 setup(
     name='azure-mgmt-nspkg',
-    version='2.0.0',
+    version='3.0.0',
     description='Microsoft Azure Resource Management Namespace Package [Internal]',
     long_description=open('README.rst', 'r').read(),
     license='MIT License',
     author='Microsoft Corporation',
-    author_email='ptvshelp@microsoft.com',
+    author_email='azpysdkhelp@microsoft.com',
     url='https://github.com/Azure/azure-sdk-for-python',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
     ],
     zip_safe=False,
     packages=[
-        'azure',
         'azure.mgmt',
     ],
+    python_requires='<3',
     install_requires=[
-        'azure-nspkg>=2.0.0',
+        'azure-nspkg>=3.0.0',
     ]
 )
