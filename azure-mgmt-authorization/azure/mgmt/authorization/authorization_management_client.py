227545a55d320d01c3d70f37b169f4155f4f007d
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-11-08 14:57:29 -0800

    Hub for role_assignments 2018-09-01-preview

--- azure-mgmt-authorization/azure/mgmt/authorization/authorization_management_client.py
+++ azure-mgmt-authorization/azure/mgmt/authorization/authorization_management_client.py
@@ -78,12 +78,13 @@ class AuthorizationManagementClient(MultiApiClientMixin, SDKClient):
     :type profile: azure.profiles.KnownProfiles
     """
 
-    DEFAULT_API_VERSION='2018-09-01-preview'
+    DEFAULT_API_VERSION='2018-01-01-preview'
     _PROFILE_TAG = "azure.mgmt.authorization.AuthorizationManagementClient"
     LATEST_PROFILE = ProfileDefinition({
         _PROFILE_TAG: {
             'classic_administrators': '2015-06-01',
             'deny_assignments': '2018-07-01-preview',
+            'role_assignments': '2018-09-01-preview',
             None: DEFAULT_API_VERSION
         }},
         _PROFILE_TAG + " latest"
