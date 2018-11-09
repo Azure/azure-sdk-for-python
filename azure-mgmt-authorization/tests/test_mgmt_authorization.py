52c3ace0517d565ff65b060aef5522c059b4e1fa
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-11-09 15:25:20 -0800

    Add role assignments tests

--- azure-mgmt-authorization/tests/test_mgmt_authorization.py
+++ azure-mgmt-authorization/tests/test_mgmt_authorization.py
@@ -38,6 +38,31 @@ class MgmtAuthorizationTest(AzureMgmtTestCase):
         ))
         assert len(roles) == 1
 
+    @ResourceGroupPreparer()
+    def test_role_assignment(self, resource_group, location):
+        contributor_id = "b24988ac-6180-42a0-ab88-20f7382dd24c"  # Reserved UUID for contributor on Azure
+
+        contributor_full_id = "/subscriptions/{}/providers/Microsoft.Authorization/roleDefinitions/{}".format(
+            self.settings.SUBSCRIPTION_ID,
+            contributor_id
+        )
+
+        role_name = "ddfe2dc0-9858-442f-aca3-ac215947a815"  # Consistent for testing, but not significant
+
+        role_assignment = self.authorization_client.role_assignments.create(
+            resource_group.id,
+            role_name,
+            {
+                'role_definition_id': contributor_full_id,
+                'principal_id': '6d33bfc8-e476-11e8-915d-f2801f1b9fd1'  # Should be service principal object ID
+            }
+        )
+
+        self.authorization_client.role_assignments.delete(
+            resource_group.id,
+            role_assignment.name,
+        )
+
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
     unittest.main()
