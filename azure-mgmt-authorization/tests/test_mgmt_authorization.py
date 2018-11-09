12a5e11b89f0edd715196793ccfb7876e373fbf4
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-11-09 13:39:54 -0800

    Add role definitions tests

--- azure-mgmt-authorization/tests/test_mgmt_authorization.py
+++ azure-mgmt-authorization/tests/test_mgmt_authorization.py
@@ -23,12 +23,20 @@ class MgmtAuthorizationTest(AzureMgmtTestCase):
         permissions = self.authorization_client.permissions.list_for_resource_group(
             resource_group.name
         )
-        
+
         permissions = list(permissions)
         self.assertEqual(len(permissions), 1)
         self.assertEqual(permissions[0].actions[0], '*')
 
-
+    @ResourceGroupPreparer()
+    def test_role_definitions(self, resource_group, location):
+        # Get "Contributor" built-in role as a RoleDefinition object
+        role_name = 'Contributor'
+        roles = list(self.authorization_client.role_definitions.list(
+            resource_group.id,
+            filter="roleName eq '{}'".format(role_name)
+        ))
+        assert len(roles) == 1
 
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
