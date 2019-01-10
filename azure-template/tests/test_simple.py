6f16b4cfc6a7790ace5a6dea323577c9c5042d04
scbedd <45376673+scbedd@users.noreply.github.com>
2019-01-10 15:39:41 -0800

    keeping test template simple

--- azure-template/tests/test_simple.py
+++ azure-template/tests/test_simple.py
@@ -1,7 +1,5 @@
 # the inclusion of the tests module is not meant to offer best practices for
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
-
-
-def test_success():
+def default_test_case():
     assert True
