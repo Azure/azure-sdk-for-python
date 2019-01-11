4cf91734ce9a4de4a6ea1ce143c33eba6e3e3aba
scbedd <45376673+scbedd@users.noreply.github.com>
2019-01-10 16:22:19 -0800

    adding a bunch of settings

--- azure-template/tests/test_simple.py
+++ azure-template/tests/test_simple.py
@@ -5,7 +5,7 @@ import unittest
 
 class TestAll(unittest.TestCase):
     def test_case_default(self):
-        assert True
+        self.assertEqual(True, True)
 
 if __name__ == '__main__':
     unittest.main()
