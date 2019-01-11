ae0e515440421112a77eb7d9d9ecbdf70f666149
scbedd <45376673+scbedd@users.noreply.github.com>
2019-01-10 16:24:31 -0800

    tests are now reflecting coverage properly

--- azure-template/tests/test_simple.py
+++ azure-template/tests/test_simple.py
@@ -1,11 +1,11 @@
 # the inclusion of the tests module is not meant to offer best practices for
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
-import unittest
+from devtools_testutils import AzureMgmtTestCase
 
-class TestAll(unittest.TestCase):
+class TemplateTest(AzureMgmtTestCase):
+    def setUp(self):
+        super(TemplateTest, self).setUp()
+
     def test_case_default(self):
         self.assertEqual(True, True)
-
-if __name__ == '__main__':
-    unittest.main()
