5f19b91dacd5b2432c823428795bb45ad63b563c
scbedd <45376673+scbedd@users.noreply.github.com>
2019-01-14 13:55:51 -0800

    removing import error in script

--- scripts/devops_tasks/devops_test_version.py
+++ scripts/devops_tasks/devops_test_version.py
@@ -1,6 +1,26 @@
-from devops_common_tasks import *
 import argparse
 import sys
+
+DEFAULT_TARGETED_PROJECTS = ['azure-keyvault']
+
+def process_glob_string(glob_string, target_root_dir):
+    individual_globs = glob_string.split(',')
+    collected_top_level_directories = []
+
+    for glob_string in individual_globs:
+        globbed = glob.glob(os.path.join(target_root_dir, glob_string, 'setup.py'))
+        collected_top_level_directories.extend([os.path.dirname(p) for p in globbed])
+
+    # dedup, in case we have double coverage from the glob strings. Example: "azure-mgmt-keyvault,azure-mgmt-*"
+    return list(set(collected_top_level_directories))
+
+def run_check_call(command_array, working_directory):
+    print('Command Array: {0}, Target Working Directory: {1}'.format(command_array, working_directory))
+    try:
+        check_call(command_array, cwd = working_directory)
+    except CalledProcessError as err:
+        print(err, file = sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Build Azure Packages, Called from DevOps YAML Pipeline')
