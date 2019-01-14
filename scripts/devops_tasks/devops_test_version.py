fcf1f53a4354c6ec4a547e8156f1d6e1e041ceef
scbedd <45376673+scbedd@users.noreply.github.com>
2019-01-14 12:03:34 -0800

    actually correcting debugging script

--- scripts/devops_tasks/devops_test_version.py
+++ scripts/devops_tasks/devops_test_version.py
@@ -1,5 +1,6 @@
 from devops_common_tasks import *
-
+import argparse
+import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Build Azure Packages, Called from DevOps YAML Pipeline')
@@ -18,7 +19,10 @@ if __name__ == '__main__':
         help = 'The name of the python that should run the build. This is for usage in special cases like in /.azure-pipelines/specialcase.test.yml. Defaults to "python"')
 
     args = parser.parse_args()
-    targeted_packages = process_glob_string(args.glob_string, root_dir)
+
+    here = os.getcwd()
+
+    targeted_packages = process_glob_string(args.glob_string, here)
 
     print('This is the sys.version: {}'.format(sys.version))
-    run_check_call([args.python_version, '--version', os.getcwd())
+    run_check_call([args.python_version, '--version'], here)
