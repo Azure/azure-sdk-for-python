fb140e4a3e26b961468097a9e96a5fe0e29ed1a3
leonard <leonard.francis@netapp.com>
2019-01-17 13:14:09 +0000

    NFSAAS-1705 add tests

---
+++ azure-mgmt-netapp/tests/accounttests.py
@@ -0,0 +1,86 @@
+import json
+import time
+from azure.mgmt.resource import ResourceManagementClient
+from devtools_testutils import AzureMgmtTestCase
+import azure.mgmt.netapp.models
+from azure.mgmt.netapp.models import NetAppAccountPatch
+from setup import *
+
+accounts = [TEST_ACC_1, TEST_ACC_2]
+
+
+def create_account(client, rg, acc_name, location=LOCATION):
+    account = client.accounts.create_or_update(
+        rg,
+        acc_name,
+        location
+    ).result()
+
+    return account
+
+def wait_for_no_account(client, rg, acc_name):
+    # a workaround for the async nature of certain ARM processes
+    co=0
+    while co<5:
+        co += 1
+        time.sleep(2)
+        try:
+            account = client.accounts.get(rg, acc_name)
+        except:
+            # not found is an exception case (status code 200 expected)
+            # and is actually what we are waiting for
+            break
+
+def delete_account(client, rg, acc_name):
+    client.accounts.delete(rg, acc_name).wait()
+    wait_for_no_account(client, rg, acc_name)
+
+
+class NetAppAccountTestCase(AzureMgmtTestCase):
+    def setUp(self):
+        super(NetAppAccountTestCase, self).setUp()
+        self.client = self.create_mgmt_client(azure.mgmt.netapp.AzureNetAppFilesManagementClient)
+
+    def test_create_delete_account(self):
+        account = create_account(self.client, TEST_RG, TEST_ACC_1)
+        self.assertEqual(account.name, TEST_ACC_1)
+
+        account_list = self.client.accounts.list(TEST_RG)
+        self.assertEqual(len(list(account_list)), 1)
+
+        delete_account(self.client, TEST_RG, TEST_ACC_1)
+        account_list = self.client.accounts.list(TEST_RG)
+        self.assertEqual(len(list(account_list)), 0)
+
+    def test_list_accounts(self):
+        account = create_account(self.client, TEST_RG, TEST_ACC_1)
+        account = create_account(self.client, TEST_RG, TEST_ACC_2)
+
+        account_list = self.client.accounts.list(TEST_RG)
+        self.assertEqual(len(list(account_list)), 2)
+        idx = 0
+        for account in account_list:
+            self.assertEqual(account.name, accounts[idx])
+            idx += 1
+
+        delete_account(self.client, TEST_RG, TEST_ACC_1)
+        delete_account(self.client, TEST_RG, TEST_ACC_2)
+
+    def test_get_account_by_name(self):
+        create_account(self.client, TEST_RG, TEST_ACC_1)
+
+        account = self.client.accounts.get(TEST_RG, TEST_ACC_1)
+        self.assertEqual(account.name, TEST_ACC_1)
+
+        delete_account(self.client, TEST_RG, TEST_ACC_1)
+
+    def test_patch_account(self):
+        create_account(self.client, TEST_RG, TEST_ACC_1)
+        tag = {'Tag1', 'Value1'}
+
+        netapp_account_patch = NetAppAccountPatch(tags=tag)
+        account = self.client.accounts.update(TEST_RG, TEST_ACC_1, tags=netapp_account_patch)
+        self.assertTrue("'Tag1', 'Value1'" in account.tags['tags'])
+
+        delete_account(self.client, TEST_RG, TEST_ACC_1)
+
---
+++ azure-mgmt-netapp/tests/setup.py
@@ -0,0 +1,22 @@
+# resource constants
+# some minor setup required to run in record mode
+#  - actual subsid to be used
+#  - rg must exist
+#  - vnet must exist
+
+TEST_RG='sdk-py-test-rg'
+TEST_ACC_1='sdk-py-test-acc-1'
+TEST_ACC_2='sdk-py-test-acc-2'
+TEST_POOL_1='sdk-py-test-pool-1'
+TEST_POOL_2='sdk-py-test-pool-2'
+TEST_VOL_1='sdk-py-test-vol-1'
+TEST_VOL_2='sdk-py-test-vol-2'
+TEST_SNAPSHOT_1='sdk-py-test-snapshot-1'
+TEST_SNAPSHOT_2='sdk-py-test-snapshot-2'
+DEFAULT_SIZE=4398046511104
+GIGABYTE=1024 * 1024 * 1024
+SUBSID='subsid'
+SERVICE_LEVEL='Premium'
+LOCATION='westus2'
+VNET='sdk-py-test-rg-vnet'
+SERVICE_LEVEL='Premium'
