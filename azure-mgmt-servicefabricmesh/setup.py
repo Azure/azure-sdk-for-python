4e93570cd6399d65cebb46ef72459ff46894ecdc
azuresdkci <azuresdkcheckins@gmail.com>
2019-02-16 00:25:24 +0000

    Packaging update of azure-mgmt-servicefabricmesh

--- azure-mgmt-servicefabricmesh/setup.py
+++ azure-mgmt-servicefabricmesh/setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages, setup
 
 # Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-mgmt-servicefabricmesh"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_PPRINT_NAME = "Service Fabric Mesh"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
