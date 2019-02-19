33398a8b2e2b50664c893d22c064d9e5a9f7479f
azuresdkci <azuresdkcheckins@gmail.com>
2019-02-19 17:59:17 +0000

    Packaging update of azure-mgmt-databox

--- azure-mgmt-databox/setup.py
+++ azure-mgmt-databox/setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages, setup
 
 # Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-mgmt-databox"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_PPRINT_NAME = "Data Box"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
