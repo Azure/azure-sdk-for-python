2beb99df523a887db70808a59e3ee2de6d0c30ab
Laurent Mazuel <lmazuel@microsoft.com>
2019-03-01 13:25:39 -0800

    Update setup.py

--- azure-cognitiveservices-vision-nspkg/setup.py
+++ azure-cognitiveservices-vision-nspkg/setup.py
@@ -32,7 +32,7 @@ if sys.version_info[0] < 3:
 
 setup(
     name='azure-cognitiveservices-vision-nspkg',
-    version='3.0.0',
+    version='3.0.1',
     description='Microsoft Azure Cognitive Services Vision Namespace Package [Internal]',
     long_description=open('README.rst', 'r').read(),
     license='MIT License',
