650b83c1ee00231d6392a44b3086d2a7e1e3e6fa
annatisch <antisch@microsoft.com>
2019-03-07 14:01:54 -0800

    Azure core first pass

---
+++ azure-core/azure/core/pipeline/transport/base.py
@@ -0,0 +1,271 @@
+# --------------------------------------------------------------------------
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the ""Software""), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+# --------------------------------------------------------------------------
+from __future__ import absolute_import
+import abc
+import json
+import logging
+import os
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse
+import xml.etree.ElementTree as ET
+
+# This file is NOT using any "requests" HTTP implementation
+# However, the CaseInsensitiveDict is handy.
+# If one day we reach the point where "requests" can be skip totally,
+# might provide our own implementation
+from requests.structures import CaseInsensitiveDict
+
+from typing import TYPE_CHECKING, Generic, TypeVar, cast, IO, List, Union, Any, Mapping, Dict, Optional, Tuple, Callable, Iterator  # pylint: disable=unused-import
+from azure.core.exceptions import ClientRequestError
+from azure.core.pipeline import ABC, AbstractContextManager
+
+HTTPResponseType = TypeVar("HTTPResponseType")
+HTTPRequestType = TypeVar("HTTPRequestType")
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class TransportConfiguration(object):
+    """HTTP transport connection configuration settings."""
+
+    def __init__(self, **kwargs):
+        self.timeout = kwargs.pop('connection_timeout', 100)
+        self.verify = kwargs.pop('connection_verify', True)
+        self.cert = kwargs.pop('connection_cert', None)
+        self.data_block_size = kwargs.pop('connection_data_block_size', 4096)
+        self.keep_alive = kwargs.pop('connection_keep_alive', False)
+
+    def __iter__(self):
+        dict_values = dict(self.__dict__)
+        for attr, value in dict_values.items():
+            yield "connection_" + attr, value
+
+    def __call__(self):
+        # type: () -> Dict[str, Union[str, int]]
+        """Return configuration to be applied to connection."""
+        debug = "Configuring request: timeout=%r, verify=%r, cert=%r"
+        _LOGGER.debug(debug, self.timeout, self.verify, self.cert)
+        return {'timeout': self.timeout,
+                'verify': self.verify,
+                'cert': self.cert}
+
+
+class HttpTransport(AbstractContextManager, ABC, Generic[HTTPRequestType, HTTPResponseType]):
+    """An http sender ABC.
+    """
+
+    @abc.abstractmethod
+    def send(self, request, **config):
+        # type: (PipelineRequest[HTTPRequestType], Any) -> PipelineResponse[HTTPRequestType, HTTPResponseType]
+        """Send the request using this HTTP sender.
+        """
+        pass
+
+    def build_context(self):
+        # type: () -> Any
+        """Allow the sender to build a context that will be passed
+        across the pipeline with the request.
+
+        Return type has no constraints. Implementation is not
+        required and None by default.
+        """
+        return None
+
+
+class HttpRequest(object):
+    """Represents a HTTP request.
+
+    URL can be given without query parameters, to be added later using "format_parameters".
+
+    Instance can be created without data, to be added later using "add_content"
+
+    Instance can be created without files, to be added later using "add_formdata"
+
+    :param str method: HTTP method (GET, HEAD, etc.)
+    :param str url: At least complete scheme/host/path
+    :param dict[str,str] headers: HTTP headers
+    :param files: Files list.
+    :param data: Body to be sent.
+    :type data: bytes or str.
+    """
+    def __init__(self, method, url, headers=None, files=None, data=None):
+        # type: (str, str, Mapping[str, str], Any, Any) -> None
+        self.method = method
+        self.url = url
+        self.headers = CaseInsensitiveDict(headers)
+        self.files = files
+        self.data = data
+
+    def __repr__(self):
+        return '<HttpRequest [%s]>' % (self.method)
+
+    @property
+    def body(self):
+        """Alias to data."""
+        return self.data
+
+    @body.setter
+    def body(self, value):
+        self.data = value
+
+    def format_parameters(self, params):
+        # type: (Dict[str, str]) -> None
+        """Format parameters into a valid query string.
+        It's assumed all parameters have already been quoted as
+        valid URL strings.
+
+        :param dict params: A dictionary of parameters.
+        """
+        query = urlparse(self.url).query
+        if query:
+            self.url = self.url.partition('?')[0]
+            existing_params = {
+                p[0]: p[-1]
+                for p in [p.partition('=') for p in query.split('&')]
+            }
+            params.update(existing_params)
+        query_params = ["{}={}".format(k, v) for k, v in params.items()]
+        query = '?' + '&'.join(query_params)
+        self.url = self.url + query
+
+    def add_content(self, data):
+        # type: (Optional[Union[Dict[str, Any], ET.Element]]) -> None
+        """Add a body to the request.
+
+        :param data: Request body data, can be a json serializable
+         object (e.g. dictionary) or a generator (e.g. file data).
+        """
+        if data is None:
+            return
+
+        if isinstance(data, ET.Element):
+            bytes_data = ET.tostring(data, encoding="utf8")
+            self.headers['Content-Length'] = str(len(bytes_data))
+            self.data = bytes_data
+            return
+
+        # By default, assume JSON
+        try:
+            self.data = json.dumps(data)
+            self.headers['Content-Length'] = str(len(self.data))
+        except TypeError:
+            self.data = data
+
+    @staticmethod
+    def _format_data(data):
+        # type: (Union[str, IO]) -> Union[Tuple[None, str], Tuple[Optional[str], IO, str]]
+        """Format field data according to whether it is a stream or
+        a string for a form-data request.
+
+        :param data: The request field data.
+        :type data: str or file-like object.
+        """
+        if hasattr(data, 'read'):
+            data = cast(IO, data)
+            data_name = None
+            try:
+                if data.name[0] != '<' and data.name[-1] != '>':
+                    data_name = os.path.basename(data.name)
+            except (AttributeError, TypeError):
+                pass
+            return (data_name, data, "application/octet-stream")
+        return (None, cast(str, data))
+
+    def add_formdata(self, content=None):
+        # type: (Optional[Dict[str, str]]) -> None
+        """Add data as a multipart form-data request to the request.
+
+        We only deal with file-like objects or strings at this point.
+        The requests is not yet streamed.
+
+        :param dict headers: Any headers to add to the request.
+        :param dict content: Dictionary of the fields of the formdata.
+        """
+        if content is None:
+            content = {}
+        content_type = self.headers.pop('Content-Type', None) if self.headers else None
+
+        if content_type and content_type.lower() == 'application/x-www-form-urlencoded':
+            # Do NOT use "add_content" that assumes input is JSON
+            self.data = {f: d for f, d in content.items() if d is not None}
+        else: # Assume "multipart/form-data"
+            self.files = {f: self._format_data(d) for f, d in content.items() if d is not None}
+
+
+class _HttpResponseBase(object):
+    """Represent a HTTP response.
+
+    No body is defined here on purpose, since async pipeline
+    will provide async ways to access the body
+    Full in-memory using "body" as bytes.
+    """
+    def __init__(self, request, internal_response):
+        # type: (HttpRequest, Any) -> None
+        self.request = request
+        self.internal_response = internal_response
+        self.status_code = None  # type: Optional[int]
+        self.headers = {}  # type: Dict[str, str]
+        self.reason = None  # type: Optional[str]
+
+    def body(self):
+        # type: () -> bytes
+        """Return the whole body as bytes in memory.
+        """
+        pass
+
+    def text(self, encoding=None):
+        # type: (str) -> str
+        """Return the whole body as a string.
+
+        :param str encoding: The encoding to apply. If None, use "utf-8".
+         Implementation can be smarter if they want (using headers).
+        """
+        return self.body().decode(encoding or "utf-8")
+
+    def raise_for_status(self):
+        """Raise for status. Should be overriden, but basic implementation provided.
+        """
+        if self.status_code >= 400:
+            raise ClientRequestError("Received status code {}".format(self.status_code))
+
+
+class HttpResponse(_HttpResponseBase):
+
+
+    def stream_download(self, chunk_size=None, callback=None):
+
+        # type: (Optional[int], Optional[Callable]) -> Iterator[bytes]
+        """Generator for streaming request body data.
+
+        Should be implemented by sub-classes if streaming download
+        is supported.
+
+        :param callback: Custom callback for monitoring progress.
+        :param int chunk_size:
+        """
+        pass
---
+++ azure-core/azure/core/pipeline/transport/requests_basic.py
@@ -0,0 +1,179 @@
+# --------------------------------------------------------------------------
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the ""Software""), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+# --------------------------------------------------------------------------
+from __future__ import absolute_import
+import contextlib
+import requests
+import threading
+
+from .base import (
+    HttpTransport,
+    HttpResponse,
+    _HttpResponseBase
+)
+
+from azure.core.exceptions import (
+    ClientRequestError,
+    TokenExpiredError,
+    TokenInvalidError,
+    AuthenticationError,
+    raise_with_traceback
+)
+
+# Matching requests, because why not?
+CONTENT_CHUNK_SIZE = 10 * 1024
+
+class RequestsContext(object):
+    def __init__(self, session):
+        self.session = session
+
+
+class _RequestsTransportResponseBase(_HttpResponseBase):
+
+    def __init__(self, request, requests_response):
+        super(_RequestsTransportResponseBase, self).__init__(request, requests_response)
+        self.status_code = requests_response.status_code
+        self.headers = requests_response.headers
+        self.reason = requests_response.reason
+
+    def body(self):
+        return self.internal_response.content
+
+    def text(self, encoding=None):
+        if encoding:
+            self.internal_response.encoding = encoding
+        return self.internal_response.text
+
+    def raise_for_status(self):
+        self.internal_response.raise_for_status()
+
+
+class RequestsTransportResponse(_RequestsTransportResponseBase, HttpResponse):
+
+    def stream_download(self, chunk_size=None, callback=None):
+        # type: (Optional[int], Optional[Callable]) -> Iterator[bytes]
+        """Generator for streaming request body data.
+
+        :param callback: Custom callback for monitoring progress.
+        :param int chunk_size:
+        """
+        chunk_size = chunk_size or CONTENT_CHUNK_SIZE
+        with contextlib.closing(self.internal_response) as response:
+            # https://github.com/PyCQA/pylint/issues/1437
+            for chunk in response.iter_content(chunk_size):  # pylint: disable=no-member
+                if not chunk:
+                    break
+                if callback and callable(callback):
+                    callback(chunk, response=response)
+                yield chunk
+
+
+class RequestsTransport(HttpTransport):
+    """Implements a basic requests HTTP sender.
+
+    Since requests team recommends to use one session per requests, you should
+    not consider this class as thread-safe, since it will use one Session
+    per instance.
+
+    In this simple implementation:
+    - You provide the configured session if you want to, or a basic session is created.
+    - All kwargs received by "send" are sent to session.request directly
+    """
+
+    _protocols = ['http://', 'https://']
+
+    def __init__(self, configuration=None, session=None):
+        # type: (Optional[requests.Session]) -> None
+        self._session_mapping = threading.local()
+        self.config = configuration
+        self.session = session or requests.Session()
+
+    def __enter__(self):
+        # type: () -> RequestsTransport
+        return self
+
+    def __exit__(self, *exc_details):  # pylint: disable=arguments-differ
+        self.close()
+
+    def _init_session(self, session):
+        # type: (requests.Session) -> None
+        """Init session level configuration of requests.
+
+        This is initialization I want to do once only on a session.
+        """
+        pass  # TODO: Apply configuration
+
+    @property  # type: ignore
+    def session(self):
+        try:
+            return self._session_mapping.session
+        except AttributeError:
+            self._session_mapping.session = requests.Session()
+            self._init_session(self._session_mapping.session)
+            return self._session_mapping.session
+
+    @session.setter
+    def session(self, value):
+        self._init_session(value)
+        self._session_mapping.session = value
+
+    def build_context(self):
+        # type: () -> RequestsContext
+        return RequestsContext(
+            session=self.session,
+        )
+
+    def close(self):
+        self.session.close()
+
+    def send(self, request, **kwargs):
+        # type: (HttpRequest, Any) -> HttpResponse
+        """Send request object according to configuration.
+
+        Allowed kwargs are:
+        - session : will override the driver session and use yours. Should NOT be done unless really required.
+        - anything else is sent straight to requests.
+
+        :param HttpRequest request: The request object to be sent.
+        """
+        try:
+            response = self.session.request(
+                request.method,
+                request.url,
+                headers=request.headers,
+                data=request.data,
+                files=request.files,
+                verify=self.config.verify,
+                timeout=self.config.timeout,
+                cert=self.config.cert,
+                proxies=self.config.proxies,
+                allow_redirects=False,
+                **kwargs)
+
+        except requests.RequestException as err:
+            msg = "Error occurred in request."
+            raise_with_traceback(ClientRequestError, msg, err)
+
+        return RequestsTransportResponse(request, response)
