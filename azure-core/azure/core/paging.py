437c696c68a3a0d635b182d9446bd8767e9e76c1
Bryan Van de Ven <bryanv@anaconda.com>
2019-04-16 14:24:20 -0700

    import serialization from msrest for now

--- azure-core/azure/core/paging.py
+++ azure-core/azure/core/paging.py
@@ -32,12 +32,12 @@ except ImportError:
 
 from typing import Dict, Any, List, Callable, Optional, TYPE_CHECKING  # pylint: disable=unused-import
 
-from .serialization import Deserializer
+from msrest.serialization import Deserializer
 from .pipeline import ClientRawResponse
 
 if TYPE_CHECKING:
     from .universal_http import ClientResponse  # pylint: disable=unused-import
-    from .serialization import Model  # pylint: disable=unused-import
+    from msrest.serialization import Model  # pylint: disable=unused-import
 
 if sys.version_info >= (3, 5, 2):
     # Not executed on old Python, no syntax error
