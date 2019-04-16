c2f8124474557a2f67bfcc2c06891e180cb6d6b0
azuresdkci <azuresdkcheckins@gmail.com>
2019-04-16 23:59:56 +0000

    Packaging update of azure-mgmt-databricks

--- azure-mgmt-databricks/setup.py
+++ azure-mgmt-databricks/setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages, setup
 
 # Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-mgmt-databricks"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_PPRINT_NAME = "Databricks Management"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
