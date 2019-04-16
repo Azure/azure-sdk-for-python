70d7425a31dbbd4a4d55be39a355f73dfe821573
Johan Stenberg <johan.stenberg@microsoft.com>
2019-04-15 19:04:27 -0700

    Updated exception hierarchy

--- azure-core/azure/core/exceptions.py
+++ azure-core/azure/core/exceptions.py
@@ -29,20 +29,23 @@ import sys
 
 from typing import Callable, Any, Optional, TYPE_CHECKING
 
-
 _LOGGER = logging.getLogger(__name__)
 
 
-def raise_with_traceback(exception, message="", *args, **kwargs):
-    # type: (Callable, str, Any, Any) -> None
+def raise_with_traceback(exception, *args, **kwargs):
+    # type: (Callable, Any, Any) -> None
     """Raise exception with a specified traceback.
 
     This MUST be called inside a "except" clause.
 
     :param Exception exception: Error type to be raised.
-    :param str message: Message to include with error, empty by default.
     :param args: Any additional args to be included with exception.
+    :param kwargs: Keyword arguments to include with the exception.
+
+    Keyword arguments:
+    message Message to be associated with the exception. If omitted, defaults to an empty string.
     """
+    message = kwargs.pop('message', '')
     exc_type, exc_value, exc_traceback = sys.exc_info()
     # If not called inside a "except", exc_type will be None. Assume it will not happen
     exc_msg = "{}, {}: {}".format(message, exc_type.__name__, exc_value)  # type: ignore
@@ -73,48 +76,49 @@ class AzureError(Exception):
             self.__traceback__ = self.exc_traceback
             raise self
 
-
-class AzureLibraryError(AzureError):
-    """An error occurred in the client while processing the pipeline."""
-
-
-class AzureLibraryResponse(AzureLibraryError):
-    """An error occurred in the client while processing the response."""
-
-
-class AzureLibraryRequest(AzureLibraryError):
-    """An error occurred in the client while building the request."""
-
-
-class DecodeError(AzureLibraryResponse):
+class DecodeError(AzureError):
     """Error raised during response deserialization."""
-
 
 class ServiceRequestError(AzureError):
     """An error occurred while attempt to make a request to the service."""
 
 
-class ConnectionError(ServiceRequestError):
+class ConnectError(ServiceRequestError):
     """An error occurred while attempting to establish the connection.
     These errors are safe to retry."""
 
 
-class ConnectionTimeoutError(ConnectionError):
-    """The request timed out while trying to connect to the remote server.
-    These errors are safe to retry."""
-
-
-class ConnectionReadError(ServiceRequestError):
-    """An error occurred during the request/response.
-    These errors may not be safe to retry."""
-
-
-class ReadTimeoutError(ConnectionReadError):
+class ReadTimeoutError(ServiceRequestError):
     """The server did not send any data in the allotted amount of time.
     These errors may not be safe to retry."""
 
 
-class ClientRequestError(ServiceRequestError):
+class ServiceResponseError(AzureError):
+    """The request was sent, but the client failed to understand the response.
+    These errors may not be safe to retry"""
+
+
+class HttpRequestError(ServiceRequestError):
+    """A request was made, and a non-success status code was received from the service.
+
+    :ivar status_code: HttpResponse's status code
+    :ivar response: The response that triggered the exception.
+    """
+
+    def __init__(self, response, *args, **kwargs):
+        """ Create a new HttpRequestError instance.
+
+        :param response: Raw HTTP response that triggered the exception.
+        """
+        super(HttpRequestError).__init__(*args, **kwargs)
+        self.response = response
+
+    @property
+    def status_code(self):
+        return self.response.status_code
+
+
+class ClientRequestError(HttpRequestError):
     """An error response with status code 4xx.
     This will not be raised directly by the Azure core pipeline."""
 
@@ -124,22 +128,27 @@ class ResourceExistsError(ClientRequestError):
     This will not be raised directly by the Azure core pipeline."""
 
 
+class ResourceNotFoundError(ClientRequestError):
+    """ An error response, typically triggered by a 412 response (for update) or 404 (for get/post)
+    """
+
+
 class ClientAuthenticationError(ClientRequestError):
     """An error response with status code 4xx.
     This will not be raised directly by the Azure core pipeline."""
 
 
 class ResourceModifiedError(ClientRequestError):
-    """An error response with status code 4xx.
+    """An error response with status code 4xx, typically 412 Conflict.
     This will not be raised directly by the Azure core pipeline."""
 
 
-class ServerError(ServiceRequestError):
+class ServerError(HttpRequestError):
     """An error response with status code 5xx.
     This will not be raised directly by the Azure core pipeline."""
 
 
-class TooManyRedirectsError(ServiceRequestError):
+class TooManyRedirectsError(HttpRequestError):
     """Reached the maximum number of redirect attempts."""
 
     def __init__(self, history, *args, **kwargs):
