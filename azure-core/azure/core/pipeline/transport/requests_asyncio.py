9641b4dfdb7a2a8a7c1e07c985be5de3807a6710
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2019-05-08 13:59:30 -0700

    fix

--- azure-core/azure/core/pipeline/transport/requests_asyncio.py
+++ azure-core/azure/core/pipeline/transport/requests_asyncio.py
@@ -52,7 +52,7 @@ def _get_running_loop():
     try:
         return asyncio.get_running_loop()
     except AttributeError:  # 3.5 / 3.6
-        loop = asyncio.get_running_loop()  # pylint: disable=protected-access
+        loop = asyncio._get_running_loop()  # pylint: disable=protected-access, no-member
         if loop is None:
             raise RuntimeError('No running event loop')
         return loop
