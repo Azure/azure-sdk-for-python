7fefb6454cdf15487729d596181d5467b2cd4968
Charles Lowell <chlowe@microsoft.com>
2019-05-19 20:28:58 -0700

    async secrets client

--- sdk/keyvault/azure-security-keyvault/azure/security/keyvault/_generated/__init__.py
+++ sdk/keyvault/azure-security-keyvault/azure/security/keyvault/_generated/__init__.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE.txt in the project root for
@@ -5,7 +6,10 @@
 # --------------------------------------------------------------------------
 from .v7_0 import models
 from msrest.serialization import Deserializer, Serializer
+from .key_vault_client_async import KeyVaultClientAsync
 
 _CLIENT_MODELS = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
 DESERIALIZE = Deserializer(_CLIENT_MODELS)
 SERIALIZE = Serializer(_CLIENT_MODELS)
+
+__all__ = ["KeyVaultClientAsync"]
