fd522fcf8494745e72598398e99d0b3e187ceea5
Charles Lowell <chlowe@microsoft.com>
2019-05-20 18:52:58 -0700

    add conftest

---
+++ azure-identity/tests/conftest.py
@@ -0,0 +1,11 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import sys
+
+# Ignore collection of async tests for Python 2
+collect_ignore = []
+if sys.version_info < (3, 5):
+    collect_ignore.append("test_credentials_async")
