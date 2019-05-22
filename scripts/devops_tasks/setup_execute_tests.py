bed55e5f2052785c9655c55cc598819a3e85d4b3
Scott Beddall <scbedd@microsoft.com>
2019-05-21 20:57:25 -0700

    adding a default test case

--- scripts/devops_tasks/setup_execute_tests.py
+++ scripts/devops_tasks/setup_execute_tests.py
@@ -21,21 +21,17 @@ dev_setup_script_location = os.path.join(root_dir, 'scripts/dev_setup.py')
 # a return code of 5 from pytest == no tests run
 # evaluating whether we want this or not.
 ALLOWED_RETURN_CODES = []
+GUARANTEED_TEST_CASE = [ os.path.join(root_dir, 'scripts/devops_tasks/faketests') ]
 
 def prep_and_run_tests(targeted_packages, python_version, test_res):
     print('running test setup for {}'.format(targeted_packages))
     run_check_call([python_version, dev_setup_script_location, '-p', ','.join([os.path.basename(p) for p in targeted_packages])], root_dir)
 
-    # if we are targeting a single package, it is a possibility that no tests running is an acceptable 
-    # situation
-    if len(targeted_packages) == 1:
-        allowed_return_code_addition = [ 5 ]
-
     print('Setup complete. Running pytest for {}'.format(targeted_packages))
     command_array = [python_version, '-m', 'pytest']
     command_array.extend(test_res)
-    command_array.extend(targeted_packages)
-    run_check_call(command_array, root_dir, ALLOWED_RETURN_CODES + allowed_return_code_addition)
+    command_array.extend(targeted_packages + GUARANTEED_TEST_CASE)
+    run_check_call(command_array, root_dir, ALLOWED_RETURN_CODES)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Install Dependencies, Install Packages, Test Azure Packages, Called from DevOps YAML Pipeline')
