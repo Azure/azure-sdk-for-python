f24fb53e547645809cf1f36c9688ae464cd232ec
Sameeksha Vaity <savaity@microsoft.com>
2019-05-22 19:20:31 -0700

    Key Vault track 2 client prototypes (#5435)
    * key and secret clients from keyvault-proto
    
    * ClientRequestError -> HttpResponseError
    
    * azure.keyvault -> azure.security.keyvault
    
    * move some files
    
    * let HttpResponseError build its own message
    
    * license headers
    
    * paint it black
    
    * fix request bodies
    
    * async secrets client
    
    * update namespaces and methods
    
    * recover returns SecretBundle
    
    * rename test file
    
    * refactor tests to prevent name conflicts with track 1
    
    * support async on 3.5
    
    * sync secrets client wraps generated code
    
    * fixing build
    
    * correcting import to fix build

--- sdk/keyvault/azure-security-keyvault/azure/security/__init__.py
+++ sdk/keyvault/azure-security-keyvault/azure/security/__init__.py
@@ -1,2 +1,7 @@
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
 # pylint:disable=missing-docstring
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
