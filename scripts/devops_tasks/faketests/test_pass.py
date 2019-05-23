8176f43b6c77a1f5f2887c22b9c412fdce703aa9
scbedd <45376673+scbedd@users.noreply.github.com>
2019-05-23 10:37:11 -0700

    allow management plane individually queued builds to pass even when no tests have run

--- scripts/devops_tasks/faketests/test_pass.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-def test_avoid_failure_test():
-    return 0
--- scripts/devops_tasks/setup_execute_tests.py
+++ scripts/devops_tasks/setup_execute_tests.py
@@ -21,16 +21,21 @@ dev_setup_script_location = os.path.join(root_dir, 'scripts/dev_setup.py')
 # a return code of 5 from pytest == no tests run
 # evaluating whether we want this or not.
 ALLOWED_RETURN_CODES = []
-GUARANTEED_TEST_CASE = [ os.path.join(root_dir, 'scripts/devops_tasks/faketests') ]
 
 def prep_and_run_tests(targeted_packages, python_version, test_res):
     print('running test setup for {}'.format(targeted_packages))
     run_check_call([python_version, dev_setup_script_location, '-p', ','.join([os.path.basename(p) for p in targeted_packages])], root_dir)
 
+    # if we are targeting a single package and that package is management plane, it is a possibility 
+    # that no tests running is an acceptable situation
+    # we explicitly handle this here.
+    if len(targeted_packages) == 1 and (len(filter (lambda x : x.contains('mgmt'), targeted_packages)) > 0):
+        ALLOWED_RETURN_CODES.add(5)
+
     print('Setup complete. Running pytest for {}'.format(targeted_packages))
     command_array = [python_version, '-m', 'pytest']
     command_array.extend(test_res)
-    command_array.extend(targeted_packages + GUARANTEED_TEST_CASE)
+    command_array.extend(targeted_packages)
     run_check_call(command_array, root_dir, ALLOWED_RETURN_CODES)
 
 if __name__ == '__main__':
