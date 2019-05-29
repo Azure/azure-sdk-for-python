07c30b22db2fdb5faf3942199c27f59bacd43533
Charles Lowell <chlowe@microsoft.com>
2019-05-29 11:04:34 -0700

    azure-identity prototype (#5246)

---
+++ sdk/identity/azure-identity/azure/identity/aio/__init__.py
@@ -0,0 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+from .credentials import AsyncClientSecretCredential, AsyncTokenCredentialChain
+
+__all__ = ["AsyncClientSecretCredential", "AsyncTokenCredentialChain"]
