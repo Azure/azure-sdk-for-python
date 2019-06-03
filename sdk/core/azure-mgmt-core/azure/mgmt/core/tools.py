3c714eadddca30faa285c412616f3020c0cecd44
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-06-03 11:53:00 -0700

    Remove deprecated RP on hooks

--- sdk/core/azure-mgmt-core/azure/mgmt/core/tools.py
+++ sdk/core/azure-mgmt-core/azure/mgmt/core/tools.py
@@ -30,6 +30,7 @@ import logging
 import time
 import uuid
 
+
 _LOGGER = logging.getLogger(__name__)
 _ARMID_RE = re.compile(
     '(?i)/subscriptions/(?P<subscription>[^/]*)(/resourceGroups/(?P<resource_group>[^/]*))?'
@@ -40,68 +41,6 @@ _CHILDREN_RE = re.compile('(?i)(/providers/(?P<child_namespace>[^/]*))?/'
 
 _ARMNAME_RE = re.compile('^[^<>%&:\\?/]{1,260}$')
 
-def register_rp_hook(r, *args, **kwargs):
-    """This is a requests hook to register RP automatically.
-
-    You should not use this command manually, this is added automatically
-    by the SDK.
-
-    See requests documentation for details of the signature of this function.
-    http://docs.python-requests.org/en/master/user/advanced/#event-hooks
-    """
-    if r.status_code == 409 and 'msrest' in kwargs:
-        rp_name = _check_rp_not_registered_err(r)
-        if rp_name:
-            session = kwargs['msrest']['session']
-            url_prefix = _extract_subscription_url(r.request.url)
-            if not _register_rp(session, url_prefix, rp_name):
-                return
-            req = r.request
-            # Change the 'x-ms-client-request-id' otherwise the Azure endpoint
-            # just returns the same 409 payload without looking at the actual query
-            if 'x-ms-client-request-id' in req.headers:
-                req.headers['x-ms-client-request-id'] = str(uuid.uuid1())
-            return session.send(req)
-
-def _check_rp_not_registered_err(response):
-    try:
-        response = json.loads(response.content.decode())
-        if response['error']['code'] == 'MissingSubscriptionRegistration':
-            match = re.match(r".*'(.*)'", response['error']['message'])
-            return match.group(1)
-    except Exception:  # pylint: disable=broad-except
-        pass
-    return None
-
-def _extract_subscription_url(url):
-    """Extract the first part of the URL, just after subscription:
-    https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/
-    """
-    match = re.match(r".*/subscriptions/[a-f0-9-]+/", url, re.IGNORECASE)
-    if not match:
-        raise ValueError("Unable to extract subscription ID from URL")
-    return match.group(0)
-
-def _register_rp(session, url_prefix, rp_name):
-    """Synchronously register the RP is paremeter.
-    
-    Return False if we have a reason to believe this didn't work
-    """
-    post_url = "{}providers/{}/register?api-version=2016-02-01".format(url_prefix, rp_name)
-    get_url = "{}providers/{}?api-version=2016-02-01".format(url_prefix, rp_name)
-    _LOGGER.warning("Resource provider '%s' used by this operation is not "
-                    "registered. We are registering for you.", rp_name)
-    post_response = session.post(post_url)
-    if post_response.status_code != 200:
-        _LOGGER.warning("Registration failed. Please register manually.")
-        return False
-
-    while True:
-        time.sleep(10)
-        rp_info = session.get(get_url).json()
-        if rp_info['registrationState'] == 'Registered':
-            _LOGGER.warning("Registration succeeded.")
-            return True
 
 def parse_resource_id(rid):
     """Parses a resource_id into its various parts.
