3151b544581132799e97d6bee6c9212d52f3d90d
Brandon Klein <bgklein@wisc.edu>
2019-06-05 13:37:46 -0700

    Make error text more verbose for custom exception

--- sdk/batch/azure-batch/azure/batch/custom/custom_errors.py
+++ sdk/batch/azure-batch/azure/batch/custom/custom_errors.py
@@ -13,7 +13,25 @@ class CreateTasksErrorException(Exception):
     :param [~Exception] errors: List of unknown errors forcing early termination
     """
     def __init__(self, message, pending_task_list=None, failure_tasks=None, errors=None):
-        self.message = message
+        if failure_tasks and errors:
+            self.message =  "Multiple errors encountered. " \
+                            "Check the `failure_tasks` and `errors` properties for additional details"
+        if errors:
+            if len(errors) > 1:
+                self.message = "The SDK encountered an internal error. " \
+                               "Check the `errors` property for the inner exceptions"
+            else:
+                self.message = errors[0].message
+        if failure_tasks:
+            if len(failure_tasks) > 1:
+                self.message = "Multiple client side errors occured when submitting the request. " \
+                               "Check the `failure_tasks` property for details on these tasks."
+            else:
+                result = failure_tasks[0]
+                self.message = "Task with id `%s` failed due to client error - %s::%s" % \
+                               result.task_id,\
+                               result.error.code,\
+                               result.error.message
         self.pending_tasks = list(pending_task_list)
         self.failure_tasks = list(failure_tasks)
         self.errors = list(errors)
