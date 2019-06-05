97ef3fbd086c5c66100f9c3b2a21a6d980ac75c7
Brandon Klein <bgklein@wisc.edu>
2019-06-05 14:02:50 -0700

    Fix parameter naming

--- sdk/batch/azure-batch/azure/batch/custom/custom_errors.py
+++ sdk/batch/azure-batch/azure/batch/custom/custom_errors.py
@@ -8,11 +8,11 @@ class CreateTasksErrorException(Exception):
     """ Aggregate Exception containing details for any failures from a task add operation.
 
     :param str message: Error message describing exit reason
-    :param [~TaskAddParameter] pending_task_list: List of tasks remaining to be submitted.
+    :param [~TaskAddParameter] pending_tasks: List of tasks remaining to be submitted.
     :param [~TaskAddResult] failure_tasks: List of tasks which failed to add
     :param [~Exception] errors: List of unknown errors forcing early termination
     """
-    def __init__(self, message, pending_task_list=None, failure_tasks=None, errors=None):
+    def __init__(self, pending_tasks=None, failure_tasks=None, errors=None):
         if failure_tasks and errors:
             self.message =  "Multiple errors encountered. " \
                             "Check the `failure_tasks` and `errors` properties for additional details"
@@ -32,6 +32,6 @@ class CreateTasksErrorException(Exception):
                                result.task_id,\
                                result.error.code,\
                                result.error.message
-        self.pending_tasks = list(pending_task_list)
+        self.pending_tasks = list(pending_tasks)
         self.failure_tasks = list(failure_tasks)
         self.errors = list(errors)
