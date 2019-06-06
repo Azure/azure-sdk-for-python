c895db29a059d0c127aacee051a101da0ebf8fe9
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2019-06-06 00:00:08 -0700

    resolving bad behavior for "python -m pytest -m 'blah'." It confuses linux boxes, which seemingly ignores the mark argument completely.  for running tests, converted to executing as shell and combining command arrray into single execution string (#5706)

--- scripts/devops_tasks/setup_execute_tests.py
+++ scripts/devops_tasks/setup_execute_tests.py
@@ -38,7 +38,7 @@ def prep_and_run_tests(targeted_packages, python_version, test_res):
     command_array = [python_version, '-m', 'pytest']
     command_array.extend(test_res)
     command_array.extend(targeted_packages)
-    run_check_call(command_array, root_dir, ALLOWED_RETURN_CODES)
+    run_check_call(command_array, root_dir, ALLOWED_RETURN_CODES, True)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Install Dependencies, Install Packages, Test Azure Packages, Called from DevOps YAML Pipeline')
