ee8ff2c880962d7b3cf64300a83814b2f0172bd2
Charles Lowell <chlowe@microsoft.com>
2019-06-07 17:18:47 -0700

    Key Vault clients use azure-identity credentials (#5694)

--- sdk/keyvault/azure-security-keyvault/tests/test_case.py
+++ sdk/keyvault/azure-security-keyvault/tests/test_case.py
@@ -3,11 +3,17 @@
 # Licensed under the MIT License. See LICENSE.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from azure_devtools.scenario_tests import GeneralNameReplacer
 from devtools_testutils import AzureMgmtTestCase
+
+from auth_request_filter import OAuthv2RequestResponsesFilter
 
 
 class KeyVaultTestCase(AzureMgmtTestCase):
+    def __init__(self, *args, **kwargs):
+        super(KeyVaultTestCase, self).__init__(*args, **kwargs)
+        # workaround for upstream issue https://github.com/Azure/azure-python-devtools/pull/57
+        self.recording_processors.append(OAuthv2RequestResponsesFilter())
+
     def setUp(self):
         self.list_test_size = 7
         super(KeyVaultTestCase, self).setUp()
