9a18e6a7ad9088e4a9991aebeb9c1a47776859e5
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-06-13 16:02:50 -0700

    Compute with autorest v4

--- sdk/compute/azure-mgmt-compute/azure/mgmt/compute/models.py
+++ sdk/compute/azure-mgmt-compute/azure/mgmt/compute/models.py
@@ -1,14 +1,18 @@
-﻿# coding=utf-8
+# coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-import warnings
-
+from .v2015_06_15.models import *
+from .v2016_03_30.models import *
+from .v2016_04_30_preview.models import *
+from .v2017_03_30.models import *
+from .v2017_09_01.models import *
+from .v2017_12_01.models import *
+from .v2018_04_01.models import *
+from .v2018_06_01.models import *
 from .v2018_09_30.models import *
-from .v2019_03_01.models import * # Note that this line is overriding some models of 2018-09-01. See link below for details.
+from .v2018_10_01.models import *
+from .v2019_03_01.models import *
 from .v2019_04_01.models import *
-
-warnings.warn("Import models from this file is deprecated. See https://aka.ms/pysdkmodels",
-              DeprecationWarning)
