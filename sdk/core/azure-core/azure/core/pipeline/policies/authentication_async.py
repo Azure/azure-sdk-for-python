92d5e95460f97d5f331e2d6829dd8491236757cf
Charles Lowell <chlowe@microsoft.com>
2019-06-13 21:00:52 -0700

    add AccessToken to azure-core

--- sdk/core/azure-core/azure/core/pipeline/policies/authentication_async.py
+++ sdk/core/azure-core/azure/core/pipeline/policies/authentication_async.py
@@ -2,7 +2,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE.txt in the project root for
 # license information.
-# --------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+import time
+
 from azure.core.pipeline import PipelineRequest, PipelineResponse
 from azure.core.pipeline.policies import AsyncHTTPPolicy
 from azure.core.pipeline.policies.authentication import _BearerTokenCredentialPolicyBase
@@ -18,13 +20,17 @@ class AsyncBearerTokenCredentialPolicy(_BearerTokenCredentialPolicyBase, AsyncHT
     """
 
     async def send(self, request: PipelineRequest) -> PipelineResponse:
-        """Aync flavor that adds a bearer token Authorization header to request and sends request to next policy.
+        """Adds a bearer token Authorization header to request and sends request to next policy.
 
         :param request: The pipeline request object to be modified.
         :type request: ~azure.core.pipeline.PipelineRequest
         :return: The pipeline response object
         :rtype: ~azure.core.pipeline.PipelineResponse
         """
-        token = await self._credential.get_token(*self._scopes)  # type: ignore
-        self._update_headers(request.http_request.headers, token)  # type: ignore
+        if self._cached_token_expired:
+            # TODO: a race condition exists here
+            # Given the default connection timeout is 100s, problems are unlikely, but if
+            # they do arise we can forego caching here (credentials have a thread-safe cache)
+            self._token = await self._credential.get_token(*self._scopes)  # type: ignore
+        self._update_headers(request.http_request.headers, self._token.token)  # type: ignore
         return await self.next.send(request)  # type: ignore
