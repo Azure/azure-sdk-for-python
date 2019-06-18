c35aa717019fbf924c82386e83d402fa03b854bc
scbedd <45376673+scbedd@users.noreply.github.com>
2019-06-18 16:24:47 -0700

    Revert "Packaging update of azure-storage-blob"
    This reverts commit 66d68773cb59db01742ff5dac64d0792577c8d91.

--- sdk/storage/azure-storage-blob/setup.py
+++ sdk/storage/azure-storage-blob/setup.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 
+
+import os
 import re
-import os.path
-from io import open
-from setuptools import find_packages, setup
+
+from setuptools import setup, find_packages
+
 
 # Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-storage-blob"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_PPRINT_NAME = "Azure Blob Storage"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
-# a-b-c => a.b.c
-namespace_name = PACKAGE_NAME.replace('-', '.')
+
 
 # azure v0.x is not compatible with this package
 # azure v0.x used to have a __version__ attribute (newer versions don't)
 try:
     import azure
+
     try:
         ver = azure.__version__
         raise Exception(
@@ -35,35 +37,46 @@ try:
 except ImportError:
     pass
 
+# azure-storage v0.36.0 and prior are not compatible with this package
+try:
+    import azure.storage
+
+    try:
+        ver = azure.storage.__version__
+        raise Exception(
+            'This package is incompatible with azure-storage=={}. '.format(ver) +
+            ' Uninstall it with "pip uninstall azure-storage".'
+        )
+    except AttributeError:
+        pass
+except ImportError:
+    pass
+
 # Version extraction inspired from 'requests'
-with open(os.path.join(package_folder_path, 'version.py'), 'r') as fd:
-    version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]',
+with open(os.path.join(package_folder_path, '__init__.py'), 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         fd.read(), re.MULTILINE).group(1)
 
 if not version:
     raise RuntimeError('Cannot find version information')
 
-with open('README.rst', encoding='utf-8') as f:
-    readme = f.read()
-with open('HISTORY.rst', encoding='utf-8') as f:
-    history = f.read()
-
 setup(
     name=PACKAGE_NAME,
     version=version,
-    description='Microsoft Azure {} Client Library for Python'.format(PACKAGE_PPRINT_NAME),
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/x-rst',
+    description='Microsoft {} Client Library for Python'.format(PACKAGE_PPRINT_NAME),
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     license='MIT License',
     author='Microsoft Corporation',
-    author_email='azpysdkhelp@microsoft.com',
-    url='https://github.com/Azure/azure-sdk-for-python',
+    author_email='ascl@microsoft.com',
+    url='https://github.com/Azure/azure-storage-python',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -72,17 +85,19 @@ setup(
     ],
     zip_safe=False,
     packages=find_packages(exclude=[
-        'tests',
         # Exclude packages that will be covered by PEP420 or nspkg
         'azure',
         'azure.storage',
+        'tests',
+        'tests.blob',
+        'tests.common'
     ]),
     install_requires=[
-        'msrest>=0.5.0',
-        'msrestazure>=0.4.32,<2.0.0',
+        #'azure-core~=0.0.1',
         'azure-common~=1.1',
     ],
     extras_require={
-        ":python_version<'3.0'": ['azure-storage-nspkg'],
-    }
+        ":python_version<'3.0'": ['futures'],
+        ":python_version<'3.4'": ['enum34>=1.0.4'],
+    },
 )
