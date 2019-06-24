65bfec5c7876331284098ed5a9e99a36dddcfe8a
yijxie <yijun.xie@micrsofot.com>
2019-06-24 00:09:53 -0700

    fix auth test error

--- sdk/eventhub/azure-eventhubs/tests/asynctests/test_auth_async.py
+++ sdk/eventhub/azure-eventhubs/tests/asynctests/test_auth_async.py
@@ -27,7 +27,7 @@ async def test_client_secret_credential_async(aad_credential, live_eventhub):
                             credential=credential,
                             user_agent='customized information')
     sender = client.create_producer(partition_id='0')
-    receiver = client.create_consumer(consumer_group="$default", partition_id='0', event_position=EventPosition.latest())
+    receiver = client.create_consumer(consumer_group="$default", partition_id='0', event_position=EventPosition("@latest"))
 
     async with receiver:
 
