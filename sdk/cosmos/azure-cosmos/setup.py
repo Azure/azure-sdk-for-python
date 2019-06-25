698faa46746d17f862ec11b174397b433afded32
Bryan Van de Ven <brvandev@microsoft.com>
2019-06-25 13:28:28 -0700

    remove Python :: 3.4 classifiers

--- sdk/cosmos/azure-cosmos/setup.py
+++ sdk/cosmos/azure-cosmos/setup.py
@@ -21,7 +21,6 @@ setup(name='azure-cosmos',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Libraries :: Python Modules'
