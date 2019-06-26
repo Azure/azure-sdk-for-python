472ad50e99acf252dd970a4b5bcc714d0da4ea21
Charles Lowell <chlowe@microsoft.com>
2019-06-25 18:48:11 -0700

    Split azure-security-keyvault (#6080)
    azure-security-keyvault becomes azure-keyvault-keys and azure-keyvault-secrets

---
+++ sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/aio/__init__.py
@@ -0,0 +1,8 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+from ._client import KeyClient
+from .._models import Key, KeyBase, DeletedKey, KeyOperationResult
+
+__all__ = ["KeyClient"]
---
+++ sdk/keyvault/azure-keyvault-keys/tests/keys_async_preparer.py
@@ -0,0 +1,22 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import asyncio
+from unittest.mock import Mock
+
+from azure.core.credentials import AccessToken
+from azure.identity.aio import EnvironmentCredential
+
+from keys_preparer import VaultClientPreparer
+
+from keys_vault_client_async import VaultClient
+
+
+class AsyncVaultClientPreparer(VaultClientPreparer):
+    def create_vault_client(self, vault_uri):
+        if self.is_live:
+            credential = EnvironmentCredential()
+        else:
+            credential = Mock(get_token=asyncio.coroutine(lambda _: AccessToken("fake-token", 0)))
+        return VaultClient(vault_uri, credential)
