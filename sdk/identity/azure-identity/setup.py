7c367555b7aadb7eb941855564a0620c0aa7e021
Charles Lowell <chlowe@microsoft.com>
2019-06-26 11:58:24 -0700

    1.0.0b1

--- sdk/identity/azure-identity/setup.py
+++ sdk/identity/azure-identity/setup.py
@@ -70,7 +70,7 @@ setup(
         ]
     ),
     install_requires=[
-        "azure-core~=0.0.1",
+        "azure-core>=1.0.0b1,<2.0.0",
         "cryptography>=2.1.4",
         "msal~=0.3.1"
     ],
