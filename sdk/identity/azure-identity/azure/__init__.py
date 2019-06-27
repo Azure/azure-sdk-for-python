1fc3b98a960511decc2ddc650edf2ed0f91cf3a3
Charles Lowell <chlowe@microsoft.com>
2019-06-27 07:28:51 -0700

    update license headers (#6111)

--- sdk/identity/azure-identity/azure/__init__.py
+++ sdk/identity/azure-identity/azure/__init__.py
@@ -1,7 +1,6 @@
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE.txt in the project root for
-# license information.
-# --------------------------------------------------------------------------
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
 # pylint:disable=missing-docstring
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
