7d864fb41f4a3ad934d35fe08c60c8deab7e65e5
Rajiv Nandivada [MSFT] <rajiv.nandivada@microsoft.com>
2019-07-02 12:15:31 -0700

    Updated managedservices for version 2019-06-01

--- sdk/managedservices/azure-mgmt-managedservices/tests/test_mgmt_managedservices.py
+++ sdk/managedservices/azure-mgmt-managedservices/tests/test_mgmt_managedservices.py
@@ -6,8 +6,8 @@
 # license information.
 #--------------------------------------------------------------------------
 import unittest
+import time
 import json
-import uuid
 from devtools_testutils import AzureMgmtTestCase
 from azure.mgmt.managedservices import ManagedServicesClient
 
@@ -19,50 +19,60 @@ class MgmtManagedServicesTest(AzureMgmtTestCase):
 
     def test_managedservices_crud(self):
         scope = 'subscriptions/38bd4bef-41ff-45b5-b3af-d03e55a4ca15'
-        assignmentid = '367257d7-b69a-4b76-9c18-6dd3e9ae33ce'
-        api_version = '2018-06-01-preview'
-        definitionid = '1cf1704a-f8a8-4082-a8be-457d3f68e812'
+        assignmentid = 'c0d5f994-63a1-484d-8c1e-a2ac825efd60'
+        api_version = '2019-06-01'
+        definitionid = '8daec8c7-7567-47ff-9009-f0a4ec429a3c'
         regdef_string='{"registrationDefinitionName":"Registration Test","description":"dpp","managedByTenantId":"bab3375b-6197-4a15-a44b-16c41faa91d7","authorizations":[{"principalId":"d6f6c88a-5b7a-455e-ba40-ce146d4d3671","roleDefinitionId":"acdd72a7-3385-48ef-bd42-f606fba81ae7"}]}'
         properties = json.loads((regdef_string))
+        sleep=15
 
         #create definition
-        defresponse = self.client.registration_definitions.create_or_update(
-                    definitionid, api_version, scope, properties)
-        definitionresourceid = defresponse.id
-        self.assertIsNotNone(defresponse)
-        self.assertEqual(definitionid, defresponse.name)
+        response = self.client.registration_definitions.create_or_update(definitionid, api_version, scope, properties)
+        time.sleep(5)
+        status_code = response._response.status_code
+        if status_code == "200":
+            self.assertIsNotNone(response._response.headers['azure-asyncoperation'])
+            time.sleep(10)
 
         #create assignment
-        assignmentproperties ={"registrationDefinitionId":definitionresourceid}
-        assignmentresponse = self.client.registration_assignments.create_or_update(
-            scope, assignmentid, api_version,assignmentproperties)
-        self.assertIsNotNone(assignmentresponse)
-        self.assertEqual(assignmentid, assignmentresponse.name)
+        definitionresourceid = scope + "/providers/Microsoft.ManagedServices/registrationDefinitions/" + definitionid
+        assignmentproperties = {"registrationDefinitionId":definitionresourceid}
+        response = self.client.registration_assignments.create_or_update(scope, assignmentid, api_version, assignmentproperties)
+        time.sleep(sleep)
 
         #get definition
-        getdefresponse = self.client.registration_definitions.get(
-            scope,definitionid,api_version)
-        self.assertIsNotNone(getdefresponse)
-        self.assertEqual(definitionid, getdefresponse.name)
+        response = self.client.registration_definitions.get(
+            scope = scope,
+            registration_definition_id = definitionid,
+            api_version = api_version)
+        self.assertEqual(definitionid, response.name)
 
-        #get assingment
-        getassignmentresponse = self.client.registration_assignments.get(
-            scope, assignmentid, api_version)
-
-        self.assertIsNotNone(getassignmentresponse)
-        self.assertEqual(assignmentid, getassignmentresponse.name)
+        #get assignment
+        response = self.client.registration_assignments.get(
+            scope = scope,
+            registration_assignment_id = assignmentid,
+            api_version = api_version)
+        self.assertEqual(assignmentid, response.name)
 
         #remove assignment
-        removeassignmentresponse = self.client.registration_assignments.delete(
-            scope, assignmentid, api_version)
-        self.assertIsNotNone(removeassignmentresponse)
-        self.assertEqual(assignmentid, removeassignmentresponse.name)
+        response = self.client.registration_assignments.delete(scope, assignmentid, api_version)
+        self.assertIsNotNone(response._response.headers['location'])
+        time.sleep(sleep)
 
         #remove definition
-        removedefinitionresponse = self.client.registration_definitions.delete(
-            definitionid, api_version, scope)
-        self.assertIsNotNone(removedefinitionresponse)
-        self.assertEqual(definitionid, removedefinitionresponse.name)
+        response = self.client.registration_definitions.delete(definitionid, api_version, scope)
+        self.assertIsNone(response)
+
+        #list assignments
+        assignments = self.client.registration_assignments.list(scope, api_version)
+        for assignment in assignments:
+            self.assertNotEqual(assignmentid, assignment.name)
+
+        #list definitions
+        definitions = self.client.registration_definitions.list(scope, api_version)
+        for definition in definitions:
+            self.assertNotEqual(definitionid, definition.name)
+
 
 if __name__ == '__main__':
     unittest.main()
