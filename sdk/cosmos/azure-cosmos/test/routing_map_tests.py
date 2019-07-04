f0740a3b85f865452f88ac3302dafb1b524cc049
Johan Stenberg (MSFT) <johan.stenberg@microsoft.com>
2019-07-03 22:36:43 -0700

    Cosmos v3 test updates (#6237)
    * Fix routing_map_tests

--- sdk/cosmos/azure-cosmos/test/routing_map_tests.py
+++ sdk/cosmos/azure-cosmos/test/routing_map_tests.py
@@ -55,8 +55,8 @@ class RoutingMapEndToEndTests(unittest.TestCase):
                 "'masterKey' and 'host' at the top of this class to run the "
                 "tests.")
         
-        cls.client = cosmos_client.CosmosClient(cls.host, {'masterKey': cls.masterKey}, cls.connectionPolicy)
-        cls.collection_link = test_config._test_config.create_multi_partition_collection_with_custom_pk_if_not_exist(cls.client)['_self']
+        cls.client = cosmos_client.CosmosClient(cls.host, {'masterKey': cls.masterKey}, connection_policy=cls.connectionPolicy)
+        cls.collection_link = test_config._test_config.create_multi_partition_collection_with_custom_pk_if_not_exist(cls.client).container_link
 
     def test_read_partition_key_ranges(self):
         partition_key_ranges = list(self.client.client_connection._ReadPartitionKeyRanges(self.collection_link))
