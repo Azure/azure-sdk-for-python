464b63e407f8c489bdcc0408df24b238f6b4aac8
Suyog Soti <suyog.soti@gmail.com>
2019-07-09 15:19:21 -0700

    add context management for traces (#6223) and fix abstract span

--- sdk/core/azure-core/azure/core/tracing/abstract_span.py
+++ sdk/core/azure-core/azure/core/tracing/abstract_span.py
@@ -1,4 +1,14 @@
-from abc import ABC, abstractmethod, abstractproperty, abstractstaticmethod
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import abc
+from abc import abstractmethod
+
+try:
+    ABC = abc.ABC
+except AttributeError:  # Python 2.7, abc exists, but not ABC
+    ABC = abc.ABCMeta("ABC", (object,), {"__slots__": ()})  # type: ignore
 
 
 class AbstractSpan(ABC):
@@ -49,7 +59,8 @@ class AbstractSpan(ABC):
         """
         pass
 
-    @abstractproperty
+    @property
+    @abstractmethod
     def span_instance(self):
         # type: () -> Any
         """
@@ -57,40 +68,45 @@ class AbstractSpan(ABC):
         """
         pass
 
-    @abstractstaticmethod
-    def end_tracer(tracer):
+    @classmethod
+    @abstractmethod
+    def end_tracer(cls, tracer):
         # type: (Any) -> None
         """
         If a tracer exists, exports and ends the tracer.
         """
         pass
 
-    @abstractstaticmethod
-    def get_current_span():
-        # type: () -> AbstractSpan
+    @classmethod
+    @abstractmethod
+    def get_current_span(cls):
+        # type: () -> Any
         """
         Get the current span from the execution context. Return None otherwise.
         """
         pass
 
-    @abstractstaticmethod
-    def get_current_tracer():
+    @classmethod
+    @abstractmethod
+    def get_current_tracer(cls):
         # type: () -> Any
         """
         Get the current tracer from the execution context. Return None otherwise.
         """
         pass
 
-    @abstractstaticmethod
-    def set_current_span(span):
-        # type: (AbstractSpan) -> None
+    @classmethod
+    @abstractmethod
+    def set_current_span(cls, span):
+        # type: (Any) -> None
         """
         Set the given span as the current span in the execution context.
         """
         pass
 
-    @abstractstaticmethod
-    def set_current_tracer(tracer):
+    @classmethod
+    @abstractmethod
+    def set_current_tracer(cls, tracer):
         # type: (Any) -> None
         """
         Set the given tracer as the current tracer in the execution context.
---
+++ sdk/core/azure-core/azure/core/tracing/context.py
@@ -0,0 +1,161 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import threading
+
+try:
+    from typing import TYPE_CHECKING
+except ImportError:
+    TYPE_CHECKING = False
+
+if TYPE_CHECKING:
+    from typing import Any, Callable
+    from typing_extensions import Protocol
+else:
+    Protocol = object
+
+try:
+    import contextvars
+except ImportError:
+    contextvars = None
+
+
+class ContextProtocol(Protocol):
+    """
+     Implements set and get variables in a thread safe way.
+     """
+
+    def __init__(self, name, default, lock):
+        # type: (string, Any, threading.Lock) -> None
+        pass
+
+    def clear(self):
+        # type: () -> None
+        """Reset the value to the default value"""
+        pass
+
+    def get(self):
+        # type: () -> Any
+        """Get the stored value."""
+        pass
+
+    def set(self, value):
+        # type: (Any) -> None
+        """Set the value in the context."""
+        pass
+
+
+class _AsyncContext(object):
+    """
+    Uses contextvars to set and get variables globally in a thread safe way.
+    """
+
+    def __init__(self, name, default, lock):
+        self.name = name
+        self.contextvar = contextvars.ContextVar(name)
+        self.default = default if callable(default) else (lambda: default)
+        self.lock = lock
+
+    def clear(self):
+        # type: () -> None
+        """Reset the value to the default value"""
+        self.contextvar.set(self.default())
+
+    def get(self):
+        # type: () -> Any
+        """Get the stored value."""
+        try:
+            return self.contextvar.get()
+        except LookupError:
+            value = self.default()
+            self.set(value)
+            return value
+
+    def set(self, value):
+        # type: (Any) -> None
+        """Set the value in the context."""
+        with self.lock:
+            self.contextvar.set(value)
+
+
+class _ThreadLocalContext(object):
+    """
+    Uses thread local storage to set and get variables globally in a thread safe way.
+    """
+    _thread_local = threading.local()
+
+    def __init__(self, name, default, lock):
+        # type: (str, Any, threading.Lock) -> None
+        self.name = name
+        self.default = default if callable(default) else (lambda: default)
+        self.lock = lock
+
+    def clear(self):
+        # type: () -> None
+        """Reset the value to the default value"""
+        setattr(self._thread_local, self.name, self.default())
+
+    def get(self):
+        # type: () -> Any
+        """Get the stored value."""
+        try:
+            return getattr(self._thread_local, self.name)
+        except AttributeError:
+            value = self.default()
+            self.set(value)
+            return value
+
+    def set(self, value):
+        # type: (Any) -> None
+        """Set the value in the context."""
+        with self.lock:
+            setattr(self._thread_local, self.name, value)
+
+
+class TracingContext:
+    _lock = threading.Lock()
+
+    def __init__(self):
+        # type: () -> None
+        self.current_span = TracingContext._get_context_class("current_span", None)
+        self.tracing_impl = TracingContext._get_context_class("tracing_impl", None)
+
+    def with_current_context(self, func):
+        # type: (Callable[[Any], Any]) -> Any
+        """
+        Passes the current spans to the new context the function will be run in.
+        :param func: The function that will be run in the new context
+        :return: The target the pass in instead of the function
+        """
+        wrapped_span = tracing_context.current_span.get()
+        wrapper_class = self.tracing_impl.get()
+        if wrapper_class is not None:
+            current_impl_span = wrapper_class.get_current_span()
+            current_impl_tracer = wrapper_class.get_current_tracer()
+
+        def call_with_current_context(*args, **kwargs):
+            if wrapper_class is not None:
+                wrapper_class.set_current_span(current_impl_span)
+                wrapper_class.set_current_tracer(current_impl_tracer)
+                current_span = wrapped_span or wrapper_class(current_impl_span)
+                self.current_span.set(current_span)
+                self.tracing_impl.set(wrapper_class)
+            return func(*args, **kwargs)
+
+        return call_with_current_context
+
+    @classmethod
+    def _get_context_class(cls, name, default_val):
+        # type: (str, Any) -> ContextProtocol
+        """
+        Returns an instance of the the context class that stores the variable.
+        :param name: The key to store the variable in the context class
+        :param default_val: The default value of the variable if unset
+        :return: An instance that implements the context protocol class
+        """
+        context_class = _AsyncContext if contextvars else _ThreadLocalContext
+        return context_class(name, default_val, cls._lock)
+
+
+tracing_context = TracingContext()
