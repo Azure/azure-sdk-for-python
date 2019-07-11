73203c26edf216d9eb02a6d4919918f52a980800
Suyog Soti <suyog.soti@gmail.com>
2019-07-11 14:16:14 -0700

    initial tests

--- sdk/core/azure-core/tests/test_tracing_decorator.py
+++ sdk/core/azure-core/tests/test_tracing_decorator.py
@@ -11,27 +11,35 @@ except ImportError:
 
 import sys
 import os
+from azure.core import HttpRequest
+from azure.core.pipeline import Pipeline, PipelineResponse
+from azure.core.pipeline.policies import HTTPPolicy
+from azure.core.pipeline.transport import HttpTransport
 from azure.core.tracing import common, AbstractSpan
 from azure.core.tracing.context import tracing_context
+from azure.core.tracing.decorator import distributed_tracing_decorator
 from azure.core.settings import settings
 from azure.core.tracing.ext.opencensus_wrapper import OpencensusSpanWrapper
 from opencensus.trace import tracer as tracer_module
 from opencensus.trace.samplers import AlwaysOnSampler
+import pytest
 
 
 class ContextHelper(object):
-    def __init__(self, environ={}):
+    def __init__(self, environ={}, tracer_to_use=None):
         self.orig_tracer = OpencensusSpanWrapper.get_current_tracer()
         self.orig_current_span = OpencensusSpanWrapper.get_current_span()
         self.orig_sdk_context_span = tracing_context.current_span.get()
         self.orig_sdk_context_tracing_impl = tracing_context.tracing_impl.get()
         self.os_env = mock.patch.dict(os.environ, environ)
+        self.tracer_to_use = tracer_to_use
 
     def __enter__(self):
         self.orig_tracer = OpencensusSpanWrapper.get_current_tracer()
         self.orig_current_span = OpencensusSpanWrapper.get_current_span()
         self.orig_sdk_context_span = tracing_context.current_span.get()
         self.orig_sdk_context_tracing_impl = tracing_context.tracing_impl.get()
+        settings.tracing_implementation.set_value(self.tracer_to_use)
         self.os_env.start()
         return self
 
@@ -39,8 +47,43 @@ class ContextHelper(object):
         OpencensusSpanWrapper.set_current_tracer(self.orig_tracer)
         OpencensusSpanWrapper.set_current_span(self.orig_current_span)
         tracing_context.current_span.set(self.orig_sdk_context_span)
-        tracing_context.tracing_impl.set(self.orig_sdk_context_tracing_impl)
+        tracing_context.tracing_impl.set(self.orig_sdk_context_tracin
+        settings.tracing_implementation.unset_value()
         self.os_env.stop()
+
+
+class MockClient:
+    @distributed_tracing_decorator
+    def __init__(self, policies=None, assert_current_span=False):
+        self.request = HttpRequest("GET", "https://bing.com")
+        if policies is None:
+            policies = []
+        policies.append(mock.Mock(spec=HTTPPolicy, send=self.verify_request))
+        self.policies = policies
+        self.transport = mock.Mock(spec=HttpTransport)
+        self.pipeline = Pipeline(self.transport, policies=policies)
+
+        self.expected_response = mock.Mock(spec=PipelineResponse)
+        self.assert_current_span = assert_current_span
+
+    def verify_request(self, request):
+        current_span = tracing_context.current_span.get()
+        if self.assert_current_span:
+            assert current_span is not None
+        return self.expected_response
+
+    @distributed_tracing_decorator
+    def make_request(self, numb_times, **kwargs):
+        if numb_times < 1:
+            return None
+        response = self.pipeline.run(self.request, **kwargs)
+        self.get_foo()
+        self.make_request(numb_times - 1, **kwargs)
+        return response
+
+    @distributed_tracing_decorator
+    def get_foo(self):
+        return 5
 
 
 class TestCommon(unittest.TestCase):
@@ -109,8 +152,22 @@ class TestCommon(unittest.TestCase):
         assert common.should_use_trace(parent_span)
         assert common.should_use_trace(None) == False
 
+
 class TestDecorator(unittest.TestCase):
-    pass
+    def test_with_nothing_imported(self):
+        with ContextHelper():
+            opencensus = sys.modules["opencensus"]
+            del sys.modules["opencensus"]
+            client = MockClient(assert_current_span=True)
+            with pytest.raises(AssertionError):
+                client.make_request(3)
+            sys.modules["opencensus"] = opencensus
+
+    def test_with_nothing_imported(self):
+        with ContextHelper():
+            client = MockClient(assert_current_span=True)
+            with pytest.raises(AssertionError):
+                client.make_request(3)
 
 if __name__ == "__main__":
     unittest.main()
