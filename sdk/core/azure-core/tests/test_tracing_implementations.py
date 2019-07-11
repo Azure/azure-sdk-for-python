b3f44fb41d8cdbd8738e975e1367d615341fb26d
Suyog Soti <suyog.soti@gmail.com>
2019-07-11 10:46:49 -0700

    charles fixes

--- sdk/core/azure-core/tests/test_tracing_implementations.py
+++ sdk/core/azure-core/tests/test_tracing_implementations.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     import mock
 
-from azure.core.tracing.ext.opencensus_wrapper import OpencensusWrapper
+from azure.core.tracing.ext.opencensus_wrapper import OpencensusSpanWrapper
 from opencensus.trace import tracer as tracer_module
 from opencensus.trace.samplers import AlwaysOnSampler
 from opencensus.ext.azure.trace_exporter import AzureExporter
@@ -18,19 +18,19 @@ import os
 
 class ContextHelper(object):
     def __init__(self, environ={}):
-        self.orig_tracer = OpencensusWrapper.get_current_tracer()
-        self.orig_current_span = OpencensusWrapper.get_current_span()
+        self.orig_tracer = OpencensusSpanWrapper.get_current_tracer()
+        self.orig_current_span = OpencensusSpanWrapper.get_current_span()
         self.os_env = mock.patch.dict(os.environ, environ)
 
     def __enter__(self):
-        self.orig_tracer = OpencensusWrapper.get_current_tracer()
-        self.orig_current_span = OpencensusWrapper.get_current_span()
+        self.orig_tracer = OpencensusSpanWrapper.get_current_tracer()
+        self.orig_current_span = OpencensusSpanWrapper.get_current_span()
         self.os_env.start()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        OpencensusWrapper.set_current_tracer(self.orig_tracer)
-        OpencensusWrapper.set_current_span(self.orig_current_span)
+        OpencensusSpanWrapper.set_current_tracer(self.orig_tracer)
+        OpencensusSpanWrapper.set_current_span(self.orig_current_span)
         self.os_env.stop()
 
 
@@ -39,7 +39,7 @@ class TestOpencensusWrapper(unittest.TestCase):
         with ContextHelper():
             tracer = tracer_module.Tracer(sampler=AlwaysOnSampler())
             with tracer.start_span(name="parent") as parent:
-                wrapped_span = OpencensusWrapper(parent)
+                wrapped_span = OpencensusSpanWrapper(parent)
             assert wrapped_span.span_instance.name == "parent"
             assert (
                 wrapped_span.span_instance.context_tracer.trace_id
@@ -50,8 +50,8 @@ class TestOpencensusWrapper(unittest.TestCase):
 
     def test_no_span_passed_in_with_no_environ(self):
         with ContextHelper():
-            tracer = OpencensusWrapper.get_current_tracer()
-            wrapped_span = OpencensusWrapper()
+            tracer = OpencensusSpanWrapper.get_current_tracer()
+            wrapped_span = OpencensusSpanWrapper()
             assert wrapped_span.span_instance.name == "parent_span"
             assert (
                 wrapped_span.span_instance.context_tracer.span_context.trace_id
@@ -62,9 +62,9 @@ class TestOpencensusWrapper(unittest.TestCase):
     def test_no_span_passed_in_with_environ(self):
         with ContextHelper(environ={"APPINSIGHTS_INSTRUMENTATIONKEY": "instrumentation_key"}) as ctx:
             assert os.environ["APPINSIGHTS_INSTRUMENTATIONKEY"] == "instrumentation_key"
-            wrapped_span = OpencensusWrapper()
+            wrapped_span = OpencensusSpanWrapper()
             assert wrapped_span.span_instance.name == "parent_span"
-            tracer = OpencensusWrapper.get_current_tracer()
+            tracer = OpencensusSpanWrapper.get_current_tracer()
             assert (
                 wrapped_span.tracer.span_context.trace_id
                 == tracer.span_context.trace_id
@@ -79,7 +79,7 @@ class TestOpencensusWrapper(unittest.TestCase):
     def test_no_span_but_in_trace(self):
         with ContextHelper():
             tracer = tracer_module.Tracer(sampler=AlwaysOnSampler())
-            wrapped_span = OpencensusWrapper()
+            wrapped_span = OpencensusSpanWrapper()
             assert wrapped_span.span_instance.name == "parent_span"
             assert (
                 wrapped_span.span_instance.context_tracer.trace_id
@@ -91,7 +91,7 @@ class TestOpencensusWrapper(unittest.TestCase):
     def test_span(self):
         with ContextHelper() as ctx:
             tracer = tracer_module.Tracer(sampler=AlwaysOnSampler())
-            wrapped_class = OpencensusWrapper()
+            wrapped_class = OpencensusSpanWrapper()
             child = wrapped_class.span()
             assert child.span_instance.name == "child_span"
             assert child.tracer.span_context.trace_id == tracer.span_context.trace_id
@@ -101,7 +101,7 @@ class TestOpencensusWrapper(unittest.TestCase):
     def test_start_finish(self):
         with ContextHelper() as ctx:
             tracer = tracer_module.Tracer(sampler=AlwaysOnSampler())
-            parent = OpencensusWrapper()
+            parent = OpencensusSpanWrapper()
             wrapped_class = parent.span()
             assert wrapped_class.span_instance.start_time is None
             assert wrapped_class.span_instance.end_time is None
@@ -113,7 +113,7 @@ class TestOpencensusWrapper(unittest.TestCase):
             
     def test_to_and_from_header(self):
         with ContextHelper() as ctx:
-            wrapped_class = OpencensusWrapper()
+            wrapped_class = OpencensusSpanWrapper()
             og_header = {
                 "traceparent": "00-2578531519ed94423ceae67588eff2c9-231ebdc614cb9ddd-01"
             }
@@ -125,7 +125,7 @@ class TestOpencensusWrapper(unittest.TestCase):
     def test_end_tracer(self):
         with ContextHelper() as ctx:
             tracer = mock.Mock(spec=tracer_module.Tracer)
-            OpencensusWrapper.end_tracer(tracer)
+            OpencensusSpanWrapper.end_tracer(tracer)
             assert tracer.finish.called
 
 
