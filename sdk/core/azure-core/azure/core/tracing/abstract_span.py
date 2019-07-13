9e627333856b18924150753e66a92c40be22205e
Suyog Soti <suyog.soti@gmail.com>
2019-07-12 18:58:42 -0700

    add opencensus implementation (#6264)

--- sdk/core/azure-core/azure/core/tracing/abstract_span.py
+++ sdk/core/azure-core/azure/core/tracing/abstract_span.py
@@ -2,65 +2,69 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-import abc
-from abc import abstractmethod
+try:
+    from typing import TYPE_CHECKING
+except ImportError:
+    TYPE_CHECKING = False
+
+if TYPE_CHECKING:
+    from typing import Any, Dict, Optional, Union
 
 try:
-    ABC = abc.ABC
-except AttributeError:  # Python 2.7, abc exists, but not ABC
-    ABC = abc.ABCMeta("ABC", (object,), {"__slots__": ()})  # type: ignore
+    from typing_extensions import Protocol
+except ImportError:
+    Protocol = object
 
 
-class AbstractSpan(ABC):
-    @abstractmethod
+class AbstractSpan(Protocol):
+    """Wraps a span from a distributed tracing implementation."""
+
     def __init__(self, span=None, name=None):
-        # type: (Any, str) -> None
+        # type: (Optional[Any], Optional[str]) -> None
         """
         If a span is given wraps the span. Else a new span is created.
         The optional arguement name is given to the new span.
         """
         pass
 
-    @abstractmethod
     def span(self, name="child_span"):
-        # type: (str) -> AbstractSpan
+        # type: (Optional[str]) -> AbstractSpan
         """
         Create a child span for the current span and append it to the child spans list.
         The child span must be wrapped by an implementation of AbstractSpan
-         """
+        """
         pass
 
-    @abstractmethod
     def start(self):
         # type: () -> None
         """Set the start time for a span."""
         pass
 
-    @abstractmethod
     def finish(self):
         # type: () -> None
         """Set the end time for a span."""
         pass
 
-    @abstractmethod
-    def to_header(self, headers):
-        # type: (Dict[str, str]) -> Dict[str, str]
+    def to_header(self):
+        # type: () -> Dict[str, str]
         """
         Returns a dictionary with the header labels and values.
         """
         pass
 
-    @abstractmethod
-    def from_header(self, headers):
-        # type: (Dict[str, str]) -> Any
+    def add_attribute(self, key, value):
+        # type: (str, Union[str, int]) -> None
         """
-        Given a dictionary returns a new tracer with the span context
-        extracted from that dictionary.
+        Add attribute (key value pair) to the current span.
+
+        :param key: The key of the key value pair
+        :type key: str
+        :param value: The value of the key value pair
+        :type value: str
         """
         pass
 
     @property
-    @abstractmethod
     def span_instance(self):
         # type: () -> Any
         """
@@ -69,16 +73,17 @@ class AbstractSpan(ABC):
         pass
 
     @classmethod
-    @abstractmethod
-    def end_tracer(cls, tracer):
-        # type: (Any) -> None
+    def link(cls, headers):
+        # type: (Dict[str, str]) -> None
         """
-        If a tracer exists, exports and ends the tracer.
+        Given a dictionary, extracts the context and links the context to the current tracer.
+
+        :param headers: A dictionary of the request header as key value pairs.
+        :type headers: dict
         """
         pass
 
     @classmethod
-    @abstractmethod
     def get_current_span(cls):
         # type: () -> Any
         """
@@ -87,7 +92,6 @@ class AbstractSpan(ABC):
         pass
 
     @classmethod
-    @abstractmethod
     def get_current_tracer(cls):
         # type: () -> Any
         """
@@ -96,7 +100,6 @@ class AbstractSpan(ABC):
         pass
 
     @classmethod
-    @abstractmethod
     def set_current_span(cls, span):
         # type: (Any) -> None
         """
@@ -105,7 +108,6 @@ class AbstractSpan(ABC):
         pass
 
     @classmethod
-    @abstractmethod
     def set_current_tracer(cls, tracer):
         # type: (Any) -> None
         """
