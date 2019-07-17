da2fbf567fa94ea82b6c30fbfdde23991d1e245c
Charles Lowell <chlowe@microsoft.com>
2019-07-17 16:22:35 -0700

    async transport adapter for MSAL

---
+++ sdk/identity/azure-identity/azure/identity/aio/_internal/msal_transport_adapter.py
@@ -0,0 +1,120 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+"""Adapter to substitute an async azure-core pipeline for Requests in MSAL application token acquisition methods."""
+
+import asyncio
+import atexit
+import threading
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    # pylint:disable=unused-import
+    from typing import Any, Dict, Iterable, Optional
+    from azure.core.pipeline.policies import AsyncHTTPPolicy
+    from azure.core.pipeline.transport import AsyncHttpTransport
+
+from azure.core.pipeline import AsyncPipeline
+from azure.core.pipeline.policies import AsyncRetryPolicy, NetworkTraceLoggingPolicy
+from azure.core.pipeline.transport import AioHttpTransport, HttpRequest
+from azure.core.configuration import Configuration
+
+from azure.identity._internal import MsalTransportResponse
+
+
+class AsyncMsalTransportAdapter:
+    """Wraps an async azure-core pipeline with the shape of a (synchronous) Requests Session"""
+
+    def __init__(
+        self,
+        config: "Optional[Configuration]" = None,
+        policies: "Optional[Iterable[AsyncHTTPPolicy]]" = None,
+        transport: "Optional[AsyncHttpTransport]" = None,
+        **kwargs: "Any"
+    ) -> None:
+        self._lock = threading.Lock()
+        self._loop = None  # type: asyncio.AbstractEventLoop
+
+        config = config or self.create_config(**kwargs)
+        policies = policies or [config.retry_policy, config.logging_policy]
+
+        self._transport = transport or AioHttpTransport(configuration=config)
+        atexit.register(self._close_transport_session)
+
+        self._pipeline = AsyncPipeline(transport=self._transport, policies=policies)
+
+    def _close_transport_session(self) -> None:
+        """If transport has a 'close' method, invoke it. This prevents aiohttp warnings."""
+
+        close = getattr(self._transport, "close", None)
+        if not callable(close):
+            return
+
+        if asyncio.iscoroutinefunction(close):
+            # we expect no loop is running because this method should be called only when the interpreter is exiting
+            asyncio.new_event_loop().run_until_complete(close())
+        else:
+            close()
+
+    def get(
+        self,
+        url: str,
+        headers: "Optional[Dict[str, str]]" = None,
+        params: "Optional[Dict[str, str]]" = None,
+        proxies: "Optional[Dict[str, str]]" = None,
+        timeout: "Optional[float]" = None,
+        verify: "Optional[bool]" = None,
+        **kwargs: "Any"
+    ) -> MsalTransportResponse:
+        request = HttpRequest("GET", url, headers=headers)
+        if params:
+            request.format_parameters(params)
+
+        future = asyncio.run_coroutine_threadsafe(
+            self._pipeline.run(request, connection_timeout=timeout, connection_verify=verify, **kwargs), self.loop
+        )
+        response = future.result(timeout=timeout)
+
+        return MsalTransportResponse(response)
+
+    def post(
+        self,
+        url: str,
+        data: "Any" = None,
+        headers: "Optional[Dict[str, str]]" = None,
+        params: "Optional[Dict[str, str]]" = None,
+        timeout: "Optional[float]" = None,
+        verify: "Optional[bool]" = None,
+        **kwargs: "Any"
+    ) -> MsalTransportResponse:
+        request = HttpRequest("POST", url, headers=headers)
+        if params:
+            request.format_parameters(params)
+        if data:
+            request.headers["Content-Type"] = "application/x-www-form-urlencoded"
+            request.set_formdata_body(data)
+
+        future = asyncio.run_coroutine_threadsafe(
+            self._pipeline.run(request, connection_timeout=timeout, connection_verify=verify, **kwargs), self.loop
+        )
+        response = future.result(timeout=timeout)
+
+        return MsalTransportResponse(response)
+
+    @staticmethod
+    def create_config(**kwargs: "Any") -> Configuration:
+        config = Configuration(**kwargs)
+        config.logging_policy = NetworkTraceLoggingPolicy(**kwargs)
+        config.retry_policy = AsyncRetryPolicy(**kwargs)
+        return config
+
+    @property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        with self._lock:
+            return self._loop
+
+    @loop.setter
+    def loop(self, value: asyncio.AbstractEventLoop) -> None:
+        with self._lock:
+            self._loop = value
