9953de952dc2ae85a96fecd6329a14f73fb4db2b
Zim Kalinowski <zikalino@microsoft.com>
2019-07-20 09:36:32 +0000

    additional changes

--- sdk/compute/azure-mgmt-compute/azure/mgmt/compute/models.py
+++ sdk/compute/azure-mgmt-compute/azure/mgmt/compute/models.py
@@ -4,15 +4,6 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from .v2015_06_15.models import *
-from .v2016_03_30.models import *
-from .v2016_04_30_preview.models import *
-from .v2017_03_30.models import *
-from .v2017_09_01.models import *
-from .v2017_12_01.models import *
-from .v2018_04_01.models import *
-from .v2018_06_01.models import *
 from .v2018_09_30.models import *
-from .v2018_10_01.models import *
 from .v2019_03_01.models import *
 from .v2019_04_01.models import *
