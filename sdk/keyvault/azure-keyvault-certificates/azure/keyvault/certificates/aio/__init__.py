aaf922080703bbda3dadd56823d198270d797416
Isabella Cai <iscai@microsoft.com>
2019-07-22 15:44:07 -0700

    deleted copied secret aio implementation

--- sdk/keyvault/azure-keyvault-certificates/azure/keyvault/certificates/aio/__init__.py
+++
@@ -1,8 +0,0 @@
-# ------------------------------------
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-# ------------------------------------
-from ._client import SecretClient
-from .._models import Secret, SecretAttributes, DeletedSecret
-
-__all__ = ["SecretClient"]
