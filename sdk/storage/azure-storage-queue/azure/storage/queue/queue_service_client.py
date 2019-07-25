d2fed78a75bafabef9b222c4f27f3dc6d244aaed
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-07-25 12:41:38 -0700

    Paging v2 (#6420)
    * First version of paging
    
    * Clean sync paging
    
    * Async paging
    
    * Name AsyncList without underscore
    
    * MyPy happyness
    
    * pylint
    
    * black
    
    * PageIterator as conf
    
    * Simplify ItemPaged
    
    * Use chain to link iterable
    
    * Make Paging more open
    
    * Docstring
    
    * pylint / mypy / black
    
    * Regenerate KV with latest Autorest + Paging branch (#6479)
    
    * Regenerate KV with latest Autorest + Paging branch
    
    * Remove unused import in tests
    
    * Make the async adapter more generic
    
    * Regeneration after operation mixin fix in Autorest
    
    * COmmit changes forgotten in #6418
    
    * Fixing syntax for 2.7
    
    * Move Storage Queue to new Paging (#6447)
    
    * Move Storage Queue to new Paging
    
    * Working Storage queue on new paging
    
    * Fix types
    
    * First shot of blob tests update
    
    * Fixing blob tests
    
    * File to new paging
    
    * Doc fix
    
    * Feedbacks from @annatisch
    
    * Fix last @annatisch comment
    
    * Simplify paging contract
    
    * Fix storage queue after new paging contract

--- sdk/storage/azure-storage-queue/azure/storage/queue/queue_service_client.py
+++ sdk/storage/azure-storage-queue/azure/storage/queue/queue_service_client.py
@@ -13,6 +13,8 @@ try:
 except ImportError:
     from urlparse import urlparse # type: ignore
 
+from azure.core.paging import ItemPaged
+
 from ._shared.shared_access_signature import SharedAccessSignature
 from ._shared.models import LocationMode, Services
 from ._shared.utils import (
@@ -302,12 +304,11 @@ class QueueServiceClient(StorageAccountHostsMixin):
     def list_queues(
             self, name_starts_with=None,  # type: Optional[str]
             include_metadata=False,  # type: Optional[bool]
-            marker=None,  # type: Optional[str]
             results_per_page=None,  # type: Optional[int]
             timeout=None,  # type: Optional[int]
             **kwargs
         ):
-        # type: (...) -> QueuePropertiesPaged
+        # type: (...) -> ItemPaged[QueueProperties]
         """Returns a generator to list the queues under the specified account.
 
         The generator will lazily follow the continuation tokens returned by
@@ -318,10 +319,6 @@ class QueueServiceClient(StorageAccountHostsMixin):
             begin with the specified prefix.
         :param bool include_metadata:
             Specifies that queue metadata be returned in the response.
-        :param str marker:
-            An opaque continuation token. This value can be retrieved from the
-            next_marker field of a previous generator object. If specified,
-            this generator will begin returning results from this point.
         :param int results_per_page:
             The maximum number of queue names to retrieve per API
             call. If the request does not specify the server will return up to 5,000 items.
@@ -330,7 +327,7 @@ class QueueServiceClient(StorageAccountHostsMixin):
             calls to the service in which case the timeout value specified will be
             applied to each individual call.
         :returns: An iterable (auto-paging) of QueueProperties.
-        :rtype: ~azure.core.queue.models.QueuePropertiesPaged
+        :rtype: ~azure.core.paging.ItemPaged[~azure.core.queue.models.QueueProperties]
 
         Example:
             .. literalinclude:: ../tests/test_queue_samples_service.py
@@ -347,8 +344,10 @@ class QueueServiceClient(StorageAccountHostsMixin):
             include=include,
             timeout=timeout,
             **kwargs)
-        return QueuePropertiesPaged(
-            command, prefix=name_starts_with, results_per_page=results_per_page, marker=marker)
+        return ItemPaged(
+            command, prefix=name_starts_with, results_per_page=results_per_page,
+            page_iterator_class=QueuePropertiesPaged
+        )
 
     def create_queue(
             self, name,  # type: str
@@ -357,7 +356,7 @@ class QueueServiceClient(StorageAccountHostsMixin):
             **kwargs
         ):
         # type: (...) -> QueueClient
-        """Creates a new queue under the specified account. 
+        """Creates a new queue under the specified account.
 
         If a queue with the same name already exists, the operation fails.
         Returns a client with which to interact with the newly created queue.
