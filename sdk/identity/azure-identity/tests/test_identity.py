5fb5e2926d0d4c0088c3590932073acce7045996
annatisch <antisch@microsoft.com>
2019-07-26 12:15:22 -0700

    [azure-core] Small fixes for aiohttp (#6490)
    * Aiohttp read timeout
    
    * Aiohttp response status code
    
    * Aiohttp content type header
    
    * Review feedback
    
    * Fixed typo
    
    * Fixed async transport sleep coroutine
    
    * Fix for stream generators
    
    * Reverted attribute error handling
    
    * Fixed trio sleep test
    
    * Fixed identity test mocks
    
    * Fix identity mock to return content-type as string
    
    * Fix universal policy tests

--- sdk/identity/azure-identity/tests/test_identity.py
+++ sdk/identity/azure-identity/tests/test_identity.py
@@ -191,7 +191,7 @@ def test_imds_credential_cache():
         text=lambda: json.dumps(token_payload),
         headers={"content-type": "application/json"},
         status_code=200,
-        content_type=["application/json"],
+        content_type="application/json",
     )
     mock_send = Mock(return_value=mock_response)
 
@@ -219,7 +219,7 @@ def test_imds_credential_retries():
     mock_response = Mock(
         text=lambda: b"{}",
         headers={"content-type": "application/json", "Retry-After": "0"},
-        content_type=["application/json"],
+        content_type="application/json",
     )
     mock_send = Mock(return_value=mock_response)
 
