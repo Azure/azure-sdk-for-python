ab5de475bddb5f62ca733b33f250d115ce1a3090
Charles Lowell <chlowe@microsoft.com>
2019-07-30 08:51:19 -0700

    Synchronous username/password auth (#6416)

--- sdk/identity/azure-identity/azure/identity/_internal/__init__.py
+++ sdk/identity/azure-identity/azure/identity/_internal/__init__.py
@@ -2,5 +2,5 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-from .msal_credentials import ConfidentialClientCredential
-from .msal_transport_adapter import MsalTransportResponse, MsalTransportAdapter
+from .msal_credentials import ConfidentialClientCredential, PublicClientCredential
+from .msal_transport_adapter import MsalTransportAdapter, MsalTransportResponse
