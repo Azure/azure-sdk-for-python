13aaea71c99e93d975c9331212ad3892d7512a31
Xiang Yan <xiangyan@microsoft.com>
2019-07-30 14:14:29 -0700

    AppConfigService

---
+++ sdk/appconfiguration/azure-data-appconfiguration/README.md
@@ -0,0 +1,209 @@
+# Azure App Configuration client library for Python
+Azure App Configuration is a managed service that helps developers centralize their application configurations simply and securely.
+
+Modern programs, especially programs running in a cloud, generally have many components that are distributed in nature. Spreading configuration settings across these components can lead to hard-to-troubleshoot errors during an application deployment. Use App Configuration to securely store all the settings for your application in one place.
+
+Use the client library for App Configuration to create and manage application configuration settings.
+
+[Source code]() | [Package (Pypi)][package] | [API reference documentation]() | [Product documentation][azconfig_docs]
+
+## Getting started
+
+### Supported Python version
+Python 2.7 and 3.4+
+### Install the package
+
+Install the Azure App Configuration client library for Python with pip:
+
+```commandline
+pip install azure-configuration
+```
+
+**Prerequisites**: You must have an [Azure subscription][azure_sub], and a [Configuration Store][configuration_store] to use this package.
+
+To create a Configuration Store, you can use the Azure Portal or [Azure CLI][azure_cli].
+
+You need to install the Azure App Configuration CLI extension first by executing the following command:
+
+After that, create the Configuration Store:
+```Powershell
+az extension add -n appconfig
+```
+
+
+
+```Powershell
+az appconfig create --name <config-store-name> --resource-group <resource-group-name> --location eastus
+```
+
+### Authenticate the client
+
+In order to interact with the App Configuration service, you'll need to create an instance of the [AzureConfigurationClient][configuration_client_class] class. To make this possible, you'll need the connection string of the Configuration Store.
+
+#### Get credentials
+Use the [Azure CLI][azure_cli] snippet below to get the connection string from the Configuration Store.
+```Powershell
+az appconfig credential list --name <config-store-name>
+```
+
+Alternatively, get the connection string from the Azure Portal.
+
+#### Create client
+
+Once you have the value of the connection string, you can create the ConfigurationClient:
+
+```python
+    from azure.configuration import AzureConfigurationClient
+
+    connection_str = "<connection_string>"
+    client = AzureConfigurationClient(connection_str)
+```
+
+## Key concepts
+
+### Configuration Setting
+A Configuration Setting is the fundamental resource within a Configuration Store. In its simplest form it is a key and a value. However, there are additional properties such as the modifiable content type and tags fields that allow the value to be interpreted or associated in different ways.
+
+The Label property of a Configuration Setting provides a way to separate Configuration Settings into different dimensions. These dimensions are user defined and can take any form. Some common examples of dimensions to use for a label include regions, semantic versions, or environments. Many applications have a required set of configuration keys that have varying values as the application exists across different dimensions.
+For example, MaxRequests may be 100 in "NorthAmerica", and 200 in "WestEurope". By creating a Configuration Setting named MaxRequests with a label of "NorthAmerica" and another, only with a different value, in the "WestEurope" label, an application can seamlessly retrieve Configuration Settings as it runs in these two dimensions.
+
+Properties of a Configuration Setting:
+
+```python
+    key : str
+    label : str
+    content_type : str
+    value : str
+    last_modified : str
+    locked : bool
+    tags : dict
+    etag : str
+```
+
+## Examples
+The following sections provide several code snippets covering some of the most common Configuration Service tasks, including:
+- [Create a Configuration Setting](#create-a-Configuration-Setting)
+- [Retrieve a Configuration Setting](#retrieve-a-Configuration-Setting)
+- [Update an existing Configuration Setting](#update-an-existing-Configuration-Setting)
+- [Delete a Configuration Setting](#delete-a-Configuration-Setting)
+- [Query multiple Configuration Settings](#query-Configuration-Settings)
+### Create a Configuration Setting
+Create a Configuration Setting to be stored in the Configuration Store.
+There are two ways to store a Configuration Setting:
+- add_configuration_setting creates a setting only if the setting does not already exist in the store.
+```python
+config_setting = ConfigurationSetting(
+    key="MyKey",
+    label="MyLabel",
+    value="my value",
+    content_type="my content type",
+    tags={"my tag": "my tag value"}
+)
+added_config_setting = client.add_configuration_setting(config_setting)
+```
+- set_configuration_setting creates a setting if it doesn't exist or overrides an existing setting.
+```python
+config_setting = ConfigurationSetting(
+    key="MyKey",
+    label="MyLabel",
+    value="my set value",
+    content_type="my set content type",
+    tags={"my set tag": "my set tag value"}
+)
+returned_config_setting = client.set_configuration_setting(config_setting)
+```
+
+### Retrieve a Configuration Setting
+Retrieve a previously stored Configuration Setting.
+
+```python
+fetched_config_setting = client.get_configuration_setting(
+    key="MyKey", label="MyLabel"
+)
+```
+
+### Update a Configuration Setting
+Use set_configuration_setting if you want to update a whole Configuration Setting object to the store.
+
+Use update_configuration_setting if you want to only update some attributes and leave other attributes as what they're
+
+```python
+#  key and label are the identifier of the setting. Only value will be updated.
+updated_kv = client.update_configuration_setting(
+    key="MyKey",
+    label="MyLabel",
+    value="my updated value"
+)
+```
+
+### Delete a Configuration Setting
+Delete an existing Configuration Setting by calling delete_configuration_setting
+
+```python
+deleted_config_setting = client.delete_configuration_setting(
+    key="MyKey", label="MyLabel"
+)
+```
+
+### Query Configuration Settings
+```python
+
+filtered_listed = client.list_configuration_settings(
+    labels=["*Labe*"], keys=["*Ke*"]
+)
+for item in filtered_listed:
+    pass  # do something
+
+```
+
+## Async Client
+Async client is supported for python 3.5+. 
+To use the async client library, import the AzureConfigurationClient from package azure.configuration.aio instead of azure.configuration
+```python
+from azure.configuration.aio import AzureConfigurationClient
+
+connection_str = "<connection_string>"
+async_client = AzureConfigurationClient(connection_str)
+```
+This async AzureConfigurationClient has the same method signatures as the sync ones except that they're async.
+For instance, to retrieve a Configuration Setting asynchronously, async_client can be used:
+```python
+fetched_config_setting = await async_client.get_configuration_setting(
+    key="MyKey", label="MyLabel"
+)
+```
+
+To use list_configuration_settings, call it synchronously and iterate over the returned async iterator asynchronously 
+```python
+
+filtered_listed = async_client.list_configuration_settings(
+    labels=["*Labe*"], keys=["*Ke*"]
+)
+async for item in filtered_listed:
+    pass  # do something
+
+```
+
+## Troubleshooting
+
+### Logging
+
+This SDK uses Python standard logging library.
+You can configure logging print out debugging information to the stdout or anywhere you want.
+
+```python
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+````
+
+Http request and response details are printed to stdout with this logging config.
+
+
+<!-- LINKS -->
+[azconfig_docs]: https://docs.microsoft.com/en-us/azure/azure-app-configuration/
+[azconfig_rest]: https://github.com/Azure/AppConfiguration#rest-api-reference
+[azure_cli]: https://docs.microsoft.com/cli/azure
+[azure_sub]: https://azure.microsoft.com/free/
+[configuration_client_class]: ./azure/configuration/azure_configuration_client.py
+[package]: https://pypi.org/project/azure-app-configuration/
---
+++ sdk/appconfiguration/azure-data-appconfiguration/azure/appconfiguration/azure_appconfiguration_client.py
@@ -0,0 +1,253 @@
+# ------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+import os
+import sys
+import platform
+from datetime import datetime
+from msrest.paging import Paged
+from azure.core.pipeline import Pipeline
+from azure.core.pipeline.transport import RequestsTransport
+from azure.core.exceptions import ResourceNotFoundError, ResourceModifiedError
+from ._generated import ConfigurationClient
+from ._generated._configuration import ConfigurationClientConfiguration
+from ._generated.models import ConfigurationSetting
+from .azure_appconfiguration_requests import AppConfigRequestsCredentialsPolicy
+from .azure_appconfiguration_client_abstract import AzureAppConfigurationClientAbstract
+from .utils import get_endpoint_from_connection_string
+
+
+class AzureAppConfigurationClient(AzureAppConfigurationClientAbstract):
+    """
+    Example
+
+    .. code-block:: python
+
+        from azure.configuration import AzureAppConfigurationClient
+        connection_str = "<my connection string>"
+        client = AzureAppConfigurationClient(connection_str)
+    """
+
+    def __init__(self, connection_string):
+        super(AzureAppConfigurationClient, self).__init__()
+        base_url = "https://" + get_endpoint_from_connection_string(connection_string)
+        program_name = os.path.basename(sys.argv[0]) or "noprogram"
+        self.config = ConfigurationClientConfiguration(
+            connection_string, base_user_agent=program_name, logging_enable=True
+        )
+        self.config.user_agent_policy.add_user_agent(
+            "{}{}".format(platform.python_implementation(), platform.python_version())
+        )
+        self.config.user_agent_policy.add_user_agent(platform.platform())
+        self._impl = ConfigurationClient(
+            connection_string,
+            base_url,
+            pipeline=self._create_azconfig_pipeline(),
+        )
+
+    def _create_azconfig_pipeline(self):
+        policies = [
+            self.config.headers_policy,
+            self.config.user_agent_policy,
+            self.config.logging_policy,  # HTTP request/response log
+            AppConfigRequestsCredentialsPolicy(self.config.credentials),
+        ]
+
+        return Pipeline(
+            RequestsTransport(configuration=self.config), policies  # Send HTTP request using requests
+        )
+
+    def list_configuration_settings(
+        self, labels=None, keys=None, accept_date_time=None, fields=None, **kwargs
+    ):  # type: (list, list, datetime, list, dict) -> Paged
+        """
+        Example
+
+        .. code-block:: python
+
+            from datetime import datetime, timedelta
+
+            accept_date_time = datetime.today() + timedelta(days=-1)
+
+            all_listed = client.list_configuration_settings()
+            for item in all_listed:
+                pass  # do something
+
+            filtered_listed = client.list_configuration_settings(
+                labels=["*Labe*"], keys=["*Ke*"], accept_date_time=accept_date_time
+            )
+            for item in filtered_listed:
+                pass  # do something
+        """
+        return super(AzureAppConfigurationClient, self).list_configuration_settings(
+            labels, keys, accept_date_time, fields, **kwargs
+        )
+
+    def get_configuration_setting(
+        self, key, label=None, accept_date_time=None, **kwargs
+    ):  # type: (str, str, datetime, dict) -> Paged
+        """
+        Example
+
+        .. code-block:: python
+
+            fetched_config_setting = client.get_configuration_setting(
+                key="MyKey", label="MyLabel"
+            )
+        """
+        return super(AzureAppConfigurationClient, self).get_configuration_setting(
+            key, label=label, accept_date_time=accept_date_time, **kwargs
+        )
+
+    def add_configuration_setting(self, configuration_setting, **kwargs):
+        # type: (ConfigurationSetting, dict) -> ConfigurationSetting
+        """
+        Example
+
+        .. code-block:: python
+
+            config_setting = ConfigurationSetting(
+                key="MyKey",
+                label="MyLabel",
+                value="my value",
+                content_type="my content type",
+                tags={"my tag": "my tag value"}
+            )
+            added_config_setting = client.add_configuration_setting(config_setting)
+        """
+        return super(AzureAppConfigurationClient, self).add_configuration_setting(configuration_setting, **kwargs)
+
+    def update_configuration_setting(
+        self,
+        key,
+        value=None,
+        content_type=None,
+        tags=None,
+        label=None,
+        etag=None,
+        **kwargs
+    ):  # type: (str, str, str, dict, str, str, dict) -> ConfigurationSetting
+        """
+        Example
+
+        .. code-block:: python
+
+            updated_kv = client.update_configuration_setting(
+                key="MyKey",
+                label="MyLabel",
+                value="my updated value",
+                content_type=None,  # None means not to update it
+                tags={"my updated tag": "my updated tag value"}
+            )
+        """
+        custom_headers = AzureAppConfigurationClientAbstract.prep_update_configuration_setting(
+            key, etag, **kwargs
+        )
+
+        current_configuration_setting = self.get_configuration_setting(key, label)
+        if value is not None:
+            current_configuration_setting.value = value
+        if content_type is not None:
+            current_configuration_setting.content_type = content_type
+        if tags is not None:
+            current_configuration_setting.tags = tags
+        return self._impl.create_or_update_configuration_setting(
+            configuration_setting=current_configuration_setting,
+            key=key,
+            label=label,
+            headers=custom_headers,
+            error_map={404: ResourceNotFoundError, 412: ResourceModifiedError},
+        )
+
+    def set_configuration_setting(
+        self, configuration_setting, **kwargs
+    ):  # type: (ConfigurationSetting, dict) -> ConfigurationSetting
+        """
+        Example
+
+        .. code-block:: python
+
+            config_setting = ConfigurationSetting(
+                key="MyKey",
+                label="MyLabel",
+                value="my set value",
+                content_type="my set content type",
+                tags={"my set tag": "my set tag value"}
+            )
+            returned_config_setting = client.set_configuration_setting(config_setting)
+        """
+        return super(AzureAppConfigurationClient, self).set_configuration_setting(configuration_setting, **kwargs)
+
+    def delete_configuration_setting(
+        self, key, label=None, etag=None, **kwargs
+    ):  # type: (str, str, str, dict) -> ConfigurationSetting
+        """
+        Example
+
+        .. code-block:: python
+
+            deleted_config_setting = client.delete_configuration_setting(
+                key="MyKey", label="MyLabel"
+            )
+        """
+        return super(AzureAppConfigurationClient, self).delete_configuration_setting(key, label, etag, **kwargs)
+
+    def list_revisions(
+        self, labels=None, keys=None, accept_date_time=None, fields=None, **kwargs
+    ):  # type: (list, list, datetime, list, dict) -> Paged
+        """
+        Example
+
+        .. code-block:: python
+
+            from datetime import datetime, timedelta
+
+            accept_date_time = datetime.today() + timedelta(days=-1)
+
+            all_revisions = client.list_revisions()
+            for item in all_revisions:
+                pass  # do something
+
+            filtered_revisions = client.list_revisions(
+                labels=["*Labe*"], keys=["*Ke*"], accept_date_time=accept_date_time
+            )
+            for item in filtered_revisions:
+                pass  # do something
+        """
+        return super(AzureAppConfigurationClient, self).list_revisions(labels, keys, accept_date_time, fields, **kwargs)
+
+
+if platform.python_version() > "3.0":
+    AzureAppConfigurationClient.__doc__ = (
+        AzureAppConfigurationClientAbstract.__doc__ + AzureAppConfigurationClient.__doc__
+    )
+    AzureAppConfigurationClient.get_configuration_setting.__doc__ = (
+        AzureAppConfigurationClientAbstract.get_configuration_setting.__doc__
+        + AzureAppConfigurationClient.get_configuration_setting.__doc__
+    )
+    AzureAppConfigurationClient.add_configuration_setting.__doc__ = (
+        AzureAppConfigurationClientAbstract.add_configuration_setting.__doc__
+        + AzureAppConfigurationClient.add_configuration_setting.__doc__
+    )
+    AzureAppConfigurationClient.set_configuration_setting.__doc__ = (
+        AzureAppConfigurationClientAbstract.set_configuration_setting.__doc__
+        + AzureAppConfigurationClient.set_configuration_setting.__doc__
+    )
+    AzureAppConfigurationClient.update_configuration_setting.__doc__ = (
+        AzureAppConfigurationClientAbstract.update_configuration_setting.__doc__
+        + AzureAppConfigurationClient.update_configuration_setting.__doc__
+    )
+    AzureAppConfigurationClient.delete_configuration_setting.__doc__ = (
+        AzureAppConfigurationClientAbstract.delete_configuration_setting.__doc__
+        + AzureAppConfigurationClient.delete_configuration_setting.__doc__
+    )
+    AzureAppConfigurationClient.list_configuration_settings.__doc__ = (
+        AzureAppConfigurationClientAbstract.list_configuration_settings.__doc__
+        + AzureAppConfigurationClient.list_configuration_settings.__doc__
+    )
+    AzureAppConfigurationClient.list_revisions.__doc__ = (
+        AzureAppConfigurationClientAbstract.list_revisions.__doc__
+        + AzureAppConfigurationClient.list_revisions.__doc__
+    )
