d3d96df297e159f6273cc55fee920d8e099fb768
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-07-31 15:17:51 -0700

    Accept extension of JSON content-type (#6583)
    * Accept extension of JSON content-type
    
    * Adding text/something+json test
    
    * Support digit

--- sdk/core/azure-core/azure/core/pipeline/policies/universal.py
+++ sdk/core/azure-core/azure/core/pipeline/policies/universal.py
@@ -266,10 +266,9 @@ class NetworkTraceLoggingPolicy(SansIOHTTPPolicy):
 class ContentDecodePolicy(SansIOHTTPPolicy):
     """Policy for decoding unstreamed response content.
     """
-    JSON_MIMETYPES = [
-        'application/json',
-        'text/json' # Because we're open minded people...
-    ]
+    # Accept "text" because we're open minded people...
+    JSON_REGEXP = re.compile(r'^(application|text)/([0-9a-z+.]+\+)?json$')
+
     # Name used in context
     CONTEXT_NAME = "deserialized_data"
 
@@ -301,7 +300,7 @@ class ContentDecodePolicy(SansIOHTTPPolicy):
         if content_type is None:
             return data
 
-        if content_type in cls.JSON_MIMETYPES:
+        if cls.JSON_REGEXP.match(content_type):
             try:
                 return json.loads(data_as_str)
             except ValueError as err:
