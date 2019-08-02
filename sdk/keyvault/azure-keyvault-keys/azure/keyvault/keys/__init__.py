5a47ae5c29f724046c692be93f1ef93008e2652f
Charles Lowell <chlowe@microsoft.com>
2019-08-01 20:03:19 -0700

    enums

--- sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/__init__.py
+++ sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/__init__.py
@@ -3,5 +3,6 @@
 # Licensed under the MIT License.
 # -------------------------------------
 from .client import KeyClient
+from .enums import JsonWebKeyCurveName, JsonWebKeyOperation, JsonWebKeyType
 
-__all__ = ["KeyClient"]
+__all__ = ["JsonWebKeyCurveName", "JsonWebKeyOperation", "JsonWebKeyType", "KeyClient"]
