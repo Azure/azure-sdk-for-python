20adbbcc0f9ee0ac8aa91a49006e4e4f7c5d6c13
Krista Pratico <krpratic@microsoft.com>
2019-08-02 18:44:09 -0700

    adjust pypy testing

--- sdk/storage/azure-storage-blob/conftest.py
+++ sdk/storage/azure-storage-blob/conftest.py
@@ -8,9 +8,9 @@
 
 import sys
 import pytest
-
+import platform
 
 # Ignore async tests for Python < 3.5
 collect_ignore_glob = []
-if sys.version_info < (3, 5):
+if sys.version_info < (3, 5) or platform.python_implementation() == "PyPy":
     collect_ignore_glob.append("tests/*_async.py")
