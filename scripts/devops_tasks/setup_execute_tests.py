04aa542148fa524df7a655481ac804dc066a9235
Scott Beddall <scbedd@microsoft.com>
2019-08-05 16:28:59 -0700

    updating gitignore. adding ability to common tox.ini

--- scripts/devops_tasks/setup_execute_tests.py
+++ scripts/devops_tasks/setup_execute_tests.py
@@ -33,9 +33,9 @@ def prep_and_run_tox(targeted_packages, tox_env, options_array=[]):
 
         # # if not present, copy it
         if not os.path.exists(destination_tox_ini):
-            print('Tox.ini not available in package folder, copying base tox.ini to package folder.')
-            shutil.copyfile(DEFAULT_TOX_INI_LOCATION, destination_tox_ini)
-
+            print('Tox.ini not available in package folder, leveraging tox.ini to package folder.')
+            tox_execution_array.extend(['-c', DEFAULT_TOX_INI_LOCATION])
+            
         if tox_env:
             tox_execution_array.extend(['-e', tox_env])
 
