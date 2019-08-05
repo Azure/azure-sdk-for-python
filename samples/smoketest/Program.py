fde8cd72169016c9036ea97eedeb4d1fe0290c69
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2019-08-05 10:43:27 -0700

    Rename program.py

--- samples/smoketest/Program.py
+++
@@ -1,8 +0,0 @@
-import smoke_test
-
-try:
-    import smoke_test_async
-except SyntaxError:
-    print("\n===================")
-    print(" Async not suported")
-    print("====================\n")
