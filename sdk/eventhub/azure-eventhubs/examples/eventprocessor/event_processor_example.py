650b2aee497475ecb2ffdd5542fa1844ad77a2f2
yijxie <yijun.xie@micrsofot.com>
2019-08-05 15:15:07 -0700

    compatible with python < 3.7

--- sdk/eventhub/azure-eventhubs/examples/eventprocessor/event_processor_example.py
+++ sdk/eventhub/azure-eventhubs/examples/eventprocessor/event_processor_example.py
@@ -41,4 +41,5 @@ async def main():
         await partition_manager.close()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
