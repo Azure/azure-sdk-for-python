164d415e0438acadf0e4f4f5491beddd28e41309
Scott Beddall <scbedd@microsoft.com>
2019-08-06 21:28:08 -0700

    individually run tests

--- scripts/devops_tasks/common_tasks.py
+++ scripts/devops_tasks/common_tasks.py
@@ -14,21 +14,22 @@ from subprocess import check_call, CalledProcessError
 import os
 import sys
 
-DEFAULT_BUILD_PACKAGES = ['azure-keyvault', 'azure-servicebus']
-OMITTED_CI_PACKAGES = ['azure-mgmt-documentdb', 'azure-servicemanagement-legacy']
+OMITTED_CI_PACKAGES = ["azure-mgmt-documentdb", "azure-servicemanagement-legacy"]
 
 # this function is where a glob string gets translated to a list of packages
 # It is called by both BUILD (package) and TEST. In the future, this function will be the central location
 # for handling targeting of release packages
 def process_glob_string(glob_string, target_root_dir):
     if glob_string:
-        individual_globs = glob_string.split(',')
+        individual_globs = glob_string.split(",")
     else:
-        individual_globs = DEFAULT_BUILD_PACKAGES
+        individual_globs = "azure-*"
     collected_top_level_directories = []
 
     for glob_string in individual_globs:
-        globbed = glob.glob(os.path.join(target_root_dir, glob_string, 'setup.py')) + glob.glob(os.path.join(target_root_dir, "sdk/*/", glob_string, 'setup.py'))
+        globbed = glob.glob(
+            os.path.join(target_root_dir, glob_string, "setup.py")
+        ) + glob.glob(os.path.join(target_root_dir, "sdk/*/", glob_string, "setup.py"))
         collected_top_level_directories.extend([os.path.dirname(p) for p in globbed])
 
     # dedup, in case we have double coverage from the glob strings. Example: "azure-mgmt-keyvault,azure-mgmt-*"
@@ -43,20 +44,41 @@ def process_glob_string(glob_string, target_root_dir):
     else:
         return remove_omitted_packages(collected_directories)
 
+
 def remove_omitted_packages(collected_directories):
-    return [package_dir for package_dir in collected_directories if
-            os.path.basename(package_dir) not in OMITTED_CI_PACKAGES]
+    return [
+        package_dir
+        for package_dir in collected_directories
+        if os.path.basename(package_dir) not in OMITTED_CI_PACKAGES
+    ]
 
 
-def run_check_call(command_array, working_directory, acceptable_return_codes = [], run_as_shell = False):
+def run_check_call(
+    command_array,
+    working_directory,
+    acceptable_return_codes=[],
+    run_as_shell=False,
+    always_exit=True,
+):
     try:
         if run_as_shell:
-            print('Command Array: {0}, Target Working Directory: {1}'.format(' '.join(command_array), working_directory))
-            check_call(' '.join(command_array), cwd = working_directory, shell = True)
+            print(
+                "Command Array: {0}, Target Working Directory: {1}".format(
+                    " ".join(command_array), working_directory
+                )
+            )
+            check_call(" ".join(command_array), cwd=working_directory, shell=True)
         else:
-            print('Command Array: {0}, Target Working Directory: {1}'.format(command_array, working_directory))
-            check_call(command_array, cwd = working_directory)
+            print(
+                "Command Array: {0}, Target Working Directory: {1}".format(
+                    command_array, working_directory
+                )
+            )
+            check_call(command_array, cwd=working_directory)
     except CalledProcessError as err:
         if err.returncode not in acceptable_return_codes:
-            print(err) #, file = sys.stderr
-            sys.exit(1)
+            print(err)  # , file = sys.stderr
+            if always_exit:
+                exit(1)
+            else:
+                return err
--- scripts/devops_tasks/setup_execute_tests.py
+++ scripts/devops_tasks/setup_execute_tests.py
@@ -15,82 +15,125 @@ import os
 
 from common_tasks import process_glob_string, run_check_call
 
-root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..', '..'))
-dev_setup_script_location = os.path.join(root_dir, 'scripts/dev_setup.py')
+root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", ".."))
+dev_setup_script_location = os.path.join(root_dir, "scripts/dev_setup.py")
 
 # a return code of 5 from pytest == no tests run
 # evaluating whether we want this or not.
 ALLOWED_RETURN_CODES = []
 
-MANAGEMENT_PACKAGE_IDENTIFIERS = ['mgmt', 'azure-cognitiveservices', 'azure-servicefabric']
+MANAGEMENT_PACKAGE_IDENTIFIERS = [
+    "mgmt",
+    "azure-cognitiveservices",
+    "azure-servicefabric",
+]
+
 
 def prep_tests(targeted_packages, python_version):
-    print('running test setup for {}'.format(targeted_packages))
-    run_check_call([python_version, dev_setup_script_location, '-p', ','.join([os.path.basename(p) for p in targeted_packages])], root_dir)
+    print("running test setup for {}".format(targeted_packages))
+    run_check_call(
+        [
+            python_version,
+            dev_setup_script_location,
+            "-p",
+            ",".join([os.path.basename(p) for p in targeted_packages]),
+        ],
+        root_dir,
+    )
+
 
 def run_tests(targeted_packages, python_version, test_res):
-    # if we are targeting only packages that are management plane, it is a possibility 
+    err_results = []
+    # if we are targeting only packages that are management plane, it is a possibility
     # that no tests running is an acceptable situation
     # we explicitly handle this here.
-    if all(map(lambda x : any([pkg_id in x for pkg_id in MANAGEMENT_PACKAGE_IDENTIFIERS]), targeted_packages)):
+    if all(
+        map(
+            lambda x: any([pkg_id in x for pkg_id in MANAGEMENT_PACKAGE_IDENTIFIERS]),
+            targeted_packages,
+        )
+    ):
         ALLOWED_RETURN_CODES.append(5)
 
-    print('Running pytest for {}'.format(targeted_packages))
-    command_array = [python_version, '-m', 'pytest']
+    # base command array without a targeted package
+    command_array = [python_version, "-m", "pytest"]
     command_array.extend(test_res)
-    command_array.extend(targeted_packages)
-    run_check_call(command_array, root_dir, ALLOWED_RETURN_CODES, True)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Install Dependencies, Install Packages, Test Azure Packages, Called from DevOps YAML Pipeline')
-    parser.add_argument(
-        '-p',
-        '--python-version',
-        dest = 'python_version',
-        default = 'python',
-        help = 'The name of the python that should run the build. This is for usage in special cases like the "Special_Python_Distro_Tests" Job in /.azure-pipelines/client.yml. Defaults to "python"')
+    # loop through the
+    print("Running pytest for {}".format(targeted_packages))
 
-    parser.add_argument(
-        'glob_string',
-        nargs='?',
-        help = ('A comma separated list of glob strings that will target the top level directories that contain packages.'
-                'Examples: All = "azure-*", Single = "azure-keyvault", Targeted Multiple = "azure-keyvault,azure-mgmt-resource"'))
-
-    parser.add_argument(
-        '--junitxml',
-        dest='test_results',
-        help=('The folder where the test results will be stored in xml format.'
-              'Example: --junitxml="junit/test-results.xml"'))
-
-    parser.add_argument(
-        '--mark_arg',
-        dest='mark_arg',
-        help=('The complete argument for `pytest -m "<input>"`. This can be used to exclude or include specific pytest markers.'
-              '--mark_arg="not cosmosEmulator"'))
-
-    parser.add_argument(
-        '--disablecov',
-        help = ('Flag that disables code coverage.'),
-        action='store_true')
-
-    parser.add_argument(
-        '--service',
-        help=('Name of service directory (under sdk/) to test.'
-              'Example: --service applicationinsights'))
-
-    parser.add_argument(
-        '-r',
-        '--runtype',
-        choices = ['setup', 'execute', 'all'],
-        default = 'all'
+    for target_package in targeted_packages:
+        err_result = run_check_call(
+            command_array + [target_package], root_dir, ALLOWED_RETURN_CODES, True, False
         )
+        if err_result:
+            err_results.append(err_result)
+
+    if err_results:
+        exit(1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Install Dependencies, Install Packages, Test Azure Packages, Called from DevOps YAML Pipeline"
+    )
+    parser.add_argument(
+        "-p",
+        "--python-version",
+        dest="python_version",
+        default="python",
+        help='The name of the python that should run the build. This is for usage in special cases like the "Special_Python_Distro_Tests" Job in /.azure-pipelines/client.yml. Defaults to "python"',
+    )
+
+    parser.add_argument(
+        "glob_string",
+        nargs="?",
+        help=(
+            "A comma separated list of glob strings that will target the top level directories that contain packages."
+            'Examples: All = "azure-*", Single = "azure-keyvault", Targeted Multiple = "azure-keyvault,azure-mgmt-resource"'
+        ),
+    )
+
+    parser.add_argument(
+        "--junitxml",
+        dest="test_results",
+        help=(
+            "The folder where the test results will be stored in xml format."
+            'Example: --junitxml="junit/test-results.xml"'
+        ),
+    )
+
+    parser.add_argument(
+        "--mark_arg",
+        dest="mark_arg",
+        help=(
+            'The complete argument for `pytest -m "<input>"`. This can be used to exclude or include specific pytest markers.'
+            '--mark_arg="not cosmosEmulator"'
+        ),
+    )
+
+    parser.add_argument(
+        "--disablecov", help=("Flag that disables code coverage."), action="store_true"
+    )
+
+    parser.add_argument(
+        "--service",
+        help=(
+            "Name of service directory (under sdk/) to test."
+            "Example: --service applicationinsights"
+        ),
+    )
+
+    parser.add_argument(
+        "-r", "--runtype", choices=["setup", "execute", "all"], default="all"
+    )
 
     args = parser.parse_args()
 
     # We need to support both CI builds of everything and individual service
     # folders. This logic allows us to do both.
     if args.service:
-        service_dir = os.path.join('sdk', args.service)
+        service_dir = os.path.join("sdk", args.service)
         target_dir = os.path.join(root_dir, service_dir)
     else:
         target_dir = root_dir
@@ -98,20 +141,20 @@ if __name__ == '__main__':
     targeted_packages = process_glob_string(args.glob_string, target_dir)
     test_results_arg = []
     if args.test_results:
-        test_results_arg.extend(['--junitxml', args.test_results])
+        test_results_arg.extend(["--junitxml", args.test_results])
 
     if args.disablecov:
-        test_results_arg.append('--no-cov')
+        test_results_arg.append("--no-cov")
     else:
-        test_results_arg.extend(['--durations=10', '--cov', '--cov-report='])
+        test_results_arg.extend(["--durations=10", "--cov-append", "--cov-report="])
 
     if args.mark_arg:
-        test_results_arg.extend(['-m', '"{}"'.format(args.mark_arg)])
+        test_results_arg.extend(["-m", '"{}"'.format(args.mark_arg)])
 
     print(targeted_packages)
 
-    if args.runtype == 'setup' or args.runtype == 'all':
+    if args.runtype == "setup" or args.runtype == "all":
         prep_tests(targeted_packages, args.python_version)
-    
-    if args.runtype == 'execute' or args.runtype == 'all':
+
+    if args.runtype == "execute" or args.runtype == "all":
         run_tests(targeted_packages, args.python_version, test_results_arg)
