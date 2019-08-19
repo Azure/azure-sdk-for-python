aa84bd599ac82e2680ee27e45ae3b743a9155d07
Bryan Van de Ven <brvandev@microsoft.com>
2019-08-19 14:53:30 -0700

    unrelated test fix

--- sdk/cosmos/azure-cosmos/test/crud_tests.py
+++ sdk/cosmos/azure-cosmos/test/crud_tests.py
@@ -1946,7 +1946,7 @@ class CRUDTests(unittest.TestCase):
 
         root_included_path = __get_first([included_path for included_path in indexing_policy['includedPaths']
                                           if included_path['path'] == '/*'])
-        self.assertEqual(0, len(root_included_path['indexes']))
+        self.assertFalse('indexes' in root_included_path)
         print(root_included_path['indexes'])
 
     def test_client_request_timeout(self):
