7106f259f6248c57efe6aeb03aa26d2e813307de
Isabella Cai <iscai@microsoft.com>
2019-08-23 10:47:08 -0700

    implemented unique recording that always relys on RUN_IDENTIFIER

--- sdk/keyvault/azure-keyvault-keys/conftest.py
+++ sdk/keyvault/azure-keyvault-keys/conftest.py
@@ -6,11 +6,20 @@
 
 import os
 import sys
-import uuid
-
-import pytest
-
 # Ignore async tests for Python < 3.5
 collect_ignore_glob = []
 if sys.version_info < (3, 5):
     collect_ignore_glob.append("tests/*_async.py")
+
+os.environ['PYTHONHASHSEED'] = '0'
+dirname = os.path.dirname(__file__)
+seed_filename = os.path.join(dirname, "seed.txt")
+
+with open(seed_filename, "w+") as f:
+    if f.readline():
+        os.environ['RUN_IDENTIFIER'] = f.readline().strip()
+    else:
+        if "RUN_IDENTIFIER" not in os.environ:
+            print("Please set your RUN_IDENTIFIER environment variable in seed.txt")
+            raise NameError
+        f.write(os.environ["RUN_IDENTIFIER"])
