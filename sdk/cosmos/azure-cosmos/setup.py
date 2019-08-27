b4031ece01c1de76bcba9ccff38ebe0ca7df98d7
antisch <antisch@microsoft.com>
2019-08-26 17:06:09 -0700

    Updated dependencies

--- sdk/cosmos/azure-cosmos/setup.py
+++ sdk/cosmos/azure-cosmos/setup.py
@@ -66,7 +66,7 @@ setup(
     ),
     install_requires=[
       'six >=1.6',
-      'requests>=2.18.4'
+      'azure-core<2.0.0,>=1.0.0b2'
     ],
     extras_require={
       ":python_version<'3.0'": ["azure-nspkg"],
