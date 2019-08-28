6bb7e191982ee024d009f311ad8165e46ada5619
Charles Lowell <chlowe@microsoft.com>
2019-08-28 07:56:03 -0700

    Make azure-identity pylint clean (#6971)

--- sdk/identity/azure-identity/azure/identity/_internal/__init__.py
+++ sdk/identity/azure-identity/azure/identity/_internal/__init__.py
@@ -6,3 +6,12 @@ from .auth_code_redirect_handler import AuthCodeRedirectServer
 from .exception_wrapper import wrap_exceptions
 from .msal_credentials import ConfidentialClientCredential, PublicClientCredential
 from .msal_transport_adapter import MsalTransportAdapter, MsalTransportResponse
+
+__all__ = [
+    "AuthCodeRedirectServer",
+    "ConfidentialClientCredential",
+    "MsalTransportAdapter",
+    "MsalTransportResponse",
+    "PublicClientCredential",
+    "wrap_exceptions",
+]
