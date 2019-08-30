5e51ce27613d5df9907f5133f389e8cbbc0c25cc
yijxie <yijun.xie@micrsofot.com>
2019-08-29 17:15:37 -0700

    fix pylint errors

--- sdk/eventhub/azure-eventhubs/azure/eventhub/aio/producer_async.py
+++ sdk/eventhub/azure-eventhubs/azure/eventhub/aio/producer_async.py
@@ -185,7 +185,7 @@ class EventHubProducer(ConsumerProducerMixin):  # pylint: disable=too-many-insta
         return EventDataBatch(max_size=(max_size or self._max_message_size_on_link), partition_key=partition_key)
 
     async def send(
-            self, event_data:Union[EventData, EventDataBatch, Iterable[EventData]],
+            self, event_data: Union[EventData, EventDataBatch, Iterable[EventData]],
             *, partition_key: Union[str, bytes] = None, timeout: float = None):
         """
         Sends an event data and blocks until acknowledgement is
