2050615e9900ba78efa37b2d5d1a362dff49ce88
yijxie <yijun.xie@micrsofot.com>
2019-09-04 20:23:44 -0700

    raise error while list_ownership got an exception

--- sdk/eventhub/azure-eventhubs-aio-eventprocessor-partitionmanagers-blobstorage/azure/eventhub/aio/eventprocessor/partitionmanagers/blobstorage/blobstoragepm.py
+++ sdk/eventhub/azure-eventhubs-aio-eventprocessor-partitionmanagers-blobstorage/azure/eventhub/aio/eventprocessor/partitionmanagers/blobstorage/blobstoragepm.py
@@ -35,11 +35,10 @@ class BlobPartitionManager(PartitionManager):
     async def list_ownership(self, eventhub_name: str, consumer_group_name: str) -> Iterable[Dict[str, Any]]:
         try:
             blobs = self._container_client.list_blobs(include=['metadata'])
-        except AzureError as azure_err:  # list_blobs has exhausted retry
-            logger.exception("An exception occurred during list_ownership for eventhub %r consumer group %r."
-                             " An empty ownership list is returned",
-                             eventhub_name, consumer_group_name, exc_info=azure_err)
-            return []
+        except Exception as err:  # pylint:disable=broad-except
+            logger.warning("An exception occurred during list_ownership for eventhub %r consumer group %r. "
+                           "Exception is %r", eventhub_name, consumer_group_name, err)
+            raise
         async for b in blobs:  # TODO: running them concurrently
             async with self._cached_ownership_locks[b.name]:
                 metadata = b.metadata
@@ -74,7 +73,7 @@ class BlobPartitionManager(PartitionManager):
                 if etag:
                     etag_match = {"if_match": '"'+etag+'"'}
                 else:
-                    etag_match = {"if_none_match": '"*"'}
+                    etag_match = {"if_none_match": '*'}
                 try:
                     blob_client = await self._container_client.upload_blob(
                         name=partition_id, data=UPLOAD_DATA, overwrite=True, metadata=metadata, **etag_match
@@ -83,16 +82,16 @@ class BlobPartitionManager(PartitionManager):
                     ownership["etag"] = uploaded_blob_properties.etag
                     ownership["last_modified_time"] = uploaded_blob_properties.last_modified.timestamp()
                     self._cached_ownership_dict[partition_id] = ownership
+                    result.append(ownership)
                 except (ResourceModifiedError, ResourceExistsError):
                     logger.info(
                         "EventProcessor instance %r of eventhub %r consumer group %r lost ownership to partition %r",
                         owner_id, eventhub_name, consumer_group_name, partition_id)
                 except Exception as err:
-                    logger.exception("An exception occurred when EventProcessor instance %r claim_ownership for "
-                                     "eventhub %r consumer group %r partition %r. The ownership is now lost",
-                                     owner_id, eventhub_name, consumer_group_name, partition_id, exc_info=err)
-                else:
-                    result.append(ownership)
+                    logger.warning("An exception occurred when EventProcessor instance %r claim_ownership for "
+                                   "eventhub %r consumer group %r partition %r. The ownership is now lost. Exception "
+                                   "is %r", owner_id, eventhub_name, consumer_group_name, partition_id, err)
+
         return result
 
     async def update_checkpoint(self, eventhub_name, consumer_group_name, partition_id, owner_id,
@@ -118,8 +117,8 @@ class BlobPartitionManager(PartitionManager):
                     owner_id, eventhub_name, consumer_group_name, partition_id)
                 raise OwnershipLostError()
             except Exception as err:
-                logger.exception(
+                logger.warning(
                     "EventProcessor instance %r of eventhub %r consumer group %r couldn't update_checkpoint to "
-                    "partition %r because of unexpected error",
-                    owner_id, eventhub_name, consumer_group_name, partition_id, exc_info=err)
+                    "partition %r because of unexpected error. Exception is",
+                    owner_id, eventhub_name, consumer_group_name, partition_id, err)
                 raise  # EventProcessor will catch the exception and handle it
