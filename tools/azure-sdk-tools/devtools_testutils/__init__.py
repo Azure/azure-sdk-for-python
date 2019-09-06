6aca6f40e9d26208dacd4246220334b011ef395f
Krista Pratico <krpratic@microsoft.com>
2019-09-05 17:28:01 -0700

    cog sci test framework

--- tools/azure-sdk-tools/devtools_testutils/__init__.py
+++ tools/azure-sdk-tools/devtools_testutils/__init__.py
@@ -1,9 +1,13 @@
 from .mgmt_testcase import (AzureMgmtTestCase, AzureMgmtPreparer)
+from .azure_testcase import AzureTestCase
 from .resource_testcase import (FakeResource, ResourceGroupPreparer)
 from .storage_testcase import (FakeStorageAccount, StorageAccountPreparer)
+from .cognitiveservices_testcase import CognitiveServiceTest, CognitiveServicesAccountPreparer
 
 __all__ = [
     'AzureMgmtTestCase', 'AzureMgmtPreparer',
     'FakeResource', 'ResourceGroupPreparer',
     'FakeStorageAccount', 'StorageAccountPreparer',
+    'CognitiveServiceTest', 'AzureTestCase',
+    'CognitiveServicesAccountPreparer'
 ]
