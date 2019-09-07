4b41fa573a36c3bc9889cba6d25e10bea644768a
yijxie <yijun.xie@micrsofot.com>
2019-09-06 21:12:17 -0700

    Remove Callable

--- sdk/eventhub/azure-eventhubs/azure/eventhub/aio/eventprocessor/event_processor.py
+++ sdk/eventhub/azure-eventhubs/azure/eventhub/aio/eventprocessor/event_processor.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # -----------------------------------------------------------------------------------
 
-from typing import Callable, Dict, Type
+from typing import Dict, Type
 import uuid
 import asyncio
 import logging
