4b47b7cc14ab93f3711e22efd9eed9f14b16a101
xiafu <xiafu@microsoft.com>
2019-09-09 01:01:27 -0700

    [Blob][IdentitySAS]Stylistic Things
    Move parsing delegation key method to response_handler.py

--- sdk/storage/azure-storage-queue/azure/storage/queue/_shared/parser.py
+++ sdk/storage/azure-storage-queue/azure/storage/queue/_shared/parser.py
@@ -6,8 +6,6 @@
 
 import sys
 
-from .models import UserDelegationKey
-
 if sys.version_info < (3,):
     def _str(value):
         if isinstance(value, unicode):  # pylint: disable=undefined-variable
@@ -20,15 +18,3 @@ else:
 
 def _to_utc_datetime(value):
     return value.strftime('%Y-%m-%dT%H:%M:%SZ')
-
-
-def _parse_to_internal_user_delegation_key(service_user_delegation_key):
-    internal_user_delegation_key = UserDelegationKey()
-    internal_user_delegation_key.signed_oid = service_user_delegation_key.signed_oid
-    internal_user_delegation_key.signed_tid = service_user_delegation_key.signed_tid
-    internal_user_delegation_key.signed_start = _to_utc_datetime(service_user_delegation_key.signed_start)
-    internal_user_delegation_key.signed_expiry = _to_utc_datetime(service_user_delegation_key.signed_expiry)
-    internal_user_delegation_key.signed_service = service_user_delegation_key.signed_service
-    internal_user_delegation_key.signed_version = service_user_delegation_key.signed_version
-    internal_user_delegation_key.value = service_user_delegation_key.value
-    return internal_user_delegation_key
