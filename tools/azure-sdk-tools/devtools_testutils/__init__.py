4a7569f0450a1e6635a76f4daf8eb7e7623c91b8
Krista Pratico <krpratic@microsoft.com>
2019-09-09 15:03:41 -0700

    move text analytics to new test framework

--- tools/azure-sdk-tools/devtools_testutils/__init__.py
+++ tools/azure-sdk-tools/devtools_testutils/__init__.py
@@ -2,12 +2,10 @@ from .mgmt_testcase import (AzureMgmtTestCase, AzureMgmtPreparer)
 from .azure_testcase import AzureTestCase
 from .resource_testcase import (FakeResource, ResourceGroupPreparer)
 from .storage_testcase import (FakeStorageAccount, StorageAccountPreparer)
-from .cognitiveservices_testcase import CognitiveServiceTest, CognitiveServicesAccountPreparer
 
 __all__ = [
     'AzureMgmtTestCase', 'AzureMgmtPreparer',
     'FakeResource', 'ResourceGroupPreparer',
     'FakeStorageAccount', 'StorageAccountPreparer',
-    'CognitiveServiceTest', 'AzureTestCase',
-    'CognitiveServicesAccountPreparer'
+    'AzureTestCase',
 ]
