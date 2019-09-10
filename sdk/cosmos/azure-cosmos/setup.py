b5b4f8b3b6715ede76ba1b5296f89d3b5a8ba97a
annatisch <antisch@microsoft.com>
2019-09-09 17:57:58 -0700

    [Cosmos] Bumped dependency (#7147)
    * Bumped dependency
    
    * Update reqs

--- sdk/cosmos/azure-cosmos/setup.py
+++ sdk/cosmos/azure-cosmos/setup.py
@@ -70,7 +70,7 @@ setup(
     ),
     install_requires=[
       'six >=1.6',
-      'azure-core<2.0.0,>=1.0.0b2'
+      'azure-core<2.0.0,>=1.0.0b3'
     ],
     extras_require={
       ":python_version<'3.0'": ["azure-nspkg"],
