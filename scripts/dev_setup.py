9a3218a63217ae6c71dbb0f3cd081bcfa97849aa
Scott Beddall <scbedd@microsoft.com>
2019-09-09 21:26:01 -0700

    bugfix in dev_setup.py, bad syntax. adding 'develop' environment to the tox.ini

--- scripts/dev_setup.py
+++ scripts/dev_setup.py
@@ -46,7 +46,7 @@ parser.add_argument(
     "--disabledevelop",
     dest="develop_mode_disabled",
     default=False,
-    action="store_true"
+    action="store_true",
     help="Add this argument if you would prefer to install the package with a simple `pip install` versus `pip install -e`",
 )
 
