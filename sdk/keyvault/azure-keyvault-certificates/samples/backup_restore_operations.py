f28ff3b873eefa5b59b92e6727195490930a499c
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2019-09-12 10:35:58 -0700

    Key Vault: reworked certificates samples, all passing (#7225)
    * reworked certificates samples, all passing
    
    * automatically run sync samples instead of within a try catch block

--- sdk/keyvault/azure-keyvault-certificates/samples/backup_restore_operations.py
+++ sdk/keyvault/azure-keyvault-certificates/samples/backup_restore_operations.py
@@ -3,24 +3,19 @@
 # Licensed under the MIT License.
 # ------------------------------------
 import os
+import time
 from azure.keyvault.certificates import CertificateClient
 from azure.identity import DefaultAzureCredential
 from azure.core.exceptions import HttpResponseError
 
 # ----------------------------------------------------------------------------------------------------------
-# Prerequistes -
+# Prerequisites:
+# 1. An Azure Key Vault (https://docs.microsoft.com/en-us/azure/key-vault/quick-create-cli)
 #
-# 1. An Azure Key Vault-
-#    https://docs.microsoft.com/en-us/azure/key-vault/quick-create-cli
+# 2. azure-keyvault-certificates and azure-identity packages (pip install these)
 #
-#  2. Microsoft Azure Key Vault PyPI package -
-#    https://pypi.python.org/pypi/azure-keyvault-certificates/
-#
-# 3. Microsoft Azure Identity package -
-#    https://pypi.python.org/pypi/azure-identity/
-#
-# 4. Set Environment variables AZURE_CLIENT_ID, AZURE_TENANT_ID, AZURE_CLIENT_SECRET, VAULT_URL.
-# How to do this - https://github.com/Azure/azure-sdk-for-python/tree/master/sdk/keyvault/azure-keyvault-certificates#createget-credentials)
+# 3. Set Environment variables AZURE_CLIENT_ID, AZURE_TENANT_ID, AZURE_CLIENT_SECRET, VAULT_URL
+#    (See https://github.com/Azure/azure-sdk-for-python/tree/master/sdk/keyvault/azure-keyvault-keys#authenticate-the-client)
 #
 # ----------------------------------------------------------------------------------------------------------
 # Sample - demonstrates the basic backup and restore operations on a vault(certificates) resource for Azure Key Vault
@@ -36,53 +31,55 @@ from azure.core.exceptions import HttpResponseError
 # 5. Restore a certificate (restore_certificate)
 # ----------------------------------------------------------------------------------------------------------
 
-def run_sample():
-    # Instantiate a certificate client that will be used to call the service.
-    # Notice that the client is using default Azure credentials.
-    # To make default credentials work, ensure that environment variables 'AZURE_CLIENT_ID',
-    # 'AZURE_CLIENT_SECRET' and 'AZURE_TENANT_ID' are set with the service principal credentials.
-    VAULT_URL = os.environ["VAULT_URL"]
-    credential = DefaultAzureCredential()
-    client = CertificateClient(vault_url=VAULT_URL, credential=credential)
-    try:
+# Instantiate a certificate client that will be used to call the service.
+# Notice that the client is using default Azure credentials.
+# To make default credentials work, ensure that environment variables 'AZURE_CLIENT_ID',
+# 'AZURE_CLIENT_SECRET' and 'AZURE_TENANT_ID' are set with the service principal credentials.
+VAULT_URL = os.environ["VAULT_URL"]
+credential = DefaultAzureCredential()
+client = CertificateClient(vault_url=VAULT_URL, credential=credential)
+try:
 
-        print("\n1. Create Certificate")
-        cert_name = 'BackupRestoreCertificate'
+    print("\n.. Create Certificate")
+    cert_name = 'BackupRestoreCertificate'
 
-        # Let's create a certificate for your key vault.
-        # if the certificate already exists in the Key Vault, then a new version of the certificate is created.
-        # A long running poller is returned for the create certificate operation.
-        create_certificate_poller = client.create_certificate(name=cert_name)
+    # Let's create a certificate for your key vault.
+    # if the certificate already exists in the Key Vault, then a new version of the certificate is created.
+    # A long running poller is returned for the create certificate operation.
+    create_certificate_poller = client.create_certificate(name=cert_name)
 
-        # the wait call awaits the completion of the create certificate operation
-        create_certificate_poller.wait()
-        print("Certificate with name '{0}' created.".format(cert_name))
+    # The wait call awaits the completion of the create certificate operation
+    create_certificate_poller.wait()
+    print("Certificate with name '{0}' created.".format(cert_name))
 
-        # Backups are good to have, if in case certificates gets deleted accidentally.
-        # For long term storage, it is ideal to write the backup to a file.
-        print("\n2. Create a backup for an existing certificate")
-        certificate_backup = client.backup_certificate(name=cert_name)
-        print("Backup created for certificate with name '{0}'.".format(cert_name))
+    # Backups are good to have, if in case certificates gets deleted accidentally.
+    # For long term storage, it is ideal to write the backup to a file.
+    print("\n.. Create a backup for an existing certificate")
+    certificate_backup = client.backup_certificate(name=cert_name)
+    print("Backup created for certificate with name '{0}'.".format(cert_name))
 
-        # The storage account certificate is no longer in use, so you can delete it.
-        client.delete_certificate(name=cert_name)
-        print("Deleted Certificate with name '{0}'".format(cert_name))
+    # The storage account certificate is no longer in use, so you can delete it.
+    client.delete_certificate(name=cert_name)
+    # To ensure certificate is deleted on the server side.
+    time.sleep(30)
+    print("Deleted Certificate with name '{0}'".format(cert_name))
 
-        # In future, if the certificate is required again, we can use the backup value to restore it in the Key Vault.
-        print("\n3. Restore the certificate using the backed up certificate bytes")
-        certificate = client.restore_certificate(certificate_backup)
-        print("Restored Certificate with name '{0}'".format(certificate.name))
+    # Even though the certificate is deleted, it can still be recovered so its name cannot be reused.
+    # In order to be able to reuse the name during restoration, we must purge the certificate
+    # after the initial deletion.
+    print("\nPurging certificate...")
+    client.purge_deleted_certificate(name=cert_name)
+    # To ensure certificate is purged on the server side.
+    time.sleep(30)
+    print("Purged Certificate with name '{0}'".format(cert_name))
 
-    except HttpResponseError as e:
-        print("\nrun_sample has caught an error. {0}".format(e.message))
+    # In future, if the certificate is required again, we can use the backup value to restore it in the Key Vault.
+    print("\n.. Restore the certificate using the backed up certificate bytes")
+    certificate = client.restore_certificate(certificate_backup)
+    print("Restored Certificate with name '{0}'".format(certificate.name))
 
-    finally:
-        print("\nrun_sample done")
+except HttpResponseError as e:
+    print("\nrun_sample has caught an error. {0}".format(e.message))
 
-
-if __name__ == "__main__":
-    try:
-        run_sample()
-
-    except Exception as e:
-        print("Top level Error: {0}".format(str(e)))
+finally:
+    print("\nrun_sample done")
