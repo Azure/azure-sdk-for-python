58e3bf101ca74483d89d29cd8a8bc979cd4d368e
Krista Pratico <krpratic@microsoft.com>
2019-09-12 09:28:45 -0700

    [cognitive-services] cog services devtools test framework (#7087)
    * cog sci test framework
    
    * wait for account creation
    
    * add fixme for luis authoring key
    
    * have mgmt testcase inherit from azure testcase
    
    * move text analytics to new test framework
    
    * add back code in constructor for mgmt testcase

--- tools/azure-sdk-tools/devtools_testutils/azure_testcase.py
+++ tools/azure-sdk-tools/devtools_testutils/azure_testcase.py
@@ -1,20 +1,18 @@
-﻿#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 #--------------------------------------------------------------------------
-from collections import namedtuple
+
 import inspect
 import os.path
 import zlib
 
 from azure_devtools.scenario_tests import (
     ReplayableTest, AzureTestError,
-    AbstractPreparer, GeneralNameReplacer,
-    OAuthRequestResponsesFilter, DeploymentNameReplacer,
-    RequestUrlNormalizer
+    GeneralNameReplacer, RequestUrlNormalizer,
 )
-from .azure_testcase import AzureTestCase
+
 from .config import TEST_SETTING_FILENAME
 from . import mgmt_settings_fake as fake_settings
 
@@ -48,7 +46,7 @@ def get_qualified_method_name(obj, method_name):
     return '{0}.{1}'.format(module_name, method_name)
 
 
-class AzureMgmtTestCase(AzureTestCase):
+class AzureTestCase(ReplayableTest):
     def __init__(self, method_name, config_file=None,
                  recording_dir=None, recording_name=None,
                  recording_processors=None, replay_processors=None,
@@ -56,12 +54,11 @@ class AzureMgmtTestCase(AzureTestCase):
         self.working_folder = os.path.dirname(__file__)
         self.qualified_test_name = get_qualified_method_name(self, method_name)
         self._fake_settings, self._real_settings = self._load_settings()
-        self.region = 'westus'
         self.scrubber = GeneralNameReplacer()
         config_file = config_file or os.path.join(self.working_folder, TEST_SETTING_FILENAME)
         if not os.path.exists(config_file):
             config_file = None
-        super(AzureMgmtTestCase, self).__init__(
+        super(AzureTestCase, self).__init__(
             method_name,
             config_file=config_file,
             recording_dir=recording_dir,
@@ -72,12 +69,37 @@ class AzureMgmtTestCase(AzureTestCase):
             replay_patches=replay_patches,
         )
 
-    def _setup_scrubber(self):
-        constants_to_scrub = ['SUBSCRIPTION_ID', 'AD_DOMAIN', 'TENANT_ID', 'CLIENT_OID', 'ADLA_JOB_ID']
-        for key in constants_to_scrub:
-            if hasattr(self.settings, key) and hasattr(self._fake_settings, key):
-                self.scrubber.register_name_pair(getattr(self.settings, key),
-                                                 getattr(self._fake_settings, key))
+    @property
+    def settings(self):
+        if self.is_live:
+            if self._real_settings:
+                return self._real_settings
+            else:
+                raise AzureTestError('Need a mgmt_settings_real.py file to run tests live.')
+        else:
+            return self._fake_settings
+
+    def _load_settings(self):
+        try:
+            from . import mgmt_settings_real as real_settings
+            return fake_settings, real_settings
+        except ImportError:
+            return fake_settings, None
+
+    def _get_recording_processors(self):
+        return [
+            self.scrubber,
+            RequestUrlNormalizer()
+        ]
+
+    def _get_replay_processors(self):
+        return [
+            RequestUrlNormalizer()
+        ]
+
+    def is_playback(self):
+        return not self.is_live
+
 
     def setUp(self):
         # Every test uses a different resource group name calculated from its
@@ -95,65 +117,39 @@ class AzureMgmtTestCase(AzureTestCase):
         # would make resource group creation fail.
         # To avoid that, we also delete the resource group in the
         # setup, and we wait for that delete to complete.
-        self._setup_scrubber()
-        super(AzureMgmtTestCase, self).setUp()
+        super(AzureTestCase, self).setUp()
 
     def tearDown(self):
-        return super(AzureMgmtTestCase, self).tearDown()
+        return super(AzureTestCase, self).tearDown()
 
-    def create_mgmt_client(self, client_class, **kwargs):
-        # Whatever the client, if subscription_id is None, fail
+    def create_basic_client(self, client_class, **kwargs):
+        # Whatever the client, if credentials is None, fail
         with self.assertRaises(ValueError):
-            self.create_basic_client(
-                client_class,
-                subscription_id=None,
+            client = client_class(
+                credentials=None,
                 **kwargs
             )
 
-        return self.create_basic_client(
-            client_class,
-            subscription_id=self.settings.SUBSCRIPTION_ID,
+        # Real client creation
+        client = client_class(
+            credentials=self.settings.get_credentials(),
             **kwargs
         )
+        if self.is_playback():
+            client.config.long_running_operation_timeout = 0
+        client.config.enable_http_logger = True
+        return client
 
+    def create_random_name(self, name):
+        return get_resource_name(name, self.qualified_test_name.encode())
 
-class AzureMgmtPreparer(AbstractPreparer):
-    def __init__(self, name_prefix, random_name_length,
-                 disable_recording=True,
-                 playback_fake_resource=None,
-                 client_kwargs=None):
-        super(AzureMgmtPreparer, self).__init__(name_prefix, random_name_length,
-                                                disable_recording=disable_recording)
-        self.client = None
-        self.resource = playback_fake_resource
-        self.client_kwargs = client_kwargs or {}
+    def get_resource_name(self, name):
+        """Alias to create_random_name for back compatibility."""
+        return self.create_random_name(name)
 
-    @property
-    def is_live(self):
-        return self.test_class_instance.is_live
+    def get_preparer_resource_name(self, prefix):
+        """Random name generation for use by preparers.
 
-    def create_random_name(self):
-        return self.test_class_instance.get_preparer_resource_name(self.name_prefix)
-
-    @property
-    def moniker(self):
-        """Override moniker generation for backwards compatibility.
-
-        azure-devtools preparers, by default, generate "monikers" which replace
-        resource names in request URIs by tacking on a resource count to
-        name_prefix. By contrast, SDK tests used the fully qualified (module + method)
-        test name and the hashing process in get_resource_name.
-
-        This property override implements the SDK test name generation so that
-        the URIs don't change and tests don't need to be re-recorded.
-        """
-        if not self.resource_moniker:
-            self.resource_moniker = self.random_name
-        return self.resource_moniker
-
-    def create_mgmt_client(self, client_class):
-        return client_class(
-            credentials=self.test_class_instance.settings.get_credentials(),
-            subscription_id=self.test_class_instance.settings.SUBSCRIPTION_ID,
-            **self.client_kwargs
-        )
+        If prefix is a blank string, use the fully qualified test name instead.
+        This is what legacy tests do for resource groups."""
+        return self.get_resource_name(prefix or self.qualified_test_name.replace('.', '_'))
