55e2da526db84faa6565ebfe5835ef34c41eb6c7
Charles Lowell <chlowe@microsoft.com>
2019-09-19 12:32:12 -0700

    async wrapper for requests Session

--- sdk/identity/azure-identity/azure/identity/aio/_internal/__init__.py
+++ sdk/identity/azure-identity/azure/identity/aio/_internal/__init__.py
@@ -3,5 +3,6 @@
 # Licensed under the MIT License.
 # ------------------------------------
 from .exception_wrapper import wrap_exceptions
+from .msal_transport_adapter import MsalTransportAdapter
 
-__all__ = ["wrap_exceptions"]
+__all__ = ["MsalTransportAdapter", "wrap_exceptions"]
