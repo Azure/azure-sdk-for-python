15735f2749392bb8382b473b028488739386f04b
Charles Lowell <chlowe@microsoft.com>
2019-09-19 16:30:11 -0700

    tests

---
+++ sdk/identity/azure-identity/tests/test_aad_client_async.py
@@ -0,0 +1,39 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+from azure.identity.aio._internal.aad_client import AadClient
+import pytest
+
+from unittest.mock import Mock
+
+
+class MockClient(AadClient):
+    def __init__(self, *args, **kwargs):
+        self.session = kwargs.pop("session")
+        super(MockClient, self).__init__(*args, **kwargs)
+
+    def _get_client_session(self, **kwargs):
+        return self.session
+
+
+@pytest.mark.asyncio
+async def test_uses_msal_correctly():
+    session = Mock()
+    transport = Mock()
+    session.get = session.post = transport
+
+    client = MockClient("client id", "tenant id", session=session)
+
+    # MSAL will raise on each call because the mock transport returns nothing useful.
+    # That's okay because we only want to verify the transport was called, i.e. that
+    # the client used the MSAL API correctly, such that MSAL tried to send a request.
+    with pytest.raises(Exception):
+        await client.obtain_token_by_authorization_code("code", "redirect uri", "scope")
+    assert transport.call_count == 1
+
+    transport.reset_mock()
+
+    with pytest.raises(Exception):
+        await client.obtain_token_by_refresh_token("refresh token", "scope")
+    assert transport.call_count == 1
