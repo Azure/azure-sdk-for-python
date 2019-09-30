d56e1c0b4df6676de03a206717b1c807ce5d5457
Xiang Yan <xiangyan@microsoft.com>
2019-09-30 10:43:13 -0700

    add typing_extensions into azure core deps

--- sdk/core/azure-core/setup.py
+++ sdk/core/azure-core/setup.py
@@ -63,6 +63,7 @@ setup(
     install_requires=[
         'requests>=2.18.4',
         'enum34>=1.0.4',
+        'typing_extensions>=3.7.2',
     ],
     extras_require={
         ":python_version<'3.0'": ['azure-nspkg'],
