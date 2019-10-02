b49977802d065609d4ab5dac687b48b020cd0512
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-10-02 13:25:52 -0700

    Multipart/mixed in azure-core (#7083)
    * Serialize HTTPRequest
    
    * Basic deserialize Http Response
    
    * http.client response transport
    
    * Multipart helper - base version
    
    * Working full mock scenario
    
    * Parse multipart response based on content-type
    
    * Adding BOM tests
    
    * Improve a bit multipart generation
    
    * Refactor prepare_multipart_mixed
    
    * Core cleaning
    
    * Fix tests
    
    * Case insensitive dict fix
    
    * Simplify code
    
    * pylint
    
    * mypy
    
    * Python 2.7 improvement
    
    * Python 2.7 improvement, part2
    
    * Remove unecessary test
    
    * Refactor parts
    
    * Recursive multipart
    
    * No multipart serialization on 2.7
    
    * pylint
    
    * mypy
    
    * Fix test for 3.5
    
    * Adding six as dep for azure-core
    
    * Make analyze job happy
    
    * Skip test that assumes dict ordering for consistency in 3.5
    
    * Accept async on_response on async scenarios
    
    * Async on_request support
    
    * Complete support of async sansio in multipart with pipeline tests
    
    * Fix mock naming
    
    * pylint
    
    * ChangeLog / Readme

--- sdk/core/azure-core/azure/core/pipeline/transport/base_async.py
+++ sdk/core/azure-core/azure/core/pipeline/transport/base_async.py
@@ -26,15 +26,22 @@
 
 import asyncio
 import abc
+from collections.abc import AsyncIterator
 
-from typing import Any, List, Union, Callable, AsyncIterator, Optional, Generic, TypeVar
-from azure.core.pipeline import PipelineRequest, PipelineResponse, Pipeline
-from azure.core.pipeline.policies import SansIOHTTPPolicy
-from .base import _HttpResponseBase
+from typing import AsyncIterator as AsyncIteratorType, Generic, TypeVar
+from .base import (
+    _HttpResponseBase,
+    _HttpClientTransportResponse,
+    PipelineContext,
+    PipelineRequest,
+    PipelineResponse,
+)
+from ..base_async import _await_result
 
 try:
     from contextlib import AbstractAsyncContextManager  # type: ignore
-except ImportError: # Python <= 3.7
+except ImportError:  # Python <= 3.7
+
     class AbstractAsyncContextManager(object):  # type: ignore
         async def __aenter__(self):
             """Return `self` upon entering the runtime context."""
@@ -65,12 +72,61 @@ def _iterate_response_content(iterator):
         raise _ResponseStopIteration()
 
 
-class AsyncHttpResponse(_HttpResponseBase):
+class _PartGenerator(AsyncIterator):
+    """Until parts is a real async iterator, wrap the sync call.
+
+    :param parts: An iterable of parts
+    """
+
+    def __init__(self, response: "AsyncHttpResponse") -> None:
+        self._response = response
+        self._parts = None
+
+    async def _parse_response(self):
+        responses = self._response._get_raw_parts(  # pylint: disable=protected-access
+            http_response_type=AsyncHttpClientTransportResponse
+        )
+        if self._response.request.multipart_mixed_info:
+            policies = self._response.request.multipart_mixed_info[
+                1
+            ]  # type: List[SansIOHTTPPolicy]
+
+            async def parse_responses(response):
+                http_request = response.request
+                context = PipelineContext(None)
+                pipeline_request = PipelineRequest(http_request, context)
+                pipeline_response = PipelineResponse(
+                    http_request, response, context=context
+                )
+
+                for policy in policies:
+                    await _await_result(
+                        policy.on_response, pipeline_request, pipeline_response
+                    )
+
+            # Not happy to make this code asyncio specific, but that's multipart only for now
+            # If we need trio and multipart, let's reinvesitgate that later
+            await asyncio.gather(*[parse_responses(res) for res in responses])
+
+        return responses
+
+    async def __anext__(self):
+        if not self._parts:
+            self._parts = iter(await self._parse_response())
+
+        try:
+            return next(self._parts)
+        except StopIteration:
+            raise StopAsyncIteration()
+
+
+class AsyncHttpResponse(_HttpResponseBase):  # pylint: disable=abstract-method
     """An AsyncHttpResponse ABC.
 
     Allows for the asynchronous streaming of data from the response.
     """
-    def stream_download(self, pipeline) -> AsyncIterator[bytes]:
+
+    def stream_download(self, pipeline) -> AsyncIteratorType[bytes]:
         """Generator for streaming response body data.
 
         Should be implemented by sub-classes if streaming download
@@ -80,8 +136,35 @@ class AsyncHttpResponse(_HttpResponseBase):
         :type pipeline: azure.core.pipeline
         """
 
+    def parts(self) -> AsyncIterator:
+        """Assuming the content-type is multipart/mixed, will return the parts as an async iterator.
 
-class AsyncHttpTransport(AbstractAsyncContextManager, abc.ABC, Generic[HTTPRequestType, AsyncHTTPResponseType]):
+        :rtype: AsyncIterator
+        :raises ValueError: If the content is not multipart/mixed
+        """
+        if not self.content_type or not self.content_type.startswith("multipart/mixed"):
+            raise ValueError(
+                "You can't get parts if the response is not multipart/mixed"
+            )
+
+        return _PartGenerator(self)
+
+
+class AsyncHttpClientTransportResponse(_HttpClientTransportResponse, AsyncHttpResponse):
+    """Create a HTTPResponse from an http.client response.
+
+    Body will NOT be read by the constructor. Call "body()" to load the body in memory if necessary.
+
+    :param HttpRequest request: The request.
+    :param httpclient_response: The object returned from an HTTP(S)Connection from http.client
+    """
+
+
+class AsyncHttpTransport(
+    AbstractAsyncContextManager,
+    abc.ABC,
+    Generic[HTTPRequestType, AsyncHTTPResponseType],
+):
     """An http sender ABC.
     """
 
