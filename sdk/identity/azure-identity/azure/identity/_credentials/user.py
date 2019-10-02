82c6e867529ffd2341a5c3f74817941dc8502c6c
Charles Lowell <chlowe@microsoft.com>
2019-10-02 14:30:59 -0700

    MSAL credentials accept authority kwarg

--- sdk/identity/azure-identity/azure/identity/_credentials/user.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/user.py
@@ -46,6 +46,9 @@ class DeviceCodeCredential(PublicClientCredential):
         If not provided, the credential will print instructions to stdout.
 
     Keyword arguments
+        - *authority*: Authority of an Azure Active Directory endpoint, for example 'login.microsoftonline.com', the
+            authority for Azure Public Cloud (which is the default). :class:`~azure.identity.KnownAuthorities` defines
+            authorities for other clouds.
         - *tenant (str)* - tenant ID or a domain associated with a tenant. If not provided, defaults to the
           'organizations' tenant, which supports only Azure Active Directory work or school accounts.
         - *timeout (int)* - seconds to wait for the user to authenticate. Defaults to the validity period of the device
@@ -190,6 +193,9 @@ class UsernamePasswordCredential(PublicClientCredential):
     :param str password: the user's password
 
     Keyword arguments
+        - *authority*: Authority of an Azure Active Directory endpoint, for example 'login.microsoftonline.com', the
+            authority for Azure Public Cloud (which is the default). :class:`~azure.identity.KnownAuthorities` defines
+            authorities for other clouds.
         - *tenant (str)* - tenant ID or a domain associated with a tenant. If not provided, defaults to the
           'organizations' tenant, which supports only Azure Active Directory work or school accounts.
 
