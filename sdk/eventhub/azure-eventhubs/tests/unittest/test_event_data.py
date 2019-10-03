a50348559cd7e77761487c0654728874709eeec4
yijxie <yijun.xie@micrsofot.com>
2019-10-03 11:58:11 -0700

    Add unittest for EventData

--- sdk/eventhub/azure-eventhubs/tests/unittest/test_event_data.py
+++ sdk/eventhub/azure-eventhubs/tests/unittest/test_event_data.py
@@ -2,6 +2,30 @@ import pytest
 from azure.eventhub import EventData
 
 
-def test_create_event_data_with_none():
-    with pytest.raises(ValueError):
-        EventData(None)
+@pytest.mark.parametrize("test_input, expected_result",
+                         [("", ""), ("AAA", "AAA"), (None, ValueError), (["a", "b", "c"], "abc"), (b"abc", "abc")])
+def test_constructor(test_input, expected_result):
+    if isinstance(expected_result, type):
+        with pytest.raises(expected_result):
+            EventData(test_input)
+    else:
+        event_data = EventData(test_input)
+        assert event_data.body_as_str() == expected_result
+        assert event_data.partition_key is None
+        assert event_data.application_properties is None
+        assert event_data.enqueued_time is None
+        assert event_data.offset is None
+        assert event_data.sequence_number is None
+        assert event_data.system_properties == {}
+        with pytest.raises(TypeError):
+            event_data.body_as_json()
+
+
+def test_body_json():
+    event_data = EventData('{"a":"b"}')
+    jo = event_data.body_as_json()
+    assert jo["a"] == "b"
+
+
+def test_app_properties():
+    pass
