2cff24ebf47d880f695389ed848b473d5313bce8
scbedd <45376673+scbedd@users.noreply.github.com>
2019-10-02 17:36:34 -0700

    resolving trailing whitespace issue

--- sdk/core/azure-core/azure/core/pipeline/base.py
+++ sdk/core/azure-core/azure/core/pipeline/base.py
@@ -116,14 +116,13 @@ class Pipeline(AbstractContextManager, Generic[HTTPRequestType, HTTPResponseType
     :param list policies: List of configured policies.
 
     .. admonition:: Example:
-    
+
         .. literalinclude:: ../examples/test_example_sync.py
             :start-after: [START build_pipeline]
             :end-before: [END build_pipeline]
             :language: python
             :dedent: 4
             :caption: Builds the pipeline for synchronous transport.
-
     """
     def __init__(self, transport, policies=None):
         # type: (HttpTransportType, PoliciesType) -> None
