bc332e9e85c7beebe24244331cd3baf414224ae9
Charles Lowell <chlowe@microsoft.com>
2019-10-04 10:36:47 -0700

    move live test logic into a helper

--- sdk/identity/azure-identity/tests/test_live.py
+++ sdk/identity/azure-identity/tests/test_live.py
@@ -8,16 +8,20 @@ from azure.identity._internal import ConfidentialClientCredential
 ARM_SCOPE = "https://management.azure.com/.default"
 
 
+def get_token(credential):
+    token = credential.get_token(ARM_SCOPE)
+    assert token
+    assert token.token
+    assert token.expires_on
+
+
 def test_certificate_credential(live_certificate_settings):
     credential = CertificateCredential(
         live_certificate_settings["tenant_id"],
         live_certificate_settings["client_id"],
         live_certificate_settings["cert_path"],
     )
-    token = credential.get_token(ARM_SCOPE)
-    assert token
-    assert token.token
-    assert token.expires_on
+    get_token(credential)
 
 
 def test_client_secret_credential(live_identity_settings):
@@ -26,18 +30,12 @@ def test_client_secret_credential(live_identity_settings):
         live_identity_settings["client_id"],
         live_identity_settings["client_secret"],
     )
-    token = credential.get_token(ARM_SCOPE)
-    assert token
-    assert token.token
-    assert token.expires_on
+    get_token(credential)
 
 
 def test_default_credential(live_identity_settings):
     credential = DefaultAzureCredential()
-    token = credential.get_token(ARM_SCOPE)
-    assert token
-    assert token.token
-    assert token.expires_on
+    get_token(credential)
 
 
 def test_confidential_client_credential(live_identity_settings):
@@ -47,7 +45,4 @@ def test_confidential_client_credential(live_identity_settings):
         authority=KnownAuthorities.AZURE_PUBLIC_CLOUD,
         tenant_id=live_identity_settings["tenant_id"],
     )
-    token = credential.get_token(ARM_SCOPE)
-    assert token
-    assert token.token
-    assert token.expires_on
+    get_token(credential)
--- sdk/identity/azure-identity/tests/test_live_async.py
+++ sdk/identity/azure-identity/tests/test_live_async.py
@@ -4,15 +4,17 @@
 # ------------------------------------
 import os
 
-try:
-    from unittest import mock
-except ImportError:  # python < 3.3
-    import mock  # type: ignore
-
-from azure.identity.aio import DefaultAzureCredential, CertificateCredential, ClientSecretCredential
 import pytest
+from azure.identity.aio import DefaultAzureCredential, CertificateCredential, ClientSecretCredential
 
 ARM_SCOPE = "https://management.azure.com/.default"
+
+
+async def get_token(credential):
+    token = await credential.get_token(ARM_SCOPE)
+    assert token
+    assert token.token
+    assert token.expires_on
 
 
 @pytest.mark.asyncio
@@ -22,10 +24,7 @@ async def test_certificate_credential(live_certificate_settings):
         live_certificate_settings["client_id"],
         live_certificate_settings["cert_path"],
     )
-    token = await credential.get_token(ARM_SCOPE)
-    assert token
-    assert token.token
-    assert token.expires_on
+    await get_token(credential)
 
 
 @pytest.mark.asyncio
@@ -35,16 +34,10 @@ async def test_client_secret_credential(live_identity_settings):
         live_identity_settings["client_id"],
         live_identity_settings["client_secret"],
     )
-    token = await credential.get_token(ARM_SCOPE)
-    assert token
-    assert token.token
-    assert token.expires_on
+    await get_token(credential)
 
 
 @pytest.mark.asyncio
 async def test_default_credential(live_identity_settings):
     credential = DefaultAzureCredential()
-    token = await credential.get_token(ARM_SCOPE)
-    assert token
-    assert token.token
-    assert token.expires_on
+    await get_token(credential)
