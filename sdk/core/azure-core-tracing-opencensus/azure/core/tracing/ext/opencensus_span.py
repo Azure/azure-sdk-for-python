46070152edbb42f1fd9c8056dc620c64e20a81f8
Laurent Mazuel <lmazuel@microsoft.com>
2019-10-04 07:57:21 -0700

    pylint

--- sdk/core/azure-core-tracing-opencensus/azure/core/tracing/ext/opencensus_span.py
+++ sdk/core/azure-core-tracing-opencensus/azure/core/tracing/ext/opencensus_span.py
@@ -10,7 +10,7 @@ from opencensus.trace.span import SpanKind as OpenCensusSpanKind
 from opencensus.trace.link import Link
 from opencensus.trace.propagation import trace_context_http_header_format
 
-from azure.core.tracing import SpanKind
+from azure.core.tracing import SpanKind  # pylint: disable=no-name-in-module
 
 try:
     from typing import TYPE_CHECKING
