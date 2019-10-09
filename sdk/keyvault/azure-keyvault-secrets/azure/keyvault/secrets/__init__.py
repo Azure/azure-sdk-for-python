0f359a3f6fb7e54c77f9d44b088f9b4ddc07a3d6
Charles Lowell <chlowe@microsoft.com>
2019-10-09 10:49:28 -0700

    expose models at package level so they get documented

--- sdk/keyvault/azure-keyvault-secrets/azure/keyvault/secrets/__init__.py
+++ sdk/keyvault/azure-keyvault-secrets/azure/keyvault/secrets/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
+from ._models import DeletedSecret, Secret, SecretProperties
 from ._client import SecretClient
 
-__all__ = ["SecretClient"]
+__all__ = ["SecretClient", "Secret", "SecretProperties", "DeletedSecret"]
