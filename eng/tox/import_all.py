2807af678094c2bfde9db508954a653fa21a6574
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2019-10-11 17:16:42 -0700

    Verify module dependency

--- eng/tox/import_all.py
+++ eng/tox/import_all.py
@@ -6,13 +6,34 @@
 # --------------------------------------------------------------------------------------------
 
 # This script is used to verify package dependency by importing all modules
+import argparse
+import logging
 import os
 
-#get current package name
-current_dir = os.path.basename(os.getcwd())
-package_name = current_dir.replace('-','.')
+logging.getLogger().setLevel(logging.INFO)
 
-#import all modules from current package
-import_script_all = 'from {0} import *'.format(package_name)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Import all modules in package"
+    )
 
-exec(import_script_all)
+    parser.add_argument(
+        "-t",
+        "--target",
+        dest="target_package",
+        help="The target package directory on disk.",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    #get target package name from target package path
+    target_pkg = os.path.basename(os.path.abspath(args.target_package))
+    package_name = target_pkg.replace('-','.')
+
+    #import all modules from current package
+    logging.info("Importing all modules from package [{0}] to verify dependency".format(package_name))
+    import_script_all = 'from {0} import *'.format(package_name)
+
+    exec(import_script_all)
+    logging.info("Verified module dependency, no issues found")
