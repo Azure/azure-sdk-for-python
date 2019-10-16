c11cd596e87cd39d3d5787753443ce08930aa2cb
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2019-10-16 11:37:25 -0700

    Verify package dependency by importing all modules (#7907)
    * New tox environment to verify package dependency

--- eng/tox/import_all.py
+++ eng/tox/import_all.py
@@ -9,8 +9,23 @@
 import argparse
 import logging
 import os
+from tox_helper_tasks import get_package_details
 
 logging.getLogger().setLevel(logging.INFO)
+
+# keyvault has dependency issue when loading private module _BearerTokenCredentialPolicyBase from azure.core.pipeline.policies
+# azure.core.tracing.opencensus and azure.eventhub.checkpointstoreblob.aio are skipped due to a known issue in loading azure.core.tracing.opencensus
+excluded_packages = [
+    "azure.core.tracing.opencensus", 
+    "azure.eventhub.checkpointstoreblob.aio",
+    "azure.keyvault.certificates", # Github issue 7879
+    "azure.keyvault.keys", # Github issue 7879
+    "azure.keyvault.secrets", # Github issue 7879
+    "azure",
+    "azure-mgmt"]
+
+def should_run_import_all(package_name):
+    return not (package_name in excluded_packages or "nspkg" in package_name)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Import all modules in package")
@@ -26,15 +41,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # get target package name from target package path
-    target_pkg = os.path.basename(os.path.abspath(args.target_package))
-    package_name = target_pkg.replace("-", ".")
+    pkg_dir = os.path.abspath(args.target_package)
+    pkg_name, _ = get_package_details(os.path.join(pkg_dir, 'setup.py'))
+    package_name = pkg_name.replace("-", ".")
 
-    # import all modules from current package
-    logging.info(
-        "Importing all modules from package [{0}] to verify dependency".format(
-            package_name
+    if should_run_import_all(package_name):
+        # import all modules from current package
+        logging.info(
+            "Importing all modules from package [{0}] to verify dependency".format(
+                package_name
+            )
         )
-    )
-    import_script_all = "from {0} import *".format(package_name)
-    exec(import_script_all)
-    logging.info("Verified module dependency, no issues found")
+        import_script_all = "from {0} import *".format(package_name)
+        exec(import_script_all)
+        logging.info("Verified module dependency, no issues found")
+    else:
+        pass
+        logging.error("Package {} is excluded from dependency check".format(package_name)) 
