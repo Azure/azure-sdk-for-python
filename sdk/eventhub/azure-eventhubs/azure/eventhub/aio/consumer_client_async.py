aa532d8f34987cd12db353b676ed34bd59cc5cbb
yijxie <yijun.xie@micrsofot.com>
2019-10-18 16:52:16 -0700

    Validate receiving partitions

--- sdk/eventhub/azure-eventhubs/azure/eventhub/aio/consumer_client_async.py
+++ sdk/eventhub/azure-eventhubs/azure/eventhub/aio/consumer_client_async.py
@@ -24,7 +24,8 @@ class EventHubConsumerClient(EventHubClient):
         # type:(str, str, Union[EventHubSharedKeyCredential, EventHubSASTokenCredential, TokenCredential], Any) -> None
         super(EventHubConsumerClient, self).__init__(host=host, event_hub_path=event_hub_path, credential=credential, **kwargs)
         self._partition_manager = kwargs.get("partition_manager")
-        self._event_processors = []
+        self._event_processors = dict()
+        self._closed = False
 
     async def receive(
             self, event_handler, consumer_group, *, partition_id=None,
@@ -34,6 +35,16 @@ class EventHubConsumerClient(EventHubClient):
 
         """
         async with self._lock:
+            if 'all' in self._event_processors:
+                raise ValueError("This consumer client is already receiving events from all partitions. "
+                                 "Shouldn't receive from any other partitions again")
+            elif partition_id is None and self._event_processors:
+                raise ValueError("This consumer client is already receiving events. "
+                                 "Shouldn't receive from all partitions again")
+            elif partition_id in self._event_processors:
+                raise ValueError("This consumer is already receiving events from partition {}. "
+                                 "Shouldn't receive from it again.".format(partition_id))
+
             event_processor = EventProcessor(
                 self, consumer_group, event_handler,
                 partition_id=partition_id,
@@ -44,9 +55,19 @@ class EventHubConsumerClient(EventHubClient):
                 initial_event_position=initial_event_position or EventPosition("-1"),
                 polling_interval=polling_interval or 10
             )
-            self._event_processors.append(event_processor)
-
-        await event_processor.start()
+            if partition_id:
+                self._event_processors[partition_id] = event_processor
+            else:
+                self._event_processors["all"] = event_processor
+        try:
+            await event_processor.start()
+        finally:
+            async with self._lock:
+                await event_processor.stop()
+                if partition_id and partition_id in self._event_processors:
+                    del self._event_processors[partition_id]
+                elif 'all' in self._event_processors:
+                    del self._event_processors['all']
 
     async def update_checkpoint(self, event):
         self._partition_manager.update_checkpoint(event)
@@ -57,6 +78,7 @@ class EventHubConsumerClient(EventHubClient):
 
         """
         async with self._lock:
-            for ep in self._event_processors:
+            for _ in range(len(self._event_processors)):
+                _, ep = self._event_processors.popitem()
                 await ep.stop()
             await super().close()
