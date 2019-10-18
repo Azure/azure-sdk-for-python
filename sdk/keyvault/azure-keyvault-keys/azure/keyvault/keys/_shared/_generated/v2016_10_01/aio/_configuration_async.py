6ffa48843602a815e6d25f3b44fe381511b2e829
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2019-10-18 16:55:12 -0700

    Key Vault: regenerated keyvault for paging logging (#8022)
    * regenerated keyvault for paging logging
    
    * fixed kwargs to pop max_page_size

--- sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/_shared/_generated/v2016_10_01/aio/_configuration_async.py
+++ sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/_shared/_generated/v2016_10_01/aio/_configuration_async.py
@@ -8,7 +8,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is
 # regenerated.
 # --------------------------------------------------------------------------
-from azure.core.configuration import Configuration, ConnectionConfiguration
+from azure.core.configuration import Configuration
 from azure.core.pipeline import policies
 
 from ..version import VERSION
@@ -38,10 +38,10 @@ class KeyVaultClientConfiguration(Configuration):
         self.credentials = credentials
 
     def _configure(self, **kwargs):
-        self.connection = ConnectionConfiguration(**kwargs)
-        self.user_agent_policy = policies.UserAgentPolicy(**kwargs)
-        self.headers_policy = policies.HeadersPolicy(**kwargs)
-        self.proxy_policy = policies.ProxyPolicy(**kwargs)
-        self.logging_policy = policies.NetworkTraceLoggingPolicy(**kwargs)
-        self.retry_policy = policies.AsyncRetryPolicy(**kwargs)
-        self.redirect_policy = policies.AsyncRedirectPolicy(**kwargs)
+        self.user_agent_policy = kwargs.get('user_agent_policy') or policies.UserAgentPolicy(**kwargs)
+        self.headers_policy = kwargs.get('headers_policy') or policies.HeadersPolicy(**kwargs)
+        self.proxy_policy = kwargs.get('proxy_policy') or policies.ProxyPolicy(**kwargs)
+        self.logging_policy = kwargs.get('logging_policy') or policies.NetworkTraceLoggingPolicy(**kwargs)
+        self.retry_policy = kwargs.get('retry_policy') or policies.AsyncRetryPolicy(**kwargs)
+        self.custom_hook_policy = kwargs.get('custom_hook_policy') or policies.CustomHookPolicy(**kwargs)
+        self.redirect_policy = kwargs.get('redirect_policy') or policies.AsyncRedirectPolicy(**kwargs)
