2a55494584220c82dbf044ba161a6b2e73066f23
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-10-18 12:10:39 -0700

    azure-keyvault meta-package

--- eng/tox/import_all.py
+++ eng/tox/import_all.py
@@ -16,13 +16,15 @@ logging.getLogger().setLevel(logging.INFO)
 # keyvault has dependency issue when loading private module _BearerTokenCredentialPolicyBase from azure.core.pipeline.policies
 # azure.core.tracing.opencensus and azure.eventhub.checkpointstoreblob.aio are skipped due to a known issue in loading azure.core.tracing.opencensus
 excluded_packages = [
-    "azure.core.tracing.opencensus", 
+    "azure.core.tracing.opencensus",
     "azure.eventhub.checkpointstoreblob.aio",
     "azure.keyvault.certificates", # Github issue 7879
     "azure.keyvault.keys", # Github issue 7879
     "azure.keyvault.secrets", # Github issue 7879
     "azure",
-    "azure-mgmt"]
+    "azure.mgmt",
+    "azure.keyvault",
+]
 
 def should_run_import_all(package_name):
     return not (package_name in excluded_packages or "nspkg" in package_name)
@@ -57,4 +59,4 @@ if __name__ == "__main__":
         logging.info("Verified module dependency, no issues found")
     else:
         pass
-        logging.error("Package {} is excluded from dependency check".format(package_name)) 
+        logging.error("Package {} is excluded from dependency check".format(package_name))
