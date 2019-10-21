c070521a3bd234d5ae79580c25b6b12c76d997a9
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2019-10-21 16:00:37 -0700

    Key Vault: pass through keys and secrets docstrings (#8055)
    * fixed keys docs
    
    * fixed secrets docs
    
    * fixed some pylint issues, pylint crashing on laptop
    
    * implemented Charles' comments
    
    * implemented Charles' comments
    
    * implemented Krista's comments

--- sdk/keyvault/azure-keyvault-secrets/samples/recover_purge_operations_async.py
+++ sdk/keyvault/azure-keyvault-secrets/samples/recover_purge_operations_async.py
@@ -18,9 +18,9 @@ from azure.core.exceptions import HttpResponseError
 #    (See https://github.com/Azure/azure-sdk-for-python/tree/master/sdk/keyvault/azure-keyvault-keys#authenticate-the-client)
 #
 # ----------------------------------------------------------------------------------------------------------
-# Sample - demonstrates the basic recover and purge operations on a vault(secret) resource for Azure Key Vault. The vault
-# has to be soft-delete enabled to perform the following operations. See for more information about soft delete:
-# # https://docs.microsoft.com/en-us/azure/key-vault/key-vault-ovw-soft-delete
+# Sample - demonstrates the basic list operations on a vault(secret) resource for Azure Key Vault.
+# The vault has to be soft-delete enabled to perform one of the following operations. See
+# https://docs.microsoft.com/en-us/azure/key-vault/key-vault-ovw-soft-delete for more information about soft-delete.
 #
 # 1. Create a secret (set_secret)
 #
@@ -74,7 +74,7 @@ async def run_sample():
 
     except HttpResponseError as e:
         if "(NotSupported)" in e.message:
-            print("\n{0} Please enable soft delete on Key Vault to perform this operation.".format(e.message))
+            print("\n{0} Please enable soft-delete on Key Vault to perform this operation.".format(e.message))
         else:
             print("\nrun_sample has caught an error. {0}".format(e.message))
 
