fd1417708c426dea101b28faa8c81c94164bd437
Xiang Yan <xiangyan@microsoft.com>
2019-10-23 16:53:26 -0700

    typo

--- sdk/appconfiguration/azure-appconfiguration/azure/appconfiguration/aio/_azure_configuration_client_async.py
+++ sdk/appconfiguration/azure-appconfiguration/azure/appconfiguration/aio/_azure_configuration_client_async.py
@@ -199,7 +199,7 @@ class AzureAppConfigurationClient:
         :type label: str
         :param etag: check if the ConfigurationSetting is changed. Set None to skip checking etag
         :type etag: str or None
-        :param `azure.core.MatchConditions match_condition: the match condition to use upon the etag
+        :param ~azure.core.MatchConditions match_condition: the match condition to use upon the etag
         :keyword datetime accept_datetime: the retrieved ConfigurationSetting that created no later than this datetime
         :keyword dict headers: if "headers" exists, its value (a dict) will be added to the http request header
         :return: The matched ConfigurationSetting object
@@ -309,7 +309,7 @@ class AzureAppConfigurationClient:
         :param configuration_setting: the ConfigurationSetting to be added (if not exists) \
         or updated (if exists) to the service
         :type configuration_setting: :class:`ConfigurationSetting`
-        :param `azure.core.MatchConditions match_condition: the match condition to use upon the etag
+        :param ~azure.core.MatchConditions match_condition: the match condition to use upon the etag
         :keyword dict headers: if "headers" exists, its value (a dict) will be added to the http request header
         :return: The ConfigurationSetting returned from the service
         :rtype: :class:`ConfigurationSetting`
@@ -378,7 +378,7 @@ class AzureAppConfigurationClient:
         :param label: label used to identify the ConfigurationSetting
         :type label: str
         :keyword str etag: check if the ConfigurationSetting is changed. Set None to skip checking etag
-        :keyword `azure.core.MatchConditions match_condition: the match condition to use upon the etag
+        :keyword ~azure.core.MatchConditions match_condition: the match condition to use upon the etag
         :keyword dict headers: if "headers" exists, its value (a dict) will be added to the http request
         :return: The deleted ConfigurationSetting returned from the service, or None if it doesn't exist.
         :rtype: :class:`ConfigurationSetting`
