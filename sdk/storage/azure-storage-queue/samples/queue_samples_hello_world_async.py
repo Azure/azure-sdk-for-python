119271f5e6c94798e8dd477537141172d7d5bb1c
Brandon Siegel <brandon.siegel@microsoft.com>
2019-10-24 11:38:25 -0700

    Update hello world samples

--- sdk/storage/azure-storage-queue/samples/queue_samples_hello_world_async.py
+++ sdk/storage/azure-storage-queue/samples/queue_samples_hello_world_async.py
@@ -6,40 +6,25 @@
 # license information.
 # --------------------------------------------------------------------------
 
-import pytest
 import asyncio
-from devtools_testutils import ResourceGroupPreparer, StorageAccountPreparer
-from multidict import CIMultiDict, CIMultiDictProxy
-from asyncqueuetestcase import (
-    AsyncQueueTestCase
-)
+import os
 
+class QueueHelloWorldSamplesAsync(object):
 
+    connection_string = os.getenv("CONNECTION_STRING")
 
-class TestQueueHelloWorldSamplesAsync(AsyncQueueTestCase):
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    @AsyncQueueTestCase.await_prepared_test
-    async def test_create_client_with_connection_string(self, resource_group, location, storage_account, storage_account_key):
-        conn_str = self.connection_string(storage_account, storage_account_key)
+    async def create_client_with_connection_string(self):
         # Instantiate the QueueServiceClient from a connection string
         from azure.storage.queue.aio import QueueServiceClient
-        queue_service = QueueServiceClient.from_connection_string(conn_str)
+        queue_service = QueueServiceClient.from_connection_string(conn_str=self.connection_string)
 
         # Get queue service properties
         properties = await queue_service.get_service_properties()
 
-        assert properties is not None
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    @AsyncQueueTestCase.await_prepared_test
-    async def test_queue_and_messages_example(self, resource_group, location, storage_account, storage_account_key):
-        conn_str = self.connection_string(storage_account, storage_account_key)
+    async def queue_and_messages_example(self):
         # Instantiate the QueueClient from a connection string
         from azure.storage.queue.aio import QueueClient
-        queue = QueueClient.from_connection_string(conn_str, "myasyncqueue")
+        queue = QueueClient.from_connection_string(conn_str=self.connection_string, queue_name="my_queue")
 
         # Create the queue
         # [START async_create_queue]
@@ -50,7 +35,8 @@ class TestQueueHelloWorldSamplesAsync(AsyncQueueTestCase):
             # Send messages
             await asyncio.gather(
                 queue.send_message(u"I'm using queues!"),
-                queue.send_message(u"This is my second message"))
+                queue.send_message(u"This is my second message")
+            )
 
             # Receive the messages
             response = queue.receive_messages(messages_per_page=2)
