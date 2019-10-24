3434e38fe52aa8c3d59c9a9ef9f36d06a14d6147
Feng Zhou <55177366+fengzhou-msft@users.noreply.github.com>
2019-10-24 16:14:29 +0800

    Peering 2019-09-01-preview (#8172)
    * peering 2019-09-01-preview
    
    * add peering test
    
    * fix recording file location
    
    * changelog

--- sdk/peering/azure-mgmt-peering/azure/mgmt/peering/operations/_peer_asns_operations.py
+++ sdk/peering/azure-mgmt-peering/azure/mgmt/peering/operations/_peer_asns_operations.py
@@ -24,7 +24,7 @@ class PeerAsnsOperations(object):
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
-    :ivar api_version: The client API version. Constant value: "2019-08-01-preview".
+    :ivar api_version: The client API version. Constant value: "2019-09-01-preview".
     """
 
     models = models
@@ -34,7 +34,7 @@ class PeerAsnsOperations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
-        self.api_version = "2019-08-01-preview"
+        self.api_version = "2019-09-01-preview"
 
         self.config = config
 
