f48e9eca2e9b97b07c6e49c0b9bf99d9d37e16c5
Brandon Siegel <brandon.siegel@microsoft.com>
2019-10-24 12:13:45 -0700

    Update authentication samples

--- sdk/storage/azure-storage-queue/samples/queue_samples_authentication.py
+++ sdk/storage/azure-storage-queue/samples/queue_samples_authentication.py
@@ -7,59 +7,40 @@
 # --------------------------------------------------------------------------
 
 from datetime import datetime, timedelta
-import pytest
-from devtools_testutils import ResourceGroupPreparer, StorageAccountPreparer
+import os
 
-try:
-    from devtools_testutils import mgmt_settings_real as settings
-except ImportError:
-    from devtools_testutils import mgmt_settings_fake as settings
+class QueueAuthSamples(object):
 
-from queuetestcase import (
-    QueueTestCase
-)
+    connection_string = os.getenv("CONNECTION_STRING")
 
+    account_url = os.getenv("ACCOUNT_URL")
+    access_key = os.getenv("ACCESS_KEY")
 
-class TestQueueAuthSamples(QueueTestCase):
+    active_directory_application_id = os.getenv("ACTIVE_DIRECTORY_APPLICATION_ID")
+    active_directory_application_secret = os.getenv("ACTIVE_DIRECTORY_APPLICATION_SECRET")
+    active_directory_tenant_id = os.getenv("ACTIVE_DIRECTORY_TENANT_ID")
 
-    active_directory_application_id = settings.ACTIVE_DIRECTORY_APPLICATION_ID
-    active_directory_application_secret = settings.ACTIVE_DIRECTORY_APPLICATION_SECRET
-    active_directory_tenant_id = settings.ACTIVE_DIRECTORY_TENANT_ID
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    def test_auth_connection_string(self, resource_group, location, storage_account, storage_account_key):
+    def auth_connection_string(self):
         # Instantiate a QueueServiceClient using a connection string
         # [START auth_from_connection_string]
         from azure.storage.queue import QueueServiceClient
-        queue_service = QueueServiceClient(self._account_url(storage_account.name), storage_account_key)
+        queue_service = QueueServiceClient.from_connection_string(conn_str=self.connection_string)
         # [END auth_from_connection_string]
 
         # Get information for the Queue Service
         properties = queue_service.get_service_properties()
 
-        assert properties is not None
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    def test_auth_shared_key(self, resource_group, location, storage_account, storage_account_key):
-
+    def auth_shared_key(self):
         # Instantiate a QueueServiceClient using a shared access key
         # [START create_queue_service_client]
         from azure.storage.queue import QueueServiceClient
-        queue_service = QueueServiceClient(self._account_url(storage_account.name), storage_account_key)
+        queue_service = QueueServiceClient(account_url=self.account_url, credential=self.access_key)
         # [END create_queue_service_client]
+
         # Get information for the Queue Service
         properties = queue_service.get_service_properties()
 
-        assert properties is not None
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    def test_auth_active_directory(self, resource_group, location, storage_account, storage_account_key):
-        if not self.is_live:
-            return
-        url = self._account_url(storage_account.name)
+    def auth_active_directory(self):
         # [START create_queue_service_client_token]
         # Get a token credential for authentication
         from azure.identity import ClientSecretCredential
@@ -71,24 +52,16 @@ class TestQueueAuthSamples(QueueTestCase):
 
         # Instantiate a QueueServiceClient using a token credential
         from azure.storage.queue import QueueServiceClient
-        queue_service = QueueServiceClient(account_url=url, credential=token_credential)
+        queue_service = QueueServiceClient(account_url=self.account_url, credential=token_credential)
         # [END create_queue_service_client_token]
 
         # Get information for the Queue Service
         properties = queue_service.get_service_properties()
 
-        assert properties is not None
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    def test_auth_shared_access_signature(self, resource_group, location, storage_account, storage_account_key):
-        # SAS URL is calculated from storage key, so this test runs live only
-        if not self.is_live:
-            return
-        connection_string = self.connection_string(storage_account, storage_account_key)
+    def auth_shared_access_signature(self):
         # Instantiate a QueueServiceClient using a connection string
         from azure.storage.queue import QueueServiceClient
-        queue_service = QueueServiceClient.from_connection_string(connection_string)
+        queue_service = QueueServiceClient.from_connection_string(conn_str=self.connection_string)
 
         # Create a SAS token to use for authentication of a client
         from azure.storage.queue import generate_account_sas
@@ -101,4 +74,7 @@ class TestQueueAuthSamples(QueueTestCase):
             expiry=datetime.utcnow() + timedelta(hours=1)
         )
 
-        assert sas_token is not None
+        token_auth_queue_service = QueueServiceClient(account_url=self.account_url, credential=sas_token)
+
+        # Get information for the Queue Service
+        properties = token_auth_queue_service.get_service_properties()
--- sdk/storage/azure-storage-queue/samples/queue_samples_authentication_async.py
+++ sdk/storage/azure-storage-queue/samples/queue_samples_authentication_async.py
@@ -7,81 +7,62 @@
 # --------------------------------------------------------------------------
 
 from datetime import datetime, timedelta
-from collections import namedtuple
-import pytest
 import asyncio
-from devtools_testutils import ResourceGroupPreparer, StorageAccountPreparer, mgmt_settings_fake as settings
+import os
 
-from asyncqueuetestcase import (
-    AsyncQueueTestCase
-)
+class QueueAuthSamplesAsync(object):
 
+    connection_string = os.getenv("CONNECTION_STRING")
 
-class TestQueueAuthSamplesAsync(AsyncQueueTestCase):
+    account_url = os.getenv("ACCOUNT_URL")
+    access_key = os.getenv("ACCESS_KEY")
 
-    active_directory_application_id = settings.ACTIVE_DIRECTORY_APPLICATION_ID
-    active_directory_application_secret = settings.ACTIVE_DIRECTORY_APPLICATION_SECRET
-    active_directory_tenant_id = settings.ACTIVE_DIRECTORY_TENANT_ID
+    active_directory_application_id = os.getenv("ACTIVE_DIRECTORY_APPLICATION_ID")
+    active_directory_application_secret = os.getenv("ACTIVE_DIRECTORY_APPLICATION_SECRET")
+    active_directory_tenant_id = os.getenv("ACTIVE_DIRECTORY_TENANT_ID")
 
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    @AsyncQueueTestCase.await_prepared_test
-    async def test_auth_connection_string(self, resource_group, location, storage_account, storage_account_key):
+    async def auth_connection_string(self):
         # Instantiate a QueueServiceClient using a connection string
         # [START async_auth_from_connection_string]
         from azure.storage.queue.aio import QueueServiceClient
-        queue_service = QueueServiceClient(self._account_url(storage_account.name), storage_account_key)
+        queue_service = QueueServiceClient.from_connection_string(conn_str=self.connection_string)
         # [END async_auth_from_connection_string]
 
         # Get information for the Queue Service
         properties = await queue_service.get_service_properties()
 
-        assert properties is not None
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    @AsyncQueueTestCase.await_prepared_test
-    async def test_auth_shared_key(self, resource_group, location, storage_account, storage_account_key):
-
+    async def auth_shared_key(self):
         # Instantiate a QueueServiceClient using a shared access key
         # [START async_create_queue_service_client]
         from azure.storage.queue.aio import QueueServiceClient
-        queue_service = QueueServiceClient(self._account_url(storage_account.name), storage_account_key)
+        queue_service = QueueServiceClient(account_url=self.account_url, credential=self.access_key)
         # [END async_create_queue_service_client]
+
         # Get information for the Queue Service
         properties = await queue_service.get_service_properties()
 
-        assert properties is not None
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    @AsyncQueueTestCase.await_prepared_test
-    async def test_auth_active_directory(self, resource_group, location, storage_account, storage_account_key):
-
+    async def auth_active_directory(self):
         # [START async_create_queue_service_client_token]
         # Get a token credential for authentication
         from azure.identity import ClientSecretCredential
-        token_credential = ClientSecretCredential(self.active_directory_tenant_id, self.active_directory_application_id,
-                                                  self.active_directory_application_secret)
+        token_credential = ClientSecretCredential(
+            self.active_directory_tenant_id,
+            self.active_directory_application_id,
+            self.active_directory_application_secret
+        )
 
         # Instantiate a QueueServiceClient using a token credential
         from azure.storage.queue.aio import QueueServiceClient
-        queue_service = QueueServiceClient(self._account_url(storage_account.name), storage_account_key)
+        queue_service = QueueServiceClient(account_url=self.account_url, credential=token_credential)
         # [END async_create_queue_service_client_token]
 
         # Get information for the Queue Service
         properties = await queue_service.get_service_properties()
 
-        assert properties is not None
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer(name_prefix='pyacrstorage')
-    @AsyncQueueTestCase.await_prepared_test
-    async def test_auth_shared_access_signature(self, resource_group, location, storage_account, storage_account_key):
- 
+    async def auth_shared_access_signature(self):
         # Instantiate a QueueServiceClient using a connection string
         from azure.storage.queue.aio import QueueServiceClient
-        queue_service = QueueServiceClient(self._account_url(storage_account.name), storage_account_key)
+        queue_service = QueueServiceClient.from_connection_string(conn_str=self.connection_string)
 
         # Create a SAS token to use for authentication of a client
         from azure.storage.queue import generate_account_sas
@@ -94,4 +75,7 @@ class TestQueueAuthSamplesAsync(AsyncQueueTestCase):
             expiry=datetime.utcnow() + timedelta(hours=1)
         )
 
-        assert sas_token is not None
+        token_auth_queue_service = QueueServiceClient(account_url=self.account_url, credential=sas_token)
+
+        # Get information for the Queue Service
+        properties = await token_auth_queue_service.get_service_properties()
