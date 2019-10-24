9354b58d8e89d426d9bd447b3487f059aa436a05
yijxie <yijun.xie@micrsofot.com>
2019-10-23 18:05:58 -0700

    Remove InMemoryPartitionManager

--- sdk/eventhub/azure-eventhubs/azure/eventhub/aio/__init__.py
+++ sdk/eventhub/azure-eventhubs/azure/eventhub/aio/__init__.py
@@ -12,7 +12,6 @@ __all__ = [
     "EventHubConsumerClient",
     "EventHubProducerClient",
     "PartitionManager",
-    "InMemoryPartitionManager",
     "FileBasedPartitionManager",
     "OwnershipLostError",
 ]
