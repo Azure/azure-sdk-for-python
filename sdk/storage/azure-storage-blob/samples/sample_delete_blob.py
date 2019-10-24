a7b8030427de46aecc28bbcc50fb2cfec62cbaaa
Catalina Peralta <caperal@microsoft.com>
2019-10-24 10:59:07 -0700

    Removed container name variable

--- sdk/storage/azure-storage-blob/samples/sample_delete_blob.py
+++ sdk/storage/azure-storage-blob/samples/sample_delete_blob.py
@@ -7,11 +7,10 @@ SOURCE_FOLDER = "./sample-blobs/"
 def batch_delete_blobs_sample(local_path):
 	# Set the connection string and container name values to initialize the Container Client
     connection_string = os.getenv('STORAGE_CONN_STR')
-    container_name = os.getenv('STORAGE_CONTAINER_NAME')
 
     blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_string)
 	# Create a ContainerClient to use the batch_delete function on a Blob Container
-    container_client = blob_service_client.get_container_client(container_name)
+    container_client = blob_service_client.get_container_client("mycontainername")
 
 	# Upload blobs
     for filename in os.listdir(local_path):
