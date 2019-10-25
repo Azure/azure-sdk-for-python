21d3822603015a5fe123c6c38bdb1d28e5cdbaa3
Charles Lowell <chlowe@microsoft.com>
2019-10-25 11:28:36 -0700

    revise docstring content

--- sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/_client.py
+++ sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/_client.py
@@ -47,10 +47,11 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def create_key(self, name, key_type, **kwargs):
         # type: (str, Union[str, azure.keyvault.keys.KeyType], **Any) -> KeyVaultKey
-        """Create a key. If ``name`` is already in use, create a new version of the key. Requires the keys/create
-        permission.
+        """Create a key or, if ``name`` is already in use, create a new version of the key.
 
-        :param str name: The name of the new key. Key Vault will generate the key's version.
+        Requires keys/create permission.
+
+        :param str name: The name of the new key.
         :param key_type: The type of key to create
         :type key_type: ~azure.keyvault.keys.KeyType or str
         :keyword int size: RSA key size in bits, for example 2048, 3072, or 4096. Applies only to RSA keys. To
@@ -99,10 +100,11 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def create_rsa_key(self, name, **kwargs):
         # type: (str, **Any) -> KeyVaultKey
-        """Create a new RSA key. If ``name`` is already in use, create a new version of the key. Requires the
-        keys/create permission.
+        """Create a new RSA key or, if ``name`` is already in use, create a new version of the key
 
-        :param str name: The name for the new key. Key Vault will generate the key's version.
+        Requires the keys/create permission.
+
+        :param str name: The name for the new key.
         :keyword int size: Key size in bits, for example 2048, 3072, or 4096.
         :keyword bool hardware_protected: Whether the key should be created in a hardware security module.
          Defaults to ``False``.
@@ -131,16 +133,17 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def create_ec_key(self, name, **kwargs):
         # type: (str, **Any) -> KeyVaultKey
-        """Create a new elliptic curve key. If ``name`` is already in use, create a new version of the key. Requires
-        the keys/create permission.
+        """Create a new elliptic curve key or, if ``name`` is already in use, create a new version of the key.
 
-        :param str name: The name for the new key. Key Vault will generate the key's version.
+        Requires the keys/create permission.
+
+        :param str name: The name for the new key.
         :keyword curve: Elliptic curve name. Defaults to the NIST P-256 elliptic curve.
         :paramtype curve: ~azure.keyvault.keys.KeyCurveName or str
         :keyword key_operations: Allowed key operations
+        :paramtype key_operations: list[~azure.keyvault.keys.KeyOperation or str]
         :keyword bool hardware_protected: Whether the key should be created in a hardware security module.
          Defaults to ``False``.
-        :paramtype key_operations: list[~azure.keyvault.keys.KeyOperation or str]
         :keyword bool enabled: Whether the key is enabled for use.
         :keyword tags: Application specific metadata in the form of key-value pairs.
         :paramtype tags: dict[str, str]
@@ -164,15 +167,17 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def begin_delete_key(self, name, **kwargs):
         # type: (str, **Any) -> DeletedKey
-        """Delete all versions of a key and its cryptographic material.
+        """Delete all versions of a key and its cryptographic material. Requires keys/delete permission.
 
-        Requires the keys/delete permission. The poller requires the keys/get permission to function properly.
+        When this method returns Key Vault has begun deleting the key. Deletion may take several seconds in a vault
+        with soft-delete enabled. This method therefore returns a poller enabling you to wait for deletion to complete.
 
         :param str name: The name of the key to delete.
-        :returns: A poller for the delete key operation. Calling `result` returns the
-         :class:`~azure.keyvault.keys.DeletedKey` without waiting for the operation to complete.
-         If you are planning to immediately purge the deleted key, call `wait` on the poller,
-         which blocks until deletion is complete.
+        :returns: A poller for the delete key operation. The poller's `result` method returns the
+         :class:`~azure.keyvault.keys.DeletedKey` without waiting for deletion to complete. If the vault has
+         soft-delete enabled and you want to permanently delete the key with :func:`purge_deleted_key`, call the
+         poller's `wait` method first. It will block until the deletion is complete. The `wait` method requires
+         keys/get permission.
         :rtype: ~azure.core.polling.LROPoller[~azure.keyvault.keys.DeletedKey]
         :raises:
             :class:`~azure.core.exceptions.ResourceNotFoundError` if the key doesn't exist,
@@ -205,7 +210,7 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def get_key(self, name, version=None, **kwargs):
         # type: (str, Optional[str], **Any) -> KeyVaultKey
-        """Get a key's attributes and, if it's an asymmetric key, its public material. Requires the keys/get permission.
+        """Get a key's attributes and, if it's an asymmetric key, its public material. Requires keys/get permission.
 
         :param str name: The name of the key to get.
         :param str version: (optional) A specific version of the key to get. If not specified, gets the latest version
@@ -231,8 +236,7 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def get_deleted_key(self, name, **kwargs):
         # type: (str, **Any) -> DeletedKey
-        """Get a deleted key. This is only possible in a vault with soft-delete enabled. Requires the keys/get
-        permission.
+        """Get a deleted key. Possible only in a vault with soft-delete enabled. Requires keys/get permission.
 
         :param str name: The name of the key
         :returns: The deleted key
@@ -255,8 +259,9 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def list_deleted_keys(self, **kwargs):
         # type: (**Any) -> ItemPaged[DeletedKey]
-        """List all deleted keys, including the public part of each. This is only possible in a vault with soft-delete
-        enabled. Requires the keys/list permission.
+        """List all deleted keys, including the public part of each. Possible only in a vault with soft-delete enabled.
+
+        Requires keys/list permission.
 
         :returns: An iterator of deleted keys
         :rtype: ~azure.core.paging.ItemPaged[~azure.keyvault.keys.DeletedKey]
@@ -279,7 +284,7 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def list_properties_of_keys(self, **kwargs):
         # type: (**Any) -> ItemPaged[KeyProperties]
-        """List identifiers, attributes, and tags of all keys in the vault. Requires the keys/list permission.
+        """List identifiers and properties of all keys in the vault. Requires keys/list permission.
 
         :returns: An iterator of keys without their cryptographic material or version information
         :rtype: ~azure.core.paging.ItemPaged[~azure.keyvault.keys.KeyProperties]
@@ -302,7 +307,7 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def list_properties_of_key_versions(self, name, **kwargs):
         # type: (str, **Any) -> ItemPaged[KeyProperties]
-        """List the identifiers, attributes, and tags of a key's versions. Requires the keys/list permission.
+        """List the identifiers and properties of a key's versions. Requires keys/list permission.
 
         :param str name: The name of the key
         :returns: An iterator of keys without their cryptographic material
@@ -327,10 +332,10 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def purge_deleted_key(self, name, **kwargs):
         # type: (str, **Any) -> None
-        """Permanently delete the specified key. This is only possible in vaults with soft-delete enabled. If a vault
-        does not have soft-delete enabled, :func:`begin_delete_key` is permanent, and this method will return an error.
+        """Permanently delete a key. Only possible in a vault with soft-delete enabled. Requires keys/purge permission.
 
-        Requires the keys/purge permission.
+        If the vault does not have soft-delete enabled, :func:`begin_delete_key` is permanent, and this method will
+        return an error.
 
         :param str name: The name of the key
         :returns: None
@@ -349,16 +354,19 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def begin_recover_deleted_key(self, name, **kwargs):
         # type: (str, **Any) -> KeyVaultKey
-        """Recover a deleted key to its latest version. This is only possible in vaults with soft-delete enabled.
-        If a vault does not have soft-delete enabled, :func:`begin_delete_key` is permanent, and this method will
-        return an error. Attempting to recover a non-deleted key will also return an error.
+        """Recover a deleted key to its latest version. Possible only in a vault with soft-delete enabled.
 
-        Requires the keys/recover permission. The poller requires the keys/get permission to function properly.
+        Requires keys/recover permission.
+
+        When this method returns Key Vault has begun recovering the key. Recovery may take several seconds. This
+        method therefore returns a poller enabling you to wait for recovery to complete. Waiting is only necessary when
+        you want to use the recovered key in another operation immediately.
 
         :param str name: The name of the deleted key
-        :returns: A poller for the recover key operation. Calling `result` on the poller returns the recovered
-         :class:`~azure.keyvault.keys.KeyVaultKey`. If you are planning to immediately use the recovered key,
-         call `wait` on the poller, which blocks until the key is ready to use.
+        :returns: A poller for the recovery operation. The poller's `result` method returns the recovered
+         :class:`~azure.keyvault.keys.KeyVaultKey` without waiting for recovery to complete. If you want to use the
+         recovered key immediately, call the poller's `wait` method, which blocks until the key is ready to use. The
+         `wait` method requires keys/get permission.
         :rtype: ~azure.core.polling.LROPoller[~azure.keyvault.keys.KeyVaultKey]
         :raises: :class:`~azure.core.exceptions.HttpResponseError`
 
@@ -387,8 +395,7 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def update_key_properties(self, name, version=None, **kwargs):
         # type: (str, Optional[str], **Any) -> KeyVaultKey
-        """Change attributes of a key. Cannot change a key's cryptographic material. Requires the keys/update
-        permission.
+        """Change a key's properties (not its cryptographic material). Requires keys/update permission.
 
         :param str name: The name of key to update
         :param str version: (optional) The version of the key to update. If unspecified, the latest version is updated.
@@ -434,11 +441,11 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def backup_key(self, name, **kwargs):
         # type: (str, **Any) -> bytes
-        """Back up a key in a protected form that can't be used outside Azure Key Vault. This is intended to allow
-        copying a key from one vault to another. Requires the key/backup permission.
+        """Back up a key in a protected form useable only by Azure Key Vault. Requires key/backup permission.
 
-        Backup / restore cannot be performed across geopolitical boundaries. For example, a backup from a vault in a
-        USA region cannot be restored to a vault in an EU region.
+        This is intended to allow copying a key from one vault to another. Both vaults must be owned by the same Azure
+        subscription. Also, backup / restore cannot be performed across geopolitical boundaries. For example, a backup
+        from a vault in a USA region cannot be restored to a vault in an EU region.
 
         :param str name: The name of the key
         :returns: The raw bytes of the key backup
@@ -461,13 +468,13 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def restore_key_backup(self, backup, **kwargs):
         # type: (bytes, **Any) -> KeyVaultKey
-        """Restore a key backup to the vault. This imports all versions of the key, with its name, attributes, and
-        access control policies. Requires the keys/restore permission.
+        """Restore a key backup to the vault. Requires keys/restore permission.
 
-        If the backed up key's name is already in use in the target vault, restoring it will fail. Also, the target
-        vault must be owned by the same Microsoft Azure subscription as the source vault.
+        This imports all versions of the key, with its name, attributes, and access control policies. If the key's name
+        is already in use, restoring it will fail. Also, the target vault must be owned by the same Microsoft Azure
+        subscription as the source vault.
 
-        :param bytes backup: The raw bytes of the key backup
+        :param bytes backup: The bytes of the key backup
         :returns: The restored key
         :rtype: ~azure.keyvault.keys.KeyVaultKey
         :raises:
@@ -488,8 +495,9 @@ class KeyClient(KeyVaultClientBase):
     @distributed_trace
     def import_key(self, name, key, **kwargs):
         # type: (str, JsonWebKey, **Any) -> KeyVaultKey
-        """Import an externally created key. If ``name`` is already in use, import the key as a new version. Requires
-        the keys/import permission.
+        """Import a key created externally. Requires keys/import permission.
+
+        If ``name`` is already in use, the key will be imported as a new version.
 
         :param str name: Name for the imported key
         :param key: The JSON web key to import
--- sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/aio/_client.py
+++ sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/aio/_client.py
@@ -45,10 +45,11 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def create_key(self, name: str, key_type: "Union[str, KeyType]", **kwargs: "Any") -> KeyVaultKey:
-        """Create a key. If ``name`` is already in use, create a new version of the key. Requires the keys/create
-        permission.
+        """Create a key or, if ``name`` is already in use, create a new version of the key.
 
-        :param str name: The name of the new key. Key Vault will generate the key's version.
+        Requires keys/create permission.
+
+        :param str name: The name of the new key.
         :param key_type: The type of key to create
         :type key_type: ~azure.keyvault.keys.KeyType or str
         :keyword int size: RSA key size in bits, for example 2048, 3072, or 4096. Applies only to RSA keys. To
@@ -97,10 +98,11 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def create_rsa_key(self, name: str, **kwargs: "Any") -> KeyVaultKey:
-        """Create a new RSA key. If ``name`` is already in use, create a new version of the key. Requires the
-        keys/create permission.
+        """Create a new RSA key or, if ``name`` is already in use, create a new version of the key
 
-        :param str name: The name for the new key
+        Requires the keys/create permission.
+
+        :param str name: The name for the new key.
         :keyword int size: Key size in bits, for example 2048, 3072, or 4096.
         :keyword bool hardware_protected: Whether the key should be created in a hardware security module.
          Defaults to ``False``.
@@ -128,16 +130,17 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def create_ec_key(self, name: str, **kwargs: "Any") -> KeyVaultKey:
-        """Create a new elliptic curve key. If ``name`` is already in use, create a new version of the key. Requires
-        the keys/create permission.
+        """Create a new elliptic curve key or, if ``name`` is already in use, create a new version of the key.
 
-        :param str name: The name for the new key. Key Vault will generate the key's version.
+        Requires the keys/create permission.
+
+        :param str name: The name for the new key.
         :keyword curve: Elliptic curve name. Defaults to the NIST P-256 elliptic curve.
         :paramtype curve: ~azure.keyvault.keys.KeyCurveName or str
         :keyword key_operations: Allowed key operations
+        :paramtype key_operations: list[~azure.keyvault.keys.KeyOperation or str]
         :keyword bool hardware_protected: Whether the key should be created in a hardware security module.
          Defaults to ``False``.
-        :paramtype key_operations: list[~azure.keyvault.keys.KeyOperation or str]
         :keyword bool enabled: Whether the key is enabled for use.
         :keyword tags: Application specific metadata in the form of key-value pairs.
         :paramtype tags: dict[str, str]
@@ -160,14 +163,12 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def delete_key(self, name: str, **kwargs: "Any") -> DeletedKey:
-        """Delete all versions of a key and its cryptographic material.
+        """Delete all versions of a key and its cryptographic material. Requires keys/delete permission.
 
-        Requires the keys/delete permission. The poller requires the keys/get permission to function properly.
+        If the vault has soft-delete enabled, deletion may take several seconds to complete.
 
-        :param str name: The name of the key to delete.
-        :returns: A coroutine for the deletion of the key. Since deleting a key is not instant,
-         we poll on the deletion of the key. Awaiting this method returns the
-         :class:`~azure.keyvault.keys.DeletedKey`
+        :param str name: The name of the key to delete
+        :returns: The deleted key
         :rtype: ~azure.keyvault.keys.DeletedKey
         :raises:
             :class:`~azure.core.exceptions.ResourceNotFoundError` if the key doesn't exist,
@@ -195,7 +196,7 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def get_key(self, name: str, version: "Optional[str]" = None, **kwargs: "Any") -> KeyVaultKey:
-        """Get a key's attributes and, if it's an asymmetric key, its public material. Requires the keys/get permission.
+        """Get a key's attributes and, if it's an asymmetric key, its public material. Requires keys/get permission.
 
         :param str name: The name of the key to get.
         :param str version: (optional) A specific version of the key to get. If not specified, gets the latest version
@@ -221,8 +222,7 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def get_deleted_key(self, name: str, **kwargs: "Any") -> DeletedKey:
-        """Get a deleted key. This is only possible in a vault with soft-delete enabled. Requires the keys/get
-        permission.
+        """Get a deleted key. Possible only in a vault with soft-delete enabled. Requires keys/get permission.
 
         :param str name: The name of the key
         :returns: The deleted key
@@ -244,8 +244,9 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace
     def list_deleted_keys(self, **kwargs: "Any") -> "AsyncIterable[DeletedKey]":
-        """List all deleted keys, including the public part of each. This is only possible in a vault with soft-delete
-        enabled. Requires the keys/list permission.
+        """List all deleted keys, including the public part of each. Possible only in a vault with soft-delete enabled.
+
+        Requires keys/list permission.
 
         :returns: An iterator of deleted keys
         :rtype: ~azure.core.async_paging.AsyncItemPaged[~azure.keyvault.keys.DeletedKey]
@@ -267,7 +268,7 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace
     def list_properties_of_keys(self, **kwargs: "Any") -> "AsyncIterable[KeyProperties]":
-        """List identifiers, attributes, and tags of all keys in the vault. Requires the keys/list permission.
+        """List identifiers and properties of all keys in the vault. Requires keys/list permission.
 
         :returns: An iterator of keys without their cryptographic material or version information
         :rtype: ~azure.core.async_paging.AsyncItemPaged[~azure.keyvault.keys.KeyProperties]
@@ -289,7 +290,7 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace
     def list_properties_of_key_versions(self, name: str, **kwargs: "Any") -> "AsyncIterable[KeyProperties]":
-        """List the identifiers, attributes, and tags of a key's versions. Requires the keys/list permission.
+        """List the identifiers and properties of a key's versions. Requires keys/list permission.
 
         :param str name: The name of the key
         :returns: An iterator of keys without their cryptographic material
@@ -313,10 +314,10 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def purge_deleted_key(self, name: str, **kwargs: "Any") -> None:
-        """Permanently delete the specified key. This is only possible in vaults with soft-delete enabled. If a vault
-        does not have soft-delete enabled, :func:`delete_key` is permanent, and this method will return an error.
+        """Permanently delete a key. Only possible in a vault with soft-delete enabled. Requires keys/purge permission.
 
-        Requires the keys/purge permission.
+        If the vault does not have soft-delete enabled, :func:`begin_delete_key` is permanent, and this method will
+        return an error.
 
         :param str name: The name of the key
         :returns: None
@@ -334,16 +335,14 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def recover_deleted_key(self, name: str, **kwargs: "Any") -> KeyVaultKey:
-        """Recover a deleted key to its latest version. This is only possible in vaults with soft-delete enabled. If a
-        vault does not have soft-delete enabled, :func:`delete_key` is permanent, and this method will return an error.
-        Attempting to recover a non-deleted key will also return an error.
+        """Recover a deleted key to its latest version. Possible only in a vault with soft-delete enabled.
 
-        Requires the keys/recover permission. The poller requires the keys/get permission to function properly.
+        Requires keys/recover permission. If the vault does not have soft-delete enabled, :func:`delete_key` is
+        permanent, and this method will raise an error. Attempting to recover a non-deleted key will also raise an
+        error.
 
         :param str name: The name of the deleted key
-        :returns: A coroutine for the recovery of the key. Since recovering a key is not instant, we poll on the
-         recovery of the key. Awaiting this method returns the recovered
-         :class:`~azure.keyvault.keys.KeyVaultKey`
+        :returns: The recovered key
         :rtype: ~azure.keyvault.keys.KeyVaultKey
         :raises: :class:`~azure.core.exceptions.HttpResponseError`
 
@@ -368,8 +367,7 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def update_key_properties(self, name: str, version: "Optional[str]" = None, **kwargs: "Any") -> KeyVaultKey:
-        """Change attributes of a key. Cannot change a key's cryptographic material. Requires the keys/update
-        permission.
+        """Change a key's properties (not its cryptographic material). Requires keys/update permission.
 
         :param str name: The name of key to update
         :param str version: (optional) The version of the key to update. If unspecified, the latest version is updated.
@@ -414,11 +412,11 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def backup_key(self, name: str, **kwargs: "Any") -> bytes:
-        """Back up a key in a protected form that can't be used outside Azure Key Vault. This is intended to allow
-        copying a key from one vault to another. Requires the key/backup permission.
+        """Back up a key in a protected form useable only by Azure Key Vault. Requires key/backup permission.
 
-        Backup / restore cannot be performed across geopolitical boundaries. For example, a backup from a vault in a
-        USA region cannot be restored to a vault in an EU region.
+        This is intended to allow copying a key from one vault to another. Both vaults must be owned by the same Azure
+        subscription. Also, backup / restore cannot be performed across geopolitical boundaries. For example, a backup
+        from a vault in a USA region cannot be restored to a vault in an EU region.
 
         :param str name: The name of the key
         :returns: The raw bytes of the key backup
@@ -440,13 +438,13 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def restore_key_backup(self, backup: bytes, **kwargs: "Any") -> KeyVaultKey:
-        """Restore a key backup to the vault. This imports all versions of the key, with its name, attributes, and
-        access control policies. Requires the keys/restore permission.
+        """Restore a key backup to the vault. Requires keys/restore permission.
 
-        If the backed up key's name is already in use in the target vault, restoring it will fail. Also, the target
-        vault must be owned by the same Microsoft Azure subscription as the source vault.
+        This imports all versions of the key, with its name, attributes, and access control policies. If the key's name
+        is already in use, restoring it will fail. Also, the target vault must be owned by the same Microsoft Azure
+        subscription as the source vault.
 
-        :param bytes backup: The raw bytes of the key backup
+        :param bytes backup: The bytes of the key backup
         :returns: The restored key
         :rtype: ~azure.keyvault.keys.KeyVaultKey
         :raises:
@@ -466,8 +464,9 @@ class KeyClient(AsyncKeyVaultClientBase):
 
     @distributed_trace_async
     async def import_key(self, name: str, key: JsonWebKey, **kwargs: "Any") -> KeyVaultKey:
-        """Import an externally created key. If ``name`` is already in use, import the key as a new version. Requires
-        the keys/import permission.
+        """Import a key created externally. Requires keys/import permission.
+
+        If ``name`` is already in use, the key will be imported as a new version.
 
         :param str name: Name for the imported key
         :param key: The JSON web key to import
