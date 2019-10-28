2ac652ce30097a237fea4c90d031db78da95899e
scbedd <45376673+scbedd@users.noreply.github.com>
2019-10-28 12:57:51 -0700

    allowing namespace != package name exceptions when generating tox

--- eng/tox/prep_sphinx_env.py
+++ eng/tox/prep_sphinx_env.py
@@ -44,6 +44,11 @@ Indices and tables
 root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", ".."))
 sphinx_conf = os.path.join(root_dir, "doc", "sphinx", "individual_build_conf.py")
 
+
+UNFRIENDLY_PACKAGE_TO_NAMESPACE = {
+    'azure-storage-file-share': 'azure.storage.fileshare'
+}
+
 def should_build_docs(package_name):
     return not ("nspkg" in package_name or "azure-mgmt" == package_name or "azure" == package_name)
 
@@ -154,6 +159,9 @@ if __name__ == "__main__":
     package_name, package_version = get_package_details(
         os.path.join(package_path, "setup.py")
     )
+
+    if package_name in UNFRIENDLY_PACKAGE_TO_NAMESPACE.keys():
+        package_name = UNFRIENDLY_PACKAGE_TO_NAMESPACE[package_name]
 
     if should_build_docs(package_name):
         source_location = move_and_rename(unzip_sdist_to_directory(args.dist_dir))
