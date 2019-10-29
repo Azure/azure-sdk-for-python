a6369928f659e3b032a6ba3d62993b0dac316d6b
Bryan Van de Ven <brvandev@microsoft.com>
2019-10-29 14:27:50 -0700

    make database_management.py more pythonic

--- sdk/cosmos/azure-cosmos/samples/database_management.py
+++ sdk/cosmos/azure-cosmos/samples/database_management.py
@@ -29,112 +29,93 @@ HOST = config.settings['host']
 MASTER_KEY = config.settings['master_key']
 DATABASE_ID = config.settings['database_id']
 
-class IDisposable:
-    """ A context manager to automatically close an object with a close method
-    in a with statement. """
+def find_database(client, id):
+    print('1. Query for Database')
 
-    def __init__(self, obj):
-        self.obj = obj
+    databases = list(client.query_databases({
+        "query": "SELECT * FROM r WHERE r.id=@id",
+        "parameters": [
+            { "name":"@id", "value": id }
+        ]
+    }))
 
-    def __enter__(self):
-        return self.obj # bound to target
-
-    def __exit__(self, exception_type, exception_val, trace):
-        # extra cleanup in here
-        self = None
-
-class DatabaseManagement:
-    @staticmethod
-    def find_database(client, id):
-        print('1. Query for Database')
-
-        databases = list(client.query_databases({
-            "query": "SELECT * FROM r WHERE r.id=@id",
-            "parameters": [
-                { "name":"@id", "value": id }
-            ]
-        }))
-
-        if len(databases) > 0:
-            print('Database with id \'{0}\' was found'.format(id))
-        else:
-            print('No database with id \'{0}\' was found'. format(id))
-        
-    @staticmethod
-    def create_database(client, id):
-        print("\n2. Create Database")
-        
-        try:
-            client.create_database(id=id)
-            print('Database with id \'{0}\' created'.format(id))
-
-        except errors.CosmosResourceExistsError:
-            print('A database with id \'{0}\' already exists'.format(id))             
+    if len(databases) > 0:
+        print('Database with id \'{0}\' was found'.format(id))
+    else:
+        print('No database with id \'{0}\' was found'. format(id))
     
-    @staticmethod
-    def read_database(client, id):
-        print("\n3. Get a Database by id")
 
-        try:
-            database = client.get_database_client(id)
-            print('Database with id \'{0}\' was found, it\'s link is {1}'.format(id, database.database_link))
+def create_database(client, id):
+    print("\n2. Create Database")
+    
+    try:
+        client.create_database(id=id)
+        print('Database with id \'{0}\' created'.format(id))
 
-        except errors.CosmosResourceNotFoundError:
-            print('A database with id \'{0}\' does not exist'.format(id))   
+    except errors.CosmosResourceExistsError:
+        print('A database with id \'{0}\' already exists'.format(id))             
 
-    @staticmethod
-    def list_databases(client):
-        print("\n4. List all Databases on an account")
-        
-        print('Databases:')
-        
-        databases = list(client.list_databases())
-        
-        if not databases:
-            return
 
-        for database in databases:
-            print(database['id'])          
+def read_database(client, id):
+    print("\n3. Get a Database by id")
 
-    @staticmethod
-    def delete_database(client, id):
-        print("\n5. Delete Database")
-        
-        try:
-           client.delete_database(id)
+    try:
+        database = client.get_database_client(id)
+        print('Database with id \'{0}\' was found, it\'s link is {1}'.format(id, database.database_link))
 
-           print('Database with id \'{0}\' was deleted'.format(id))
+    except errors.CosmosResourceNotFoundError:
+        print('A database with id \'{0}\' does not exist'.format(id))   
 
-        except errors.CosmosResourceNotFoundError:
-            print('A database with id \'{0}\' does not exist'.format(id))
+
+def list_databases(client):
+    print("\n4. List all Databases on an account")
+    
+    print('Databases:')
+    
+    databases = list(client.list_databases())
+    
+    if not databases:
+        return
+
+    for database in databases:
+        print(database['id'])          
+
+
+def delete_database(client, id):
+    print("\n5. Delete Database")
+    
+    try:
+        client.delete_database(id)
+
+        print('Database with id \'{0}\' was deleted'.format(id))
+
+    except errors.CosmosResourceNotFoundError:
+        print('A database with id \'{0}\' does not exist'.format(id))
+
 
 def run_sample():     
-    with IDisposable(cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY} )) as client:
-        try:
-            # query for a database
-            DatabaseManagement.find_database(client, DATABASE_ID)
-            
-            # create a database
-            DatabaseManagement.create_database(client, DATABASE_ID)
-                        
-            # get a database using its id
-            DatabaseManagement.read_database(client, DATABASE_ID)
-
-            # list all databases on an account
-            DatabaseManagement.list_databases(client)
-
-            # delete database by id
-            DatabaseManagement.delete_database(client, DATABASE_ID)
-
-        except errors.CosmosHttpResponseError as e:
-            print('\nrun_sample has caught an error. {0}'.format(e.message))
+    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY} )
+    try:
+        # query for a database
+        find_database(client, DATABASE_ID)
         
-        finally:
-            print("\nrun_sample done")
+        # create a database
+        create_database(client, DATABASE_ID)
+                    
+        # get a database using its id
+        read_database(client, DATABASE_ID)
+
+        # list all databases on an account
+        list_databases(client)
+
+        # delete database by id
+        delete_database(client, DATABASE_ID)
+
+    except errors.CosmosHttpResponseError as e:
+        print('\nrun_sample has caught an error. {0}'.format(e.message))
+    
+    finally:
+        print("\nrun_sample done")
 
 if __name__ == '__main__':
-    try:
-        run_sample()
-
-    except Exception as e:
-            print("Top level Error: args:{0}, message:{1}".format(e.args,e.message))
+    run_sample()
