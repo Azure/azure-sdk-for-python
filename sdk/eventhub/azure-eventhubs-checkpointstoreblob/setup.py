a4eecccdad5d8718193c984db4023975a1a4eb15
yijxie <yijun.xie@micrsofot.com>
2019-10-29 10:39:43 -0700

    checkpointstoreblob supports 2.7

--- sdk/eventhub/azure-eventhubs-checkpointstoreblob/setup.py
+++ sdk/eventhub/azure-eventhubs-checkpointstoreblob/setup.py
@@ -55,6 +55,8 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -64,7 +66,6 @@ setup(
     ],
     zip_safe=False,
     packages=find_packages(exclude=exclude_packages),
-    python_requires=">=3.5.3",
     install_requires=[
         'azure-storage-blob<=12.1,>=12.0.0b4',
         'azure-eventhub<6.0.0,>=5.0.0b4',
