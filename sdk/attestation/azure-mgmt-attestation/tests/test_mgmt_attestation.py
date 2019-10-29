eed2a32d4487979b7b252ec66eae342e9c8b43e4
Zim Kalinowski <zikalino@microsoft.com>
2019-10-29 16:48:58 +0800

    another attempt

--- sdk/attestation/azure-mgmt-attestation/tests/test_mgmt_attestation.py
+++ sdk/attestation/azure-mgmt-attestation/tests/test_mgmt_attestation.py
@@ -29,12 +29,12 @@ class MgmtAttestationTest(AzureMgmtTestCase):
 
         certs = { 'keys': 
        [
-         {"kty":"EC",
-          "crv":"P-256",
-          "x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
-          "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
-          "use":"enc",
-          "kid":"1"},
+         # {"kty":"EC",
+         #  "crv":"P-256",
+         # "x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
+         # "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+         # "use":"enc",
+         # "kid":"1"},
 
          {"kty":"RSA",
           "n": "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
