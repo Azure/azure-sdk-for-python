c2f6b2ffa34196f244f677f643e85fb0726abdb9
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-11-01 12:03:12 -0700

    azure-keyvault meta-package (#8024)
    * azure-keyvault meta-package
    
    * Mismatch quote
    
    * Fix MD
    
    * COC link
    
    * Missing word
    
    * Feedback from @chlowell
    
    * Update incorrect setup.py
    
    * Add missing MANIFEST.in
    
    * Dep analysis fix
    
    * Update README.md
    
    * Skip CI for azure-keyvault
    
    * Move to stable and remove certificates
    
    * Stable classifier
    
    * Fix req file
    
    * 4.0.0

---
+++ sdk/keyvault/azure-keyvault/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+#--------------------------------------------------------------------------
+
+from setuptools import setup
+
+with open("README.md", encoding="utf-8") as f:
+    README = f.read()
+with open("HISTORY.md", encoding="utf-8") as f:
+    HISTORY = f.read()
+
+setup(
+    name='azure-keyvault',
+    version='4.0.0',
+    description='Microsoft Azure Key Vault Client Libraries for Python',
+    long_description=README + "\n\n" + HISTORY,
+    long_description_content_type="text/markdown",
+    license='MIT License',
+    author='Microsoft Corporation',
+    author_email="azurekeyvault@microsoft.com",
+    url="https://github.com/Azure/azure-sdk-for-python/tree/master/sdk/keyvault/azure-keyvault",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+    ],
+    zip_safe=False,
+    install_requires=[
+        'azure-keyvault-secrets~=4.0',
+        'azure-keyvault-keys~=4.0',
+    ],
+)
