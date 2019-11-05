cf28c8566ec2e2870476601e287b648652e5c815
Yunhao Ling <adam_ling@outlook.com>
2019-11-04 17:20:27 -0800

    remove timeout and retry in sample

--- sdk/eventhub/azure-eventhubs-checkpointstoreblob-aio/samples/event_processor_blob_storage_example.py
+++ sdk/eventhub/azure-eventhubs-checkpointstoreblob-aio/samples/event_processor_blob_storage_example.py
@@ -5,8 +5,6 @@ from azure.eventhub.aio import EventHubConsumerClient
 from azure.eventhub.extensions.checkpointstoreblobaio import BlobPartitionManager
 from azure.storage.blob.aio import ContainerClient
 
-RECEIVE_TIMEOUT = 5  # timeout in seconds for a receiving operation. 0 or None means no timeout
-RETRY_TOTAL = 3  # max number of retries for receive operations within the receive timeout. Actual number of retries clould be less if RECEIVE_TIMEOUT is too small
 CONNECTION_STR = os.environ["EVENT_HUB_CONN_STR"]
 STORAGE_CONNECTION_STR = os.environ["AZURE_STORAGE_CONN_STR"]
 
@@ -29,7 +27,7 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     container_client = ContainerClient.from_connection_string(STORAGE_CONNECTION_STR, "eventprocessor")
     partition_manager = BlobPartitionManager(container_client=container_client)
-    client = EventHubConsumerClient.from_connection_string(CONNECTION_STR, partition_manager=partition_manager, receive_timeout=RECEIVE_TIMEOUT, retry_total=RETRY_TOTAL)
+    client = EventHubConsumerClient.from_connection_string(CONNECTION_STR, partition_manager=partition_manager)
     try:
         loop.run_until_complete(client.receive(process_events, "$default"))
     except KeyboardInterrupt:
