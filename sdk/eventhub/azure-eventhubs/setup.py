f10d58670203808f37f5f0cc13eb9e4f59da71a3
Kieran Brantner-Magee <kibrantn@microsoft.com>
2019-11-05 10:44:21 -0800

    Revert "Packaging update of azure-eventhubs"
    This reverts commit a725b0343362a7f4b7cd2842881c38036d2ff892.

--- sdk/eventhub/azure-eventhubs/setup.py
+++ sdk/eventhub/azure-eventhubs/setup.py
@@ -11,41 +11,27 @@ import os.path
 from io import open
 from setuptools import find_packages, setup
 
+
 # Change the PACKAGE_NAME only to change folder and different name
-PACKAGE_NAME = "azure-eventhubs"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_NAME = "azure-eventhub"
+PACKAGE_PPRINT_NAME = "Event Hubs"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
 # a-b-c => a.b.c
 namespace_name = PACKAGE_NAME.replace('-', '.')
 
-# azure v0.x is not compatible with this package
-# azure v0.x used to have a __version__ attribute (newer versions don't)
-try:
-    import azure
-    try:
-        ver = azure.__version__
-        raise Exception(
-            'This package is incompatible with azure=={}. '.format(ver) +
-            'Uninstall it with "pip uninstall azure".'
-        )
-    except AttributeError:
-        pass
-except ImportError:
-    pass
-
 # Version extraction inspired from 'requests'
-with open(os.path.join(package_folder_path, 'version.py'), 'r') as fd:
-    version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]',
+with open(os.path.join(package_folder_path, '__init__.py'), 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         fd.read(), re.MULTILINE).group(1)
 
 if not version:
     raise RuntimeError('Cannot find version information')
 
-with open('README.rst', encoding='utf-8') as f:
+with open('README.rst') as f:
     readme = f.read()
-with open('HISTORY.rst', encoding='utf-8') as f:
+with open('HISTORY.rst') as f:
     history = f.read()
 
 setup(
@@ -53,17 +39,17 @@ setup(
     version=version,
     description='Microsoft Azure {} Client Library for Python'.format(PACKAGE_PPRINT_NAME),
     long_description=readme + '\n\n' + history,
-    long_description_content_type='text/x-rst',
     license='MIT License',
     author='Microsoft Corporation',
     author_email='azpysdkhelp@microsoft.com',
     url='https://github.com/Azure/azure-sdk-for-python',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -71,14 +57,15 @@ setup(
     ],
     zip_safe=False,
     packages=find_packages(exclude=[
-        'tests',
-        # Exclude packages that will be covered by PEP420 or nspkg
-        'azure',
-    ]),
+        "azure",
+        "examples",
+        "tests",
+        "tests.asynctests"]),
     install_requires=[
-        'msrest>=0.5.0',
+        'uamqp~=1.1.0',
         'msrestazure>=0.4.32,<2.0.0',
         'azure-common~=1.1',
+        'azure-storage-blob~=1.3'
     ],
     extras_require={
         ":python_version<'3.0'": ['azure-nspkg'],
