98aaaad90b1ff1b3029636c978f4f8458e4f8909
Charles Lowell <chlowe@microsoft.com>
2019-11-06 09:31:58 -0800

    add --manual option to pytest

--- sdk/identity/azure-identity/conftest.py
+++ sdk/identity/azure-identity/conftest.py
@@ -12,26 +12,30 @@ from azure.identity._constants import AZURE_CLI_CLIENT_ID, EnvironmentVariables
 if sys.version_info < (3, 5):
     collect_ignore_glob = ["*_async.py"]
 
-run_manual_tests = False
-stdout_captured = True
+
+def pytest_addoption(parser):
+    parser.addoption("--manual", action="store_true", default=False, help="run manual tests")
 
 
 def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "manual: the tested credential requires manual interaction, e.g. InteractiveBrowserCredential"
-    )
-    config.addinivalue_line("markers", "prints: the tested credential prints to stdout, e.g. DeviceCodeCredential")
-    global run_manual_tests, stdout_captured
-    run_manual_tests = config.getoption("markexpr") == "manual"
+    config.addinivalue_line("markers", "manual: mark test as requiring manual interaction")
+    config.addinivalue_line("markers", "prints: mark test as printing important information to stdout")
+
+
+def pytest_collection_modifyitems(config, items):
     stdout_captured = config.getoption("capture") != "no"
+    run_manual_tests = config.getoption("--manual")
+    if not stdout_captured and run_manual_tests:
+        return
 
-
-def pytest_runtest_setup(item):
-    # ensure manual tests only run when manual marker is selected with 'pytest -m manual'
-    if item.get_closest_marker("manual") and not run_manual_tests:
-        pytest.skip("To run manual tests, select 'manual' marker with 'pytest -m manual'")
-    elif item.get_closest_marker("prints") and stdout_captured:
-        pytest.skip("This test prints to stdout. Run pytest with '-s' to ensure the output is visible.")
+    # skip manual tests or tests which print to stdout, as appropriate
+    skip_manual = pytest.mark.skip(reason="run pytest with '--manual' to run manual tests")
+    skip_prints = pytest.mark.skip(reason="this test prints to stdout, run pytest with '-s' to make output visible")
+    for test in items:
+        if not run_manual_tests and "manual" in test.keywords:
+            test.add_marker(skip_manual)
+        elif stdout_captured and "prints" in test.keywords:
+            test.add_marker(skip_prints)
 
 
 @pytest.fixture()
