e42c85c6e553b44ea4245e7d7f6127f6be956071
scbedd <45376673+scbedd@users.noreply.github.com>
2019-11-06 14:42:02 -0800

    appropriate version sorting in get_options.js. updating service mapping to include new service 'Storage Sync' updates to doc index yml

--- doc/sphinx/_static/js/get_options.js
+++ doc/sphinx/_static/js/get_options.js
@@ -1,4 +1,72 @@
 WINDOW_CONTENTS = window.location.href.split('/')
+SPLIT_REGEX = /([a-zA-Z]+)|([0-9]+)/g;
+REPLACE_REGEX = /[^a-zA-Z0-9]*/g
+
+// this function compares the individual version specifiers of two different versions.
+// If were comparing versions 1.0.0rc1 vs 1.1.2. This function compares
+// 1 compared to 1
+// 0 compared to 1
+// 0rc1 vs 2
+// the key here is that it removes all non alpha-numeric numbers, then splits by character TRANSITION
+// Then it compares 1-1 for THOSE.
+function comparePreviewSpecifer(v1, v2){
+    var v1parts = v1.replace(REPLACE_REGEX, '').split(SPLIT_REGEX).filter(a => a)
+    var v2parts = v2.replace(REPLACE_REGEX, '').split(SPLIT_REGEX).filter(a => a)
+
+    if (v2parts.length === 0) {
+        return 1;
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v1parts[i] === v2parts[i]) {
+            continue;
+        }
+
+        if (v1parts[i] > v2parts[i] && v2parts[i] !== undefined) {
+            return 1;
+        }
+
+        if (v1parts[i] < v2parts[i] && v2parts[i] !== undefined){
+            return -1;
+        }
+    }
+
+    // we have broken out due to v2parts having additional preview parts
+    if (v2parts.length > v1parts.length) {
+      return 1
+    }
+
+    if (v1parts.length > v2parts.length) {
+      return -1
+    }
+
+    return 0;
+}
+
+function compareVersionNumbers(v1, v2){
+    var v1parts = v1.toLowerCase().split('.');
+    var v2parts = v2.toLowerCase().split('.');
+
+    // classic versions of this algorithm error if we all the values aren't entirely valid versions
+    // given that we will have preview versions here, compare lexically when we hit that part.
+    if (v2parts.length === 0) {
+        return 1;
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v1parts[i] === v2parts[i]) {
+            continue;
+        }
+
+        return comparePreviewSpecifer(v1parts[i], v2parts[i])
+    }
+
+    return 0;
+}
 
 function currentVersion(){
     if (WINDOW_CONTENTS.includes('$web') && WINDOW_CONTENTS.length > 5)
@@ -43,23 +111,29 @@ function hideSelectors(selectors){
   })
 }
 
+function processResponse(responseText){
+  parser = new DOMParser();
+  xmlDoc = parser.parseFromString(responseText,"text/xml");
+  
+  nameElements = Array.from(xmlDoc.getElementsByTagName('Name'))
+  options = []
+
+  for (var i in nameElements){
+    options.push(nameElements[i].textContent.split('/')[3])
+  }
+
+  var result = options.sort(compareVersionNumbers).reverse()
+
+  return result
+}
+
 function populateOptions(optionSelector, otherSelectors){
   if(currentPackage()){
     var versionRequestUrl = "https://azuresdkdocs.blob.core.windows.net/$web?restype=container&comp=list&prefix=" + SELECTED_LANGUAGE + "/" + currentPackage() + "/versions/"
     
     httpGetAsync(versionRequestUrl, function(responseText){
       if(responseText){
-        data_stored = responseText
-
-        parser = new DOMParser();
-        xmlDoc = parser.parseFromString(responseText,"text/xml");
-        
-        nameElements = Array.from(xmlDoc.getElementsByTagName('Name'))
-        options = []
-
-        for (var i in nameElements){
-          options.push(nameElements[i].textContent.split('/')[3])
-        }
+        options = processResponse(responseText)
 
         populateVersionDropDown(optionSelector, options)
         showSelectors(otherSelectors)
@@ -97,15 +171,7 @@ function populateIndexList(selector, packageName)
 
   httpGetAsync(url, function (responseText){
     if(responseText){
-      parser = new DOMParser();
-      xmlDoc = parser.parseFromString(responseText,"text/xml");
-
-      nameElements = Array.from(xmlDoc.getElementsByTagName('Name'))
-      options = []
-
-      for (var i in nameElements){
-        options.push(nameElements[i].textContent.split('/')[3])
-      }
+      options = processResponse(responseText)
 
       for (var i in options){
         $(selector).append('<li><a target="new" href="' + getPackageUrl(SELECTED_LANGUAGE, packageName, options[i]) + '">' + options[i] + '</a></li>')
