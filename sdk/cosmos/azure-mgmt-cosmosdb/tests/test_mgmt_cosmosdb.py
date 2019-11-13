b4777f1da56379b1e8094a775c7c5a468944db0a
Zim Kalinowski <zikalino@microsoft.com>
2019-11-14 00:59:16 +0800

    udpated test names

--- sdk/cosmos/azure-mgmt-cosmosdb/tests/test_mgmt_cosmosdb.py
+++ sdk/cosmos/azure-mgmt-cosmosdb/tests/test_mgmt_cosmosdb.py
@@ -22,7 +22,7 @@ class MgmtCosmosDBTest(AzureMgmtTestCase):
 
     @ResourceGroupPreparer()
     def test_accounts_create(self, resource_group, location):
-        account_name = self.get_resource_name('pycosmosdbtst')
+        account_name = self.get_resource_name('pycosmosdbx1')
 
         self.assertFalse(self.client.database_accounts.check_name_exists(account_name))
 
@@ -43,7 +43,7 @@ class MgmtCosmosDBTest(AzureMgmtTestCase):
 
     @ResourceGroupPreparer()
     def test_accounts_features(self, resource_group, location):
-        account_name = self.get_resource_name('pycosmosdbtest')
+        account_name = self.get_resource_name('pycosmosdbx2')
 
         if not self.is_playback():
             async_cosmosdb_create = self.client.database_accounts.create_or_update(
@@ -109,7 +109,7 @@ class MgmtCosmosDBTest(AzureMgmtTestCase):
 
     @ResourceGroupPreparer()
     def test_accounts_delete(self, resource_group, location):
-        account_name = self.get_resource_name('pydocumentdbtst')
+        account_name = self.get_resource_name('pydocumentdbx3')
 
         if not self.is_playback():
             async_cosmosdb_create = self.client.database_accounts.create_or_update(
