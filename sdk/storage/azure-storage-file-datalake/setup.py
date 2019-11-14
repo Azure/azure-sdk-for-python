03fdad908b2472fd77bc05ee1c5a74977ea92ae4
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2019-11-14 11:47:50 -0800

    Version dependency with azure-storage-blob to 12.0

--- sdk/storage/azure-storage-file-datalake/setup.py
+++ sdk/storage/azure-storage-file-datalake/setup.py
@@ -93,7 +93,7 @@ setup(
     install_requires=[
         "azure-core<2.0.0,>=1.0.0",
         "msrest>=0.6.10",
-        "azure-storage-blob<13.0.0,>=12.0.0"
+        "azure-storage-blob~=12.0"
     ],
     extras_require={
         ":python_version<'3.0'": ['futures', 'azure-storage-nspkg<4.0.0,>=3.0.0'],
