4b91d822e8f5fbd61f07d120ad2abc6a1def1f64
Isabella Cai <iscai@microsoft.com>
2019-11-14 15:52:13 -0800

    fixed linting issue

--- sdk/keyvault/azure-keyvault-certificates/azure/keyvault/certificates/__init__.py
+++ sdk/keyvault/azure-keyvault-certificates/azure/keyvault/certificates/__init__.py
@@ -3,8 +3,12 @@
 # Licensed under the MIT License.
 # ------------------------------------
 from .client import CertificateClient
-from .enums import CertificatePolicyAction, KeyCurveName, KeyType, SecretContentType, KeyUsageType, WellKnownIssuerNames
-from .models import AdministratorContact, CertificatePolicy, CertificateContact, LifetimeAction, CertificateIssuer, IssuerProperties
+from .enums import(
+    CertificatePolicyAction, KeyCurveName, KeyType, SecretContentType, KeyUsageType, WellKnownIssuerNames
+)
+from .models import(
+    AdministratorContact, CertificatePolicy, CertificateContact, LifetimeAction, CertificateIssuer, IssuerProperties
+)
 
 __all__ = [
     "CertificatePolicyAction",
