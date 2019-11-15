f9c839f352eeedbfc4b03e17e9b7fd9e603b3a7c
Charles Lowell <chlowe@microsoft.com>
2019-11-15 15:36:16 -0800

    more tests

--- sdk/identity/azure-identity/tests/test_shared_cache_credential.py
+++ sdk/identity/azure-identity/tests/test_shared_cache_credential.py
@@ -14,9 +14,9 @@ from msal import TokenCache
 import pytest
 
 try:
-    from unittest.mock import Mock, patch
+    from unittest.mock import Mock
 except ImportError:  # python < 3.3
-    from mock import Mock, patch  # type: ignore
+    from mock import Mock  # type: ignore
 
 from helpers import build_aad_response, build_id_token, mock_response, Request, validating_transport
 
@@ -24,9 +24,15 @@ from helpers import build_aad_response, build_id_token, mock_response, Request, 
 def test_empty_cache():
     with pytest.raises(ClientAuthenticationError, match=NO_ACCOUNTS):
         SharedTokenCacheCredential(_cache=TokenCache()).get_token("scope")
+    with pytest.raises(ClientAuthenticationError, match=NO_ACCOUNTS):
+        SharedTokenCacheCredential(_cache=TokenCache(), username="not@cache").get_token("scope")
+    with pytest.raises(ClientAuthenticationError, match=NO_ACCOUNTS):
+        SharedTokenCacheCredential(_cache=TokenCache(), tenant_id="not-cached").get_token("scope")
+    with pytest.raises(ClientAuthenticationError, match=NO_ACCOUNTS):
+        SharedTokenCacheCredential(_cache=TokenCache(), tenant_id="not-cached", username="not@cache").get_token("scope")
 
 
-def test_no_matching_account():
+def test_no_matching_account_for_username():
     """one cached account, username specified, username doesn't match -> credential should raise"""
 
     upn = "spam@eggs"
@@ -42,7 +48,69 @@ def test_no_matching_account():
     assert upn in discovered_accounts and tenant in discovered_accounts
 
 
-def test_single_matching_account():
+def test_no_matching_account_for_tenant():
+    """one cached account, tenant specified, tenant doesn't match -> credential should raise"""
+
+    upn = "spam@eggs"
+    tenant = "some-guid"
+    account = get_account_event(username=upn, uid="uid", utid=tenant, refresh_token="refresh-token")
+    cache = populated_cache(account)
+
+    with pytest.raises(ClientAuthenticationError) as ex:
+        SharedTokenCacheCredential(_cache=cache, tenant_id="not-" + tenant).get_token("scope")
+
+    assert ex.value.message.startswith(NO_MATCHING_ACCOUNTS[: NO_MATCHING_ACCOUNTS.index("{")])
+    discovered_accounts = ex.value.message.splitlines()[-1]
+    assert upn in discovered_accounts and tenant in discovered_accounts
+
+
+def test_no_matching_account_for_tenant_and_username():
+    """one cached account, tenant and username specified, neither match -> credential should raise"""
+
+    upn = "spam@eggs"
+    tenant = "some-guid"
+    account = get_account_event(username=upn, uid="uid", utid=tenant, refresh_token="refresh-token")
+    cache = populated_cache(account)
+
+    with pytest.raises(ClientAuthenticationError) as ex:
+        SharedTokenCacheCredential(_cache=cache, tenant_id="not-" + tenant, username="not" + upn).get_token("scope")
+
+    assert ex.value.message.startswith(NO_MATCHING_ACCOUNTS[: NO_MATCHING_ACCOUNTS.index("{")])
+    discovered_accounts = ex.value.message.splitlines()[-1]
+    assert upn in discovered_accounts and tenant in discovered_accounts
+
+
+def test_no_matching_account_for_tenant_or_username():
+    """two cached accounts, username and tenant specified, one account matches each -> credential should raise"""
+
+    refresh_token_a = "refresh-token-a"
+    refresh_token_b = "refresh-token-b"
+    upn_a = "a@foo"
+    upn_b = "b@foo"
+    tenant_a = "tenant-a"
+    tenant_b = "tenant-b"
+    account_a = get_account_event(username=upn_a, uid="uid_a", utid=tenant_a, refresh_token=refresh_token_a)
+    account_b = get_account_event(username=upn_b, uid="uid_b", utid=tenant_b, refresh_token=refresh_token_b)
+    cache = populated_cache(account_a, account_b)
+
+    transport = Mock(side_effect=Exception())  # credential shouldn't use the network
+
+    credential = SharedTokenCacheCredential(username=upn_a, tenant_id=tenant_b, _cache=cache, transport=transport)
+    with pytest.raises(ClientAuthenticationError) as ex:
+        credential.get_token("scope")
+    assert ex.value.message.startswith(NO_MATCHING_ACCOUNTS[: NO_MATCHING_ACCOUNTS.index("{")])
+    discovered_accounts = ex.value.message.splitlines()[-1]
+    assert all(s in discovered_accounts for s in (upn_a, upn_b, tenant_a, tenant_b))
+
+    credential = SharedTokenCacheCredential(username=upn_b, tenant_id=tenant_a, _cache=cache, transport=transport)
+    with pytest.raises(ClientAuthenticationError) as ex:
+        credential.get_token("scope")
+    assert ex.value.message.startswith(NO_MATCHING_ACCOUNTS[: NO_MATCHING_ACCOUNTS.index("{")])
+    discovered_accounts = ex.value.message.splitlines()[-1]
+    assert all(s in discovered_accounts for s in (upn_a, upn_b, tenant_a, tenant_b))
+
+
+def test_single_account_matching_username():
     """one cached account, username specified, username matches -> credential should auth that account"""
 
     upn = "spam@eggs"
@@ -57,6 +125,45 @@ def test_single_matching_account():
         responses=[mock_response(json_payload=build_aad_response(access_token=expected_token))],
     )
     credential = SharedTokenCacheCredential(_cache=cache, transport=transport, username=upn)
+    token = credential.get_token(scope)
+    assert token.token == expected_token
+
+
+def test_single_account_matching_tenant():
+    """one cached account, tenant specified, tenant matches -> credential should auth that account"""
+
+    tenant_id = "tenant-id"
+    refresh_token = "refresh-token"
+    scope = "scope"
+    account = get_account_event(uid="uid_a", utid=tenant_id, username="spam@eggs", refresh_token=refresh_token)
+    cache = populated_cache(account)
+
+    expected_token = "***"
+    transport = validating_transport(
+        requests=[Request(required_data={"refresh_token": refresh_token, "scope": scope})],
+        responses=[mock_response(json_payload=build_aad_response(access_token=expected_token))],
+    )
+    credential = SharedTokenCacheCredential(_cache=cache, transport=transport, tenant_id=tenant_id)
+    token = credential.get_token(scope)
+    assert token.token == expected_token
+
+
+def test_single_account_matching_tenant_and_username():
+    """one cached account, tenant and username specified, both match -> credential should auth that account"""
+
+    upn = "spam@eggs"
+    tenant_id = "tenant-id"
+    refresh_token = "refresh-token"
+    scope = "scope"
+    account = get_account_event(uid="uid_a", utid=tenant_id, username=upn, refresh_token=refresh_token)
+    cache = populated_cache(account)
+
+    expected_token = "***"
+    transport = validating_transport(
+        requests=[Request(required_data={"refresh_token": refresh_token, "scope": scope})],
+        responses=[mock_response(json_payload=build_aad_response(access_token=expected_token))],
+    )
+    credential = SharedTokenCacheCredential(_cache=cache, transport=transport, tenant_id=tenant_id, username=upn)
     token = credential.get_token(scope)
     assert token.token == expected_token
 
@@ -86,8 +193,7 @@ def test_no_refresh_token():
     account = get_account_event(uid="uid_a", utid="utid", username="spam@eggs", refresh_token=None)
     cache = populated_cache(account)
 
-    # credential has no refresh token to redeem => it shouldn't use the network
-    transport = Mock(side_effect=Exception())
+    transport = Mock(side_effect=Exception())  # credential shouldn't use the network
 
     credential = SharedTokenCacheCredential(_cache=cache, transport=transport)
     with pytest.raises(ClientAuthenticationError, match=NO_ACCOUNTS):
@@ -98,8 +204,8 @@ def test_no_refresh_token():
         credential.get_token("scope")
 
 
-def test_two_accounts_username_unspecified():
-    """two cached accounts, no username specified -> credential should raise"""
+def test_two_accounts_no_username_or_tenant():
+    """two cached accounts, no username or tenant specified -> credential should raise"""
 
     upn_a = "a@foo"
     upn_b = "b@foo"
@@ -141,18 +247,63 @@ def test_two_accounts_username_specified():
     assert token.token == expected_token
 
 
+def test_two_accounts_tenant_specified():
+    """two cached accounts, tenant specified, one account matches -> credential should auth that account"""
+
+    scope = "scope"
+    expected_refresh_token = "refresh-token-a"
+    upn_a = "a@foo"
+    upn_b = "b@foo"
+    tenant_id = "tenant-id"
+    account_a = get_account_event(username=upn_a, uid="uid_a", utid=tenant_id, refresh_token=expected_refresh_token)
+    account_b = get_account_event(username=upn_b, uid="uid_b", utid="utid", refresh_token="refresh_token_b")
+    cache = populated_cache(account_a, account_b)
+
+    expected_token = "***"
+    transport = validating_transport(
+        requests=[Request(required_data={"refresh_token": expected_refresh_token, "scope": scope})],
+        responses=[mock_response(json_payload=build_aad_response(access_token=expected_token))],
+    )
+    credential = SharedTokenCacheCredential(tenant_id=tenant_id, _cache=cache, transport=transport)
+    token = credential.get_token(scope)
+    assert token.token == expected_token
+
+
+def test_two_accounts_tenant_and_username_specified():
+    """two cached accounts, tenant and username specified, one account matches both -> credential should auth that account"""
+
+    scope = "scope"
+    expected_refresh_token = "refresh-token-a"
+    upn_a = "a@foo"
+    upn_b = "b@foo"
+    tenant_id = "tenant-id"
+    account_a = get_account_event(username=upn_a, uid="uid_a", utid=tenant_id, refresh_token=expected_refresh_token)
+    account_b = get_account_event(username=upn_b, uid="uid_b", utid="utid", refresh_token="refresh_token_b")
+    cache = populated_cache(account_a, account_b)
+
+    expected_token = "***"
+    transport = validating_transport(
+        requests=[Request(required_data={"refresh_token": expected_refresh_token, "scope": scope})],
+        responses=[mock_response(json_payload=build_aad_response(access_token=expected_token))],
+    )
+    credential = SharedTokenCacheCredential(tenant_id=tenant_id, username=upn_a, _cache=cache, transport=transport)
+    token = credential.get_token(scope)
+    assert token.token == expected_token
+
+
 def test_same_username_different_tenants():
     """two cached accounts, same username, different tenants"""
 
-    expected_access_token = "***"
-    expected_refresh_token = "refresh-token"
+    access_token_a = "access-token-a"
+    access_token_b = "access-token-b"
+    refresh_token_a = "refresh-token-a"
+    refresh_token_b = "refresh-token-b"
 
     upn = "spam@eggs"
     tenant_a = "tenant-a"
     tenant_b = "tenant-b"
-    uid = "some-guid"
-    account_a = get_account_event(username=upn, uid=uid, utid=tenant_a, refresh_token="refresh_token")
-    account_b = get_account_event(username=upn, uid=uid, utid=tenant_b, refresh_token=expected_refresh_token)
+    account_a = get_account_event(username=upn, uid="another-guid", utid=tenant_a, refresh_token=refresh_token_a)
+    account_b = get_account_event(username=upn, uid="more-guid", utid=tenant_b, refresh_token=refresh_token_b)
     cache = populated_cache(account_a, account_b)
 
     # with no tenant specified the credential can't select an identity
@@ -166,16 +317,68 @@ def test_same_username_different_tenants():
     assert discovered_accounts.count(upn) == 2
     assert tenant_a in discovered_accounts and tenant_b in discovered_accounts
 
-    # with username and tenant_id specified, the credential should auth the matching account
-    expected_access_token = "***"
+    # with tenant specified, the credential should auth the matching account
     scope = "scope"
     transport = validating_transport(
-        requests=[Request(required_data={"refresh_token": expected_refresh_token, "scope": scope})],
-        responses=[mock_response(json_payload=build_aad_response(access_token=expected_access_token))],
+        requests=[Request(required_data={"refresh_token": refresh_token_a, "scope": scope})],
+        responses=[mock_response(json_payload=build_aad_response(access_token=access_token_a))],
     )
-    credential = SharedTokenCacheCredential(username=upn, tenant_id=tenant_b, _cache=cache, transport=transport)
+    credential = SharedTokenCacheCredential(tenant_id=tenant_a, _cache=cache, transport=transport)
     token = credential.get_token(scope)
-    assert token.token == expected_access_token
+    assert token.token == access_token_a
+
+    transport = validating_transport(
+        requests=[Request(required_data={"refresh_token": refresh_token_b, "scope": scope})],
+        responses=[mock_response(json_payload=build_aad_response(access_token=access_token_b))],
+    )
+    credential = SharedTokenCacheCredential(tenant_id=tenant_b, _cache=cache, transport=transport)
+    token = credential.get_token(scope)
+    assert token.token == access_token_b
+
+
+def test_same_tenant_different_usernames():
+    """two cached accounts, same tenant, different usernames"""
+
+    access_token_a = "access-token-a"
+    access_token_b = "access-token-b"
+    refresh_token_a = "refresh-token-a"
+    refresh_token_b = "refresh-token-b"
+
+    upn_a = "spam@eggs"
+    upn_b = "eggs@spam"
+    tenant_id = "the-tenant"
+    account_a = get_account_event(username=upn_a, uid="another-guid", utid=tenant_id, refresh_token=refresh_token_a)
+    account_b = get_account_event(username=upn_b, uid="more-guid", utid=tenant_id, refresh_token=refresh_token_b)
+    cache = populated_cache(account_a, account_b)
+
+    # with no username specified the credential can't select an identity
+    transport = Mock(side_effect=Exception())  # (so it shouldn't use the network)
+    credential = SharedTokenCacheCredential(tenant_id=tenant_id, _cache=cache, transport=transport)
+    with pytest.raises(ClientAuthenticationError) as ex:
+        credential.get_token("scope")
+    # error message should indicate multiple matching accounts, and list discovered accounts
+    assert ex.value.message.startswith(MULTIPLE_MATCHING_ACCOUNTS[: MULTIPLE_MATCHING_ACCOUNTS.index("{")])
+    discovered_accounts = ex.value.message.splitlines()[-1]
+    assert discovered_accounts.count(tenant_id) == 2
+    assert upn_a in discovered_accounts and upn_b in discovered_accounts
+
+    # with a username specified, the credential should auth the matching account
+    scope = "scope"
+    transport = validating_transport(
+        requests=[Request(required_data={"refresh_token": refresh_token_b, "scope": scope})],
+        responses=[mock_response(json_payload=build_aad_response(access_token=access_token_a))],
+    )
+    credential = SharedTokenCacheCredential(username=upn_b, _cache=cache, transport=transport)
+    token = credential.get_token(scope)
+    assert token.token == access_token_a
+
+    transport = validating_transport(
+        requests=[Request(required_data={"refresh_token": refresh_token_a, "scope": scope})],
+        responses=[mock_response(json_payload=build_aad_response(access_token=access_token_a))],
+    )
+    credential = SharedTokenCacheCredential(username=upn_a, _cache=cache, transport=transport)
+    token = credential.get_token(scope)
+    assert token.token == access_token_a
 
 
 def get_account_event(
@@ -196,7 +399,7 @@ def get_account_event(
             foci="1",
         ),
         "client_id": client_id,
-        "token_endpoint": "https://" + authority + "/some/path",
+        "token_endpoint": "https://" + "/".join((authority, utid, "/path")),
         "scope": scopes or ["scope"],
     }
 
