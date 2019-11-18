11f7d5913bebb10c576d81d15d438dfc98ee0c2c
Xiang Yan <xiangyan@microsoft.com>
2019-11-18 08:57:05 -0800

    fix test

--- sdk/core/azure-core/tests/test_universal_pipeline.py
+++ sdk/core/azure-core/tests/test_universal_pipeline.py
@@ -160,7 +160,7 @@ def test_retry_seekable_body():
         'body_position': 10,
     }
     increment = http_retry.increment(setting, response)
-    assert increment
+    assert not increment
 
 def test_raw_deserializer():
     raw_deserializer = ContentDecodePolicy()
