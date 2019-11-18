3ff31498fb12da110a3e54e70b5cd92ed1214a4c
Isabella Cai <iscai@microsoft.com>
2019-11-18 09:58:55 -0800

    fixed test to be 2.7 compatible

--- sdk/keyvault/azure-keyvault-certificates/tests/test_certificates_models.py
+++ sdk/keyvault/azure-keyvault-certificates/tests/test_certificates_models.py
@@ -5,16 +5,12 @@ def test_policy_expected_errors():
         cert_policy = CertificatePolicy("issuer-name", "subject-name")
         assert False, "Error should've thrown since we only have one positional parameter"
     except TypeError as e:
-        if str(e) == '__init__() takes 2 positional arguments but 3 were given':
-            pass
-        else:
-            raise e
+        if '__init__' not in str(e):
+            raise
 
     try:
         cert_policy = CertificatePolicy("issuer-name")
         assert False, "Error should be thrown since we haven't set a subject_name or sans"
     except ValueError as e:
-        if str(e) == "You need to set either subject_name or one of the subject alternative names parameters":
-            pass
-        else:
-            raise e
+        if "subject_name" not in str(e):
+            raise
