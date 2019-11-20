9d7e4fd729fcda3b139d0bef3199cef4cbdec607
Adam Ling (MSFT) <47871814+yunhaoling@users.noreply.github.com>
2019-11-20 15:19:28 -0800

    [EventHubs] API Review Change: Renaming (#8717)
    * Change eventhub_name and fully_qualified_namespace
    
    * eventdata application_properties to properties
    
    * partition -> partition_id
    
    * log update checkpoint no-op from info to warning
    
    * rename get_properties to get_eventhub_properties on client object
    
    * Move consumer group into contructor and rename to consumer_group
    
    * Rename partition manager to checkpoint store
    
    * rename event_hub_path to eventhub_name in samples
    
    * Partition manager to block checkpoint store
    
    * trying to make CI happy, update some legacy code
    
    * draft change log update
    
    * fix pylint error
    
    * Update test code to make ci pass
    
    * update test
    
    * Fix pylint and unimported TokenCredential error
    
    * update livetest and fix bug
    
    * fix pylint
    
    * Updates according to the review

--- sdk/eventhub/azure-eventhubs-checkpointstoreblob/tests/test_storage_blob_partition_manager.py
+++ sdk/eventhub/azure-eventhubs-checkpointstoreblob/tests/test_storage_blob_partition_manager.py
@@ -10,7 +10,7 @@ import os
 import uuid
 import warnings
 
-from azure.eventhub.extensions.checkpointstoreblob import BlobPartitionManager
+from azure.eventhub.extensions.checkpointstoreblob import BlobCheckpointStore
 
 
 def get_live_storage_blob_client():
@@ -44,15 +44,15 @@ def remove_live_storage_blob_client(container_str):
 def _claim_and_list_ownership(live_storage_blob_client):
     fully_qualified_namespace = 'test_namespace'
     eventhub_name = 'eventhub'
-    consumer_group_name = '$default'
+    consumer_group = '$default'
     ownership_cnt = 8
     with live_storage_blob_client:
-        partition_manager = BlobPartitionManager(container_client=live_storage_blob_client)
+        checkpoint_store = BlobCheckpointStore(container_client=live_storage_blob_client)
 
-        ownership_list = partition_manager.list_ownership(
+        ownership_list = checkpoint_store.list_ownership(
             fully_qualified_namespace=fully_qualified_namespace,
             eventhub_name=eventhub_name,
-            consumer_group_name=consumer_group_name)
+            consumer_group=consumer_group)
         assert len(ownership_list) == 0
 
         ownership_list = []
@@ -61,7 +61,7 @@ def _claim_and_list_ownership(live_storage_blob_client):
             ownership = {}
             ownership['fully_qualified_namespace'] = fully_qualified_namespace
             ownership['eventhub_name'] = eventhub_name
-            ownership['consumer_group_name'] = consumer_group_name
+            ownership['consumer_group'] = consumer_group
             ownership['owner_id'] = 'ownerid'
             ownership['partition_id'] = str(i)
             ownership['last_modified_time'] = time.time()
@@ -69,12 +69,12 @@ def _claim_and_list_ownership(live_storage_blob_client):
             ownership["sequence_number"] = "1"
             ownership_list.append(ownership)
 
-        partition_manager.claim_ownership(ownership_list)
+        checkpoint_store.claim_ownership(ownership_list)
 
-        ownership_list = partition_manager.list_ownership(
+        ownership_list = checkpoint_store.list_ownership(
             fully_qualified_namespace=fully_qualified_namespace,
             eventhub_name=eventhub_name,
-            consumer_group_name=consumer_group_name)
+            consumer_group=consumer_group)
         assert len(ownership_list) == ownership_cnt
 
 
@@ -92,20 +92,20 @@ def test_claim_and_list_ownership():
 def _update_checkpoint(live_storage_blob_client):
     fully_qualified_namespace = 'test_namespace'
     eventhub_name = 'eventhub'
-    consumer_group_name = '$default'
+    consumer_group = '$default'
     partition_cnt = 8
 
     with live_storage_blob_client:
-        partition_manager = BlobPartitionManager(container_client=live_storage_blob_client)
+        checkpoint_store = BlobCheckpointStore(container_client=live_storage_blob_client)
         for i in range(partition_cnt):
-            partition_manager.update_checkpoint(
-                fully_qualified_namespace, eventhub_name, consumer_group_name, str(i),
+            checkpoint_store.update_checkpoint(
+                fully_qualified_namespace, eventhub_name, consumer_group, str(i),
                 '2', 20)
 
-        checkpoint_list = partition_manager.list_checkpoints(
+        checkpoint_list = checkpoint_store.list_checkpoints(
             fully_qualified_namespace=fully_qualified_namespace,
             eventhub_name=eventhub_name,
-            consumer_group_name=consumer_group_name)
+            consumer_group=consumer_group)
         assert len(checkpoint_list) == partition_cnt
         for checkpoint in checkpoint_list:
             assert checkpoint['offset'] == '2'
