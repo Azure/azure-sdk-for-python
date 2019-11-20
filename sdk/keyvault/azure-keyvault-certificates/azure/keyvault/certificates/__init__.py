ab7f9433945adaeda6c9c8fb4f3d1404c6d65f3b
Isabella Cai <iscai@microsoft.com>
2019-11-20 13:08:09 -0800

    fixed linting mistake

--- sdk/keyvault/azure-keyvault-certificates/azure/keyvault/certificates/__init__.py
+++ sdk/keyvault/azure-keyvault-certificates/azure/keyvault/certificates/__init__.py
@@ -3,7 +3,14 @@
 # Licensed under the MIT License.
 # ------------------------------------
 from ._client import CertificateClient
-from ._enums import CertificatePolicyAction, KeyCurveName, KeyType, SecretContentType, KeyUsageType, WellKnownIssuerNames
+from ._enums import(
+    CertificatePolicyAction,
+    KeyCurveName,
+    KeyType,
+    SecretContentType,
+    KeyUsageType,
+    WellKnownIssuerNames
+)
 from ._models import(
     AdministratorContact,
     CertificateContact,
--- sdk/keyvault/azure-keyvault-certificates/azure/keyvault/certificates/_models.py
+++ sdk/keyvault/azure-keyvault-certificates/azure/keyvault/certificates/_models.py
@@ -6,7 +6,14 @@
 # pylint: disable=too-many-lines,too-many-public-methods
 from ._shared import parse_vault_id
 from ._shared._generated.v7_0 import models
-from ._enums import CertificatePolicyAction, KeyUsageType, KeyCurveName, KeyType, SecretContentType, WellKnownIssuerNames
+from ._enums import(
+    CertificatePolicyAction,
+    KeyUsageType,
+    KeyCurveName,
+    KeyType,
+    SecretContentType,
+    WellKnownIssuerNames
+)
 
 try:
     from typing import TYPE_CHECKING
