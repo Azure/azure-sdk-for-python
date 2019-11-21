6a25257729a38ff6976b01259e22bf4f3a992782
Yunhao Ling <adam_ling@outlook.com>
2019-11-20 21:24:30 -0800

    fix unittest

--- sdk/eventhub/azure-eventhubs/tests/unittest/test_event_data.py
+++ sdk/eventhub/azure-eventhubs/tests/unittest/test_event_data.py
@@ -48,7 +48,7 @@ def test_app_properties():
 
 def test_event_data_batch():
     batch = EventDataBatch(max_size=100, partition_key="par")
-    batch.try_add(EventData("A"))
+    batch.add(EventData("A"))
     assert batch.size == 89 and len(batch) == 1
     with pytest.raises(ValueError):
-        batch.try_add(EventData("A"))
+        batch.add(EventData("A"))
