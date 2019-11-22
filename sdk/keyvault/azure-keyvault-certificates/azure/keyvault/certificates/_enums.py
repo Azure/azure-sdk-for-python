590a85e8fd5294d81f6acb4a082a1a0ca6dea6f6
Isabella Cai <iscai@microsoft.com>
2019-11-22 12:43:10 -0800

    fixed enum name of p_256_k to p_256k

--- sdk/keyvault/azure-keyvault-certificates/azure/keyvault/certificates/_enums.py
+++ sdk/keyvault/azure-keyvault-certificates/azure/keyvault/certificates/_enums.py
@@ -48,7 +48,7 @@ class KeyCurveName(str, Enum):
     p_256 = "P-256"  #: The NIST P-256 elliptic curve, AKA SECG curve SECP256R1.
     p_384 = "P-384"  #: The NIST P-384 elliptic curve, AKA SECG curve SECP384R1.
     p_521 = "P-521"  #: The NIST P-521 elliptic curve, AKA SECG curve SECP521R1.
-    p_256_k = "P-256K"  #: The SECG SECP256K1 elliptic curve.
+    p_256k = "P-256K"  #: The SECG SECP256K1 elliptic curve.
 
 
 class WellKnownIssuerNames(str, Enum):
