e5f08eb05f3d5c7df1c691bcb9d726db498944b9
Xiang Yan <xiangyan@microsoft.com>
2019-11-22 14:37:29 -0800

    update error message

--- sdk/core/azure-core/azure/core/pipeline/transport/_requests_basic.py
+++ sdk/core/azure-core/azure/core/pipeline/transport/_requests_basic.py
@@ -231,7 +231,7 @@ class RequestsTransport(HttpTransport):
                 timeout = (timeout, read_timeout)
             else:
                 if 'read_timeout' in kwargs:
-                    raise ValueError('Conflict, set tuple connection_timeout and read_timeout together')
+                    raise ValueError('Conflict, cannot set tuple connection_timeout and read_timeout together')
                 _LOGGER.warning("Tuple timeout setting is deprecated")
             response = self.session.request(  # type: ignore
                 request.method,
