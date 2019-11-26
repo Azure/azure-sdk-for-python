003ff0d8fc3d1c0f27e5c4dbb2c0c608c95e8edb
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-11-26 10:35:26 -0800

    Adapt test framework to support azure-core based Mgmt client

--- tools/azure-sdk-tools/devtools_testutils/azure_testcase.py
+++ tools/azure-sdk-tools/devtools_testutils/azure_testcase.py
@@ -201,6 +201,10 @@ class AzureTestCase(ReplayableTest):
             credentials = self.settings.get_credentials()
 
         # Real client creation
+        kwargs.setdefault("logging_enable", True)
+        # FIXME decide what is the final argument for that
+        # if self.is_playback():
+        #     kwargs.setdefault("polling_interval", 0)
         if _is_autorest_v3(client_class):
             client = client_class(
                 credential=credentials,
@@ -213,8 +217,15 @@ class AzureTestCase(ReplayableTest):
             )
 
         if self.is_playback():
-            client.config.long_running_operation_timeout = 0
-        client.config.enable_http_logger = True
+            try:
+                client._config.polling_interval = 0  # FIXME in azure-mgmt-core, make this a kwargs
+            except AttributeError:
+                pass
+
+        if hasattr(client, "config"):  # Autorest v2
+            if self.is_playback():
+                client.config.long_running_operation_timeout = 0
+            client.config.enable_http_logger = True
         return client
 
     def create_random_name(self, name):
