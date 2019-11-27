e9163a3d2e359c75cc3a9893e2e7c652179eab47
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2019-11-27 14:55:42 -0800

    Update blob checkpoint store sample (#8897)

--- sdk/eventhub/azure-eventhubs-checkpointstoreblob/samples/event_processor_blob_storage_example.py
+++ sdk/eventhub/azure-eventhubs-checkpointstoreblob/samples/event_processor_blob_storage_example.py
@@ -1,34 +1,23 @@
-import logging
 import os
 from azure.eventhub import EventHubConsumerClient
 from azure.eventhub.extensions.checkpointstoreblob import BlobCheckpointStore
-from azure.storage.blob import ContainerClient
 
 CONNECTION_STR = os.environ["EVENT_HUB_CONN_STR"]
 STORAGE_CONNECTION_STR = os.environ["AZURE_STORAGE_CONN_STR"]
 
-logging.basicConfig(level=logging.INFO)
 
-
-def do_operation(event):
-    # put your code here
+def on_event(partition_context, event):
+    # do something with event
     print(event)
-
-
-def process_events(partition_context, events):
-    # put your code here
-    print("received events: {} from partition: {}".format(len(events), partition_context.partition_id))
-    for event in events:
-        do_operation(event)
-    partition_context.update_checkpoint(events[-1])
+    partition_context.update_checkpoint(event)
 
 
 if __name__ == '__main__':
     checkpoint_store = BlobCheckpointStore.from_connection_string(STORAGE_CONNECTION_STR, "eventprocessor")
     client = EventHubConsumerClient.from_connection_string(
-        CONNECTION_STR, checkpoint_store=checkpoint_store)
+        CONNECTION_STR, "$default", checkpoint_store=checkpoint_store)
 
     try:
-        client.receive(process_events, "$default")
+        client.receive(on_event)
     except KeyboardInterrupt:
         client.close()
