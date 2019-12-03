977efcae310054330b206542d78b983773478f91
Xiang Yan <xiangsjtu@gmail.com>
2019-12-03 12:15:31 -0800

    rewind body/file in retry (#8871)
    * rewind body/file in retry

--- sdk/core/azure-core/azure/core/pipeline/policies/_retry.py
+++ sdk/core/azure-core/azure/core/pipeline/policies/_retry.py
@@ -28,6 +28,7 @@
 This module is the requests implementation of Pipeline ABC
 """
 from __future__ import absolute_import  # we have a "requests" module that conflicts with "requests" on Py2.7
+from io import SEEK_SET, UnsupportedOperation
 import logging
 import time
 import email
@@ -325,7 +326,30 @@ class RetryPolicy(HTTPPolicy):
                         )
                     )
 
-        return not self.is_exhausted(settings)
+        if self.is_exhausted(settings):
+            return False
+
+        if response.http_request.body and hasattr(response.http_request.body, 'read'):
+            if 'body_position' not in settings:
+                return False
+            try:
+                # attempt to rewind the body to the initial position
+                response.http_request.body.seek(settings['body_position'], SEEK_SET)
+            except (UnsupportedOperation, ValueError, AttributeError):
+                # if body is not seekable, then retry would not work
+                return False
+        file_positions = settings.get('file_positions')
+        if response.http_request.files and file_positions:
+            try:
+                for value in response.http_request.files.values():
+                    file_name, body = value[0], value[1]
+                    if file_name in file_positions:
+                        position = file_positions[file_name]
+                        body.seek(position, SEEK_SET)
+            except (UnsupportedOperation, ValueError, AttributeError):
+                # if body is not seekable, then retry would not work
+                return False
+        return True
 
     def update_context(self, context, retry_settings):
         """Updates retry history in pipeline context.
@@ -351,6 +375,29 @@ class RetryPolicy(HTTPPolicy):
         retry_active = True
         response = None
         retry_settings = self.configure_retries(request.context.options)
+        body_position = None
+        file_positions = None
+        if request.http_request.body and hasattr(request.http_request.body, 'read'):
+            try:
+                body_position = request.http_request.body.tell()
+            except (AttributeError, UnsupportedOperation):
+                # if body position cannot be obtained, then retries will not work
+                pass
+        else:
+            if request.http_request.files:
+                file_positions = {}
+                try:
+                    for value in request.http_request.files.values():
+                        name, body = value[0], value[1]
+                        if name and body and hasattr(body, 'read'):
+                            position = body.tell()
+                            file_positions[name] = position
+                except (AttributeError, UnsupportedOperation):
+                    file_positions = None
+
+        retry_settings['body_position'] = body_position
+        retry_settings['file_positions'] = file_positions
+
         while retry_active:
             try:
                 response = self.next.send(request)
