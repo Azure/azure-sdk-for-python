3ca6a252a796efda505084f218effb90f0ca9174
SDK Automation <sdkautomation@microsoft.com>
2019-12-05 01:12:57 +0000

    Generated from 04788d8848fd4fa6c5197d2b8970e4fca0339da5
    fix define conflict

--- sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/models/__init__.py
+++ sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/models/__init__.py
@@ -18,6 +18,7 @@ try:
     from ._models_py3 import AmlComputeNodeInformation
     from ._models_py3 import AmlComputeNodesInformation
     from ._models_py3 import AmlComputeProperties
+    from ._models_py3 import AmlUserFeature
     from ._models_py3 import ClusterUpdateParameters
     from ._models_py3 import Compute
     from ._models_py3 import ComputeNodesInformation
@@ -30,7 +31,7 @@ try:
     from ._models_py3 import DataLakeAnalytics
     from ._models_py3 import DataLakeAnalyticsProperties
     from ._models_py3 import ErrorDetail
-    from ._models_py3 import ErrorResponse
+    from ._models_py3 import ErrorResponse, ErrorResponseException
     from ._models_py3 import HDInsight
     from ._models_py3 import HDInsightProperties
     from ._models_py3 import Identity
@@ -40,13 +41,29 @@ try:
     from ._models_py3 import Operation
     from ._models_py3 import OperationDisplay
     from ._models_py3 import Password
+    from ._models_py3 import PrivateEndpoint
+    from ._models_py3 import PrivateEndpointConnection
+    from ._models_py3 import PrivateLinkResource
+    from ._models_py3 import PrivateLinkResourceListResult
+    from ._models_py3 import PrivateLinkServiceConnectionState
+    from ._models_py3 import QuotaBaseProperties
+    from ._models_py3 import QuotaUpdateParameters
     from ._models_py3 import RegistryListCredentialsResult
     from ._models_py3 import Resource
     from ._models_py3 import ResourceId
+    from ._models_py3 import ResourceName
+    from ._models_py3 import ResourceQuota
+    from ._models_py3 import ResourceSkuLocationInfo
+    from ._models_py3 import ResourceSkuZoneDetails
+    from ._models_py3 import Restriction
     from ._models_py3 import ScaleSettings
     from ._models_py3 import ServicePrincipalCredentials
+    from ._models_py3 import Sku
+    from ._models_py3 import SKUCapability
     from ._models_py3 import SslConfiguration
     from ._models_py3 import SystemService
+    from ._models_py3 import UpdateWorkspaceQuotas
+    from ._models_py3 import UpdateWorkspaceQuotasResult
     from ._models_py3 import Usage
     from ._models_py3 import UsageName
     from ._models_py3 import UserAccountCredentials
@@ -57,6 +74,7 @@ try:
     from ._models_py3 import VirtualMachineSizeListResult
     from ._models_py3 import VirtualMachineSshCredentials
     from ._models_py3 import Workspace
+    from ._models_py3 import WorkspaceSku
     from ._models_py3 import WorkspaceUpdateParameters
 except (SyntaxError, ImportError):
     from ._models import AKS
@@ -67,6 +85,7 @@ except (SyntaxError, ImportError):
     from ._models import AmlComputeNodeInformation
     from ._models import AmlComputeNodesInformation
     from ._models import AmlComputeProperties
+    from ._models import AmlUserFeature
     from ._models import ClusterUpdateParameters
     from ._models import Compute
     from ._models import ComputeNodesInformation
@@ -79,7 +98,7 @@ except (SyntaxError, ImportError):
     from ._models import DataLakeAnalytics
     from ._models import DataLakeAnalyticsProperties
     from ._models import ErrorDetail
-    from ._models import ErrorResponse
+    from ._models import ErrorResponse, ErrorResponseException
     from ._models import HDInsight
     from ._models import HDInsightProperties
     from ._models import Identity
@@ -89,13 +108,29 @@ except (SyntaxError, ImportError):
     from ._models import Operation
     from ._models import OperationDisplay
     from ._models import Password
+    from ._models import PrivateEndpoint
+    from ._models import PrivateEndpointConnection
+    from ._models import PrivateLinkResource
+    from ._models import PrivateLinkResourceListResult
+    from ._models import PrivateLinkServiceConnectionState
+    from ._models import QuotaBaseProperties
+    from ._models import QuotaUpdateParameters
     from ._models import RegistryListCredentialsResult
     from ._models import Resource
     from ._models import ResourceId
+    from ._models import ResourceName
+    from ._models import ResourceQuota
+    from ._models import ResourceSkuLocationInfo
+    from ._models import ResourceSkuZoneDetails
+    from ._models import Restriction
     from ._models import ScaleSettings
     from ._models import ServicePrincipalCredentials
+    from ._models import Sku
+    from ._models import SKUCapability
     from ._models import SslConfiguration
     from ._models import SystemService
+    from ._models import UpdateWorkspaceQuotas
+    from ._models import UpdateWorkspaceQuotasResult
     from ._models import Usage
     from ._models import UsageName
     from ._models import UserAccountCredentials
@@ -106,18 +141,29 @@ except (SyntaxError, ImportError):
     from ._models import VirtualMachineSizeListResult
     from ._models import VirtualMachineSshCredentials
     from ._models import Workspace
+    from ._models import WorkspaceSku
     from ._models import WorkspaceUpdateParameters
+from ._paged_models import AmlUserFeaturePaged
 from ._paged_models import ComputeResourcePaged
 from ._paged_models import OperationPaged
+from ._paged_models import ResourceQuotaPaged
 from ._paged_models import UsagePaged
 from ._paged_models import WorkspacePaged
+from ._paged_models import WorkspaceSkuPaged
 from ._azure_machine_learning_workspaces_enums import (
     ProvisioningState,
     UsageUnit,
+    QuotaUnit,
+    Status,
     ResourceIdentityType,
     VmPriority,
+    RemoteLoginPortPublicAccess,
     AllocationState,
+    NodeState,
     ComputeType,
+    ReasonCode,
+    PrivateEndpointServiceConnectionStatus,
+    PrivateEndpointConnectionProvisioningState,
     UnderlyingResourceAction,
 )
 
@@ -130,6 +176,7 @@ __all__ = [
     'AmlComputeNodeInformation',
     'AmlComputeNodesInformation',
     'AmlComputeProperties',
+    'AmlUserFeature',
     'ClusterUpdateParameters',
     'Compute',
     'ComputeNodesInformation',
@@ -142,7 +189,7 @@ __all__ = [
     'DataLakeAnalytics',
     'DataLakeAnalyticsProperties',
     'ErrorDetail',
-    'ErrorResponse',
+    'ErrorResponse', 'ErrorResponseException',
     'HDInsight',
     'HDInsightProperties',
     'Identity',
@@ -152,13 +199,29 @@ __all__ = [
     'Operation',
     'OperationDisplay',
     'Password',
+    'PrivateEndpoint',
+    'PrivateEndpointConnection',
+    'PrivateLinkResource',
+    'PrivateLinkResourceListResult',
+    'PrivateLinkServiceConnectionState',
+    'QuotaBaseProperties',
+    'QuotaUpdateParameters',
     'RegistryListCredentialsResult',
     'Resource',
     'ResourceId',
+    'ResourceName',
+    'ResourceQuota',
+    'ResourceSkuLocationInfo',
+    'ResourceSkuZoneDetails',
+    'Restriction',
     'ScaleSettings',
     'ServicePrincipalCredentials',
+    'Sku',
+    'SKUCapability',
     'SslConfiguration',
     'SystemService',
+    'UpdateWorkspaceQuotas',
+    'UpdateWorkspaceQuotasResult',
     'Usage',
     'UsageName',
     'UserAccountCredentials',
@@ -169,16 +232,27 @@ __all__ = [
     'VirtualMachineSizeListResult',
     'VirtualMachineSshCredentials',
     'Workspace',
+    'WorkspaceSku',
     'WorkspaceUpdateParameters',
     'OperationPaged',
     'WorkspacePaged',
+    'AmlUserFeaturePaged',
     'UsagePaged',
+    'ResourceQuotaPaged',
     'ComputeResourcePaged',
+    'WorkspaceSkuPaged',
     'ProvisioningState',
     'UsageUnit',
+    'QuotaUnit',
+    'Status',
     'ResourceIdentityType',
     'VmPriority',
+    'RemoteLoginPortPublicAccess',
     'AllocationState',
+    'NodeState',
     'ComputeType',
+    'ReasonCode',
+    'PrivateEndpointServiceConnectionStatus',
+    'PrivateEndpointConnectionProvisioningState',
     'UnderlyingResourceAction',
 ]
