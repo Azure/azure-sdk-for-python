ad28d44b7a8dce32c9bffe35f26068847fed2056
Charles Lowell <chlowe@microsoft.com>
2019-12-06 13:44:06 -0800

    More robust and performant challenge auth policy (#8999)

--- sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/_shared/challenge_auth_policy.py
+++ sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/_shared/challenge_auth_policy.py
@@ -2,7 +2,21 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-from azure.core.pipeline import PipelineRequest
+"""Policy implementing Key Vault's challenge authentication protocol.
+
+Normally the protocol is only used for the client's first service request, upon which:
+1. The challenge authentication policy sends a copy of the request, without authorization or content.
+2. Key Vault responds 401 with a header (the 'challenge') detailing how the client should authenticate such a request.
+3. The policy authenticates according to the challenge and sends the original request with authorization.
+
+The policy caches the challenge and thus knows how to authenticate future requests. However, authentication
+requirements can change. For example, a vault may move to a new tenant. In such a case the policy will attempt the
+protocol again.
+"""
+
+import copy
+
+from azure.core.pipeline import PipelineContext, PipelineRequest
 from azure.core.pipeline.policies import HTTPPolicy
 from azure.core.pipeline.policies._authentication import _BearerTokenCredentialPolicyBase
 from azure.core.pipeline.transport import HttpRequest
@@ -40,6 +54,22 @@ class ChallengeAuthPolicyBase(_BearerTokenCredentialPolicyBase):
         ChallengeCache.set_challenge_for_url(request.http_request.url, challenge)
         return challenge
 
+    @staticmethod
+    def _get_challenge_request(request):
+        # type: (PipelineRequest) -> PipelineRequest
+
+        # The challenge request is intended to provoke an authentication challenge from Key Vault, to learn how the
+        # service request should be authenticated. It should be identical to the service request but with no body.
+        challenge_request = HttpRequest(
+            request.http_request.method, request.http_request.url, headers=request.http_request.headers
+        )
+        challenge_request.headers["Content-Length"] = "0"
+
+        options = copy.deepcopy(request.context.options)
+        context = PipelineContext(request.context.transport, **options)
+
+        return PipelineRequest(http_request=challenge_request, context=context)
+
 
 class ChallengeAuthPolicy(ChallengeAuthPolicyBase, HTTPPolicy):
     """policy for handling HTTP authentication challenges"""
@@ -49,15 +79,8 @@ class ChallengeAuthPolicy(ChallengeAuthPolicyBase, HTTPPolicy):
 
         challenge = ChallengeCache.get_challenge_for_url(request.http_request.url)
         if not challenge:
-            # provoke a challenge with an unauthorized, bodiless request
-            no_body = HttpRequest(
-                request.http_request.method, request.http_request.url, headers=request.http_request.headers
-            )
-            if request.http_request.body:
-                # no_body was created with request's headers -> if request has a body, no_body's content-length is wrong
-                no_body.headers["Content-Length"] = "0"
-
-            challenger = self.next.send(PipelineRequest(http_request=no_body, context=request.context))
+            challenge_request = self._get_challenge_request(request)
+            challenger = self.next.send(challenge_request)
             try:
                 challenge = self._update_challenge(request, challenger)
             except ValueError:
@@ -68,6 +91,9 @@ class ChallengeAuthPolicy(ChallengeAuthPolicyBase, HTTPPolicy):
         response = self.next.send(request)
 
         if response.http_response.status_code == 401:
+            # any cached token must be invalid
+            self._token = None
+
             # cached challenge could be outdated; maybe this response has a new one?
             try:
                 challenge = self._update_challenge(request, response)
@@ -84,9 +110,10 @@ class ChallengeAuthPolicy(ChallengeAuthPolicyBase, HTTPPolicy):
         # type: (PipelineRequest, HttpChallenge) -> None
         """authenticate according to challenge, add Authorization header to request"""
 
-        scope = challenge.get_resource()
-        if not scope.endswith("/.default"):
-            scope += "/.default"
+        if self._need_new_token:
+            # azure-identity credentials require an AADv2 scope but the challenge may specify an AADv1 resource
+            scope = challenge.get_scope() or challenge.get_resource() + "/.default"
+            self._token = self._credential.get_token(scope)
 
-        access_token = self._credential.get_token(scope)
-        self._update_headers(request.http_request.headers, access_token.token)
+        # ignore mypy's warning because although self._token is Optional, get_token raises when it fails to get a token
+        self._update_headers(request.http_request.headers, self._token.token)  # type: ignore
--- sdk/keyvault/azure-keyvault-keys/tests/test_challenge_auth.py
+++ sdk/keyvault/azure-keyvault-keys/tests/test_challenge_auth.py
@@ -6,29 +6,46 @@
 Tests for the HTTP challenge authentication implementation. These tests aren't parallelizable, because
 the challenge cache is global to the process.
 """
+import functools
+import time
+from uuid import uuid4
 
 try:
-    from unittest.mock import Mock
+    from unittest.mock import Mock, patch
 except ImportError:  # python < 3.3
-    from mock import Mock
+    from mock import Mock, patch  # type: ignore
 
 from azure.core.credentials import AccessToken
 from azure.core.pipeline import Pipeline
+from azure.core.pipeline.policies import SansIOHTTPPolicy
 from azure.core.pipeline.transport import HttpRequest
 from azure.keyvault.keys._shared import ChallengeAuthPolicy, HttpChallenge, HttpChallengeCache
-import pytest
 
 from keys_helpers import mock_response, Request, validating_transport
 
 
-def test_challenge_cache():
-    # ensure the test starts with an empty cache
-    HttpChallengeCache.clear()
+def empty_challenge_cache(fn):
+    @functools.wraps(fn)
+    def wrapper():
+        HttpChallengeCache.clear()
+        assert len(HttpChallengeCache._cache) == 0
+        return fn()
 
-    url_a = "https://azure.service.a"
+    return wrapper
+
+
+def get_random_url():
+    """The challenge cache is keyed on URLs. Random URLs defend against tests interfering with each other."""
+
+    return "https://{}/{}".format(uuid4(), uuid4()).replace("-", "")
+
+
+@empty_challenge_cache
+def test_challenge_cache():
+    url_a = get_random_url()
     challenge_a = HttpChallenge(url_a, "Bearer authorization=authority A, resource=resource A")
 
-    url_b = "https://azure.service.b"
+    url_b = get_random_url()
     challenge_b = HttpChallenge(url_b, "Bearer authorization=authority B, resource=resource B")
 
     for url, challenge in zip((url_a, url_b), (challenge_a, challenge_b)):
@@ -53,59 +70,73 @@ def test_challenge_parsing():
     assert challenge.get_resource() == resource
 
 
-def test_policy():
-    # ensure the test starts with an empty cache
-    HttpChallengeCache.clear()
+@empty_challenge_cache
+def test_scope():
+    """The policy's token requests should always be for an AADv2 scope"""
 
-    expected_scope = "https://challenge.resource/.default"
-    expected_token = "expected_token"
-    challenge = Mock(
+    expected_content = b"a duck"
+
+    def test_with_challenge(challenge, expected_scope):
+        expected_token = "expected_token"
+
+        class Requests:
+            count = 0
+
+        def send(request):
+            Requests.count += 1
+            if Requests.count == 1:
+                # first request should be unauthorized and have no content
+                assert not request.body
+                assert request.headers["Content-Length"] == "0"
+                return challenge
+            elif Requests.count == 2:
+                # second request should be authorized according to challenge and have the expected content
+                assert request.headers["Content-Length"]
+                assert request.body == expected_content
+                assert expected_token in request.headers["Authorization"]
+                return Mock(status_code=200)
+            raise ValueError("unexpected request")
+
+        def get_token(*scopes):
+            assert len(scopes) is 1
+            assert scopes[0] == expected_scope
+            return AccessToken(expected_token, 0)
+
+        credential = Mock(get_token=Mock(wraps=get_token))
+        pipeline = Pipeline(policies=[ChallengeAuthPolicy(credential=credential)], transport=Mock(send=send))
+        request = HttpRequest("POST", get_random_url())
+        request.set_bytes_body(expected_content)
+        pipeline.run(request)
+
+        assert credential.get_token.call_count == 1
+
+    endpoint = "https://authority.net/tenant"
+
+    # an AADv1 resource becomes an AADv2 scope with the addition of '/.default'
+    resource = "https://challenge.resource"
+    scope = resource + "/.default"
+
+    challenge_with_resource = Mock(
         status_code=401,
-        headers={
-            "WWW-Authenticate": 'Bearer authorization="https://login.authority.net/tenant", resource={}'.format(
-                expected_scope
-            )
-        },
+        headers={"WWW-Authenticate": 'Bearer authorization="{}", resource={}'.format(endpoint, resource)},
     )
-    success = Mock(status_code=200)
-    data = {"spam": "eggs"}
 
-    responses = (r for r in (challenge, success))
+    challenge_with_scope = Mock(
+        status_code=401, headers={"WWW-Authenticate": 'Bearer authorization="{}", scope={}'.format(endpoint, scope)}
+    )
 
-    def send(request):
-        response = next(responses)
-        if response is challenge:
-            # this is the first request
-            assert not request.body
-            assert request.headers["Content-Length"] == "0"
-        elif response is success:
-            # this is the second request
-            assert request.body == data
-            assert expected_token in request.headers["Authorization"]
-        return response
-
-    def get_token(*scopes):
-        assert len(scopes) is 1
-        assert scopes[0] == expected_scope
-        return AccessToken(expected_token, 0)
-
-    credential = Mock(get_token=Mock(wraps=get_token))
-    pipeline = Pipeline(policies=[ChallengeAuthPolicy(credential=credential)], transport=Mock(send=send))
-    pipeline.run(HttpRequest("POST", "https://azure.service", data=data))
-
-    assert credential.get_token.call_count == 1
+    test_with_challenge(challenge_with_resource, scope)
+    test_with_challenge(challenge_with_scope, scope)
 
 
+@empty_challenge_cache
 def test_policy_updates_cache():
     """
     It's possible for the challenge returned for a request to change, e.g. when a vault is moved to a new tenant.
     When the policy receives a 401, it should update the cached challenge for the requested URL, if one exists.
     """
 
-    # ensure the test starts with an empty cache
-    HttpChallengeCache.clear()
-
-    url = "https://azure.service/path"
+    url = get_random_url()
     first_scope = "https://first-scope"
     first_token = "first-scope-token"
     second_scope = "https://second-scope"
@@ -114,13 +145,15 @@ def test_policy_updates_cache():
 
     # mocking a tenant change:
     # 1. first request -> respond with challenge
-    # 2. second request should be authorized according to the challenge -> respond with success
-    # 3. third request should match the second -> respond with a new challenge
-    # 4. fourth request should be authorized according to the new challenge -> respond with success
-    # 5. fifth request should match the fourth -> respond with success
+    # 2. second request should be authorized according to the challenge
+    # 3. third request should match the second (using a cached access token)
+    # 4. fourth request should also match the second -> respond with a new challenge
+    # 5. fifth request should be authorized according to the new challenge
+    # 6. sixth request should match the fifth
     transport = validating_transport(
         requests=(
             Request(url),
+            Request(url, required_headers={"Authorization": "Bearer {}".format(first_token)}),
             Request(url, required_headers={"Authorization": "Bearer {}".format(first_token)}),
             Request(url, required_headers={"Authorization": "Bearer {}".format(first_token)}),
             Request(url, required_headers={"Authorization": "Bearer {}".format(second_token)}),
@@ -129,18 +162,140 @@ def test_policy_updates_cache():
         responses=(
             mock_response(status_code=401, headers={"WWW-Authenticate": challenge_fmt.format(first_scope)}),
             mock_response(status_code=200),
+            mock_response(status_code=200),
             mock_response(status_code=401, headers={"WWW-Authenticate": challenge_fmt.format(second_scope)}),
             mock_response(status_code=200),
             mock_response(status_code=200),
         ),
     )
 
-    tokens = (t for t in [first_token] * 2 + [second_token] * 2)
-    credential = Mock(get_token=lambda _: AccessToken(next(tokens), 0))
+    credential = Mock(get_token=Mock(return_value=AccessToken(first_token, time.time() + 3600)))
     pipeline = Pipeline(policies=[ChallengeAuthPolicy(credential=credential)], transport=transport)
 
-    # policy should complete and cache the first challenge
-    pipeline.run(HttpRequest("GET", url))
+    # policy should complete and cache the first challenge and access token
+    for _ in range(2):
+        pipeline.run(HttpRequest("GET", url))
+        assert credential.get_token.call_count == 1
 
-    # The next request will receive a challenge. The policy should handle it and update the cache entry.
-    pipeline.run(HttpRequest("GET", url))
+    # The next request will receive a new challenge. The policy should handle it and update caches.
+    credential.get_token.return_value = AccessToken(second_token, time.time() + 3600)
+    for _ in range(2):
+        pipeline.run(HttpRequest("GET", url))
+        assert credential.get_token.call_count == 2
+
+
+@empty_challenge_cache
+def test_token_expiration():
+    """policy should not use a cached token which has expired"""
+
+    url = get_random_url()
+
+    expires_on = time.time() + 3600
+    first_token = "*"
+    second_token = "**"
+
+    token = AccessToken(first_token, expires_on)
+
+    def get_token(*_, **__):
+        return token
+
+    credential = Mock(get_token=Mock(wraps=get_token))
+    transport = validating_transport(
+        requests=[
+            Request(),
+            Request(required_headers={"Authorization": "Bearer " + first_token}),
+            Request(required_headers={"Authorization": "Bearer " + first_token}),
+            Request(required_headers={"Authorization": "Bearer " + second_token}),
+        ],
+        responses=[
+            mock_response(
+                status_code=401, headers={"WWW-Authenticate": 'Bearer authorization="{}", resource=foo'.format(url)}
+            )
+        ]
+        + [mock_response()] * 3,
+    )
+    pipeline = Pipeline(policies=[ChallengeAuthPolicy(credential=credential)], transport=transport)
+
+    for _ in range(2):
+        pipeline.run(HttpRequest("GET", url))
+        assert credential.get_token.call_count == 1
+
+    token = AccessToken(second_token, time.time() + 3600)
+    with patch("time.time", lambda: expires_on):
+        pipeline.run(HttpRequest("GET", url))
+    assert credential.get_token.call_count == 2
+
+
+@empty_challenge_cache
+def test_preserves_options_and_headers():
+    """After a challenge, the original request should be sent with its options and headers preserved.
+
+    If a policy mutates the options or headers of the challenge (unauthorized) request, the options of the service
+    request should be present when it is sent with authorization.
+    """
+
+    url = get_random_url()
+    token = "**"
+
+    def get_token(*_, **__):
+        return AccessToken(token, 0)
+
+    credential = Mock(get_token=Mock(wraps=get_token))
+
+    transport = validating_transport(
+        requests=[Request()] * 2 + [Request(required_headers={"Authorization": "Bearer " + token})],
+        responses=[
+            mock_response(
+                status_code=401, headers={"WWW-Authenticate": 'Bearer authorization="{}", resource=foo'.format(url)}
+            )
+        ]
+        + [mock_response()] * 2,
+    )
+    challenge_policy = ChallengeAuthPolicy(credential=credential)
+    policies = get_policies_for_request_mutation_test(challenge_policy)
+    pipeline = Pipeline(policies=policies, transport=transport)
+
+    response = pipeline.run(HttpRequest("GET", url))
+
+    # ensure the mock sans I/O policies were called
+    for policy in policies:
+        if hasattr(policy, "on_request"):
+            assert policy.on_request.called, "mock policy wasn't invoked"
+
+
+def get_policies_for_request_mutation_test(challenge_policy):
+    # create mock policies to add, remove, and verify an option and header
+    key = "foo"
+    value = "bar"
+    do_not_handle = lambda _: False
+
+    def add(request):
+        # add the expected option and header
+        request.context.options[key] = value
+        request.http_request.headers[key] = value
+
+    adder = Mock(spec_set=SansIOHTTPPolicy, on_request=Mock(wraps=add), on_exception=do_not_handle)
+
+    def remove(request):
+        # remove expected header and all options of unauthorized (challenge) requests
+        if not request.http_request.headers.get("Authorization"):
+            request.http_request.headers.pop(key, None)
+            request.context.options = {}
+
+    remover = Mock(spec_set=SansIOHTTPPolicy, on_request=Mock(wraps=remove), on_exception=do_not_handle)
+
+    def verify(request):
+        # authorized (non-challenge) requests should have the expected option and header
+        if request.http_request.headers.get("Authorization"):
+            assert request.context.options.get(key) == value, "request option not preserved across challenge"
+            assert request.http_request.headers.get(key) == value, "headers not preserved across challenge"
+
+    verifier = Mock(spec=SansIOHTTPPolicy, on_request=Mock(wraps=verify))
+
+    # Mutating the challenge request shouldn't affect the authorized request.
+    # This is the pipeline flow:
+    #  1. add option and header
+    #  2. challenge auth
+    #  3. remove option, header from unauthorized request
+    #  4. verify option, header on authorized request
+    return [adder, challenge_policy, remover, verifier]
