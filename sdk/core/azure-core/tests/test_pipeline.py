7b5691130ad10ab1a4906640fe042cbed77477bf
Xiang Yan <xiangsjtu@gmail.com>
2019-12-11 15:13:51 -0800

    retry on 429

--- sdk/core/azure-core/tests/test_pipeline.py
+++ sdk/core/azure-core/tests/test_pipeline.py
@@ -125,8 +125,8 @@ class TestRequestsTransport(unittest.TestCase):
     def test_retry_code_class_variables(self):
         retry_policy = RetryPolicy()
         assert retry_policy._RETRY_CODES is not None
-        assert len(retry_policy._RETRY_CODES) == 498
         assert 408 in retry_policy._RETRY_CODES
+        assert 429 in retry_policy._RETRY_CODES
         assert 501 not in retry_policy._RETRY_CODES
 
     def test_basic_requests_separate_session(self):
