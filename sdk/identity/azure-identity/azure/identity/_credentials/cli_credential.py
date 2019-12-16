4bcbe8a25def37cfbfdfdaf6cc133738d511122c
Ruijun Hu (Wicresoft North America Ltd) <v-ruih@microsoft.com>
2019-12-16 07:22:19 +0000

    exceptions handling

--- sdk/identity/azure-identity/azure/identity/_credentials/cli_credential.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/cli_credential.py
@@ -4,7 +4,7 @@
 # ------------------------------------
 import json
 from datetime import datetime
-from subprocess import run, PIPE
+from subprocess import run, PIPE, CalledProcessError
 
 from azure.core.credentials import AccessToken
 from azure.core.exceptions import ClientAuthenticationError
@@ -12,6 +12,7 @@ from azure.core.exceptions import ClientAuthenticationError
 class CliCredential(object):
 
     _DEFAULT_PREFIX = "/.default"
+    _CLI_NOT_INSTALLED_ERR = "Azure CLI not installed"
 
     def get_token(self, *scopes, **kwargs): # pylint:disable=unused-argument
         command = ['az', 'account', 'get-access-token']
@@ -20,10 +21,11 @@ class CliCredential(object):
             resource = scopes[0]
             if resource.endswith(self._DEFAULT_PREFIX):
                 resource = resource[:-len(self._DEFAULT_PREFIX)]
-            
+
             command.extend(['--resource', resource])
 
-        # TODO: Add cli installed check
+        self.__check_cli_installed()
+
         get_access_token = run(command, shell=True, stdout=PIPE, stderr=PIPE)
 
         get_access_token_stdout = get_access_token.stdout.decode("utf-8")
@@ -36,9 +38,21 @@ class CliCredential(object):
         access_token = get_access_token_object['accessToken']
         expires_on = int((
             datetime.strptime(
-                get_access_token_object['expiresOn'], '%Y-%m-%d %H:%M:%S.%f').utcnow() - datetime(1970, 1, 1)
+                self.__microseconds_parsed(get_access_token_object['expiresOn']), '%Y-%m-%d %H:%M:%S.%f').utcnow()
+                - datetime(1970, 1, 1)
             ).total_seconds())
 
-        # TODO: Make sure microseconds is parsed correctly
-
         return AccessToken(access_token, expires_on)
+
+    @classmethod
+    def __check_cli_installed(cls):
+        try:
+            run(['az'], shell=True, check=True)
+        except CalledProcessError as e:
+            raise ClientAuthenticationError(message=cls._CLI_NOT_INSTALLED_ERR)
+
+    @classmethod
+    def __microseconds_parsed(cls, timestamp):
+        _index = timestamp.index('.')
+        _microseconds = timestamp[_index + 1:]
+        return '.'.join([timestamp[:_index]] + ['{:06d}'.format(int(_microseconds))])
