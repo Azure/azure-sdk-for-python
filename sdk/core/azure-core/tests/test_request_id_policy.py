58e4d7c327e5883e97748e17ad4f9262bd4b4dec
Xiang Yan <xiangsjtu@gmail.com>
2019-12-17 14:25:39 -0800

    update test

--- sdk/core/azure-core/tests/test_request_id_policy.py
+++ sdk/core/azure-core/tests/test_request_id_policy.py
@@ -11,9 +11,9 @@ from itertools import product
 import pytest
 
 auto_request_id_values = (True, False, None)
-request_id_init_values = ("foo", None)
-request_id_set_values = ("bar", None)
-request_id_req_values = ("baz", None)
+request_id_init_values = ("foo", None, "_unset")
+request_id_set_values = ("bar", None, "_unset")
+request_id_req_values = ("baz", None, "_unset")
 full_combination = list(product(auto_request_id_values, request_id_init_values, request_id_set_values, request_id_req_values))
 
 @pytest.mark.parametrize("auto_request_id, request_id_init, request_id_set, request_id_req", full_combination)
@@ -22,23 +22,23 @@ def test_request_id_policy(auto_request_id, request_id_init, request_id_set, req
     kwargs = {}
     if auto_request_id is not None:
         kwargs['auto_request_id'] = auto_request_id
-    if request_id_init is not None:
+    if request_id_init != "_unset":
         kwargs['request_id'] = request_id_init
     request_id_policy = RequestIdPolicy(**kwargs)
-    if request_id_set is not None:
+    if request_id_set != "_unset":
         request_id_policy.set_request_id(request_id_set)
     request = HttpRequest('GET', 'http://127.0.0.1/')
     pipeline_request = PipelineRequest(request, PipelineContext(None))
-    if request_id_req is not None:
+    if request_id_req != "_unset":
         pipeline_request.context.options['request_id'] = request_id_req
     with patch('uuid.uuid1', return_value="VALUE"):
         request_id_policy.on_request(pipeline_request)
 
-    if request_id_req:
+    if request_id_req != "_unset":
         assert request.headers["x-ms-client-request-id"] == request_id_req
-    elif request_id_set:
+    elif request_id_set != "_unset":
         assert request.headers["x-ms-client-request-id"] == request_id_set
-    elif request_id_init:
+    elif request_id_init != "_unset":
         assert request.headers["x-ms-client-request-id"] == request_id_init
     elif auto_request_id or auto_request_id is None:
         assert request.headers["x-ms-client-request-id"] == "VALUE"
