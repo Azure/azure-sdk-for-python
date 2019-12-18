bb8a17b997743661033b0cd7114cd5a8452faf7f
xiafu <xiafu@microsoft.com>
2019-12-18 13:07:49 -0800

    fix recording

--- sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_file_client_async.py
+++ sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_file_client_async.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-
+# pylint: disable=too-many-lines
 import functools
 import time
 from io import BytesIO
