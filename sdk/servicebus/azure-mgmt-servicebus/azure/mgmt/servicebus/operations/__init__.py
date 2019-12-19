16100a18ffd646ae3eac2dae5c111b3f312fc400
SDK Automation <sdkautomation@microsoft.com>
2019-12-19 02:05:34 +0000

    Generated from e60813e0af04515d19704838de75ee6b2d27c741
    ServiceBus: Added Namespace properties for BYOK

--- sdk/servicebus/azure-mgmt-servicebus/azure/mgmt/servicebus/operations/__init__.py
+++ sdk/servicebus/azure-mgmt-servicebus/azure/mgmt/servicebus/operations/__init__.py
@@ -9,17 +9,17 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from .operations import Operations
-from .namespaces_operations import NamespacesOperations
-from .disaster_recovery_configs_operations import DisasterRecoveryConfigsOperations
-from .migration_configs_operations import MigrationConfigsOperations
-from .queues_operations import QueuesOperations
-from .topics_operations import TopicsOperations
-from .subscriptions_operations import SubscriptionsOperations
-from .rules_operations import RulesOperations
-from .regions_operations import RegionsOperations
-from .premium_messaging_regions_operations import PremiumMessagingRegionsOperations
-from .event_hubs_operations import EventHubsOperations
+from ._operations import Operations
+from ._namespaces_operations import NamespacesOperations
+from ._disaster_recovery_configs_operations import DisasterRecoveryConfigsOperations
+from ._migration_configs_operations import MigrationConfigsOperations
+from ._queues_operations import QueuesOperations
+from ._topics_operations import TopicsOperations
+from ._subscriptions_operations import SubscriptionsOperations
+from ._rules_operations import RulesOperations
+from ._regions_operations import RegionsOperations
+from ._premium_messaging_regions_operations import PremiumMessagingRegionsOperations
+from ._event_hubs_operations import EventHubsOperations
 
 __all__ = [
     'Operations',
