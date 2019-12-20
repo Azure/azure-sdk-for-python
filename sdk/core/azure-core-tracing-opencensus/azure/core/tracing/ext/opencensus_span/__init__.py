7ca3078dd199fa357a2f4c42c4e7f7c2f15b1eaf
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-12-20 09:49:49 -0800

    Map internal to Client in OpenCensus

--- sdk/core/azure-core-tracing-opencensus/azure/core/tracing/ext/opencensus_span/__init__.py
+++ sdk/core/azure-core-tracing-opencensus/azure/core/tracing/ext/opencensus_span/__init__.py
@@ -89,6 +89,7 @@ class OpenCensusSpan(HttpSpanMixin, object):
             OpenCensusSpanKind.CLIENT if value == SpanKind.PRODUCER else  # No producer in opencensus
             OpenCensusSpanKind.SERVER if value == SpanKind.SERVER else
             OpenCensusSpanKind.CLIENT if value == SpanKind.CONSUMER else  # No consumer in opencensus
+            OpenCensusSpanKind.CLIENT if value == SpanKind.INTERNAL else  # No consumer in opencensus
             OpenCensusSpanKind.UNSPECIFIED if value == SpanKind.UNSPECIFIED else
             None
         )
