9fe77f7af694e07950f2e7fea97944f7da41d5e4
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-12-26 10:17:09 -0800

    Need Python 3.x at least

--- sdk/core/azure-core-tracing-opentelemetry/setup.py
+++ sdk/core/azure-core-tracing-opentelemetry/setup.py
@@ -56,6 +56,7 @@ setup(
     packages=[
         'azure.core.tracing.ext.opentelemetry_span',
     ],
+    python_requires=">=3.5.0",
     install_requires=[
         'opentelemetry-api>=0.3a0',
         'azure-core<2.0.0,>=1.0.0',
