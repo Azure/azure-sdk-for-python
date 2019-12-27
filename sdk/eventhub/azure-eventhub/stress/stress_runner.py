6fd64d4162c16712747b5189404fef9fb82b0753
Yunhao Ling <adam_ling@outlook.com>
2019-12-27 12:08:16 -0800

    add license and remove unused import

--- sdk/eventhub/azure-eventhub/stress/stress_runner.py
+++ sdk/eventhub/azure-eventhub/stress/stress_runner.py
@@ -1,9 +1,13 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 import configparser
 from subprocess import Popen
 from typing import cast
 
 from azure.storage.blob import BlobServiceClient
-from azure.storage.blob import ContainerClient
 from azure.servicebus.control_client import ServiceBusService, EventHub
 
 
