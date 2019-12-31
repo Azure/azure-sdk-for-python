745c5d9c748922b40fc8ac9d383ad8a290a60999
SDK Automation <sdkautomation@microsoft.com>
2019-12-31 05:52:18 +0000

    Generated from 7251b6c23981c8f75e3494a12f12ddcca5db1b1c
    fixes in test scenario

--- sdk/mixedreality/azure-mgmt-mixedreality/azure/mgmt/mixedreality/operations/_mixed_reality_client_operations.py
+++ sdk/mixedreality/azure-mgmt-mixedreality/azure/mgmt/mixedreality/operations/_mixed_reality_client_operations.py
@@ -9,87 +9,16 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from msrest.service_client import SDKClient
-from msrest import Serializer, Deserializer
-from msrestazure import AzureConfiguration
-from .version import VERSION
 from msrest.pipeline import ClientRawResponse
+from .. import models
 import uuid
-from .operations.operations import Operations
-from .operations.spatial_anchors_accounts_operations import SpatialAnchorsAccountsOperations
-from . import models
 
 
-class MixedRealityClientConfiguration(AzureConfiguration):
-    """Configuration for MixedRealityClient
-    Note that all parameters used to create this instance are saved as instance
-    attributes.
-
-    :param credentials: Credentials needed for the client to connect to Azure.
-    :type credentials: :mod:`A msrestazure Credentials
-     object<msrestazure.azure_active_directory>`
-    :param subscription_id: Azure subscription ID.
-    :type subscription_id: str
-    :param str base_url: Service URL
-    """
-
-    def __init__(
-            self, credentials, subscription_id, base_url=None):
-
-        if credentials is None:
-            raise ValueError("Parameter 'credentials' must not be None.")
-        if subscription_id is None:
-            raise ValueError("Parameter 'subscription_id' must not be None.")
-        if not base_url:
-            base_url = 'https://management.azure.com'
-
-        super(MixedRealityClientConfiguration, self).__init__(base_url)
-
-        self.add_user_agent('azure-mgmt-mixedreality/{}'.format(VERSION))
-        self.add_user_agent('Azure-SDK-For-Python')
-
-        self.credentials = credentials
-        self.subscription_id = subscription_id
-
-
-class MixedRealityClient(SDKClient):
-    """Mixed Reality Client
-
-    :ivar config: Configuration for client.
-    :vartype config: MixedRealityClientConfiguration
-
-    :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.mixedreality.operations.Operations
-    :ivar spatial_anchors_accounts: SpatialAnchorsAccounts operations
-    :vartype spatial_anchors_accounts: azure.mgmt.mixedreality.operations.SpatialAnchorsAccountsOperations
-
-    :param credentials: Credentials needed for the client to connect to Azure.
-    :type credentials: :mod:`A msrestazure Credentials
-     object<msrestazure.azure_active_directory>`
-    :param subscription_id: Azure subscription ID.
-    :type subscription_id: str
-    :param str base_url: Service URL
-    """
-
-    def __init__(
-            self, credentials, subscription_id, base_url=None):
-
-        self.config = MixedRealityClientConfiguration(credentials, subscription_id, base_url)
-        super(MixedRealityClient, self).__init__(self.config.credentials, self.config)
-
-        client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self.api_version = '2019-02-28-preview'
-        self._serialize = Serializer(client_models)
-        self._deserialize = Deserializer(client_models)
-
-        self.operations = Operations(
-            self._client, self.config, self._serialize, self._deserialize)
-        self.spatial_anchors_accounts = SpatialAnchorsAccountsOperations(
-            self._client, self.config, self._serialize, self._deserialize)
+class MixedRealityClientOperationsMixin(object):
 
     def check_name_availability_local(
             self, location, name, type, custom_headers=None, raw=False, **operation_config):
-        """Check Name Availability for global uniqueness.
+        """Check Name Availability for local uniqueness.
 
         :param location: The location in which uniqueness will be verified.
         :type location: str
@@ -146,7 +75,6 @@ class MixedRealityClient(SDKClient):
             raise models.ErrorResponseException(self._deserialize, response)
 
         deserialized = None
-
         if response.status_code == 200:
             deserialized = self._deserialize('CheckNameAvailabilityResponse', response)
 
