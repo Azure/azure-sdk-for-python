9bafa107417f0158a80bcd83196e6c867e7969ed
yijxie <yijun.xie@micrsofot.com>
2020-01-03 14:40:29 -0800

    Make sure client is closed

--- sdk/eventhub/azure-eventhub/tests/livetest/asynctests/test_properties_async.py
+++ sdk/eventhub/azure-eventhub/tests/livetest/asynctests/test_properties_async.py
@@ -14,9 +14,10 @@ async def test_get_properties(live_eventhub):
     client = EventHubConsumerClient(live_eventhub['hostname'], live_eventhub['event_hub'], '$default',
         EventHubSharedKeyCredential(live_eventhub['key_name'], live_eventhub['access_key'])
     )
-    properties = await client.get_eventhub_properties()
-    assert properties['eventhub_name'] == live_eventhub['event_hub'] and properties['partition_ids'] == ['0', '1']
-    await client.close()
+    async with client:
+        properties = await client.get_eventhub_properties()
+        assert properties['eventhub_name'] == live_eventhub['event_hub'] and properties['partition_ids'] == ['0', '1']
+
 
 @pytest.mark.liveTest
 @pytest.mark.asyncio
@@ -24,9 +25,9 @@ async def test_get_partition_ids(live_eventhub):
     client = EventHubConsumerClient(live_eventhub['hostname'], live_eventhub['event_hub'], '$default',
         EventHubSharedKeyCredential(live_eventhub['key_name'], live_eventhub['access_key'])
     )
-    partition_ids = await client.get_partition_ids()
-    assert partition_ids == ['0', '1']
-    await client.close()
+    async with client:
+        partition_ids = await client.get_partition_ids()
+        assert partition_ids == ['0', '1']
 
 
 @pytest.mark.liveTest
@@ -35,12 +36,12 @@ async def test_get_partition_properties(live_eventhub):
     client = EventHubProducerClient(live_eventhub['hostname'], live_eventhub['event_hub'],
         EventHubSharedKeyCredential(live_eventhub['key_name'], live_eventhub['access_key'])
     )
-    properties = await client.get_partition_properties('0')
-    assert properties['eventhub_name'] == live_eventhub['event_hub'] \
-        and properties['id'] == '0' \
-        and 'beginning_sequence_number' in properties \
-        and 'last_enqueued_sequence_number' in properties \
-        and 'last_enqueued_offset' in properties \
-        and 'last_enqueued_time_utc' in properties \
-        and 'is_empty' in properties
-    await client.close()
+    async with client:
+        properties = await client.get_partition_properties('0')
+        assert properties['eventhub_name'] == live_eventhub['event_hub'] \
+            and properties['id'] == '0' \
+            and 'beginning_sequence_number' in properties \
+            and 'last_enqueued_sequence_number' in properties \
+            and 'last_enqueued_offset' in properties \
+            and 'last_enqueued_time_utc' in properties \
+            and 'is_empty' in properties
