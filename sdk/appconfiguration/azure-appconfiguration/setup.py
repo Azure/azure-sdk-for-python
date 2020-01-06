a938ab3c6b1922765b50a67a0828db9405f81ed2
azure-sdk <azuresdk@microsoft.com>
2020-01-06 22:08:04 +0000

    Increment package version after release of azure_appconfiguration

--- sdk/appconfiguration/azure-appconfiguration/setup.py
+++ sdk/appconfiguration/azure-appconfiguration/setup.py
@@ -73,7 +73,7 @@ setup(
     author_email='azpysdkhelp@microsoft.com',
     url='https://github.com/Azure/azure-sdk-for-python/tree/master/sdk/appconfiguration/azure-appconfiguration',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
