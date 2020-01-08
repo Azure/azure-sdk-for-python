fad06b131f2ef31a41fd39f8bfa2f603127d2426
KieranBrantnerMagee <kibrantn@microsoft.com>
2020-01-08 14:45:46 -0800

    Kibrantn/service bus preparer for tests final (#8003)
    * Create preparers for SB test resources.  
    * Fix some failing SB tests.  (Adjust magic number usage, fix naming)
    * Fix bug in servicebus common message.py that allows invalid kwargs to be passed into the message.

--- sdk/servicebus/azure-servicebus/tests/test_sb_client.py
+++ sdk/servicebus/azure-servicebus/tests/test_sb_client.py
@@ -12,6 +12,7 @@ import time
 from datetime import datetime, timedelta
 
 from azure.common import AzureHttpError, AzureConflictHttpError
+from azure.mgmt.servicebus.models import AccessRights
 from azure.servicebus import ServiceBusClient, QueueClient
 from azure.servicebus.common.message import Message, PeekMessage, BatchMessage
 from azure.servicebus.common.constants import ReceiveSettleMode
@@ -21,112 +22,150 @@ from azure.servicebus.common.errors import (
     ServiceBusAuthorizationError,
     ServiceBusResourceNotFound
 )
+from devtools_testutils import AzureMgmtTestCase, ResourceGroupPreparer
+from servicebus_preparer import (
+    ServiceBusNamespacePreparer, 
+    ServiceBusTopicPreparer, 
+    ServiceBusQueuePreparer,
+    ServiceBusNamespaceAuthorizationRulePreparer,
+    ServiceBusQueueAuthorizationRulePreparer
+)
 
-@pytest.mark.liveTest
-def test_sb_client_bad_credentials(live_servicebus_config, standard_queue):
-    client = ServiceBusClient(
-        service_namespace=live_servicebus_config['hostname'],
-        shared_access_key_name="invalid",
-        shared_access_key_value="invalid",
-        debug=False)
+class ServiceBusClientTests(AzureMgmtTestCase):
 
-    with pytest.raises(AzureHttpError):
-        client.get_queue(standard_queue)
+    @pytest.mark.liveTest
+    @ResourceGroupPreparer(name_prefix='servicebustest')
+    @ServiceBusNamespacePreparer(name_prefix='servicebustest')
+    @ServiceBusQueuePreparer(name_prefix='servicebustest', dead_lettering_on_message_expiration=True)
+    def test_sb_client_bad_credentials(self, servicebus_namespace, servicebus_queue, **kwargs):
 
-@pytest.mark.liveTest
-def test_sb_client_bad_namespace(live_servicebus_config):
-    
-    
-    client = ServiceBusClient(
-        service_namespace="invalid",
-        shared_access_key_name="invalid",
-        shared_access_key_value="invalid",
-        debug=False)
+        client = ServiceBusClient(
+            service_namespace=servicebus_namespace.name,
+            shared_access_key_name="invalid",
+            shared_access_key_value="invalid",
+            debug=False)
+        with pytest.raises(AzureHttpError):
+            client.get_queue(servicebus_queue.name)
 
-    with pytest.raises(ServiceBusConnectionError):
-        client.get_queue("testq")
+    @pytest.mark.liveTest
+    @ResourceGroupPreparer(name_prefix='servicebustest')
+    @ServiceBusNamespacePreparer(name_prefix='servicebustest')
+    def test_sb_client_bad_namespace(self, **kwargs):
 
-@pytest.mark.liveTest
-def test_sb_client_bad_entity(live_servicebus_config):
+        client = ServiceBusClient(
+            service_namespace="invalid",
+            shared_access_key_name="invalid",
+            shared_access_key_value="invalid",
+            debug=False)
 
-    client = ServiceBusClient(
-        service_namespace=live_servicebus_config['hostname'],
-        shared_access_key_name=live_servicebus_config['key_name'],
-        shared_access_key_value=live_servicebus_config['access_key'],
-        debug=False)
+        with pytest.raises(ServiceBusConnectionError):
+            client.get_queue("testq")
 
-    with pytest.raises(ServiceBusResourceNotFound):
-        client.get_queue("invalid")
+    @pytest.mark.liveTest
+    @ResourceGroupPreparer(name_prefix='servicebustest')
+    @ServiceBusNamespacePreparer(name_prefix='servicebustest')
+    def test_sb_client_bad_entity(self, servicebus_namespace, servicebus_namespace_key_name, servicebus_namespace_primary_key, **kwargs):
 
-    with pytest.raises(ServiceBusResourceNotFound):
-        client.get_topic("invalid")
+        client = ServiceBusClient(
+            service_namespace=servicebus_namespace.name,
+            shared_access_key_name=servicebus_namespace_key_name,
+            shared_access_key_value=servicebus_namespace_primary_key,
+            debug=False)
 
-@pytest.mark.liveTest
-def test_sb_client_entity_conflict(live_servicebus_config, standard_queue):
+        with pytest.raises(ServiceBusResourceNotFound):
+            client.get_queue("invalid")
 
-    client = ServiceBusClient(
-        service_namespace=live_servicebus_config['hostname'],
-        shared_access_key_name=live_servicebus_config['key_name'],
-        shared_access_key_value=live_servicebus_config['access_key'],
-        debug=False)
+        with pytest.raises(ServiceBusResourceNotFound):
+            client.get_topic("invalid")
 
-    with pytest.raises(AzureConflictHttpError):
-        client.create_queue(standard_queue)
+    @pytest.mark.liveTest
+    @ResourceGroupPreparer(name_prefix='servicebustest')
+    @ServiceBusNamespacePreparer(name_prefix='servicebustest')
+    @ServiceBusQueuePreparer(name_prefix='servicebustest', dead_lettering_on_message_expiration=True)
+    def test_sb_client_entity_conflict(self, servicebus_namespace, servicebus_namespace_key_name, servicebus_namespace_primary_key, servicebus_queue, **kwargs):
 
-    with pytest.raises(AzureConflictHttpError):
-        client.create_queue(standard_queue, lock_duration=300)
+        client = ServiceBusClient(
+            service_namespace=servicebus_namespace.name,
+            shared_access_key_name=servicebus_namespace_key_name,
+            shared_access_key_value=servicebus_namespace_primary_key,
+            debug=False)
 
-@pytest.mark.liveTest
-def test_sb_client_entity_delete(live_servicebus_config, standard_queue):
+        with pytest.raises(AzureConflictHttpError):
+            client.create_queue(servicebus_queue.name)
 
-    client = ServiceBusClient(
-        service_namespace=live_servicebus_config['hostname'],
-        shared_access_key_name=live_servicebus_config['key_name'],
-        shared_access_key_value=live_servicebus_config['access_key'],
-        debug=False)
+        with pytest.raises(AzureConflictHttpError):
+            client.create_queue(servicebus_queue.name, lock_duration=300)
 
-    with pytest.raises(ServiceBusResourceNotFound):
-        client.delete_queue("invalid", fail_not_exist=True)
+    @pytest.mark.liveTest
+    @ResourceGroupPreparer(name_prefix='servicebustest')
+    @ServiceBusNamespacePreparer(name_prefix='servicebustest')
+    @ServiceBusQueuePreparer(name_prefix='servicebustest', dead_lettering_on_message_expiration=True)
+    def test_sb_client_entity_delete(self, servicebus_namespace, servicebus_namespace_key_name, servicebus_namespace_primary_key, servicebus_queue, **kwargs):
 
-    client.delete_queue("invalid", fail_not_exist=False)
-    client.delete_queue(standard_queue)
+        client = ServiceBusClient(
+            service_namespace=servicebus_namespace.name,
+            shared_access_key_name=servicebus_namespace_key_name,
+            shared_access_key_value=servicebus_namespace_primary_key,
+            debug=False)
 
-@pytest.mark.liveTest
-def test_sb_client_readonly_credentials(servicebus_conn_str_readonly, standard_queue):
-    client = ServiceBusClient.from_connection_string(servicebus_conn_str_readonly, debug=False)
-    with pytest.raises(AzureHttpError):
-        client.get_queue(standard_queue)
+        with pytest.raises(ServiceBusResourceNotFound):
+            client.delete_queue("invalid", fail_not_exist=True)
 
-    client = QueueClient.from_connection_string(servicebus_conn_str_readonly, name=standard_queue)
-    with client.get_receiver(idle_timeout=5) as receiver:
-        messages = receiver.fetch_next()
+        client.delete_queue("invalid", fail_not_exist=False)
+        client.delete_queue(servicebus_queue.name)
 
-    with pytest.raises(ServiceBusAuthorizationError):
-        client.send(Message("test"))
+    @pytest.mark.liveTest
+    @ResourceGroupPreparer(name_prefix='servicebustest')
+    @ServiceBusNamespacePreparer(name_prefix='servicebustest')
+    @ServiceBusQueuePreparer(name_prefix='servicebustest', dead_lettering_on_message_expiration=True)
+    @ServiceBusNamespaceAuthorizationRulePreparer(name_prefix='servicebustest', access_rights=[AccessRights.listen])
+    def test_sb_client_readonly_credentials(self, servicebus_authorization_rule_connection_string, servicebus_queue, **kwargs):
+        client = ServiceBusClient.from_connection_string(servicebus_authorization_rule_connection_string, debug=False)
+        with pytest.raises(AzureHttpError):
+            client.get_queue(servicebus_queue.name)
 
-@pytest.mark.liveTest
-def test_sb_client_writeonly_credentials(servicebus_conn_str_writeonly, standard_queue):
-    client = ServiceBusClient.from_connection_string(servicebus_conn_str_writeonly)
-    with pytest.raises(AzureHttpError):
-        client.get_queue(standard_queue)
-
-    client = QueueClient.from_connection_string(servicebus_conn_str_writeonly, name=standard_queue, debug=False)
-    with pytest.raises(ServiceBusAuthorizationError):
+        client = QueueClient.from_connection_string(servicebus_authorization_rule_connection_string, name=servicebus_queue.name)
         with client.get_receiver(idle_timeout=5) as receiver:
             messages = receiver.fetch_next()
 
-    client.send([Message("test1"), Message("test2")])
-    with pytest.raises(TypeError):
-        client.send("cat")
-    with pytest.raises(TypeError):
-        client.send(1234)
-    with pytest.raises(TypeError):
-        client.send([1,2,3])
-    with pytest.raises(TypeError):
-        client.send([Message("test1"), "test2"])
+        with pytest.raises(ServiceBusAuthorizationError):
+            client.send(Message("test"))
 
-@pytest.mark.liveTest
-def test_sb_client_wrong_conn_str(queue_servicebus_conn_str, standard_queue):
-    client = ServiceBusClient.from_connection_string(queue_servicebus_conn_str)
-    with pytest.raises(AzureHttpError):
-        client.get_queue(standard_queue)
+    @pytest.mark.liveTest
+    @ResourceGroupPreparer(name_prefix='servicebustest')
+    @ServiceBusNamespacePreparer(name_prefix='servicebustest')
+    @ServiceBusQueuePreparer(name_prefix='servicebustest', dead_lettering_on_message_expiration=True)
+    @ServiceBusNamespaceAuthorizationRulePreparer(name_prefix='servicebustest', access_rights=[AccessRights.send])
+    def test_sb_client_writeonly_credentials(self, servicebus_authorization_rule_connection_string, servicebus_queue, **kwargs):
+        client = ServiceBusClient.from_connection_string(servicebus_authorization_rule_connection_string)
+        with pytest.raises(AzureHttpError):
+            client.get_queue(servicebus_queue.name)
+
+        client = QueueClient.from_connection_string(servicebus_authorization_rule_connection_string, name=servicebus_queue.name, debug=False)
+        with pytest.raises(ServiceBusAuthorizationError):
+            with client.get_receiver(idle_timeout=5) as receiver:
+                messages = receiver.fetch_next()
+
+        client.send([Message("test1"), Message("test2")])
+        with pytest.raises(TypeError):
+            client.send("cat")
+        with pytest.raises(TypeError):
+            client.send(1234)
+        with pytest.raises(TypeError):
+            client.send([1,2,3])
+        with pytest.raises(TypeError):
+            client.send([Message("test1"), "test2"])
+
+
+    @pytest.mark.liveTest
+    @ResourceGroupPreparer(name_prefix='servicebustest')
+    @ServiceBusNamespacePreparer(name_prefix='servicebustest')
+    @ServiceBusNamespaceAuthorizationRulePreparer(name_prefix='servicebustest')
+    @ServiceBusQueuePreparer(name_prefix='servicebustest_queue_one', parameter_name='wrong_queue', dead_lettering_on_message_expiration=True)
+    @ServiceBusQueuePreparer(name_prefix='servicebustest_queue_two', dead_lettering_on_message_expiration=True)
+    @ServiceBusQueueAuthorizationRulePreparer(name_prefix='servicebustest_queue_two')
+    def test_sb_client_incorrect_queue_conn_str(self, servicebus_queue_authorization_rule_connection_string, wrong_queue, **kwargs):
+        
+        client = ServiceBusClient.from_connection_string(servicebus_queue_authorization_rule_connection_string)
+        with pytest.raises(AzureHttpError):
+            client.get_queue(wrong_queue.name)
