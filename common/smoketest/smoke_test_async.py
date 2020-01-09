1eccd0325514920a198a510d987706f8d0c8d69e
Daniel Jurek <djurek@microsoft.com>
2020-01-09 13:53:45 -0800

    smoke tests: put executing code into a function and call the funciton instead of importing it directly

--- common/smoketest/smoke_test_async.py
+++ common/smoketest/smoke_test_async.py
@@ -9,19 +9,21 @@ from key_vault_secrets_async import KeyVaultSecrets
 from event_hubs_async import EventHubAsync
 from storage_blob_async import StorageBlobAsync
 
-print("")
-print("==========================================")
-print("   AZURE TRACK 2 SDKs SMOKE TEST ASYNC")
-print("==========================================")
+
+def execute_async_smoke_tests:
+    print("")
+    print("==========================================")
+    print("   AZURE TRACK 2 SDKs SMOKE TEST ASYNC")
+    print("==========================================")
 
 
-async def main():
-    await KeyVaultCertificates().run()
-    await KeyVaultKeys().run()
-    await KeyVaultSecrets().run()
-    await EventHubAsync().run()
-    await StorageBlobAsync().run()
+    async def main():
+        await KeyVaultCertificates().run()
+        await KeyVaultKeys().run()
+        await KeyVaultSecrets().run()
+        await EventHubAsync().run()
+        await StorageBlobAsync().run()
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
