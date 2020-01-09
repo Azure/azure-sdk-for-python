10e6e9282b265aa09a4162afb55df8e76225d95b
Zim Kalinowski <zikalino@microsoft.com>
2020-01-09 08:29:05 +0800

    Manually generate iothub (#9378)
    * multi-api package
    
    * history and version
    
    * Packaging update of azure-mgmt-iothub
    
    * rerecorded tests
    
    Co-authored-by: Azure SDK Bot <adxpysdk@microsoft.com>

--- sdk/iothub/azure-mgmt-iothub/azure/mgmt/iothub/v2019_11_04/__init__.py
+++ sdk/iothub/azure-mgmt-iothub/azure/mgmt/iothub/v2019_11_04/__init__.py
@@ -9,5 +9,11 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "0.10.0"
+from ._configuration import IotHubClientConfiguration
+from ._iot_hub_client import IotHubClient
+__all__ = ['IotHubClient', 'IotHubClientConfiguration']
+
+from .version import VERSION
+
+__version__ = VERSION
 
--- sdk/iothub/azure-mgmt-iothub/azure/mgmt/iothub/v2019_11_04/operations/__init__.py
+++ sdk/iothub/azure-mgmt-iothub/azure/mgmt/iothub/v2019_11_04/operations/__init__.py
@@ -9,5 +9,16 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "0.10.0"
+from ._operations import Operations
+from ._iot_hub_resource_operations import IotHubResourceOperations
+from ._resource_provider_common_operations import ResourceProviderCommonOperations
+from ._certificates_operations import CertificatesOperations
+from ._iot_hub_operations import IotHubOperations
 
+__all__ = [
+    'Operations',
+    'IotHubResourceOperations',
+    'ResourceProviderCommonOperations',
+    'CertificatesOperations',
+    'IotHubOperations',
+]
