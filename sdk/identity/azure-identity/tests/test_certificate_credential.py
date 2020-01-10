a43058b3b9de657ecf29f768cea8f8fd659efcc1
Charles Lowell <chlowe@microsoft.com>
2020-01-10 09:47:23 -0800

    offline tests

--- sdk/identity/azure-identity/tests/test_certificate_credential.py
+++ sdk/identity/azure-identity/tests/test_certificate_credential.py
@@ -12,17 +12,21 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
+import pytest
 from six.moves.urllib_parse import urlparse
 
 from helpers import build_aad_response, urlsafeb64_decode, mock_response, Request, validating_transport
 
 try:
-    from unittest.mock import Mock, patch
+    from unittest.mock import Mock
 except ImportError:  # python < 3.3
-    from mock import Mock, patch  # type: ignore
+    from mock import Mock  # type: ignore
 
 CERT_PATH = os.path.join(os.path.dirname(__file__), "certificate.pem")
 CERT_WITH_PASSWORD_PATH = os.path.join(os.path.dirname(__file__), "certificate-with-password.pem")
+CERT_PASSWORD = "password"
+BOTH_CERTS = ((CERT_PATH, None), (CERT_WITH_PASSWORD_PATH, CERT_PASSWORD))
+
 
 def test_policies_configurable():
     policy = Mock(spec_set=SansIOHTTPPolicy, on_request=Mock())
@@ -50,7 +54,8 @@ def test_user_agent():
     credential.get_token("scope")
 
 
-def test_request_url():
+@pytest.mark.parametrize("cert_path,cert_password", BOTH_CERTS)
+def test_request_url(cert_path, cert_password):
     authority = "authority.com"
     tenant_id = "expected_tenant"
     access_token = "***"
@@ -65,13 +70,15 @@ def test_request_url():
         validate_url(request.url)
         return mock_response(json_payload={"token_type": "Bearer", "expires_in": 42, "access_token": access_token})
 
-    for cert_path, password in ((CERT_PATH, None), (CERT_WITH_PASSWORD_PATH, 'password')):
-        cred = CertificateCredential(tenant_id, "client_id", cert_path, password=password, transport=Mock(send=mock_send), authority=authority)
-        token = cred.get_token("scope")
-        assert token.token == access_token
+    cred = CertificateCredential(
+        tenant_id, "client-id", cert_path, password=cert_password, transport=Mock(send=mock_send), authority=authority
+    )
+    token = cred.get_token("scope")
+    assert token.token == access_token
 
 
-def test_request_body():
+@pytest.mark.parametrize("cert_path,cert_password", BOTH_CERTS)
+def test_request_body(cert_path, cert_password):
     access_token = "***"
     authority = "authority.com"
     client_id = "client-id"
@@ -82,14 +89,15 @@ def test_request_body():
         assert request.body["grant_type"] == "client_credentials"
         assert request.body["scope"] == expected_scope
 
-        with open(CERT_PATH, "rb") as cert_file:
+        with open(cert_path, "rb") as cert_file:
             validate_jwt(request, client_id, cert_file.read())
 
         return mock_response(json_payload={"token_type": "Bearer", "expires_in": 42, "access_token": access_token})
 
-    cred = CertificateCredential(tenant_id, client_id, CERT_PATH, transport=Mock(send=mock_send), authority=authority)
+    cred = CertificateCredential(
+        tenant_id, client_id, cert_path, password=cert_password, transport=Mock(send=mock_send), authority=authority
+    )
     token = cred.get_token(expected_scope)
-
     assert token.token == access_token
 
 
