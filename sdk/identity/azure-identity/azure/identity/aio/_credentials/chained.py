a3cb1a49e167a18d2cdc8d81c437ae0fe9c8bc70
Charles Lowell <chlowe@microsoft.com>
2020-01-10 11:32:47 -0800

    ChainedTokenCredential needs its own init now

--- sdk/identity/azure-identity/azure/identity/aio/_credentials/chained.py
+++ sdk/identity/azure-identity/azure/identity/aio/_credentials/chained.py
@@ -6,30 +6,36 @@ import asyncio
 from typing import TYPE_CHECKING
 
 from azure.core.exceptions import ClientAuthenticationError
-from ... import ChainedTokenCredential as SyncChainedTokenCredential
 from .base import AsyncCredentialBase
+from ..._credentials.chained import _get_error_message
 
 if TYPE_CHECKING:
     from typing import Any
     from azure.core.credentials import AccessToken
+    from azure.core.credentials_async import AsyncTokenCredential
 
 
-class ChainedTokenCredential(SyncChainedTokenCredential, AsyncCredentialBase):
+class ChainedTokenCredential(AsyncCredentialBase):
     """A sequence of credentials that is itself a credential.
 
     Its :func:`get_token` method calls ``get_token`` on each credential in the sequence, in order, returning the first
     valid token received.
 
     :param credentials: credential instances to form the chain
-    :type credentials: :class:`azure.core.credentials.TokenCredential`
+    :type credentials: :class:`azure.core.credentials.AsyncTokenCredential`
     """
+
+    def __init__(self, *credentials: "AsyncTokenCredential") -> None:
+        if not credentials:
+            raise ValueError("at least one credential is required")
+        self.credentials = credentials
 
     async def close(self):
         """Close the transport sessions of all credentials in the chain."""
 
         await asyncio.gather(*(credential.close() for credential in self.credentials))
 
-    async def get_token(self, *scopes: str, **kwargs: "Any") -> "AccessToken":  # pylint:disable=unused-argument
+    async def get_token(self, *scopes: str, **kwargs: "Any") -> "AccessToken":
         """Asynchronously request a token from each credential, in order, returning the first token received.
 
         If no credential provides a token, raises :class:`azure.core.exceptions.ClientAuthenticationError`
@@ -48,5 +54,5 @@ class ChainedTokenCredential(SyncChainedTokenCredential, AsyncCredentialBase):
                 history.append((credential, ex.message))
             except Exception as ex:  # pylint: disable=broad-except
                 history.append((credential, str(ex)))
-        error_message = self._get_error_message(history)
+        error_message = _get_error_message(history)
         raise ClientAuthenticationError(message=error_message)
