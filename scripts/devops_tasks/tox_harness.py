138299afe2590b626c91086c1bed92fc66e505d6
scbedd <45376673+scbedd@users.noreply.github.com>
2020-01-10 18:37:39 -0800

    disabling parallel spinner

--- scripts/devops_tasks/tox_harness.py
+++ scripts/devops_tasks/tox_harness.py
@@ -31,7 +31,7 @@ coverage_dir = os.path.join(root_dir, "_coverage/")
 pool_size = multiprocessing.cpu_count() * 2
 DEFAULT_TOX_INI_LOCATION = os.path.join(root_dir, "eng/tox/tox.ini")
 IGNORED_TOX_INIS = ["azure-cosmos"]
-
+CI_LOGGING_COLLECTION_ENVS = ["whl", "sdist", "depends"]
 
 class ToxWorkItem:
     def __init__(self, target_package_path, tox_env, options_array):
@@ -59,7 +59,7 @@ class Worker(Thread):
 
 
 def in_ci():
-    return os.getenv("TF_BUILD", False)
+    return True #os.getenv("TF_BUILD", False)
 
 
 class ThreadPool:
@@ -99,7 +99,6 @@ def combine_coverage_files(targeted_packages):
         # this combine step is required only for modules if report has package name starts with .tox
         logging.error("tox.ini is not found in path {}".format(root_dir))
 
-
 def collect_tox_coverage_files(targeted_packages):
     root_coverage_dir = os.path.join(root_dir, "_coverage/")
 
@@ -133,6 +132,19 @@ def collect_tox_coverage_files(targeted_packages):
         dest = os.path.join(root_dir, ".coverage")
 
         shutil.move(source, dest)
+
+# def collect_tox_logs(targeted_packages):
+#     root_log_dir = os.path.join(root_dir, "_logs/")
+
+#     # generate coverage files
+#     for package_dir in [package for package in targeted_packages]:
+#         logdirs = [os.path.join(package_dir, ".tox", env) for env in CI_LOGGING_COLLECTION_ENVS]
+#         collection_dir = os.path.join(root_log_dir, package_dir)
+
+#         for logdir in logdirs:
+#             logging.info("Moving logdir {} to collection dir {}".format(logdir, collection_dir))
+#             if os.path.exists(logdir):
+#                 shutil.copytree(logdir, collection_dir)
 
 
 def individual_workload(tox_command_tuple, workload_results):
@@ -282,6 +294,7 @@ def prep_and_run_tox(targeted_packages, parsed_args, options_array=[]):
 
         if in_ci():
             replace_dev_reqs(destination_dev_req)
+            os.environ["TOX_PARALLEL_NO_SPINNER"] = "1"
 
         if parsed_args.tox_env:
             tox_execution_array.extend(["-e", parsed_args.tox_env])
@@ -301,3 +314,7 @@ def prep_and_run_tox(targeted_packages, parsed_args, options_array=[]):
 
     if not parsed_args.disablecov:
         collect_tox_coverage_files(targeted_packages)
+
+    # if in_ci():
+    #     collect_tox_logs(targeted_packages)
+
