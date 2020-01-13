772e3a5b2034f135259cef2e2127233c103e308b
Laurent Mazuel <laurent.mazuel@gmail.com>
2020-01-13 15:35:46 -0800

    OpenTelemetry plugin (#7703)
    * Introducing change_context to replace set_current_span
    
    * OpenTelemetry plugin:first pass
    
    * Plug OpenTelemetry in azure-core
    
    * Restore proper settings and tracing tests in azure-core
    
    * Create HttpSpanMixin
    
    * Need opentelemetry-sdk for testing
    
    * WIP core test
    
    * Use stable azure-core
    
    * Support Python 3.8
    
    * OT Readme
    
    * Do not include YAML
    
    * Dependency update
    
    * Update dep
    
    * Add decorator tests in azure-core
    
    * Remove OpenCensus specific tests
    
    * Remove useless OpenCensus tests
    
    * OT testing
    
    * Versionning
    
    * Fix Link for OT
    
    * Fix setup.py
    
    * pylint
    
    * Dependency work
    
    * pylint
    
    * Need Python 3.x at least
    
    * Remove diff with master
    
    * Remove core changes
    
    * Feedback
    
    * Clarify tests

---
+++ sdk/core/azure-core-tracing-opentelemetry/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+#--------------------------------------------------------------------------
+
+import re
+import os.path
+from io import open
+from setuptools import find_packages, setup  # type: ignore
+
+# Change the PACKAGE_NAME only to change folder and different name
+PACKAGE_NAME = "azure-core-tracing-opentelemetry"
+PACKAGE_PPRINT_NAME = "Azure Core OpenTelemetry plugin"
+
+package_folder_path = "azure/core/tracing/ext/opentelemetry_span"
+
+# Version extraction inspired from 'requests'
+with open(os.path.join(package_folder_path, '_version.py'), 'r') as fd:
+    version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]',  # type: ignore
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+with open('HISTORY.md', encoding='utf-8') as f:
+    history = f.read()
+
+setup(
+    name=PACKAGE_NAME,
+    version=version,
+    description='Microsoft Azure {} Library for Python'.format(PACKAGE_PPRINT_NAME),
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    author='Microsoft Corporation',
+    author_email='azpysdkhelp@microsoft.com',
+    url='https://github.com/Azure/azure-sdk-for-python/tree/master/sdk/core/azure-core-tracing-opentelemetry',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+    ],
+    zip_safe=False,
+    packages=[
+        'azure.core.tracing.ext.opentelemetry_span',
+    ],
+    python_requires=">=3.5.0",
+    install_requires=[
+        'opentelemetry-api>=0.3a0',
+        'azure-core<2.0.0,>=1.0.0',
+    ],
+    extras_require={
+        ":python_version<'3.5'": ['typing'],
+    }
+)
