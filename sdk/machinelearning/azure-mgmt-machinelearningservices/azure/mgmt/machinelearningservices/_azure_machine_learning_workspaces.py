e1bfc5d5b3a425fae88cc70b8ad099656e0909e2
SDK Automation <sdkautomation@microsoft.com>
2020-01-14 06:44:48 +0000

    Generated from 465c1c1a3d0c5505620750d02f639bdd31a4223f
    made encryption.status as required property

--- sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/_azure_machine_learning_workspaces.py
+++ sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/_azure_machine_learning_workspaces.py
@@ -13,15 +13,20 @@ from msrest.service_client import SDKClient
 from msrest import Serializer, Deserializer
 
 from ._configuration import AzureMachineLearningWorkspacesConfiguration
+from .operations import AzureMachineLearningWorkspacesOperationsMixin
 from .operations import Operations
 from .operations import WorkspacesOperations
+from .operations import WorkspaceFeaturesOperations
 from .operations import UsagesOperations
 from .operations import VirtualMachineSizesOperations
+from .operations import QuotasOperations
 from .operations import MachineLearningComputeOperations
+from .operations import PrivateEndpointConnectionsOperations
+from .operations import PrivateLinkResourcesOperations
 from . import models
 
 
-class AzureMachineLearningWorkspaces(SDKClient):
+class AzureMachineLearningWorkspaces(AzureMachineLearningWorkspacesOperationsMixin, SDKClient):
     """These APIs allow end users to operate on Azure Machine Learning Workspace resources.
 
     :ivar config: Configuration for client.
@@ -31,12 +36,20 @@ class AzureMachineLearningWorkspaces(SDKClient):
     :vartype operations: azure.mgmt.machinelearningservices.operations.Operations
     :ivar workspaces: Workspaces operations
     :vartype workspaces: azure.mgmt.machinelearningservices.operations.WorkspacesOperations
+    :ivar workspace_features: WorkspaceFeatures operations
+    :vartype workspace_features: azure.mgmt.machinelearningservices.operations.WorkspaceFeaturesOperations
     :ivar usages: Usages operations
     :vartype usages: azure.mgmt.machinelearningservices.operations.UsagesOperations
     :ivar virtual_machine_sizes: VirtualMachineSizes operations
     :vartype virtual_machine_sizes: azure.mgmt.machinelearningservices.operations.VirtualMachineSizesOperations
+    :ivar quotas: Quotas operations
+    :vartype quotas: azure.mgmt.machinelearningservices.operations.QuotasOperations
     :ivar machine_learning_compute: MachineLearningCompute operations
     :vartype machine_learning_compute: azure.mgmt.machinelearningservices.operations.MachineLearningComputeOperations
+    :ivar private_endpoint_connections: PrivateEndpointConnections operations
+    :vartype private_endpoint_connections: azure.mgmt.machinelearningservices.operations.PrivateEndpointConnectionsOperations
+    :ivar private_link_resources: PrivateLinkResources operations
+    :vartype private_link_resources: azure.mgmt.machinelearningservices.operations.PrivateLinkResourcesOperations
 
     :param credentials: Credentials needed for the client to connect to Azure.
     :type credentials: :mod:`A msrestazure Credentials
@@ -53,7 +66,7 @@ class AzureMachineLearningWorkspaces(SDKClient):
         super(AzureMachineLearningWorkspaces, self).__init__(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self.api_version = '2019-05-01'
+        self.api_version = '2020-01-01'
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
@@ -61,9 +74,17 @@ class AzureMachineLearningWorkspaces(SDKClient):
             self._client, self.config, self._serialize, self._deserialize)
         self.workspaces = WorkspacesOperations(
             self._client, self.config, self._serialize, self._deserialize)
+        self.workspace_features = WorkspaceFeaturesOperations(
+            self._client, self.config, self._serialize, self._deserialize)
         self.usages = UsagesOperations(
             self._client, self.config, self._serialize, self._deserialize)
         self.virtual_machine_sizes = VirtualMachineSizesOperations(
             self._client, self.config, self._serialize, self._deserialize)
+        self.quotas = QuotasOperations(
+            self._client, self.config, self._serialize, self._deserialize)
         self.machine_learning_compute = MachineLearningComputeOperations(
+            self._client, self.config, self._serialize, self._deserialize)
+        self.private_endpoint_connections = PrivateEndpointConnectionsOperations(
+            self._client, self.config, self._serialize, self._deserialize)
+        self.private_link_resources = PrivateLinkResourcesOperations(
             self._client, self.config, self._serialize, self._deserialize)
--- sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/models/__init__.py
+++ sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/models/__init__.py
@@ -18,6 +18,7 @@ try:
     from ._models_py3 import AmlComputeNodeInformation
     from ._models_py3 import AmlComputeNodesInformation
     from ._models_py3 import AmlComputeProperties
+    from ._models_py3 import AmlUserFeature
     from ._models_py3 import ClusterUpdateParameters
     from ._models_py3 import Compute
     from ._models_py3 import ComputeNodesInformation
@@ -29,24 +30,42 @@ try:
     from ._models_py3 import DataFactory
     from ._models_py3 import DataLakeAnalytics
     from ._models_py3 import DataLakeAnalyticsProperties
+    from ._models_py3 import EncryptionProperty
     from ._models_py3 import ErrorDetail
-    from ._models_py3 import ErrorResponse
+    from ._models_py3 import ErrorResponse, ErrorResponseException
     from ._models_py3 import HDInsight
     from ._models_py3 import HDInsightProperties
     from ._models_py3 import Identity
+    from ._models_py3 import KeyVaultProperties
     from ._models_py3 import ListWorkspaceKeysResult
     from ._models_py3 import MachineLearningServiceError, MachineLearningServiceErrorException
     from ._models_py3 import NodeStateCounts
     from ._models_py3 import Operation
     from ._models_py3 import OperationDisplay
     from ._models_py3 import Password
+    from ._models_py3 import PrivateEndpoint
+    from ._models_py3 import PrivateEndpointConnection
+    from ._models_py3 import PrivateLinkResource
+    from ._models_py3 import PrivateLinkResourceListResult
+    from ._models_py3 import PrivateLinkServiceConnectionState
+    from ._models_py3 import QuotaBaseProperties
+    from ._models_py3 import QuotaUpdateParameters
     from ._models_py3 import RegistryListCredentialsResult
     from ._models_py3 import Resource
     from ._models_py3 import ResourceId
+    from ._models_py3 import ResourceName
+    from ._models_py3 import ResourceQuota
+    from ._models_py3 import ResourceSkuLocationInfo
+    from ._models_py3 import ResourceSkuZoneDetails
+    from ._models_py3 import Restriction
     from ._models_py3 import ScaleSettings
     from ._models_py3 import ServicePrincipalCredentials
+    from ._models_py3 import Sku
+    from ._models_py3 import SKUCapability
     from ._models_py3 import SslConfiguration
     from ._models_py3 import SystemService
+    from ._models_py3 import UpdateWorkspaceQuotas
+    from ._models_py3 import UpdateWorkspaceQuotasResult
     from ._models_py3 import Usage
     from ._models_py3 import UsageName
     from ._models_py3 import UserAccountCredentials
@@ -57,6 +76,7 @@ try:
     from ._models_py3 import VirtualMachineSizeListResult
     from ._models_py3 import VirtualMachineSshCredentials
     from ._models_py3 import Workspace
+    from ._models_py3 import WorkspaceSku
     from ._models_py3 import WorkspaceUpdateParameters
 except (SyntaxError, ImportError):
     from ._models import AKS
@@ -67,6 +87,7 @@ except (SyntaxError, ImportError):
     from ._models import AmlComputeNodeInformation
     from ._models import AmlComputeNodesInformation
     from ._models import AmlComputeProperties
+    from ._models import AmlUserFeature
     from ._models import ClusterUpdateParameters
     from ._models import Compute
     from ._models import ComputeNodesInformation
@@ -78,24 +99,42 @@ except (SyntaxError, ImportError):
     from ._models import DataFactory
     from ._models import DataLakeAnalytics
     from ._models import DataLakeAnalyticsProperties
+    from ._models import EncryptionProperty
     from ._models import ErrorDetail
-    from ._models import ErrorResponse
+    from ._models import ErrorResponse, ErrorResponseException
     from ._models import HDInsight
     from ._models import HDInsightProperties
     from ._models import Identity
+    from ._models import KeyVaultProperties
     from ._models import ListWorkspaceKeysResult
     from ._models import MachineLearningServiceError, MachineLearningServiceErrorException
     from ._models import NodeStateCounts
     from ._models import Operation
     from ._models import OperationDisplay
     from ._models import Password
+    from ._models import PrivateEndpoint
+    from ._models import PrivateEndpointConnection
+    from ._models import PrivateLinkResource
+    from ._models import PrivateLinkResourceListResult
+    from ._models import PrivateLinkServiceConnectionState
+    from ._models import QuotaBaseProperties
+    from ._models import QuotaUpdateParameters
     from ._models import RegistryListCredentialsResult
     from ._models import Resource
     from ._models import ResourceId
+    from ._models import ResourceName
+    from ._models import ResourceQuota
+    from ._models import ResourceSkuLocationInfo
+    from ._models import ResourceSkuZoneDetails
+    from ._models import Restriction
     from ._models import ScaleSettings
     from ._models import ServicePrincipalCredentials
+    from ._models import Sku
+    from ._models import SKUCapability
     from ._models import SslConfiguration
     from ._models import SystemService
+    from ._models import UpdateWorkspaceQuotas
+    from ._models import UpdateWorkspaceQuotasResult
     from ._models import Usage
     from ._models import UsageName
     from ._models import UserAccountCredentials
@@ -106,18 +145,30 @@ except (SyntaxError, ImportError):
     from ._models import VirtualMachineSizeListResult
     from ._models import VirtualMachineSshCredentials
     from ._models import Workspace
+    from ._models import WorkspaceSku
     from ._models import WorkspaceUpdateParameters
+from ._paged_models import AmlUserFeaturePaged
 from ._paged_models import ComputeResourcePaged
 from ._paged_models import OperationPaged
+from ._paged_models import ResourceQuotaPaged
 from ._paged_models import UsagePaged
 from ._paged_models import WorkspacePaged
+from ._paged_models import WorkspaceSkuPaged
 from ._azure_machine_learning_workspaces_enums import (
     ProvisioningState,
+    EncryptionStatus,
     UsageUnit,
+    QuotaUnit,
+    Status,
     ResourceIdentityType,
     VmPriority,
+    RemoteLoginPortPublicAccess,
     AllocationState,
+    NodeState,
     ComputeType,
+    ReasonCode,
+    PrivateEndpointServiceConnectionStatus,
+    PrivateEndpointConnectionProvisioningState,
     UnderlyingResourceAction,
 )
 
@@ -130,6 +181,7 @@ __all__ = [
     'AmlComputeNodeInformation',
     'AmlComputeNodesInformation',
     'AmlComputeProperties',
+    'AmlUserFeature',
     'ClusterUpdateParameters',
     'Compute',
     'ComputeNodesInformation',
@@ -141,24 +193,42 @@ __all__ = [
     'DataFactory',
     'DataLakeAnalytics',
     'DataLakeAnalyticsProperties',
+    'EncryptionProperty',
     'ErrorDetail',
-    'ErrorResponse',
+    'ErrorResponse', 'ErrorResponseException',
     'HDInsight',
     'HDInsightProperties',
     'Identity',
+    'KeyVaultProperties',
     'ListWorkspaceKeysResult',
     'MachineLearningServiceError', 'MachineLearningServiceErrorException',
     'NodeStateCounts',
     'Operation',
     'OperationDisplay',
     'Password',
+    'PrivateEndpoint',
+    'PrivateEndpointConnection',
+    'PrivateLinkResource',
+    'PrivateLinkResourceListResult',
+    'PrivateLinkServiceConnectionState',
+    'QuotaBaseProperties',
+    'QuotaUpdateParameters',
     'RegistryListCredentialsResult',
     'Resource',
     'ResourceId',
+    'ResourceName',
+    'ResourceQuota',
+    'ResourceSkuLocationInfo',
+    'ResourceSkuZoneDetails',
+    'Restriction',
     'ScaleSettings',
     'ServicePrincipalCredentials',
+    'Sku',
+    'SKUCapability',
     'SslConfiguration',
     'SystemService',
+    'UpdateWorkspaceQuotas',
+    'UpdateWorkspaceQuotasResult',
     'Usage',
     'UsageName',
     'UserAccountCredentials',
@@ -169,16 +239,28 @@ __all__ = [
     'VirtualMachineSizeListResult',
     'VirtualMachineSshCredentials',
     'Workspace',
+    'WorkspaceSku',
     'WorkspaceUpdateParameters',
     'OperationPaged',
     'WorkspacePaged',
+    'AmlUserFeaturePaged',
     'UsagePaged',
+    'ResourceQuotaPaged',
     'ComputeResourcePaged',
+    'WorkspaceSkuPaged',
     'ProvisioningState',
+    'EncryptionStatus',
     'UsageUnit',
+    'QuotaUnit',
+    'Status',
     'ResourceIdentityType',
     'VmPriority',
+    'RemoteLoginPortPublicAccess',
     'AllocationState',
+    'NodeState',
     'ComputeType',
+    'ReasonCode',
+    'PrivateEndpointServiceConnectionStatus',
+    'PrivateEndpointConnectionProvisioningState',
     'UnderlyingResourceAction',
 ]
