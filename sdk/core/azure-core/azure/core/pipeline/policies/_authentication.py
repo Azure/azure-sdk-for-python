fd68f6fca18e71be20b8f39547b8617165c9c126
Charles Lowell <chlowe@microsoft.com>
2020-01-14 16:19:33 -0800

    fix whitespace

--- sdk/core/azure-core/azure/core/pipeline/policies/_authentication.py
+++ sdk/core/azure-core/azure/core/pipeline/policies/_authentication.py
@@ -23,6 +23,7 @@ if TYPE_CHECKING:
 # pylint:disable=too-few-public-methods
 class _BearerTokenCredentialPolicyBase(object):
     """Base class for a Bearer Token Credential Policy.
+
     :param credential: The credential.
     :type credential: ~azure.core.credentials.TokenCredential
     :param str scopes: Lets you specify the type of access needed.
@@ -47,6 +48,7 @@ class _BearerTokenCredentialPolicyBase(object):
     def _update_headers(headers, token):
         # type: (Dict[str, str], str) -> None
         """Updates the Authorization header with the bearer token.
+
         :param dict headers: The HTTP Request headers
         :param str token: The OAuth token.
         """
@@ -60,6 +62,7 @@ class _BearerTokenCredentialPolicyBase(object):
 
 class BearerTokenCredentialPolicy(_BearerTokenCredentialPolicyBase, SansIOHTTPPolicy):
     """Adds a bearer token Authorization header to requests.
+
     :param credential: The credential.
     :type credential: ~azure.core.TokenCredential
     :param str scopes: Lets you specify the type of access needed.
@@ -69,6 +72,7 @@ class BearerTokenCredentialPolicy(_BearerTokenCredentialPolicyBase, SansIOHTTPPo
     def on_request(self, request):
         # type: (PipelineRequest) -> None
         """Adds a bearer token Authorization header to request and sends request to next policy.
+
         :param request: The pipeline request object
         :type request: ~azure.core.pipeline.PipelineRequest
         """
