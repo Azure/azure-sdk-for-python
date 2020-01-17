57ca70cf7180ace147a87f4fc977510a1a069815
Xiang Yan <xiangsjtu@gmail.com>
2020-01-17 11:08:26 -0800

    update test framework to use test preparer

--- tools/azure-sdk-tools/devtools_testutils/__init__.py
+++ tools/azure-sdk-tools/devtools_testutils/__init__.py
@@ -3,11 +3,12 @@ from .azure_testcase import AzureTestCase, is_live
 from .resource_testcase import (FakeResource, ResourceGroupPreparer)
 from .storage_testcase import (FakeStorageAccount, StorageAccountPreparer)
 from .keyvault_preparer import KeyVaultPreparer
+from .app_configuration_preparer import AppConfigurationPreparer
 
 __all__ = [
     'AzureMgmtTestCase', 'AzureMgmtPreparer',
     'FakeResource', 'ResourceGroupPreparer',
     'FakeStorageAccount', 'StorageAccountPreparer',
     'AzureTestCase', 'is_live',
-    "KeyVaultPreparer"
+    "KeyVaultPreparer", 'AppConfigurationPreparer'
 ]
