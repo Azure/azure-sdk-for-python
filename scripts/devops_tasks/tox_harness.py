addf9d09853f8823ddd9f7e46ae2f27ff1833dfb
scbedd <45376673+scbedd@users.noreply.github.com>
2020-01-17 16:37:30 -0800

    run all tests regardless of exit result

--- scripts/devops_tasks/tox_harness.py
+++ scripts/devops_tasks/tox_harness.py
@@ -222,16 +222,23 @@ def replace_dev_reqs(file):
 def execute_tox_serial(tox_command_tuples):
     for index, cmd_tuple in enumerate(tox_command_tuples):
         tox_dir = os.path.join(cmd_tuple[1], "./.tox/")
+        return_code = 0
 
         logging.info(
             "Running tox for {}. {} of {}.".format(
                 os.path.basename(cmd_tuple[1]), index + 1, len(tox_command_tuples)
             )
         )
-        run_check_call(cmd_tuple[0], cmd_tuple[1])
+        try:
+            run_check_call(cmd_tuple[0], cmd_tuple[1])
+        except Exception as e:
+            logging.error(e)
+            return_code = 1
 
         if in_ci():
             shutil.rmtree(tox_dir)
+
+        sys.exit(return_code)
 
 
 def prep_and_run_tox(targeted_packages, parsed_args, options_array=[]):
