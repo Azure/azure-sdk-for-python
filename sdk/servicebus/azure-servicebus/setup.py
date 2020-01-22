6e59ea63933ed9f2d5bafc3a2951bcf84faf5571
Chidozie Ononiwu <chononiw@microsoft.com>
2020-01-22 13:42:21 -0800

    add long discription

--- sdk/servicebus/azure-servicebus/setup.py
+++ sdk/servicebus/azure-servicebus/setup.py
@@ -53,6 +53,7 @@ setup(
     version=version,
     description='Microsoft Azure {} Client Library for Python'.format(PACKAGE_PPRINT_NAME),
     long_description=readme + '\n\n' + changelog,
+    long_description_content_type='text/markdown',
     license='MIT License',
     author='Microsoft Corporation',
     author_email='azpysdkhelp@microsoft.com',
