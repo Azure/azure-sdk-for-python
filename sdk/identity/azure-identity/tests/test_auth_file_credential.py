0eed34d58dcc9845d8db01671510eb69a7492b68
Ruijun Hu (Wicresoft North America Ltd) <v-ruih@microsoft.com>
2020-01-22 18:17:34 +0800

    update tests, now test get_token by validating http request

--- sdk/identity/azure-identity/tests/test_auth_file_credential.py
+++ sdk/identity/azure-identity/tests/test_auth_file_credential.py
@@ -7,23 +7,38 @@ import os
 from azure.identity import AuthFileCredential
 from azure.core.exceptions import ClientAuthenticationError
 
+from helpers import validating_transport, Request, mock_response, build_aad_response
+
 import pytest
 try:
-    from unittest.mock import Mock, patch
+    from unittest.mock import patch
 except ImportError:
-    from mock import Mock, patch
+    from mock import patch
 
 
 def test_auth_file_credential_parse():
-    credential = AuthFileCredential('{}/authfile.json'.format(os.path.dirname(__file__)))
-    credential._ensure_credential()
-    
-    inner = credential._credential
+    client_id = 'mockclientid'   # same value with that in authfile.json
+    secret = 'mockclientsecret'  # same value with that in authfile.json
+    tenant_id =  'mocktenantid'  # same value with that in authfile.json
+    access_token = '***'
 
-    assert inner is not None
-    assert inner._form_data['client_id'] == 'mockclientid'
-    assert inner._form_data['client_secret'] == 'mockclientsecret'
-    assert 'mocktenantid' in inner._client._auth_url
+    transport = validating_transport(
+        requests=[Request(url_substring=tenant_id, required_data={"client_id": client_id, "client_secret": secret})],
+        responses=[
+            mock_response(
+                json_payload={
+                    "token_type": "Bearer",
+                    "expires_in": 42,
+                    "ext_expires_in": 42,
+                    "access_token": access_token
+                }
+            )
+        ]
+    )
+
+    token = AuthFileCredential('{}/authfile.json'.format(os.path.dirname(__file__)), transport=transport).get_token('scope')
+
+    assert token.token == access_token
 
 def test_bad_auth_file():
     def test_file_not_found():
