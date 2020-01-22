b994aa6b21cebde3a0947c0cc4ac88858e031edc
Ruijun Hu (Wicresoft North America Ltd) <v-ruih@microsoft.com>
2020-01-22 16:18:01 +0800

    cover malformed auth file and bad value auth file in tests

--- sdk/identity/azure-identity/tests/test_auth_file_credential.py
+++ sdk/identity/azure-identity/tests/test_auth_file_credential.py
@@ -7,6 +7,13 @@ import os
 from azure.identity import AuthFileCredential
 from azure.core.exceptions import ClientAuthenticationError
 
+import pytest
+try:
+    from unittest.mock import Mock, patch
+except ImportError:
+    from mock import Mock, patch
+
+
 def test_auth_file_credential_parse():
     credential = AuthFileCredential('{}/authfile.json'.format(os.path.dirname(__file__)))
     credential._ensure_credential()
@@ -19,8 +26,27 @@ def test_auth_file_credential_parse():
     assert 'mocktenantid' in inner._client._auth_url
 
 def test_bad_auth_file():
-    credential = AuthFileCredential('Bad*Path')
-    try:
-        credential.get_token("https://mock.scope/.default/")
-    except ClientAuthenticationError as e:
-        assert 'Error parsing SDK Auth File' in e.message
+    def test_file_not_found():
+        with pytest.raises(ClientAuthenticationError) as e:
+            credential = AuthFileCredential('Bad*Path')
+            token = credential.get_token("https://mock.scope/.default/")
+
+        assert 'No file found on the given path' == str(e.value)
+    
+    def test_file_no_json():
+        with pytest.raises(ClientAuthenticationError) as nojson_e:
+            credential = AuthFileCredential('{}/authfile_nojson.json'.format(os.path.dirname(__file__)))
+            token = credential.get_token("https://mock.scope/.default")
+
+        assert 'Error parsing SDK Auth File' == str(nojson_e.value)
+
+    def test_file_bad_value():
+        with pytest.raises(ClientAuthenticationError) as nojson_e:
+            credential = AuthFileCredential('{}/authfile_badvalue.json'.format(os.path.dirname(__file__)))
+            token = credential.get_token("https://mock.scope/.default")
+
+        assert 'Error parsing SDK Auth File' == str(nojson_e.value)
+
+    test_file_not_found()
+    test_file_no_json()
+    test_file_bad_value()
