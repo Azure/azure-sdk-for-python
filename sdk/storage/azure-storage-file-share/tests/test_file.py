3e35ee888d5d6b24871178110bf6b8711f2ef08a
antisch <antisch@microsoft.com>
2020-01-23 15:13:53 -0800

    Merged async file tests

--- sdk/storage/azure-storage-file-share/tests/test_file.py
+++ sdk/storage/azure-storage-file-share/tests/test_file.py
@@ -574,8 +574,6 @@ class StorageFileTest(StorageTestCase):
 
     @GlobalStorageAccountPreparer()
     def test_set_file_metadata_with_upper_case(self, resource_group, location, storage_account, storage_account_key):
-        #if not self.is_live:
-        #    return
         self._setup(storage_account, storage_account_key)
         metadata = {'hello': 'world', 'number': '42', 'UP': 'UPval'}
         file_client = self._create_file()
