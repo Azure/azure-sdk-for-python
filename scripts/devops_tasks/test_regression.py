c77d72e0d623031066eea9e3a2da7908a268b83e
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2020-01-29 01:43:30 -0800

    Code restructuring to make it more readable

--- scripts/devops_tasks/test_regression.py
+++ scripts/devops_tasks/test_regression.py
@@ -13,7 +13,15 @@ import glob
 import sys
 import os
 import logging
-from common_tasks import process_glob_string, parse_setup, run_check_call, parse_require, install_package_from_whl, filter_dev_requirements, OmmitType
+from common_tasks import (
+    process_glob_string,
+    parse_setup,
+    run_check_call,
+    parse_require,
+    install_package_from_whl,
+    filter_dev_requirements,
+    OmmitType,
+)
 from git_helper import get_release_tag, checkout_code_repo, clone_repo
 
 AZURE_GLOB_STRING = "azure*"
@@ -26,127 +34,217 @@ GIT_MASTER_BRANCH = "master"
 VENV_NAME = "regressionenv"
 AZURE_SDK_FOR_PYTHON_GIT_URL = "https://github.com/Azure/azure-sdk-for-python.git"
 TEMP_FOLDER_NAME = ".tmp_code_path"
-COSMOS_TEST_ARG = 'not cosmosEmulator'
+COSMOS_TEST_ARG = "not cosmosEmulator"
 
 logging.getLogger().setLevel(logging.INFO)
 
-def run_regression_test(pkg_name, dep_packages, whl_directory, isLatestDepend, tmp_path, working_dir):
 
-    # pkg_name: Name of the package for which regression is tested
-    # dep_packages: List of path to packages in code repo which takes <package-name> as dependency
-    # whl_directory: directory (passed as paremeter) in which whl for <package-name> is available
-    # tmp_path: Temporary path passed as parameter to clone code repo to run test
-    # isLatestDepend: Passed as true or false to run regression using latest or oldest released version of dependent package
+class CustomVirtualEnv:
+    def __init__(self, path):
+        self.path = os.path.join(path, VENV_NAME)
 
-    venv_path = os.path.join(tmp_path, VENV_NAME)
-    #create a virtual environment to run test
-    create_virtual_env(venv_path)
-    python_sym_link = os.path.abspath(os.path.join(venv_path, "Scripts", "python"))
+    def create(self):
+        logging.info("Creating virtual environment [{}]".format(self.path))
+        run_check_call([sys.executable, "-m", "venv", "ENV_DIR", self.path], root_dir)
 
-    logging.info("Dependent packages for [{0}]: {1}".format(pkg_name, dep_packages))
-    # Run test for each dependent package in its own virtual environment
-    for dep_pkg_path in dep_packages:
-        dep_pkg_name, _, _, _ = parse_setup(dep_pkg_path)
-        logging.info("Starting regression test of {0} against released {1}".format(pkg_name, dep_pkg_name))
-        pre_test_step(dep_pkg_path, whl_directory, pkg_name, venv_path, working_dir, python_sym_link)
+    def _process_venv(self, activate_env):
+        if activate_env:
+            # clear any previously installed packages
+            run_check_call(
+                [sys.executable, "-m", "venv", "--clear", "ENV_DIR", self.path],
+                root_dir,
+            )
+
+        scriptName = "activate.bat" if activate_env else "deactivate.bat"
+        venv_script = os.path.join(self.path, "Scripts", scriptName)
+        operation_type = "Activating" if activate_env else "Deactivating"
+
+        if os.path.exists(venv_script):
+            logging.info(
+                "{0} virtual environment {1}".format(operation_type, venv_script)
+            )
+            run_check_call([venv_script,], root_dir)
+        else:
+            logging.error(
+                "Script to process virtualenv is missing. path [{}]".format(venv_script)
+            )
+            sys.exit(1)
+
+    def activate(self):
+        self._process_venv(True)
+
+    def deactivate(self):
+        self._process_venv(False)
+
+
+class RegressionContext:
+    def __init__(self, whl_dir, tmp_path, is_latest):
+        self.whl_directory = whl_dir
+        self.temp_path = tmp_path
+        self.is_latest_depend_test = is_latest
+        self.venv = CustomVirtualEnv(self.temp_path)
+        self.venv.create()
+        self.python_symlink = os.path.abspath(
+            os.path.join(self.venv.path, "Scripts", "python")
+        )
+
+    def init_for_pkg(self, pkg_root):
+        # This method will set package root for which regression is tested
+        self.package_root_path = pkg_root
+        self.package_name, _, _, _ = parse_setup(self.package_root_path)
+
+    def activate(self, dep_pkg_root_path):
+        self.dep_pkg_root_path = dep_pkg_root_path
+        self.venv.activate()
+
+    def deactivate(self, dep_pkg_root_path):
+        # This function can be used to reset code repo to master branch and also to deactivate virtual env
+        # revert to master branch
+        run_check_call(["git", "clean", "-fd"], dep_pkg_root_path)
+        run_check_call(["git", "checkout", GIT_MASTER_BRANCH], dep_pkg_root_path)
+        self.venv.deactivate()
+
+
+class RegressionTest:
+    def __init__(self, context, package_dependency_dict):
+        self.context = context
+        self.package_dependency_dict = package_dependency_dict
+
+    def run(self):
+        pkg_name = self.context.package_name
+        if pkg_name in self.package_dependency_dict:
+            dep_packages = self.package_dependency_dict[pkg_name]
+            logging.info("Running regression test for {}".format(pkg_name))
+            logging.info(
+                "Dependent packages for [{0}]: {1}".format(pkg_name, dep_packages)
+            )
+
+            for dep_pkg_path in dep_packages:
+                dep_pkg_name, _, _, _ = parse_setup(dep_pkg_path)
+                logging.info(
+                    "Starting regression test of {0} against released {1}".format(
+                        pkg_name, dep_pkg_name
+                    )
+                )
+                self._run_test(dep_pkg_path)
+                logging.info(
+                    "Completed regression test of {0} against released {1}".format(
+                        pkg_name, dep_pkg_name
+                    )
+                )
+            logging.info("Completed regression test for {}".format(pkg_name))
+        else:
+            logging.info(
+                "Package {} is not added as required by any package".format(pkg_name)
+            )
+
+    def _run_test(self, dep_pkg_path):
+        # Get code repo with released tag of dependent package
+        self._get_code_repo(dep_pkg_path)
+
         try:
-            run_test(dep_pkg_path, pkg_name, isLatestDepend, python_sym_link)
+            # activate virtual env
+            self.context.activate(dep_pkg_path)
+            # install packages required to run tests after updating relative referefnce to abspath
+            run_check_call(
+                [
+                    self.context.python_symlink,
+                    "-m",
+                    "pip",
+                    "install",
+                    "-r",
+                    test_tools_req_file,
+                ],
+                self.context.package_root_path,
+            )
+            # Install pre-built whl for current package
+            install_package_from_whl(
+                self.context.package_name,
+                self.context.whl_directory,
+                self.context.temp_path,
+                self.context.python_symlink,
+            )
+            # install package to be tested and run pytest
+            self._execute_test(dep_pkg_path)
         finally:
-            post_test_step(venv_path, dep_pkg_path, python_sym_link)
-        logging.info("Completed regression test of {0} against released {1}".format(pkg_name, dep_pkg_name))
+            # deactivate virtual env and revert repo
+            self.context.deactivate(dep_pkg_path)
 
+    def _get_code_repo(self, dependent_pkg_path):
+        # find GA released tags for package and run test using that code base
+        dep_pkg_name, _, _, _ = parse_setup(dependent_pkg_path)
+        release_tag = get_release_tag(dep_pkg_name, self.context.is_latest_depend_test)
+        if not release_tag:
+            logging.info(
+                "Skipping package {} from test since release tag is not avaiable".format(
+                    dep_pkg_name
+                )
+            )
+            return
 
-def pre_test_step(dependent_pkg_path, whl_directory, pkg_name, venv_path, working_dir, python_sym_link):
-    # This function will execute any pre step required before running test for individual dependent packages
-    # pre steps are like: checking out master branch to revert code repo
-    # start and activate new virtual environment
-    # install pre build whl for the package for which regression is tested. (for e.g. azure-core)
-    logging.info("Running pre-run-step for package: {}".format(dependent_pkg_path))
-    # activate virtual env and change working directory to package root
-    process_virtual_env(True, venv_path, python_sym_link)
-    # install packages required to run tests after updating relative referefnce to abspath
-    install_requirements(test_tools_req_file, working_dir, python_sym_link)
-    # Install pre-built whl for current package
-    install_package_from_whl(pkg_name, whl_directory, root_dir, python_sym_link)
+        # todo: Exlcusion based on package name and release tag
+        pass
+        # checkout git hub repo with release tag
+        checkout_code_repo(release_tag, dependent_pkg_path)
 
-def get_package_test_path(pkg_root_path):
-    paths = glob.glob(os.path.join(pkg_root_path, "test*"))
-    if paths is None:
-        logging.error("'test' folder is not found in {}".format(pkg_root_path))
-        sys.exit(1)
-    return paths[0]
+    def _execute_test(self, dep_pkg_path):
+        # install dependent package from source
+        self._install_packages(dep_pkg_path, self.context.package_name)
+        logging.info("Running test for {}".format(dep_pkg_path))
+        pkg_test_dir = self._get_package_test_dir(dep_pkg_path)
+        run_check_call(
+            [
+                self.context.python_symlink,
+                "-m",
+                "pytest",
+                "--verbose",
+                "-m",
+                COSMOS_TEST_ARG,
+                pkg_test_dir,
+            ],
+            self.context.temp_path,
+        )
 
-def run_test(dependent_pkg_path, package_name, isLatest, python_sym_link):
-    # find GA released tags for package and run test using that code base
-    dep_pkg_name, _, _, _ = parse_setup(dependent_pkg_path)
-    release_tag = get_release_tag(dep_pkg_name, isLatest)
-    if not release_tag:
-        logging.info("Skipping package {} from test since release tag is not avaiable".format(dep_pkg_name))
-        return
+    def _get_package_test_dir(self, pkg_root_path):
+        # Returns path to test or tests folder within package root directory.
+        paths = glob.glob(os.path.join(pkg_root_path, "test*"))
+        if paths is None:
+            logging.error("'test' folder is not found in {}".format(pkg_root_path))
+            sys.exit(1)
+        return paths[0]
 
-    # checkout git hub repo with release tag    
-    checkout_code_repo(release_tag, dependent_pkg_path)
-    
-    # install dependent package from source
-    install_packages(dependent_pkg_path, package_name, python_sym_link)
-    logging.info("Running test for {}".format(dependent_pkg_path))
-    pkg_test_path = get_package_test_path(dependent_pkg_path)
-    run_check_call([python_sym_link, "-m", "pytest", "--verbose", "-m", COSMOS_TEST_ARG, pkg_test_path], root_dir)
+    def _install_packages(self, dependent_pkg_path, pkg_to_exclude):
+        python_executable = self.context.python_symlink
+        working_dir = self.context.package_root_path
+        temp_dir = self.context.temp_path
 
-
-def install_packages(dependent_pkg_path, package_name, python_sym_link):
-     # install dev requirement but skip already installed package which is being tested
-    filtered_dev_req_path = filter_dev_requirements(dependent_pkg_path, [package_name,], dependent_pkg_path)
-    logging.info("Installing filtered dev requirements from {}".format(filtered_dev_req_path))
-    install_requirements(filtered_dev_req_path, dependent_pkg_path, python_sym_link)    
-    # install dependent package which is being verified
-    run_check_call([python_sym_link, "-m", "pip", "install", dependent_pkg_path], root_dir)   
-
-
-def post_test_step(venv_path, dependent_pkg_path, python_sym_link):
-    # This function can be used to reset code repo to master branch and also to deactivate virtual env
-    # revert to master branch
-    run_check_call(["git", "clean", "-fd"], dependent_pkg_path)
-    run_check_call(["git", "checkout", GIT_MASTER_BRANCH], dependent_pkg_path)
-    process_virtual_env(False, venv_path, python_sym_link)
-
-
-def process_virtual_env(activate_env, venv_path, python_sym_link):
-    # for now this will work only on Windows. I will update this to make it platform independent
-    if activate_env:
-        # clear any previously installed packages
-        run_check_call([sys.executable, "-m", "venv", "--clear", "ENV_DIR", venv_path], root_dir)
-
-    scriptName = "activate.bat" if activate_env else "deactivate.bat"
-    venv_script = os.path.join(venv_path, "Scripts", scriptName)
-    operation_type = "Activating" if activate_env else "Deactivating"
-
-    if os.path.exists(venv_script):
-        logging.info("{0} virtual environment {1}".format(operation_type, venv_script))
-        run_check_call([venv_script,], root_dir)
-    else:
-        logging.error("Script to process virtualenv is missing. path [{}]".format(venv_script))
-        sys.exit(1)
-    
-
-def create_virtual_env(venv_path):
-    logging.info("Creating virtual environment [{}]".format(venv_path))
-    run_check_call([sys.executable, "-m", "venv", "ENV_DIR", venv_path], root_dir)
-
-
-def install_requirements(req_path, working_dir, python_sym_link):
-    # install packages required to run tests
-    run_check_call([python_sym_link, "-m", "pip", "install", "-r", req_path], working_dir)   
+        # install dev requirement but skip already installed package which is being tested
+        filtered_dev_req_path = filter_dev_requirements(
+            dependent_pkg_path, [pkg_to_exclude,], dependent_pkg_path
+        )
+        logging.info(
+            "Installing filtered dev requirements from {}".format(filtered_dev_req_path)
+        )
+        run_check_call(
+            [python_executable, "-m", "pip", "install", "-r", filtered_dev_req_path],
+            working_dir,
+        )
+        # install dependent package which is being verified
+        run_check_call(
+            [python_executable, "-m", "pip", "install", dependent_pkg_path], temp_dir
+        )
 
 
 # This method identifies package dependency map for all packages in azure sdk
 def find_package_dependency(glob_string, repo_root_dir):
-    package_paths = process_glob_string(glob_string, repo_root_dir, "", OmmitType.Regression)
+    package_paths = process_glob_string(
+        glob_string, repo_root_dir, "", OmmitType.Regression
+    )
     dependency_map = {}
     for pkg_root in package_paths:
         dependent_pkg_name, _, _, requires = parse_setup(pkg_root)
         # todo: This should be removed once issue in storage file share test is resolved
-        if "cosmos" not in dependent_pkg_name:
+        if "storage" not in dependent_pkg_name:
             continue
         # Get a list of package names from install requires
         required_pkgs = [parse_require(r)[0] for r in requires]
@@ -173,7 +271,9 @@ def run_main(args):
     core_repo_root = os.path.join(temp_dir, GIT_REPO_NAME)
     # Make sure root_dir where script is running is not same as code repo which will be reverted to old released branch to run test
     if root_dir == core_repo_root:
-        logging.error("Invalid path to clone github code repo. Temporary path can not be same as current source root directory")
+        logging.error(
+            "Invalid path to clone github code repo. Temporary path can not be same as current source root directory"
+        )
         exit(1)
 
     # Make sure temp path exists
@@ -189,24 +289,30 @@ def run_main(args):
     targeted_packages = process_glob_string(args.glob_string, target_dir)
     if len(targeted_packages) == 0:
         exit(0)
-       
+
     # clone code repo only if it doesn't exists
     if not os.path.exists(core_repo_root):
         clone_repo(temp_dir, AZURE_SDK_FOR_PYTHON_GIT_URL)
     else:
-        logging.info("Path {} already exists. Skipping step to clone github repo".format(core_repo_root))
+        logging.info(
+            "Path {} already exists. Skipping step to clone github repo".format(
+                core_repo_root
+            )
+        )
 
     # find package dependency map for azure sdk
     pkg_dependency = find_package_dependency(AZURE_GLOB_STRING, core_repo_root)
     logging.info("Regression test will run for: {}".format(pkg_dependency.keys()))
-    for pkg_path in targeted_packages:
-        package_name, _, _, _ = parse_setup(pkg_path)
-        if package_name in pkg_dependency:
-            logging.info("Running regression test for {}".format(package_name))
-            run_regression_test(package_name, pkg_dependency[package_name], args.whl_dir, args.verify_latest, temp_dir, pkg_path)
-            logging.info("Completed regression test for {}".format(package_name))
 
-    
+    # Create regression text context. One context object will be reused for all packages
+    context = RegressionContext(args.whl_dir, temp_dir, args.verify_latest)
+
+    for pkg_path in targeted_packages:
+        context.init_for_pkg(pkg_path)
+        RegressionTest(context, pkg_dependency).run()
+    logging.info("Regression test is completed successfully")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Run regression test for a package against released dependent packages"
@@ -232,9 +338,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--whl-dir",
         required=True,
-        help=(
-            "Directory in which whl is pre built for all eligible package"
-        ),
+        help=("Directory in which whl is pre built for all eligible package"),
     )
 
     parser.add_argument(
