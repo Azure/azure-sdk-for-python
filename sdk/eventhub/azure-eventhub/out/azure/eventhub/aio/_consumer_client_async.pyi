5dca611f4a98b077f47ebdd6ff99887a6a9fa0df
yijxie <yijun.xie@micrsofot.com>
2020-01-30 12:17:21 -0800

    Remove receive from this review file

--- sdk/eventhub/azure-eventhub/out/azure/eventhub/aio/_consumer_client_async.pyi
+++ sdk/eventhub/azure-eventhub/out/azure/eventhub/aio/_consumer_client_async.pyi
@@ -1,22 +1,7 @@
 class EventHubConsumerClient(ClientBaseAsync):
 
-    # receive is existing API. Won't be deleted after receive_batch is added
-    async def receive(
-            self,
-            on_event: Callable[[PartitionContext, EventData], Awaitable[None]],
-            *,
-            partition_id: Optional[str]=...,
-            owner_level: Optional[int]=...,
-            prefetch: int=...,
-            track_last_enqueued_event_properties: bool=...,
-            starting_position: Optional[Union[str, int, datetime.datetime, Dict[str, Any]]]=...,
-            starting_position_inclusive: Union[bool, Dict[str, bool]]=...,
-            on_error: Optional[Callable[[PartitionContext, Exception], Awaitable[None]]]=...,
-            on_partition_initialize: Optional[Callable[[PartitionContext], Awaitable[None]]]=...,
-            on_partition_close: Optional[Callable[[PartitionContext, CloseReason], Awaitable[None]]]=...
-    ) -> None: ...
+    #...
 
-    # The new method for this API review
     async def receive_batch(
             self, on_event_batch: Callable[[PartitionContext, List[EventData]], Awaitable[None]],
             max_batch_size: int,
@@ -33,3 +18,4 @@ class EventHubConsumerClient(ClientBaseAsync):
             on_partition_close: Optional[Callable[[PartitionContext, CloseReason], Awaitable[None]]]=...
     ) -> None: ...
 
+    # ...
