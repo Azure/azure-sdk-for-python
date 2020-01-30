6d2968524ac7a86d2b8301561018a6f42729717e
yijxie <yijun.xie@micrsofot.com>
2020-01-30 12:08:48 -0800

    Add receive_batch sample

---
+++ sdk/eventhub/azure-eventhub/samples/async_samples/recv_batch_with_checkpoint_async.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+"""
+An example to show receiving events from an Event Hub with checkpoint store doing checkpoint by batch asynchronously.
+In the `receive_batch` method of `EventHubConsumerClient`:
+If no partition id is specified, the checkpoint_store are used for load-balance and checkpoint.
+If partition id is specified, the checkpoint_store can only be used for checkpoint.
+"""
+
+import asyncio
+import os
+from azure.eventhub.aio import EventHubConsumerClient
+from azure.eventhub.extensions.checkpointstoreblobaio import BlobCheckpointStore
+
+CONNECTION_STR = os.environ["EVENT_HUB_CONN_STR"]
+STORAGE_CONNECTION_STR = os.environ["AZURE_STORAGE_CONN_STR"]
+BLOB_CONTAINER_NAME = "your-blob-container-name"  # Please make sure the blob container resource exists.
+
+
+async def on_event_batch(partition_context, event_batch):
+    if event_batch:
+        print("Received events from partition: {}.".format(partition_context.partition_id))
+        # Put your code here.
+
+        await partition_context.update_checkpoint(event_batch[-1])
+    else:
+        # this is a heartbeat.
+        pass
+
+
+async def receive(client):
+    """
+    Without specifying partition_id, the receive will try to receive events from all partitions and if provided with
+    a checkpoint store, the client will load-balance partition assignment with other EventHubConsumerClient instances
+    which also try to receive events from all partitions and use the same storage resource.
+    """
+    await client.receive_batch(
+        on_event=on_event_batch,
+        max_batch_size=100,
+        max_wait_time=3,
+        starting_position="-1",  # "-1" is from the beginning of the partition.
+    )
+    # With specified partition_id, load-balance will be disabled, for example:
+    # await client.receive_batch(on_event_batch=on_event_batch, max_batch_size=100, max_wait_time=3, partition_id='0'))
+
+
+async def main():
+    checkpoint_store = BlobCheckpointStore.from_connection_string(STORAGE_CONNECTION_STR, BLOB_CONTAINER_NAME)
+    client = EventHubConsumerClient.from_connection_string(
+        CONNECTION_STR,
+        consumer_group="$Default",
+        checkpoint_store=checkpoint_store,  # For load-balancing and checkpoint. Leave None for no load-balancing.
+    )
+    async with client:
+        await receive(client)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
