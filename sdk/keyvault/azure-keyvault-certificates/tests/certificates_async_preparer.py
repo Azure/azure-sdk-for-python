ad33b0692ab8cebcac5e1cee8570c453669dbb61
Charles Lowell <chlowe@microsoft.com>
2020-02-03 12:58:40 -0800

    Simplify and speed up Key Vault test playback (#9608)

--- sdk/keyvault/azure-keyvault-certificates/tests/certificates_async_preparer.py
+++ sdk/keyvault/azure-keyvault-certificates/tests/certificates_async_preparer.py
@@ -28,10 +28,20 @@ class AiohttpTestTransport(AioHttpTransport):
         return response
 
 
+class NoSleepTransport(AiohttpTestTransport):
+    """Prevents the transport from sleeping, e.g. to observe a Retry-After header"""
+
+    async def sleep(self, _):
+        return
+
+
 class AsyncVaultClientPreparer(VaultClientPreparer):
     def create_vault_client(self, vault_uri):
         if self.is_live:
             credential = EnvironmentCredential()
+            transport = AiohttpTestTransport()
         else:
             credential = Mock(get_token=asyncio.coroutine(lambda _: AccessToken("fake-token", 0)))
-        return VaultClient(vault_uri, credential, transport=AiohttpTestTransport(), **self.client_kwargs)
+            transport = NoSleepTransport()
+
+        return VaultClient(vault_uri, credential, transport=transport, is_live=self.is_live, **self.client_kwargs)
