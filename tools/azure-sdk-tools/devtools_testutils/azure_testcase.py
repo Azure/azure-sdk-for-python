ab4c566a088459d33384553e0b2d488f223c50ba
Laurent Mazuel <laurent.mazuel@gmail.com>
2020-02-04 10:27:18 -0800

    Test update to support autorest v3 generated client (#9695)
    * Allow credential in testframework
    
    * Adapt test framework to support azure-core based Mgmt client
    
    * Fixing more credential things
    
    * Fix missing script declaration
    
    * Update azure_testcase.py

--- tools/azure-sdk-tools/devtools_testutils/azure_testcase.py
+++ tools/azure-sdk-tools/devtools_testutils/azure_testcase.py
@@ -5,7 +5,13 @@
 #--------------------------------------------------------------------------
 import inspect
 import os.path
+import sys
 import zlib
+try:
+    from inspect import getfullargspec as get_arg_spec
+except ImportError:
+    from inspect import getargspec as get_arg_spec
+
 import pytest
 
 from azure_devtools.scenario_tests import (
@@ -57,6 +63,14 @@ def is_live():
             config_file = None
         is_live._cache = TestConfig(config_file=config_file).record_mode
     return is_live._cache
+
+
+def _is_autorest_v3(client_class):
+    """ IS this client a autorestv3/track2 one?.
+    Could be refined later if necessary.
+    """
+    args = get_arg_spec(client_class.__init__).args
+    return "credential" in args
 
 
 class AzureTestCase(ReplayableTest):
@@ -161,35 +175,60 @@ class AzureTestCase(ReplayableTest):
         return super(AzureTestCase, self).tearDown()
 
     def create_basic_client(self, client_class, **kwargs):
-        # Whatever the client, if credentials is None, fail
-        with self.assertRaises(ValueError):
-            client = client_class(
-                credentials=None,
-                **kwargs
-            )
 
         tenant_id = os.environ.get("AZURE_TENANT_ID", None)
         client_id = os.environ.get("AZURE_CLIENT_ID", None)
         secret = os.environ.get("AZURE_CLIENT_SECRET", None)
 
         if tenant_id and client_id and secret and self.is_live:
-            from msrestazure.azure_active_directory import ServicePrincipalCredentials
-            credentials = ServicePrincipalCredentials(
-                tenant=tenant_id,
-                client_id=client_id,
-                secret=secret
-            )
+            if _is_autorest_v3(client_class):
+                # Create azure-identity class
+                from azure.identity import ClientSecretCredential
+                credentials = ClientSecretCredential(
+                    tenant=tenant_id,
+                    client_id=client_id,
+                    client_secret=secret
+                )
+            else:
+                # Create msrestazure class
+                from msrestazure.azure_active_directory import ServicePrincipalCredentials
+                credentials = ServicePrincipalCredentials(
+                    tenant=tenant_id,
+                    client_id=client_id,
+                    secret=secret
+                )
         else:
-            credentials = self.settings.get_credentials()
+            if _is_autorest_v3(client_class):
+                credentials = self.settings.get_azure_core_credentials()
+            else:
+                credentials = self.settings.get_credentials()
 
         # Real client creation
-        client = client_class(
-            credentials=credentials,
-            **kwargs
-        )
+        # FIXME decide what is the final argument for that
+        # if self.is_playback():
+        #     kwargs.setdefault("polling_interval", 0)
+        if _is_autorest_v3(client_class):
+            kwargs.setdefault("logging_enable", True)
+            client = client_class(
+                credential=credentials,
+                **kwargs
+            )
+        else:
+            client = client_class(
+                credentials=credentials,
+                **kwargs
+            )
+
         if self.is_playback():
-            client.config.long_running_operation_timeout = 0
-        client.config.enable_http_logger = True
+            try:
+                client._config.polling_interval = 0  # FIXME in azure-mgmt-core, make this a kwargs
+            except AttributeError:
+                pass
+
+        if hasattr(client, "config"):  # Autorest v2
+            if self.is_playback():
+                client.config.long_running_operation_timeout = 0
+            client.config.enable_http_logger = True
         return client
 
     def create_random_name(self, name):
