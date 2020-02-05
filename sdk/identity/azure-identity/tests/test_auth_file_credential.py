682d8de10b1e77893803a5b4d4816369f0d38122
cxznmhdcxz <v-ruih@microsoft.com>
2020-02-05 12:31:57 +0800

    updated tests

--- sdk/identity/azure-identity/tests/test_auth_file_credential.py
+++ sdk/identity/azure-identity/tests/test_auth_file_credential.py
@@ -3,23 +3,25 @@
 # Licensed under the MIT License.
 # ------------------------------------
 import os
+import json
 
 from azure.identity import AuthFileCredential
 from azure.core.exceptions import ClientAuthenticationError
 
-from helpers import validating_transport, Request, mock_response, build_aad_response
+from helpers import validating_transport, Request, mock_response
 
 import pytest
 try:
-    from unittest.mock import patch
+    from unittest.mock import patch, mock_open
 except ImportError:
-    from mock import patch
+    from mock import patch, mock_open
 
 
 def test_auth_file_credential_parse():
-    client_id = 'mockclientid'   # same value with that in authfile.json
-    secret = 'mockclientsecret'  # same value with that in authfile.json
-    tenant_id =  'mocktenantid'  # same value with that in authfile.json
+    client_id = 'mockclientid'
+    secret = 'mockclientsecret'
+    tenant_id =  'mocktenantid'
+    authority = 'https://login.microsoftonline.com'
     access_token = '***'
 
     transport = validating_transport(
@@ -36,7 +38,15 @@ def test_auth_file_credential_parse():
         ]
     )
 
-    token = AuthFileCredential('{}/authfile.json'.format(os.path.dirname(__file__)), transport=transport).get_token('scope')
+    mock_data = {
+        'clientId': client_id,
+        'tenantId': tenant_id,
+        'clientSecret': secret,
+        'activeDirectoryEndpointUrl': authority
+    }
+    mock_file = mock_open(read_data=json.dumps(mock_data))
+    with patch('azure.identity._credentials.auth_file.open', mock_file, create=True) as m:
+        token = AuthFileCredential('authfile', transport=transport).get_token('scope')
 
     assert token.token == access_token
 
@@ -45,18 +55,20 @@ def test_file_not_found():
         credential = AuthFileCredential('Bad*Path')
         token = credential.get_token("https://mock.scope/.default/")
 
-    assert 'No file found on the given path' == str(e.value)
+    assert 'No file found on the given path' in str(e.value)
 
 def test_file_no_json():
-    with pytest.raises(ClientAuthenticationError) as nojson_e:
-        credential = AuthFileCredential('{}/authfile_nojson.json'.format(os.path.dirname(__file__)))
-        token = credential.get_token("https://mock.scope/.default")
+    with patch('azure.identity._credentials.auth_file.open', mock_open(read_data='not*a*json'), create=True) as m:
+        with pytest.raises(ClientAuthenticationError) as nojson_e:
+            credential = AuthFileCredential('{}/authfile_nojson.json'.format(os.path.dirname(__file__)))
+            token = credential.get_token("https://mock.scope/.default")
 
-    assert 'Error parsing SDK Auth File' == str(nojson_e.value)
+    assert 'Error parsing SDK Auth File' in str(nojson_e.value)
 
 def test_file_bad_value():
-    with pytest.raises(ClientAuthenticationError) as nojson_e:
-        credential = AuthFileCredential('{}/authfile_badvalue.json'.format(os.path.dirname(__file__)))
-        token = credential.get_token("https://mock.scope/.default")
+    with patch('azure.identity._credentials.auth_file.open', mock_open(read_data='{"foo":"bar"}'), create=True) as m:
+        with pytest.raises(ClientAuthenticationError) as badvalue_e:
+            credential = AuthFileCredential('{}/authfile_badvalue.json'.format(os.path.dirname(__file__)))
+            token = credential.get_token("https://mock.scope/.default")
 
-    assert 'Error parsing SDK Auth File' == str(nojson_e.value)
+    assert 'Error parsing SDK Auth File' in str(badvalue_e.value)
