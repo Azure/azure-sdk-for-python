49d77481fd2b56053fd0d06399786370894a9ab1
Charles Lowell <chlowe@microsoft.com>
2020-02-07 10:42:43 -0800

    Adjust tests for soft-delete enabled by default (#9770)

--- sdk/keyvault/azure-keyvault-secrets/tests/test_samples_secrets_async.py
+++ sdk/keyvault/azure-keyvault-secrets/tests/test_samples_secrets_async.py
@@ -32,7 +32,7 @@ def test_create_secret_client():
 
 class TestExamplesKeyVault(AsyncKeyVaultTestCase):
     @ResourceGroupPreparer(random_name_enabled=True)
-    @KeyVaultPreparer(enable_soft_delete=True)
+    @KeyVaultPreparer()
     @AsyncVaultClientPreparer()
     @AsyncKeyVaultTestCase.await_prepared_test
     async def test_example_secret_crud_operations(self, vault_client, **kwargs):
@@ -99,7 +99,7 @@ class TestExamplesKeyVault(AsyncKeyVaultTestCase):
         # [END delete_secret]
 
     @ResourceGroupPreparer(random_name_enabled=True)
-    @KeyVaultPreparer(enable_soft_delete=True)
+    @KeyVaultPreparer()
     @AsyncVaultClientPreparer()
     @AsyncKeyVaultTestCase.await_prepared_test
     async def test_example_secret_list_operations(self, vault_client, **kwargs):
@@ -148,7 +148,7 @@ class TestExamplesKeyVault(AsyncKeyVaultTestCase):
         # [END list_deleted_secrets]
 
     @ResourceGroupPreparer(random_name_enabled=True)
-    @KeyVaultPreparer()
+    @KeyVaultPreparer(enable_soft_delete=False)
     @AsyncVaultClientPreparer()
     @AsyncKeyVaultTestCase.await_prepared_test
     async def test_example_secrets_backup_restore(self, vault_client, **kwargs):
@@ -177,7 +177,7 @@ class TestExamplesKeyVault(AsyncKeyVaultTestCase):
         # [END restore_secret_backup]
 
     @ResourceGroupPreparer(random_name_enabled=True)
-    @KeyVaultPreparer(enable_soft_delete=True)
+    @KeyVaultPreparer()
     @AsyncVaultClientPreparer()
     @AsyncKeyVaultTestCase.await_prepared_test
     async def test_example_secrets_recover(self, vault_client, **kwargs):
