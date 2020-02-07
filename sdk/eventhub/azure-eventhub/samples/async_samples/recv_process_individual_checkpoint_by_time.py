4fd8f2a9574d47726256e8ffc38c10b6047cb1cc
yijxie <yijun.xie@micrsofot.com>
2020-02-07 14:46:25 -0800

    Fix back pressure control

--- sdk/eventhub/azure-eventhub/samples/async_samples/recv_process_individual_checkpoint_by_time.py
+++ sdk/eventhub/azure-eventhub/samples/async_samples/recv_process_individual_checkpoint_by_time.py
@@ -21,25 +21,45 @@ logging.basicConfig(
     format='%(threadName)s | %(asctime)s | %(name)-12s | %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M:%S',)
 log = logging.getLogger(__name__)
 
-task_semaphor = asyncio.Semaphore(100)  # For back pressure control. At most 100 concurrent upload_doc() are running.
+MAX_UPLOAD_DOC_TASK_COUNT = 100
+upload_doc_task_count = 0
+
+
+def reduce_task_count(fut):
+    global upload_doc_task_count
+    upload_doc_task_count -= 1
+
+
+async def schedule_task(coro):
+    global upload_doc_task_count
+    while upload_doc_task_count >= MAX_UPLOAD_DOC_TASK_COUNT:
+        await asyncio.sleep(0.2)  # check every 0.2 second
+    else:
+        asyncio.create_task(coro).add_done_callback(
+            reduce_task_count
+        )
+        upload_doc_task_count += 1
+
 
 async def upload_doc(doc):
-    async with task_semaphor:  # blocking if there are already 100 upload_doc() are running
-        await asyncio.sleep(0.1)  # http request to upload a doc
+    await asyncio.sleep(0.1)  # http request to upload a doc
+
 
 # checkpoint by time
 async def on_event(partition_context, event):
     log.info(f"<check> batch process pid:{partition_context.partition_id} last sq {event.sequence_number}")
-    task = asyncio.create_task(upload_doc(event.body_as_str()))
+    await schedule_task(upload_doc(event.body_as_str()))  # at most MAX_UPLOAD_DOC_TASK_COUNT
     if getattr(partition_context, "last_checkpoint_time", time.time()) + 10 >= time.time():  # every 10 seconds
         await partition_context.update_checkpoint()
         partition_context.last_checkpoint_time = time.time()
+
 
 async def receive(client):
     await client.receive_batch(
         on_event=on_event,
         starting_position="-1",  # optional, "-1" is from the beginning of the partition.
     )
+
 
 async def main():
     checkpoint_store = BlobCheckpointStore.from_connection_string(STORAGE_CONNECTION_STR, BLOB_CONTAINER_NAME)
