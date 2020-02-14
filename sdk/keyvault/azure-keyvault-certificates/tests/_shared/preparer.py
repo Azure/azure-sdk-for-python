edacd442ebdce17b7f7e102e3ed51eff28db6bd4
Charles Lowell <chlowe@microsoft.com>
2020-02-14 12:13:57 -0800

    use a replay patch to prevent sleeping

--- sdk/keyvault/azure-keyvault-certificates/tests/_shared/preparer.py
+++ sdk/keyvault/azure-keyvault-certificates/tests/_shared/preparer.py
@@ -2,30 +2,17 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-import functools
-
 try:
     from unittest.mock import Mock
 except ImportError:  # python < 3.3
     from mock import Mock
 
 from azure.core.credentials import AccessToken
-from azure.core.pipeline.transport import RequestsTransport
 from azure.identity import EnvironmentCredential
-
 from devtools_testutils import AzureMgmtPreparer
 
 
-class NoSleepTransport(RequestsTransport):
-    """Prevents the transport from sleeping, e.g. to observe a Retry-After header"""
-
-    def sleep(self, _):
-        return
-
-
 class KeyVaultClientPreparer(AzureMgmtPreparer):
-    POLLING_METHOD_PREFIXES = ("begin_",)
-
     def __init__(self, client_cls, name_prefix="vault", random_name_enabled=True, **kwargs):
         super(KeyVaultClientPreparer, self).__init__(name_prefix, 24, random_name_enabled=random_name_enabled, **kwargs)
         self._client_cls = client_cls
@@ -36,27 +23,7 @@ class KeyVaultClientPreparer(AzureMgmtPreparer):
 
         return Mock(get_token=lambda *_: AccessToken("fake-token", 0))
 
-    def create_transport(self):
-        if self.is_live:
-            return RequestsTransport()
-
-        return NoSleepTransport()
-
     def create_resource(self, _, **kwargs):
         credential = self.create_credential()
-        self.client_kwargs["transport"] = self.create_transport()
-        client = self.create_client(kwargs.get("vault_uri"), credential)
-
+        client = self._client_cls(kwargs.get("vault_uri"), credential, **self.client_kwargs)
         return {"client": client}
-
-    def create_client(self, vault_uri, credential):
-        client = self._client_cls(vault_uri, credential, **self.client_kwargs)
-        if not self.is_live:
-            # ensure pollers don't sleep during playback
-            for attr in dir(client):
-                if any(attr.startswith(prefix) for prefix in self.POLLING_METHOD_PREFIXES):
-                    fn = getattr(client, attr)
-                    wrapper = functools.partial(fn, _polling_interval=0)
-                    setattr(client, attr, wrapper)
-
-        return client
