2c4943bd78c14ee800719b3b9484e9c1461e0ed5
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2020-02-21 16:36:00 -0800

    Install sdist from pre-built package (#9951)
    * Install sdist from prebuilt package

--- eng/tox/create_package_and_install.py
+++ eng/tox/create_package_and_install.py
@@ -5,8 +5,8 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-# This script is used to create and install the appropriate wheel for a tox environment.
-# it should be executed from tox with `{toxenvdir}/python` to ensure that the wheel
+# This script is used to create and install the appropriate package for a tox environment.
+# it should be executed from tox with `{toxenvdir}/python` to ensure that the package
 # can be successfully tested from within a tox environment.
 
 from subprocess import check_call
@@ -17,7 +17,7 @@ import sys
 import glob
 import shutil
 
-from tox_helper_tasks import find_whl, get_package_details
+from tox_helper_tasks import find_whl, find_sdist, get_package_details
 logging.getLogger().setLevel(logging.INFO)
 
 
@@ -30,51 +30,71 @@ def cleanup_build_artifacts(build_folder):
         shutil.rmtree(results[0])
 
     # clean up build results
-    shutil.rmtree(os.path.join(build_folder, "build"))
+    build_path = os.path.join(build_folder, "build")
+    if os.path.exists(build_path):
+        shutil.rmtree(build_path)
 
 
-def discover_whls(setuppy_path, args):
-    wheels = []
+def discover_packages(setuppy_path, args):
+    packages = []
     if os.getenv("PREBUILT_WHEEL_DIR") is not None and not args.force_create:
-        wheels = discover_prebuilt_whl(os.getenv("PREBUILT_WHEEL_DIR"), setuppy_path)
+        packages = discover_prebuilt_package(os.getenv("PREBUILT_WHEEL_DIR"), setuppy_path, args.package_type)
     else:
-        wheels = build_and_discover_whl(setuppy_path, args.distribution_directory, args.target_setup)
-    return wheels
+        packages = build_and_discover_package(setuppy_path, args.distribution_directory, args.target_setup, args.package_type)
+    return packages
 
 
-def discover_prebuilt_whl(whl_directory, setuppy_path):
-    wheels = []
+def discover_prebuilt_package(dist_directory, setuppy_path, package_type):
+    packages = []
     pkg_name, _, version = get_package_details(setuppy_path)
-    whl_name = find_whl(whl_directory, pkg_name, version)
-    if whl_name is None:
-        logging.error("whl is missing in prebuilt whl directory {0} for package {1} and version {2}".format(whl_directory, pkg_name, version))
+    if package_type == "wheel":
+        prebuilt_package = find_whl(dist_directory, pkg_name, version)
+    else:
+        prebuilt_package = find_sdist(dist_directory, pkg_name, version)
+
+    if prebuilt_package is None:
+        logging.error("Package is missing in prebuilt directory {0} for package {1} and version {2}".format(dist_directory, pkg_name, version))
         exit(1)
-    wheels.append(whl_name)
-    return wheels
+    packages.append(prebuilt_package)
+    return packages
 
 
-def build_and_discover_whl(setuppy_path, dist_dir, target_setup):
-    check_call(
-        [
-            sys.executable,
-            setuppy_path,
-            "bdist_wheel",
-            "-d",
-            dist_dir,
-        ]
-    )
+def build_and_discover_package(setuppy_path, dist_dir, target_setup, package_type):
 
-    wheels = [
-        f for f in os.listdir(args.distribution_directory) if f.endswith(".whl")
+    if package_type == "wheel":
+        check_call(
+            [
+                sys.executable,
+                setuppy_path,
+                "bdist_wheel",
+                "-d",
+                dist_dir,
+            ]
+        )
+    else:
+        check_call(
+            [
+                sys.executable,
+                setuppy_path,
+                "sdist",
+                "--format",
+                "zip",
+                "-d",
+                dist_dir,
+            ]
+        )
+
+    prebuilt_packages = [
+        f for f in os.listdir(args.distribution_directory) if f.endswith(".whl" if package_type == "wheel" else ".zip")
     ]
 
     cleanup_build_artifacts(target_setup)
-    return wheels
+    return prebuilt_packages
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Build a package directory into a wheel. Then install it."
+        description="Build a package directory into wheel or sdist. Then install it."
     )
     parser.add_argument(
         "-d",
@@ -88,7 +108,7 @@ if __name__ == "__main__":
         "-p",
         "--path-to-setup",
         dest="target_setup",
-        help="The path to the setup.py (not including the file) for the package we want to package into a wheel and install.",
+        help="The path to the setup.py (not including the file) for the package we want to package into a wheel/sdist and install.",
         required=True,
     )
 
@@ -132,45 +152,52 @@ if __name__ == "__main__":
         help="Force recreate whl even if it is prebuilt",
     )
 
+    parser.add_argument(
+        "--package-type",
+        dest="package_type",
+        help="Package type to build",
+        default="wheel",
+    )
+
 
     args = parser.parse_args()
 
     setup_py_path = os.path.join(args.target_setup, "setup.py")
-    discovered_wheels = discover_whls(setup_py_path, args)
+    discovered_packages = discover_packages(setup_py_path, args)
 
     if args.skip_install:
         logging.info(
             "Flag to skip install whl is passed. Skipping package installation"
         )
     else:
-        for wheel in discovered_wheels:
+        for built_package in discovered_packages:
             # if the environment variable is set, that means that this is running where we
-            # want to use the pre-built wheels
-            pkg_wheel_path = ""
+            # want to use the pre-built packages
+            built_pkg_path = ""
             if os.getenv("PREBUILT_WHEEL_DIR") is not None and not args.force_create:
-                # find the wheel in the set of prebuilt wheels
-                whl_path = os.path.join(os.environ["PREBUILT_WHEEL_DIR"], wheel)
-                if os.path.isfile(whl_path):
-                    pkg_wheel_path = whl_path
-                    logging.info("Installing {w} from wheel directory".format(w=wheel))
+                # find the prebuilt package in the set of prebuilt wheels
+                package_path = os.path.join(os.environ["PREBUILT_WHEEL_DIR"], built_package)
+                if os.path.isfile(package_path):
+                    built_pkg_path = package_path
+                    logging.info("Installing {w} from directory".format(w=built_package))
                 # it does't exist, so we need to error out
                 else:
                     logging.error(
-                        "{w} not present in the prebuilt wheels directory. Exiting.".format(
-                            w=wheel
+                        "{w} not present in the prebuilt package directory. Exiting.".format(
+                            w=built_package
                         )
                     )
                     exit(1)
             else:
-                pkg_wheel_path = os.path.abspath(os.path.join(args.distribution_directory, wheel))
-                logging.info("Installing {w} from fresh wheel.".format(w=wheel))
+                built_pkg_path = os.path.abspath(os.path.join(args.distribution_directory, built_package))
+                logging.info("Installing {w} from fresh built package.".format(w=built_package))
 
             commands = [
                 sys.executable,
                 "-m",
                 "pip",
                 "install",
-                pkg_wheel_path
+                built_pkg_path
             ]
 
 
@@ -190,4 +217,4 @@ if __name__ == "__main__":
                 check_call(commands, cwd= args.work_dir)
             else:
                 check_call(commands)
-            logging.info("Installed {w}".format(w=wheel))
+            logging.info("Installed {w}".format(w=built_package))
