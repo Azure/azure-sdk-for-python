da2110e1ebb4546842fb2f057352acdb6e2c827c
SDK Automation <sdkautomation@microsoft.com>
2020-02-27 20:20:36 +0000

    Generated from 7982d4edc92142d0605df0ae697bb61a46e06b70
    Revert the previous revert

--- sdk/resources/azure-mgmt-msi/azure/mgmt/msi/_managed_service_identity_client.py
+++ sdk/resources/azure-mgmt-msi/azure/mgmt/msi/_managed_service_identity_client.py
@@ -11,44 +11,12 @@
 
 from msrest.service_client import SDKClient
 from msrest import Serializer, Deserializer
-from msrestazure import AzureConfiguration
-from .version import VERSION
-from .operations.operations import Operations
-from .operations.user_assigned_identities_operations import UserAssignedIdentitiesOperations
+
+from ._configuration import ManagedServiceIdentityClientConfiguration
+from .operations import SystemAssignedIdentitiesOperations
+from .operations import Operations
+from .operations import UserAssignedIdentitiesOperations
 from . import models
-
-
-class ManagedServiceIdentityClientConfiguration(AzureConfiguration):
-    """Configuration for ManagedServiceIdentityClient
-    Note that all parameters used to create this instance are saved as instance
-    attributes.
-
-    :param credentials: Credentials needed for the client to connect to Azure.
-    :type credentials: :mod:`A msrestazure Credentials
-     object<msrestazure.azure_active_directory>`
-    :param subscription_id: The Id of the Subscription to which the identity
-     belongs.
-    :type subscription_id: str
-    :param str base_url: Service URL
-    """
-
-    def __init__(
-            self, credentials, subscription_id, base_url=None):
-
-        if credentials is None:
-            raise ValueError("Parameter 'credentials' must not be None.")
-        if subscription_id is None:
-            raise ValueError("Parameter 'subscription_id' must not be None.")
-        if not base_url:
-            base_url = 'https://management.azure.com'
-
-        super(ManagedServiceIdentityClientConfiguration, self).__init__(base_url)
-
-        self.add_user_agent('azure-mgmt-msi/{}'.format(VERSION))
-        self.add_user_agent('Azure-SDK-For-Python')
-
-        self.credentials = credentials
-        self.subscription_id = subscription_id
 
 
 class ManagedServiceIdentityClient(SDKClient):
@@ -57,6 +25,8 @@ class ManagedServiceIdentityClient(SDKClient):
     :ivar config: Configuration for client.
     :vartype config: ManagedServiceIdentityClientConfiguration
 
+    :ivar system_assigned_identities: SystemAssignedIdentities operations
+    :vartype system_assigned_identities: azure.mgmt.msi.operations.SystemAssignedIdentitiesOperations
     :ivar operations: Operations operations
     :vartype operations: azure.mgmt.msi.operations.Operations
     :ivar user_assigned_identities: UserAssignedIdentities operations
@@ -82,6 +52,8 @@ class ManagedServiceIdentityClient(SDKClient):
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
+        self.system_assigned_identities = SystemAssignedIdentitiesOperations(
+            self._client, self.config, self._serialize, self._deserialize)
         self.operations = Operations(
             self._client, self.config, self._serialize, self._deserialize)
         self.user_assigned_identities = UserAssignedIdentitiesOperations(
