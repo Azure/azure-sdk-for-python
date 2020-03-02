cfb9939767d1d18bbb1e84f9fc09194c69fdea5c
yijxie <yijun.xie@micrsofot.com>
2020-03-02 15:23:01 -0800

    Update sample for different version of storage service api

--- sdk/eventhub/azure-eventhub-checkpointstoreblob-aio/samples/event_processor_blob_storage_example.py
+++ sdk/eventhub/azure-eventhub-checkpointstoreblob-aio/samples/event_processor_blob_storage_example.py
@@ -6,6 +6,7 @@ from azure.eventhub.extensions.checkpointstoreblobaio import BlobCheckpointStore
 CONNECTION_STR = os.environ["EVENT_HUB_CONN_STR"]
 STORAGE_CONNECTION_STR = os.environ["AZURE_STORAGE_CONN_STR"]
 BLOB_CONTAINER_NAME = "your-blob-container-name"  # Please make sure the blob container resource exists.
+STORAGE_SERVICE_API_VERSION = "2019-02-02"
 
 
 async def on_event(partition_context, event):
@@ -23,7 +24,8 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     checkpoint_store = BlobCheckpointStore.from_connection_string(
         STORAGE_CONNECTION_STR,
-        container_name=BLOB_CONTAINER_NAME
+        container_name=BLOB_CONTAINER_NAME,
+        api_verison=STORAGE_SERVICE_API_VERSION  # api_version default value is "2019-02-02"
     )
     client = EventHubConsumerClient.from_connection_string(
         CONNECTION_STR,
--- sdk/eventhub/azure-eventhub-checkpointstoreblob/samples/event_processor_blob_storage_example.py
+++ sdk/eventhub/azure-eventhub-checkpointstoreblob/samples/event_processor_blob_storage_example.py
@@ -5,6 +5,7 @@ from azure.eventhub.extensions.checkpointstoreblob import BlobCheckpointStore
 CONNECTION_STR = os.environ["EVENT_HUB_CONN_STR"]
 STORAGE_CONNECTION_STR = os.environ["AZURE_STORAGE_CONN_STR"]
 BLOB_CONTAINER_NAME = "your-blob-container-name"  # Please make sure the blob container resource exists.
+STORAGE_SERVICE_API_VERSION = "2019-02-02"
 
 
 def on_event(partition_context, event):
@@ -17,7 +18,8 @@ def on_event(partition_context, event):
 if __name__ == '__main__':
     checkpoint_store = BlobCheckpointStore.from_connection_string(
         STORAGE_CONNECTION_STR,
-        container_name=BLOB_CONTAINER_NAME
+        container_name=BLOB_CONTAINER_NAME,
+        api_version=STORAGE_SERVICE_API_VERSION  # api_version default value is "2019-02-02"
     )
     client = EventHubConsumerClient.from_connection_string(
         CONNECTION_STR,
