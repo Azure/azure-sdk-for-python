e90deb22bf4d68108cfb4588e9ac384cbeae2970
Isabella Cai <iscai@microsoft.com>
2020-03-03 10:04:50 -0800

    removed test_length_with_emoji

--- sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_entities.py
+++ sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_entities.py
@@ -66,14 +66,6 @@ class TestRecognizeEntities(TextAnalyticsTest):
                 self.assertIsNotNone(entity.score)
 
     @GlobalTextAnalyticsAccountPreparer()
-    def test_length_with_emoji(self, resource_group, location, text_analytics_account, text_analytics_account_key):
-        text_analytics = TextAnalyticsClient(text_analytics_account, TextAnalyticsApiKeyCredential(text_analytics_account_key))
-
-        result = text_analytics.recognize_pii_entities(["👩 SSN: 123-12-1234"])
-        self.assertEqual(result[0].entities[0].grapheme_offset, 7)
-        self.assertEqual(result[0].entities[0].grapheme_length, 11)
-
-    @GlobalTextAnalyticsAccountPreparer()
     def test_passing_only_string(self, resource_group, location, text_analytics_account, text_analytics_account_key):
         text_analytics = TextAnalyticsClient(text_analytics_account, TextAnalyticsApiKeyCredential(text_analytics_account_key))
 
--- sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_entities_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_entities_async.py
@@ -85,15 +85,6 @@ class TestRecognizeEntities(AsyncTextAnalyticsTest):
 
     @GlobalTextAnalyticsAccountPreparer()
     @AsyncTextAnalyticsTest.await_prepared_test
-    async def test_length_with_emoji(self, resource_group, location, text_analytics_account, text_analytics_account_key):
-        text_analytics = TextAnalyticsClient(text_analytics_account, TextAnalyticsApiKeyCredential(text_analytics_account_key))
-
-        result = await text_analytics.recognize_pii_entities(["👩 SSN: 123-12-1234"])
-        self.assertEqual(result[0].entities[0].grapheme_offset, 7)
-        self.assertEqual(result[0].entities[0].grapheme_length, 11)
-
-    @GlobalTextAnalyticsAccountPreparer()
-    @AsyncTextAnalyticsTest.await_prepared_test
     async def test_passing_only_string(self, resource_group, location, text_analytics_account, text_analytics_account_key):
         text_analytics = TextAnalyticsClient(text_analytics_account, TextAnalyticsApiKeyCredential(text_analytics_account_key))
 
