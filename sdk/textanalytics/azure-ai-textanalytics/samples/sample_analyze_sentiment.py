6f839090cbd4e5d1b8c6381158eb2051d444d0ed
Isabella Cai <iscai@microsoft.com>
2020-03-04 15:40:20 -0800

    removed alternative scenario, rounding, and demonstrated grapheme in analyze_sentiment

--- sdk/textanalytics/azure-ai-textanalytics/samples/sample_analyze_sentiment.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/sample_analyze_sentiment.py
@@ -10,7 +10,7 @@
 FILE: sample_analyze_sentiment.py
 
 DESCRIPTION:
-    This sample demonstrates how to analyze sentiment in a batch of documents.
+    This sample demonstrates how to analyze sentiment in documents.
     An overall and per-sentence sentiment is returned.
 
 USAGE:
@@ -47,58 +47,21 @@ class AnalyzeSentimentSample(object):
             print("Document text: {}".format(documents[idx]))
             print("Overall sentiment: {}".format(doc.sentiment))
         # [END batch_analyze_sentiment]
-            print("Overall confidence scores: positive={0:.3f}; neutral={1:.3f}; negative={2:.3f} \n".format(
+            print("Overall confidence scores: positive={}; neutral={}; negative={} \n".format(
                 doc.confidence_scores.positive,
                 doc.confidence_scores.neutral,
                 doc.confidence_scores.negative,
             ))
             for idx, sentence in enumerate(doc.sentences):
                 print("Sentence {} sentiment: {}".format(idx+1, sentence.sentiment))
-                print("Sentence confidence scores: positive={0:.3f}; neutral={1:.3f}; negative={2:.3f}".format(
+                print("Sentence confidence scores: positive={}; neutral={}; negative={}".format(
                     sentence.confidence_scores.positive,
                     sentence.confidence_scores.neutral,
                     sentence.confidence_scores.negative,
                 ))
-                print("Offset: {}".format(sentence.grapheme_offset))
-                print("Length: {}\n".format(sentence.grapheme_length))
             print("------------------------------------")
-
-    def alternative_scenario_analyze_sentiment(self):
-        """This sample demonstrates how to retrieve batch statistics, the
-        model version used, and the raw response returned from the service.
-
-        It additionally shows an alternative way to pass in the input documents
-        using a list[TextDocumentInput] and supplying your own IDs and language hints along
-        with the text.
-        """
-        from azure.ai.textanalytics import TextAnalyticsClient, TextAnalyticsApiKeyCredential
-        text_analytics_client = TextAnalyticsClient(endpoint=self.endpoint, credential=TextAnalyticsApiKeyCredential(self.key))
-
-        documents = [
-            {"id": "0", "language": "en", "text": "I had the best day of my life."},
-            {"id": "1", "language": "en",
-             "text": "This was a waste of my time. The speaker put me to sleep."},
-            {"id": "2", "language": "es", "text": "No tengo dinero ni nada que dar..."},
-            {"id": "3", "language": "fr",
-             "text": "L'hôtel n'était pas très confortable. L'éclairage était trop sombre."}
-        ]
-
-        extras = []
-
-        def callback(resp):
-            extras.append(resp.statistics)
-            extras.append(resp.model_version)
-            extras.append(resp.raw_response)
-
-        result = text_analytics_client.analyze_sentiment(
-            documents,
-            show_stats=True,
-            model_version="latest",
-            raw_response_hook=callback
-        )
 
 
 if __name__ == '__main__':
     sample = AnalyzeSentimentSample()
     sample.analyze_sentiment()
-    sample.alternative_scenario_analyze_sentiment()
