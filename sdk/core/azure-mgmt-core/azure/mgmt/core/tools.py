b8da59445e942b24be925eeb91fbcac7a6028545
Laurent Mazuel <laurent.mazuel@gmail.com>
2020-03-06 10:25:19 -0800

    Add __all__ and black to tools

--- sdk/core/azure-mgmt-core/azure/mgmt/core/tools.py
+++ sdk/core/azure-mgmt-core/azure/mgmt/core/tools.py
@@ -33,13 +33,24 @@ import uuid
 
 _LOGGER = logging.getLogger(__name__)
 _ARMID_RE = re.compile(
-    '(?i)/subscriptions/(?P<subscription>[^/]*)(/resourceGroups/(?P<resource_group>[^/]*))?'
-    '(/providers/(?P<namespace>[^/]*)/(?P<type>[^/]*)/(?P<name>[^/]*)(?P<children>.*))?')
+    "(?i)/subscriptions/(?P<subscription>[^/]*)(/resourceGroups/(?P<resource_group>[^/]*))?"
+    "(/providers/(?P<namespace>[^/]*)/(?P<type>[^/]*)/(?P<name>[^/]*)(?P<children>.*))?"
+)
 
-_CHILDREN_RE = re.compile('(?i)(/providers/(?P<child_namespace>[^/]*))?/'
-                          '(?P<child_type>[^/]*)/(?P<child_name>[^/]*)')
+_CHILDREN_RE = re.compile(
+    "(?i)(/providers/(?P<child_namespace>[^/]*))?/"
+    "(?P<child_type>[^/]*)/(?P<child_name>[^/]*)"
+)
 
-_ARMNAME_RE = re.compile('^[^<>%&:\\?/]{1,260}$')
+_ARMNAME_RE = re.compile("^[^<>%&:\\?/]{1,260}$")
+
+
+__all__ = [
+    "parse_resource_id",
+    "resource_id",
+    "is_valid_resource_id",
+    "is_valid_resource_name",
+]
 
 
 def parse_resource_id(rid):
@@ -74,52 +85,67 @@ def parse_resource_id(rid):
     match = _ARMID_RE.match(rid)
     if match:
         result = match.groupdict()
-        children = _CHILDREN_RE.finditer(result['children'] or '')
+        children = _CHILDREN_RE.finditer(result["children"] or "")
         count = None
         for count, child in enumerate(children):
-            result.update({
-                key + '_%d' % (count + 1): group for key, group in child.groupdict().items()})
-        result['last_child_num'] = count + 1 if isinstance(count, int) else None
+            result.update(
+                {
+                    key + "_%d" % (count + 1): group
+                    for key, group in child.groupdict().items()
+                }
+            )
+        result["last_child_num"] = count + 1 if isinstance(count, int) else None
         result = _populate_alternate_kwargs(result)
     else:
         result = dict(name=rid)
     return {key: value for key, value in result.items() if value is not None}
+
 
 def _populate_alternate_kwargs(kwargs):
     """ Translates the parsed arguments into a format used by generic ARM commands
     such as the resource and lock commands.
     """
 
-    resource_namespace = kwargs['namespace']
-    resource_type = kwargs.get('child_type_{}'.format(kwargs['last_child_num'])) or kwargs['type']
-    resource_name = kwargs.get('child_name_{}'.format(kwargs['last_child_num'])) or kwargs['name']
+    resource_namespace = kwargs["namespace"]
+    resource_type = (
+        kwargs.get("child_type_{}".format(kwargs["last_child_num"])) or kwargs["type"]
+    )
+    resource_name = (
+        kwargs.get("child_name_{}".format(kwargs["last_child_num"])) or kwargs["name"]
+    )
 
     _get_parents_from_parts(kwargs)
-    kwargs['resource_namespace'] = resource_namespace
-    kwargs['resource_type'] = resource_type
-    kwargs['resource_name'] = resource_name
+    kwargs["resource_namespace"] = resource_namespace
+    kwargs["resource_type"] = resource_type
+    kwargs["resource_name"] = resource_name
     return kwargs
+
 
 def _get_parents_from_parts(kwargs):
     """ Get the parents given all the children parameters.
     """
     parent_builder = []
-    if kwargs['last_child_num'] is not None:
-        parent_builder.append('{type}/{name}/'.format(**kwargs))
-        for index in range(1, kwargs['last_child_num']):
-            child_namespace = kwargs.get('child_namespace_{}'.format(index))
+    if kwargs["last_child_num"] is not None:
+        parent_builder.append("{type}/{name}/".format(**kwargs))
+        for index in range(1, kwargs["last_child_num"]):
+            child_namespace = kwargs.get("child_namespace_{}".format(index))
             if child_namespace is not None:
-                parent_builder.append('providers/{}/'.format(child_namespace))
-            kwargs['child_parent_{}'.format(index)] = ''.join(parent_builder)
+                parent_builder.append("providers/{}/".format(child_namespace))
+            kwargs["child_parent_{}".format(index)] = "".join(parent_builder)
             parent_builder.append(
-                '{{child_type_{0}}}/{{child_name_{0}}}/'
-                .format(index).format(**kwargs))
-        child_namespace = kwargs.get('child_namespace_{}'.format(kwargs['last_child_num']))
+                "{{child_type_{0}}}/{{child_name_{0}}}/".format(index).format(**kwargs)
+            )
+        child_namespace = kwargs.get(
+            "child_namespace_{}".format(kwargs["last_child_num"])
+        )
         if child_namespace is not None:
-            parent_builder.append('providers/{}/'.format(child_namespace))
-        kwargs['child_parent_{}'.format(kwargs['last_child_num'])] = ''.join(parent_builder)
-    kwargs['resource_parent'] = ''.join(parent_builder) if kwargs['name'] else None
+            parent_builder.append("providers/{}/".format(child_namespace))
+        kwargs["child_parent_{}".format(kwargs["last_child_num"])] = "".join(
+            parent_builder
+        )
+    kwargs["resource_parent"] = "".join(parent_builder) if kwargs["name"] else None
     return kwargs
+
 
 def resource_id(**kwargs):
     """Create a valid resource id string from the given parts.
@@ -144,27 +170,30 @@ def resource_id(**kwargs):
     :rtype: str
     """
     kwargs = {k: v for k, v in kwargs.items() if v is not None}
-    rid_builder = ['/subscriptions/{subscription}'.format(**kwargs)]
+    rid_builder = ["/subscriptions/{subscription}".format(**kwargs)]
     try:
         try:
-            rid_builder.append('resourceGroups/{resource_group}'.format(**kwargs))
+            rid_builder.append("resourceGroups/{resource_group}".format(**kwargs))
         except KeyError:
             pass
-        rid_builder.append('providers/{namespace}'.format(**kwargs))
-        rid_builder.append('{type}/{name}'.format(**kwargs))
+        rid_builder.append("providers/{namespace}".format(**kwargs))
+        rid_builder.append("{type}/{name}".format(**kwargs))
         count = 1
         while True:
             try:
-                rid_builder.append('providers/{{child_namespace_{}}}'
-                                   .format(count).format(**kwargs))
+                rid_builder.append(
+                    "providers/{{child_namespace_{}}}".format(count).format(**kwargs)
+                )
             except KeyError:
                 pass
-            rid_builder.append('{{child_type_{0}}}/{{child_name_{0}}}'
-                               .format(count).format(**kwargs))
+            rid_builder.append(
+                "{{child_type_{0}}}/{{child_name_{0}}}".format(count).format(**kwargs)
+            )
             count += 1
     except KeyError:
         pass
-    return '/'.join(rid_builder)
+    return "/".join(rid_builder)
+
 
 def is_valid_resource_id(rid, exception_type=None):
     """Validates the given resource id.
