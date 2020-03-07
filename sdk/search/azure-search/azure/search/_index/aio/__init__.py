10dbc5b32a5c0e4a57d8bd9895ddeae24a657f7c
Bryan Van de Ven <brvandev@microsoft.com>
2020-03-06 16:28:43 -0800

    Azure Search Implementation (#9775)
    * Checkpoint
    
    * build config updates
    
    * feedback
    
    * add credential unit tests
    
    * add API to top level __init__
    
    * add IndexBatch unit tests
    
    * add unit tests for query wrappers
    
    * use standard service verbs
    
    * initial client tests
    
    * more tests
    
    * remove assert_called_once
    
    * moreclient unit tests
    
    * add async client impl
    
    * some docs work
    
    * README edits
    
    * add tracing decorators
    
    * mypy/pylint
    
    * add close, ctx manager methods
    
    * add index search/query examples
    
    * update version
    
    * add snippets to docstrings
    
    * add a live test
    
    * add basic crud samples
    
    * add initial recording
    
    * add more live tests
    
    * typos
    
    * black formatting
    
    * typo
    
    * hide index subpackage
    
    * re-record
    
    * move conftests to tests dir
    
    * don't use async list comp
    
    * update tests and new swagger
    
    * remove internal __all__
    
    * lint/black
    
    * IndexBatch -> IndexDocumentsBatch
    
    * update version to 11
    
    * formatting
    
    * typo
    
    * add odata function for escaping queries
    
    * update README
    
    * update README
    
    * update some docs
    
    * more docs tweaks
    
    * service_name -> endpoint
    
    * search_endpoint -> endpoint
    
    * re-gen swagger

---
+++ sdk/search/azure-search/azure/search/_index/aio/__init__.py
@@ -0,0 +1,7 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+from ._search_index_client_async import SearchIndexClient
+
+__all__ = ("SearchIndexClient",)
