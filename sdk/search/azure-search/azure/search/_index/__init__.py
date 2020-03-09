7346c3d2bb28776c0247a4477c59534be9762446
Bryan Van de Ven <brvandev@microsoft.com>
2020-03-09 15:50:48 -0700

    import ref docs

--- sdk/search/azure-search/azure/search/_index/__init__.py
+++ sdk/search/azure-search/azure/search/_index/__init__.py
@@ -6,6 +6,7 @@ from ._credential import SearchApiKeyCredential  # pylint: disable=unused-import
 from ._index_documents_batch import IndexDocumentsBatch  # pylint: disable=unused-import
 from ._search_index_client import (  # pylint: disable=unused-import
     odata,
+    SearchItemPaged,
     SearchIndexClient,
 )
 from ._queries import (  # pylint: disable=unused-import
