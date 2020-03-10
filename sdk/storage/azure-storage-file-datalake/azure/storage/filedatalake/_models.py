4e9bb38e9e0297c7d7b4b0140c329bfc53f844e7
Xiaoxi Fu <49707495+xiafu-msft@users.noreply.github.com>
2020-03-09 17:02:45 -0700

    [ADLS]Update AccessPolicy and ChangeLog (#10208)

--- sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_models.py
+++ sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_models.py
@@ -117,7 +117,7 @@ class DirectoryProperties(DictMixin):
     :ivar str etag: The ETag contains a value that you can use to perform operations
         conditionally.
     :ivar bool deleted: if the current directory marked as deleted
-    :ivar dict metadata: Name-value pairs associated with the blob as metadata.
+    :ivar dict metadata: Name-value pairs associated with the directory as metadata.
     :ivar ~azure.storage.filedatalake.LeaseProperties lease:
         Stores all the lease information for the directory.
     :ivar ~datetime.datetime last_modified:
@@ -164,7 +164,7 @@ class FileProperties(DictMixin):
     :ivar str etag: The ETag contains a value that you can use to perform operations
         conditionally.
     :ivar bool deleted: if the current file marked as deleted
-    :ivar dict metadata: Name-value pairs associated with the blob as metadata.
+    :ivar dict metadata: Name-value pairs associated with the file as metadata.
     :ivar ~azure.storage.filedatalake.LeaseProperties lease:
         Stores all the lease information for the file.
     :ivar ~datetime.datetime last_modified:
@@ -221,7 +221,7 @@ class PathProperties(object):
          read, write, or execute permission.  The sticky bit is also supported.
          Both symbolic (rwxrw-rw-) and 4-digit octal notation (e.g. 0766) are
          supported.
-    :ivar datetime last_modified:  A datetime object representing the last time the blob was modified.
+    :ivar datetime last_modified:  A datetime object representing the last time the directory/file was modified.
     :ivar bool is_directory: is the path a directory or not.
     :ivar str etag: The ETag contains a value that you can use to perform operations
         conditionally.
@@ -458,9 +458,51 @@ class FileSasPermissions(BlobSasPermissions):
 
 
 class AccessPolicy(BlobAccessPolicy):
-    def __init__(self, permission=None, start=None, expiry=None):
+    """Access Policy class used by the set and get access policy methods in each service.
+
+    A stored access policy can specify the start time, expiry time, and
+    permissions for the Shared Access Signatures with which it's associated.
+    Depending on how you want to control access to your resource, you can
+    specify all of these parameters within the stored access policy, and omit
+    them from the URL for the Shared Access Signature. Doing so permits you to
+    modify the associated signature's behavior at any time, as well as to revoke
+    it. Or you can specify one or more of the access policy parameters within
+    the stored access policy, and the others on the URL. Finally, you can
+    specify all of the parameters on the URL. In this case, you can use the
+    stored access policy to revoke the signature, but not to modify its behavior.
+
+    Together the Shared Access Signature and the stored access policy must
+    include all fields required to authenticate the signature. If any required
+    fields are missing, the request will fail. Likewise, if a field is specified
+    both in the Shared Access Signature URL and in the stored access policy, the
+    request will fail with status code 400 (Bad Request).
+
+    :param permission:
+        The permissions associated with the shared access signature. The
+        user is restricted to operations allowed by the permissions.
+        Required unless an id is given referencing a stored access policy
+        which contains this field. This field must be omitted if it has been
+        specified in an associated stored access policy.
+    :type permission: str or ~azure.storage.datalake.FileSystemSasPermissions
+    :param expiry:
+        The time at which the shared access signature becomes invalid.
+        Required unless an id is given referencing a stored access policy
+        which contains this field. This field must be omitted if it has
+        been specified in an associated stored access policy. Azure will always
+        convert values to UTC. If a date is passed in without timezone info, it
+        is assumed to be UTC.
+    :type expiry: ~datetime.datetime or str
+    :keyword start:
+        The time at which the shared access signature becomes valid. If
+        omitted, start time for this call is assumed to be the time when the
+        storage service receives the request. Azure will always convert values
+        to UTC. If a date is passed in without timezone info, it is assumed to
+        be UTC.
+    :paramtype start: ~datetime.datetime or str
+    """
+    def __init__(self, permission=None, expiry=None, **kwargs):
         super(AccessPolicy, self).__init__(
-            permission=permission, start=start, expiry=expiry,
+            permission=permission, expiry=expiry, start=kwargs.pop('start', None)
         )
 
 
