16618c062641500248f9ed17b8e4ff22950d988a
Daniel Jurek <djurek@microsoft.com>
2020-03-13 17:37:12 -0700

    Smoke Tests: enable using other clouds (#10200)
    * fix async tests and install async requirements
    
    * fix Identity for KV to support AZURE_AUTHORITY_HOST with a hostname (requires hostname without protocol wrapping)
    
    * add AZURE_AUTHORITY_HOST to tests.yml (this one uses a python-specific value because python uses just the host and doesn't use the URL like other languages)
    
    * update naming for authority host variable
    
    * add ability to use gov cloud
    
    * public -> gov for gov cloud instances, tweaks to async event hubs

--- common/smoketest/key_vault_keys_async.py
+++ common/smoketest/key_vault_keys_async.py
@@ -14,7 +14,8 @@ class KeyVaultKeys:
         # * AZURE_CLIENT_ID
         # * AZURE_CLIENT_SECRET
         # * AZURE_TENANT_ID
-        credential = DefaultAzureCredential()
+        authority_host = os.environ.get('AZURE_AUTHORITY_HOST') or KnownAuthorities.AZURE_PUBLIC_CLOUD
+        credential = DefaultAzureCredential(authority=authority_host)
         self.key_client = KeyClient(
             vault_url=os.environ["AZURE_PROJECT_URL"], credential=credential
         )
