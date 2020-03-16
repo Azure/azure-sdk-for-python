9866bcbc79baed48e440c942a89b715833a02032
SDK Automation <sdkautomation@microsoft.com>
2020-03-16 02:22:03 +0000

    Generated from 077843252b037763da806beed7b72543bb62b26d
    Deleted the preview versions of topologies and allowedConnections from the readme file at v3

--- sdk/security/azure-mgmt-security/azure/mgmt/security/operations/_device_security_groups_operations.py
+++ sdk/security/azure-mgmt-security/azure/mgmt/security/operations/_device_security_groups_operations.py
@@ -16,8 +16,8 @@ from msrestazure.azure_exceptions import CloudError
 from .. import models
 
 
-class AdaptiveApplicationControlsOperations(object):
-    """AdaptiveApplicationControlsOperations operations.
+class DeviceSecurityGroupsOperations(object):
+    """DeviceSecurityGroupsOperations operations.
 
     You should not instantiate directly this class, but create a Client instance that will create it for you and attach it as attribute.
 
@@ -25,7 +25,7 @@ class AdaptiveApplicationControlsOperations(object):
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
-    :ivar api_version: API version for the operation. Constant value: "2015-06-01-preview".
+    :ivar api_version: API version for the operation. Constant value: "2019-08-01".
     """
 
     models = models
@@ -35,96 +35,105 @@ class AdaptiveApplicationControlsOperations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
-        self.api_version = "2015-06-01-preview"
+        self.api_version = "2019-08-01"
 
         self.config = config
 
     def list(
-            self, include_path_recommendations=None, summary=None, custom_headers=None, raw=False, **operation_config):
-        """Gets a list of application control VM/server groups for the
-        subscription.
+            self, resource_id, custom_headers=None, raw=False, **operation_config):
+        """Use this method get the list of device security groups for the
+        specified IoT Hub resource.
 
-        :param include_path_recommendations: Include the policy rules
-        :type include_path_recommendations: bool
-        :param summary: Return output in a summarized form
-        :type summary: bool
+        :param resource_id: The identifier of the resource.
+        :type resource_id: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: AppWhitelistingGroups or ClientRawResponse if raw=true
-        :rtype: ~azure.mgmt.security.models.AppWhitelistingGroups or
-         ~msrest.pipeline.ClientRawResponse
+        :return: An iterator like instance of DeviceSecurityGroup
+        :rtype:
+         ~azure.mgmt.security.models.DeviceSecurityGroupPaged[~azure.mgmt.security.models.DeviceSecurityGroup]
         :raises: :class:`CloudError<msrestazure.azure_exceptions.CloudError>`
         """
-        # Construct URL
-        url = self.list.metadata['url']
-        path_format_arguments = {
-            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str', pattern=r'^[0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}$')
-        }
-        url = self._client.format_url(url, **path_format_arguments)
+        def prepare_request(next_link=None):
+            if not next_link:
+                # Construct URL
+                url = self.list.metadata['url']
+                path_format_arguments = {
+                    'resourceId': self._serialize.url("resource_id", resource_id, 'str')
+                }
+                url = self._client.format_url(url, **path_format_arguments)
 
-        # Construct parameters
-        query_parameters = {}
-        query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
-        if include_path_recommendations is not None:
-            query_parameters['includePathRecommendations'] = self._serialize.query("include_path_recommendations", include_path_recommendations, 'bool')
-        if summary is not None:
-            query_parameters['summary'] = self._serialize.query("summary", summary, 'bool')
+                # Construct parameters
+                query_parameters = {}
+                query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
 
-        # Construct headers
-        header_parameters = {}
-        header_parameters['Accept'] = 'application/json'
-        if self.config.generate_client_request_id:
-            header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
-        if custom_headers:
-            header_parameters.update(custom_headers)
-        if self.config.accept_language is not None:
-            header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
+            else:
+                url = next_link
+                query_parameters = {}
 
-        # Construct and send request
-        request = self._client.get(url, query_parameters, header_parameters)
-        response = self._client.send(request, stream=False, **operation_config)
+            # Construct headers
+            header_parameters = {}
+            header_parameters['Accept'] = 'application/json'
+            if self.config.generate_client_request_id:
+                header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
+            if custom_headers:
+                header_parameters.update(custom_headers)
+            if self.config.accept_language is not None:
+                header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
 
-        if response.status_code not in [200]:
-            exp = CloudError(response)
-            exp.request_id = response.headers.get('x-ms-request-id')
-            raise exp
+            # Construct and send request
+            request = self._client.get(url, query_parameters, header_parameters)
+            return request
 
-        deserialized = None
-        if response.status_code == 200:
-            deserialized = self._deserialize('AppWhitelistingGroups', response)
+        def internal_paging(next_link=None):
+            request = prepare_request(next_link)
 
+            response = self._client.send(request, stream=False, **operation_config)
+
+            if response.status_code not in [200]:
+                exp = CloudError(response)
+                exp.request_id = response.headers.get('x-ms-request-id')
+                raise exp
+
+            return response
+
+        # Deserialize response
+        header_dict = None
         if raw:
-            client_raw_response = ClientRawResponse(deserialized, response)
-            return client_raw_response
+            header_dict = {}
+        deserialized = models.DeviceSecurityGroupPaged(internal_paging, self._deserialize.dependencies, header_dict)
 
         return deserialized
-    list.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.Security/applicationWhitelistings'}
+    list.metadata = {'url': '/{resourceId}/providers/Microsoft.Security/deviceSecurityGroups'}
 
     def get(
-            self, group_name, custom_headers=None, raw=False, **operation_config):
-        """Gets an application control VM/server group.
+            self, resource_id, device_security_group_name, custom_headers=None, raw=False, **operation_config):
+        """Use this method to get the device security group for the specified IoT
+        Hub resource.
 
-        :param group_name: Name of an application control VM/server group
-        :type group_name: str
+        :param resource_id: The identifier of the resource.
+        :type resource_id: str
+        :param device_security_group_name: The name of the device security
+         group. Note that the name of the device security group is case
+         insensitive.
+        :type device_security_group_name: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: AppWhitelistingGroup or ClientRawResponse if raw=true
-        :rtype: ~azure.mgmt.security.models.AppWhitelistingGroup or
+        :return: DeviceSecurityGroup or ClientRawResponse if raw=true
+        :rtype: ~azure.mgmt.security.models.DeviceSecurityGroup or
          ~msrest.pipeline.ClientRawResponse
         :raises: :class:`CloudError<msrestazure.azure_exceptions.CloudError>`
         """
         # Construct URL
         url = self.get.metadata['url']
         path_format_arguments = {
-            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str', pattern=r'^[0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}$'),
-            'ascLocation': self._serialize.url("self.config.asc_location", self.config.asc_location, 'str'),
-            'groupName': self._serialize.url("group_name", group_name, 'str')
+            'resourceId': self._serialize.url("resource_id", resource_id, 'str'),
+            'deviceSecurityGroupName': self._serialize.url("device_security_group_name", device_security_group_name, 'str')
         }
         url = self._client.format_url(url, **path_format_arguments)
 
@@ -153,39 +162,44 @@ class AdaptiveApplicationControlsOperations(object):
 
         deserialized = None
         if response.status_code == 200:
-            deserialized = self._deserialize('AppWhitelistingGroup', response)
+            deserialized = self._deserialize('DeviceSecurityGroup', response)
 
         if raw:
             client_raw_response = ClientRawResponse(deserialized, response)
             return client_raw_response
 
         return deserialized
-    get.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.Security/locations/{ascLocation}/applicationWhitelistings/{groupName}'}
+    get.metadata = {'url': '/{resourceId}/providers/Microsoft.Security/deviceSecurityGroups/{deviceSecurityGroupName}'}
 
-    def put(
-            self, group_name, body, custom_headers=None, raw=False, **operation_config):
-        """Update an application control VM/server group.
+    def create_or_update(
+            self, resource_id, device_security_group_name, device_security_group, custom_headers=None, raw=False, **operation_config):
+        """Use this method to creates or updates the device security group on a
+        specified IoT Hub resource.
 
-        :param group_name: Name of an application control VM/server group
-        :type group_name: str
-        :param body: The updated VM/server group data
-        :type body: ~azure.mgmt.security.models.AppWhitelistingPutGroupData
+        :param resource_id: The identifier of the resource.
+        :type resource_id: str
+        :param device_security_group_name: The name of the device security
+         group. Note that the name of the device security group is case
+         insensitive.
+        :type device_security_group_name: str
+        :param device_security_group: Security group object.
+        :type device_security_group:
+         ~azure.mgmt.security.models.DeviceSecurityGroup
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: AppWhitelistingGroup or ClientRawResponse if raw=true
-        :rtype: ~azure.mgmt.security.models.AppWhitelistingGroup or
+        :return: DeviceSecurityGroup or ClientRawResponse if raw=true
+        :rtype: ~azure.mgmt.security.models.DeviceSecurityGroup or
          ~msrest.pipeline.ClientRawResponse
         :raises: :class:`CloudError<msrestazure.azure_exceptions.CloudError>`
         """
         # Construct URL
-        url = self.put.metadata['url']
+        url = self.create_or_update.metadata['url']
         path_format_arguments = {
-            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str', pattern=r'^[0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}$'),
-            'ascLocation': self._serialize.url("self.config.asc_location", self.config.asc_location, 'str'),
-            'groupName': self._serialize.url("group_name", group_name, 'str')
+            'resourceId': self._serialize.url("resource_id", resource_id, 'str'),
+            'deviceSecurityGroupName': self._serialize.url("device_security_group_name", device_security_group_name, 'str')
         }
         url = self._client.format_url(url, **path_format_arguments)
 
@@ -205,34 +219,40 @@ class AdaptiveApplicationControlsOperations(object):
             header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
 
         # Construct body
-        body_content = self._serialize.body(body, 'AppWhitelistingPutGroupData')
+        body_content = self._serialize.body(device_security_group, 'DeviceSecurityGroup')
 
         # Construct and send request
         request = self._client.put(url, query_parameters, header_parameters, body_content)
         response = self._client.send(request, stream=False, **operation_config)
 
-        if response.status_code not in [200]:
+        if response.status_code not in [200, 201]:
             exp = CloudError(response)
             exp.request_id = response.headers.get('x-ms-request-id')
             raise exp
 
         deserialized = None
         if response.status_code == 200:
-            deserialized = self._deserialize('AppWhitelistingGroup', response)
+            deserialized = self._deserialize('DeviceSecurityGroup', response)
+        if response.status_code == 201:
+            deserialized = self._deserialize('DeviceSecurityGroup', response)
 
         if raw:
             client_raw_response = ClientRawResponse(deserialized, response)
             return client_raw_response
 
         return deserialized
-    put.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.Security/locations/{ascLocation}/applicationWhitelistings/{groupName}'}
+    create_or_update.metadata = {'url': '/{resourceId}/providers/Microsoft.Security/deviceSecurityGroups/{deviceSecurityGroupName}'}
 
     def delete(
-            self, group_name, custom_headers=None, raw=False, **operation_config):
-        """Delete an application control VM/server group.
+            self, resource_id, device_security_group_name, custom_headers=None, raw=False, **operation_config):
+        """User this method to deletes the device security group.
 
-        :param group_name: Name of an application control VM/server group
-        :type group_name: str
+        :param resource_id: The identifier of the resource.
+        :type resource_id: str
+        :param device_security_group_name: The name of the device security
+         group. Note that the name of the device security group is case
+         insensitive.
+        :type device_security_group_name: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
@@ -245,9 +265,8 @@ class AdaptiveApplicationControlsOperations(object):
         # Construct URL
         url = self.delete.metadata['url']
         path_format_arguments = {
-            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str', pattern=r'^[0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}$'),
-            'ascLocation': self._serialize.url("self.config.asc_location", self.config.asc_location, 'str'),
-            'groupName': self._serialize.url("group_name", group_name, 'str')
+            'resourceId': self._serialize.url("resource_id", resource_id, 'str'),
+            'deviceSecurityGroupName': self._serialize.url("device_security_group_name", device_security_group_name, 'str')
         }
         url = self._client.format_url(url, **path_format_arguments)
 
@@ -268,7 +287,7 @@ class AdaptiveApplicationControlsOperations(object):
         request = self._client.delete(url, query_parameters, header_parameters)
         response = self._client.send(request, stream=False, **operation_config)
 
-        if response.status_code not in [200, 202, 204]:
+        if response.status_code not in [200, 204]:
             exp = CloudError(response)
             exp.request_id = response.headers.get('x-ms-request-id')
             raise exp
@@ -276,4 +295,4 @@ class AdaptiveApplicationControlsOperations(object):
         if raw:
             client_raw_response = ClientRawResponse(None, response)
             return client_raw_response
-    delete.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.Security/locations/{ascLocation}/applicationWhitelistings/{groupName}'}
+    delete.metadata = {'url': '/{resourceId}/providers/Microsoft.Security/deviceSecurityGroups/{deviceSecurityGroupName}'}
