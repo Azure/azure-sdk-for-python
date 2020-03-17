9962cfdf24f454a7ace78474d58677123c74583e
yijxie <yijun.xie@micrsofot.com>
2020-03-17 12:23:15 -0700

    add typing-extensions for 3.5.3

--- sdk/eventhub/azure-eventhub/setup.py
+++ sdk/eventhub/azure-eventhub/setup.py
@@ -74,6 +74,6 @@ setup(
     extras_require={
         ":python_version<'3.0'": ['azure-nspkg'],
         ":python_version<'3.5'": ["typing"],
-        ":python_version='3.5.3'": ["typing-extensions"],
+        ":python_version<'3.5.4'": ["typing-extensions"],
     }
 )
