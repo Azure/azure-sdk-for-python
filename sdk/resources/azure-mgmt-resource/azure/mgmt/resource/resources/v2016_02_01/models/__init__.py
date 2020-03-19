46fb1a3e6e7ee824679a0019ad58879da9d04cf7
SDK Automation <sdkautomation@microsoft.com>
2020-03-19 18:22:13 +0000

    Generated from e72378ace86be8c90a7b0a0cdcb5aab17efcfcd8
    Add api-version 2019-11-01 for resources/subscriptions

--- sdk/resources/azure-mgmt-resource/azure/mgmt/resource/resources/v2016_02_01/models/__init__.py
+++ sdk/resources/azure-mgmt-resource/azure/mgmt/resource/resources/v2016_02_01/models/__init__.py
@@ -28,6 +28,7 @@ try:
     from ._models_py3 import ErrorResponse
     from ._models_py3 import ExportTemplateRequest
     from ._models_py3 import GenericResource
+    from ._models_py3 import GenericResourceExpanded
     from ._models_py3 import GenericResourceFilter
     from ._models_py3 import HttpMessage
     from ._models_py3 import Identity
@@ -70,6 +71,7 @@ except (SyntaxError, ImportError):
     from ._models import ErrorResponse
     from ._models import ExportTemplateRequest
     from ._models import GenericResource
+    from ._models import GenericResourceExpanded
     from ._models import GenericResourceFilter
     from ._models import HttpMessage
     from ._models import Identity
@@ -95,7 +97,7 @@ except (SyntaxError, ImportError):
     from ._models import TemplateLink
 from ._paged_models import DeploymentExtendedPaged
 from ._paged_models import DeploymentOperationPaged
-from ._paged_models import GenericResourcePaged
+from ._paged_models import GenericResourceExpandedPaged
 from ._paged_models import ProviderPaged
 from ._paged_models import ResourceGroupPaged
 from ._paged_models import TagDetailsPaged
@@ -123,6 +125,7 @@ __all__ = [
     'ErrorResponse',
     'ExportTemplateRequest',
     'GenericResource',
+    'GenericResourceExpanded',
     'GenericResourceFilter',
     'HttpMessage',
     'Identity',
@@ -148,7 +151,7 @@ __all__ = [
     'TemplateLink',
     'DeploymentExtendedPaged',
     'ProviderPaged',
-    'GenericResourcePaged',
+    'GenericResourceExpandedPaged',
     'ResourceGroupPaged',
     'TagDetailsPaged',
     'DeploymentOperationPaged',
