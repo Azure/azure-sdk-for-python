c38ceaf78ca9e1df046ab348add7b8c4c4fd9a51
SDK Automation <sdkautomation@microsoft.com>
2020-03-20 04:38:32 +0000

    Generated from fc67c2be909891f6b6e3a532f46f3477c0620c6d
    update package name and output folder in readme.typescript.md

--- sdk/support/azure-mgmt-support/azure/mgmt/support/_configuration.py
+++ sdk/support/azure-mgmt-support/azure/mgmt/support/_configuration.py
@@ -21,7 +21,7 @@ class MicrosoftSupportConfiguration(AzureConfiguration):
     :param credentials: Credentials needed for the client to connect to Azure.
     :type credentials: :mod:`A msrestazure Credentials
      object<msrestazure.azure_active_directory>`
-    :param subscription_id: Azure subscription id
+    :param subscription_id: Azure subscription Id.
     :type subscription_id: str
     :param str base_url: Service URL
     """
