53511f23fbca8f5d31a08cb5ddffc79406c10abe
Krista Pratico <krpratic@microsoft.com>
2020-03-24 18:27:29 -0700

    try adding single test

---
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_receipt.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+from azure.ai.formrecognizer import (
+    FormRecognizerClient,
+    FormRecognizerApiKeyCredential
+)
+from testcase import FormRecognizerTest, GlobalFormRecognizerAccountPreparer
+
+
+class TestReceipt(FormRecognizerTest):
+
+    @GlobalFormRecognizerAccountPreparer()
+    def test_receipt(self, resource_group, location, form_recognizer_account, form_recognizer_account_key):
+        client = FormRecognizerClient(form_recognizer_account, FormRecognizerApiKeyCredential(form_recognizer_account_key))
+
+        response = client.begin_extract_receipts(
+            form="https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/contoso-allinone.jpg",
+            include_text_details=True
+        )
+
+        result = response.result()
+        self.assertIsNotNone(result)
