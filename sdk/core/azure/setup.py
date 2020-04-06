9da33e39858e1dad840226acfabd03f4519b5b1e
Laurent Mazuel <laurent.mazuel@gmail.com>
2020-04-06 11:44:34 -0700

    Missing MD marker

--- sdk/core/azure/setup.py
+++ sdk/core/azure/setup.py
@@ -37,6 +37,7 @@ setup(
     version='5.0.0',
     description='Microsoft Azure Client Libraries for Python',
     long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     license='MIT License',
     author='Microsoft Corporation',
     author_email='azpysdkhelp@microsoft.com',
