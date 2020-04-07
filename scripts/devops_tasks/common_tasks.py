b0dcd9bb6b0455da55eced9ebf73a1f6b67e9ebb
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2020-04-06 17:52:55 -0700

    Review comment fix

--- scripts/devops_tasks/common_tasks.py
+++ scripts/devops_tasks/common_tasks.py
@@ -38,6 +38,8 @@ OMITTED_CI_PACKAGES = [
     "azure-mgmt-documentdb",
     "azure-servicemanagement-legacy",
     "azure-mgmt-scheduler",
+    "azure",
+    "azure-mgmt",
 ]
 MANAGEMENT_PACKAGE_IDENTIFIERS = [
     "mgmt",
@@ -48,7 +50,6 @@ MANAGEMENT_PACKAGE_IDENTIFIERS = [
     "azure-synapse"
 ]
 META_PACKAGES = ["azure", "azure-mgmt", "azure-keyvault"]
-DEPRECATED_META_PACKAGES = ["azure", "azure-mgmt",]
 REGRESSION_EXCLUDED_PACKAGES = [
     "azure-common",
 ]
@@ -213,13 +214,12 @@ def process_glob_string(
         collected_top_level_directories.extend([os.path.dirname(p) for p in globbed])
 
     # dedup, in case we have double coverage from the glob strings. Example: "azure-mgmt-keyvault,azure-mgmt-*"
-    # remove deprecated packages always
     collected_directories = list(
         set(
             [
                 p
                 for p in collected_top_level_directories
-                if additional_contains_filter in p and os.path.basename(p) not in DEPRECATED_META_PACKAGES
+                if additional_contains_filter in p
             ]
         )
     )
