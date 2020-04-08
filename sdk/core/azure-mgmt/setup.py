3956d48fd51ac4cd6024d8f9a007b86724ec2e88
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2020-04-08 15:50:55 -0700

    `azure` and `azure-mgmt` buildable but not installable. (#10742)
    * make azure and azure-mgmt metapackages _buildable_, but not _installable_
    
    * missing import

--- sdk/core/azure-mgmt/setup.py
+++ sdk/core/azure-mgmt/setup.py
@@ -7,7 +7,7 @@
 #--------------------------------------------------------------------------
 
 from setuptools import setup
-
+import sys
 
 message = """
 
@@ -29,29 +29,30 @@ https://github.com/Azure/azure-sdk-for-python/issues/10646
 
 """
 
-raise RuntimeError(message)
-
-setup(
-    name='azure-mgmt',
-    version='5.0.0',
-    description='Microsoft Azure Resource Management Client Libraries for Python',
-    long_description=open('README.md', 'r').read(),
-    long_description_content_type='text/markdown',
-    license='MIT License',
-    author='Microsoft Corporation',
-    author_email='azpysdkhelp@microsoft.com',
-    url='https://github.com/Azure/azure-sdk-for-python',
-    classifiers=[
-        'Development Status :: 7 - Inactive',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'License :: OSI Approved :: MIT License',
-    ],
-    zip_safe=False,
-)
+if "sdist" in sys.argv:
+    setup(
+        name='azure-mgmt',
+        version='5.0.0',
+        description='Microsoft Azure Resource Management Client Libraries for Python',
+        long_description=open('README.md', 'r').read(),
+        long_description_content_type='text/markdown',
+        license='MIT License',
+        author='Microsoft Corporation',
+        author_email='azpysdkhelp@microsoft.com',
+        url='https://github.com/Azure/azure-sdk-for-python',
+        classifiers=[
+            'Development Status :: 7 - Inactive',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'License :: OSI Approved :: MIT License',
+        ],
+        zip_safe=False,
+    )
+else:
+    raise RuntimeError(message)
