a361296a65081c5236fdbbcf26f1a1edeada9eb4
Zim Kalinowski <zikalino@microsoft.com>
2020-04-14 11:45:44 +0200

    Extending script (#10822)
    * merged scripts
    
    * updated scripts
    
    * calculating total rps and packages
    
    * don't print packages

--- tools/azure-sdk-tools/devtools_testutils/mgmt_test_stats.py
+++ tools/azure-sdk-tools/devtools_testutils/mgmt_test_stats.py
@@ -8,12 +8,14 @@ print("|----------------------------------------|------------|")
 pattern = ".\/sdk\/[a-z]+\/azure-mgmt-[a-z-]+$" 
 dirs = [x[0] for x in walk(".") if re.match(pattern, x[0])]
 dirs.sort(key=lambda x: x.split("/").pop())
-total = 0
+total_rps = 0
+total_packages = 0
 manual = 0
 auto = 0
 none = 0
+all = {}
 for d in dirs:
-  total += 1
+  total_packages += 1
   coverage = "-"
   test_dir = join(d, "tests")
   if exists(test_dir):
@@ -21,9 +23,9 @@ for d in dirs:
     has_manual = False
     has_auto = False
     for t in test_files:
-      coverage = "MANUAL"
+      coverage = "M"
       if t.startswith("test_cli_"):
-        coverage = "AUTO"
+        coverage = "A"
         with open(join(test_dir, t)) as f:
           content = f.readlines()
           coverage = [x for x in content if x.startswith("# Coverage %      :")]
@@ -35,16 +37,52 @@ for d in dirs:
     auto += 1 if has_auto else 0
   else:
     none += 1
+  all[d.split("/").pop()] = coverage
 
-  print("| {:38} | {:10} |".format(d.split("/").pop(), coverage if coverage in ["-" , "AUTO", "MANUAL"] else "{:4.2f}".format(coverage)))
+if exists("../azure-rest-api-specs/specification"):
+  pattern = ".+/resource-manager$"
+  dirs = [x[0] for x in walk("../azure-rest-api-specs/specification") if re.match(pattern, x[0])]
+  total = 0
+  have_python = 0
+  packages = []
+  missing = []
+  for d in dirs:
+    rp_name = d.split('/')[-2]
+    total_rps += 1
+    readme_files = [f for f in listdir(d) if f in ['readme.md', 'readme.python.md']]
+    readme_file = None
+    if 'readme.python.md' in readme_files:
+      readme_file = join(d, 'readme.python.md')
+    elif 'readme.md' in readme_files:
+      readme_file = join(d, 'readme.md')
+
+    if readme_file is None:
+      all[rp_name] = "- (R)"
+    else:
+      with open(readme_file, encoding='utf8') as f:
+        content = f.readlines()
+        package_name = [x for x in content if x.strip().startswith("package-name: ")]
+        if len(package_name) > 0:
+          have_python += 1
+          package_name = package_name[0].split(': ')[1].rstrip()
+          if package_name not in all:
+            all[package_name] = '- (S)'
+        else:
+          all[rp_name] = "- (R)"
+
+packages = sorted([x for x in all.keys()])
+for p in packages:
+  coverage = all[p]
+  print("| {:38} | {:10} |".format(p, coverage if coverage in ["- (R)", "- (S)", "-" , "A", "M"] else "{:4.2f}".format(coverage)))
 
 print("| {:38} | {:10} |".format("", ""))
-print("| {:38} | {:10} |".format("**TOTAL**", "**{}**".format(total)))
+print("| {:38} | {:10} |".format("**TOTAL RPS**", "**{}**".format(total_rps)))
+print("| {:38} | {:10} |".format("**TOTAL PACKAGES**", "**{}**".format(total_packages)))
 print("| {:38} | {:10} |".format("**MANUAL**", "**{}**".format(manual)))
-print("| {:38} | {:10} |".format("**MANUAL %**", "**{:4.2f}**".format(100 * manual / total)))
+print("| {:38} | {:10} |".format("**MANUAL %**", "**{:4.2f}**".format(100 * manual / total_packages)))
 print("| {:38} | {:10} |".format("**AUTO**", "**{}**".format(auto)))
-print("| {:38} | {:10} |".format("**AUTO %**", "**{:4.2f}**".format(100 * auto / total)))
+print("| {:38} | {:10} |".format("**AUTO %**", "**{:4.2f}**".format(100 * auto / total_packages)))
 print("| {:38} | {:10} |".format("**NONE**", "**{}**".format(none)))
-print("| {:38} | {:10} |".format("**NONE %**", "**{:4.2f}**".format(100 * none / total)))
+print("| {:38} | {:10} |".format("**NONE %**", "**{:4.2f}**".format(100 * none / total_packages)))
 
 
