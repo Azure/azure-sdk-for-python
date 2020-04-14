ad547198f3bdfd5532005c6805a0fd842ed0dd44
iscai-msft <iscai@microsoft.com>
2020-04-14 14:36:58 -0400

    added label data differentiation

--- sdk/formrecognizer/azure-ai-formrecognizer/samples/async_samples/sample_differentiate_custom_forms_with_labelled_and_unlabelled_models_async.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/samples/async_samples/sample_differentiate_custom_forms_with_labelled_and_unlabelled_models_async.py
@@ -18,8 +18,8 @@ USAGE:
     Set the environment variables with your own values before running the sample:
     1) AZURE_FORM_RECOGNIZER_ENDPOINT - the endpoint to your Cognitive Services resource.
     2) AZURE_FORM_RECOGNIZER_KEY - your Form Recognizer subscription key
-    3) CUSTOM_TRAINED_labeled_MODEL_ID - the ID of your custom model trained with labeled data
-    4) CUSTOM_TRAINED_UNlabeled_MODEL_ID - the ID of your custom model trained with unlabeled data
+    3) CUSTOM_TRAINED_LABELED_MODEL_ID - the ID of your custom model trained with labeled data
+    4) CUSTOM_TRAINED_UNLABELED_MODEL_ID - the ID of your custom model trained with unlabeled data
 """
 
 import os
@@ -30,8 +30,8 @@ class DifferentiateCustomFormsWithlabeledAndUnlabeledModelsAsync(object):
 
     endpoint = os.environ["AZURE_FORM_RECOGNIZER_ENDPOINT"]
     key = os.environ["AZURE_FORM_RECOGNIZER_KEY"]
-    labeled_model_id = os.environ["CUSTOM_TRAINED_labeled_MODEL_ID"]
-    unlabeled_model_id = os.environ["CUSTOM_TRAINED_UNlabeled_MODEL_ID"]
+    labeled_model_id = os.environ["CUSTOM_TRAINED_LABELED_MODEL_ID"]
+    unlabeled_model_id = os.environ["CUSTOM_TRAINED_UNLABELED_MODEL_ID"]
 
     async def recognize_custom_forms(self):
         from azure.core.credentials import AzureKeyCredential
@@ -51,17 +51,38 @@ class DifferentiateCustomFormsWithlabeledAndUnlabeledModelsAsync(object):
             # The main difference is found in the labels of its fields
             # The form recognized with a labeled model will have the labels it was trained with,
             # the unlabeled one will be denoted with indices
+            print("--------Recognizing forms with labeled custom model--------")
             for labeled_form in forms_with_labeled_model:
                 for label, field in labeled_form.fields.items():
-                    print("Field '{}' has value '{}' with a confidence score of {}".format(
-                            label, field.value, field.confidence
-                        ))
+                    # With your labeled custom model, you will not get back label data but will get back value data
+                    # This is because your custom model didn't have to use any machine learning to deduce the label,
+                    # the label was directly provided to it
+                    print("Field '{}' has value '{}' based on '{}' within bounding box '{}', with a confidence score of {}".format(
+                        label,
+                        field.value,
+                        field.value_data.text,
+                        ", ".join(["[{}, {}]".format(p.x, p.y) for p in field.value_data.bounding_box]) if field.value_data.bounding_box else "N/A",
+                        field.confidence
+                    ))
 
+            print("-----------------------------------------------------------")
+            print("-------Recognizing forms with unlabeled custom model-------")
             for unlabeled_form in forms_with_unlabeled_model:
                 for label, field in unlabeled_form.fields.items():
-                    print("Field '{}' has value '{}' with a confidence score of {}".format(
-                            label, field.value, field.confidence
-                        ))
+                    # The unlabeled custom model will also include data about your labels
+                    print("Field '{}' has label '{}' within bounding box '{}', with a confidence score of {}".format(
+                        label,
+                        field.label_data.text,
+                        ", ".join(["[{}, {}]".format(p.x, p.y) for p in field.label_data.bounding_box]) if field.label_data.bounding_box else "N/A",
+                        field.confidence
+                    ))
+                    print("Field '{}' has value '{}' based on '{}' within bounding box '{}', with a confidence score of {}".format(
+                        label,
+                        field.value,
+                        field.value_data.text,
+                        ", ".join(["[{}, {}]".format(p.x, p.y) for p in field.value_data.bounding_box]) if field.value_data.bounding_box else "N/A",
+                        field.confidence
+                    ))
 
 
 async def main():
