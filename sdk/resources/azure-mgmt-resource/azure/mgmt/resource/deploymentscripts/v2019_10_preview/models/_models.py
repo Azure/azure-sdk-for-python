2ae56c6a408a61571aa62741faa96714df0ea1e2
SDK Automation <sdkautomation@microsoft.com>
2020-04-15 18:57:47 +0000

    Generated from 7d36862eb495034ab93d34e60e9116b24d3da204
    Update readme.python.md

--- sdk/resources/azure-mgmt-resource/azure/mgmt/resource/deploymentscripts/v2019_10_preview/models/_models.py
+++ sdk/resources/azure-mgmt-resource/azure/mgmt/resource/deploymentscripts/v2019_10_preview/models/_models.py
@@ -475,6 +475,35 @@ class DeploymentScriptPropertiesBase(Model):
         self.outputs = None
 
 
+class DeploymentScriptsError(Model):
+    """Deployment scripts error response.
+
+    :param error:
+    :type error:
+     ~azure.mgmt.resource.deploymentscripts.v2019_10_preview.models.ErrorResponse
+    """
+
+    _attribute_map = {
+        'error': {'key': 'error', 'type': 'ErrorResponse'},
+    }
+
+    def __init__(self, **kwargs):
+        super(DeploymentScriptsError, self).__init__(**kwargs)
+        self.error = kwargs.get('error', None)
+
+
+class DeploymentScriptsErrorException(HttpOperationError):
+    """Server responsed with exception of type: 'DeploymentScriptsError'.
+
+    :param deserialize: A deserializer
+    :param response: Server response to be deserialized.
+    """
+
+    def __init__(self, deserialize, response, *args):
+
+        super(DeploymentScriptsErrorException, self).__init__(deserialize, response, 'DeploymentScriptsError', *args)
+
+
 class DeploymentScriptUpdateParameter(AzureResourceBase):
     """Deployment script parameters to be updated. .
 
@@ -507,35 +536,6 @@ class DeploymentScriptUpdateParameter(AzureResourceBase):
     def __init__(self, **kwargs):
         super(DeploymentScriptUpdateParameter, self).__init__(**kwargs)
         self.tags = kwargs.get('tags', None)
-
-
-class DeploymentScriptsError(Model):
-    """Deployment scripts error response.
-
-    :param error:
-    :type error:
-     ~azure.mgmt.resource.deploymentscripts.v2019_10_preview.models.ErrorResponse
-    """
-
-    _attribute_map = {
-        'error': {'key': 'error', 'type': 'ErrorResponse'},
-    }
-
-    def __init__(self, **kwargs):
-        super(DeploymentScriptsError, self).__init__(**kwargs)
-        self.error = kwargs.get('error', None)
-
-
-class DeploymentScriptsErrorException(HttpOperationError):
-    """Server responsed with exception of type: 'DeploymentScriptsError'.
-
-    :param deserialize: A deserializer
-    :param response: Server response to be deserialized.
-    """
-
-    def __init__(self, deserialize, response, *args):
-
-        super(DeploymentScriptsErrorException, self).__init__(deserialize, response, 'DeploymentScriptsError', *args)
 
 
 class EnvironmentVariable(Model):
