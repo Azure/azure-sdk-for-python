3866a93bef95a0b7c9f03d2e9750df21d884c981
Xiang Yan <xiangsjtu@gmail.com>
2020-04-17 18:19:59 -0700

    update mac tests

--- sdk/identity/azure-identity/tests/test_macos_vscode_credential.py
+++ sdk/identity/azure-identity/tests/test_macos_vscode_credential.py
@@ -5,6 +5,7 @@
 import sys
 import pytest
 from azure.core.credentials import AccessToken
+from azure.identity import CredentialUnavailableError
 from azure.core.pipeline.policies import SansIOHTTPPolicy
 from azure.identity._internal.user_agent import USER_AGENT
 from helpers import build_aad_response, mock_response, Request, validating_transport
@@ -14,8 +15,10 @@ try:
 except (ImportError, OSError):
     pass
 try:
+    from unittest import mock
     from unittest.mock import Mock
 except ImportError:  # python < 3.3
+    import mock
     from mock import Mock  # type: ignore
 
 @pytest.mark.skipif(not sys.platform.startswith('darwin'), reason="This test only runs on MacOS")
@@ -29,50 +32,46 @@ def test_no_scopes():
 
 @pytest.mark.skipif(not sys.platform.startswith('darwin'), reason="This test only runs on MacOS")
 def test_policies_configurable():
-    key_chain = Keychain()
-    key_chain.set_generic_password("VS Code Azure", "Azure", "value")
-
     policy = Mock(spec_set=SansIOHTTPPolicy, on_request=Mock())
 
     def send(*_, **__):
         return mock_response(json_payload=build_aad_response(access_token="**"))
 
-    credential = MacOSVSCodeCredential(policies=[policy], transport=Mock(send=send))
-
-    credential.get_token("scope")
-
-    assert policy.on_request.called
+    with mock.patch('Keychain.get_generic_password', return_value="VALUE"):
+        credential = MacOSVSCodeCredential(policies=[policy], transport=Mock(send=send))
+        credential.get_token("scope")
+        assert policy.on_request.called
 
 
 @pytest.mark.skipif(not sys.platform.startswith('darwin'), reason="This test only runs on MacOS")
 def test_user_agent():
-    key_chain = Keychain()
-    key_chain.set_generic_password("VS Code Azure", "Azure", "value")
-
     transport = validating_transport(
         requests=[Request(required_headers={"User-Agent": USER_AGENT})],
         responses=[mock_response(json_payload=build_aad_response(access_token="**"))],
     )
 
-    credential = MacOSVSCodeCredential(transport=transport)
+    with mock.patch('Keychain.get_generic_password', return_value="VALUE"):
+        credential = MacOSVSCodeCredential(transport=transport)
+        credential.get_token("scope")
 
-    credential.get_token("scope")
+
+@pytest.mark.skipif(not sys.platform.startswith('darwin'), reason="This test only runs on MacOS")
+def test_credential_unavailable_error():
+    with mock.patch('Keychain.get_generic_password', return_value=None):
+        credential = MacOSVSCodeCredential()
+        with pytest.raises(CredentialUnavailableError):
+            credential.get_token("scope")
 
 
 @pytest.mark.skipif(not sys.platform.startswith('darwin'), reason="This test only runs on MacOS")
 def test_get_token():
-    key_chain = Keychain()
-    key_chain.set_generic_password("VS Code Azure", "Azure", "value")
-
     expected_token = AccessToken("token", 42)
 
     mock_client = Mock(spec=object)
     mock_client.obtain_token_by_refresh_token = Mock(return_value=expected_token)
 
-    credential = MacOSVSCodeCredential(
-        _client=mock_client,
-    )
-
-    token = credential.get_token("scope")
-    assert token is expected_token
-    assert mock_client.obtain_token_by_refresh_token.call_count == 1
+    with mock.patch('Keychain.get_generic_password', return_value="VALUE"):
+        credential = MacOSVSCodeCredential(_client=mock_client)
+        token = credential.get_token("scope")
+        assert token is expected_token
+        assert mock_client.obtain_token_by_refresh_token.call_count == 1
--- sdk/identity/azure-identity/tests/test_macos_vscode_credential_async.py
+++ sdk/identity/azure-identity/tests/test_macos_vscode_credential_async.py
@@ -4,11 +4,13 @@
 # ------------------------------------
 import sys
 import pytest
+from azure.identity import CredentialUnavailableError
 from azure.core.credentials import AccessToken
 from azure.identity._internal.user_agent import USER_AGENT
 from azure.core.pipeline.policies import SansIOHTTPPolicy
 from helpers import build_aad_response, mock_response, Request
 from helpers_async import async_validating_transport, AsyncMockTransport, wrap_in_future
+from unittest import mock
 from unittest.mock import Mock
 try:
     from azure.identity.aio._credentials.macos_vscode_credential import MacOSVSCodeCredential
@@ -30,52 +32,48 @@ async def test_no_scopes():
 @pytest.mark.skipif(not sys.platform.startswith('darwin'), reason="This test only runs on MacOS")
 @pytest.mark.asyncio
 async def test_policies_configurable():
-    key_chain = Keychain()
-    key_chain.set_generic_password("VS Code Azure", "Azure", "value")
-
     policy = Mock(spec_set=SansIOHTTPPolicy, on_request=Mock())
 
     async def send(*_, **__):
         return mock_response(json_payload=build_aad_response(access_token="**"))
 
-    credential = MacOSVSCodeCredential(policies=[policy], transport=Mock(send=send))
-
-    await credential.get_token("scope")
-
-    assert policy.on_request.called
+    with mock.patch('Keychain.get_generic_password', return_value="VALUE"):
+        credential = MacOSVSCodeCredential(policies=[policy], transport=Mock(send=send))
+        await credential.get_token("scope")
+        assert policy.on_request.called
 
 
 @pytest.mark.skipif(not sys.platform.startswith('darwin'), reason="This test only runs on MacOS")
 @pytest.mark.asyncio
 async def test_user_agent():
-    key_chain = Keychain()
-    key_chain.set_generic_password("VS Code Azure", "Azure", "value")
-
     transport = async_validating_transport(
         requests=[Request(required_headers={"User-Agent": USER_AGENT})],
         responses=[mock_response(json_payload=build_aad_response(access_token="**"))],
     )
 
-    credential = MacOSVSCodeCredential(transport=transport)
+    with mock.patch('Keychain.get_generic_password', return_value="VALUE"):
+        credential = MacOSVSCodeCredential(transport=transport)
+        await credential.get_token("scope")
 
-    await credential.get_token("scope")
+@pytest.mark.skipif(not sys.platform.startswith('darwin'), reason="This test only runs on MacOS")
+@pytest.mark.asyncio
+async def test_credential_unavailable_error():
+    with mock.patch('Keychain.get_generic_password', return_value=None):
+        credential = MacOSVSCodeCredential()
+        with pytest.raises(CredentialUnavailableError):
+            await credential.get_token("scope")
 
 
 @pytest.mark.skipif(not sys.platform.startswith('darwin'), reason="This test only runs on MacOS")
 @pytest.mark.asyncio
 async def test_get_token():
-    key_chain = Keychain()
-    key_chain.set_generic_password("VS Code Azure", "Azure", "value")
-
     expected_token = AccessToken("token", 42)
 
     mock_client = Mock(spec=object)
     token_by_refresh_token = Mock(return_value=expected_token)
     mock_client.obtain_token_by_refresh_token = wrap_in_future(token_by_refresh_token)
 
-    credential = MacOSVSCodeCredential(
-        _client=mock_client,
-    )
-
-    token = await credential.get_token("scope")
-    assert token is expected_token
+    with mock.patch('Keychain.get_generic_password', return_value="VALUE"):
+        credential = MacOSVSCodeCredential(_client=mock_client)
+        token = await credential.get_token("scope")
+        assert token is expected_token
