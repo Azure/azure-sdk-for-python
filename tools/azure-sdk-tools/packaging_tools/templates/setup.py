e2aba71544ba84ba4e7bf1b99fe75e7e12ae216b
Zim Kalinowski <zikalino@microsoft.com>
2020-04-19 06:25:37 +0200

    updated setup template

--- tools/azure-sdk-tools/packaging_tools/templates/setup.py
+++ tools/azure-sdk-tools/packaging_tools/templates/setup.py
@@ -85,8 +85,8 @@ setup(
         {%- endif %}
         'azure-common~=1.1',
         {%- if need_azurecore %}
-        'azure-core>=1.4.1',<'2.0.0',
-        'azure-mgmt-core>=1.0.0',<'2.0.0',
+        'azure-mgmt-core>=1.0.0,<2.0.0',
+        'azure-identity>=1.3.1,<2.0.0',
         {%- endif %}
     ],
     extras_require={
