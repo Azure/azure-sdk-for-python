82018a6557a066a44c55ac8741506d6ae318a787
Laurent Mazuel <laurent.mazuel@gmail.com>
2020-04-20 16:15:22 -0700

    Syntax error

--- sdk/core/azure-mgmt-core/azure/mgmt/core/policies/__init__.py
+++ sdk/core/azure-mgmt-core/azure/mgmt/core/policies/__init__.py
@@ -32,7 +32,7 @@ class ARMHttpLoggingPolicy(HttpLoggingPolicy):
     """HttpLoggingPolicy with ARM specific safe headers fopr loggers.
     """
 
-    DEFAULT_HEADERS_WHITELIST = HttpLoggingPolicy.DEFAULT_HEADERS_WHITELIST + set([
+    DEFAULT_HEADERS_WHITELIST = HttpLoggingPolicy.DEFAULT_HEADERS_WHITELIST | set([
         # https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/request-limits-and-throttling#remaining-requests
         "x-ms-ratelimit-remaining-subscription-reads",
         "x-ms-ratelimit-remaining-subscription-writes",
