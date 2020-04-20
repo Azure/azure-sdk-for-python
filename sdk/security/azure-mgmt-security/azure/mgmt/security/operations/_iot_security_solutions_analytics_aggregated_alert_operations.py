dec87c89c7ee571eceb9ef8561ce90d87e8bba5d
SDK Automation <sdkautomation@microsoft.com>
2020-04-20 21:00:38 +0000

    Generated from 053ab9d5e64bdd16918dc9e62c0e735703f0325b
    Update CreateOrUpdateInformationProtectionPolicy_example.json

--- sdk/security/azure-mgmt-security/azure/mgmt/security/operations/_iot_security_solutions_analytics_aggregated_alert_operations.py
+++ sdk/security/azure-mgmt-security/azure/mgmt/security/operations/_iot_security_solutions_analytics_aggregated_alert_operations.py
@@ -16,8 +16,8 @@ from msrestazure.azure_exceptions import CloudError
 from .. import models
 
 
-class IoTSecuritySolutionsAnalyticsAggregatedAlertOperations(object):
-    """IoTSecuritySolutionsAnalyticsAggregatedAlertOperations operations.
+class IotSecuritySolutionsAnalyticsAggregatedAlertOperations(object):
+    """IotSecuritySolutionsAnalyticsAggregatedAlertOperations operations.
 
     You should not instantiate directly this class, but create a Client instance that will create it for you and attach it as attribute.
 
@@ -39,16 +39,95 @@ class IoTSecuritySolutionsAnalyticsAggregatedAlertOperations(object):
 
         self.config = config
 
-    def get(
-            self, resource_group_name, solution_name, aggregated_alert_name, custom_headers=None, raw=False, **operation_config):
-        """Security Analytics of a security solution.
+    def list(
+            self, resource_group_name, solution_name, top=None, custom_headers=None, raw=False, **operation_config):
+        """Use this method to get the aggregated alert list of yours IoT Security
+        solution.
 
         :param resource_group_name: The name of the resource group within the
          user's subscription. The name is case insensitive.
         :type resource_group_name: str
-        :param solution_name: The solution manager name
+        :param solution_name: The name of the IoT Security solution.
         :type solution_name: str
-        :param aggregated_alert_name: Identifier of the aggregated alert
+        :param top: Number of results to retrieve.
+        :type top: int
+        :param dict custom_headers: headers that will be added to the request
+        :param bool raw: returns the direct response alongside the
+         deserialized response
+        :param operation_config: :ref:`Operation configuration
+         overrides<msrest:optionsforoperations>`.
+        :return: An iterator like instance of IoTSecurityAggregatedAlert
+        :rtype:
+         ~azure.mgmt.security.models.IoTSecurityAggregatedAlertPaged[~azure.mgmt.security.models.IoTSecurityAggregatedAlert]
+        :raises: :class:`CloudError<msrestazure.azure_exceptions.CloudError>`
+        """
+        def prepare_request(next_link=None):
+            if not next_link:
+                # Construct URL
+                url = self.list.metadata['url']
+                path_format_arguments = {
+                    'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str', pattern=r'^[0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}$'),
+                    'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1, pattern=r'^[-\w\._\(\)]+$'),
+                    'solutionName': self._serialize.url("solution_name", solution_name, 'str')
+                }
+                url = self._client.format_url(url, **path_format_arguments)
+
+                # Construct parameters
+                query_parameters = {}
+                query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
+                if top is not None:
+                    query_parameters['$top'] = self._serialize.query("top", top, 'int')
+
+            else:
+                url = next_link
+                query_parameters = {}
+
+            # Construct headers
+            header_parameters = {}
+            header_parameters['Accept'] = 'application/json'
+            if self.config.generate_client_request_id:
+                header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
+            if custom_headers:
+                header_parameters.update(custom_headers)
+            if self.config.accept_language is not None:
+                header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
+
+            # Construct and send request
+            request = self._client.get(url, query_parameters, header_parameters)
+            return request
+
+        def internal_paging(next_link=None):
+            request = prepare_request(next_link)
+
+            response = self._client.send(request, stream=False, **operation_config)
+
+            if response.status_code not in [200]:
+                exp = CloudError(response)
+                exp.request_id = response.headers.get('x-ms-request-id')
+                raise exp
+
+            return response
+
+        # Deserialize response
+        header_dict = None
+        if raw:
+            header_dict = {}
+        deserialized = models.IoTSecurityAggregatedAlertPaged(internal_paging, self._deserialize.dependencies, header_dict)
+
+        return deserialized
+    list.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Security/iotSecuritySolutions/{solutionName}/analyticsModels/default/aggregatedAlerts'}
+
+    def get(
+            self, resource_group_name, solution_name, aggregated_alert_name, custom_headers=None, raw=False, **operation_config):
+        """Use this method to get a single the aggregated alert of yours IoT
+        Security solution. This aggregation is performed by alert name.
+
+        :param resource_group_name: The name of the resource group within the
+         user's subscription. The name is case insensitive.
+        :type resource_group_name: str
+        :param solution_name: The name of the IoT Security solution.
+        :type solution_name: str
+        :param aggregated_alert_name: Identifier of the aggregated alert.
         :type aggregated_alert_name: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
@@ -106,14 +185,14 @@ class IoTSecuritySolutionsAnalyticsAggregatedAlertOperations(object):
 
     def dismiss(
             self, resource_group_name, solution_name, aggregated_alert_name, custom_headers=None, raw=False, **operation_config):
-        """Security Analytics of a security solution.
+        """Use this method to dismiss an aggregated IoT Security Solution Alert.
 
         :param resource_group_name: The name of the resource group within the
          user's subscription. The name is case insensitive.
         :type resource_group_name: str
-        :param solution_name: The solution manager name
+        :param solution_name: The name of the IoT Security solution.
         :type solution_name: str
-        :param aggregated_alert_name: Identifier of the aggregated alert
+        :param aggregated_alert_name: Identifier of the aggregated alert.
         :type aggregated_alert_name: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
