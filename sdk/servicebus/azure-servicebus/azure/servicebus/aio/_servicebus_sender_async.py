aafe6cebe022a7936dd8714b811c1140ae481d87
KieranBrantnerMagee <kibrantn@microsoft.com>
2020-04-21 14:08:50 -0700

    Ensure that open() leaves handlers in a sane state even on failure, and that recovery can happen properly. (#10935)
    Cleans up any current state by calling close() if open() fails.  There is a second ongoing discussion surrounding whether we want to allow reopening at all, but that should be set as a distinct state-machine flag to make intent clear and decouple it from _handler state side-effects.

--- sdk/servicebus/azure-servicebus/azure/servicebus/aio/_servicebus_sender_async.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/aio/_servicebus_sender_async.py
@@ -120,12 +120,16 @@ class ServiceBusSender(BaseHandlerAsync, SenderMixin):
             await self._handler.close_async()
         auth = None if self._connection else (await create_authentication(self))
         self._create_handler(auth)
-        await self._handler.open_async(connection=self._connection)
-        while not await self._handler.client_ready_async():
-            await asyncio.sleep(0.05)
-        self._running = True
-        self._max_message_size_on_link = self._handler.message_handler._link.peer_max_message_size \
-                                         or uamqp.constants.MAX_MESSAGE_LENGTH_BYTES
+        try:
+            await self._handler.open_async(connection=self._connection)
+            while not await self._handler.client_ready_async():
+                await asyncio.sleep(0.05)
+            self._running = True
+            self._max_message_size_on_link = self._handler.message_handler._link.peer_max_message_size \
+                                             or uamqp.constants.MAX_MESSAGE_LENGTH_BYTES
+        except:
+            self.close()
+            raise
 
     async def _send(self, message, timeout=None, last_exception=None):
         await self._open()
