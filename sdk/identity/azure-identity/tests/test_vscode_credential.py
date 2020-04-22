767906fefd33cffcee756c55ebd594143ed09ccb
Xiang Yan <xiangsjtu@gmail.com>
2020-04-21 17:00:40 -0700

    add tests for win apis

--- sdk/identity/azure-identity/tests/test_vscode_credential.py
+++ sdk/identity/azure-identity/tests/test_vscode_credential.py
@@ -65,3 +65,26 @@ def test_get_token():
         token = credential.get_token("scope")
         assert token is expected_token
         assert mock_client.obtain_token_by_refresh_token.call_count == 1
+
+
+if sys.platform.startswith('win'):
+    from azure.identity._credentials.win_vscode_adapter import _read_credential, _cred_write
+
+
+@pytest.mark.skipif(not sys.platform.startswith('win'), reason="This test only runs on Windows")
+def test_win_api():
+    service_name = u"VS Code Azure"
+    account_name = u"Azure"
+    target = u"{}/{}".format(service_name, account_name)
+    comment = u"comment"
+    token_written = u"test_refresh_token"
+    user_name = u"Azure"
+    credential = {"Type": 0x1,
+                   "TargetName": target,
+                   "UserName": user_name,
+                   "CredentialBlob": token_written,
+                   "Comment": comment,
+                   "Persist": 0x2}
+    _cred_write(credential)
+    token_read = _read_credential(service_name, account_name)
+    assert token_read == token_written
