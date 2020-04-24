a1dab99014ad2b2571cb0fb1da1d513175d8edc7
SDK Automation <sdkautomation@microsoft.com>
2020-04-24 04:40:00 +0000

    Generated from 60714c5be56a601fd212571d00c95686c44a0870
    Fix example of get group ids

--- sdk/search/azure-mgmt-search/azure/mgmt/search/models/__init__.py
+++ sdk/search/azure-mgmt-search/azure/mgmt/search/models/__init__.py
@@ -10,33 +10,33 @@
 # --------------------------------------------------------------------------
 
 try:
-    from .check_name_availability_input_py3 import CheckNameAvailabilityInput
-    from .check_name_availability_output_py3 import CheckNameAvailabilityOutput
-    from .admin_key_result_py3 import AdminKeyResult
-    from .query_key_py3 import QueryKey
-    from .sku_py3 import Sku
-    from .search_service_py3 import SearchService
-    from .identity_py3 import Identity
-    from .resource_py3 import Resource
-    from .operation_display_py3 import OperationDisplay
-    from .operation_py3 import Operation
-    from .search_management_request_options_py3 import SearchManagementRequestOptions
+    from ._models_py3 import AdminKeyResult
+    from ._models_py3 import CheckNameAvailabilityInput
+    from ._models_py3 import CheckNameAvailabilityOutput
+    from ._models_py3 import Identity
+    from ._models_py3 import Operation
+    from ._models_py3 import OperationDisplay
+    from ._models_py3 import QueryKey
+    from ._models_py3 import Resource
+    from ._models_py3 import SearchManagementRequestOptions
+    from ._models_py3 import SearchService
+    from ._models_py3 import Sku
 except (SyntaxError, ImportError):
-    from .check_name_availability_input import CheckNameAvailabilityInput
-    from .check_name_availability_output import CheckNameAvailabilityOutput
-    from .admin_key_result import AdminKeyResult
-    from .query_key import QueryKey
-    from .sku import Sku
-    from .search_service import SearchService
-    from .identity import Identity
-    from .resource import Resource
-    from .operation_display import OperationDisplay
-    from .operation import Operation
-    from .search_management_request_options import SearchManagementRequestOptions
-from .operation_paged import OperationPaged
-from .query_key_paged import QueryKeyPaged
-from .search_service_paged import SearchServicePaged
-from .search_management_client_enums import (
+    from ._models import AdminKeyResult
+    from ._models import CheckNameAvailabilityInput
+    from ._models import CheckNameAvailabilityOutput
+    from ._models import Identity
+    from ._models import Operation
+    from ._models import OperationDisplay
+    from ._models import QueryKey
+    from ._models import Resource
+    from ._models import SearchManagementRequestOptions
+    from ._models import SearchService
+    from ._models import Sku
+from ._paged_models import OperationPaged
+from ._paged_models import QueryKeyPaged
+from ._paged_models import SearchServicePaged
+from ._search_management_client_enums import (
     UnavailableNameReason,
     SkuName,
     HostingMode,
@@ -47,17 +47,17 @@ from .search_management_client_enums import (
 )
 
 __all__ = [
+    'AdminKeyResult',
     'CheckNameAvailabilityInput',
     'CheckNameAvailabilityOutput',
-    'AdminKeyResult',
-    'QueryKey',
-    'Sku',
-    'SearchService',
     'Identity',
-    'Resource',
-    'OperationDisplay',
     'Operation',
+    'OperationDisplay',
+    'QueryKey',
+    'Resource',
     'SearchManagementRequestOptions',
+    'SearchService',
+    'Sku',
     'OperationPaged',
     'QueryKeyPaged',
     'SearchServicePaged',
