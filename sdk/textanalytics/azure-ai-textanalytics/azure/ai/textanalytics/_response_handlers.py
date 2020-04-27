13d7e5fc6e8f46db17a6c082694e18c548795af9
iscai-msft <iscai@microsoft.com>
2020-04-27 13:39:01 -0400

    detect language passing

--- sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_response_handlers.py
+++ sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_response_handlers.py
@@ -79,7 +79,7 @@ def prepare_result(func):
 def language_result(language):
     return DetectLanguageResult(
         id=language.id,
-        primary_language=DetectedLanguage._from_generated(language.detected_languages[0]),  # pylint: disable=protected-access
+        primary_language=DetectedLanguage._from_generated(language.detected_language),  # pylint: disable=protected-access
         statistics=TextDocumentStatistics._from_generated(language.statistics),  # pylint: disable=protected-access
     )
 
