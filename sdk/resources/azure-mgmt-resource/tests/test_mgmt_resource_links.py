2c422172bd5e280aee2aaf3f4e451ab403d03d5e
Zim Kalinowski <zikalino@microsoft.com>
2020-04-28 13:27:24 +0000

    fixed links test

--- sdk/resources/azure-mgmt-resource/tests/test_mgmt_resource_links.py
+++ sdk/resources/azure-mgmt-resource/tests/test_mgmt_resource_links.py
@@ -37,21 +37,35 @@ class MgmtResourceLinksTest(AzureMgmtTestCase):
                 resource_type="availabilitySets",
                 resource_name=resource_name,
                 parameters={'location': location},
-                api_version='2019-12-01'
+                api_version='2019-07-01' # '2016-09-01'
             )
             result = create_result.result()
             self.result_id = result.id
+            create_result = self.resource_client.resources.begin_create_or_update(
+                resource_group_name=resource_group.name,
+                resource_provider_namespace="Microsoft.Compute",
+                parent_resource_path="",
+                resource_type="availabilitySets",
+                resource_name=resource_name + "2",
+                parameters={'location': location},
+                api_version='2019-07-01' # '2016-09-01'
+            )
+            result = create_result.result()
+            self.result_id_2 = result.id
         else:
             self.result_id = resource_group.id + "/providers/Microsoft.Compute/availabilitySets/" + resource_name
+            self.result_id_2 = resource_group.id + "/providers/Microsoft.Compute/availabilitySets/" + resource_name + "2"
 
         SUBSCRIPTION_ID = self.settings.SUBSCRIPTION_ID
         link = self.client.resource_links.create_or_update(
-            # resource_group.id+'/providers/Microsoft.Resources/links/myLink',
-            self.result_id + '/Microsoft.Resources/links/myLink',
+            # resource_group.id + '/providers/Microsoft.Resources/links/mylink',
+            self.result_id + '/providers/Microsoft.Resources/links/myLink',
             '2019-07-01',
             {
-                'target_id' : resource_group.id + "/providers/Microsoft.Compute/availabilitySets/mylinkedavailset",
-                'notes': 'Testing links'
+                'properties': {
+                  'target_id': self.result_id_2,
+                  'notes': 'Testing links'
+                }
             }
         )
         self.assertEqual(link.name, 'myLink')
@@ -66,10 +80,10 @@ class MgmtResourceLinksTest(AzureMgmtTestCase):
         links = list(self.client.resource_links.list_at_subscription())
         self.assertTrue(any(link.name=='myLink' for link in links))
 
-        links = list(self.client.resource_links.list_at_source_scope(resource_group.id))
+        links = list(self.client.resource_links.list_at_source_scope(self.result_id))
         self.assertTrue(any(link.name=='myLink' for link in links))
 
-        links = list(self.client.resource_links.list_at_source_scope(resource_group.id, 'atScope()'))
+        links = list(self.client.resource_links.list_at_source_scope(self.result_id, 'atScope()'))
         self.assertTrue(any(link.name=='myLink' for link in links))
 
         self.client.resource_links.delete(link.id)
