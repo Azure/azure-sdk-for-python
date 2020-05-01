012f52f1183a2471f6bcd3afaa32e5def8bbd3a6
Xiang Yan <xiangsjtu@gmail.com>
2020-05-01 14:11:54 -0700

    Identity use pbyte (#11173)
    * identity_vscode_cred_format

--- sdk/identity/azure-identity/tests/test_vscode_credential_async.py
+++ sdk/identity/azure-identity/tests/test_vscode_credential_async.py
@@ -69,3 +69,36 @@ async def test_redeem_token():
         credential = VSCodeCredential(_client=mock_client)
         token = await credential.get_token("scope")
         assert token is expected_token
+        token_by_refresh_token.assert_called_with("VALUE", ("scope",))
+
+@pytest.mark.asyncio
+async def test_cache_refresh_token():
+    expected_token = AccessToken("token", 42)
+
+    mock_client = mock.Mock(spec=object)
+    token_by_refresh_token = mock.Mock(return_value=expected_token)
+    mock_client.obtain_token_by_refresh_token = wrap_in_future(token_by_refresh_token)
+    mock_client.get_cached_access_token = mock.Mock(return_value=None)
+    mock_get_credentials = mock.Mock(return_value="VALUE")
+
+    with mock.patch(VSCodeCredential.__module__ + ".get_credentials", mock_get_credentials):
+        credential = VSCodeCredential(_client=mock_client)
+        token = await credential.get_token("scope")
+        assert mock_get_credentials.call_count == 1
+        token = await credential.get_token("scope")
+        assert mock_get_credentials.call_count == 1
+
+
+@pytest.mark.asyncio
+async def test_no_obtain_token_if_cached():
+    expected_token = AccessToken("token", 42)
+
+    mock_client = mock.Mock(spec=object)
+    token_by_refresh_token = mock.Mock(return_value=expected_token)
+    mock_client.obtain_token_by_refresh_token = wrap_in_future(token_by_refresh_token)
+    mock_client.get_cached_access_token = mock.Mock(return_value='VALUE')
+
+    with mock.patch(VSCodeCredential.__module__ + ".get_credentials", return_value="VALUE"):
+        credential = VSCodeCredential(_client=mock_client)
+        token = await credential.get_token("scope")
+        assert token_by_refresh_token.call_count == 0
