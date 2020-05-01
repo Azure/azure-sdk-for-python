217766ea99de59d8928b002e8440bec4a5a855f2
Yunhao Ling <adam_ling@outlook.com>
2020-05-01 12:43:51 -0700

    re-enable schedule tests in session

--- sdk/servicebus/azure-servicebus/tests/async_tests/test_sessions_async.py
+++ sdk/servicebus/azure-servicebus/tests/async_tests/test_sessions_async.py
@@ -617,7 +617,6 @@ class ServiceBusAsyncSessionTests(AzureMgmtTestCase):
             await renewer.shutdown()
 
 
-    @pytest.mark.skip(reason='requires scheduling functionality')
     @pytest.mark.liveTest
     @pytest.mark.live_test_only
     @CachedResourceGroupPreparer(name_prefix='servicebustest')
@@ -638,7 +637,7 @@ class ServiceBusAsyncSessionTests(AzureMgmtTestCase):
                 message_id_b = uuid.uuid4()
                 message_b = Message(content, session_id=session_id)
                 message_b.properties.message_id = message_id_b
-                tokens = await sender.schedule(enqueue_time, message_a, message_b)
+                tokens = await sender.schedule([message_a, message_b], enqueue_time)
                 assert len(tokens) == 2
 
             renewer = AutoLockRenew()
@@ -658,7 +657,6 @@ class ServiceBusAsyncSessionTests(AzureMgmtTestCase):
             await renewer.shutdown()
 
 
-    @pytest.mark.skip(reasion="requires scheduling")
     @pytest.mark.liveTest
     @pytest.mark.live_test_only
     @CachedResourceGroupPreparer(name_prefix='servicebustest')
@@ -673,9 +671,9 @@ class ServiceBusAsyncSessionTests(AzureMgmtTestCase):
             async with sb_client.get_queue_sender(servicebus_queue.name) as sender:
                 message_a = Message("Test scheduled message", session_id=session_id)
                 message_b = Message("Test scheduled message", session_id=session_id)
-                tokens = await sender.schedule(enqueue_time, message_a, message_b)
+                tokens = await sender.schedule([message_a, message_b], enqueue_time)
                 assert len(tokens) == 2
-                await sender.cancel_scheduled_messages(*tokens)
+                await sender.cancel_scheduled_messages(tokens)
 
             renewer = AutoLockRenew()
             messages = []
