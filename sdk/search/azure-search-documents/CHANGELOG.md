bb76245958e0fc68e8a5981b6898fdfc8e1a7e10
azure-sdk <azuresdk@microsoft.com>
2020-05-04 18:50:46 +0000

    Increment package version after release of azure_search_documents

--- sdk/search/azure-search-documents/CHANGELOG.md
+++ sdk/search/azure-search-documents/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Release History
 
+## 1.0.0b4 (Unreleased)
+
+
 ## 1.0.0b3 (2020-05-04)
 
 **Features**
--- sdk/search/azure-search-documents/azure/search/documents/_version.py
+++ sdk/search/azure-search-documents/azure/search/documents/_version.py
@@ -3,6 +3,6 @@
 # Licensed under the MIT License.
 # ------------------------------------
 
-VERSION = "1.0.0b3"  # type: str
+VERSION = "1.0.0b4"  # type: str
 
 SDK_MONIKER = "search-documents/{}".format(VERSION)  # type: str
