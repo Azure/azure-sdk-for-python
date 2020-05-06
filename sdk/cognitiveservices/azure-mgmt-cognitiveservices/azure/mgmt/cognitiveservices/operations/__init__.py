e9616b97754f4309ea7d7b9fff969f1456df134a
SDK Automation <sdkautomation@microsoft.com>
2020-05-06 06:09:04 +0000

    Generated from 52683b65cf6ac59ddc81ece0dccea9357b280846
    fix prettier

--- sdk/cognitiveservices/azure-mgmt-cognitiveservices/azure/mgmt/cognitiveservices/operations/__init__.py
+++ sdk/cognitiveservices/azure-mgmt-cognitiveservices/azure/mgmt/cognitiveservices/operations/__init__.py
@@ -12,11 +12,15 @@
 from ._accounts_operations import AccountsOperations
 from ._resource_skus_operations import ResourceSkusOperations
 from ._operations import Operations
+from ._private_endpoint_connections_operations import PrivateEndpointConnectionsOperations
+from ._private_link_resources_operations import PrivateLinkResourcesOperations
 from ._cognitive_services_management_client_operations import CognitiveServicesManagementClientOperationsMixin
 
 __all__ = [
     'AccountsOperations',
     'ResourceSkusOperations',
     'Operations',
+    'PrivateEndpointConnectionsOperations',
+    'PrivateLinkResourcesOperations',
     'CognitiveServicesManagementClientOperationsMixin',
 ]
