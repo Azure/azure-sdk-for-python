299e7476a46a5c874a5d63228ccdd5089c97f0c3
SDK Automation <sdkautomation@microsoft.com>
2020-05-08 02:14:49 +0000

    Generated from 87bda288a13ceddb8bc764e7c3bc8fa60a6ba3c4
    Remove quotes from data queries in RI apis

--- sdk/consumption/azure-mgmt-consumption/azure/mgmt/consumption/operations/__init__.py
+++ sdk/consumption/azure-mgmt-consumption/azure/mgmt/consumption/operations/__init__.py
@@ -9,19 +9,23 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from .usage_details_operations import UsageDetailsOperations
-from .marketplaces_operations import MarketplacesOperations
-from .budgets_operations import BudgetsOperations
-from .tags_operations import TagsOperations
-from .charges_operations import ChargesOperations
-from .balances_operations import BalancesOperations
-from .reservations_summaries_operations import ReservationsSummariesOperations
-from .reservations_details_operations import ReservationsDetailsOperations
-from .reservation_recommendations_operations import ReservationRecommendationsOperations
-from .price_sheet_operations import PriceSheetOperations
-from .forecasts_operations import ForecastsOperations
-from .operations import Operations
-from .aggregated_cost_operations import AggregatedCostOperations
+from ._usage_details_operations import UsageDetailsOperations
+from ._marketplaces_operations import MarketplacesOperations
+from ._budgets_operations import BudgetsOperations
+from ._tags_operations import TagsOperations
+from ._charges_operations import ChargesOperations
+from ._balances_operations import BalancesOperations
+from ._reservations_summaries_operations import ReservationsSummariesOperations
+from ._reservations_details_operations import ReservationsDetailsOperations
+from ._reservation_recommendations_operations import ReservationRecommendationsOperations
+from ._reservation_transactions_operations import ReservationTransactionsOperations
+from ._price_sheet_operations import PriceSheetOperations
+from ._forecasts_operations import ForecastsOperations
+from ._operations import Operations
+from ._aggregated_cost_operations import AggregatedCostOperations
+from ._events_operations import EventsOperations
+from ._lots_operations import LotsOperations
+from ._credits_operations import CreditsOperations
 
 __all__ = [
     'UsageDetailsOperations',
@@ -33,8 +37,12 @@ __all__ = [
     'ReservationsSummariesOperations',
     'ReservationsDetailsOperations',
     'ReservationRecommendationsOperations',
+    'ReservationTransactionsOperations',
     'PriceSheetOperations',
     'ForecastsOperations',
     'Operations',
     'AggregatedCostOperations',
+    'EventsOperations',
+    'LotsOperations',
+    'CreditsOperations',
 ]
