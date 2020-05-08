5748d1e611e40d5b1562d1b1ffdbeab09cfa5a86
00Kai0 <sunkaihuisos@gmail.com>
2020-05-08 16:17:19 +0800

    update sample

--- sdk/appconfiguration/azure-mgmt-appconfiguration/samples/create_an_app_configuration_store.py
+++ sdk/appconfiguration/azure-mgmt-appconfiguration/samples/create_an_app_configuration_store.py
@@ -3,27 +3,26 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+import os
 
+from azure.identity import EnvironmentCredential
 from azure.mgmt.appconfiguration import AppConfigurationManagementClient
 from azure.mgmt.resource import ResourceManagementClient
 
-from _prepare import (
-    client_credential,
-    SUBSCRIPTION_ID
-)
 
 def main():
 
+    SUBSCRIPTION_ID = os.environ.get("SUBSCRIPTION_ID", None)
     GROUP_NAME = "testgroupx"
-    CONFIG_STORE_NAME = "configstorex"
+    CONFIG_STORE_NAME = "configstorexyz"
 
     # Create client
     appconfig_client = AppConfigurationManagementClient(
-        credential=client_credential,
+        credential=EnvironmentCredential(),
         subscription_id=SUBSCRIPTION_ID
     )
     resource_client = ResourceManagementClient(
-        credential=client_credential,
+        credential=EnvironmentCredential(),
         subscription_id=SUBSCRIPTION_ID
     )
 
@@ -34,31 +33,46 @@ def main():
     )
 
     # Create appconfiguration store
-    config_store = appconfig_client.configuration_stores.begin_create(
+    appconfig_store = appconfig_client.configuration_stores.begin_create(
         GROUP_NAME,
         CONFIG_STORE_NAME,
         {
-          "location": "westus",
+          "location": "eastus",
           "sku": {
-            "name": "Standard"  # Free can not use private endpoint
+            "name": "Standard"
           }
         }
     ).result()
+    print("Create appconfigruation store:\n{}".format(appconfig_store))
 
-    # Get appconfiguration store connection string
-    keys = appconfig_client.configuration_stores.list_keys(
+    # Get appconfiguration store
+    appconfig_store = appconfig_client.configuration_stores.get(
         GROUP_NAME,
         CONFIG_STORE_NAME
     )
-    keys = list(keys)
-    connection_string = keys[0].connection_string
-    print("connection_str: {}".format(connection_string))
+    print("Get appconfigruation store:\n{}".format(appconfig_store))
+
+    # Update appconfiguration store
+    appconfig_store = appconfig_client.configuration_stores.begin_update(
+        GROUP_NAME,
+        CONFIG_STORE_NAME,
+        {
+          "tags": {
+            "category": "Marketing"
+          },
+          "sku": {
+            "name": "Standard"
+          }
+        }
+    ).result()
+    print("Update appconfigruation store:\n{}".format(appconfig_store))
 
     # Delete appconfiguration store
     appconfig_client.configuration_stores.begin_delete(
         GROUP_NAME,
         CONFIG_STORE_NAME
     ).result()
+    print("Delete appconfiguration store")
 
     # Delete Group
     resource_client.resource_groups.begin_delete(
---
+++ sdk/appconfiguration/azure-mgmt-appconfiguration/samples/readme.md
@@ -0,0 +1,16 @@
+# Quickstart
+1. Install requirements (Unavaliable Now)   
+cd to current path and run `pip install -r requirement.txt`
+
+2. Set up env var   
+In linux
+```bash
+export AZURE_TENANT_ID=""
+export AZURE_CLIENT_ID=""
+export AZURE_CLIENT_SECRET=""
+export SUBSCRIPTION_ID=""
+```
+About how to create app registration, see here: https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app
+
+3. Start sample
+run `python create_an_app_configuration_store.py` to start this demo.
