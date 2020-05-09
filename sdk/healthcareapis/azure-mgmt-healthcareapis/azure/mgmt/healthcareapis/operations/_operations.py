da6461a2f6b44b64767c30ad3c0f8bb71f150a50
SDK Automation <sdkautomation@microsoft.com>
2020-05-09 01:17:11 +0000

    Generated from 46407f924c614e2b4944ef07b247f5a8ab787513
    accessPolicies property can be omitted

--- sdk/healthcareapis/azure-mgmt-healthcareapis/azure/mgmt/healthcareapis/operations/_operations.py
+++ sdk/healthcareapis/azure-mgmt-healthcareapis/azure/mgmt/healthcareapis/operations/_operations.py
@@ -24,7 +24,7 @@ class Operations(object):
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
-    :ivar api_version: The version of the API. Constant value: "2018-08-20-preview".
+    :ivar api_version: Client Api Version. Constant value: "2019-09-16".
     """
 
     models = models
@@ -34,7 +34,7 @@ class Operations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
-        self.api_version = "2018-08-20-preview"
+        self.api_version = "2019-09-16"
 
         self.config = config
 
@@ -60,7 +60,7 @@ class Operations(object):
 
                 # Construct parameters
                 query_parameters = {}
-                query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str', min_length=10)
+                query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
 
             else:
                 url = next_link
