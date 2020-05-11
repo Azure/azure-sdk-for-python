5349e4b4689d3e44cb7f1c4b31170c7b78d807ab
Rakshith Bhyravabhotla <sabhyrav@microsoft.com>
2020-05-11 13:12:28 -0700

    Update sdk/storage/azure-storage/setup.py

--- sdk/storage/azure-storage/setup.py
+++ sdk/storage/azure-storage/setup.py
@@ -20,7 +20,7 @@ https://aka.ms/azsdk/python/all
 
 Here's a non-exhaustive list of common packages:
 
--  [azure-storage-blob](https://pypi.org/project/azure-storage-blob) : Blob storage client
+- [azure-storage-blob](https://pypi.org/project/azure-storage-blob) : Blob storage client
 - [azure-storage-file-share](https://pypi.org/project/azure-storage-file-share) : Storage file share client
 - [azure-storage-file-datalake](https://pypi.org/project/azure-storage-file-datalake) : ADLS Gen2 client
 - [azure-storage-queue](https://pypi.org/project/azure-storage-queue): Queue storage client
