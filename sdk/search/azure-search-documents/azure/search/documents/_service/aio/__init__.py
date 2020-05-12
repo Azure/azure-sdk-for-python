5379638fb7ac311bdf5a6c824de3de62b15c13c6
Bryan Van de Ven <bryan@bokeh.org>
2020-05-12 14:42:43 -0700

    Search docs/readme updates (#11391)
    * fix top level API ref docs link
    
    * fix az create sample
    
    * improve 'create a service' links
    
    * snippet index
    
    * use non-versioned link for ref docs
    
    * add missing subclients from top level

--- sdk/search/azure-search-documents/azure/search/documents/_service/aio/__init__.py
+++ sdk/search/azure-search-documents/azure/search/documents/_service/aio/__init__.py
@@ -4,4 +4,17 @@
 # ------------------------------------
 from ._search_service_client_async import SearchServiceClient
 
-__all__ = "SearchServiceClient"
+from ._datasources_client import SearchDataSourcesClient
+from ._indexers_client import SearchIndexersClient
+from ._indexes_client import SearchIndexesClient
+from ._skillsets_client import SearchSkillsetsClient
+from ._synonym_maps_client import SearchSynonymMapsClient
+
+__all__ = (
+    "SearchServiceClient",
+    "SearchDataSourcesClient",
+    "SearchIndexersClient",
+    "SearchIndexesClient",
+    "SearchSkillsetsClient",
+    "SearchSynonymMapsClient",
+)
