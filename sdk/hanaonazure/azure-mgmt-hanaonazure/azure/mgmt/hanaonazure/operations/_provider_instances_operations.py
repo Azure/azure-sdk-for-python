72a895d077157b77b183805fab9358ecd50aa96c
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2020-05-19 09:27:16 +0800

    release-for-hanaonazure-mgmt (#11441)

--- sdk/hanaonazure/azure-mgmt-hanaonazure/azure/mgmt/hanaonazure/operations/_provider_instances_operations.py
+++ sdk/hanaonazure/azure-mgmt-hanaonazure/azure/mgmt/hanaonazure/operations/_provider_instances_operations.py
@@ -17,8 +17,8 @@ from msrestazure.polling.arm_polling import ARMPolling
 from .. import models
 
 
-class SapMonitorsOperations(object):
-    """SapMonitorsOperations operations.
+class ProviderInstancesOperations(object):
+    """ProviderInstancesOperations operations.
 
     You should not instantiate directly this class, but create a Client instance that will create it for you and attach it as attribute.
 
@@ -41,20 +41,24 @@ class SapMonitorsOperations(object):
         self.config = config
 
     def list(
-            self, custom_headers=None, raw=False, **operation_config):
-        """Gets a list of SAP monitors in the specified subscription.
+            self, resource_group_name, sap_monitor_name, custom_headers=None, raw=False, **operation_config):
+        """Gets a list of provider instances in the specified SAP monitor.
 
-        Gets a list of SAP monitors in the specified subscription. The
-        operations returns various properties of each SAP monitor.
+        Gets a list of provider instances in the specified SAP monitor. The
+        operations returns various properties of each provider instances.
 
+        :param resource_group_name: Name of the resource group.
+        :type resource_group_name: str
+        :param sap_monitor_name: Name of the SAP monitor resource.
+        :type sap_monitor_name: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: An iterator like instance of SapMonitor
+        :return: An iterator like instance of ProviderInstance
         :rtype:
-         ~azure.mgmt.hanaonazure.models.SapMonitorPaged[~azure.mgmt.hanaonazure.models.SapMonitor]
+         ~azure.mgmt.hanaonazure.models.ProviderInstancePaged[~azure.mgmt.hanaonazure.models.ProviderInstance]
         :raises:
          :class:`ErrorResponseException<azure.mgmt.hanaonazure.models.ErrorResponseException>`
         """
@@ -63,7 +67,9 @@ class SapMonitorsOperations(object):
                 # Construct URL
                 url = self.list.metadata['url']
                 path_format_arguments = {
-                    'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str')
+                    'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str'),
+                    'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
+                    'sapMonitorName': self._serialize.url("sap_monitor_name", sap_monitor_name, 'str')
                 }
                 url = self._client.format_url(url, **path_format_arguments)
 
@@ -103,29 +109,31 @@ class SapMonitorsOperations(object):
         header_dict = None
         if raw:
             header_dict = {}
-        deserialized = models.SapMonitorPaged(internal_paging, self._deserialize.dependencies, header_dict)
+        deserialized = models.ProviderInstancePaged(internal_paging, self._deserialize.dependencies, header_dict)
 
         return deserialized
-    list.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.HanaOnAzure/sapMonitors'}
+    list.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.HanaOnAzure/sapMonitors/{sapMonitorName}/providerInstances'}
 
     def get(
-            self, resource_group_name, sap_monitor_name, custom_headers=None, raw=False, **operation_config):
-        """Gets properties of a SAP monitor.
+            self, resource_group_name, sap_monitor_name, provider_instance_name, custom_headers=None, raw=False, **operation_config):
+        """Gets properties of a provider instance.
 
-        Gets properties of a SAP monitor for the specified subscription,
-        resource group, and resource name.
+        Gets properties of a provider instance for the specified subscription,
+        resource group, SapMonitor name, and resource name.
 
         :param resource_group_name: Name of the resource group.
         :type resource_group_name: str
         :param sap_monitor_name: Name of the SAP monitor resource.
         :type sap_monitor_name: str
+        :param provider_instance_name: Name of the provider instance.
+        :type provider_instance_name: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: SapMonitor or ClientRawResponse if raw=true
-        :rtype: ~azure.mgmt.hanaonazure.models.SapMonitor or
+        :return: ProviderInstance or ClientRawResponse if raw=true
+        :rtype: ~azure.mgmt.hanaonazure.models.ProviderInstance or
          ~msrest.pipeline.ClientRawResponse
         :raises:
          :class:`ErrorResponseException<azure.mgmt.hanaonazure.models.ErrorResponseException>`
@@ -135,7 +143,8 @@ class SapMonitorsOperations(object):
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str'),
             'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
-            'sapMonitorName': self._serialize.url("sap_monitor_name", sap_monitor_name, 'str')
+            'sapMonitorName': self._serialize.url("sap_monitor_name", sap_monitor_name, 'str'),
+            'providerInstanceName': self._serialize.url("provider_instance_name", provider_instance_name, 'str')
         }
         url = self._client.format_url(url, **path_format_arguments)
 
@@ -162,24 +171,25 @@ class SapMonitorsOperations(object):
 
         deserialized = None
         if response.status_code == 200:
-            deserialized = self._deserialize('SapMonitor', response)
+            deserialized = self._deserialize('ProviderInstance', response)
 
         if raw:
             client_raw_response = ClientRawResponse(deserialized, response)
             return client_raw_response
 
         return deserialized
-    get.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.HanaOnAzure/sapMonitors/{sapMonitorName}'}
+    get.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.HanaOnAzure/sapMonitors/{sapMonitorName}/providerInstances/{providerInstanceName}'}
 
 
     def _create_initial(
-            self, resource_group_name, sap_monitor_name, sap_monitor_parameter, custom_headers=None, raw=False, **operation_config):
+            self, resource_group_name, sap_monitor_name, provider_instance_name, provider_instance_parameter, custom_headers=None, raw=False, **operation_config):
         # Construct URL
         url = self.create.metadata['url']
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str'),
             'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
-            'sapMonitorName': self._serialize.url("sap_monitor_name", sap_monitor_name, 'str')
+            'sapMonitorName': self._serialize.url("sap_monitor_name", sap_monitor_name, 'str'),
+            'providerInstanceName': self._serialize.url("provider_instance_name", provider_instance_name, 'str')
         }
         url = self._client.format_url(url, **path_format_arguments)
 
@@ -199,7 +209,7 @@ class SapMonitorsOperations(object):
             header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
 
         # Construct body
-        body_content = self._serialize.body(sap_monitor_parameter, 'SapMonitor')
+        body_content = self._serialize.body(provider_instance_parameter, 'ProviderInstance')
 
         # Construct and send request
         request = self._client.put(url, query_parameters, header_parameters, body_content)
@@ -211,9 +221,9 @@ class SapMonitorsOperations(object):
         deserialized = None
 
         if response.status_code == 200:
-            deserialized = self._deserialize('SapMonitor', response)
+            deserialized = self._deserialize('ProviderInstance', response)
         if response.status_code == 201:
-            deserialized = self._deserialize('SapMonitor', response)
+            deserialized = self._deserialize('ProviderInstance', response)
 
         if raw:
             client_raw_response = ClientRawResponse(deserialized, response)
@@ -222,43 +232,48 @@ class SapMonitorsOperations(object):
         return deserialized
 
     def create(
-            self, resource_group_name, sap_monitor_name, sap_monitor_parameter, custom_headers=None, raw=False, polling=True, **operation_config):
-        """Creates a SAP monitor.
+            self, resource_group_name, sap_monitor_name, provider_instance_name, provider_instance_parameter, custom_headers=None, raw=False, polling=True, **operation_config):
+        """Creates a provider instance.
 
-        Creates a SAP monitor for the specified subscription, resource group,
-        and resource name.
+        Creates a provider instance for the specified subscription, resource
+        group, SapMonitor name, and resource name.
 
         :param resource_group_name: Name of the resource group.
         :type resource_group_name: str
         :param sap_monitor_name: Name of the SAP monitor resource.
         :type sap_monitor_name: str
-        :param sap_monitor_parameter: Request body representing a SAP Monitor
-        :type sap_monitor_parameter: ~azure.mgmt.hanaonazure.models.SapMonitor
+        :param provider_instance_name: Name of the provider instance.
+        :type provider_instance_name: str
+        :param provider_instance_parameter: Request body representing a
+         provider instance
+        :type provider_instance_parameter:
+         ~azure.mgmt.hanaonazure.models.ProviderInstance
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: The poller return type is ClientRawResponse, the
          direct response alongside the deserialized response
         :param polling: True for ARMPolling, False for no polling, or a
          polling object for personal polling strategy
-        :return: An instance of LROPoller that returns SapMonitor or
-         ClientRawResponse<SapMonitor> if raw==True
+        :return: An instance of LROPoller that returns ProviderInstance or
+         ClientRawResponse<ProviderInstance> if raw==True
         :rtype:
-         ~msrestazure.azure_operation.AzureOperationPoller[~azure.mgmt.hanaonazure.models.SapMonitor]
+         ~msrestazure.azure_operation.AzureOperationPoller[~azure.mgmt.hanaonazure.models.ProviderInstance]
          or
-         ~msrestazure.azure_operation.AzureOperationPoller[~msrest.pipeline.ClientRawResponse[~azure.mgmt.hanaonazure.models.SapMonitor]]
+         ~msrestazure.azure_operation.AzureOperationPoller[~msrest.pipeline.ClientRawResponse[~azure.mgmt.hanaonazure.models.ProviderInstance]]
         :raises:
          :class:`ErrorResponseException<azure.mgmt.hanaonazure.models.ErrorResponseException>`
         """
         raw_result = self._create_initial(
             resource_group_name=resource_group_name,
             sap_monitor_name=sap_monitor_name,
-            sap_monitor_parameter=sap_monitor_parameter,
+            provider_instance_name=provider_instance_name,
+            provider_instance_parameter=provider_instance_parameter,
             custom_headers=custom_headers,
             raw=True,
             **operation_config
         )
 
         def get_long_running_output(response):
-            deserialized = self._deserialize('SapMonitor', response)
+            deserialized = self._deserialize('ProviderInstance', response)
 
             if raw:
                 client_raw_response = ClientRawResponse(deserialized, response)
@@ -273,17 +288,18 @@ class SapMonitorsOperations(object):
         elif polling is False: polling_method = NoPolling()
         else: polling_method = polling
         return LROPoller(self._client, raw_result, get_long_running_output, polling_method)
-    create.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.HanaOnAzure/sapMonitors/{sapMonitorName}'}
+    create.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.HanaOnAzure/sapMonitors/{sapMonitorName}/providerInstances/{providerInstanceName}'}
 
 
     def _delete_initial(
-            self, resource_group_name, sap_monitor_name, custom_headers=None, raw=False, **operation_config):
+            self, resource_group_name, sap_monitor_name, provider_instance_name, custom_headers=None, raw=False, **operation_config):
         # Construct URL
         url = self.delete.metadata['url']
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str'),
             'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
-            'sapMonitorName': self._serialize.url("sap_monitor_name", sap_monitor_name, 'str')
+            'sapMonitorName': self._serialize.url("sap_monitor_name", sap_monitor_name, 'str'),
+            'providerInstanceName': self._serialize.url("provider_instance_name", provider_instance_name, 'str')
         }
         url = self._client.format_url(url, **path_format_arguments)
 
@@ -312,16 +328,18 @@ class SapMonitorsOperations(object):
             return client_raw_response
 
     def delete(
-            self, resource_group_name, sap_monitor_name, custom_headers=None, raw=False, polling=True, **operation_config):
-        """Deletes a SAP monitor.
+            self, resource_group_name, sap_monitor_name, provider_instance_name, custom_headers=None, raw=False, polling=True, **operation_config):
+        """Deletes a provider instance.
 
-        Deletes a SAP monitor with the specified subscription, resource group,
-        and monitor name.
+        Deletes a provider instance for the specified subscription, resource
+        group, SapMonitor name, and resource name.
 
         :param resource_group_name: Name of the resource group.
         :type resource_group_name: str
         :param sap_monitor_name: Name of the SAP monitor resource.
         :type sap_monitor_name: str
+        :param provider_instance_name: Name of the provider instance.
+        :type provider_instance_name: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: The poller return type is ClientRawResponse, the
          direct response alongside the deserialized response
@@ -337,6 +355,7 @@ class SapMonitorsOperations(object):
         raw_result = self._delete_initial(
             resource_group_name=resource_group_name,
             sap_monitor_name=sap_monitor_name,
+            provider_instance_name=provider_instance_name,
             custom_headers=custom_headers,
             raw=True,
             **operation_config
@@ -354,75 +373,4 @@ class SapMonitorsOperations(object):
         elif polling is False: polling_method = NoPolling()
         else: polling_method = polling
         return LROPoller(self._client, raw_result, get_long_running_output, polling_method)
-    delete.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.HanaOnAzure/sapMonitors/{sapMonitorName}'}
-
-    def update(
-            self, resource_group_name, sap_monitor_name, tags=None, custom_headers=None, raw=False, **operation_config):
-        """Patches the Tags field of a SAP monitor.
-
-        Patches the Tags field of a SAP monitor for the specified subscription,
-        resource group, and monitor name.
-
-        :param resource_group_name: Name of the resource group.
-        :type resource_group_name: str
-        :param sap_monitor_name: Name of the SAP monitor resource.
-        :type sap_monitor_name: str
-        :param tags: Tags field of the resource.
-        :type tags: dict[str, str]
-        :param dict custom_headers: headers that will be added to the request
-        :param bool raw: returns the direct response alongside the
-         deserialized response
-        :param operation_config: :ref:`Operation configuration
-         overrides<msrest:optionsforoperations>`.
-        :return: SapMonitor or ClientRawResponse if raw=true
-        :rtype: ~azure.mgmt.hanaonazure.models.SapMonitor or
-         ~msrest.pipeline.ClientRawResponse
-        :raises:
-         :class:`ErrorResponseException<azure.mgmt.hanaonazure.models.ErrorResponseException>`
-        """
-        tags_parameter = models.Tags(tags=tags)
-
-        # Construct URL
-        url = self.update.metadata['url']
-        path_format_arguments = {
-            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str'),
-            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
-            'sapMonitorName': self._serialize.url("sap_monitor_name", sap_monitor_name, 'str')
-        }
-        url = self._client.format_url(url, **path_format_arguments)
-
-        # Construct parameters
-        query_parameters = {}
-        query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
-
-        # Construct headers
-        header_parameters = {}
-        header_parameters['Accept'] = 'application/json'
-        header_parameters['Content-Type'] = 'application/json; charset=utf-8'
-        if self.config.generate_client_request_id:
-            header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
-        if custom_headers:
-            header_parameters.update(custom_headers)
-        if self.config.accept_language is not None:
-            header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
-
-        # Construct body
-        body_content = self._serialize.body(tags_parameter, 'Tags')
-
-        # Construct and send request
-        request = self._client.patch(url, query_parameters, header_parameters, body_content)
-        response = self._client.send(request, stream=False, **operation_config)
-
-        if response.status_code not in [200]:
-            raise models.ErrorResponseException(self._deserialize, response)
-
-        deserialized = None
-        if response.status_code == 200:
-            deserialized = self._deserialize('SapMonitor', response)
-
-        if raw:
-            client_raw_response = ClientRawResponse(deserialized, response)
-            return client_raw_response
-
-        return deserialized
-    update.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.HanaOnAzure/sapMonitors/{sapMonitorName}'}
+    delete.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.HanaOnAzure/sapMonitors/{sapMonitorName}/providerInstances/{providerInstanceName}'}
