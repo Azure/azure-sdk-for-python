2d207a63e899767c3794375a5c786c26dda642e3
SDK Automation <sdkautomation@microsoft.com>
2020-05-25 05:04:17 +0000

    Generated from 2d3a320884a0dfd28f3ee277cd2fe56fd170ba46
    Added api-version parameter under convertToSinglePlacementGroup for VMSS

--- sdk/compute/azure-mgmt-compute/azure/mgmt/compute/v2019_12_01/models/_compute_management_client_enums.py
+++ sdk/compute/azure-mgmt-compute/azure/mgmt/compute/v2019_12_01/models/_compute_management_client_enums.py
@@ -247,6 +247,12 @@ class DiffDiskOptions(str, Enum):
     local = "Local"
 
 
+class DiffDiskPlacement(str, Enum):
+
+    cache_disk = "CacheDisk"
+    resource_disk = "ResourceDisk"
+
+
 class PassNames(str, Enum):
 
     oobe_system = "OobeSystem"
@@ -333,6 +339,7 @@ class IPVersion(str, Enum):
 class OrchestrationServiceNames(str, Enum):
 
     automatic_repairs = "AutomaticRepairs"
+    
 
 
 class OrchestrationServiceState(str, Enum):
@@ -417,6 +424,7 @@ class StorageAccountType(str, Enum):
 
     standard_lrs = "Standard_LRS"
     standard_zrs = "Standard_ZRS"
+    premium_lrs = "Premium_LRS"
 
 
 class HostCaching(str, Enum):
