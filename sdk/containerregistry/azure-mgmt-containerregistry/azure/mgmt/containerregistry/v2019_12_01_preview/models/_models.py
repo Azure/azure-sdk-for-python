8a70e3e6c73ffcfcbd3a1558935ff1737c4c7d6f
Wei Dong <40835867+dw511214992@users.noreply.github.com>
2020-05-26 15:37:22 +0800

    Release azure mgmt containerregistry (#11460)
    * Update from master
    
    * update changelog, version and test recording
    
    Co-authored-by: SDK Automation <sdkautomation@microsoft.com>

--- sdk/containerregistry/azure-mgmt-containerregistry/azure/mgmt/containerregistry/v2019_12_01_preview/models/_models.py
+++ sdk/containerregistry/azure-mgmt-containerregistry/azure/mgmt/containerregistry/v2019_12_01_preview/models/_models.py
@@ -3187,6 +3187,11 @@ class Replication(Resource):
      called.
     :vartype status:
      ~azure.mgmt.containerregistry.v2019_12_01_preview.models.Status
+    :param region_endpoint_enabled: Specifies whether the replication's
+     regional endpoint is enabled. Requests will not be routed to a replication
+     whose regional endpoint is disabled, however its data will continue to be
+     synced with other replications. Default value: True .
+    :type region_endpoint_enabled: bool
     """
 
     _validation = {
@@ -3206,12 +3211,14 @@ class Replication(Resource):
         'tags': {'key': 'tags', 'type': '{str}'},
         'provisioning_state': {'key': 'properties.provisioningState', 'type': 'str'},
         'status': {'key': 'properties.status', 'type': 'Status'},
+        'region_endpoint_enabled': {'key': 'properties.regionEndpointEnabled', 'type': 'bool'},
     }
 
     def __init__(self, **kwargs):
         super(Replication, self).__init__(**kwargs)
         self.provisioning_state = None
         self.status = None
+        self.region_endpoint_enabled = kwargs.get('region_endpoint_enabled', True)
 
 
 class ReplicationUpdateParameters(Model):
@@ -3219,15 +3226,22 @@ class ReplicationUpdateParameters(Model):
 
     :param tags: The tags for the replication.
     :type tags: dict[str, str]
+    :param region_endpoint_enabled: Specifies whether the replication's
+     regional endpoint is enabled. Requests will not be routed to a replication
+     whose regional endpoint is disabled, however its data will continue to be
+     synced with other replications.
+    :type region_endpoint_enabled: bool
     """
 
     _attribute_map = {
         'tags': {'key': 'tags', 'type': '{str}'},
+        'region_endpoint_enabled': {'key': 'properties.regionEndpointEnabled', 'type': 'bool'},
     }
 
     def __init__(self, **kwargs):
         super(ReplicationUpdateParameters, self).__init__(**kwargs)
         self.tags = kwargs.get('tags', None)
+        self.region_endpoint_enabled = kwargs.get('region_endpoint_enabled', None)
 
 
 class Request(Model):
