660fdb83e34230c6490e383682ec0c288eec0923
changlong-liu <59815250+changlong-liu@users.noreply.github.com>
2020-05-28 16:46:32 +0800

    Sdk automation/azure mgmt loganalytics (#11675)
    * Update from master
    
    * changelog
    
    Co-authored-by: SDK Automation <sdkautomation@microsoft.com>

--- sdk/loganalytics/azure-mgmt-loganalytics/azure/mgmt/loganalytics/operations/__init__.py
+++ sdk/loganalytics/azure-mgmt-loganalytics/azure/mgmt/loganalytics/operations/__init__.py
@@ -20,12 +20,15 @@ from ._operation_statuses_operations import OperationStatusesOperations
 from ._shared_keys_operations import SharedKeysOperations
 from ._usages_operations import UsagesOperations
 from ._workspaces_operations import WorkspacesOperations
+from ._deleted_workspaces_operations import DeletedWorkspacesOperations
 from ._clusters_operations import ClustersOperations
 from ._storage_insight_configs_operations import StorageInsightConfigsOperations
 from ._saved_searches_operations import SavedSearchesOperations
+from ._available_service_tiers_operations import AvailableServiceTiersOperations
 from ._gateways_operations import GatewaysOperations
 from ._schema_operations import SchemaOperations
 from ._workspace_purge_operations import WorkspacePurgeOperations
+from ._tables_operations import TablesOperations
 
 __all__ = [
     'DataExportsOperations',
@@ -39,10 +42,13 @@ __all__ = [
     'SharedKeysOperations',
     'UsagesOperations',
     'WorkspacesOperations',
+    'DeletedWorkspacesOperations',
     'ClustersOperations',
     'StorageInsightConfigsOperations',
     'SavedSearchesOperations',
+    'AvailableServiceTiersOperations',
     'GatewaysOperations',
     'SchemaOperations',
     'WorkspacePurgeOperations',
+    'TablesOperations',
 ]
