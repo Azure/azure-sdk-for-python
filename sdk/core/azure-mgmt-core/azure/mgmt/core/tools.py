a5b1a74d35bd641c806f15a31427482f805b48be
Changlong Liu <changlong.liu@microsoft.com>
2020-05-29 15:30:22 +0800

    use regex

--- sdk/core/azure-mgmt-core/azure/mgmt/core/tools.py
+++ sdk/core/azure-mgmt-core/azure/mgmt/core/tools.py
@@ -30,13 +30,13 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 _ARMID_RE = re.compile(
-    "(?i)/subscriptions/(?P<subscription>[^/]*)(/resourceGroups/(?P<resource_group>[^/]*))?"
-    "(/providers/(?P<namespace>[^/]*)/(?P<type>[^/]*)/(?P<name>[^/]*)(?P<children>.*))?"
+    "(?i)/subscriptions/(?P<subscription>[^/]+)(/resourceGroups/(?P<resource_group>[^/]+))?"
+    "(/providers/(?P<namespace>[^/]+)/(?P<type>[^/]*)/(?P<name>[^/]+)(?P<children>.*))?"
 )
 
 _CHILDREN_RE = re.compile(
-    "(?i)(/providers/(?P<child_namespace>[^/]*))?/"
-    "(?P<child_type>[^/]*)/(?P<child_name>[^/]*)"
+    "(?i)(/providers/(?P<child_namespace>[^/]+))?/"
+    "(?P<child_type>[^/]*)/(?P<child_name>[^/]+)"
 )
 
 _ARMNAME_RE = re.compile("^[^<>%&:\\?/]{1,260}$")
@@ -204,9 +204,7 @@ def is_valid_resource_id(rid, exception_type=None):
     """
     is_valid = False
     try:
-        parsed_id = parse_resource_id(rid)
-        is_valid = rid and resource_id(**parsed_id).lower() == rid.lower() and all(
-            len(v) > 0 for k, v in parsed_id.items() if rid.find('/{}/'.format(k)) >= 0)
+        is_valid = rid and resource_id(**parse_resource_id(rid)).lower() == rid.lower()
     except KeyError:
         pass
     if not is_valid and exception_type:
