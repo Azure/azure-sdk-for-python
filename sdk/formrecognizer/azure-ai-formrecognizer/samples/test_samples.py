2dbb94235dee46ba73e7ecd07bceeae328564f62
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2020-05-31 23:23:04 -0700

    changes

--- sdk/formrecognizer/azure-ai-formrecognizer/samples/test_samples.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/samples/test_samples.py
@@ -30,14 +30,14 @@ def run(cmd):
     return proc.returncode, stdout, stderr
 
 for file_name in os.listdir('.'):
-    if file_name.startswith(file_name):
+    if file_name.startswith('sample_'):
         code, _, err = run([sys.executable, file_name])
         print(file_name)
         assert code == 0
         assert err is None
 
 for file_name in os.listdir('./async_samples'):
-    if file_name.startswith('async_samples/' + file_name):
+    if file_name.startswith('async_samples/' + 'sample_'):
         code, _, err = run([sys.executable, file_name])
         print(file_name)
         assert code == 0
