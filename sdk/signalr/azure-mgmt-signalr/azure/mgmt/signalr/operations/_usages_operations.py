7fe45ee8b7f8b0b61ad015d49a92050fca21a06e
changlong-liu <59815250+changlong-liu@users.noreply.github.com>
2020-06-02 01:41:04 +0800

    Sdk automation/azure mgmt signalr (#11703)
    * Update from master
    
    * changelog and run test
    
    * general breaking changelog
    
    * Update CHANGELOG.md
    
    Co-authored-by: SDK Automation <sdkautomation@microsoft.com>
    Co-authored-by: Zim Kalinowski <zikalino@microsoft.com>

--- sdk/signalr/azure-mgmt-signalr/azure/mgmt/signalr/operations/_usages_operations.py
+++ sdk/signalr/azure-mgmt-signalr/azure/mgmt/signalr/operations/_usages_operations.py
@@ -11,19 +11,20 @@
 
 import uuid
 from msrest.pipeline import ClientRawResponse
-from msrestazure.azure_exceptions import CloudError
 
 from .. import models
 
 
-class Operations(object):
-    """Operations operations.
+class UsagesOperations(object):
+    """UsagesOperations operations.
+
+    You should not instantiate directly this class, but create a Client instance that will create it for you and attach it as attribute.
 
     :param client: Client for service requests.
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
-    :ivar api_version: Client Api Version. Constant value: "2018-10-01".
+    :ivar api_version: Client Api Version. Constant value: "2020-05-01".
     """
 
     models = models
@@ -33,30 +34,36 @@ class Operations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
-        self.api_version = "2018-10-01"
+        self.api_version = "2020-05-01"
 
         self.config = config
 
     def list(
-            self, custom_headers=None, raw=False, **operation_config):
-        """Lists all of the available REST API operations of the
-        Microsoft.SignalRService provider.
+            self, location, custom_headers=None, raw=False, **operation_config):
+        """List usage quotas for Azure SignalR service by location.
 
+        :param location: the location like "eastus"
+        :type location: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: An iterator like instance of Operation
+        :return: An iterator like instance of SignalRUsage
         :rtype:
-         ~azure.mgmt.signalr.models.OperationPaged[~azure.mgmt.signalr.models.Operation]
-        :raises: :class:`CloudError<msrestazure.azure_exceptions.CloudError>`
+         ~azure.mgmt.signalr.models.SignalRUsagePaged[~azure.mgmt.signalr.models.SignalRUsage]
+        :raises:
+         :class:`ErrorResponseException<azure.mgmt.signalr.models.ErrorResponseException>`
         """
-        def internal_paging(next_link=None, raw=False):
-
+        def prepare_request(next_link=None):
             if not next_link:
                 # Construct URL
                 url = self.list.metadata['url']
+                path_format_arguments = {
+                    'location': self._serialize.url("location", location, 'str'),
+                    'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str')
+                }
+                url = self._client.format_url(url, **path_format_arguments)
 
                 # Construct parameters
                 query_parameters = {}
@@ -78,22 +85,23 @@ class Operations(object):
 
             # Construct and send request
             request = self._client.get(url, query_parameters, header_parameters)
+            return request
+
+        def internal_paging(next_link=None):
+            request = prepare_request(next_link)
+
             response = self._client.send(request, stream=False, **operation_config)
 
             if response.status_code not in [200]:
-                exp = CloudError(response)
-                exp.request_id = response.headers.get('x-ms-request-id')
-                raise exp
+                raise models.ErrorResponseException(self._deserialize, response)
 
             return response
 
         # Deserialize response
-        deserialized = models.OperationPaged(internal_paging, self._deserialize.dependencies)
-
+        header_dict = None
         if raw:
             header_dict = {}
-            client_raw_response = models.OperationPaged(internal_paging, self._deserialize.dependencies, header_dict)
-            return client_raw_response
+        deserialized = models.SignalRUsagePaged(internal_paging, self._deserialize.dependencies, header_dict)
 
         return deserialized
-    list.metadata = {'url': '/providers/Microsoft.SignalRService/operations'}
+    list.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.SignalRService/locations/{location}/usages'}
