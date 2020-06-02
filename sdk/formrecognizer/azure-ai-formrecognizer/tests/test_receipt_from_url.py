7bb73e4d126623a8ad2cdf5db804dfbf16244984
iscai-msft <iscai@microsoft.com>
2020-06-02 18:34:37 -0400

    updated tests

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_receipt_from_url.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_receipt_from_url.py
@@ -9,7 +9,7 @@ from datetime import date, time
 from azure.core.exceptions import HttpResponseError, ServiceRequestError, ClientAuthenticationError
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer._generated.models import AnalyzeOperationResult
-from azure.ai.formrecognizer._response_handlers import prepare_us_receipt
+from azure.ai.formrecognizer._response_handlers import prepare_receipt
 from azure.ai.formrecognizer import FormRecognizerClient
 from testcase import FormRecognizerTest, GlobalFormRecognizerAccountPreparer
 
@@ -65,7 +65,7 @@ class TestReceiptFromUrl(FormRecognizerTest):
 
         def callback(raw_response, _, headers):
             analyze_result = client._client._deserialize(AnalyzeOperationResult, raw_response)
-            extracted_receipt = prepare_us_receipt(analyze_result)
+            extracted_receipt = prepare_receipt(analyze_result)
             responses.append(analyze_result)
             responses.append(extracted_receipt)
 
@@ -83,17 +83,6 @@ class TestReceiptFromUrl(FormRecognizerTest):
         read_results = raw_response.analyze_result.read_results
         document_results = raw_response.analyze_result.document_results
 
-        # check hardcoded values
-        self.assertFormFieldTransformCorrect(receipt.merchant_address, actual.get("MerchantAddress"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.merchant_name, actual.get("MerchantName"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.merchant_phone_number, actual.get("MerchantPhoneNumber"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.subtotal, actual.get("Subtotal"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.tax, actual.get("Tax"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.tip, actual.get("Tip"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.total, actual.get("Total"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.transaction_date, actual.get("TransactionDate"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.transaction_time, actual.get("TransactionTime"), read_results)
-
         # check dict values
         self.assertFormFieldTransformCorrect(receipt.fields.get("MerchantAddress"), actual.get("MerchantAddress"), read_results)
         self.assertFormFieldTransformCorrect(receipt.fields.get("MerchantName"), actual.get("MerchantName"), read_results)
@@ -114,7 +103,7 @@ class TestReceiptFromUrl(FormRecognizerTest):
         self.assertEqual(receipt.receipt_type.type, actual["ReceiptType"].value_string)
 
         # check receipt items
-        self.assertReceiptItemsTransformCorrect(receipt.receipt_items, actual["Items"], read_results)
+        self.assertReceiptItemsTransformCorrect(receipt.fields["Items"].value, actual["Items"], read_results)
 
         # Check page metadata
         self.assertFormPagesTransformCorrect(receipt.pages, read_results)
@@ -127,7 +116,7 @@ class TestReceiptFromUrl(FormRecognizerTest):
 
         def callback(raw_response, _, headers):
             analyze_result = client._client._deserialize(AnalyzeOperationResult, raw_response)
-            extracted_receipt = prepare_us_receipt(analyze_result)
+            extracted_receipt = prepare_receipt(analyze_result)
             responses.append(analyze_result)
             responses.append(extracted_receipt)
 
@@ -145,17 +134,6 @@ class TestReceiptFromUrl(FormRecognizerTest):
         read_results = raw_response.analyze_result.read_results
         document_results = raw_response.analyze_result.document_results
 
-        # check hardcoded values
-        self.assertFormFieldTransformCorrect(receipt.merchant_address, actual.get("MerchantAddress"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.merchant_name, actual.get("MerchantName"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.merchant_phone_number, actual.get("MerchantPhoneNumber"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.subtotal, actual.get("Subtotal"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.tax, actual.get("Tax"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.tip, actual.get("Tip"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.total, actual.get("Total"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.transaction_date, actual.get("TransactionDate"), read_results)
-        self.assertFormFieldTransformCorrect(receipt.transaction_time, actual.get("TransactionTime"), read_results)
-
         # check dict values
         self.assertFormFieldTransformCorrect(receipt.fields.get("MerchantAddress"), actual.get("MerchantAddress"), read_results)
         self.assertFormFieldTransformCorrect(receipt.fields.get("MerchantName"), actual.get("MerchantName"), read_results)
@@ -176,7 +154,7 @@ class TestReceiptFromUrl(FormRecognizerTest):
         self.assertEqual(receipt.receipt_type.type, actual["ReceiptType"].value_string)
 
         # check receipt items
-        self.assertReceiptItemsTransformCorrect(receipt.receipt_items, actual["Items"], read_results)
+        self.assertReceiptItemsTransformCorrect(receipt.fields["Items"].value, actual["Items"], read_results)
 
         # Check page metadata
         self.assertFormPagesTransformCorrect(receipt.pages, read_results)
@@ -196,7 +174,7 @@ class TestReceiptFromUrl(FormRecognizerTest):
 
         self.assertFormPagesHasValues(receipt.pages)
         for field, value in receipt.__dict__.items():
-            if field not in ["receipt_type", "receipt_items", "page_range", "pages", "fields", "form_type", "receipt_locale"]:
+            if field not in ["receipt_type", "receipt_items", "page_range", "pages", "fields", "form_type"]:
                 field = getattr(receipt, field)
                 self.assertTextContentHasValues(field.value_data.text_content, receipt.page_range.first_page_number)
 
@@ -212,21 +190,21 @@ class TestReceiptFromUrl(FormRecognizerTest):
         result = poller.result()
         self.assertEqual(len(result), 1)
         receipt = result[0]
-        self.assertEqual(receipt.merchant_address.value, '123 Main Street Redmond, WA 98052')
-        self.assertEqual(receipt.merchant_name.value, 'Contoso Contoso')
-        self.assertEqual(receipt.merchant_phone_number.value, '+19876543210')
-        self.assertEqual(receipt.subtotal.value, 11.7)
-        self.assertEqual(receipt.tax.value, 1.17)
-        self.assertEqual(receipt.tip.value, 1.63)
-        self.assertEqual(receipt.total.value, 14.5)
-        self.assertEqual(receipt.transaction_date.value, date(year=2019, month=6, day=10))
-        self.assertEqual(receipt.transaction_time.value, time(hour=13, minute=59, second=0))
+        self.assertEqual(receipt.fields.get("MerchantAddress").value, '123 Main Street Redmond, WA 98052')
+        self.assertEqual(receipt.fields.get("MerchantName").value, 'Contoso Contoso')
+        self.assertEqual(receipt.fields.get("MerchantPhoneNumber").value, '+19876543210')
+        self.assertEqual(receipt.fields.get("Subtotal").value, 11.7)
+        self.assertEqual(receipt.fields.get("Tax").value, 1.17)
+        self.assertEqual(receipt.fields.get("Tip").value, 1.63)
+        self.assertEqual(receipt.fields.get("Total").value, 14.5)
+        self.assertEqual(receipt.fields.get("TransactionDate").value, date(year=2019, month=6, day=10))
+        self.assertEqual(receipt.fields.get("TransactionTime").value, time(hour=13, minute=59, second=0))
         self.assertEqual(receipt.page_range.first_page_number, 1)
         self.assertEqual(receipt.page_range.last_page_number, 1)
         self.assertFormPagesHasValues(receipt.pages)
         self.assertIsNotNone(receipt.receipt_type.confidence)
         self.assertEqual(receipt.receipt_type.type, 'Itemized')
-        self.assertReceiptItemsHasValues(receipt.receipt_items, receipt.page_range.first_page_number, False)
+        self.assertReceiptItemsHasValues(receipt.fields["Items"].value, receipt.page_range.first_page_number, False)
 
     @GlobalFormRecognizerAccountPreparer()
     def test_receipt_url_png(self, resource_group, location, form_recognizer_account, form_recognizer_account_key):
@@ -237,13 +215,13 @@ class TestReceiptFromUrl(FormRecognizerTest):
         result = poller.result()
         self.assertEqual(len(result), 1)
         receipt = result[0]
-        self.assertEqual(receipt.merchant_address.value, '123 Main Street Redmond, WA 98052')
-        self.assertEqual(receipt.merchant_name.value, 'Contoso Contoso')
-        self.assertEqual(receipt.subtotal.value, 1098.99)
-        self.assertEqual(receipt.tax.value, 104.4)
-        self.assertEqual(receipt.total.value, 1203.39)
-        self.assertEqual(receipt.transaction_date.value, date(year=2019, month=6, day=10))
-        self.assertEqual(receipt.transaction_time.value, time(hour=13, minute=59, second=0))
+        self.assertEqual(receipt.fields.get("MerchantAddress").value, '123 Main Street Redmond, WA 98052')
+        self.assertEqual(receipt.fields.get("MerchantName").value, 'Contoso Contoso')
+        self.assertEqual(receipt.fields.get("Subtotal").value, 1098.99)
+        self.assertEqual(receipt.fields.get("Tax").value, 104.4)
+        self.assertEqual(receipt.fields.get("Total").value, 1203.39)
+        self.assertEqual(receipt.fields.get("TransactionDate").value, date(year=2019, month=6, day=10))
+        self.assertEqual(receipt.fields.get("TransactionTime").value, time(hour=13, minute=59, second=0))
         self.assertEqual(receipt.page_range.first_page_number, 1)
         self.assertEqual(receipt.page_range.last_page_number, 1)
         self.assertFormPagesHasValues(receipt.pages)
@@ -259,22 +237,22 @@ class TestReceiptFromUrl(FormRecognizerTest):
 
         self.assertEqual(len(result), 3)
         receipt = result[0]
-        self.assertEqual(receipt.merchant_address.value, '123 Hobbit Lane 567 Main St. Redmond, WA Redmond, WA')
-        self.assertEqual(receipt.merchant_name.value, 'Bilbo Baggins')
-        self.assertEqual(receipt.merchant_phone_number.value, '+15555555555')
-        self.assertEqual(receipt.subtotal.value, 300.0)
-        self.assertEqual(receipt.total.value, 100.0)
+        self.assertEqual(receipt.fields.get("MerchantAddress").value, '123 Hobbit Lane 567 Main St. Redmond, WA Redmond, WA')
+        self.assertEqual(receipt.fields.get("MerchantName").value, 'Bilbo Baggins')
+        self.assertEqual(receipt.fields.get("MerchantPhoneNumber").value, '+15555555555')
+        self.assertEqual(receipt.fields.get("Subtotal").value, 300.0)
+        self.assertEqual(receipt.fields.get("Total").value, 100.0)
         self.assertEqual(receipt.page_range.first_page_number, 1)
         self.assertEqual(receipt.page_range.last_page_number, 1)
         self.assertFormPagesHasValues(receipt.pages)
         self.assertIsNotNone(receipt.receipt_type.confidence)
         self.assertEqual(receipt.receipt_type.type, 'Itemized')
         receipt = result[2]
-        self.assertEqual(receipt.merchant_address.value, '123 Hobbit Lane 567 Main St. Redmond, WA Redmond, WA')
-        self.assertEqual(receipt.merchant_name.value, 'Frodo Baggins')
-        self.assertEqual(receipt.merchant_phone_number.value, '+15555555555')
-        self.assertEqual(receipt.subtotal.value, 3000.0)
-        self.assertEqual(receipt.total.value, 1000.0)
+        self.assertEqual(receipt.fields.get("MerchantAddress").value, '123 Hobbit Lane 567 Main St. Redmond, WA Redmond, WA')
+        self.assertEqual(receipt.fields.get("MerchantName").value, 'Frodo Baggins')
+        self.assertEqual(receipt.fields.get("MerchantPhoneNumber").value, '+15555555555')
+        self.assertEqual(receipt.fields.get("Subtotal").value, 3000.0)
+        self.assertEqual(receipt.fields.get("Total").value, 1000.0)
         self.assertEqual(receipt.page_range.first_page_number, 3)
         self.assertEqual(receipt.page_range.last_page_number, 3)
         self.assertFormPagesHasValues(receipt.pages)
@@ -289,7 +267,7 @@ class TestReceiptFromUrl(FormRecognizerTest):
 
         def callback(raw_response, _, headers):
             analyze_result = client._client._deserialize(AnalyzeOperationResult, raw_response)
-            extracted_receipt = prepare_us_receipt(analyze_result)
+            extracted_receipt = prepare_receipt(analyze_result)
             responses.append(analyze_result)
             responses.append(extracted_receipt)
 
@@ -311,15 +289,6 @@ class TestReceiptFromUrl(FormRecognizerTest):
         for receipt, actual in zip(returned_model, actual):
             if actual.fields is None:  # second page is blank
                 continue
-            self.assertFormFieldTransformCorrect(receipt.merchant_address, actual.fields.get("MerchantAddress"), read_results)
-            self.assertFormFieldTransformCorrect(receipt.merchant_name, actual.fields.get("MerchantName"), read_results)
-            self.assertFormFieldTransformCorrect(receipt.merchant_phone_number, actual.fields.get("MerchantPhoneNumber"), read_results)
-            self.assertFormFieldTransformCorrect(receipt.subtotal, actual.fields.get("Subtotal"), read_results)
-            self.assertFormFieldTransformCorrect(receipt.tax, actual.fields.get("Tax"), read_results)
-            self.assertFormFieldTransformCorrect(receipt.tip, actual.fields.get("Tip"), read_results)
-            self.assertFormFieldTransformCorrect(receipt.total, actual.fields.get("Total"), read_results)
-            self.assertFormFieldTransformCorrect(receipt.transaction_date, actual.fields.get("TransactionDate"), read_results)
-            self.assertFormFieldTransformCorrect(receipt.transaction_time, actual.fields.get("TransactionTime"), read_results)
 
             # check dict values
             self.assertFormFieldTransformCorrect(receipt.fields.get("MerchantAddress"), actual.fields.get("MerchantAddress"), read_results)
@@ -341,7 +310,7 @@ class TestReceiptFromUrl(FormRecognizerTest):
             self.assertEqual(receipt.receipt_type.type, actual.fields["ReceiptType"].value_string)
 
             # check receipt items
-            self.assertReceiptItemsTransformCorrect(receipt.receipt_items, actual.fields["Items"], read_results)
+            self.assertReceiptItemsTransformCorrect(receipt.fields["Items"].value, actual.fields["Items"], read_results)
 
         # Check form pages
         self.assertFormPagesTransformCorrect(returned_model, read_results)
