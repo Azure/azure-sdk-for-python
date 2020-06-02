855627c2f5c3741a9a6c77953385b436d1ee38b7
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2020-06-02 14:12:51 -0700

    Add license header

--- sdk/servicebus/azure-servicebus/azure/servicebus/management/__init__.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/management/__init__.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from ._management_client import ServiceBusManagementClient
 from ._shared_key_policy import ServiceBusSharedKeyCredentialPolicy
 from ._generated.models import QueueDescription, AuthorizationRule, MessageCountDetails, QueueRuntimeInfo, \
--- sdk/servicebus/azure-servicebus/azure/servicebus/management/_constants.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/management/_constants.py
@@ -1,6 +1,10 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 API_VERSION = "2017-04"
 ENTRY_TAG = "{http://www.w3.org/2005/Atom}entry"
 CONTENT_TAG = "{http://www.w3.org/2005/Atom}content"
 QUEUE_DESCRIPTION_TAG = "{http://schemas.microsoft.com/netservices/2010/10/servicebus/connect}QueueDescription"
 TITLE_TAG = "{http://www.w3.org/2005/Atom}title"
-
