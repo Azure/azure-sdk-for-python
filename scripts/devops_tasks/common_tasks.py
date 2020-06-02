b1e9ace0c075bb7896910c5efc3617fadd74d674
Scott Beddall <scbedd@microsoft.com>
2020-06-02 15:50:15 -0700

    adjustments to pass the analyze check + allow no tests

--- scripts/devops_tasks/common_tasks.py
+++ scripts/devops_tasks/common_tasks.py
@@ -48,7 +48,8 @@ MANAGEMENT_PACKAGE_IDENTIFIERS = [
     "azure-servicefabric",
     "nspkg",
     "azure-keyvault",
-    "azure-synapse"
+    "azure-synapse",
+    "azure-attestation"
 ]
 META_PACKAGES = ["azure", "azure-mgmt", "azure-keyvault"]
 REGRESSION_EXCLUDED_PACKAGES = [
