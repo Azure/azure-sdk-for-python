da3165e8f76d85747ac6d70b1a8acf5b5f94016b
Charles Lowell <chlowe@microsoft.com>
2020-06-03 12:49:59 -0700

    Add AzureCliCredential and VSCodeCredential to public API

--- sdk/identity/azure-identity/tests/test_cli_credential_async.py
+++ sdk/identity/azure-identity/tests/test_cli_credential_async.py
@@ -8,7 +8,7 @@ import sys
 from unittest import mock
 
 from azure.identity import CredentialUnavailableError
-from azure.identity.aio._credentials.azure_cli import AzureCliCredential
+from azure.identity.aio import AzureCliCredential
 from azure.identity._credentials.azure_cli import CLI_NOT_FOUND
 from azure.core.exceptions import ClientAuthenticationError
 import pytest
