4dd9761595d048530ecab0f5f62c6b3cf94473ba
Xiang Yan <xiangsjtu@gmail.com>
2020-06-09 16:21:23 -0700

    update

--- sdk/identity/azure-identity/azure/identity/_credentials/linux_vscode_adapter.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/linux_vscode_adapter.py
@@ -5,37 +5,31 @@
 import os
 import json
 import ctypes as ct
+from .._constants import VSCODE_CREDENTIALS_SECTION
 
 
 def _c_str(string):
     return ct.c_char_p(string.encode("utf-8"))
 
 
-class _SecretSchemaAttribute(ct.Structure):
+class _SECRET_SCHEMA_ATTRIBUTE(ct.Structure):
     _fields_ = [
         ("name", ct.c_char_p),
         ("type", ct.c_uint),
     ]
 
-class _SecretSchema(ct.Structure):
+
+class _SECRET_SCHEMA(ct.Structure):
     _fields_ = [
         ("name", ct.c_char_p),
         ("flags", ct.c_uint),
-        ("attributes", _SecretSchemaAttribute * 2),
+        ("attributes", _SECRET_SCHEMA_ATTRIBUTE * 2),
     ]
-_PSecretSchema = ct.POINTER(_SecretSchema)
+_PSECRET_SCHEMA = ct.POINTER(_SECRET_SCHEMA)
+
 
 try:
     _libsecret = ct.cdll.LoadLibrary("libsecret-1.so.0")
-    _libsecret.secret_schema_new.argtypes = [
-        ct.c_char_p,
-        ct.c_int,
-        ct.c_char_p,
-        ct.c_int,
-        ct.c_char_p,
-        ct.c_int,
-        ct.c_void_p,
-    ]
     _libsecret.secret_password_lookup_sync.argtypes = [
         ct.c_void_p,
         ct.c_void_p,
@@ -47,7 +41,6 @@ try:
         ct.c_void_p,
     ]
     _libsecret.secret_password_lookup_sync.restype = ct.c_char_p
-    _libsecret.secret_schema_unref.argtypes = [ct.c_void_p]
 except OSError:
     _libsecret = None
 
@@ -73,19 +66,16 @@ def _get_refresh_token(service_name, account_name):
         return None
 
     err = ct.c_int()
-    #schema = _libsecret.secret_schema_new(
-    #    _c_str("org.freedesktop.Secret.Generic"), 2, _c_str("service"), 0, _c_str("account"), 0, None
-    #)
-    attribute1 = _SecretSchemaAttribute()
+    attribute1 = _SECRET_SCHEMA_ATTRIBUTE()
     setattr(attribute1, "name", _c_str("service"))
     setattr(attribute1, "type", 0)
-    attribute2 = _SecretSchemaAttribute()
+    attribute2 = _SECRET_SCHEMA_ATTRIBUTE()
     setattr(attribute2, "name", _c_str("account"))
     setattr(attribute2, "type", 0)
     attributes = [attribute1, attribute2]
-    pattributes = (_SecretSchemaAttribute * 2)(*attributes)
-    schema = _SecretSchema()
-    pschema = _PSecretSchema(schema)
+    pattributes = (_SECRET_SCHEMA_ATTRIBUTE * 2)(*attributes)
+    schema = _SECRET_SCHEMA()
+    pschema = _SECRET_SCHEMA(schema)
     ct.memset(pschema, 0, ct.sizeof(schema))
     setattr(schema, "name", _c_str("org.freedesktop.Secret.Generic"))
     setattr(schema, "flags", 2)
@@ -100,7 +90,6 @@ def _get_refresh_token(service_name, account_name):
         _c_str(account_name),
         None,
     )
-    #_libsecret.secret_schema_unref(schema)
     if err.value == 0:
         return p_str.decode("utf-8")
 
@@ -108,5 +97,9 @@ def _get_refresh_token(service_name, account_name):
 
 
 def get_credentials():
-    # Disable linux support for further investigation
-    raise NotImplementedError("Not supported")
+    try:
+        environment_name = _get_user_settings()
+        credentials = _get_refresh_token(VSCODE_CREDENTIALS_SECTION, environment_name)
+        return credentials
+    except Exception:  # pylint: disable=broad-except
+        return None
