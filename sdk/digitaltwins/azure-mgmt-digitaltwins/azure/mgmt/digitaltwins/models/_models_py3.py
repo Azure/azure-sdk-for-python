9949be281bfa1e5cb3c56c6d0a7f57057f997c21
Changlong Liu <changlong.liu@microsoft.com>
2020-06-10 11:02:48 +0800

    adjust model for m4 issue #83

--- sdk/digitaltwins/azure-mgmt-digitaltwins/azure/mgmt/digitaltwins/models/_models_py3.py
+++ sdk/digitaltwins/azure-mgmt-digitaltwins/azure/mgmt/digitaltwins/models/_models_py3.py
@@ -13,6 +13,29 @@ from msrest.serialization import Model
 from msrest.exceptions import HttpOperationError
 
 
+class DigitalTwinsSkuInfo(Model):
+    """Information about the SKU of the DigitalTwinsInstance.
+
+    Variables are only populated by the server, and will be ignored when
+    sending a request.
+
+    All required parameters must be populated in order to send to Azure.
+
+    :ivar name: Required. The name of the SKU. Default value: "F1" .
+    :vartype name: str
+    """
+
+    _validation = {
+        'name': {'required': True, 'constant': True},
+    }
+
+    _attribute_map = {
+        'name': {'key': 'name', 'type': 'str'},
+    }
+
+    name = "F1"
+
+
 class CheckNameRequest(Model):
     """The result returned from a database check name availability request.
 
@@ -333,29 +356,6 @@ class DigitalTwinsPatchDescription(Model):
     def __init__(self, *, tags=None, **kwargs) -> None:
         super(DigitalTwinsPatchDescription, self).__init__(**kwargs)
         self.tags = tags
-
-
-class DigitalTwinsSkuInfo(Model):
-    """Information about the SKU of the DigitalTwinsInstance.
-
-    Variables are only populated by the server, and will be ignored when
-    sending a request.
-
-    All required parameters must be populated in order to send to Azure.
-
-    :ivar name: Required. The name of the SKU. Default value: "F1" .
-    :vartype name: str
-    """
-
-    _validation = {
-        'name': {'required': True, 'constant': True},
-    }
-
-    _attribute_map = {
-        'name': {'key': 'name', 'type': 'str'},
-    }
-
-    name = "F1"
 
 
 class ErrorDefinition(Model):
