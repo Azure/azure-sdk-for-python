d65b4e5a2cc9836d5489f0e59d18af1513477d7f
Krista Pratico <krpratic@microsoft.com>
2020-06-11 09:16:44 -0700

    switch to testing in canary region

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_receipt_from_url.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_receipt_from_url.py
@@ -293,7 +293,7 @@ class TestReceiptFromUrl(FormRecognizerTest):
 
         # check hardcoded values
         for receipt, actual in zip(returned_model, actual):
-            if actual.fields is None:  # second page is blank
+            if not actual.fields:  # second page is blank
                 continue
 
             # check dict values
