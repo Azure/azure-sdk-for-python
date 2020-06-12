0ed191091aecd8d004c94a85f3d9f0ea933ba6c0
antisch <antisch@microsoft.com>
2020-06-11 19:31:10 -0700

    Started test conversion

--- sdk/storage/azure-storage-blob/tests/test_quick_query.py
+++ sdk/storage/azure-storage-blob/tests/test_quick_query.py
@@ -9,7 +9,8 @@
 from _shared.testcase import StorageTestCase, GlobalStorageAccountPreparer
 from azure.storage.blob import (
     BlobServiceClient,
-    DelimitedTextConfiguration,
+    CSVDialect,
+    JSONEncoder
 )
 
 # ------------------------------------------------------------------------------
@@ -44,6 +45,48 @@ CSV_DATA = b'Service,Package,Version,RepoPath,MissingDocs\r\nApp Configuration,'
            b'Service,Package,Version,RepoPath,MissingDocs\r\n' \
            b'App Configuration,azure-data-appconfiguration,1.0.1,appconfiguration,FALSE\r\n' \
            b'Event Hubs,azure-messaging-eventhubs,5.0.1,eventhubs,FALSE'
+CONVERTED_CSV_DATA = b"Service;Package;Version;RepoPath;MissingDocs.App Configuration;azure-data-appconfiguration;" \
+                     b"'1.0.1';appconfiguration;FALSE.Event Hubs;azure-messaging-eventhubs;'5.0.1';eventhubs;" \
+                     b"FALSE.Event Hubs - Azure Storage CheckpointStore;azure-messaging-eventhubs-checkpointstore-blob;" \
+                     b"'1.0.1';eventhubs;FALSE.Identity;azure-identity;'1.1.0-beta.1';identity;FALSE.Key Vault - Certificates;" \
+                     b"azure-security-keyvault-certificates;'4.0.0';keyvault;FALSE.Key Vault - Keys;azure-security-keyvault-keys;" \
+                     b"'4.2.0-beta.1';keyvault;FALSE.Key Vault - Secrets;azure-security-keyvault-secrets;'4.1.0';keyvault;" \
+                     b"FALSE.Storage - Blobs;azure-storage-blob;'12.4.0';storage;FALSE.Storage - Blobs Batch;" \
+                     b"azure-storage-blob-batch;'12.4.0-beta.1';storage;FALSE.Storage - Blobs Cryptography;" \
+                     b"azure-storage-blob-cryptography;'12.4.0';storage;FALSE.Storage - File Shares;azure-storage-file-share;" \
+                     b"'12.2.0';storage;FALSE.Storage - Queues;azure-storage-queue;'12.3.0';storage;FALSE.Text Analytics;" \
+                     b"azure-ai-textanalytics;'1.0.0-beta.2';textanalytics;FALSE.Tracing;azure-core-tracing-opentelemetry;" \
+                     b"'1.0.0-beta.2';core;FALSE.Service;Package;Version;RepoPath;MissingDocs.App Configuration;" \
+                     b"azure-data-appconfiguration;'1.0.1';appconfiguration;FALSE.Event Hubs;azure-messaging-eventhubs;" \
+                     b"'5.0.1';eventhubs;FALSE.Event Hubs - Azure Storage CheckpointStore;azure-messaging-eventhubs-checkpointstore-blob;" \
+                     b"'1.0.1';eventhubs;FALSE.Identity;azure-identity;'1.1.0-beta.1';identity;" \
+                     b"FALSE.Key Vault - Certificates;azure-security-keyvault-certificates;'4.0.0';" \
+                     b"keyvault;FALSE.Key Vault - Keys;azure-security-keyvault-keys;'4.2.0-beta.1';keyvault;FALSE.Key Vault - Secrets;" \
+                     b"azure-security-keyvault-secrets;'4.1.0';keyvault;FALSE.Storage - Blobs;azure-storage-blob;'12.4.0';" \
+                     b"storage;FALSE.Storage - Blobs Batch;azure-storage-blob-batch;'12.4.0-beta.1';storage;FALSE.Storage - Blobs Cryptography;" \
+                     b"azure-storage-blob-cryptography;'12.4.0';storage;FALSE.Storage - File Shares;azure-storage-file-share;" \
+                     b"'12.2.0';storage;FALSE.Storage - Queues;azure-storage-queue;'12.3.0';storage;FALSE.Text Analytics;" \
+                     b"azure-ai-textanalytics;'1.0.0-beta.2';textanalytics;FALSE.Tracing;azure-core-tracing-opentelemetry;" \
+                     b"'1.0.0-beta.2';core;FALSE.Service;Package;Version;RepoPath;MissingDocs.App Configuration;" \
+                     b"azure-data-appconfiguration;'1.0.1';appconfiguration;FALSE.Event Hubs;azure-messaging-eventhubs;" \
+                     b"'5.0.1';eventhubs;FALSE."
+
+ERROR_CSV_DATA = b'Service,Package,Version,RepoPath,MissingDocs\r\nApp Configuration,' \
+                 b'azure-data-appconfiguration,1.0.1,appconfiguration,FALSE\r\nEvent Hubs,' \
+                 b'azure-messaging-eventhubs,5.0.1,eventhubs,FALSE\r\nEvent Hubs - Azure Storage CheckpointStore,' \
+                 b'azure-messaging-eventhubs-checkpointstore-blob,1.0.1,eventhubs,FALSE\r\nIdentity,azure-identity,' \
+                 b'1.1.0-beta.1,identity,FALSE\r\nKey Vault - Certificates,azure-security-keyvault-certificates,' \
+                 b'4.0.0,keyvault,FALSE\r\nKey Vault - Keys,azure-security-keyvault-keys,4.2.0-beta.1,keyvault,' \
+                 b'FALSE\r\nKey Vault - Secrets,azure-security-keyvault-secrets,4.1.0,keyvault,FALSE\r\n' \
+                 b'Storage - Blobs,azure-storage-blob,12.4.0,storage,FALSE\r\nStorage - Blobs Batch,' \
+                 b'azure-storage-blob-batch,12.4.0-beta.1,storage,FALSE\r\nStorage - Blobs Cryptography,' \
+                 b'azure-storage-blob-cryptography,12.4.0,storage,FALSE\r\nStorage - File Shares,' \
+                 b'azure-storage-file-share,12.2.0,storage,FALSE\r\nStorage - Queues,' \
+                 b'azure-storage-queue,12.3.0,storage,FALSE\r\nText Analytics,' \
+                 b'azure-ai-textanalytics,1.0.0-beta.2,textanalytics,FALSE\r\nTracing,' \
+                 b'azure-core-tracing-opentelemetry,1.0.0-beta.2,core,FALSE\r\nService,Package,Version,RepoPath,' \
+                 b'MissingDocs\r\nApp Configuration,azure-data-appconfiguration,1.0.1,appconfiguration,FALSE\r\n' \
+                 b'Event Hubs,azure-messaging-eventhubs,5.0.1,eventhubs,FALSE'
 
 
 # ------------------------------------------------------------------------------
@@ -77,7 +120,7 @@ class StorageQuickQueryTest(StorageTestCase):
     # -- Test cases for APIs supporting CPK ----------------------------------------------
 
     @GlobalStorageAccountPreparer()
-    def test_quick_query(self, resource_group, location, storage_account, storage_account_key):
+    def test_quick_query_readall(self, resource_group, location, storage_account, storage_account_key):
         # Arrange
         bsc = BlobServiceClient(
             self.account_url(storage_account, "blob"),
@@ -87,27 +130,49 @@ class StorageQuickQueryTest(StorageTestCase):
         # upload the csv file
         blob_name = self._get_blob_reference()
         blob_client = bsc.get_blob_client(self.container_name, blob_name)
-        blob_client.upload_blob(CSV_DATA)
+        blob_client.upload_blob(CSV_DATA, overwrite=True)
 
         errors = []
 
-        def progress_callback(error, bytes_processed, total_bytes):
-            if error:
-                errors.append(error)
-            if not bytes_processed:
-                print("All bytes have been processed")
-                print("Total Bytes should be {}".format(total_bytes))
-            else:
-                print(bytes_processed)
+        def on_error(error):
+            errors.append(error)
 
-        resp = blob_client.query("SELECT * from BlobStorage", progress_callback=progress_callback)
-        resp.readall()
+        reader = blob_client.query_blob("SELECT * from BlobStorage", errors=on_error)
+        data = reader.readall()
 
         self.assertEqual(len(errors), 0)
-        self.assertEqual(resp.total_bytes, len(CSV_DATA))
+        self.assertEqual(len(reader), len(CSV_DATA))
+        self.assertEqual(reader.size, reader.tell())
+        #self.assertEqual(data, CSV_DATA)
 
     @GlobalStorageAccountPreparer()
-    def test_quick_query_with_serialization_setting(self, resource_group, location, storage_account,
+    def test_quick_query_readall_with_error(self, resource_group, location, storage_account, storage_account_key):
+        # Arrange
+        bsc = BlobServiceClient(
+            self.account_url(storage_account, "blob"),
+            credential=storage_account_key)
+        self._setup(bsc)
+
+        # upload the csv file
+        blob_name = self._get_blob_reference()
+        blob_client = bsc.get_blob_client(self.container_name, blob_name)
+        blob_client.upload_blob(ERROR_CSV_DATA, overwrite=True)
+
+        errors = []
+
+        def on_error(error):
+            errors.append(error)
+
+        reader = blob_client.query_blob("SELECT * from BlobStorage", errors=on_error)
+        data = reader.readall()
+
+        self.assertEqual(len(errors), 0)
+        self.assertEqual(len(reader), len(CSV_DATA))
+        self.assertEqual(reader.size, reader.tell())
+        self.assertEqual(data, CSV_DATA)
+
+    @GlobalStorageAccountPreparer()
+    def test_quick_query_readall_with_serialization_setting(self, resource_group, location, storage_account,
                                                     storage_account_key):
         # Arrange
         bsc = BlobServiceClient(
@@ -118,32 +183,82 @@ class StorageQuickQueryTest(StorageTestCase):
         # upload the csv file
         blob_name = self._get_blob_reference()
         blob_client = bsc.get_blob_client(self.container_name, blob_name)
-        blob_client.upload_blob(CSV_DATA)
+        blob_client.upload_blob(CSV_DATA, overwrite=True)
 
         errors = []
 
-        def progress_callback(error, bytes_processed, total_bytes):
-            if error:
-                errors.append(error)
-            if not bytes_processed:
-                print("All bytes have been processed")
-                print("Total Bytes should be {}".format(total_bytes))
-            else:
-                print(bytes_processed)
+        def on_error(error):
+            errors.append(error)
 
-        input_seri = DelimitedTextConfiguration(column_separator=',', field_quote='"', record_separator='\n',
-                                                escape_char='', headers_present=False
-                                                )
-        output_seri = DelimitedTextConfiguration(column_separator=';', field_quote="'", record_separator='.',
-                                                 escape_char='\\', headers_present=True
-                                                 )
-        resp = blob_client.query("SELECT * from BlobStorage", progress_callback=progress_callback,
-                                 input_serialization=input_seri, output_serialization=output_seri)
+        input_format = CSVDialect(
+            delimiter=',',
+            quotechar='"',
+            lineterminator='\n',
+            escapechar=''
+        )
+        output_format = CSVDialect(
+            delimiter=';',
+            quotechar="'",
+            lineterminator='.',
+            escapechar='\\'
+        )
+        resp = blob_client.query_blob(
+            "SELECT * from BlobStorage",
+            errors=on_error,
+            blob_format=input_format,
+            output_format=output_format,
+            has_header=False)
         query_result = resp.readall()
 
         self.assertEqual(len(errors), 0)
-        self.assertEqual(resp.total_bytes, len(CSV_DATA))
-        self.assertTrue(len(query_result) > 0)
+        self.assertEqual(resp.size, len(CSV_DATA))
+        self.assertEqual(query_result, CONVERTED_CSV_DATA)
+
+    @GlobalStorageAccountPreparer()
+    def test_quick_query_readall_with_fatal_error(self, resource_group, location, storage_account,
+                                                    storage_account_key):
+        # Arrange
+        bsc = BlobServiceClient(
+            self.account_url(storage_account, "blob"),
+            credential=storage_account_key)
+        self._setup(bsc)
+
+        data1 = b'{name: owner}'
+        data2 = b'{name2: owner2}'
+        data3 = b'{version:0,begin:1601-01-01T00:00:00.000Z,intervalSecs:3600,status:Finalized,config:' \
+                b'{version:0,configVersionEtag:0x8d75ef460eb1a12,numShards:1,recordsFormat:avro,formatSchemaVersion:3,' \
+                b'shardDistFnVersion:1},chunkFilePaths:[$blobchangefeed/log/00/1601/01/01/0000/],storageDiagnostics:' \
+                b'{version:0,lastModifiedTime:2019-11-01T17:53:18.861Z,' \
+                b'data:{aid:d305317d-a006-0042-00dd-902bbb06fc56}}}'
+        data = data1 + b'\n' + data2 + b'\n' + data1
+
+        # upload the json file
+        blob_name = self._get_blob_reference()
+        blob_client = bsc.get_blob_client(self.container_name, blob_name)
+        blob_client.upload_blob(data, overwrite=True)
+
+        errors = []
+
+        def on_error(error):
+            errors.append(error)
+
+        input_format = JSONEncoder()
+        output_format = CSVDialect(
+            delimiter=';',
+            quotechar="'",
+            lineterminator='.',
+            escapechar='\\'
+        )
+        resp = blob_client.query_blob(
+            "SELECT * from BlobStorage",
+            errors=on_error,
+            blob_format=input_format,
+            output_format=output_format)
+        query_result = resp.readall()
+
+        self.assertEqual(len(errors), 0)
+        self.assertEqual(resp.size, len(CSV_DATA))
+        self.assertEqual(query_result, CONVERTED_CSV_DATA)
 
     @GlobalStorageAccountPreparer()
     def test_quick_query_with_serialization_json_setting(self, resource_group, location, storage_account,
