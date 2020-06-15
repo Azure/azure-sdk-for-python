e3e434a7cc0a1325eea153341c075cc9c9e32ceb
Charles Lowell <chlowe@microsoft.com>
2020-06-15 16:32:04 -0700

    package scaffold

---
+++ sdk/keyvault/azure-keyvault-administration/azure/__init__.py
@@ -0,0 +1,5 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
