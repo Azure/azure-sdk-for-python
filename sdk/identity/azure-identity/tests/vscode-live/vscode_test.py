37043c90f1c8656652befa69e1e7fc6a8d144801
Xiang Yan <xiangsjtu@gmail.com>
2020-06-16 13:31:06 -0700

    add license

--- sdk/identity/azure-identity/tests/vscode-live/vscode_test.py
+++ sdk/identity/azure-identity/tests/vscode-live/vscode_test.py
@@ -1,3 +1,8 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
 from azure.identity import VSCodeCredential
 
 def test_live():
