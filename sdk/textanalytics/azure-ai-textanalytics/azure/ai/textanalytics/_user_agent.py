32b0ab5d745ca8e2f614afcc6876327921d25000
iscai-msft <iscai@microsoft.com>
2020-06-18 16:28:06 -0400

    fix user agent string (again)

--- sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_user_agent.py
+++ sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_user_agent.py
@@ -6,6 +6,4 @@
 
 from ._version import VERSION
 
-USER_AGENT = "ai-textanalytics/{} Python/{} ({})".format(
-    VERSION, platform.python_version(), platform.platform()
-)
+USER_AGENT = "ai-textanalytics/{}".format(VERSION)
