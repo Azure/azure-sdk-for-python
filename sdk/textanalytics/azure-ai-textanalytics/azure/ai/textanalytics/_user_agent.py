fcb044762dfc5e65a8e5fed44570f1e5b8e2a767
iscai-msft <iscai@microsoft.com>
2020-06-18 15:55:01 -0400

    fix duplicate platform info in user agent

--- sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_user_agent.py
+++ sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_user_agent.py
@@ -7,6 +7,4 @@
 import platform
 from ._version import VERSION
 
-USER_AGENT = "ai-textanalytics/{} Python/{} ({})".format(
-    VERSION, platform.python_version(), platform.platform()
-)
+USER_AGENT = "ai-textanalytics/{}".format(VERSION)
