1d3647d764647157aa7c1e270d02e063e9d46f70
SDK Automation <sdkautomation@microsoft.com>
2020-06-23 16:29:10 +0000

    Update from master

--- sdk/storage/azure-mgmt-storage/azure/mgmt/storage/v2019_06_01/aio/operations_async/_queue_operations_async.py
+++ sdk/storage/azure-mgmt-storage/azure/mgmt/storage/v2019_06_01/aio/operations_async/_queue_operations_async.py
@@ -95,7 +95,6 @@ class QueueOperations:
         header_parameters['Content-Type'] = self._serialize.header("content_type", content_type, 'str')
         header_parameters['Accept'] = 'application/json'
 
-        # Construct and send request
         body_content_kwargs = {}  # type: Dict[str, Any]
         body_content = self._serialize.body(queue, 'StorageQueue')
         body_content_kwargs['content'] = body_content
@@ -170,7 +169,6 @@ class QueueOperations:
         header_parameters['Content-Type'] = self._serialize.header("content_type", content_type, 'str')
         header_parameters['Accept'] = 'application/json'
 
-        # Construct and send request
         body_content_kwargs = {}  # type: Dict[str, Any]
         body_content = self._serialize.body(queue, 'StorageQueue')
         body_content_kwargs['content'] = body_content
@@ -240,7 +238,6 @@ class QueueOperations:
         header_parameters = {}  # type: Dict[str, Any]
         header_parameters['Accept'] = 'application/json'
 
-        # Construct and send request
         request = self._client.get(url, query_parameters, header_parameters)
         pipeline_response = await self._client._pipeline.run(request, stream=False, **kwargs)
         response = pipeline_response.http_response
@@ -305,7 +302,6 @@ class QueueOperations:
         # Construct headers
         header_parameters = {}  # type: Dict[str, Any]
 
-        # Construct and send request
         request = self._client.delete(url, query_parameters, header_parameters)
         pipeline_response = await self._client._pipeline.run(request, stream=False, **kwargs)
         response = pipeline_response.http_response
@@ -353,6 +349,10 @@ class QueueOperations:
         api_version = "2019-06-01"
 
         def prepare_request(next_link=None):
+            # Construct headers
+            header_parameters = {}  # type: Dict[str, Any]
+            header_parameters['Accept'] = 'application/json'
+
             if not next_link:
                 # Construct URL
                 url = self.list.metadata['url']  # type: ignore
@@ -370,15 +370,11 @@ class QueueOperations:
                 if filter is not None:
                     query_parameters['$filter'] = self._serialize.query("filter", filter, 'str')
 
+                request = self._client.get(url, query_parameters, header_parameters)
             else:
                 url = next_link
                 query_parameters = {}  # type: Dict[str, Any]
-            # Construct headers
-            header_parameters = {}  # type: Dict[str, Any]
-            header_parameters['Accept'] = 'application/json'
-
-            # Construct and send request
-            request = self._client.get(url, query_parameters, header_parameters)
+                request = self._client.get(url, query_parameters, header_parameters)
             return request
 
         async def extract_data(pipeline_response):
