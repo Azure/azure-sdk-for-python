ad8a41cdb380b06a7111841d83c69d185fa930ea
SDK Automation <sdkautomation@microsoft.com>
2020-06-25 08:36:14 +0000

    Generated from 721f9b85bac7b4ecb27ee1e9d92d26433e7bc906
    Fix CDN Endpoints_Update example.

--- sdk/cdn/azure-mgmt-cdn/azure/mgmt/cdn/operations/_policies_operations.py
+++ sdk/cdn/azure-mgmt-cdn/azure/mgmt/cdn/operations/_policies_operations.py
@@ -27,7 +27,7 @@ class PoliciesOperations(object):
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
-    :ivar api_version: Version of the API to be used with the client request. Current version is 2017-04-02. Constant value: "2019-06-15-preview".
+    :ivar api_version: Version of the API to be used with the client request. Current version is 2017-04-02. Constant value: "2019-06-15".
     """
 
     models = models
@@ -37,7 +37,7 @@ class PoliciesOperations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
-        self.api_version = "2019-06-15-preview"
+        self.api_version = "2019-06-15"
 
         self.config = config
 
