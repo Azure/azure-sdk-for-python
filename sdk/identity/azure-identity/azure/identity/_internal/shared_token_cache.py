54ab8e609368fafb70bf3ff8b5ebb3a8aec8f98f
Charles Lowell <chlowe@microsoft.com>
2020-06-29 13:41:44 -0700

    don't double-check initialization

--- sdk/identity/azure-identity/azure/identity/_internal/shared_token_cache.py
+++ sdk/identity/azure-identity/azure/identity/_internal/shared_token_cache.py
@@ -114,9 +114,6 @@ class SharedTokenCacheBase(ABC):
         self._initialized = False
 
     def _initialize(self):
-        if self._initialized:
-            return
-
         if not self._cache and self.supported():
             allow_unencrypted = self._client_kwargs.pop("allow_unencrypted_cache", False)
             try:
