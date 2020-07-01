3c8696a188c8c2ae66b1af0278ca08343d6946b5
Adam Ling <adam_ling@outlook.com>
2020-07-01 13:39:13 -0700

    remove credential

--- sdk/servicebus/azure-servicebus/samples/async_samples/client_identity_authentication_async.py
+++ sdk/servicebus/azure-servicebus/samples/async_samples/client_identity_authentication_async.py
@@ -39,7 +39,7 @@ from azure.servicebus.aio import ServiceBusClient
 from azure.identity.aio import EnvironmentCredential
 
 
-FULLY_QUALIFIED_NAMESPACE = 't1sbtest.servicebus.windows.net' #os.environ['SERVICE_BUS_NAMESPACE']
+FULLY_QUALIFIED_NAMESPACE = os.environ['SERVICE_BUS_NAMESPACE']
 QUEUE_NAME = os.environ["SERVICE_BUS_QUEUE_NAME"]
 
 
