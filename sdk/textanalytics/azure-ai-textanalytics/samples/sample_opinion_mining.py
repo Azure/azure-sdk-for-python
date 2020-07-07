2641e2dfd52eff05b756bf5b1a8533f757e2ebd7
iscai-msft <iscai@microsoft.com>
2020-07-07 16:41:23 -0400

    add better example

--- sdk/textanalytics/azure-ai-textanalytics/samples/sample_opinion_mining.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/sample_opinion_mining.py
@@ -1,5 +1,5 @@
 
-documents = ["The food is good and not yummy"]
+documents = ["It has a sleek premium aluminum design that makes it beautiful to look at."]
 
 result = text_analytics_client.analyze_sentiment(documents)
 doc_result = [doc for doc in result if not doc.is_error][0]
@@ -7,19 +7,19 @@ doc_result = [doc for doc in result if not doc.is_error][0]
 
 for sentence in doc_result:
     for aspect in sentence.aspects:
-        print(aspect.text)  # 'food'
-        print(aspect.sentiment)  # 'mixed'
-        print(aspect.confidence_scores.positive)  # 0.52
-        print(aspect.confidence_scores.negative)  # 0.48
-        print(aspect.offset)  # 4
-        print(aspect.length)  # 4
+        print(aspect.text)  # 'design'
+        print(aspect.sentiment)  # 'positive'
+        print(aspect.confidence_scores.positive)  # 1.0
+        print(aspect.confidence_scores.negative)  # 0.0
+        print(aspect.offset)  # 32
+        print(aspect.length)  # 6
 
         for opinion in aspect.opinions:
             # there should be two opinions. Will include both values in comments
-            print(opinion.text)  # 'good' & 'yummy'
-            print(opinion.sentiment)  # 'positive' & 'negative'
-            print(opinion.confidence_scores.positive)  # 1.0 & 0.03
-            print(opinion.confidence_scores.negative)  # 0.0 & 0.97
-            print(opinion.offset)  # 12 & 25
-            print(opinion.length)  # 4 & 5
-            print(opinion.is_negated)  # False & True
+            print(opinion.text)  # 'sleek' & 'premium'
+            print(opinion.sentiment)  # 'positive' & 'positive'
+            print(opinion.confidence_scores.positive)  # 1.0 & 1.0
+            print(opinion.confidence_scores.negative)  # 0.0 & 0.0
+            print(opinion.offset)  # 9 & 15
+            print(opinion.length)  # 5 & 7
+            print(opinion.is_negated)  # False & False
