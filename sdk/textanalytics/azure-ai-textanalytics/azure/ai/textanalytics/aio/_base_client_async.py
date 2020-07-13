0205200b4699999c27b60ea5fa7225c5b8daa381
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2020-07-13 16:24:41 -0400

    [text analytics] generate v3.1-preview.1 code (#12495)

--- sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/aio/_base_client_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/aio/_base_client_async.py
@@ -7,8 +7,8 @@ from typing import Any
 from azure.core.credentials import AzureKeyCredential
 from azure.core.pipeline.policies import AzureKeyCredentialPolicy
 from ._policies_async import AsyncTextAnalyticsResponseHookPolicy
-from .._generated.aio import TextAnalyticsClient
 from .._user_agent import USER_AGENT
+from .._multiapi import load_generated_api
 
 
 def _authentication_policy(credential):
@@ -27,7 +27,9 @@ def _authentication_policy(credential):
 
 class AsyncTextAnalyticsClientBase(object):
     def __init__(self, endpoint, credential, **kwargs):
-        self._client = TextAnalyticsClient(
+        api_version = kwargs.pop("api_version", None)
+        _TextAnalyticsClient = load_generated_api(api_version, aio=True)
+        self._client = _TextAnalyticsClient(
             endpoint=endpoint,
             credential=credential,
             sdk_moniker=USER_AGENT,
