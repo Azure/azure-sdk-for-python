1cce42aba144ed05acab69ecbeb9b2d4d93049db
Adam Ling <adam_ling@outlook.com>
2020-07-15 14:38:01 -0700

    fix pylint

--- sdk/servicebus/azure-servicebus/azure/servicebus/_common/constants.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_common/constants.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # -------------------------------------------------------------------------
-
 from enum import Enum
 
 from uamqp import constants
