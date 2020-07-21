4a63b46ecc5468d9f2b59f688d792495316d7364
Charles Lowell <chlowe@microsoft.com>
2020-07-21 16:45:17 -0700

    document and test VSCodeCredential's authority, tenant_id

--- sdk/identity/azure-identity/azure/identity/aio/_credentials/vscode_credential.py
+++ sdk/identity/azure-identity/azure/identity/aio/_credentials/vscode_credential.py
@@ -18,11 +18,21 @@ if TYPE_CHECKING:
 
 
 class VSCodeCredential(AsyncCredentialBase):
-    """Authenticates by redeeming a refresh token previously saved by VS Code"""
+    """Authenticates as the Azure user signed in to Visual Studio Code.
+
+    :keyword str authority: Authority of an Azure Active Directory endpoint, for example 'login.microsoftonline.com',
+          the authority for Azure Public Cloud (which is the default). :class:`~azure.identity.KnownAuthorities`
+          defines authorities for other clouds.
+    :keyword str tenant_id: ID of the tenant the credential should authenticate in. Defaults to the "organizations"
+        tenant, which supports only Azure Active Directory work or school accounts.
+    """
 
     def __init__(self, **kwargs: "Any") -> None:
-        self._client = kwargs.pop("_client", None) or AadClient("organizations", AZURE_VSCODE_CLIENT_ID, **kwargs)
         self._refresh_token = None
+        self._client = kwargs.pop("_client", None)
+        if not self._client:
+            tenant_id = kwargs.pop("tenant_id", None) or "organizations"
+            self._client = AadClient(tenant_id, AZURE_VSCODE_CLIENT_ID, **kwargs)
 
     async def __aenter__(self):
         if self._client:
