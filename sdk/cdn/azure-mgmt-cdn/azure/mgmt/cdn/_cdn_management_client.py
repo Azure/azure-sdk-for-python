6667a7736f55e81b22e21a810f619b5e80103365
SDK Automation <sdkautomation@microsoft.com>
2020-07-21 03:06:30 +0000

    Generated from ee57324bf35a286b432daecf1c5a56289f4e94ef
    Fix duplicate parameters for CDN API versions with WAF.

--- sdk/cdn/azure-mgmt-cdn/azure/mgmt/cdn/_cdn_management_client.py
+++ sdk/cdn/azure-mgmt-cdn/azure/mgmt/cdn/_cdn_management_client.py
@@ -17,6 +17,7 @@ from .operations import CdnManagementClientOperationsMixin
 from .operations import ProfilesOperations
 from .operations import EndpointsOperations
 from .operations import OriginsOperations
+from .operations import OriginGroupsOperations
 from .operations import CustomDomainsOperations
 from .operations import ResourceUsageOperations
 from .operations import Operations
@@ -38,6 +39,8 @@ class CdnManagementClient(CdnManagementClientOperationsMixin, SDKClient):
     :vartype endpoints: azure.mgmt.cdn.operations.EndpointsOperations
     :ivar origins: Origins operations
     :vartype origins: azure.mgmt.cdn.operations.OriginsOperations
+    :ivar origin_groups: OriginGroups operations
+    :vartype origin_groups: azure.mgmt.cdn.operations.OriginGroupsOperations
     :ivar custom_domains: CustomDomains operations
     :vartype custom_domains: azure.mgmt.cdn.operations.CustomDomainsOperations
     :ivar resource_usage: ResourceUsage operations
@@ -66,7 +69,7 @@ class CdnManagementClient(CdnManagementClientOperationsMixin, SDKClient):
         super(CdnManagementClient, self).__init__(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self.api_version = '2019-06-15-preview'
+        self.api_version = '2020-04-15'
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
@@ -75,6 +78,8 @@ class CdnManagementClient(CdnManagementClientOperationsMixin, SDKClient):
         self.endpoints = EndpointsOperations(
             self._client, self.config, self._serialize, self._deserialize)
         self.origins = OriginsOperations(
+            self._client, self.config, self._serialize, self._deserialize)
+        self.origin_groups = OriginGroupsOperations(
             self._client, self.config, self._serialize, self._deserialize)
         self.custom_domains = CustomDomainsOperations(
             self._client, self.config, self._serialize, self._deserialize)
