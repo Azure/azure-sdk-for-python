a55d1124c779e9837b6d7c59897fb6937e2803f7
LibbaLawrence <46537478+LibbaLawrence@users.noreply.github.com>
2020-07-21 09:43:35 -0400

    oops missed the other hostname

--- sdk/table/azure-table/tests/test_table_client.py
+++ sdk/table/azure-table/tests/test_table_client.py
@@ -406,7 +406,7 @@ class StorageTableClientTest(TableTestCase):
         self.assertEqual(service.account_name, None)
         self.assertEqual(service.table_name, "foo")
         self.assertEqual(service.credential, None)
-        self.assertEqual(service.primary_hostname, 'local-machine:11002/custom/account/path')
+        self.assertEqual(service._primary_hostname, 'local-machine:11002/custom/account/path')
         self.assertTrue(service.url.startswith('http://local-machine:11002/custom/account/path'))
 
         service = TableClient.from_table_url("http://local-machine:11002/custom/account/path/foo" + self.sas_token)
