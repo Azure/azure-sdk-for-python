97d815c8928cb5246b3e5b8bb4ce84ec4ab65de5
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2020-07-21 19:28:15 -0700

    Add isodate dependency in setup.py

--- sdk/servicebus/azure-servicebus/setup.py
+++ sdk/servicebus/azure-servicebus/setup.py
@@ -82,7 +82,8 @@ setup(
         'msrestazure>=0.4.32,<2.0.0',
         'azure-common~=1.1',
         'msrest>=0.6.17,<2.0.0',
-        'azure-core<2.0.0,>=1.6.0'
+        'azure-core<2.0.0,>=1.6.0',
+        "isodate>=0.6.0",
     ],
     extras_require={
         ":python_version<'3.0'": ['azure-nspkg', 'futures'],
