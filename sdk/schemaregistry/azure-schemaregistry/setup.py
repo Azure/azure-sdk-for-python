97ec05b6ae94e3c16e93c88ab8145aad829c5d8f
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2020-07-24 10:55:37 -0700

    Sync msrest and azure-core version with shared requirement

--- sdk/schemaregistry/azure-schemaregistry/setup.py
+++ sdk/schemaregistry/azure-schemaregistry/setup.py
@@ -79,7 +79,7 @@ setup(
     ]),
     install_requires=[
         'msrest>=0.6.17,<2.0.0',
-        'azure-core<2.0.0,>=1.6.0'
+        'msrest>=0.5.0'
     ],
     extras_require={
         ":python_version<'3.0'": ['azure-nspkg', 'futures'],
