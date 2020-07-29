2470e92ff8492e4967247c3ac226d3969c460a09
iscai-msft <iscai@microsoft.com>
2020-07-29 13:44:47 -0400

    test you can pass custom hook policy to client

--- sdk/keyvault/azure-keyvault-secrets/tests/test_secrets_async.py
+++ sdk/keyvault/azure-keyvault-secrets/tests/test_secrets_async.py
@@ -335,3 +335,12 @@ class KeyVaultSecretTest(KeyVaultTestCase):
         assert passed_in_allowed_headers.issubset(
             client._client._config.http_logging_policy.allowed_header_names
         )
+
+    class _CustomHookPolicy(object):
+        pass
+
+    @ResourceGroupPreparer(random_name_enabled=True)
+    @KeyVaultPreparer()
+    @KeyVaultClientPreparer(client_kwargs={"custom_hook_policy": _CustomHookPolicy()})
+    async def test_custom_hook_policy(self, client, **kwargs):
+        assert isinstance(client._client._config.custom_hook_policy, KeyVaultSecretTest._CustomHookPolicy)
