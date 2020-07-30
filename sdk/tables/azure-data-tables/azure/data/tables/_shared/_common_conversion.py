d10d7af5b1c7e4a5ba7f03f93495d145d98ce6d4
LibbaLawrence <46537478+LibbaLawrence@users.noreply.github.com>
2020-07-30 12:41:50 -0400

    adding back md5 with nosec

--- sdk/tables/azure-data-tables/azure/data/tables/_shared/_common_conversion.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_shared/_common_conversion.py
@@ -6,8 +6,15 @@
 import base64
 import hashlib
 import hmac
+from io import (SEEK_SET)
 import six
 from azure.data.tables._shared.parser import _str
+
+
+from ._error import (
+    _ERROR_VALUE_SHOULD_BE_BYTES_OR_STREAM,
+    _ERROR_VALUE_SHOULD_BE_SEEKABLE_STREAM,
+)
 
 
 def _to_str(value):
@@ -75,6 +82,25 @@ def _sign_string(key, string_to_sign, key_is_base64=True):
     digest = signed_hmac_sha256.digest()
     encoded_digest = _encode_base64(digest)
     return encoded_digest
+
+
+def _get_content_md5(data):
+    md5 = hashlib.md5()  # nosec
+    if isinstance(data, bytes):
+        md5.update(data)
+    elif hasattr(data, 'read'):
+        pos = 0
+        pos = data.tell()
+        for chunk in iter(lambda: data.read(4096), b""):
+            md5.update(chunk)
+        try:
+            data.seek(pos, SEEK_SET)
+        except (AttributeError, IOError):
+            raise ValueError(_ERROR_VALUE_SHOULD_BE_SEEKABLE_STREAM.format('data'))
+    else:
+        raise ValueError(_ERROR_VALUE_SHOULD_BE_BYTES_OR_STREAM.format('data'))
+
+    return base64.b64encode(md5.digest()).decode('utf-8')
 
 
 def _lower(text):
