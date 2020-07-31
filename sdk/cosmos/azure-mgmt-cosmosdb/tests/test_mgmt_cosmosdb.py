376c1eedc140c9b02746ee0aad203c77966e070e
Qiaoqiao Zhang <qiaozha@microsoft.com>
2020-07-31 04:49:16 +0000

    breaking changes for test

--- sdk/cosmos/azure-mgmt-cosmosdb/tests/test_mgmt_cosmosdb.py
+++ sdk/cosmos/azure-mgmt-cosmosdb/tests/test_mgmt_cosmosdb.py
@@ -31,9 +31,13 @@ class MgmtCosmosDBTest(AzureMgmtTestCase):
             account_name,
             {
                 'location': location,
-                'locations': [{
-                    'location_name': self.region
-                }]
+                'properties': {
+                    'locations': [{
+                        'location_name': self.region
+                    }],
+                    "createMode": "Default"
+                }
+                
             }
         )
         account = async_cosmosdb_create.result()
