0e6021375313601a094ea433a1c831cd7ce6d9e9
Charles Lowell <chlowe@microsoft.com>
2020-08-03 12:13:28 -0700

    Update Identity documentation (#12870)

--- sdk/identity/azure-identity/azure/identity/aio/_credentials/vscode.py
+++ sdk/identity/azure-identity/azure/identity/aio/_credentials/vscode.py
@@ -48,16 +48,14 @@ class VisualStudioCodeCredential(AsyncCredentialBase):
     @log_get_token_async
     async def get_token(self, *scopes, **kwargs):
         # type: (*str, **Any) -> AccessToken
-        """Request an access token for `scopes`.
+        """Request an access token for `scopes` as the user currently signed in to Visual Studio Code.
 
         .. note:: This method is called by Azure SDK clients. It isn't intended for use in application code.
 
-        When this method is called, the credential will try to get the refresh token saved by VS Code. If a refresh
-        token can be found, it will redeem the refresh token for an access token and return the access token.
-
         :param str scopes: desired scopes for the access token. This method requires at least one scope.
         :rtype: :class:`azure.core.credentials.AccessToken`
-        :raises ~azure.identity.CredentialUnavailableError: fail to get refresh token.
+        :raises ~azure.identity.CredentialUnavailableError: the credential cannot retrieve user details from Visual
+          Studio Code
         """
         if not scopes:
             raise ValueError("'get_token' requires at least one scope")
@@ -76,7 +74,7 @@ class VisualStudioCodeCredential(AsyncCredentialBase):
         if not self._refresh_token:
             self._refresh_token = get_credentials()
             if not self._refresh_token:
-                raise CredentialUnavailableError(message="No Azure user is logged in to Visual Studio Code.")
+                raise CredentialUnavailableError(message="Failed to get Azure user details from Visual Studio Code.")
 
         token = await self._client.obtain_token_by_refresh_token(scopes, self._refresh_token, **kwargs)
         return token
