a0bb333df6801fd7b2f17a8e4e13335df729a3d2
Swathi Pillalamarri <t-swpill@microsoft.com>
2020-08-03 16:30:13 -0400

    removed unnecessary functions

--- sdk/eventgrid/azure-eventgrid/samples/consume_samples/functionsapp/QueueTrigger1/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-import azure.functions as func
-
-
-def main(msg: func.QueueMessage) -> None:
-    logging.info('Python queue trigger function processed a queue item: %s',
-                 msg.get_body().decode('utf-8'))
--- sdk/eventgrid/azure-eventgrid/samples/consume_samples/functionsapp/ServiceBusQueueTrigger1/__init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-import azure.functions as func
-
-def main(msg: func.ServiceBusMessage):
-    logging.info('Python ServiceBus queue trigger processed message: %s',
-                 msg.get_body().decode('utf-8'))
