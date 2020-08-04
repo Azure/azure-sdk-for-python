fc2c76c13d305e0a0467b51935ea91cf310bcace
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2020-08-04 18:23:11 -0400

    [text analytics] switching SSN number in pii test (#12891)

--- sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_pii_entities.py
+++ sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_pii_entities.py
@@ -88,7 +88,7 @@ class TestRecognizePIIEntities(TextAnalyticsTest):
     @GlobalTextAnalyticsAccountPreparer()
     @TextAnalyticsClientPreparer()
     def test_length_with_emoji(self, client):
-        result = client.recognize_pii_entities(["👩 SSN: 123-12-1234"])
+        result = client.recognize_pii_entities(["👩 SSN: 859-98-0987"])
         self.assertEqual(result[0].entities[0].offset, 7)
         self.assertEqual(result[0].entities[0].length, 11)
 
