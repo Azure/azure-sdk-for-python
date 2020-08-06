19d0cb625024710e5239943a0df807ddbd6a8ed8
Laurent Mazuel <laurent.mazuel@gmail.com>
2020-08-06 16:00:29 -0700

    Fix setup.py

--- sdk/anomalydetector/azure-ai-anomalydetector/setup.py
+++ sdk/anomalydetector/azure-ai-anomalydetector/setup.py
@@ -12,7 +12,7 @@ from io import open
 from setuptools import find_packages, setup
 
 # Change the PACKAGE_NAME only to change folder and different name
-PACKAGE_NAME = "azure-cognitiveservices-anomalydetector"
+PACKAGE_NAME = "azure-ai-anomalydetector"
 PACKAGE_PPRINT_NAME = "Cognitive Services Anomaly Detector"
 
 # a-b-c => a/b/c
