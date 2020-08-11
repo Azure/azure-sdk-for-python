8b481be720f5d27aabcbb5682404ef831fd208c1
Charles Lowell <chlowe@microsoft.com>
2020-08-11 15:21:56 -0700

    class for mixing in proactive token refreshing

---
+++ sdk/identity/azure-identity/azure/identity/_internal/proactive_refresh.py
@@ -0,0 +1,73 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import abc
+import logging
+import time
+from typing import TYPE_CHECKING
+
+from .._constants import DEFAULT_REFRESH_OFFSET, DEFAULT_TOKEN_REFRESH_RETRY_DELAY
+
+try:
+    ABC = abc.ABC
+except AttributeError:  # Python 2.7, abc exists, but not ABC
+    ABC = abc.ABCMeta("ABC", (object,), {"__slots__": ()})  # type: ignore
+
+if TYPE_CHECKING:
+    # pylint:disable=ungrouped-imports,unused-import
+    from typing import Any, Optional
+    from azure.core.credentials import AccessToken
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ProactiveRefresh(ABC):
+    def __init__(self, *args, **kwargs):
+        # type: (*Any, **Any) -> None
+        self._last_request_time = 0
+        super(ProactiveRefresh, self).__init__(*args, **kwargs)
+
+    @abc.abstractmethod
+    def _acquire_token_silently(self, *scopes):
+        # type: (*str) -> Optional[AccessToken]
+        """Attempt to acquire an access token from a cache or by redeeming a refresh token"""
+
+    @abc.abstractmethod
+    def _request_token(self, *scopes, **kwargs):
+        # type: (*str, **Any) -> AccessToken
+        """Request an access token from the STS"""
+
+    def _should_refresh(self, token):
+        # type: (AccessToken) -> bool
+        now = int(time.time())
+        if token.expires_on - now > DEFAULT_REFRESH_OFFSET:
+            return False
+        if now - self._last_request_time < DEFAULT_TOKEN_REFRESH_RETRY_DELAY:
+            return False
+        return True
+
+    def _get_token_impl(self, *scopes, **kwargs):
+        # type: (*str, **Any) -> AccessToken
+        if not scopes:
+            raise ValueError('"get_token" requires at least one scope')
+
+        try:
+            token = self._acquire_token_silently(*scopes)
+            if not token:
+                self._last_request_time = int(time.time())
+                token = self._request_token(*scopes)
+            elif self._should_refresh(token):
+                try:
+                    self._last_request_time = int(time.time())
+                    token = self._request_token(*scopes, **kwargs)
+                except Exception:  # pylint:disable=broad-except
+                    pass
+            _LOGGER.info("%s.get_token succeeded", self.__class__.__name__)
+            return token
+
+        except Exception as ex:
+            _LOGGER.warning(
+                "%s.get_token failed: %s", self.__class__.__name__, ex, exc_info=_LOGGER.isEnabledFor(logging.DEBUG)
+            )
+            raise
---
+++ sdk/identity/azure-identity/azure/identity/aio/_internal/proactive_refresh.py
@@ -0,0 +1,63 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import abc
+import logging
+import time
+from typing import TYPE_CHECKING
+
+from ..._constants import DEFAULT_REFRESH_OFFSET, DEFAULT_TOKEN_REFRESH_RETRY_DELAY
+
+if TYPE_CHECKING:
+    # pylint:disable=ungrouped-imports,unused-import
+    from typing import Any, Optional
+    from azure.core.credentials import AccessToken
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ProactiveRefresh(abc.ABC):
+    def __init__(self, *args: "Any", **kwargs: "Any") -> None:
+        self._last_request_time = 0
+        super(ProactiveRefresh, self).__init__(*args, **kwargs)
+
+    @abc.abstractmethod
+    async def _acquire_token_silently(self, *scopes: str) -> "Optional[AccessToken]":
+        """Attempt to acquire an access token from a cache or by redeeming a refresh token"""
+
+    @abc.abstractmethod
+    async def _request_token(self, *scopes: str, **kwargs: "Any") -> "AccessToken":
+        """Request an access token from the STS"""
+
+    def _should_refresh(self, token: "AccessToken") -> bool:
+        now = int(time.time())
+        if token.expires_on - now > DEFAULT_REFRESH_OFFSET:
+            return False
+        if now - self._last_request_time < DEFAULT_TOKEN_REFRESH_RETRY_DELAY:
+            return False
+        return True
+
+    async def _get_token_impl(self, *scopes: str, **kwargs: "Any") -> "AccessToken":
+        if not scopes:
+            raise ValueError('"get_token" requires at least one scope')
+
+        try:
+            token = await self._acquire_token_silently(*scopes)
+            if not token:
+                self._last_request_time = int(time.time())
+                token = await self._request_token(*scopes)
+            elif self._should_refresh(token):
+                try:
+                    self._last_request_time = int(time.time())
+                    token = await self._request_token(*scopes, **kwargs)
+                except Exception:  # pylint:disable=broad-except
+                    pass
+            _LOGGER.info("%s.get_token succeeded", self.__class__.__name__)
+            return token
+
+        except Exception as ex:
+            _LOGGER.warning(
+                "%s.get_token failed: %s", self.__class__.__name__, ex, exc_info=_LOGGER.isEnabledFor(logging.DEBUG)
+            )
+            raise
