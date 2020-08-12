ba81eac0426a14b5bd290b97d0739cc00cbf4425
xiafu <xiafu@microsoft.com>
2020-08-12 01:51:50 -0700

    skip versioning test

--- sdk/storage/azure-storage-blob/tests/test_container.py
+++ sdk/storage/azure-storage-blob/tests/test_container.py
@@ -1314,6 +1314,7 @@ class StorageContainerTest(StorageTestCase):
             raise_on_any_failure=False
         )
 
+    @pytest.mark.playback_test_only
     @pytest.mark.skipif(sys.version_info < (3, 0), reason="Batch not supported on Python 2.7")
     @GlobalStorageAccountPreparer()
     def test_batch_set_standard_blob_tier_for_version(self, resource_group, location, storage_account, storage_account_key):
