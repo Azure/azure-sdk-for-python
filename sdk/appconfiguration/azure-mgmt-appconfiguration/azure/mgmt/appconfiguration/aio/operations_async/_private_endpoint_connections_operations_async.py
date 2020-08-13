d762a36d058f971ac0e9a8197dd50bacf04ca255
SDK Automation <sdkautomation@microsoft.com>
2020-08-13 06:14:50 +0000

    Generated from 252aae51d01c13bd9f5f3215a5770e7a2c9e6213
    Update readme for onboard autorest.schema to sdkautomation

--- sdk/appconfiguration/azure-mgmt-appconfiguration/azure/mgmt/appconfiguration/aio/operations_async/_private_endpoint_connections_operations_async.py
+++ sdk/appconfiguration/azure-mgmt-appconfiguration/azure/mgmt/appconfiguration/aio/operations_async/_private_endpoint_connections_operations_async.py
@@ -64,9 +64,13 @@ class PrivateEndpointConnectionsOperations:
         cls = kwargs.pop('cls', None)  # type: ClsType["models.PrivateEndpointConnectionListResult"]
         error_map = {404: ResourceNotFoundError, 409: ResourceExistsError}
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2019-11-01-preview"
+        api_version = "2020-06-01"
 
         def prepare_request(next_link=None):
+            # Construct headers
+            header_parameters = {}  # type: Dict[str, Any]
+            header_parameters['Accept'] = 'application/json'
+
             if not next_link:
                 # Construct URL
                 url = self.list_by_configuration_store.metadata['url']  # type: ignore
@@ -80,15 +84,11 @@ class PrivateEndpointConnectionsOperations:
                 query_parameters = {}  # type: Dict[str, Any]
                 query_parameters['api-version'] = self._serialize.query("api_version", api_version, 'str')
 
+                request = self._client.get(url, query_parameters, header_parameters)
             else:
                 url = next_link
                 query_parameters = {}  # type: Dict[str, Any]
-            # Construct headers
-            header_parameters = {}  # type: Dict[str, Any]
-            header_parameters['Accept'] = 'application/json'
-
-            # Construct and send request
-            request = self._client.get(url, query_parameters, header_parameters)
+                request = self._client.get(url, query_parameters, header_parameters)
             return request
 
         async def extract_data(pipeline_response):
@@ -140,7 +140,7 @@ class PrivateEndpointConnectionsOperations:
         cls = kwargs.pop('cls', None)  # type: ClsType["models.PrivateEndpointConnection"]
         error_map = {404: ResourceNotFoundError, 409: ResourceExistsError}
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2019-11-01-preview"
+        api_version = "2020-06-01"
 
         # Construct URL
         url = self.get.metadata['url']  # type: ignore
@@ -160,7 +160,6 @@ class PrivateEndpointConnectionsOperations:
         header_parameters = {}  # type: Dict[str, Any]
         header_parameters['Accept'] = 'application/json'
 
-        # Construct and send request
         request = self._client.get(url, query_parameters, header_parameters)
         pipeline_response = await self._client._pipeline.run(request, stream=False, **kwargs)
         response = pipeline_response.http_response
@@ -189,7 +188,7 @@ class PrivateEndpointConnectionsOperations:
         cls = kwargs.pop('cls', None)  # type: ClsType["models.PrivateEndpointConnection"]
         error_map = {404: ResourceNotFoundError, 409: ResourceExistsError}
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2019-11-01-preview"
+        api_version = "2020-06-01"
         content_type = kwargs.pop("content_type", "application/json")
 
         # Construct URL
@@ -211,7 +210,6 @@ class PrivateEndpointConnectionsOperations:
         header_parameters['Content-Type'] = self._serialize.header("content_type", content_type, 'str')
         header_parameters['Accept'] = 'application/json'
 
-        # Construct and send request
         body_content_kwargs = {}  # type: Dict[str, Any]
         body_content = self._serialize.body(private_endpoint_connection, 'PrivateEndpointConnection')
         body_content_kwargs['content'] = body_content
@@ -225,7 +223,6 @@ class PrivateEndpointConnectionsOperations:
             error = self._deserialize(models.Error, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
-        deserialized = None
         if response.status_code == 200:
             deserialized = self._deserialize('PrivateEndpointConnection', pipeline_response)
 
@@ -245,7 +242,7 @@ class PrivateEndpointConnectionsOperations:
         private_endpoint_connection_name: str,
         private_endpoint_connection: "models.PrivateEndpointConnection",
         **kwargs
-    ) -> "models.PrivateEndpointConnection":
+    ) -> AsyncLROPoller["models.PrivateEndpointConnection"]:
         """Update the state of the specified private endpoint connection associated with the configuration
     store.
 
@@ -264,8 +261,8 @@ class PrivateEndpointConnectionsOperations:
          polling object for personal polling strategy
         :paramtype polling: bool or ~azure.core.polling.AsyncPollingMethod
         :keyword int polling_interval: Default waiting time between two polls for LRO operations if no Retry-After header is present.
-        :return: PrivateEndpointConnection, or the result of cls(response)
-        :rtype: ~app_configuration_management_client.models.PrivateEndpointConnection
+        :return: An instance of AsyncLROPoller that returns either PrivateEndpointConnection or the result of cls(response)
+        :rtype: ~azure.core.polling.AsyncLROPoller[~app_configuration_management_client.models.PrivateEndpointConnection]
         :raises ~azure.core.exceptions.HttpResponseError:
         """
         polling = kwargs.pop('polling', True)  # type: Union[bool, AsyncPollingMethod]
@@ -319,7 +316,7 @@ class PrivateEndpointConnectionsOperations:
         cls = kwargs.pop('cls', None)  # type: ClsType[None]
         error_map = {404: ResourceNotFoundError, 409: ResourceExistsError}
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2019-11-01-preview"
+        api_version = "2020-06-01"
 
         # Construct URL
         url = self._delete_initial.metadata['url']  # type: ignore
@@ -338,7 +335,6 @@ class PrivateEndpointConnectionsOperations:
         # Construct headers
         header_parameters = {}  # type: Dict[str, Any]
 
-        # Construct and send request
         request = self._client.delete(url, query_parameters, header_parameters)
         pipeline_response = await self._client._pipeline.run(request, stream=False, **kwargs)
         response = pipeline_response.http_response
@@ -359,7 +355,7 @@ class PrivateEndpointConnectionsOperations:
         config_store_name: str,
         private_endpoint_connection_name: str,
         **kwargs
-    ) -> None:
+    ) -> AsyncLROPoller[None]:
         """Deletes a private endpoint connection.
 
         :param resource_group_name: The name of the resource group to which the container registry
@@ -375,8 +371,8 @@ class PrivateEndpointConnectionsOperations:
          polling object for personal polling strategy
         :paramtype polling: bool or ~azure.core.polling.AsyncPollingMethod
         :keyword int polling_interval: Default waiting time between two polls for LRO operations if no Retry-After header is present.
-        :return: None, or the result of cls(response)
-        :rtype: None
+        :return: An instance of AsyncLROPoller that returns either None or the result of cls(response)
+        :rtype: ~azure.core.polling.AsyncLROPoller[None]
         :raises ~azure.core.exceptions.HttpResponseError:
         """
         polling = kwargs.pop('polling', True)  # type: Union[bool, AsyncPollingMethod]
