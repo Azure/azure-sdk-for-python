d5dcba1187d6c09bdc46b54bdc4f7c257e197227
samiam2642 <samiam2642ml@hotmail.com>
2020-08-16 18:50:24 -0700

    Release azure mgmt reservations (#13132)
    * added new quota tests and recordings
    
    * updated recordings to referene prod ARM endpoint instead of dogfood
    
    Co-authored-by: Samuel Mallon <samallon@microsoft.com>

--- sdk/reservations/azure-mgmt-reservations/tests/test_mgmt_reservations.py
+++ sdk/reservations/azure-mgmt-reservations/tests/test_mgmt_reservations.py
@@ -6,8 +6,11 @@ from azure.mgmt.reservations.models import (
     InstanceFlexibility,
     AppliedScopeType
 )
+from datetime import (
+    datetime,
+    timezone
+)
 import unittest
-
 
 # change the custom endpoint to set the environment
 _CUSTOM_ENDPOINT = "https://management.azure.com/"
@@ -16,9 +19,113 @@ class MgmtReservationsTest(AzureMgmtTestCase):
 
     def setUp(self):
         super(MgmtReservationsTest, self).setUp()
+        self._register_equality_checkers()
         self.reservation_client = self.create_basic_client(AzureReservationAPI, base_url=_CUSTOM_ENDPOINT)
         # self.reservation_client = self.create_basic_client(AzureReservationAPI)
 
+    def _register_equality_checkers(self):
+        self.addTypeEqualityFunc(AutoQuotaIncreaseDetail, self._test_aqi_details_equality)
+        self.addTypeEqualityFunc(Actions, self._test_actions_equality)
+        self.addTypeEqualityFunc(EmailActions, self._test_email_actions_equality)
+        self.addTypeEqualityFunc(EmailAction, self._test_email_action_equality)
+        self.addTypeEqualityFunc(SupportRequestAction, self._test_support_action_equality)
+        self.addTypeEqualityFunc(AqiSettings, self._test_aqi_settings_equality)
+
+    def _test_actions_equality(self, expected, actual, msg=None):
+        self.assertEqual(
+            expected.email_actions,
+            actual.email_actions,
+            msg
+        )
+
+    def _test_email_actions_equality(self, expected, actual, msg=None):
+        self.assertEqual(
+            expected.email_addresses,
+            actual.email_addresses,
+            msg
+        )
+
+    def _test_email_action_equality(self, expected, actual, msg=None):
+        self.assertEqual(
+            expected.email_address,
+            actual.email_address,
+            msg
+        )
+
+    def _test_support_action_equality(self, expected, actual, msg=None):
+        self.assertEqual(
+            expected.alternate_email_addresses,
+            actual.alternate_email_addresses,
+            msg
+        )
+        self.assertEqual(
+            expected.severity,
+            actual.severity,
+            msg
+        )
+        self.assertEqual(
+            expected.first_name,
+            actual.first_name,
+            msg
+        )
+        self.assertEqual(
+            expected.last_name,
+            actual.last_name,
+            msg
+        )
+        self.assertEqual(
+            expected.country,
+            actual.country,
+            msg
+        )
+        self.assertEqual(
+            expected.phone_number,
+            actual.phone_number,
+            msg
+        )
+        self.assertEqual(
+            expected.primary_email_address,
+            actual.primary_email_address,
+            msg
+        )
+        self.assertEqual(
+            expected.support_language,
+            actual.support_language,
+            msg
+        )
+        self.assertEqual(
+            expected.preferred_contact_method,
+            actual.preferred_contact_method,
+            msg
+        )
+
+    def _test_aqi_settings_equality(self, expected, actual, msg=None):
+        self.assertEqual(
+            expected.auto_quota_increase_state,
+            actual.auto_quota_increase_state,
+            msg)
+
+    def _test_aqi_details_equality(self, expected, actual, msg=None):
+        self.assertEqual(
+            expected.settings,
+            actual.settings,
+            msg)
+        self.assertEqual(
+            expected.on_failure,
+            actual.on_failure,
+            msg
+        )
+        self.assertEqual(
+            expected.on_success,
+            actual.on_success,
+            msg
+        )
+        self.assertEqual(
+            expected.support_ticket_action,
+            actual.support_ticket_action,
+            msg
+        )
+        
     def _validate_reservation_order(self, reservation_order):
         self.assertIsNotNone(reservation_order)
         self.assertIsNotNone(reservation_order.id)
@@ -133,6 +240,68 @@ class MgmtReservationsTest(AzureMgmtTestCase):
         for reservation in reservation_list:
             self._validate_reservation(reservation)
 
+    def _create_auto_quota_increase_details(self, enabled):
+        if (enabled):
+            return AutoQuotaIncreaseDetail(
+                settings = AqiSettings(auto_quota_increase_state = "Enabled"),
+                on_failure = Actions(
+                    email_actions = EmailActions(
+                        email_addresses = [
+                            EmailAction(
+                                email_address = "failure@contoso.com"
+                            ),
+                            EmailAction(
+                                email_address = "failure1@contoso.com"
+                            )
+                        ]
+                    )
+                ),
+                on_success = Actions(
+                    email_actions = EmailActions(
+                        email_addresses = [
+                            EmailAction(
+                                email_address = "success@contoso.com"
+                            ),
+                            EmailAction(
+                                email_address = "success1@contoso.com"
+                            )
+                        ]
+                    )
+                ),
+                support_ticket_action = SupportRequestAction(
+                    severity = "Minimal",
+                    first_name = "Jane",
+                    last_name = "Doe",
+                    country = "United States",
+                    phone_number = "+1 555-555-5555",
+                    primary_email_address = "primary@contoso.com",
+                    support_language = "en-US",
+                    preferred_contact_method = "Email",
+                    alternate_email_addresses = [
+                        "support1@contoso.com",
+                        "support2@contoso.com"
+                    ]
+                )
+            )
+        else:
+            return AutoQuotaIncreaseDetail(
+                settings = AqiSettings(auto_quota_increase_state = "Disabled")
+            )
+
+    def _test_auto_quota_increase_create(self, enabled):
+        aqi_details = self._create_auto_quota_increase_details(enabled)
+        create_response = self.reservation_client.auto_quota_increase.create(
+            self.settings.SUBSCRIPTION_ID,
+            aqi_details
+        )
+        self.assertEqual(aqi_details, create_response)
+        # Now validate that we can get back the same results as well
+        get_response = \
+            self.reservation_client.auto_quota_increase.get_properties(
+                self.settings.SUBSCRIPTION_ID
+            )
+        self.assertEqual(aqi_details, get_response)
+
     def test_reservation_operations(self):
         reservation_order_id = self._calculate_reservation_order()
         self._purchase_reservation_order(reservation_order_id)
@@ -194,6 +363,122 @@ class MgmtReservationsTest(AzureMgmtTestCase):
             self.assertIsNotNone(operation.name)
             self.assertIsNotNone(operation.display)
 
+    def test_auto_quota_increase_enabled(self):
+        self._test_auto_quota_increase_create(True)
+
+    def test_auto_quota_increase_disabled(self):
+        self._test_auto_quota_increase_create(False)
+
+    def _test_list_quota(
+        self,
+        resource_provider,
+        location,
+        sku,
+        resource_type):
+        quota = self.reservation_client.quota.list(
+            self.settings.SUBSCRIPTION_ID,
+            resource_provider,
+            location
+        )
+        for quota_page in quota:
+            if sku is None:
+                self.assertGreater(quota_page.properties.limit, 0)
+                return quota_page
+            if (sku is None or (quota_page.properties.name.value == sku
+                and quota_page.properties.resource_type == resource_type)
+                ):
+                self.assertGreater(quota_page.properties.limit, 0)
+                return quota_page
+        self.fail("Did not find specified sku: {sku}".format(sku = sku))
+
+    def _test_update_quota(
+        self,
+        resource_provider,
+        location,
+        sku,
+        resource_type):
+        current_quota = self._test_list_quota(
+            resource_provider,
+            location,
+            sku,
+            resource_type
+        )
+        current_quota.properties.limit += 1
+        new_quota = self.reservation_client.quota.update(
+            self.settings.SUBSCRIPTION_ID,
+            resource_provider,
+            location,
+            sku,
+            current_quota.properties
+        ).result()
+        self.assertEqual(current_quota.properties.limit, new_quota['properties']['limit'])
+
+    def test_list_quota_compute(self):
+        self._test_list_quota(
+            "Microsoft.Compute",
+            "WestUS",
+            "standardDv2Family",
+            None
+        )
+
+    def test_list_quota_batch_ai(self):
+        self._test_list_quota(
+            "Microsoft.MachineLearningServices",
+            "WestUS",
+            "standardDv2Family",
+            "dedicated"
+        )
+
+    def test_update_quota_compute(self):
+        val = self._test_update_quota(
+            "Microsoft.Compute",
+            "WestUS",
+            "standardDv2Family",
+            None
+        )
+
+    def test_update_quota_batch_ai(self):
+        val = self._test_update_quota(
+            "Microsoft.MachineLearningServices",
+            "westus",
+            "standardDv2Family",
+            "dedicated"
+        )
+
+    def _test_list_quota_request_status(self, provider_id, location):
+        last_year = datetime.now(timezone.utc)
+        last_year = last_year.replace(year = last_year.year - 1)
+        statuses = self.reservation_client.quota_request_status.list(
+             self.settings.SUBSCRIPTION_ID,
+             provider_id,
+             location
+        )
+        fail = True
+        for status in statuses:
+            fail = False
+            self.assertTrue(
+                status.name.startswith(self.settings.SUBSCRIPTION_ID)
+            )
+            self.assertIsNotNone(status.provisioning_state)
+            self.assertGreaterEqual(
+                status.request_submit_time,
+                last_year
+            )
+            self.assertEqual("Microsoft.Capacity/serviceLimitsRequests", status.type)
+        if fail:
+            self.fail("Did not retrieve any statuses.")
+
+    def test_list_quota_request_status_compute(self):
+        statuses = self._test_list_quota_request_status(
+            "Microsoft.Compute",
+            "WestUS"
+        )
+
+    def test_list_quota_request_status_batch_ai(self):
+        statuses = self._test_list_quota_request_status(
+            "Microsoft.MachineLearningServices",
+            "westus"
+        )
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
     unittest.main()
