9d6117fdd8f7b5c9a832ec2956d74e86bdf6002c
Rakshith Bhyravabhotla <sabhyrav@microsoft.com>
2020-08-19 14:16:29 -0700

    Apply suggestions from code review

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/_publisher_client.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/_publisher_client.py
@@ -44,6 +44,7 @@ class EventGridPublisherClient(object):
         :param events: A list of CloudEvent/EventGridEvent/CustomEvent to be sent.
         :type events: Union[List[models.CloudEvent], List[models.EventGridEvent], List[models.CustomEvent]]
         :rtype: None
+        raise: :class:`ValueError`, when events do not follow specified SendType.
          """
 
         if all(isinstance(e, CloudEvent) for e in events):
