783364f0e8b95d9f57fb8da704ae9712ff8683b5
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2020-08-20 00:02:32 -0700

    analyze

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/_publisher_client_async.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/_publisher_client_async.py
@@ -6,7 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import Any
+from typing import Any, TYPE_CHECKING
 
 from azure.core import AsyncPipelineClient
 from msrest import Deserializer, Serializer
