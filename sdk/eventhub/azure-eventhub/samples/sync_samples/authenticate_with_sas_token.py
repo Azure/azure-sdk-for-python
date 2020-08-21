a1f68b01413daf043e6289dab152e422f35b8bf1
Kieran Brantner-Magee <kibrantn@microsoft.com>
2020-08-21 16:38:40 -0700

    Adjust to use generate_sas_token without referencing the internal modules so that we're not opening that can of worms.

--- sdk/eventhub/azure-eventhub/samples/sync_samples/authenticate_with_sas_token.py
+++ sdk/eventhub/azure-eventhub/samples/sync_samples/authenticate_with_sas_token.py
@@ -11,22 +11,31 @@ Example to demonstrate utilizing SAS (Shared Access Signature) tokens to authent
 
 # pylint: disable=C0111
 
-import time
-import datetime
 import os
-
+import time
+import hmac
+import hashlib
+import base64
+try:
+    from urllib.parse import quote as url_parse_quote
+except ImportError:
+    from urllib import pathname2url as url_parse_quote
+    
 from azure.core.credentials import AccessToken
 from azure.eventhub import EventHubConsumerClient
 
-# Caller must provide SAS credentials
-SAS_TOKEN = os.environ['EVENT_HUB_SAS_TOKEN']
 
-# Target namespace and hub must also be specified.  Consumer group is set to default unless required otherwise.
-FULLY_QUALIFIED_NAMESPACE = os.environ['EVENT_HUB_HOSTNAME']
-EVENTHUB_NAME = os.environ['EVENT_HUB_NAME']
-CONSUMER_GROUP = "$Default"
+def generate_sas_token(uri, sas_name, sas_value, token_ttl):
+    """Performs the signing and encoding needed to generate a sas token from a sas key."""
+    sas = sas_value.encode('utf-8')
+    expiry = str(int(time.time() + token_ttl))
+    string_to_sign = (uri + '\n' + expiry).encode('utf-8')
+    signed_hmac_sha256 = hmac.HMAC(sas, string_to_sign, hashlib.sha256)
+    signature = url_parse_quote.quote(base64.b64encode(signed_hmac_sha256.digest()))
+    return 'SharedAccessSignature sr={}&sig={}&se={}&skn={}'.format(uri, signature, expiry, sas_name)
 
-class CustomizedSASToken(object):
+
+class CustomizedSASCredential(object):
     def __init__(self, token, expiry):
         """
         :param str token: The token string
@@ -40,18 +49,29 @@ class CustomizedSASToken(object):
     def get_token(self, *scopes, **kwargs):
         """
         This method is automatically called when token is about to expire.
-        If you need to update a token, call the above `update_token`
         """
         return AccessToken(self.token, self.expiry)
 
 
-token_ttl = datetime.timedelta(seconds=300)
+# Target namespace and hub must also be specified.  Consumer group is set to default unless required otherwise.
+FULLY_QUALIFIED_NAMESPACE = os.environ['EVENT_HUB_HOSTNAME']
+EVENTHUB_NAME = os.environ['EVENT_HUB_NAME']
+CONSUMER_GROUP = "$Default"
+
+# The following part creates a SAS token. Users can use any way to create a SAS token.
+SAS_POLICY = os.environ['EVENT_HUB_SAS_POLICY']
+SAS_KEY = os.environ['EVENT_HUB_SAS_KEY']
+
+uri = "sb://{}/{}".format(FULLY_QUALIFIED_NAMESPACE, EVENTHUB_NAME)
+token_ttl = 3000  # seconds
+sas_token = generate_sas_token(uri, SAS_POLICY, SAS_KEY, token_ttl)
+# end of creating a SAS token
 
 consumer_client = EventHubConsumerClient(
     fully_qualified_namespace=FULLY_QUALIFIED_NAMESPACE,
     eventhub_name=EVENTHUB_NAME,
     consumer_group=CONSUMER_GROUP,
-    credential=CustomizedSASToken(SAS_TOKEN, time.time() + token_ttl.seconds),
+    credential=CustomizedSASCredential(sas_token, time.time() + token_ttl),
     logging_enable=True
 )
 
