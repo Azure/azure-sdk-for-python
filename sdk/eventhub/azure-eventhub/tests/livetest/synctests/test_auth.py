58e814a4301ce2fc9b76196de5262e2478e49a3b
Kieran Brantner-Magee <kibrantn@microsoft.com>
2020-08-26 11:28:33 -0700

    Add SAS token support to EventHub for connection string and credentialed authentication. (initial commit, tests not yet working)

--- sdk/eventhub/azure-eventhub/tests/livetest/synctests/test_auth.py
+++ sdk/eventhub/azure-eventhub/tests/livetest/synctests/test_auth.py
@@ -8,8 +8,13 @@ import time
 import threading
 
 from azure.identity import EnvironmentCredential
-from azure.eventhub import EventData, EventHubProducerClient, EventHubConsumerClient
+from azure.eventhub import EventData, EventHubProducerClient, EventHubConsumerClient, EventHubSharedKeyCredential, EventHubSASTokenCredential
 
+from devtools_testutils import AzureMgmtTestCase, CachedResourceGroupPreparer
+from tests.eventhub_preparer import (
+    CachedEventHubNamespacePreparer, 
+    CachedEventHubPreparer
+)
 
 @pytest.mark.liveTest
 def test_client_secret_credential(live_eventhub):
@@ -46,3 +51,80 @@ def test_client_secret_credential(live_eventhub):
     assert on_event.called is True
     assert on_event.partition_id == "0"
     assert list(on_event.event.body)[0] == 'A single message'.encode('utf-8')
+
+
+from azure.eventhub._client_base import _generate_sas_token as generate_sas_token
+import datetime
+
+class EventHubClientTests(AzureMgmtTestCase):
+
+    @pytest.mark.liveTest
+    @pytest.mark.live_test_only
+    @CachedResourceGroupPreparer(name_prefix='eventhubtest')
+    @CachedEventHubNamespacePreparer(name_prefix='eventhubtest')
+    @CachedEventHubPreparer(name_prefix='eventhubtest')
+    def test_client_sas_credential(self,
+                                   eventhub,
+                                   eventhub_namespace,
+                                   eventhub_namespace_key_name,
+                                   eventhub_namespace_primary_key,
+                                   eventhub_namespace_connection_string,
+                                   **kwargs):
+        # This should "just work" to validate known-good.
+        hostname = "{}.servicebus.windows.net".format(eventhub_namespace.name)
+        producer_client = EventHubProducerClient.from_connection_string(eventhub_namespace_connection_string, eventhub_name = eventhub.name)
+
+        with producer_client:
+            batch = producer_client.create_batch(partition_id='0')
+            batch.add(EventData(body='A single message'))
+            producer_client.send_batch(batch)
+
+        # This should also work, but now using SAS tokens.
+
+        #credential = EventHubSharedKeyCredential(eventhub_namespace_key_name, eventhub_namespace_primary_key)
+        hostname = "{}.servicebus.windows.net".format(eventhub_namespace.name)
+        auth_uri = "sb://{}/{}".format(hostname, eventhub.name)
+        #token = credential.get_token(auth_uri).token
+        token = generate_sas_token(auth_uri, eventhub_namespace_key_name, eventhub_namespace_primary_key, datetime.timedelta(seconds=300)).token
+        producer_client = EventHubProducerClient(fully_qualified_namespace=hostname,
+                                                 eventhub_name=eventhub_namespace.name,
+                                                 credential=EventHubSASTokenCredential(token, time.time() + 3000))
+
+        with producer_client:
+            batch = producer_client.create_batch(partition_id='0')
+            batch.add(EventData(body='A single message'))
+            producer_client.send_batch(batch)
+
+        #token_conn_str = "Endpoint=sb://{}/;SharedAccessToken={};".format(hostname, )
+        #conn_str_producer_client = EventHubProducerClient.from_connection_string(token_conn_str,
+        #                                                                         eventhub_name=live_eventhub['event_hub')
+        #
+        #with conn_str_producer_client:
+        #    batch = conn_str_producer_client.create_batch(partition_id='0')
+        #    batch.add(EventData(body='A single message'))
+        #    conn_str_producer_client.send_batch(batch)
+
+
+
+
+
+    #credential = EventHubSharedKeyCredential(live_eventhub['key_name'], live_eventhub['access_key'])
+    #auth_uri = "sb://{}{}".format(live_eventhub['hostname'], live_eventhub['event_hub'])
+    #token = credential.get_token(auth_uri)
+    #producer_client = EventHubProducerClient(fully_qualified_namespace=live_eventhub['hostname'],
+    #                                         eventhub_name=live_eventhub['event_hub'],
+    #                                         credential=EventHubSASTokenCredential(token, 3000))
+    #
+    #with producer_client:
+    #    batch = producer_client.create_batch(partition_id='0')
+    #    batch.add(EventData(body='A single message'))
+    #    producer_client.send_batch(batch)
+    #
+    ##token_conn_str = "Endpoint=sb://{}/;SharedAccessToken={};".format(hostname, )
+    ##conn_str_producer_client = EventHubProducerClient.from_connection_string(token_conn_str,
+    ##                                                                         eventhub_name=live_eventhub['event_hub')
+    ##
+    ##with conn_str_producer_client:
+    ##    batch = conn_str_producer_client.create_batch(partition_id='0')
+    ##    batch.add(EventData(body='A single message'))
+    ##    conn_str_producer_client.send_batch(batch)
