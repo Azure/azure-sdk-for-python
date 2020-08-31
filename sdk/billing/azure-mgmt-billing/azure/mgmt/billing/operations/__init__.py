1e8dee39136a568b2453d16e861d6e7280e68397
SDK Automation <sdkautomation@microsoft.com>
2020-08-31 07:14:48 +0000

    Generated from 496a7b7d65b61555ad20808a4ec42d5fa825fb87
    Fixed prettier-check error
    
    Fixed prettier-check error

--- sdk/billing/azure-mgmt-billing/azure/mgmt/billing/operations/__init__.py
+++ sdk/billing/azure-mgmt-billing/azure/mgmt/billing/operations/__init__.py
@@ -9,14 +9,46 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from .enrollment_accounts_operations import EnrollmentAccountsOperations
-from .billing_periods_operations import BillingPeriodsOperations
-from .invoices_operations import InvoicesOperations
-from .operations import Operations
+from ._billing_accounts_operations import BillingAccountsOperations
+from ._address_operations import AddressOperations
+from ._available_balances_operations import AvailableBalancesOperations
+from ._instructions_operations import InstructionsOperations
+from ._billing_profiles_operations import BillingProfilesOperations
+from ._customers_operations import CustomersOperations
+from ._invoice_sections_operations import InvoiceSectionsOperations
+from ._billing_permissions_operations import BillingPermissionsOperations
+from ._billing_subscriptions_operations import BillingSubscriptionsOperations
+from ._products_operations import ProductsOperations
+from ._invoices_operations import InvoicesOperations
+from ._transactions_operations import TransactionsOperations
+from ._policies_operations import PoliciesOperations
+from ._billing_property_operations import BillingPropertyOperations
+from ._operations import Operations
+from ._billing_role_definitions_operations import BillingRoleDefinitionsOperations
+from ._billing_role_assignments_operations import BillingRoleAssignmentsOperations
+from ._agreements_operations import AgreementsOperations
+from ._enrollment_accounts_operations import EnrollmentAccountsOperations
+from ._billing_periods_operations import BillingPeriodsOperations
 
 __all__ = [
+    'BillingAccountsOperations',
+    'AddressOperations',
+    'AvailableBalancesOperations',
+    'InstructionsOperations',
+    'BillingProfilesOperations',
+    'CustomersOperations',
+    'InvoiceSectionsOperations',
+    'BillingPermissionsOperations',
+    'BillingSubscriptionsOperations',
+    'ProductsOperations',
+    'InvoicesOperations',
+    'TransactionsOperations',
+    'PoliciesOperations',
+    'BillingPropertyOperations',
+    'Operations',
+    'BillingRoleDefinitionsOperations',
+    'BillingRoleAssignmentsOperations',
+    'AgreementsOperations',
     'EnrollmentAccountsOperations',
     'BillingPeriodsOperations',
-    'InvoicesOperations',
-    'Operations',
 ]
