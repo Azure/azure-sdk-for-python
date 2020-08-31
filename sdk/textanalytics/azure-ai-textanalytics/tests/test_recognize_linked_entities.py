23d9197ca2b3527304b2dbd45d6e11c6c6593515
iscai-msft <iscai@microsoft.com>
2020-08-31 16:21:11 -0400

    fix copy paste mistakes in tests

--- sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_linked_entities.py
+++ sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_linked_entities.py
@@ -596,8 +596,6 @@ class TestRecognizeLinkedEntities(TextAnalyticsTest):
         "text_analytics_account": "https://cognitiveusw2dev.azure-api.net/"
     })
     def test_bing_id(self, client):
-        # make sure that the addition of the string_index_type kwarg for v3.1-preview.1 doesn't
-        # cause v3.0 calls to fail
         result = client.recognize_linked_entities(["Microsoft was founded by Bill Gates and Paul Allen"])
         for doc in result:
             for entity in doc.entities:
--- sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_linked_entities_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_linked_entities_async.py
@@ -632,8 +632,6 @@ class TestRecognizeLinkedEntities(AsyncTextAnalyticsTest):
         "text_analytics_account": "https://cognitiveusw2dev.azure-api.net/"
     })
     async def test_bing_id(self, client):
-        # make sure that the addition of the string_index_type kwarg for v3.1-preview.1 doesn't
-        # cause v3.0 calls to fail
         result = await client.recognize_linked_entities(["Microsoft was founded by Bill Gates and Paul Allen"])
         for doc in result:
             for entity in doc.entities:
