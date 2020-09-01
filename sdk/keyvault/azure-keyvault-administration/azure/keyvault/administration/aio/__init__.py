e720dcf2d61049385db0a76da0d430dafd8fffda
Charles Lowell <chlowe@microsoft.com>
2020-09-01 09:09:49 -0700

    Add KeyVaultAccessControlClient for data plane RBAC (#13372)

--- sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/aio/__init__.py
+++ sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/aio/__init__.py
@@ -2,3 +2,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
+from ._access_control_client import KeyVaultAccessControlClient
+
+__all__ = ["KeyVaultAccessControlClient"]
