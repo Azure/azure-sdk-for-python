567d1649ab8e74accd811b42ff25020c2200fe5f
KieranBrantnerMagee <kibrantn@microsoft.com>
2020-09-01 15:50:49 -0700

    [ServiceBus] Allow messages to be sent twice concurrently, improve some exceptions in that space. (#13415)
    * Allow messages to be sent twice concurrently.  Providing the wrong type when constructing a batch now results in a TypeError, not a ValueError.
    * Add an inner exception to ServiceBusError unknown exceptions for better diagnostics.
    * Add tests for double-sending
    * Add tests for sending something that's not a message returning a TypeError instead of a ValueError.
    * Move the resend-enablement into copy_messages_to_sendable via _to_outgoing_message on Message to better consolidate functionality.
    * Rename copy_messages_to_sendable_... to transform_messages_... to represent its expanded functionality better.

--- sdk/servicebus/azure-servicebus/azure/servicebus/aio/_servicebus_sender_async.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/aio/_servicebus_sender_async.py
@@ -19,7 +19,7 @@ from .._common.constants import (
     MGMT_REQUEST_SEQUENCE_NUMBERS
 )
 from .._common import mgmt_handlers
-from .._common.utils import copy_messages_to_sendable_if_needed
+from .._common.utils import transform_messages_to_sendable_if_needed
 from ._async_utils import create_authentication
 
 if TYPE_CHECKING:
@@ -272,7 +272,7 @@ class ServiceBusSender(BaseHandler, SenderMixin):
                 :caption: Send message.
 
         """
-        message = copy_messages_to_sendable_if_needed(message)
+        message = transform_messages_to_sendable_if_needed(message)
         try:
             batch = await self.create_batch()
             batch._from_list(message)  # pylint: disable=protected-access
@@ -281,6 +281,8 @@ class ServiceBusSender(BaseHandler, SenderMixin):
             pass
         if isinstance(message, BatchMessage) and len(message) == 0:  # pylint: disable=len-as-condition
             raise ValueError("A BatchMessage or list of Message must have at least one Message")
+        if not isinstance(message, BatchMessage) and not isinstance(message, Message):
+            raise TypeError("Can only send azure.servicebus.<BatchMessage,Message> or lists of Messages.")
 
         await self._do_retryable_operation(
             self._send,
