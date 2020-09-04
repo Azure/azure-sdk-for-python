01a39a7ade429c42e04c59291034c81264d4a7ef
Yunhao Ling <adam_ling@outlook.com>
2020-09-04 10:21:26 -0700

    update avro dependency

--- sdk/schemaregistry/azure-schemaregistry-avroserializer/setup.py
+++ sdk/schemaregistry/azure-schemaregistry-avroserializer/setup.py
@@ -66,6 +66,6 @@ setup(
     packages=find_packages(exclude=exclude_packages),
     install_requires=[
         'azure-schemaregistry<2.0.0,>=1.0.0b1',
-        'avro<2.0.0,>=1.0.0'
+        'avro<2.0.0,>=1.10.0'
     ]
 )
