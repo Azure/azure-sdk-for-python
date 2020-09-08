c7865f9433b4819d9978fa913c7b6212f2c4d18f
dw511214992 <dw511214992@gmail.com>
2020-09-08 15:59:02 +0800

    update test

--- sdk/compute/azure-mgmt-compute/tests/test_mgmt_managed_disks.py
+++ sdk/compute/azure-mgmt-compute/tests/test_mgmt_managed_disks.py
@@ -136,7 +136,7 @@ class MgmtManagedDisksTest(AzureMgmtTestCase):
         )
         disk_resource = async_creation.result()
 
-    @ResourceGroupPreparer()
+    @ResourceGroupPreparer(location='eastus')
     def test_create_vm_implicit_md(self, resource_group, location):
         '''Create a VM with implicit MD'''
         virtual_machine_models = self.compute_client.virtual_machines.models
@@ -182,7 +182,7 @@ class MgmtManagedDisksTest(AzureMgmtTestCase):
             resource_group.name,
             'mySecondDisk',
             {
-                'location': self.region,
+                'location': 'eastus',
                 'disk_size_gb': 20,
                 'creation_data': {
                     'create_option': disks_models.DiskCreateOption.empty
