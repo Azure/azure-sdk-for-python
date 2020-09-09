8f4398fed54e34af30d7ae4b8ee6da0b4588138f
Adam Ling (MSFT) <adam_ling@outlook.com>
2020-09-09 10:52:18 -0700

    [SchemaRegistry] small fix in setup.py (#13677)

--- sdk/schemaregistry/azure-schemaregistry/setup.py
+++ sdk/schemaregistry/azure-schemaregistry/setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages, setup
 
 # Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-schemaregistry"
-PACKAGE_PPRINT_NAME = "SchemaRegistry"
+PACKAGE_PPRINT_NAME = "Schema Registry"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
