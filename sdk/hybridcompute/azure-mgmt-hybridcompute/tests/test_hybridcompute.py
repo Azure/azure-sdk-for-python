f9d89c576791322bdd8edf1fef91e27e811cc4d4
Changlong Liu <changlong.liu@microsoft.com>
2020-09-17 12:56:22 +0800

    run test

--- sdk/hybridcompute/azure-mgmt-hybridcompute/tests/test_hybridcompute.py
+++ sdk/hybridcompute/azure-mgmt-hybridcompute/tests/test_hybridcompute.py
@@ -7,21 +7,30 @@ from devtools_testutils import AzureMgmtTestCase
 from azure.mgmt.hybridcompute import HybridComputeManagementClient
 from azure.mgmt.hybridcompute.models import ErrorResponseException
 
+import sys
+import logging
+logger = logging.getLogger('msrest')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
 
 class HybridComputeTests(AzureMgmtTestCase):
     def setUp(self):
         super(HybridComputeTests, self).setUp()
         self.client = self.create_mgmt_client(HybridComputeManagementClient)
 
-    def test_get(self):
-        resource_group_name = 'azure-sdk-test'
-        machine_name = 'DESKTOP-RUC1OF2'
-        machine = self.client.machines.get(
-            resource_group_name,
-            machine_name
-        )
-        self.assertEqual(machine.name, machine_name)
-        self.assertEqual(machine.location, 'eastus')
+    # def test_get(self):
+    #     resource_group_name = 'azure-sdk-test'
+    #     machine_name = 'test'
+    #     machine = self.client.machines.get(
+    #         resource_group_name,
+    #         machine_name
+    #     )
+    #     self.assertEqual(machine.name, machine_name)
+    #     self.assertEqual(machine.location, 'eastus')
 
     def test_list_by_resource_group(self):
         resource_group_name = 'azure-sdk-test'
@@ -32,20 +41,20 @@ class HybridComputeTests(AzureMgmtTestCase):
         result = list(self.client.machines.list_by_subscription())
         self.assertEqual(len(result), 1)
 
-    def test_delete(self):
-        resource_group_name = 'azure-sdk-test'
-        machine_name = 'DESKTOP-RUC1OF2'
+    # def test_delete(self):
+    #     resource_group_name = 'azure-sdk-test'
+    #     machine_name = 'test'
 
-        machine = self.client.machines.get(
-            resource_group_name,
-            machine_name
-        )
-        self.assertIsNotNone(machine)
+    #     machine = self.client.machines.get(
+    #         resource_group_name,
+    #         machine_name
+    #     )
+    #     self.assertIsNotNone(machine)
 
-        self.client.machines.delete(
-            resource_group_name,
-            machine_name
-        )
-        with self.assertRaises(ErrorResponseException):
-            self.client.machines.get(resource_group_name,machine_name)
+    #     self.client.machines.delete(
+    #         resource_group_name,
+    #         machine_name
+    #     )
+    #     with self.assertRaises(ErrorResponseException):
+    #         self.client.machines.get(resource_group_name,machine_name)
 
