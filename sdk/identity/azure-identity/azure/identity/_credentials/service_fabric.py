29479b7339e01819092e56111654146de50efe1c
McCoy Patiño <mcpatino@microsoft.com>
2020-09-23 09:37:02 -0700

    Add start of ServiceFabricCredential

---
+++ sdk/identity/azure-identity/azure/identity/_credentials/service_fabric.py
@@ -0,0 +1,8 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import logging
+import os
+
+class ServiceFabricCredential(object):
