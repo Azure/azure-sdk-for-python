c3df9608ab5ba56bd0bbdef9a3b2d666b12510f5
SDK Automation <sdkautomation@microsoft.com>
2020-09-23 06:31:32 +0000

    Generated from 8f20ad7d5cc33b60b44d69351b250406c70613b9

--- sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/models/_azure_machine_learning_workspaces_enums.py
+++ sdk/machinelearning/azure-mgmt-machinelearningservices/azure/mgmt/machinelearningservices/models/_azure_machine_learning_workspaces_enums.py
@@ -23,14 +23,57 @@ class ProvisioningState(str, Enum):
     canceled = "Canceled"
 
 
+class EncryptionStatus(str, Enum):
+
+    enabled = "Enabled"
+    disabled = "Disabled"
+
+
+class PrivateEndpointServiceConnectionStatus(str, Enum):
+
+    pending = "Pending"
+    approved = "Approved"
+    rejected = "Rejected"
+    disconnected = "Disconnected"
+    timeout = "Timeout"
+
+
+class PrivateEndpointConnectionProvisioningState(str, Enum):
+
+    succeeded = "Succeeded"
+    creating = "Creating"
+    deleting = "Deleting"
+    failed = "Failed"
+
+
 class UsageUnit(str, Enum):
 
     count = "Count"
 
 
+class QuotaUnit(str, Enum):
+
+    count = "Count"
+
+
+class Status(str, Enum):
+
+    undefined = "Undefined"
+    success = "Success"
+    failure = "Failure"
+    invalid_quota_below_cluster_minimum = "InvalidQuotaBelowClusterMinimum"
+    invalid_quota_exceeds_subscription_limit = "InvalidQuotaExceedsSubscriptionLimit"
+    invalid_vm_family_name = "InvalidVMFamilyName"
+    operation_not_supported_for_sku = "OperationNotSupportedForSku"
+    operation_not_enabled_for_region = "OperationNotEnabledForRegion"
+
+
 class ResourceIdentityType(str, Enum):
 
     system_assigned = "SystemAssigned"
+    user_assigned = "UserAssigned"
+    system_assigned_user_assigned = "SystemAssigned,UserAssigned"
+    none = "None"
 
 
 class VmPriority(str, Enum):
@@ -39,21 +82,78 @@ class VmPriority(str, Enum):
     low_priority = "LowPriority"
 
 
+class RemoteLoginPortPublicAccess(str, Enum):
+
+    enabled = "Enabled"
+    disabled = "Disabled"
+    not_specified = "NotSpecified"
+
+
 class AllocationState(str, Enum):
 
     steady = "Steady"
     resizing = "Resizing"
 
 
+class ApplicationSharingPolicy(str, Enum):
+
+    personal = "Personal"
+    shared = "Shared"
+
+
+class SshPublicAccess(str, Enum):
+
+    enabled = "Enabled"
+    disabled = "Disabled"
+
+
+class ComputeInstanceState(str, Enum):
+
+    creating = "Creating"
+    create_failed = "CreateFailed"
+    deleting = "Deleting"
+    running = "Running"
+    restarting = "Restarting"
+    restart_failed = "RestartFailed"
+    job_running = "JobRunning"
+    setting_up = "SettingUp"
+    starting = "Starting"
+    start_failed = "StartFailed"
+    stop_failed = "StopFailed"
+    stopped = "Stopped"
+    stopping = "Stopping"
+    user_setting_up = "UserSettingUp"
+    unknown = "Unknown"
+    unusable = "Unusable"
+
+
+class NodeState(str, Enum):
+
+    idle = "idle"
+    running = "running"
+    preparing = "preparing"
+    unusable = "unusable"
+    leaving = "leaving"
+    preempted = "preempted"
+
+
 class ComputeType(str, Enum):
 
     aks = "AKS"
     aml_compute = "AmlCompute"
+    compute_instance = "ComputeInstance"
     data_factory = "DataFactory"
     virtual_machine = "VirtualMachine"
     hd_insight = "HDInsight"
     databricks = "Databricks"
     data_lake_analytics = "DataLakeAnalytics"
+
+
+class ReasonCode(str, Enum):
+
+    not_specified = "NotSpecified"
+    not_available_for_region = "NotAvailableForRegion"
+    not_available_for_subscription = "NotAvailableForSubscription"
 
 
 class UnderlyingResourceAction(str, Enum):
