170161963a4d7f39c1beb84af1faee6abc6dc6b8
antisch <antisch@microsoft.com>
2020-09-24 06:05:28 +1200

    Added implementation skeleton

---
+++ sdk/training/implementation-tutorial/samples/sample_conditional_async.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import asyncio
+
+
+async def main():
+    # Add your sample here
+    pass
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
