725ba494963e3c8236f1dc024972e127ed561703
Tamer Sherif <tasherif@microsoft.com>
2020-09-25 21:55:37 -0700

    removed etag

--- sdk/storage/azure-storage-file-share/azure/storage/fileshare/_lease.py
+++ sdk/storage/azure-storage-file-share/azure/storage/fileshare/_lease.py
@@ -117,10 +117,6 @@ class ShareLeaseClient(object):
 
         .. versionadded:: 12.6.0
 
-        :keyword str etag:
-            An ETag value, or the wildcard character (*). Used to check if the resource has changed,
-            and act according to the condition specified by the `match_condition` parameter.
-
         :keyword int timeout:
             The timeout parameter is expressed in seconds.
         :return: None
--- sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_lease_async.py
+++ sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_lease_async.py
@@ -109,10 +109,6 @@ class ShareLeaseClient(LeaseClientBase):
 
         .. versionadded:: 12.6.0
 
-        :keyword str etag:
-            An ETag value, or the wildcard character (*). Used to check if the resource has changed,
-            and act according to the condition specified by the `match_condition` parameter.
-
         :keyword int timeout:
             The timeout parameter is expressed in seconds.
         :return: None
