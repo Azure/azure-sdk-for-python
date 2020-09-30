5ef5e524bf55bf15b4e68084384bbfe170fd7902
scbedd <45376673+scbedd@users.noreply.github.com>
2020-09-30 13:27:39 -0700

    roll a custom UsePythonVersion that can reach out to the github actions repos for python installers

---
+++ scripts/devops_tasks/resolve_installer_location.py
@@ -0,0 +1,56 @@
+import platform
+import json
+import argparse
+import urllib
+from urllib.request import urlopen
+from packaging.version import Version
+from packaging.version import parse
+import pdb
+
+MANIFEST_LOCATION = "https://raw.githubusercontent.com/actions/python-versions/main/versions-manifest.json"
+
+
+def get_installer_url(requested_version, version_manifest):
+    if version_manifest[requested_version]:
+        found_installers = version_manifest[requested_version].files
+
+        # filter anything that's not x64
+        x64_installers = [file_def for file_def in found_installers if file_def["arch"] == "x64"]
+
+        # if we're on linux, we should go after the one that aligns with our platform version.
+        
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Takes the incoming request and attempts to resolve the versionspec"
+    )
+
+    parser.add_argument(
+        "versionSpec",
+        nargs="?",
+        help=(
+            "The version specifier passed in to the UsePythonVersion extended task."
+        ),
+    )
+
+    args = parser.parse_args()
+    max_precached_version = Version('3.8.6')
+    version_from_spec = Version(args.versionSpec)
+
+    with urllib.request.urlopen(MANIFEST_LOCATION) as url:
+        version_manifest = json.load(url)
+
+    version_dict = { i['version'] : i for i in version_manifest }
+
+    if version_from_spec > max_precached_version:
+        print("Requested version {} is newer than versions pre-cached on agent. Invoking.")
+        print("##vso[task.setvariable variable=_PythonNeedsInstall;]true")
+
+        version_url = get_installer_url(args.versionSpec, version_dict)
+        print("##vso[task.setvariable variable=_PythonInstallerLocation;]{}".format(version_url))
+    else:
+        print("##vso[task.setvariable variable=_PythonNeedsInstall;]false")
+
+
+
