1d42bdaaa25a0adfaa5047720d393e22883a6594
Kieran Brantner-Magee <kibrantn@microsoft.com>
2020-09-30 17:28:02 -0700

    Adjust conditional in preparer to be more precise until we understand the existing behavior better.  Only targets the failure mode we specifically observed, where you have a non live test with no recording seeding a cached resource name as random rather than moniker.

--- tools/azure-devtools/src/azure_devtools/scenario_tests/preparers.py
+++ tools/azure-devtools/src/azure_devtools/scenario_tests/preparers.py
@@ -40,13 +40,11 @@ class AbstractPreparer(object):
         self.live_test = not isinstance(test_class_instance, ReplayableTest)
         self.test_class_instance = test_class_instance
 
-        if (self.live_test or test_class_instance.in_recording) and not self._use_cache:
-            if not self._use_cache:
-                resource_name = self.random_name
-                if not self.live_test and isinstance(self, RecordingProcessor):
-                    test_class_instance.recording_processors.append(self)
-            else:
-                resource_name = self.moniker
+        if (self.live_test or test_class_instance.in_recording) \
+                and not (not self.live_test and test_class_instance.in_recording and self._use_cache):
+            resource_name = self.random_name
+            if not self.live_test and isinstance(self, RecordingProcessor):
+                test_class_instance.recording_processors.append(self)
         else:
             resource_name = self.moniker
 
