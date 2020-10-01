fd6ddd62dd41d0271f7e57bdb564c62c347dc69b
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2020-10-01 11:23:53 -0700

    addressing issues with docs and readme (#13988)
    Address #13977

--- sdk/tables/azure-data-tables/samples/async_samples/sample_insert_delete_entities_async.py
+++ sdk/tables/azure-data-tables/samples/async_samples/sample_insert_delete_entities_async.py
@@ -43,19 +43,19 @@ class InsertDeleteEntity(object):
         from azure.core.exceptions import ResourceExistsError, HttpResponseError
 
         table_client = TableClient.from_connection_string(self.connection_string, self.table_name)
-
         # Create a table in case it does not already exist
         # [START create_entity]
-        try:
-            await table_client.create_table()
-        except HttpResponseError:
-            print("Table already exists")
+        async with table_client:
+            try:
+                await table_client.create_table()
+            except HttpResponseError:
+                print("Table already exists")
 
-        try:
-            entity = await table_client.create_entity(entity=self.entity)
-            print(entity)
-        except ResourceExistsError:
-            print("Entity already exists")
+            try:
+                entity = await table_client.create_entity(entity=self.entity)
+                print(entity)
+            except ResourceExistsError:
+                print("Entity already exists")
         # [END create_entity]
 
 
@@ -67,19 +67,20 @@ class InsertDeleteEntity(object):
         table_client = TableClient(account_url=self.account_url, credential=self.access_key, table_name=self.table_name)
 
         # [START delete_entity]
-        try:
-            resp = await table_client.create_entity(entity=self.entity)
-        except ResourceExistsError:
-            print("Entity already exists!")
+        async with table_client:
+            try:
+                resp = await table_client.create_entity(entity=self.entity)
+            except ResourceExistsError:
+                print("Entity already exists!")
 
-        try:
-            await table_client.delete_entity(
-                row_key=self.entity["RowKey"],
-                partition_key=self.entity["PartitionKey"]
-            )
-            print("Successfully deleted!")
-        except ResourceNotFoundError:
-            print("Entity does not exists")
+            try:
+                await table_client.delete_entity(
+                    row_key=self.entity["RowKey"],
+                    partition_key=self.entity["PartitionKey"]
+                )
+                print("Successfully deleted!")
+            except ResourceNotFoundError:
+                print("Entity does not exists")
         # [END delete_entity]
 
 
