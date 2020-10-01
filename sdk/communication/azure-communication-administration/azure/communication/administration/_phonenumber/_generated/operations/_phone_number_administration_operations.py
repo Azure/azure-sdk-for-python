72d50ec0f3e887cd4fe63fc3c8b474cf53668e08
Heli Wang <h379wang@uwaterloo.ca>
2020-10-01 13:11:27 -0700

    refresh swagger and auto generated code (#14182)

--- sdk/communication/azure-communication-administration/azure/communication/administration/_phonenumber/_generated/operations/_phone_number_administration_operations.py
+++ sdk/communication/azure-communication-administration/azure/communication/administration/_phonenumber/_generated/operations/_phone_number_administration_operations.py
@@ -533,7 +533,7 @@ class PhoneNumberAdministrationOperations(object):
         pipeline_response = self._client._pipeline.run(request, stream=False, **kwargs)
         response = pipeline_response.http_response
 
-        if response.status_code not in [202]:
+        if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
             error = self._deserialize(models.ErrorResponse, response)
             raise HttpResponseError(response=response, model=error)
@@ -594,7 +594,7 @@ class PhoneNumberAdministrationOperations(object):
         pipeline_response = self._client._pipeline.run(request, stream=False, **kwargs)
         response = pipeline_response.http_response
 
-        if response.status_code not in [202]:
+        if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
             error = self._deserialize(models.ErrorResponse, response)
             raise HttpResponseError(response=response, model=error)
