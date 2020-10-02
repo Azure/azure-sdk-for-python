4b2bf016241b245b916283d922558ce45ee51fff
iscai-msft <iscai@microsoft.com>
2020-10-02 15:40:24 -0400

    update tests

--- sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_pii_entities_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/tests/test_recognize_pii_entities_async.py
@@ -566,7 +566,7 @@ class TestRecognizePIIEntities(AsyncTextAnalyticsTest):
     @GlobalTextAnalyticsAccountPreparer()
     @TextAnalyticsClientPreparer(client_kwargs={"api_version": TextAnalyticsApiVersion.V3_0})
     async def test_recognize_pii_entities_v3(self, client):
-        with pytest.raises(NotImplementedError) as excinfo:
+        with pytest.raises(ValueError) as excinfo:
             await client.recognize_pii_entities(["this should fail"])
 
         assert "'recognize_pii_entities' endpoint is only available for API version v3.1-preview and up" in str(excinfo.value)
