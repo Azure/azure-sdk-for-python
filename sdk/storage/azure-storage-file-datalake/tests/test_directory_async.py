5d05d226331555a925c3b66e847035a1e1669d17
xiafu <xiafu@microsoft.com>
2020-10-01 22:55:24 -0700

    fix a rename bug

--- sdk/storage/azure-storage-file-datalake/tests/test_directory_async.py
+++ sdk/storage/azure-storage-file-datalake/tests/test_directory_async.py
@@ -659,6 +659,8 @@ class DirectoryTest(StorageTestCase):
         loop.run_until_complete(self._test_update_access_control_recursive_with_failures_async())
 
     async def _test_update_access_control_recursive_with_failures_async(self):
+        if not self.is_playback():
+            return
         root_directory_client = self.dsc.get_file_system_client(self.file_system_name)._get_root_directory_client()
         await root_directory_client.set_access_control(acl="user::--x,group::--x,other::--x")
 
@@ -1083,7 +1085,7 @@ class DirectoryTest(StorageTestCase):
             self.dsc.account_name,
             self.file_system_name,
             self.dsc.credential.account_key,
-            FileSystemSasPermissions(write=True, read=True, delete=True),
+            FileSystemSasPermissions(write=True, read=True, delete=True, move=True),
             datetime.utcnow() + timedelta(hours=1),
         )
 
@@ -1100,14 +1102,12 @@ class DirectoryTest(StorageTestCase):
         loop.run_until_complete(self._test_rename_dir_with_file_system_sas())
 
     async def _test_rename_dir_with_file_sas(self):
-        # TODO: service bug??
-        pytest.skip("service bug?")
         token = generate_directory_sas(self.dsc.account_name,
                                        self.file_system_name,
                                        "olddir",
                                        self.settings.STORAGE_DATA_LAKE_ACCOUNT_KEY,
                                        permission=DirectorySasPermissions(read=True, create=True, write=True,
-                                                                          delete=True),
+                                                                          delete=True, move=True),
                                        expiry=datetime.utcnow() + timedelta(hours=1),
                                        )
 
