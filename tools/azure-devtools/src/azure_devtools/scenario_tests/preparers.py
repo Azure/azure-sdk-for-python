ea44afa071f3765c14f4723da6c914ed1414bd1c
Kieran Brantner-Magee <kibrantn@microsoft.com>
2020-10-01 22:41:26 -0700

    See if we can identify why the preparer is going sideways with the cosmos changes in CI.

--- tools/azure-devtools/src/azure_devtools/scenario_tests/preparers.py
+++ tools/azure-devtools/src/azure_devtools/scenario_tests/preparers.py
@@ -44,6 +44,9 @@ class AbstractPreparer(object):
         # If the first cached test run does not have any http traffic, a recording will not have been
         # generated, so in_recording will be True even if live_test is false, so a random name would be given.
         # In cached mode we need to avoid this because then for tests with recordings, they would not have a moniker.
+        print("LIVE TEST: " + str(self.live_test))
+        print("IN RECORDING: " + str(test_class_instance.in_recording))
+        print("USE CACHE: " + str(self._use_cache))
         if (self.live_test or test_class_instance.in_recording) \
                 and not (not self.live_test and test_class_instance.in_recording and self._use_cache):
             resource_name = self.random_name
