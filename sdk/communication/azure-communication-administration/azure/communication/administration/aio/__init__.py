d2bfad3d01f3e52cd23154d1b687fef40a4c5a3b
Heli Wang <h379wang@edu.uwaterloo.ca>
2020-10-02 11:02:55 -0700

    Revert "Azure Communication Services - Phone Number Admin - Implementing long running operations for phone number search  (#14157)"
    This reverts commit fd7191de8705d4d06e141209bd6a2b9ab1d92fbb.

--- sdk/communication/azure-communication-administration/azure/communication/administration/aio/__init__.py
+++ sdk/communication/azure-communication-administration/azure/communication/administration/aio/__init__.py
@@ -1,9 +1,7 @@
 from ._communication_identity_client_async import CommunicationIdentityClient
 from ._phone_number_administration_client_async import PhoneNumberAdministrationClient
-from ._polling_async import PhoneNumberPollingAsync
 
 __all__ = [
     'CommunicationIdentityClient',
-    'PhoneNumberAdministrationClient',
-    'PhoneNumberPollingAsync'
+    'PhoneNumberAdministrationClient'
 ]
