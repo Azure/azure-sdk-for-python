097f8524c641ca7bcba1e2bd9a99aec294f0951d
antisch <antisch@microsoft.com>
2020-10-06 11:05:59 +1300

    Fixed env var

--- sdk/api-learn/implementation-tutorial/samples/sample_conditional_async.py
+++ sdk/api-learn/implementation-tutorial/samples/sample_conditional_async.py
@@ -18,7 +18,7 @@ from azure.core import MatchConditions
 
 
 async def main():
-    url = os.environ.get('APP_CONFIG_HOSTNAME')
+    url = os.environ.get('APP_CONFIG_URL')
     credential = DefaultAzureCredential()
     async with AppConfigurationClient(account_url=url, credential=credential) as client:
 
