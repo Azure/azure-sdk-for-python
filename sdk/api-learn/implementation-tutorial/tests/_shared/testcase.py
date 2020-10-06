21cc3ded5047a7e42f47cd55a590671b7eed0339
seankane-msft <seankane@microsoft.com>
2020-10-06 11:44:03 -0700

    beginnings of the implementation of testing for python quality unit

---
+++ sdk/api-learn/implementation-tutorial/tests/_shared/testcase.py
@@ -0,0 +1,21 @@
+from devtools_testutils import AzureMgmtTestCase
+
+class AppConfigTestCase(AzureMgmtTestCase):
+    def __init__(self, *args, **kwargs):
+        super(AppConfigTestCase, self).__init__(*args, **kwargs)
+
+    def get_connection_string(self, account, key):
+        return "DefaultEndpointsProtocol=https;AccountName=" + account.name + ";AccountKey=" + str(key) + ";EndpointSuffix=core.windows.net"
+
+    def enable_logging(self):
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
+        self.logger.handlers = [handler]
+        self.logger.setLevel(logging.INFO)
+        self.logger.propagate = True
+        self.logger.disabled = False
+
+    def disable_logging(self):
+        self.logger.propagate = False
+        self.logger.disabled = True
+        self.logger.handlers = []
---
+++ sdk/api-learn/implementation-tutorial/tests/test_app_config.py
@@ -0,0 +1,21 @@
+import os
+
+from _shared.testcase import AppConfigTestCase
+from azure.appconfiguration import AppConfigurationClient
+from azure.identity import DefaultAzureCredential
+
+class AppConfigurationClientTest(AppConfigTestCase):
+    def __init__(self, *args, **kwargs):
+        super(AppConfigurationClientTest, self).__init__(*args, **kwargs)
+        if self.is_playback():
+            self.connection_str = "Endpoint=https://fake_app_config.azconfig-test.io;Id=0-l4-s0:h5htBaY5Z1LwFz50bIQv;Secret=bgyvBgwsQIw0s8myrqJJI3nLrj81M/kzSgSuP4BBoVg="
+        else:
+            self.connection_str = os.getenv('LEARN_APPCONFIG_CONNECTION')
+
+    def setUp(self):
+        super(AppConfigurationClientTest, self).setUp()
+
+    def test_create_client(self):
+        url = os.environ.get('APP_CONFIG_URL')
+        credential = DefaultAzureCredential()
+        client = AppConfigurationClient(account_url=url, credential=credential)
