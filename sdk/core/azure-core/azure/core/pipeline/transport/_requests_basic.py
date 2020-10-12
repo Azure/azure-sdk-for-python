b5068d6123abca4c95d15520233936dcf35159e9
Xiang Yan <xiangyan@microsoft.com>
2020-10-12 11:58:01 -0700

    customize httpadapter to honor kwargs in send method

--- sdk/core/azure-core/azure/core/pipeline/transport/_requests_basic.py
+++ sdk/core/azure-core/azure/core/pipeline/transport/_requests_basic.py
@@ -43,6 +43,7 @@ from ._base import (
     HttpResponse,
     _HttpResponseBase
 )
+from ._adapters import HTTPAdapter
 
 PipelineType = TypeVar("PipelineType")
 
@@ -213,7 +214,7 @@ class RequestsTransport(HttpTransport):
         """
         session.trust_env = self._use_env_settings
         disable_retries = Retry(total=False, redirect=False, raise_on_status=False)
-        adapter = requests.adapters.HTTPAdapter(max_retries=disable_retries)
+        adapter = HTTPAdapter(max_retries=disable_retries)
         for p in self._protocols:
             session.mount(p, adapter)
 
