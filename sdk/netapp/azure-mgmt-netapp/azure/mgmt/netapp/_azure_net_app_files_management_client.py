99ec9724dc3053ea49a2fcef56061c2ce5ff1fdf
SDK Automation <sdkautomation@microsoft.com>
2020-10-14 18:44:51 +0000

    Generated from c582ae31136d1f0f3da4570dcac8c7800fd45429

--- sdk/netapp/azure-mgmt-netapp/azure/mgmt/netapp/_azure_net_app_files_management_client.py
+++ sdk/netapp/azure-mgmt-netapp/azure/mgmt/netapp/_azure_net_app_files_management_client.py
@@ -73,7 +73,7 @@ class AzureNetAppFilesManagementClient(SDKClient):
         super(AzureNetAppFilesManagementClient, self).__init__(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self.api_version = '2020-06-01'
+        self.api_version = '2020-07-01'
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
