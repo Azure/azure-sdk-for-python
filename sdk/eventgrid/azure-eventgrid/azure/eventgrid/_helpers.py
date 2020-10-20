a1f769a869eb39454250e1741f3cd70a3e4ff3f8
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2020-10-20 14:31:20 +0530

    lint

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/_helpers.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/_helpers.py
@@ -2,11 +2,11 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+from typing import TYPE_CHECKING, Any
 import hashlib
 import hmac
 import base64
 import six
-from typing import TYPE_CHECKING, Any
 try:
     from urllib.parse import quote
 except ImportError:
