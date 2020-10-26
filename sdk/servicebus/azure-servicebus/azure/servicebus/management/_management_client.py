b65f309217dfd159b6994e10b521b8dd17d55ceb
bradleydamato <bradleydamato@gmail.com>
2020-10-26 18:30:29 -0400

    ServiceBusAdministrationClient update_* methods now accept dictionary representations

--- sdk/servicebus/azure-servicebus/azure/servicebus/management/_management_client.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/management/_management_client.py
@@ -280,8 +280,11 @@ class ServiceBusAdministrationClient:  # pylint:disable=too-many-public-methods
         :type queue: ~azure.servicebus.management.QueueProperties
         :rtype: None
         """
-
-        to_update = queue._to_internal_entity()
+        if isinstance(queue,dict):
+            dict_to_queue_props = QueueProperties(queue.pop('name'),**queue) 
+            to_update=dict_to_queue_props._to_internal_entity()
+        else:
+            to_update = queue._to_internal_entity()
         to_update.__dict__.update((k, v) for k, v in kwargs.items() if k in to_update.__dict__.keys()) 
 
         to_update.default_message_time_to_live = avoid_timedelta_overflow(to_update.default_message_time_to_live)
@@ -483,8 +486,11 @@ class ServiceBusAdministrationClient:  # pylint:disable=too-many-public-methods
         :type topic: ~azure.servicebus.management.TopicProperties
         :rtype: None
         """
-
-        to_update = topic._to_internal_entity()
+        if isinstance(topic,dict):
+            dict_to_topic_props = TopicProperties(topic.pop('name'),**topic)
+            to_update = dict_to_topic_props._to_internal_entity()
+        else:
+            to_update = topic._to_internal_entity()
         to_update.__dict__.update((k, v) for k, v in kwargs.items() if k in to_update.__dict__.keys()) 
         to_update.default_message_time_to_live = kwargs.get(
             "default_message_time_to_live") or topic.default_message_time_to_live
@@ -693,8 +699,11 @@ class ServiceBusAdministrationClient:  # pylint:disable=too-many-public-methods
         :rtype: None
         """
         _validate_entity_name_type(topic_name, display_name='topic_name')
-
-        to_update = subscription._to_internal_entity()
+        if isinstance(subscription,dict):
+            dict_to_subscription_props = SubscriptionProperties(subscription.pop('name'),**subscription)
+            to_update = dict_to_subscription_props._to_internal_entity()
+        else:
+            to_update = subscription._to_internal_entity()
         to_update.__dict__.update((k, v) for k, v in kwargs.items() if k in to_update.__dict__.keys()) 
 
         to_update.default_message_time_to_live = avoid_timedelta_overflow(to_update.default_message_time_to_live)
@@ -858,8 +867,11 @@ class ServiceBusAdministrationClient:  # pylint:disable=too-many-public-methods
         :rtype: None
         """
         _validate_topic_and_subscription_types(topic_name, subscription_name)
-
-        to_update = rule._to_internal_entity()
+        if isinstance(rule,dict):
+            dict_to_rule_props = RuleProperties(rule.pop("name"),**rule)
+            to_update = dict_to_rule_props._to_internal_entity()
+        else:
+            to_update = rule._to_internal_entity()
         to_update.__dict__.update((k, v) for k, v in kwargs.items() if k in to_update.__dict__.keys()) 
 
         create_entity_body = CreateRuleBody(
