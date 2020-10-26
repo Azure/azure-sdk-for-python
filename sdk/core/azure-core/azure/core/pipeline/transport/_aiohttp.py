74a3de571c645bff7827abf48f7df2d6e806f6d7
Xiang Yan <xiangyan@microsoft.com>
2020-10-26 16:55:06 -0700

    fix mypy failure for new aiohttp

--- sdk/core/azure-core/azure/core/pipeline/transport/_aiohttp.py
+++ sdk/core/azure-core/azure/core/pipeline/transport/_aiohttp.py
@@ -263,8 +263,11 @@ class AioHttpTransportResponse(AsyncHttpResponse):
     def __init__(self, request: HttpRequest, aiohttp_response: aiohttp.ClientResponse, block_size=None) -> None:
         super(AioHttpTransportResponse, self).__init__(request, aiohttp_response, block_size=block_size)
         # https://aiohttp.readthedocs.io/en/stable/client_reference.html#aiohttp.ClientResponse
+        headers = aiohttp_response.headers
+        if isinstance(headers, aiohttp.multidict.CIMultiDictProxy[str]):
+            headers = aiohttp.multidict.CIMultiDict(headers)
         self.status_code = aiohttp_response.status
-        self.headers = aiohttp_response.headers
+        self.headers = headers
         self.reason = aiohttp_response.reason
         self.content_type = aiohttp_response.headers.get('content-type')
         self._body = None
