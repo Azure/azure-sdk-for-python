96f3128870798915a498da3451d74d5bc6dcab71
Xiang Yan <xiangyan@microsoft.com>
2020-10-27 10:10:52 -0700

    update

--- sdk/core/azure-core/azure/core/pipeline/transport/_base.py
+++ sdk/core/azure-core/azure/core/pipeline/transport/_base.py
@@ -513,7 +513,7 @@ class _HttpResponseBase(object):
         self.request = request
         self.internal_response = internal_response
         self.status_code = None  # type: Optional[int]
-        self.headers = {}  # type: Dict[str, str]
+        self.headers = {}  # type: Any
         self.reason = None  # type: Optional[str]
         self.content_type = None  # type: Optional[str]
         self.block_size = block_size or 4096  # Default to same as Requests
