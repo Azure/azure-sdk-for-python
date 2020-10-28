70411ecf20a7eb7246c3f09981b5be06221d2d78
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2020-10-28 17:24:42 +0800

    [T2] ContainerRegistry (#13028)

--- sdk/containerregistry/azure-mgmt-containerregistry/setup.py
+++ sdk/containerregistry/azure-mgmt-containerregistry/setup.py
@@ -81,8 +81,8 @@ setup(
     ]),
     install_requires=[
         'msrest>=0.5.0',
-        'msrestazure>=0.4.32,<2.0.0',
         'azure-common~=1.1',
+        'azure-mgmt-core>=1.2.0,<2.0.0',
     ],
     extras_require={
         ":python_version<'3.0'": ['azure-mgmt-nspkg'],
