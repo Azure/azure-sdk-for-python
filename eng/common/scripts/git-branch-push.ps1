marche0133/azure-sdk-for-android

Use a new branch for quing the TestPipeline, this should prevent check ffrom showing up on eng/common Sync PRs

eng/common/scripts/git-branch-push.ps1
@@ -27,6 +27,8 @@ param(
     [Parameter(Mandatory = $false)]
     [string] $PushArgs = "",
 
+    [string] $ForkName = "azure-sdk-fork",
+
     [Parameter(Mandatory = $false)]
     [boolean] $SkipCommit = $false
 )
@@ -36,16 +38,16 @@ param(
 # would fail the first time git wrote command output.
 $ErrorActionPreference = "Continue"
 
-Write-Host "git remote add azure-sdk-fork $GitUrl"
-git remote add azure-sdk-fork $GitUrl
+Write-Host "git remote add $ForkName $GitUrl"
+git remote add $ForkName $GitUrl
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Unable to add remote LASTEXITCODE=$($LASTEXITCODE), see command output above."
     exit $LASTEXITCODE
 }
 
-Write-Host "git fetch azure-sdk-fork"
-git fetch azure-sdk-fork
+Write-Host "git fetch $ForkName"
+git fetch $ForkName
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Unable to fetch remote LASTEXITCODE=$($LASTEXITCODE), see command output above."
@@ -84,15 +86,15 @@ $tryNumber = 0
 do
 {
     $needsRetry = $false
-    Write-Host "git push azure-sdk-fork $PRBranchName $PushArgs"
-    git push azure-sdk-fork $PRBranchName $PushArgs
+    Write-Host "git push $ForkName $PRBranchName $PushArgs"
+    git push $ForkName $PRBranchName $PushArgs
     $tryNumber++
     if ($LASTEXITCODE -ne 0)
     {
         $needsRetry = $true
         Write-Host "Git push failed with LASTEXITCODE=$($LASTEXITCODE) Need to fetch and rebase: attempt number=$($tryNumber)"
-        Write-Host "git fetch azure-sdk-fork"
-        git fetch azure-sdk-fork
+        Write-Host "git fetch $ForkName"
+        git fetch $ForkName
         if ($LASTEXITCODE -ne 0)
         {
             Write-Error "Unable to fetch remote LASTEXITCODE=$($LASTEXITCODE), see command output above."
@@ -110,8 +112,8 @@ do
                 continue
             }
 
-            Write-Host "git reset --hard azure-sdk-fork/${PRBranchName}"
-            git reset --hard azure-sdk-fork/${PRBranchName}
+            Write-Host "git reset --hard $ForkName/${PRBranchName}"
+            git reset --hard $ForkName/${PRBranchName}
             if ($LASTEXITCODE -ne 0)
             {
                 Write-Error "Unable to hard reset branch LASTEXITCODE=$($LASTEXITCODE), see command output above."

Azure/azure-sdk-for-python

eng/common/scripts/git-branch-push.ps1
@@ -27,6 +27,8 @@ param(
     [Parameter(Mandatory = $false)]
     [string] $PushArgs = "",
 
+    [string] $RemoteName = "azure-sdk-fork",
+
     [Parameter(Mandatory = $false)]
     [boolean] $SkipCommit = $false
 )
@@ -36,16 +38,16 @@ param(
 # would fail the first time git wrote command output.
 $ErrorActionPreference = "Continue"
 
-Write-Host "git remote add azure-sdk-fork $GitUrl"
-git remote add azure-sdk-fork $GitUrl
+Write-Host "git remote add $RemoteName $GitUrl"
+git remote add $RemoteName $GitUrl
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Unable to add remote LASTEXITCODE=$($LASTEXITCODE), see command output above."
     exit $LASTEXITCODE
 }
 
-Write-Host "git fetch azure-sdk-fork"
-git fetch azure-sdk-fork
+Write-Host "git fetch $RemoteName"
+git fetch $RemoteName
 if ($LASTEXITCODE -ne 0)
 {
     Write-Error "Unable to fetch remote LASTEXITCODE=$($LASTEXITCODE), see command output above."
@@ -84,15 +86,15 @@ $tryNumber = 0
 do
 {
     $needsRetry = $false
-    Write-Host "git push azure-sdk-fork $PRBranchName $PushArgs"
-    git push azure-sdk-fork $PRBranchName $PushArgs
+    Write-Host "git push $RemoteName $PRBranchName $PushArgs"
+    git push $RemoteName $PRBranchName $PushArgs
     $tryNumber++
     if ($LASTEXITCODE -ne 0)
     {
         $needsRetry = $true
         Write-Host "Git push failed with LASTEXITCODE=$($LASTEXITCODE) Need to fetch and rebase: attempt number=$($tryNumber)"
-        Write-Host "git fetch azure-sdk-fork"
-        git fetch azure-sdk-fork
+        Write-Host "git fetch $RemoteName"
+        git fetch $RemoteName
         if ($LASTEXITCODE -ne 0)
         {
             Write-Error "Unable to fetch remote LASTEXITCODE=$($LASTEXITCODE), see command output above."
@@ -110,8 +112,8 @@ do
                 continue
             }
 
-            Write-Host "git reset --hard azure-sdk-fork/${PRBranchName}"
-            git reset --hard azure-sdk-fork/${PRBranchName}
+            Write-Host "git reset --hard $RemoteName/${PRBranchName}"
+            git reset --hard $RemoteName/${PRBranchName}
             if ($LASTEXITCODE -ne 0)
             {
                 Write-Error "Unable to hard reset branch LASTEXITCODE=$($LASTEXITCODE), see command output above."