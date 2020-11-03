eeefa1a526a19a5fd9fb127213db74093f7b04d5
SDK Automation <sdkautomation@microsoft.com>
2020-11-03 21:15:33 +0000

    Generated from f808919cbcfd61a60f48fafddf96be21ebf03648

--- sdk/keyvault/azure-mgmt-keyvault/azure/mgmt/keyvault/v2019_09_01/models/_key_vault_management_client_enums.py
+++ sdk/keyvault/azure-mgmt-keyvault/azure/mgmt/keyvault/v2019_09_01/models/_key_vault_management_client_enums.py
@@ -34,6 +34,7 @@ class AccessPolicyUpdateKind(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)
 
 class CertificatePermissions(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
+    ALL = "all"
     GET = "get"
     LIST = "list"
     DELETE = "delete"
@@ -58,8 +59,51 @@ class CreateMode(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     RECOVER = "recover"
     DEFAULT = "default"
 
+class DeletionRecoveryLevel(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+    """The deletion recovery level currently in effect for the object. If it contains 'Purgeable',
+    then the object can be permanently deleted by a privileged user; otherwise, only the system can
+    purge the object at the end of the retention interval.
+    """
+
+    PURGEABLE = "Purgeable"
+    RECOVERABLE_PURGEABLE = "Recoverable+Purgeable"
+    RECOVERABLE = "Recoverable"
+    RECOVERABLE_PROTECTED_SUBSCRIPTION = "Recoverable+ProtectedSubscription"
+
+class JsonWebKeyCurveName(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+    """The elliptic curve name. For valid values, see JsonWebKeyCurveName.
+    """
+
+    P256 = "P-256"
+    P384 = "P-384"
+    P521 = "P-521"
+    P256_K = "P-256K"
+
+class JsonWebKeyOperation(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+    """The permitted JSON web key operations of the key. For more information, see
+    JsonWebKeyOperation.
+    """
+
+    ENCRYPT = "encrypt"
+    DECRYPT = "decrypt"
+    SIGN = "sign"
+    VERIFY = "verify"
+    WRAP_KEY = "wrapKey"
+    UNWRAP_KEY = "unwrapKey"
+    IMPORT_ENUM = "import"
+
+class JsonWebKeyType(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+    """The type of the key. For valid values, see JsonWebKeyType.
+    """
+
+    EC = "EC"
+    EC_HSM = "EC-HSM"
+    RSA = "RSA"
+    RSA_HSM = "RSA-HSM"
+
 class KeyPermissions(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
+    ALL = "all"
     ENCRYPT = "encrypt"
     DECRYPT = "decrypt"
     WRAP_KEY = "wrapKey"
@@ -123,6 +167,7 @@ class Reason(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
 class SecretPermissions(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
+    ALL = "all"
     GET = "get"
     LIST = "list"
     SET = "set"
@@ -147,6 +192,7 @@ class SkuName(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
 class StoragePermissions(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
+    ALL = "all"
     GET = "get"
     LIST = "list"
     DELETE = "delete"
