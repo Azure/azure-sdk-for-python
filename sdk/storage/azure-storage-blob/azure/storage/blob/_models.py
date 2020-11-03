475f2a1595eba907019138778c391d6abac0b747
Tamer Sherif <tasherif@microsoft.com>
2020-11-02 16:35:05 -0800

    added more missing blob perms

--- sdk/storage/azure-storage-blob/azure/storage/blob/_models.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/_models.py
@@ -888,10 +888,10 @@ class ContainerSasPermissions(object):
     :param bool delete:
         Delete any blob in the container. Note: You cannot grant permissions to
         delete a container with a container SAS. Use an account SAS instead.
-    :param bool delete_previous_version:
-        Delete the previous blob version for the versioning enabled storage account.
     :param bool list:
         List blobs in the container.
+    :param bool delete_previous_version:
+        Delete the previous blob version for the versioning enabled storage account.
     :param bool tag:
         Set or get tags on the blobs in the container.
     :param bool add:
@@ -900,6 +900,10 @@ class ContainerSasPermissions(object):
         Write a new blob to the container, snapshot any blob in the container, or copy a blob to
         a new blob in the container. Note: You cannot grant permissions to create a container
         with a container SAS. Use an account SAS to create a container instead.
+    :param bool move:
+        Sets the move permission.
+    :param bool execute:
+        Sets the execute permission.
     """
     def __init__(self,
                  read=False,
@@ -909,7 +913,9 @@ class ContainerSasPermissions(object):
                  delete_previous_version=False,
                  tag=False,
                  add=False,
-                 create=False):  # pylint: disable=redefined-builtin
+                 create=False,
+                 move=False,
+                 execute=False):  # pylint: disable=redefined-builtin
         self.read = read
         self.add = add
         self.create = create
@@ -918,6 +924,8 @@ class ContainerSasPermissions(object):
         self.list = list
         self.delete_previous_version = delete_previous_version
         self.tag = tag
+        self.move = move
+        self.execute = execute
         self._str = (('r' if self.read else '') +
                      ('a' if self.add else '') +
                      ('c' if self.create else '') +
@@ -925,7 +933,9 @@ class ContainerSasPermissions(object):
                      ('d' if self.delete else '') +
                      ('x' if self.delete_previous_version else '') +
                      ('l' if self.list else '') +
-                     ('t' if self.tag else ''))
+                     ('t' if self.tag else '') +
+                     ('m' if self.move else '') +
+                     ('e' if self.execute else ''))
 
     def __str__(self):
         return self._str
@@ -951,8 +961,11 @@ class ContainerSasPermissions(object):
         p_tag = 't' in permission
         p_add = 'a' in permission
         p_create = 'c' in permission
+        p_move = 'm' in permission
+        p_execute = 'e' in permission
         parsed = cls(read=p_read, write=p_write, delete=p_delete, list=p_list,
-                     delete_previous_version=p_delete_previous_version, tag=p_tag, add=p_add, create=p_create)
+                     delete_previous_version=p_delete_previous_version, tag=p_tag, add=p_add, create=p_create,
+                     move=p_move, execute=p_execute)
 
         return parsed
 
@@ -964,10 +977,6 @@ class BlobSasPermissions(object):
     :param bool read:
         Read the content, properties, metadata and block list. Use the blob as
         the source of a copy operation.
-    :param bool add:
-        Add a block to an append blob.
-    :param bool create:
-        Write a new blob, snapshot a blob, or copy a blob to a new blob.
     :param bool write:
         Create or write content, properties, metadata, or block list. Snapshot
         or lease the blob. Resize the blob (page blob only). Use the blob as the
@@ -978,23 +987,48 @@ class BlobSasPermissions(object):
         Delete the previous blob version for the versioning enabled storage account.
     :param bool tag:
         Set or get tags on the blob.
+    :param bool add:
+        Add a block to an append blob.
+    :param bool create:
+        Write a new blob, snapshot a blob, or copy a blob to a new blob.
+    :param bool list:
+        Sets the list permission.
+    :param bool move:
+        Sets the move permission.
+    :param bool execute:
+        Sets the execute permission.
     """
-    def __init__(self, read=False, add=False, create=False, write=False,
-                 delete=False, delete_previous_version=False, tag=True):
+    def __init__(self,
+                 read=False,
+                 write=False,
+                 delete=False,
+                 delete_previous_version=False,
+                 tag=True,
+                 add=False,
+                 create=False,
+                 list=False,    # pylint: disable=redefined-builtin
+                 move=False,
+                 execute=False):  # pylint: disable=redefined-builtin
         self.read = read
         self.add = add
         self.create = create
         self.write = write
         self.delete = delete
+        self.list = list
         self.delete_previous_version = delete_previous_version
         self.tag = tag
+        self.move = move
+        self.execute = execute
         self._str = (('r' if self.read else '') +
                      ('a' if self.add else '') +
                      ('c' if self.create else '') +
                      ('w' if self.write else '') +
                      ('d' if self.delete else '') +
                      ('x' if self.delete_previous_version else '') +
-                     ('t' if self.tag else ''))
+                     ('l' if self.list else '') +
+                     ('t' if self.tag else '') +
+                     ('m' if self.move else '') +
+                     ('e' if self.execute else ''))
 
     def __str__(self):
         return self._str
@@ -1013,15 +1047,18 @@ class BlobSasPermissions(object):
         :rtype: ~azure.storage.blob.BlobSasPermissions
         """
         p_read = 'r' in permission
-        p_add = 'a' in permission
-        p_create = 'c' in permission
         p_write = 'w' in permission
         p_delete = 'd' in permission
+        p_list = 'l' in permission
         p_delete_previous_version = 'x' in permission
         p_tag = 't' in permission
-
-        parsed = cls(read=p_read, add=p_add, create=p_create, write=p_write, delete=p_delete,
-                     delete_previous_version=p_delete_previous_version, tag=p_tag)
+        p_add = 'a' in permission
+        p_create = 'c' in permission
+        p_move = 'm' in permission
+        p_execute = 'e' in permission
+        parsed = cls(read=p_read, write=p_write, delete=p_delete,
+                     delete_previous_version=p_delete_previous_version, tag=p_tag, add=p_add, create=p_create,
+                     list=p_list, move=p_move, execute=p_execute)
 
         return parsed
 
