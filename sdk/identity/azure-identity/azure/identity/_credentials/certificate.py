3d9dfa97cad23bea0e05162ec5e875e54e829447
Charles Lowell <chlowe@microsoft.com>
2020-11-03 16:58:34 -0800

    Validate tenant IDs used in URLs (#14955)

--- sdk/identity/azure-identity/azure/identity/_credentials/certificate.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/certificate.py
@@ -10,6 +10,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 import six
 
+from .._internal import validate_tenant_id
 from .._internal.client_credential_base import ClientCredentialBase
 
 if TYPE_CHECKING:
@@ -40,6 +41,7 @@ class CertificateCredential(ClientCredentialBase):
 
     def __init__(self, tenant_id, client_id, certificate_path, **kwargs):
         # type: (str, str, str, **Any) -> None
+        validate_tenant_id(tenant_id)
         if not certificate_path:
             raise ValueError(
                 "'certificate_path' must be the path to a PEM file containing an x509 certificate and its private key"
