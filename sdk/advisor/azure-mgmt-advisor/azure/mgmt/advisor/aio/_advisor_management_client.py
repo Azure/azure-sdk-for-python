c8179e3bb4cd197a1bf7890330aac38d1daa59de
00Kai0 <sunkaihuisos@gmail.com>
2020-11-04 10:50:49 +0800

    generate advisor track2 sdk

--- sdk/advisor/azure-mgmt-advisor/azure/mgmt/advisor/aio/_advisor_management_client.py
+++ sdk/advisor/azure-mgmt-advisor/azure/mgmt/advisor/aio/_advisor_management_client.py
@@ -6,16 +6,14 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import TYPE_CHECKING
+from typing import Any, Optional, TYPE_CHECKING
 
-from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
-    from typing import Any, Optional
-
-    from azure.core.credentials import TokenCredential
+    from azure.core.credentials_async import AsyncTokenCredential
 
 from ._configuration import AdvisorManagementClientConfiguration
 from .operations import RecommendationMetadataOperations
@@ -23,24 +21,24 @@ from .operations import ConfigurationsOperations
 from .operations import RecommendationsOperations
 from .operations import Operations
 from .operations import SuppressionsOperations
-from . import models
+from .. import models
 
 
 class AdvisorManagementClient(object):
     """REST APIs for Azure Advisor.
 
     :ivar recommendation_metadata: RecommendationMetadataOperations operations
-    :vartype recommendation_metadata: azure.mgmt.advisor.operations.RecommendationMetadataOperations
+    :vartype recommendation_metadata: azure.mgmt.advisor.aio.operations.RecommendationMetadataOperations
     :ivar configurations: ConfigurationsOperations operations
-    :vartype configurations: azure.mgmt.advisor.operations.ConfigurationsOperations
+    :vartype configurations: azure.mgmt.advisor.aio.operations.ConfigurationsOperations
     :ivar recommendations: RecommendationsOperations operations
-    :vartype recommendations: azure.mgmt.advisor.operations.RecommendationsOperations
+    :vartype recommendations: azure.mgmt.advisor.aio.operations.RecommendationsOperations
     :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.advisor.operations.Operations
+    :vartype operations: azure.mgmt.advisor.aio.operations.Operations
     :ivar suppressions: SuppressionsOperations operations
-    :vartype suppressions: azure.mgmt.advisor.operations.SuppressionsOperations
+    :vartype suppressions: azure.mgmt.advisor.aio.operations.SuppressionsOperations
     :param credential: Credential needed for the client to connect to Azure.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: The Azure subscription ID.
     :type subscription_id: str
     :param str base_url: Service URL
@@ -48,16 +46,15 @@ class AdvisorManagementClient(object):
 
     def __init__(
         self,
-        credential,  # type: "TokenCredential"
-        subscription_id,  # type: str
-        base_url=None,  # type: Optional[str]
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> None
+        credential: "AsyncTokenCredential",
+        subscription_id: str,
+        base_url: Optional[str] = None,
+        **kwargs: Any
+    ) -> None:
         if not base_url:
             base_url = 'https://management.azure.com'
         self._config = AdvisorManagementClientConfiguration(credential, subscription_id, **kwargs)
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -75,15 +72,12 @@ class AdvisorManagementClient(object):
         self.suppressions = SuppressionsOperations(
             self._client, self._config, self._serialize, self._deserialize)
 
-    def close(self):
-        # type: () -> None
-        self._client.close()
+    async def close(self) -> None:
+        await self._client.close()
 
-    def __enter__(self):
-        # type: () -> AdvisorManagementClient
-        self._client.__enter__()
+    async def __aenter__(self) -> "AdvisorManagementClient":
+        await self._client.__aenter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
-        self._client.__exit__(*exc_details)
+    async def __aexit__(self, *exc_details) -> None:
+        await self._client.__aexit__(*exc_details)
