239addb683e33dab3125130c959559f55d463fcf
McCoy Patiño <mcpatino@microsoft.com>
2020-11-05 13:18:40 -0800

    Apply feedback

--- sdk/identity/azure-identity/azure/identity/_credentials/azure_arc.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/azure_arc.py
@@ -39,7 +39,7 @@ class AzureArcCredential(GetTokenMixin):
         # type: (**Any) -> None
         super(AzureArcCredential, self).__init__()
 
-        client_args = _get_client_args(self, **kwargs)
+        client_args = _get_client_args(**kwargs)
         if client_args:
             self._client = ManagedIdentityClient(**client_args)
         else:
@@ -62,22 +62,22 @@ class AzureArcCredential(GetTokenMixin):
         return self._client.request_token(*scopes, **kwargs)
 
 
-def _get_policies(credential, config, **kwargs):
-    # type: (AzureArcCredential, Configuration, **Any) -> List[PolicyType]
+def _get_policies(config, **kwargs):
+    # type: (Configuration, **Any) -> List[PolicyType]
     return [
         HeadersPolicy(**kwargs),
         UserAgentPolicy(base_user_agent=USER_AGENT, **kwargs),
         config.proxy_policy,
         config.retry_policy,
+        ArcChallengeAuthPolicy(),
         NetworkTraceLoggingPolicy(**kwargs),
         DistributedTracingPolicy(**kwargs),
         HttpLoggingPolicy(**kwargs),
-        ArcChallengeAuthPolicy(credential),
     ]
 
 
-def _get_client_args(credential, **kwargs):
-    # type: (AzureArcCredential, **Any) -> Optional[dict]
+def _get_client_args(**kwargs):
+    # type: (**Any) -> Optional[dict]
     identity_config = kwargs.pop("_identity_config", None) or {}
 
     url = os.environ.get(EnvironmentVariables.IDENTITY_ENDPOINT)
@@ -90,7 +90,8 @@ def _get_client_args(credential, **kwargs):
     return dict(
         kwargs,
         _identity_config=identity_config,
-        policies=_get_policies(credential, config),
+        base_headers={"Metadata": "true"},
+        policies=_get_policies(config),
         request_factory=functools.partial(_get_request, url),
     )
 
@@ -102,34 +103,21 @@ def _get_request(url, scope, identity_config):
     return request
 
 
-def _update_headers(request, secret_key=None):
-    # type: (PipelineRequest, str) -> PipelineRequest
-    request.http_request.headers = {"Metadata": "true"}
-    if secret_key:
-        request.http_request.headers["Authorization"] = "Basic {}".format(secret_key)
-
-    return request
-
-
 def _get_secret_key(response):
     # type: (PipelineResponse) -> str
     # expecting header containing path to secret key file
     header = response.http_response.headers.get("Www-Authenticate")
-    if header is None:
+    if not header:
         raise CredentialUnavailableError(message="Did not receive a value from Www-Authenticate header")
-
-    secret_key = None
 
     # expecting header with structure like 'Basic realm=<file path>'
     key_file = header.split("=")[1]
     with open(key_file, "r") as file:
         try:
-            secret_key = file.read()
+            return file.read()
         except Exception as error:  # pylint:disable=broad-except
             # user is expected to have obtained read permission prior to this being called
             raise CredentialUnavailableError(message="Could not read file {} contents: {}".format(key_file, error))
-
-    return secret_key
 
 
 class ArcChallengeAuthPolicyBase(object):
@@ -137,36 +125,23 @@ class ArcChallengeAuthPolicyBase(object):
 
     def __init__(self, **kwargs):
         # type: (**Any) -> None
-        self._token = None  # type: Optional[AccessToken]
         super(ArcChallengeAuthPolicyBase, self).__init__(**kwargs)
-
-    @property
-    def _need_new_token(self):
-        # type: () -> bool
-        return not self._token or self._token.expires_on - time.time() < 300
 
 
 class ArcChallengeAuthPolicy(ArcChallengeAuthPolicyBase, HTTPPolicy):
     """Policy for handling Azure Arc's challenge authentication"""
 
-    def __init__(self, credential, **kwargs):
-        # type: (AzureArcCredential, **Any) -> None
-        self._credential = credential
-        self._scope = None
-        self._secret_key = None
+    def __init__(self, **kwargs):
+        # type: (**Any) -> None
         super(ArcChallengeAuthPolicy, self).__init__(**kwargs)
 
     def send(self, request):
         # type: (PipelineRequest) -> HttpResponse
-        if self._scope is None:
-            self._scope = request.http_request.query.get("resource")
-
-        request = _update_headers(request)
         response = self.next.send(request)
 
         if response.http_response.status_code == 401:
-            self._secret_key = _get_secret_key(response)
-            request = _update_headers(request, secret_key=self._secret_key)
+            secret_key = _get_secret_key(response)
+            request.http_request.headers["Authorization"] = "Basic {}".format(secret_key)
             response = self.next.send(request)
 
         return response
