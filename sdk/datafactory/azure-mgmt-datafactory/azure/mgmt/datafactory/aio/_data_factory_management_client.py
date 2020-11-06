d409e995c57bde2fac323ed9cf58d65c86fe8478
00Kai0 <sunkaihuisos@gmail.com>
2020-11-06 15:08:35 +0800

    generate datafactory track2 sdk

--- sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/aio/_data_factory_management_client.py
+++ sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/aio/_data_factory_management_client.py
@@ -6,16 +6,14 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import TYPE_CHECKING
+from typing import Any, Optional, TYPE_CHECKING
 
-from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
-    from typing import Any, Optional
-
-    from azure.core.credentials import TokenCredential
+    from azure.core.credentials_async import AsyncTokenCredential
 
 from ._configuration import DataFactoryManagementClientConfiguration
 from .operations import Operations
@@ -35,48 +33,48 @@ from .operations import DataFlowsOperations
 from .operations import DataFlowDebugSessionOperations
 from .operations import ManagedVirtualNetworksOperations
 from .operations import ManagedPrivateEndpointsOperations
-from . import models
+from .. import models
 
 
 class DataFactoryManagementClient(object):
     """The Azure Data Factory V2 management API provides a RESTful set of web services that interact with Azure Data Factory V2 services.
 
     :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.datafactory.operations.Operations
+    :vartype operations: azure.mgmt.datafactory.aio.operations.Operations
     :ivar factories: FactoriesOperations operations
-    :vartype factories: azure.mgmt.datafactory.operations.FactoriesOperations
+    :vartype factories: azure.mgmt.datafactory.aio.operations.FactoriesOperations
     :ivar exposure_control: ExposureControlOperations operations
-    :vartype exposure_control: azure.mgmt.datafactory.operations.ExposureControlOperations
+    :vartype exposure_control: azure.mgmt.datafactory.aio.operations.ExposureControlOperations
     :ivar integration_runtimes: IntegrationRuntimesOperations operations
-    :vartype integration_runtimes: azure.mgmt.datafactory.operations.IntegrationRuntimesOperations
+    :vartype integration_runtimes: azure.mgmt.datafactory.aio.operations.IntegrationRuntimesOperations
     :ivar integration_runtime_object_metadata: IntegrationRuntimeObjectMetadataOperations operations
-    :vartype integration_runtime_object_metadata: azure.mgmt.datafactory.operations.IntegrationRuntimeObjectMetadataOperations
+    :vartype integration_runtime_object_metadata: azure.mgmt.datafactory.aio.operations.IntegrationRuntimeObjectMetadataOperations
     :ivar integration_runtime_nodes: IntegrationRuntimeNodesOperations operations
-    :vartype integration_runtime_nodes: azure.mgmt.datafactory.operations.IntegrationRuntimeNodesOperations
+    :vartype integration_runtime_nodes: azure.mgmt.datafactory.aio.operations.IntegrationRuntimeNodesOperations
     :ivar linked_services: LinkedServicesOperations operations
-    :vartype linked_services: azure.mgmt.datafactory.operations.LinkedServicesOperations
+    :vartype linked_services: azure.mgmt.datafactory.aio.operations.LinkedServicesOperations
     :ivar datasets: DatasetsOperations operations
-    :vartype datasets: azure.mgmt.datafactory.operations.DatasetsOperations
+    :vartype datasets: azure.mgmt.datafactory.aio.operations.DatasetsOperations
     :ivar pipelines: PipelinesOperations operations
-    :vartype pipelines: azure.mgmt.datafactory.operations.PipelinesOperations
+    :vartype pipelines: azure.mgmt.datafactory.aio.operations.PipelinesOperations
     :ivar pipeline_runs: PipelineRunsOperations operations
-    :vartype pipeline_runs: azure.mgmt.datafactory.operations.PipelineRunsOperations
+    :vartype pipeline_runs: azure.mgmt.datafactory.aio.operations.PipelineRunsOperations
     :ivar activity_runs: ActivityRunsOperations operations
-    :vartype activity_runs: azure.mgmt.datafactory.operations.ActivityRunsOperations
+    :vartype activity_runs: azure.mgmt.datafactory.aio.operations.ActivityRunsOperations
     :ivar triggers: TriggersOperations operations
-    :vartype triggers: azure.mgmt.datafactory.operations.TriggersOperations
+    :vartype triggers: azure.mgmt.datafactory.aio.operations.TriggersOperations
     :ivar trigger_runs: TriggerRunsOperations operations
-    :vartype trigger_runs: azure.mgmt.datafactory.operations.TriggerRunsOperations
+    :vartype trigger_runs: azure.mgmt.datafactory.aio.operations.TriggerRunsOperations
     :ivar data_flows: DataFlowsOperations operations
-    :vartype data_flows: azure.mgmt.datafactory.operations.DataFlowsOperations
+    :vartype data_flows: azure.mgmt.datafactory.aio.operations.DataFlowsOperations
     :ivar data_flow_debug_session: DataFlowDebugSessionOperations operations
-    :vartype data_flow_debug_session: azure.mgmt.datafactory.operations.DataFlowDebugSessionOperations
+    :vartype data_flow_debug_session: azure.mgmt.datafactory.aio.operations.DataFlowDebugSessionOperations
     :ivar managed_virtual_networks: ManagedVirtualNetworksOperations operations
-    :vartype managed_virtual_networks: azure.mgmt.datafactory.operations.ManagedVirtualNetworksOperations
+    :vartype managed_virtual_networks: azure.mgmt.datafactory.aio.operations.ManagedVirtualNetworksOperations
     :ivar managed_private_endpoints: ManagedPrivateEndpointsOperations operations
-    :vartype managed_private_endpoints: azure.mgmt.datafactory.operations.ManagedPrivateEndpointsOperations
+    :vartype managed_private_endpoints: azure.mgmt.datafactory.aio.operations.ManagedPrivateEndpointsOperations
     :param credential: Credential needed for the client to connect to Azure.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: The subscription identifier.
     :type subscription_id: str
     :param str base_url: Service URL
@@ -85,16 +83,15 @@ class DataFactoryManagementClient(object):
 
     def __init__(
         self,
-        credential,  # type: "TokenCredential"
-        subscription_id,  # type: str
-        base_url=None,  # type: Optional[str]
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> None
+        credential: "AsyncTokenCredential",
+        subscription_id: str,
+        base_url: Optional[str] = None,
+        **kwargs: Any
+    ) -> None:
         if not base_url:
             base_url = 'https://management.azure.com'
         self._config = DataFactoryManagementClientConfiguration(credential, subscription_id, **kwargs)
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -136,15 +133,12 @@ class DataFactoryManagementClient(object):
         self.managed_private_endpoints = ManagedPrivateEndpointsOperations(
             self._client, self._config, self._serialize, self._deserialize)
 
-    def close(self):
-        # type: () -> None
-        self._client.close()
+    async def close(self) -> None:
+        await self._client.close()
 
-    def __enter__(self):
-        # type: () -> DataFactoryManagementClient
-        self._client.__enter__()
+    async def __aenter__(self) -> "DataFactoryManagementClient":
+        await self._client.__aenter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
-        self._client.__exit__(*exc_details)
+    async def __aexit__(self, *exc_details) -> None:
+        await self._client.__aexit__(*exc_details)
