ac77e0093456c5b267b944bf59e5858751ceaf67
SDK Automation <sdkautomation@microsoft.com>
2020-11-09 09:49:10 +0000

    Generated from 2b2e2bf8ebc3ab41316ef595bb6a27ea2b0e9689

--- sdk/netapp/azure-mgmt-netapp/azure/mgmt/netapp/operations/_snapshots_operations.py
+++ sdk/netapp/azure-mgmt-netapp/azure/mgmt/netapp/operations/_snapshots_operations.py
@@ -27,7 +27,7 @@ class SnapshotsOperations(object):
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
-    :ivar api_version: Version of the API to be used with the client request. Constant value: "2020-06-01".
+    :ivar api_version: Version of the API to be used with the client request. Constant value: "2020-07-01".
     """
 
     models = models
@@ -37,7 +37,7 @@ class SnapshotsOperations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
-        self.api_version = "2020-06-01"
+        self.api_version = "2020-07-01"
 
         self.config = config
 
