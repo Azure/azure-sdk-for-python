ca233c063975bf29c43e9b5e0fb5ccef3a21c6cb
Yuchao Yan <yuchaoyan@microsoft.com>
2020-11-10 11:59:07 +0800

    version,changelog

--- sdk/securityinsight/azure-mgmt-securityinsight/setup.py
+++ sdk/securityinsight/azure-mgmt-securityinsight/setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages, setup
 
 # Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-mgmt-securityinsight"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_PPRINT_NAME = "SecurityInsight Management"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
@@ -81,8 +81,8 @@ setup(
     ]),
     install_requires=[
         'msrest>=0.5.0',
-        'msrestazure>=0.4.32,<2.0.0',
         'azure-common~=1.1',
+        'azure-mgmt-core>=1.2.0,<2.0.0'
     ],
     extras_require={
         ":python_version<'3.0'": ['azure-mgmt-nspkg'],
