e6b4f08218e44599a2936895199af167263bfaeb
Laurent Mazuel <laurent.mazuel@gmail.com>
2020-11-11 13:21:00 -0800

    More stuff

--- tools/azure-sdk-tools/packaging_tools/generate_script_v2.py
+++ tools/azure-sdk-tools/packaging_tools/generate_script_v2.py
@@ -1,15 +1,39 @@
 import argparse
 import json
+import glob
 import logging
+import os
 from pathlib import Path
+import re
+from subprocess import check_call
 import tempfile
 
 from .swaggertosdk.SwaggerToSdkCore import (
     CONFIG_FILE,
 )
+from azure_devtools.ci_tools.git_tools import get_diff_file_list
 from .generate_sdk import generate
 
 _LOGGER = logging.getLogger(__name__)
+_SDK_FOLDER_RE = re.compile(r"^(sdk/[\w-]+)/(azure[\w-]+)/", re.ASCII)
+
+
+DEFAULT_DEST_FOLDER = "./dist"
+
+def create_package(name, dest_folder=DEFAULT_DEST_FOLDER):
+    # a package will exist in either one, or the other folder. this is why we can resolve both at the same time.
+    absdirs = [os.path.dirname(package) for package in (glob.glob('{}/setup.py'.format(name)) + glob.glob('sdk/*/{}/setup.py'.format(name)))]
+
+    absdirpath = os.path.abspath(absdirs[0])
+    check_call(['python', 'setup.py', 'bdist_wheel', '-d', dest_folder], cwd=absdirpath)
+    check_call(['python', 'setup.py', "sdist", "--format", "zip", '-d', dest_folder], cwd=absdirpath)
+
+
+def get_package_names(sdk_folder):
+    files = get_diff_file_list(sdk_folder)
+    matches = {_SDK_FOLDER_RE.search(f) for f in files}
+    package_names = {match.groups() for match in matches if match is not None}
+    return package_names
 
 
 def main(generate_input, generate_output):
@@ -17,22 +41,53 @@ def main(generate_input, generate_output):
         data = json.load(reader)
 
     spec_folder = data['specFolder']
+    sdk_folder = "."
     input_readme = data["relatedReadmeMdFiles"][0]
 
     relative_path_readme = str(Path(spec_folder, input_readme))
 
     generate(
         CONFIG_FILE,
-        ".",
+        sdk_folder,
         [],
         relative_path_readme,
         spec_folder,
         force_generation=True
     )
+    package_names = get_package_names(sdk_folder)
+    result = {
+        "packages": []
+    }
 
-    result = {}
+    for folder_name, package_name in package_names:
+        package_entry = {}
+        package_entry['packageName'] = package_name
+        package_entry["path"] = [folder_name]
+        package_entry['readmeMd'] = data["relatedReadmeMdFiles"]
+
+        # Changelog
+        package_entry["changelog"] = {
+            "content": "Feature: something \n Breaking Changes: something\n",
+            "hasBreakingChange": True
+        },
+        # Built package
+        create_package(package_name)
+        dist_path = Path(sdk_folder, folder_name, package_name, "dist")
+        package_entry["artifacts"] = [
+            str(dist_path / package_file) for package_file in os.listdir(dist_path)
+        ]
+        # Installation package
+        package_entry["installInstructions"] = {
+            "full": "You can install the use using pip install of the artificats.",
+            "lite": "pip install <package-name>"
+        },
+        package_entry["result"]: "success"
+
+        result['packages'].append(package_entry)
+
     with open(generate_output, "w") as writer:
         json.dump(result, writer)
+
 
 def generate_main():
     """Main method"""
