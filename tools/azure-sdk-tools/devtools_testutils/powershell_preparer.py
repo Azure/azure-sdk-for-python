b17de260efe7b6aa4612e4ab33a068c51af833df
seankane-msft <seankane@microsoft.com>
2020-11-12 12:47:06 -0800

    test resources for tables, powershell preparer

---
+++ tools/azure-sdk-tools/devtools_testutils/powershell_preparer.py
@@ -0,0 +1,80 @@
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import functools
+from dotenv import load_dotenv, find_dotenv
+import os
+from . import AzureMgmtPreparer
+from .resource_testcase import RESOURCE_GROUP_PARAM
+from azure_devtools.scenario_tests.exceptions import AzureTestError
+
+class PowerShellPreparer(AzureMgmtPreparer):
+    def __init__(
+        self, directory,
+        name_prefix='',
+        sku='Standard_LRS', location='westus', kind='StorageV2',
+        parameter_name='storage_account',
+        resource_group_parameter_name=RESOURCE_GROUP_PARAM,
+        disable_recording=True, playback_fake_resource=None,
+        client_kwargs=None,
+        random_name_enabled=False,
+        use_cache=True,
+        **kwargs
+    ):
+        super(PowerShellPreparer, self).__init__(
+            name_prefix, 24, disable_recording=disable_recording,
+            playback_fake_resource=playback_fake_resource,
+            client_kwargs=client_kwargs, random_name_enabled=random_name_enabled
+        )
+        load_dotenv(find_dotenv())
+        self.directory = directory
+        self.fake_values = {}
+        self.real_values = {}
+        self._set_secrets(**kwargs)
+        self._set_mgmt_settings_real_values()
+
+    def _set_secrets(self, **kwargs):
+        keys = kwargs.keys()
+        needed_keys = []
+        for key in keys:
+            if self.directory in key:
+                needed_keys.append(key)
+                self.fake_values[key] = kwargs[key]
+        for key in self.fake_values:
+            kwargs.pop(key)
+
+        self.needed_keys = needed_keys
+
+    def _set_mgmt_settings_real_values(self):
+        os.environ["AZURE_TENANT_ID"] = os.environ["{}_TENANT_ID".format(self.directory.upper())]
+        os.environ["AZURE_CLIENT_ID"] = os.environ["{}_CLIENT_ID".format(self.directory.upper())]
+        os.environ["AZURE_CLIENT_SECRET"] = os.environ["{}_CLIENT_SECRET".format(self.directory.upper())]
+
+    def create_resource(self, name, **kwargs):
+
+        if self.is_live:
+            for key in self.needed_keys:
+
+                scrubbed_value = self.fake_values[key]
+                if scrubbed_value:
+                    self.real_values[key.lower()] = os.environ[key.upper()]
+                    self.test_class_instance.scrubber.register_name_pair(
+                        self.real_values[key.lower()],
+                        scrubbed_value
+                    )
+                else:
+                    template = 'To pass a live ID you must provide the scrubbed value for recordings to \
+                        prevent secrets from being written to files. {} was not given. For example: \
+                            @PowerShellPreparer("schemaregistry", schemaregistry_endpoint="fake_endpoint.servicebus.windows.net")'
+                    raise AzureTestError(template.format(key))
+            return self.real_values
+
+        else:
+            return self.fake_values
+
+    def remove_resource(self, name, **kwargs):
+        pass
+
+SchemaRegistryPowerShellPreparer = functools.partial(PowerShellPreparer, "schemaregistry", schemaregistry_endpoint="fake_resource.servicebus.windows.net", schemaregistry_group="fakegroup")
