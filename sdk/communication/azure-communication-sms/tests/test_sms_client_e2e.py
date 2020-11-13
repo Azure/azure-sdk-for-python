7322a04cb706c9c6515eda0acb8412963743c682
turalf <tufarhad@microsoft.com>
2020-11-13 10:26:46 -0800

    Enable sms tests

--- sdk/communication/azure-communication-sms/tests/test_sms_client_e2e.py
+++ sdk/communication/azure-communication-sms/tests/test_sms_client_e2e.py
@@ -15,9 +15,6 @@ from _shared.testcase import (
     ResponseReplacerProcessor
 )
 
-SKIP_PHONE_NUMBER_TESTS = False
-PHONE_NUMBER_TEST_SKIP_REASON= "Phone Number infra for live tests not ready yet"
-
 class SMSClientTest(CommunicationTestCase):
     def __init__(self, method_name):
         super(SMSClientTest, self).__init__(method_name)
@@ -37,7 +34,6 @@ class SMSClientTest(CommunicationTestCase):
         self.sms_client = SmsClient.from_connection_string(self.connection_str)
 
     @pytest.mark.live_test_only
-    @pytest.mark.skipif(SKIP_PHONE_NUMBER_TESTS, reason=PHONE_NUMBER_TEST_SKIP_REASON)
     def test_send_sms(self):
 
         # calling send() with sms values
