f61277864dd605ed0a6bae9ceaa51ce00917462a
Rakshith Bhyravabhotla <sabhyrav@microsoft.com>
2020-11-14 00:46:37 +0530

    [monitor] README + samples _ ci.yml (#15288)
    * README + minor improvs
    
    * changes
    
    * Update sdk/monitor/microsoft-opentelemetry-exporter-azuremonitor/samples/README.md
    
    * samples readme
    
    * Update sdk/monitor/ci.yml
    
    Co-authored-by: Scott Beddall <45376673+scbedd@users.noreply.github.com>
    
    * Update sdk/monitor/microsoft-opentelemetry-exporter-azuremonitor/README.md
    
    Co-authored-by: Leighton Chen <lechen@microsoft.com>
    
    * Update sdk/monitor/microsoft-opentelemetry-exporter-azuremonitor/README.md
    
    Co-authored-by: Leighton Chen <lechen@microsoft.com>
    
    * readme
    
    * Apply suggestions from code review
    
    Co-authored-by: Leighton Chen <lechen@microsoft.com>
    
    * Update sdk/monitor/microsoft-opentelemetry-exporter-azuremonitor/README.md
    
    * comments
    
    * conn str
    
    Co-authored-by: Scott Beddall <45376673+scbedd@users.noreply.github.com>
    Co-authored-by: Leighton Chen <lechen@microsoft.com>

--- sdk/monitor/microsoft-opentelemetry-exporter-azuremonitor/samples/traces/sample_trace.py
+++ sdk/monitor/microsoft-opentelemetry-exporter-azuremonitor/samples/traces/sample_trace.py
@@ -1,27 +1,30 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-# pylint: disable=import-error
-# pylint: disable=no-member
-# pylint: disable=no-name-in-module
 import os
-import requests
 from opentelemetry import trace
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 
 from microsoft.opentelemetry.exporter.azuremonitor import AzureMonitorSpanExporter
 
+
+# Callback function to add os_type: linux to span properties
+def callback_function(envelope):
+    envelope.data.base_data.properties["os_type"] = "linux"
+    return True
+
+
+exporter = AzureMonitorSpanExporter(
+    connection_string = os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+)
+exporter.add_telemetry_processor(callback_function)
+
 trace.set_tracer_provider(TracerProvider())
 tracer = trace.get_tracer(__name__)
-RequestsInstrumentor().instrument()
-span_processor = BatchExportSpanProcessor(
-    AzureMonitorSpanExporter(
-        connection_string = os.environ["AZURE_MONITOR_CONNECTION_STRING"]
-    )
-)
+span_processor = BatchExportSpanProcessor(exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
 
-response = requests.get(url="http://127.0.0.1:8080/")
+with tracer.start_as_current_span("hello"):
+    print("Hello, World!")
 
 input("Press any key to exit...")
