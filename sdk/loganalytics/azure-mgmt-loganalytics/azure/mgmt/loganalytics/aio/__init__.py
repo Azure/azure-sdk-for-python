769d0c654bfebd3fda57dc9304e4a51fdee9cc99
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2020-11-16 17:22:28 +0800

    generate loganalytics track2 version (#15337)

--- sdk/loganalytics/azure-mgmt-loganalytics/azure/mgmt/loganalytics/aio/__init__.py
+++ sdk/loganalytics/azure-mgmt-loganalytics/azure/mgmt/loganalytics/aio/__init__.py
@@ -7,13 +7,4 @@
 # --------------------------------------------------------------------------
 
 from ._operational_insights_management_client import OperationalInsightsManagementClient
-from ._version import VERSION
-
-__version__ = VERSION
 __all__ = ['OperationalInsightsManagementClient']
-
-try:
-    from ._patch import patch_sdk  # type: ignore
-    patch_sdk()
-except ImportError:
-    pass
