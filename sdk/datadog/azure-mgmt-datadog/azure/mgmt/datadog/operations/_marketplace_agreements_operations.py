1721819a1d3a67191363d754f14adf8afb63a66c
Yuchao Yan <yuchaoyan@microsoft.com>
2020-11-17 17:51:52 +0800

    test,version,CHANGELOG

--- sdk/datadog/azure-mgmt-datadog/azure/mgmt/datadog/operations/_marketplace_agreements_operations.py
+++ sdk/datadog/azure-mgmt-datadog/azure/mgmt/datadog/operations/_marketplace_agreements_operations.py
@@ -23,8 +23,8 @@ if TYPE_CHECKING:
     T = TypeVar('T')
     ClsType = Optional[Callable[[PipelineResponse[HttpRequest, HttpResponse], T, Dict[str, Any]], Any]]
 
-class MonitoredResourcesOperations(object):
-    """MonitoredResourcesOperations operations.
+class MarketplaceAgreementsOperations(object):
+    """MarketplaceAgreementsOperations operations.
 
     You should not instantiate this class directly. Instead, you should create a Client instance that
     instantiates it for you and attaches it as an attribute.
@@ -47,31 +47,23 @@ class MonitoredResourcesOperations(object):
 
     def list(
         self,
-        resource_group_name,  # type: str
-        monitor_name,  # type: str
         **kwargs  # type: Any
     ):
-        # type: (...) -> Iterable["_models.MonitoredResourceListResponse"]
-        """List the resources currently being monitored by the Datadog monitor resource.
+        # type: (...) -> Iterable["_models.DatadogAgreementResourceListResponse"]
+        """List Datadog marketplace agreements in the subscription.
 
-        List the resources currently being monitored by the Datadog monitor resource.
+        List Datadog marketplace agreements in the subscription.
 
-        :param resource_group_name: The name of the resource group to which the Datadog resource
-         belongs.
-        :type resource_group_name: str
-        :param monitor_name: Monitor resource name.
-        :type monitor_name: str
         :keyword callable cls: A custom type or function that will be passed the direct response
-        :return: An iterator like instance of either MonitoredResourceListResponse or the result of cls(response)
-        :rtype: ~azure.core.paging.ItemPaged[~microsoft_datadog_client.models.MonitoredResourceListResponse]
+        :return: An iterator like instance of either DatadogAgreementResourceListResponse or the result of cls(response)
+        :rtype: ~azure.core.paging.ItemPaged[~microsoft_datadog_client.models.DatadogAgreementResourceListResponse]
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["_models.MonitoredResourceListResponse"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.DatadogAgreementResourceListResponse"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2020-02-01-preview"
         accept = "application/json"
 
         def prepare_request(next_link=None):
@@ -84,15 +76,12 @@ class MonitoredResourcesOperations(object):
                 url = self.list.metadata['url']  # type: ignore
                 path_format_arguments = {
                     'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
-                    'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
-                    'monitorName': self._serialize.url("monitor_name", monitor_name, 'str'),
                 }
                 url = self._client.format_url(url, **path_format_arguments)
                 # Construct parameters
                 query_parameters = {}  # type: Dict[str, Any]
-                query_parameters['api-version'] = self._serialize.query("api_version", api_version, 'str')
 
-                request = self._client.post(url, query_parameters, header_parameters)
+                request = self._client.get(url, query_parameters, header_parameters)
             else:
                 url = next_link
                 query_parameters = {}  # type: Dict[str, Any]
@@ -100,7 +89,7 @@ class MonitoredResourcesOperations(object):
             return request
 
         def extract_data(pipeline_response):
-            deserialized = self._deserialize('MonitoredResourceListResponse', pipeline_response)
+            deserialized = self._deserialize('DatadogAgreementResourceListResponse', pipeline_response)
             list_of_elem = deserialized.value
             if cls:
                 list_of_elem = cls(list_of_elem)
@@ -122,4 +111,67 @@ class MonitoredResourcesOperations(object):
         return ItemPaged(
             get_next, extract_data
         )
-    list.metadata = {'url': '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Datadog/monitors/{monitorName}/listMonitoredResources'}  # type: ignore
+    list.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.Datadog/agreements'}  # type: ignore
+
+    def create(
+        self,
+        body=None,  # type: Optional["_models.DatadogAgreementResource"]
+        **kwargs  # type: Any
+    ):
+        # type: (...) -> "_models.DatadogAgreementResource"
+        """Create Datadog marketplace agreement in the subscription.
+
+        Create Datadog marketplace agreement in the subscription.
+
+        :param body:
+        :type body: ~microsoft_datadog_client.models.DatadogAgreementResource
+        :keyword callable cls: A custom type or function that will be passed the direct response
+        :return: DatadogAgreementResource, or the result of cls(response)
+        :rtype: ~microsoft_datadog_client.models.DatadogAgreementResource
+        :raises: ~azure.core.exceptions.HttpResponseError
+        """
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.DatadogAgreementResource"]
+        error_map = {
+            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
+        }
+        error_map.update(kwargs.pop('error_map', {}))
+        content_type = kwargs.pop("content_type", "application/json")
+        accept = "application/json"
+
+        # Construct URL
+        url = self.create.metadata['url']  # type: ignore
+        path_format_arguments = {
+            'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
+        }
+        url = self._client.format_url(url, **path_format_arguments)
+
+        # Construct parameters
+        query_parameters = {}  # type: Dict[str, Any]
+
+        # Construct headers
+        header_parameters = {}  # type: Dict[str, Any]
+        header_parameters['Content-Type'] = self._serialize.header("content_type", content_type, 'str')
+        header_parameters['Accept'] = self._serialize.header("accept", accept, 'str')
+
+        body_content_kwargs = {}  # type: Dict[str, Any]
+        if body is not None:
+            body_content = self._serialize.body(body, 'DatadogAgreementResource')
+        else:
+            body_content = None
+        body_content_kwargs['content'] = body_content
+        request = self._client.put(url, query_parameters, header_parameters, **body_content_kwargs)
+        pipeline_response = self._client._pipeline.run(request, stream=False, **kwargs)
+        response = pipeline_response.http_response
+
+        if response.status_code not in [200]:
+            map_error(status_code=response.status_code, response=response, error_map=error_map)
+            error = self._deserialize(_models.ResourceProviderDefaultErrorResponse, response)
+            raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
+
+        deserialized = self._deserialize('DatadogAgreementResource', pipeline_response)
+
+        if cls:
+            return cls(pipeline_response, deserialized, {})
+
+        return deserialized
+    create.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.Datadog/agreements/default'}  # type: ignore
