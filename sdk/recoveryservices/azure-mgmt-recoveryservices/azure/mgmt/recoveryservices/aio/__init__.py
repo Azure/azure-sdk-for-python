f4a80ace332eb44c6829ab1205645e99b4dcf819
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2020-11-18 10:12:37 +0800

    [T2] Recoveryservices (#15198)

--- sdk/recoveryservices/azure-mgmt-recoveryservices/azure/mgmt/recoveryservices/aio/__init__.py
+++ sdk/recoveryservices/azure-mgmt-recoveryservices/azure/mgmt/recoveryservices/aio/__init__.py
@@ -7,13 +7,4 @@
 # --------------------------------------------------------------------------
 
 from ._recovery_services_client import RecoveryServicesClient
-from ._version import VERSION
-
-__version__ = VERSION
 __all__ = ['RecoveryServicesClient']
-
-try:
-    from ._patch import patch_sdk  # type: ignore
-    patch_sdk()
-except ImportError:
-    pass
