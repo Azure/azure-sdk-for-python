1e34379af311875c2c2c81b8cb098a19e0625abb
msyyc <70930885+msyyc@users.noreply.github.com>
2020-11-18 14:35:30 +0800

    test,version,CHANGELOG (#14838)

--- sdk/consumption/azure-mgmt-consumption/azure/mgmt/consumption/aio/__init__.py
+++ sdk/consumption/azure-mgmt-consumption/azure/mgmt/consumption/aio/__init__.py
@@ -7,13 +7,4 @@
 # --------------------------------------------------------------------------
 
 from ._consumption_management_client import ConsumptionManagementClient
-from ._version import VERSION
-
-__version__ = VERSION
 __all__ = ['ConsumptionManagementClient']
-
-try:
-    from ._patch import patch_sdk  # type: ignore
-    patch_sdk()
-except ImportError:
-    pass
