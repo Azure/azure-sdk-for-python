2a60dd73c4421690b7a7e8de349a3a7dbfdb1343
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2020-11-18 18:17:25 -0500

    [text analytics] add sample stories and improve documents (#15429)

--- sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_detect_language_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_detect_language_async.py
@@ -13,6 +13,10 @@ DESCRIPTION:
     This sample demonstrates how to detect language in a batch of different
     documents.
 
+    In this sample, we own a hotel with a lot of international clientele. We
+    are looking to catalog the reviews we have for our hotel by language, so
+    we can translate these reviews into English.
+
 USAGE:
     python sample_detect_language_async.py
 
@@ -28,6 +32,11 @@ import asyncio
 class DetectLanguageSampleAsync(object):
 
     async def detect_language_async(self):
+        print(
+            "In this sample we own a hotel with customers from all around the globe. We want to eventually "
+            "translate these reviews into English so our manager can read them. However, we first need to know which language "
+            "they are in for more accurate translation. This is the step we will be covering in this sample\n"
+        )
         # [START detect_language_async]
         from azure.core.credentials import AzureKeyCredential
         from azure.ai.textanalytics.aio import TextAnalyticsClient
@@ -37,24 +46,37 @@ class DetectLanguageSampleAsync(object):
 
         text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
         documents = [
-            "This document is written in English.",
-            "Este es un document escrito en Español.",
-            "这是一个用中文写的文件",
-            "Dies ist ein Dokument in deutsche Sprache.",
-            "Detta är ett dokument skrivet på engelska."
+            """
+            The concierge Paulette was extremely helpful. Sadly when we arrived the elevator was broken, but with Paulette's help we barely noticed this inconvenience.
+            She arranged for our baggage to be brought up to our room with no extra charge and gave us a free meal to refurbish all of the calories we lost from
+            walking up the stairs :). Can't say enough good things about my experience!
+            """,
+            """
+            最近由于工作压力太大，我们决定去富酒店度假。那儿的温泉实在太舒服了，我跟我丈夫都完全恢复了工作前的青春精神！加油！
+            """
         ]
         async with text_analytics_client:
             result = await text_analytics_client.detect_language(documents)
 
-        for idx, doc in enumerate(result):
-            if not doc.is_error:
-                print("Document text: {}".format(documents[idx]))
-                print("Language detected: {}".format(doc.primary_language.name))
-                print("ISO6391 name: {}".format(doc.primary_language.iso6391_name))
-                print("Confidence score: {}\n".format(doc.primary_language.confidence_score))
+        reviewed_docs = [doc for doc in result if not doc.is_error]
+
+        print("Let's see what language each review is in!")
+
+        for idx, doc in enumerate(reviewed_docs):
+            print("Review #{} is in '{}', which has ISO639-1 name '{}'\n".format(
+                idx, doc.primary_language.name, doc.primary_language.iso6391_name
+            ))
             if doc.is_error:
                 print(doc.id, doc.error)
         # [END detect_language_async]
+        print(
+            "When actually storing the reviews, we want to map the review to their ISO639-1 name "
+            "so everything is more standardized"
+        )
+
+        review_to_language = {}
+        for idx, doc in enumerate(reviewed_docs):
+            review_to_language[documents[idx]] = doc.primary_language.iso6391_name
 
 
 async def main():
