1e24bbd1592e5970876a1ef6bbfa63f3a5f2f598
SDKAuto <sdkautomation@microsoft.com>
2020-11-25 00:41:33 +0000

    CodeGen from PR 11627 in Azure/azure-rest-api-specs [Hub Generated] Review request for Microsoft.Logic to add version stable/2019-05-01 (#11627)
    * Adding integration service environment encryption configuration details to Logic spec
    
    * Correcting json format errors
    
    * Correcting json formats
    
    * Updating examples
    
    * Fixing encryption key properties definition
    
    Co-authored-by: Rama Krishna Rayudu <rarayudu@microsoft.com>

--- sdk/logic/azure-mgmt-logic/azure/mgmt/logic/operations/__init__.py
+++ sdk/logic/azure-mgmt-logic/azure/mgmt/logic/operations/__init__.py
@@ -9,28 +9,33 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-from .workflows_operations import WorkflowsOperations
-from .workflow_versions_operations import WorkflowVersionsOperations
-from .workflow_triggers_operations import WorkflowTriggersOperations
-from .workflow_version_triggers_operations import WorkflowVersionTriggersOperations
-from .workflow_trigger_histories_operations import WorkflowTriggerHistoriesOperations
-from .workflow_runs_operations import WorkflowRunsOperations
-from .workflow_run_actions_operations import WorkflowRunActionsOperations
-from .workflow_run_action_repetitions_operations import WorkflowRunActionRepetitionsOperations
-from .workflow_run_action_repetitions_request_histories_operations import WorkflowRunActionRepetitionsRequestHistoriesOperations
-from .workflow_run_action_request_histories_operations import WorkflowRunActionRequestHistoriesOperations
-from .workflow_run_action_scope_repetitions_operations import WorkflowRunActionScopeRepetitionsOperations
-from .workflow_run_operations import WorkflowRunOperations
-from .integration_accounts_operations import IntegrationAccountsOperations
-from .integration_account_assemblies_operations import IntegrationAccountAssembliesOperations
-from .integration_account_batch_configurations_operations import IntegrationAccountBatchConfigurationsOperations
-from .integration_account_schemas_operations import IntegrationAccountSchemasOperations
-from .integration_account_maps_operations import IntegrationAccountMapsOperations
-from .integration_account_partners_operations import IntegrationAccountPartnersOperations
-from .integration_account_agreements_operations import IntegrationAccountAgreementsOperations
-from .integration_account_certificates_operations import IntegrationAccountCertificatesOperations
-from .integration_account_sessions_operations import IntegrationAccountSessionsOperations
-from .operations import Operations
+from ._workflows_operations import WorkflowsOperations
+from ._workflow_versions_operations import WorkflowVersionsOperations
+from ._workflow_triggers_operations import WorkflowTriggersOperations
+from ._workflow_version_triggers_operations import WorkflowVersionTriggersOperations
+from ._workflow_trigger_histories_operations import WorkflowTriggerHistoriesOperations
+from ._workflow_runs_operations import WorkflowRunsOperations
+from ._workflow_run_actions_operations import WorkflowRunActionsOperations
+from ._workflow_run_action_repetitions_operations import WorkflowRunActionRepetitionsOperations
+from ._workflow_run_action_repetitions_request_histories_operations import WorkflowRunActionRepetitionsRequestHistoriesOperations
+from ._workflow_run_action_request_histories_operations import WorkflowRunActionRequestHistoriesOperations
+from ._workflow_run_action_scope_repetitions_operations import WorkflowRunActionScopeRepetitionsOperations
+from ._workflow_run_operations import WorkflowRunOperations
+from ._integration_accounts_operations import IntegrationAccountsOperations
+from ._integration_account_assemblies_operations import IntegrationAccountAssembliesOperations
+from ._integration_account_batch_configurations_operations import IntegrationAccountBatchConfigurationsOperations
+from ._integration_account_schemas_operations import IntegrationAccountSchemasOperations
+from ._integration_account_maps_operations import IntegrationAccountMapsOperations
+from ._integration_account_partners_operations import IntegrationAccountPartnersOperations
+from ._integration_account_agreements_operations import IntegrationAccountAgreementsOperations
+from ._integration_account_certificates_operations import IntegrationAccountCertificatesOperations
+from ._integration_account_sessions_operations import IntegrationAccountSessionsOperations
+from ._integration_service_environments_operations import IntegrationServiceEnvironmentsOperations
+from ._integration_service_environment_skus_operations import IntegrationServiceEnvironmentSkusOperations
+from ._integration_service_environment_network_health_operations import IntegrationServiceEnvironmentNetworkHealthOperations
+from ._integration_service_environment_managed_apis_operations import IntegrationServiceEnvironmentManagedApisOperations
+from ._integration_service_environment_managed_api_operations import IntegrationServiceEnvironmentManagedApiOperations
+from ._operations import Operations
 
 __all__ = [
     'WorkflowsOperations',
@@ -54,5 +59,10 @@ __all__ = [
     'IntegrationAccountAgreementsOperations',
     'IntegrationAccountCertificatesOperations',
     'IntegrationAccountSessionsOperations',
+    'IntegrationServiceEnvironmentsOperations',
+    'IntegrationServiceEnvironmentSkusOperations',
+    'IntegrationServiceEnvironmentNetworkHealthOperations',
+    'IntegrationServiceEnvironmentManagedApisOperations',
+    'IntegrationServiceEnvironmentManagedApiOperations',
     'Operations',
 ]
