2b4d17709d47732ec7acef76d1a0e3f691fc9952
antisch <antisch@microsoft.com>
2020-12-02 09:39:54 +1300

    Optional client sharing

--- sdk/storage/azure-storage-file-share/tests/perfstress_tests/T1_legacy_tests/_test_base.py
+++ sdk/storage/azure-storage-file-share/tests/perfstress_tests/T1_legacy_tests/_test_base.py
@@ -11,15 +11,18 @@ from azure_devtools.perfstress_tests import PerfStressTest
 from azure.storage.file import FileService
 
 class _LegacyServiceTest(PerfStressTest):
+    service_client = None
+    async_service_client = None
 
     def __init__(self, arguments):
         super().__init__(arguments)
         connection_string = self.get_from_env("AZURE_STORAGE_CONNECTION_STRING")
-        self.service_client = FileService(connection_string=connection_string)
-        if self.args.max_range_size:
-            _LegacyServiceTest.service_client.MAX_RANGE_SIZE = self.args.max_range_size
+        if not _LegacyServiceTest.service_client or self.args.no_client_share:
+            _LegacyServiceTest.service_client = FileService(connection_string=connection_string)
+            if self.args.max_range_size:
+                _LegacyServiceTest.service_client.MAX_RANGE_SIZE = self.args.max_range_size
         self.async_service_client = None
-
+        self.service_client = _LegacyServiceTest.service_client
 
     @staticmethod
     def add_arguments(parser):
@@ -27,6 +30,7 @@ class _LegacyServiceTest(PerfStressTest):
         parser.add_argument('-r', '--max-range-size', nargs='?', type=int, help='Maximum size of data uploading in single HTTP PUT. Defaults to 4*1024*1024', default=4*1024*1024)
         parser.add_argument('-c', '--max-concurrency', nargs='?', type=int, help='Maximum number of concurrent threads used for data transfer. Defaults to 1', default=1)
         parser.add_argument('-s', '--size', nargs='?', type=int, help='Size of data to transfer.  Default is 10240.', default=10240)
+        parser.add_argument('--no-client-share', action='store_true', help='Create one ServiceClient per test instance.  Default is to share a single ServiceClient.', default=False)
 
 
 class _LegacyShareTest(_LegacyServiceTest):
