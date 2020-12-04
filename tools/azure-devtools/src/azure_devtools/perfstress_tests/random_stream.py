96ad9afa3ff1a600da527cbefc17b44de4e15057
annatisch <antisch@microsoft.com>
2020-12-04 13:53:39 +1300

    [Perfstress] Added framework (#15510)
    * Migrated perf framework
    
    * Updated framework
    
    * Added T2 File tests
    
    * Added legacy tests
    
    * Fixed headers and imports
    
    * Fix legacy test setup
    
    * Python naming refactor
    
    * Renamed directory
    
    * Separated T1 tests
    
    * Added env var utility
    
    * Fixed concurrency
    
    * Use random bytes for data
    
    * Cleaned up some args
    
    * Added system perf tests
    
    * Support system test dependencies
    
    * Optional client sharing
    
    * Removed fileshare tests
    
    * Fixed system tests

---
+++ tools/azure-devtools/src/azure_devtools/perfstress_tests/random_stream.py
@@ -0,0 +1,35 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import os
+
+def get_random_bytes(buffer_length):
+    return os.urandom(buffer_length)
+
+
+class RandomStream:
+    def __init__(self, length, initial_buffer_length=1024*1024):
+        self._base_data = get_random_bytes(initial_buffer_length)
+        self._base_data_length = initial_buffer_length
+        self._position = 0
+        self._remaining = length
+
+    def read(self, size=None):
+        if self._remaining == 0:
+            return None
+
+        if size is None:
+            e = self._base_data_length
+        else:
+            e = size
+        e = min(e, self._remaining)
+        if e > self._base_data_length:
+            self._base_data = get_random_bytes(e)
+            self._base_data_length = e
+        self._remaining = self._remaining - e
+        return self._base_data[:e]
+
+    def remaining(self):
+        return self._remaining
