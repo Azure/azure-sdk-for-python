7993cfb44d4da0b7bd333d475087acb6fdbec18a
hivyas <hivyas@microsoft.com>
2020-12-07 18:59:48 -0800

    updating swagger and import statement

--- sdk/media/azure-media-livevideoanalytics-edge/README.md
+++ sdk/media/azure-media-livevideoanalytics-edge/README.md
@@ -23,6 +23,9 @@ pip install azure-media-livevideoanalytics-edge
 * Python 2.7, or 3.5 or later is required to use this package.
 * You need an active [Azure subscription][azure_sub], and a [IoT device connection string][iot_device_connection_string] to use this package.
 
+| SDK  | LVA Edge Module  |
+|---|---|
+| 1.0.0b1  | 2.0  |
 ### Creating a graph topology and making requests
 Please visit the [Examples](#examples) for starter code
 ## Key concepts
--- sdk/media/azure-media-livevideoanalytics-edge/azure/media/livevideoanalytics/edge/__init__.py
+++ sdk/media/azure-media-livevideoanalytics-edge/azure/media/livevideoanalytics/edge/__init__.py
@@ -1,7 +1,68 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)  # type: ignore
-from azure.media.livevideoanalytics.edge._generated.models import (MediaGraphTopologySetRequestBody,
-MediaGraphTopologySetRequest, MediaGraphInstanceSetRequest, MediaGraphInstanceSetRequestBody)
+#from azure.media.livevideoanalytics.edge._generated.models import (MediaGraphTopologySetRequestBody,
+#MediaGraphTopologySetRequest, MediaGraphInstanceSetRequest, MediaGraphInstanceSetRequestBody)
 
+from ._generated.models import *
+
+__all__ = [
+    "MethodRequest",
+    "ItemNonSetRequestBase",
+    "MediaGraphSink"
+    "MediaGraphAssetSink",
+    "MediaGraphCertificateSource",
+    "MediaGraphProcessor",
+    "MediaGraphExtensionProcessorBase",
+    "MediaGraphCognitiveServicesVisionExtension",
+    "MediaGraphCredentials",
+    "MediaGraphEndpoint",
+    "MediaGraphFileSink",
+    "MediaGraphGrpcExtension",
+    "MediaGraphGrpcExtensionDataTransfer",
+    "MediaGraphHttpExtension",
+    "MediaGraphHttpHeaderCredentials",
+    "MediaGraphImage",
+    "MediaGraphImageFormat",
+    "MediaGraphImageFormatBmp",
+    "MediaGraphImageFormatJpeg",
+    "MediaGraphImageFormatPng",
+    "MediaGraphImageFormatRaw",
+    "MediaGraphImageScale",
+    "MediaGraphInstance",
+    "MediaGraphInstanceActivateRequest",
+    "MediaGraphInstanceCollection",
+    "MediaGraphInstanceDeActivateRequest",
+    "MediaGraphInstanceDeleteRequest",
+    "MediaGraphInstanceGetRequest",
+    "MediaGraphInstanceListRequest",
+    "MediaGraphInstanceProperties",
+    "MediaGraphInstanceSetRequest",
+    "MediaGraphInstanceSetRequestBody",
+    "MediaGraphIoTHubMessageSink",
+    "MediaGraphSource",
+    "MediaGraphIoTHubMessageSource",
+    "MediaGraphMotionDetectionProcessor",
+    "MediaGraphNodeInput",
+    "MediaGraphOutputSelector",
+    "MediaGraphParameterDeclaration",
+    "MediaGraphParameterDefinition",
+    "MediaGraphPemCertificateList",
+    "MediaGraphRtspSource",
+    "MediaGraphSamplingOptions",
+    "MediaGraphSignalGateProcessor",
+    "MediaGraphSystemData",
+    "MediaGraphTlsEndpoint",
+    "MediaGraphTlsValidationOptions",
+    "MediaGraphTopology",
+    "MediaGraphTopologyCollection",
+    "MediaGraphTopologyDeleteRequest",
+    "MediaGraphTopologyGetRequest",
+    "MediaGraphTopologyListRequest",
+    "MediaGraphTopologyProperties",
+    "MediaGraphTopologySetRequest",
+    "MediaGraphTopologySetRequestBody",
+    "MediaGraphUnsecuredEndpoint",
+    "MediaGraphUsernamePasswordCredentials"
+]
 def _OverrideTopologySetRequestSerialize(self):
     graph_body = MediaGraphTopologySetRequestBody(name=self.graph.name)
     graph_body.system_data = self.graph.system_data
