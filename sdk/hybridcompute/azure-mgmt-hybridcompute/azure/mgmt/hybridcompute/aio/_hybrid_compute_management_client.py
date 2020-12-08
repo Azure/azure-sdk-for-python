f3b4fcd755a44958f638d8df9cf7098e406b0d58
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2020-12-08 11:27:49 +0800

    generate hybridcompute track2 sdk (#15658)

--- sdk/hybridcompute/azure-mgmt-hybridcompute/azure/mgmt/hybridcompute/aio/_hybrid_compute_management_client.py
+++ sdk/hybridcompute/azure-mgmt-hybridcompute/azure/mgmt/hybridcompute/aio/_hybrid_compute_management_client.py
@@ -6,35 +6,33 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import TYPE_CHECKING
+from typing import Any, Optional, TYPE_CHECKING
 
-from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
-    from typing import Any, Optional
-
-    from azure.core.credentials import TokenCredential
+    from azure.core.credentials_async import AsyncTokenCredential
 
 from ._configuration import HybridComputeManagementClientConfiguration
 from .operations import MachinesOperations
 from .operations import MachineExtensionsOperations
 from .operations import Operations
-from . import models
+from .. import models
 
 
 class HybridComputeManagementClient(object):
     """The Hybrid Compute Management Client.
 
     :ivar machines: MachinesOperations operations
-    :vartype machines: azure.mgmt.hybridcompute.operations.MachinesOperations
+    :vartype machines: azure.mgmt.hybridcompute.aio.operations.MachinesOperations
     :ivar machine_extensions: MachineExtensionsOperations operations
-    :vartype machine_extensions: azure.mgmt.hybridcompute.operations.MachineExtensionsOperations
+    :vartype machine_extensions: azure.mgmt.hybridcompute.aio.operations.MachineExtensionsOperations
     :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.hybridcompute.operations.Operations
+    :vartype operations: azure.mgmt.hybridcompute.aio.operations.Operations
     :param credential: Credential needed for the client to connect to Azure.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: Subscription credentials which uniquely identify Microsoft Azure subscription. The subscription ID forms part of the URI for every service call.
     :type subscription_id: str
     :param str base_url: Service URL
@@ -43,16 +41,15 @@ class HybridComputeManagementClient(object):
 
     def __init__(
         self,
-        credential,  # type: "TokenCredential"
-        subscription_id,  # type: str
-        base_url=None,  # type: Optional[str]
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> None
+        credential: "AsyncTokenCredential",
+        subscription_id: str,
+        base_url: Optional[str] = None,
+        **kwargs: Any
+    ) -> None:
         if not base_url:
             base_url = 'https://management.azure.com'
         self._config = HybridComputeManagementClientConfiguration(credential, subscription_id, **kwargs)
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -66,15 +63,12 @@ class HybridComputeManagementClient(object):
         self.operations = Operations(
             self._client, self._config, self._serialize, self._deserialize)
 
-    def close(self):
-        # type: () -> None
-        self._client.close()
+    async def close(self) -> None:
+        await self._client.close()
 
-    def __enter__(self):
-        # type: () -> HybridComputeManagementClient
-        self._client.__enter__()
+    async def __aenter__(self) -> "HybridComputeManagementClient":
+        await self._client.__aenter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
-        self._client.__exit__(*exc_details)
+    async def __aexit__(self, *exc_details) -> None:
+        await self._client.__aexit__(*exc_details)
