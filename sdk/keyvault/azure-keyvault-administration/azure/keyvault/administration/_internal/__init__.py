60cc3e99321d65201fe779b17d99e3434d9f124b
Charles Lowell <chlowe@microsoft.com>
2020-12-08 14:54:08 -0800

    azure-keyvault-administration review feedback (#15180)

--- sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/_internal/__init__.py
+++ sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/_internal/__init__.py
@@ -48,6 +48,28 @@ def parse_vault_id(url):
     )
 
 
+BackupLocation = namedtuple("BackupLocation", ["container_url", "folder_name"])
+
+
+def parse_blob_storage_url(blob_storage_url):
+    # type: (str) -> BackupLocation
+    """Parse the blob container URL and folder name from a backup's blob storage URL.
+
+    For example, https://<account>.blob.core.windows.net/backup/mhsm-account-2020090117323313 parses to
+    (container_url="https://<account>.blob.core.windows.net/backup", folder_name="mhsm-account-2020090117323313").
+    """
+
+    try:
+        folder_name = blob_storage_url.rstrip("/").split("/")[-1]
+        container_url = blob_storage_url[: blob_storage_url.rindex(folder_name) - 1]
+        return BackupLocation(container_url, folder_name)
+    except:  # pylint:disable=broad-except
+        raise ValueError(
+            '"blob_storage_url" should be the URL of a blob holding a Key Vault backup, for example '
+            '"https://<account>.blob.core.windows.net/backup/mhsm-account-2020090117323313"'
+        )
+
+
 try:
     # pylint:disable=unused-import
     from .async_challenge_auth_policy import AsyncChallengeAuthPolicy
