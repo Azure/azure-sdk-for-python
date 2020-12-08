b385c79c9e880425ef5460573b6e13e7334c2908
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2020-12-08 11:27:09 +0800

    generate healthcareapis track2 version (#15646)

--- sdk/healthcareapis/azure-mgmt-healthcareapis/azure/mgmt/healthcareapis/aio/_healthcare_apis_management_client.py
+++ sdk/healthcareapis/azure-mgmt-healthcareapis/azure/mgmt/healthcareapis/aio/_healthcare_apis_management_client.py
@@ -6,16 +6,14 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import TYPE_CHECKING
+from typing import Any, Optional, TYPE_CHECKING
 
-from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
-    from typing import Any, Optional
-
-    from azure.core.credentials import TokenCredential
+    from azure.core.credentials_async import AsyncTokenCredential
 
 from ._configuration import HealthcareApisManagementClientConfiguration
 from .operations import ServicesOperations
@@ -23,24 +21,24 @@ from .operations import Operations
 from .operations import OperationResultsOperations
 from .operations import PrivateEndpointConnectionsOperations
 from .operations import PrivateLinkResourcesOperations
-from . import models
+from .. import models
 
 
 class HealthcareApisManagementClient(object):
     """Azure Healthcare APIs Client.
 
     :ivar services: ServicesOperations operations
-    :vartype services: azure.mgmt.healthcareapis.operations.ServicesOperations
+    :vartype services: azure.mgmt.healthcareapis.aio.operations.ServicesOperations
     :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.healthcareapis.operations.Operations
+    :vartype operations: azure.mgmt.healthcareapis.aio.operations.Operations
     :ivar operation_results: OperationResultsOperations operations
-    :vartype operation_results: azure.mgmt.healthcareapis.operations.OperationResultsOperations
+    :vartype operation_results: azure.mgmt.healthcareapis.aio.operations.OperationResultsOperations
     :ivar private_endpoint_connections: PrivateEndpointConnectionsOperations operations
-    :vartype private_endpoint_connections: azure.mgmt.healthcareapis.operations.PrivateEndpointConnectionsOperations
+    :vartype private_endpoint_connections: azure.mgmt.healthcareapis.aio.operations.PrivateEndpointConnectionsOperations
     :ivar private_link_resources: PrivateLinkResourcesOperations operations
-    :vartype private_link_resources: azure.mgmt.healthcareapis.operations.PrivateLinkResourcesOperations
+    :vartype private_link_resources: azure.mgmt.healthcareapis.aio.operations.PrivateLinkResourcesOperations
     :param credential: Credential needed for the client to connect to Azure.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: The subscription identifier.
     :type subscription_id: str
     :param str base_url: Service URL
@@ -49,16 +47,15 @@ class HealthcareApisManagementClient(object):
 
     def __init__(
         self,
-        credential,  # type: "TokenCredential"
-        subscription_id,  # type: str
-        base_url=None,  # type: Optional[str]
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> None
+        credential: "AsyncTokenCredential",
+        subscription_id: str,
+        base_url: Optional[str] = None,
+        **kwargs: Any
+    ) -> None:
         if not base_url:
             base_url = 'https://management.azure.com'
         self._config = HealthcareApisManagementClientConfiguration(credential, subscription_id, **kwargs)
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -76,15 +73,12 @@ class HealthcareApisManagementClient(object):
         self.private_link_resources = PrivateLinkResourcesOperations(
             self._client, self._config, self._serialize, self._deserialize)
 
-    def close(self):
-        # type: () -> None
-        self._client.close()
+    async def close(self) -> None:
+        await self._client.close()
 
-    def __enter__(self):
-        # type: () -> HealthcareApisManagementClient
-        self._client.__enter__()
+    async def __aenter__(self) -> "HealthcareApisManagementClient":
+        await self._client.__aenter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
-        self._client.__exit__(*exc_details)
+    async def __aexit__(self, *exc_details) -> None:
+        await self._client.__aexit__(*exc_details)
