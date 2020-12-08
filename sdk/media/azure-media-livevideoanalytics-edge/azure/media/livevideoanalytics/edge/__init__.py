5acee232eece1553fdbc0763141c4fdc8f138f33
Laurent Mazuel <laurent.mazuel@gmail.com>
2020-12-08 09:25:47 -0800

    Improve import system

--- sdk/media/azure-media-livevideoanalytics-edge/azure/media/livevideoanalytics/edge/__init__.py
+++ sdk/media/azure-media-livevideoanalytics-edge/azure/media/livevideoanalytics/edge/__init__.py
@@ -1,68 +1,8 @@
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)  # type: ignore
-#from azure.media.livevideoanalytics.edge._generated.models import (MediaGraphTopologySetRequestBody,
-#MediaGraphTopologySetRequest, MediaGraphInstanceSetRequest, MediaGraphInstanceSetRequestBody)
-
 from ._generated.models import *
+from ._generated import models
 
-__all__ = [
-    "MethodRequest",
-    "ItemNonSetRequestBase",
-    "MediaGraphSink"
-    "MediaGraphAssetSink",
-    "MediaGraphCertificateSource",
-    "MediaGraphProcessor",
-    "MediaGraphExtensionProcessorBase",
-    "MediaGraphCognitiveServicesVisionExtension",
-    "MediaGraphCredentials",
-    "MediaGraphEndpoint",
-    "MediaGraphFileSink",
-    "MediaGraphGrpcExtension",
-    "MediaGraphGrpcExtensionDataTransfer",
-    "MediaGraphHttpExtension",
-    "MediaGraphHttpHeaderCredentials",
-    "MediaGraphImage",
-    "MediaGraphImageFormat",
-    "MediaGraphImageFormatBmp",
-    "MediaGraphImageFormatJpeg",
-    "MediaGraphImageFormatPng",
-    "MediaGraphImageFormatRaw",
-    "MediaGraphImageScale",
-    "MediaGraphInstance",
-    "MediaGraphInstanceActivateRequest",
-    "MediaGraphInstanceCollection",
-    "MediaGraphInstanceDeActivateRequest",
-    "MediaGraphInstanceDeleteRequest",
-    "MediaGraphInstanceGetRequest",
-    "MediaGraphInstanceListRequest",
-    "MediaGraphInstanceProperties",
-    "MediaGraphInstanceSetRequest",
-    "MediaGraphInstanceSetRequestBody",
-    "MediaGraphIoTHubMessageSink",
-    "MediaGraphSource",
-    "MediaGraphIoTHubMessageSource",
-    "MediaGraphMotionDetectionProcessor",
-    "MediaGraphNodeInput",
-    "MediaGraphOutputSelector",
-    "MediaGraphParameterDeclaration",
-    "MediaGraphParameterDefinition",
-    "MediaGraphPemCertificateList",
-    "MediaGraphRtspSource",
-    "MediaGraphSamplingOptions",
-    "MediaGraphSignalGateProcessor",
-    "MediaGraphSystemData",
-    "MediaGraphTlsEndpoint",
-    "MediaGraphTlsValidationOptions",
-    "MediaGraphTopology",
-    "MediaGraphTopologyCollection",
-    "MediaGraphTopologyDeleteRequest",
-    "MediaGraphTopologyGetRequest",
-    "MediaGraphTopologyListRequest",
-    "MediaGraphTopologyProperties",
-    "MediaGraphTopologySetRequest",
-    "MediaGraphTopologySetRequestBody",
-    "MediaGraphUnsecuredEndpoint",
-    "MediaGraphUsernamePasswordCredentials"
-]
+__all__ = models.__all__
+
 def _OverrideTopologySetRequestSerialize(self):
     graph_body = MediaGraphTopologySetRequestBody(name=self.graph.name)
     graph_body.system_data = self.graph.system_data
