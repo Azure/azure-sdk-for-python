585b4a13145d45a736138810db54d85b164fa03b
Tamer Sherif <tasherif@microsoft.com>
2020-12-10 14:15:19 -0800

    cleaned up model name

--- sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_data_lake_service_client.py
+++ sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_data_lake_service_client.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from copy import deepcopy
 from typing import Optional, List, Dict, Any
 
 try:
