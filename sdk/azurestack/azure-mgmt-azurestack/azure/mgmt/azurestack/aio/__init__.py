41a7940c3b026fc9ec94445b3735ab7e220895cb
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2020-12-10 12:05:34 +0800

    [T2] azurestack (#15629)

--- sdk/azurestack/azure-mgmt-azurestack/azure/mgmt/azurestack/aio/__init__.py
+++ sdk/azurestack/azure-mgmt-azurestack/azure/mgmt/azurestack/aio/__init__.py
@@ -7,13 +7,4 @@
 # --------------------------------------------------------------------------
 
 from ._azure_stack_management_client import AzureStackManagementClient
-from ._version import VERSION
-
-__version__ = VERSION
 __all__ = ['AzureStackManagementClient']
-
-try:
-    from ._patch import patch_sdk  # type: ignore
-    patch_sdk()
-except ImportError:
-    pass
