4be6dbe35454ed7c864a0ee39303644f3d6284b1
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2020-12-10 12:00:49 +0800

    [T2] costmanagement (#15476)

--- sdk/costmanagement/azure-mgmt-costmanagement/azure/mgmt/costmanagement/aio/_cost_management_client.py
+++ sdk/costmanagement/azure-mgmt-costmanagement/azure/mgmt/costmanagement/aio/_cost_management_client.py
@@ -6,16 +6,14 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import TYPE_CHECKING
+from typing import Any, Optional, TYPE_CHECKING
 
-from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
-    from typing import Any, Optional
-
-    from azure.core.credentials import TokenCredential
+    from azure.core.credentials_async import AsyncTokenCredential
 
 from ._configuration import CostManagementClientConfiguration
 from .operations import ViewsOperations
@@ -25,42 +23,41 @@ from .operations import DimensionsOperations
 from .operations import QueryOperations
 from .operations import Operations
 from .operations import ExportsOperations
-from . import models
+from .. import models
 
 
 class CostManagementClient(object):
     """CostManagementClient.
 
     :ivar views: ViewsOperations operations
-    :vartype views: azure.mgmt.costmanagement.operations.ViewsOperations
+    :vartype views: azure.mgmt.costmanagement.aio.operations.ViewsOperations
     :ivar alerts: AlertsOperations operations
-    :vartype alerts: azure.mgmt.costmanagement.operations.AlertsOperations
+    :vartype alerts: azure.mgmt.costmanagement.aio.operations.AlertsOperations
     :ivar forecast: ForecastOperations operations
-    :vartype forecast: azure.mgmt.costmanagement.operations.ForecastOperations
+    :vartype forecast: azure.mgmt.costmanagement.aio.operations.ForecastOperations
     :ivar dimensions: DimensionsOperations operations
-    :vartype dimensions: azure.mgmt.costmanagement.operations.DimensionsOperations
+    :vartype dimensions: azure.mgmt.costmanagement.aio.operations.DimensionsOperations
     :ivar query: QueryOperations operations
-    :vartype query: azure.mgmt.costmanagement.operations.QueryOperations
+    :vartype query: azure.mgmt.costmanagement.aio.operations.QueryOperations
     :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.costmanagement.operations.Operations
+    :vartype operations: azure.mgmt.costmanagement.aio.operations.Operations
     :ivar exports: ExportsOperations operations
-    :vartype exports: azure.mgmt.costmanagement.operations.ExportsOperations
+    :vartype exports: azure.mgmt.costmanagement.aio.operations.ExportsOperations
     :param credential: Credential needed for the client to connect to Azure.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param str base_url: Service URL
     """
 
     def __init__(
         self,
-        credential,  # type: "TokenCredential"
-        base_url=None,  # type: Optional[str]
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> None
+        credential: "AsyncTokenCredential",
+        base_url: Optional[str] = None,
+        **kwargs: Any
+    ) -> None:
         if not base_url:
             base_url = 'https://management.azure.com'
         self._config = CostManagementClientConfiguration(credential, **kwargs)
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -82,15 +79,12 @@ class CostManagementClient(object):
         self.exports = ExportsOperations(
             self._client, self._config, self._serialize, self._deserialize)
 
-    def close(self):
-        # type: () -> None
-        self._client.close()
+    async def close(self) -> None:
+        await self._client.close()
 
-    def __enter__(self):
-        # type: () -> CostManagementClient
-        self._client.__enter__()
+    async def __aenter__(self) -> "CostManagementClient":
+        await self._client.__aenter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
-        self._client.__exit__(*exc_details)
+    async def __aexit__(self, *exc_details) -> None:
+        await self._client.__aexit__(*exc_details)
