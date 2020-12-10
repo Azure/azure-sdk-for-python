eca57ae5c9e21df78be8332185b7e448e7f62152
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2020-12-10 13:16:51 +0800

    [T2]hybridkubernetes (#15725)

--- sdk/hybridkubernetes/azure-mgmt-hybridkubernetes/azure/mgmt/hybridkubernetes/aio/_connected_kubernetes_client.py
+++ sdk/hybridkubernetes/azure-mgmt-hybridkubernetes/azure/mgmt/hybridkubernetes/aio/_connected_kubernetes_client.py
@@ -6,32 +6,30 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import TYPE_CHECKING
+from typing import Any, Optional, TYPE_CHECKING
 
-from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
-    from typing import Any, Optional
-
-    from azure.core.credentials import TokenCredential
+    from azure.core.credentials_async import AsyncTokenCredential
 
 from ._configuration import ConnectedKubernetesClientConfiguration
 from .operations import ConnectedClusterOperations
 from .operations import Operations
-from . import models
+from .. import models
 
 
 class ConnectedKubernetesClient(object):
     """Azure Connected Cluster Resource Provider API for adopting any Kubernetes Cluster.
 
     :ivar connected_cluster: ConnectedClusterOperations operations
-    :vartype connected_cluster: azure.mgmt.hybridkubernetes.operations.ConnectedClusterOperations
+    :vartype connected_cluster: azure.mgmt.hybridkubernetes.aio.operations.ConnectedClusterOperations
     :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.hybridkubernetes.operations.Operations
+    :vartype operations: azure.mgmt.hybridkubernetes.aio.operations.Operations
     :param credential: Credential needed for the client to connect to Azure.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: The ID of the target subscription.
     :type subscription_id: str
     :param str base_url: Service URL
@@ -40,16 +38,15 @@ class ConnectedKubernetesClient(object):
 
     def __init__(
         self,
-        credential,  # type: "TokenCredential"
-        subscription_id,  # type: str
-        base_url=None,  # type: Optional[str]
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> None
+        credential: "AsyncTokenCredential",
+        subscription_id: str,
+        base_url: Optional[str] = None,
+        **kwargs: Any
+    ) -> None:
         if not base_url:
             base_url = 'https://management.azure.com'
         self._config = ConnectedKubernetesClientConfiguration(credential, subscription_id, **kwargs)
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -61,15 +58,12 @@ class ConnectedKubernetesClient(object):
         self.operations = Operations(
             self._client, self._config, self._serialize, self._deserialize)
 
-    def close(self):
-        # type: () -> None
-        self._client.close()
+    async def close(self) -> None:
+        await self._client.close()
 
-    def __enter__(self):
-        # type: () -> ConnectedKubernetesClient
-        self._client.__enter__()
+    async def __aenter__(self) -> "ConnectedKubernetesClient":
+        await self._client.__aenter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
-        self._client.__exit__(*exc_details)
+    async def __aexit__(self, *exc_details) -> None:
+        await self._client.__aexit__(*exc_details)
