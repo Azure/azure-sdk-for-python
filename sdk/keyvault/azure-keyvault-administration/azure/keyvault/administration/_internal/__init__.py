b2ca061b6cf03cfae1da3676f0e415cad42912c9
Charles Lowell <chlowe@microsoft.com>
2020-12-11 08:27:58 -0800

    Align azure-keyvault-administration API with other SDKs (#15717)

--- sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/_internal/__init__.py
+++ sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/_internal/__init__.py
@@ -4,11 +4,7 @@
 # ------------------------------------
 from collections import namedtuple
 
-try:
-    import urllib.parse as parse
-except ImportError:
-    # pylint:disable=import-error
-    import urlparse as parse  # type: ignore
+from six.moves.urllib_parse import urlparse
 
 from .challenge_auth_policy import ChallengeAuthPolicy, ChallengeAuthPolicyBase
 from .client_base import KeyVaultClientBase
@@ -29,7 +25,7 @@ _VaultId = namedtuple("VaultId", ["vault_url", "collection", "name", "version"])
 
 def parse_vault_id(url):
     try:
-        parsed_uri = parse.urlparse(url)
+        parsed_uri = urlparse(url)
     except Exception:  # pylint: disable=broad-except
         raise ValueError("'{}' is not not a valid url".format(url))
     if not (parsed_uri.scheme and parsed_uri.hostname):
@@ -51,7 +47,7 @@ def parse_vault_id(url):
 BackupLocation = namedtuple("BackupLocation", ["container_url", "folder_name"])
 
 
-def parse_blob_storage_url(blob_storage_url):
+def parse_folder_url(folder_url):
     # type: (str) -> BackupLocation
     """Parse the blob container URL and folder name from a backup's blob storage URL.
 
@@ -60,12 +56,22 @@ def parse_blob_storage_url(blob_storage_url):
     """
 
     try:
-        folder_name = blob_storage_url.rstrip("/").split("/")[-1]
-        container_url = blob_storage_url[: blob_storage_url.rindex(folder_name) - 1]
+        parsed = urlparse(folder_url)
+
+        # the first segment of the path is the container name
+        stripped_path = parsed.path.strip("/")
+        container = stripped_path.split("/")[0]
+
+        # the rest of the path is the folder name
+        folder_name = stripped_path[len(container) + 1 :]
+
+        # this intentionally discards any SAS token in the URL--methods require the SAS token as a separate parameter
+        container_url = "{}://{}/{}".format(parsed.scheme, parsed.netloc, container)
+
         return BackupLocation(container_url, folder_name)
     except:  # pylint:disable=broad-except
         raise ValueError(
-            '"blob_storage_url" should be the URL of a blob holding a Key Vault backup, for example '
+            '"folder_url" should be the URL of a blob holding a Key Vault backup, for example '
             '"https://<account>.blob.core.windows.net/backup/mhsm-account-2020090117323313"'
         )
 
