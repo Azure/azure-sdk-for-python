36644ffd9c36c9c69dbd29a68c1eeb47fe1b7c56
Jorge Beauregard <69869951+jbeauregardb@users.noreply.github.com>
2020-12-15 13:04:16 -0600

    [Communication] - Identity- Added AAD support (#15750)
    * Added AAD support
    
    * Added tests
    
    * Added mock credential class
    
    * Lint issues
    
    * Addressed comments
    
    * Trailing spaces

--- sdk/communication/azure-communication-administration/tests/test_communication_identity_client_async.py
+++ sdk/communication/azure-communication-administration/tests/test_communication_identity_client_async.py
@@ -5,20 +5,46 @@
 # license information.
 # --------------------------------------------------------------------------
 import pytest
+from azure.core.credentials import AccessToken
 from azure.communication.administration.aio import CommunicationIdentityClient
+from azure.communication.administration._shared.utils import parse_connection_str
 from azure_devtools.scenario_tests import RecordingProcessor
 from devtools_testutils import ResourceGroupPreparer
 from _shared.helper import URIIdentityReplacer
 from _shared.asynctestcase  import AsyncCommunicationTestCase
 from _shared.testcase import BodyReplacerProcessor
 from _shared.communication_service_preparer import CommunicationServicePreparer
+from azure.identity import DefaultAzureCredential
 
+class FakeTokenCredential(object):
+    def __init__(self):
+        self.token = AccessToken("Fake Token", 0)
+
+    def get_token(self, *args):
+        return self.token
 class CommunicationIdentityClientTestAsync(AsyncCommunicationTestCase):
     def setUp(self):
         super(CommunicationIdentityClientTestAsync, self).setUp()
         self.recording_processors.extend([
             BodyReplacerProcessor(keys=["id", "token"]),
             URIIdentityReplacer()])
+    
+    @ResourceGroupPreparer(random_name_enabled=True)
+    @CommunicationServicePreparer()
+    @pytest.mark.asyncio
+    @AsyncCommunicationTestCase.await_prepared_test
+    async def test_create_user_from_managed_identity(self, connection_string):
+        endpoint, access_key = parse_connection_str(connection_string)
+        from devtools_testutils import is_live
+        if not is_live():
+            credential = FakeTokenCredential()
+        else:
+            credential = DefaultAzureCredential()
+        identity_client = CommunicationIdentityClient(endpoint, credential)
+        async with identity_client:
+            user = await identity_client.create_user()
+
+        assert user.identifier is not None
 
     @ResourceGroupPreparer(random_name_enabled=True)
     @CommunicationServicePreparer()
