fa99bf4a4169ef80e0cebcf330687661538fef16
Hector Hernandez Guzman <hectorh@microsoft.com>
2020-12-15 15:53:07 -0800

    lint

--- sdk/monitor/microsoft-opentelemetry-exporter-azuremonitor/microsoft/opentelemetry/exporter/azuremonitor/_utils.py
+++ sdk/monitor/microsoft-opentelemetry-exporter-azuremonitor/microsoft/opentelemetry/exporter/azuremonitor/_utils.py
@@ -1,9 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 import locale
-import os
 import platform
-import sys
 import threading
 import time
 
