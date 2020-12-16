83019bbb0966f90b3a0ba3c84c98d8e8473e8737
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2020-12-16 11:47:11 -0500

    Customer at symbol issue (#15813)
    * applied the blobs fix on canconicalized resource to tables, adding two tests for cosmos next
    
    * adding two respective tests for cosmos, fails on cosmos because of a forbidden request
    
    * adding note to changelog documenting bug fix

--- sdk/tables/azure-data-tables/tests/test_table_entity_async.py
+++ sdk/tables/azure-data-tables/tests/test_table_entity_async.py
@@ -249,6 +249,42 @@ class StorageTableEntityTest(TableTestCase):
     # --Test cases for entities ------------------------------------------
     @CachedResourceGroupPreparer(name_prefix="tablestest")
     @CachedStorageAccountPreparer(name_prefix="tablestest")
+    async def test_url_encoding_at_symbol(self, storage_account, storage_account_key):
+
+        await self._set_up(storage_account, storage_account_key)
+        try:
+            entity = {
+                u"PartitionKey": u"PK",
+                u"RowKey": u"table@storage.com",
+                u"Value": 100
+            }
+
+            await self.table.create_entity(entity)
+
+            f = u"RowKey eq '{}'".format(entity["RowKey"])
+            entities = self.table.query_entities(filter=f)
+
+            count = 0
+            async for e in entities:
+                assert e.PartitionKey == entity[u"PartitionKey"]
+                assert e.RowKey == entity[u"RowKey"]
+                assert e.Value == entity[u"Value"]
+                await self.table.delete_entity(e.PartitionKey, e.RowKey)
+                count += 1
+
+            assert count == 1
+
+            entities = self.table.query_entities(filter=f)
+            count = 0
+            async for e in entities:
+                count += 1
+            assert count == 0
+
+        finally:
+            await self._tear_down()
+
+    @CachedResourceGroupPreparer(name_prefix="tablestest")
+    @CachedStorageAccountPreparer(name_prefix="tablestest")
     async def test_insert_entity_dictionary(self, resource_group, location, storage_account, storage_account_key):
         # Arrange
         await self._set_up(storage_account, storage_account_key)
