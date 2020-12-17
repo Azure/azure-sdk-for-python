cbe5c49343227b82a9b4701f2a6bb3b02d5f48a0
Krista Pratico <krpratic@microsoft.com>
2020-12-17 10:12:22 -0800

    [metricsadvisor] fix sample/readme issues (#15854)
    Resolves #15845

--- sdk/metricsadvisor/azure-ai-metricsadvisor/samples/sample_alert_configuration.py
+++ sdk/metricsadvisor/azure-ai-metricsadvisor/samples/sample_alert_configuration.py
@@ -188,11 +188,13 @@ def sample_list_alerts(alert_config_id):
         end_time=datetime.datetime(2020, 9, 9),
         time_mode="AnomalyTime",
     )
+
+    tolist = []
     for result in results:
+        tolist.append(result)
         print("Alert id: {}".format(result.id))
         print("Create on: {}".format(result.created_on))
-    return results
-
+    return tolist
     # [END list_alerts]
 
 
