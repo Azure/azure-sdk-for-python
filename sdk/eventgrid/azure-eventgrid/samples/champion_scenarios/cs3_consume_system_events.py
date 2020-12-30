ec80bd4ac496b245998766dc65ba258cd14c25ab
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2020-12-30 22:14:59 +0530

    some changes

--- sdk/eventgrid/azure-eventgrid/samples/champion_scenarios/cs3_consume_system_events.py
+++ sdk/eventgrid/azure-eventgrid/samples/champion_scenarios/cs3_consume_system_events.py
@@ -15,8 +15,9 @@ import json
 from azure.eventgrid import EventGridConsumer
 
 consumer = EventGridConsumer()
+path = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "./cs3_event_grid_event_system_event.json"))
 
-with open('./cs3_event_grid_event_system_event.json', 'r') as f:
+with open(path, 'r') as f:
     eg_event_received_message = json.loads(f.read())
 # returns List[DeserializedEvent]
 event = consumer.decode_eventgrid_event(eg_event_received_message)
