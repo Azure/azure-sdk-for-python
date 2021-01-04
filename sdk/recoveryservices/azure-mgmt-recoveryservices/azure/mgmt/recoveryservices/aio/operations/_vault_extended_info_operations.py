41542c103bb33918b20e6910a26d2ce926e07c2f
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2021-01-04 11:12:49 +0800

    GA recoveryservices track2 sdk (#15864)

--- sdk/recoveryservices/azure-mgmt-recoveryservices/azure/mgmt/recoveryservices/aio/operations/_vault_extended_info_operations.py
+++ sdk/recoveryservices/azure-mgmt-recoveryservices/azure/mgmt/recoveryservices/aio/operations/_vault_extended_info_operations.py
@@ -13,7 +13,7 @@ from azure.core.pipeline import PipelineResponse
 from azure.core.pipeline.transport import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core.exceptions import ARMErrorFormat
 
-from ... import models
+from ... import models as _models
 
 T = TypeVar('T')
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, AsyncHttpResponse], T, Dict[str, Any]], Any]]
@@ -32,7 +32,7 @@ class VaultExtendedInfoOperations:
     :param deserializer: An object model deserializer.
     """
 
-    models = models
+    models = _models
 
     def __init__(self, client, config, serializer, deserializer) -> None:
         self._client = client
@@ -45,7 +45,7 @@ class VaultExtendedInfoOperations:
         resource_group_name: str,
         vault_name: str,
         **kwargs
-    ) -> "models.VaultExtendedInfoResource":
+    ) -> "_models.VaultExtendedInfoResource":
         """Get the vault extended info.
 
         :param resource_group_name: The name of the resource group where the recovery services vault is
@@ -58,7 +58,7 @@ class VaultExtendedInfoOperations:
         :rtype: ~azure.mgmt.recoveryservices.models.VaultExtendedInfoResource
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.VaultExtendedInfoResource"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.VaultExtendedInfoResource"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
@@ -103,9 +103,9 @@ class VaultExtendedInfoOperations:
         self,
         resource_group_name: str,
         vault_name: str,
-        resource_extended_info_details: "models.VaultExtendedInfoResource",
+        resource_extended_info_details: "_models.VaultExtendedInfoResource",
         **kwargs
-    ) -> "models.VaultExtendedInfoResource":
+    ) -> "_models.VaultExtendedInfoResource":
         """Create vault extended info.
 
         :param resource_group_name: The name of the resource group where the recovery services vault is
@@ -120,7 +120,7 @@ class VaultExtendedInfoOperations:
         :rtype: ~azure.mgmt.recoveryservices.models.VaultExtendedInfoResource
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.VaultExtendedInfoResource"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.VaultExtendedInfoResource"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
@@ -170,9 +170,9 @@ class VaultExtendedInfoOperations:
         self,
         resource_group_name: str,
         vault_name: str,
-        resource_extended_info_details: "models.VaultExtendedInfoResource",
+        resource_extended_info_details: "_models.VaultExtendedInfoResource",
         **kwargs
-    ) -> "models.VaultExtendedInfoResource":
+    ) -> "_models.VaultExtendedInfoResource":
         """Update vault extended info.
 
         :param resource_group_name: The name of the resource group where the recovery services vault is
@@ -187,7 +187,7 @@ class VaultExtendedInfoOperations:
         :rtype: ~azure.mgmt.recoveryservices.models.VaultExtendedInfoResource
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.VaultExtendedInfoResource"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.VaultExtendedInfoResource"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
