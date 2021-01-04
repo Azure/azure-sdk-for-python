1603d16dc35bd2c2751aedea0abe60e330458dd4
Yuchao Yan <yuchaoyan@microsoft.com>
2021-01-04 17:40:44 +0800

    test,version,CHANGELOG

--- sdk/appplatform/azure-mgmt-appplatform/setup.py
+++ sdk/appplatform/azure-mgmt-appplatform/setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages, setup
 
 # Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-mgmt-appplatform"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_PPRINT_NAME = "Appplatform Management"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
