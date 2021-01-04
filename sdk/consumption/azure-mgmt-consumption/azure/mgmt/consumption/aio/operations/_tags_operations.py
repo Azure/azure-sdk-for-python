a9e01e1231779351e16aabb0225c975784cddf4a
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2021-01-04 11:56:35 +0800

    GA consumption track2 sdk (#15897)

--- sdk/consumption/azure-mgmt-consumption/azure/mgmt/consumption/aio/operations/_tags_operations.py
+++ sdk/consumption/azure-mgmt-consumption/azure/mgmt/consumption/aio/operations/_tags_operations.py
@@ -13,7 +13,7 @@ from azure.core.pipeline import PipelineResponse
 from azure.core.pipeline.transport import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core.exceptions import ARMErrorFormat
 
-from ... import models
+from ... import models as _models
 
 T = TypeVar('T')
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, AsyncHttpResponse], T, Dict[str, Any]], Any]]
@@ -32,7 +32,7 @@ class TagsOperations:
     :param deserializer: An object model deserializer.
     """
 
-    models = models
+    models = _models
 
     def __init__(self, client, config, serializer, deserializer) -> None:
         self._client = client
@@ -44,7 +44,7 @@ class TagsOperations:
         self,
         scope: str,
         **kwargs
-    ) -> "models.TagsResult":
+    ) -> "_models.TagsResult":
         """Get all available tag keys for the defined scope.
 
         :param scope: The scope associated with tags operations. This includes
@@ -63,7 +63,7 @@ class TagsOperations:
         :rtype: ~azure.mgmt.consumption.models.TagsResult
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.TagsResult"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.TagsResult"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
@@ -92,7 +92,7 @@ class TagsOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(models.ErrorResponse, response)
+            error = self._deserialize(_models.ErrorResponse, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('TagsResult', pipeline_response)
