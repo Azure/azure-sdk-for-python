9f31f9289f7b47d533d80cc7001ff5298e01ec9a
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2021-01-04 11:56:35 +0800

    GA consumption track2 sdk (#15897)

--- sdk/consumption/azure-mgmt-consumption/azure/mgmt/consumption/aio/operations/_reservations_details_operations.py
+++ sdk/consumption/azure-mgmt-consumption/azure/mgmt/consumption/aio/operations/_reservations_details_operations.py
@@ -14,7 +14,7 @@ from azure.core.pipeline import PipelineResponse
 from azure.core.pipeline.transport import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core.exceptions import ARMErrorFormat
 
-from ... import models
+from ... import models as _models
 
 T = TypeVar('T')
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, AsyncHttpResponse], T, Dict[str, Any]], Any]]
@@ -33,7 +33,7 @@ class ReservationsDetailsOperations:
     :param deserializer: An object model deserializer.
     """
 
-    models = models
+    models = _models
 
     def __init__(self, client, config, serializer, deserializer) -> None:
         self._client = client
@@ -46,7 +46,7 @@ class ReservationsDetailsOperations:
         reservation_order_id: str,
         filter: str,
         **kwargs
-    ) -> AsyncIterable["models.ReservationDetailsListResult"]:
+    ) -> AsyncIterable["_models.ReservationDetailsListResult"]:
         """Lists the reservations details for provided date range.
 
         :param reservation_order_id: Order Id of the reservation.
@@ -59,7 +59,7 @@ class ReservationsDetailsOperations:
         :rtype: ~azure.core.async_paging.AsyncItemPaged[~azure.mgmt.consumption.models.ReservationDetailsListResult]
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.ReservationDetailsListResult"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.ReservationDetailsListResult"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
@@ -105,7 +105,7 @@ class ReservationsDetailsOperations:
             response = pipeline_response.http_response
 
             if response.status_code not in [200]:
-                error = self._deserialize(models.ErrorResponse, response)
+                error = self._deserialize(_models.ErrorResponse, response)
                 map_error(status_code=response.status_code, response=response, error_map=error_map)
                 raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
@@ -122,7 +122,7 @@ class ReservationsDetailsOperations:
         reservation_id: str,
         filter: str,
         **kwargs
-    ) -> AsyncIterable["models.ReservationDetailsListResult"]:
+    ) -> AsyncIterable["_models.ReservationDetailsListResult"]:
         """Lists the reservations details for provided date range.
 
         :param reservation_order_id: Order Id of the reservation.
@@ -137,7 +137,7 @@ class ReservationsDetailsOperations:
         :rtype: ~azure.core.async_paging.AsyncItemPaged[~azure.mgmt.consumption.models.ReservationDetailsListResult]
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.ReservationDetailsListResult"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.ReservationDetailsListResult"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
@@ -184,7 +184,7 @@ class ReservationsDetailsOperations:
             response = pipeline_response.http_response
 
             if response.status_code not in [200]:
-                error = self._deserialize(models.ErrorResponse, response)
+                error = self._deserialize(_models.ErrorResponse, response)
                 map_error(status_code=response.status_code, response=response, error_map=error_map)
                 raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
@@ -204,7 +204,7 @@ class ReservationsDetailsOperations:
         reservation_id: Optional[str] = None,
         reservation_order_id: Optional[str] = None,
         **kwargs
-    ) -> AsyncIterable["models.ReservationDetailsListResult"]:
+    ) -> AsyncIterable["_models.ReservationDetailsListResult"]:
         """Lists the reservations details for the defined scope and provided date range.
 
         :param scope: The scope associated with reservations details operations. This includes
@@ -232,7 +232,7 @@ class ReservationsDetailsOperations:
         :rtype: ~azure.core.async_paging.AsyncItemPaged[~azure.mgmt.consumption.models.ReservationDetailsListResult]
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.ReservationDetailsListResult"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.ReservationDetailsListResult"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
@@ -287,7 +287,7 @@ class ReservationsDetailsOperations:
             response = pipeline_response.http_response
 
             if response.status_code not in [200]:
-                error = self._deserialize(models.ErrorResponse, response)
+                error = self._deserialize(_models.ErrorResponse, response)
                 map_error(status_code=response.status_code, response=response, error_map=error_map)
                 raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
