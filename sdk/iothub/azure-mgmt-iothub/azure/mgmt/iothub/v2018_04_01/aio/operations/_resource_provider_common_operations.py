36695f881412d3eb251805a5c1d1d8adfb951624
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2021-01-04 11:14:34 +0800

    GA iothub track2 sdk (#15866)

--- sdk/iothub/azure-mgmt-iothub/azure/mgmt/iothub/v2018_04_01/aio/operations/_resource_provider_common_operations.py
+++ sdk/iothub/azure-mgmt-iothub/azure/mgmt/iothub/v2018_04_01/aio/operations/_resource_provider_common_operations.py
@@ -13,7 +13,7 @@ from azure.core.pipeline import PipelineResponse
 from azure.core.pipeline.transport import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core.exceptions import ARMErrorFormat
 
-from ... import models
+from ... import models as _models
 
 T = TypeVar('T')
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, AsyncHttpResponse], T, Dict[str, Any]], Any]]
@@ -32,7 +32,7 @@ class ResourceProviderCommonOperations:
     :param deserializer: An object model deserializer.
     """
 
-    models = models
+    models = _models
 
     def __init__(self, client, config, serializer, deserializer) -> None:
         self._client = client
@@ -43,7 +43,7 @@ class ResourceProviderCommonOperations:
     async def get_subscription_quota(
         self,
         **kwargs
-    ) -> "models.UserSubscriptionQuotaListResult":
+    ) -> "_models.UserSubscriptionQuotaListResult":
         """Get the number of iot hubs in the subscription.
 
         Get the number of free and paid iot hubs in the subscription.
@@ -53,7 +53,7 @@ class ResourceProviderCommonOperations:
         :rtype: ~azure.mgmt.iothub.models.UserSubscriptionQuotaListResult
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.UserSubscriptionQuotaListResult"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.UserSubscriptionQuotaListResult"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
@@ -82,7 +82,7 @@ class ResourceProviderCommonOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(models.ErrorDetails, response)
+            error = self._deserialize(_models.ErrorDetails, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('UserSubscriptionQuotaListResult', pipeline_response)
