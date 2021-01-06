888fd7556673e87bba299294291de6cd70fa99cf
00Kai0 <sunkaihuisos@gmail.com>
2021-01-06 11:39:20 +0800

    update changelog

--- sdk/healthbot/azure-mgmt-healthbot/azure/mgmt/healthbot/_version.py
+++ sdk/healthbot/azure-mgmt-healthbot/azure/mgmt/healthbot/_version.py
@@ -6,4 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "2020-10-20-preview"
+VERSION = "1.0.0b1"
