809f9dfafd67e2d812d9887ec936e844b49aa162
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-01-06 11:02:54 -0500

    added samples to pipeline, fixed up a few samples to actually work (#15947)
    * added samples to pipeline, fixed up a few samples to actually work
    
    * fixed up samples to pass, changed table names, inserted a few sleeps
    
    * fixed up unicode and python2 issues, added cleanup methods and unique table names
    
    * added unicode endings
    
    * fixing up samples to use with, and removing unnecessary comments
    
    * bug on a sample

--- sdk/tables/azure-data-tables/samples/sample_query_table.py
+++ sdk/tables/azure-data-tables/samples/sample_query_table.py
@@ -22,35 +22,52 @@ USAGE:
 import os
 import copy
 import random
+from dotenv import find_dotenv, load_dotenv
+
 
 class SampleTablesQuery(object):
-    connection_string = os.getenv("AZURE_TABLES_CONNECTION_STRING")
-    access_key = os.getenv("AZURE_TABLES_KEY")
-    account_url = os.getenv("AZURE_TABLES_ACCOUNT_URL")
-    account_name = os.getenv("AZURE_TABLES_ACCOUNT_NAME")
-    table_name = "OfficeSupplies"
+
+    def __init__(self):
+        load_dotenv(find_dotenv())
+        self.access_key = os.getenv("TABLES_PRIMARY_STORAGE_ACCOUNT_KEY")
+        self.endpoint = os.getenv("TABLES_STORAGE_ENDPOINT_SUFFIX")
+        self.account_name = os.getenv("TABLES_STORAGE_ACCOUNT_NAME")
+        self.account_url = "{}.table.{}".format(self.account_name, self.endpoint)
+        self.connection_string = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix={}".format(
+            self.account_name,
+            self.access_key,
+            self.endpoint
+        )
+        self.table_name = "SampleQueryTable"
 
 
     def _insert_random_entities(self):
         from azure.data.tables import TableClient
-        brands = ["Crayola", "Sharpie", "Chameleon"]
-        colors = ["red", "blue", "orange", "yellow"]
-        names = ["marker", "pencil", "pen"]
+        from azure.core.exceptions import ResourceExistsError
+        brands = [u"Crayola", u"Sharpie", u"Chameleon"]
+        colors = [u"red", u"blue", u"orange", u"yellow"]
+        names = [u"marker", u"pencil", u"pen"]
         entity_template = {
-            "PartitionKey": "pk",
-            "RowKey": "row",
+            u"PartitionKey": u"pk",
+            u"RowKey": u"row",
         }
 
-        table_client = TableClient.from_connection_string(self.connection_string, self.table_name)
-        table_client.create_table()
+        with TableClient.from_connection_string(self.connection_string, self.table_name) as table_client:
+            try:
+                table_client.create_table()
+            except ResourceExistsError:
+                print(u"Table already exists")
 
-        for i in range(10):
-            e = copy.deepcopy(entity_template)
-            e["RowKey"] += str(i)
-            e["Name"] = random.choice(names)
-            e["Brand"] = random.choice(brands)
-            e["Color"] = random.choice(colors)
-            table_client.create_entity(entity=e)
+            for i in range(10):
+                e = copy.deepcopy(entity_template)
+                try:
+                    e[u"RowKey"] += unicode(i)
+                except NameError:
+                    e[u"RowKey"] += str(i)
+                e[u"Name"] = random.choice(names)
+                e[u"Brand"] = random.choice(brands)
+                e[u"Color"] = random.choice(colors)
+                table_client.create_entity(entity=e)
 
 
     def sample_query_entities(self):
@@ -58,22 +75,22 @@ class SampleTablesQuery(object):
         from azure.data.tables import TableClient
         from azure.core.exceptions import HttpResponseError
 
-        table_client = TableClient.from_connection_string(self.connection_string, self.table_name)
         # [START query_entities]
-        try:
-            entity_name = "marker"
-            name_filter = "Name eq '{}'".format(entity_name)
-            queried_entities = table_client.query_entities(filter=name_filter, select=["Brand","Color"])
+        with TableClient.from_connection_string(self.connection_string, self.table_name) as table_client:
+            try:
+                entity_name = u"marker"
+                name_filter = u"Name eq '{}'".format(entity_name)
+                queried_entities = table_client.query_entities(filter=name_filter, select=[u"Brand",u"Color"])
 
-            for entity_chosen in queried_entities:
-                print(entity_chosen)
+                for entity_chosen in queried_entities:
+                    print(entity_chosen)
 
-        except HttpResponseError as e:
-            print(e.message)
+            except HttpResponseError as e:
+                print(e.message)
         # [END query_entities]
 
-        finally:
-            table_client.delete_table()
+            finally:
+                table_client.delete_table()
 
 if __name__ == '__main__':
     stq = SampleTablesQuery()
