31afa39a89ff5d0b94f9b7c63c0bcdeb1dbbefb9
turalf <tufarhad@microsoft.com>
2021-01-12 09:19:12 -0800

    Change return type for begin_search_available_phone_numbers

--- sdk/communication/azure-communication-administration/azure/communication/administration/_phone_numbers_client.py
+++ sdk/communication/azure-communication-administration/azure/communication/administration/_phone_numbers_client.py
@@ -122,7 +122,7 @@ class PhoneNumbersClient(object):
         :paramtype polling: bool or ~azure.core.polling.PollingMethod
         :keyword int polling_interval: Default waiting time between two polls for LRO operations if no Retry-After header is present.
         :return: An instance of LROPoller that returns either None or the result of cls(response)
-        :rtype: ~azure.core.polling.LROPoller[None]
+        :rtype: ~azure.core.polling.LROPoller[PhoneNumberSearchResult]
         :raises ~azure.core.exceptions.HttpResponseError:
         """
 
