58e2dc223c31ba8d5e17f27b86b5a52348269d2f
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2021-01-13 14:45:24 +0800

    release monitor 2.0.0 (#15921)

--- sdk/monitor/azure-mgmt-monitor/azure/mgmt/monitor/v2018_04_16/models/__init__.py
+++ sdk/monitor/azure-mgmt-monitor/azure/mgmt/monitor/v2018_04_16/models/__init__.py
@@ -39,7 +39,7 @@ except (SyntaxError, ImportError):
     from ._models import Source  # type: ignore
     from ._models import TriggerCondition  # type: ignore
 
-from ._monitor_client_enums import (
+from ._monitor_management_client_enums import (
     AlertSeverity,
     ConditionalOperator,
     Enabled,
--- sdk/monitor/azure-mgmt-monitor/azure/mgmt/monitor/v2018_11_27_preview/aio/operations/_vm_insights_operations.py
+++ sdk/monitor/azure-mgmt-monitor/azure/mgmt/monitor/v2018_11_27_preview/aio/operations/_vm_insights_operations.py
@@ -13,7 +13,7 @@ from azure.core.pipeline import PipelineResponse
 from azure.core.pipeline.transport import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core.exceptions import ARMErrorFormat
 
-from ... import models
+from ... import models as _models
 
 T = TypeVar('T')
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, AsyncHttpResponse], T, Dict[str, Any]], Any]]
@@ -32,7 +32,7 @@ class VMInsightsOperations:
     :param deserializer: An object model deserializer.
     """
 
-    models = models
+    models = _models
 
     def __init__(self, client, config, serializer, deserializer) -> None:
         self._client = client
@@ -44,7 +44,7 @@ class VMInsightsOperations:
         self,
         resource_uri: str,
         **kwargs
-    ) -> "models.VMInsightsOnboardingStatus":
+    ) -> "_models.VMInsightsOnboardingStatus":
         """Retrieves the VM Insights onboarding status for the specified resource or resource scope.
 
         :param resource_uri: The fully qualified Azure Resource manager identifier of the resource, or
@@ -55,7 +55,7 @@ class VMInsightsOperations:
         :rtype: ~$(python-base-namespace).v2018_11_27_preview.models.VMInsightsOnboardingStatus
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.VMInsightsOnboardingStatus"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.VMInsightsOnboardingStatus"]
         error_map = {
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
@@ -84,7 +84,7 @@ class VMInsightsOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(models.ResponseWithError, response)
+            error = self._deserialize(_models.ResponseWithError, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('VMInsightsOnboardingStatus', pipeline_response)
