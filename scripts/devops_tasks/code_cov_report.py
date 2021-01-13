6e5ae1cec7ed3e3cc488e60a665b690d9637012d
seankane-msft <seankane@microsoft.com>
2021-01-13 16:15:12 -0500

    cleaned up code cov function, removing commented out code and unused code

--- scripts/devops_tasks/code_cov_report.py
+++ scripts/devops_tasks/code_cov_report.py
@@ -27,6 +27,7 @@ def create_coverage_report():
         logging.info("Name: {}".format(name))
         folder = name[1]
         package = name[2]
+        print("Folder: {}\tPackage: {}".format(folder, package))
         if (folder, package) not in packages_to_report:
             packages_to_report.append((folder, package))
             logging.info("Found a package: {}".format(package))
@@ -62,7 +63,7 @@ def create_coverage_report():
                 package_name,
                 package_name.replace('-', '.')
             )
-            print(test_str)
+            print("Test string: ", test_str)
 
             if test_str in child.attrib['name']:
                 # print("condense_package")
@@ -83,7 +84,6 @@ def create_coverage_report():
 
             first_package_classes = first_package.find('classes')
 
-
             for node in nodes[1:]:
                 # print(node.tag, node.attrib)
                 temp_classes = node.find('classes')
@@ -98,58 +98,13 @@ def create_coverage_report():
 
         packages_root.remove(n)
 
-    ET.ElementTree(root).write(coverage_file)
+    ET.ElementTree(root).write("coverage-new.xml")
 
     print("\n\n\nFinal packages")
     for package in packages_root:
         print(package.tag, package.attrib)
 
     return
-    # ET.ElementTree(root).write("coverage-new.xml")
-
-    # master_package = False
-    # for child in packages:
-    #     print()
-    #     print(child.tag, child.attrib)
-
-    #     # Should this be combined into another node
-    #     for p in packages_to_report:
-    #         if p[1] in child.attrib['name']:
-    #             print("FOUND: ", p)
-
-    #             # Need to make sure it's not the master one
-    #             test_str = "sdk.{}.{}.azure.{}".format(p[0], p[1], p[0])
-    #             print(test_str)
-    #             print(child.attrib['name'])
-
-    #             if test_str != child.attrib['name']:
-    #                 print("Condense package")
-
-
-    # quit()
-
-
-
-
-
-
-
-    # new_packages = []
-    # for p in packages_to_report:
-    #     n = create_new_node(p, packages_root)
-    #     new_packages.append(n)
-
-    # # Now remove all the old packages
-    # packages_nodes = root.findall('packages') # THere should only be one, but returns a list
-    # for packages_node in packages_nodes:
-    #     for p in packages_node.findall('package'):
-    #         packages_node.remove(p)
-
-    # for np in new_packages:
-    #     packages_nodes[0].append(np)
-
-    # ET.ElementTree(root).write("coverage-new.xml")
-
 
 def recursive_set_name(root):
     # Stopping condition
@@ -187,7 +142,7 @@ def create_new_node(package_tuple, packages_root):
 
     ET.ElementTree(new_node).write("temp.xml")
 
-    quit()
+    # quit()
     return new_node
 
 
