b4ea0e35fdd708ab3b3bf24817997e58f555283d
Abigail Hartman <Abigail.Hartman@microsoft.com>
2021-01-13 16:37:26 -0800

    Reverting changes to TA

--- sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_lro.py
+++ sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_lro.py
@@ -5,7 +5,12 @@
 # ------------------------------------
 
 from six.moves.urllib.parse import urlencode
-from azure.core.polling.base_polling import OperationResourcePolling, OperationState
+from azure.core.polling.base_polling import LROBasePolling, OperationResourcePolling, OperationFailed, BadStatus
+
+
+_FINISHED = frozenset(["succeeded", "cancelled", "failed", "partiallysucceeded"])
+_FAILED = frozenset(["failed"])
+_SUCCEEDED = frozenset(["succeeded", "partiallysucceeded"])
 
 
 class TextAnalyticsOperationResourcePolling(OperationResourcePolling):
@@ -24,20 +29,62 @@ class TextAnalyticsOperationResourcePolling(OperationResourcePolling):
             "?" + urlencode(self._query_params)
 
 
-class TextAnalyticsOperationState(OperationState):
-    def __init__(self):
-        self._finished = frozenset(["succeeded", "cancelled", "failed", "partiallysucceeded"])
-        self._failed = frozenset(["failed"])
-        self._succeeded = frozenset(["succeeded", "partiallysucceeded"])
+class TextAnalyticsLROPollingMethod(LROBasePolling):
 
-    @property
     def finished(self):
-        return self._finished
-    
-    @property
-    def failed(self):
-        return self._failed
-    
-    @property
-    def succeeded(self):
-        return self._succeeded
+        """Is this polling finished?
+        :rtype: bool
+        """
+        return TextAnalyticsLROPollingMethod._finished(self.status())
+
+    @staticmethod
+    def _finished(status):
+        if hasattr(status, "value"):
+            status = status.value
+        return str(status).lower() in _FINISHED
+
+    @staticmethod
+    def _failed(status):
+        if hasattr(status, "value"):
+            status = status.value
+        return str(status).lower() in _FAILED
+
+    @staticmethod
+    def _raise_if_bad_http_status_and_method(response):
+        """Check response status code is valid.
+
+        Must be 200, 201, 202, or 204.
+
+        :raises: BadStatus if invalid status.
+        """
+        code = response.status_code
+        if code in {200, 201, 202, 204}:
+            return
+        raise BadStatus(
+            "Invalid return status {!r} for {!r} operation".format(
+                code, response.request.method
+            )
+        )
+
+    def _poll(self):
+        """Poll status of operation so long as operation is incomplete and
+        we have an endpoint to query.
+
+        :param callable update_cmd: The function to call to retrieve the
+         latest status of the long running operation.
+        :raises: OperationFailed if operation status 'Failed' or 'Canceled'.
+        :raises: BadStatus if response status invalid.
+        :raises: BadResponse if response invalid.
+        """
+
+        while not self.finished():
+            self._delay()
+            self.update_status()
+
+        if TextAnalyticsLROPollingMethod._failed(self.status()):
+            raise OperationFailed("Operation failed or canceled")
+
+        final_get_url = self._operation.get_final_get_url(self._pipeline_response)
+        if final_get_url:
+            self._pipeline_response = self.request_status(final_get_url)
+            TextAnalyticsLROPollingMethod._raise_if_bad_http_status_and_method(self._pipeline_response.http_response)
--- sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_text_analytics_client.py
+++ sdk/textanalytics/azure-ai-textanalytics/azure/ai/textanalytics/_text_analytics_client.py
@@ -16,7 +16,6 @@ from functools import partial
 from six.moves.urllib.parse import urlparse
 from azure.core.paging import ItemPaged
 from azure.core.polling import LROPoller
-from azure.core.polling.base_polling import LROBasePolling
 from azure.core.tracing.decorator import distributed_trace
 from azure.core.exceptions import HttpResponseError
 from ._base_client import TextAnalyticsClientBase
@@ -33,7 +32,7 @@ from ._response_handlers import (
     analyze_paged_result,
     _get_deserialize
 )
-from ._lro import TextAnalyticsOperationResourcePolling, TextAnalyticsOperationState
+from ._lro import TextAnalyticsOperationResourcePolling, TextAnalyticsLROPollingMethod
 
 if TYPE_CHECKING:
     from azure.core.credentials import TokenCredential, AzureKeyCredential
@@ -463,12 +462,11 @@ class TextAnalyticsClient(TextAnalyticsClientBase):
                 model_version=model_version,
                 string_index_type=self._string_code_unit,
                 cls=kwargs.pop("cls", partial(self._healthcare_result_callback, doc_id_order, show_stats=show_stats)),
-                polling=LROBasePolling(
+                polling=TextAnalyticsLROPollingMethod(
                     timeout=polling_interval,
                     lro_algorithms=[
                         TextAnalyticsOperationResourcePolling(show_stats=show_stats)
                     ],
-                    operation_state=TextAnalyticsOperationState(),
                     **kwargs),
                 continuation_token=continuation_token,
                 **kwargs
@@ -768,12 +766,11 @@ class TextAnalyticsClient(TextAnalyticsClientBase):
             return self._client.begin_analyze(
                 body=analyze_body,
                 cls=kwargs.pop("cls", partial(self._analyze_result_callback, doc_id_order, show_stats=show_stats)),
-                polling=LROBasePolling(
+                polling=TextAnalyticsLROPollingMethod(
                     timeout=polling_interval,
                     lro_algorithms=[
                         TextAnalyticsOperationResourcePolling(show_stats=show_stats)
                     ],
-                    operation_state=TextAnalyticsOperationState(),
                     **kwargs),
                 continuation_token=continuation_token,
                 **kwargs
