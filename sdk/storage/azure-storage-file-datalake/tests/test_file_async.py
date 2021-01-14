a08f29002c36b7fe69185d2a81b23e216ae168ab
tasherif-msft <69483382+tasherif-msft@users.noreply.github.com>
2021-01-13 20:48:23 -0800

    Updated Datalake minimum dependency (#16151)
    * Updated datalake minimum dependency
    
    * updated min dep

--- sdk/storage/azure-storage-file-datalake/tests/test_file_async.py
+++ sdk/storage/azure-storage-file-datalake/tests/test_file_async.py
@@ -519,12 +519,10 @@ class FileTest(StorageTestCase):
 
     @record
     def test_account_sas_async(self):
-        pytest.skip("Re-enable this test after min dependency on blobs is updated.")
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self._test_account_sas())
 
     def test_account_sas_raises_if_sas_already_in_uri(self):
-        pytest.skip("Re-enable this test after min dependency on blobs is updated.")
         with self.assertRaises(ValueError):
             DataLakeFileClient(self.dsc.url + "?sig=foo", self.file_system_name, "foo", credential=AzureSasCredential("?foo=bar"))
 
