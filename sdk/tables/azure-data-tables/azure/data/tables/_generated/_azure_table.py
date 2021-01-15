b5803d8ef0fc13db56ac3c934924baf56cb09b61
seankane-msft <seankane@microsoft.com>
2021-01-15 14:12:35 -0500

    updating from new cosmos spec

--- sdk/tables/azure-data-tables/azure/data/tables/_generated/_azure_table.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_generated/_azure_table.py
@@ -28,7 +28,7 @@ class AzureTable(object):
     :vartype table: azure.data.tables.operations.TableOperations
     :ivar service: ServiceOperations operations
     :vartype service: azure.data.tables.operations.ServiceOperations
-    :param url: The URL of the service account or table that is the target of the desired operation.
+    :param url: The URL of the service account or table that is the targe of the desired operation.
     :type url: str
     """
 
