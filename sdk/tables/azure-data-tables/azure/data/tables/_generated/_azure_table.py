62c3c04ff62b7df402f34e3759c9d2c213f46242
seankane-msft <seankane@microsoft.com>
2021-01-15 13:55:09 -0500

    updated generated code with chris scotts changes to api spec

--- sdk/tables/azure-data-tables/azure/data/tables/_generated/_azure_table.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_generated/_azure_table.py
@@ -28,7 +28,7 @@ class AzureTable(object):
     :vartype table: azure.data.tables.operations.TableOperations
     :ivar service: ServiceOperations operations
     :vartype service: azure.data.tables.operations.ServiceOperations
-    :param url: The URL of the service account or table that is the target of the desired operation.
+    :param url: The URL of the service account or table that is the targe of the desired operation.
     :type url: str
     """
 
@@ -44,6 +44,7 @@ class AzureTable(object):
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
+        self._serialize.client_side_validation = False
         self._deserialize = Deserializer(client_models)
 
         self.table = TableOperations(
