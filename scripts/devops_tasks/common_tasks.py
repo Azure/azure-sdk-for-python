61ced2a04c772fc21e8d34eb534df5e4dd680517
scbedd <45376673+scbedd@users.noreply.github.com>
2021-01-14 18:05:44 -0800

    update find_whl in common_tasks to do the same logic that our previous one did

--- scripts/devops_tasks/common_tasks.py
+++ scripts/devops_tasks/common_tasks.py
@@ -353,8 +353,9 @@ def find_whl(package_name, version, whl_directory):
     parsed_version = parse(version)
 
     logging.info("Searching whl for package {0}-{1}".format(package_name, parsed_version.base_version))
-    whl_name = "**/{0}-{1}-*.whl".format(package_name.replace("-", "_"), parsed_version.base_version)
-    paths = glob.glob(os.path.join(whl_directory, whl_name), recursive=True)
+    whl_name_format = "{0}-{1}-*.whl".format(pkg_name.replace("-", "_"), pkg_version)
+    whls = [os.path.relpath(w, whl_dir) for w in glob.glob(os.path.join(whl_dir, "**", whl_name_format), recursive=True)]
+
     if not paths:
         logging.error(
             "whl is not found in whl directory {0} for package {1}-{2}".format(
