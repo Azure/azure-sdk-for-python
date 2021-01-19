7103bf7d7866419bd25fde37ba58560e5b5c70ad
turalf <tufarhad@microsoft.com>
2021-01-19 12:11:27 -0800

    Add CommunicationIdentifierModel to models

--- sdk/communication/azure-communication-chat/azure/communication/chat/_shared/models.py
+++ sdk/communication/azure-communication-chat/azure/communication/chat/_shared/models.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
+import msrest
 
 class CommunicationUserIdentifier(object):
     """
@@ -53,3 +54,44 @@ class MicrosoftTeamsUserIdentifier(object):
     def __init__(self, user_id, is_anonymous=False):
         self.user_id = user_id
         self.is_anonymous = is_anonymous
+
+class CommunicationIdentifierModel(msrest.serialization.Model):
+    """Test.
+
+    All required parameters must be populated in order to send to Azure.
+
+    :param kind: Required. Test.
+    :type kind: str
+    :param id: Test.
+    :type id: str
+    :param phone_number: Test.
+    :type phone_number: str
+    :param is_anonymous: Test.
+    :type is_anonymous: bool
+    :param microsoft_teams_user_id: Test.
+    :type microsoft_teams_user_id: str
+    """
+
+    _validation = {
+        'kind': {'required': True},
+    }
+
+    _attribute_map = {
+        'kind': {'key': 'kind', 'type': 'str'},
+        'id': {'key': 'id', 'type': 'str'},
+        'phone_number': {'key': 'phoneNumber', 'type': 'str'},
+        'is_anonymous': {'key': 'isAnonymous', 'type': 'bool'},
+        'microsoft_teams_user_id': {'key': 'microsoftTeamsUserId', 'type': 'str'},
+    }
+
+    def __init__(
+        self,
+        **kwargs
+    ):
+        super(CommunicationIdentifierModel, self).__init__(**kwargs)
+        self.kind = kwargs['kind']
+        self.id = kwargs.get('id', None)
+        self.phone_number = kwargs.get('phone_number', None)
+        self.is_anonymous = kwargs.get('is_anonymous', None)
+        self.microsoft_teams_user_id = kwargs.get('microsoft_teams_user_id', None)
+
