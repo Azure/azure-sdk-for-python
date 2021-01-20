cea0c6320e62ca4d6f90347e122ec3b4a9a34cf3
Charles Lowell <chlowe@microsoft.com>
2021-01-20 14:41:05 -0800

    BearerTokenCredentialPolicy handles CAE claims challenges

--- sdk/core/azure-core/azure/core/pipeline/policies/_authentication.py
+++ sdk/core/azure-core/azure/core/pipeline/policies/_authentication.py
@@ -3,10 +3,14 @@
 # Licensed under the MIT License. See LICENSE.txt in the project root for
 # license information.
 # -------------------------------------------------------------------------
+import base64
+from collections import namedtuple
+import re
 import time
 import six
 
-from . import SansIOHTTPPolicy
+from . import HTTPPolicy, SansIOHTTPPolicy
+from .._tools import await_result
 from ...exceptions import ServiceRequestError
 
 try:
@@ -16,9 +20,10 @@ except ImportError:
 
 if TYPE_CHECKING:
     # pylint:disable=unused-import
-    from typing import Any, Dict, Optional
+    from typing import Any, Dict, List, Optional
     from azure.core.credentials import AccessToken, TokenCredential, AzureKeyCredential, AzureSasCredential
     from azure.core.pipeline import PipelineRequest
+    from azure.core.pipeline import PipelineRequest, PipelineResponse
 
 
 # pylint:disable=too-few-public-methods
@@ -71,27 +76,111 @@ class _BearerTokenCredentialPolicyBase(object):
         return not self._token or self._token.expires_on - time.time() < 300
 
 
-class BearerTokenCredentialPolicy(_BearerTokenCredentialPolicyBase, SansIOHTTPPolicy):
+class BearerTokenCredentialPolicy(_BearerTokenCredentialPolicyBase, HTTPPolicy):
     """Adds a bearer token Authorization header to requests.
 
-    :param credential: The credential.
-    :type credential: ~azure.core.TokenCredential
-    :param str scopes: Lets you specify the type of access needed.
-    :raises: :class:`~azure.core.exceptions.ServiceRequestError`
+    :param ~azure.core.credentials.TokenCredential credential: credential for authorizing requests
+    :param str scopes: required authentication scopes
     """
 
     def on_request(self, request):
-        # type: (PipelineRequest) -> None
+        """This method is for backward compatibility. It has no implementation."""
+
+    def on_response(self, request, response):
+        """This method is for backward compatibility. It has no implementation."""
+
+    def on_exception(self, request):
+        """This method is for backward compatibility. It has no implementation."""
+
+    def send(self, request):
+        # type: (PipelineRequest) -> PipelineResponse
         """Adds a bearer token Authorization header to request and sends request to next policy.
 
-        :param request: The pipeline request object
-        :type request: ~azure.core.pipeline.PipelineRequest
+        :param ~azure.core.pipeline.PipelineRequest request: The request
         """
+
+        # this is copied from SansIOHTTPPolicy for backward compatibility
+        await_result(self.on_request, request)
+        try:
+            response = self._send(request)
+        except Exception:  # pylint: disable=broad-except
+            if not await_result(self.on_exception, request):
+                raise
+        else:
+            await_result(self.on_response, request, response)
+        return response
+
+    def _send(self, request):
+        # type: (PipelineRequest) -> PipelineResponse
         self._enforce_https(request)
+        self.on_before_request(request)
+
+        response = self.next.send(request)
+
+        if response.http_response.status_code == 401:
+            self._token = None  # any cached token is invalid
+            challenge = response.http_response.headers.get("WWW-Authenticate")
+            if challenge and self.on_challenge(request, challenge):
+                response = self.next.send(request)
+
+        return response
+
+    def on_before_request(self, request):
+        # type: (PipelineRequest) -> None
+        """Executed before sending the request.
+
+        Base implementation authorizes `request`, acquiring an access token as necessary.
+        """
 
         if self._token is None or self._need_new_token:
             self._token = self._credential.get_token(*self._scopes)
         self._update_headers(request.http_request.headers, self._token.token)
+
+    def on_challenge(self, request, challenge):
+        # type: (PipelineRequest, str) -> bool
+        """Authorize request according to an authentication challenge.
+
+        Base implementation handles CAE claims directives. Clients expecting other challenges must override.
+
+        :param ~azure.core.pipeline.PipelineRequest request: the request which elicited an authentication challenge
+        :param str challenge: the response's WWW-Authenticate header, unparsed. It may contain multiple challenges.
+        :return: a bool indicating whether the method satisfied the challenge
+        """
+
+        parsed_challenges = _parse_challenges(challenge)
+        if len(parsed_challenges) != 1 or "claims" not in parsed_challenges[0].parameters:
+            # no or multiple challenges, or no claims directive
+            return False
+
+        encoded_claims = parsed_challenges[0].parameters["claims"]
+        padding_needed = 4 - len(encoded_claims) % 4
+        try:
+            claims = base64.urlsafe_b64decode(encoded_claims + "=" * padding_needed).decode()
+        except Exception:  # pylint:disable=broad-except
+            return False
+
+        self._token = self._credential.get_token(*self._scopes, claims_challenge=claims)
+        self._update_headers(request.http_request.headers, self._token.token)
+        return True
+
+
+# these expressions are for challenges with comma delimited parameters having quoted values, e.g.
+# Bearer authorization="https://login.microsoftonline.com/", resource="https://vault.azure.net"
+_AUTHENTICATION_CHALLENGE = re.compile(r'(?:(\w+) ((?:\w+=".*?"(?:, )?)+)(?:, )?)')
+_CHALLENGE_PARAMETER = re.compile(r'(?:(\w+)="([^"]*)")+')
+
+_AuthenticationChallenge = namedtuple("_AuthenticationChallenge", "scheme,parameters")
+
+
+def _parse_challenges(header):
+    # type: (str) -> List[_AuthenticationChallenge]
+    result = []
+    challenges = re.findall(_AUTHENTICATION_CHALLENGE, header)
+    for scheme, parameter_list in challenges:
+        parameters = re.findall(_CHALLENGE_PARAMETER, parameter_list)
+        challenge = _AuthenticationChallenge(scheme, dict(parameters))
+        result.append(challenge)
+    return result
 
 
 class AzureKeyCredentialPolicy(SansIOHTTPPolicy):
