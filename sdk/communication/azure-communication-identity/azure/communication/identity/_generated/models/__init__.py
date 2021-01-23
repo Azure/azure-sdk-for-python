4ecbdd0552620fe6715b5ea8e46d4d5ead01e1ee
Bert Ong <823691+beltr0n@users.noreply.github.com>
2021-01-22 17:15:29 -0800

    update swagger + re-generate code

--- sdk/communication/azure-communication-identity/azure/communication/identity/_generated/models/__init__.py
+++ sdk/communication/azure-communication-identity/azure/communication/identity/_generated/models/__init__.py
@@ -8,18 +8,26 @@
 
 try:
     from ._models_py3 import CommunicationIdentity
-    from ._models_py3 import CommunicationIdentityToken
-    from ._models_py3 import CommunicationIdentityUpdateRequest
-    from ._models_py3 import CommunicationTokenRequest
+    from ._models_py3 import CommunicationIdentityAccessTokenRequest
+    from ._models_py3 import CommunicationIdentityAccessTokenResult
+    from ._models_py3 import CommunicationIdentityCreateRequest
+    from ._models_py3 import CommunicationUserToken
 except (SyntaxError, ImportError):
     from ._models import CommunicationIdentity  # type: ignore
-    from ._models import CommunicationIdentityToken  # type: ignore
-    from ._models import CommunicationIdentityUpdateRequest  # type: ignore
-    from ._models import CommunicationTokenRequest  # type: ignore
+    from ._models import CommunicationIdentityAccessTokenRequest  # type: ignore
+    from ._models import CommunicationIdentityAccessTokenResult  # type: ignore
+    from ._models import CommunicationIdentityCreateRequest  # type: ignore
+    from ._models import CommunicationUserToken  # type: ignore
+
+from ._communication_identity_client_enums import (
+    CommunicationTokenScope,
+)
 
 __all__ = [
     'CommunicationIdentity',
-    'CommunicationIdentityToken',
-    'CommunicationIdentityUpdateRequest',
-    'CommunicationTokenRequest',
+    'CommunicationIdentityAccessTokenRequest',
+    'CommunicationIdentityAccessTokenResult',
+    'CommunicationIdentityCreateRequest',
+    'CommunicationUserToken',
+    'CommunicationTokenScope',
 ]
