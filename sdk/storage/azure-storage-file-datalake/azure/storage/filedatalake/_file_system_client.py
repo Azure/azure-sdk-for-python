ee2303bf7e9718a0977a84c8ba201636c22bcd79
Tamer Sherif <tasherif@microsoft.com>
2021-01-22 16:04:36 -0800

    removed imports

--- sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_file_system_client.py
+++ sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_file_system_client.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-import functools
 from typing import Optional
 
 from ._deserialize import deserialize_path_properties
