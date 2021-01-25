ae82df38bb80e439419c90632423c8b5cc881159
seankane-msft <seankane@microsoft.com>
2021-01-25 13:28:52 -0500

    removing the raise exception on run_check_call in gen_cov_xml()

--- scripts/devops_tasks/create_coverage.py
+++ scripts/devops_tasks/create_coverage.py
@@ -112,8 +112,7 @@ def generate_coverage_xml():
     if os.path.exists(coverage_dir):
         logging.info("Generating coverage XML")
         commands = ["coverage", "xml", "-i", "--omit", '"*test*,*example*"']
-        err = run_check_call(commands, root_dir, always_exit = False)
-        raise err
+        run_check_call(commands, root_dir, always_exit = False)
     else:
         logging.error("Coverage file is not available in {} to generate coverage XML".format(coverage_dir))
 
