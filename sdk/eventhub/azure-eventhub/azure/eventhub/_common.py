5949411f429990cf3ede656cfc6aa199993c6e34
Swathi Pillalamarri <swathip@microsoft.com>
2021-01-25 16:06:13 -0500

    add contains to DictMixin

--- sdk/eventhub/azure-eventhub/azure/eventhub/_common.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_common.py
@@ -447,6 +447,9 @@ class DictMixin(object):
         # type: (Any) -> Any
         return self.__dict__[key]
 
+    def __contains__(self, key):
+        return key in self.__dict__
+
     def __repr__(self):
         # type: () -> str
         return str(self)
