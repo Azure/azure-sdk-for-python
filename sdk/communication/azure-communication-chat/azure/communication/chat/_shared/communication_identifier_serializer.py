c12bce4ba173c247012cd6b1774d2320b93f33ba
turalf <tufarhad@microsoft.com>
2021-01-27 14:17:37 -0800

    Make id mandatory for deserilize

--- sdk/communication/azure-communication-chat/azure/communication/chat/_shared/communication_identifier_serializer.py
+++ sdk/communication/azure-communication-chat/azure/communication/chat/_shared/communication_identifier_serializer.py
@@ -32,12 +32,14 @@ class CommunicationUserIdentifierSerializer(object):
         if isinstance(communicationIdentifier, PhoneNumberIdentifier):
             return CommunicationIdentifierModel(
                 kind=CommunicationIdentifierKind.PhoneNumber,
-                id=communicationIdentifier.phone_number
+                id=communicationIdentifier.identifier,
+                phone_number=communicationIdentifier.phone_number
             )
         if isinstance(communicationIdentifier, MicrosoftTeamsUserIdentifier):
             return CommunicationIdentifierModel(
                 kind=CommunicationIdentifierKind.MicrosoftTeamsUser,
-                id=communicationIdentifier.user_id,
+                id=communicationIdentifier.identifier,
+                microsoft_teams_user_id =communicationIdentifier.user_id,
                 communication_cloud_environment=communicationIdentifier.cloud
             )
 
@@ -59,27 +61,27 @@ class CommunicationUserIdentifierSerializer(object):
         """
 
         identifier, kind = identifierModel.id, identifierModel.kind
+        if not identifier:
+            raise ValueError("Identifier must have a valid id")
 
         if kind == CommunicationIdentifierKind.CommunicationUser:
-            if not identifier:
-                raise ValueError("CommunictionUser must have a valid id")
             return CommunicationUserIdentifier(id)
         if kind == CommunicationIdentifierKind.PhoneNumber:
             if not identifierModel.phone_number:
                 raise ValueError("PhoneNumberIdentifier must have a valid attribute - phone_number")
-            return PhoneNumberIdentifier(identifierModel.phone_number)
+            return PhoneNumberIdentifier(identifierModel.phone_number, identifier=identifier)
         if kind == CommunicationIdentifierKind.MicrosoftTeamsUser:
             if identifierModel.is_anonymous not in [True, False]:
                 raise ValueError("MicrosoftTeamsUser must have a valid attribute - is_anonymous")
             if not identifierModel.microsoft_teams_user_id:
                 raise ValueError("MicrosoftTeamsUser must have a valid attribute - microsoft_teams_user_id")
+            if not identifierModel.communication_cloud_environment:
+                raise ValueError("MicrosoftTeamsUser must have a valid attribute - communication_cloud_environment")
             return MicrosoftTeamsUserIdentifier(
                 identifierModel.microsoft_teams_user_id,
+                identifier=identifier,
                 is_anonymous=identifierModel.is_anonymous,
                 cloud=identifierModel.communication_cloud_environment
             )
-
-        if not identifier:
-            raise ValueError("UnknownIdentifier must have a valid id")
 
         return UnknownIdentifier(identifier)
