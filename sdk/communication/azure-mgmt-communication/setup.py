61e3341d0a98d5ed12dbc3bdcb26a9532109ecf3
turalf <tufarhad@microsoft.com>
2021-01-29 11:20:43 -0800

    Update the core dependency in mgmt package.

--- sdk/communication/azure-mgmt-communication/setup.py
+++ sdk/communication/azure-mgmt-communication/setup.py
@@ -84,7 +84,7 @@ setup(
         'msrest>=0.5.0',
         'msrestazure>=0.4.32,<2.0.0',
         'azure-common~=1.1',
-        "azure-core<2.0.0,>=1.10.1",
+        "azure-core<2.0.0,>=1.9.0",
         'azure-mgmt-core>=1.2.0,<2.0.0',
     ],
     extras_require={
