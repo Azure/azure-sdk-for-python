0462893979d88395bd0a7e3f8ddca316378dbca2
Krista Pratico <krpratic@microsoft.com>
2021-02-04 08:39:14 -0800

    [formrecognizer] use ARM template for tests (#16432)
    * try something
    
    try again
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    skipping most tests so i can test quickly
    
    try
    
    speed up testing even more
    
    even faster
    
    even faster
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    * add test-resources.json files
    
    * refactor testcase and preparers
    
    * refactor all tests
    
    * fix before next test run
    
    * unskip tests
    
    * fix for recorded tests
    
    * fix operation location replacer
    
    * don't use images hosted on github
    
    * fix envar
    
    * fix pipeline error
    
    * try fix pipeline error

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_training_async.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_training_async.py
@@ -11,9 +11,9 @@ from azure.core.exceptions import ClientAuthenticationError, HttpResponseError
 from azure.ai.formrecognizer._generated.models import Model
 from azure.ai.formrecognizer._models import CustomFormModel
 from azure.ai.formrecognizer.aio import FormTrainingClient
-from testcase import FormRecognizerTest, GlobalFormRecognizerAccountPreparer
+from preparers import FormRecognizerPreparer
 from asynctestcase import AsyncFormRecognizerTest
-from testcase import GlobalClientPreparer as _GlobalClientPreparer
+from preparers import GlobalClientPreparer as _GlobalClientPreparer
 
 
 GlobalClientPreparer = functools.partial(_GlobalClientPreparer, FormTrainingClient)
@@ -21,24 +21,24 @@ GlobalClientPreparer = functools.partial(_GlobalClientPreparer, FormTrainingClie
 
 class TestTrainingAsync(AsyncFormRecognizerTest):
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True)
-    async def test_polling_interval(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_polling_interval(self, client, formrecognizer_storage_container_sas_url):
         def check_poll_value(poll):
             if self.is_live:
                 self.assertEqual(poll, 5)
             else:
                 self.assertEqual(poll, 0)
         check_poll_value(client._client._config.polling_interval)
-        poller = await client.begin_training(training_files_url=container_sas_url, use_training_labels=False, polling_interval=6)
+        poller = await client.begin_training(training_files_url=formrecognizer_storage_container_sas_url, use_training_labels=False, polling_interval=6)
         await poller.wait()
         self.assertEqual(poller._polling_method._timeout, 6)
-        poller2 = await client.begin_training(training_files_url=container_sas_url, use_training_labels=False)
+        poller2 = await client.begin_training(training_files_url=formrecognizer_storage_container_sas_url, use_training_labels=False)
         await poller2.wait()
         check_poll_value(poller2._polling_method._timeout)  # goes back to client default
         await client.close()
 
-    @GlobalFormRecognizerAccountPreparer()
+    @FormRecognizerPreparer()
     @GlobalClientPreparer()
     async def test_training_encoded_url(self, client):
         with self.assertRaises(HttpResponseError):
@@ -50,21 +50,21 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
                 self.assertIn("https://fakeuri.com/blank%20space", poller._polling_method._initial_response.http_request.body)
                 await poller.wait()
 
-    @GlobalFormRecognizerAccountPreparer()
-    async def test_training_auth_bad_key(self, resource_group, location, form_recognizer_account, form_recognizer_account_key):
-        client = FormTrainingClient(form_recognizer_account, AzureKeyCredential("xxxx"))
+    @FormRecognizerPreparer()
+    async def test_training_auth_bad_key(self, formrecognizer_test_endpoint, formrecognizer_test_api_key):
+        client = FormTrainingClient(formrecognizer_test_endpoint, AzureKeyCredential("xxxx"))
         with self.assertRaises(ClientAuthenticationError):
             async with client:
                 poller = await client.begin_training("xx", use_training_labels=False)
                 result = await poller.result()
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True)
-    async def test_training(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_training(self, client, formrecognizer_storage_container_sas_url):
 
         async with client:
             poller = await client.begin_training(
-                training_files_url=container_sas_url,
+                training_files_url=formrecognizer_storage_container_sas_url,
                 use_training_labels=False,
                 model_name="my unlabeled model")
             model = await poller.result()
@@ -86,11 +86,11 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
                 self.assertIsNotNone(field.label)
                 self.assertIsNotNone(field.name)
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True, multipage=True)
-    async def test_training_multipage(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_training_multipage(self, client, formrecognizer_multipage_storage_container_sas_url):
         async with client:
-            poller = await client.begin_training(container_sas_url, use_training_labels=False)
+            poller = await client.begin_training(formrecognizer_multipage_storage_container_sas_url, use_training_labels=False)
             model = await poller.result()
 
         self.assertIsNotNone(model.model_id)
@@ -109,9 +109,9 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
                 self.assertIsNotNone(field.label)
                 self.assertIsNotNone(field.name)
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True)
-    async def test_training_transform(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_training_transform(self, client, formrecognizer_storage_container_sas_url):
 
         raw_response = []
 
@@ -123,7 +123,7 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
 
         async with client:
             poller = await client.begin_training(
-                training_files_url=container_sas_url,
+                training_files_url=formrecognizer_storage_container_sas_url,
                 use_training_labels=False,
                 cls=callback)
             model = await poller.result()
@@ -132,9 +132,9 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
         custom_model = raw_response[1]
         self.assertModelTransformCorrect(custom_model, raw_model, unlabeled=True)
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True, multipage=True)
-    async def test_training_multipage_transform(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_training_multipage_transform(self, client, formrecognizer_multipage_storage_container_sas_url):
 
         raw_response = []
 
@@ -145,18 +145,18 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
             raw_response.append(custom_model)
 
         async with client:
-            poller = await client.begin_training(container_sas_url, use_training_labels=False, cls=callback)
+            poller = await client.begin_training(formrecognizer_multipage_storage_container_sas_url, use_training_labels=False, cls=callback)
             model = await poller.result()
 
         raw_model = raw_response[0]
         custom_model = raw_response[1]
         self.assertModelTransformCorrect(custom_model, raw_model, unlabeled=True)
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True)
-    async def test_training_with_labels(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_training_with_labels(self, client, formrecognizer_storage_container_sas_url):
         async with client:
-            poller = await client.begin_training(training_files_url=container_sas_url, use_training_labels=True, model_name="my labeled model")
+            poller = await client.begin_training(training_files_url=formrecognizer_storage_container_sas_url, use_training_labels=True, model_name="my labeled model")
             model = await poller.result()
 
         self.assertIsNotNone(model.model_id)
@@ -176,11 +176,11 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
                 self.assertIsNotNone(field.accuracy)
                 self.assertIsNotNone(field.name)
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True, multipage=True)
-    async def test_training_multipage_with_labels(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_training_multipage_with_labels(self, client, formrecognizer_multipage_storage_container_sas_url):
         async with client:
-            poller = await client.begin_training(container_sas_url, use_training_labels=True)
+            poller = await client.begin_training(formrecognizer_multipage_storage_container_sas_url, use_training_labels=True)
             model = await poller.result()
 
         self.assertIsNotNone(model.model_id)
@@ -200,9 +200,9 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
                 self.assertIsNotNone(field.accuracy)
                 self.assertIsNotNone(field.name)
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True)
-    async def test_training_with_labels_transform(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_training_with_labels_transform(self, client, formrecognizer_storage_container_sas_url):
 
         raw_response = []
 
@@ -213,16 +213,16 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
             raw_response.append(custom_model)
 
         async with client:
-            poller = await client.begin_training(training_files_url=container_sas_url, use_training_labels=True, cls=callback)
+            poller = await client.begin_training(training_files_url=formrecognizer_storage_container_sas_url, use_training_labels=True, cls=callback)
             model = await poller.result()
 
         raw_model = raw_response[0]
         custom_model = raw_response[1]
         self.assertModelTransformCorrect(custom_model, raw_model)
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True, multipage=True)
-    async def test_train_multipage_w_lbls_trnsfrm(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_train_multipage_w_lbls_trnsfrm(self, client, formrecognizer_multipage_storage_container_sas_url):
 
         raw_response = []
 
@@ -233,48 +233,48 @@ class TestTrainingAsync(AsyncFormRecognizerTest):
             raw_response.append(custom_model)
 
         async with client:
-            poller = await client.begin_training(container_sas_url, use_training_labels=True, cls=callback)
+            poller = await client.begin_training(formrecognizer_multipage_storage_container_sas_url, use_training_labels=True, cls=callback)
             model = await poller.result()
 
         raw_model = raw_response[0]
         custom_model = raw_response[1]
         self.assertModelTransformCorrect(custom_model, raw_model)
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True)
-    async def test_training_with_files_filter(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
+    async def test_training_with_files_filter(self, client, formrecognizer_storage_container_sas_url):
         async with client:
-            poller = await client.begin_training(training_files_url=container_sas_url, use_training_labels=False, include_subfolders=True)
+            poller = await client.begin_training(training_files_url=formrecognizer_storage_container_sas_url, use_training_labels=False, include_subfolders=True)
             model = await poller.result()
             self.assertEqual(len(model.training_documents), 6)
             self.assertEqual(model.training_documents[-1].name, "subfolder/Form_6.jpg")  # we traversed subfolders
 
-            poller = await client.begin_training(container_sas_url, use_training_labels=False, prefix="subfolder", include_subfolders=True)
+            poller = await client.begin_training(formrecognizer_storage_container_sas_url, use_training_labels=False, prefix="subfolder", include_subfolders=True)
             model = await poller.result()
             self.assertEqual(len(model.training_documents), 1)
             self.assertEqual(model.training_documents[0].name, "subfolder/Form_6.jpg")  # we filtered for only subfolders
 
             with pytest.raises(HttpResponseError) as e:
-                poller = await client.begin_training(training_files_url=container_sas_url, use_training_labels=False, prefix="xxx")
+                poller = await client.begin_training(training_files_url=formrecognizer_storage_container_sas_url, use_training_labels=False, prefix="xxx")
                 model = await poller.result()
             self.assertIsNotNone(e.value.error.code)
             self.assertIsNotNone(e.value.error.message)
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True)
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer()
     @pytest.mark.live_test_only
-    async def test_training_continuation_token(self, client, container_sas_url):
+    async def test_training_continuation_token(self, client, formrecognizer_storage_container_sas_url):
         async with client:
-            initial_poller = await client.begin_training(training_files_url=container_sas_url, use_training_labels=False)
+            initial_poller = await client.begin_training(training_files_url=formrecognizer_storage_container_sas_url, use_training_labels=False)
             cont_token = initial_poller.continuation_token()
             poller = await client.begin_training(None, None, continuation_token=cont_token)
             result = await poller.result()
             self.assertIsNotNone(result)
             await initial_poller.wait()  # necessary so azure-devtools doesn't throw assertion error
 
-    @GlobalFormRecognizerAccountPreparer()
-    @GlobalClientPreparer(training=True, client_kwargs={"api_version": "2.0"})
-    async def test_training_with_model_name_bad_api_version(self, client, container_sas_url):
+    @FormRecognizerPreparer()
+    @GlobalClientPreparer(client_kwargs={"api_version": "2.0"})
+    async def test_training_with_model_name_bad_api_version(self, client, formrecognizer_storage_container_sas_url):
         with pytest.raises(ValueError) as excinfo:
             poller = await client.begin_training(training_files_url="url", use_training_labels=True, model_name="not supported in v2.0")
             result = await poller.result()
