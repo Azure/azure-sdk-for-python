1d02efcb1e914b52da207931c917d30653afb0b1
Krista Pratico <krpratic@microsoft.com>
2021-02-04 08:39:14 -0800

    [formrecognizer] use ARM template for tests (#16432)
    * try something
    
    try again
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    skipping most tests so i can test quickly
    
    try
    
    speed up testing even more
    
    even faster
    
    even faster
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    try
    
    * add test-resources.json files
    
    * refactor testcase and preparers
    
    * refactor all tests
    
    * fix before next test run
    
    * unskip tests
    
    * fix for recorded tests
    
    * fix operation location replacer
    
    * don't use images hosted on github
    
    * fix envar
    
    * fix pipeline error
    
    * try fix pipeline error

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_logging.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_logging.py
@@ -9,7 +9,8 @@ import logging
 import pytest
 from azure.ai.formrecognizer import FormRecognizerClient, FormTrainingClient
 from azure.core.credentials import AzureKeyCredential
-from testcase import FormRecognizerTest, GlobalFormRecognizerAccountPreparer
+from testcase import FormRecognizerTest
+from preparers import FormRecognizerPreparer
 
 class MockHandler(logging.Handler):
     def __init__(self):
@@ -22,10 +23,10 @@ class MockHandler(logging.Handler):
 
 class TestLogging(FormRecognizerTest):
 
-    @GlobalFormRecognizerAccountPreparer()
+    @FormRecognizerPreparer()
     @pytest.mark.live_test_only
-    def test_logging_info_fr_client(self, resource_group, location, form_recognizer_account, form_recognizer_account_key):
-        client = FormRecognizerClient(form_recognizer_account, AzureKeyCredential(form_recognizer_account_key))
+    def test_logging_info_fr_client(self, formrecognizer_test_endpoint, formrecognizer_test_api_key):
+        client = FormRecognizerClient(formrecognizer_test_endpoint, AzureKeyCredential(formrecognizer_test_api_key))
         mock_handler = MockHandler()
 
         logger = logging.getLogger("azure")
@@ -43,10 +44,10 @@ class TestLogging(FormRecognizerTest):
                 else:
                     assert message.message.find("REDACTED") == -1
 
-    @GlobalFormRecognizerAccountPreparer()
+    @FormRecognizerPreparer()
     @pytest.mark.live_test_only
-    def test_logging_info_ft_client(self, resource_group, location, form_recognizer_account, form_recognizer_account_key):
-        client = FormTrainingClient(form_recognizer_account, AzureKeyCredential(form_recognizer_account_key))
+    def test_logging_info_ft_client(self, formrecognizer_test_endpoint, formrecognizer_test_api_key):
+        client = FormTrainingClient(formrecognizer_test_endpoint, AzureKeyCredential(formrecognizer_test_api_key))
         mock_handler = MockHandler()
 
         logger = logging.getLogger("azure")
