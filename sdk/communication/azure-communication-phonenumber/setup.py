01684d95417265b335bcb14c65981cc2d3ededb6
Sam Cheung <sacheu@microsoft.com>
2021-02-03 23:22:06 -0800

    Fix requires version

--- sdk/communication/azure-communication-phonenumber/setup.py
+++ sdk/communication/azure-communication-phonenumber/setup.py
@@ -63,7 +63,7 @@ setup(
         'azure.communication'
     ]),
     install_requires=[
-        "msrest>=0.5.0",
+        "msrest>=0.6.0",
         "azure-core<2.0.0,>=1.9.0",
     ],
     extras_require={
