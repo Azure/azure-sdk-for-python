67ec6b16d9e106f1fec7a0ada72e1a3eb81b453d
Xiang Yan <xiangyan@microsoft.com>
2021-02-04 13:21:51 -0800

    update

--- sdk/search/azure-search-documents/setup.py
+++ sdk/search/azure-search-documents/setup.py
@@ -79,8 +79,7 @@ setup(
     install_requires=[
         "azure-core<2.0.0,>=1.4.0",
         "msrest>=0.6.10",
-        "typing-extensions",
-        'six>=1.11.0'
+        "typing-extensions"
     ],
     extras_require={
         ":python_version<'3.0'": ['futures', 'azure-search-nspkg'],
