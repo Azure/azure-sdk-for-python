4860a267a363af51f646ec92b3e8f1246ceb4a1c
McCoy Patiño <mcpatino@microsoft.com>
2021-02-05 16:22:56 -0800

    Update tests, re-record

--- sdk/keyvault/azure-keyvault-keys/tests/test_crypto_client_async.py
+++ sdk/keyvault/azure-keyvault-keys/tests/test_crypto_client_async.py
@@ -286,16 +286,17 @@ class CryptoClientTests(KeyVaultTestCase):
         the_year_3000 = datetime(3000, 1, 1, tzinfo=_UTC)
 
         rsa_wrap_algorithms = [algo for algo in KeyWrapAlgorithm if algo.startswith("RSA")]
+        rsa_encryption_algorithms = [algo for algo in EncryptionAlgorithm if algo.startswith("RSA")]
         key_name = self.get_resource_name("rsa-not-yet-valid")
         not_yet_valid_key = await key_client.create_rsa_key(key_name, not_before=the_year_3000)
-        await test_operations(not_yet_valid_key, [str(the_year_3000)], EncryptionAlgorithm, rsa_wrap_algorithms)
+        await test_operations(not_yet_valid_key, [str(the_year_3000)], rsa_encryption_algorithms, rsa_wrap_algorithms)
 
         # nor should they succeed with a key whose exp has passed
         the_year_2000 = datetime(2000, 1, 1, tzinfo=_UTC)
 
         key_name = self.get_resource_name("rsa-expired")
         expired_key = await key_client.create_rsa_key(key_name, expires_on=the_year_2000)
-        await test_operations(expired_key, [str(the_year_2000)], EncryptionAlgorithm, rsa_wrap_algorithms)
+        await test_operations(expired_key, [str(the_year_2000)], rsa_encryption_algorithms, rsa_wrap_algorithms)
 
         # when exp and nbf are set, error messages should contain both
         the_year_3001 = datetime(3001, 1, 1, tzinfo=_UTC)
@@ -303,8 +304,39 @@ class CryptoClientTests(KeyVaultTestCase):
         key_name = self.get_resource_name("rsa-valid")
         valid_key = await key_client.create_rsa_key(key_name, not_before=the_year_3000, expires_on=the_year_3001)
         await test_operations(
-            valid_key, (str(the_year_3000), str(the_year_3001)), EncryptionAlgorithm, rsa_wrap_algorithms
+            valid_key, (str(the_year_3000), str(the_year_3001)), rsa_encryption_algorithms, rsa_wrap_algorithms
         )
+
+    @KeyVaultPreparer()
+    async def test_encrypt_valid_arguments(self, azure_keyvault_url, **kwargs):
+        key_client = self.create_key_client(azure_keyvault_url)
+        key_name = self.get_resource_name("rsa-key")
+        rsa_key = await key_client.create_rsa_key(key_name)
+
+        crypto_client = self.create_crypto_client(rsa_key)
+        with pytest.raises(ValueError) as ex:
+            await crypto_client.encrypt(EncryptionAlgorithm.rsa_oaep, b"...", iv=b"...")
+        assert "iv" in str(ex.value)
+        with pytest.raises(ValueError) as ex:
+            await crypto_client.encrypt(EncryptionAlgorithm.rsa_oaep, b"...", additional_authenticated_data=b"...")
+        assert "additional_authenticated_data" in str(ex.value)
+
+    @KeyVaultPreparer()
+    async def test_decrypt_valid_arguments(self, azure_keyvault_url, **kwargs):
+        key_client = self.create_key_client(azure_keyvault_url)
+        key_name = self.get_resource_name("rsa-key")
+        rsa_key = await key_client.create_rsa_key(key_name)
+
+        crypto_client = self.create_crypto_client(rsa_key)
+        with pytest.raises(ValueError) as ex:
+            await crypto_client.decrypt(EncryptionAlgorithm.rsa_oaep, b"...", iv=b"...")
+        assert "iv" in str(ex.value)
+        with pytest.raises(ValueError) as ex:
+            await crypto_client.decrypt(EncryptionAlgorithm.rsa_oaep, b"...", additional_authenticated_data=b"...")
+        assert "additional_authenticated_data" in str(ex.value)
+        with pytest.raises(ValueError) as ex:
+            await crypto_client.decrypt(EncryptionAlgorithm.rsa_oaep, b"...", authentication_tag=b"...")
+        assert "authentication_tag" in str(ex.value)
 
 
 def test_custom_hook_policy():
