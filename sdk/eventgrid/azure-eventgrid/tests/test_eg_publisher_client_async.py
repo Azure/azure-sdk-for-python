d30ed521db6443dbe3d8efe20f480347eb9d5612
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2021-02-09 10:42:28 -0800

    Fix tests

--- sdk/eventgrid/azure-eventgrid/tests/test_eg_publisher_client_async.py
+++ sdk/eventgrid/azure-eventgrid/tests/test_eg_publisher_client_async.py
@@ -101,8 +101,10 @@ class EventGridPublisherClientTests(AzureMgmtTestCase):
         eg_event = {
                 "subject":"sample", 
                 "data":b"eventgridevent", 
-                "event_type":"Sample.EventGrid.Event",
-                "data_version":"2.0"
+                "eventType":"Sample.EventGrid.Event",
+                "dataVersion":"2.0",
+                "id": "123-ddf-133-324255ffd",
+                "eventTime": dt.datetime.utcnow()
         }
         with pytest.raises(TypeError, match="Data in EventGridEvent cannot be bytes*"):
             await client.send(eg_event)
