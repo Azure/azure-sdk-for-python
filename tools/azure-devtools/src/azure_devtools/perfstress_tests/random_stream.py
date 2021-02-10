e550447f993a1831dde06e6a9f0cdd8531dda8eb
annatisch <antisch@microsoft.com>
2021-02-11 06:19:04 +1300

    [Perfstress][Storage] Added Blobs perf tests (#15833)
    * First pass
    
    * Update blob tests
    
    * Added writestream to framework
    
    * Blob tests memory
    
    * Make streams seekable
    
    * Fix upload tests
    
    * More efficient upload
    
    * Fix listing memory
    
    * Support reset between iterations
    
    * Some efficiencies
    
    * Rethink of reset
    
    * Reset first
    
    * Added readme
    
    * Fix readme typo
    
    Co-authored-by: tasherif-msft <69483382+tasherif-msft@users.noreply.github.com>
    
    * Updated readme with env var info
    
    Co-authored-by: tasherif-msft <69483382+tasherif-msft@users.noreply.github.com>

--- tools/azure-devtools/src/azure_devtools/perfstress_tests/random_stream.py
+++ tools/azure-devtools/src/azure_devtools/perfstress_tests/random_stream.py
@@ -5,31 +5,73 @@
 
 import os
 
+_DEFAULT_LENGTH = 1024*1024
+_BYTE_BUFFER = [_DEFAULT_LENGTH, os.urandom(_DEFAULT_LENGTH)]
+
+
 def get_random_bytes(buffer_length):
-    return os.urandom(buffer_length)
+    if buffer_length > _BYTE_BUFFER[0]:
+        _BYTE_BUFFER[0] = buffer_length
+        _BYTE_BUFFER[1] = os.urandom(buffer_length)
+    return _BYTE_BUFFER[1][:buffer_length]
 
 
 class RandomStream:
-    def __init__(self, length, initial_buffer_length=1024*1024):
+    def __init__(self, length, initial_buffer_length=_DEFAULT_LENGTH):
         self._base_data = get_random_bytes(initial_buffer_length)
-        self._base_data_length = initial_buffer_length
+        self._data_length = length
+        self._base_buffer_length = initial_buffer_length
         self._position = 0
         self._remaining = length
+    
+    def reset(self):
+        self._position = 0
+        self._remaining = self._data_length
 
     def read(self, size=None):
         if self._remaining == 0:
-            return None
+            return b""
 
         if size is None:
-            e = self._base_data_length
+            e = self._base_buffer_length
         else:
             e = size
         e = min(e, self._remaining)
-        if e > self._base_data_length:
+        if e > self._base_buffer_length:
             self._base_data = get_random_bytes(e)
-            self._base_data_length = e
+            self._base_buffer_length = e
         self._remaining = self._remaining - e
+        self._position += e
         return self._base_data[:e]
+    
+    def tell(self):
+        return self._position
+
+    def seek(self, index):
+        self._position = index
 
     def remaining(self):
         return self._remaining
+
+
+class WriteStream:
+
+    def __init__(self):
+        self._position = 0
+    
+    def reset(self):
+        self._position = 0
+
+    def write(self, content):
+        length = len(content)
+        self._position += length
+        return length
+
+    def seek(self, index):
+        self._position = index
+
+    def seekable(self):
+        return True
+    
+    def tell(self):
+        return self._position
