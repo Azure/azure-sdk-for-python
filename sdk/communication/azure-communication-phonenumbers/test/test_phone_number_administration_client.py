4684093d9ac9a21e8fc7f519735b1e69276a0840
Jorge Beauregard <jbeauregardb@microsoft.com>
2021-02-11 11:18:59 -0600

    Added sanitization

--- sdk/communication/azure-communication-phonenumbers/test/test_phone_number_administration_client.py
+++ sdk/communication/azure-communication-phonenumbers/test/test_phone_number_administration_client.py
@@ -98,7 +98,9 @@ class NewTests(CommunicationTestCase):
 =======
         self.phone_number_client = PhoneNumbersClient.from_connection_string(self.connection_str)
         self.recording_processors.extend([
-            BodyReplacerProcessor(),
+            BodyReplacerProcessor(
+                keys=["id", "token", "phoneNumber", "phonenumbers"]
+            ),
             ResponseReplacerProcessor(keys=[self._resource_name])])
 
     def test_list_acquired_phone_numbers(self):
@@ -140,7 +142,7 @@ class NewTests(CommunicationTestCase):
     
     def test_update_phone_number_capabilities(self):
         poller = self.phone_number_client.begin_update_phone_number_capabilities(
-          "+16194895842",
+          "+16194895875",
           PhoneNumberCapabilityValue.OUTBOUND,
           PhoneNumberCapabilityValue.OUTBOUND,
           polling = True
@@ -166,8 +168,12 @@ class NewTests(CommunicationTestCase):
         purchase_poller = self.phone_number_client.begin_purchase_phone_numbers(phone_number_to_buy.search_id, polling=True)
         assert purchase_poller.result()
 <<<<<<< HEAD
+<<<<<<< HEAD
         release_poller = self.phone_number_client.begin_release_phone_number(phone_number_to_buy.phone_number)
 =======
         release_poller = self.phone_number_client.begin_release_phone_number("+")
 >>>>>>> ea76a164a... Added new pnm redesign code
+=======
+        release_poller = self.phone_number_client.begin_release_phone_number(phone_number_to_buy.phone_number)
+>>>>>>> 8bb023cc3... Added sanitization
         assert release_poller.status() == 'succeeded'
