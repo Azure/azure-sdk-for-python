a53129e8a3fc820a3c377e7bfb538579d000478e
swathipil <76007337+swathipil@users.noreply.github.com>
2021-02-12 12:52:24 -0500

    [SB] tests.yml - sample live test changes (#16329)
    * tests.yml - sample live test changes
    
    * update arm template + samples for sb
    
    * update env variables
    
    * update mgmt sample resource names to avoid conflict
    
    * randomized names of resources created w mgmt to avoid conflict
    
    * ignore samples with errors, fix in diff PR

--- scripts/devops_tasks/test_run_samples.py
+++ scripts/devops_tasks/test_run_samples.py
@@ -25,7 +25,26 @@ IGNORED_SAMPLES = {
         "__init__.py",
         "consume_cloud_events_from_eventhub.py",
         "consume_cloud_events_from_service_bus_queue.py",
-        "consume_cloud_events_from_storage_queue.py"]
+        "consume_cloud_events_from_storage_queue.py"],
+    "azure-servicebus": [
+        "failure_and_recovery.py",
+        "mgmt_queue.py",
+        "mgmt_rule.py",
+        "mgmt_subscription.py",
+        "mgmt_topic.py",
+        "proxy.py",
+        "receive_deferred_message_queue.py",
+        "receive_iterator_queue.py",
+        "session_pool_receive.py",
+        "mgmt_queue_async.py",
+        "mgmt_rule_async.py",
+        "mgmt_subscription_async.py",
+        "mgmt_topic_async.py",
+        "proxy_async.py",
+        "receive_deferred_message_queue_async.py",
+        "receive_iterator_queue_async.py",
+        "session_pool_receive_async.py"
+    ]
 }
 
 
