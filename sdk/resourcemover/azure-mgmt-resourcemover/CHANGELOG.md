eddbaf30b8f1a9eab5ea6038680979d62fccde5f
Kaihui (Kerwin) Sun <sunkaihuisos@gmail.com>
2021-02-19 15:24:02 +0800

    release resourcemover 2021-01-01 (#16823)

--- sdk/resourcemover/azure-mgmt-resourcemover/CHANGELOG.md
+++ sdk/resourcemover/azure-mgmt-resourcemover/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release History
 
+## 1.0.0 (2021-02-19)
+
+* GA release
+
 ## 1.0.0b2 (2021-02-03)
 
 **Bugfixes**
--- sdk/resourcemover/azure-mgmt-resourcemover/azure/mgmt/resourcemover/operations/_move_resources_operations.py
+++ sdk/resourcemover/azure-mgmt-resourcemover/azure/mgmt/resourcemover/operations/_move_resources_operations.py
@@ -8,7 +8,7 @@
 from typing import TYPE_CHECKING
 import warnings
 
-from azure.core.exceptions import HttpResponseError, ResourceExistsError, ResourceNotFoundError, map_error
+from azure.core.exceptions import ClientAuthenticationError, HttpResponseError, ResourceExistsError, ResourceNotFoundError, map_error
 from azure.core.paging import ItemPaged
 from azure.core.pipeline import PipelineResponse
 from azure.core.pipeline.transport import HttpRequest, HttpResponse
@@ -16,7 +16,7 @@ from azure.core.polling import LROPoller, NoPolling, PollingMethod
 from azure.mgmt.core.exceptions import ARMErrorFormat
 from azure.mgmt.core.polling.arm_polling import ARMPolling
 
-from .. import models
+from .. import models as _models
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
@@ -39,7 +39,7 @@ class MoveResourcesOperations(object):
     :param deserializer: An object model deserializer.
     """
 
-    models = models
+    models = _models
 
     def __init__(self, client, config, serializer, deserializer):
         self._client = client
@@ -54,7 +54,7 @@ class MoveResourcesOperations(object):
         filter=None,  # type: Optional[str]
         **kwargs  # type: Any
     ):
-        # type: (...) -> Iterable["models.MoveResourceCollection"]
+        # type: (...) -> Iterable["_models.MoveResourceCollection"]
         """Lists the Move Resources in the move collection.
 
         :param resource_group_name: The Resource Group Name.
@@ -62,22 +62,25 @@ class MoveResourcesOperations(object):
         :param move_collection_name: The Move Collection Name.
         :type move_collection_name: str
         :param filter: The filter to apply on the operation. For example, you can use
-     $filter=Properties/ProvisioningState eq 'Succeeded'.
+         $filter=Properties/ProvisioningState eq 'Succeeded'.
         :type filter: str
         :keyword callable cls: A custom type or function that will be passed the direct response
         :return: An iterator like instance of either MoveResourceCollection or the result of cls(response)
         :rtype: ~azure.core.paging.ItemPaged[~resource_mover_service_api.models.MoveResourceCollection]
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.MoveResourceCollection"]
-        error_map = {404: ResourceNotFoundError, 409: ResourceExistsError}
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.MoveResourceCollection"]
+        error_map = {
+            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
+        }
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2019-10-01-preview"
+        api_version = "2021-01-01"
+        accept = "application/json"
 
         def prepare_request(next_link=None):
             # Construct headers
             header_parameters = {}  # type: Dict[str, Any]
-            header_parameters['Accept'] = 'application/json'
+            header_parameters['Accept'] = self._serialize.header("accept", accept, 'str')
 
             if not next_link:
                 # Construct URL
@@ -130,15 +133,18 @@ class MoveResourcesOperations(object):
         resource_group_name,  # type: str
         move_collection_name,  # type: str
         move_resource_name,  # type: str
-        body=None,  # type: Optional["models.MoveResource"]
+        body=None,  # type: Optional["_models.MoveResource"]
         **kwargs  # type: Any
     ):
-        # type: (...) -> Optional["models.MoveResource"]
-        cls = kwargs.pop('cls', None)  # type: ClsType[Optional["models.MoveResource"]]
-        error_map = {404: ResourceNotFoundError, 409: ResourceExistsError}
+        # type: (...) -> Optional["_models.MoveResource"]
+        cls = kwargs.pop('cls', None)  # type: ClsType[Optional["_models.MoveResource"]]
+        error_map = {
+            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
+        }
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2019-10-01-preview"
+        api_version = "2021-01-01"
         content_type = kwargs.pop("content_type", "application/json")
+        accept = "application/json"
 
         # Construct URL
         url = self._create_initial.metadata['url']  # type: ignore
@@ -157,7 +163,7 @@ class MoveResourcesOperations(object):
         # Construct headers
         header_parameters = {}  # type: Dict[str, Any]
         header_parameters['Content-Type'] = self._serialize.header("content_type", content_type, 'str')
-        header_parameters['Accept'] = 'application/json'
+        header_parameters['Accept'] = self._serialize.header("accept", accept, 'str')
 
         body_content_kwargs = {}  # type: Dict[str, Any]
         if body is not None:
@@ -166,7 +172,6 @@ class MoveResourcesOperations(object):
             body_content = None
         body_content_kwargs['content'] = body_content
         request = self._client.put(url, query_parameters, header_parameters, **body_content_kwargs)
-
         pipeline_response = self._client._pipeline.run(request, stream=False, **kwargs)
         response = pipeline_response.http_response
 
@@ -189,10 +194,10 @@ class MoveResourcesOperations(object):
         resource_group_name,  # type: str
         move_collection_name,  # type: str
         move_resource_name,  # type: str
-        body=None,  # type: Optional["models.MoveResource"]
+        body=None,  # type: Optional["_models.MoveResource"]
         **kwargs  # type: Any
     ):
-        # type: (...) -> LROPoller["models.MoveResource"]
+        # type: (...) -> LROPoller["_models.MoveResource"]
         """Creates or updates a Move Resource in the move collection.
 
         :param resource_group_name: The Resource Group Name.
@@ -214,7 +219,7 @@ class MoveResourcesOperations(object):
         :raises ~azure.core.exceptions.HttpResponseError:
         """
         polling = kwargs.pop('polling', True)  # type: Union[bool, PollingMethod]
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.MoveResource"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.MoveResource"]
         lro_delay = kwargs.pop(
             'polling_interval',
             self._config.polling_interval
@@ -240,7 +245,14 @@ class MoveResourcesOperations(object):
                 return cls(pipeline_response, deserialized, {})
             return deserialized
 
-        if polling is True: polling_method = ARMPolling(lro_delay, lro_options={'final-state-via': 'azure-async-operation'},  **kwargs)
+        path_format_arguments = {
+            'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
+            'moveCollectionName': self._serialize.url("move_collection_name", move_collection_name, 'str'),
+            'moveResourceName': self._serialize.url("move_resource_name", move_resource_name, 'str'),
+        }
+
+        if polling is True: polling_method = ARMPolling(lro_delay, lro_options={'final-state-via': 'azure-async-operation'}, path_format_arguments=path_format_arguments,  **kwargs)
         elif polling is False: polling_method = NoPolling()
         else: polling_method = polling
         if cont_token:
@@ -261,11 +273,14 @@ class MoveResourcesOperations(object):
         move_resource_name,  # type: str
         **kwargs  # type: Any
     ):
-        # type: (...) -> Optional["models.OperationStatus"]
-        cls = kwargs.pop('cls', None)  # type: ClsType[Optional["models.OperationStatus"]]
-        error_map = {404: ResourceNotFoundError, 409: ResourceExistsError}
+        # type: (...) -> Optional["_models.OperationStatus"]
+        cls = kwargs.pop('cls', None)  # type: ClsType[Optional["_models.OperationStatus"]]
+        error_map = {
+            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
+        }
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2019-10-01-preview"
+        api_version = "2021-01-01"
+        accept = "application/json"
 
         # Construct URL
         url = self._delete_initial.metadata['url']  # type: ignore
@@ -283,7 +298,7 @@ class MoveResourcesOperations(object):
 
         # Construct headers
         header_parameters = {}  # type: Dict[str, Any]
-        header_parameters['Accept'] = 'application/json'
+        header_parameters['Accept'] = self._serialize.header("accept", accept, 'str')
 
         request = self._client.delete(url, query_parameters, header_parameters)
         pipeline_response = self._client._pipeline.run(request, stream=False, **kwargs)
@@ -310,7 +325,7 @@ class MoveResourcesOperations(object):
         move_resource_name,  # type: str
         **kwargs  # type: Any
     ):
-        # type: (...) -> LROPoller["models.OperationStatus"]
+        # type: (...) -> LROPoller["_models.OperationStatus"]
         """Deletes a Move Resource from the move collection.
 
         :param resource_group_name: The Resource Group Name.
@@ -330,7 +345,7 @@ class MoveResourcesOperations(object):
         :raises ~azure.core.exceptions.HttpResponseError:
         """
         polling = kwargs.pop('polling', True)  # type: Union[bool, PollingMethod]
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.OperationStatus"]
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.OperationStatus"]
         lro_delay = kwargs.pop(
             'polling_interval',
             self._config.polling_interval
@@ -355,7 +370,14 @@ class MoveResourcesOperations(object):
                 return cls(pipeline_response, deserialized, {})
             return deserialized
 
-        if polling is True: polling_method = ARMPolling(lro_delay, lro_options={'final-state-via': 'azure-async-operation'},  **kwargs)
+        path_format_arguments = {
+            'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str'),
+            'moveCollectionName': self._serialize.url("move_collection_name", move_collection_name, 'str'),
+            'moveResourceName': self._serialize.url("move_resource_name", move_resource_name, 'str'),
+        }
+
+        if polling is True: polling_method = ARMPolling(lro_delay, lro_options={'final-state-via': 'azure-async-operation'}, path_format_arguments=path_format_arguments,  **kwargs)
         elif polling is False: polling_method = NoPolling()
         else: polling_method = polling
         if cont_token:
@@ -376,7 +398,7 @@ class MoveResourcesOperations(object):
         move_resource_name,  # type: str
         **kwargs  # type: Any
     ):
-        # type: (...) -> "models.MoveResource"
+        # type: (...) -> "_models.MoveResource"
         """Gets the Move Resource.
 
         :param resource_group_name: The Resource Group Name.
@@ -390,10 +412,13 @@ class MoveResourcesOperations(object):
         :rtype: ~resource_mover_service_api.models.MoveResource
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-        cls = kwargs.pop('cls', None)  # type: ClsType["models.MoveResource"]
-        error_map = {404: ResourceNotFoundError, 409: ResourceExistsError}
+        cls = kwargs.pop('cls', None)  # type: ClsType["_models.MoveResource"]
+        error_map = {
+            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
+        }
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2019-10-01-preview"
+        api_version = "2021-01-01"
+        accept = "application/json"
 
         # Construct URL
         url = self.get.metadata['url']  # type: ignore
@@ -411,7 +436,7 @@ class MoveResourcesOperations(object):
 
         # Construct headers
         header_parameters = {}  # type: Dict[str, Any]
-        header_parameters['Accept'] = 'application/json'
+        header_parameters['Accept'] = self._serialize.header("accept", accept, 'str')
 
         request = self._client.get(url, query_parameters, header_parameters)
         pipeline_response = self._client._pipeline.run(request, stream=False, **kwargs)
