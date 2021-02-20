3de22c505ec20f5b4d8ad49e54525a0266cae781
turalf <tural.ferhadov@gmail.com>
2021-02-19 18:02:11 -0800

    Check schema of the object to determine the type [preview4] (#16838)
    * Replace identifier with rawId
    
    * Change serilizer
    
    * Replace indentifier with rawId in test code
    
    * Sync models across modules
    
    * fix typo in serizliser
    
    * Rearrange imports
    
    * Replace rawId with raw_id
    
    * remove trailing newline
    
    Co-authored-by: turalf <tufarhad@microsoft.com>

--- sdk/communication/azure-communication-phonenumbers/azure/communication/phonenumbers/_shared/models.py
+++ sdk/communication/azure-communication-phonenumbers/azure/communication/phonenumbers/_shared/models.py
@@ -37,13 +37,13 @@ class UnknownIdentifier(object):
     Represents an identifier of an unknown type.
     It will be encountered in communications with endpoints that are not
     identifiable by this version of the SDK.
-    :ivar identifier: Unknown communication identifier.
-    :vartype identifier: str
+    :ivar raw_id: Unknown communication identifier.
+    :vartype raw_id: str
     :param identifier: Value to initialize UnknownIdentifier.
     :type identifier: str
     """
     def __init__(self, identifier):
-        self.identifier = identifier
+        self.raw_id = identifier
 
 class _CaseInsensitiveEnumMeta(EnumMeta):
     def __getitem__(cls, name):
