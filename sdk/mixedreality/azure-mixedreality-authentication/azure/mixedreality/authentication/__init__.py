4f66a0dd6fb843cc6e3ce797ecfb89626f5fc633
Craig Treasure <Craig.K.Treasure@microsoft.com>
2021-02-23 13:53:09 -0800

    Added the Azure.MixedReality.Authentication library (#16714)
    This change adds an authentication library for Mixed Reality services, which all currently utilize a custom STS for authentication. Upcoming Mixed Reality client libraries will depend on this library to perform authentication using identities from Azure.Identity. See the added README.md for more information about the library.
    
    [ApiView link](https://apiview.dev/Assemblies/Review/986545855b9c499faeff72b25115ab96)

---
+++ sdk/mixedreality/azure-mixedreality-authentication/azure/mixedreality/authentication/__init__.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+from ._version import VERSION
+from ._client import MixedRealityStsClient
+
+__version__ = VERSION
+__all__ = ['MixedRealityStsClient']
