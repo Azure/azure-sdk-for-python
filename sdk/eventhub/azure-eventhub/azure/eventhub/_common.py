aff466438094d2719739e8c5c590fb5840e00171
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-02-24 11:39:29 -0800

    [EventHubs] Doc warning on changing the value of the instance variable EventDataBatch.max_size_in_bytes (#16644)
    * add warning on max_size_in_bytes in the docstring of EventDataBatch
    
    * Update sdk/eventhub/azure-eventhub/azure/eventhub/_common.py
    
    * Update sdk/eventhub/azure-eventhub/azure/eventhub/_common.py
    
    Co-authored-by: swathipil <76007337+swathipil@users.noreply.github.com>
    
    * fix markdown syntaxt
    
    Co-authored-by: swathipil <76007337+swathipil@users.noreply.github.com>

--- sdk/eventhub/azure-eventhub/azure/eventhub/_common.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_common.py
@@ -338,6 +338,10 @@ class EventDataBatch(object):
     **Please use the create_batch method of EventHubProducerClient
     to create an EventDataBatch object instead of instantiating an EventDataBatch object directly.**
 
+    **WARNING: Updating the value of the instance variable max_size_in_bytes on an instantiated EventDataBatch object
+    is HIGHLY DISCOURAGED. The updated max_size_in_bytes value may conflict with the maximum size of events allowed
+    by the Event Hubs service and result in a sending failure.**
+
     :param int max_size_in_bytes: The maximum size of bytes data that an EventDataBatch object can hold.
     :param str partition_id: The specific partition ID to send to.
     :param str partition_key: With the given partition_key, event data will be sent to a particular partition of the
