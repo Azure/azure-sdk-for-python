d122ef1bb3a95bf4b27c04f61439058665bf0f70
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-02-24 09:42:23 -0800

    Update sdk/eventhub/azure-eventhub/azure/eventhub/_common.py
    Co-authored-by: swathipil <76007337+swathipil@users.noreply.github.com>

--- sdk/eventhub/azure-eventhub/azure/eventhub/_common.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_common.py
@@ -338,9 +338,7 @@ class EventDataBatch(object):
     **Please use the create_batch method of EventHubProducerClient
     to create an EventDataBatch object instead of instantiating an EventDataBatch object directly.**
 
-    **It is highly recommended not to update the value of the instance variable `max_size_in_bytes` on an
-    instantiated EventDataBatch object which might lead to sending failure as the Event Hub service
-    has a fixed quota on the maximum allowed size of events.**
+    **WARNING: Updating the value of the instance variable `max_size_in_bytes` on an instantiated EventDataBatch object is HIGHLY DISCOURAGED. The updated `max_size_in_bytes` value may conflict with the maximum size of events allowed by the Event Hubs service and result in a sending failure.**
 
     :param int max_size_in_bytes: The maximum size of bytes data that an EventDataBatch object can hold.
     :param str partition_id: The specific partition ID to send to.
