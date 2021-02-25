44fafd814737a690dff4061a96cce2f10aeec6b6
MIDDLEEAST\v-moshaban <v-moshaban@microsoft.com>
2021-02-25 16:35:32 +0200

    [samples] added 'batch_translation_async' sample

---
+++ sdk/documenttranslation/azure-ai-documenttranslation/samples/async_samples/sample_batch_translation_async.py
@@ -0,0 +1,101 @@
+# coding=utf-8
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+import os
+import asyncio
+
+
+class BatchTranslationAsync(object):
+
+    async def sample_batch_translation():
+        # import libraries
+        from azure.core.credentials import AzureKeyCredential
+        from azure.ai.textanalytics.aio import DocumentTranslationClient
+        from azure.ai.documenttranslation import (
+            BatchDocumentInput,
+            StorageTarget
+        )
+
+        # get service secrets
+        endpoint = os.environ["AZURE_DOCUMENT_TRANSLATION_ENDPOINT"]
+        key = os.environ["AZURE_DOCUMENT_TRANSLATION_KEY"]
+        source_container_url = os.environ["AZURE_SOURCE_CONTAINER_URL"]
+        target_container_url_es = os.environ["AZURE_TARGET_CONTAINER_URL_ES"]
+        target_container_url_fr = os.environ["AZURE_TARGET_CONTAINER_URL_FR"]
+
+        # create service client
+        client = DocumentTranslationClient(endpoint, AzureKeyCredential(key))
+
+        # prepare translation job input
+        batch = [
+            BatchDocumentInput(
+                source_url=source_container_url,
+                source_language="en",
+                targets=[
+                    StorageTarget(
+                        target_url=target_container_url_es,
+                        language="es"
+                    ),
+                    StorageTarget(
+                        target_url=target_container_url_fr,
+                        language="fr"
+                    )
+                ],
+                storage_type="file"
+            )
+        ]
+
+        # run job
+        async with client:
+            job_detail = await client.create_translation_job(batch)
+
+            print("Job initial status: {}".format(job_detail.status))
+            print("Number of translations on documents: {}".format(job_detail.documents_total_count))
+
+            # get job result
+            job_result = await client.wait_until_done(job_detail.id)  # type: JobStatusDetail
+            if job_result.status == "Succeeded":
+                print("We translated our documents!")
+                if job_result.documents_failed_count > 0:
+                    check_documents(client, job_result.id)
+
+            if job_result.status in ["Failed", "ValidationFailed"]:
+                if job_result.error:
+                    print("Translation job failed: {}: {}".format(job_result.error.code, job_result.error.message))
+                check_documents(client, job_result.id)
+                exit(1)
+
+
+    def check_documents(client, job_id):
+        from azure.core.exceptions import ResourceNotFoundError
+
+        try:
+            doc_statuses = client.list_documents_statuses(job_id)  # type: ItemPaged[DocumentStatusDetail]
+        except ResourceNotFoundError as err:
+            print("Failed to process any documents in source/target container.")
+            raise err
+
+        docs_to_retry = []
+        for document in doc_statuses:
+            if document.status == "Failed":
+                print("Document at {} failed to be translated to {} language".format(
+                    document.url, document.translate_to
+                ))
+                print("Document ID: {}, Error Code: {}, Message: {}".format(
+                    document.id, document.error.code, document.error.message
+                ))
+                if document.url not in docs_to_retry:
+                    docs_to_retry.append(document.url)
+
+
+async def main():
+    sample = BatchTranslationAsync()
+    await sample.sample_batch_translation()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
