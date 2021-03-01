09a2fea490f8a13dbc36af7740af51aed7f24a47
Xiang Yan <xiangyan@microsoft.com>
2021-03-01 08:48:59 -0800

    move uuid.uuid4() into ctor

--- sdk/appconfiguration/azure-appconfiguration/tests/perfstress_tests/set.py
+++ sdk/appconfiguration/azure-appconfiguration/tests/perfstress_tests/set.py
@@ -20,6 +20,7 @@ class SetTest(PerfStressTest):
         super().__init__(arguments)
         connection_string = self.get_from_env("AZURE_APP_CONFIG_CONNECTION_STRING")
         self.key = "KEY"
+        self.value = str(uuid.uuid4())
         self.service_client = SyncAppConfigClient.from_connection_string(connection_string=connection_string)
         self.async_service_client = AsyncAppConfigClient.from_connection_string(connection_string=connection_string)
 
@@ -30,13 +31,13 @@ class SetTest(PerfStressTest):
     def run_sync(self):
         kv = ConfigurationSetting(
             key=self.key,
-            value="VALUE" + str(uuid.uuid4()),
+            value="VALUE" + self.value,
         )
         self.service_client.set_configuration_setting(kv)
 
     async def run_async(self):
         kv = ConfigurationSetting(
             key=self.key,
-            value="VALUE" + str(uuid.uuid4()),
+            value="VALUE" + self.value,
         )
         await self.async_service_client.set_configuration_setting(kv)
