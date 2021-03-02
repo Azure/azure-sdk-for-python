eaaef69cdad78d39376c1f82ffca9dd2eebda827
Lakshman Sundaralingam <lsundaralingam@users.noreply.github.com>
2021-03-02 10:05:00 -0800

    [Communication]: SMS 1:N Messages, Custom Tags, and Idempotence (#16836)
    * Updated SMS clients with new swagger
    
    * Updated SMS client tests
    
    * Updated SMS samples
    
    * Added resending failed messages to SMS samples
    
    * Updated SMS clients to latest swagger
    
    * Updated SMS client tests and recordings
    
    * Updated SMS samples with changes from new swagger
    
    * Added more test cases
    
    * Rebased README file
    
    * Fixed pylint issues
    
    * Added Idempotence Parameters
    
    * Updated SWAGGER formatting
    
    * Added custom SendMessageRequest model to rename the smsSendOptions field to sendSmsOptions
    
    * Updated SMS options parameter
    
    * Hide repeatability_result from SmsSendResult
    
    * Updated failure message in SMS samples
    
    * Updated formatting in models file
    
    * Updated SmsSendResult
    
    * Refactored SmsSendResult
    
    * Fixed pylint issue
    
    * Updated CHANGELOG
    
    * Use UTC time zone for repeatability_first_sent
    
    * Cleaned up CHANGELOG
    
    * Updated SMS samples
    
    * Updated SmsSendOptions
    
    * Add test for idempotency
    
    * Added case when single recipient phone number is a string
    
    * Updated method documentation in SMS clients

--- sdk/communication/azure-communication-sms/tests/test_sms_client_e2e_async.py
+++ sdk/communication/azure-communication-sms/tests/test_sms_client_e2e_async.py
@@ -8,9 +8,6 @@ import os
 import pytest
 from azure.core.credentials import AccessToken
 from azure.communication.sms.aio import SmsClient
-from azure.communication.sms import (
-    PhoneNumberIdentifier, SendSmsOptions
-)
 from azure.communication.sms._shared.utils import parse_connection_str
 from _shared.asynctestcase import AsyncCommunicationTestCase
 from _shared.testcase import (
@@ -18,13 +15,13 @@ from _shared.testcase import (
 )
 from azure.identity import DefaultAzureCredential
 
-
 class FakeTokenCredential(object):
     def __init__(self):
         self.token = AccessToken("Fake Token", 0)
 
     def get_token(self, *args):
         return self.token
+
 class SMSClientTestAsync(AsyncCommunicationTestCase):
     def __init__(self, method_name):
         super(SMSClientTestAsync, self).__init__(method_name)
@@ -43,19 +40,43 @@ class SMSClientTestAsync(AsyncCommunicationTestCase):
 
     @AsyncCommunicationTestCase.await_prepared_test
     @pytest.mark.live_test_only
-    async def test_send_sms_async(self):
+    async def test_send_sms_single_async(self):
 
         sms_client = SmsClient.from_connection_string(self.connection_str)
 
         async with sms_client:
             # calling send() with sms values
-            sms_response = await sms_client.send(
-                from_phone_number=PhoneNumberIdentifier(self.phone_number),
-                to_phone_numbers=[PhoneNumberIdentifier(self.phone_number)],
+            sms_responses = await sms_client.send(
+                from_=self.phone_number,
+                to=self.phone_number,
                 message="Hello World via SMS",
-                send_sms_options=SendSmsOptions(enable_delivery_report=True))  # optional property
+                enable_delivery_report=True,  # optional property
+                tag="custom-tag")  # optional property
+            
+            assert len(sms_responses) is 1
 
-            assert sms_response.message_id is not None
+            for sms_response in sms_responses:
+                self.verify_sms_response(sms_response)
+    
+    @AsyncCommunicationTestCase.await_prepared_test
+    @pytest.mark.live_test_only
+    async def test_send_sms_multiple_async(self):
+
+        sms_client = SmsClient.from_connection_string(self.connection_str)
+
+        async with sms_client:
+            # calling send() with sms values
+            sms_responses = await sms_client.send(
+                from_=self.phone_number,
+                to=[self.phone_number, self.phone_number],
+                message="Hello World via SMS",
+                enable_delivery_report=True,  # optional property
+                tag="custom-tag")  # optional property
+            
+            assert len(sms_responses) is 2
+
+            for sms_response in sms_responses:
+                self.verify_sms_response(sms_response)
 
     @AsyncCommunicationTestCase.await_prepared_test
     @pytest.mark.live_test_only
@@ -67,13 +88,67 @@ class SMSClientTestAsync(AsyncCommunicationTestCase):
         else:
             credential = DefaultAzureCredential()
         sms_client = SmsClient(endpoint, credential)
-        print(sms_client)
+
         async with sms_client:
             # calling send() with sms values
-            sms_response = await sms_client.send(
-                from_phone_number=PhoneNumberIdentifier(self.phone_number),
-                to_phone_numbers=[PhoneNumberIdentifier(self.phone_number)],
+            sms_responses = await sms_client.send(
+                from_=self.phone_number,
+                to=[self.phone_number],
                 message="Hello World via SMS",
-                send_sms_options=SendSmsOptions(enable_delivery_report=True))  # optional property
+                enable_delivery_report=True,  # optional property
+                tag="custom-tag")  # optional property
+            
+            assert len(sms_responses) is 1
 
-            assert sms_response.message_id is not None
+            for sms_response in sms_responses:
+                self.verify_sms_response(sms_response)
+    
+    @AsyncCommunicationTestCase.await_prepared_test
+    @pytest.mark.live_test_only
+    async def test_send_sms_invalid_to_phone_number_async(self):
+
+        sms_client = SmsClient.from_connection_string(self.connection_str)
+
+        async with sms_client:
+            # calling send() with sms values
+            sms_responses = await sms_client.send(
+                from_=self.phone_number,
+                to=["+1234567891011"],
+                message="Hello World via SMS",
+                enable_delivery_report=True,  # optional property
+                tag="custom-tag")  # optional property
+            
+            assert len(sms_responses) is 1
+
+        for sms_response in sms_responses:
+            assert sms_response.http_status_code == 400
+            assert not sms_response.successful
+    
+    @AsyncCommunicationTestCase.await_prepared_test
+    @pytest.mark.live_test_only
+    async def test_send_sms_unique_message_ids_async(self):
+
+        sms_client = SmsClient.from_connection_string(self.connection_str)
+
+        async with sms_client:
+            # calling send() with sms values
+            sms_responses_1 = await sms_client.send(
+                from_=self.phone_number,
+                to=[self.phone_number],
+                message="Hello World via SMS")
+        
+            # calling send() again with the same sms values
+            sms_responses_2 = await sms_client.send(
+                from_=self.phone_number,
+                to=[self.phone_number],
+                message="Hello World via SMS")
+        
+            assert sms_responses_1[0].message_id != sms_responses_2[0].message_id
+    
+    def verify_sms_response(self, sms_response):
+        assert sms_response.to == self.phone_number
+        assert sms_response.message_id is not None
+        assert sms_response.http_status_code == 202
+        assert sms_response.error_message is None
+        assert sms_response.successful
+            
