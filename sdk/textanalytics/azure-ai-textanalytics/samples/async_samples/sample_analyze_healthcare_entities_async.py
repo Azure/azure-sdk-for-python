7c0f15b2abed1d1a9191f6eee68b3567b77ae6e2
iscai-msft <iscai@microsoft.com>
2021-03-02 12:07:54 -0500

    add back dosage of medication extration to samples

--- sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_analyze_healthcare_entities_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_analyze_healthcare_entities_async.py
@@ -37,9 +37,18 @@ class AnalyzeHealthcareEntitiesSampleAsync(object):
 
     async def analyze_healthcare_entities_async(self):
 
+        print(
+            "In this sample we will be combing through the prescriptions our pharmacy has fulfilled "
+            "so we can catalog how much inventory we have"
+        )
+        print(
+            "We start out with a list of prescription documents."
+        )
+
         # [START analyze_healthcare_entities_async]
         import os
         from azure.core.credentials import AzureKeyCredential
+        from azure.ai.textanalytics import HealthcareEntityRelationType
         from azure.ai.textanalytics.aio import TextAnalyticsClient
 
         endpoint = os.environ["AZURE_TEXT_ANALYTICS_ENDPOINT"]
@@ -65,7 +74,7 @@ class AnalyzeHealthcareEntitiesSampleAsync(object):
             result = await poller.result()
             docs = [doc async for doc in result if not doc.is_error]
 
-        print("Results of Healthcare Entities Analysis:")
+        print("Let's first visualize the outputted healthcare result:")
         for idx, doc in enumerate(docs):
             for entity in doc.entities:
                 print("Entity: {}".format(entity.text))
@@ -84,7 +93,43 @@ class AnalyzeHealthcareEntitiesSampleAsync(object):
                     print("...Role '{}' with entity '{}'".format(role.name, role.entity.text))
             print("------------------------------------------")
 
+        print("Now, let's get all of medication dosage relations from the documents")
+        dosage_of_medication_relations = [
+            entity_relation
+            for doc in docs
+            for entity_relation in doc.entity_relations if entity_relation.relation_type == HealthcareEntityRelationType.DOSAGE_OF_MEDICATION
+        ]
         # [END analyze_healthcare_entities_async]
+
+        print(
+            "Now, I will create a dictionary of medication to total dosage. "
+            "I will use a regex to extract the dosage amount. For simplicity sake, I will assume "
+            "all dosages are represented with numbers and have mg unit."
+        )
+        import re
+        from collections import defaultdict
+
+        medication_to_dosage = defaultdict(int)
+
+        for relation in dosage_of_medication_relations:
+            # The DosageOfMedication relation should only contain the dosage and medication roles
+
+            dosage_role = next(filter(lambda x: x.name == "Attribute", relation.roles))
+            medication_role = next(filter(lambda x: x.name == "Entity", relation.roles))
+
+            try:
+                dosage_value = int(re.findall(r"\d+", dosage_role.entity.text)[0]) # we find the numbers in the dosage
+                medication_to_dosage[medication_role.entity.text] += dosage_value
+            except StopIteration:
+                # Error handling for if there's no dosage in numbers.
+                pass
+
+        [
+            print("We have fulfilled '{}' total mg of '{}'".format(
+                dosage, medication
+            ))
+            for medication, dosage in medication_to_dosage.items()
+        ]
 
 async def main():
     sample = AnalyzeHealthcareEntitiesSampleAsync()
--- sdk/textanalytics/azure-ai-textanalytics/samples/sample_analyze_healthcare_entities.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/sample_analyze_healthcare_entities.py
@@ -34,10 +34,18 @@ class AnalyzeHealthcareEntitiesSample(object):
 
     def analyze_healthcare_entities(self):
 
+        print(
+            "In this sample we will be combing through the prescriptions our pharmacy has fulfilled "
+            "so we can catalog how much inventory we have"
+        )
+        print(
+            "We start out with a list of prescription documents."
+        )
+
         # [START analyze_healthcare_entities]
         import os
         from azure.core.credentials import AzureKeyCredential
-        from azure.ai.textanalytics import TextAnalyticsClient
+        from azure.ai.textanalytics import TextAnalyticsClient, HealthcareEntityRelationType
 
         endpoint = os.environ["AZURE_TEXT_ANALYTICS_ENDPOINT"]
         key = os.environ["AZURE_TEXT_ANALYTICS_KEY"]
@@ -62,7 +70,7 @@ class AnalyzeHealthcareEntitiesSample(object):
 
         docs = [doc for doc in result if not doc.is_error]
 
-        print("Results of Healthcare Entities Analysis:")
+        print("Let's first visualize the outputted healthcare result:")
         for idx, doc in enumerate(docs):
             for entity in doc.entities:
                 print("Entity: {}".format(entity.text))
@@ -80,7 +88,45 @@ class AnalyzeHealthcareEntitiesSample(object):
                 for role in relation.roles:
                     print("...Role '{}' with entity '{}'".format(role.name, role.entity.text))
             print("------------------------------------------")
+
+        print("Now, let's get all of medication dosage relations from the documents")
+        dosage_of_medication_relations = [
+            entity_relation
+            for doc in docs
+            for entity_relation in doc.entity_relations if entity_relation.relation_type == HealthcareEntityRelationType.DOSAGE_OF_MEDICATION
+        ]
         # [END analyze_healthcare_entities]
+
+        print(
+            "Now, I will create a dictionary of medication to total dosage. "
+            "I will use a regex to extract the dosage amount. For simplicity sake, I will assume "
+            "all dosages are represented with numbers and have mg unit."
+        )
+        import re
+        from collections import defaultdict
+
+        medication_to_dosage = defaultdict(int)
+
+        for relation in dosage_of_medication_relations:
+            # The DosageOfMedication relation should only contain the dosage and medication roles
+
+            dosage_role = next(filter(lambda x: x.name == "Attribute", relation.roles))
+            medication_role = next(filter(lambda x: x.name == "Entity", relation.roles))
+
+            try:
+                dosage_value = int(re.findall(r"\d+", dosage_role.entity.text)[0]) # we find the numbers in the dosage
+                medication_to_dosage[medication_role.entity.text] += dosage_value
+            except StopIteration:
+                # Error handling for if there's no dosage in numbers.
+                pass
+
+        [
+            print("We have fulfilled '{}' total mg of '{}'".format(
+                dosage, medication
+            ))
+            for medication, dosage in medication_to_dosage.items()
+        ]
+
 
 if __name__ == "__main__":
     sample = AnalyzeHealthcareEntitiesSample()
