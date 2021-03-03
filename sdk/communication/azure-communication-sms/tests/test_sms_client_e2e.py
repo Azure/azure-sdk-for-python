d182d3c55ccc20b08d599fe760fcb27098efc0e5
Lakshman Sundaralingam <lakshmans@microsoft.com>
2021-03-03 14:45:17 -0800

    Updated SMS tests

--- sdk/communication/azure-communication-sms/tests/test_sms_client_e2e.py
+++ sdk/communication/azure-communication-sms/tests/test_sms_client_e2e.py
@@ -6,12 +6,23 @@
 
 import os
 import pytest
+from azure.core.credentials import AccessToken
+from azure.core.exceptions import HttpResponseError
 from azure.communication.sms import SmsClient
+from azure.communication.sms._shared.utils import parse_connection_str
 from _shared.testcase import (
     CommunicationTestCase,
     BodyReplacerProcessor,
     ResponseReplacerProcessor
 )
+from azure.identity import DefaultAzureCredential
+
+class FakeTokenCredential(object):
+    def __init__(self):
+        self.token = AccessToken("Fake Token", 0)
+
+    def get_token(self, *args):
+        return self.token
 
 class SMSClientTest(CommunicationTestCase):
     def __init__(self, method_name):
@@ -26,32 +37,31 @@ class SMSClientTest(CommunicationTestCase):
             self.phone_number = os.getenv("AZURE_COMMUNICATION_SERVICE_PHONE_NUMBER")
 
         self.recording_processors.extend([
-            BodyReplacerProcessor(keys=["to", "from", "messageId"]),
+            BodyReplacerProcessor(keys=["to", "from", "messageId", "repeatabilityRequestId", "repeatabilityFirstSent"]),
             ResponseReplacerProcessor(keys=[self._resource_name])])
-
-        self.sms_client = SmsClient.from_connection_string(self.connection_str)
-
-    @pytest.mark.live_test_only
-    def test_send_sms_single(self):
-
-        # calling send() with sms values
-        sms_responses = self.sms_client.send(
-            from_=self.phone_number,
-            to=self.phone_number,
-            message="Hello World via SMS",
-            enable_delivery_report=True,  # optional property
-            tag="custom-tag")  # optional property
-        
-        assert len(sms_responses) is 1
-
-        for sms_response in sms_responses:
-            self.verify_sms_response(sms_response)
     
     @pytest.mark.live_test_only
-    def test_send_sms_multiple(self):
+    def test_send_sms_single(self):
+        
+        sms_client = SmsClient.from_connection_string(self.connection_str)
 
         # calling send() with sms values
-        sms_responses = self.sms_client.send(
+        sms_responses = sms_client.send(
+            from_=self.phone_number,
+            to=self.phone_number,
+            message="Hello World via SMS")
+
+        assert len(sms_responses) is 1
+
+        self.verify_successful_sms_response(sms_responses[0])
+    
+    @pytest.mark.live_test_only
+    def test_send_sms_multiple_with_options(self):
+        
+        sms_client = SmsClient.from_connection_string(self.connection_str)
+        
+        # calling send() with sms values
+        sms_responses = sms_client.send(
             from_=self.phone_number,
             to=[self.phone_number, self.phone_number],
             message="Hello World via SMS",
@@ -60,45 +70,100 @@ class SMSClientTest(CommunicationTestCase):
         
         assert len(sms_responses) is 2
 
-        for sms_response in sms_responses:
-            self.verify_sms_response(sms_response)
+        self.verify_successful_sms_response(sms_responses[0])
+        self.verify_successful_sms_response(sms_responses[1])
     
     @pytest.mark.live_test_only
-    def test_send_sms_invalid_to_phone_number(self):
+    def test_send_sms_from_managed_identity(self):
+        endpoint, access_key = parse_connection_str(self.connection_str)
+        from devtools_testutils import is_live
+        if not is_live():
+            credential = FakeTokenCredential()
+        else:
+            credential = DefaultAzureCredential()
+        sms_client = SmsClient(endpoint, credential)
 
         # calling send() with sms values
-        sms_responses = self.sms_client.send(
+        sms_responses = sms_client.send(
             from_=self.phone_number,
-            to=["+1234567891011"],
-            message="Hello World via SMS",
-            enable_delivery_report=True,  # optional property
-            tag="custom-tag")  # optional property
+            to=[self.phone_number],
+            message="Hello World via SMS")
         
         assert len(sms_responses) is 1
 
-        for sms_response in sms_responses:
-            assert sms_response.http_status_code == 400
-            assert not sms_response.successful
+        self.verify_successful_sms_response(sms_responses[0])
+
+    @pytest.mark.live_test_only
+    def test_send_sms_fake_from_phone_number(self):
+
+        sms_client = SmsClient.from_connection_string(self.connection_str)
+        
+        with pytest.raises(HttpResponseError) as ex:
+            # calling send() with sms values
+            sms_client.send(
+                from_="+15550000000",
+                to=[self.phone_number],
+                message="Hello World via SMS")
+        
+        assert str(ex.value.status_code) == "400"
+        assert ex.value.message is not None
+
+    @pytest.mark.live_test_only
+    def test_send_sms_fake_to_phone_number(self):
+
+        sms_client = SmsClient.from_connection_string(self.connection_str)
+
+        # calling send() with sms values
+        sms_responses = sms_client.send(
+            from_=self.phone_number,
+            to=["+15550000000"],
+            message="Hello World via SMS")
+        
+        assert len(sms_responses) is 1
+
+        assert sms_responses[0].message_id is None
+        assert sms_responses[0].http_status_code == 400
+        assert sms_responses[0].error_message == "Invalid To phone number format."
+        assert not sms_responses[0].successful
     
+    @pytest.mark.live_test_only
+    def test_send_sms_unauthorized_from_phone_number(self):
+
+        sms_client = SmsClient.from_connection_string(self.connection_str)
+        
+        with pytest.raises(HttpResponseError) as ex:
+            # calling send() with sms values
+            sms_client.send(
+                from_="+18007342577",
+                to=[self.phone_number],
+                message="Hello World via SMS")
+        
+        assert str(ex.value.status_code) == "404"
+        assert ex.value.message is not None
+
     @pytest.mark.live_test_only
     def test_send_sms_unique_message_ids(self):
 
+        sms_client = SmsClient.from_connection_string(self.connection_str)
+
         # calling send() with sms values
-        sms_responses_1 = self.sms_client.send(
+        sms_responses_1 = sms_client.send(
             from_=self.phone_number,
             to=[self.phone_number],
             message="Hello World via SMS")
         
         # calling send() again with the same sms values
-        sms_responses_2 = self.sms_client.send(
+        sms_responses_2 = sms_client.send(
             from_=self.phone_number,
             to=[self.phone_number],
             message="Hello World via SMS")
         
+        self.verify_successful_sms_response(sms_responses_1[0])
+        self.verify_successful_sms_response(sms_responses_2[0])
         # message ids should be unique due to having a different idempotency key
         assert sms_responses_1[0].message_id != sms_responses_2[0].message_id
 
-    def verify_sms_response(self, sms_response):
+    def verify_successful_sms_response(self, sms_response):
         assert sms_response.to == self.phone_number
         assert sms_response.message_id is not None
         assert sms_response.http_status_code == 202
