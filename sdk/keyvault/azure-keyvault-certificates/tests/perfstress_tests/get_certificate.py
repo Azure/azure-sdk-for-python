f177581164a10017d962cc2c6225e02e6db7e6de
McCoy Patiño <mcpatino@microsoft.com>
2021-03-03 11:34:47 -0800

    Add perf test for certificates

---
+++ sdk/keyvault/azure-keyvault-certificates/tests/perfstress_tests/get_certificate.py
@@ -0,0 +1,48 @@
+from azure_devtools.perfstress_tests import PerfStressTest
+from azure.identity import ClientSecretCredential
+from azure.identity.aio import ClientSecretCredential as AsyncClientSecretCredential
+from azure.keyvault.certificates import CertificateClient, CertificatePolicy
+from azure.keyvault.certificates.aio import CertificateClient as AsyncCertificateClient
+
+
+class GetCertificateTest(PerfStressTest):
+
+    def __init__(self, arguments):
+        super().__init__(arguments)
+
+        # Auth configuration
+        tenant_id = self.get_from_env("AZURE_TENANT_ID")
+        client_id = self.get_from_env("AZURE_CLIENT_ID")
+        client_secret = self.get_from_env("AZURE_CLIENT_SECRET")
+        self.credential = ClientSecretCredential(tenant_id, client_id, client_secret)
+        self.async_credential = AsyncClientSecretCredential(tenant_id, client_id, client_secret)
+
+        # Create clients
+        vault_url = self.get_from_env("AZURE_KEYVAULT_URL")
+        self.client = CertificateClient(vault_url, self.credential)
+        self.async_client = AsyncCertificateClient(vault_url, self.async_credential)
+
+    async def global_setup(self):
+        """The global setup is run only once."""
+        await super().global_setup()
+        await self.async_client.create_certificate("livekvtestperfcert", CertificatePolicy.get_default())
+
+    async def global_cleanup(self):
+        """The global cleanup is run only once."""
+        await self.async_client.delete_certificate("livekvtestperfcert")
+        await self.async_client.purge_deleted_certificate("livekvtestperfcert")
+        await super().global_cleanup()
+
+    async def close(self):
+        """This is run after cleanup."""
+        await self.async_client.close()
+        await self.async_credential.close()
+        await super().close()
+
+    def run_sync(self):
+        """The synchronous perf test."""
+        self.client.get_certificate("livekvtestperfcert")
+
+    async def run_async(self):
+        """The asynchronous perf test."""
+        await self.async_client.get_certificate("livekvtestperfcert")
