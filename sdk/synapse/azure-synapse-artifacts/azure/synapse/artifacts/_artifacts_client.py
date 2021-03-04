419f6596a9a718f63321eeffcc23a762cbb039f4
Xiang Yan <xiangsjtu@gmail.com>
2021-03-04 09:20:35 -0800

    march release (#16966)
    * march release
    
    * update
    
    * update python version
    
    * Stop Python 3.5 support

--- sdk/synapse/azure-synapse-artifacts/azure/synapse/artifacts/_artifacts_client.py
+++ sdk/synapse/azure-synapse-artifacts/azure/synapse/artifacts/_artifacts_client.py
@@ -33,6 +33,7 @@ from .operations import WorkspaceOperations
 from .operations import SqlPoolsOperations
 from .operations import BigDataPoolsOperations
 from .operations import IntegrationRuntimesOperations
+from .operations import LibraryOperations
 from .operations import WorkspaceGitRepoManagementOperations
 from . import models
 
@@ -70,6 +71,8 @@ class ArtifactsClient(object):
     :vartype big_data_pools: azure.synapse.artifacts.operations.BigDataPoolsOperations
     :ivar integration_runtimes: IntegrationRuntimesOperations operations
     :vartype integration_runtimes: azure.synapse.artifacts.operations.IntegrationRuntimesOperations
+    :ivar library: LibraryOperations operations
+    :vartype library: azure.synapse.artifacts.operations.LibraryOperations
     :ivar workspace_git_repo_management: WorkspaceGitRepoManagementOperations operations
     :vartype workspace_git_repo_management: azure.synapse.artifacts.operations.WorkspaceGitRepoManagementOperations
     :param credential: Credential needed for the client to connect to Azure.
@@ -124,6 +127,8 @@ class ArtifactsClient(object):
         self.big_data_pools = BigDataPoolsOperations(
             self._client, self._config, self._serialize, self._deserialize)
         self.integration_runtimes = IntegrationRuntimesOperations(
+            self._client, self._config, self._serialize, self._deserialize)
+        self.library = LibraryOperations(
             self._client, self._config, self._serialize, self._deserialize)
         self.workspace_git_repo_management = WorkspaceGitRepoManagementOperations(
             self._client, self._config, self._serialize, self._deserialize)
