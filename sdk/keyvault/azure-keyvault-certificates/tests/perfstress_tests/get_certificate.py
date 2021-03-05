5ba279e2c732b36d71731b0ce0286858d98d9bb0
McCoy Patiño <mcpatino@microsoft.com>
2021-03-04 16:00:12 -0800

    Add license headers

--- sdk/keyvault/azure-keyvault-certificates/tests/perfstress_tests/get_certificate.py
+++ sdk/keyvault/azure-keyvault-certificates/tests/perfstress_tests/get_certificate.py
@@ -1,3 +1,8 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
 from azure_devtools.perfstress_tests import PerfStressTest
 from azure.identity import EnvironmentCredential
 from azure.identity.aio import EnvironmentCredential as AsyncEnvironmentCredential
