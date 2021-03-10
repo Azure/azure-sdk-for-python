7bda25a738b14c00b50d94824db29488bb290f53
iscai-msft <iscai@microsoft.com>
2021-03-10 11:43:16 -0500

    add code for httprequest

--- sdk/core/azure-core/azure/core/protocol/_http_request.py
+++ sdk/core/azure-core/azure/core/protocol/_http_request.py
@@ -33,11 +33,43 @@ from ._types import (
     RequestFiles,
     ByteStream,
 )
+import xml.etree.ElementTree as ET
+from ..pipeline.transport import HttpRequest as PipelineTransportHttpRequest
+
+try:
+    binary_type = str
+    from urlparse import urlparse  # type: ignore
+except ImportError:
+    binary_type = bytes  # type: ignore
+    from urllib.parse import urlparse
+
+def _is_stream(content):
+    return isinstance(content, binary_type) or any(
+        hasattr(content, attr) for attr in ["read", "__iter__", "__aiter__"]
+    )
 
 class HttpRequest(object):
     """Represents a HTTP request.
 
-    URL can be given without query parameters, to be added later using "format_parameters".
+    :param str method: HTTP method for your call, i.e. "GET", "HEAD"
+    :param str url: URL for your request
+    :keyword params: Query parameters to include in the URL, as a dictionary
+    :paramtype params: dict[str, any]
+    :keyword headers: Dictionary of HTTP headers to include in the
+     request.
+    :paramtype headers: dict[str, any]
+    :keyword cookies: Dictionary of Cookie items to include in the request.
+    :paramtype cookies: dict[str, str] or ~http.cookiejar.CookieJar
+    :keyword content: Binary content to include in the body of the
+     request, as bytes or a byte iterator.
+    :paramtype content: bytes or iterator of bytes
+    :keyword dict data: Form data to include in the body of the request,
+     as a dictionary.
+    :keyword files: A dictionary of upload files to include in the
+     body of the request.
+    :paramtype files: Dictionary of str to IO of string or bytes
+    :keyword any json: A JSON serializable object to include in the body
+     of the request.
     """
 
     def __init__(
@@ -52,13 +84,40 @@ class HttpRequest(object):
         data: dict = None,
         files: RequestFiles = None,
         json: Any = None,
-        stream_response: bool = True,
     ) -> None:
-        self.method = method
-        self.url = url
-        # doesn't expose params, automatically formats URL with it
-        self.headers = headers
-        self.stream_response = stream_response
+        # hacking by utilizing pipeline transport HttpRequest
+        self._http_request = PipelineTransportHttpRequest(
+            method=method,
+            url=url,
+            headers=headers,
+            files=files,
+            data=content
+        )
+        if params:
+            self._http_request.format_parameters(params)
+        if data:
+            self._http_request.set_formdata_body(data)
+        if content is not None:
+            content_type = self._http_request.headers.get("Content-Type")
+            if _is_stream(content):
+                self._http_request.set_streamed_data_body(content)
+            elif isinstance(content, ET.Element):
+                self._http_request.set_xml_body(content)
+            elif content_type and content_type.startswith("text/"):
+                self._http_request.set_text_body(content)
+            else:
+                self._http_request.data = content
+        if json:
+            self._http_request.set_json_body(json)
+
+        self.url = self._http_request.url
+        self.method = self._http_request.method
+        self.headers = self._http_request.headers
+        # pipeline transport HttpRequest hacks data. It can either
+        # be binary content, or the formdata dict
+        self.content = self._http_request.data
+        self.data = self._http_request.data
+        self.files = self._http_request.files
 
 
     def __repr__(self):
@@ -71,9 +130,3 @@ class HttpRequest(object):
             return HttpRequest(self.method, self.url, self.headers, None, None)
         except (ValueError, TypeError):
             return copy.copy(self)
-
-    @property
-    def content(self) -> bytes:
-        """The query parameters of the request as a dict.
-        """
-        return b''
