37a9ecc2a40aa1ce2222b6e4d7a816e2fb20e468
Jorge Beauregard <69869951+jbeauregardb@users.noreply.github.com>
2021-03-16 13:40:12 -0600

    [Communication] - Phone Numbers - Added async samples (#17314)
    * Added samples for PNM
    
    * Force pipeline
    
    * Regenerated swagger
    
    * Addressed comments

--- sdk/communication/azure-communication-phonenumbers/samples/list_purchased_phone_numbers_sample.py
+++ sdk/communication/azure-communication-phonenumbers/samples/list_purchased_phone_numbers_sample.py
@@ -9,7 +9,7 @@
 """
 FILE: list_purchased_phone_numbers_sample.py
 DESCRIPTION:
-    This sample demonstrates how to get all off you acquired phone numbers using your connection string
+    This sample demonstrates how to get all of you acquired phone numbers using your connection string
 USAGE:
     python list_purchased_phone_numbers_sample.py
     Set the environment variables with your own values before running the sample:
@@ -26,7 +26,7 @@ phone_numbers_client = PhoneNumbersClient.from_connection_string(connection_str)
 
 def list_purchased_phone_numbers():
     purchased_phone_numbers = phone_numbers_client.list_purchased_phone_numbers()
-    print('Acquired phone numbers:')
+    print('Purchased phone numbers:')
     for acquired_phone_number in purchased_phone_numbers:
         print(acquired_phone_number.phone_number)
 
