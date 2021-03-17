9ce4471f5a177da56eda60bdf60ec438125ceaca
catalinaperalta <catalinaperaltah@hotmail.com>
2021-03-17 14:53:16 -0700

    regenerate with latest preview 3 changes (#17400)

--- sdk/formrecognizer/azure-ai-formrecognizer/azure/ai/formrecognizer/_generated/_form_recognizer_client.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/azure/ai/formrecognizer/_generated/_form_recognizer_client.py
@@ -24,6 +24,7 @@ if TYPE_CHECKING:
     from typing import Any, Optional
 
     from azure.core.credentials import TokenCredential
+    from azure.core.pipeline.transport import HttpRequest, HttpResponse
 
 class _SDKClient(object):
     def __init__(self, *args, **kwargs):
