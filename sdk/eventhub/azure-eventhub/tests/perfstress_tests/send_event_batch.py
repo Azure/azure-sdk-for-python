839b48832c2e07e39d4e60ad1b8d2839569d1f47
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-03-19 02:29:13 -0700

    review feedback

--- sdk/eventhub/azure-eventhub/tests/perfstress_tests/send_event_batch.py
+++ sdk/eventhub/azure-eventhub/tests/perfstress_tests/send_event_batch.py
@@ -14,27 +14,25 @@ class SendEventBatchTest(_SendTest):
     def __init__(self, arguments):
         super().__init__(arguments)
         self.data = get_random_bytes(self.args.event_size)
+        self.to_send_event_data_batches = []
+        self._preload_event_data_batches()
 
-    def run_sync(self):
+    def _preload_event_data_batches(self):
         batch = self.producer.create_batch()
-        for i in range(self.args.num_events):
+        for _ in range(self.args.num_events):
             try:
                 batch.add(EventData(self.data))
             except ValueError:
                 # Batch full
-                self.producer.send_batch(batch)
+                self.to_send_event_data_batches.append(batch)
                 batch = self.producer.create_batch()
                 batch.add(EventData(self.data))
-        self.producer.send_batch(batch)
+        self.to_send_event_data_batches.append(batch)
+
+    def run_sync(self):
+        for event_data_batch in self.to_send_event_data_batches:
+            self.producer.send_batch(event_data_batch)
 
     async def run_async(self):
-        batch = await self.async_producer.create_batch()
-        for i in range(self.args.num_events):
-            try:
-                batch.add(EventData(self.data))
-            except ValueError:
-                # Batch full
-                await self.async_producer.send_batch(batch)
-                batch = await self.async_producer.create_atch()
-                batch.add(EventData(self.data))
-        await self.async_producer.send_batch(batch)
+        for event_data_batch in self.to_send_event_data_batches:
+            await self.async_producer.send_batch(event_data_batch)
