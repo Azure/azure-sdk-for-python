6a7a6a7b2ef76ac9b2a5377e8555b085fa2c9cfb
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2021-03-22 10:50:14 -0700

    warning

--- sdk/core/azure-core-tracing-opentelemetry/azure/core/tracing/ext/opentelemetry_span/__init__.py
+++ sdk/core/azure-core-tracing-opentelemetry/azure/core/tracing/ext/opentelemetry_span/__init__.py
@@ -4,6 +4,7 @@
 # ------------------------------------
 """Implements azure.core.tracing.AbstractSpan to wrap OpenTelemetry spans."""
 
+import warnings
 from opentelemetry import trace
 from opentelemetry.trace import Span, Link, Tracer, SpanKind as OpenTelemetrySpanKind
 from opentelemetry.context import attach, detach, get_current
@@ -120,6 +121,11 @@ class OpenTelemetrySpan(HttpSpanMixin, object):
     def kind(self, value):
         # type: (SpanKind) -> None
         """Set the span kind of this span."""
+        warnings.warn(
+            """Kind must be set while creating the span for Opentelemetry. It might be possible
+            that one of the packages you are using doesn't follow the latest Opentelemtry Spec.
+            Try updating the azure pacakges to the latest versions."""
+        )
         kind = (
             OpenTelemetrySpanKind.CLIENT if value == SpanKind.CLIENT else
             OpenTelemetrySpanKind.PRODUCER if value == SpanKind.PRODUCER else
@@ -214,6 +220,11 @@ class OpenTelemetrySpan(HttpSpanMixin, object):
         :param headers: A key value pair dictionary
         :type headers: dict
         """
+        warnings.warn(
+            """Link must be added while creating the span for Opentelemetry. It might be possible
+            that one of the packages you are using doesn't follow the latest Opentelemtry Spec.
+            Try updating the azure pacakges to the latest versions."""
+        )
         ctx = extract(DictGetter(), headers)
         span_ctx = get_span_from_context(ctx).get_span_context()
         current_span = cls.get_current_span()
