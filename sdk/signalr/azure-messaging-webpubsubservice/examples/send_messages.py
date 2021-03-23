9d8e0f9ec884c84739b798f868a32d6372df1530
Johan Stenberg <johan.stenberg@microsoft.com>
2021-03-23 12:42:25 -0700

    Updated example II

--- sdk/signalr/azure-messaging-webpubsubservice/examples/send_messages.py
+++ sdk/signalr/azure-messaging-webpubsubservice/examples/send_messages.py
@@ -9,25 +9,32 @@ api_key = os.environ['API_KEY']
 
 client = WebPubSubServiceClient(endpoint, credential=AzureKeyCredential(api_key))
 
-# Send message to everybody...
-request = prepare_send_to_all('skalman', 'Hello, all!')
+# Send message to everybody on the given hub...
+request = prepare_send_to_all('ahub', 'Hello, all!')
 response = client.send_request(request)
-response.raise_for_status()
-print(response)
+try:
+    response.raise_for_status()
+    print('Successfully sent a message')
+except:
+    print('Failed to send message: {}'.format(response))
+
 
 # Add a user to a group
-request = prepare_add_user_to_group('skalman', group='someGroup', user_id='me')
+request = prepare_add_user_to_group('ahub', group='someGroup', user_id='me')
 response = client.send_request(request)
-response.raise_for_status()
-print(response)
+try:
+    response.raise_for_status()
+except:
+    print('Failed to add user to group: {}'.format(response))
+
 
 # Add a connection to a group
-request = prepare_add_connection_to_group(hub='skalman', group='thegroup', connection_id='`7')
+request = prepare_add_connection_to_group(hub='ahub', group='thegroup', connection_id='`7')
 response = client.send_request(request)
 print(response)
 
 # Check if a group exists
-request = prepare_check_group_existence('skalman', group='missing')
+request = prepare_check_group_existence(hub='ahub', group='missing')
 response = client.send_request(request)
 print(response)
 
