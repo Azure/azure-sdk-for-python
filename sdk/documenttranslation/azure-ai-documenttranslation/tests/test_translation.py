6e729b0c604f42de6105f93293214276658708d1
MIDDLEEAST\v-moshaban <v-moshaban@microsoft.com>
2021-03-24 13:01:09 +0200

    [test] translation -> multiple sources single targets

--- sdk/documenttranslation/azure-ai-documenttranslation/tests/test_translation.py
+++ sdk/documenttranslation/azure-ai-documenttranslation/tests/test_translation.py
@@ -91,3 +91,49 @@ class TestTranslation(DocumentTranslationTest):
         self.assertIsNotNone(job_result.documents_not_yet_started_count)
         self.assertIsNotNone(job_result.documents_cancelled_count)
         self.assertIsNotNone(job_result.total_characters_charged)
+
+    @DocumentTranslationPreparer()
+    @DocumentTranslationClientPreparer()
+    def test_multiple_sources_single_target(self, client):
+        self._setup()  # set up test resources
+
+        # prepare translation inputs
+        translation_inputs = [
+            DocumentTranslationInput(
+                source_url=self.source_container_sas_url,
+                targets=[
+                    TranslationTarget(
+                        target_url=self.target_container_sas_url,
+                        language_code="es"
+                    )
+                ]
+            ),
+            DocumentTranslationInput(
+                source_url=self.additional_source_container_sas_url,
+                targets=[
+                    TranslationTarget(
+                        target_url=self.additional_target_container_sas_url,
+                        language_code="fr"
+                    )
+                ]
+            )
+        ]
+
+        # submit job
+        job_detail = client.create_translation_job(translation_inputs)
+        self.assertIsNotNone(job_detail.id)
+
+        # wait for result
+        job_result = client.wait_until_done(job_detail.id)
+
+        # assert
+        self.assertEqual(job_result.status, "Succeeded")  # job succeeded
+        self.assertIsNotNone(job_result.created_on)
+        self.assertIsNotNone(job_result.last_updated_on)
+        self.assertIsNotNone(job_result.documents_total_count)
+        self.assertIsNotNone(job_result.documents_failed_count)
+        self.assertIsNotNone(job_result.documents_succeeded_count)
+        self.assertIsNotNone(job_result.documents_in_progress_count)
+        self.assertIsNotNone(job_result.documents_not_yet_started_count)
+        self.assertIsNotNone(job_result.documents_cancelled_count)
+        self.assertIsNotNone(job_result.total_characters_charged)
