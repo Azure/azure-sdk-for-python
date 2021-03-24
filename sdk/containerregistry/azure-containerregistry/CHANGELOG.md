f4b814975542d7100a6304628b7695d60da45c6c
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-03-24 15:51:03 -0400

    ACR Base PR (#17509)
    * generated code from swagger
    
    * adding files from template
    
    * adding a tests.yml file
    
    * outlining base structures
    
    * adding more skeleton code
    
    * formatting code
    
    * adding some async files
    
    * fixes for apiview
    
    * added models and clients based on feedback from arch board
    
    * running black to keep code clean
    
    * more changes to auth
    
    * have a basic pipeline
    
    * added an auth policy for username & password
    
    * have a working auth policy
    
    * need to add a page iterator class for returned repositories
    
    * working list tags and list repositories methods
    
    * adding a change to the tests.yml file to run in live mode once I get there
    
    * added more methods
    
    * adding test infra
    
    * adding swagger file
    
    * regen with Jeremys swagger, fixing up implementations
    
    * added delete method, more models are filled out
    
    * code formatting and adding helper method to repository client
    
    * adding changes from newest swagger
    
    * updated generated code
    
    * adding tests for list and delete not exists, small changes to tests.yml
    
    * adding more list tests
    
    * updating readme and setup
    
    * adding more things the pipelines need
    
    * fixing pylint issues to get pipeline green
    
    * making all tests live tests
    
    * updating ci and readme with tables copies
    
    * updates to readme
    
    * adding more changes
    
    * updating readme
    
    * removing old sections from readme
    
    * more readme deletions
    
    * adding auth policy back in
    
    * ignore swagger readme
    
    * adding devtoolsback
    
    * readme fix, linting fix
    
    * updating shared reqs
    
    * regenerated code, fixed changelog, modified test structure
    
    * formatting
    
    * fixed up all lint and mypy issues
    
    * formatting
    
    * adding devtools to dev_reqs
    
    * including samples and license in manifest, thank you scbedd
    
    * updating swagger
    
    * small change to one test
    
    * adding kristas nits
    
    * linting fixes

---
+++ sdk/containerregistry/azure-containerregistry/CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release History
+
+## 0.0.1 (Unreleased)
+* First release of the Azure Container Registry library for Python
---
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/_helpers.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
