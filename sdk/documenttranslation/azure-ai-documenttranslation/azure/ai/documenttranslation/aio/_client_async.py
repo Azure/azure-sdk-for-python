f5c7016add6ec9133d476d951a68f322402b5c12
Krista Pratico <krpratic@microsoft.com>
2021-03-24 13:31:34 -0700

    small fixes found while updating samples

--- sdk/documenttranslation/azure-ai-documenttranslation/azure/ai/documenttranslation/aio/_client_async.py
+++ sdk/documenttranslation/azure-ai-documenttranslation/azure/ai/documenttranslation/aio/_client_async.py
@@ -88,7 +88,6 @@ class DocumentTranslationClient(object):
             # pylint: disable=protected-access
             inputs=DocumentTranslationInput._to_generated_list(inputs),
             cls=lambda pipeline_response, _, response_headers: response_headers,
-            polling=True,
             **kwargs
         )
 
@@ -159,7 +158,7 @@ class DocumentTranslationClient(object):
                 **kwargs
             ),
         )
-        return poller.result()
+        return await poller.result()
 
     @distributed_trace
     def list_submitted_jobs(self, **kwargs):
