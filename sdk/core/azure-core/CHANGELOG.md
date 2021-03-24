ed19a278a4730417fc6445b15ce1d8ec75b4e1ca
Swathi Pillalamarri <swathip@microsoft.com>
2021-03-24 17:31:53 -0400

    update version + add core tests

--- sdk/core/azure-core/CHANGELOG.md
+++ sdk/core/azure-core/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release History
 
-## 1.12.1 (Unreleased)
+## 1.13.0 (Unreleased)
 
 ### Features
 
--- sdk/core/azure-core/tests/test_connection_string_parsing.py
+++ sdk/core/azure-core/tests/test_connection_string_parsing.py
@@ -75,3 +75,107 @@ class CoreConnectionStringParserTests(AzureMgmtTestCase):
             with pytest.raises(ValueError) as e:
                 parse_result = parse_connection_string(conn_str)
             self.assertEqual(str(e.value), "Connection string is either blank or malformed.")
+
+    def test_case_insensitive_clear_method(self):
+        conn_str = 'enDpoiNT=XXXXENDPOINTXXXX;sharedaccesskeyname=XXXXPOLICYXXXX;SHAREDACCESSKEY=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        parse_result = parse_connection_string(conn_str, False)
+        parse_result.clear()
+        assert len(parse_result) == 0
+
+    def test_case_insensitive_copy_method(self):
+        conn_str = 'enDpoiNT=XXXXENDPOINTXXXX;sharedaccesskeyname=XXXXPOLICYXXXX;SHAREDACCESSKEY=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        parse_result = parse_connection_string(conn_str, False)
+        copied = parse_result.copy()
+        assert copied == parse_result
+    
+    def test_case_insensitive_get_method(self):
+        conn_str = 'Endpoint=XXXXENDPOINTXXXX;SharedAccessKeyName=XXXXPOLICYXXXX;SharedAccessKey=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        parse_result = parse_connection_string(conn_str, False)
+        assert parse_result.get("Endpoint") == 'XXXXENDPOINTXXXX'
+        assert parse_result.get("endPoint") == 'XXXXENDPOINTXXXX'
+        assert parse_result.get("SharedAccessKeyName") == 'XXXXPOLICYXXXX'
+        assert parse_result.get("sharedAccESSkEynAME") == 'XXXXPOLICYXXXX'
+        assert parse_result.get("SharedAccessKey") == 'THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        assert parse_result.get("sharedaccesskey") == 'THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        assert parse_result.get("accesskey") is None
+        assert parse_result.get("accesskey", "XXothertestkeyXX=") == "XXothertestkeyXX="
+
+    def test_case_insensitive_keys_method(self):
+        conn_str = 'enDpoiNT=XXXXENDPOINTXXXX;sharedaccesskeyname=XXXXPOLICYXXXX;SHAREDACCESSKEY=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        parse_result = parse_connection_string(conn_str, False)
+        keys = parse_result.keys()
+        assert len(keys) == 3
+        assert "enDpoiNT" in keys
+        assert "endpoint" in keys
+    
+    def test_case_insensitive_pop_method(self):
+        conn_str = 'enDpoiNT=XXXXENDPOINTXXXX;sharedaccesskeyname=XXXXPOLICYXXXX;SHAREDACCESSKEY=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        parse_result = parse_connection_string(conn_str, False)
+        endpoint = parse_result.pop("endpoint")
+        sharedaccesskey = parse_result.pop("shAredAccESSkey")
+        assert len(parse_result) == 1
+        assert endpoint == "XXXXENDPOINTXXXX"
+        assert sharedaccesskey == "THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX="
+
+    def test_case_insensitive_update_with_insensitive_method(self):
+        conn_str = 'enDpoiNT=XXXXENDPOINTXXXX;sharedaccesskeyname=XXXXPOLICYXXXX;SHAREDACCESSKEY=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        conn_str2 = 'hostName=XXXXENDPOINTXXXX;ACCessKEy=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX=;'
+        parse_result_insensitive = parse_connection_string(conn_str, False)
+        parse_result_insensitive2 = parse_connection_string(conn_str2, False)
+
+        parse_result_insensitive.update(parse_result_insensitive2)
+        assert len(parse_result_insensitive) == 5
+        assert parse_result_insensitive["hostname"] == "XXXXENDPOINTXXXX"
+        assert parse_result_insensitive["ACCESSkey"] == "THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX="
+
+        # check that update replace duplicate case insensitive keys
+        conn_str_duplicate_key = "endpoint=XXXXENDPOINT2XXXX;ACCessKEy=TestKey"
+        parse_result_insensitive_dupe = parse_connection_string(conn_str_duplicate_key, False)
+        parse_result_insensitive.update(parse_result_insensitive_dupe)
+        assert parse_result_insensitive_dupe["endPoint"] == "XXXXENDPOINT2XXXX"
+        assert parse_result_insensitive_dupe["ACCESSKEY"] == "TestKey" 
+        assert len(parse_result_insensitive) == 5
+
+    def test_case_insensitive_update_with_sensitive_method(self):
+        conn_str = 'enDpoiNT=XXXXENDPOINTXXXX;sharedaccesskeyname=XXXXPOLICYXXXX;SHAREDACCESSKEY=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        conn_str2 = 'hostName=XXXXENDPOINTXXXX;ACCessKEy=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX=;'
+        parse_result_insensitive = parse_connection_string(conn_str, False)
+        parse_result_sensitive = parse_connection_string(conn_str2, True)
+
+        parse_result_insensitive.update(parse_result_sensitive)
+        assert len(parse_result_insensitive) == 5
+        assert parse_result_insensitive["hostname"] == "XXXXENDPOINTXXXX"
+        assert parse_result_insensitive["ACCESSkey"] == "THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX="
+
+        # check that update with duplicate case sensitive keys makes duplicate keys case sensitive 
+        conn_str_duplicate_key = "endpoint=XXXXENDPOINT2XXXX;ACCessKEy=TestKey"
+        parse_result_sensitive_dupe = parse_connection_string(conn_str_duplicate_key, True)
+        parse_result_insensitive.update(parse_result_sensitive_dupe)
+        with pytest.raises(KeyError):
+            parse_result_sensitive_dupe["endPoint"]
+        with pytest.raises(KeyError):
+            parse_result_sensitive_dupe["ACCESSKEY"]
+
+        assert parse_result_insensitive["sharedAccESSkEynAME"] == 'XXXXPOLICYXXXX'
+        assert parse_result_insensitive["SharedAccessKey"] == 'THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        assert parse_result_sensitive_dupe["endpoint"] == "XXXXENDPOINT2XXXX"
+        assert parse_result_sensitive_dupe["ACCessKEy"] == "TestKey" 
+        assert len(parse_result_insensitive) == 5
+
+    def test_case_sensitive_update_with_insensitive_method(self):
+        conn_str = 'enDpoiNT=XXXXENDPOINTXXXX;sharedaccesskeyname=XXXXPOLICYXXXX;SHAREDACCESSKEY=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        conn_str2 = 'hostName=XXXXENDPOINTXXXX;ACCessKEy=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX=;'
+        parse_result_insensitive = parse_connection_string(conn_str, False)
+        parse_result_sensitive = parse_connection_string(conn_str2, True)
+
+        parse_result_sensitive.update(parse_result_insensitive)
+        assert len(parse_result_sensitive) == 5
+        assert parse_result_sensitive["hostName"] == "XXXXENDPOINTXXXX"
+        with pytest.raises(KeyError):
+            parse_result_sensitive["hostname"]
+
+    def test_case_insensitive_values_method(self):
+        conn_str = 'enDpoiNT=XXXXENDPOINTXXXX;sharedaccesskeyname=XXXXPOLICYXXXX;SHAREDACCESSKEY=THISISATESTKEYXXXXXXXXXXXXXXXXXXXXXXXXXXXX='
+        parse_result = parse_connection_string(conn_str, False)
+        values = parse_result.values()
+        assert len(values) == 3
