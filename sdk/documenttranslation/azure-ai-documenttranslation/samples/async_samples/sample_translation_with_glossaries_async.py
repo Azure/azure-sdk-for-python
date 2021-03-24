009720f3d6ddc8b6e882f89eb5cb65817ccd1cc0
Krista Pratico <krpratic@microsoft.com>
2021-03-24 13:22:41 -0700

    updates for samples for glossaries

--- sdk/documenttranslation/azure-ai-documenttranslation/samples/async_samples/sample_translation_with_glossaries_async.py
+++ sdk/documenttranslation/azure-ai-documenttranslation/samples/async_samples/sample_translation_with_glossaries_async.py
@@ -4,92 +4,90 @@
 # Licensed under the MIT License.
 # ------------------------------------
 
-import os
+"""
+FILE: sample_translation_with_glossaries_async.py
+
+DESCRIPTION:
+    This sample demonstrates how to create a translation job and apply custom glossaries to the translation.
+
+    To set up your containers for translation and generate SAS tokens to your containers (or files)
+    with the appropriate permissions, see the README.
+
+USAGE:
+    python sample_translation_with_glossaries_async.py
+
+    Set the environment variables with your own values before running the sample:
+    1) AZURE_DOCUMENT_TRANSLATION_ENDPOINT - the endpoint to your Form Recognizer resource.
+    2) AZURE_DOCUMENT_TRANSLATION_KEY - your Form Recognizer API key.
+    3) AZURE_SOURCE_CONTAINER_URL - the container SAS URL to your source container which has the documents
+        to be translated.
+    4) AZURE_TARGET_CONTAINER_URL - the container SAS URL to your target container where the translated documents
+        will be written.
+    5) AZURE_TRANSLATION_GLOSSARY_URL - the SAS URL to your glossary file
+"""
+
 import asyncio
 
-class CustomTranslationSampleAsync(object):
 
-    async def custom_translation_async(self):
-        # import libraries
-        from azure.core.credentials import AzureKeyCredential
-        from azure.ai.documenttranslation.aio import DocumentTranslationClient
-        from azure.ai.documenttranslation import (
-            DocumentTranslationInput,
-            TranslationTarget
-        )
+async def test_sample_translation_with_glossaries_async():
+    import os
+    from azure.core.credentials import AzureKeyCredential
+    from azure.ai.documenttranslation.aio import DocumentTranslationClient
+    from azure.ai.documenttranslation import (
+        DocumentTranslationInput,
+        TranslationTarget,
+        TranslationGlossary
+    )
 
-        # get service secrets
-        endpoint = os.environ["AZURE_DOCUMENT_TRANSLATION_ENDPOINT"]
-        key = os.environ["AZURE_DOCUMENT_TRANSLATION_KEY"]
-        source_container_url = os.environ["AZURE_SOURCE_CONTAINER_URL"]
-        target_container_url_fr = os.environ["AZURE_TARGET_CONTAINER_URL_FR"]
-        category_id = os.environ["AZURE_DOCUMENT_TRANSLATION_MODEL_ID"]
+    endpoint = os.environ["AZURE_DOCUMENT_TRANSLATION_ENDPOINT"]
+    key = os.environ["AZURE_DOCUMENT_TRANSLATION_KEY"]
+    source_container_url = os.environ["AZURE_SOURCE_CONTAINER_URL"]
+    target_container_url = os.environ["AZURE_TARGET_CONTAINER_URL"]
+    glossary_url = os.environ["AZURE_TRANSLATION_GLOSSARY_URL"]
 
-        # prepare translation job input
-        translation_inputs = [
-            DocumentTranslationInput(
+    client = DocumentTranslationClient(endpoint, AzureKeyCredential(key))
+
+    inputs = DocumentTranslationInput(
                 source_url=source_container_url,
                 targets=[
                     TranslationTarget(
-                        target_url=target_container_url_fr,
-                        language_code="fr",
-                        category_id=category_id,
-                        glossaries=["https://exampleglossary"]
+                        target_url=target_container_url,
+                        language_code="es",
+                        glossaries=[TranslationGlossary(glossary_url=glossary_url, format="TSV")]
                     )
-                ],
-                prefix="document_2021"
+                ]
             )
-        ]
 
-        # create translation client
-        client = DocumentTranslationClient(endpoint, AzureKeyCredential(key))
+    async with client:
+        job = await client.create_translation_job(inputs=[inputs])  # type: JobStatusResult
 
-        # run translation job
-        async with client:
-            job_detail = await client.create_translation_job(translation_inputs)
+        job_result = await client.wait_until_done(job.id)  # type: JobStatusResult
 
-            print("Job initial status: {}".format(job_detail.status))
-            print("Number of translations on documents: {}".format(job_detail.documents_total_count))
+        print("Job status: {}".format(job_result.status))
+        print("Job created on: {}".format(job_result.created_on))
+        print("Job last updated on: {}".format(job_result.last_updated_on))
+        print("Total number of translations on documents: {}".format(job_result.documents_total_count))
 
-            job_result = await client.wait_until_done(job_detail.id)  # type: JobStatusResult
-            if job_result.status == "Succeeded":
-                print("We translated our documents!")
-                if job_result.documents_failed_count > 0:
-                    await self.check_documents(client, job_result.id)
+        print("\nOf total documents...")
+        print("{} failed".format(job_result.documents_failed_count))
+        print("{} succeeded".format(job_result.documents_succeeded_count))
+        print("{} in progress".format(job_result.documents_in_progress_count))
+        print("{} not yet started".format(job_result.documents_not_yet_started_count))
+        print("{} cancelled\n".format(job_result.documents_cancelled_count))
 
-            elif job_result.status in ["Failed", "ValidationFailed"]:
-                if job_result.error:
-                    print("Translation job failed: {}: {}".format(job_result.error.code, job_result.error.message))
-                await self.check_documents(client, job_result.id)
-                exit(1)
-
-
-    async def check_documents(self, client, job_id):
-        from azure.core.exceptions import ResourceNotFoundError
-
-        try:
-            doc_statuses = client.list_all_document_statuses(job_id)  # type: AsyncItemPaged[DocumentStatusResult]
-        except ResourceNotFoundError as err:
-            print("Failed to process any documents in source/target container due to insufficient permissions.")
-            raise err
-
-        docs_to_retry = []
-        async for document in doc_statuses:
-            if document.status == "Failed":
-                print("Document at {} failed to be translated to {} language".format(
-                    document.translated_document_url, document.translate_to
-                ))
-                print("Document ID: {}, Error Code: {}, Message: {}".format(
-                    document.id, document.error.code, document.error.message
-                ))
-                if document.translated_document_url not in docs_to_retry:
-                    docs_to_retry.append(document.translated_document_url)
+        doc_results = client.list_all_document_statuses(job_result.id)  # type: AsyncItemPaged[DocumentStatusResult]
+        async for document in doc_results:
+            print("Document ID: {}".format(document.id))
+            print("Document status: {}".format(document.status))
+            if document.status == "Succeeded":
+                print("Document location: {}".format(document.translated_document_url))
+                print("Translated to language: {}\n".format(document.translate_to))
+            else:
+                print("Error Code: {}, Message: {}\n".format(document.error.code, document.error.message))
 
 
 async def main():
-    sample = CustomTranslationSampleAsync()
-    await sample.custom_translation_async()
-
+    await test_sample_translation_with_glossaries_async()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
