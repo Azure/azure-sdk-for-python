c26d4226abc750f8d7ff69b27d1c37aa9b62950a
seankane-msft <seankane@microsoft.com>
2021-03-29 10:51:05 -0400

    cleanup

--- sdk/containerregistry/azure-containerregistry/tests/testcase.py
+++ sdk/containerregistry/azure-containerregistry/tests/testcase.py
@@ -8,6 +8,7 @@ import json
 import os
 import re
 import six
+import subprocess
 import time
 
 from azure.containerregistry import (
@@ -27,8 +28,10 @@ from devtools_testutils import AzureTestCase
 
 REDACTED = "REDACTED"
 
+
 class AcrBodyReplacer(RecordingProcessor):
     """Replace request body for oauth2 exchanges"""
+
     def __init__(self, replacement="redacted"):
         self._replacement = replacement
         self._401_replacement = 'Bearer realm="https://fake_url.azurecr.io/oauth2/token",service="fake_url.azurecr.io",scope="fake_scope",error="invalid_token"'
@@ -50,27 +53,27 @@ class AcrBodyReplacer(RecordingProcessor):
         return bytes(s, "utf-8")
 
     def process_request(self, request):
-        if request.body:# and isinstance(request.body, six.binary_type):
+        if request.body:
             request.body = self._scrub_body(request.body)
 
         return request
 
     def process_response(self, response):
         try:
-            headers = response['headers']
+            headers = response["headers"]
             auth_header = None
             if "www-authenticate" in headers:
-                response['headers']["www-authenticate"] = self._401_replacement
+                response["headers"]["www-authenticate"] = self._401_replacement
 
-            body = response['body']
+            body = response["body"]
             try:
-                refresh = json.loads(body['string'])
+                refresh = json.loads(body["string"])
                 if "refresh_token" in refresh.keys():
-                    refresh['refresh_token'] = REDACTED
-                    body['string'] = json.dumps(refresh)
+                    refresh["refresh_token"] = REDACTED
+                    body["string"] = json.dumps(refresh)
                 if "access_token" in refresh.keys():
                     refresh["access_token"] = REDACTED
-                    body['string'] = json.dumps(refresh)
+                    body["string"] = json.dumps(refresh)
 
             except json.decoder.JSONDecodeError:
                 pass
@@ -84,6 +87,7 @@ class FakeTokenCredential(object):
     """Protocol for classes able to provide OAuth tokens.
     :param str scopes: Lets you specify the type of access needed.
     """
+
     def __init__(self):
         self.token = AccessToken("YOU SHALL NOT PASS", 0)
 
@@ -92,7 +96,6 @@ class FakeTokenCredential(object):
 
 
 class ContainerRegistryTestClass(AzureTestCase):
-
     def __init__(self, method_name):
         super(ContainerRegistryTestClass, self).__init__(method_name)
         self.vcr.match_on = ["path", "method", "query"]
@@ -105,19 +108,27 @@ class ContainerRegistryTestClass(AzureTestCase):
     def _fake_sleep(self, *args, **kwargs):
         pass
 
-    def _import_tag_to_be_deleted(self, endpoint, repository="hello-world", resource_group="fake_rg"):
+    def _import_tag_to_be_deleted(
+        self, endpoint, repository="hello-world", resource_group="fake_rg"
+    ):
         if not self.is_live:
             return
         registry = endpoint.split(".")[0]
         command = [
-            "powershell.exe", "Import-AzcontainerRegistryImage",
-            "-ResourceGroupName", "'{}'".format(resource_group),
-            "-RegistryName", "'{}'".format(registry),
-            "-SourceImage", "'library/hello-world'",
-            "-SourceRegistryUri", "'registry.hub.docker.com'",
-            "-TargetTag", "'hello-world:to_be_deleted'"
-            "-Mode", "'Force'"
-
+            "powershell.exe",
+            "Import-AzcontainerRegistryImage",
+            "-ResourceGroupName",
+            "'{}'".format(resource_group),
+            "-RegistryName",
+            "'{}'".format(registry),
+            "-SourceImage",
+            "'library/hello-world'",
+            "-SourceRegistryUri",
+            "'registry.hub.docker.com'",
+            "-TargetTag",
+            "'hello-world:to_be_deleted'",
+            "-Mode",
+            "'Force'",
         ]
         subprocess.check_call(command)
 
