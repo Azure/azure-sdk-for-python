4917b0e23a8b14302df895a602884acc96a4f5b6
SDKAuto <sdkautomation@microsoft.com>
2021-03-30 02:52:05 +0000

    CodeGen from PR 13715 in Azure/azure-rest-api-specs resourcegraph readme t2 config (#13715)

--- sdk/resources/azure-mgmt-resourcegraph/azure/mgmt/resourcegraph/operations/_resource_graph_client_operations.py
+++ sdk/resources/azure-mgmt-resourcegraph/azure/mgmt/resourcegraph/operations/_resource_graph_client_operations.py
@@ -30,8 +30,7 @@ class ResourceGraphClientOperationsMixin(object):
         **kwargs  # type: Any
     ):
         # type: (...) -> "_models.QueryResponse"
-        """Queries the resources managed by Azure Resource Manager for all subscriptions specified in the
-        request.
+        """Queries the resources managed by Azure Resource Manager for scopes specified in the request.
 
         :param query: Request specifying query and its options.
         :type query: ~azure.mgmt.resourcegraph.models.QueryRequest
@@ -45,7 +44,7 @@ class ResourceGraphClientOperationsMixin(object):
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2020-04-01-preview"
+        api_version = "2021-03-01"
         content_type = kwargs.pop("content_type", "application/json")
         accept = "application/json"
 
@@ -80,115 +79,3 @@ class ResourceGraphClientOperationsMixin(object):
 
         return deserialized
     resources.metadata = {'url': '/providers/Microsoft.ResourceGraph/resources'}  # type: ignore
-
-    def resource_changes(
-        self,
-        parameters,  # type: "_models.ResourceChangesRequestParameters"
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> "_models.ResourceChangeList"
-        """List changes to a resource for a given time interval.
-
-        :param parameters: the parameters for this request for changes.
-        :type parameters: ~azure.mgmt.resourcegraph.models.ResourceChangesRequestParameters
-        :keyword callable cls: A custom type or function that will be passed the direct response
-        :return: ResourceChangeList, or the result of cls(response)
-        :rtype: ~azure.mgmt.resourcegraph.models.ResourceChangeList
-        :raises: ~azure.core.exceptions.HttpResponseError
-        """
-        cls = kwargs.pop('cls', None)  # type: ClsType["_models.ResourceChangeList"]
-        error_map = {
-            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
-        }
-        error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2020-04-01-preview"
-        content_type = kwargs.pop("content_type", "application/json")
-        accept = "application/json"
-
-        # Construct URL
-        url = self.resource_changes.metadata['url']  # type: ignore
-
-        # Construct parameters
-        query_parameters = {}  # type: Dict[str, Any]
-        query_parameters['api-version'] = self._serialize.query("api_version", api_version, 'str')
-
-        # Construct headers
-        header_parameters = {}  # type: Dict[str, Any]
-        header_parameters['Content-Type'] = self._serialize.header("content_type", content_type, 'str')
-        header_parameters['Accept'] = self._serialize.header("accept", accept, 'str')
-
-        body_content_kwargs = {}  # type: Dict[str, Any]
-        body_content = self._serialize.body(parameters, 'ResourceChangesRequestParameters')
-        body_content_kwargs['content'] = body_content
-        request = self._client.post(url, query_parameters, header_parameters, **body_content_kwargs)
-        pipeline_response = self._client._pipeline.run(request, stream=False, **kwargs)
-        response = pipeline_response.http_response
-
-        if response.status_code not in [200]:
-            map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.ErrorResponse, response)
-            raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
-
-        deserialized = self._deserialize('ResourceChangeList', pipeline_response)
-
-        if cls:
-            return cls(pipeline_response, deserialized, {})
-
-        return deserialized
-    resource_changes.metadata = {'url': '/providers/Microsoft.ResourceGraph/resourceChanges'}  # type: ignore
-
-    def resource_change_details(
-        self,
-        parameters,  # type: "_models.ResourceChangeDetailsRequestParameters"
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> "_models.ResourceChangeData"
-        """Get resource change details.
-
-        :param parameters: The parameters for this request for resource change details.
-        :type parameters: ~azure.mgmt.resourcegraph.models.ResourceChangeDetailsRequestParameters
-        :keyword callable cls: A custom type or function that will be passed the direct response
-        :return: ResourceChangeData, or the result of cls(response)
-        :rtype: ~azure.mgmt.resourcegraph.models.ResourceChangeData
-        :raises: ~azure.core.exceptions.HttpResponseError
-        """
-        cls = kwargs.pop('cls', None)  # type: ClsType["_models.ResourceChangeData"]
-        error_map = {
-            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
-        }
-        error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2020-04-01-preview"
-        content_type = kwargs.pop("content_type", "application/json")
-        accept = "application/json"
-
-        # Construct URL
-        url = self.resource_change_details.metadata['url']  # type: ignore
-
-        # Construct parameters
-        query_parameters = {}  # type: Dict[str, Any]
-        query_parameters['api-version'] = self._serialize.query("api_version", api_version, 'str')
-
-        # Construct headers
-        header_parameters = {}  # type: Dict[str, Any]
-        header_parameters['Content-Type'] = self._serialize.header("content_type", content_type, 'str')
-        header_parameters['Accept'] = self._serialize.header("accept", accept, 'str')
-
-        body_content_kwargs = {}  # type: Dict[str, Any]
-        body_content = self._serialize.body(parameters, 'ResourceChangeDetailsRequestParameters')
-        body_content_kwargs['content'] = body_content
-        request = self._client.post(url, query_parameters, header_parameters, **body_content_kwargs)
-        pipeline_response = self._client._pipeline.run(request, stream=False, **kwargs)
-        response = pipeline_response.http_response
-
-        if response.status_code not in [200]:
-            map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.ErrorResponse, response)
-            raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
-
-        deserialized = self._deserialize('ResourceChangeData', pipeline_response)
-
-        if cls:
-            return cls(pipeline_response, deserialized, {})
-
-        return deserialized
-    resource_change_details.metadata = {'url': '/providers/Microsoft.ResourceGraph/resourceChangeDetails'}  # type: ignore
