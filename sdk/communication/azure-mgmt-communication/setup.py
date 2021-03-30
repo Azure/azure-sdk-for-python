6dc4c330d5f573ef79ce06cd7ad102774cc811d9
Yan Zhang (WICRESOFT NORTH AMERICA LTD) <v-yanzhang@microsoft.com>
2021-03-30 10:02:27 +0800

    remove modification

--- sdk/communication/azure-mgmt-communication/setup.py
+++ sdk/communication/azure-mgmt-communication/setup.py
@@ -82,9 +82,7 @@ setup(
     ]),
     install_requires=[
         'msrest>=0.5.0',
-        'azure-mgmt-core>=1.2.0,<2.0.0',
         'azure-common~=1.1',
-        'azure-core>=1.9.0,<2.0.0',
         'azure-mgmt-core>=1.2.0,<2.0.0',
     ],
     extras_require={
