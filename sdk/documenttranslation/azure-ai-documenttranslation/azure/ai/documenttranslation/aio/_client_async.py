831333563628d8a9221c22934f52e2efd1649949
Mohamed Shaban <v-moshaban@microsoft.com>
2021-03-31 19:12:55 +0200

    [doc translator] adding tests (#17511)
    * [tests][sync] test supported formats
    
    * [tests][sync] test translation
    
    * [tests] comeplete the supported formats test
    
    * [tests] translation test
    
    * [tests] translation -> single source multiple target
    
    * [test] translation -> multiple sources single targets
    
    * [test] translation -> prefix and suffix
    
    * [test] translation -> list all doc statuses
    
    * [test] updated tests to create their own containers
    
    * [tests] update base test functions to support offline tests
    
    * [test] adding more tests
    
    * [tests] status tests
    
    * [tests] list statuses
    
    * [tests] list submitted jobs
    
    * [tests] doc status
    
    * [tests] update paging in list all statuses
    
    * [tests] some refactoring
    
    * [tests] added meaningful assertions + method refactoring
    
    * [test] cancel job
    
    * [tests] refactor helper functions
    
    * [tests] added async tests
    
    * [tests] skip some tests (functionality is hidden for now)
    
    * [tests][pr reviews] correct silly mistake -> calling function with argument self!
    
    * [test] lazy fix -> sample trsnaltion jobs -> wait until done for status to be succeeded
    
    * [test] some bugs
    
    * [tests] fix gettting first item if async iterator
    
    * [tests] fix cancelled job status propagation
    
    * [tests] fix iterator len issue in sync client
    
    * [tests] fix 'self' keyword used unnecessarily
    
    * [test] fix get length of async iterator
    
    * [tests] update tests after actually running them
    
    * [tests] sync client updates
    
    * [tests] add sync client test recordings
    
    * [tests] fixed async client tests
    
    * [tests] adding async recordings
    
    * [tests] adding conftest.py for py2 async tests
    
    * [tests] update recordings
    
    * [tests] pr reviews
    
    * [tests] created AsyncDocumentTranslationTest for python 3
    
    * [tests] setting polling interval to zero when running recorded tests
    
    * [tests] updated polling interval for wait_until_done method in client
    
    * [tests] updating recorded tests
    
    * [tests] updated records -> downgrade azure core bug
    
    * [tests] fixing linting errors

--- sdk/documenttranslation/azure-ai-documenttranslation/azure/ai/documenttranslation/aio/_client_async.py
+++ sdk/documenttranslation/azure-ai-documenttranslation/azure/ai/documenttranslation/aio/_client_async.py
@@ -203,7 +203,7 @@ class DocumentTranslationClient(object):
             initial_response=pipeline_response,
             deserialization_callback=callback,
             polling_method=AsyncLROBasePolling(
-                timeout=30,
+                timeout=self._client._config.polling_interval,  # pylint: disable=protected-access
                 lro_algorithms=[TranslationPolling()],
                 **kwargs
             ),
