b3c4208937fb3be383fee9c898aaa49028be3a51
Swathi Pillalamarri <swathip@microsoft.com>
2021-03-30 20:34:34 -0400

    updated param description

--- sdk/core/azure-core/azure/core/utils/_connection_string_parser.py
+++ sdk/core/azure-core/azure/core/utils/_connection_string_parser.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
@@ -11,12 +12,10 @@ def parse_connection_string(conn_str, case_sensitive_keys=False):
     """Parses the connection string into a dict of its component parts, with the option of preserving case
     of keys, and validates that each key in the connection string has a provided value. If case of keys
     is not preserved (ie. `case_sensitive_keys=False`), then a dict with LOWERCASE KEYS will be returned.
-    :param conn_str: String with connection details provided by Azure services.
-    :type conn_str: str
-    :param case_sensitive_keys: Indicates whether returned object should retrieve
-        keys with case sensitive checking. True means that case of keys will be preserved.
-        Default is False.
-    :type case_sensitive_keys: bool
+
+    :param str conn_str: String with connection details provided by Azure services.
+    :param bool case_sensitive_keys: Indicates whether the casing of the keys will be preserved. When `False`(the
+        default), all keys will be lower-cased. If set to `True`, the original casing of the keys will be preserved.
     :rtype: Mapping
     :raises:
         ValueError: if each key in conn_str does not have a corresponding value and
