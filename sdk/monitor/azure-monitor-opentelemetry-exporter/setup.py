08cc885651b13b7335e61b4f040b2c4b5b0cc3a4
Leighton Chen <lechen@microsoft.com>
2021-04-01 13:56:33 -0700

    version

--- sdk/monitor/azure-monitor-opentelemetry-exporter/setup.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/setup.py
@@ -74,7 +74,7 @@ setup(
         'azure.monitor',
         'azure.monitor.opentelemetry'
     ]),
-    python_requires=">=3.5.0",
+    python_requires=">=3.6.0",
     install_requires=[
         "azure-core<2.0.0,>=1.6.0",
         "msrest>=0.6.10",
