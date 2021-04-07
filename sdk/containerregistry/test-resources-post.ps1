b93e2e327054c4f598055dc129335735806d9613
seankane-msft <seankane@microsoft.com>
2021-04-07 14:44:47 -0400

    fixing up the test-resources-post script

--- sdk/containerregistry/test-resources-post.ps1
+++ sdk/containerregistry/test-resources-post.ps1
@@ -1,5 +1,35 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-# Install docker
+param (
+    [hashtable] $DeploymentOutputs,
+    [string] $TenantId,
+    [string] $TestApplicationId,
+    [string] $TestApplicationSecret
+)
 
+$version = (Get-Module Az).Version
+# Detect if we are running an azure powershell version without the Import-AzContainerRegistryImage command
+$minimumVersion = New-Object -TypeName Version -ArgumentList "5.6.0"
+if ($version -lt $minimumVersion) {
+    Update-Module Az -RequiredVersion "5.7.0" -Force
+}
+
+Import-AzContainerRegistryImage `
+    -ResourceGroupName $DeploymentOutputs['CONTAINERREGISTRY_RESOURCE_GROUP'] `
+    -RegistryName $DeploymentOutputs['CONTAINERREGISTRY_USERNAME'] `
+    -SourceImage 'library/hello-world' -SourceRegistryUri 'registry.hub.docker.com' `
+    -TargetTag @('library/hello-world:latest', 'library/hello-world:v1', 'library/hello-world:v2', 'library/hello-world:v3', 'library/hello-world:v4') `
+    -Mode 'Force'
+
+Import-AzContainerRegistryImage `
+    -ResourceGroupName $DeploymentOutputs['CONTAINERREGISTRY_RESOURCE_GROUP'] `
+    -RegistryName $DeploymentOutputs['CONTAINERREGISTRY_USERNAME'] `
+    -SourceImage 'library/alpine' -SourceRegistryUri 'registry.hub.docker.com' `
+    -Mode 'Force'
+
+Import-AzContainerRegistryImage `
+    -ResourceGroupName $DeploymentOutputs['CONTAINERREGISTRY_RESOURCE_GROUP'] `
+    -RegistryName $DeploymentOutputs['CONTAINERREGISTRY_USERNAME'] `
+    -SourceImage 'library/busybox' -SourceRegistryUri 'registry.hub.docker.com' `
+    -Mode 'Force'
