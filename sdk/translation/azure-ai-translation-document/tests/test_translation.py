99a1c756f451d32a3568d40936df8e2bc8c2ee09
Krista Pratico <krpratic@microsoft.com>
2021-04-08 16:58:24 -0700

    skip bad target tests (#17925)

--- sdk/translation/azure-ai-translation-document/tests/test_translation.py
+++ sdk/translation/azure-ai-translation-document/tests/test_translation.py
@@ -182,6 +182,7 @@ class TestTranslation(DocumentTranslationTest):
             job = client.wait_until_done(job.id)
         assert e.value.error.code == "InvalidDocumentAccessLevel"
 
+    @pytest.mark.skip("https://github.com/Azure/azure-sdk-for-python/issues/17914")
     @DocumentTranslationPreparer()
     @DocumentTranslationClientPreparer()
     def test_bad_input_target(self, client):
