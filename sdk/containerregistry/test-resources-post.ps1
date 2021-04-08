99bc805f3cbb822dd0ff1404690f293bc58cb9b0
seankane-msft <seankane@microsoft.com>
2021-04-08 11:20:04 -0400

    flipping orders

--- sdk/containerregistry/test-resources-post.ps1
+++ sdk/containerregistry/test-resources-post.ps1
@@ -8,14 +8,14 @@ param (
     [string] $TestApplicationSecret
 )
 
+Import-Module Az -Force
+
 $version = (Get-Module Az).Version
 # Detect if we are running an azure powershell version without the Import-AzContainerRegistryImage command
 $minimumVersion = New-Object -TypeName Version -ArgumentList "5.6.0"
 if ($version -lt $minimumVersion) {
     Update-Module Az -RequiredVersion "5.7.0" -Force
 }
-
-Import-Module Az -Force
 
 Import-AzContainerRegistryImage `
     -ResourceGroupName $DeploymentOutputs['CONTAINERREGISTRY_RESOURCE_GROUP'] `
