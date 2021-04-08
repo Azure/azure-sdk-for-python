cb464b2887ee1985774e87b46587268a2d436d57
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-04-08 11:51:40 -0400

    [Tables] Cosmos workaround for Merge/Update (#17888)
    Closes #13844

--- sdk/tables/azure-data-tables/azure/data/tables/aio/_table_batch_async.py
+++ sdk/tables/azure-data-tables/azure/data/tables/aio/_table_batch_async.py
@@ -8,7 +8,7 @@ import msrest
 
 from azure.core.pipeline import PipelineResponse
 
-
+from .._common_conversion import _is_cosmos_endpoint, _transform_patch_to_cosmos_post
 from .._models import UpdateMode
 from .._serialize import (
     _get_match_headers,
@@ -458,6 +458,8 @@ class TableBatchOperations(object):
         request = self._client._client.patch(  # pylint: disable=protected-access
             url, query_parameters, header_parameters, **body_content_kwargs
         )
+        if _is_cosmos_endpoint(url):
+            _transform_patch_to_cosmos_post(request)
         self._requests.append(request)
 
     _batch_merge_entity.metadata = {
