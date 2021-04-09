75140fc7231758347b89e5cacae4d0bcca0cb2fe
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-04-09 13:47:18 -0400

    Acr remove delete (#17943)
    #17766

--- sdk/containerregistry/azure-containerregistry/tests/test_container_registry_client_async.py
+++ sdk/containerregistry/azure-containerregistry/tests/test_container_registry_client_async.py
@@ -63,7 +63,7 @@ class TestContainerRegistryClient(AsyncContainerRegistryTestClass):
     @acr_preparer()
     async def test_delete_repository(self, containerregistry_endpoint, containerregistry_resource_group):
         repository = self.get_resource_name("repo")
-        self._import_tag_to_be_deleted(
+        self.import_repo(
             containerregistry_endpoint, resource_group=containerregistry_resource_group, repository=repository
         )
         client = self.create_registry_client(containerregistry_endpoint)
@@ -72,8 +72,6 @@ class TestContainerRegistryClient(AsyncContainerRegistryTestClass):
         assert isinstance(result, DeletedRepositoryResult)
         assert result.deleted_registry_artifact_digests is not None
         assert result.deleted_tags is not None
-
-        self.sleep(5)
 
         async for repo in client.list_repositories():
             if repo == repository:
