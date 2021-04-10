63df5bbace46571aa28761c013e83b3e973cca13
Lakshman Sundaralingam <lakshmans@microsoft.com>
2021-04-09 18:25:30 -0700

    Updated expected error code

--- sdk/communication/azure-communication-sms/tests/test_sms_client_e2e.py
+++ sdk/communication/azure-communication-sms/tests/test_sms_client_e2e.py
@@ -133,7 +133,7 @@ class SMSClientTest(CommunicationTestCase):
                 to=[self.phone_number],
                 message="Hello World via SMS")
         
-        assert str(ex.value.status_code) == "404"
+        assert str(ex.value.status_code) == "401"
         assert ex.value.message is not None
 
     @pytest.mark.live_test_only
