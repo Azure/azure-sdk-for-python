bf43552adf0287778b87d65969212a173306550e
Leo Li <jixli@microsoft.com>
2021-04-14 08:24:48 +0800

    Correct expiration epoch time

--- sdk/communication/azure-communication-chat/azure/communication/chat/_shared/utils.py
+++ sdk/communication/azure-communication-chat/azure/communication/chat/_shared/utils.py
@@ -17,8 +17,8 @@ from azure.core.credentials import AccessToken
 
 
 def _convert_datetime_to_utc_int(expires_on):
-    epoch = time.mktime(datetime(1970, 1, 1).timetuple())
-    return epoch-time.mktime(expires_on.timetuple())
+    epoch = time.localtime().tm_gmtoff
+    return time.mktime(expires_on.timetuple()) + epoch
 
 def parse_connection_str(conn_str):
     # type: (str) -> Tuple[str, str, str, str]
