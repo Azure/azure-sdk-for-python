1450db230eeb7595e39f9d5b680cbc23469d6fbd
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-04-15 10:56:25 -0700

    more mypy fix

--- sdk/servicebus/azure-servicebus/azure/servicebus/management/_model_workaround.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/management/_model_workaround.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-from typing import Dict, Iterable, Any, Optional, Tuple, Type
+from typing import Dict, Iterable, Any, Optional, Tuple, Type, Union
 from collections import OrderedDict
 from datetime import timedelta
 from msrest.serialization import Model
