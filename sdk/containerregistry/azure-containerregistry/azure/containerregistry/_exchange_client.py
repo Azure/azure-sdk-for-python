063aef4d8524184ed522d24735bc7dc92a3b2f6a
seankane-msft <seankane@microsoft.com>
2021-04-15 15:15:35 -0400

    adding cache to first token

--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/_exchange_client.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/_exchange_client.py
@@ -3,6 +3,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
+from datetime import datetime, timedelta
 import re
 from typing import TYPE_CHECKING
 
@@ -57,6 +58,21 @@ class ACRExchangeClient(object):
             **kwargs
         )
         self._credential = credential
+        self._token_value = None
+        self._expiry = None
+
+    def get_aad_token(self):
+        if not self._token_value or self.token_is_expired:
+            self._token_value = self._credential.get_token(self._credential_scopes).token
+            self._expiry = datetime.now()
+        return self._token_value
+
+    def token_is_expired(self):
+        # type: () -> bool
+        if not self._expiry:
+            return True
+        # Good for sixty minutes, we'll use 59 to give us an extra cushion
+        return datetime.now() - self._expiry > timedelta(minutes=59)
 
     def get_acr_access_token(self, challenge, **kwargs):
         # type: (str, Dict[str, Any]) -> str
@@ -69,7 +85,7 @@ class ACRExchangeClient(object):
     def exchange_aad_token_for_refresh_token(self, service=None, **kwargs):
         # type: (str, Dict[str, Any]) -> str
         refresh_token = self._client.authentication.exchange_aad_access_token_for_acr_refresh_token(
-            service=service, access_token=self._credential.get_token(self._credential_scopes).token, **kwargs
+            service=service, access_token=self.get_aad_token(), **kwargs
         )
         return refresh_token.refresh_token
 
