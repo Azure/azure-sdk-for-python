42a92ae509f2f47ababa9a61b8607a202233b145
msyyc <70930885+msyyc@users.noreply.github.com>
2021-04-15 17:23:16 +0800

    [AutoRelease] t2-hybridcompute-2021-04-15-18223 (#18062)
    * CodeGen from PR 13910 in Azure/azure-rest-api-specs
    hybridcompute readme t2 config (#13910)
    
    * readme t2 config
    
    * hybridcompute t2 readme config
    
    * version,CHANGELOG
    
    * test
    
    * hybridcompute changelog config
    
    Co-authored-by: SDKAuto <sdkautomation@microsoft.com>
    Co-authored-by: PythonSdkPipelines <PythonSdkPipelines>
    Co-authored-by: Zed <601306339@qq.com>
    Co-authored-by: Zed Lei <59104634+RAY-316@users.noreply.github.com>

--- sdk/hybridcompute/azure-mgmt-hybridcompute/azure/mgmt/hybridcompute/__init__.py
+++ sdk/hybridcompute/azure-mgmt-hybridcompute/azure/mgmt/hybridcompute/__init__.py
@@ -7,6 +7,9 @@
 # --------------------------------------------------------------------------
 
 from ._hybrid_compute_management_client import HybridComputeManagementClient
+from ._version import VERSION
+
+__version__ = VERSION
 __all__ = ['HybridComputeManagementClient']
 
 try:
