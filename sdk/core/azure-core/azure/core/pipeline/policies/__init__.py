53fab2e65909ca8f613768b92033608b22a45ecf
Rakshith Bhyravabhotla <sabhyrav@microsoft.com>
2021-04-16 10:55:43 -0700

    Update sdk/core/azure-core/azure/core/pipeline/policies/__init__.py

--- sdk/core/azure-core/azure/core/pipeline/policies/__init__.py
+++ sdk/core/azure-core/azure/core/pipeline/policies/__init__.py
@@ -25,10 +25,7 @@
 # --------------------------------------------------------------------------
 
 from ._base import HTTPPolicy, SansIOHTTPPolicy, RequestHistory
-from ._authentication import (
-    BearerTokenCredentialPolicy, AzureKeyCredentialPolicy, AzureSasCredentialPolicy,
-    AzureNamedKeyCredentialPolicy
-    )
+from ._authentication import BearerTokenCredentialPolicy, AzureKeyCredentialPolicy, AzureSasCredentialPolicy
 from ._custom_hook import CustomHookPolicy
 from ._redirect import RedirectPolicy
 from ._retry import RetryPolicy, RetryMode
