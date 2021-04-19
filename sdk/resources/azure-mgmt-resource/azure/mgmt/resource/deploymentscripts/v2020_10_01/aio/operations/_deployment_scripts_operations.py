28545a0bfd5586dcf2de8162cd4af9bfaa72f7f6
msyyc <70930885+msyyc@users.noreply.github.com>
2021-04-19 13:50:13 +0800

    [AutoRelease] t2-resource-2021-04-16-89132 (#18097)
    * CodeGen from PR 13974 in Azure/azure-rest-api-specs
    resource t2 readme config (#13974)
    
    * version,CHANGELOG
    
    * test
    
    * all version change
    
    Co-authored-by: SDKAuto <sdkautomation@microsoft.com>
    Co-authored-by: PythonSdkPipelines <PythonSdkPipelines>
    Co-authored-by: Zed <601306339@qq.com>
    Co-authored-by: Zed Lei <59104634+RAY-316@users.noreply.github.com>

--- sdk/resources/azure-mgmt-resource/azure/mgmt/resource/deploymentscripts/v2020_10_01/aio/operations/_deployment_scripts_operations.py
+++ sdk/resources/azure-mgmt-resource/azure/mgmt/resource/deploymentscripts/v2020_10_01/aio/operations/_deployment_scripts_operations.py
@@ -63,7 +63,7 @@ class DeploymentScriptsOperations:
         url = self._create_initial.metadata['url']  # type: ignore
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
-            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1, pattern=r'^[-\w\._\(\)]+$'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1),
             'scriptName': self._serialize.url("script_name", script_name, 'str', max_length=90, min_length=1),
         }
         url = self._client.format_url(url, **path_format_arguments)
@@ -86,7 +86,7 @@ class DeploymentScriptsOperations:
 
         if response.status_code not in [200, 201]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.DeploymentScriptsError, response)
+            error = self._deserialize.failsafe_deserialize(_models.DeploymentScriptsError, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         if response.status_code == 200:
@@ -118,8 +118,8 @@ class DeploymentScriptsOperations:
         :type deployment_script: ~azure.mgmt.resource.deploymentscripts.v2020_10_01.models.DeploymentScript
         :keyword callable cls: A custom type or function that will be passed the direct response
         :keyword str continuation_token: A continuation token to restart a poller from a saved state.
-        :keyword polling: True for ARMPolling, False for no polling, or a
-         polling object for personal polling strategy
+        :keyword polling: Pass in True if you'd like the AsyncARMPolling polling method,
+         False for no polling, or your own initialized polling object for a personal polling strategy.
         :paramtype polling: bool or ~azure.core.polling.AsyncPollingMethod
         :keyword int polling_interval: Default waiting time between two polls for LRO operations if no Retry-After header is present.
         :return: An instance of AsyncLROPoller that returns either DeploymentScript or the result of cls(response)
@@ -154,7 +154,7 @@ class DeploymentScriptsOperations:
 
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
-            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1, pattern=r'^[-\w\._\(\)]+$'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1),
             'scriptName': self._serialize.url("script_name", script_name, 'str', max_length=90, min_length=1),
         }
 
@@ -205,7 +205,7 @@ class DeploymentScriptsOperations:
         url = self.update.metadata['url']  # type: ignore
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
-            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1, pattern=r'^[-\w\._\(\)]+$'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1),
             'scriptName': self._serialize.url("script_name", script_name, 'str', max_length=90, min_length=1),
         }
         url = self._client.format_url(url, **path_format_arguments)
@@ -231,7 +231,7 @@ class DeploymentScriptsOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.DeploymentScriptsError, response)
+            error = self._deserialize.failsafe_deserialize(_models.DeploymentScriptsError, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('DeploymentScript', pipeline_response)
@@ -271,7 +271,7 @@ class DeploymentScriptsOperations:
         url = self.get.metadata['url']  # type: ignore
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
-            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1, pattern=r'^[-\w\._\(\)]+$'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1),
             'scriptName': self._serialize.url("script_name", script_name, 'str', max_length=90, min_length=1),
         }
         url = self._client.format_url(url, **path_format_arguments)
@@ -290,7 +290,7 @@ class DeploymentScriptsOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.DeploymentScriptsError, response)
+            error = self._deserialize.failsafe_deserialize(_models.DeploymentScriptsError, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('DeploymentScript', pipeline_response)
@@ -331,7 +331,7 @@ class DeploymentScriptsOperations:
         url = self.delete.metadata['url']  # type: ignore
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
-            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1, pattern=r'^[-\w\._\(\)]+$'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1),
             'scriptName': self._serialize.url("script_name", script_name, 'str', max_length=90, min_length=1),
         }
         url = self._client.format_url(url, **path_format_arguments)
@@ -350,7 +350,7 @@ class DeploymentScriptsOperations:
 
         if response.status_code not in [200, 204]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.DeploymentScriptsError, response)
+            error = self._deserialize.failsafe_deserialize(_models.DeploymentScriptsError, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         if cls:
@@ -414,7 +414,7 @@ class DeploymentScriptsOperations:
             response = pipeline_response.http_response
 
             if response.status_code not in [200]:
-                error = self._deserialize(_models.DeploymentScriptsError, response)
+                error = self._deserialize.failsafe_deserialize(_models.DeploymentScriptsError, response)
                 map_error(status_code=response.status_code, response=response, error_map=error_map)
                 raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
@@ -454,7 +454,7 @@ class DeploymentScriptsOperations:
         url = self.get_logs.metadata['url']  # type: ignore
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
-            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1, pattern=r'^[-\w\._\(\)]+$'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1),
             'scriptName': self._serialize.url("script_name", script_name, 'str', max_length=90, min_length=1),
         }
         url = self._client.format_url(url, **path_format_arguments)
@@ -473,7 +473,7 @@ class DeploymentScriptsOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.DeploymentScriptsError, response)
+            error = self._deserialize.failsafe_deserialize(_models.DeploymentScriptsError, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('ScriptLogsList', pipeline_response)
@@ -518,7 +518,7 @@ class DeploymentScriptsOperations:
         url = self.get_logs_default.metadata['url']  # type: ignore
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
-            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1, pattern=r'^[-\w\._\(\)]+$'),
+            'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1),
             'scriptName': self._serialize.url("script_name", script_name, 'str', max_length=90, min_length=1),
         }
         url = self._client.format_url(url, **path_format_arguments)
@@ -539,7 +539,7 @@ class DeploymentScriptsOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.DeploymentScriptsError, response)
+            error = self._deserialize.failsafe_deserialize(_models.DeploymentScriptsError, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('ScriptLog', pipeline_response)
@@ -582,7 +582,7 @@ class DeploymentScriptsOperations:
                 url = self.list_by_resource_group.metadata['url']  # type: ignore
                 path_format_arguments = {
                     'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
-                    'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1, pattern=r'^[-\w\._\(\)]+$'),
+                    'resourceGroupName': self._serialize.url("resource_group_name", resource_group_name, 'str', max_length=90, min_length=1),
                 }
                 url = self._client.format_url(url, **path_format_arguments)
                 # Construct parameters
@@ -610,7 +610,7 @@ class DeploymentScriptsOperations:
             response = pipeline_response.http_response
 
             if response.status_code not in [200]:
-                error = self._deserialize(_models.DeploymentScriptsError, response)
+                error = self._deserialize.failsafe_deserialize(_models.DeploymentScriptsError, response)
                 map_error(status_code=response.status_code, response=response, error_map=error_map)
                 raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
