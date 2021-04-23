5348bf6a710e7ed36198d8a5d18f1dd9fc4d3dcc
Pedro Lanzagorta <pelanzag@microsoft.com>
2021-04-23 15:37:17 -0700

    I add DataLakeAnalyticsScopeActivity model.

--- sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/models/_models_py3.py
+++ sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/models/_models_py3.py
@@ -3984,7 +3984,7 @@ class ExecutionActivity(Activity):
     """Base class for all execution activities.
 
     You probably want to use the sub-classes and not this class directly. Known
-    sub-classes are: AzureDataExplorerCommandActivity, AzureFunctionActivity, AzureMLBatchExecutionActivity, AzureMLExecutePipelineActivity, AzureMLUpdateResourceActivity, CopyActivity, CustomActivity, DataLakeAnalyticsUSQLActivity, DatabricksNotebookActivity, DatabricksSparkJarActivity, DatabricksSparkPythonActivity, DeleteActivity, ExecuteDataFlowActivity, ExecuteSSISPackageActivity, GetMetadataActivity, HDInsightHiveActivity, HDInsightMapReduceActivity, HDInsightPigActivity, HDInsightSparkActivity, HDInsightStreamingActivity, LookupActivity, SqlServerStoredProcedureActivity, WebActivity.
+    sub-classes are: AzureDataExplorerCommandActivity, AzureFunctionActivity, AzureMLBatchExecutionActivity, AzureMLExecutePipelineActivity, AzureMLUpdateResourceActivity, CopyActivity, CustomActivity, DataLakeAnalyticsUSQLActivity, DataLakeAnalyticsScopeActivity, DatabricksNotebookActivity, DatabricksSparkJarActivity, DatabricksSparkPythonActivity, DeleteActivity, ExecuteDataFlowActivity, ExecuteSSISPackageActivity, GetMetadataActivity, HDInsightHiveActivity, HDInsightMapReduceActivity, HDInsightPigActivity, HDInsightSparkActivity, HDInsightStreamingActivity, LookupActivity, SqlServerStoredProcedureActivity, WebActivity.
 
     All required parameters must be populated in order to send to Azure.
 
@@ -4024,7 +4024,7 @@ class ExecutionActivity(Activity):
     }
 
     _subtype_map = {
-        'type': {'AzureDataExplorerCommand': 'AzureDataExplorerCommandActivity', 'AzureFunctionActivity': 'AzureFunctionActivity', 'AzureMLBatchExecution': 'AzureMLBatchExecutionActivity', 'AzureMLExecutePipeline': 'AzureMLExecutePipelineActivity', 'AzureMLUpdateResource': 'AzureMLUpdateResourceActivity', 'Copy': 'CopyActivity', 'Custom': 'CustomActivity', 'DataLakeAnalyticsU-SQL': 'DataLakeAnalyticsUSQLActivity', 'DatabricksNotebook': 'DatabricksNotebookActivity', 'DatabricksSparkJar': 'DatabricksSparkJarActivity', 'DatabricksSparkPython': 'DatabricksSparkPythonActivity', 'Delete': 'DeleteActivity', 'ExecuteDataFlow': 'ExecuteDataFlowActivity', 'ExecuteSSISPackage': 'ExecuteSSISPackageActivity', 'GetMetadata': 'GetMetadataActivity', 'HDInsightHive': 'HDInsightHiveActivity', 'HDInsightMapReduce': 'HDInsightMapReduceActivity', 'HDInsightPig': 'HDInsightPigActivity', 'HDInsightSpark': 'HDInsightSparkActivity', 'HDInsightStreaming': 'HDInsightStreamingActivity', 'Lookup': 'LookupActivity', 'SqlServerStoredProcedure': 'SqlServerStoredProcedureActivity', 'WebActivity': 'WebActivity'}
+        'type': {'AzureDataExplorerCommand': 'AzureDataExplorerCommandActivity', 'AzureFunctionActivity': 'AzureFunctionActivity', 'AzureMLBatchExecution': 'AzureMLBatchExecutionActivity', 'AzureMLExecutePipeline': 'AzureMLExecutePipelineActivity', 'AzureMLUpdateResource': 'AzureMLUpdateResourceActivity', 'Copy': 'CopyActivity', 'Custom': 'CustomActivity', 'DataLakeAnalyticsU-SQL': 'DataLakeAnalyticsUSQLActivity','DataLakeAnalyticsScopeActivity':'DataLakeAnalyticsScopeActivity', 'DatabricksNotebook': 'DatabricksNotebookActivity', 'DatabricksSparkJar': 'DatabricksSparkJarActivity', 'DatabricksSparkPython': 'DatabricksSparkPythonActivity', 'Delete': 'DeleteActivity', 'ExecuteDataFlow': 'ExecuteDataFlowActivity', 'ExecuteSSISPackage': 'ExecuteSSISPackageActivity', 'GetMetadata': 'GetMetadataActivity', 'HDInsightHive': 'HDInsightHiveActivity', 'HDInsightMapReduce': 'HDInsightMapReduceActivity', 'HDInsightPig': 'HDInsightPigActivity', 'HDInsightSpark': 'HDInsightSparkActivity', 'HDInsightStreaming': 'HDInsightStreamingActivity', 'Lookup': 'LookupActivity', 'SqlServerStoredProcedure': 'SqlServerStoredProcedureActivity', 'WebActivity': 'WebActivity'}
     }
 
     def __init__(
@@ -12409,6 +12409,114 @@ class DataLakeAnalyticsUSQLActivity(ExecutionActivity):
         self.runtime_version = runtime_version
         self.compilation_mode = compilation_mode
 
+
+class DataLakeAnalyticsScopeActivity(ExecutionActivity):
+    """Data Lake Analytics Scope activity.
+
+    All required parameters must be populated in order to send to Azure.
+
+    :param additional_properties: Unmatched properties from the message are deserialized to this
+     collection.
+    :type additional_properties: dict[str, object]
+    :param name: Required. Activity name.
+    :type name: str
+    :param type: Required. Type of activity.Constant filled by server.
+    :type type: str
+    :param description: Activity description.
+    :type description: str
+    :param depends_on: Activity depends on condition.
+    :type depends_on: list[~azure.mgmt.datafactory.models.ActivityDependency]
+    :param user_properties: Activity user properties.
+    :type user_properties: list[~azure.mgmt.datafactory.models.UserProperty]
+    :param linked_service_name: Linked service reference.
+    :type linked_service_name: ~azure.mgmt.datafactory.models.LinkedServiceReference
+    :param policy: Activity policy.
+    :type policy: ~azure.mgmt.datafactory.models.ActivityPolicy
+    :param script_file_name: Required. Case-sensitive Scope script file name.
+     Type: string (or Expression with resultType string).
+    :type script_file_name: object
+    :param script_folder_path: Required. Case-sensitive path to folder that contains the Scope script.
+     Type: string (or Expression with resultType string).
+    :type script_folder_path: object
+    :param script_linked_service: Required. Script linked service reference.
+    :type script_linked_service: ~azure.mgmt.datafactory.models.LinkedServiceReference
+    :param degree_of_parallelism: The maximum number of nodes simultaneously used to run the job.
+     Default value is 1. Type: integer (or Expression with resultType integer), minimum: 1.
+    :type degree_of_parallelism: object
+    :param priority: Determines which jobs out of all that are queued should be selected to run
+     first. The lower the number, the higher the priority. Default value is 1000. Type: integer (or
+     Expression with resultType integer), minimum: 1.
+    :type priority: object
+    :param parameters: Parameters for U-SQL job request.
+    :type parameters: dict[str, object]
+    :param runtime_version: Runtime version of the U-SQL engine to use. Type: string (or Expression
+     with resultType string).
+    :type runtime_version: object
+    :param compilation_mode: Compilation mode of U-SQL. Must be one of these values : Semantic,
+     Full and SingleBox. Type: string (or Expression with resultType string).
+    :type compilation_mode: object
+    """
+
+    _validation = {
+        'name': {'required': True},
+        'type': {'required': True},
+        'script_file_name': {'required': True},
+        'script_folder_path': {'required': True},
+        'script_linked_service': {'required': True},
+    }
+    
+    _attribute_map = {
+        'additional_properties': {'key': '', 'type': '{object}'},
+        'name': {'key': 'name', 'type': 'str'},
+        'type': {'key': 'type', 'type': 'str'},
+        'description': {'key': 'description', 'type': 'str'},
+        'depends_on': {'key': 'dependsOn', 'type': '[ActivityDependency]'},
+        'user_properties': {'key': 'userProperties', 'type': '[UserProperty]'},
+        'linked_service_name': {'key': 'linkedServiceName', 'type': 'LinkedServiceReference'},
+        'policy': {'key': 'policy', 'type': 'ActivityPolicy'},
+        # Type Props
+        'script_file_name': {'key': 'typeProperties.scriptFilePath', 'type': 'object'},
+        'script_folder_path': {'key': 'typeProperties.scriptFolderPath', 'type': 'object'},
+        'script_linked_service': {'key': 'typeProperties.scriptLinkedService', 'type': 'LinkedServiceReference'},
+        'degree_of_parallelism': {'key': 'typeProperties.degreeOfParallelism', 'type': 'object'},
+        # End Type Props
+        'priority': {'key': 'typeProperties.priority', 'type': 'object'},
+        'parameters': {'key': 'typeProperties.parameters', 'type': '{object}'},
+        'runtime_version': {'key': 'typeProperties.runtimeVersion', 'type': 'object'},
+        'compilation_mode': {'key': 'typeProperties.compilationMode', 'type': 'object'},
+    }
+
+    def __init__(
+        self,
+        *,
+        name: str,
+        script_file_name: object,
+        script_folder_path: object,
+        script_linked_service: "LinkedServiceReference",
+        additional_properties: Optional[Dict[str, object]] = None,
+        description: Optional[str] = None,
+        depends_on: Optional[List["ActivityDependency"]] = None,
+        user_properties: Optional[List["UserProperty"]] = None,
+        linked_service_name: Optional["LinkedServiceReference"] = None,
+        policy: Optional["ActivityPolicy"] = None,
+        degree_of_parallelism: Optional[object] = None,
+        priority: Optional[object] = None,
+        parameters: Optional[Dict[str, object]] = None,
+        runtime_version: Optional[object] = None,
+        compilation_mode: Optional[object] = None,
+        **kwargs
+    ):
+        super(DataLakeAnalyticsScopeActivity, self).__init__(additional_properties=additional_properties, name=name, description=description,
+                                                            depends_on=depends_on, user_properties=user_properties, linked_service_name=linked_service_name, policy=policy, **kwargs)
+        self.type = 'DataLakeAnalyticsScope'  # type: str
+        self.script_file_name = script_file_name
+        self.script_folder_path = script_folder_path
+        self.script_linked_service = script_linked_service
+        self.degree_of_parallelism = degree_of_parallelism
+        self.priority = priority
+        self.parameters = parameters
+        self.runtime_version = runtime_version
+        self.compilation_mode = compilation_mode
 
 class DatasetCompression(msrest.serialization.Model):
     """The compression method used on a dataset.
