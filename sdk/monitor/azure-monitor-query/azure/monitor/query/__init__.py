f5b92753e43d23b4251a65a03a1de1bf0db717e9
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2021-04-27 14:20:04 -0700

    namespace

---
+++ sdk/monitor/azure-monitor-query/azure/monitor/query/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------
+
+from ._generated import MonitorQueryClient
+from ._version import VERSION
+
+__all__ = [
+    "MonitorQueryClient",
+]
+
+__version__ == VERSION
