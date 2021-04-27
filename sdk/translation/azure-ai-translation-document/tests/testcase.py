723b2c2a46c591b156d8aa9925c432d2718513e8
MIDDLEEAST\v-moshaban <v-moshaban@microsoft.com>
2021-04-27 02:10:38 +0200

    updated 'cancel job' tests

--- sdk/translation/azure-ai-translation-document/tests/testcase.py
+++ sdk/translation/azure-ai-translation-document/tests/testcase.py
@@ -225,3 +225,44 @@ class DocumentTranslationTest(AzureTestCase):
             result_job_ids.append(job_details.id)
 
         return result_job_ids
+
+    
+    def _create_translation_job_with_dummy_docs(self, client, docs_count, **kwargs):
+        '''
+            appropriated this method from another PR! #18302
+            please resolve conflict before merge
+            keep in mind it's the exact same method
+        '''
+        # get input parms
+        wait_for_job = kwargs.pop('wait', False)
+        language_code = kwargs.pop('language_code', "es")
+
+        # prepare containers and test data
+        blob_text = b'blob text'
+        blob_data = [Document(data=blob_text)] * docs_count
+        source_container_sas_url = self.create_source_container(data=blob_data)
+        target_container_sas_url = self.create_target_container()
+
+        # prepare translation inputs
+        translation_inputs = [
+            DocumentTranslationInput(
+                source_url=source_container_sas_url,
+                targets=[
+                    TranslationTarget(
+                        target_url=target_container_sas_url,
+                        language_code=language_code
+                    )
+                ]
+            )
+        ]
+
+        # submit job
+        job_details = client.create_translation_job(translation_inputs)
+        self.assertIsNotNone(job_details.id)
+        # wait for result
+        if wait_for_job:
+                client.wait_until_done(job_details.id)
+        # validate
+        self._validate_translation_job(job_details=job_details)
+
+        return job_details.id
