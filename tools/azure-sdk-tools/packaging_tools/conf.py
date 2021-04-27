9ecd35f6f100534bccce260d59e2311441d79eff
msyyc <70930885+msyyc@users.noreply.github.com>
2021-04-27 10:10:30 +0800

    Update conf.py

--- tools/azure-sdk-tools/packaging_tools/conf.py
+++ tools/azure-sdk-tools/packaging_tools/conf.py
@@ -17,7 +17,7 @@ _CONFIG = {
     "package_doc_id": "",
     "is_stable": False,
     "is_arm": True,
-    "need_msrestazure": True
+    "need_azuremgmtcore": True
 }
 
 def read_conf(folder: Path) -> Dict[str, Any]:
