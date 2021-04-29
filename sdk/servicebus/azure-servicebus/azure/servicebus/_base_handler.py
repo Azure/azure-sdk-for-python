8ff2d1e55d0f2275d4258161698b520213376e15
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-04-29 10:44:11 -0700

    rename to ServiceBusAzureSasCredential

--- sdk/servicebus/azure-servicebus/azure/servicebus/_base_handler.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_base_handler.py
@@ -197,7 +197,7 @@ class ServiceBusSharedKeyCredential(object):
         return _generate_sas_token(scopes[0], self.policy, self.key)
 
 
-class ServiceBusAzureSasTokenCredential(object):
+class ServiceBusAzureSasCredential(object):
     """The shared access token credential used for authentication
     when AzureSasCredential is provided.
     :param azure_sas_credential: The credential to be used for authentication.
@@ -232,7 +232,7 @@ class BaseHandler:  # pylint:disable=too-many-instance-attributes
         )
         self._mgmt_target = "{}{}".format(self._entity_path, MANAGEMENT_PATH_SUFFIX)
         if isinstance(credential, AzureSasCredential):
-            self._credential = ServiceBusAzureSasTokenCredential(credential)
+            self._credential = ServiceBusAzureSasCredential(credential)
         else:
             self._credential = credential # type: ignore
         self._container_id = CONTAINER_PREFIX + str(uuid.uuid4())[:8]
