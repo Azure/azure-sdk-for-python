1ef3345f85722ab1962e26fcc61eb93a22f4b318
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2021-04-29 01:16:05 -0700

    Post Process Event Names Scrip

---
+++ sdk/eventgrid/azure-eventgrid/swagger/postprocess_eventnames.py
@@ -0,0 +1,23 @@
+import inspect
+import re
+from azure.eventgrid._generated import models
+
+def event_tuples(system_events):
+    tup_list = []
+    for event in system_events:
+        class_name = event[0].replace("EventData", "EventName")
+        try:
+            event_name = re.findall("Microsoft.[a-zA-Z]+.[a-zA-Z]+", event[1].__doc__)[0]
+        except:
+            print(event[0])
+        tup_list.append((class_name, event_name))
+    return tup_list
+
+def generate_enum_content(tuples):
+    for tup in tup_list:
+        print(tup[0] + " = '" + tup[1] + "'\n")
+
+system_events = [m for m in inspect.getmembers(models) if m[0].endswith('EventData')]
+tup_list = event_tuples(system_events)
+
+generate_enum_content(tup_list)
