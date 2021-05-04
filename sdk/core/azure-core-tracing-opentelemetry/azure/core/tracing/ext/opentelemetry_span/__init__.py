0f60b8d5cde1b7279d2edbe25c900b0b34aed3e2
Rakshith Bhyravabhotla <sabhyrav@microsoft.com>
2021-05-04 10:34:33 -0700

    Update sdk/core/azure-core-tracing-opentelemetry/azure/core/tracing/ext/opentelemetry_span/__init__.py

--- sdk/core/azure-core-tracing-opentelemetry/azure/core/tracing/ext/opentelemetry_span/__init__.py
+++ sdk/core/azure-core-tracing-opentelemetry/azure/core/tracing/ext/opentelemetry_span/__init__.py
@@ -299,24 +299,3 @@ class OpenTelemetrySpan(HttpSpanMixin, object):
                 detach(token)
 
         return call_with_current_context
-
-    def set_http_attributes(self, request, response=None):
-        # type: (HttpRequest, Optional[HttpResponseType]) -> None
-        """
-        Add correct attributes for a http client span.
-
-        :param request: The request made
-        :type request: HttpRequest
-        :param response: The response received by the server. Is None if no response received.
-        :type response: ~azure.core.pipeline.transport.HttpResponse or ~azure.core.pipeline.transport.AsyncHttpResponse
-        """
-        self.add_attribute(self._SPAN_COMPONENT, "http")
-        self.add_attribute(self._HTTP_METHOD, request.method)
-        self.add_attribute(self._HTTP_URL, request.url)
-        user_agent = request.headers.get("User-Agent")
-        if user_agent:
-            self.add_attribute(self._HTTP_USER_AGENT, user_agent)
-        if response and response.status_code:
-            self.add_attribute(self._HTTP_STATUS_CODE, response.status_code)
-        else:
-            self.add_attribute(self._HTTP_STATUS_CODE, 504)
