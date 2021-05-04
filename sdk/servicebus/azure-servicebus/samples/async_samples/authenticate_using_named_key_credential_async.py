e315bbc0e20f204996875ef0d51c6233fa56ab0a
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2021-05-04 11:31:43 -0700

    oops

--- sdk/servicebus/azure-servicebus/samples/async_samples/authenticate_using_named_key_credential_async.py
+++ sdk/servicebus/azure-servicebus/samples/async_samples/authenticate_using_named_key_credential_async.py
@@ -29,5 +29,4 @@ async def send_message():
             await sender.send_messages([ServiceBusMessage("hello")])
 
 loop = asyncio.get_event_loop()
-start_time = time.time()
 loop.run_until_complete(send_message())
