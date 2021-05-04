0a3feb2840f1ccee4b2204a370f170721fce1301
antisch <antisch@microsoft.com>
2021-05-04 09:22:12 -0700

    Fix pylint

--- sdk/tables/azure-data-tables/azure/data/tables/_entity.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_entity.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from typing import Dict, Any
 from enum import Enum
 from typing import Any, Dict, Union, NamedTuple
 
