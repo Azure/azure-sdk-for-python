30adb56dfa31214b297c554e5767941cc8b5033a
SDKAuto <sdkautomation@microsoft.com>
2021-05-07 02:37:01 +0000

    CodeGen from PR 14266 in Azure/azure-rest-api-specs only track2 (#14266)
    Co-authored-by: Yan Zhang (WICRESOFT NORTH AMERICA LTD) <v-yanzhang@microsoft.com>

--- sdk/managementpartner/azure-mgmt-managementpartner/azure/mgmt/managementpartner/aio/operations/_partner_operations.py
+++ sdk/managementpartner/azure-mgmt-managementpartner/azure/mgmt/managementpartner/aio/operations/_partner_operations.py
@@ -85,7 +85,7 @@ class PartnerOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.Error, response)
+            error = self._deserialize.failsafe_deserialize(_models.Error, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('PartnerResponse', pipeline_response)
@@ -141,7 +141,7 @@ class PartnerOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.Error, response)
+            error = self._deserialize.failsafe_deserialize(_models.Error, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('PartnerResponse', pipeline_response)
@@ -197,7 +197,7 @@ class PartnerOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.Error, response)
+            error = self._deserialize.failsafe_deserialize(_models.Error, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         deserialized = self._deserialize('PartnerResponse', pipeline_response)
@@ -253,7 +253,7 @@ class PartnerOperations:
 
         if response.status_code not in [200]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
-            error = self._deserialize(_models.Error, response)
+            error = self._deserialize.failsafe_deserialize(_models.Error, response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
         if cls:
