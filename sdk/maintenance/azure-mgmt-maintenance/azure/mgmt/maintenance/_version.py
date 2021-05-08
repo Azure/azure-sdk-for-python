ce3602bb1dc17852fd57455b5112d38f77c4f05d
PythonSdkPipelines <PythonSdkPipelines>
2021-05-08 01:22:30 +0000

    version,CHANGELOG

--- sdk/maintenance/azure-mgmt-maintenance/azure/mgmt/maintenance/_version.py
+++ sdk/maintenance/azure-mgmt-maintenance/azure/mgmt/maintenance/_version.py
@@ -6,4 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "1.1.0"
+VERSION = "2.0.0"
