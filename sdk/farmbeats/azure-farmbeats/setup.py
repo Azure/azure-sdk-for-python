e41f71433f583f43fddd88ac21a1a1898935e8a3
Agnivesh Adhikari <agnivesh.adhikari@gmail.com>
2021-05-10 17:00:25 +0530

    Added final version info to setup

--- sdk/farmbeats/azure-farmbeats/setup.py
+++ sdk/farmbeats/azure-farmbeats/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="azure-farmbeats",
-    version="0.1.0",
+    version="1.0.0b1",
     description="Azure Farmbeats Client Library v0",
     packages=find_packages(),
     author="Microsoft Corporation",
