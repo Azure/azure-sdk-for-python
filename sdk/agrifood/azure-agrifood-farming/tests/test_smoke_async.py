eb9027c2ca74f28b735cc5a8becbccdee922162e
iscai-msft <iscai@microsoft.com>
2021-05-13 15:15:10 -0400

    add tests

--- sdk/agrifood/azure-agrifood-farming/tests/test_smoke_async.py
+++ sdk/agrifood/azure-agrifood-farming/tests/test_smoke_async.py
@@ -6,15 +6,16 @@
 import datetime
 from azure.core.exceptions import HttpResponseError
 from azure.agrifood.farming.models import Farmer, SatelliteDataIngestionJob, SatelliteData
-from testcase import FarmBeatsPowerShellPreparer, FarmBeatsTest
+from testcase_async import FarmBeatsTestAsync
+from testcase import FarmBeatsPowerShellPreparer
 
 
-class FarmBeatsSmokeTestCase(FarmBeatsTest):
+class FarmBeatsSmokeTestCaseAsync(FarmBeatsTestAsync):
 
     @FarmBeatsPowerShellPreparer()
-    def test_farmer(self, farmbeats_endpoint, farmbeats_farmer_id):
+    async def test_farmer(self, farmbeats_endpoint, farmbeats_farmer_id):
         client = self.create_client(farmbeats_endpoint=farmbeats_endpoint)
-        farmer = client.farmers.create_or_update(
+        farmer = await client.farmers.create_or_update(
             farmer_id=farmbeats_farmer_id,
             body=Farmer()
         )
@@ -24,25 +25,25 @@ class FarmBeatsSmokeTestCase(FarmBeatsTest):
         assert farmer.created_date_time
         assert farmer.modified_date_time
 
-        retrieved_farmer = client.farmers.get(farmer_id=farmbeats_farmer_id)
+        retrieved_farmer = await client.farmers.get(farmer_id=farmbeats_farmer_id)
         assert farmer.id == retrieved_farmer.id
         assert farmer.e_tag == retrieved_farmer.e_tag
         assert farmer.created_date_time == retrieved_farmer.created_date_time
         assert farmer.modified_date_time == retrieved_farmer.modified_date_time
 
     @FarmBeatsPowerShellPreparer()
-    def test_boundary(self, farmbeats_endpoint, farmbeats_farmer_id, farmbeats_boundary_id):
+    async def test_boundary(self, farmbeats_endpoint, farmbeats_farmer_id, farmbeats_boundary_id):
         client = self.create_client(farmbeats_endpoint=farmbeats_endpoint)
         boundary_id = farmbeats_boundary_id + "boundary"
         boundary = self.create_boundary_if_not_exist(client, farmbeats_farmer_id, boundary_id)
-        assert boundary == client.boundaries.get(
+        assert boundary == await client.boundaries.get(
             farmer_id=farmbeats_farmer_id,
             boundary_id=boundary_id
         )
         self.delete_boundary(client, farmbeats_farmer_id, boundary_id)
 
     @FarmBeatsPowerShellPreparer()
-    def test_satellite_job(self, farmbeats_endpoint, farmbeats_farmer_id, farmbeats_boundary_id, farmbeats_job_id_prefix):
+    async def test_satellite_job(self, farmbeats_endpoint, farmbeats_farmer_id, farmbeats_boundary_id, farmbeats_job_id_prefix):
         client = self.create_client(farmbeats_endpoint=farmbeats_endpoint)
         boundary_id = "{}-scenes".format(farmbeats_boundary_id)
         try:
@@ -55,7 +56,7 @@ class FarmBeatsSmokeTestCase(FarmBeatsTest):
         start_date_time = datetime.datetime(2020, 1, 1)
         end_date_time = datetime.datetime(2020, 12, 31)
 
-        satellite_job_poller = client.scenes.begin_create_satellite_data_ingestion_job(
+        satellite_job_poller = await client.scenes.begin_create_satellite_data_ingestion_job(
             job_id=job_id,
             body=SatelliteDataIngestionJob(
                 farmer_id=farmbeats_farmer_id,
@@ -69,7 +70,7 @@ class FarmBeatsSmokeTestCase(FarmBeatsTest):
                 )
             ),
         )
-        satellite = satellite_job_poller.result()
+        satellite = await satellite_job_poller.result()
         assert satellite.farmer_id == farmbeats_farmer_id
         assert satellite.id == job_id
         assert satellite.boundary_id == boundary_id
@@ -77,7 +78,7 @@ class FarmBeatsSmokeTestCase(FarmBeatsTest):
         assert satellite.end_date_time == end_date_time
 
     @FarmBeatsPowerShellPreparer()
-    def test_scenes(self, farmbeats_endpoint, farmbeats_farmer_id, farmbeats_boundary_id):
+    async def test_scenes(self, farmbeats_endpoint, farmbeats_farmer_id, farmbeats_boundary_id):
         client = self.create_client(farmbeats_endpoint=farmbeats_endpoint)
         boundary_id = "{}-scenes".format(farmbeats_boundary_id)
         start_date_time = datetime.datetime(2020, 1, 1)
@@ -88,14 +89,14 @@ class FarmBeatsSmokeTestCase(FarmBeatsTest):
             start_date_time=start_date_time,
             end_date_time=end_date_time,
         )
-        for scene in scenes:
+        async for scene in scenes:
             assert scene.farmer_id == farmbeats_farmer_id
             assert scene.boundary_id == boundary_id
 
     @FarmBeatsPowerShellPreparer()
-    def test_download_image(self, farmbeats_endpoint, farmbeats_farmer_id, farmbeats_boundary_id):
+    async def test_download_image(self, farmbeats_endpoint, farmbeats_farmer_id, farmbeats_boundary_id):
         client = self.create_client(farmbeats_endpoint=farmbeats_endpoint)
         boundary_id = "{}-scenes".format(farmbeats_boundary_id)
-        scenes = [f for f in client.scenes.list(farmer_id=farmbeats_farmer_id, boundary_id=boundary_id)]
+        scenes = [f async for f in client.scenes.list(farmer_id=farmbeats_farmer_id, boundary_id=boundary_id)]
 
         assert scenes
