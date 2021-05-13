62b94e82ae0ec9fd30005b770b7a4cb96a366e33
hivyas <hivyas@microsoft.com>
2021-05-12 19:11:01 -0700

    reverting old sdk folder'

--- sdk/media/azure-media-analytics-edge/azure/media/analyticsedge/_generated/models/_direct_methodsfor_live_video_analyticson_io_tedge_enums.py
+++ sdk/media/azure-media-analytics-edge/azure/media/analyticsedge/_generated/models/_direct_methodsfor_live_video_analyticson_io_tedge_enums.py
@@ -30,110 +30,77 @@ class MediaGraphGrpcExtensionDataTransferMode(with_metaclass(_CaseInsensitiveEnu
     """How frame data should be transmitted to the inference engine.
     """
 
-    #: Frames are transferred embedded into the gRPC messages.
-    EMBEDDED = "Embedded"
-    #: Frames are transferred through shared memory.
-    SHARED_MEMORY = "SharedMemory"
+    EMBEDDED = "Embedded"  #: Frames are transferred embedded into the gRPC messages.
+    SHARED_MEMORY = "SharedMemory"  #: Frames are transferred through shared memory.
 
 class MediaGraphImageFormatRawPixelFormat(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """The pixel format that will be used to encode images.
     """
 
-    #: Planar YUV 4:2:0, 12bpp, (1 Cr and Cb sample per 2x2 Y samples).
-    YUV420_P = "Yuv420p"
-    #: Packed RGB 5:6:5, 16bpp, (msb)   5R 6G 5B(lsb), big-endian.
-    RGB565_BE = "Rgb565be"
-    #: Packed RGB 5:6:5, 16bpp, (msb)   5R 6G 5B(lsb), little-endian.
-    RGB565_LE = "Rgb565le"
-    #: Packed RGB 5:5:5, 16bpp, (msb)1X 5R 5G 5B(lsb), big-endian , X=unused/undefined.
-    RGB555_BE = "Rgb555be"
-    #: Packed RGB 5:5:5, 16bpp, (msb)1X 5R 5G 5B(lsb), little-endian, X=unused/undefined.
-    RGB555_LE = "Rgb555le"
-    #: Packed RGB 8:8:8, 24bpp, RGBRGB.
-    RGB24 = "Rgb24"
-    #: Packed RGB 8:8:8, 24bpp, BGRBGR.
-    BGR24 = "Bgr24"
-    #: Packed ARGB 8:8:8:8, 32bpp, ARGBARGB.
-    ARGB = "Argb"
-    #: Packed RGBA 8:8:8:8, 32bpp, RGBARGBA.
-    RGBA = "Rgba"
-    #: Packed ABGR 8:8:8:8, 32bpp, ABGRABGR.
-    ABGR = "Abgr"
-    #: Packed BGRA 8:8:8:8, 32bpp, BGRABGRA.
-    BGRA = "Bgra"
+    YUV420_P = "Yuv420p"  #: Planar YUV 4:2:0, 12bpp, (1 Cr and Cb sample per 2x2 Y samples).
+    RGB565_BE = "Rgb565be"  #: Packed RGB 5:6:5, 16bpp, (msb)   5R 6G 5B(lsb), big-endian.
+    RGB565_LE = "Rgb565le"  #: Packed RGB 5:6:5, 16bpp, (msb)   5R 6G 5B(lsb), little-endian.
+    RGB555_BE = "Rgb555be"  #: Packed RGB 5:5:5, 16bpp, (msb)1X 5R 5G 5B(lsb), big-endian , X=unused/undefined.
+    RGB555_LE = "Rgb555le"  #: Packed RGB 5:5:5, 16bpp, (msb)1X 5R 5G 5B(lsb), little-endian, X=unused/undefined.
+    RGB24 = "Rgb24"  #: Packed RGB 8:8:8, 24bpp, RGBRGB.
+    BGR24 = "Bgr24"  #: Packed RGB 8:8:8, 24bpp, BGRBGR.
+    ARGB = "Argb"  #: Packed ARGB 8:8:8:8, 32bpp, ARGBARGB.
+    RGBA = "Rgba"  #: Packed RGBA 8:8:8:8, 32bpp, RGBARGBA.
+    ABGR = "Abgr"  #: Packed ABGR 8:8:8:8, 32bpp, ABGRABGR.
+    BGRA = "Bgra"  #: Packed BGRA 8:8:8:8, 32bpp, BGRABGRA.
 
 class MediaGraphImageScaleMode(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """Describes the modes for scaling an input video frame into an image, before it is sent to an
     inference engine.
     """
 
-    #: Use the same aspect ratio as the input frame.
-    PRESERVE_ASPECT_RATIO = "PreserveAspectRatio"
-    #: Center pad the input frame to match the given dimensions.
-    PAD = "Pad"
-    #: Stretch input frame to match given dimensions.
-    STRETCH = "Stretch"
+    PRESERVE_ASPECT_RATIO = "PreserveAspectRatio"  #: Use the same aspect ratio as the input frame.
+    PAD = "Pad"  #: Center pad the input frame to match the given dimensions.
+    STRETCH = "Stretch"  #: Stretch input frame to match given dimensions.
 
 class MediaGraphInstanceState(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """Allowed states for a graph instance.
     """
 
-    #: The media graph instance is idle and not processing media.
-    INACTIVE = "Inactive"
-    #: The media graph instance is transitioning into the active state.
-    ACTIVATING = "Activating"
-    #: The media graph instance is active and processing media.
-    ACTIVE = "Active"
-    #: The media graph instance is transitioning into the inactive state.
-    DEACTIVATING = "Deactivating"
+    INACTIVE = "Inactive"  #: The media graph instance is idle and not processing media.
+    ACTIVATING = "Activating"  #: The media graph instance is transitioning into the active state.
+    ACTIVE = "Active"  #: The media graph instance is active and processing media.
+    DEACTIVATING = "Deactivating"  #: The media graph instance is transitioning into the inactive state.
 
 class MediaGraphMotionDetectionSensitivity(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """Enumeration that specifies the sensitivity of the motion detection processor.
     """
 
-    #: Low Sensitivity.
-    LOW = "Low"
-    #: Medium Sensitivity.
-    MEDIUM = "Medium"
-    #: High Sensitivity.
-    HIGH = "High"
+    LOW = "Low"  #: Low Sensitivity.
+    MEDIUM = "Medium"  #: Medium Sensitivity.
+    HIGH = "High"  #: High Sensitivity.
 
 class MediaGraphOutputSelectorOperator(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """The operator to compare streams by.
     """
 
-    #: A media type is the same type or a subtype.
-    IS_ENUM = "is"
-    #: A media type is not the same type or a subtype.
-    IS_NOT = "isNot"
+    IS_ENUM = "is"  #: A media type is the same type or a subtype.
+    IS_NOT = "isNot"  #: A media type is not the same type or a subtype.
 
 class MediaGraphOutputSelectorProperty(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """The stream property to compare with.
     """
 
-    #: The stream's MIME type or subtype.
-    MEDIA_TYPE = "mediaType"
+    MEDIA_TYPE = "mediaType"  #: The stream's MIME type or subtype.
 
 class MediaGraphParameterType(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """The type of the parameter.
     """
 
-    #: A string parameter value.
-    STRING = "String"
-    #: A string to hold sensitive information as parameter value.
-    SECRET_STRING = "SecretString"
-    #: A 32-bit signed integer as parameter value.
-    INT = "Int"
-    #: A 64-bit double-precision floating point type as parameter value.
-    DOUBLE = "Double"
-    #: A boolean value that is either true or false.
-    BOOL = "Bool"
+    STRING = "String"  #: A string parameter value.
+    SECRET_STRING = "SecretString"  #: A string to hold sensitive information as parameter value.
+    INT = "Int"  #: A 32-bit signed integer as parameter value.
+    DOUBLE = "Double"  #: A 64-bit double-precision floating point type as parameter value.
+    BOOL = "Bool"  #: A boolean value that is either true or false.
 
 class MediaGraphRtspTransport(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """Underlying RTSP transport. This is used to enable or disable HTTP tunneling.
     """
 
-    #: HTTP/HTTPS transport. This should be used when HTTP tunneling is desired.
-    HTTP = "Http"
-    #: TCP transport. This should be used when HTTP tunneling is NOT desired.
-    TCP = "Tcp"
+    HTTP = "Http"  #: HTTP/HTTPS transport. This should be used when HTTP tunneling is desired.
+    TCP = "Tcp"  #: TCP transport. This should be used when HTTP tunneling is NOT desired.
