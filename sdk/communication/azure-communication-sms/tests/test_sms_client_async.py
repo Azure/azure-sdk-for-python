c41191c116369a99e26d959d11279a7e715c717b
Lakshman Sundaralingam <lakshmans@microsoft.com>
2021-05-13 17:04:30 -0700

    Added unit tests to check if idempotence parameters are being set

--- sdk/communication/azure-communication-sms/tests/test_sms_client_async.py
+++ sdk/communication/azure-communication-sms/tests/test_sms_client_async.py
@@ -61,3 +61,26 @@ class TestSMSClientAsync(aiounittest.AsyncTestCase):
         self.assertEqual(202, sms_response.http_status_code)
         self.assertIsNotNone(sms_response.error_message)
         self.assertTrue(sms_response.successful)
+    
+    @patch(
+        "azure.communication.sms._generated.aio.operations._sms_operations.SmsOperations.send"
+    )
+    async def test_send_message_parameters(self, mock_send):
+        phone_number = "+14255550123"
+        msg = "Hello World via SMS"
+        tag = "custom-tag"
+
+        sms_client = SmsClient("https://endpoint", FakeTokenCredential())
+        await sms_client.send(
+            from_=phone_number,
+            to=[phone_number],
+            message=msg,
+            enable_delivery_report=True,
+            tag=tag)
+
+        self.assertEqual(phone_number, mock_send.call_args[0][0].from_property)
+        self.assertEqual(phone_number, mock_send.call_args[0][0].sms_recipients[0].to)
+        self.assertIsNotNone(mock_send.call_args[0][0].sms_recipients[0].repeatability_request_id)
+        self.assertIsNotNone(mock_send.call_args[0][0].sms_recipients[0].repeatability_first_sent)
+        self.assertTrue(mock_send.call_args[0][0].sms_send_options.enable_delivery_report)
+        self.assertEqual(tag, mock_send.call_args[0][0].sms_send_options.tag)
