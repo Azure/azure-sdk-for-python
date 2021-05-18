65a937d4c1a80c44f7c17ac4fba84fb5a3c344c7
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2021-05-18 10:45:13 -0700

    Add Testing of Conda Artifacts (#18478)
    * take advantage of additional test-matrix, add ci.conda.tests to jobs/ci.yml which further extends test matrix for conda artifacts
        * test our conda packages on windows/mac/linux py 3.6, 3.8, 3.9
    * add common conda requirements file

--- scripts/devops_tasks/build_conda_artifacts.py
+++ scripts/devops_tasks/build_conda_artifacts.py
@@ -33,7 +33,7 @@ from distutils.dir_util import copy_tree
 
 VERSION_REGEX = re.compile(r"\s*AZURESDK_CONDA_VERSION\s*:\s*[\'](.*)[\']\s*")
 
-SUMMARY_TEMPLATE = "- Generated from {}."
+SUMMARY_TEMPLATE = " - Generated from {}."
 
 NAMESPACE_EXTENSION_TEMPLATE = """__path__ = __import__('pkgutil').extend_path(__path__, __name__)  # type: str
 """
