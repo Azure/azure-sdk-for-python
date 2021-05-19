e4bb0856fc07f8c979499cae39b68a0ea3b306c5
seankane-msft <seankane@microsoft.com>
2021-05-19 15:46:21 -0400

    turning non writeables into properties

--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/_models.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/_models.py
@@ -16,25 +16,13 @@ from ._generated.models import (
 )
 
 if TYPE_CHECKING:
+    from datetime import datetime
     from ._generated.models import ManifestAttributesBase
-    from ._generated.models import ArtifactTagProperties as GeneratedArtifactTagProperties
 
 
 class ArtifactManifestProperties(object):
     """Represents properties of a registry artifact
 
-    :ivar architecture: CPU Architecture of an artifact
-    :vartype architecture: ~azure.containerregistry.ArtifactArchitecture
-    :ivar created_on: Time and date an artifact was created
-    :vartype created_on: ~datetime.datetime
-    :ivar str digest: Digest for the artifact
-    :ivar last_updated_on: Time and date an artifact was last updated
-    :vartype last_updated_on: datetime.datetime
-    :ivar operating_system: Operating system for the artifact
-    :vartype operating_system: ~azure.containerregistry.ArtifactOperatingSystem
-    :ivar str repository_name: Repository name the artifact belongs to
-    :ivar str size: Size of the artifact
-    :ivar List[str] tags: Tags associated with a registry artifact
     :ivar bool can_delete: Delete Permissions for an artifact
     :ivar bool can_write: Delete Permissions for an artifact
     :ivar bool can_read: Delete Permissions for an artifact
@@ -42,18 +30,18 @@ class ArtifactManifestProperties(object):
     """
 
     def __init__(self, **kwargs):
-        self.architecture = kwargs.get("cpu_architecture", None)
-        if self.architecture is not None:
-            self.architecture = ArtifactArchitecture(self.architecture)
-        self.created_on = kwargs.get("created_on", None)
-        self.digest = kwargs.get("digest", None)
-        self.last_updated_on = kwargs.get("last_updated_on", None)
-        self.operating_system = kwargs.get("operating_system", None)
-        if self.operating_system is not None:
-            self.operating_system = ArtifactOperatingSystem(self.operating_system)
-        self.repository_name = kwargs.get("repository_name", None)
-        self.size = kwargs.get("size", None)
-        self.tags = kwargs.get("tags", None)
+        self._architecture = kwargs.get("cpu_architecture", None)
+        if self._architecture is not None:
+            self._architecture = ArtifactArchitecture(self._architecture)
+        self._created_on = kwargs.get("created_on", None)
+        self._digest = kwargs.get("digest", None)
+        self._last_updated_on = kwargs.get("last_updated_on", None)
+        self._operating_system = kwargs.get("operating_system", None)
+        if self._operating_system is not None:
+            self._operating_system = ArtifactOperatingSystem(self._operating_system)
+        self._repository_name = kwargs.get("repository_name", None)
+        self._size = kwargs.get("size", None)
+        self._tags = kwargs.get("tags", None)
         self.can_delete = kwargs.get("can_delete")
         self.can_read = kwargs.get("can_read")
         self.can_list = kwargs.get("can_list")
@@ -86,17 +74,50 @@ class ArtifactManifestProperties(object):
             can_list=self.can_list,
         )
 
+    @property
+    def architecture(self):
+        # type: () -> ArtifactArchitecture
+        return self._architecture
+
+    @property
+    def created_on(self):
+        # type: () -> datetime
+        return self._created_on
+
+    @property
+    def digest(self):
+        # type: () -> str
+        return self._digest
+
+    @property
+    def last_updated_on(self):
+        # type: () -> datetime
+        return self._last_updated_on
+
+    @property
+    def operating_system(self):
+        # type: () -> ArtifactOperatingSystem
+        return self._operating_system
+
+    @property
+    def repository_name(self):
+        # type: () -> str
+        return self._repository_name
+
+    @property
+    def size(self):
+        # type: () -> int
+        return self._size
+
+    @property
+    def tags(self):
+        # type: () -> List[str]
+        return self._tags
+
 
 class RepositoryProperties(object):
     """Model for storing properties of a single repository
 
-    :ivar created_on: Time the repository was created
-    :vartype created_on: datetime.datetime
-    :ivar last_updated_on: Time the repository was last updated
-    :vartype last_updated_on: datetime.datetime
-    :ivar int manifest_count: Number of manifest in the repository
-    :ivar str name: Name of the repository
-    :ivar int tag_count: Number of tags associated with the repository
     :ivar bool can_delete: Delete Permissions for an artifact
     :ivar bool can_write: Delete Permissions for an artifact
     :ivar bool can_read: Delete Permissions for an artifact
@@ -105,11 +126,11 @@ class RepositoryProperties(object):
     """
 
     def __init__(self, **kwargs):
-        self.created_on = kwargs.get("created_on", None)
-        self.last_updated_on = kwargs.get("last_updated_on", None)
-        self.manifest_count = kwargs.get("manifest_count", None)
-        self.name = kwargs.get("name", None)
-        self.tag_count = kwargs.get("tag_count", None)
+        self._created_on = kwargs.get("created_on", None)
+        self._last_updated_on = kwargs.get("last_updated_on", None)
+        self._manifest_count = kwargs.get("manifest_count", None)
+        self._name = kwargs.get("name", None)
+        self._tag_count = kwargs.get("tag_count", None)
         self.can_delete = kwargs.get("can_delete")
         self.can_read = kwargs.get("can_read")
         self.can_list = kwargs.get("can_list")
@@ -141,6 +162,30 @@ class RepositoryProperties(object):
             can_list=self.can_list,
         )
 
+    @property
+    def created_on(self):
+        # type: () -> datetime
+        return self._created_on
+
+    @property
+    def last_udpated_on(self):
+        # type: () -> datetime
+        return self._last_updated_on
+
+    @property
+    def manifest_count(self):
+        # type: () -> int
+        return self._manifest_count
+
+    @property
+    def name(self):
+        # type: () -> str
+        return self._name
+
+    @property
+    def tag_count(self):
+        # type: () -> int
+        return self._tag_count
 
 class ManifestOrder(str, Enum):
     """Enum for ordering registry artifacts"""
@@ -159,13 +204,6 @@ class TagOrder(str, Enum):
 class ArtifactTagProperties(object):
     """Model for storing properties of a single tag
 
-    :ivar created_on: Time the tag was created
-    :vartype created_on: datetime.datetime
-    :ivar str digest: Digest for the tag
-    :ivar last_updated_on: Time the tag was last updated
-    :vartype last_updated_on: datetime.datetime
-    :ivar str name: Name of the image the tag corresponds to
-    :ivar str repository: Repository the tag belongs to
     :ivar bool can_delete: Delete Permissions for an artifact
     :ivar bool can_write: Delete Permissions for an artifact
     :ivar bool can_read: Delete Permissions for an artifact
@@ -173,11 +211,11 @@ class ArtifactTagProperties(object):
     """
 
     def __init__(self, **kwargs):
-        self.created_on = kwargs.get("created_on", None)
-        self.digest = kwargs.get("digest", None)
-        self.last_updated_on = kwargs.get("last_updated_on", None)
-        self.name = kwargs.get("name", None)
-        self.repository_name = kwargs.get("repository_name", None)
+        self._created_on = kwargs.get("created_on", None)
+        self._digest = kwargs.get("digest", None)
+        self._last_updated_on = kwargs.get("last_updated_on", None)
+        self._name = kwargs.get("name", None)
+        self._repository_name = kwargs.get("repository_name", None)
         self.can_delete = kwargs.get("can_delete")
         self.can_read = kwargs.get("can_read")
         self.can_list = kwargs.get("can_list")
@@ -206,6 +244,31 @@ class ArtifactTagProperties(object):
             can_write=self.can_write,
             can_list=self.can_list,
         )
+
+    @property
+    def created_on(self):
+        # type: () -> datetime
+        return self._created_on
+
+    @property
+    def digest(self):
+        # type: () -> str
+        return self._digest
+
+    @property
+    def last_updated_on(self):
+        # type: () -> datetime
+        return self.last_updated_on
+
+    @property
+    def name(self):
+        # type: () -> str
+        return self._name
+
+    @property
+    def repository_name(self):
+        # type: () -> str
+        return self._repository_name
 
 
 class ArtifactArchitecture(str, Enum):
