49fe15db13d23df29892da5ed6702a7dd308a671
Richard Kogelnig <richard.kogelnig@microsoft.com>
2021-05-21 15:16:22 +0100

    AsyncLROPoller was introduced in 1.6.0

--- sdk/remoterendering/azure-mixedreality-remoterendering/setup.py
+++ sdk/remoterendering/azure-mixedreality-remoterendering/setup.py
@@ -64,7 +64,7 @@ setup(
         'azure.mixedreality'
     ]),
     install_requires=[
-        'azure-core<2.0.0,>=1.4.0',
+        'azure-core<2.0.0,>=1.6.0',
         'msrest>=0.5.0'
     ],
     extras_require={
