64aaaada23a3b9b0388675a87f856358fb023220
Charles Lowell <chlowe@microsoft.com>
2021-05-21 09:32:23 -0700

    redact headers

--- sdk/identity/azure-identity/tests/recording_processors.py
+++ sdk/identity/azure-identity/tests/recording_processors.py
@@ -12,22 +12,31 @@ from azure_devtools.scenario_tests import RecordingProcessor
 import six
 
 
-SECRETS = frozenset({
-    "access_token",
-    "client_secret",
-    "code",
-    "device_code",
-    "message",
-    "password",
-    "refresh_token",
-    "user_code",
-})
+SECRET_FIELDS = frozenset(
+    {
+        "access_token",
+        "client_secret",
+        "code",
+        "device_code",
+        "message",
+        "password",
+        "refresh_token",
+        "user_code",
+    }
+)
+
+SECRET_HEADERS = frozenset(
+    {
+        "secret",  # managed identity headers are not dangerous to record but
+        "X-IDENTITY-SECRET",  # redacting them prevents anyone worrying whether they are
+    }
+)
 
 
 class RecordingRedactor(RecordingProcessor):
     """Removes authentication secrets from recordings.
 
-    :keyword bool record_unique_values:
+    :keyword bool record_unique_values: Defaults to False. Set True for tests requiring unique, consistent fake values.
     """
 
     def __init__(self, record_unique_values=False):
@@ -37,6 +46,12 @@ class RecordingRedactor(RecordingProcessor):
     def process_request(self, request):
         # bodies typically contain secrets and are often formed by msal anyway, i.e. not this library's responsibility
         request.body = None
+
+        for header in SECRET_HEADERS:
+            if header in request.headers:
+                fake_value = self._get_fake_value(request.headers[header])
+                request.headers[header] = fake_value
+
         return request
 
     def process_response(self, response):
@@ -46,18 +61,19 @@ class RecordingRedactor(RecordingProcessor):
             return response
 
         for field in body:
-            if field in SECRETS:
-                redacted_value = "redacted"
-                if self._record_unique_values:
-                    # include a hash of the secret instead of a simple replacement
-                    # because some tests (e.g. for CAE) require unique, consistent values
-                    digest = hashlib.sha256(six.ensure_binary(body[field])).digest()
-                    redacted_value += six.ensure_str(binascii.hexlify(digest))[:6]
-                body[field] = redacted_value
+            if field in SECRET_FIELDS:
+                fake_value = self._get_fake_value(body[field])
+                body[field] = fake_value
 
         response["body"]["string"] = json.dumps(body)
         return response
 
+    def _get_fake_value(self, real_value):
+        redacted_value = "redacted"
+        if self._record_unique_values:
+            digest = hashlib.sha256(six.ensure_binary(real_value)).digest()
+            redacted_value += six.ensure_str(binascii.hexlify(digest))[:6]
+        return redacted_value
 
 class IdTokenProcessor(RecordingProcessor):
     def process_response(self, response):
