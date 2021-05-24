fba0a5014d70d70204693557d9771f03bcd9f77f
SDKAuto <sdkautomation@microsoft.com>
2021-05-24 08:50:58 +0000

    CodeGen from PR 14165 in Azure/azure-rest-api-specs imagebuilder wave4 (#14165)
    * imagebuilder wave4
    
    * only track2
    
    Co-authored-by: Yan Zhang (WICRESOFT NORTH AMERICA LTD) <v-yanzhang@microsoft.com>

--- sdk/compute/azure-mgmt-imagebuilder/azure/mgmt/imagebuilder/aio/_image_builder_client.py
+++ sdk/compute/azure-mgmt-imagebuilder/azure/mgmt/imagebuilder/aio/_image_builder_client.py
@@ -6,33 +6,31 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import TYPE_CHECKING
+from typing import Any, Optional, TYPE_CHECKING
 
-from azure.mgmt.core import ARMPipelineClient
+from azure.core.pipeline.transport import AsyncHttpResponse, HttpRequest
+from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
-    from typing import Any, Optional
-
-    from azure.core.credentials import TokenCredential
-    from azure.core.pipeline.transport import HttpRequest, HttpResponse
+    from azure.core.credentials_async import AsyncTokenCredential
 
 from ._configuration import ImageBuilderClientConfiguration
 from .operations import VirtualMachineImageTemplatesOperations
 from .operations import Operations
-from . import models
+from .. import models
 
 
 class ImageBuilderClient(object):
     """Azure Virtual Machine Image Builder Client.
 
     :ivar virtual_machine_image_templates: VirtualMachineImageTemplatesOperations operations
-    :vartype virtual_machine_image_templates: azure.mgmt.imagebuilder.operations.VirtualMachineImageTemplatesOperations
+    :vartype virtual_machine_image_templates: azure.mgmt.imagebuilder.aio.operations.VirtualMachineImageTemplatesOperations
     :ivar operations: Operations operations
-    :vartype operations: azure.mgmt.imagebuilder.operations.Operations
+    :vartype operations: azure.mgmt.imagebuilder.aio.operations.Operations
     :param credential: Credential needed for the client to connect to Azure.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: Subscription credentials which uniquely identify Microsoft Azure subscription. The subscription Id forms part of the URI for every service call.
     :type subscription_id: str
     :param str base_url: Service URL
@@ -41,16 +39,15 @@ class ImageBuilderClient(object):
 
     def __init__(
         self,
-        credential,  # type: "TokenCredential"
-        subscription_id,  # type: str
-        base_url=None,  # type: Optional[str]
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> None
+        credential: "AsyncTokenCredential",
+        subscription_id: str,
+        base_url: Optional[str] = None,
+        **kwargs: Any
+    ) -> None:
         if not base_url:
             base_url = 'https://management.azure.com'
         self._config = ImageBuilderClientConfiguration(credential, subscription_id, **kwargs)
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -62,33 +59,29 @@ class ImageBuilderClient(object):
         self.operations = Operations(
             self._client, self._config, self._serialize, self._deserialize)
 
-    def _send_request(self, http_request, **kwargs):
-        # type: (HttpRequest, Any) -> HttpResponse
+    async def _send_request(self, http_request: HttpRequest, **kwargs: Any) -> AsyncHttpResponse:
         """Runs the network request through the client's chained policies.
 
         :param http_request: The network request you want to make. Required.
         :type http_request: ~azure.core.pipeline.transport.HttpRequest
         :keyword bool stream: Whether the response payload will be streamed. Defaults to True.
         :return: The response of your network call. Does not do error handling on your response.
-        :rtype: ~azure.core.pipeline.transport.HttpResponse
+        :rtype: ~azure.core.pipeline.transport.AsyncHttpResponse
         """
         path_format_arguments = {
             'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
         }
         http_request.url = self._client.format_url(http_request.url, **path_format_arguments)
         stream = kwargs.pop("stream", True)
-        pipeline_response = self._client._pipeline.run(http_request, stream=stream, **kwargs)
+        pipeline_response = await self._client._pipeline.run(http_request, stream=stream, **kwargs)
         return pipeline_response.http_response
 
-    def close(self):
-        # type: () -> None
-        self._client.close()
+    async def close(self) -> None:
+        await self._client.close()
 
-    def __enter__(self):
-        # type: () -> ImageBuilderClient
-        self._client.__enter__()
+    async def __aenter__(self) -> "ImageBuilderClient":
+        await self._client.__aenter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
-        self._client.__exit__(*exc_details)
+    async def __aexit__(self, *exc_details) -> None:
+        await self._client.__aexit__(*exc_details)
