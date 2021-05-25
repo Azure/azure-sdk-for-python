c6bdc1f60d147c75984d9796b7c9c8a4dfeca641
Jiri Burant <jiriburant@microsoft.com>
2021-05-25 13:42:37 +0200

    Added basic functionality to the sip client.

--- sdk/communication/azure-communication-siprouting/azure/communication/siprouting/_sip_routing_client.py
+++ sdk/communication/azure-communication-siprouting/azure/communication/siprouting/_sip_routing_client.py
@@ -1,15 +1,20 @@
 from ._generated._azure_communication_sip_routing_service import AzureCommunicationSIPRoutingService
+from ._shared.utils import parse_connection_str
+from ._generated.models import SipConfiguration
+
+from azure.core.tracing.decorator import distributed_trace
+
 try:
     from urllib.parse import urlparse
 except ImportError:
     from urlparse import urlparse  # type: ignore
 
 
-class SIPRoutingClient():
+class SIPRoutingClient(object):
     def __init__(
             self,
             endpoint,  # type: str
-            credential,
+            credential, 
             **kwargs  # type: any
     ):
         # type: (...) -> None
@@ -33,8 +38,66 @@ class SIPRoutingClient():
             **kwargs
         )
 
+    @classmethod
+    def from_connection_string(cls, connection_string, **kwargs):
+        # type: (str, **Any) -> None
+        """Factory method for creating client from connection string
+        
+        :param connection_string: Connection string containing endpoint and credentials
+        :type connection_string: str
+        :returns: The newly created client.
+        :rtype: ~SIPRoutingClient
+        """
+        endpoint, credential = parse_connection_str(connection_string)
+        return cls(endpoint, credential, **kwargs)
+
+    @distributed_trace
     def get_sip_configuration(
             self,
-            cancellation_token):
-        acs_resource_calling_configuration = self._rest_service.get_sip_configuration()
+            **kwargs):
+
+        acs_resource_calling_configuration = self._rest_service.get_sip_configuration(kwargs)
         return acs_resource_calling_configuration
+
+    @distributed_trace
+    def update_sip_trunk_configuration(
+            self,
+            online_pstn_gateways,
+            online_pstn_routing_settings,
+            **kwargs
+            ):
+
+        if not online_pstn_gateways:
+            raise ValueError("Online PSTN gateways can not be null")
+
+        if not online_pstn_routing_settings:
+            raise ValueError("Online PSTN routing setting can not be null")
+
+        updated_sip_configuration = SipConfiguration(trunks = online_pstn_gateways, routes = online_pstn_routing_settings)
+        return self._rest_service.patch_sip_configuration(updated_sip_configuration, kwargs)
+
+    @distributed_trace
+    def update_pstn_gateways(
+            self,
+            online_pstn_gateways,
+            **kwargs
+            ):
+
+        if not online_pstn_gateways:
+            raise ValueError("Online PSTN gateways can not be null")
+
+        updated_sip_configuration = SipConfiguration(trunks = online_pstn_gateways)
+        return self._rest_service.patch_sip_configuration(updated_sip_configuration, kwargs)
+
+    @distributed_trace
+    def update_routing_settings(
+            self,
+            online_pstn_routing_settings,
+            **kwargs
+            ):
+
+        if not online_pstn_routing_settings:
+            raise ValueError("Online PSTN routing setting can not be null")
+
+        updated_sip_configuration = SipConfiguration(routes = online_pstn_routing_settings)
+        return self._rest_service.patch_sip_configuration(updated_sip_configuration, kwargs)
