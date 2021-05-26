192743ea5fd9135b688d8482b29c88129bf4b583
Travis Prescott <travisj.prescott@gmail.com>
2021-05-26 11:12:48 -0700

    Disable client-side validation.

--- sdk/cognitiveservices/azure-cognitiveservices-vision-computervision/azure/cognitiveservices/vision/computervision/_computer_vision_client.py
+++ sdk/cognitiveservices/azure-cognitiveservices-vision-computervision/azure/cognitiveservices/vision/computervision/_computer_vision_client.py
@@ -40,5 +40,6 @@ class ComputerVisionClient(ComputerVisionClientOperationsMixin, SDKClient):
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self.api_version = '3.2'
         self._serialize = Serializer(client_models)
+        self._serialize.client_side_validation = False
         self._deserialize = Deserializer(client_models)
 
