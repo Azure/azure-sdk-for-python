84f2e692fc5cb3e2702b11c2497ca7ce22b3c881
McCoy Patiño <mcpatino@microsoft.com>
2021-05-25 17:57:14 -0700

    Update tests

--- sdk/keyvault/azure-keyvault-administration/tests/test_access_control_async.py
+++ sdk/keyvault/azure-keyvault-administration/tests/test_access_control_async.py
@@ -76,7 +76,7 @@ class AccessControlTests(KeyVaultTestCase):
         # create custom role definition
         role_name = self.get_resource_name("role-name")
         definition_name = self.get_replayable_uuid("definition-name")
-        permissions = [KeyVaultPermission(allowed_data_actions=[KeyVaultDataAction.READ_HSM_KEY])]
+        permissions = [KeyVaultPermission(data_actions=[KeyVaultDataAction.READ_HSM_KEY])]
         created_definition = await client.set_role_definition(
             role_scope=scope,
             role_definition_name=definition_name,
@@ -89,12 +89,12 @@ class AccessControlTests(KeyVaultTestCase):
         assert created_definition.name == definition_name
         assert created_definition.description == "test"
         assert len(created_definition.permissions) == 1
-        assert created_definition.permissions[0].allowed_data_actions == [KeyVaultDataAction.READ_HSM_KEY]
+        assert created_definition.permissions[0].data_actions == [KeyVaultDataAction.READ_HSM_KEY]
         assert created_definition.assignable_scopes == [KeyVaultRoleScope.GLOBAL]
 
         # update custom role definition
         permissions = [
-            KeyVaultPermission(allowed_data_actions=[], denied_data_actions=[KeyVaultDataAction.READ_HSM_KEY])
+            KeyVaultPermission(data_actions=[], not_data_actions=[KeyVaultDataAction.READ_HSM_KEY])
         ]
         updated_definition = await client.set_role_definition(
             role_scope=scope, role_definition_name=definition_name, permissions=permissions
@@ -102,8 +102,8 @@ class AccessControlTests(KeyVaultTestCase):
         assert updated_definition.role_name == ""
         assert updated_definition.description == ""
         assert len(updated_definition.permissions) == 1
-        assert len(updated_definition.permissions[0].allowed_data_actions) == 0
-        assert updated_definition.permissions[0].denied_data_actions == [KeyVaultDataAction.READ_HSM_KEY]
+        assert len(updated_definition.permissions[0].data_actions) == 0
+        assert updated_definition.permissions[0].not_data_actions == [KeyVaultDataAction.READ_HSM_KEY]
         assert updated_definition.assignable_scopes == [KeyVaultRoleScope.GLOBAL]
 
         # assert that the created role definition isn't duplicated
