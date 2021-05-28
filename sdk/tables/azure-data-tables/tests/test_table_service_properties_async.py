bdd1e35211a73c17f0e6db92ed9ca554bcf2ccb4
seankane-msft <seankane@microsoft.com>
2021-05-28 12:39:17 -0400

    reverting back to 30s sleep

--- sdk/tables/azure-data-tables/tests/test_table_service_properties_async.py
+++ sdk/tables/azure-data-tables/tests/test_table_service_properties_async.py
@@ -37,7 +37,7 @@ class TableServicePropertiesTest(AzureTestCase, TableTestCase):
         # Assert
         assert resp is None
         if self.is_live:
-            time.sleep(20)
+            time.sleep(30)
         self._assert_properties_default(await tsc.get_service_properties())
 
     # --Test cases per feature ---------------------------------------
@@ -53,7 +53,7 @@ class TableServicePropertiesTest(AzureTestCase, TableTestCase):
 
         # Assert
         if self.is_live:
-            time.sleep(20)
+            time.sleep(30)
         received_props = await tsc.get_service_properties()
         self._assert_logging_equal(received_props['analytics_logging'], logging)
 
@@ -86,7 +86,7 @@ class TableServicePropertiesTest(AzureTestCase, TableTestCase):
 
         # Assert
         if self.is_live:
-            time.sleep(20)
+            time.sleep(30)
         received_props = await tsc.get_service_properties()
         self._assert_metrics_equal(received_props['minute_metrics'], minute_metrics)
 
@@ -116,7 +116,7 @@ class TableServicePropertiesTest(AzureTestCase, TableTestCase):
 
         # Assert
         if self.is_live:
-            time.sleep(20)
+            time.sleep(30)
         received_props = await tsc.get_service_properties()
         self._assert_cors_equal(received_props['cors'], cors)
 
