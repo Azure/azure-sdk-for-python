3e09e245b9f552e283d1a0ea034c4c42dfaaac3d
Jiri Burant <83883278+jiriburant@users.noreply.github.com>
2021-06-01 09:46:45 +0200

    Removed async mock to be compatible with the builds.

--- sdk/communication/azure-communication-siprouting/tests/test_sip_routing_client_async.py
+++ sdk/communication/azure-communication-siprouting/tests/test_sip_routing_client_async.py
@@ -7,12 +7,13 @@
 import aiounittest
 import json
 import pytest
+import asyncio
 
 from azure.communication.siprouting.aio import SIPRoutingClient
 from azure.core.credentials import AccessToken
 from azure.communication.siprouting._generated.models import Trunk, TrunkRoute
 
-from unittest.mock import Mock, patch, AsyncMock
+from unittest.mock import Mock, patch
 
 
 class TestSIPRoutingClientAsync(aiounittest.AsyncTestCase):
@@ -54,57 +55,45 @@ class TestSIPRoutingClientAsync(aiounittest.AsyncTestCase):
         self.response = self.mock_response(
             status_code=200, json_payload=self.payload)
 
-    @pytest.mark.asyncio
-    async def test_get_sip_configuration(self):
-        async def mock_send(*_, **__):
-            return self.response
-
-        test_client = SIPRoutingClient(
-            "https://endpoint", AccessToken("Fake Token", 0), transport=AsyncMock(send=mock_send))
+    @patch("azure.communication.siprouting._generated.aio._azure_communication_sip_routing_service.AzureCommunicationSIPRoutingServiceOperationsMixin.get_sip_configuration",
+           return_value={"trunks": {"trunk_1": Trunk(sip_signaling_port=4001)}, "routes": [TrunkRoute(
+               name= "route_1",
+               number_pattern= "x",
+               trunks= ["trunk_1"])
+               ]})
+    async def test_get_sip_configuration(self, mock):
+        test_client = self.get_simple_test_client()
 
         response = await test_client.get_sip_configuration()
 
-        self.assertEqual(response.trunks, self.test_trunks)
-        self.assertEqual(response.routes, self.test_routes)
+        self.assertEqual(response['trunks'], self.test_trunks)
+        self.assertEqual(response['routes'], self.test_routes)
 
-    @pytest.mark.asyncio
-    async def test_update_sip_trunk_configuration(self):
-        mock = AsyncMock(send=AsyncMock(return_value=self.response))
-        test_client = SIPRoutingClient(
-            "https://endpoint", AccessToken("Fake Token", 0), transport=mock)
+    @patch("azure.communication.siprouting._generated.aio._azure_communication_sip_routing_service.AzureCommunicationSIPRoutingService.patch_sip_configuration")
+    async def test_update_sip_trunk_configuration(self, mock):
+        test_client = self.get_simple_test_client()
 
         await test_client.update_sip_trunk_configuration(
             self.test_trunks, self.test_routes)
 
-        self.assertEqual(
-            json.loads(mock.send.call_args.args[0].body), self.payload)
-    
-    @pytest.mark.asyncio
-    async def test_update_pstn_gateways(self):
-        expected_request_body = {"trunks": {
-            "trunk_1": {"sipSignalingPort": 4001}}}
-        mock = AsyncMock(send=AsyncMock(return_value=self.response))
-        test_client = SIPRoutingClient(
-            "https://endpoint", AccessToken("Fake Token", 0), transport=mock)
+        self.assertEqual(mock.call_args[1]['body'].trunks, self.test_trunks)
+        self.assertEqual(mock.call_args[1]['body'].routes, self.test_routes)
+
+    @patch("azure.communication.siprouting._generated.aio._azure_communication_sip_routing_service.AzureCommunicationSIPRoutingService.patch_sip_configuration")
+    async def test_update_pstn_gateways(self, mock):
+        test_client = self.get_simple_test_client()
 
         await test_client.update_pstn_gateways(self.test_trunks)
 
-        self.assertEqual(
-            json.loads(mock.send.call_args.args[0].body), expected_request_body)
+        self.assertEqual(mock.call_args[1]['body'].trunks, self.test_trunks)
 
-    @pytest.mark.asyncio
-    async def test_update_routing_settings(self):
-        expected_request_body = {"routes": [
-            {"name": "route_1", "numberPattern": "x", "trunks": ["trunk_1"]}]}
-
-        mock = AsyncMock(send=AsyncMock(return_value=self.response))
-        test_client = SIPRoutingClient(
-            "https://endpoint", AccessToken("Fake Token", 0), transport=mock)
+    @patch("azure.communication.siprouting._generated.aio._azure_communication_sip_routing_service.AzureCommunicationSIPRoutingService.patch_sip_configuration")
+    async def test_update_routing_settings(self, mock):
+        test_client = self.get_simple_test_client()
 
         await test_client.update_routing_settings(self.test_routes)
 
-        self.assertEqual(
-            json.loads(mock.send.call_args.args[0].body), expected_request_body)
+        self.assertEqual(mock.call_args[1]['body'].routes, self.test_routes)
 
     @pytest.mark.asyncio
     async def test_update_sip_trunk_configuration_no_online_pstn_gateways_raises_value_error(self):
@@ -143,8 +132,7 @@ class TestSIPRoutingClientAsync(aiounittest.AsyncTestCase):
 
     @pytest.mark.asyncio
     async def test_update_routing_settings_no_routting_raises_value_error(self):
-        # test_client = self.get_simple_test_client()
-        test_client = SIPRoutingClient("https://endpoint", AccessToken("Fake Token", 0))
+        test_client = self.get_simple_test_client()
 
         try:
             await test_client.update_routing_settings(None)
@@ -152,8 +140,3 @@ class TestSIPRoutingClientAsync(aiounittest.AsyncTestCase):
             raised = True
 
         self.assertTrue(raised)
-
-#test_client = TestSIPRoutingClient()
-
-#test_client.setUp()
-#res = asyncio.run( test_client.test_update_sip_trunk_configuration_no_online_pstn_gateways_raises_value_error())
