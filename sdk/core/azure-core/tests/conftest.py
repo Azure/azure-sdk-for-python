77d9c94666a2d6271eb12f988b44667d3d830cfe
iscai-msft <iscai@microsoft.com>
2021-06-01 13:58:14 -0400

    move pytest and fixture to tests folder

--- sdk/core/azure-core/tests/conftest.py
+++ sdk/core/azure-core/tests/conftest.py
@@ -23,7 +23,33 @@
 # IN THE SOFTWARE.
 #
 # --------------------------------------------------------------------------
+import os
+import signal
+import subprocess
+import pytest
 import sys
+
+def start_testserver():
+    os.environ["FLASK_APP"] = "core.testserver.app.py"
+    cmd = "flask run"
+    if os.name == 'nt': #On windows, subprocess creation works without being in the shell
+        return subprocess.Popen(cmd.format("set"))
+
+    return subprocess.Popen(cmd.format("export"), shell=True, preexec_fn=os.setsid) #On linux, have to set shell=True
+
+def terminate_testserver(process):
+    os.environ["FLASK_APP"] = ""
+    if os.name == 'nt':
+        process.kill()
+    else:
+        os.killpg(os.getpgid(process.pid), signal.SIGTERM)  # Send the signal to all the process groups
+
+@pytest.fixture(scope="session")
+def testserver():
+    """Start the Autorest testserver."""
+    server = start_testserver()
+    yield
+    terminate_testserver(server)
 
 # Ignore collection of async tests for Python 2
 collect_ignore = []
