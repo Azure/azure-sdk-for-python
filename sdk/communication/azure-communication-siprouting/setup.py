03f50644234109600c8f054b60037540cbb5164c
Jiri Burant <83883278+jiriburant@users.noreply.github.com>
2021-06-02 12:39:47 +0200

    Downgraded six version to match the frozen  requirements.

--- sdk/communication/azure-communication-siprouting/setup.py
+++ sdk/communication/azure-communication-siprouting/setup.py
@@ -63,7 +63,7 @@ setup(
     install_requires=[
         'azure-core<2.0.0,>=1.2.2',
         'msrest>=0.5.0',
-        'six>=1.12.0'
+        'six>=1.11.0'
     ],
     extras_require={
         ":python_version<'3.0'": ['azure-communication-nspkg'],
