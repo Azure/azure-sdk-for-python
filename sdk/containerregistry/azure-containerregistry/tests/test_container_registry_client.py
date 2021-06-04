d040674372d133d622342bb66f128b13aa219841
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-06-04 08:50:19 -0700

    [ACR] Add auth scope (#19022)

--- sdk/containerregistry/azure-containerregistry/tests/test_container_registry_client.py
+++ sdk/containerregistry/azure-containerregistry/tests/test_container_registry_client.py
@@ -14,7 +14,7 @@ from azure.containerregistry import (
     ArtifactTagProperties,
     TagOrder,
 )
-from azure.core.exceptions import ResourceNotFoundError
+from azure.core.exceptions import ResourceNotFoundError, ClientAuthenticationError
 from azure.core.paging import ItemPaged
 
 from testcase import ContainerRegistryTestClass
@@ -544,3 +544,12 @@ class TestContainerRegistryClient(ContainerRegistryTestClass):
         digest = digest[:-10] + u"a" * 10
 
         client.delete_manifest(repo, digest)
+
+    # Live only, the fake credential doesn't check auth scope the same way
+    @pytest.mark.live_test_only
+    @acr_preparer()
+    def test_incorrect_authentication_scope(self, containerregistry_endpoint):
+        client = self.create_registry_client(containerregistry_endpoint, authentication_scope="https://microsoft.com")
+
+        with pytest.raises(ClientAuthenticationError):
+            properties = client.get_repository_properties(HELLO_WORLD)
