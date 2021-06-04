5d04e858a880ec6dac8a14d69384e2f64bb20cc6
seankane-msft <seankane@microsoft.com>
2021-06-04 07:52:12 -0700

    adding back tests

--- sdk/containerregistry/azure-containerregistry/tests/test_container_registry_client.py
+++ sdk/containerregistry/azure-containerregistry/tests/test_container_registry_client.py
@@ -544,3 +544,22 @@ class TestContainerRegistryClient(ContainerRegistryTestClass):
         digest = digest[:-10] + u"a" * 10
 
         client.delete_manifest(repo, digest)
+
+    @acr_preparer()
+    def test_expiration_time_parsing(self, containerregistry_endpoint):
+        from azure.containerregistry._authentication_policy import ContainerRegistryChallengePolicy
+        client = self.create_registry_client(containerregistry_endpoint)
+
+        for repo in client.list_repository_names():
+            pass
+
+        for policy in client._client._client._pipeline._impl_policies:
+            if isinstance(policy, ContainerRegistryChallengePolicy):
+                policy._exchange_client._expiration_time = 0
+                break
+
+        count = 0
+        for repo in client.list_repository_names():
+            count += 1
+
+        assert count >= 1
