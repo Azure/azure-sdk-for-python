2e104ce68cf2b7ce9b7cd40ec2cda4089dad30cf
seankane-msft <seankane@microsoft.com>
2021-06-09 12:39:43 -0700

    removing storage stuff

--- tools/azure-sdk-tools/devtools_testutils/storage/__init__.py
+++
@@ -1,9 +0,0 @@
-from .api_assert_policy import ApiVersionAssertPolicy
-from .processors import XMSRequestIDBody
-from .storage_testclass import StorageTestCase
-
-__all__ = [
-    "ApiVersionAssertPolicy",
-    "StorageTestCase",
-    "XMSRequestIDBody",
-]
