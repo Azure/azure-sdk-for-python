521a88debfc085eb49af86d36ce70fedb5999cbe
Xiang Yan <xiangyan@microsoft.com>
2021-06-11 18:43:15 -0700

    suppress min number less than 14

--- sdk/metricsadvisor/azure-ai-metricsadvisor/tests/base_testcase.py
+++ sdk/metricsadvisor/azure-ai-metricsadvisor/tests/base_testcase.py
@@ -185,8 +185,8 @@ class TestMetricsAdvisorAdministrationClientBase(AzureTestCase):
                     sensitivity=50,
                     anomaly_detector_direction="Both",
                     suppress_condition=SuppressCondition(
-                        min_number=50,
-                        min_ratio=50
+                        min_number=5,
+                        min_ratio=5
                     )
                 )
             )
@@ -306,8 +306,8 @@ class TestMetricsAdvisorAdministrationClientBase(AzureTestCase):
                     sensitivity=50,
                     anomaly_detector_direction="Both",
                     suppress_condition=SuppressCondition(
-                        min_number=50,
-                        min_ratio=50
+                        min_number=5,
+                        min_ratio=5
                     )
                 ),
                 hard_threshold_condition=HardThresholdCondition(
