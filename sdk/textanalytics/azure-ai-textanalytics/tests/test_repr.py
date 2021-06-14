acc449dc227e8629988443fb6085ed257de037f0
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2021-06-14 14:20:52 -0400

    [text analytics] change return type of analyze actions to list of list (#18994)
    * change return type of analyze actions to list of list
    
    * address johan's and krista's comments
    
    * limit # of action calls in readme example
    
    Co-authored-by: Krista Pratico <krpratic@microsoft.com>

--- sdk/textanalytics/azure-ai-textanalytics/tests/test_repr.py
+++ sdk/textanalytics/azure-ai-textanalytics/tests/test_repr.py
@@ -441,54 +441,6 @@ class TestRepr():
         assert error.code == "UnsupportedLanguageCode"
         assert error.message == "Supplied language not supported. Pass in one of: de,en,es,fr,it,ja,ko,nl,pt-PT,zh-Hans,zh-Hant"
 
-    def test_analyze_actions_result_recognize_entities(self, recognize_entities_result):
-        model = _models.AnalyzeActionsResult(
-            document_results=[recognize_entities_result[0]],
-            is_error=False,
-            action_type=_models.AnalyzeActionsType.RECOGNIZE_ENTITIES,
-            completed_on=datetime.datetime(1, 1, 1)
-        )
-
-        model_repr = (
-            "AnalyzeActionsResult(document_results=[{}], is_error={}, action_type={}, completed_on={})".format(
-                recognize_entities_result[1], False, "recognize_entities", datetime.datetime(1, 1, 1)
-            )
-        )
-
-        assert repr(model) == model_repr
-
-    def test_analyze_actions_result_recognize_pii_entities(self, recognize_pii_entities_result):
-        model = _models.AnalyzeActionsResult(
-            document_results=[recognize_pii_entities_result[0]],
-            is_error=False,
-            action_type=_models.AnalyzeActionsType.RECOGNIZE_PII_ENTITIES,
-            completed_on=datetime.datetime(1, 1, 1)
-        )
-
-        model_repr = (
-            "AnalyzeActionsResult(document_results=[{}], is_error={}, action_type={}, completed_on={})".format(
-                recognize_pii_entities_result[1], False, "recognize_pii_entities", datetime.datetime(1, 1, 1)
-            )
-        )
-
-        assert repr(model) == model_repr
-
-    def test_analyze_actions_result_extract_key_phrases(self, extract_key_phrases_result):
-        model = _models.AnalyzeActionsResult(
-            document_results=[extract_key_phrases_result[0]],
-            is_error=False,
-            action_type=_models.AnalyzeActionsType.EXTRACT_KEY_PHRASES,
-            completed_on=datetime.datetime(1, 1, 1)
-        )
-
-        model_repr = (
-            "AnalyzeActionsResult(document_results=[{}], is_error={}, action_type={}, completed_on={})".format(
-                extract_key_phrases_result[1], False, "extract_key_phrases", datetime.datetime(1, 1, 1)
-            )
-        )
-
-        assert repr(model) == model_repr
-
     def test_analyze_healthcare_entities_result_item(
         self, healthcare_entity, healthcare_relation, text_analytics_warning, text_document_statistics
     ):
