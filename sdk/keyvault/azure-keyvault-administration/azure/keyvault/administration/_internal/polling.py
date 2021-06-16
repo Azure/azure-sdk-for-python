b728a58e73125b9ddb305655992cc7cca73912d1
McCoy Patiño <mcpatino@microsoft.com>
2021-06-16 15:45:59 -0700

    don't depend on _async_url

--- sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/_internal/polling.py
+++ sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/_internal/polling.py
@@ -4,19 +4,23 @@
 # ------------------------------------
 import base64
 
-from azure.core.polling.base_polling import BadResponse, LROBasePolling, OperationFailed, OperationResourcePolling
+from azure.core.polling.base_polling import LROBasePolling, OperationFailed, OperationResourcePolling
 
 
 class KeyVaultBackupClientPolling(OperationResourcePolling):
     def __init__(self):
+        self._polling_url = None
         super(KeyVaultBackupClientPolling, self).__init__(operation_location_header="azure-asyncoperation")
+
+    def get_polling_url(self):
+        return self._polling_url
 
     def get_final_get_url(self, pipeline_response):
         return None
 
     def set_initial_status(self, pipeline_response):
         response = pipeline_response.http_response
-        self._async_url = response.headers["azure-asyncoperation"]
+        self._polling_url = response.headers["azure-asyncoperation"]
 
         if response.status_code in {200, 201, 202, 204}:
             return self.get_status(pipeline_response)
