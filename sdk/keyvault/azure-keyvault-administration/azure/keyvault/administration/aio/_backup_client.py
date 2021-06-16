380445faf9bbbae9290302ae2dfb6c4b273af16d
McCoy Patiño <39780829+mccoyp@users.noreply.github.com>
2021-06-16 16:42:46 -0700

    [Key Vault] Custom polling method for admin backup client (#19204)

--- sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/aio/_backup_client.py
+++ sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/aio/_backup_client.py
@@ -2,12 +2,14 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
+import base64
 import functools
+import pickle
 from typing import TYPE_CHECKING
 
-from azure.core.polling.async_base_polling import AsyncLROBasePolling
-
+from .._backup_client import _parse_status_url
 from .._internal import AsyncKeyVaultClientBase, parse_folder_url
+from .._internal.async_polling import KeyVaultAsyncBackupClientPollingMethod
 from .._internal.polling import KeyVaultBackupClientPolling
 from .._models import KeyVaultBackupOperation
 
@@ -37,15 +39,43 @@ class KeyVaultBackupClient(AsyncKeyVaultClientBase):
         :keyword str continuation_token: a continuation token to restart polling from a saved state
         :returns: An AsyncLROPoller. Call `result()` on this object to get a :class:`KeyVaultBackupOperation`.
         :rtype: ~azure.core.polling.AsyncLROPoller[~azure.keyvault.administration.KeyVaultBackupOperation]
+
+        Example:
+            .. literalinclude:: ../tests/test_examples_administration_async.py
+                :start-after: [START begin_backup]
+                :end-before: [END begin_backup]
+                :language: python
+                :caption: Create a vault backup
+                :dedent: 8
         """
         polling_interval = kwargs.pop("_polling_interval", 5)
         sas_parameter = self._models.SASTokenParameter(storage_resource_uri=blob_storage_url, token=sas_token)
+
+        continuation_token = kwargs.pop("continuation_token", None)
+        status_response = None
+        if continuation_token:
+            status_url = base64.b64decode(continuation_token.encode()).decode("ascii")
+            try:
+                job_id = _parse_status_url(status_url)
+            except Exception as ex:  # pylint: disable=broad-except
+                raise ValueError(
+                    "The provided continuation_token is malformed. A valid token can be obtained from the operation "
+                    + "poller's continuation_token() method"
+                ) from ex
+
+            pipeline_response = await self._client.full_backup_status(
+                vault_base_url=self._vault_url, job_id=job_id, cls=lambda pipeline_response, _, __: pipeline_response
+            )
+            if "azure-asyncoperation" not in pipeline_response.http_response.headers:
+                pipeline_response.http_response.headers["azure-asyncoperation"] = status_url
+            status_response = base64.b64encode(pickle.dumps(pipeline_response)).decode("ascii")
+
         return await self._client.begin_full_backup(
             vault_base_url=self._vault_url,
             azure_storage_blob_container_uri=sas_parameter,
             cls=KeyVaultBackupOperation._from_generated,
-            continuation_token=kwargs.pop("continuation_token", None),
-            polling=AsyncLROBasePolling(
+            continuation_token=status_response,
+            polling=KeyVaultAsyncBackupClientPollingMethod(
                 lro_algorithms=[KeyVaultBackupClientPolling()], timeout=polling_interval, **kwargs
             ),
             **kwargs
@@ -64,14 +94,47 @@ class KeyVaultBackupClient(AsyncKeyVaultClientBase):
         :keyword str continuation_token: a continuation token to restart polling from a saved state
         :keyword str key_name: name of a single key in the backup. When set, only this key will be restored.
         :rtype: ~azure.core.polling.AsyncLROPoller
+
+        Examples:
+            .. literalinclude:: ../tests/test_examples_administration_async.py
+                :start-after: [START begin_restore]
+                :end-before: [END begin_restore]
+                :language: python
+                :caption: Restore a vault backup
+                :dedent: 8
+
+            .. literalinclude:: ../tests/test_examples_administration_async.py
+                :start-after: [START begin_selective_restore]
+                :end-before: [END begin_selective_restore]
+                :language: python
+                :caption: Restore a single key
+                :dedent: 8
         """
         # AsyncLROBasePolling passes its kwargs to pipeline.run(), so we remove unexpected args before constructing it
         continuation_token = kwargs.pop("continuation_token", None)
         key_name = kwargs.pop("key_name", None)
 
+        status_response = None
+        if continuation_token:
+            status_url = base64.b64decode(continuation_token.encode()).decode("ascii")
+            try:
+                job_id = _parse_status_url(status_url)
+            except Exception as ex:  # pylint: disable=broad-except
+                raise ValueError(
+                    "The provided continuation_token is malformed. A valid token can be obtained from the operation "
+                    + "poller's continuation_token() method"
+                ) from ex
+
+            pipeline_response = await self._client.restore_status(
+                vault_base_url=self._vault_url, job_id=job_id, cls=lambda pipeline_response, _, __: pipeline_response
+            )
+            if "azure-asyncoperation" not in pipeline_response.http_response.headers:
+                pipeline_response.http_response.headers["azure-asyncoperation"] = status_url
+            status_response = base64.b64encode(pickle.dumps(pipeline_response)).decode("ascii")
+
         container_url, folder_name = parse_folder_url(folder_url)
         sas_parameter = self._models.SASTokenParameter(storage_resource_uri=container_url, token=sas_token)
-        polling = AsyncLROBasePolling(
+        polling = KeyVaultAsyncBackupClientPollingMethod(
             lro_algorithms=[KeyVaultBackupClientPolling()], timeout=kwargs.pop("_polling_interval", 5), **kwargs
         )
 
@@ -90,7 +153,7 @@ class KeyVaultBackupClient(AsyncKeyVaultClientBase):
             vault_base_url=self._vault_url,
             restore_blob_details=restore_details,
             cls=lambda *_: None,  # poller.result() returns None
-            continuation_token=continuation_token,
+            continuation_token=status_response,
             polling=polling,
             **kwargs
         )
