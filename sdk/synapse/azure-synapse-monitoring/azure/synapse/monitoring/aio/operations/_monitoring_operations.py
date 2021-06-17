732c555cb70683bb6f2678164599ef1db2851501
iscai-msft <iscai@microsoft.com>
2021-06-17 19:33:55 -0400

    switch from send_request to pipeline.run for now

--- sdk/synapse/azure-synapse-monitoring/azure/synapse/monitoring/aio/operations/_monitoring_operations.py
+++ sdk/synapse/azure-synapse-monitoring/azure/synapse/monitoring/aio/operations/_monitoring_operations.py
@@ -76,9 +76,7 @@ class MonitoringOperations:
         }
         request.url = self._client.format_url(request.url, **path_format_arguments)
 
-        pipeline_response = await self._client.send_request(
-            request, stream=False, _return_pipeline_response=True, **kwargs
-        )
+        await self._client._pipeline.run(request, stream=False, **kwargs)
         response = pipeline_response.http_response
 
         if response.status_code not in [200]:
@@ -136,9 +134,7 @@ class MonitoringOperations:
         }
         request.url = self._client.format_url(request.url, **path_format_arguments)
 
-        pipeline_response = await self._client.send_request(
-            request, stream=False, _return_pipeline_response=True, **kwargs
-        )
+        await self._client._pipeline.run(request, stream=False, **kwargs)
         response = pipeline_response.http_response
 
         if response.status_code not in [200]:
