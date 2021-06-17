cc8bdfceb23e5ae9f78323edc2a4e66e348bb17a
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-06-17 18:52:28 -0400

    Tables aad (#19299)
    closes #19244

--- sdk/tables/azure-data-tables/azure/data/tables/_table_service_client.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_table_service_client.py
@@ -44,11 +44,12 @@ class TableServiceClient(TablesBaseClient):
         authenticated with a SAS token.
     :keyword credential:
         The credentials with which to authenticate. This is optional if the
-        account URL already has a SAS token. The value can be one of AzureNamedKeyCredential
-        or AzureSasCredential from azure-core.
+        account URL already has a SAS token. The value can be one of AzureNamedKeyCredential (azure-core),
+        AzureSasCredential (azure-core), or TokenCredentials from azure-identity.
     :paramtype credential:
         :class:`~azure.core.credentials.AzureNamedKeyCredential` or
-        :class:`~azure.core.credentials.AzureSasCredential`
+        :class:`~azure.core.credentials.AzureSasCredential` or
+        :class:`~azure.core.credentials.TokenCredential`
     :keyword str api_version:
         The Storage API version to use for requests. Default value is '2019-02-02'.
         Setting to an older version may result in reduced feature compatibility.
