6fbde5b9081dd951bf40953afaf498331ebfdd5d
McCoy Patiño <39780829+mccoyp@users.noreply.github.com>
2021-06-22 15:24:20 -0700

    [Key Vault] Performance tests for certificates, keys, and secrets (#19002)

--- sdk/keyvault/azure-keyvault-keys/tests/perfstress_tests/unwrap.py
+++ sdk/keyvault/azure-keyvault-keys/tests/perfstress_tests/unwrap.py
@@ -2,14 +2,23 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
+import os
+
 from azure_devtools.perfstress_tests import PerfStressTest
 from azure.identity import DefaultAzureCredential
 from azure.identity.aio import DefaultAzureCredential as AsyncDefaultAzureCredential
 from azure.keyvault.keys import KeyClient
 from azure.keyvault.keys.aio import KeyClient as AsyncKeyClient
+from azure.keyvault.keys.crypto import CryptographyClient, EncryptionAlgorithm
+from azure.keyvault.keys.crypto.aio import CryptographyClient as AsyncCryptographyClient
+from azure.mgmt.keyvault.models import KeyPermissions, Permissions
 
 
-class GetKeyTest(PerfStressTest):
+# without keys/get, a CryptographyClient created with a key ID performs all ops remotely
+NO_GET = Permissions(keys=[p.value for p in KeyPermissions if p.value != "get"])
+
+
+class UnwrapTest(PerfStressTest):
 
     def __init__(self, arguments):
         super().__init__(arguments)
@@ -22,12 +31,20 @@ class GetKeyTest(PerfStressTest):
         vault_url = self.get_from_env("AZURE_KEYVAULT_URL")
         self.client = KeyClient(vault_url, self.credential, **self._client_kwargs)
         self.async_client = AsyncKeyClient(vault_url, self.async_credential, **self._client_kwargs)
-        self.key_name = "livekvtestgetkeyperfkey"
+        self.key_name = "livekvtestunwrapperfkey"
 
     async def global_setup(self):
         """The global setup is run only once."""
         await super().global_setup()
-        await self.async_client.create_rsa_key(self.key_name)
+        rsa_key = await self.async_client.create_rsa_key(self.key_name)
+        self.crypto_client = CryptographyClient(rsa_key.id, self.credential, permissions=NO_GET, **self._client_kwargs)
+        self.async_crypto_client = AsyncCryptographyClient(
+            rsa_key.id, self.async_credential, permissions=NO_GET, **self._client_kwargs
+        )
+
+        self.test_algorithm = EncryptionAlgorithm.rsa_oaep_256
+        key_bytes = os.urandom(32)
+        self.encrypted_key = self.crypto_client.wrap_key(self.test_algorithm, key_bytes).encrypted_key
 
     async def global_cleanup(self):
         """The global cleanup is run only once."""
@@ -38,13 +55,14 @@ class GetKeyTest(PerfStressTest):
     async def close(self):
         """This is run after cleanup."""
         await self.async_client.close()
+        await self.async_crypto_client.close()
         await self.async_credential.close()
         await super().close()
 
     def run_sync(self):
         """The synchronous perf test."""
-        self.client.get_key(self.key_name)
+        self.crypto_client.unwrap_key(self.test_algorithm, self.encrypted_key)
 
     async def run_async(self):
         """The asynchronous perf test."""
-        await self.async_client.get_key(self.key_name)
+        await self.async_crypto_client.unwrap_key(self.test_algorithm, self.encrypted_key)
