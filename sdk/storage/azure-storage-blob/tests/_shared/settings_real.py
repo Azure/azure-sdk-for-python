ee879074e3f2356d30f5dc24346ff47d2dc9927b
xiafu <xiafu@microsoft.com>
2021-06-24 16:42:45 -0700

    async

---
+++ sdk/storage/azure-storage-blob/tests/_shared/settings_real.py
@@ -0,0 +1,17 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# ----------------------------------------------------------------s----------
+
+STORAGE_ACCOUNT_NAME = "emilydevtest"
+STORAGE_ACCOUNT_KEY = ""
+
+
+ACCOUNT_URL_SUFFIX = 'core.windows.net'
+RUN_IN_LIVE = "True"
+SKIP_LIVE_RECORDING = "False"
+
+PROTOCOL = "http"
+
+ENABLE_LOGGING = True
