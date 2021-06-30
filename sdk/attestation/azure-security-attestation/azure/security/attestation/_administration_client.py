347e714ea10fab9117886b2569fd011829b662c9
Larry Osterman <LarryOsterman@users.noreply.github.com>
2021-06-30 14:44:54 -0700

    Fixed ][ typo that got missed.

--- sdk/attestation/azure-security-attestation/azure/security/attestation/_administration_client.py
+++ sdk/attestation/azure-security-attestation/azure/security/attestation/_administration_client.py
@@ -496,7 +496,7 @@ class AttestationAdministrationClient(object):
         :keyword validation_callback: Function callback to allow clients to perform custom validation of the token.
             if the token is invalid, the `validation_callback` function should throw
             an exception.
-        :paramtype validation_callback: ~typing.Callable[[~azure.security.attestation.AttestationToken, ~azure.security.attestation.AttestationSigner], None][
+        :paramtype validation_callback: ~typing.Callable[[~azure.security.attestation.AttestationToken, ~azure.security.attestation.AttestationSigner], None]
         :keyword bool validate_signature: if True, validate the signature of the token being validated.
         :keyword bool validate_expiration: If True, validate the expiration time of the token being validated.
         :keyword str issuer: Expected issuer, used if validate_issuer is true.
