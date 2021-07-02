9fcb63352b748ff61d1aa5cc93afa4f0ab9062de
SDKAuto <sdkautomation@microsoft.com>
2021-07-02 02:27:41 +0000

    CodeGen from PR 15050 in Azure/azure-rest-api-specs python track2 config (#15050)
    * python track2 config
    
    * redis
    
    * redis

--- sdk/redis/azure-mgmt-redis/azure/mgmt/redis/aio/_redis_management_client.py
+++ sdk/redis/azure-mgmt-redis/azure/mgmt/redis/aio/_redis_management_client.py
@@ -8,6 +8,7 @@
 
 from typing import Any, Optional, TYPE_CHECKING
 
+from azure.core.pipeline.transport import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
@@ -21,6 +22,8 @@ from .operations import RedisOperations
 from .operations import FirewallRulesOperations
 from .operations import PatchSchedulesOperations
 from .operations import LinkedServerOperations
+from .operations import PrivateEndpointConnectionsOperations
+from .operations import PrivateLinkResourcesOperations
 from .. import models
 
 
@@ -37,6 +40,10 @@ class RedisManagementClient(object):
     :vartype patch_schedules: azure.mgmt.redis.aio.operations.PatchSchedulesOperations
     :ivar linked_server: LinkedServerOperations operations
     :vartype linked_server: azure.mgmt.redis.aio.operations.LinkedServerOperations
+    :ivar private_endpoint_connections: PrivateEndpointConnectionsOperations operations
+    :vartype private_endpoint_connections: azure.mgmt.redis.aio.operations.PrivateEndpointConnectionsOperations
+    :ivar private_link_resources: PrivateLinkResourcesOperations operations
+    :vartype private_link_resources: azure.mgmt.redis.aio.operations.PrivateLinkResourcesOperations
     :param credential: Credential needed for the client to connect to Azure.
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: Gets subscription credentials which uniquely identify the Microsoft Azure subscription. The subscription ID forms part of the URI for every service call.
@@ -72,6 +79,27 @@ class RedisManagementClient(object):
             self._client, self._config, self._serialize, self._deserialize)
         self.linked_server = LinkedServerOperations(
             self._client, self._config, self._serialize, self._deserialize)
+        self.private_endpoint_connections = PrivateEndpointConnectionsOperations(
+            self._client, self._config, self._serialize, self._deserialize)
+        self.private_link_resources = PrivateLinkResourcesOperations(
+            self._client, self._config, self._serialize, self._deserialize)
+
+    async def _send_request(self, http_request: HttpRequest, **kwargs: Any) -> AsyncHttpResponse:
+        """Runs the network request through the client's chained policies.
+
+        :param http_request: The network request you want to make. Required.
+        :type http_request: ~azure.core.pipeline.transport.HttpRequest
+        :keyword bool stream: Whether the response payload will be streamed. Defaults to True.
+        :return: The response of your network call. Does not do error handling on your response.
+        :rtype: ~azure.core.pipeline.transport.AsyncHttpResponse
+        """
+        path_format_arguments = {
+            'subscriptionId': self._serialize.url("self._config.subscription_id", self._config.subscription_id, 'str'),
+        }
+        http_request.url = self._client.format_url(http_request.url, **path_format_arguments)
+        stream = kwargs.pop("stream", True)
+        pipeline_response = await self._client._pipeline.run(http_request, stream=stream, **kwargs)
+        return pipeline_response.http_response
 
     async def close(self) -> None:
         await self._client.close()
