4fa2f77166f3b33f8e7c268b190ea32c9c7a5fc3
SDKAuto <sdkautomation@microsoft.com>
2021-07-04 12:44:41 +0000

    CodeGen from PR 14950 in Azure/azure-rest-api-specs New api version support to include disable local auth flag for event hub (#14950)
    * New api version support to include disable local auth flag for event hub
    
    * Updated api-version in examples
    
    * Added auth examples in NameSpaces
    
    * Fixing LintDiff error
    
    * Fixing LintDiff error contd
    
    * Removed UserassignedIdentities as part of definition
    
    * Changed to UserAssignedIdentity as a reference in userAssignedIdentities object
    
    Co-authored-by: Sayan Ghosh <sayanghosh@microsoft.com>

--- sdk/eventhub/azure-mgmt-eventhub/azure/mgmt/eventhub/v2018_01_01_preview/_version.py
+++ sdk/eventhub/azure-mgmt-eventhub/azure/mgmt/eventhub/v2018_01_01_preview/_version.py
@@ -6,14 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._event_hub_management_client import EventHubManagementClient
-from ._version import VERSION
-
-__version__ = VERSION
-__all__ = ['EventHubManagementClient']
-
-try:
-    from ._patch import patch_sdk  # type: ignore
-    patch_sdk()
-except ImportError:
-    pass
+VERSION = "1.0.0b1"
