a114e7ed3e11bd0e70c26894c99096a0872bbffe
Josue Garcia <t-jgarcia@microsoft.com>
2021-07-14 23:11:28 -0400

    template update

--- sdk/eventhub/azure-eventhub-checkpointstoretable/tests/test_simple.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/tests/test_simple.py
@@ -3,7 +3,7 @@
 # Pytest should be leveraged to test your project.
 
 from devtools_testutils import AzureMgmtTestCase
-from azure.template import template_main
+#from azure.template import template_main
 
 # this test case highlights that there are some additional Test capabilities present in devtools_testutils
 # as a package owner you are not required to use these. Standard PyTest implementation will work.
