dd4c78db7415f4f0d9aaefa8aaa9e8bb479eaab1
SDKAuto <sdkautomation@microsoft.com>
2021-07-15 03:29:53 +0000

    CodeGen from PR 15207 in Azure/azure-rest-api-specs Deviceprovisiongservices t1 config (#15207)
    * sql t2 readme config
    
    * readme config
    
    * Update readme.python.md
    
    * conflient resolve
    
    * t1 revert
    
    * Update readme.python.md
    
    Co-authored-by: msyyc <70930885+msyyc@users.noreply.github.com>

--- sdk/iothub/azure-mgmt-iothubprovisioningservices/azure/mgmt/iothubprovisioningservices/models/_iot_dps_client_enums.py
+++ sdk/iothub/azure-mgmt-iothubprovisioningservices/azure/mgmt/iothubprovisioningservices/models/_iot_dps_client_enums.py
@@ -33,6 +33,33 @@ class State(str, Enum):
     failover_failed = "FailoverFailed"
 
 
+class PublicNetworkAccess(str, Enum):
+
+    enabled = "Enabled"
+    disabled = "Disabled"
+
+
+class IpFilterActionType(str, Enum):
+
+    accept = "Accept"
+    reject = "Reject"
+
+
+class IpFilterTargetType(str, Enum):
+
+    all = "all"
+    service_api = "serviceApi"
+    device_api = "deviceApi"
+
+
+class PrivateLinkServiceConnectionStatus(str, Enum):
+
+    pending = "Pending"
+    approved = "Approved"
+    rejected = "Rejected"
+    disconnected = "Disconnected"
+
+
 class AllocationPolicy(str, Enum):
 
     hashed = "Hashed"
