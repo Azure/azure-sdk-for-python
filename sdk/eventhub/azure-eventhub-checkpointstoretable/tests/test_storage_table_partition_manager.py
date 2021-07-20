d882bfdd8daaba87d46e5e2232a01a76bad7f915
Josue Garcia <t-jgarcia@microsoft.com>
2021-07-20 15:27:56 -0400

    update on tests

---
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/tests/test_storage_table_partition_manager.py
@@ -0,0 +1,7 @@
+import pytest
+from azure.eventhub.extensions.checkpointstoretable import TableCheckpointStore
+
+
+def test_constructor():
+    client = TableCheckpointStore()
+    assert client is not None
