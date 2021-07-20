e8368af21ac221108f0b43f8858c5b4f59d952e6
Swathi Pillalamarri <swathip@microsoft.com>
2021-07-20 09:40:58 -0700

    add contains

--- sdk/eventhub/azure-eventhub/azure/eventhub/_mixin.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_mixin.py
@@ -41,6 +41,9 @@ class DictMixin(object):
         """Compare objects by comparing all attributes."""
         return not self.__eq__(other)
 
+    def __contains__(self, key):
+        return key in self.__dict__
+
     def __str__(self):
         # type: () -> str
         return str({k: v for k, v in self.__dict__.items() if not k.startswith("_")})
