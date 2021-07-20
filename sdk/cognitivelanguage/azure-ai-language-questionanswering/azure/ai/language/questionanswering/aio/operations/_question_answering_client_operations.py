a81df8b80715d9fa0677d26030de31321ed82cb4
antisch <antisch@microsoft.com>
2021-07-20 09:50:02 -0700

    Fixed docstring

--- sdk/cognitivelanguage/azure-ai-language-questionanswering/azure/ai/language/questionanswering/aio/operations/_question_answering_client_operations.py
+++ sdk/cognitivelanguage/azure-ai-language-questionanswering/azure/ai/language/questionanswering/aio/operations/_question_answering_client_operations.py
@@ -38,11 +38,11 @@ class QuestionAnsweringClientOperationsMixin:
     ) -> "_models.KnowledgeBaseAnswers":
         """Answers the specified question using your knowledge base.
 
-        :keyword project_name: The name of the project to use.
-        :paramtype project_name: str
         :param knowledge_base_query_options: Post body of the request.
         :type knowledge_base_query_options:
          ~azure.ai.language.questionanswering.models.KnowledgeBaseQueryOptions
+        :keyword project_name: The name of the project to use.
+        :paramtype project_name: str
         :keyword deployment_name: The name of the specific deployment of the project to use.
         :paramtype deployment_name: str
         :keyword callable cls: A custom type or function that will be passed the direct response
