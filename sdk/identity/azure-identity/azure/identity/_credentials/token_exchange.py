c0e85fc63071c77616fc0d567bea4f82d039b824
Charles Lowell <chlowe@microsoft.com>
2021-07-21 10:08:52 -0700

    TokenExchangeCredential (also internal)

---
+++ sdk/identity/azure-identity/azure/identity/_credentials/token_exchange.py
@@ -0,0 +1,68 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import os
+import time
+from typing import TYPE_CHECKING
+
+from .client_assertion import ClientAssertionCredential
+from .. import CredentialUnavailableError
+from .._constants import EnvironmentVariables
+
+if TYPE_CHECKING:
+    # pylint:disable=unused-import,ungrouped-imports
+    from typing import Any
+    from azure.core.credentials import AccessToken
+
+
+class TokenFileMixin(object):
+    def __init__(self):
+        # type: () -> None
+        super(TokenFileMixin, self).__init__()
+        self._jwt = ""
+        self._last_read_time = 0
+        self._token_file_path = os.environ[EnvironmentVariables.TOKEN_FILE_PATH]
+
+    def get_service_account_token(self):
+        # type: () -> str
+        now = int(time.time())
+        if now - self._last_read_time > 300:
+            with open(self._token_file_path) as f:
+                self._jwt = f.read()
+            self._last_read_time = now
+        return self._jwt
+
+
+class TokenExchangeCredential(TokenFileMixin):
+    def __init__(self, **kwargs):
+        # type: (**Any) -> None
+        super(TokenExchangeCredential, self).__init__()
+        self._available = all(os.environ.get(var) for var in EnvironmentVariables.TOKEN_EXCHANGE_VARS)
+        if self._available:
+            self._credential = ClientAssertionCredential(
+                os.environ[EnvironmentVariables.AZURE_TENANT_ID],
+                os.environ[EnvironmentVariables.AZURE_CLIENT_ID],
+                self.get_service_account_token,
+                **kwargs
+            )
+
+    def __enter__(self):
+        if self._credential:
+            self._credential.__enter__()
+        return self
+
+    def __exit__(self, *args):
+        if self._credential:
+            self._credential.__exit__(*args)
+
+    def close(self):
+        # type: () -> None
+        """Close the credential's transport session."""
+        self.__exit__()
+
+    def get_token(self, *scopes, **kwargs):
+        # type: (*str, **Any) -> AccessToken
+        if not self._available:
+            raise CredentialUnavailableError(message="Token exchange configuration not found in environment")
+        return self._credential.get_token(*scopes, **kwargs)
---
+++ sdk/identity/azure-identity/azure/identity/aio/_credentials/token_exchange.py
@@ -0,0 +1,43 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import os
+from typing import TYPE_CHECKING
+
+from .client_assertion import ClientAssertionCredential
+from .._internal import AsyncContextManager
+from ... import CredentialUnavailableError
+from ..._constants import EnvironmentVariables
+from ..._credentials.token_exchange import TokenFileMixin
+
+if TYPE_CHECKING:
+    # pylint:disable=unused-import,ungrouped-imports
+    from typing import Any
+    from azure.core.credentials import AccessToken
+
+
+class TokenExchangeCredential(AsyncContextManager, TokenFileMixin):
+    def __init__(self, **kwargs: "Any") -> None:
+        super().__init__()
+        self._available = all(os.environ.get(var) for var in EnvironmentVariables.TOKEN_EXCHANGE_VARS)
+        if self._available:
+            self._credential = ClientAssertionCredential(
+                os.environ[EnvironmentVariables.AZURE_TENANT_ID],
+                os.environ[EnvironmentVariables.AZURE_CLIENT_ID],
+                self.get_service_account_token,
+                **kwargs
+            )
+
+    async def __aenter__(self):
+        if self._credential:
+            await self._credential.__aenter__()
+        return self
+
+    async def close(self) -> None:
+        await self._credential.close()
+
+    async def get_token(self, *scopes: str, **kwargs: "Any") -> "AccessToken":
+        if not self._available:
+            raise CredentialUnavailableError(message="Token exchange configuration not found in environment")
+        return await self._credential.get_token(*scopes, **kwargs)
