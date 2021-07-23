ca44e1808f4a2415a5224960a4c831f3a55411bc
Xiang Yan <xiangsjtu@gmail.com>
2021-07-23 09:24:44 -0700

    deprecate loop parameter (#19927)
    * deprecate loop parameter
    
    * update

--- sdk/core/azure-core/azure/core/pipeline/transport/_aiohttp.py
+++ sdk/core/azure-core/azure/core/pipeline/transport/_aiohttp.py
@@ -23,6 +23,7 @@
 # IN THE SOFTWARE.
 #
 # --------------------------------------------------------------------------
+import sys
 from typing import Any, Optional, AsyncIterator as AsyncIteratorType
 from collections.abc import AsyncIterator
 try:
@@ -58,7 +59,6 @@ class AioHttpTransport(AsyncHttpTransport):
     Fully asynchronous implementation using the aiohttp library.
 
     :param session: The client session.
-    :param loop: The event loop.
     :param bool session_owner: Session owner. Defaults True.
 
     :keyword bool use_env_settings: Uses proxy settings from environment. Defaults to True.
@@ -73,6 +73,8 @@ class AioHttpTransport(AsyncHttpTransport):
             :caption: Asynchronous transport with aiohttp.
     """
     def __init__(self, *, session: Optional[aiohttp.ClientSession] = None, loop=None, session_owner=True, **kwargs):
+        if loop and sys.version_info >= (3, 10):
+            raise ValueError("Starting with Python 3.10, asyncio doesn’t support loop as a parameter anymore")
         self._loop = loop
         self._session_owner = session_owner
         self.session = session
