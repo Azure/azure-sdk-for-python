9bca97afccfde706b0c1350f2a70b1616c6e7376
Josue Garcia <t-jgarcia@microsoft.com>
2021-07-26 00:07:16 -0400

    environment

--- sdk/eventhub/azure-eventhub-checkpointstoretable/tests/test_storage_table_partition_manager.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/tests/test_storage_table_partition_manager.py
@@ -3,8 +3,9 @@ from azure.eventhub.extensions.checkpointstoretable import TableCheckpointStore
 from azure.data.tables import TableServiceClient
 import uuid
 import warnings
+import os
 
-STORAGE_CONN_STR = ["Azure storage connection str"]
+STORAGE_CONN_STR = os.environ["AZURE_STORAGE_CONN_STR"]
 
 def get_live_storage_table_client(storage_connection_str):
     try:
