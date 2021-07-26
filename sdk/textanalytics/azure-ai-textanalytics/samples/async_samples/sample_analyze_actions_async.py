2f4b85b6fa782f38b5a9247b5bf68506e0af3556
Krista Pratico <krpratic@microsoft.com>
2021-07-26 15:34:43 -0700

    target swagger in repo and fix sample docs whitespace

--- sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_analyze_actions_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_analyze_actions_async.py
@@ -50,15 +50,15 @@ async def sample_analyze_async():
     )
 
     documents = [
-        'We went to Contoso Steakhouse located at midtown NYC last week for a dinner party, and we adore the spot!'\
-        'They provide marvelous food and they have a great menu. The chief cook happens to be the owner (I think his name is John Doe)'\
-        'and he is super nice, coming out of the kitchen and greeted us all.'\
+        'We went to Contoso Steakhouse located at midtown NYC last week for a dinner party, and we adore the spot! '
+        'They provide marvelous food and they have a great menu. The chief cook happens to be the owner (I think his name is John Doe) '
+        'and he is super nice, coming out of the kitchen and greeted us all.'
         ,
 
-        'We enjoyed very much dining in the place!'\
-        'The Sirloin steak I ordered was tender and juicy, and the place was impeccably clean. You can even pre-order from their'\
-        'online menu at www.contososteakhouse.com, call 312-555-0176 or send email to order@contososteakhouse.com!'\
-        'The only complaint I have is the food didn\'t come fast enough. Overall I highly recommend it!'\
+        'We enjoyed very much dining in the place! '
+        'The Sirloin steak I ordered was tender and juicy, and the place was impeccably clean. You can even pre-order from their '
+        'online menu at www.contososteakhouse.com, call 312-555-0176 or send email to order@contososteakhouse.com! '
+        'The only complaint I have is the food didn\'t come fast enough. Overall I highly recommend it!'
     ]
 
     async with text_analytics_client:
--- sdk/textanalytics/azure-ai-textanalytics/samples/sample_analyze_actions.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/sample_analyze_actions.py
@@ -49,15 +49,15 @@ def sample_analyze_actions():
     )
 
     documents = [
-        'We went to Contoso Steakhouse located at midtown NYC last week for a dinner party, and we adore the spot!'\
-        'They provide marvelous food and they have a great menu. The chief cook happens to be the owner (I think his name is John Doe)'\
-        'and he is super nice, coming out of the kitchen and greeted us all.'\
+        'We went to Contoso Steakhouse located at midtown NYC last week for a dinner party, and we adore the spot! '
+        'They provide marvelous food and they have a great menu. The chief cook happens to be the owner (I think his name is John Doe) '
+        'and he is super nice, coming out of the kitchen and greeted us all.'
         ,
 
-        'We enjoyed very much dining in the place!'\
-        'The Sirloin steak I ordered was tender and juicy, and the place was impeccably clean. You can even pre-order from their'\
-        'online menu at www.contososteakhouse.com, call 312-555-0176 or send email to order@contososteakhouse.com!'\
-        'The only complaint I have is the food didn\'t come fast enough. Overall I highly recommend it!'\
+        'We enjoyed very much dining in the place! '
+        'The Sirloin steak I ordered was tender and juicy, and the place was impeccably clean. You can even pre-order from their '
+        'online menu at www.contososteakhouse.com, call 312-555-0176 or send email to order@contososteakhouse.com! '
+        'The only complaint I have is the food didn\'t come fast enough. Overall I highly recommend it!'
     ]
 
     poller = text_analytics_client.begin_analyze_actions(
