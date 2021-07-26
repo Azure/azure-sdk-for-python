fc36f6689ddc32d7c54b22ee4586934327510917
Josue Garcia <t-jgarcia@microsoft.com>
2021-07-26 00:51:34 -0400

    redo

--- sdk/eventhub/azure-eventhub-checkpointstoretable/tests/test_storage_table_partition_manager.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/tests/test_storage_table_partition_manager.py
@@ -9,7 +9,6 @@ STORAGE_CONN_STR = ["Azure storage connection str"]
 
 def get_live_storage_table_client(storage_connection_str):
     try:
-        storage_connection_str
         table_name = chr(random.randint(ord('a'), ord('z'))) +(uuid.uuid4().hex)
         table_service_client = TableServiceClient.from_connection_string(storage_connection_str)
         table_service_client.create_table_if_not_exists(table_name)
