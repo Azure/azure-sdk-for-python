a13b24835895632a2bc961816122b919ff0993c6
iscai-msft <iscai@microsoft.com>
2021-07-27 12:41:13 -0400

    add chardet dep to setup.py

--- sdk/core/azure-core/setup.py
+++ sdk/core/azure-core/setup.py
@@ -68,6 +68,7 @@ setup(
     install_requires=[
         'requests>=2.18.4',
         'six>=1.11.0',
+        'chardet>=3.0.2,<5',
     ],
     extras_require={
         ":python_version<'3.0'": ['azure-nspkg'],
