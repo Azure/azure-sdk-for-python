3f846b1209879fc71e5f0b0b97134a52ca26dcfc
SDKAuto <sdkautomation@microsoft.com>
2021-07-28 13:34:19 +0000

    CodeGen from PR 14545 in Azure/azure-rest-api-specs [Hub Generated] Review request for Microsoft.StorageCache to add version stable/2021-05-01 (#14545)
    * Adds base for updating Microsoft.StorageCache from version stable/2021-03-01 to version 2021-05-01
    
    * Updates readme
    
    * Updates API version in new specs and examples
    
    * 2021-05-01 API Changes
    
    * prettier fix
    
    * Lintdiff fix
    
    * Add create only fields
    
    * Create only tag for CacheIdentity
    
    * Create only tag for CacheIdentity
    
    * Remove create only for key rotation
    
    * Fix s360 errors
    
    * Fix read only
    
    * Revert package-lock.json
    
    * Revert package-lock.json

--- sdk/storage/azure-mgmt-storagecache/azure/mgmt/storagecache/aio/operations/_usage_models_operations.py
+++ sdk/storage/azure-mgmt-storagecache/azure/mgmt/storagecache/aio/operations/_usage_models_operations.py
@@ -43,7 +43,7 @@ class UsageModelsOperations:
 
     def list(
         self,
-        **kwargs
+        **kwargs: Any
     ) -> AsyncIterable["_models.UsageModelsResult"]:
         """Get the list of Cache Usage Models available to this subscription.
 
@@ -57,7 +57,7 @@ class UsageModelsOperations:
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2021-03-01"
+        api_version = "2021-05-01"
         accept = "application/json"
 
         def prepare_request(next_link=None):
