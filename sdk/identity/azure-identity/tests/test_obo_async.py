c46044729324fb1352e32d34af69c4d0e026506b
Charles Lowell <chlowe@microsoft.com>
2021-07-29 13:59:23 -0700

    tests

--- sdk/identity/azure-identity/tests/test_obo_async.py
+++ sdk/identity/azure-identity/tests/test_obo_async.py
@@ -3,34 +3,24 @@
 # Licensed under the MIT License.
 # ------------------------------------
 import os
+from urllib.parse import urlparse
+from unittest.mock import Mock
 
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock  # type: ignore
-
-from azure_devtools.scenario_tests import RecordingProcessor
 from azure.core.pipeline.policies import ContentDecodePolicy, SansIOHTTPPolicy
-from azure.identity import CredentialUnavailableError, OnBehalfOfCredential, UserAssertion, UsernamePasswordCredential
+from azure.identity import CredentialUnavailableError, UserAssertion, UsernamePasswordCredential
+from azure.identity.aio import OnBehalfOfCredential
 from azure.identity._internal.user_agent import USER_AGENT
-from azure.mgmt.resource import SubscriptionClient
+from azure.mgmt.resource.subscriptions.aio import SubscriptionClient
 import pytest
-from six.moves.urllib_parse import urlparse
 
 from helpers import build_aad_response, build_id_token, FAKE_CLIENT_ID, get_discovery_response, mock_response
 from recorded_test_case import RecordedTestCase
-
-
-class SubscriptionListRemover(RecordingProcessor):
-    def process_response(self, response):
-        if "/subscriptions/" in response["body"]["string"]:
-            response["body"]["string"] = '{"value":[]}'
-        return response
+from test_obo import SubscriptionListRemover
 
 
 class RecordedTests(RecordedTestCase):
     def __init__(self, *args, **kwargs):
-        super(RecordedTests, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
         if self.is_live:
             missing_variables = [
@@ -70,7 +60,8 @@ class RecordedTests(RecordedTestCase):
                 "username": "username",
             }
 
-    def test_obo(self):
+    @RecordedTestCase.await_prepared_test
+    async def test_obo(self):
         client_id = self.obo_settings["client_id"]
         client_secret = self.obo_settings["client_secret"]
         tenant_id = self.obo_settings["tenant_id"]
@@ -82,8 +73,9 @@ class RecordedTests(RecordedTestCase):
 
         # wrap a real client to avoid showing a specific one in the snippet and thus implying the client type matters
         class AzureClient(SubscriptionClient):
-            def get_resource(self):
-                list(self.subscriptions.list())
+            async def get_resource(self):
+                async for _ in self.subscriptions.list():
+                    pass
 
         # [START snippet]
         credential = OnBehalfOfCredential(tenant_id, client_id, client_secret)
@@ -92,25 +84,23 @@ class RecordedTests(RecordedTestCase):
         # typically the assertion is an access token from an incoming HTTP request from the user
         with UserAssertion(user_token):
             # all client calls in this block are authenticated on behalf of the same user
-            client.get_resource()
+            await client.get_resource()
         # [END snippet]
 
 
-def test_caching():
+@pytest.mark.asyncio
+async def test_caching():
     client_id = "client-id"
     scope = "scope"
     tenant = "tenant-id"
 
-    def send(request, **_):
-        assert request.headers["User-Agent"] == USER_AGENT
+    async def send(request, **_):
         parsed = urlparse(request.url)
         authority = "https://{}/{}".format(parsed.netloc, tenant)
-        if "/oauth2/v2.0/token" not in parsed.path:
-            return get_discovery_response(authority)
         return mock_response(
             json_payload=build_aad_response(
                 access_token=request.body["assertion"],
-                refresh_token="***",
+                refresh_token=request.body["assertion"],
                 id_token=build_id_token(aud=client_id, iss=authority),
             )
         )
@@ -119,34 +109,27 @@ def test_caching():
     credential = OnBehalfOfCredential(tenant, client_id, "secret", transport=transport)
 
     with UserAssertion("A"):
-        token = credential.get_token(scope)
+        token = await credential.get_token(scope)
         assert token.token == "A"
         requests_sent = transport.send.call_count  # exact request count is up to msal
         assert requests_sent > 0
 
         # credential should return a cached token
-        token = credential.get_token(scope)
+        token = await credential.get_token(scope)
         assert token.token == "A"
         assert transport.send.call_count == requests_sent
 
     with UserAssertion("B"):
-        token = credential.get_token(scope)
+        token = await credential.get_token(scope)
     assert token.token == "B"
     assert transport.send.call_count > requests_sent
 
 
-def test_nested_assertion():
-    with UserAssertion("A"):
-        for assertion in ("A", "B"):
-            with pytest.raises(ValueError):
-                with UserAssertion(assertion):
-                    pass
-
-
-def test_requires_assertion():
+@pytest.mark.asyncio
+async def test_requires_assertion():
     """The credential should raise CredentialUnavailableError when no user assertion is set"""
     with pytest.raises(CredentialUnavailableError):
-        OnBehalfOfCredential("tenant-id", "client-id", "secret").get_token("scope")
+        await OnBehalfOfCredential("tenant-id", "client-id", "secret").get_token("scope")
 
 
 def test_tenant_id_validation():
@@ -160,18 +143,20 @@ def test_tenant_id_validation():
             OnBehalfOfCredential(tenant, "client-id", "secret")
 
 
-def test_no_scopes():
+@pytest.mark.asyncio
+async def test_no_scopes():
     """The credential should raise ValueError when get_token is called with no scopes"""
     credential = OnBehalfOfCredential("tenant-id", "client-id", "client-secret")
     with pytest.raises(ValueError):
         with UserAssertion("..."):
-            credential.get_token()
+            await credential.get_token()
 
 
-def test_policies_configurable():
+@pytest.mark.asyncio
+async def test_policies_configurable():
     policy = Mock(spec_set=SansIOHTTPPolicy, on_request=Mock(), on_exception=lambda _: False)
 
-    def send(request, **_):
+    async def send(request, **_):
         parsed = urlparse(request.url)
         tenant = parsed.path.split("/")[1]
         if "/oauth2/v2.0/token" not in parsed.path:
@@ -182,5 +167,20 @@ def test_policies_configurable():
         "tenant-id", "client-id", "client-secret", policies=[ContentDecodePolicy(), policy], transport=Mock(send=send)
     )
     with UserAssertion("..."):
-        credential.get_token("scope")
+        await credential.get_token("scope")
     assert policy.on_request.called
+
+
+@pytest.mark.asyncio
+async def test_user_agent():
+    async def send(request, **_):
+        assert request.headers["User-Agent"] == USER_AGENT
+        parsed = urlparse(request.url)
+        tenant = parsed.path.split("/")[1]
+        if "/oauth2/v2.0/token" not in parsed.path:
+            return get_discovery_response("https://{}/{}".format(parsed.netloc, tenant))
+        return mock_response(json_payload=build_aad_response(access_token="***"))
+
+    credential = OnBehalfOfCredential("tenant-id", "client-id", "client-secret", transport=Mock(send=send))
+    with UserAssertion("..."):
+        await credential.get_token("scope")
