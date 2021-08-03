7f9234502dee42915270597955f8664593d72b16
swathipil <76007337+swathipil@users.noreply.github.com>
2021-08-03 09:33:10 -0700

    [EventHubs] update import DictMixin from _mixin (#20041)
    * update import
    
    * update imports

--- sdk/eventhub/azure-eventhub/azure/eventhub/_connection_string_parser.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_connection_string_parser.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-from ._common import DictMixin
+from ._mixin import DictMixin
 from ._client_base import _parse_conn_str
 
 
