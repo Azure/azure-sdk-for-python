ed7b7fc4654f5f66f890777d1c0f42ae47719372
Krista Pratico <krpratic@microsoft.com>
2021-08-03 09:51:25 -0700

    add mock tests

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_logging_async.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_logging_async.py
@@ -7,8 +7,16 @@
 
 import logging
 import pytest
+import json
+import asyncio
+import functools
+try:
+    from unittest import mock
+except ImportError:  # python < 3.3
+    import mock  # type: ignore
 from azure.ai.formrecognizer.aio import FormRecognizerClient, FormTrainingClient
 from azure.core.credentials import AzureKeyCredential
+from azure.core.exceptions import HttpResponseError
 from preparers import FormRecognizerPreparer
 from asynctestcase import AsyncFormRecognizerTest
 
@@ -20,6 +28,24 @@ class MockHandler(logging.Handler):
 
     def emit(self, record):
         self.messages.append(record)
+
+
+def get_completed_future(result=None):
+    future = asyncio.Future()
+    future.set_result(result)
+    return future
+
+
+def wrap_in_future(fn):
+    """Return a completed Future whose result is the return of fn.
+    Added to simplify using unittest.Mock in async code. Python 3.8's AsyncMock would be preferable.
+    """
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        result = fn(*args, **kwargs)
+        return get_completed_future(result)
+    return wrapper
 
 
 class TestLogging(AsyncFormRecognizerTest):
@@ -64,3 +90,36 @@ class TestLogging(AsyncFormRecognizerTest):
                     assert message.message.find("REDACTED") != -1
                 else:
                     assert message.message.find("REDACTED") == -1
+
+    @FormRecognizerPreparer()
+    async def test_mock_quota_exceeded_403(self, formrecognizer_test_endpoint, formrecognizer_test_api_key):
+
+        response = mock.Mock(status_code=403, headers={"Retry-After": 186688}, reason="Bad Request")
+        response.text = lambda encoding=None: json.dumps(
+            {"error": {"code": "403", "message": "Out of call volume quota for FormRecognizer F0 pricing tier. "
+            "Please retry after 1 day. To increase your call volume switch to a paid tier."}}
+        )
+        transport = mock.Mock(send=wrap_in_future(lambda request, **kwargs: response))
+
+        client = FormRecognizerClient(formrecognizer_test_endpoint, AzureKeyCredential(formrecognizer_test_api_key), transport=transport)
+
+        with pytest.raises(HttpResponseError) as e:
+            poller = await client.begin_recognize_receipts_from_url(self.receipt_url_jpg)
+        assert e.value.status_code == 403
+        assert e.value.error.message == 'Out of call volume quota for FormRecognizer F0 pricing tier. Please retry after 1 day. To increase your call volume switch to a paid tier.'
+
+    @FormRecognizerPreparer()
+    async def test_mock_quota_exceeded_429(self, formrecognizer_test_endpoint, formrecognizer_test_api_key):
+        response = mock.Mock(status_code=429, headers={"Retry-After": 186688}, reason="Bad Request")
+        response.text = lambda encoding=None: json.dumps(
+            {"error": {"code": "429", "message": "Out of call volume quota for FormRecognizer F0 pricing tier. "
+            "Please retry after 1 day. To increase your call volume switch to a paid tier."}}
+        )
+        transport = mock.Mock(send=wrap_in_future(lambda request, **kwargs: response))
+
+        client = FormRecognizerClient(formrecognizer_test_endpoint, AzureKeyCredential(formrecognizer_test_api_key), transport=transport)
+
+        with pytest.raises(HttpResponseError) as e:
+            poller = await client.begin_recognize_receipts_from_url(self.receipt_url_jpg)
+        assert e.value.status_code == 429
+        assert e.value.error.message == 'Out of call volume quota for FormRecognizer F0 pricing tier. Please retry after 1 day. To increase your call volume switch to a paid tier.'
