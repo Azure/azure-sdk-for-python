19487ff9f1790033bcc9e3354b7bdee309a5bc71
Krista Pratico <krpratic@microsoft.com>
2021-08-03 11:27:24 -0700

    fix samples with renamed params (#20063)

--- sdk/translation/azure-ai-translation-document/samples/async_samples/sample_list_translations_with_filters_async.py
+++ sdk/translation/azure-ai-translation-document/samples/async_samples/sample_list_translations_with_filters_async.py
@@ -48,7 +48,7 @@ async def sample_list_translations_with_filters_async():
         start = datetime(2021, 4, 12)
         end = datetime(2021, 4, 14)
         statuses = ["Cancelled", "Failed"]
-        order_by = ["createdDateTimeUtc desc"]
+        order_by = ["created_on desc"]
         results_per_page = 2
         skip = 3
 
