a46f6774dd47cb1f7052bc537385a8dbbd90511a
Lu Chen <luc@microsoft.com>
2021-08-04 13:55:14 -0700

    fix api view

--- sdk/communication/azure-communication-chat/azure/communication/chat/_chat_thread_client.py
+++ sdk/communication/azure-communication-chat/azure/communication/chat/_chat_thread_client.py
@@ -289,8 +289,7 @@ class ChatThreadClient(object):
         :paramtype chat_message_type: Union[str, ~azure.communication.chat.ChatMessageType]
         :keyword str sender_display_name: The display name of the message sender. This property is used to
             populate sender name for push notifications.
-        :keyword metadata : Message metadata.
-        :paramtype metadata: Dict[str, str]
+        :keyword dict[str, str] metadata: Message metadata.
         :return: SendChatMessageResult
         :rtype: ~azure.communication.chat.SendChatMessageResult
         :raises: ~azure.core.exceptions.HttpResponseError, ValueError
@@ -416,8 +415,7 @@ class ChatThreadClient(object):
         :type message_id: str
         :param content: Chat message content.
         :type content: str
-        :keyword metadata : Message metadata.
-        :paramtype metadata: Dict[str, str]
+        :keyword dict[str, str] metadata: Message metadata.
         :return: None
         :rtype: None
         :raises: ~azure.core.exceptions.HttpResponseError, ValueError
