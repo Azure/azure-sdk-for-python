877f1d85ca16494e38d2112754c7dfb8eb4584ae
Josue Garcia <t-jgarcia@microsoft.com>
2021-08-03 22:54:38 -0400

    new

--- sdk/eventhub/azure-eventhub-checkpointstoretable/azure/eventhub/extensions/checkpointstoretable/_tablestoragecs.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/azure/eventhub/extensions/checkpointstoretable/_tablestoragecs.py
@@ -2,10 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-import datetime
 import time
 import logging
 import calendar
+import datetime
 import dateutil.parser
 from azure.data.tables import TableClient, UpdateMode
 from azure.core import MatchConditions
