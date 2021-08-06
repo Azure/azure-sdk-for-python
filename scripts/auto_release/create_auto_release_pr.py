440fac1dab39bfa427b9e732124a90e3268a6119
BigCat20196 <1095260342@qq.com>
2021-08-06 14:50:10 +0800

    Update create_auto_release_pr.py

--- scripts/auto_release/create_auto_release_pr.py
+++ scripts/auto_release/create_auto_release_pr.py
@@ -2,32 +2,19 @@ import os
 
 from ghapi.all import GhApi
 
-NEW_BRANCH = os.getenv('NEW_BRANCH')
-TARGET_BRANCH = os.getenv('TARGET_BRANCH')
-ISSUE_LINK = os.getenv('ISSUE_LINK')
-TEST_RESULT = os.getenv('TEST_RESULT')
-
-# Generate PR for auto release SDK
-def create_auto_release_pr(api):
-    pr_title = "[AutoRelease] {}(Do not merge)".format(NEW_BRANCH)
-    pr_head = "{}:{}".format(os.getenv('USR_NAME'), NEW_BRANCH)
-    pr_base = TARGET_BRANCH
-    pr_body = TEST_RESULT
-    res_create = api.pulls.create(pr_title, pr_head, pr_base, pr_body)
-
-    return res_create.number
-
-# Add issue link on PR
-def add_comment(api, pr_number):
-    api.issues.create_comment(issue_number=pr_number, body='issue link:{}'.format(ISSUE_LINK))
-
-
 def main():
+    # Generate PR for auto release SDK
     api = GhApi(owner='Azure', repo='azure-sdk-for-python', token=os.getenv('USR_TOKEN'))
-    pr_number = create_auto_release_pr(api)
+    pr_title = "[AutoRelease] {}(Do not merge)".format(os.getenv('NEW_BRANCH'))
+    pr_head = "{}:{}".format(os.getenv('USR_NAME'), os.getenv('NEW_BRANCH'))
+    pr_base = os.getenv('TARGET_BRANCH')
+    pr_body = "{} {}".format(os.getenv('TEST_RESULT'), os.getenv('ISSUE_LINK'))
+    res_create = api.pulls.create(pr_title, pr_head, pr_base, pr_body)
+    pr_number = res_create.number
 
+    # Add issue link on PR
     api = GhApi(owner='Azure', repo='azure-sdk-for-python', token=os.getenv('UPDATE_TOKEN'))
-    add_comment(api, pr_number)
+    api.issues.create_comment(issue_number=pr_number, body='issue link:{}'.format(os.getenv('ISSUE_LINK')))
 
 if __name__ == '__main__':
     main()
