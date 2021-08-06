9ca5f21390293193fe1214f0c89f436bd85635e8
BigCat20196 <1095260342@qq.com>
2021-08-06 10:40:44 +0800

    add_comment

--- scripts/auto_release/create_auto_release_pr.py
+++ scripts/auto_release/create_auto_release_pr.py
@@ -5,20 +5,21 @@ from ghapi.all import GhApi
 NEW_BRANCH = os.getenv('NEW_BRANCH')
 TARGET_BRANCH = os.getenv('TARGET_BRANCH')
 ISSUE_LINK = os.getenv('ISSUE_LINK')
+TEST_RESULT = os.getenv('TEST_RESULT')
 
 # Generate PR for auto release SDK
 def create_auto_release_pr(api):
     pr_title = "[AutoRelease] {}(Do not merge)".format(NEW_BRANCH)
     pr_head = "{}:{}".format(os.getenv('USR_NAME'), NEW_BRANCH)
     pr_base = TARGET_BRANCH
-    pr_body = "issue link:"
+    pr_body = TEST_RESULT
     res_create = api.pulls.create(pr_title, pr_head, pr_base, pr_body)
 
     return res_create.number
 
-# Change the PR body to the issue link
-def update_auto_release_pr(api, pr_number):
-    api.pulls.update(pull_number=pr_number, body='issue link:{}'.format(ISSUE_LINK))
+# Add issue link on PR
+def add_comment(api, pr_number):
+    api.issues.create_comment(pull_number=pr_number, body='issue link:{}'.format(ISSUE_LINK))
 
 
 def main():
@@ -26,7 +27,7 @@ def main():
     pr_number = create_auto_release_pr(api)
 
     api = GhApi(owner='Azure', repo='azure-sdk-for-python', token=os.getenv('UPDATE_TOKEN'))
-    update_auto_release_pr(api, pr_number)
+    add_comment(api, pr_number)
 
 if __name__ == '__main__':
     main()
