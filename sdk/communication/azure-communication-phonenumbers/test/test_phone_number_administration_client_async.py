3a661c249f4edb759b09ae78a64b07ebc12c95e4
megheshpatil <58992144+megheshpatil@users.noreply.github.com>
2021-08-09 14:11:23 -0700

    Update test_phone_number_administration_client_async.py
    Reverted back to the original changes

--- sdk/communication/azure-communication-phonenumbers/test/test_phone_number_administration_client_async.py
+++ sdk/communication/azure-communication-phonenumbers/test/test_phone_number_administration_client_async.py
@@ -264,5 +264,5 @@ class PhoneNumbersClientTestAsync(AsyncCommunicationTestCase):
                     polling = True
                 )
         
-        assert str(ex.value.status_code) == "403"
+        assert str(ex.value.status_code) == "404"
         assert ex.value.message is not None
