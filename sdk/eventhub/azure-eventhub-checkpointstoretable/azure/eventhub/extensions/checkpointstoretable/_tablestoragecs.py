f9af61648f16f4b6538c6bd023bf5ca66b8c526b
Josue Garcia <t-jgarcia@microsoft.com>
2021-08-11 19:43:39 -0400

    update

--- sdk/eventhub/azure-eventhub-checkpointstoretable/azure/eventhub/extensions/checkpointstoretable/_tablestoragecs.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/azure/eventhub/extensions/checkpointstoretable/_tablestoragecs.py
@@ -143,7 +143,7 @@ class TableCheckpointStore(CheckpointStore):
             )
             ownership['last_modified_time'] = _to_timestamp(updated_entity.metadata.get('timestamp'))
             return ownership
-        except ResourceNotFoundError:
+        except (ResourceNotFoundError, ValueError):
             metadata = self._table_client.create_entity(
             entity=ownership_entity,
             headers={'Prefer': 'return-content'}, **kwargs
