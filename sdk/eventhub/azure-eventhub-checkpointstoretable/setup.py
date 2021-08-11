b148f1ca7cb13f334d77e668f0082c0ed156ce13
Josue Garcia <t-jgarcia@microsoft.com>
2021-08-11 16:22:51 -0400

    update

--- sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
@@ -1,15 +1,25 @@
+
 from setuptools import setup, find_packages
 import os
 from io import open
 import re
 
+# example setup.py Feel free to copy the entire "azure-template" folder into a package folder named
+# with "azure-<yourpackagename>". Ensure that the below arguments to setup() are updated to reflect
+# your package.
+
+# this setup.py is set up in a specific way to keep the azure* and azure-mgmt-* namespaces WORKING all the way
+# up from python 2.7. Reference here: https://github.com/Azure/azure-sdk-for-python/wiki/Azure-packaging
 
 PACKAGE_NAME = "azure-eventhub-checkpointstoretable"
 PACKAGE_PPRINT_NAME = "Event Hubs checkpointer implementation with Azure Table Storage"
 
-package_folder_path = "azure/eventhub/extensions/checkpointstoretable"
+# a-b-c => a/b/c
+package_folder_path = PACKAGE_NAME.replace("-", "/")
+# a-b-c => a.b.c
+namespace_name = PACKAGE_NAME.replace("-", ".")
 
-namespace_name = "azure.eventhub.extensions.checkpointstoretable"
+package_folder_path = "azure/eventhub/extensions/checkpointstoretable"
 
 # Version extraction inspired from 'requests'
 with open(os.path.join(package_folder_path, "_version.py"), "r") as fd:
@@ -17,17 +27,15 @@ with open(os.path.join(package_folder_path, "_version.py"), "r") as fd:
 if not version:
     raise RuntimeError("Cannot find version information")
 
-with open('README.md') as f:
-    readme = f.read()
-with open('CHANGELOG.md') as f:
-    changelog = f.read()
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name=PACKAGE_NAME,
     version=version,
     description="Microsoft Azure {} Client Library for Python".format(PACKAGE_PPRINT_NAME),
     # ensure that these are updated to reflect the package owners' information
-    long_description=readme + '\n\n' + changelog,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Azure/azure-sdk-for-python",
     author="Microsoft Corporation",
@@ -49,27 +57,18 @@ setup(
     packages=find_packages(
         exclude=[
             "tests",
-            "samples",
             # Exclude packages that will be covered by PEP420 or nspkg
             # This means any folder structure that only consists of a __init__.py.
             # For example, for storage, this would mean adding 'azure.storage'
             # in addition to the default 'azure' that is seen here.
             "azure",
-            "azure.eventhub",
-            "azure.eventhub.extensions",
         ]
     ),
     install_requires=[
-        # dependencies for the vendored storage tables
         "azure-core<2.0.0,>=1.14.0",
-        "msrest>=0.6.19",
-        # end of dependencies for the vendored storage tables
-        'azure-eventhub<6.0.0,>=5.0.0',
     ],
     extras_require={
-        ":python_version<'3.0'": ['futures', 'azure-data-nspkg<2.0.0,>=1.0.0'],
-        ":python_version<'3.4'": ['enum34>=1.0.4'],
-        ":python_version<'3.5'": ["typing"],
+        ":python_version<'3.0'": ["azure-nspkg"],
     },
     project_urls={
         "Bug Reports": "https://github.com/Azure/azure-sdk-for-python/issues",
