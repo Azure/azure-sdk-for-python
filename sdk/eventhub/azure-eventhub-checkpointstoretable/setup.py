d6b450e5f96082300a8cafba89e31214b6f2faf6
Josue Garcia <t-jgarcia@microsoft.com>
2021-08-10 23:27:46 -0400

    update

--- sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
@@ -1,76 +1,76 @@
-import re
-import os.path
-import sys
+from setuptools import setup, find_packages
+import os
 from io import open
-from setuptools import find_packages, setup
+import re
 
+# example setup.py Feel free to copy the entire "azure-template" folder into a package folder named
+# with "azure-<yourpackagename>". Ensure that the below arguments to setup() are updated to reflect
+# your package.
 
-# Change the PACKAGE_NAME only to change folder and different name
-PACKAGE_NAME = "azure-eventhub-checkpointstoreblob"
-PACKAGE_PPRINT_NAME = "Event Hubs checkpointer implementation with Blob Storage"
+# this setup.py is set up in a specific way to keep the azure* and azure-mgmt-* namespaces WORKING all the way
+# up from python 2.7. Reference here: https://github.com/Azure/azure-sdk-for-python/wiki/Azure-packaging
 
-package_folder_path = "azure/eventhub/extensions/checkpointstoreblob"
-namespace_name = "azure.eventhub.extensions.checkpointstoreblob"
+PACKAGE_NAME = "azure-eventhub-checkpointstoretable"
+PACKAGE_PPRINT_NAME = "Event Hubs checkpointer implementation with Azure Table Storage"
+
+# a-b-c => a/b/c
+package_folder_path = PACKAGE_NAME.replace("-", "/")
+# a-b-c => a.b.c
+namespace_name = PACKAGE_NAME.replace("-", ".")
+
+package_folder_path = "azure/eventhub/extensions/checkpointstoretable"
 
 # Version extraction inspired from 'requests'
-with open(os.path.join(package_folder_path, '_version.py'), 'r') as fd:
-    version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
-
+with open(os.path.join(package_folder_path, "_version.py"), "r") as fd:
+    version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
 if not version:
-    raise RuntimeError('Cannot find version information')
+    raise RuntimeError("Cannot find version information")
 
-with open('README.md') as f:
-    readme = f.read()
-with open('CHANGELOG.md') as f:
-    changelog = f.read()
-
-exclude_packages = [
-        'tests',
-        'samples',
-        # Exclude packages that will be covered by PEP420 or nspkg
-        'azure',
-        'azure.eventhub',
-        'azure.eventhub.extensions',
-    ]
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name=PACKAGE_NAME,
     version=version,
-    description='Microsoft Azure {} Client Library for Python'.format(PACKAGE_PPRINT_NAME),
-    long_description=readme + '\n\n' + changelog,
-    long_description_content_type='text/markdown',
-    license='MIT License',
-    author='Microsoft Corporation',
-    author_email='azpysdkhelp@microsoft.com',
-    url='https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/eventhub/azure-eventhub-checkpointstoreblob',
+    description="Microsoft Azure {} Client Library for Python".format(PACKAGE_PPRINT_NAME),
+    # ensure that these are updated to reflect the package owners' information
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Azure/azure-sdk-for-python",
+    author="Microsoft Corporation",
+    author_email="azuresdkengsysadmins@microsoft.com",
+    license="MIT License",
+    # ensure that the development status reflects the status of your package
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
     ],
-    zip_safe=False,
-    packages=find_packages(exclude=exclude_packages),
+    packages=find_packages(
+        exclude=[
+            "tests",
+            # Exclude packages that will be covered by PEP420 or nspkg
+            # This means any folder structure that only consists of a __init__.py.
+            # For example, for storage, this would mean adding 'azure.storage'
+            # in addition to the default 'azure' that is seen here.
+            "azure",
+        ]
+    ),
     install_requires=[
-        # dependencies for the vendored storage blob
-        "azure-core<2.0.0,>=1.10.0",
-        "msrest>=0.6.18",
-        "cryptography>=2.1.4",
-        # end of dependencies for the vendored storage blob
-        'azure-eventhub<6.0.0,>=5.0.0',
+        "azure-core<2.0.0,>=1.14.0",
     ],
     extras_require={
-        # dependencies for the vendored storage blob
-        ":python_version<'3.0'": ['futures'],
-        ":python_version<'3.4'": ['enum34>=1.0.4'],
-        ":python_version<'3.5'": ["typing"]
-        # end of dependencies for the vendored storage blob
+        ":python_version<'3.0'": ["azure-nspkg"],
+    },
+    project_urls={
+        "Bug Reports": "https://github.com/Azure/azure-sdk-for-python/issues",
+        "Source": "https://github.com/Azure/azure-sdk-python",
     },
 )
