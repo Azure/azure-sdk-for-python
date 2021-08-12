0b9d28aee498156bfe1fe5a097ed5d6ee5e6883b
Josue Garcia <t-jgarcia@microsoft.com>
2021-08-12 18:04:22 -0400

    update

--- sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
@@ -68,7 +68,7 @@ setup(
     install_requires=[
         "azure-core<2.0.0,>=1.14.0",
         'azure-eventhub<6.0.0,>=5.0.0',
-        "msrest>=0.6.21",
+        "msrest>=0.6.18",
     ],
     extras_require={
         ":python_version<'3.0'": ["azure-nspkg"],
