fb8fc5778acb09ffaa023643152bb99ba1389854
Jg1255 <68622473+Jg1255@users.noreply.github.com>
2021-08-13 14:01:55 -0400

    Update sdk/eventhub/azure-eventhub-checkpointstoretable/azure/eventhub/extensions/checkpointstoretable/_tablestoragecs.py
    Co-authored-by: swathipil <76007337+swathipil@users.noreply.github.com>

--- sdk/eventhub/azure-eventhub-checkpointstoretable/azure/eventhub/extensions/checkpointstoretable/_tablestoragecs.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/azure/eventhub/extensions/checkpointstoretable/_tablestoragecs.py
@@ -203,6 +203,7 @@ class TableCheckpointStore(CheckpointStore):
     def list_ownership(
         self, fully_qualified_namespace, eventhub_name, consumer_group, **kwargs
     ):
+    # type: (str, str, str, Any) -> Iterable[Dict[str, Any]]
         """Retrieves a complete ownership list from the storage table.
 
         :param str fully_qualified_namespace: The fully qualified namespace that the Event Hub belongs to.
