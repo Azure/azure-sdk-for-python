55bbd8bbe8ba5917ce478f6a3931d02206bea31c
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2021-08-19 14:54:00 -0700

    fix test

--- sdk/monitor/azure-monitor-query/tests/test_logs_response.py
+++ sdk/monitor/azure-monitor-query/tests/test_logs_response.py
@@ -1,14 +1,10 @@
-from datetime import datetime, time, timedelta
+from datetime import datetime
 import pytest
-import json
+import six
 import os
-from msrest.serialization import UTC
 
 from azure.identity import ClientSecretCredential
-from azure.core.exceptions import HttpResponseError
-from azure.monitor.query import LogsQueryClient, LogsBatchQuery
-
-from azure.monitor.query._helpers import construct_iso8601
+from azure.monitor.query import LogsQueryClient
 
 def _credential():
     credential  = ClientSecretCredential(
@@ -27,9 +23,9 @@ def test_query_response_types():
 
     # returns LogsQueryResult 
     result = client.query(os.environ['LOG_WORKSPACE_ID'], query, timespan=None)
-    assert result.tables[0].rows[0][0].__class__ == datetime # TimeGenerated generated is a datetime
-    assert result.tables[0].rows[0][1].__class__ == str # _ResourceId generated is a string
-    assert result.tables[0].rows[0][2].__class__ == bool # Success generated is a bool
-    assert result.tables[0].rows[0][3].__class__ == int # ItemCount generated is a int
-    assert result.tables[0].rows[0][4].__class__ == float # DurationMs generated is a real
+    assert isinstance(result.tables[0].rows[0][0], datetime) # TimeGenerated generated is a datetime
+    assert isinstance(result.tables[0].rows[0][1], six.string_types) # _ResourceId generated is a string
+    assert isinstance(result.tables[0].rows[0][2], bool) # Success generated is a bool
+    assert isinstance(result.tables[0].rows[0][3], int) # ItemCount generated is a int
+    assert isinstance(result.tables[0].rows[0][4], float) # DurationMs generated is a real
 
