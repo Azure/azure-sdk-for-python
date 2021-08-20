02671ab892f7760e3568a6250a2d5c2b76381396
Rakshith Bhyravabhotla <sabhyrav@microsoft.com>
2021-08-20 02:17:20 -0700

    More Renaming in query (#20303)
    * More Reanaming in query
    
    * changelog
    
    * commit 2
    
    * some changes
    
    * remove errror
    
    * Update sdk/monitor/azure-monitor-query/azure/monitor/query/_models.py
    
    * Apply suggestions from code review
    
    Co-authored-by: Adam Ling (MSFT) <adam_ling@outlook.com>
    
    * Update sdk/monitor/azure-monitor-query/CHANGELOG.md
    
    Co-authored-by: Adam Ling (MSFT) <adam_ling@outlook.com>

--- sdk/monitor/azure-monitor-query/samples/sample_log_query_client.py
+++ sdk/monitor/azure-monitor-query/samples/sample_log_query_client.py
@@ -20,18 +20,26 @@ client = LogsQueryClient(credential)
 query = """AppRequests |
 summarize avgRequestDuration=avg(DurationMs) by bin(TimeGenerated, 10m), _ResourceId"""
 
+query = """
+AppRequests
+| where TimeGenerated > ago(1h)
+| fork
+    ( summarize avgRequestDuration=avg(DurationMs) by bin(TimeGenerated, 10m), _ResourceId )
+"""
+
 # returns LogsQueryResult 
 response = client.query(os.environ['LOG_WORKSPACE_ID'], query, timespan=timedelta(days=1))
 
 if not response.tables:
     print("No results for the query")
 
-try:
-    table = response.tables[0]
-    df = pd.DataFrame(table.rows, columns=[col.name for col in table.columns])
-    print(df)
-except TypeError:
-    print(response.error)
+for table in response.tables:
+    try:
+        print ([col.name for col in table.columns])
+        df = pd.DataFrame(table.rows, columns=[col.name for col in table.columns])
+        print(df)
+    except TypeError:
+        print(response.error)
 # [END send_logs_query]
 """
     TimeGenerated                                        _ResourceId          avgRequestDuration
