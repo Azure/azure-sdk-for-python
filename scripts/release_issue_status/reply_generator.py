0a20a20eae5cd0695fff397d9ba12ceca588800c
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2021-08-24 09:56:20 +0800

    Update reply_generator.py

--- scripts/release_issue_status/reply_generator.py
+++ scripts/release_issue_status/reply_generator.py
@@ -1,99 +1,109 @@
-import os
-import re
-
 from github import Github
+import re
+import os
 
 
-def update_issue_body(issue_link):
-    # Get Issue Number
-    issue_number = int(issue_link.replace("https://github.com/Azure/sdk-release-request/issues/", ""))
-    g = Github(os.getenv('TOKEN'))
-    sdk_repo = g.get_repo('Azure/sdk-release-request')
-    rest_repo = g.get_repo('Azure/azure-rest-api-specs')
-    issue_info = sdk_repo.get_issue(number=issue_number)
-    issue_body = issue_info.body
+class ReplyGenerator(object):
 
-    issue_body_list = [i for i in issue_body.split("\n") if i]
-    # Get the link in issue body
-    for row in issue_body_list:
-        if 'link' in row.lower():
-            link = row.split(":", 1)[-1].strip()
-            break
+    def __init__(self, issue_object):
+        self.issue_object = issue_object
+        self.labels = [label.name for label in self.issue_object.labels]
+        self.comments = self.issue_object.get_comments()
+        self.body = self.issue_object.body
+        self.body_parse = {}
+        self._body_parse()
+        self.rest_specs_object = None
+        self.whether_change_readme = False
+        self._whether_change_readme()
+        self.whl_link = None
+        self.changelog_content = None
+        self.latest_pr_number = None
 
-    if link.count('https') > 1:
-        link = link.split(']')[0]
-        link = link.replace('[', "").replace(']', "").replace('(', "").replace(')', "")
+    def _whether_change_readme(self):
+        g = Github(os.getenv('TOKEN'))
+        repo = g.get_repo('Azure/azure-rest-api-specs')
+        self.rest_specs_object = repo
+        contents = repo.get_contents(self.body_parse['readme_path'])
+        contents = str(contents.decoded_content)
+        pattern_tag = re.compile(r'tag: package-[\w+-.]+')
+        package_tag = pattern_tag.search(contents).group()
+        package_tag = package_tag.split(':')[1].strip()
+        readme_python_contents = str(repo.get_contents(self.body_parse['readme_python_path']).decoded_content)
+        whether_multi_api = 'multi-api' in readme_python_contents
+        whether_same_tag = package_tag == self.body_parse['readme_tag']
+        self.whether_change_readme = not (whether_same_tag and (not whether_multi_api or 'MultiAPI' in self.labels))
 
-    try:
-        package_name, readme_link = get_pkname_and_readme_link(rest_repo, link)
-    except Exception as e:
-        print(e)
-        return False
+    def _body_parse(self):
+        pattern_readme = re.compile(r'/specification/([\w-]+/)+readme.md')
+        pattern_resource_manager = re.compile(r'/specification/([\w-]+/)+resource-manager')
+        pattern_tag = re.compile(r'package-[\w+-.]+')
+        readme_path = pattern_readme.search(self.body).group()
+        readme_tag = pattern_tag.search(self.body).group()
+        resource_manager = pattern_resource_manager.search(self.body).group()
+        self.body_parse['readme_path'] = readme_path
+        self.body_parse['readme_python_path'] = readme_path[:readme_path.rfind('/')] + '/readme.python.md'
+        self.body_parse['readme_tag'] = readme_tag
+        self.body_parse['resource_manager'] = resource_manager
 
-    readme_link = readme_link.replace('python.', '')
-    issue_body_list.insert(0, f'\n{readme_link}')
-    issue_body_list.insert(1, package_name)
-    issue_body_up = ''
-    for raw in issue_body_list:
-        if raw == '---\r' or raw == '---':
-            issue_body_up += '\n'
-        issue_body_up += raw + '\n'
-    try:
-        issue_info.edit(body=issue_body_up)
-        return True
-    except Exception as e:
-        print(e)
-        return False
+    def _get_latest_pr_from_readme(self):
+        commits = self.rest_specs_object.get_commits(path=self.body_parse['resource_manager'])
+        latest_commit = [commit for commit in commits][0]
+        latest_pr_brief = latest_commit.commit.message
+        latest_pr_number = re.findall('\(\#[0-9]+\)', latest_pr_brief)
+        latest_pr_number_int = []
+        for number in latest_pr_number:
+            number = int(re.search('\d+', number).group())
+            latest_pr_number_int.append(number)
+        latest_pr_number_int.sort()
 
+        return latest_pr_number_int[-1]
 
-def get_pkname_and_readme_link(rest_repo, link):
-    # change commit link to pull json link
-    pk_name = ''
-    if 'commit' in link:
-        commit_sha = link.split('commit/')[-1]
-        commit = rest_repo.get_commit(commit_sha)
-        link = commit.files[0].blob_url
+    def _latest_pr_parse(self):
+        latest_pr = self.rest_specs_object.get_issue(self.latest_pr_number)
+        latest_pr_comments = latest_pr.get_comments()
+        b = [i for i in latest_pr_comments]
+        for comment in latest_pr_comments:
+            if '<h3>Swagger Generation Artifacts</h3>' in comment.body:
+                return self._swagger_generator_parse(comment.body)
 
-    # if link is a pr, it can get both pakeage name and readme link.
-    if 'pull' in link:
-        pk_name, readme_link = get_pkname_and_readme_from_pull(rest_repo, link)
-    elif '/resource-manager' not in link:
-        readme_link = link + '/resource-manager/readme.python.md'
-    else:
-        readme_link = link.split('/resource-manager')[0] + '/resource-manager/readme.python.md'
-    # get the package name by readme link
-    if not pk_name:
-        readme_link_part = '/specification' + readme_link.split('/specification')[-1]
-        readme_contents = str(rest_repo.get_contents(readme_link_part).decoded_content)
-        pk_name = re.findall(r'package-name: (.*?)\\n', readme_contents)[0]
+    def _swagger_generator_parse(self, context):
+        track1_info_model = ''
+        try:
+            if '<b> azure-sdk-for-python</b>' in context:
+                pattern_python_t1 = re.compile('<b> azure-sdk-for-python</b>.+?</details>', re.DOTALL)
+                python_t1 = re.search(pattern_python_t1, context).group()
+                prttern_python_track1 = re.compile('<ul>\s+?<li>\s+?<a.+</ul>', re.DOTALL)
+                python_track1_info = re.search(prttern_python_track1, python_t1).group()
+                track1_info_model = '<details open><summary><b> python-track1</b></summary>{} </details>'.format(
+                    python_track1_info)
+        except Exception as e:
+            print('track1 generate error')
+        pattern_python = re.compile('<b> azure-sdk-for-python-track2</b>.+?</details>', re.DOTALL)
+        python = re.search(pattern_python, context).group()
+        prttern_python_track2 = re.compile('<ul>\s*?<li>\s*?<a.*</ul>', re.DOTALL)
+        python_track2_info = re.search(prttern_python_track2, python).group()
+        track2_info_model = '<details open><summary><b> python-track2</b></summary>{} </details>'.format(python_track2_info)
+        info_model = 'hi @{} Please check the package whether works well and the changelog info is as below:\n' \
+                     '{}\n{}\n' \
+                     '\n* (The version of the package is only a temporary version for testing)\n' \
+                     '\nhttps://github.com/Azure/azure-rest-api-specs/pull/{}\n' \
+            .format(self.issue_object.user.login, track1_info_model, track2_info_model, str(self.latest_pr_number))
 
-    return pk_name, readme_link
+        return info_model
 
+    def reply_owner(self, reply_content):
+        self.issue_object.create_comment(reply_content)
 
-def get_pkname_and_readme_from_pull(rest_repo, pr_link):
-    pr_number = int(pr_link.replace("https://github.com/Azure/azure-rest-api-specs/pull/", "").strip('/'))
-    pr_info = rest_repo.get_issue(number=pr_number)
+    def add_label(self, label_name):
+        if label_name not in self.labels:
+            self.labels.append(label_name)
+        self.issue_object.set_labels(*self.labels)
 
-    # Get package name
-    pr_comments = pr_info.get_comments()
-    package_names = []
-    for comment in pr_comments:
-        if "Swagger Generation Artifacts" in comment.body:
-            package_names = re.findall(r"<b>track\d_(.*?)</b>", comment.body)
-            break
-
-    if len(package_names) != 1:
-        return '', ''
-    package_name = package_names[0]
-
-    # Get Readme link
-    pr_info1 = rest_repo.get_pull(number=pr_number)
-    for pr_changed_file in pr_info1.get_files():
-        contents_url = pr_changed_file.contents_url
-        if '/resource-manager' in contents_url:
-            pk_url_name = re.findall(r'/specification/(.*?)/resource-manager/', contents_url)[0]
-            break
-    readme_link = 'https://github.com/Azure/azure-rest-api-specs/blob/main/specification/{}/' \
-                  'resource-manager/readme.python.md'.format(pk_url_name)
-
-    return package_name, readme_link
+    def run(self):
+        if not self.whether_change_readme:
+            self.latest_pr_number = self._get_latest_pr_from_readme()
+            reply_content = self._latest_pr_parse()
+            self.reply_owner(reply_content)
+            self.add_label('P1')
+        else:
+            print('issue {} need config readme***********'.format(self.issue_object.number))
