2682c0e41a3f43a4b5896447f299a20777858b6f
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2021-08-25 17:16:48 +0800

    Update update_issue_body.py

--- scripts/release_issue_status/update_issue_body.py
+++ scripts/release_issue_status/update_issue_body.py
@@ -1,5 +1,6 @@
 import re
 
+
 def update_issue_body(sdk_repo, rest_repo, issue_link):
     # Get Issue Number
     issue_number = int(issue_link.replace("https://github.com/Azure/sdk-release-request/issues/", ""))
@@ -46,8 +47,33 @@ def get_pkname_and_readme_link(rest_repo, link):
 
     # if link is a pr, it can get both pakeage name and readme link.
     if 'pull' in link:
-        pk_name, readme_link = get_pkname_and_readme_from_pull(rest_repo, link)
+        pr_number = int(link.replace("https://github.com/Azure/azure-rest-api-specs/pull/", "").strip('/'))
+        pr_info = rest_repo.get_issue(number=pr_number)
+        # Get package name
+        pr_comments = pr_info.get_comments()
+        pk_names = []
+        for comment in pr_comments:
+            if "Swagger Generation Artifacts" in comment.body:
+                pk_names = re.findall(r"<b>track\d_(.*?)</b>", comment.body)
+                break
+
+        if len(pk_names) != 1:
+            raise Exception('muliple package names exist')
+        pk_name = pk_names[0]
+
+        # Get Readme link
+        pr_info1 = rest_repo.get_pull(number=pr_number)
+        for pr_changed_file in pr_info1.get_files():
+            contents_url = pr_changed_file.contents_url
+            if '/resource-manager' in contents_url:
+                pk_url_name = re.findall(r'/specification/(.*?)/resource-manager/', contents_url)[0]
+                break
+        readme_link = 'https://github.com/Azure/azure-rest-api-specs/blob/main/specification/{}/' \
+                      'resource-manager/readme.python.md'.format(pk_url_name)
+
+    # if link is a rest url(i.e. https://github.com/Azure/azure-rest-api-specs/blob/main/specification/xxx/resource-manager/readme.python.md)
     elif '/resource-manager' not in link:
+        # (i.e. https://github.com/Azure/azure-rest-api-specs/tree/main/specification/xxxx)
         readme_link = link + '/resource-manager/readme.python.md'
     else:
         readme_link = link.split('/resource-manager')[0] + '/resource-manager/readme.python.md'
@@ -58,32 +84,3 @@ def get_pkname_and_readme_link(rest_repo, link):
         pk_name = re.findall(r'package-name: (.*?)\\n', readme_contents)[0]
 
     return pk_name, readme_link
-
-
-def get_pkname_and_readme_from_pull(rest_repo, pr_link):
-    pr_number = int(pr_link.replace("https://github.com/Azure/azure-rest-api-specs/pull/", "").strip('/'))
-    pr_info = rest_repo.get_issue(number=pr_number)
-
-    # Get package name
-    pr_comments = pr_info.get_comments()
-    package_names = []
-    for comment in pr_comments:
-        if "Swagger Generation Artifacts" in comment.body:
-            package_names = re.findall(r"<b>track\d_(.*?)</b>", comment.body)
-            break
-
-    if len(package_names) != 1:
-        return '', ''
-    package_name = package_names[0]
-
-    # Get Readme link
-    pr_info1 = rest_repo.get_pull(number=pr_number)
-    for pr_changed_file in pr_info1.get_files():
-        contents_url = pr_changed_file.contents_url
-        if '/resource-manager' in contents_url:
-            pk_url_name = re.findall(r'/specification/(.*?)/resource-manager/', contents_url)[0]
-            break
-    readme_link = 'https://github.com/Azure/azure-rest-api-specs/blob/main/specification/{}/' \
-                  'resource-manager/readme.python.md'.format(pk_url_name)
-    
-    return package_name, readme_link
