2f36b67acae11a137b22af20dd2b5c5ac64f1bfd
SDKAuto <sdkautomation@microsoft.com>
2021-08-26 02:40:56 +0000

    CodeGen from PR 15798 in Azure/azure-rest-api-specs logz readme config (#15798)

--- sdk/logz/azure-mgmt-logz/azure/mgmt/logz/_configuration.py
+++ sdk/logz/azure-mgmt-logz/azure/mgmt/logz/_configuration.py
@@ -48,7 +48,7 @@ class MicrosoftLogzConfiguration(Configuration):
 
         self.credential = credential
         self.subscription_id = subscription_id
-        self.api_version = "2020-10-01-preview"
+        self.api_version = "2020-10-01"
         self.credential_scopes = kwargs.pop('credential_scopes', ['https://management.azure.com/.default'])
         kwargs.setdefault('sdk_moniker', 'mgmt-logz/{}'.format(VERSION))
         self._configure(**kwargs)
