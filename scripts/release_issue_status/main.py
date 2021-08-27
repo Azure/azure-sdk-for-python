af36ccc190abdfa5d455334a4193670afd80671b
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2021-08-27 14:21:24 +0800

    Update main.py

--- scripts/release_issue_status/main.py
+++ scripts/release_issue_status/main.py
@@ -6,11 +6,12 @@ from datetime import date, datetime
 import subprocess as sp
 from azure.storage.blob import BlobClient
 import reply_generator as rg
-from update_issue_body import update_issue_body
+from update_issue_body import update_issue_body, find_readme_link
 import traceback
 
 _NULL = ' '
 _FILE_OUT = 'release_issue_status.csv'
+_FILE_OUT_PYTHON = 'release_python_status.md
 _PYTHON_SDK_ADMINISTRATORS = {'msyyc', 'RAY-316', 'BigCat20196'}
 
 def my_print(cmd):
@@ -21,6 +22,20 @@ def print_check(cmd):
     my_print(cmd)
     sp.check_call(cmd, shell=True)
 
+
+def output_python_md(issue_status_python):
+    with open(_FILE_OUT_PYTHON, 'w') as file_out:
+        file_out.write('| issue | author | package | assignee | bot advice | created date of issue | delay from created date |\n')
+        file_out.write('| ------ | ------ | ------ | ------ | ------ | ------ | :-----: |\n')
+        file_out.writelines([item.output_python() for item in sorted(issue_status_python, key=_key_select)])
+
+
+def output_csv(issue_status):
+    with open(_FILE_OUT, 'w') as file_out:
+        file_out.write('language,issue,author,package,created date,delay from created date,latest update time,'
+                       'delay from latest update,status,bot advice\n')
+        file_out.writelines([item.output() for item in sorted(issue_status, key=_key_select)])
+  
 
 class IssueStatus:
     link = _NULL
@@ -38,6 +53,7 @@ class IssueStatus:
     whether_author_comment = True
     issue_object = _NULL
     labels = _NULL
+    assignee = _NULL
 
     def output(self):
         return '{},{},{},{},{},{},{},{},{},{}\n'.format(self.language, self.link, self.author,
@@ -47,6 +63,13 @@ class IssueStatus:
                                                         str(date.fromtimestamp(self.latest_update)),
                                                         self.delay_from_latest_update,
                                                         self.status, self.bot_advice)
+
+    
+    def output_python(self):
+        return '| [#{}]({}) | {} | {} | {} | {} | {} | {} |\n'.format(self.link.split('/')[-1], self.link, self.author, 
+                                                                      self.package, self.assignee, self.bot_advice, 
+                                                                      str(date.fromtimestamp(self.create_date)), 
+                                                                      self.delay_from_create_date)
 
 
 def _extract(str_list, key_word):
@@ -87,9 +110,10 @@ def _extract_author_latest_comment(comments):
 
 def _whether_author_comment(comments):
     q = set(comment.user.login for comment in comments)
-    diff = q.difference(_PYTHON_SDK_ADMINISTRATORS)
+    for administrators in _PYTHON_SDK_ADMINISTRATORS:
+        q.discard(administrators)
 
-    return  len(diff) > 0
+    return True if len(q) > 0 else False
 
 
 def _latest_comment_time(comments, delay_from_create_date):
@@ -108,6 +132,7 @@ def main():
     label1 = sdk_repo.get_label('ManagementPlane')
     open_issues = sdk_repo.get_issues(state='open', labels=[label1])
     issue_status = []
+    issue_status_python = []
     duplicated_issue = dict()
     start_time = time.time()
     for item in open_issues:
@@ -129,7 +154,9 @@ def main():
         issue.issue_object = item
         issue.labels = [label.name for label in item.labels]
         issue.days_from_latest_commit = _latest_comment_time(item.get_comments(), issue.delay_from_create_date)
-
+        if item.assignee:
+            issue.assignee = item.assignee.login
+        
         issue_status.append(issue)
         key = (issue.language, issue.package)
         duplicated_issue[key] = duplicated_issue.get(key, 0) + 1
@@ -152,17 +179,24 @@ def main():
             item.bot_advice = 'new issue and better to confirm quickly.'
             if 'auto-link' not in item.labels:
                 try:
-                    update_issue_body(sdk_repo, rest_repo, item.link)
+                    readme_link = update_issue_body(sdk_repo, rest_repo, item.link)
                 except Exception as e:
                     item.bot_advice = 'failed to modify the body of the new issue. Please modify manually'
                     item.labels.append('attention')
                 item.labels.append('auto-link')
                 item.issue_object.set_labels(*item.labels)
+            else:
+                try:
+                    readme_link = find_readme_link(sdk_repo, item.issue_object.number)
+                except Exception as e:
+                    print('Issue: {}  updates body failed'.format(item.issue_object.number))
+                    item.bot_advice = 'failed to find Readme link, Please check !!'
+                    item.labels.append('attention')
+                    continue
             try:
-                time.sleep(5)
-                reply = rg.ReplyGenerator(issue_object=item.issue_object, rest_repo=rest_repo)
-                reply.run()
+                reply = rg.begin_reply_generate(issue_object=item.issue_object, rest_repo=rest_repo, readme_link=readme_link)
             except Exception as e:
+                item.bot_advice = 'auto reply failed, Please intervene manually !!'
                 print('Error from auto reply ========================')
                 print('Issue:{}'.format(item.issue_object.number))
                 print(traceback.format_exc())
@@ -173,10 +207,9 @@ def main():
         if item.days_from_latest_commit >= 30 and item.language == 'Python' and '30days attention' not in item.labels:
             item.labels.append('30days attention')
             item.issue_object.set_labels(*item.labels)
-            item.issue_object.create_comment(f'hi @{item.author}, the issue is closed since there is no reply for a long time. Please reopen it if necessary or create new one.')
-            item.issue_object.edit(state='close')
+            issue.issue_object.edit(state='close')
         elif item.days_from_latest_commit >= 15 and item.language == 'Python' and '15days attention' not in item.labels:
-            item.issue_object.create_comment(f'hi @{item.author}, this release-request has been delayed more than 15 days,'
+            item.issue_object.create_comment('hi @{} ,this release-request has been delayed more than 15 days,'
                                              ' please deal with it ASAP. We will close the issue if there is still no response after 15 days!')
             item.labels.append('15days attention')
             item.issue_object.set_labels(*item.labels)
@@ -184,12 +217,13 @@ def main():
         # judge whether there is duplicated issue for same package
         if item.package != _NULL and duplicated_issue.get((item.language, item.package)) > 1:
             item.bot_advice = f'Warning:There is duplicated issue for {item.package}. ' + item.bot_advice
+            
+        if item.language == 'Python':
+            issue_status_python.append(item)
 
     # output result
-    with open(_FILE_OUT, 'w') as file_out:
-        file_out.write('language,issue,author,package,created date,delay from created date,latest update time,'
-                       'delay from latest update,status,bot advice\n')
-        file_out.writelines([item.output() for item in sorted(issue_status, key=_key_select)])
+    output_python_md(issue_status_python)
+    output_csv(issue_status)
 
     # commit to github
     print_check('git add .')
@@ -201,6 +235,11 @@ def main():
                                              blob_name=_FILE_OUT)
     with open(_FILE_OUT, 'rb') as data:
         blob.upload_blob(data, overwrite=True)
+        
+    blob_1 = BlobClient.from_connection_string(conn_str=os.getenv('CONN_STR'), container_name=os.getenv('FILE'),
+                                             blob_name=_FILE_OUT_PYTHON)
+    with open(_FILE_OUT_PYTHON, 'rb') as data:
+        blob_1.upload_blob(data, overwrite=True)
 
 
 if __name__ == '__main__':
