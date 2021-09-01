4735b987bd300a9d1fed4e6c1f4d824931ef2c23
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2021-08-31 21:16:05 -0400

    have LRO support rest requests and responses (#20483)
    * have LRO support rest requests and responses
    
    * pass operation config to send_request
    
    * fix polling location fail tests

--- sdk/core/azure-core/tests/testserver_tests/coretestserver/coretestserver/test_routes/__init__.py
+++ sdk/core/azure-core/tests/testserver_tests/coretestserver/coretestserver/test_routes/__init__.py
@@ -9,6 +9,7 @@ from .basic import basic_api
 from .encoding import encoding_api
 from .errors import errors_api
 from .multipart import multipart_api
+from .polling import polling_api
 from .streams import streams_api
 from .urlencoded import urlencoded_api
 from .xml_route import xml_api
@@ -19,6 +20,7 @@ __all__ = [
     "encoding_api",
     "errors_api",
     "multipart_api",
+    "polling_api",
     "streams_api",
     "urlencoded_api",
     "xml_api",
