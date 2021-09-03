f65954ca89f4078836b1258c963f8664a40a5602
BigCat20196 <1095260342@qq.com>
2021-09-03 10:23:49 +0800

    Update auto-close

--- scripts/release_issue_status/main.py
+++ scripts/release_issue_status/main.py
@@ -220,9 +220,7 @@ def main():
             item.bot_advice = 'delay for a long time and better to handle now.'
         if item.comment_num > 1 and item.language == 'Python':
             try:
-                if auto_close_issue(request_repo, item.issue_object.number, item.package):
-                    item.labels.append('auto-closed')
-                    item.issue_object.set_labels(*item.labels)          
+                auto_close_issue(request_repo, item)
             except Exception as e:
                 item.bot_advice = 'auto-close failed, please check!'
                 print(f"=====issue: {item.issue_object.number}, {e}")
