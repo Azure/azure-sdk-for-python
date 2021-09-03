46d89b9b57ddee526c23dc8cdc05ed9aba90912d
Yalin Li <yall@microsoft.com>
2021-09-03 16:00:22 -0700

    [Keyvault] Remove exception message parsing from samples in keys, certificates and secrets (#20540)

--- sdk/keyvault/azure-keyvault-keys/samples/list_operations.py
+++ sdk/keyvault/azure-keyvault-keys/samples/list_operations.py
@@ -41,52 +41,46 @@ from azure.core.exceptions import HttpResponseError
 VAULT_URL = os.environ["VAULT_URL"]
 credential = DefaultAzureCredential()
 client = KeyClient(vault_url=VAULT_URL, credential=credential)
-try:
-    # Let's create keys with RSA and EC type. If the key
-    # already exists in the Key Vault, then a new version of the key is created.
-    print("\n.. Create Key")
-    rsa_key = client.create_rsa_key("rsaKeyName")
-    ec_key = client.create_ec_key("ecKeyName")
-    print("Key with name '{0}' was created of type '{1}'.".format(rsa_key.name, rsa_key.key_type))
-    print("Key with name '{0}' was created of type '{1}'.".format(ec_key.name, ec_key.key_type))
 
-    # You need to check the type of all the keys in the vault.
-    # Let's list the keys and print their key types.
-    # List operations don 't return the keys with their type information.
-    # So, for each returned key we call get_key to get the key with its type information.
-    print("\n.. List keys from the Key Vault")
-    keys = client.list_properties_of_keys()
-    for key in keys:
-        retrieved_key = client.get_key(key.name)
-        print(
-            "Key with name '{0}' with type '{1}' was found.".format(retrieved_key.name, retrieved_key.key_type)
-        )
+# Let's create keys with RSA and EC type. If the key
+# already exists in the Key Vault, then a new version of the key is created.
+print("\n.. Create Key")
+rsa_key = client.create_rsa_key("rsaKeyName")
+ec_key = client.create_ec_key("ecKeyName")
+print("Key with name '{0}' was created of type '{1}'.".format(rsa_key.name, rsa_key.key_type))
+print("Key with name '{0}' was created of type '{1}'.".format(ec_key.name, ec_key.key_type))
 
-    # The rsa key size now should now be 3072, default - 2048. So you want to update the key in Key Vault to ensure
-    # it reflects the new key size. Calling create_rsa_key on an existing key creates a new version of the key in
-    # the Key Vault with the new key size.
-    new_key = client.create_rsa_key(rsa_key.name, size=3072)
-    print("New version was created for Key with name '{0}' with the updated size.".format(new_key.name))
+# You need to check the type of all the keys in the vault.
+# Let's list the keys and print their key types.
+# List operations don 't return the keys with their type information.
+# So, for each returned key we call get_key to get the key with its type information.
+print("\n.. List keys from the Key Vault")
+keys = client.list_properties_of_keys()
+for key in keys:
+    retrieved_key = client.get_key(key.name)
+    print(
+        "Key with name '{0}' with type '{1}' was found.".format(retrieved_key.name, retrieved_key.key_type)
+    )
 
-    # You should have more than one version of the rsa key at this time. Lets print all the versions of this key.
-    print("\n.. List versions of a key using its name")
-    key_versions = client.list_properties_of_key_versions(rsa_key.name)
-    for key in key_versions:
-        print("Key '{0}' has version: '{1}'".format(key.name, key.version))
+# The rsa key size now should now be 3072, default - 2048. So you want to update the key in Key Vault to ensure
+# it reflects the new key size. Calling create_rsa_key on an existing key creates a new version of the key in
+# the Key Vault with the new key size.
+new_key = client.create_rsa_key(rsa_key.name, size=3072)
+print("New version was created for Key with name '{0}' with the updated size.".format(new_key.name))
 
-    # Both the rsa key and ec key are not needed anymore. Let's delete those keys.
-    print("\n.. Delete the created keys...")
-    for key_name in (ec_key.name, rsa_key.name):
-        client.begin_delete_key(key_name).wait()
+# You should have more than one version of the rsa key at this time. Lets print all the versions of this key.
+print("\n.. List versions of a key using its name")
+key_versions = client.list_properties_of_key_versions(rsa_key.name)
+for key in key_versions:
+    print("Key '{0}' has version: '{1}'".format(key.name, key.version))
 
-    # You can list all the deleted and non-purged keys, assuming Key Vault is soft-delete enabled.
-    print("\n.. List deleted keys from the Key Vault (requires soft-delete)")
-    deleted_keys = client.list_deleted_keys()
-    for deleted_key in deleted_keys:
-        print("Key with name '{0}' has recovery id '{1}'".format(deleted_key.name, deleted_key.recovery_id))
+# Both the rsa key and ec key are not needed anymore. Let's delete those keys.
+print("\n.. Delete the created keys...")
+for key_name in (ec_key.name, rsa_key.name):
+    client.begin_delete_key(key_name).wait()
 
-except HttpResponseError as e:
-    if "(NotSupported)" in e.message:
-        print("\n{0} Please enable soft delete on Key Vault to perform this operation.".format(e.message))
-    else:
-        print("\nThis sample has caught an error. {0}".format(e.message))
+# You can list all the deleted and non-purged keys, assuming Key Vault is soft-delete enabled.
+print("\n.. List deleted keys from the Key Vault (requires soft-delete)")
+deleted_keys = client.list_deleted_keys()
+for deleted_key in deleted_keys:
+    print("Key with name '{0}' has recovery id '{1}'".format(deleted_key.name, deleted_key.recovery_id))
--- sdk/keyvault/azure-keyvault-secrets/samples/backup_restore_operations.py
+++ sdk/keyvault/azure-keyvault-secrets/samples/backup_restore_operations.py
@@ -38,37 +38,34 @@ from azure.core.exceptions import HttpResponseError
 VAULT_URL = os.environ["VAULT_URL"]
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=VAULT_URL, credential=credential)
-try:
-    # Let's create a secret holding storage account credentials.
-    # if the secret already exists in the Key Vault, then a new version of the secret is created.
-    print("\n.. Create Secret")
-    secret = client.set_secret("backupRestoreSecretName", "backupRestoreSecretValue")
-    print("Secret with name '{0}' created with value '{1}'".format(secret.name, secret.value))
 
-    # Backups are good to have, if in case secrets gets deleted accidentally.
-    # For long term storage, it is ideal to write the backup to a file.
-    print("\n.. Create a backup for an existing Secret")
-    secret_backup = client.backup_secret(secret.name)
-    print("Backup created for secret with name '{0}'.".format(secret.name))
+# Let's create a secret holding storage account credentials.
+# if the secret already exists in the Key Vault, then a new version of the secret is created.
+print("\n.. Create Secret")
+secret = client.set_secret("backupRestoreSecretName", "backupRestoreSecretValue")
+print("Secret with name '{0}' created with value '{1}'".format(secret.name, secret.value))
 
-    # The storage account secret is no longer in use, so you delete it.
-    print("\n.. Deleting secret...")
-    delete_operation = client.begin_delete_secret(secret.name)
-    deleted_secret = delete_operation.result()
-    print("Deleted secret with name '{0}'".format(deleted_secret.name))
+# Backups are good to have, if in case secrets gets deleted accidentally.
+# For long term storage, it is ideal to write the backup to a file.
+print("\n.. Create a backup for an existing Secret")
+secret_backup = client.backup_secret(secret.name)
+print("Backup created for secret with name '{0}'.".format(secret.name))
 
-    # Wait for the deletion to complete before purging the secret.
-    # The purge will take some time, so wait before restoring the backup to avoid a conflict.
-    delete_operation.wait()
-    print("\n.. Purge the secret")
-    client.purge_deleted_secret(deleted_secret.name)
-    time.sleep(60)
-    print("Purged secret with name '{0}'".format(deleted_secret.name))
+# The storage account secret is no longer in use, so you delete it.
+print("\n.. Deleting secret...")
+delete_operation = client.begin_delete_secret(secret.name)
+deleted_secret = delete_operation.result()
+print("Deleted secret with name '{0}'".format(deleted_secret.name))
 
-    # In the future, if the secret is required again, we can use the backup value to restore it in the Key Vault.
-    print("\n.. Restore the secret using the backed up secret bytes")
-    secret = client.restore_secret_backup(secret_backup)
-    print("Restored secret with name '{0}'".format(secret.name))
+# Wait for the deletion to complete before purging the secret.
+# The purge will take some time, so wait before restoring the backup to avoid a conflict.
+delete_operation.wait()
+print("\n.. Purge the secret")
+client.purge_deleted_secret(deleted_secret.name)
+time.sleep(60)
+print("Purged secret with name '{0}'".format(deleted_secret.name))
 
-except HttpResponseError as e:
-    print("\nThis sample has caught an error. {0}".format(e.message))
+# In the future, if the secret is required again, we can use the backup value to restore it in the Key Vault.
+print("\n.. Restore the secret using the backed up secret bytes")
+secret = client.restore_secret_backup(secret_backup)
+print("Restored secret with name '{0}'".format(secret.name))
