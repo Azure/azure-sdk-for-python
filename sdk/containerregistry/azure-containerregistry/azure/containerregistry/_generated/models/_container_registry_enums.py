35dfc09c032dadd5fc6ff25ef1ed8c859593f90e
YalinLi0312 <yall@microsoft.com>
2021-09-03 13:39:04 -0700

    Get rid of public enum class

--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/_generated/models/_container_registry_enums.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/_generated/models/_container_registry_enums.py
@@ -6,10 +6,25 @@
 
 from enum import Enum, EnumMeta
 from six import with_metaclass
-from azure.core import CaseInsensitiveEnumMeta
+
+class _CaseInsensitiveEnumMeta(EnumMeta):
+    def __getitem__(self, name):
+        return super().__getitem__(name.upper())
+
+    def __getattr__(cls, name):
+        """Return the enum member matching `name`
+        We use __getattr__ instead of descriptors or inserting into the enum
+        class' __dict__ in order to support `name` and `value` being both
+        properties for enum members (which live in the class' __dict__) and
+        enum members themselves.
+        """
+        try:
+            return cls._member_map_[name.upper()]
+        except KeyError:
+            raise AttributeError(name)
 
 
-class ArtifactArchitecture(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
+class ArtifactArchitecture(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
     I386 = "386"
     AMD64 = "amd64"
@@ -25,7 +40,7 @@ class ArtifactArchitecture(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
     S390_X = "s390x"
     WASM = "wasm"
 
-class ArtifactOperatingSystem(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
+class ArtifactOperatingSystem(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
     AIX = "aix"
     ANDROID = "android"
@@ -42,7 +57,7 @@ class ArtifactOperatingSystem(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)
     SOLARIS = "solaris"
     WINDOWS = "windows"
 
-class ManifestOrderBy(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
+class ManifestOrderBy(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """Sort options for ordering manifests in a collection.
     """
 
@@ -53,7 +68,7 @@ class ManifestOrderBy(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
     #: Order manifest by LastUpdatedOn field, from least recently updated to most recently updated.
     LAST_UPDATED_ON_ASCENDING = "timeasc"
 
-class TagOrderBy(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
+class TagOrderBy(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
 
     #: Do not provide an orderby value in the request.
     NONE = "none"
@@ -62,17 +77,9 @@ class TagOrderBy(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
     #: Order tags by LastUpdatedOn field, from least recently updated to most recently updated.
     LAST_UPDATED_ON_ASCENDING = "timeasc"
 
-class TokenGrantType(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
+class TokenGrantType(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     """Grant type is expected to be refresh_token
     """
 
     REFRESH_TOKEN = "refresh_token"
     PASSWORD = "password"
-
-class ContainerRegistryAudience(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
-    """Supported container registry audience"""
-
-    ARM_CHINA = "https://management.chinacloudapi.cn"
-    ARM_GERMANY = "https://management.microsoftazure.de"
-    ARM_GOVERNMENT = "https://management.usgovcloudapi.net"
-    ARM_PUBLIC_CLOUD = "https://management.azure.com"
