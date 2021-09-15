772d36a3ae172d2b727ec4f02d7e4a9db54d7705
Xiang Yan <xiangyan@microsoft.com>
2021-09-15 14:23:53 -0700

    update error log

--- sdk/identity/azure-identity/azure/identity/aio/_credentials/chained.py
+++ sdk/identity/azure-identity/azure/identity/aio/_credentials/chained.py
@@ -78,5 +78,7 @@ class ChainedTokenCredential(AsyncContextManager):
 
         within_credential_chain.set(False)
         attempts = _get_error_message(history)
-        message = self.__class__.__name__ + " failed to retrieve a token from the included credentials." + attempts
+        message = self.__class__.__name__ + " failed to retrieve a token from the included credentials." + attempts \
+                  + "\nTo mitigate this issue, please refer to the troubleshooting guidelines here at " \
+                    "https://aka.ms/azsdk/python/identity/defaultazurecredential/troubleshoot."
         raise ClientAuthenticationError(message=message)
