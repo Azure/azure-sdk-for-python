76e71b945e63358786aedf41072552911f90aa4a
Franko Morales <fmorales@microsoft.com>
2021-09-17 15:18:26 -0700

    Cleaning Download async tests

--- sdk/communication/azure-communication-callingserver/tests/test_download_content_async_live.py
+++ sdk/communication/azure-communication-callingserver/tests/test_download_content_async_live.py
@@ -7,90 +7,57 @@
     Adding testing for the download recording async use cases.
 '''
 from io import BytesIO
-from parameterized.parameterized import parameterized
 import pytest
 
-from _shared.testcase import (
-    CommunicationTestCase
-)
 from _shared.asynctestcase import AsyncCommunicationTestCase
 
 from azure.communication.callingserver.aio import CallingServerClient
 
-class TestDownloadAsyncLive(CommunicationTestCase):
+class TestDownloadAsyncLive(AsyncCommunicationTestCase):
     '''
     Download async testing class.
     '''
-    @pytest.mark.playback_test_only
-    @AsyncCommunicationTestCase.await_prepared_test
-    @parameterized.expand([[{}], [{'block_size': 400}]])
-    #pylint: disable=missing-function-docstring
-    async def test_download_content_to_stream(self, download_options):
-        metadata_url = \
+
+    def __init__(self, method_name, *args, **kwargs):
+        super().__init__(method_name, *args, **kwargs)
+        self._metadata_url = \
             "https://storage.asm.skype.com/v1/objects/" + \
                 "0-wus-d4-6afafe78b9e313d77933b87578eabc9e/content/acsmetadata"
-        connection_string = \
+        self._connection_string = \
             "endpoint=https://acstelephonyportaltesting.communication.azure.com/;" + \
                 "accesskey=/cJGRzQtFVNneQVqbUlRvsvOLwEgQwsWDQxjLnWPWcTSg3RwAfnYY4v9Ce/" + \
                     "mN4iAZ50znB8B0UMmQ/cDHLnEtw=="
+        self._calling_server_client = \
+            CallingServerClient.from_connection_string(self._connection_string)
 
-        calling_server_client = CallingServerClient.from_connection_string(connection_string)
-        downloader = await calling_server_client.download(metadata_url, **download_options)
-        stream = BytesIO()
-        await downloader.readinto(stream)
+    @pytest.mark.playback_test_only
+    @AsyncCommunicationTestCase.await_prepared_test
+    #pylint: disable=missing-function-docstring
+    async def test_download_content_to_stream(self):
+        stream = await self._execute_test()
         metadata = stream.getvalue()
-
-        self.assertIsNotNone(metadata)
-        self.assertTrue(metadata.__contains__(b"0-wus-d4-6afafe78b9e313d77933b87578eabc9e"))
+        self._verify_metadata(metadata)
 
     @AsyncCommunicationTestCase.await_prepared_test
     @pytest.mark.playback_test_only
-    @pytest.mark.skip
     #pylint: disable=missing-function-docstring
     async def test_download_content_to_stream_on_chunks(self):
-        metadata_url = \
-            "https://storage.asm.skype.com/v1/objects/" + \
-                "0-wus-d4-6afafe78b9e313d77933b87578eabc9e/content/acsmetadata"
-        connection_string = \
-            "endpoint=https://acstelephonyportaltesting.communication.azure.com/;" + \
-                "accesskey=/cJGRzQtFVNneQVqbUlRvsvOLwEgQwsWDQxjLnWPWcTSg3RwAfnYY4v9Ce/" + \
-                    "mN4iAZ50znB8B0UMmQ/cDHLnEtw=="
-
-        calling_server_client = CallingServerClient.from_connection_string(connection_string)
-        downloader = await calling_server_client.download(metadata_url, block_size=400)
-        stream = BytesIO()
-        await downloader.readinto(stream)
+        stream = await self._execute_test(block_size=400)
         metadata = stream.getvalue()
-        self.assertIsNotNone(metadata)
-        self.assertTrue(metadata.__contains__(b"0-wus-d4-6afafe78b9e313d77933b87578eabc9e"))
+        self._verify_metadata(metadata)
 
     @AsyncCommunicationTestCase.await_prepared_test
     @pytest.mark.playback_test_only
-    @pytest.mark.skip
     #pylint: disable=missing-function-docstring
     async def test_download_content_to_stream_on_chunks_parallel(self):
-        metadata_url = \
-            "https://storage.asm.skype.com/v1/objects/" + \
-                "0-wus-d4-6afafe78b9e313d77933b87578eabc9e/content/acsmetadata"
-        connection_string = \
-            "endpoint=https://acstelephonyportaltesting.communication.azure.com/;" + \
-                "accesskey=/cJGRzQtFVNneQVqbUlRvsvOLwEgQwsWDQxjLnWPWcTSg3RwAfnYY4v9Ce/" + \
-                    "mN4iAZ50znB8B0UMmQ/cDHLnEtw=="
-
-        calling_server_client = CallingServerClient.from_connection_string(connection_string)
-        downloader = await calling_server_client.download(
-            metadata_url,
+        stream = await self._execute_test(
             max_concurrency=3,
             block_size=100)
-        stream = BytesIO()
-        await downloader.readinto(stream)
         metadata = stream.getvalue()
-        self.assertIsNotNone(metadata)
-        self.assertTrue(metadata.__contains__(b"0-wus-d4-6afafe78b9e313d77933b87578eabc9e"))
+        self._verify_metadata(metadata)
 
     @AsyncCommunicationTestCase.await_prepared_test
     @pytest.mark.playback_test_only
-    @pytest.mark.skip
     #pylint: disable=missing-function-docstring
     async def test_download_content_to_stream_with_redirection(self):
         metadata_url = \
@@ -108,3 +75,14 @@ class TestDownloadAsyncLive(CommunicationTestCase):
         metadata = stream.getvalue()
         self.assertIsNotNone(metadata)
         self.assertTrue(metadata.__contains__(b"0-weu-d18-f71ce6295ef910657759449422ebb5f4"))
+
+    async def _execute_test(self, **download_options):
+        downloader = await self._calling_server_client.download(self._metadata_url,
+            **download_options)
+        stream = BytesIO()
+        await downloader.readinto(stream)
+        return stream
+
+    def _verify_metadata(self, metadata):
+        self.assertIsNotNone(metadata)
+        self.assertTrue(metadata.__contains__(b"0-wus-d4-6afafe78b9e313d77933b87578eabc9e"))
