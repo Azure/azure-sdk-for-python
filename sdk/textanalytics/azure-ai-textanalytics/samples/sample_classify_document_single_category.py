bc032f49b94f6e182ea178e2cc74caf22b290a53
Krista Pratico <krpratic@microsoft.com>
2021-09-22 18:57:15 -0700

    update sample names

--- sdk/textanalytics/azure-ai-textanalytics/samples/sample_classify_document_single_category.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/sample_classify_document_single_category.py
@@ -7,17 +7,17 @@
 # --------------------------------------------------------------------------
 
 """
-FILE: sample_classify_document_categories.py
+FILE: sample_classify_document_single_category.py
 
 DESCRIPTION:
-    This sample demonstrates how to classify documents into custom categories. Here we have a few
-    movie plot summaries that must be categorized into movie genres like Sci-Fi, Horror, Comedy, Romance, etc.
+    This sample demonstrates how to classify documents into a custom category. Here we several
+    support tickets that need to be classified as internet, printer, email or hardware issues.
     Classifying documents is available as an action type through the begin_analyze_actions API.
 
     To train a model to classify your documents, see TODO
 
 USAGE:
-    python sample_classify_document_categories.py
+    python sample_classify_document_single_category.py
 
     Set the environment variables with your own values before running the sample:
     1) AZURE_TEXT_ANALYTICS_ENDPOINT - the endpoint to your Cognitive Services resource.
@@ -30,11 +30,11 @@ USAGE:
 import os
 
 
-def sample_classify_document_categories():
+def sample_classify_document_single_category():
     from azure.core.credentials import AzureKeyCredential
     from azure.ai.textanalytics import (
         TextAnalyticsClient,
-        ClassifyDocumentMultiCategoriesAction
+        ClassifyDocumentSingleCategoryAction
     )
 
     endpoint = os.environ["AZURE_TEXT_ANALYTICS_ENDPOINT"]
@@ -48,22 +48,16 @@ def sample_classify_document_categories():
     )
 
     documents = [
-        "In the not-too-distant future, Earth's dying sun spells the end for humanity. In a last-ditch effort to "
-        "save the planet, a crew of eight men and women ventures into space with a device that could revive the "
-        "star. However, an accident, a grave mistake and a distress beacon from a long-lost spaceship throw "
-        "the crew and its desperate mission into a tailspin.",
-
-        "Despite his family's generations-old ban on music, young Miguel dreams of becoming an accomplished "
-        "musician like his idol Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself "
-        "in the stunning and colorful Land of the Dead. After meeting a charming trickster named Héctor, "
-        "the two new friends embark on an extraordinary journey to unlock the real story behind Miguel's "
-        "family history"
+        "My internet has stopped working. I tried resetting the router, but it just keeps blinking red.",
+        "I submitted 3 jobs to print but the printer is unresponsive. I can't see it under my devices either.",
+        "My computer will not boot. Pushing the power button does nothing - just a black screen.",
+        "I seem to not be receiving all my emails on time. Emails from 2 days ago show up as just received.",
     ]
 
     poller = text_analytics_client.begin_analyze_actions(
         documents,
         actions=[
-            ClassifyDocumentMultiCategoriesAction(
+            ClassifyDocumentSingleCategoryAction(
                 project_name=project_name,
                 deployment_name=deployed_model_name
             ),
@@ -74,17 +68,15 @@ def sample_classify_document_categories():
     for doc, classification_results in zip(documents, document_results):
         for classification_result in classification_results:
             if not classification_result.is_error:
-                classifications = classification_result.classifications
-                print("The movie plot '{}' was classified as the following genres:\n".format(doc))
-                for classification in classifications:
-                    print("'{}' with confidence score {}.".format(
-                        classification.category, classification.confidence_score
-                    ))
+                classification = classification_result.classification
+                print("The document text '{}' was classified as '{}' with confidence score {}.".format(
+                    doc, classification.category, classification.confidence_score)
+                )
             else:
-                print("Movie plot '{}' has an error with code '{}' and message '{}'".format(
+                print("Document text '{}' has an error with code '{}' and message '{}'".format(
                     doc, classification_result.code, classification_result.message
                 ))
 
 
 if __name__ == "__main__":
-    sample_classify_document_categories()
+    sample_classify_document_single_category()
