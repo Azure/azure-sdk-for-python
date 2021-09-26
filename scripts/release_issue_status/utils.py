8685032c19bce929e30b12e99bb4fd77feeef689
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2021-09-26 10:44:59 +0800

    Update utils.py

--- scripts/release_issue_status/utils.py
+++ scripts/release_issue_status/utils.py
@@ -122,9 +122,6 @@ def get_python_pipelines():
     python_piplines = {}
     pipeline_client = PipelinesClient(base_url='https://dev.azure.com/azure-sdk',
                                       creds=BasicAuthentication(os.getenv('PIPELINE_TOKEN'), ''))
-    print("*********************", os.getenv('PIPELINE_TOKEN'))
-    print("++++", os.getenv('PIPELINE_HEADERS'))
-    print("+++", os.getenv('USR_NAME'))
     pipelines = pipeline_client.list_pipelines(project='internal')
     for pipeline in pipelines:
         if re.findall('^python - \w*$', pipeline.name):
