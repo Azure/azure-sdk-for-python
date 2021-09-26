6aec14fe918b20f80ec4797130aca795aca62f10
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2021-09-26 10:01:15 +0800

    Update utils.py

--- scripts/release_issue_status/utils.py
+++ scripts/release_issue_status/utils.py
@@ -124,6 +124,7 @@ def get_python_pipelines():
                                       creds=BasicAuthentication(os.getenv('PIPELINE_TOKEN'), ''))
     print("*********************", os.getenv('PIPELINE_TOKEN'))
     print("++++", os.getenv('PIPELINE_HEADERS'))
+    print("+++", os.getenv('USR_NAME'))
     pipelines = pipeline_client.list_pipelines(project='internal')
     for pipeline in pipelines:
         if re.findall('^python - \w*$', pipeline.name):
