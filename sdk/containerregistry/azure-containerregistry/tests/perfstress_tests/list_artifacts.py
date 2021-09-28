a3dacca87ba6b7f094b6175635e550d56a0fc83c
YalinLi0312 <yall@microsoft.com>
2021-09-28 15:52:13 -0700

    Add ACR performance tests

---
+++ sdk/containerregistry/azure-containerregistry/tests/perfstress_tests/list_artifacts.py
@@ -0,0 +1,36 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+from azure_devtools.perfstress_tests import PerfStressTest
+from azure.containerregistry import ContainerRegistryClient
+from azure.containerregistry.aio import ContainerRegistryClient as AsyncContainerRegistryClient
+
+
+class ListArtifactsTest(PerfStressTest):
+
+    def __init__(self, arguments):
+        super().__init__(arguments)
+
+        # Create clients
+        account_url = self.get_from_env("CONTAINERREGISTRY_ANONREGISTRY_ENDPOINT")
+        audience = "https://management.azure.com"
+        self.anon_client = ContainerRegistryClient(endpoint=account_url, credential=None, audience=audience)
+        self.async_anon_client = AsyncContainerRegistryClient(endpoint=account_url, credential=None, audience=audience)
+        
+        self.repository = "node"
+
+    async def global_setup(self):
+        await super().global_setup()
+
+    async def close(self):
+        await self.async_anon_client.close()
+        await super().close()
+
+    def run_sync(self):
+        for manifest in self.anon_client.list_manifest_properties(self.repository):
+            self.anon_client.get_manifest_properties(self.repository, manifest.digest)
+
+    async def run_async(self):
+        async for manigest in self.async_anon_client.list_manifest_properties(self.repository):
+            self.async_anon_client.get_manifest_properties(self.repository, manigest.digest)
---
+++ sdk/containerregistry/azure-containerregistry/tests/perfstress_tests/list_repositories.py
@@ -0,0 +1,36 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+from azure_devtools.perfstress_tests import PerfStressTest
+from azure.containerregistry import ContainerRegistryClient
+from azure.containerregistry.aio import ContainerRegistryClient as AsyncContainerRegistryClient
+
+
+class ListRepositoriesTest(PerfStressTest):
+
+    def __init__(self, arguments):
+        super().__init__(arguments)
+
+        # Create clients
+        account_url = self.get_from_env("CONTAINERREGISTRY_ANONREGISTRY_ENDPOINT")
+        audience = "https://management.azure.com"
+        self.anon_client = ContainerRegistryClient(endpoint=account_url, credential=None, audience=audience)
+        self.async_anon_client = AsyncContainerRegistryClient(endpoint=account_url, credential=None, audience=audience)
+        
+        self.repository = "node"
+
+    async def global_setup(self):
+        await super().global_setup()
+
+    async def close(self):
+        await self.async_anon_client.close()
+        await super().close()
+
+    def run_sync(self):
+        for repository_name in self.anon_client.list_repository_names():
+            self.anon_client.get_repository_properties(repository_name)
+
+    async def run_async(self):
+        async for repository_name in self.async_anon_client.list_repository_names():
+            self.async_anon_client.get_repository_properties(repository_name)
