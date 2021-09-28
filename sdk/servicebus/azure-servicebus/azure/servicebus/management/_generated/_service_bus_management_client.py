6ee2215fa753ab3bdb1332b752b5014cc7ad2655
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-09-28 12:08:19 -0700

    upload recordings

--- sdk/servicebus/azure-servicebus/azure/servicebus/management/_generated/_service_bus_management_client.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/management/_generated/_service_bus_management_client.py
@@ -52,7 +52,6 @@ class ServiceBusManagementClient(ServiceBusManagementClientOperationsMixin):
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
-        self._serialize.client_side_validation = False
         self._deserialize = Deserializer(client_models)
 
         self.entity = EntityOperations(
