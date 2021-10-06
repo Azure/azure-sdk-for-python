018ef31fbc7f090509d758bf9a1e30b3c95d6eab
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-10-05 18:38:30 -0700

    another test

--- sdk/schemaregistry/azure-schemaregistry-avroserializer/setup.py
+++ sdk/schemaregistry/azure-schemaregistry-avroserializer/setup.py
@@ -42,7 +42,8 @@ exclude_packages = [
     ]
 install_packages = [
     'azure-schemaregistry==1.0.0b3',
-    'avro<2.0.0,>=1.10.0'
+    'avro<2.0.0,>=1.10.0',
+    'backports.functools-lru-cache >= 1.6.4; python_version == "2.7"'
 ]
 
 setup(
