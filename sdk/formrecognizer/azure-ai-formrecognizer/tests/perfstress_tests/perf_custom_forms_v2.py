a8b7b6de97fb4da9501a6e9defca863e736c2238
Catalina Peralta <catalinaperaltah@hotmail.com>
2021-10-07 10:52:56 -0700

    add perf test for new clients

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/perfstress_tests/perf_custom_forms_v2.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/perfstress_tests/perf_custom_forms_v2.py
@@ -5,12 +5,16 @@
 # ------------------------------------
 
 import os
+import pytest
+import functools
+from io import BytesIO
+from datetime import date, time
 from azure_devtools.perfstress_tests import PerfStressTest
 from azure.core.credentials import AzureKeyCredential
-from azure.ai.formrecognizer import DocumentAnalysisClient
-from azure.ai.formrecognizer.aio import DocumentAnalysisClient as AsyncDocumentAnalysisClient, DocumentModelAdministrationClient as AsyncDocumentModelAdministrationClient
+from azure.ai.formrecognizer import FormRecognizerClient, FormContentType
+from azure.ai.formrecognizer.aio import FormRecognizerClient as AsyncFormRecognizerClient, FormTrainingClient as AsyncFormTrainingClient
 
-class AnalyzeCustomDocuments(PerfStressTest):   
+class RecognizeCustomForms(PerfStressTest):   
 
     def __init__(self, arguments):
         super().__init__(arguments)
@@ -24,16 +28,18 @@ class AnalyzeCustomDocuments(PerfStressTest):
         form_recognizer_account_key = os.environ["FORMRECOGNIZER_TEST_API_KEY"]
 
         # assign the clients that will be used in the perf tests
-        self.service_client = DocumentAnalysisClient(formrecognizer_test_endpoint, AzureKeyCredential(form_recognizer_account_key))
-        self.async_service_client = AsyncDocumentAnalysisClient(formrecognizer_test_endpoint, AzureKeyCredential(form_recognizer_account_key))
+        self.service_client = FormRecognizerClient(formrecognizer_test_endpoint, AzureKeyCredential(form_recognizer_account_key))
+        self.async_service_client = AsyncFormRecognizerClient(formrecognizer_test_endpoint, AzureKeyCredential(form_recognizer_account_key))
 
         # training client will be used for model training in set up
-        self.async_training_client = AsyncDocumentModelAdministrationClient(formrecognizer_test_endpoint, AzureKeyCredential(form_recognizer_account_key))
+        self.async_training_client = AsyncFormTrainingClient(formrecognizer_test_endpoint, AzureKeyCredential(form_recognizer_account_key))
 
     async def global_setup(self):
         """The global setup is run only once."""
-        poller = await self.async_training_client.begin_build_model(
-            self.formrecognizer_storage_container_sas_url,)
+        poller = await self.async_training_client.begin_training(
+            self.formrecognizer_storage_container_sas_url, 
+            use_training_labels=True, 
+            model_name="labeled")
         model = await poller.result()
         self.model_id = model.model_id
 
@@ -50,16 +56,18 @@ class AnalyzeCustomDocuments(PerfStressTest):
 
     def run_sync(self):
         """The synchronous perf test."""
-        poller = self.service_client.begin_analyze_document(
-            self.model_id,
-            self.custom_form_jpg)
+        poller = self.service_client.begin_recognize_custom_forms(
+            self.model_id, 
+            self.custom_form_jpg, 
+            content_type=FormContentType.IMAGE_JPEG)
         result = poller.result()
         assert result
 
     async def run_async(self):
         """The asynchronous perf test."""
-        poller = await self.async_service_client.begin_analyze_document(
-            self.model_id,
-            self.custom_form_jpg,)
+        poller = await self.async_service_client.begin_recognize_custom_forms(
+            self.model_id, 
+            self.custom_form_jpg,
+            content_type=FormContentType.IMAGE_JPEG)
         result = await poller.result()
         assert result
