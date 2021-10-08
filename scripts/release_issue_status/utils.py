5ace80b338e4f4baa8e0d87c1bf236282b6a2b3b
Zed Lei <59104634+RAY-316@users.noreply.github.com>
2021-10-08 17:04:31 +0800

    Update utils.py

--- scripts/release_issue_status/utils.py
+++ scripts/release_issue_status/utils.py
@@ -10,6 +10,8 @@ from bs4 import BeautifulSoup
 from msrest.authentication import BasicAuthentication
 import requests
 
+_FILE_OUT = 'published_issues_python.csv'
+
 logging.basicConfig(level=logging.INFO,
                     format='[auto-reply  log] - %(funcName)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
@@ -194,13 +196,14 @@ def auto_close_issue(sdk_repo, item):
         issue_info.edit(state='closed')
         item.issue_object.add_to_labels('auto-closed')
         logging.info(f"issue number：{issue_number} has been closed!")
-        
-    created_at = issue_info.created_at.strftime('%Y-%m-%d')
-    closed_at = issue_info.closed_at.strftime('%Y-%m-%d')
-    assignee = issue_info.assignee.login
-    link = issue_info.html_url
-    closed_issue_info = f'{package_name},{assignee},{created_at},{closed_at},{link}\n'
-    print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&',closed_issue_info)
+     
+        created_at = issue_info.created_at.strftime('%Y-%m-%d')
+        closed_at = issue_info.closed_at.strftime('%Y-%m-%d')
+        assignee = issue_info.assignee.login
+        link = issue_info.html_url
+        closed_issue_info = f'{package_name},{assignee},{created_at},{closed_at},{link}\n'
+        with open(_FILE_OUT, 'a+') as file_out:
+            file_out.write(closed_issue_info)
 
 def _get_last_released_date(package_name):
     pypi_link = f'https://pypi.org/project/{package_name}/#history'
