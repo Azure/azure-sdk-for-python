92abbc00943bd3f7c845f590511147861c1d7a10
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-10-08 14:51:21 -0700

    [EventHubs&AMQP Python] Port amqp send large message (#19937)
    * port amqp send large message
    
    * send perf test in parallel
    
    * use context manager for executor
    
    * add throughput
    
    * improve test code
    
    * update test matrix
    
    * update test
    
    * handle server busy
    
    * fix timeout
    
    * precision to 2 decimal points
    
    * update pyamqp changes and update tests

--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/synctests/test_producer.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/synctests/test_producer.py
@@ -8,36 +8,34 @@ def test_send_batch_event(live_eventhub):
     client = EventHubProducerClient.from_connection_string(live_eventhub["connection_str"])
     batch = client.create_batch()
 
-    # TODO: there is a bug that currently a batch message of size > 64*1024 could not be sent via a single transfer frame
-    #  we probably need to support spliting a single message into multiple frames when the message is large (what uamqp library is doing)
-    #  or check if tweaking link/session/connection frame settings allows us to send a large message
-    # while True:
-    #     try:
-    #         batch.add(EventData(b'test'))
-    #     except ValueError:
-    #         break
-    # client.send_batch(batch)
+    # large message that should be split into multiple transfer frames
+    while True:
+        try:
+            batch.add(EventData(b'test'))
+        except ValueError:
+            break
+    client.send_batch(batch)
 
+    # small message that fits in one transfer frame
+    batch = client.create_batch()
     for _ in range(100):
         batch.add(EventData(b'test' * 60))
     client.send_batch(batch)
 
 
 @pytest.mark.liveTest
-def test_send_batch_event(live_eventhub):
+def test_send_batch_event_single_partition(live_eventhub):
     client = EventHubProducerClient.from_connection_string(live_eventhub["connection_str"])
     batch = client.create_batch(partition_id="0")
 
-    # TODO: there is a bug that currently a batch message of size > 64*1024 could not be sent via a single transfer frame
-    #  we probably need to support spliting a single message into multiple frames when the message is large (what uamqp library is doing)
-    #  or check if tweaking link/session/connection frame settings allows us to send a large message
-    # while True:
-    #     try:
-    #         batch.add(EventData(b'test'))
-    #     except ValueError:
-    #         break
-    # client.send_batch(batch)
+    while True:
+        try:
+            batch.add(EventData(b'test'))
+        except ValueError:
+            break
+    client.send_batch(batch)
 
+    batch = client.create_batch()
     for _ in range(100):
         batch.add(EventData(b'test' * 60))
     client.send_batch(batch)
