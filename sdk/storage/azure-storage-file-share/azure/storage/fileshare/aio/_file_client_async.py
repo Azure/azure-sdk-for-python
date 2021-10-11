0c016cf35670aa0f58611e519442fae63d77f0ab
Xiaoxi Fu <49707495+xiafu-msft@users.noreply.github.com>
2021-10-11 10:54:20 -0700

    [Storage]Edit doc for api_version (#21085)
    * [Storage]Edit doc for api_version
    
    * pylint fix

--- sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_file_client_async.py
+++ sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_file_client_async.py
@@ -108,8 +108,8 @@ class ShareFileClient(AsyncStorageAccountHostsMixin, ShareFileClientBase):
         an instance of a AzureSasCredential from azure.core.credentials or an account
         shared access key.
     :keyword str api_version:
-        The Storage API version to use for requests. Default value is '2019-07-07'.
-        Setting to an older version may result in reduced feature compatibility.
+        The Storage API version to use for requests. Default value is the most recent service version that is
+        compatible with the current SDK. Setting to an older version may result in reduced feature compatibility.
 
         .. versionadded:: 12.1.0
 
