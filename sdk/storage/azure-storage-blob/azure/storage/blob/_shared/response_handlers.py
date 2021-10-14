69b74366c8d42ebdb1c5fed602d8c0f4854bda81
xiafu <xiafu@microsoft.com>
2021-10-14 00:23:46 -0700

    fix pylint

--- sdk/storage/azure-storage-blob/azure/storage/blob/_shared/response_handlers.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/_shared/response_handlers.py
@@ -112,8 +112,7 @@ def process_storage_error(storage_error):   # pylint:disable=too-many-statements
             error_dict = error_body.get('error', {})
         elif not error_code:
             _LOGGER.warning(
-                'Unexpected return type {} from ContentDecodePolicy.deserialize_from_http_generics.'.format(
-                    type(error_body)))
+                'Unexpected return type %s from ContentDecodePolicy.deserialize_from_http_generics.', type(error_body))
             error_dict = {'message': str(error_body)}
 
         # If we extracted from a Json or XML response
