b25f4560be42dc7f22b4ea54226238ec2713e8d5
Adam Ling (MSFT) <adam_ling@outlook.com>
2021-10-14 17:47:10 -0700

    finish code

--- sdk/eventhub/azure-eventhub/stress/pyamqp/sync_receive.py
+++ sdk/eventhub/azure-eventhub/stress/pyamqp/sync_receive.py
@@ -4,19 +4,51 @@
 # --------------------------------------------------------------------------------------------
 
 import os
+import dotenv
+import logging
 import threading
+from logging.handlers import RotatingFileHandler
 import time
 
 from azure.eventhub import EventHubConsumerClient
+from azure.eventhub import parse_connection_string
+
+logger = logging.getLogger('RECEIVE_PERF_TEST')
+logger.setLevel(logging.INFO)
+logger.addHandler(RotatingFileHandler("receive_perf_test.log"))
+
+dotenv.load_dotenv()
+CONN_STRS = [
+    os.environ["EVENT_HUB_CONN_STR_BASIC_NORTHEU"],
+    os.environ["EVENT_HUB_CONN_STR_STANDARD_NORTHEU"],
+    os.environ["EVENT_HUB_CONN_STR_BASIC_WESTUS2"],
+    os.environ["EVENT_HUB_CONN_STR_STANDARD_WESTUS2"]
+]
+EH_NAME_EVENT_SIZE_PAIR = [
+    ('pyamqp_16', 16),
+    ('pyamqp_128', 128),
+    ('pyamqp_1024', 1024)
+]
+PREFETCH_LIST = [300, 3000, 30000]
+PARTITION_ID = "0"
 
 
-CONNECTION_STR = os.environ['EVENT_HUB_CONN_STR']
-EVENTHUB_NAME = os.environ['EVENT_HUB_NAME']
+def receive_fixed_time_interval(
+    conn_str,
+    eventhub_name,
+    single_event_size,
+    prefetch=300,
+    batch_receiving=False,
+    description=None,
+    partition_id="0"
+):
 
-
-def test_receive_fixed_time_interval():
-
-    consumer_client = EventHubConsumerClient.from_connection_string(CONNECTION_STR, consumer_group="$Default", eventhub_name=EVENTHUB_NAME)
+    consumer_client = EventHubConsumerClient.from_connection_string(
+        conn_str,
+        consumer_group="$Default",
+        eventhub_name=eventhub_name,
+        prefetch=prefetch
+    )
 
     last_received_count = [0]
     received_count = [0]
@@ -28,6 +60,9 @@ def test_receive_fixed_time_interval():
     def on_event(partition_context, event):
         received_count[0] += 1
 
+    def on_event_batch(partition_context, events):
+        received_count[0] += len(events)
+
     def monitor():
         while run_flag[0]:
             snap = received_count[0]
@@ -36,13 +71,19 @@ def test_receive_fixed_time_interval():
             all_perf_records.append(perf)
             time.sleep(check_interval)
 
+    target = consumer_client.receive_batch if batch_receiving else consumer_client.receive
+    kwargs = {
+        "partition_id": partition_id,
+        "starting_position": "-1",  # "-1" is from the beginning of the partition.
+    }
+    if batch_receiving:
+        kwargs["on_event_batch"] = on_event_batch
+    else:
+        kwargs["on_event"] = on_event
+
     thread = threading.Thread(
-        target=consumer_client.receive,
-        kwargs={
-            "on_event": on_event,
-            "partition_id": "0",
-            "starting_position": "-1",  # "-1" is from the beginning of the partition.
-        }
+        target=target,
+        kwargs=kwargs
     )
 
     monitor_thread = threading.Thread(
@@ -60,7 +101,104 @@ def test_receive_fixed_time_interval():
 
     valid_perf_records = all_perf_records[10:]  # skip the first 10 records to let the receiving program be stable
     avg_perf = sum(valid_perf_records) / len(valid_perf_records)
-    print("The average performance is {} events/s".format(avg_perf))
+
+    logger.info(
+        "EH Namespace: {}.\nMethod: {}, The average performance is {} events/s, throughput: {} bytes/s.\n"
+        "Configs are: Single message size: {} bytes, Run duration: {} seconds.".format(
+            parse_connection_string(conn_str).fully_qualified_namespace,
+            description or "receive_fixed_time_interval",
+            avg_perf,
+            avg_perf * single_event_size,
+            single_event_size,
+            run_duration
+        )
+    )
 
 
-test_receive_fixed_time_interval()
+def receive_fixed_amount(
+    conn_str,
+    eventhub_name,
+    single_event_size,
+    prefetch=300,
+    batch_receiving=False,
+    description=None,
+    partition_id="0"
+):
+    consumer_client = EventHubConsumerClient.from_connection_string(
+        conn_str,
+        consumer_group="$Default",
+        eventhub_name=eventhub_name,
+        prefetch=prefetch
+    )
+    run_times = 3
+    fixed_amount = 5000
+    perf_records = []
+    received_count = [0]
+
+    def on_event(partition_context, event):
+        received_count[0] += 1
+        if received_count[0] == fixed_amount:
+            consumer_client.close()
+
+    def on_event_batch(partition_context, events):
+        received_count[0] += len(events)
+        if received_count[0] >= fixed_amount:
+            consumer_client.close()
+
+    for i in range(run_times):
+        start_time = time.time()
+        with consumer_client:
+            if batch_receiving:
+                consumer_client.receive_batch(
+                    on_event_batch=on_event_batch,
+                    partition_id=partition_id,
+                    starting_position="-1",
+                    max_batch_size=prefetch
+                )
+            else:
+                consumer_client.receive(
+                    on_event=on_event,
+                    partition_id=partition_id,
+                    starting_position="-1"
+                )
+        end_time = time.time()
+        total_time = end_time - start_time
+        speed = fixed_amount/total_time
+        perf_records.append(speed)
+        received_count[0] = 0
+    avg_perf = sum(perf_records) / len(perf_records)
+
+    logger.info(
+        "EH Namespace: {}.\nMethod: {}, The average performance is {} events/s, throughput: {} bytes/s.\n"
+        "Configs are: Single message size: {} bytes, Total events to receive: {}.".format(
+            parse_connection_string(conn_str).fully_qualified_namespace,
+            description or "receive_fixed_amount",
+            avg_perf,
+            avg_perf * single_event_size,
+            single_event_size,
+            fixed_amount
+        )
+    )
+
+
+if __name__ == "__main__":
+    for conn_str in CONN_STRS:
+        for eh_name, single_event_size in EH_NAME_EVENT_SIZE_PAIR:
+            for prefetch in PREFETCH_LIST:
+                for batch_receiving in [True, False]:
+                    print('------------------- receiving fixed amount -------------------')
+                    receive_fixed_amount(
+                        conn_str=conn_str,
+                        eventhub_name=eh_name,
+                        single_event_size=single_event_size,
+                        prefetch=prefetch,
+                        batch_receiving=batch_receiving
+                    )
+                    print('------------------- receiving fixed interval -------------------')
+                    receive_fixed_time_interval(
+                        conn_str=conn_str,
+                        eventhub_name=eh_name,
+                        single_event_size=single_event_size,
+                        prefetch=prefetch,
+                        batch_receiving=batch_receiving
+                    )
--- sdk/eventhub/azure-eventhub/stress/pyamqp/sync_send.py
+++ sdk/eventhub/azure-eventhub/stress/pyamqp/sync_send.py
@@ -4,6 +4,7 @@
 # --------------------------------------------------------------------------------------------
 
 import os
+import dotenv
 import time
 import logging
 from logging.handlers import RotatingFileHandler
@@ -11,12 +12,22 @@ from concurrent.futures import ThreadPoolExecutor
 
 from azure.eventhub import EventHubProducerClient, EventData
 
-logger = logging.getLogger('PERF_TEST')
+logger = logging.getLogger('SEND_PERF_TEST')
 logger.setLevel(logging.INFO)
-logger.addHandler(RotatingFileHandler("perf_test.log"))
+logger.addHandler(RotatingFileHandler("send_perf_test.log"))
 
-CONNECTION_STR = os.environ['EVENT_HUB_CONN_STR']
-EVENTHUB_NAME = os.environ['EVENT_HUB_NAME']
+dotenv.load_dotenv()
+CONN_STRS = [
+    os.environ["EVENT_HUB_CONN_STR_BASIC_NORTHEU"],
+    os.environ["EVENT_HUB_CONN_STR_STANDARD_NORTHEU"],
+    os.environ["EVENT_HUB_CONN_STR_BASIC_WESTUS2"],
+    os.environ["EVENT_HUB_CONN_STR_STANDARD_WESTUS2"]
+]
+EVENTHUB_NAME = "pyamqp"
+
+SINGLE_EVENT_SIZE_LIST = [16, 128, 1024]
+PARALLEL_THREAD_COUNT_LIST = [1, 4, 16]
+FIXED_AMOUNT_OF_EVENTS = 50_000
 
 
 def pre_prepare_client(client, data):
@@ -24,13 +35,13 @@ def pre_prepare_client(client, data):
     client.send_batch([EventData(data)])  # precall to set up the sender link
 
 
-def send_batch_message(num_of_events, single_message_size, run_times=1, description=None):
+def send_batch_message(conn_str, eventhub_name, num_of_events, single_event_size, run_times=1, description=None):
 
     client = EventHubProducerClient.from_connection_string(
-        conn_str=CONNECTION_STR, eventhub_name=EVENTHUB_NAME
+        conn_str=conn_str, eventhub_name=eventhub_name
     )
 
-    data = b'a' * single_message_size
+    data = b'a' * single_event_size
     perf_records = []
     pre_prepare_client(client, data)
 
@@ -60,10 +71,10 @@ def send_batch_message(num_of_events, single_message_size, run_times=1, descript
         "Configs are: Num of events: {} events, Single message size: {} bytes.".format(
             description or "send_batch_message",
             avg_perf,
-            avg_perf * single_message_size,
+            avg_perf * single_event_size,
             run_times,
             num_of_events,
-            single_message_size
+            single_event_size
         )
     )
     return avg_perf
@@ -83,7 +94,7 @@ def send_batch_message_worker_thread(client, data, run_flag):
     return total_cnt
 
 
-def send_batch_message_in_parallel(single_message_size, parallel_count=4, run_times=1, run_duration=30, description=None):
+def send_batch_message_in_parallel(conn_str, eventhub_name, single_event_size, parallel_thread_count=4, run_times=1, run_duration=30, description=None):
 
     perf_records = []
 
@@ -92,18 +103,18 @@ def send_batch_message_in_parallel(single_message_size, parallel_count=4, run_ti
         futures = []
         clients = [
             EventHubProducerClient.from_connection_string(
-                conn_str=CONNECTION_STR, eventhub_name=EVENTHUB_NAME
-            ) for _ in range(parallel_count)
+                conn_str=conn_str, eventhub_name=eventhub_name
+            ) for _ in range(parallel_thread_count)
         ]
 
-        data = b'a' * single_message_size
+        data = b'a' * single_event_size
 
         for client in clients:
             pre_prepare_client(client, data)
 
-        with ThreadPoolExecutor(max_workers=parallel_count) as executor:
+        with ThreadPoolExecutor(max_workers=parallel_thread_count) as executor:
             run_flag = [True]
-            for i in range(parallel_count):
+            for i in range(parallel_thread_count):
                 futures.append(
                     executor.submit(
                         send_batch_message_worker_thread,
@@ -124,13 +135,13 @@ def send_batch_message_in_parallel(single_message_size, parallel_count=4, run_ti
 
     logger.info(
         "Method: {}, The average performance is {} events/s, throughput: {} bytes/s, run times: {}.\n"
-        "Configs are: Single message size: {} bytes, Parallel count: {} threads, Run duration: {} seconds.".format(
+        "Configs are: Single message size: {} bytes, Parallel thread count: {} threads, Run duration: {} seconds.".format(
             description or "send_batch_message_in_parallel",
             avg_perf,
-            avg_perf * single_message_size,
+            avg_perf * single_event_size,
             run_times,
-            single_message_size,
-            parallel_count,
+            single_event_size,
+            parallel_thread_count,
             run_duration
         )
     )
@@ -139,17 +150,26 @@ def send_batch_message_in_parallel(single_message_size, parallel_count=4, run_ti
 if __name__ == '__main__':
     logger.info('------------------- START OF TEST -------------------')
 
-    for message_size_power in range(4, 11, 3):  # 2**4, 2**7, 2**10
-        print('-------------------  sending fixed amount of message  ------------------------')
-        send_batch_message(50_000, pow(2, message_size_power), description='sending fixed amount message')
-
-    for parallel_count_power in range(0, 5, 2):  # 0,2**0, 2**2, 2**4
-        for message_size_power in range(4, 11, 3):  # 2**4, 2**7, 2**10
-            print('-------------------  multiple threads sending messages for a fixed period ------------------------')
-            send_batch_message_in_parallel(
-                single_message_size=pow(2, message_size_power),
-                parallel_count=pow(2, parallel_count_power),
-                description='multiple threads sending messages'
+    for conn_str in CONN_STRS:
+        for single_event_size in SINGLE_EVENT_SIZE_LIST:
+            print('-------------------  sending fixed amount of message  -------------------')
+            send_batch_message(
+                conn_str=conn_str,
+                eventhub_name=EVENTHUB_NAME,
+                num_of_events=FIXED_AMOUNT_OF_EVENTS,
+                single_event_size=single_event_size,
+                description='sending fixed amount message'
             )
+
+        for parallel_thread_count in PARALLEL_THREAD_COUNT_LIST:
+            for single_event_size in SINGLE_EVENT_SIZE_LIST:
+                print('-------------------  multiple threads sending messages for a fixed period -------------------')
+                send_batch_message_in_parallel(
+                    conn_str=conn_str,
+                    eventhub_name=EVENTHUB_NAME,
+                    single_event_size=single_event_size,
+                    parallel_thread_count=parallel_thread_count,
+                    description='multiple threads sending messages'
+                )
 
     logger.info('------------------- END OF TEST -------------------')
