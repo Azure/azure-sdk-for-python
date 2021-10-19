fe3b3713b0df887ce9fb60bdfc152c78a1207e66
Paymaun Heidari <paymaun.heidari@microsoft.com>
2021-10-18 20:34:12 -0700

    WIP

--- sdk/modelsrepository/azure-iot-modelsrepository/azure/iot/modelsrepository/_model_query.py
+++ sdk/modelsrepository/azure-iot-modelsrepository/azure/iot/modelsrepository/_model_query.py
@@ -12,10 +12,12 @@ Note that this implementation is not representative of what an eventual full
 parser implementation would necessarily look like from an API perspective
 """
 import logging
+from six import text_type
 from .dtmi_conventions import is_valid_dtmi
 from ._common import ModelType, ModelProperties
 
 _LOGGER = logging.getLogger(__name__)
+
 
 class Model(object):
     def __init__(
@@ -62,8 +64,7 @@ class ModelQuery(object):
 
     def _parse_interface(self, root):
         dtmi_id = root.get(ModelProperties.id.value)
-
-        root_dtmi = dtmi_id if isinstance(dtmi_id, str) else None
+        root_dtmi = dtmi_id if isinstance(dtmi_id, text_type) else None
         extends = self._parse_extends(root)
         contents = self._parse_contents(root)
 
@@ -87,12 +88,12 @@ class ModelQuery(object):
 
     def _parse_component(self, component):
         dependencies = set()
-        if isinstance(component, str) and is_valid_dtmi(component):
+        if isinstance(component, text_type) and is_valid_dtmi(component):
             dependencies.add(component)
         elif isinstance(component, list):
             # If it's a list, could have DTMIs or nested models
             for item in component:
-                if isinstance(item, str):
+                if isinstance(item, text_type):
                     # If there are strings in the list, that's a DTMI reference, so add it
                     dependencies.add(item)
                 elif _is_interface_or_component(item):
@@ -109,6 +110,6 @@ def _is_interface_or_component(root):
 
     interface = root.get(ModelProperties.type.value)
     return (
-        isinstance(interface, str) and
+        isinstance(interface, text_type) and
         interface in [ModelType.interface.value, ModelType.component.value]
     )
