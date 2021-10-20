75d36eff8bb07d848750f2eab8186a459126dbb9
Swathi Pillalamarri <swathip@microsoft.com>
2021-10-20 14:21:19 -0700

    add license in test file

--- sdk/schemaregistry/azure-schemaregistry-avroserializer/tests/test_avro_serializer_async.py
+++ sdk/schemaregistry/azure-schemaregistry-avroserializer/tests/test_avro_serializer_async.py
@@ -1,3 +1,9 @@
+# --------------------------------------------------------------------------
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+# The MIT License (MIT)
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the ""Software""), to
 # deal in the Software without restriction, including without limitation the
