ae0c2cca8fada417f91d67e66592630ee219c5aa
PythonSdkPipelines <PythonSdkPipelines>
2021-10-26 04:23:51 +0000

    version,CHANGELOG

--- sdk/cosmos/azure-mgmt-cosmosdb/azure/mgmt/cosmosdb/_version.py
+++ sdk/cosmos/azure-mgmt-cosmosdb/azure/mgmt/cosmosdb/_version.py
@@ -6,4 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "0.7.0"
+VERSION = "7.0.0b2"
