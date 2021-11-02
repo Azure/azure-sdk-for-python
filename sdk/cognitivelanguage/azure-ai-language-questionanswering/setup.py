05c8dc8acf644db055c62b362b582d75b400609c
annatisch <antisch@microsoft.com>
2021-11-02 14:16:10 -0700

    [QnA][CLU] Release prep (#21518)
    * Added release date
    
    * Bump core version
    
    * Fix async samples
    
    * Bug in setup.py

--- sdk/cognitivelanguage/azure-ai-language-questionanswering/setup.py
+++ sdk/cognitivelanguage/azure-ai-language-questionanswering/setup.py
@@ -67,7 +67,7 @@ setup(
         'azure.ai.language',
     ]),
     install_requires=[
-        'azure-core<2.0.0,>=1.19.0',
+        'azure-core<2.0.0,>=1.19.1',
         'msrest>=0.6.21',
     ],
     extras_require={
