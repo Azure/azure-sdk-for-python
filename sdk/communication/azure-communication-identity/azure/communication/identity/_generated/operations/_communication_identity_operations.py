3faa04deaebd14edca33d0215dcbb14429cd98ec
Aigerim Beishenbekova <aigerimb@DESKTOP-PQMEAO3.corp.microsoft.com>
2021-11-03 15:58:02 +0100

    reverting the last commit

--- sdk/communication/azure-communication-identity/azure/communication/identity/_generated/operations/_communication_identity_operations.py
+++ sdk/communication/azure-communication-identity/azure/communication/identity/_generated/operations/_communication_identity_operations.py
@@ -11,7 +11,8 @@ import warnings
 
 from azure.core.exceptions import ClientAuthenticationError, HttpResponseError, ResourceExistsError, ResourceNotFoundError, map_error
 from azure.core.pipeline import PipelineResponse
-from azure.core.pipeline.transport import HttpRequest, HttpResponse
+from azure.core.pipeline.transport import HttpResponse
+from azure.core.rest import HttpRequest
 from azure.core.tracing.decorator import distributed_trace
 from msrest import Serializer
 
