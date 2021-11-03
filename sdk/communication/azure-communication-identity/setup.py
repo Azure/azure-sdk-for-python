e9ce68e40a37290eec8edffc87650145ec99bf5f
Aigerim Beishenbekova <aigerimb@DESKTOP-PQMEAO3.corp.microsoft.com>
2021-11-03 18:44:34 +0100

    upgraded min version of azure-core&msrest in shared_requirements.txt

--- sdk/communication/azure-communication-identity/setup.py
+++ sdk/communication/azure-communication-identity/setup.py
@@ -61,7 +61,7 @@ setup(
         'azure.communication'
     ]),
     install_requires=[
-        'msrest>=0.6.21',
+        "msrest>=0.6.21",
         "azure-core<2.0.0,>=1.19.1"
     ],
     extras_require={
