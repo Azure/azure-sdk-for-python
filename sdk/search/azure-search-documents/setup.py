6044f0ecddde07bed14f55beb0628af5c3ed62f7
Travis Prescott <travisj.prescott@gmail.com>
2021-11-04 14:47:29 -0700

    Update setup.py

--- sdk/search/azure-search-documents/setup.py
+++ sdk/search/azure-search-documents/setup.py
@@ -79,7 +79,7 @@ setup(
         'azure.search',
     ]),
     install_requires=[
-        "azure-core<2.0.0,>=1.19.0",
+        "azure-core<2.0.0,>=1.19.1",
         "msrest>=0.6.21",
         "azure-common~=1.1",
         "typing-extensions"
