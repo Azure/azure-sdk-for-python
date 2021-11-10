c182952b523d49c2708fb8052297080779a3b696
McCoy Patiño <39780829+mccoyp@users.noreply.github.com>
2021-11-09 18:26:28 -0800

    [Test Proxy] Add fixture to automatically start/stop Docker container (#21538)
    Co-authored-by: scbedd <45376673+scbedd@users.noreply.github.com>

--- tools/azure-sdk-tools/devtools_testutils/__init__.py
+++ tools/azure-sdk-tools/devtools_testutils/__init__.py
@@ -15,6 +15,7 @@ from .storage_testcase import (
 )
 from .keyvault_preparer import KeyVaultPreparer
 from .powershell_preparer import PowerShellPreparer
+from .proxy_docker_startup import start_test_proxy, stop_test_proxy, test_proxy
 from .proxy_testcase import recorded_by_proxy
 from .sanitizers import (
     add_body_key_sanitizer,
@@ -57,6 +58,9 @@ __all__ = [
     "CachedResourceGroupPreparer",
     "PowerShellPreparer",
     "recorded_by_proxy",
+    "test_proxy",
+    "start_test_proxy",
+    "stop_test_proxy",
     "ResponseCallback",
     "RetryCounter",
     "FakeTokenCredential",
