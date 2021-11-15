6ac509f04332b43c42fa4f3d1e42ef565077452d
Yiming Lei <59104634+RAY-316@users.noreply.github.com>
2021-11-15 16:23:48 +0800

    Update main.py

--- scripts/release_issue_status/main.py
+++ scripts/release_issue_status/main.py
@@ -149,7 +149,7 @@ def auto_reply(item, request_repo, rest_repo, sdk_repo, duplicated_issue, python
     if 'auto-link' not in item.labels:
         item.issue_object.add_to_labels('auto-link')
         try:
-            package_name, readme_link, output_folder = update_issue_body(request_repo, rest_repo,
+            package_name, readme_link, output_folder = update_issue_body(assigner_repoes, rest_repo,
                                                                          item.issue_object.number)
             logging.info("pkname, readme", package_name, readme_link)
             item.package = package_name
