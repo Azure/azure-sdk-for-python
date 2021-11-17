7d3feed85de6c6614ec5fa9d139995476badde1d
antisch <antisch@microsoft.com>
2021-11-16 17:33:51 -0800

    Added new base class

---
+++ tools/azure-devtools/src/azure_devtools/perfstress_tests/_perf_stress_base.py
@@ -0,0 +1,237 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from typing import List, Tuple
+import abc
+import datetime
+import threading
+
+
+class _PerfStressTestABC(abc.ABC):
+
+    @property
+    @abc.abstractmethod
+    def completed_operations(self) -> int:
+        """
+        Total number of operations completed by run_all().
+        Reset after warmup.
+        """
+
+    @property
+    @abc.abstractmethod
+    def last_completion_time(self) -> datetime.datetime:
+        """
+        Elapsed time between start of warmup/run and last completed operation.
+        Reset after warmup.
+        """
+
+    @property
+    @abc.abstractmethod
+    def latencies(self) -> List[datetime.timedelta]:
+        """
+        Elapsed time between start and end of each completed operation.
+        Reset after warmup.
+        Only populated if "--latency" option is enabled.
+        NOT YET SUPPORTED IN PYTHON
+        """
+
+    @property
+    @abc.abstractmethod
+    def corrected_latencies(self) -> List[datetime.timedelta]:
+        """
+        Elapsed time between scheduled start and actual end of each completed operation.
+        Reset after warmup.
+        Only populated if both "--latency" and "--rate" options are enabled.
+        NOT YET SUPPORTED IN PYTHON
+        """
+
+    @property
+    @abc.abstractmethod
+    def pending_operations(self) -> Tuple[datetime.timedelta, int]:
+        """
+        Channel containing the scheduled start time of each operation.
+        Also includes a Stopwatch measuring elapsed time since scheduled start.
+        """
+
+    @abc.abstractmethod
+    async def global_setup(self) -> None:
+        """
+        Setup called once across all parallel test instances.
+        Used to setup state that can be used by all test instances.
+        """
+
+    @abc.abstractmethod
+    async def global_cleanup(self) -> None:
+        """
+        Cleanup called once across all parallel test instances.
+        Used to cleanup state that can be used by all test instances.
+        """
+
+    @abc.abstractmethod
+    async def setup(self) -> None:
+        """
+        Setup called once per parallel test instance.
+        Used to setup state specific to this test instance.
+        """
+
+    @abc.abstractmethod
+    async def cleanup(self):
+        """
+        Cleanup called once per parallel test instance.
+        Used to cleanup state specific to this test instance.
+        """
+
+    @abc.abstractmethod
+    async def post_setup(self) -> None:
+        """
+        Post-setup called once per parallel test instance.
+        Used by base classes to setup state (like test-proxy) after all derived class setup is complete.
+        """
+
+    @abc.abstractmethod
+    async def pre_cleanup(self) -> None:
+        """
+        Pre-cleanup called once per parallel test instance.
+        Used by base classes to cleanup state (like test-proxy) before all derived class cleanup runs.
+        """
+
+    @abc.abstractmethod
+    async def close(self):
+        """
+        Close any open client resources/connections per parallel test instance.
+        """
+
+    @abc.abstractmethod
+    def run_all_sync(self):
+        """
+        Run all sync tests, including both warmup and duration.
+        """
+
+    @abc.abstractmethod
+    async def run_all_async(self):
+        """
+        Run all async tests, including both warmup and duration.
+        """
+
+
+class _PerfStressTestBase(_PerfStressTestABC):
+    """Base class for implementing a python perf test."""
+
+    args = {}
+    _global_parallel_index_lock = threading.Lock()
+    _global_parallel_index = 0
+
+    def __init__(self, arguments):
+        self.args = arguments
+        self._completed_operations = 0
+        self._last_completion_time = None
+
+        with _PerfStressTestBase._global_parallel_index_lock:
+            self._parallel_index = _PerfStressTestBase._global_parallel_index
+            _PerfStressTestBase._global_parallel_index += 1
+
+    @property
+    def completed_operations(self) -> int:
+        """
+        Total number of operations completed by run_all().
+        Reset after warmup.
+        """
+        return self._completed_operations
+
+    @property
+    def last_completion_time(self) -> datetime.datetime:
+        """
+        Elapsed time between start of warmup/run and last completed operation.
+        Reset after warmup.
+        """
+        return self._last_completion_time
+
+    @property
+    def latencies(self) -> List[datetime.timedelta]:
+        """
+        Elapsed time between start and end of each completed operation.
+        Reset after warmup.
+        Only populated if "--latency" option is enabled.
+        NOT YET SUPPORTED IN PYTHON
+        """
+        raise NotImplementedError()
+
+    @property
+    def corrected_latencies(self) -> List[datetime.timedelta]:
+        """
+        Elapsed time between scheduled start and actual end of each completed operation.
+        Reset after warmup.
+        Only populated if both "--latency" and "--rate" options are enabled.
+        NOT YET SUPPORTED IN PYTHON
+        """
+        raise NotImplementedError()
+
+    @property
+    def pending_operations(self) -> Tuple[datetime.timedelta, int]:
+        """
+        Channel containing the scheduled start time of each operation.
+        Also includes a Stopwatch measuring elapsed time since scheduled start.
+        """
+        raise NotImplementedError()
+
+    async def global_setup(self) -> None:
+        """
+        Setup called once across all parallel test instances.
+        Used to setup state that can be used by all test instances.
+        """
+        return
+
+    async def global_cleanup(self) -> None:
+        """
+        Cleanup called once across all parallel test instances.
+        Used to cleanup state that can be used by all test instances.
+        """
+        return
+
+    async def setup(self) -> None:
+        """
+        Setup called once per parallel test instance.
+        Used to setup state specific to this test instance.
+        """
+        return
+
+    async def cleanup(self):
+        """
+        Cleanup called once per parallel test instance.
+        Used to cleanup state specific to this test instance.
+        """
+        return
+
+    async def post_setup(self) -> None:
+        """
+        Post-setup called once per parallel test instance.
+        Used by base classes to setup state (like test-proxy) after all derived class setup is complete.
+        """
+        return
+
+    async def pre_cleanup(self) -> None:
+        """
+        Pre-cleanup called once per parallel test instance.
+        Used by base classes to cleanup state (like test-proxy) before all derived class cleanup runs.
+        """
+        return
+
+    async def close(self):
+        """
+        Close any open client resources/connections per parallel test instance.
+        """
+        return
+
+    def run_all_sync(self):
+        """
+        Run all sync tests, including both warmup and duration.
+        """
+        raise NotImplementedError("run_all_sync must be implemented for {}".format(self.__class__.__name__))
+
+    async def run_all_async(self):
+        """
+        Run all async tests, including both warmup and duration.
+        """
+        raise NotImplementedError("run_all_async must be implemented for {}".format(self.__class__.__name__))
