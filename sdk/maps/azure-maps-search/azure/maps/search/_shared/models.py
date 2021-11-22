cc326ce280293a841b879bc7376f995620169fcd
Alex Tsao <alextts627@gmail.com>
2021-11-22 10:03:57 +0800

    Update Shared models

--- sdk/maps/azure-maps-search/azure/maps/search/_shared/models.py
+++ sdk/maps/azure-maps-search/azure/maps/search/_shared/models.py
@@ -1,3 +1,20 @@
-from ._generated.models import *
+from .._generated.models import *
+import msrest.serialization
+from typing import List
 
-__all__ = ['SearchModels']
+class Coordinates(msrest.serialization.Model):
+    _attribute_map = {
+        'latitude': {'key': 'latitude', 'type': 'float'},
+        'longitude': {'key': 'longitude', 'type': 'float'},
+    }
+
+    def __init__(
+        self,
+        **kwargs
+    ):
+        super(Coordinates, self).__init__(**kwargs)
+        self.latitude = kwargs.get('latitude', None)
+        self.longitude = kwargs.get('longitude', None)
+
+    def toCoordinateList(self): # type: (...) -> List[float]
+        return [self.latitude, self.longitude]
