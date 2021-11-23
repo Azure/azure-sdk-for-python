70987786209c743a63a9be1c9b6accb8c6b02f09
sammiee5311 <sammiee5311@gmail.com>
2021-11-23 11:27:08 +0900

    [EventHubs]Update async samples

--- sdk/eventhub/azure-eventhub/samples/async_samples/send_and_receive_amqp_annotated_message_async.py
+++ sdk/eventhub/azure-eventhub/samples/async_samples/send_and_receive_amqp_annotated_message_async.py
@@ -114,5 +114,4 @@ async def main():
     await receive_and_parse_message(consumer)
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
