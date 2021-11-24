0432797ec9fef1651106806ec74c2612661056fc
Petr Švihlík <petrsvihlik@microsoft.com>
2021-11-25 00:11:59 +0100

    initial implementation of configurable autorefresh

--- sdk/communication/azure-communication-identity/azure/communication/identity/_shared/user_credential.py
+++ sdk/communication/azure-communication-identity/azure/communication/identity/_shared/user_credential.py
@@ -4,7 +4,8 @@
 # license information.
 # --------------------------------------------------------------------------
 from threading import Lock, Condition
-from datetime import datetime, timedelta
+from datetime import timedelta
+from threading import Timer
 
 from typing import (  # pylint: disable=unused-import
     cast,
@@ -23,7 +24,7 @@ class CommunicationTokenCredential(object):
     :param str token: The token used to authenticate to an Azure Communication service
     :keyword token_refresher: The async token refresher to provide capacity to fetch fresh token
     :keyword refresh_proactively: Whether to refresh the token proactively or not
-    :keyword refresh_time_before_token_expiry: The time before the token expires to refresh the token
+    :keyword refresh_time_before_expiry: The time before the token expires to refresh the token
     :raises: TypeError
     """
 
@@ -35,15 +36,24 @@ class CommunicationTokenCredential(object):
                  **kwargs  # type: Any
                  ):
         if not isinstance(token, six.string_types):
-            raise TypeError("token must be a string.")
+            raise TypeError("Token must be a string.")
         self._token = create_access_token(token)
         self._token_refresher = kwargs.pop('token_refresher', None)
         self._refresh_proactively = kwargs.pop('refresh_proactively', False)
-        self._refresh_time_before_token_expiry = kwargs.pop('refresh_time_before_token_expiry', timedelta(
+        self._refresh_time_before_expiry = kwargs.pop('refresh_time_before_expiry', timedelta(
             minutes=self._DEFAULT_AUTOREFRESH_INTERVAL_MINUTES))
-
+        self._timer = None
         self._lock = Condition(Lock())
         self._some_thread_refreshing = False
+        if(self._refresh_proactively):
+            self._schedule_refresh()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        if self._timer is not None:
+            self._timer.cancel()
 
     def get_token(self):
         # type () -> ~azure.core.credentials.AccessToken
@@ -54,8 +64,11 @@ class CommunicationTokenCredential(object):
         if not self._token_refresher or not self._token_expiring():
             return self._token
 
-        should_this_thread_refresh = False
+        self._update_token_and_reschedule()
+        return self._token
 
+    def _update_token_and_reschedule(self):
+        should_this_thread_refresh = False
         with self._lock:
             while self._token_expiring():
                 if self._some_thread_refreshing:
@@ -82,19 +95,31 @@ class CommunicationTokenCredential(object):
                     self._lock.notify_all()
 
                 raise
+        if(self._refresh_proactively):
+            self._schedule_refresh()
         return self._token
+
+    def _schedule_refresh(self):
+        if self._timer is not None:
+            self._timer.cancel()
+
+        timespan = self._token.expires_on - \
+            get_current_utc_as_int() - self._refresh_time_before_expiry.total_seconds()
+        self._timer = Timer(timespan, self._update_token_and_reschedule)
+        self._timer.start()
 
     def _wait_till_inprogress_thread_finish_refreshing(self):
         self._lock.release()
         self._lock.acquire()
 
     def _token_expiring(self):
+        if(self._refresh_proactively):
+            interval = self._refresh_time_before_expiry
+        else:
+            interval = timedelta(
+                minutes=self._ON_DEMAND_REFRESHING_INTERVAL_MINUTES)
         return self._token.expires_on - get_current_utc_as_int() <\
-            timedelta(minutes=self._ON_DEMAND_REFRESHING_INTERVAL_MINUTES).total_seconds()
+            interval.total_seconds()
 
     def _is_currenttoken_valid(self):
-        return self._get_utc_now() < self._token.expires_on
-
-    @classmethod
-    def _get_utc_now(cls):
-        return datetime.now().replace(tzinfo=TZ_UTC)
+        return get_current_utc_as_int() < self._token.expires_on
