c3a8859cbedf423093757a549e313e7df96cc0ec
JoongWonChoi <qweasd1130@naver.com>
2021-11-30 14:25:25 +0900

    update text-analytics-samples -- asyncio.run(...)

--- sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_analyze_actions_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_analyze_actions_async.py
@@ -161,5 +161,5 @@ async def main():
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
+    
--- sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_authentication_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_authentication_async.py
@@ -91,5 +91,4 @@ async def main():
     await sample_authentication_with_azure_active_directory_async()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
