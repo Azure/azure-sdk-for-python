ef1f9af44d75453581a0eb94600d5e6607437570
Krista Pratico <krpratic@microsoft.com>
2021-11-30 15:52:10 -0800

    convert data_feed_ingestion

--- sdk/metricsadvisor/azure-ai-metricsadvisor/tests/test_data_feed_ingestion_aad.py
+++ sdk/metricsadvisor/azure-ai-metricsadvisor/tests/test_data_feed_ingestion_aad.py
@@ -6,39 +6,49 @@
 # --------------------------------------------------------------------------
 
 import datetime
+import functools
 from dateutil.tz import tzutc
 import pytest
-from base_testcase_aad import TestMetricsAdvisorAdministrationClientBase
+from devtools_testutils import recorded_by_proxy
+from azure.ai.metricsadvisor import MetricsAdvisorAdministrationClient
+from base_testcase import TestMetricsAdvisorAdministrationClientBase, MetricsAdvisorClientPreparer
+MetricsAdvisorPreparer = functools.partial(MetricsAdvisorClientPreparer, MetricsAdvisorAdministrationClient, aad=True)
 
 
 class TestMetricsAdvisorAdministrationClient(TestMetricsAdvisorAdministrationClientBase):
 
-    def test_get_data_feed_ingestion_progress(self):
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_get_data_feed_ingestion_progress(self, client):
 
-        ingestion = self.admin_client.get_data_feed_ingestion_progress(
+        ingestion = client.get_data_feed_ingestion_progress(
             data_feed_id=self.data_feed_id
         )
         assert ingestion.latest_success_timestamp is not None
         assert ingestion.latest_active_timestamp is not None
 
-    def test_list_data_feed_ingestion_status(self):
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_list_data_feed_ingestion_status(self, client):
 
-        ingestions = self.admin_client.list_data_feed_ingestion_status(
+        ingestions = client.list_data_feed_ingestion_status(
             data_feed_id=self.data_feed_id,
             start_time=datetime.datetime(2021, 8, 9, tzinfo=tzutc()),
             end_time=datetime.datetime(2021, 9, 16, tzinfo=tzutc()),
         )
         assert len(list(ingestions)) > 0
 
-    def test_list_data_feed_ingestion_status_with_skip(self):
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_list_data_feed_ingestion_status_with_skip(self, client):
 
-        ingestions = self.admin_client.list_data_feed_ingestion_status(
+        ingestions = client.list_data_feed_ingestion_status(
             data_feed_id=self.data_feed_id,
             start_time=datetime.datetime(2021, 8, 9, tzinfo=tzutc()),
             end_time=datetime.datetime(2021, 9, 16, tzinfo=tzutc()),
         )
 
-        ingestions_with_skips = self.admin_client.list_data_feed_ingestion_status(
+        ingestions_with_skips = client.list_data_feed_ingestion_status(
             data_feed_id=self.data_feed_id,
             start_time=datetime.datetime(2021, 8, 9, tzinfo=tzutc()),
             end_time=datetime.datetime(2021, 9, 16, tzinfo=tzutc()),
@@ -48,10 +58,11 @@ class TestMetricsAdvisorAdministrationClient(TestMetricsAdvisorAdministrationCli
         ingestions_with_skips_list = list(ingestions_with_skips)
         assert len(ingestions_list) == len(ingestions_with_skips_list) + 5
 
-    def test_refresh_data_feed_ingestion(self):
-        self.admin_client.refresh_data_feed_ingestion(
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_refresh_data_feed_ingestion(self, client):
+        client.refresh_data_feed_ingestion(
             self.data_feed_id,
             start_time=datetime.datetime(2021, 10, 1, tzinfo=tzutc()),
             end_time=datetime.datetime(2021, 10, 2, tzinfo=tzutc()),
         )
-
