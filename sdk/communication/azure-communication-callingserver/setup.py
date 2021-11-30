c6f6e4de409e5e79e7714add22fd26d0fc892eb0
Franko Morales <67804607+cochi2@users.noreply.github.com>
2021-11-30 14:00:29 -0800

    Matching Communication.Identity core's version (#21948)
    * Matching azure.core dependency with Communication.Identity

--- sdk/communication/azure-communication-callingserver/setup.py
+++ sdk/communication/azure-communication-callingserver/setup.py
@@ -61,7 +61,7 @@ setup(
         'azure.communication'
     ]),
    install_requires=[
-        'azure-core<2.0.0,>=1.16.0',
+        'azure-core<2.0.0,>=1.19.1',
         'msrest>=0.6.0',
         'six>=1.11.0',
         'validators'
