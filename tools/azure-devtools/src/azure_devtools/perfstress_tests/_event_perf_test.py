d88fb67cf605b0bd706d1dc1e1bac6e6e74448f3
antisch <antisch@microsoft.com>
2021-12-03 13:14:36 -0800

    Added event sample test

--- tools/azure-devtools/src/azure_devtools/perfstress_tests/_event_perf_test.py
+++ tools/azure-devtools/src/azure_devtools/perfstress_tests/_event_perf_test.py
@@ -3,93 +3,106 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import cProfile
-import os
 import asyncio
 import threading
 import time
-from typing import Optional, Any, Dict, List
-
-from urllib.parse import urljoin
+from collections import defaultdict
 
 from ._perf_stress_base import _PerfTestBase
-from ._policies import PerfTestProxyPolicy
 
 
 class EventPerfTest(_PerfTestBase):
 
     def __init__(self, arguments):
         super().__init__(arguments)
-        self._thread_lock = threading.Lock()
-        self._thread_condition = threading.Condition()
-        self._async_lock = asyncio.Lock()
-        self._async_condition = asyncio.Condition()
-        self._start_time = None
-        self._duration = None
+        if self.args.profile:
+            raise NotImplementedError("Profiler support for event tests pending.")
+
+        if self.args.sync:
+            self._condition = threading.Condition()
+        else:
+            self._condition = asyncio.Condition()
+        self._start_time = time.time()
         self._error = None
+        self._processing = None
+        self._completed_parallel_operations = defaultdict(int)
 
-    def event_raised_sync(self, *args, **kwargs):
-        with self._thread_lock:
-            self._completed_operations += 1
-            self._last_completion_time = time.time() - self._start_time
+    @property
+    def completed_operations(self) -> int:
+        """
+        Total number of operations completed by run_all().
+        Reset after warmup.
+        """
+        return sum(self._completed_parallel_operations.values())
+
+    def event_raised_sync(self, index, num_events=1):
+        self._completed_parallel_operations[index] += num_events
+        self._last_completion_time = time.time() - self._start_time
     
-    def error_raised_sync(self, *args, **kwargs):
-        self._error = True
+    def error_raised_sync(self, error):
+        with self._condition:
+            self._error = error
+            self._condition.notify_all()
 
-    async def event_raised_async(self, *args, **kwargs):
-        async with self._async_lock:
-            self._completed_operations += 1
-            self._last_completion_time = time.time() - self._start_time
+    async def event_raised_async(self, index, num_events=1):
+        self._completed_parallel_operations[index] += num_events
+        self._last_completion_time = time.time() - self._start_time
 
-    async def error_raised_async(self, *args, **kwargs):
-        self._error = True
-    
-    def shutdown_condition(self):
-        return self._error or self._last_completion_time >= self._duration
+    async def error_raised_async(self, error):
+        async with self._condition:
+            self._error = error
+            self._condition.notify_all()
+
+    async def setup(self) -> None:
+        """
+        Setup called once per parallel test instance.
+        Used to setup state specific to this test instance.
+        """
+        if self.args.sync:
+            self._processing = threading.Thread(target=self.start_events_sync)
+            self._processing.daemon = True
+            self._processing.start()
+        else:
+            self._processing = asyncio.ensure_future(self.start_events_async())
+
+    async def cleanup(self) -> None:
+        """
+        Cleanup called once per parallel test instance.
+        Used to cleanup state specific to this test instance.
+        """
+        if self.args.sync:
+            self.stop_events_sync()
+            self._processing.join()
+        else:
+            await self.stop_events_async()
+            await self._processing
+        try:
+            raise self._error
+        except TypeError:
+            pass
 
     def run_all_sync(self, duration: int) -> None:
         """
         Run all sync tests, including both warmup and duration.
         """
-        self._completed_operations = 0
-        self._last_completion_time = 0.0
-        self._start_time = time.time()
-        self._duration = duration
-        if self.args.profile:
-            raise NotImplementedError("Profiler support for event tests pending.")
-        else:
-            with self._thread_condition:
-                thread = threading.Thread(target=self.start_events_sync)
-                thread.daemon = True
-                thread.start()
-                self._thread_condition.wait_for(self.shutdown_condition)
-                self.stop_events_sync()
-                thread.join()
-        try:
-            raise self._error
-        except TypeError:
-            pass
+        with self._condition:
+            self._completed_parallel_operations = defaultdict(int)
+            self._last_completion_time = 0.0
+            self._start_time = time.time()
+            self._condition.wait(timeout=duration)
 
     async def run_all_async(self, duration: int) -> None:
         """
         Run all async tests, including both warmup and duration.
         """
-        self._completed_operations = 0
-        self._last_completion_time = 0.0
-        self._start_time = time.time()
-        self._duration = duration
-        if self.args.profile:
-            raise NotImplementedError("Profiler support for event tests pending.")
-        else:
-            async with self._async_condition:
-                task = asyncio.ensure_future(self.start_events_async())
-                await self._async_condition.wait_for(self.shutdown_condition)
-                await self.stop_events_async()
-                await task
-        try:
-            raise self._error
-        except TypeError:
-            pass
+        async with self._condition:
+            self._completed_parallel_operations = defaultdict(int)
+            self._last_completion_time = 0.0
+            self._start_time = time.time()
+            try:
+                await asyncio.wait_for(self._condition.wait(), timeout=duration)
+            except asyncio.TimeoutError:
+                pass
 
     def start_events_sync(self) -> None:
         """
