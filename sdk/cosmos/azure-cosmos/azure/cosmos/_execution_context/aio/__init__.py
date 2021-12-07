93c06914a052e612d4aabe2bc10554b9c306dd53
Simon Moreno <30335873+simorenoh@users.noreply.github.com>
2021-12-07 15:23:17 -0500

    [Cosmos] CosmosDB asynchronous client (#21404)
    * initial commit
    
    * Client Constructor (#20310)
    
    * Removed some stuff
    
    * Looking at constructors
    
    * Updated request
    
    * Added client close
    
    * working client creation
    
    Co-authored-by: simorenoh <simonmorenohe@gmail.com>
    
    * read database
    
    database read works, but ignored exception is returned:
    Fatal error on SSL transport
    NoneType has no attribute 'send' (_loop._proactor.send)
    RuntimeError: Event loop is closed
    Unclosed connector/ connection
    
    * Update simon_testfile.py
    
    * with coroutine
    
    Added methods needed to use async with when initializing client, but logs output "Exception ignored... Runtime Error: Event loop is closed"
    
    * Update simon_testfile.py
    
    * small changes
    
    * async with returns no exceptions
    
    * async read container
    
    * async item read
    
    * cleaning up
    
    * create item/ database methods
    
    * item delete working
    
    * docs replace functionality
    
    missing upsert and other resources
    
    * upsert functionality
    
    missing read_all_items and both query methods for container class
    
    * missing query methods
    
    * CRUD for udf, sproc, triggers
    
    * initial query logic + container methods
    
    * missing some execution logic and tests
    
    * oops
    
    * fully working queries
    
    * small fix to query_items()
    
    also fixed README and added examples_async
    
    * Update _cosmos_client_connection_async.py
    
    * Update _cosmos_client_connection.py
    
    * documentation update
    
    * updated MIT dates and get_user_client() description
    
    * Update CHANGELOG.md
    
    * Delete simon_testfile.py
    
    * leftover retry utility
    
    * Update README.md
    
    * docs and removed six package
    
    * changes based on comments
    
    still missing discussion resolution on SSL verification and tests for async functionality under test module (apart from samples which are basically end to end tests)
    
    * small change in type hints
    
    * updated readme
    
    * fixes based on conversations
    
    * added missing type comments
    
    * update changelog for ci pipeline
    
    * added typehints, moved params into keywords, added decorators, made _connection_policy private
    
    * changes based on sync with central sdk
    
    * remove is_system_key from scripts (only used in execute_sproc)
    
    is_system_key verifies that an empty partition key is properly dealt with if ['partitionKey']['systemKey'] exists in the container options - however, we do not allow containers to be created with empty partition key values in the python sdk, so the functionality is needless
    
    * Revert "remove is_system_key from scripts (only used in execute_sproc)"
    
    Reverting last commit, will find way to init is_system_key for now
    
    * async script proxy using composition
    
    * pylint
    
    * capitalized constants
    
    * Apply suggestions from code review
    
    Clarifying comments for README
    
    Co-authored-by: Gahl Levy <75269480+gahl-levy@users.noreply.github.com>
    
    * closing python code snippet
    
    * last doc updates
    
    * Update sdk/cosmos/azure-cosmos/CHANGELOG.md
    
    Co-authored-by: Simon Moreno <30335873+simorenoh@users.noreply.github.com>
    
    Co-authored-by: annatisch <antisch@microsoft.com>
    Co-authored-by: Gahl Levy <75269480+gahl-levy@users.noreply.github.com>
    Co-authored-by: Travis Prescott <tjprescott@users.noreply.github.com>

--- sdk/cosmos/azure-cosmos/azure/cosmos/_execution_context/aio/__init__.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/_execution_context/aio/__init__.py
@@ -1,5 +1,5 @@
 # The MIT License (MIT)
-# Copyright (c) 2014 Microsoft Corporation
+# Copyright (c) 2021 Microsoft Corporation
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -18,5 +18,3 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-VERSION = "4.3.0b1"
