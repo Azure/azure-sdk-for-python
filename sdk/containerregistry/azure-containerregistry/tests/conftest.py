93227f5325842eb830808be0d15eb69fae1d0e06
YalinLi0312 <yall@microsoft.com>
2021-12-15 03:33:01 -0800

    Migrate tests to the new test proxy infra

--- sdk/containerregistry/azure-containerregistry/tests/conftest.py
+++ sdk/containerregistry/azure-containerregistry/tests/conftest.py
@@ -5,8 +5,34 @@
 # license information.
 # --------------------------------------------------------------------------
 
+import sys
+
+import pytest
+from devtools_testutils import add_general_regex_sanitizer, test_proxy
+
 # Fixture
 from testcase import load_registry
 
+# Ignore async tests for Python < 3.5
+collect_ignore_glob = []
+if sys.version_info < (3, 5):
+    collect_ignore_glob.append("*_async.py")
+
+
 def pytest_configure(config):
     config.addinivalue_line("usefixtures", "load_registry")
+    
+@pytest.fixture(scope="session", autouse=True)
+def add_sanitizers(test_proxy):
+    # sanitizes table/cosmos account names in URLs
+    add_general_regex_sanitizer(
+        value="fakeendpoint",
+        regex="(?<=\\/\\/)[a-z]+(?=(?:|-secondary)\\.(?:table|blob|queue)\\.(?:cosmos|core)\\."
+              "(?:azure|windows)\\.(?:com|net))",
+    )
+    # sanitizes random UUIDs that are sent in batch request headers and bodies
+    add_general_regex_sanitizer(
+        value="00000000-0000-0000-0000-000000000000",
+        regex="batch[a-z]*_([0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b)",
+        group_for_replace="1",
+    )
