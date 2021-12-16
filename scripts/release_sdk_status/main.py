1c8b6a930db426e0d5ac99ece8825f8d269606bf
BigCat20196 <1095260342@qq.com>
2021-12-16 11:01:49 +0800

    test

--- scripts/release_sdk_status/main.py
+++ scripts/release_sdk_status/main.py
@@ -296,8 +296,9 @@ def main():
     all_sdk_status = sdk_info_from_pypi(sdk_info, cli_dependency)
     all_sdk_status1 = test_sdk(all_sdk_status)
     print('**')
-    print(os.getenv('CONN_STR'))
+    print(os.getenv('SWAGGER_REPO'))
     print(os.getenv('SDK_PATH'))
+    print(os.getenv('SDK_PATH1'))
 
     print_check('pwd', path=os.getenv('SDK_PATH'))
     print_check('ls', path=os.getenv('SDK_PATH'))
