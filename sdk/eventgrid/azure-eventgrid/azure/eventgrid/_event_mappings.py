ad9c440c58bd6a344c9791ac74d264227e257d07
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2021-12-16 13:00:36 -0800

    warning

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/_event_mappings.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/_event_mappings.py
@@ -1,6 +1,8 @@
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
+
+# THE VALUES IN THE ENUM ARE AUTO-GENERATED. DO NOT EDIT THIS MANUALLY.
 # --------------------------------------------------------------------------------------------
 from enum import Enum
 
