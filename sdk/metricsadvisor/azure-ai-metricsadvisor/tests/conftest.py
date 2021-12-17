bedaa4cf9d989876096978d374561ad4ddc71f09
Krista Pratico <krpratic@microsoft.com>
2021-12-17 13:27:38 -0800

    updates to drop 2.7 support

--- sdk/metricsadvisor/azure-ai-metricsadvisor/tests/conftest.py
+++ sdk/metricsadvisor/azure-ai-metricsadvisor/tests/conftest.py
@@ -3,12 +3,3 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # -------------------------------------------------------------------------
-
-import sys
-
-import pytest
-
-# Ignore async tests for Python < 3.5
-collect_ignore = []
-if sys.version_info < (3, 5):
-    collect_ignore.append("async_tests")
