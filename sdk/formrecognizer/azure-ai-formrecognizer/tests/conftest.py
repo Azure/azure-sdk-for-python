a8c60deb913843928fc4a2b1922f5ceda82034c1
Catalina Peralta <catalinaperaltah@hotmail.com>
2021-12-17 16:19:22 -0800

    remove unused code from conftest.py

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/conftest.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/conftest.py
@@ -15,11 +15,6 @@ collect_ignore_glob = []
 if sys.version_info < (3, 6):
     collect_ignore_glob.append("*_async.py")
 
-# autouse=True will trigger this fixture on each pytest run, even if it's not explicitly used by a test method
-# @pytest.fixture(scope="session", autouse=True)
-# def start_proxy(test_proxy):
-#     return
-
 @pytest.fixture(scope="session", autouse=True)
 def add_sanitizers(test_proxy):
     add_remove_header_sanitizer(headers="Ocp-Apim-Subscription-Key")
