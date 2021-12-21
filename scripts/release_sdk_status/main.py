fb52e86acf1b14bc7d35a23cf0ee50d1479d0d9b
BigCat20196 <1095260342@qq.com>
2021-12-21 17:27:08 +0800

    fix path

--- scripts/release_sdk_status/main.py
+++ scripts/release_sdk_status/main.py
@@ -212,7 +212,8 @@ def run_playback_test(service_name):
     print(f'****{service_name}*****')
     if os.path.exists(test_path):
         print_check('pip install -r dev_requirements.txt', path=service_path)
-        print_check('python setup.py install', path=service_path)
+        print_check('pip install -e .', path=service_path)
+        # print_check('python setup.py install', path=service_path)
         if os.path.exists(coverage_path+'/operations') and os.path.exists(coverage_path+'/models'):
             operations_path = coverage_path+'/operations'
             models_path = coverage_path+'/models'
