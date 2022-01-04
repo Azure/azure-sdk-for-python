847e5e2ebe3ea66d930c7bdced5cf4cef8555a59
catalinaperalta <catalinaperaltah@hotmail.com>
2022-01-04 11:37:02 -0800

    [formrecognizer] Migrate to test proxy (#21822)
    * use assert in testcase
    
    * replace with assert is not none
    
    * update remaining assertions
    
    * initial test proxy work
    
    * add ci.yml update
    
    * temp
    
    * temp
    
    * test fixes
    
    * change assert for error
    
    * rstrip endpoint
    
    * add sanitizers
    
    * clean up replacers and FormRecognizerTest, move test vars
    
    * use pytest.raises
    
    * update test_business_card.py
    
    * add business card recordings
    
    * replace blob sas urls
    
    * fix deleted receipt test
    
    * update business card tests
    
    * update content tests
    
    * update custom forms tests
    
    * update custom model tests
    
    * update container url sanitizer and custom model import
    
    * update general document tests
    
    * update layout tests
    
    * delete business card recordings
    
    * remove all old recordings
    
    * update dac prebuilt tests
    
    * rstrip async client endpoint
    
    * add oauth functions back in
    
    * update compose tests
    
    * skip copy model tests
    
    * update dmac mgmt tests
    
    * update dmac training tests
    
    * update ftc compose tests
    
    * update ftc copy model
    
    * update conftest access token sanitizer
    
    * update ftc mgmt tests
    
    * update ftc training tests
    
    * update get children tests
    
    * update frc identity doc tests
    
    * update frc invoice tests
    
    * update frc receipt tests
    
    * update multiapi tests
    
    * clean up empty recordings
    
    * remove extra empty recording
    
    * update preparer values to match recordings
    
    * dont check for body matching in requests that change during playback
    
    * add playback condition in _get_blob_url
    
    * default testing_container_sas_url
    
    * set bodiless matcher for url tests
    
    * set bodiless matcher for mgmt tests using DMAC and FTC
    
    * move rstrip up in code
    
    * remove unused code from conftest.py
    
    * make large file tests run only live
    
    * add missing imports
    
    * remove extra space
    
    * remove some more large recordings
    
    * add missing recording
    
    * add more recordings

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_dac_analyze_custom_model_async.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_dac_analyze_custom_model_async.py
@@ -4,7 +4,10 @@
 # Licensed under the MIT License.
 # ------------------------------------
 
+import pytest
 import functools
+from devtools_testutils.aio import recorded_by_proxy_async
+from devtools_testutils import set_bodiless_matcher
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer._generated.v2021_09_30_preview.models import AnalyzeResultOperation
 from azure.ai.formrecognizer.aio import DocumentAnalysisClient, DocumentModelAdministrationClient
@@ -21,22 +24,24 @@ DocumentModelAdministrationClientPreparer = functools.partial(_GlobalClientPrepa
 class TestDACAnalyzeCustomModelAsync(AsyncFormRecognizerTest):
 
     @FormRecognizerPreparer()
-    async def test_analyze_document_none_model_id(self, formrecognizer_test_endpoint, formrecognizer_test_api_key):
+    async def test_analyze_document_none_model_id(self, formrecognizer_test_endpoint, formrecognizer_test_api_key, **kwargs):
         client = DocumentAnalysisClient(formrecognizer_test_endpoint, AzureKeyCredential(formrecognizer_test_api_key))
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             async with client:
                 await client.begin_analyze_document(model=None, document=b"xx")
 
     @FormRecognizerPreparer()
-    async def test_analyze_document_empty_model_id(self, formrecognizer_test_endpoint, formrecognizer_test_api_key):
+    async def test_analyze_document_empty_model_id(self, formrecognizer_test_endpoint, formrecognizer_test_api_key, **kwargs):
         client = DocumentAnalysisClient(formrecognizer_test_endpoint, AzureKeyCredential(formrecognizer_test_api_key))
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             async with client:
                 await client.begin_analyze_document(model="", document=b"xx")
 
     @FormRecognizerPreparer()
     @DocumentModelAdministrationClientPreparer()
-    async def test_custom_document_transform(self, client, formrecognizer_storage_container_sas_url):
+    @recorded_by_proxy_async
+    async def test_custom_document_transform(self, client, formrecognizer_storage_container_sas_url, **kwargs):
+        set_bodiless_matcher()
         da_client = client.get_document_analysis_client()
 
         responses = []
@@ -82,7 +87,9 @@ class TestDACAnalyzeCustomModelAsync(AsyncFormRecognizerTest):
 
     @FormRecognizerPreparer()
     @DocumentModelAdministrationClientPreparer()
-    async def test_custom_document_multipage_transform(self, client, formrecognizer_multipage_storage_container_sas_url):
+    @recorded_by_proxy_async
+    async def test_custom_document_multipage_transform(self, client, formrecognizer_multipage_storage_container_sas_url, **kwargs):
+        set_bodiless_matcher()
         da_client = client.get_document_analysis_client()
 
         responses = []
@@ -128,7 +135,9 @@ class TestDACAnalyzeCustomModelAsync(AsyncFormRecognizerTest):
 
     @FormRecognizerPreparer()
     @DocumentModelAdministrationClientPreparer()
-    async def test_custom_document_selection_mark(self, client, formrecognizer_selection_mark_storage_container_sas_url):
+    @recorded_by_proxy_async
+    async def test_custom_document_selection_mark(self, client, formrecognizer_selection_mark_storage_container_sas_url, **kwargs):
+        set_bodiless_matcher()
         da_client = client.get_document_analysis_client()
         with open(self.selection_form_pdf, "rb") as fd:
             myfile = fd.read()
@@ -172,7 +181,9 @@ class TestDACAnalyzeCustomModelAsync(AsyncFormRecognizerTest):
 
     @FormRecognizerPreparer()
     @DocumentModelAdministrationClientPreparer()
-    async def test_pages_kwarg_specified(self, client, formrecognizer_storage_container_sas_url):
+    @recorded_by_proxy_async
+    async def test_pages_kwarg_specified(self, client, formrecognizer_storage_container_sas_url, **kwargs):
+        set_bodiless_matcher()
         da_client = client.get_document_analysis_client()
 
         with open(self.form_jpg, "rb") as fd:
@@ -190,7 +201,9 @@ class TestDACAnalyzeCustomModelAsync(AsyncFormRecognizerTest):
 
     @FormRecognizerPreparer()
     @DocumentModelAdministrationClientPreparer()
-    async def test_custom_document_signature_field(self, client, formrecognizer_storage_container_sas_url):
+    @recorded_by_proxy_async
+    async def test_custom_document_signature_field(self, client, formrecognizer_storage_container_sas_url, **kwargs):
+        set_bodiless_matcher()
         da_client = client.get_document_analysis_client()
 
         with open(self.form_jpg, "rb") as fd:
