2c61ec190f36b8418fbd98126f962bed74c53a94
Aigerim Beishenbekova <aigerimb@DESKTOP-2660OE9.corp.microsoft.com>
2022-01-04 20:00:47 +0600

    commented from test_proactive_refresher_should_be_called_after_specified_time

--- sdk/communication/azure-communication-identity/tests/test_user_credential.py
+++ sdk/communication/azure-communication-identity/tests/test_user_credential.py
@@ -61,8 +61,7 @@ class TestCommunicationTokenCredential(TestCase):
         self.assertEqual(refresher.call_count, 2)
         self.assertEqual(access_token.token, self.expired_token)
 
-    # @patch_threading_timer(user_credential.__name__+'.Timer')
-    def test_uses_initial_token_as_expected(self):  # , timer_mock):
+    def test_uses_initial_token_as_expected(self):
         refresher = MagicMock(
             return_value=self.expired_token)
         credential = CommunicationTokenCredential(
@@ -73,7 +72,7 @@ class TestCommunicationTokenCredential(TestCase):
         self.assertEqual(refresher.call_count, 0)
         self.assertEqual(access_token.token, self.sample_token)
 
-    '''def test_proactive_refresher_should_not_be_called_before_specified_time(self):
+    def test_proactive_refresher_should_not_be_called_before_specified_time(self):
         refresh_minutes = 30
         token_validity_minutes = 60
         start_timestamp = get_current_utc_as_int()
@@ -100,7 +99,7 @@ class TestCommunicationTokenCredential(TestCase):
         # check that next refresh is always scheduled
         assert credential._timer is not None
 
-    def test_proactive_refresher_should_be_called_after_specified_time(self):
+    '''def test_proactive_refresher_should_be_called_after_specified_time(self):
         refresh_minutes = 30
         token_validity_minutes = 60
         start_timestamp = get_current_utc_as_int()
