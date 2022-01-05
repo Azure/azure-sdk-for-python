fbaf84032e60648517985a33f3709ea2ffce011f
Jacob Lauzon <jalauzon@microsoft.com>
2022-01-05 15:27:56 -0800

    Update azure-core dependency to 1.11.0

--- sdk/storage/azure-storage-file-share/setup.py
+++ sdk/storage/azure-storage-file-share/setup.py
@@ -79,7 +79,7 @@ setup(
         'tests',
     ]),
     install_requires=[
-        "azure-core<2.0.0,>=1.10.0",
+        "azure-core<2.0.0,>=1.11.0",
         "msrest>=0.6.21",
         "cryptography>=2.1.4"
     ],
