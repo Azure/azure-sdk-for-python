69a4255e845e2db9af987e80fa5368cdf7f67ea5
Xiang Yan <xiangsjtu@gmail.com>
2022-01-05 11:10:39 -0800

    enable identity for py27 (#22341)
    * enable identity for py27
    
    * update

--- sdk/appconfiguration/azure-appconfiguration/setup.py
+++ sdk/appconfiguration/azure-appconfiguration/setup.py
@@ -59,7 +59,7 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
