492abbcb636bfe06b50df2bd84702a99b0c55995
swathipil <76007337+swathipil@users.noreply.github.com>
2022-01-06 14:33:27 -0800

    drop Python 2.7/3.6 (#22320)

--- sdk/servicebus/azure-servicebus/conftest.py
+++ sdk/servicebus/azure-servicebus/conftest.py
@@ -10,11 +10,7 @@ import uuid
 
 import pytest
 
-# Ignore async tests for Python < 3.5
 collect_ignore = []
-if sys.version_info < (3, 5):
-    collect_ignore.append("tests/async_tests")
-    collect_ignore.append("samples/async_samples")
 
 # Only run stress tests on request.
 if not any([arg.startswith('test_stress') or arg.endswith('StressTest') for arg in sys.argv]):
