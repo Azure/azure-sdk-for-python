ff33d03dcf2a3121a7caee5c3755b767b2307507
SDKAuto <sdkautomation@microsoft.com>
2022-01-06 10:18:04 +0000

    CodeGen from PR 17155 in Azure/azure-rest-api-specs Subscription RP v2021-10-01 endpoint fix, s360 correctness (#17155)
    * Copied the last item from private repo and renamed to 2021-10-01
    
    * Updated the version to 2021-10-01
    
    * Error response is updated. It is required by OpenAPI Validation Tools(linter)
    
    * http status response on post method is updated to 201 from 202; required by OpenAPI Validation Tools(linter)
    
    * description is added in #definitions.Operation.properties.display; required by OpenAPI Validation Tools(linter)
    
    * updated error response to match it previous
    
    * systemData properties are added; required by Swagger validation on the PR
    
    * updated readme.md for the new version
    
    * removed a file getSubscriptionOperation.json as long as it is not referenced; required by Swagger Avocado on the PR
    
    * Modified style by npm prettier
    
    * executed npm run prettier; required by prettier check on PR
    
    * added type:object; required by swagger LintDiff
    
    * added type:object; required by swagger LintDiff
    
    * reverted back the post response of acceptOwnership to 202
    
    * http status response on post method is updated to 201 from 202; required by OpenAPI Validation Tools(linter)
    
    * reverted back the post response of acceptOwnership to 202
    
    * Updated property name value to subscriptionId
    
    * Added missing property in response body of Get List of Alias subscription API
    
    * Updated response of default policies from 201 to 200 based on API response
    
    * Updated example for response of default policies from 201 to 200 based on API response
    
    * s360 fix: error response is fixed on managementpartner
    
    * added Microsoft.Subscription/stable/2016-06-01/subscriptions.json in input file list
    
    * Lint diff errors are fixed
    
    * reverting back the changes for 2-16-06-01
    
    * added v2016-06-01 for the new
    
    * executed prettier
    
    Co-authored-by: Ochi <ochisamand@microsoft.com>

--- sdk/managementpartner/azure-mgmt-managementpartner/azure/mgmt/managementpartner/aio/__init__.py
+++ sdk/managementpartner/azure-mgmt-managementpartner/azure/mgmt/managementpartner/aio/__init__.py
@@ -8,3 +8,8 @@
 
 from ._ace_provisioning_management_partner_api import ACEProvisioningManagementPartnerAPI
 __all__ = ['ACEProvisioningManagementPartnerAPI']
+
+# `._patch.py` is used for handwritten extensions to the generated code
+# Example: https://github.com/Azure/azure-sdk-for-python/blob/main/doc/dev/customize_code/how-to-patch-sdk-code.md
+from ._patch import patch_sdk
+patch_sdk()
