28fbba51406ba537190a8133847232e5eb84388e
Catalina Peralta <catalinaperaltah@hotmail.com>
2022-01-07 18:30:19 -0800

    add perf tests to cover response handling

---
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/perfstress_tests/perf_dmac_list_models.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+import os
+from azure_devtools.perfstress_tests import PerfStressTest
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentModelAdministrationClient
+from azure.ai.formrecognizer.aio import DocumentModelAdministrationClient as AsyncDocumentModelAdministrationClient
+
+class ListModels(PerfStressTest):   
+
+    def __init__(self, arguments):
+        super().__init__(arguments)
+
+        # read test related env vars
+        formrecognizer_test_endpoint = os.environ["FORMRECOGNIZER_TEST_ENDPOINT"]
+        form_recognizer_account_key = os.environ["FORMRECOGNIZER_TEST_API_KEY"]
+
+        # assign the clients that will be used in the perf tests
+        self.admin_client = DocumentModelAdministrationClient(formrecognizer_test_endpoint, AzureKeyCredential(form_recognizer_account_key))
+        self.async_admin_client = AsyncDocumentModelAdministrationClient(formrecognizer_test_endpoint, AzureKeyCredential(form_recognizer_account_key))
+
+    async def close(self):
+        """This is run after cleanup."""
+        await self.async_admin_client.close()
+        self.admin_client.close()
+        await super().close()
+
+    def run_sync(self):
+        """The synchronous perf test."""
+        result = self.admin_client.list_models()
+        assert result
+
+    async def run_async(self):
+        """The asynchronous perf test."""
+        result = self.async_admin_client.list_models()
+        assert result
