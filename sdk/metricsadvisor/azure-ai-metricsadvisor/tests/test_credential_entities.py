a6a5a7dcb4780b87fdde2b749d7eb6af14d07eaf
Krista Pratico <krpratic@microsoft.com>
2022-01-07 17:20:30 -0800

    [metricsadvisor] convert to test proxy (#21974)
    * update sync base testcases and conftest to use test proxy
    
    * remove dep on unittest
    
    * add sanitizers for test secrets
    
    * convert one test
    
    * fix weird spacing
    
    * refactor base testcase into preparer
    
    * convert test_alert_config.py + recordings
    
    * recordings
    
    * updates for aad tests
    
    * test_credential and test_credential_entities, no recordings b/c need to fix sanitizer
    
    * add rstrip / to client endpoint
    
    * convert data_feed_ingestion
    
    * convert data_feeds
    
    * convert detection_config
    
    * convert hooks
    
    * update ma client tests
    
    * refactor to simplify base test class and secrets needed
    
    * refactor, parametrize auth option
    
    * refactor / parametrize async + add preparer
    
    * convert async tests to proxy
    
    * add oauth sanitizer
    
    * add test proxy to ci.yaml
    
    * update testcases and have proxy auto start
    
    * rename test_id -> ids, fix clean up so we raise better errors, add more randomization in names
    
    * rerecord and delete old recordings
    
    * rename tests to fix max path length and try removing max parallel for live tests
    
    * ugh fix for py2.7
    
    * fix envars for live tests
    
    * fixes for live tests
    
    * adjust list data feeds tests to be less flaky
    
    * add rstrip before sanitizing

--- sdk/metricsadvisor/azure-ai-metricsadvisor/tests/test_credential_entities.py
+++ sdk/metricsadvisor/azure-ai-metricsadvisor/tests/test_credential_entities.py
@@ -6,74 +6,108 @@
 # --------------------------------------------------------------------------
 
 import pytest
-
+import functools
+from devtools_testutils import recorded_by_proxy
+from azure.ai.metricsadvisor import MetricsAdvisorAdministrationClient
 from azure.ai.metricsadvisor.models import (
     DatasourceSqlConnectionString,
     DatasourceDataLakeGen2SharedKey,
     DatasourceServicePrincipal,
     DatasourceServicePrincipalInKeyVault
 )
-from base_testcase import TestMetricsAdvisorAdministrationClientBase
+from base_testcase import TestMetricsAdvisorClientBase, MetricsAdvisorClientPreparer, CREDENTIALS, ids
+MetricsAdvisorPreparer = functools.partial(MetricsAdvisorClientPreparer, MetricsAdvisorAdministrationClient, aad=False)
 
 
-class TestMetricsAdvisorAdministrationClient(TestMetricsAdvisorAdministrationClientBase):
+class TestMetricsAdvisorAdministrationClient(TestMetricsAdvisorClientBase):
 
-    def test_create_datasource_sql_connection_string(self):
+    @pytest.mark.parametrize("credential", CREDENTIALS, ids=ids)
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_create_datasource_sql_connection_string(self, client, variables):
         credential_name = self.create_random_name("testsqlcredential")
+        if self.is_live:
+            variables["credential_name"] = credential_name
         try:
-            credential = self.admin_client.create_datasource_credential(
+            credential = client.create_datasource_credential(
                 datasource_credential=DatasourceSqlConnectionString(
-                    name=credential_name,
+                    name=variables["credential_name"],
                     connection_string=self.sql_server_connection_string,
                     description="my credential",
                 )
             )
-            self.assertIsNotNone(credential.id)
-            self.assertEqual(credential.name, credential_name)
-            self.assertEqual(credential.credential_type, 'AzureSQLConnectionString')
-        finally:
-            self.admin_client.delete_datasource_credential(credential.id)
+            if self.is_live:
+                variables["credential_id"] = credential.id
+            assert credential.id is not None
+            assert credential.name == variables["credential_name"]
+            assert credential.credential_type == 'AzureSQLConnectionString'
 
-    def test_datasource_datalake_gen2_shared_key(self):
+        finally:
+            self.clean_up(client.delete_datasource_credential, variables, key="credential_id")
+        return variables
+
+    @pytest.mark.parametrize("credential", CREDENTIALS, ids=ids)
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_datasource_datalake_gen2_shared_key(self, client, variables):
         credential_name = self.create_random_name("testdatalakecredential")
+        if self.is_live:
+            variables["credential_name"] = credential_name
         try:
-            credential = self.admin_client.create_datasource_credential(
+            credential = client.create_datasource_credential(
                 datasource_credential=DatasourceDataLakeGen2SharedKey(
-                    name=credential_name,
-                    account_key=self.azure_datalake_account_key,
+                    name=variables["credential_name"],
+                    account_key="azure_datalake_account_key",
                     description="my credential",
                 )
             )
-            self.assertIsNotNone(credential.id)
-            self.assertEqual(credential.name, credential_name)
-            self.assertEqual(credential.credential_type, 'DataLakeGen2SharedKey')
-        finally:
-            self.admin_client.delete_datasource_credential(credential.id)
+            if self.is_live:
+                variables["credential_id"] = credential.id
+            assert credential.id is not None
+            assert credential.name == variables["credential_name"]
+            assert credential.credential_type == 'DataLakeGen2SharedKey'
 
-    def test_datasource_service_principal(self):
+        finally:
+            self.clean_up(client.delete_datasource_credential, variables, key="credential_id")
+        return variables
+
+    @pytest.mark.parametrize("credential", CREDENTIALS, ids=ids)
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_datasource_service_principal(self, client, variables):
         credential_name = self.create_random_name("testserviceprincipalcredential")
+        if self.is_live:
+            variables["credential_name"] = credential_name
         try:
-            credential = self.admin_client.create_datasource_credential(
+            credential = client.create_datasource_credential(
                 datasource_credential=DatasourceServicePrincipal(
-                    name=credential_name,
+                    name=variables["credential_name"],
                     client_id="client_id",
                     client_secret="client_secret",
                     tenant_id="tenant_id",
                     description="my credential",
                 )
             )
-            self.assertIsNotNone(credential.id)
-            self.assertEqual(credential.name, credential_name)
-            self.assertEqual(credential.credential_type, 'ServicePrincipal')
+            if self.is_live:
+                variables["credential_id"] = credential.id
+            assert credential.id is not None
+            assert credential.name == variables["credential_name"]
+            assert credential.credential_type == 'ServicePrincipal'
         finally:
-            self.admin_client.delete_datasource_credential(credential.id)
+            self.clean_up(client.delete_datasource_credential, variables, key="credential_id")
+        return variables
 
-    def test_datasource_service_principal_in_kv(self):
+    @pytest.mark.parametrize("credential", CREDENTIALS, ids=ids)
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_datasource_service_principal_in_kv(self, client, variables):
         credential_name = self.create_random_name("testserviceprincipalcredential")
+        if self.is_live:
+            variables["credential_name"] = credential_name
         try:
-            credential = self.admin_client.create_datasource_credential(
+            credential = client.create_datasource_credential(
                 datasource_credential=DatasourceServicePrincipalInKeyVault(
-                    name=credential_name,
+                    name=variables["credential_name"],
                     key_vault_endpoint="key_vault_endpoint",
                     key_vault_client_id="key_vault_client_id",
                     key_vault_client_secret="key_vault_client_secret",
@@ -83,88 +117,128 @@ class TestMetricsAdvisorAdministrationClient(TestMetricsAdvisorAdministrationCli
                     description="my credential",
                 )
             )
-            self.assertIsNotNone(credential.id)
-            self.assertEqual(credential.name, credential_name)
-            self.assertEqual(credential.credential_type, 'ServicePrincipalInKV')
+            if self.is_live:
+                variables["credential_id"] = credential.id
+            assert credential.id is not None
+            assert credential.name == variables["credential_name"]
+            assert credential.credential_type == 'ServicePrincipalInKV'
         finally:
-            self.admin_client.delete_datasource_credential(credential.id)
+            self.clean_up(client.delete_datasource_credential, variables, key="credential_id")
+        return variables
 
-    def test_list_datasource_credentials(self):
+    @pytest.mark.parametrize("credential", CREDENTIALS, ids=ids)
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_list_datasource_credentials(self, client, variables):
         credential_name = self.create_random_name("testsqlcredential")
+        if self.is_live:
+            variables["credential_name"] = credential_name
         try:
-            credential = self.admin_client.create_datasource_credential(
+            credential = client.create_datasource_credential(
                 datasource_credential=DatasourceSqlConnectionString(
-                    name=credential_name,
+                    name=variables["credential_name"],
                     connection_string=self.sql_server_connection_string,
                     description="my credential",
                 )
             )
-            credentials = self.admin_client.list_datasource_credentials()
+            if self.is_live:
+                variables["credential_id"] = credential.id
+            credentials = client.list_datasource_credentials()
             assert len(list(credentials)) > 0
         finally:
-            self.admin_client.delete_datasource_credential(credential.id)
+            self.clean_up(client.delete_datasource_credential, variables, key="credential_id")
+        return variables
 
-    def test_update_datasource_sql_connection_string(self):
+    @pytest.mark.parametrize("credential", CREDENTIALS, ids=ids)
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_update_datasource_sql_connection_string(self, client, variables):
         credential_name = self.create_random_name("testsqlcredential")
+        if self.is_live:
+            variables["credential_name"] = credential_name
         try:
-            credential = self.admin_client.create_datasource_credential(
+            credential = client.create_datasource_credential(
                 datasource_credential=DatasourceSqlConnectionString(
-                    name=credential_name,
+                    name=variables["credential_name"],
                     connection_string=self.sql_server_connection_string,
                     description="my credential",
                 )
             )
+            if self.is_live:
+                variables["credential_id"] = credential.id
             credential.connection_string = "update"
             credential.description = "update"
-            credential_updated = self.admin_client.update_datasource_credential(credential)
-            self.assertEqual(credential_updated.description, "update")
+            credential_updated = client.update_datasource_credential(credential)
+            assert credential_updated.description == "update"
         finally:
-            self.admin_client.delete_datasource_credential(credential.id)
+            self.clean_up(client.delete_datasource_credential, variables, key="credential_id")
+        return variables
 
-    def test_update_datasource_datalake_gen2_shared_key(self):
+    @pytest.mark.parametrize("credential", CREDENTIALS, ids=ids)
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_update_datasource_datalake_gen2_shared_key(self, client, variables):
         credential_name = self.create_random_name("testdatalakecredential")
+        if self.is_live:
+            variables["credential_name"] = credential_name
         try:
-            credential = self.admin_client.create_datasource_credential(
+            credential = client.create_datasource_credential(
                 datasource_credential=DatasourceDataLakeGen2SharedKey(
-                    name=credential_name,
-                    account_key=self.azure_datalake_account_key,
+                    name=variables["credential_name"],
+                    account_key="azure_datalake_account_key",
                     description="my credential",
                 )
             )
+            if self.is_live:
+                variables["credential_id"] = credential.id
             credential.account_key = "update"
             credential.description = "update"
-            credential_updated = self.admin_client.update_datasource_credential(credential)
-            self.assertEqual(credential_updated.description, "update")
+            credential_updated = client.update_datasource_credential(credential)
+            assert credential_updated.description == "update"
         finally:
-            self.admin_client.delete_datasource_credential(credential.id)
+            self.clean_up(client.delete_datasource_credential, variables, key="credential_id")
+        return variables
 
-    def test_update_datasource_service_principal(self):
+    @pytest.mark.parametrize("credential", CREDENTIALS, ids=ids)
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_update_datasource_service_principal(self, client, variables):
         credential_name = self.create_random_name("testserviceprincipalcredential")
+        if self.is_live:
+            variables["credential_name"] = credential_name
         try:
-            credential = self.admin_client.create_datasource_credential(
+            credential = client.create_datasource_credential(
                 datasource_credential=DatasourceServicePrincipal(
-                    name=credential_name,
+                    name=variables["credential_name"],
                     client_id="client_id",
                     client_secret="client_secret",
                     tenant_id="tenant_id",
                     description="my credential",
                 )
             )
+            if self.is_live:
+                variables["credential_id"] = credential.id
             credential.client_id = "update"
             credential.client_secret = "update"
             credential.tenant_id = "update"
             credential.description = "update"
-            credential_updated = self.admin_client.update_datasource_credential(credential)
-            self.assertEqual(credential_updated.description, "update")
+            credential_updated = client.update_datasource_credential(credential)
+            assert credential_updated.description == "update"
         finally:
-            self.admin_client.delete_datasource_credential(credential.id)
+            self.clean_up(client.delete_datasource_credential, variables, key="credential_id")
+        return variables
 
-    def test_update_datasource_service_principal_in_kv(self):
+    @pytest.mark.parametrize("credential", CREDENTIALS, ids=ids)
+    @MetricsAdvisorPreparer()
+    @recorded_by_proxy
+    def test_update_datasource_service_principal_in_kv(self, client, variables):
         credential_name = self.create_random_name("testserviceprincipalcredential")
+        if self.is_live:
+            variables["credential_name"] = credential_name
         try:
-            credential = self.admin_client.create_datasource_credential(
+            credential = client.create_datasource_credential(
                 datasource_credential=DatasourceServicePrincipalInKeyVault(
-                    name=credential_name,
+                    name=variables["credential_name"],
                     key_vault_endpoint="key_vault_endpoint",
                     key_vault_client_id="key_vault_client_id",
                     key_vault_client_secret="key_vault_client_secret",
@@ -174,6 +248,8 @@ class TestMetricsAdvisorAdministrationClient(TestMetricsAdvisorAdministrationCli
                     description="my credential",
                 )
             )
+            if self.is_live:
+                variables["credential_id"] = credential.id
             credential.key_vault_endpoint = "update"
             credential.key_vault_client_id = "update"
             credential.key_vault_client_secret = "update"
@@ -181,7 +257,8 @@ class TestMetricsAdvisorAdministrationClient(TestMetricsAdvisorAdministrationCli
             credential.service_principal_secret_name_in_kv = "update"
             credential.tenant_id = "update"
             credential.description = "update"
-            credential_updated = self.admin_client.update_datasource_credential(credential)
-            self.assertEqual(credential_updated.description, "update")
+            credential_updated = client.update_datasource_credential(credential)
+            assert credential_updated.description == "update"
         finally:
-            self.admin_client.delete_datasource_credential(credential.id)
+            self.clean_up(client.delete_datasource_credential, variables, key="credential_id")
+        return variables
