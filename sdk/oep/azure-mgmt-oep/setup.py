fa52ffdc4bdd2328fdb69697476932482887b451
PythonSdkPipelines <PythonSdkPipelines>
2022-01-10 07:35:13 +0000

    version,CHANGELOG

--- sdk/oep/azure-mgmt-oep/setup.py
+++ sdk/oep/azure-mgmt-oep/setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages, setup
 
 # Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-mgmt-oep"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_PPRINT_NAME = "Oep Management"
 
 # a-b-c => a/b/c
 package_folder_path = PACKAGE_NAME.replace('-', '/')
