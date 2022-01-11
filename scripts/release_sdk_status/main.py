3c4e1f9742733019934431737baf285032a5ee43
Yuchao Yan <yuchaoyan@microsoft.com>
2022-01-11 09:52:04 +0800

    [Release sdk status] optimize statistics (#22378)
    * add parameter
    
    * trigger swagger pipeline and sheduled trigger
    
    * avoid duplicated trigger
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * optmization
    
    * special case
    
    * special case
    
    * optimize track2 ga version rule
    
    * delete upload code

--- scripts/release_sdk_status/main.py
+++ scripts/release_sdk_status/main.py
@@ -10,9 +10,13 @@ from pathlib import Path
 from typing import List
 from github import Github
 from github.Repository import Repository
+import time
+from packaging.version import parse
+from pathlib import Path
 
 SERVICE_TEST_PATH = {}
 MAIN_REPO_SWAGGER = 'https://github.com/Azure/azure-rest-api-specs/tree/main'
+PR_URL = 'https://github.com/Azure/azure-rest-api-specs/pull/'
 
 def my_print(cmd):
     print('== ' + cmd + ' ==\n')
@@ -26,6 +30,12 @@ def print_check(cmd, path=''):
         sp.check_call(cmd, shell=True)
 
 
+def version_sort(versions: List[str]) -> List[str]:
+    versions_package = [parse(version) for version in versions]
+    versions_package.sort()
+    return [str(version) for version in versions_package]
+
+
 class PyPIClient:
     def __init__(self, host="https://pypi.org", package_name='', track_config='',
                  readme_link='', rm_link='', cli_version='', multi_api=''):
@@ -34,10 +44,10 @@ class PyPIClient:
         self._package_name = package_name
         self.version_date_dict = {}
         self.whether_track2 = None  # whether published track2 to pypi
-        self.track1_ga = 'NO'
-        self.track1_latest = 'NA'
-        self.track2_ga = 'NO'
-        self.track2_latest = 'NA'
+        self.track1_ga_version = 'NA'
+        self.track1_latest_version = 'NA'
+        self.track2_ga_version = 'NA'
+        self.track2_latest_version = 'NA'
         self.pypi_link = 'NA'
         self.track_config = track_config
         self.readme_link = readme_link
@@ -88,63 +98,73 @@ class PyPIClient:
         if 199 < response.status_code < 400:
             self.get_release_dict(response)
             self.bot_analysis()
-            return '{},{},{},{},{},{},{},{},{},{},{},{},{},'.format(self._package_name,
-                                                                    self.pypi_link,
-                                                                    self.track1_latest,
-                                                                    self.version_date_dict[self.track1_latest],
-                                                                    self.track1_ga,
-                                                                    self.track2_latest,
-                                                                    self.track2_ga,
-                                                                    self.version_date_dict[self.track2_latest],
-                                                                    self.cli_version,
-                                                                    self.track_config,
-                                                                    self.bot_warning,
-                                                                    self.rm_link,
-                                                                    self.multi_api)
+            return '{package_name},{pypi_link},{track1_latest_version},{track1_latest_release_date},' \
+                   '{track1_ga_version},{track2_latest_version},{track2_latest_release_date},{track2_ga_version},' \
+                   '{cli_version},{track_config},{bot},{readme_link},{multiapi},'.format(
+                        package_name=self._package_name,
+                        pypi_link=self.pypi_link,
+                        track1_latest_version=self.track1_latest_version,
+                        track1_latest_release_date=self.version_date_dict[self.track1_latest_version],
+                        track1_ga_version=self.track1_ga_version,
+                        track2_latest_version=self.track2_latest_version,
+                        track2_latest_release_date=self.version_date_dict[self.track2_latest_version],
+                        track2_ga_version=self.track2_ga_version,
+                        cli_version=self.cli_version,
+                        track_config=self.track_config,
+                        bot=self.bot_warning,
+                        readme_link=self.rm_link,
+                        multiapi=self.multi_api)
         else:
             self.pypi_link = 'NA'
         return
 
-    def version_handler(self, version_list):
-        # Scenario 1
-        # rule 1: this package have track2 version
-        # rule 2: check whether CLI is using this package
-        # rule 3: by comparing the versions of CLI and package, we can judge whether cli is using track1 or 2
-        # rule 4: judge whether track1 is exist
-        # rule 5: whether track1 is GA
-        # rule 6: whether track2 is GA
-        # Scenario 2
-        # rule 7: this package doesn't have track2 version
-        # rule 8: check whether CLI is using this package
-        # rule 9: whether track1 is GA
-        ga_re = re.compile(r'[A-Za-z]')
-        version_index = 0
-        versions = list(reversed(version_list))
+    def find_track1_ga_version(self, versions: List[str]) -> None:
+        if '1.0.0' in versions:
+            self.track1_ga_version = '1.0.0'
+
+    def find_track2_ga_version(self, versions: List[str]) -> None:
         for version in versions:
-            if 'b1' in version and self.whether_track2 is None:
-                self.whether_track2 = version
-                if self.cli_version != 'NA':
-                    if int(self.cli_version.split('.')[0]) >= int(version.split('.')[0]):
-                        self.cli_version = 'track2_' + self.cli_version
-                    else:
-                        self.cli_version = 'track1_' + self.cli_version
-                if version_index != 0:
-                    self.track1_latest = versions[version_index - 1]
-                self.track2_latest = versions[-1]
-                if not re.findall(ga_re, self.track1_latest) and len(self.track1_latest) != 0 and int(
-                        self.track1_latest.split('.')[0]) > 0:
-                    self.track1_ga = 'YES'
-                if not re.findall(ga_re, self.track2_latest):
-                    self.track2_ga = 'YES'
+            if re.search(r'[a-zA-z]', version) is None:
+                self.track2_ga_version = version
                 break
-            version_index += 1
-        if self.whether_track2 is None:
-            if self.cli_version != 'NA':
-                self.cli_version = 'track1_' + self.cli_version
-            self.track1_latest = versions[-1]
-            if not re.findall(ga_re, self.track1_latest) and len(self.track1_latest) != 0 and int(
-                    self.track1_latest.split('.')[0]) > 0:
-                self.track1_ga = 'YES'
+
+    def handle_cli_version(self, track1_versions: List[str], track2_versions: List[str]) ->None:
+        if self.cli_version == 'NA':
+            return
+        if self.cli_version in track1_versions:
+            self.cli_version = 'track1_' + self.cli_version
+        elif self.cli_version in track2_versions:
+            self.cli_version = 'track2_' + self.cli_version
+        else:
+            my_print(f'do not find cli_version {self.cli_version} in track1 versions {str(track1_versions)} and '
+                     f'track2 versions {str(track2_versions)}')
+
+    def get_track1_track2_versions(self, versions: List[str]) -> (List[str], List[str]):
+        first_track2_version = ''
+        for version in versions:
+            if 'b' in version:
+                first_track2_version = version
+                my_print(f'get first track2 version {version} in {versions}')
+                break
+
+        # azure-mgmt-streamanalytics set 1.0.0rc1 as first track2 version
+        if self.get_package_name() == 'azure-mgmt-streamanalytics':
+            first_track2_version = '1.0.0rc1'
+
+        if first_track2_version:
+            idx = versions.index(first_track2_version)
+            return versions[0:idx], versions[idx:]
+        else:
+            return versions, []
+
+    def version_handler(self, version_list: List[str]):
+        versions_sorted = version_sort(version_list)
+        track1_versions, track2_versions = self.get_track1_track2_versions(versions_sorted)
+        self.find_track1_ga_version(track1_versions)
+        self.find_track2_ga_version(track2_versions)
+        self.handle_cli_version(track1_versions, track2_versions)
+        self.track1_latest_version = self.track1_latest_version if len(track1_versions) == 0 else track1_versions[-1]
+        self.track2_latest_version = self.track2_latest_version if len(track2_versions) == 0 else track2_versions[-1]
 
     def bot_analysis(self):
         # rule 1: readme.python.md must exist
@@ -247,12 +267,12 @@ def write_to_csv(sdk_status_list, csv_name):
     with open(csv_name, 'w') as file_out:
         file_out.write('package name,'
                        'pypi link,'
-                       'latest track1,'
-                       'release date,'
-                       'track1 GA,'
-                       'latest track2,'
-                       'track2 GA,'
-                       'release date,'
+                       'latest track1 version,'
+                       'latest track1 release date,'
+                       'track1 GA version,'
+                       'latest track2 version,'
+                       'latest track2 release date,'
+                       'track2 GA version,'
                        'cli dependency,'
                        'readme config,'
                        'bot advice,'
@@ -266,19 +286,27 @@ def write_to_csv(sdk_status_list, csv_name):
             [package for package in sorted(sdk_status_list, key=lambda x: x.split(',')[10], reverse=True)])
 
 
+def complement_version(version: str) -> str:
+    num = version.count('.')
+    if num == 0:
+        return f'{version}.0.0'
+    elif num == 1:
+        return f'{version}.0'
+    return version
+
+
 def get_cli_dependency():
-    CLI_URL = 'https://github.com/azure/azure-cli/blob/dev/src/azure-cli/setup.py'
-    cli_lines = project_html(CLI_URL).xpath('//table[@class="highlight tab-size js-file-line-container"]//text()')
+    g = Github(os.getenv('TOKEN'))  # please fill user_token
+    repo = g.get_repo('Azure/azure-cli')
+    cli_lines = repo.get_contents('src/azure-cli/setup.py').decoded_content.decode('UTF-8').split('\n')
     cli_dependency = {}
     for line in cli_lines:
         if 'azure-mgmt-' in line:
-            line = line[1:-1]
-            if '==' in line:
-                line = line.split('==')
-                cli_dependency[line[0]] = line[1]
-            elif '~=' in line:
-                line = line.split('~=')
-                cli_dependency[line[0]] = line[1]
+            name_pattern = re.compile(r'azure-mgmt-[a-zA-z\-]+')
+            version_pattern = re.compile(r'\d+[\.\da-z]+')
+            name = name_pattern.search(line).group(0)
+            version = version_pattern.search(line).group(0)
+            cli_dependency[name] = complement_version(version)
     return cli_dependency
 
 
@@ -301,13 +329,14 @@ def sdk_info_from_swagger():
     sdk_folder_re = re.compile('output-folder: \$\(python-sdks-folder\)/')
     resource_manager = []
     SWAGGER_FOLDER = os.getenv('SWAGGER_REPO')
-    readme_folders = glob.glob(f'{SWAGGER_FOLDER}/specification/*/resource-manager/readme.md')
+    target_file_pattern = str(Path(f'{SWAGGER_FOLDER}/specification/*/resource-manager/readme.md'))
+    readme_folders = glob.glob(target_file_pattern)
     my_print(f'total readme folders: {len(readme_folders)}')
-
     for folder in readme_folders:
         sdk_folder_path = False
         multi_api = ''
-        service_name = re.findall(r'specification/(.*?)/resource-manager/', folder)[0]
+        linux_folder = Path(folder).as_posix()
+        service_name = re.findall(r'specification/(.*?)/resource-manager/', linux_folder)[0]
         track_config = 0
         package_name = ''
         folder = folder.replace('readme.md', '')
@@ -341,6 +370,7 @@ def sdk_info_from_swagger():
         TRACK_CONFIG = {0: 'NA', 1: 'track1', 2: 'track2', 3: 'both'}
         track_config = TRACK_CONFIG.get(track_config, 'Rule error')
         readme_html = folder.replace(SWAGGER_FOLDER, MAIN_REPO_SWAGGER)
+        readme_html = Path(readme_html).as_posix()
         if package_name != '':
             resource_manager.append('{},{},{},{},{}\n'.format(package_name,
                                                               track_config,
@@ -360,14 +390,6 @@ def commit_to_github():
     print_check('git push -f origin HEAD')
 
 
-def upload_to_azure(out_file):
-    # upload to storage account(it is created in advance)
-    blob = BlobClient.from_connection_string(conn_str=os.getenv('CONN_STR'), container_name=os.getenv('FILE'),
-                                             blob_name=out_file)
-    with open(out_file, 'rb') as data:
-        blob.upload_blob(data, overwrite=True)
-
-
 def count_sdk_status():
     cli_dependency = get_cli_dependency()
     sdk_info = sdk_info_from_swagger()
@@ -376,7 +398,6 @@ def count_sdk_status():
     out_file = 'release_sdk_status.csv'
     write_to_csv(all_sdk_status, out_file)
     commit_to_github()
-    upload_to_azure(out_file)
 
 
 def get_all_readme_html() -> List[str]:
@@ -408,11 +429,20 @@ def get_latest_pr_from_readme(rest_repo: Repository, service_html: str):
 def trigger_pipeline(readme_html: List[str]) -> None:
     g = Github(os.getenv('TOKEN'))  # please fill user_token
     rest_repo = g.get_repo('Azure/azure-rest-api-specs')
+    record = set()
     for item in readme_html:
         num = get_latest_pr_from_readme(rest_repo, item)
         pr = rest_repo.get_pull(num)
-        pr.create_issue_comment(body='/azp run')
-        my_print(f'get latest PR "{num}" from {item} and comment "/azp run" successfully')
+        # TODO: skip too old PR
+
+        # avoid duplicated trigger
+        if num not in record:
+            pr.create_issue_comment(body='/azp run')
+            record.add(num)
+            time.sleep(300)
+            my_print(f'get latest PR "{PR_URL}{num}" from {item} and comment "/azp run" successfully')
+        else:
+            my_print(f'get latest PR "{PR_URL}{num}" from {item} and but it is already triggered')
 
 
 def trigger_swagger_pipeline() -> None:
