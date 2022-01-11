e9327e943003fbb2329f78c040b9842530a0211b
Xiang Yan <xiangyan@microsoft.com>
2022-01-11 13:04:59 -0800

    add back py36 support

--- sdk/synapse/azure-synapse-spark/setup.py
+++ sdk/synapse/azure-synapse-spark/setup.py
@@ -50,6 +50,8 @@ setup(
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
