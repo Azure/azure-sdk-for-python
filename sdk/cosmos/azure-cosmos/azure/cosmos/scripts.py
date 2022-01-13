ab2b0ea3ef7528900a2f2e27cc7deda440b53f70
Simon Moreno <30335873+simorenoh@users.noreply.github.com>
2022-01-13 13:14:44 -0500

    [Cosmos] Remove support for Python27, set Python36 as minimum, and update cosmos emulator pipeline (#22475)
    * removed six package and updated packaging/ documentation to reflect new standards
    
    * massive oops, will rotate keys
    
    * change 3.7 to 3.6
    
    * pylint
    
    * addressed Anna's comments

--- sdk/cosmos/azure-cosmos/azure/cosmos/scripts.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/scripts.py
@@ -24,8 +24,6 @@
 
 from typing import Any, List, Dict, Union, Iterable, Optional
 
-import six
-
 from azure.cosmos._cosmos_client_connection import CosmosClientConnection
 from ._base import build_options
 from .partition_key import NonePartitionKeyValue
@@ -55,7 +53,7 @@ class ScriptsProxy(object):
 
     def _get_resource_link(self, script_or_id, typ):
         # type: (Union[Dict[str, Any], str], str) -> str
-        if isinstance(script_or_id, six.string_types):
+        if isinstance(script_or_id, str):
             return u"{}/{}/{}".format(self.container_link, typ, script_or_id)
         return script_or_id["_self"]
 
