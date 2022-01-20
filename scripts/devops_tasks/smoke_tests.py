9cdc93e874d55183638f6fb7e9a7567dabf6501d
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2022-01-20 00:05:38 -0800

    build

--- scripts/devops_tasks/smoke_tests.py
+++ scripts/devops_tasks/smoke_tests.py
@@ -5,4 +5,4 @@ from common_tasks import get_all_track2_packages
 if __name__ == '__main__':
     packages = get_all_track2_packages('.')
     for pkg in packages:
-        importlib.import_module(pkg.replace('-', '.'))
+        importlib.import_module(pkg[0].replace('-', '.'))
