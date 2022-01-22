a42bed682143a57b018d56254a0cd7a630ccab26
wiboris <wiboris@microsoft.com>
2022-01-21 16:08:14 -0800

    updating vm

--- sdk/batch/azure-batch/tests/batch_preparers.py
+++ sdk/batch/azure-batch/tests/batch_preparers.py
@@ -183,7 +183,7 @@ class PoolPreparer(AzureMgmtPreparer):
             group = self._get_resource_group(**kwargs)
             batch_account = self._get_batch_account(**kwargs)
             user = models.UserAccount(name='task-user', password='kt#_gahr!@aGERDXA', elevation_level=models.ElevationLevel.admin)
-            vm_size = 'Standard_D1_v2'
+            vm_size = 'standard_d2_v2'
 
             if self.config == 'paas':
                 vm_size = 'small'
