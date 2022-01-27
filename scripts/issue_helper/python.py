57b4a00fa1f9a73a5d3237c743e875f42055bf81
Yuchao Yan <yuchaoyan@microsoft.com>
2022-01-27 16:44:04 +0800

    [Issue helper] pipeline to collect open issues for all languages (#22804)
    * initial
    
    * rename yml
    
    * git push function
    
    * only collect issues with label 'Mgmt'
    
    * handle exception
    
    * date format
    
    * update log print
    
    * update bot policy
    
    * skip self-create issue

---
+++ scripts/issue_helper/python.py
@@ -0,0 +1,10 @@
+from common import Common
+
+_PYTHON_OWNER = {'msyyc', 'BigCat20196'}
+_PYTHON_REPO = 'Azure/azure-sdk-for-python'
+_FILE_OUT_NAME_PYTHON = 'sdk_issue_python.md'
+
+
+def python_process() -> None:
+    instance = Common(_PYTHON_OWNER, _PYTHON_REPO, _FILE_OUT_NAME_PYTHON)
+    instance.run()
