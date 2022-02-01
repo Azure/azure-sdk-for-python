19d58b8c4a9edbc56482cf523b4c122a8d6049e4
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2022-02-01 15:07:19 -0800

    include mgmt core

--- scripts/devops_tasks/smoke_tests.py
+++ scripts/devops_tasks/smoke_tests.py
@@ -1,11 +1,15 @@
 import logging
+import importlib
 from common_tasks import get_all_track2_packages
 
 if __name__ == '__main__':
     packages = get_all_track2_packages('.')
     print(packages)
     for pkg in packages:
-        logging.info('Importing the library {}'.format(pkg[0]))
+        #logging.info('Importing the library {}'.format(pkg[0]))
         qualified_namespace = pkg[0].replace('-', '.')
         logging.info('Importing the library {}'.format(qualified_namespace))
-        exec('from {} import *'.format(qualified_namespace))
+        try:
+            importlib.__import__(qualified_namespace, globals(), locals(), ['__all__'], 0)
+        except ModuleNotFoundError as err:
+            logging.warning(err)
