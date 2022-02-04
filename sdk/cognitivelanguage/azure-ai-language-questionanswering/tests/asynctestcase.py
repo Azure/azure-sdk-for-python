31ff6c6a66476c3711a45ac34b8a2b1252f9b45c
Mo Shaban <mshaban@microsoft.com>
2022-02-04 02:46:16 +0200

    qna authoring tests (#21914)
    * generating LLC client
    
    * Update swagger directives
    
    * Python 2 support for now
    
    * update swagger reference
    
    * regenerate client
    
    * add version file
    
    * regen llc client
    
    * regen with authoring
    
    * regen with export fix update
    
    * create and deploy project sample
    
    * export import samples
    
    * some changes
    
    * knowledge source samples
    
    * update "create and deploy" sample
    
    * tmp commit
    
    * update "export import sample"
    
    * update "update knowledge sources sample"
    
    * update samples
    
    * fix "export_import" sample
    
    * minor fix
    
    * update sample for "create and deploy"
    
    * fix list sources
    
    * update "update knowledge sources" sample
    
    * fix synonyms sample
    
    * update main readme
    
    * update samples readme
    
    * remove u" python 2.7 support
    
    * adding async samples
    
    * restructure
    
    * update samples readme
    
    * add project create test
    
    * add "create and deploy" tests
    
    * add "export import" tests
    
    * add "synonyms" test
    
    * update tests
    
    * update tests
    
    * update and record tests
    
    * add missing recording
    
    * fix "create" tests
    
    * update recorded tests for "create" tests
    
    * add async "create and deploy" sample
    
    * add async test "export and import"
    
    * add async tests "update knowledge sources"
    
    * revert readme changes
    
    Co-authored-by: antisch <antisch@microsoft.com>

--- sdk/cognitivelanguage/azure-ai-language-questionanswering/tests/asynctestcase.py
+++ sdk/cognitivelanguage/azure-ai-language-questionanswering/tests/asynctestcase.py
@@ -36,3 +36,66 @@ class AsyncQuestionAnsweringTest(QuestionAnsweringTest):
 
     def generate_fake_token(self):
         return AsyncFakeTokenCredential()
+
+class QnaAuthoringAsyncHelper:
+
+    async def create_test_project(
+        client,
+        project_name = "IssacNewton",
+        is_deployable = False,
+        add_sources = False,
+        get_export_url = False,
+        delete_old_project = False,
+        add_qnas = False
+    ):
+        # create project
+        await client.create_project(
+            project_name=project_name,
+            options={
+                "description": "biography of Sir Issac Newton",
+                "language": "en",
+                "multilingualResource": True,
+                "settings": {
+                    "defaultAnswer": "no answer"
+                }
+            })
+
+        # add sources
+        if is_deployable or add_sources:
+            await QnaAuthoringAsyncHelper.add_sources(client, project_name)
+
+        if get_export_url:
+            return await QnaAuthoringAsyncHelper.export_project(client, project_name, delete_project=delete_old_project)
+
+    async def add_sources(client, project_name):
+        update_sources_poller = await client.begin_update_sources(
+            project_name=project_name,
+            sources=[
+                {
+                    "op": "add",
+                    "value": {
+                        "displayName": "Issac Newton Bio",
+                        "sourceUri": "https://wikipedia.org/wiki/Isaac_Newton",
+                        "sourceKind": "url"
+                    }
+                }
+            ]
+        )
+        await update_sources_poller.result()
+
+    async def export_project(client, project_name, delete_project=True):
+        # export project
+        export_poller = await client.begin_export(
+            project_name=project_name,
+            format="json"
+        )
+        result = await export_poller.result()
+
+        # delete old project
+        if delete_project:
+            delete_poller = await client.begin_delete_project(
+                project_name=project_name
+            )
+            await delete_poller.result()
+
+        return result["resultUrl"]
