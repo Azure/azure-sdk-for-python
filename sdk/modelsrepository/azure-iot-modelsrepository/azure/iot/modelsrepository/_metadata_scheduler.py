f0bfdd018ef2416434ad5f9a2a80d7b3e77ad044
Victoria Litvinova <vilit@microsoft.com>
2022-02-04 14:38:35 -0800

    remove unneeded class

--- sdk/modelsrepository/azure-iot-modelsrepository/azure/iot/modelsrepository/_metadata_scheduler.py
+++ sdk/modelsrepository/azure-iot-modelsrepository/azure/iot/modelsrepository/_metadata_scheduler.py
@@ -3,47 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-import logging
-
-_LOGGER = logging.getLogger(__name__)
-
-
-class RepositoryFeatures(object):
-    def __init__(self, **kwargs):
-        """
-        :keyword expanded: Whether the repository supports expanded models.
-        :paramtype expanded: bool
-        :keyword index: Whether the repository has an index file.
-        :paramtype index: bool
-        """
-        self.expanded = kwargs.get("expanded")
-        self.index = kwargs.get("index")
-
-
-class ModelsRepositoryMetadata(object):
-    def __init__(self, **kwargs):
-        """
-        ModelsRepositoryMetadata is used to deserialize the contents
-        of the Models Repository metadata file as defined by
-        https://github.com/Azure/iot-plugandplay-models-tools/wiki/Publishing-Metadata.
-
-        :keyword commitId: Current Commit Id used
-        :paramtype commitId: str
-        :keyword publishDateUtc: Publish Date
-        :paramtype publishDateUtc: str
-        :keyword sourceRepo: Source Repository name
-        :paramtype sourceRepo: str
-        :keyword totalModelCount: Total number of Models in Model Repository
-        :paramtype totalModelCount: int
-        :keyword features: Model Repository features
-        :paramtype features: RepositoryFeatures
-        """
-        self.commit_id = kwargs.get("commitId")
-        self.publish_date_utc = kwargs.get("publishDateUtc")
-        self.source_repo = kwargs.get("sourceRepo")
-        self.total_model_count = kwargs.get("totalModelCount")
-        self.features = RepositoryFeatures(kwargs.get("features"))
-
 
 class MetadataScheduler(object):
     def __init__(self, enabled=True):
