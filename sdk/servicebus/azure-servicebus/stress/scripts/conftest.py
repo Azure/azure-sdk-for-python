786bc157c684f41f8c5fddc10916457c861b308c
Swathi Pillalamarri <swathip@microsoft.com>
2022-02-10 15:10:00 -0800

    lint + ignore stress tests

--- sdk/servicebus/azure-servicebus/stress/scripts/conftest.py
+++ sdk/servicebus/azure-servicebus/stress/scripts/conftest.py
@@ -13,6 +13,24 @@ LOGGING_ENABLE = False
 ENV_FILE = os.environ.get('ENV_FILE')
 load_dotenv(dotenv_path=ENV_FILE, override=True)
 
+# Allow us to pass stress_test_duration from the command line. TODO: may not actually be using this.
+def pytest_addoption(parser):
+    parser.addoption('--stress_test_duration_seconds', action="store", default=None)
+
+# Note: This is duplicated between here and the basic conftest, so that it does not throw warnings if you're
+# running locally to this SDK. (Everything works properly, pytest just makes a bit of noise.)
+def pytest_configure(config):
+    # register an additional marker
+    config.addinivalue_line(
+        "markers", "liveTest: mark test to be a live test only"
+    )
+    config.addinivalue_line(
+        "markers", "live_test_only: mark test to be a live test only"
+    )
+    config.addinivalue_line(
+        "markers", "playback_test_only: mark test to be a playback test only"
+    )
+
 # fixture needs to be visible from conftest
 
 @pytest.fixture(scope="session", autouse=True)
