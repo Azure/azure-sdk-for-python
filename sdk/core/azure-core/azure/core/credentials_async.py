3eec726fb9b94897df29a23a135e60ed83fbef91
McCoy Patiño <39780829+mccoyp@users.noreply.github.com>
2022-02-11 18:17:28 -0800

    [Core] Document TokenCredential implementation requirements (#22931)

--- sdk/core/azure-core/azure/core/credentials_async.py
+++ sdk/core/azure-core/azure/core/credentials_async.py
@@ -5,13 +5,27 @@
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Any
+    from typing import Any, Optional
     from typing_extensions import Protocol
     from .credentials import AccessToken
 
     class AsyncTokenCredential(Protocol):
-        async def get_token(self, *scopes: str, **kwargs: Any) -> AccessToken:
-            pass
+        """Protocol for classes able to provide OAuth tokens."""
+
+        async def get_token(
+            self, *scopes: str, claims: Optional[str] = None, tenant_id: Optional[str] = None, **kwargs: Any
+        ) -> AccessToken:
+            """Request an access token for `scopes`.
+
+            :param str scopes: The type of access needed.
+
+            :keyword str claims: Additional claims required in the token, such as those returned in a resource
+                provider's claims challenge following an authorization failure.
+            :keyword str tenant_id: Optional tenant to include in the token request.
+
+            :rtype: AccessToken
+            :return: An AccessToken instance containing the token string and its expiration time in Unix time.
+            """
 
         async def close(self) -> None:
             pass
