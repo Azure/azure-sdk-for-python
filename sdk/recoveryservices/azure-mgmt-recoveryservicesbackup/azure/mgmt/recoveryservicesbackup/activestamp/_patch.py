0ce66542eb77bc9c82bdb763215a5044738a7b1a
iscai-msft <iscai@microsoft.com>
2022-02-16 13:03:12 -0500

    patch activestamp code

--- sdk/recoveryservices/azure-mgmt-recoveryservicesbackup/azure/mgmt/recoveryservicesbackup/activestamp/_patch.py
+++ sdk/recoveryservices/azure-mgmt-recoveryservicesbackup/azure/mgmt/recoveryservicesbackup/activestamp/_patch.py
@@ -24,8 +24,42 @@
 # IN THE SOFTWARE.
 #
 # --------------------------------------------------------------------------
+import copy
+from typing import List
+import importlib
+import urllib.parse
+from ._recovery_services_backup_client import RecoveryServicesBackupClient as RecoveryServicesBackupClientGenerated
+from azure.core.pipeline.policies import SansIOHTTPPolicy
+
+class RemoveDuplicateParamsPolicy(SansIOHTTPPolicy):
+    def __init__(self, duplicate_param_names):
+        # type: (List[str]) -> None
+        self.duplicate_param_names = duplicate_param_names
+
+    def on_request(self, request):
+        parsed_url = urllib.parse.urlparse(request.http_request.url)
+        query_params = urllib.parse.parse_qs(parsed_url.query)
+        updated_query_params = copy.copy(query_params)
+        for query in query_params:
+            if query in self.duplicate_param_names and (isinstance(query_params[query], list) and len(query_params[query]) > 1):
+                updated_query_params[query] = [query_params[query][-1]]
+        # service returned will be later in the url because of how we format
+        request.http_request.url = request.http_request.url.replace(parsed_url.query, "") + urllib.parse.urlencode(updated_query_params, doseq=True)
+        return super().on_request(request)
+
+DUPLICATE_PARAMS_POLICY = RemoveDuplicateParamsPolicy(duplicate_param_names=["$filter", "$skiptoken", "api-version"])
+
+class RecoveryServicesBackupClient(RecoveryServicesBackupClientGenerated):
+    def __init__(self, *args, **kwargs):
+        per_call_policies = kwargs.pop("per_call_policies", [])
+        try:
+            per_call_policies.append(DUPLICATE_PARAMS_POLICY)
+        except AttributeError:
+            per_call_policies = [per_call_policies, DUPLICATE_PARAMS_POLICY]
+        super().__init__(*args, per_call_policies=per_call_policies, **kwargs)
 
 # This file is used for handwritten extensions to the generated code. Example:
 # https://github.com/Azure/azure-sdk-for-python/blob/main/doc/dev/customize_code/how-to-patch-sdk-code.md
 def patch_sdk():
-    pass
+    curr_package = importlib.import_module("azure.mgmt.recoveryservicesbackup.activestamp")
+    curr_package.RecoveryServicesBackupClient = RecoveryServicesBackupClient
