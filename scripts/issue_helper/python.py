62d72cdec1f829368ebb3b8254e451cd6bc8fb2a
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2022-02-22 14:55:54 +0800

    [Issue helper] Skip issue created by owners (#23165)
    * initial
    
    * rename yml
    
    * git push function
    
    * only collect issues with label 'Mgmt'
    
    * handle exception
    
    * date format
    
    * update log print
    
    * update bot policy
    
    * skip self-create issue
    
    * add ziwei and zhenbiao
    
    * Update python.py
    
    * skip self author
    
    * skip self author
    
    * skip self author
    
    * Update python.py
    
    * Update python.py
    
    Co-authored-by: msyyc <70930885+msyyc@users.noreply.github.com>
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>

--- scripts/issue_helper/python.py
+++ scripts/issue_helper/python.py
@@ -1,9 +1,22 @@
+from typing import List
+
+from github.Issue import Issue
+
 from common import Common
 
 _PYTHON_OWNER = {'msyyc', 'BigCat20196', 'Wzb123456789', 'kazrael2119'}
 _PYTHON_REPO = 'Azure/azure-sdk-for-python'
 _FILE_OUT_NAME_PYTHON = 'sdk_issue_python.md'
 
+class Python(Common):
+
+    def collect_open_issues(self) -> List[Issue]:
+        open_issues = super().collect_open_issues()
+        # Skip issue created by owners
+        filtered_issues = [i for i in open_issues if i.user.login not in self.language_owner]
+        return filtered_issues
+
+
 def python_process() -> None:
-    instance = Common(_PYTHON_OWNER, _PYTHON_REPO, _FILE_OUT_NAME_PYTHON)
+    instance = Python(_PYTHON_OWNER, _PYTHON_REPO, _FILE_OUT_NAME_PYTHON)
     instance.run()
