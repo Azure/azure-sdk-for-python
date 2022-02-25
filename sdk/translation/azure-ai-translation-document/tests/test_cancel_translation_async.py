8cc02bf850ecbd9d79ef7179675dc4e80f11f6e8
Krista Pratico <krpratic@microsoft.com>
2022-02-25 14:22:13 -0800

    [translation] convert to test proxy (#21764)
    * remove unittest dependency
    
    * initial work
    
    * some small changse
    
    * remove endpoint trailing slash on sanitizer value
    
    * updates for playback
    
    * test translation - convert to proxy + recordings
    
    * cancel, document status, supported formats tests - conversion + recordings
    
    * list translations/documents - convert and record
    
    * add test proxy to ci/tests.yml
    
    * fixes
    
    * rstrip endpoint
    
    * add oauth sanitizer
    
    * update conftest to automatically start proxy
    
    * update rstrip endpoint with better err msg
    
    * try pinning storage api version for sas urls
    
    * set bodiless matcher for sas urls
    
    * remove proxy from tests.yml
    
    * rerecord
    
    * rerecord with 1251067 image tag
    
    * use latest tag to get logging
    
    * go back to tag 1209124
    
    * try rerecord
    
    * pin new version of proxy tool
    
    * patch from main, bumping proxy version
    
    * additional logging output
    
    * sanitizers to only be test level.
    
    * import appropriate deferred_send function from sanitizers.go
    
    * this will work, but is extremely inefficient. need to do better with the SAS token overrides
    
    * few test fixes
    
    * restore everything to align with main
    
    * rerecord all tests
    
    * try modifying ppe to only run with one python version
    
    * try fixing python version filtering for ppe
    
    * switch to passing kwargs based on latest test guidance
    
    * pop variables
    
    Co-authored-by: scbedd <45376673+scbedd@users.noreply.github.com>

--- sdk/translation/azure-ai-translation-document/tests/test_cancel_translation_async.py
+++ sdk/translation/azure-ai-translation-document/tests/test_cancel_translation_async.py
@@ -7,6 +7,7 @@ import functools
 from testcase import Document
 from asynctestcase import AsyncDocumentTranslationTest
 from preparer import DocumentTranslationPreparer, DocumentTranslationClientPreparer as _DocumentTranslationClientPreparer
+from devtools_testutils.aio import recorded_by_proxy_async
 from azure.ai.translation.document import DocumentTranslationInput, TranslationTarget
 from azure.ai.translation.document.aio import DocumentTranslationClient
 from azure.core.exceptions import HttpResponseError
@@ -17,7 +18,8 @@ class TestCancelTranslation(AsyncDocumentTranslationTest):
 
     @DocumentTranslationPreparer()
     @DocumentTranslationClientPreparer()
-    async def test_cancel_translation(self, client):
+    @recorded_by_proxy_async
+    async def test_cancel_translation(self, **kwargs):
         '''
             some notes (test sporadically failing):
             1. use a large number of translations
@@ -26,19 +28,21 @@ class TestCancelTranslation(AsyncDocumentTranslationTest):
             2. wait sometime after calling 'cancel' and before calling 'get status'
                 - in order for the cancel status to propagate
         '''
+        client = kwargs.pop("client")
+        variables = kwargs.pop("variables", {})
+        # submit translation operation
+        docs_count = 8 # large number of docs
+        poller = await self._begin_and_validate_translation_with_multiple_docs_async(client, docs_count, wait=False, variables=variables)
+
+        # cancel translation
+        await client.cancel_translation(poller.id)
+
+        # check translation status
+        translation_details = await client.get_translation_status(poller.id)
+        assert translation_details.status in ["Canceled", "Canceling"]
+        self._validate_translations(translation_details)
         try:
-            # submit translation operation
-            docs_count = 8 # large number of docs
-            poller = await self._begin_and_validate_translation_with_multiple_docs_async(client, docs_count, wait=False)
-
-            # cancel translation
-            await client.cancel_translation(poller.id)
-
-            # check translation status
-            translation_details = await client.get_translation_status(poller.id)
-            assert translation_details.status in ["Canceled", "Canceling"]
-            self._validate_translations(translation_details)
-
             await poller.wait()
         except HttpResponseError:
             pass  # expected if the operation was already in a terminal state.
+        return variables
