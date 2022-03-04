9a8b0743aa70b79f831af99397d9df0b1d094b08
Jacob Lauzon <96087589+jalauzon-msft@users.noreply.github.com>
2022-03-03 18:01:20 -0800

    [Storage] Address comments from API Review for March release (#23294)

--- sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_file_client_async.py
+++ sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_file_client_async.py
@@ -710,7 +710,7 @@ class ShareFileClient(AsyncStorageAccountHostsMixin, ShareFileClientBase):
         :keyword file_last_write_time:
             Last write time for the file.
         :paramtype file_last_write_time:~datetime.datetime or str
-        :keyword dict(str,str) metadata:
+        :keyword Dict[str,str] metadata:
             A name-value pair to associate with a file storage object.
         :keyword source_lease:
             Required if the source file has an active lease. Value can be a ShareLeaseClient object
