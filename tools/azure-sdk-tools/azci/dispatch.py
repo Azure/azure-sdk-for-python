2ae3e7a339ccea79ef47f63ef58d27fb77988d1e
Scott Beddall <sbeddall@gmail.com>
2022-03-07 18:52:50 -0800

    resolve the repo root confusion

--- tools/azure-sdk-tools/azci/dispatch.py
+++ tools/azure-sdk-tools/azci/dispatch.py
@@ -2,6 +2,9 @@ import argparse
 import pdb
 import os
 
+from .run_configuration import RunConfiguration
+from .variables import ENV_NAME_REPO_ROOT
+
 
 def main() -> None:
     """
@@ -15,45 +18,60 @@ def main() -> None:
     }
 
     parser = argparse.ArgumentParser(
-        description="""This is the primary entry point for azure sdk automation during CI and local testing.  
-    While this help text only documents accepting a single positional parameter. Unmatched args will be passed on to deeper invocations. For instance
-    "pytest"-specific params like "--pdb" will be passed on to subseqeuent abstracted pytest invocations."""
+        description="""
+            This is the primary entry point for azure sdk automation during CI and local testing.  
+            While this help text only documents accepting a single positional parameter. Unmatched args will be passed on to deeper invocations. For instance
+            "pytest"-specific params like "--pdb" will be passed on to subseqeuent abstracted pytest invocations."""
     )
     parser.add_argument(
         dest="command", help=("The operation being invoked."), choices=["ask", "build", "version", "test"]
     )
     parser.add_argument(
-        "--root",
+        "--python_repo_root",
         dest="root",
         help=(
-            "The root of the azure-sdk-for-python repo. Overridden by environment variable AZURE_PYTHON_REPO_ROOT. If neither provided, CWD is used to discover the root of the repo by inference."
+            "The root of the azure-sdk-for-python repo. Overridden by environment variable "
+            + "{}. In absence of both, repo root will be evaluated based on folder traversal and guess/check.".format(
+                ENV_NAME_REPO_ROOT
+            )
         ),
         required=False,
     )
     args, unmatched = parser.parse_known_args()
-    options_mapping.get(args.command)(unmatched)
+    options_mapping.get(args.command)(unmatched, args.root)
 
 
 def determine_target_packages(glob_str: str) -> list[str]:
     pass
 
 
-def build(args):
+def build(args, repo_root_arg=None):
+    run_config = RunConfiguration(repo_root_arg, args)
+    print(run_config)
 
     pdb.set_trace()
     pass
 
 
-def test(args):
+def test(args, repo_root_arg=None):
+    run_config = RunConfiguration(args)
+    print(run_config)
+
     pdb.set_trace()
     pass
 
 
-def version(args):
+def version(args, repo_root_arg=None):
+    run_config = RunConfiguration(args)
+    print(run_config)
+
     pdb.set_trace()
     pass
 
 
-def ask(args):
+def ask(args, repo_root_arg=None):
+    run_config = RunConfiguration(args)
+    print(run_config)
+
     pdb.set_trace()
     pass
--- tools/azure-sdk-tools/azci/run_configuration.py
+++ tools/azure-sdk-tools/azci/run_configuration.py
@@ -1,10 +1,16 @@
 import logging
+import os
+import pdb
+
+from .variables import ENV_NAME_REPO_ROOT, FILES_IN_ROOT
 
 
 class RunConfiguration:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, root_folder, remaining_args, **kwargs):
         self.log_level = kwargs.get("log_level", logging.INFO)
-        configure_logging(args)
+        self.configure_logging(kwargs)
+        self.repo_root = self.resolve_repo_root(root_folder)
+        pdb.set_trace()
         pass
 
     def initialize_run(self, customizations={}):
@@ -13,8 +19,28 @@ class RunConfiguration:
     def configure_logging(self, args):
         pass
 
-    def resolve_repo_root(self, root_arg):
-        pass
+    def resolve_repo_root(self, root_folder):
+        if os.getenv(ENV_NAME_REPO_ROOT):
+            return os.path.abspath(os.getenv(ENV_NAME_REPO_ROOT))
+
+        if root_folder:
+            return os.path.abspath(root_folder)
+        else:
+            current_directory = os.getcwd()
+
+            while current_directory:
+                directory_contents = os.listdir(current_directory)
+
+                if all(item in directory_contents for item in FILES_IN_ROOT):
+                    return os.path.abspath(current_directory)
+
+                head, _ = os.path.split(current_directory)
+
+                if current_directory == head:
+                    print("azci Unable to locate root of repository. Exiting.")
+                    exit(1)
+                else:
+                    current_directory = head
 
     def __str__(self):
         return "Implement me!"
