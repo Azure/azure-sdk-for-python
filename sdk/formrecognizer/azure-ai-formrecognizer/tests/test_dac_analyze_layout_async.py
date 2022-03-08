e9ed76bc65a0fb15f569013ae1bd34d88f4abd27
catalinaperalta <catalinaperaltah@hotmail.com>
2022-03-07 16:25:16 -0800

    [formrecognizer] Improve live pipeline tests (#23239)
    * sleep for a couple of seconds between tests
    
    * sleep between tests
    
    * increase sleep time
    
    * test increased sleep time

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_dac_analyze_layout_async.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_dac_analyze_layout_async.py
@@ -20,6 +20,9 @@ DocumentAnalysisClientPreparer = functools.partial(_GlobalClientPreparer, Docume
 
 class TestDACAnalyzeLayoutAsync(AsyncFormRecognizerTest):
 
+    def teardown(self):
+        self.sleep(4)
+
     @FormRecognizerPreparer()
     @DocumentAnalysisClientPreparer()
     @recorded_by_proxy_async
