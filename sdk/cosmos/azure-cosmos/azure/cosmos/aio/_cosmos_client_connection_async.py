dbb7998c4ddf8935fec05786064a56ef8512e731
Simon Moreno <30335873+simorenoh@users.noreply.github.com>
2022-03-09 20:21:09 -0500

    [Cosmos] Add max_integrated_cache_staleness param option to item methods (#22946)
    * consistency level gets set to default found in database account
    
    * async client default change
    
    * updated docs based on finding and updated samples to reflect best practices
    
    * Update CHANGELOG.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update CHANGELOG.md
    
    * formatting
    
    * formatting
    
    * updated consistency for first request to Eventual (lowest latency)
    
    * pylint
    
    * from_connection_string methods
    
    * from_connection_string2
    
    * Update sdk/cosmos/azure-cosmos/README.md
    
    Co-authored-by: Gahl Levy <75269480+gahl-levy@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Gahl Levy <75269480+gahl-levy@users.noreply.github.com>
    
    * Update README.md
    
    * removed forceful header usage, changed setup to only check for Session consistency to start client session
    
    * need to set header if Session consistency  for updating session if needed (thanks Jake!)
    
    * Apply suggestions from code review
    
    Kushagra improved documentation and comments
    
    Co-authored-by: Kushagra Thapar <kushuthapar@gmail.com>
    
    * added test for session token
    
    * Update CHANGELOG.md
    
    * Update _cosmos_client_connection_async.py
    
    * added max_integrated_cache_staleness to item methods in containers
    
    * added validation and provisional comments
    
    * pylint
    
    * only applied to read-only operations
    
    * Update container.py
    
    * Update CHANGELOG.md
    
    * Apply suggestions from code review
    
    Co-authored-by: Kushagra Thapar <kushuthapar@gmail.com>
    
    * Update _base.py
    
    * updated param comments to mention integrated cache configuration
    
    * moved to kwargs
    
    * added tests to verify functionality
    
    * Update test_integrated_cache.py
    
    * Update test_integrated_cache.py
    
    * updates to test to ensure it works with setup
    
    * added headers test and new way to track client headers before sending out
    
    these changes will also likely be used for creating the diagnostics later on
    
    * Update test_integrated_cache.py
    
    * Create test_axq.py
    
    * Added mocking tests for max integrated cache staleness. Fixed issue with int value being false
    
    * upgrade version for release
    
    Co-authored-by: Gahl Levy <75269480+gahl-levy@users.noreply.github.com>
    Co-authored-by: Kushagra Thapar <kushuthapar@gmail.com>
    Co-authored-by: Kushagra Thapar <kuthapar@microsoft.com>

--- sdk/cosmos/azure-cosmos/azure/cosmos/aio/_cosmos_client_connection_async.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/aio/_cosmos_client_connection_async.py
@@ -141,7 +141,7 @@ class CosmosClientConnection(object):  # pylint: disable=too-many-public-methods
         if consistency_level is not None:
             self.default_headers[http_constants.HttpHeaders.ConsistencyLevel] = consistency_level
 
-        # Keeps the latest response headers from server.
+        # Keeps the latest response headers from the server.
         self.last_response_headers = None
 
         self._useMultipleWriteLocations = False
