b1024b0e8ea16ac9d583517e03a5a31a877093dd
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2022-03-15 11:22:54 -0400

    [webpubsub] switch to customizations code (#23504)

--- sdk/webpubsub/azure-messaging-webpubsubservice/azure/messaging/webpubsubservice/aio/_vendor.py
+++ sdk/webpubsub/azure-messaging-webpubsubservice/azure/messaging/webpubsubservice/aio/_vendor.py
@@ -14,24 +14,13 @@ if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from msrest import Deserializer, Serializer
 
-    from azure.core import PipelineClient
-
-
-def _format_url_section(template, **kwargs):
-    components = template.split("/")
-    while components:
-        try:
-            return template.format(**kwargs)
-        except KeyError as key:
-            formatted_components = template.split("/")
-            components = [c for c in formatted_components if "{}".format(key.args[0]) not in c]
-            template = "/".join(components)
+    from azure.core import AsyncPipelineClient
 
 
 class MixinABC(ABC):
     """DO NOT use this class. It is for internal typing use only."""
 
-    _client: "PipelineClient"
+    _client: "AsyncPipelineClient"
     _config: WebPubSubServiceClientConfiguration
     _serialize: "Serializer"
     _deserialize: "Deserializer"
