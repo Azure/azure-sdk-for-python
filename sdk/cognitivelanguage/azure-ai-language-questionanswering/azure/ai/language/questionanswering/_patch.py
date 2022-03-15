a5a868ee647ea83edaadd89675e3a04e80d55560
iscai-msft <iscai@microsoft.com>
2022-03-15 16:37:13 -0400

    update QnA client docs to include default_language

--- sdk/cognitivelanguage/azure-ai-language-questionanswering/azure/ai/language/questionanswering/_patch.py
+++ sdk/cognitivelanguage/azure-ai-language-questionanswering/azure/ai/language/questionanswering/_patch.py
@@ -11,7 +11,19 @@ from ._question_answering_client import QuestionAnsweringClient as QuestionAnswe
 
 
 class QuestionAnsweringClient(QuestionAnsweringClientGenerated):
-    __doc__ = QuestionAnsweringClientGenerated.__doc__
+    """The language service API is a suite of natural language processing (NLP) skills built with best-in-class Microsoft machine learning algorithms.
+
+    The API can be used to analyze unstructured text for tasks such as sentiment
+    analysis, key phrase extraction, language detection and question answering.
+    Further documentation can be found in https://docs.microsoft.com/azure/cognitive-services/text-analytics/overview
+
+    :param endpoint: Supported Cognitive Services endpoint (e.g.,
+     https://<resource-name>.api.cognitiveservices.azure.com).
+    :type endpoint: str
+    :param credential: Credential needed for the client to connect to Azure.
+    :type credential: ~azure.core.credentials.AzureKeyCredential
+    :keyword str default_language: Sets the default language to use for all operations.
+    """
 
     def __init__(self, endpoint: str, credential: AzureKeyCredential, **kwargs: Any) -> None:
         super().__init__(endpoint, credential, **kwargs)
