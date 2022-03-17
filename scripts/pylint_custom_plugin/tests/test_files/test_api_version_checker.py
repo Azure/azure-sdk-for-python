54639607cac581d02ae0f2395e452320a00be196
Libba Lawrence <llawrence@microsoft.com>
2022-03-17 08:27:03 -0700

    edit test file

--- scripts/pylint_custom_plugin/tests/test_files/test_api_version_checker.py
+++ scripts/pylint_custom_plugin/tests/test_files/test_api_version_checker.py
@@ -6,13 +6,12 @@ class SomeClient():
     """
     :param str endpoint: Something.
     :param credential: Something.
-    :type credential: :class:`~azure.core.credentials.AzureKeyCredential` or
-        :class:`~azure.core.credentials.TokenCredential`
+    :type credential: : Something
     :keyword api_version:
         The API version of the service to use for requests. It defaults to API version v2.1.
         Setting to an older version may result in reduced feature compatibility. To use the
         latest supported API version and features, instantiate a DocumentAnalysisClient instead.
-    :paramtype api_version: str or ~azure.ai.formrecognizer.FormRecognizerApiVersion
+    :paramtype api_version: str 
     
     """
 
