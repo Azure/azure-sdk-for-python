e9403a0456b2c740d101e4e89f482dd0531e8381
Libba Lawrence <llawrence@microsoft.com>
2022-03-17 09:36:36 -0700

    some syntax changing

--- scripts/pylint_custom_plugin/tests/test_files/test_enum_checker.py
+++ scripts/pylint_custom_plugin/tests/test_files/test_enum_checker.py
@@ -1,7 +1,7 @@
 # Test file for enum checker
 
 
-class EnumPython2(CaseInsensitiveEnumMeta(str,Enum)):
+class EnumPython2(CaseInsensitiveEnumMeta(str, Enum)):
     ONE = "one"
     TWO = "two"
 
@@ -9,4 +9,3 @@ class EnumPython3(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
     ONE = "one"
     TWO = "two"
 
-    
