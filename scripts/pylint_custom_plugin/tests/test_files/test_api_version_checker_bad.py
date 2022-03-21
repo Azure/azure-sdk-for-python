3a950198e2fb3cbc94a1de19ac39596b6dca153a
Libba Lawrence <llawrence@microsoft.com>
2022-03-21 08:38:58 -0700

    fixing docstring on bad test

--- scripts/pylint_custom_plugin/tests/test_files/test_api_version_checker_bad.py
+++ scripts/pylint_custom_plugin/tests/test_files/test_api_version_checker_bad.py
@@ -7,7 +7,7 @@ class SomeClient():
         """
         :param str endpoint: Something.
         :param credential: Something.
-        :type credential: : Something.
+        :type credential: TokenCredential.
         
         """
         pass
