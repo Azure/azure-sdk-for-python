d0b2ec35fc9671df4e325b3c1db7e6e6e05e86fc
Libba Lawrence <llawrence@microsoft.com>
2022-03-21 08:13:45 -0700

    switching around the order of keyword and param to see the effect on the checker

--- scripts/pylint_custom_plugin/tests/test_files/test_api_version_checker_good_class.py
+++ scripts/pylint_custom_plugin/tests/test_files/test_api_version_checker_good_class.py
@@ -5,13 +5,15 @@ class SomeClient():
 
     """
     :param str endpoint: Something.
-    :param credential: Something.
-    :type credential: : Something
     :keyword api_version:
         The API version of the service to use for requests. It defaults to API version v2.1.
         Setting to an older version may result in reduced feature compatibility. To use the
         latest supported API version and features, instantiate a DocumentAnalysisClient instead.
     :paramtype api_version: str 
+    :param credential: Something.
+    :type credential: : Something.
+    :keyword key : Something2.
+    
     
     """
 
