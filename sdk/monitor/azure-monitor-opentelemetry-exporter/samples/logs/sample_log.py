6fbfdebc38716895fbae0d1d571834cb291989c2
Leighton Chen <lechen@microsoft.com>
2022-03-21 14:15:40 -0700

    Implementing logging exporter for Azure Monitor (#23486)

---
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/samples/logs/sample_log.py
@@ -0,0 +1,35 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+"""
+An example to show an application using Opentelemetry logging sdk. Logging calls to the standard Python
+logging library are tracked and telemetry is exported to application insights with the AzureMonitorLogExporter.
+"""
+import os
+import logging
+
+from opentelemetry.sdk._logs import (
+    LogEmitterProvider,
+    OTLPHandler,
+    set_log_emitter_provider,
+)
+from opentelemetry.sdk._logs.export import BatchLogProcessor
+
+from azure.monitor.opentelemetry.exporter import AzureMonitorLogExporter
+
+log_emitter_provider = LogEmitterProvider()
+set_log_emitter_provider(log_emitter_provider)
+
+exporter = AzureMonitorLogExporter.from_connection_string(
+    os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+)
+
+log_emitter_provider.add_log_processor(BatchLogProcessor(exporter))
+handler = OTLPHandler()
+
+# Attach OTel handler to root logger
+logging.getLogger().addHandler(handler)
+logging.getLogger().setLevel(logging.NOTSET)
+
+logger = logging.getLogger(__name__)
+
+logger.warning("Hello World!")
