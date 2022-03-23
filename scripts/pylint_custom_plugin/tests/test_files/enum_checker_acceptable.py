c8510da36797252f97c3bb130a4bc02575f66cfc
Libba Lawrence <llawrence@microsoft.com>
2022-03-23 16:50:10 -0700

    had to import with_metaclass for py2

--- scripts/pylint_custom_plugin/tests/test_files/enum_checker_acceptable.py
+++ scripts/pylint_custom_plugin/tests/test_files/enum_checker_acceptable.py
@@ -1,5 +1,6 @@
 # Test file for enum checker
 from enum import Enum
+from six import with_metaclass
 from azure.core import CaseInsensitiveEnumMeta
 
 class EnumPython2(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
