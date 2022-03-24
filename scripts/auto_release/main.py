f852b38327513c9c246795966d4e81b1b40f3b90
Jiefeng Chen (WICRESOFT NORTH AMERICA LTD) <v-jiechen@microsoft.com>
2022-03-24 14:37:31 +0800

    test

--- scripts/auto_release/main.py
+++ scripts/auto_release/main.py
@@ -128,9 +128,8 @@ def execute_simple_command(cmd_line, cwd=None, shell=False, env=None):
     try:
         process = subprocess.Popen(
             cmd_line,
-            bufsize=5,
-            stderr=subprocess.STDOUT,
-            stdout=subprocess.PIPE,
+            # stderr=subprocess.STDOUT,
+            # stdout=subprocess.PIPE,
             universal_newlines=True,
             cwd=cwd,
             shell=shell,
