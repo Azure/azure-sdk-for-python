a450ad571a8d2c66500c518b17ca68febbffb2fa
Xiang Yan <xiangsjtu@gmail.com>
2022-03-24 17:50:45 -0700

    Update sdk/identity/azure-identity/setup.py
    Co-authored-by: McCoy Patiño <39780829+mccoyp@users.noreply.github.com>

--- sdk/identity/azure-identity/setup.py
+++ sdk/identity/azure-identity/setup.py
@@ -52,7 +52,7 @@ setup(
     author_email="azpysdkhelp@microsoft.com",
     url="https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/identity/azure-identity",
     classifiers=[
-        "Development Status :: 5 - Production/",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
