c2cb7e410d7bcda61ddc240e5a26a6ab0c0316c2
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2022-03-28 10:13:52 +0800

    Update main.py

--- scripts/auto_release/main.py
+++ scripts/auto_release/main.py
@@ -262,7 +262,7 @@ class CodegenTestPR:
     def check_file_with_packaging_tool(self):
         os.chdir(Path(f'sdk/{self.sdk_folder}'))
         print_check(f'python -m packaging_tools --build-conf azure-mgmt-{self.package_name}')
-        log(' packaging_tools --build-conf successfully ')
+        log('packaging_tools --build-conf successfully ')
 
     def check_pprint_name(self):
         pprint_name = self.package_name.capitalize()
