a5f26977c049037cda3cd9943f5fd9cbfb90b1e8
Jiefeng Chen (WICRESOFT NORTH AMERICA LTD) <v-jiechen@microsoft.com>
2022-03-30 11:27:54 +0800

    update release helper

--- scripts/release_helper/js.py
+++ scripts/release_helper/js.py
@@ -7,28 +7,23 @@ import os
 _JS_OWNER = {'qiaozha', 'lirenhe'}
 
 # 'github assignee': 'token'
-_ASSIGNEE_TOKEN_JS = {'qiaozha': os.getenv('AZURESDK_BOT_TOKEN'), 'dw511214992': os.getenv('AZURESDK_BOT_TOKEN')}
+_ASSIGNEE_TOKEN_JS = {'qiaozha': os.getenv('AZURESDK_BOT_TOKEN'), 'MaryGao': os.getenv('AZURESDK_BOT_TOKEN')}
 
-# Set the start time to 2022-02-28
-SATRAT_DAY = datetime.datetime(2022, 2, 28).date()
 
 class IssueProcessJs(IssueProcess):
-    pass
+    def auto_assign_policy(self) -> str:
+        weeks = datetime.datetime.now().isocalendar()[1]
+        assignees = list(self.assignee_candidates)
+        assignees.sort(key=list(_ASSIGNEE_TOKEN_JS.keys()).index)
+        random_idx = weeks % len(assignees)
+        return assignees[random_idx]
 
 
 class Js(Common):
     def __init__(self, issues, assignee_token, language_owner):
-        super(Js, self).__init__(issues, self.assign_policy(assignee_token), language_owner)
+        super(Js, self).__init__(issues, assignee_token, language_owner)
         self.file_out_name = 'release_js_status.md'
-
-    @staticmethod
-    def assign_policy(assignee_token):
-        assignees = list(assignee_token.keys())
-        today = datetime.datetime.today().date()
-        apart_days = (today - SATRAT_DAY).days
-        index = (apart_days // 7) % len(assignees)
-        return {assignees[index]: _ASSIGNEE_TOKEN_JS[assignees[index]]}
-
+        self.issue_process_function = IssueProcessJs
 
 def js_process(issues: List[Any]):
     instance = Js(issues, _ASSIGNEE_TOKEN_JS, _JS_OWNER)
