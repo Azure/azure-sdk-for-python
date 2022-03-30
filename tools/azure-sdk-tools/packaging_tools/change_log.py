c851c77dcc1dab0372ba88c68e8f5f72a37da80a
Yuchao Yan <yuchaoyan@microsoft.com>
2022-03-30 13:41:53 +0800

    Update tools/azure-sdk-tools/packaging_tools/change_log.py

--- tools/azure-sdk-tools/packaging_tools/change_log.py
+++ tools/azure-sdk-tools/packaging_tools/change_log.py
@@ -67,7 +67,7 @@ class ChangeLog:
             return
 
         # Is this a new operation, inside a known operation group?
-        function_name, change_parameter_name, *remaining_path = remaining_path
+        function_name, parameter_name, *remaining_path = remaining_path
         if not remaining_path:
             if is_deletion:
                 self.breaking_changes.append(_REMOVE_OPERATION.format(operation_name, function_name))
