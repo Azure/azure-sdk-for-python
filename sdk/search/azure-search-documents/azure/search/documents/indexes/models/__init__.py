356979609815261c07a24043977f14e9af722a8a
Kashif Khan <kashifkhan@microsoft.com>
2022-03-31 14:20:44 -0500

    fix pylint issues

--- sdk/search/azure-search-documents/azure/search/documents/indexes/models/__init__.py
+++ sdk/search/azure-search-documents/azure/search/documents/indexes/models/__init__.py
@@ -39,10 +39,10 @@ from .._generated.models import (
     AnalyzeResult,
     AnalyzedTokenInfo,
     AsciiFoldingTokenFilter,
-    BM25Similarity as BM25SimilarityAlgorithm,
+    BM25Similarity,
     CharFilter,
     CjkBigramTokenFilter,
-    ClassicSimilarity as ClassicSimilarityAlgorithm,
+    ClassicSimilarity,
     ClassicTokenizer,
     CommonGramTokenFilter,
     ConditionalSkill,
@@ -79,7 +79,7 @@ from .._generated.models import (
     KeyPhraseExtractionSkill,
     KeyPhraseExtractionSkillLanguage,
     KeywordMarkerTokenFilter,
-    KeywordTokenizerV2 as KeywordTokenizer,
+    KeywordTokenizerV2,
     LanguageDetectionSkill,
     LengthTokenFilter,
     LexicalAnalyzer,
@@ -102,7 +102,7 @@ from .._generated.models import (
     OcrSkill,
     OcrSkillLanguage,
     OutputFieldMappingEntry,
-    PathHierarchyTokenizerV2 as PathHierarchyTokenizer,
+    PathHierarchyTokenizerV2,
     PatternCaptureTokenFilter,
     PatternReplaceCharFilter,
     PatternReplaceTokenFilter,
@@ -137,7 +137,7 @@ from .._generated.models import (
     SentimentSkillLanguage,
     ShaperSkill,
     ShingleTokenFilter,
-    Similarity as SimilarityAlgorithm,
+    Similarity,
     SnowballTokenFilter,
     SnowballTokenFilterLanguage,
     SoftDeleteColumnDeletionDetectionPolicy,
@@ -150,7 +150,7 @@ from .._generated.models import (
     StopAnalyzer,
     StopwordsList,
     StopwordsTokenFilter,
-    Suggester as SearchSuggester,
+    Suggester,
     SynonymTokenFilter,
     TagScoringFunction,
     TagScoringParameters,
@@ -182,6 +182,26 @@ from ._models import (
     SentimentSkillVersion,
     SynonymMap,
 )
+
+class BM25SimilarityAlgorithm(BM25Similarity):
+    pass
+
+class ClassicSimilarityAlgorithm(ClassicSimilarity):
+    pass
+
+class KeywordTokenizer(KeywordTokenizerV2):
+    pass
+
+class PathHierarchyTokenizer(PathHierarchyTokenizerV2):
+    pass
+
+class SimilarityAlgorithm(Similarity):
+    pass
+
+class SearchSuggester(Suggester):
+    pass
+
+
 
 
 __all__ = (
