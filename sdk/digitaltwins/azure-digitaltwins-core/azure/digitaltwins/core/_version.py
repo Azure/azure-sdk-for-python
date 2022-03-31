6ed2c4f53f7a9a33640bd32e87ac601535c54d49
azure-sdk <azuresdk@microsoft.com>
2022-03-31 18:59:01 +0000

    Increment package version after release of azure-digitaltwins-core

--- sdk/digitaltwins/azure-digitaltwins-core/azure/digitaltwins/core/_version.py
+++ sdk/digitaltwins/azure-digitaltwins-core/azure/digitaltwins/core/_version.py
@@ -3,6 +3,6 @@
 # Licensed under the MIT License.
 # ------------------------------------
 
-VERSION = "1.2.0b1"
+VERSION = "1.2.0b2"
 
 SDK_MONIKER = "digitaltwins-core/{}".format(VERSION)  # type: str
