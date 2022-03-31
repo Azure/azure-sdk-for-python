d07134a4d97b8f2bd076c3bf39c1e82a89a3e9b5
l0lawrence <llawrence@microsoft.com>
2022-03-31 12:45:09 -0700

    expanding out *args to reflect pylint.utils function args

--- scripts/pylint_custom_plugin/regex_path_filter.py
+++ scripts/pylint_custom_plugin/regex_path_filter.py
@@ -6,14 +6,14 @@ class RegexPathFilter:
         self.old_expand_modules = utils.expand_modules
         utils.expand_modules = self.new_expand_modules
     
-    def new_expand_modules(self, *args, **kwargs):
-        modules, errors = self.old_expand_modules(*args, **kwargs)
+    def new_expand_modules(self, files_or_modules, black_list, black_list_re, **kwargs):
+        modules, errors = self.old_expand_modules(files_or_modules, black_list, black_list_re, **kwargs)
         new_modules = self.filtered(modules)
         return new_modules, errors
 
     def filtered(self, modules):
         paths = [r"sdk\\storage\\.*\\_shared\\models.py", r"sdk\\storage\\.*\\_models.py", r"sdk\\storage\\.*\\_lease.py", r"sdk\\storage\\.*\\.*client.*"]
-        reg = re.compile("(%s|%s|%s|%s)" % (paths[0],paths[1],paths[2],paths[3]))
+        reg = re.compile("(%s|%s|%s|%s)" % (paths[0], paths[1], paths[2], paths[3]))
         new_modules = []
         for m in modules:
             if not reg.match(m['path']):
