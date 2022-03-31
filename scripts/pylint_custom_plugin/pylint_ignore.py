d3ed5ad9bac711ee5365a1d08efbfa70d11c7c09
l0lawrence <llawrence@microsoft.com>
2022-03-31 08:37:39 -0700

    refactoring PylintIgnorePaths to better fit our storage purposes

--- scripts/pylint_custom_plugin/pylint_ignore.py
+++ scripts/pylint_custom_plugin/pylint_ignore.py
@@ -4,21 +4,18 @@ from pylint.utils import utils
 
 class PylintIgnorePaths:
     def __init__(self):
-        
         self.paths = [r"sdk\\storage\\.*\\_shared\\models.py", r"sdk\\storage\\.*\\_models.py", r"sdk\\storage\\.*\\_lease.py", r"sdk\\storage\\.*\\.*client.*"]
-        self.original_expand_modules = utils.expand_modules
-        utils.expand_modules = self.patched_expand
-
-    def patched_expand(self, *args, **kwargs):
-        result, errors = self.original_expand_modules(*args, **kwargs)
-
-        def keep_item(item):
-            for path in self.paths:
-                if re.match(path, item['path']):
-                    return False
-
-            return True
-
-        result = list(filter(keep_item, result))
-
+        self.store_expand_modules = utils.expand_modules
+        utils.expand_modules = self.filter_expand_modules
+    
+    def filter_expand_modules(self, *args, **kwargs):
+        result, errors = self.store_expand_modules(*args, **kwargs)
+        result = list(filter(self.keep_item,result))
         return result, errors
+    
+    def keep_item(self,item):
+        for path in self.paths:
+            if re.match(path, item["path"]):
+                return False
+    
+        return True
