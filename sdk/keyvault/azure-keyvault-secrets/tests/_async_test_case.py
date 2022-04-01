8f1608faf5729c48f2f00948ee98eb59ac76ae59
Kashif Khan <361477+kashifkhan@users.noreply.github.com>
2022-03-31 19:08:30 -0500

    [KeyVault] Enable Test Proxy for KeyVault Secrets (#22807)
    * switch to assert from self.assert*
    
    * Adding in conftest
    
    * Make sleep patches into fixtures
    
    * Change class name for pytest
    
    * move from aztestcase to azrectestcase
    
    * fix import
    
    * change class name for pytest
    
    * decorate sync tests with proxy
    
    * decorate async tests
    
    * missing imports
    
    * changes to get the tests collectd by pytest
    
    * add in sanitizers & test proxy changes
    
    * secrets client async preparer
    
    * changes for preparer
    
    * update with further sanitizers
    
    * update to test and recordings
    
    * changes to async test case
    
    * update test_parse to record output
    
    * minor cleanups
    
    * move to async prepaper
    
    * updates
    
    * minor changes to get tests working
    
    * more recordings from test proxy
    
    * switch to assert from self.assert*
    
    * Adding in conftest
    
    * updates
    
    * updates
    
    * further merges
    
    * merges
    
    * merges
    
    * changes
    
    * more recordings for async tests
    
    * Changes for async tests and work around for test proxy
    
    * Changes to to get tests working
    
    * change for EnvironmentVariablesLoader & recordings
    
    * clean up for client creation
    
    * fix patches for sleep
    
    * add in http cache clean
    
    * mods for win-http
    
    * recordings using test proxy
    
    * set default vault url
    
    * changes for auth headers
    
    * changes for auth update
    
    * new recordings
    
    * delete vcrpy recordings
    
    * remove unused imports & code cleanup
    
    * minor changes around patching sleep
    
    * new recordings for 7.3
    
    * new recordings
    
    * clean up recordings of 7.3 preview
    
    * remove 7.3 preview tests
    
    * removing customer default handler for auth
    
    * new recording for test
    
    * code clean up and formatting
    
    * remove temp fix for async recordings
    
    * code clean up for imports, better error messages
    
    * add cache challenge in to a teardown
    
    * new recordings with cache challenge change
    
    * change kv url error
    
    * fix wrong masking name
    
    * Fix SecretsClientPreparer typo
    
    * remove code block for async tests
    
    * remove unused imports

---
+++ sdk/keyvault/azure-keyvault-secrets/tests/_async_test_case.py
@@ -0,0 +1,43 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import os
+
+import pytest
+from azure.keyvault.secrets._shared.client_base import DEFAULT_VERSION
+from devtools_testutils import AzureRecordedTestCase, is_live
+
+
+class AsyncSecretsClientPreparer(AzureRecordedTestCase):
+    def __init__(self, **kwargs) -> None:
+        self.azure_keyvault_url = "https://vaultname.vault.azure.net"
+
+        if is_live():
+            self.azure_keyvault_url = os.environ["AZURE_KEYVAULT_URL"]
+
+        self.is_logging_enabled = kwargs.pop("logging_enable", True)
+        if is_live():
+            os.environ["AZURE_TENANT_ID"] = os.environ["KEYVAULT_TENANT_ID"]
+            os.environ["AZURE_CLIENT_ID"] = os.environ["KEYVAULT_CLIENT_ID"]
+            os.environ["AZURE_CLIENT_SECRET"] = os.environ["KEYVAULT_CLIENT_SECRET"]
+
+    def __call__(self, fn):
+        async def _preparer(test_class, api_version, **kwargs):
+            self._skip_if_not_configured(api_version)
+            if not self.is_logging_enabled:
+                kwargs.update({"logging_enable": False})
+            client = self.create_client(self.azure_keyvault_url, api_version=api_version, **kwargs)
+            await fn(test_class, client)
+
+        return _preparer
+
+    def create_client(self, vault_uri, **kwargs):
+        from azure.keyvault.secrets.aio import SecretClient
+
+        credential = self.get_credential(SecretClient, is_async=True)
+        return self.create_client_from_credential(SecretClient, credential=credential, vault_url=vault_uri, **kwargs)
+
+    def _skip_if_not_configured(self, api_version, **kwargs):
+        if is_live() and api_version != DEFAULT_VERSION:
+            pytest.skip("This test only uses the default API version for live tests")
