015482adefd3981c7ae40b85dd573fbbf29b49cd
catalinaperalta <catalinaperaltah@hotmail.com>
2022-04-01 17:32:44 -0700

    skip signature tests until service issue is resolved (#23790)

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_dac_analyze_custom_model_async.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_dac_analyze_custom_model_async.py
@@ -217,6 +217,7 @@ class TestDACAnalyzeCustomModelAsync(AsyncFormRecognizerTest):
                 result = await poller.result()
                 assert result
 
+    @pytest.mark.skip()
     @FormRecognizerPreparer()
     @DocumentModelAdministrationClientPreparer()
     @recorded_by_proxy_async
