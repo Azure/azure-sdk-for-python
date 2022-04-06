2f8846098102f8c77e30bfd8d90de52f293b33bb
Swathi Pillalamarri <swathip@microsoft.com>
2022-04-06 15:44:29 -0700

    update async auto renewer

--- sdk/servicebus/azure-servicebus/azure/servicebus/_common/auto_lock_renewer.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_common/auto_lock_renewer.py
@@ -258,8 +258,8 @@ class AutoLockRenewer(object):  # pylint:disable=too-many-instance-attributes
         :type receiver: ~azure.servicebus.ServiceBusReceiver
         :param renewable: A locked entity that needs to be renewed.
         :type renewable: Union[~azure.servicebus.ServiceBusReceivedMessage, ~azure.servicebus.ServiceBusSession]
-        :param max_lock_renewal_duration: A time in seconds that the lock should be maintained for. If set to None,
-         `max_lock_renewal_duration` value passed to constructor will be used. Default value is None.
+        :param max_lock_renewal_duration: A time in seconds that the lock should be maintained for.
+         Default value is None. If specified, this value will override the default value specified at the constructor.
         :type max_lock_renewal_duration: Optional[float]
         :param on_lock_renew_failure: A callback may be specified to be called when the lock is lost on the renewable
          that is being registered. Default value is None (no callback).
