246e3a4a3fccbdac920b9ef57610a813ac3dc12e
vincenttran-msft <101599632+vincenttran-msft@users.noreply.github.com>
2022-04-07 13:48:08 -0700

    [Storage] Fixed parsing of path_name and file_system_name in async clients (#23854)

--- sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/aio/_file_system_client_async.py
+++ sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/aio/_file_system_client_async.py
@@ -91,15 +91,15 @@ class FileSystemClient(AsyncStorageAccountHostsMixin, FileSystemClientBase):
             **kwargs)
         # to override the class field _container_client sync version
         kwargs.pop('_hosts', None)
-        self._container_client = ContainerClient(self._blob_account_url, file_system_name,
+        self._container_client = ContainerClient(self._blob_account_url, self.file_system_name,
                                                  credential=credential,
                                                  _hosts=self._container_client._hosts,# pylint: disable=protected-access
                                                  **kwargs)  # type: ignore # pylint: disable=protected-access
         self._client = AzureDataLakeStorageRESTAPI(self.url, base_url=self.url,
-                                                   file_system=file_system_name, pipeline=self._pipeline)
+                                                   file_system=self.file_system_name, pipeline=self._pipeline)
         self._datalake_client_for_blob_operation = AzureDataLakeStorageRESTAPI(self._container_client.url,
                                                                                base_url=self._container_client.url,
-                                                                               file_system=file_system_name,
+                                                                               file_system=self.file_system_name,
                                                                                pipeline=self._pipeline)
         api_version = get_api_version(kwargs)
         self._client._config.version = api_version  # pylint: disable=protected-access
