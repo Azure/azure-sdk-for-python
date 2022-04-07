4eafe774b3ea52ea60093c8e0256262410382d5a
Jiefeng Chen (WICRESOFT NORTH AMERICA LTD) <v-jiechen@microsoft.com>
2022-04-07 17:21:14 +0800

    test

--- scripts/release_issue_status/reply_generator.py
+++ scripts/release_issue_status/reply_generator.py
@@ -30,6 +30,7 @@ def get_links(readme_link):
     pattern_resource_manager = re.compile(r'/specification/([\w-]+/)+resource-manager')
     pattern_tag = re.compile(r'package-[\w+-.]+')
     readme_path = pattern_readme.search(readme_link).group()
+    print(f"comment body: {comment_body}")
     readme_tag = pattern_tag.search(comment_body).group()
     resource_manager = pattern_resource_manager.search(readme_link).group()
     link_dict['readme_path'] = readme_path
