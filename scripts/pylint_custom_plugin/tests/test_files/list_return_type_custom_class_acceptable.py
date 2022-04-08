75b5df6ca8b5a724c7858da3078d5db9b6de1a95
l0lawrence <llawrence@microsoft.com>
2022-04-08 11:20:30 -0700

    removing excess in test file

--- scripts/pylint_custom_plugin/tests/test_files/list_return_type_custom_class_acceptable.py
+++ scripts/pylint_custom_plugin/tests/test_files/list_return_type_custom_class_acceptable.py
@@ -21,50 +21,8 @@ class SearchClient(): #@
         """Search the Azure search index for documents.
 
         """
-        include_total_result_count = kwargs.pop("include_total_count", None)
-        facets = kwargs.pop("facets", None)
-        filter_arg = kwargs.pop("filter", None)
-        highlight_fields = kwargs.pop("highlight_fields", None)
-        highlight_post_tag = kwargs.pop("highlight_post_tag", None)
-        highlight_pre_tag = kwargs.pop("highlight_pre_tag", None)
-        minimum_coverage = kwargs.pop("minimum_coverage", None)
-        order_by = kwargs.pop("order_by", None)
-        query_type = kwargs.pop("query_type", None)
-        scoring_parameters = kwargs.pop("scoring_parameters", None)
-        scoring_profile = kwargs.pop("scoring_profile", None)
-        search_fields = kwargs.pop("search_fields", None)
-        search_fields_str = ",".join(search_fields) if search_fields else None
-        search_mode = kwargs.pop("search_mode", None)
-        query_language = kwargs.pop("query_language", None)
-        query_speller = kwargs.pop("query_speller", None)
-        select = kwargs.pop("select", None)
-        skip = kwargs.pop("skip", None)
-        top = kwargs.pop("top", None)
-        session_id = kwargs.pop("session_id", None)
-        scoring_statistics = kwargs.pop("scoring_statistics", None)
-
-        query_answer = kwargs.pop("query_answer", None)
-        query_answer_count = kwargs.pop("query_answer_count", None)
-        answers = query_answer if not query_answer_count else '{}|count-{}'.format(
-            query_answer, query_answer_count
-        )
-
-        query_caption = kwargs.pop("query_caption", None)
-        query_caption_highlight = kwargs.pop("query_caption_highlight", None)
-        captions = query_caption if not query_caption_highlight else '{}|highlight-{}'.format(
-            query_caption, query_caption_highlight
-        )
-
-        semantic_fields = kwargs.pop("semantic_fields", None)
-        semantic_configuration = kwargs.pop("semantic_configuration_name", None)
-
-        query = SearchQuery(
-        )
-        if isinstance(select, list):
-            query.select(select)
-
-        kwargs["headers"] = self._merge_client_headers(kwargs.get("headers"))
-        kwargs["api_version"] = self._api_version
+        self._client = None
+        query = None
         return SearchItemPaged(
             self._client, query, kwargs, page_iterator_class=None
         )
