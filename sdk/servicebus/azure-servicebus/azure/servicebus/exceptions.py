04725786cb95e400e62d8e8b5a8b4c244f3da8fa
Swathi Pillalamarri <swathip@microsoft.com>
2022-04-13 15:34:51 -0700

    retry if not non-retry condition

--- sdk/servicebus/azure-servicebus/azure/servicebus/exceptions.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/exceptions.py
@@ -112,10 +112,11 @@ def _handle_amqp_exception_with_condition(
         error=exception,
         condition=condition,
         status_code=status_code,
-        retryable=True,
     )
     if condition in _NO_RETRY_CONDITION_ERROR_CODES:
         error._retryable = False  # pylint: disable=protected-access
+    else:
+        error._retryable = True # pylint: disable=protected-access
 
     return error
 
