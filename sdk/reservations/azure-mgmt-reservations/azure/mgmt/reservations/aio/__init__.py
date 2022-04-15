4439cc94c125cfbf601380bc82ae8dd3ab7d6ee5
SDKAuto <sdkautomation@microsoft.com>
2022-04-15 02:36:25 +0000

    CodeGen from PR 18646 in Azure/azure-rest-api-specs Adding new input enum value to Microsoft.Capacity version 2022-03-01 (#18646)

--- sdk/reservations/azure-mgmt-reservations/azure/mgmt/reservations/aio/__init__.py
+++ sdk/reservations/azure-mgmt-reservations/azure/mgmt/reservations/aio/__init__.py
@@ -8,3 +8,8 @@
 
 from ._azure_reservation_api import AzureReservationAPI
 __all__ = ['AzureReservationAPI']
+
+# `._patch.py` is used for handwritten extensions to the generated code
+# Example: https://github.com/Azure/azure-sdk-for-python/blob/main/doc/dev/customize_code/how-to-patch-sdk-code.md
+from ._patch import patch_sdk
+patch_sdk()
