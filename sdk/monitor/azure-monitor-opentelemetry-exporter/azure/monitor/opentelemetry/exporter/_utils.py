478e4d5e51b923227d22b354fa41ed15712b8c1f
Leighton Chen <lechen@microsoft.com>
2022-04-21 17:41:08 -0700

    kwargs

--- sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/_utils.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/_utils.py
@@ -57,8 +57,8 @@ class PeriodicTask(threading.Thread):
     :param args: The kwargs passed in while calling `function`.
     """
 
-    def __init__(self, interval, function, *args, name=None, **kwargs):
-        super().__init__(name=name)
+    def __init__(self, interval, function, *args, **kwargs):
+        super().__init__(name=kwargs.pop('name', None))
         self.interval = interval
         self.function = function
         self.args = args or []
