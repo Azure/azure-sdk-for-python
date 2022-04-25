87289a550addf9edd48eef757912d4c5896a1348
AFRICA\mshaban <mshaban@microsoft.com>
2022-04-25 16:37:10 +0200

    samples for issue resolution

---
+++ sdk/cognitivelanguage/azure-ai-language-conversations/samples/reslotion_and_pii/sample_issue_resolution.py
@@ -0,0 +1,109 @@
+# coding=utf-8
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+"""
+FILE: sample_issue_resolution.py
+
+DESCRIPTION:
+    This sample demonstrates how to analyze a conversation for issue resolution.
+
+    For more info about how to setup a CLU conversation project, see the README.
+
+USAGE:
+    python sample_issue_resolution.py
+
+    Set the environment variables with your own values before running the sample:
+    1) AZURE_CONVERSATIONS_ENDPOINT                       - endpoint for your CLU resource.
+    2) AZURE_CONVERSATIONS_KEY                            - API key for your CLU resource.
+"""
+
+def sample_issue_resolution():
+    # [START analyze_conversation_app]
+    # import libraries
+    import os
+    from azure.core.credentials import AzureKeyCredential
+
+    from azure.ai.language.conversations import ConversationAnalysisClient
+    from azure.ai.language.conversations.models import (
+        AnalyzeConversationJobsInput,
+        MultiLanguageConversationAnalysisInput,
+        TextConversation,
+        TextConversationItem,
+        AnalyzeConversationSummarizationTask,
+        ConversationSummarizationTaskParameters
+    )
+
+    # get secrets
+    endpoint = os.environ["AZURE_CONVERSATIONS_ENDPOINT"]
+    key = os.environ["AZURE_CONVERSATIONS_KEY"]
+
+    # analyze quey
+    client = ConversationAnalysisClient(endpoint, AzureKeyCredential(key))
+    with client:
+        # submit job
+        poller = client.begin_submit_conversation_job(
+            body=AnalyzeConversationJobsInput(
+                analysis_input=MultiLanguageConversationAnalysisInput(
+                    conversations=TextConversation(
+                        id=1,
+                        language="en",
+                        conversation_items=[
+                            TextConversationItem(
+                                id=1,
+                                participant_id="agent",
+                                text="Hello, how can I help you?"
+                            ),
+                            TextConversationItem(
+                                id=2,
+                                participant_id="customer",
+                                text="How to upgrade Office? I am getting error messages the whole day."
+                            ),
+                            TextConversationItem(
+                                id=3,
+                                participant_id="agent",
+                                text="Press the upgrade button please. Then sign in and follow the instructions."
+                            )
+                        ]
+                    )
+                ),
+                tasks=[
+                    AnalyzeConversationSummarizationTask(
+                        parameters=ConversationSummarizationTaskParameters(
+                            summary_aspects=["Issue", "Resolution"]
+                        )
+                    )
+                ]
+            )
+        )
+
+        # wait till done
+        poller.result()
+
+        # view result
+        result = client.get_conversation_job_status()
+        task_result = result.tasks.items[0]
+        print("... view task status:")
+        print("status: {}".format(task_result.status))
+        issue_resolution_result = task_result.results
+        if issue_resolution_result.errors:
+            print("... errors occured")
+            for error in issue_resolution_result.errors:
+                print(error)
+        conversation_result = issue_resolution_result.conversations[0]
+        if conversation_result.warnings:
+            print("... view task status:")
+            for warning in conversation_result.warnings:
+                print(warning)
+        summaries = conversation_result.summaries
+        print("... view task result:")
+        print("issue: {}".format(summaries[0].text))
+        print("resolution: {}".format(summaries[1].text))
+
+    # [END analyze_conversation_app]
+
+
+if __name__ == '__main__':
+    sample_issue_resolution()
---
+++ sdk/cognitivelanguage/azure-ai-language-conversations/samples/reslotion_and_pii/sample_issue_resolution_dict_parms.py
@@ -0,0 +1,111 @@
+# coding=utf-8
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+"""
+FILE: sample_issue_resolution_dict_parms.py
+
+DESCRIPTION:
+    This sample demonstrates how to analyze a conversation for issue resolution.
+
+    For more info about how to setup a CLU conversation project, see the README.
+
+USAGE:
+    python sample_issue_resolution_dict_parms.py
+
+    Set the environment variables with your own values before running the sample:
+    1) AZURE_CONVERSATIONS_ENDPOINT                       - endpoint for your CLU resource.
+    2) AZURE_CONVERSATIONS_KEY                            - API key for your CLU resource.
+"""
+
+def sample_issue_resolution_dict_parms():
+    # [START analyze_conversation_app]
+    # import libraries
+    import os
+    from azure.core.credentials import AzureKeyCredential
+
+    from azure.ai.language.conversations import ConversationAnalysisClient
+
+    # get secrets
+    endpoint = os.environ["AZURE_CONVERSATIONS_ENDPOINT"]
+    key = os.environ["AZURE_CONVERSATIONS_KEY"]
+
+    # analyze quey
+    client = ConversationAnalysisClient(endpoint, AzureKeyCredential(key))
+    with client:
+
+        poller = client.begin_submit_conversation_job(
+            body={
+                "displayName": "Analyze conversations from xxx",
+                "analysisInput": {
+                    "conversations": [
+                        {
+                            "conversationItems": [
+                                {
+                                    "text": "Hello, how can I help you?",
+                                    "modality": "text",
+                                    "id": "1",
+                                    "participantId": "Agent"
+                                },
+                                {
+                                    "text": "How to upgrade Office? I am getting error messages the whole day.",
+                                    "modality": "text",
+                                    "id": "2",
+                                    "participantId": "Customer"
+                                },
+                                {
+                                    "text": "Press the upgrade button please. Then sign in and follow the instructions.",
+                                    "modality": "text",
+                                    "id": "3",
+                                    "participantId": "Agent"
+                                }
+                            ],
+                            "modality": "text",
+                            "id": "conversation1",
+                            "language": "en"
+                        },
+                    ]
+                },
+                "tasks": [
+                    {
+                        "taskName": "analyze 1",
+                        "kind": "IssueResolutionSummarization",
+                        "parameters": {
+                            "modelVersion": "2022-04-01",
+                            "summaryAspects": "Issue, Resolution"
+                        }
+                    }
+                ]
+            }
+        )
+
+        # wait till done
+        poller.result()
+
+        # view result
+        result = client.get_conversation_job_status()
+        task_result = result.tasks.items[0]
+        print("... view task status:")
+        print("status: {}".format(task_result.status))
+        issue_resolution_result = task_result.results
+        if issue_resolution_result.errors:
+            print("... errors occured")
+            for error in issue_resolution_result.errors:
+                print(error)
+        conversation_result = issue_resolution_result.conversations[0]
+        if conversation_result.warnings:
+            print("... view task status:")
+            for warning in conversation_result.warnings:
+                print(warning)
+        summaries = conversation_result.summaries
+        print("... view task result:")
+        print("issue: {}".format(summaries[0].text))
+        print("resolution: {}".format(summaries[1].text))
+
+    # [END analyze_conversation_app]
+
+
+if __name__ == '__main__':
+    sample_issue_resolution_dict_parms()
