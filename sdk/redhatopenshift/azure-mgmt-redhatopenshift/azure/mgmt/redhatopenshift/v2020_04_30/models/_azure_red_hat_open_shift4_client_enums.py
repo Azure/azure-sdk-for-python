77684443617abf6412873e34afe668cf0c81653c
SDKAuto <sdkautomation@microsoft.com>
2022-04-27 03:15:39 +0000

    CodeGen from PR 18350 in Azure/azure-rest-api-specs Azure Red Hat OpenShift 2022-04-01 API (#18350)
    * test
    
    * Fix Swagger LintDiff failures
    
    * More lint errors
    
    * Update readme.md
    
    * Update readme.python.md
    
    Co-authored-by: Peter Kostyukov <pkostyuk@redhat.com>
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>

--- sdk/redhatopenshift/azure-mgmt-redhatopenshift/azure/mgmt/redhatopenshift/v2020_04_30/models/_azure_red_hat_open_shift4_client_enums.py
+++ sdk/redhatopenshift/azure-mgmt-redhatopenshift/azure/mgmt/redhatopenshift/v2020_04_30/models/_azure_red_hat_open_shift4_client_enums.py
@@ -6,27 +6,12 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from enum import Enum, EnumMeta
+from enum import Enum
 from six import with_metaclass
-
-class _CaseInsensitiveEnumMeta(EnumMeta):
-    def __getitem__(self, name):
-        return super().__getitem__(name.upper())
-
-    def __getattr__(cls, name):
-        """Return the enum member matching `name`
-        We use __getattr__ instead of descriptors or inserting into the enum
-        class' __dict__ in order to support `name` and `value` being both
-        properties for enum members (which live in the class' __dict__) and
-        enum members themselves.
-        """
-        try:
-            return cls._member_map_[name.upper()]
-        except KeyError:
-            raise AttributeError(name)
+from azure.core import CaseInsensitiveEnumMeta
 
 
-class ProvisioningState(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class ProvisioningState(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
     """ProvisioningState represents a provisioning state.
     """
 
@@ -37,14 +22,14 @@ class ProvisioningState(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     SUCCEEDED = "Succeeded"
     UPDATING = "Updating"
 
-class Visibility(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class Visibility(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
     """Visibility represents visibility.
     """
 
     PRIVATE = "Private"
     PUBLIC = "Public"
 
-class VMSize(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class VMSize(with_metaclass(CaseInsensitiveEnumMeta, str, Enum)):
     """VMSize represents a VM size.
     """
 
