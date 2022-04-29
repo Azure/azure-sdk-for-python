ae04879571a634e41fddbd4f9a07fc3d0407a68a
YalinLi0312 <yall@microsoft.com>
2022-04-29 11:35:05 -0700

    Run autorest --python --v3 .\swagger\README.md

--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/_container_registry_client.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/_container_registry_client.py
@@ -847,7 +847,7 @@ class ContainerRegistryClient(ContainerRegistryBaseClient):
             raise ValueError("The parameter repository and tag_or_digest cannot be None.")
 
         response = self._client.container_registry.get_manifest(
-            name=repository, reference=tag_or_digest, accept=OCI_MANIFEST_MEDIA_TYPE, cls=_return_response, **kwargs
+            name=repository, reference=tag_or_digest, headers=OCI_MANIFEST_MEDIA_TYPE, cls=_return_response, **kwargs
         )
         digest = response.http_response.headers['Docker-Content-Digest']
         content = response.http_response.internal_response._content
