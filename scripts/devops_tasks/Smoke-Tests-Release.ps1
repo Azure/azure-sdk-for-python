7c02b71269978339628fefa3104829cd61137503
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2022-04-28 17:02:09 -0700

    more changes

--- scripts/devops_tasks/Smoke-Tests-Release.ps1
+++ scripts/devops_tasks/Smoke-Tests-Release.ps1
@@ -15,8 +15,12 @@ New-Item -Path $smoketestsDir -ItemType Directory
 
 # Run smoketests python script to create requirements.txt
 Write-Host "Creating requirements.txt in the smoketest folder"
-# virtualenv env
-# env/Scripts/activate
+virtualenv envrelease
+source envrelease/bin/activate
+if ($LASTEXITCODE) {
+    exit $LASTEXITCODE
+}
+Write-Host "Creating requirements.txt in the smoketest folder"
 $fileName = Join-Path $repoRoot scripts devops_tasks  get_track2_packages.py
 python $fileName release
 if ($LASTEXITCODE) {
@@ -37,13 +41,13 @@ if ($LASTEXITCODE) {
 }
 
 Write-Host "The following packages are installed"
-Write-Host pip list
+pip list
 if ($LASTEXITCODE) {
     exit $LASTEXITCODE
 }
 Pop-Location
 
-
+Write-Host "Importing __all__ from all the installed modules"
 $fileName = Join-Path $repoRoot scripts devops_tasks smoke_tests.py
 python $fileName
 if ($LASTEXITCODE) {
