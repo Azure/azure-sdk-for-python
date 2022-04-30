d6c774d1179f7b3d3e9228a1577cb00382b55442
Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2022-04-29 17:59:56 -0700

    mgmt core

--- scripts/devops_tasks/common_tasks.py
+++ scripts/devops_tasks/common_tasks.py
@@ -495,7 +495,8 @@ def get_package_properties(setup_py_path):
 
 def is_track2_package(reqs):
     for req in reqs:
-        if req.startswith('azure-core'):
+        # the package must be dependant on azure core or azure mgmt core
+        if req.startswith('azure-core') or req.startswith('azure-mgmt-core'):
             return True
     return False
 
