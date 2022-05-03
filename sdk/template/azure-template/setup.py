d1ac4f13e09f06e92ea977d69b198806c6533a17
scbedd <45376673+scbedd@users.noreply.github.com>
2022-05-03 15:45:20 -0700

    updating classifiers

--- sdk/template/azure-template/setup.py
+++ sdk/template/azure-template/setup.py
@@ -43,11 +43,13 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
     ],
     packages=find_packages(
