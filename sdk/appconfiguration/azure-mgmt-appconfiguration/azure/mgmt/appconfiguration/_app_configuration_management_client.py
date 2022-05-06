727ed07d074a1bfa10d6870dd19cea41459e0815
SDKAuto <sdkautomation@microsoft.com>
2022-05-06 07:55:50 +0000

    CodeGen from PR 18811 in Azure/azure-rest-api-specs [App Configuration] Promote 2022-05-01 stable version (#18811)
    * Add 2022-05-01 folder based on 2021-10-01-preview
    
    * Update 2022-05-01 folder
    
    * Fix readme issue

--- sdk/appconfiguration/azure-mgmt-appconfiguration/azure/mgmt/appconfiguration/_app_configuration_management_client.py
+++ sdk/appconfiguration/azure-mgmt-appconfiguration/azure/mgmt/appconfiguration/_app_configuration_management_client.py
@@ -7,11 +7,12 @@
 # --------------------------------------------------------------------------
 
 from copy import deepcopy
-from typing import Any, Optional, TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
+
+from msrest import Deserializer, Serializer
 
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
-from msrest import Deserializer, Serializer
 
 from . import models
 from ._configuration import AppConfigurationManagementClientConfiguration
@@ -26,23 +27,26 @@ class AppConfigurationManagementClient:
 
     :ivar configuration_stores: ConfigurationStoresOperations operations
     :vartype configuration_stores:
-     app_configuration_management_client.operations.ConfigurationStoresOperations
+     azure.mgmt.appconfiguration.operations.ConfigurationStoresOperations
     :ivar operations: Operations operations
-    :vartype operations: app_configuration_management_client.operations.Operations
+    :vartype operations: azure.mgmt.appconfiguration.operations.Operations
     :ivar private_endpoint_connections: PrivateEndpointConnectionsOperations operations
     :vartype private_endpoint_connections:
-     app_configuration_management_client.operations.PrivateEndpointConnectionsOperations
+     azure.mgmt.appconfiguration.operations.PrivateEndpointConnectionsOperations
     :ivar private_link_resources: PrivateLinkResourcesOperations operations
     :vartype private_link_resources:
-     app_configuration_management_client.operations.PrivateLinkResourcesOperations
+     azure.mgmt.appconfiguration.operations.PrivateLinkResourcesOperations
     :ivar key_values: KeyValuesOperations operations
-    :vartype key_values: app_configuration_management_client.operations.KeyValuesOperations
+    :vartype key_values: azure.mgmt.appconfiguration.operations.KeyValuesOperations
     :param credential: Credential needed for the client to connect to Azure.
     :type credential: ~azure.core.credentials.TokenCredential
     :param subscription_id: The Microsoft Azure subscription ID.
     :type subscription_id: str
-    :param base_url: Service URL. Default value is 'https://management.azure.com'.
+    :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
+    :keyword api_version: Api Version. Default value is "2022-05-01". Note that overriding this
+     default value may result in unsupported behavior.
+    :paramtype api_version: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
      Retry-After header is present.
     """
@@ -70,7 +74,7 @@ class AppConfigurationManagementClient:
 
     def _send_request(
         self,
-        request,  # type: HttpRequest
+        request: HttpRequest,
         **kwargs: Any
     ) -> HttpResponse:
         """Runs the network request through the client's chained policies.
