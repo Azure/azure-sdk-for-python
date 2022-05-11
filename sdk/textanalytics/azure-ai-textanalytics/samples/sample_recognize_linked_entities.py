3b11ae12aa3fcc82f4eb8a69680d84c17f7ea74d
Krista Pratico <krpratic@microsoft.com>
2022-05-10 17:12:46 -0700

    [textanalytics] update documentation (#24318)
    * renames from TA->Language for samples
    
    * update docstrings, readme, changelog
    
    * update language
    
    * fix readme toc
    
    * missed an instance
    
    * include healthcare action on readme list
    
    * feedback
    
    * add sentence about dropping v3.2-preview.2

--- sdk/textanalytics/azure-ai-textanalytics/samples/sample_recognize_linked_entities.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/sample_recognize_linked_entities.py
@@ -20,8 +20,8 @@ USAGE:
     python sample_recognize_linked_entities.py
 
     Set the environment variables with your own values before running the sample:
-    1) AZURE_TEXT_ANALYTICS_ENDPOINT - the endpoint to your Cognitive Services resource.
-    2) AZURE_TEXT_ANALYTICS_KEY - your Text Analytics subscription key
+    1) AZURE_LANGUAGE_ENDPOINT - the endpoint to your Language resource.
+    2) AZURE_LANGUAGE_KEY - your Language subscription key
 """
 
 import os
@@ -38,8 +38,8 @@ def sample_recognize_linked_entities():
     from azure.core.credentials import AzureKeyCredential
     from azure.ai.textanalytics import TextAnalyticsClient
 
-    endpoint = os.environ["AZURE_TEXT_ANALYTICS_ENDPOINT"]
-    key = os.environ["AZURE_TEXT_ANALYTICS_KEY"]
+    endpoint = os.environ["AZURE_LANGUAGE_ENDPOINT"]
+    key = os.environ["AZURE_LANGUAGE_KEY"]
 
     text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
     documents = [
