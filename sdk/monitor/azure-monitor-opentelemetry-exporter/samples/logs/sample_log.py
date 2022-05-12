db028c0747ce22a3444fecd5a973ab169e251cc9
Jens Hedegaard Nielsen <jenshnielsen@gmail.com>
2022-05-12 20:54:30 +0200

    Update Azure monitor OpenTelemetry exporter example (#24422)

--- sdk/monitor/azure-monitor-opentelemetry-exporter/samples/logs/sample_log.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/samples/logs/sample_log.py
@@ -9,7 +9,7 @@ import logging
 
 from opentelemetry.sdk._logs import (
     LogEmitterProvider,
-    OTLPHandler,
+    LoggingHandler,
     get_log_emitter_provider,
     set_log_emitter_provider,
 )
@@ -24,7 +24,7 @@ exporter = AzureMonitorLogExporter.from_connection_string(
 get_log_emitter_provider().add_log_processor(BatchLogProcessor(exporter))
 
 # Attach OTel handler to namespaced logger
-handler = OTLPHandler()
+handler = LoggingHandler()
 logger = logging.getLogger(__name__)
 logger.addHandler(handler)
 logger.setLevel(logging.NOTSET)
