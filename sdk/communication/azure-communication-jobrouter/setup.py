160725df78aefb4d79069596eb989e95a0fa6635
Rajarshi Sarkar <73562869+sarkar-rajarshi@users.noreply.github.com>
2022-05-16 17:42:33 -0700

    remove unwanted dev requirements + update azure.core to 1.23.0

--- sdk/communication/azure-communication-jobrouter/setup.py
+++ sdk/communication/azure-communication-jobrouter/setup.py
@@ -61,7 +61,7 @@ setup(
     ]),
     python_requires=">=3.6",
    install_requires=[
-        'azure-core<2.0.0,>=1.2.2',
+        'azure-core<2.0.0,>=1.23.0',
         'msrest>=0.6.21',
         'six>=1.11.0'
     ],
