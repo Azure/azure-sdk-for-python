d193437467dd4e03dda669d2c0ec6046e8353dbe
Neehar Duvvuri <neduvvur@microsoft.com>
2022-05-31 14:01:49 -0400

    remove init

--- sdk/ml/azure-ai-ml/setup.py
+++ sdk/ml/azure-ai-ml/setup.py
@@ -59,7 +59,6 @@ setup(
     packages=find_packages(exclude=[
         'samples',
         'tests',
-        'tests.*',
         # Exclude packages that will be covered by PEP420 or nspkg
         'azure',
         'azure.ai',
