a71c2138aa1c37f90d9ed2633a854adb1b2fe6d5
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2022-06-01 14:43:44 -0400

    [ML] Add in rest of unit tests (#24622)
    * [ML] Add in rest of unit tests
    
    * add an init to tests
    
    * fix test_utils import
    
    * add env yml files
    
    * add knack dependency
    
    * fix more tests
    
    * fix gitignore
    
    * add test_file
    
    * add tests to ignore doc settings
    
    * exclude more tests
    
    * fix tests ignore thing
    
    * update cspell ignore list to ignore a couple names in azure-ai-ml. update azure-ai-ml dev_requirements to install in a good state within our tox environments
    
    * ignore conftest, not user facing regardless
    
    * ignore tests dir for cspell
    
    * remove init
    
    * change test import
    
    * fix import thing
    
    * rename utils
    
    Co-authored-by: Neehar Duvvuri <neduvvur@microsoft.com>
    Co-authored-by: scbedd <45376673+scbedd@users.noreply.github.com>

---
+++ sdk/ml/azure-ai-ml/tests/test_configs/dsl_pipeline/parallel_component_with_file_input/src/score.py
@@ -0,0 +1,38 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""This module simulate run() which can specify succeed every n items from argument."""
+import argparse
+
+from pathlib import Path
+
+
+def init():
+    """Init."""
+    global OUTPUT_PATH
+
+    parser = argparse.ArgumentParser(allow_abbrev=False, description="ParallelRunStep Agent")
+    parser.add_argument("--job_output_path", type=str, default=0)
+    args, _ = parser.parse_known_args()
+    OUTPUT_PATH = args.job_output_path
+    print("Pass through init done")
+
+
+def run(mini_batch):
+    """Run."""
+
+    for file_path in mini_batch:
+        file = Path(file_path)
+        print("Processing {}".format(file))
+        assert file.exists()
+
+        # Two customers reported transient error when using OutputFileDatasetConfig.
+        # It hits "FileNotFoundError" when writing to a file in the output_dir folder,
+        #  even the folder did exist per logs.
+        # This is to simulate such case and hope we can repro in our gated build.
+        output_dir = Path(OUTPUT_PATH)
+        print("output_dir", output_dir)
+        print("output_dir exits", Path(output_dir).exists())
+        (Path(output_dir) / file.name).write_text(file_path)
+
+    return mini_batch
