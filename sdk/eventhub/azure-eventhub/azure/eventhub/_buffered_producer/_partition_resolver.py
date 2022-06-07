5f933f3796785bb5208ac92cda3770296ef722e3
swathipil <76007337+swathipil@users.noreply.github.com>
2022-06-07 11:42:29 -0700

    [EventHubs] update buffered producer docstring (#24729)
    * run black
    
    * update buffer_concurrency docstring
    
    * docstring debug method update
    
    * lint
    
    * update release date
    
    * lint

--- sdk/eventhub/azure-eventhub/azure/eventhub/_buffered_producer/_partition_resolver.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_buffered_producer/_partition_resolver.py
@@ -17,80 +17,80 @@ def rot(x, k):
 
 
 def mix(a, b, c):
-    a &= 0xffffffff
-    b &= 0xffffffff
-    c &= 0xffffffff
+    a &= 0xFFFFFFFF
+    b &= 0xFFFFFFFF
+    c &= 0xFFFFFFFF
     a -= c
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     a ^= rot(c, 4)
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     c += b
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     b -= a
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     b ^= rot(a, 6)
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     a += c
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     c -= b
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     c ^= rot(b, 8)
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     b += a
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     a -= c
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     a ^= rot(c, 16)
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     c += b
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     b -= a
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     b ^= rot(a, 19)
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     a += c
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     c -= b
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     c ^= rot(b, 4)
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     b += a
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     return a, b, c
 
 
 def final(a, b, c):
-    a &= 0xffffffff
-    b &= 0xffffffff
-    c &= 0xffffffff
+    a &= 0xFFFFFFFF
+    b &= 0xFFFFFFFF
+    c &= 0xFFFFFFFF
     c ^= b
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     c -= rot(b, 14)
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     a ^= c
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     a -= rot(c, 11)
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     b ^= a
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     b -= rot(a, 25)
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     c ^= b
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     c -= rot(b, 16)
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     a ^= c
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     a -= rot(c, 4)
-    a &= 0xffffffff
+    a &= 0xFFFFFFFF
     b ^= a
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     b -= rot(a, 14)
-    b &= 0xffffffff
+    b &= 0xFFFFFFFF
     c ^= b
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     c -= rot(b, 24)
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
     return a, b, c
 
 
@@ -102,63 +102,153 @@ def compute_hash(data, init_val=0, init_val2=0):
     """
     length = lenpos = len(data)
 
-    a = b = c = (0xdeadbeef + length + init_val)
+    a = b = c = 0xDEADBEEF + length + init_val
 
     c += init_val2
-    c &= 0xffffffff
+    c &= 0xFFFFFFFF
 
     p = 0  # string offset
     while lenpos > 12:
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
-        a &= 0xffffffff
-        b += (ord(data[p + 4]) + (ord(data[p + 5]) << 8) + (ord(data[p + 6]) << 16) + (ord(data[p + 7]) << 24))
-        b &= 0xffffffff
-        c += (ord(data[p + 8]) + (ord(data[p + 9]) << 8) + (ord(data[p + 10]) << 16) + (ord(data[p + 11]) << 24))
-        c &= 0xffffffff
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
+        a &= 0xFFFFFFFF
+        b += (
+            ord(data[p + 4])
+            + (ord(data[p + 5]) << 8)
+            + (ord(data[p + 6]) << 16)
+            + (ord(data[p + 7]) << 24)
+        )
+        b &= 0xFFFFFFFF
+        c += (
+            ord(data[p + 8])
+            + (ord(data[p + 9]) << 8)
+            + (ord(data[p + 10]) << 16)
+            + (ord(data[p + 11]) << 24)
+        )
+        c &= 0xFFFFFFFF
         a, b, c = mix(a, b, c)
         p += 12
         lenpos -= 12
 
     if lenpos == 12:
-        c += (ord(data[p + 8]) + (ord(data[p + 9]) << 8) + (ord(data[p + 10]) << 16) + (ord(data[p + 11]) << 24))
-        b += (ord(data[p + 4]) + (ord(data[p + 5]) << 8) + (ord(data[p + 6]) << 16) + (ord(data[p + 7]) << 24))
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
+        c += (
+            ord(data[p + 8])
+            + (ord(data[p + 9]) << 8)
+            + (ord(data[p + 10]) << 16)
+            + (ord(data[p + 11]) << 24)
+        )
+        b += (
+            ord(data[p + 4])
+            + (ord(data[p + 5]) << 8)
+            + (ord(data[p + 6]) << 16)
+            + (ord(data[p + 7]) << 24)
+        )
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
     if lenpos == 11:
-        c += (ord(data[p + 8]) + (ord(data[p + 9]) << 8) + (ord(data[p + 10]) << 16))
-        b += (ord(data[p + 4]) + (ord(data[p + 5]) << 8) + (ord(data[p + 6]) << 16) + (ord(data[p + 7]) << 24))
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
+        c += ord(data[p + 8]) + (ord(data[p + 9]) << 8) + (ord(data[p + 10]) << 16)
+        b += (
+            ord(data[p + 4])
+            + (ord(data[p + 5]) << 8)
+            + (ord(data[p + 6]) << 16)
+            + (ord(data[p + 7]) << 24)
+        )
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
     if lenpos == 10:
-        c += (ord(data[p + 8]) + (ord(data[p + 9]) << 8))
-        b += (ord(data[p + 4]) + (ord(data[p + 5]) << 8) + (ord(data[p + 6]) << 16) + (ord(data[p + 7]) << 24))
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
+        c += ord(data[p + 8]) + (ord(data[p + 9]) << 8)
+        b += (
+            ord(data[p + 4])
+            + (ord(data[p + 5]) << 8)
+            + (ord(data[p + 6]) << 16)
+            + (ord(data[p + 7]) << 24)
+        )
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
     if lenpos == 9:
-        c += (ord(data[p + 8]))
-        b += (ord(data[p + 4]) + (ord(data[p + 5]) << 8) + (ord(data[p + 6]) << 16) + (ord(data[p + 7]) << 24))
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
+        c += ord(data[p + 8])
+        b += (
+            ord(data[p + 4])
+            + (ord(data[p + 5]) << 8)
+            + (ord(data[p + 6]) << 16)
+            + (ord(data[p + 7]) << 24)
+        )
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
     if lenpos == 8:
-        b += (ord(data[p + 4]) + (ord(data[p + 5]) << 8) + (ord(data[p + 6]) << 16) + (ord(data[p + 7]) << 24))
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
+        b += (
+            ord(data[p + 4])
+            + (ord(data[p + 5]) << 8)
+            + (ord(data[p + 6]) << 16)
+            + (ord(data[p + 7]) << 24)
+        )
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
     if lenpos == 7:
-        b += (ord(data[p + 4]) + (ord(data[p + 5]) << 8) + (ord(data[p + 6]) << 16))
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
+        b += ord(data[p + 4]) + (ord(data[p + 5]) << 8) + (ord(data[p + 6]) << 16)
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
     if lenpos == 6:
-        b += ((ord(data[p + 5]) << 8) + ord(data[p + 4]))
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
+        b += (ord(data[p + 5]) << 8) + ord(data[p + 4])
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
     if lenpos == 5:
-        b += (ord(data[p + 4]))
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
+        b += ord(data[p + 4])
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
     if lenpos == 4:
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16) + (ord(data[p + 3]) << 24))
+        a += (
+            ord(data[p + 0])
+            + (ord(data[p + 1]) << 8)
+            + (ord(data[p + 2]) << 16)
+            + (ord(data[p + 3]) << 24)
+        )
     if lenpos == 3:
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16))
+        a += ord(data[p + 0]) + (ord(data[p + 1]) << 8) + (ord(data[p + 2]) << 16)
     if lenpos == 2:
-        a += (ord(data[p + 0]) + (ord(data[p + 1]) << 8))
+        a += ord(data[p + 0]) + (ord(data[p + 1]) << 8)
     if lenpos == 1:
         a += ord(data[p + 0])
 
-    a &= 0xffffffff
-    b &= 0xffffffff
-    c &= 0xffffffff
+    a &= 0xFFFFFFFF
+    b &= 0xFFFFFFFF
+    c &= 0xFFFFFFFF
     if lenpos == 0:
         return c, b
 
@@ -172,8 +262,8 @@ def generate_hash_code(partition_key):
         return 0
 
     hash_tuple = compute_hash(partition_key, 0, 0)
-    hash_value = (hash_tuple[0] ^ hash_tuple[1]) & 0xffff
-    return c_signed_short.unpack(struct.pack('>H', hash_value))[0]
+    hash_value = (hash_tuple[0] ^ hash_tuple[1]) & 0xFFFF
+    return c_signed_short.unpack(struct.pack(">H", hash_value))[0]
 
 
 class PartitionResolver:
