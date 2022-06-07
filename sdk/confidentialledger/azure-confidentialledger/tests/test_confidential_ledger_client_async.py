5cb5b0bd6599aa1f9df10232d5b04e66b1bdfa4e
Lyndon Shi <lyshi@microsoft.com>
2022-06-07 15:14:24 -0700

    remove post and wait

--- sdk/confidentialledger/azure-confidentialledger/tests/test_confidential_ledger_client_async.py
+++ sdk/confidentialledger/azure-confidentialledger/tests/test_confidential_ledger_client_async.py
@@ -92,8 +92,11 @@ class ConfidentialLedgerClientTest(ConfidentialLedgerTestCase):
         self.assertEqual(latest_entry["contents"], entry_contents)
         self.assertEqual(latest_entry["collectionId"], append_result_sub_ledger_id)
 
-        await client.confidential_ledger.post_ledger_entry_and_wait_for_commit(
+        await client.confidential_ledger.post_ledger_entry_wait_for_commit(
             {"contents": "Test entry 2 from Python SDK"}
+        )
+        await client.confidential_ledger.wait_until_durable(
+            transaction_id=append_result_transaction_id
         )
 
         latest_entry = await client.confidential_ledger.get_current_ledger_entry()
@@ -177,9 +180,12 @@ class ConfidentialLedgerClientTest(ConfidentialLedgerTestCase):
         self.assertEqual(latest_entry["contents"], entry_contents)
         self.assertEqual(latest_entry["collectionId"], append_result_sub_ledger_id)
 
-        await client.confidential_ledger.post_ledger_entry_and_wait_for_commit(
+        await client.confidential_ledger.post_ledger_entry_wait_for_commit(
             {"contents": f"Test entry 2 from Python SDK. Collection: {collection_id}"},
             collection_id=collection_id,
+        )
+        await client.confidential_ledger.wait_until_durable(
+            transaction_id=append_result_transaction_id
         )
 
         latest_entry = await client.confidential_ledger.get_current_ledger_entry(
