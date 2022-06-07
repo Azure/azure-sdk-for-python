82d4b0fd503f8a435b26fe7feebc13a88e9f46cb
Jiefeng Chen (WICRESOFT NORTH AMERICA LTD) <v-jiechen@microsoft.com>
2022-06-07 11:12:49 +0800

    change branch for test

--- scripts/auto_release/main.py
+++ scripts/auto_release/main.py
@@ -81,8 +81,8 @@ def all_files(path: str, files: List[str]):
 
 
 def checkout_azure_default_branch():
-    usr = 'Azure'
-    branch = 'main'
+    usr = 'azure-sdk'
+    branch = 't2-communication-2022-06-06-53443'
     print_exec(f'git remote add {usr} https://github.com/{usr}/azure-sdk-for-python.git')
     print_check(f'git fetch {usr} {branch}')
     print_check(f'git checkout {usr}/{branch}')
