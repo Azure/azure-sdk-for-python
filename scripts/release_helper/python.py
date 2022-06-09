38c743bee99ba57c627bd2fefdcb32be099b47b5
Jiefeng Chen (WICRESOFT NORTH AMERICA LTD) <v-jiechen@microsoft.com>
2022-06-09 17:14:29 +0800

    add package name

--- scripts/release_helper/python.py
+++ scripts/release_helper/python.py
@@ -1,9 +1,11 @@
+import re
+
 from common import IssueProcess, Common
 from typing import Any, List
 import os
 
 # assignee dict which will be assigned to handle issues
-_PYTHON_OWNER = {'BigCat20196', 'msyyc'}
+_PYTHON_OWNER = {'BigCat20196', 'msyyc', 'azure-sdk'}
 
 # 'github assignee': 'token'
 _ASSIGNEE_TOKEN_PYTHON = {
@@ -13,8 +15,26 @@ _ASSIGNEE_TOKEN_PYTHON = {
 
 
 class IssueProcessPython(IssueProcess):
-    pass
 
+    def get_package_name(self) -> None:
+        pattern_resource_manager = re.compile(r'/specification/([\w-]+/)+resource-manager')
+        readme_python_path = pattern_resource_manager.search(self.readme_link).group() + '/readme.python.md'
+        contents = str(self.issue_package.rest_repo.get_contents(readme_python_path).decoded_content)
+        pattern_package = re.compile(r'package-name: [\w+-.]+')
+        self.package_name = pattern_package.search(contents).group().split(':')[-1].strip()
+
+    def edit_issue_body(self) -> None:
+        self.get_package_name()
+        issue_body_list = [i for i in self.issue_package.issue.body.split("\n") if i]
+        issue_body_list.insert(0, f'\n{self.readme_link.replace("/readme.md", "")}')
+        issue_body_list.insert(1, self.package_name)
+        issue_body_up = ''
+        # solve format problems
+        for raw in issue_body_list:
+            if raw == '---\r' or raw == '---':
+                issue_body_up += '\n'
+            issue_body_up += raw + '\n'
+        self.issue_package.issue.edit(body=issue_body_up)
 
 class Python(Common):
     def __init__(self, issues, assignee_token, language_owner):
