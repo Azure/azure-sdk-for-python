fb9e6667dc848591c9ad547dc6f5cfa30157cca0
Yuchao Yan <yuchaoyan@microsoft.com>
2022-06-09 10:05:54 +0800

    Update setup.py

--- sdk/deviceupdate/azure-iot-deviceupdate/setup.py
+++ sdk/deviceupdate/azure-iot-deviceupdate/setup.py
@@ -67,7 +67,7 @@ setup(
         'pytyped': ['py.typed'],
     },
     install_requires=[
-        "azure-core<2.0.0,>=1.20.1",
+        "azure-core<2.0.0,>=1.23.0",
         "msrest>=0.6.21",
         'six>=1.11.0',
     ],
