751f1d96aa60cacd87d393ae071074da1e97bd97
Jiefeng Chen (WICRESOFT NORTH AMERICA LTD) <v-jiechen@microsoft.com>
2022-06-09 17:52:56 +0800

    fix bug

--- scripts/release_helper/python.py
+++ scripts/release_helper/python.py
@@ -16,19 +16,19 @@ _ASSIGNEE_TOKEN_PYTHON = {
 
 class IssueProcessPython(IssueProcess):
 
-    def get_package_name(self) -> str:
+    def get_package_name(self) -> None:
         pattern_resource_manager = re.compile(r'/specification/([\w-]+/)+resource-manager')
         readme_python_path = pattern_resource_manager.search(self.readme_link).group() + '/readme.python.md'
         contents = str(self.issue_package.rest_repo.get_contents(readme_python_path).decoded_content)
         pattern_package = re.compile(r'package-name: [\w+-.]+')
-        package_name = pattern_package.search(contents).group().split(':')[-1].strip()
-        print(f'*** package_name: {package_name}')
-        return package_name
+        self.package_name = pattern_package.search(contents).group().split(':')[-1].strip()
+        print(f'*** package_name: {self.package_name}')
 
     def edit_issue_body(self) -> None:
+        self.get_package_name()
         issue_body_list = [i for i in self.issue_package.issue.body.split("\n") if i]
         issue_body_list.insert(0, f'\n{self.readme_link.replace("/readme.md", "")}')
-        issue_body_list.insert(1, self.get_package_name())
+        issue_body_list.insert(1, self.package_name)
         issue_body_up = ''
         # solve format problems
         for raw in issue_body_list:
@@ -41,6 +41,7 @@ class Python(Common):
     def __init__(self, issues, assignee_token, language_owner):
         super(Python, self).__init__(issues, assignee_token, language_owner)
         self.file_out_name = 'release_python_status.md'
+        self.issue_process_function = IssueProcessPython
 
 
 def python_process(issues: List[Any]):
