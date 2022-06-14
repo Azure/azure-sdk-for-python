2f91f81faa99c28b92a529657ba5ee79d705c915
Lyndon Shi <lyshi@microsoft.com>
2022-06-14 15:13:27 -0700

    complete poller nonasync

--- sdk/confidentialledger/azure-confidentialledger/azure/confidentialledger/operations/_patch.py
+++ sdk/confidentialledger/azure-confidentialledger/azure/confidentialledger/operations/_patch.py
@@ -9,20 +9,21 @@ Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python
 
 import logging
 import time
-from typing import Any, IO, Callable, ClassVar, List, Optional, Union
+from typing import Any, IO, Callable, List, Optional, Union, cast
 
-from azure.core.polling import PollingMethod, LROPoller
+from azure.core.polling import PollingMethod, LROPoller, NoPolling
 
 from azure.confidentialledger.operations._operations import (
     ConfidentialLedgerOperations as GeneratedOperations,
 )
 from azure.confidentialledger.operations._operations import JSON
 
+logger = logging.getLogger(__name__)
+
 __all__: List[str] = [
     "ConfidentialLedgerOperations"
 ]  # Add all objects you want publicly available to users at this package level
 
-logger = logging.getLogger(__name__)
 
 def patch_sdk():
     """Do not remove from this file.
@@ -38,7 +39,7 @@ class StatePollingMethod(PollingMethod):
     completes when 'state' becomes a desired value.
     """
 
-    def __init__(self, operation: Callable[[], JSON], desired_state: str, polling_interval_s: float = 0.5):
+    def __init__(self, operation: Callable[[], JSON], desired_state: str, polling_interval_s: float):
         self._operation = operation
         self._desired_state = desired_state
         self._polling_interval_s = polling_interval_s
@@ -83,135 +84,100 @@ class StatePollingMethod(PollingMethod):
 class ConfidentialLedgerOperations(GeneratedOperations):
     def get_ledger_entry(
         self, transaction_id: str, *, collection_id: Optional[str] = None, **kwargs: Any
-    ):
-        """Gets the ledger entry at the specified transaction id. A collection id may optionally be
-        specified to indicate the collection from which to fetch the value.
+    ) -> LROPoller[JSON]:
+        polling = kwargs.pop("polling", True)  # type: Union[bool, PollingMethod]
+        lro_delay = kwargs.pop("polling_interval", 0.5)
 
-        To return older ledger entries, the relevant sections of the ledger must be read from disk and
-        validated. To prevent blocking within the enclave, the response will indicate whether the entry
-        is ready and part of the response, or if the loading is still ongoing.
-
-        :param transaction_id: Identifies a write transaction.
-        :type transaction_id: str
-        :keyword collection_id: The collection id. Default value is None.
-        :paramtype collection_id: str
-        :keyword retry_backoff_factor: Interval, in seconds, between retries while waiting for results,
-            defaults to 0.5.
-        :paramtype retry_backoff_factor: float
-        :keyword retry_total: Maximum number of times to try the query, defaults to 6. Retries are
-            attempted if the result is not Ready.
-        :paramtype retry_total: int
-        :return: JSON object
-        :rtype: JSON
-        :raises: ~azure.core.exceptions.HttpResponseError
-
-        Example:
-            .. code-block:: python
-
-                # response body for status code(s): 200
-                response.json() == {
-                    "entry": {
-                        "collectionId": {
-                            "collectionId": "str"  # Required.
-                        },
-                        "contents": "str",  # Required. Contents of the ledger entry.
-                        "transactionId": "str"  # Optional. A unique identifier for the state
-                          of the ledger. If returned as part of a LedgerEntry, it indicates the state
-                          from which the entry was read.
-                    },
-                    "state": "str"  # Required. State of a ledger query. Known values are:
-                      "Loading", "Ready".
-                }
-        """
-
-        retry_backoff_factor = kwargs.pop("retry_backoff_factor", 0.5)
-        retry_total = kwargs.pop("retry_total", 6)
-
-        ready_const = "Ready"  # Value of 'state' field when the entry is available.
-        most_recent_state = None
-        for _ in range(retry_total):
-            result = super().get_ledger_entry(
+        def operation() -> JSON:
+            return super().get_ledger_entry(
                 transaction_id, collection_id=collection_id, **kwargs
             )
-            if result["state"] == ready_const:
-                return result
 
-            most_recent_state = result["state"]
-            time.sleep(retry_backoff_factor)
+        initial_response = operation()
 
-        raise TimeoutError(
-            "After {0} attempts, the query still had state {1}, not {2}".format(
-                retry_total, most_recent_state, ready_const
-            )
-        )
+        if polling is True:
+            polling_method = cast(PollingMethod, StatePollingMethod(operation, "Ready", lro_delay))
+        elif polling is False:
+            polling_method = cast(PollingMethod, NoPolling())
+        else:
+            polling_method = polling
 
-    def get_receipt(self, transaction_id: str, **kwargs: Any) -> JSON:
-        """Gets a receipt certifying ledger contents at a particular transaction id.
+        return LROPoller(self._client, initial_response, None, polling_method)
 
-        :param transaction_id: Identifies a write transaction.
-        :type transaction_id: str
-        :keyword retry_backoff_factor: Interval, in seconds, between retries while waiting for results,
-            defaults to 0.5.
-        :paramtype retry_backoff_factor: float
-        :keyword retry_total: Maximum number of times to try the query, defaults to 6. Retries are
-            attempted if the result is not Ready.
-        :paramtype retry_total: int
-        :return: JSON object
-        :rtype: JSON
-        :raises: ~azure.core.exceptions.HttpResponseError
+    def get_receipt(self, transaction_id: str, **kwargs: Any) -> LROPoller[JSON]:
+        polling = kwargs.pop("polling", True)  # type: Union[bool, PollingMethod]
+        lro_delay = kwargs.pop("polling_interval", 0.5)
 
-        Example:
-            .. code-block:: python
+        def operation() -> JSON:
+            return super().get_receipt(transaction_id=transaction_id, **kwargs)
 
-                # response body for status code(s): 200
-                response.json() == {
-                    "receipt": {
-                        "cert": "str",  # Optional.
-                        "leaf": "str",  # Optional.
-                        "leafComponents": {
-                            "claimsDigest": "str",  # Optional.
-                            "commitEvidence": "str",  # Optional.
-                            "writeSetDigest": "str"  # Optional.
-                        },
-                        "nodeId": "str",  # Required.
-                        "proof": [
-                            {
-                                "left": "str",  # Optional. Required.
-                                "right": "str"  # Optional. Required.
-                            }
-                        ],
-                        "root": "str",  # Optional.
-                        "serviceEndorsements": [
-                            "str"  # Optional.
-                        ],
-                        "signature": "str"  # Required.
-                    },
-                    "state": "str",  # Required. State of a ledger query. Known values are:
-                      "Loading", "Ready".
-                    "transactionId": "str"  # Required. A unique identifier for the state of the
-                      ledger. If returned as part of a LedgerEntry, it indicates the state from which
-                      the entry was read.
-                }
+        initial_response = operation()
+
+        if polling is True:
+            polling_method = cast(PollingMethod, StatePollingMethod(operation, "Ready", lro_delay))
+        elif polling is False:
+            polling_method = cast(PollingMethod, NoPolling())
+        else:
+            polling_method = polling
+
+        return LROPoller(self._client, initial_response, None, polling_method)
+
+    def begin_post_ledger_entry(
+        self,
+        entry: Union[JSON, IO],
+        *,
+        collection_id: Optional[str] = None,
+        **kwargs: Any,
+    ) -> LROPoller[JSON]:
+        """Writes a ledger entry and returns a poller to wait for it to be durably committed.
+
+        A collection id may optionally be specified.
         """
 
-        retry_backoff_factor = kwargs.pop("retry_backoff_factor", 0.5)
-        retry_total = kwargs.pop("retry_total", 6)
+        # Pop arguments that are unexpected in the pipeline.
+        polling = kwargs.pop("polling", True)  # type: Union[bool, PollingMethod]
+        lro_delay = kwargs.pop("polling_interval", 0.5)
 
-        ready_const = "Ready"  # Value of 'state' field when the receipt is available.
-        most_recent_state = None
-        for _ in range(retry_total):
-            result = super().get_receipt(transaction_id=transaction_id, **kwargs)
-            if result["state"] == ready_const:
-                return result
-
-            most_recent_state = result["state"]
-            time.sleep(retry_backoff_factor)
-
-        raise TimeoutError(
-            "After {0} attempts, the query still had state {1}, not {2}".format(
-                retry_total, most_recent_state, ready_const
-            )
+        post_result = self.post_ledger_entry(
+            entry, collection_id=collection_id, **kwargs
         )
+        transaction_id = post_result["transactionId"]
+
+        kwargs["polling"] = polling
+        kwargs["polling_interval"] = lro_delay
+        kwargs["_post_ledger_entry_response"] = post_result
+        return self.begin_wait_for_commit(transaction_id, **kwargs)
+
+    def begin_wait_for_commit(
+        self,
+        transaction_id,  # type: str
+        **kwargs,  # type: Any
+    ) -> LROPoller[JSON]:
+        """Creates a poller that queries the state of the specified transaction until it is
+        Committed, a state that indicates the transaction is durably stored in the Confidential
+        Ledger.
+        """
+        polling = kwargs.pop("polling", True)  # type: Union[bool, PollingMethod]
+        lro_delay = kwargs.pop("polling_interval", 0.5)
+
+        # If this poller was called from begin_post_ledger_entry, we should return the
+        # post_ledger_entry response, not the transaction status.
+        post_result = kwargs.pop("_post_ledger_entry_response", None)
+        deserialization_callback = lambda x: x if post_result is None else post_result
+
+        def operation() -> JSON:
+            return super().get_transaction_status(transaction_id=transaction_id, **kwargs)
+
+        initial_response = operation()
+
+        if polling is True:
+            polling_method = cast(PollingMethod, StatePollingMethod(operation, "Ready", lro_delay))
+        elif polling is False:
+            polling_method = cast(PollingMethod, NoPolling())
+        else:
+            polling_method = polling
+        
+        return LROPoller(self._client, initial_response, deserialization_callback, polling_method)
 
     def post_ledger_entry(
         self,
@@ -258,92 +224,3 @@ class ConfidentialLedgerOperations(GeneratedOperations):
             },
         )
         return super().post_ledger_entry(entry, collection_id=collection_id, **kwargs)
-
-    def post_ledger_entry_and_wait_for_commit(
-        self,
-        entry: Union[JSON, IO],
-        *,
-        collection_id: Optional[str] = None,
-        **kwargs: Any,
-    ):
-        """Writes a ledger entry and waits for it to be durably committed.
-
-        The result is the expected JSON response with an additional field
-        'transactionId' which represents the transaction identifier for this write operation.
-
-        A collection id may optionally be specified.
-
-        :param entry: Ledger entry.
-        :type entry: Union[JSON, IO]
-        :keyword collection_id: The collection id. Default value is None.
-        :paramtype collection_id: str
-        :keyword retry_backoff_factor: Interval, in seconds, between retries, defaults to 0.5.
-        :paramtype retry_backoff_factor: float
-        :keyword retry_total: Maximum number of times to try the query, defaults to 3. Retries are
-            attempted if the specified transaction is not Committed yet.
-        :paramtype retry_total: int
-        :return: JSON object
-        :rtype: JSON
-        :raises: ~azure.core.exceptions.HttpResponseError
-        """
-
-        # Pop arguments that are unexpected in the pipeline.
-        retry_backoff_factor = kwargs.pop("retry_backoff_factor", 0.5)
-        retry_total = kwargs.pop("retry_total", 3)
-
-        post_result = self.post_ledger_entry(
-            entry, collection_id=collection_id, **kwargs
-        )
-        transaction_id = post_result["transactionId"]
-
-        try:
-            kwargs["retry_backoff_factor"] = retry_backoff_factor
-            kwargs["retry_total"] = retry_total
-            self.wait_until_durable(transaction_id, **kwargs)
-        except TimeoutError as e:
-            raise TimeoutError(
-                f"Timed out while waiting for commit; however, the entry may be successfully committed eventually."
-            ) from e
-
-        return post_result
-
-    def wait_until_durable(
-        self,
-        transaction_id,  # type: str
-        **kwargs,  # type: Any
-    ):
-        # type: (...) -> None
-        """Queries the status of the specified transaction until it is Committed, indicating that
-        the transaction is durably stored in the Confidential Ledger. If this state is not reached
-        by `max_queries`, a TimeoutError is raised.
-
-        :param transaction_id: Identifies the transaction to wait for.
-        :type transaction_id: str
-        :keyword retry_backoff_factor: Interval, in seconds, between retries, defaults to 0.5.
-        :paramtype retry_backoff_factor: float
-        :keyword retry_total: Maximum number of times to try the query, defaults to 3. Retries are
-            attempted if the specified transaction is not Committed yet.
-        :paramtype retry_total: int
-        :return: None.
-        :rtype: None
-        :raises: ~azure.core.exceptions.HttpResponseError
-        :raises: TimeoutError
-        """
-
-        retry_backoff_factor = kwargs.pop("retry_backoff_factor", 0.5)
-        retry_total = kwargs.pop("retry_total", 3)
-
-        committed_const = "Committed"
-        for attempt_num in range(retry_total):
-            transaction_status = self.get_transaction_status(
-                transaction_id=transaction_id, **kwargs
-            )
-            if transaction_status["state"] == committed_const:
-                return
-
-            if attempt_num < retry_total - 1:
-                time.sleep(retry_backoff_factor)
-
-        raise TimeoutError(
-            "Transaction {0} is not {1} yet".format(transaction_id, committed_const)
-        )
