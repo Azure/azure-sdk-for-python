07abc8e4f09a33a4ff9674ae6b2224f50dae57d3
Yashika Jain <t-yjain@microsoft.com>
2022-06-14 16:19:06 +0530

    linting corrected

--- sdk/loadtestservice/azure-analytics-loadtestservice/azure/analytics/loadtestservice/operations/_patch.py
+++ sdk/loadtestservice/azure-analytics-loadtestservice/azure/analytics/loadtestservice/operations/_patch.py
@@ -7,15 +7,11 @@
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
 
-from typing import IO, List, cast
-import sys
+from typing import List, cast
 from msrest import Serializer
 from azure.core.exceptions import ClientAuthenticationError, HttpResponseError, ResourceExistsError, ResourceNotFoundError, map_error
-from azure.core.pipeline import PipelineResponse
-from azure.core.pipeline.transport import HttpResponse
 from azure.core.rest import HttpRequest
 from azure.core.utils import case_insensitive_dict
-from azure.core.paging import ItemPaged
 from .._vendor import _format_url_section
 from ._operations import TestOperations as TestOperationsGenerated, JSON, ClsType
 
@@ -28,40 +24,44 @@ def build_upload_test_file_request(
         file_content,
         **kwargs,
     ) -> HttpRequest:
-        
-        _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
-        _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
-        api_version = kwargs.pop('api_version', _params.pop('api-version', "2022-06-01-preview"))  # type: str
-        accept = _headers.pop('Accept', "application/json")
+    """
+    Core logic for uploading a file
+    """
+    _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
+    _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
+    api_version = kwargs.pop('api_version', _params.pop('api-version', "2022-06-01-preview"))
+    accept = _headers.pop('Accept', "application/json")
 
         # Construct URL
-        _url = "/loadtests/{testId}/files/{fileId}"
-        path_format_arguments = {
-            "testId": _SERIALIZER.url("test_id", test_id, 'str', max_length=50, min_length=2, pattern=r'^[a-z0-9_-]*$'),
-            "fileId": _SERIALIZER.url("file_id", file_id, 'str', max_length=50, min_length=2, pattern=r'^[a-z0-9_-]*$'),   
-        }
+    _url = "/loadtests/{testId}/files/{fileId}"
+    path_format_arguments = {
+        "testId": _SERIALIZER.url("test_id", test_id, 'str',
+        max_length=50, min_length=2, pattern=r'^[a-z0-9_-]*$'),
+        "fileId": _SERIALIZER.url("file_id", file_id, 'str',
+        max_length=50, min_length=2, pattern=r'^[a-z0-9_-]*$'),
+    }
 
-        _url = _format_url_section(_url, **path_format_arguments)
+    _url = _format_url_section(_url, **path_format_arguments)
 
-        # Construct parameters
-        _params['api-version'] = _SERIALIZER.query("api_version", api_version, 'str')
+    # Construct parameters
+    _params['api-version'] = _SERIALIZER.query("api_version", api_version, 'str')
 
-        # Construct headers
-        _headers['Accept'] = _SERIALIZER.header("accept", accept, 'str')
+    # Construct headers
+    _headers['Accept'] = _SERIALIZER.header("accept", accept, 'str')
 
-        return HttpRequest(
-            method="PUT",
-            url=_url,
-            files=file_content,
-            params=_params,
-            headers=_headers,
-            **kwargs
-        )
-            
+    return HttpRequest(
+        method="PUT",
+        url=_url,
+        files=file_content,
+        params=_params,
+        headers=_headers,
+        **kwargs
+    )
+
 class TestOperations(TestOperationsGenerated):
-    def __init__(self, *args, **kwargs):
-        super(TestOperations, self).__init__(*args, **kwargs)
-    
+    """
+    for performing the operations on test
+    """
     def upload_test_file(
         self,
         test_id,
@@ -70,6 +70,7 @@ class TestOperations(TestOperationsGenerated):
         **kwargs
     ) -> JSON:
         """
+        Uploading a test file
         """
 
         error_map = {
@@ -79,12 +80,11 @@ class TestOperations(TestOperationsGenerated):
 
         _headers = kwargs.pop("headers", {}) or {}
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
-
-        api_version = kwargs.pop('api_version', _params.pop('api-version', "2022-06-01-preview"))  # type: str
+        api_version = kwargs.pop('api_version', _params.pop('api-version', "2022-06-01-preview"))
         cls = kwargs.pop('cls', None)  # type: ClsType[JSON]
 
         _content=file_content
-            
+
         request = build_upload_test_file_request(
             test_id,
             file_id,
@@ -95,7 +95,7 @@ class TestOperations(TestOperationsGenerated):
         )
         request.url = self._client.format_url(request.url)
         request.method = "PUT"
-           
+
         pipeline_response = self._client._pipeline.run( #type: ignore # pylint: disable=protected-access
             request,
             stream=False,
@@ -110,14 +110,14 @@ class TestOperations(TestOperationsGenerated):
         if response.content:
             deserialized = response.json()
         else:
-            deserialized = None   
-
+            deserialized = None
         if cls:
-            return cls(pipeline_response, cast(JSON, deserialized), {})     
+            return cls(pipeline_response, cast(JSON, deserialized), {})
 
         return cast(JSON, deserialized)
 
-__all__: List[str] = ["TestOperations"]  # Add all objects you want publicly available to users at this package level
+__all__: List[str] = ["TestOperations"]
+# Add all objects you want publicly available to users at this package level
 
 def patch_sdk():
     """Do not remove from this file.
