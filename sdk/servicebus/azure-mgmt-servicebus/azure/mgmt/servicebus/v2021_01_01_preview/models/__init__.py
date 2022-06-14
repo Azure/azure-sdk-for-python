2ae00b6e10e4e9e340ea090e120de5d91895d538
PythonSdkPipelines <PythonSdkPipelines>
2022-06-14 06:29:06 +0000

    code and test

--- sdk/servicebus/azure-mgmt-servicebus/azure/mgmt/servicebus/v2021_01_01_preview/models/__init__.py
+++ sdk/servicebus/azure-mgmt-servicebus/azure/mgmt/servicebus/v2021_01_01_preview/models/__init__.py
@@ -6,116 +6,61 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-try:
-    from ._models_py3 import AccessKeys
-    from ._models_py3 import Action
-    from ._models_py3 import ArmDisasterRecovery
-    from ._models_py3 import ArmDisasterRecoveryListResult
-    from ._models_py3 import CheckNameAvailability
-    from ._models_py3 import CheckNameAvailabilityResult
-    from ._models_py3 import ConnectionState
-    from ._models_py3 import CorrelationFilter
-    from ._models_py3 import DictionaryValue
-    from ._models_py3 import Encryption
-    from ._models_py3 import ErrorAdditionalInfo
-    from ._models_py3 import ErrorResponse
-    from ._models_py3 import ErrorResponseError
-    from ._models_py3 import FailoverProperties
-    from ._models_py3 import Identity
-    from ._models_py3 import KeyVaultProperties
-    from ._models_py3 import MessageCountDetails
-    from ._models_py3 import MigrationConfigListResult
-    from ._models_py3 import MigrationConfigProperties
-    from ._models_py3 import NWRuleSetIpRules
-    from ._models_py3 import NWRuleSetVirtualNetworkRules
-    from ._models_py3 import NetworkRuleSet
-    from ._models_py3 import NetworkRuleSetListResult
-    from ._models_py3 import Operation
-    from ._models_py3 import OperationDisplay
-    from ._models_py3 import OperationListResult
-    from ._models_py3 import PrivateEndpoint
-    from ._models_py3 import PrivateEndpointConnection
-    from ._models_py3 import PrivateEndpointConnectionListResult
-    from ._models_py3 import PrivateLinkResource
-    from ._models_py3 import PrivateLinkResourcesListResult
-    from ._models_py3 import RegenerateAccessKeyParameters
-    from ._models_py3 import Resource
-    from ._models_py3 import ResourceNamespacePatch
-    from ._models_py3 import Rule
-    from ._models_py3 import RuleListResult
-    from ._models_py3 import SBAuthorizationRule
-    from ._models_py3 import SBAuthorizationRuleListResult
-    from ._models_py3 import SBNamespace
-    from ._models_py3 import SBNamespaceListResult
-    from ._models_py3 import SBNamespaceUpdateParameters
-    from ._models_py3 import SBQueue
-    from ._models_py3 import SBQueueListResult
-    from ._models_py3 import SBSku
-    from ._models_py3 import SBSubscription
-    from ._models_py3 import SBSubscriptionListResult
-    from ._models_py3 import SBTopic
-    from ._models_py3 import SBTopicListResult
-    from ._models_py3 import SqlFilter
-    from ._models_py3 import SqlRuleAction
-    from ._models_py3 import Subnet
-    from ._models_py3 import SystemData
-    from ._models_py3 import TrackedResource
-    from ._models_py3 import UserAssignedIdentityProperties
-except (SyntaxError, ImportError):
-    from ._models import AccessKeys  # type: ignore
-    from ._models import Action  # type: ignore
-    from ._models import ArmDisasterRecovery  # type: ignore
-    from ._models import ArmDisasterRecoveryListResult  # type: ignore
-    from ._models import CheckNameAvailability  # type: ignore
-    from ._models import CheckNameAvailabilityResult  # type: ignore
-    from ._models import ConnectionState  # type: ignore
-    from ._models import CorrelationFilter  # type: ignore
-    from ._models import DictionaryValue  # type: ignore
-    from ._models import Encryption  # type: ignore
-    from ._models import ErrorAdditionalInfo  # type: ignore
-    from ._models import ErrorResponse  # type: ignore
-    from ._models import ErrorResponseError  # type: ignore
-    from ._models import FailoverProperties  # type: ignore
-    from ._models import Identity  # type: ignore
-    from ._models import KeyVaultProperties  # type: ignore
-    from ._models import MessageCountDetails  # type: ignore
-    from ._models import MigrationConfigListResult  # type: ignore
-    from ._models import MigrationConfigProperties  # type: ignore
-    from ._models import NWRuleSetIpRules  # type: ignore
-    from ._models import NWRuleSetVirtualNetworkRules  # type: ignore
-    from ._models import NetworkRuleSet  # type: ignore
-    from ._models import NetworkRuleSetListResult  # type: ignore
-    from ._models import Operation  # type: ignore
-    from ._models import OperationDisplay  # type: ignore
-    from ._models import OperationListResult  # type: ignore
-    from ._models import PrivateEndpoint  # type: ignore
-    from ._models import PrivateEndpointConnection  # type: ignore
-    from ._models import PrivateEndpointConnectionListResult  # type: ignore
-    from ._models import PrivateLinkResource  # type: ignore
-    from ._models import PrivateLinkResourcesListResult  # type: ignore
-    from ._models import RegenerateAccessKeyParameters  # type: ignore
-    from ._models import Resource  # type: ignore
-    from ._models import ResourceNamespacePatch  # type: ignore
-    from ._models import Rule  # type: ignore
-    from ._models import RuleListResult  # type: ignore
-    from ._models import SBAuthorizationRule  # type: ignore
-    from ._models import SBAuthorizationRuleListResult  # type: ignore
-    from ._models import SBNamespace  # type: ignore
-    from ._models import SBNamespaceListResult  # type: ignore
-    from ._models import SBNamespaceUpdateParameters  # type: ignore
-    from ._models import SBQueue  # type: ignore
-    from ._models import SBQueueListResult  # type: ignore
-    from ._models import SBSku  # type: ignore
-    from ._models import SBSubscription  # type: ignore
-    from ._models import SBSubscriptionListResult  # type: ignore
-    from ._models import SBTopic  # type: ignore
-    from ._models import SBTopicListResult  # type: ignore
-    from ._models import SqlFilter  # type: ignore
-    from ._models import SqlRuleAction  # type: ignore
-    from ._models import Subnet  # type: ignore
-    from ._models import SystemData  # type: ignore
-    from ._models import TrackedResource  # type: ignore
-    from ._models import UserAssignedIdentityProperties  # type: ignore
+from ._models_py3 import AccessKeys
+from ._models_py3 import Action
+from ._models_py3 import ArmDisasterRecovery
+from ._models_py3 import ArmDisasterRecoveryListResult
+from ._models_py3 import CheckNameAvailability
+from ._models_py3 import CheckNameAvailabilityResult
+from ._models_py3 import ConnectionState
+from ._models_py3 import CorrelationFilter
+from ._models_py3 import DictionaryValue
+from ._models_py3 import Encryption
+from ._models_py3 import ErrorAdditionalInfo
+from ._models_py3 import ErrorResponse
+from ._models_py3 import ErrorResponseError
+from ._models_py3 import FailoverProperties
+from ._models_py3 import Identity
+from ._models_py3 import KeyVaultProperties
+from ._models_py3 import MessageCountDetails
+from ._models_py3 import MigrationConfigListResult
+from ._models_py3 import MigrationConfigProperties
+from ._models_py3 import NWRuleSetIpRules
+from ._models_py3 import NWRuleSetVirtualNetworkRules
+from ._models_py3 import NetworkRuleSet
+from ._models_py3 import NetworkRuleSetListResult
+from ._models_py3 import Operation
+from ._models_py3 import OperationDisplay
+from ._models_py3 import OperationListResult
+from ._models_py3 import PrivateEndpoint
+from ._models_py3 import PrivateEndpointConnection
+from ._models_py3 import PrivateEndpointConnectionListResult
+from ._models_py3 import PrivateLinkResource
+from ._models_py3 import PrivateLinkResourcesListResult
+from ._models_py3 import RegenerateAccessKeyParameters
+from ._models_py3 import Resource
+from ._models_py3 import ResourceNamespacePatch
+from ._models_py3 import Rule
+from ._models_py3 import RuleListResult
+from ._models_py3 import SBAuthorizationRule
+from ._models_py3 import SBAuthorizationRuleListResult
+from ._models_py3 import SBNamespace
+from ._models_py3 import SBNamespaceListResult
+from ._models_py3 import SBNamespaceUpdateParameters
+from ._models_py3 import SBQueue
+from ._models_py3 import SBQueueListResult
+from ._models_py3 import SBSku
+from ._models_py3 import SBSubscription
+from ._models_py3 import SBSubscriptionListResult
+from ._models_py3 import SBTopic
+from ._models_py3 import SBTopicListResult
+from ._models_py3 import SqlFilter
+from ._models_py3 import SqlRuleAction
+from ._models_py3 import Subnet
+from ._models_py3 import SystemData
+from ._models_py3 import TrackedResource
+from ._models_py3 import UserAssignedIdentityProperties
+
 
 from ._service_bus_management_client_enums import (
     AccessRights,
