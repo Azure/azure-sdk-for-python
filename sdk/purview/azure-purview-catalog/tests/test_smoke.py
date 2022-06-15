cfd268ba87210b24b8989e9f1ed58205642a8040
yifan-zhou922 <78060542+yifan-zhou922@users.noreply.github.com>
2022-06-15 09:00:58 +0800

    Update python SDK for Purview Catalog (#24674)
    * update python SDK
    
    * update
    
    * update SDK
    
    * try fixing the test error by adding abstract class
    
    * Revert "try fixing the test error by adding abstract class"
    
    This reverts commit 80da3705d24f203061971a2d4c6c94918830138d.
    
    * Update _patch.py
    
    * update patch
    
    * update patch
    
    * Update setup.py
    
    * update tests
    
    * Update test_smoke.py
    
    * Update shared_requirements.txt
    
    * import Serializer
    
    * hide real account information
    
    * Update CHANGELOG.md
    
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>

--- sdk/purview/azure-purview-catalog/tests/test_smoke.py
+++ sdk/purview/azure-purview-catalog/tests/test_smoke.py
@@ -7,16 +7,14 @@
 from testcase import PurviewCatalogTest, PurviewCatalogPowerShellPreparer
 from urllib.parse import urlparse
 from azure.purview.catalog.operations._operations import build_entity_delete_by_guids_request, build_entity_list_by_guids_request
-
+from azure.purview.catalog.operations._patch import build_glossary_import_glossary_terms_via_csv_request_initial, build_entity_import_business_metadata_request
 
 class PurviewCatalogSmokeTest(PurviewCatalogTest):
-
     @PurviewCatalogPowerShellPreparer()
     def test_basic_smoke_test(self, purviewcatalog_endpoint):
         client = self.create_client(endpoint=purviewcatalog_endpoint)
         response = client.types.get_all_type_definitions()
-
-        assert set(response.keys()) == set(['enumDefs', 'structDefs', 'classificationDefs', 'entityDefs', 'relationshipDefs'])
+        assert set(response.keys()) == set(['enumDefs', 'structDefs', 'classificationDefs', 'entityDefs', 'relationshipDefs','businessMetadataDefs'])
 
     def test_delete_by_guids(self):
         request = build_entity_delete_by_guids_request(guids=["foo", "bar"])
@@ -25,3 +23,11 @@ class PurviewCatalogSmokeTest(PurviewCatalogTest):
     def test_list_by_guids(self):
         request = build_entity_list_by_guids_request(guids=["foo", "bar"])
         assert "guid=foo&guid=bar" in urlparse(request.url).query
+
+    def test_glossary_import(self):
+        request = build_glossary_import_glossary_terms_via_csv_request_initial(glossary_guid="111",api_version="2022-03-01-preview",files={},include_term_hierarchy=False)
+        assert "/glossary/111/terms/import" in urlparse(request.url)
+
+    def test_businessmetadata_import(self):
+        request = build_entity_import_business_metadata_request(files={})
+        assert "/atlas/v2/entity/businessmetadata/import" in urlparse(request.url)
