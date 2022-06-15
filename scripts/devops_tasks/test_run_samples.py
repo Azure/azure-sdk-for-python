2af74c117be54d5b8abb0dece943eeefafd7f5d7
Krista Pratico <krpratic@microsoft.com>
2022-06-14 17:06:19 -0700

    [textanalytics] merge single/multi category result (#24797)
    * merge SingleCategoryClassifyResult and MultiCategoryClassifyResult -> ClassifyDocumentResult
    
    * unskip samples
    
    * add back sample extract summary - this will fail until other PR is merged which removes it

--- scripts/devops_tasks/test_run_samples.py
+++ scripts/devops_tasks/test_run_samples.py
@@ -141,12 +141,6 @@ IGNORED_SAMPLES = {
     "azure-ai-textanalytics": [
         "sample_analyze_healthcare_entities_with_cancellation.py",
         "sample_analyze_healthcare_entities_with_cancellation_async.py",
-        "sample_single_category_classify.py",
-        "sample_multi_category_classify.py",
-        "sample_recognize_custom_entities.py",
-        "sample_multi_category_classify_async.py",
-        "sample_recognize_custom_entities_async.py",
-        "sample_single_category_classify_async.py",
     ]
 }
 
