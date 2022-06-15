3e3b6552e14dacd8fa100a8d5dba2bb7ed71a895
Yashika Jain <t-yjain@microsoft.com>
2022-06-15 13:32:59 +0530

    async naming

--- sdk/loadtestservice/azure-analytics-loadtestservice/tests/test_smoke_async.py
+++ sdk/loadtestservice/azure-analytics-loadtestservice/tests/test_smoke_async.py
@@ -11,13 +11,13 @@ from testcase_async import LoadtestserviceAsyncTest
 class LoadtestserviceSmokeAsyncTest(LoadtestserviceAsyncTest):
 
     @LoadtestservicePowerShellPreparer()
-    async def test_smoke_list_search_async(self, loadtestservice_endpoint):
+    async def test_smoke_list_search(self, loadtestservice_endpoint):
         client = self.create_client(endpoint=loadtestservice_endpoint)
         result=client.test.list_load_test_search()
         assert result is not None
 
     @LoadtestservicePowerShellPreparer()   
-    async def test_smoke_test_runs_search_async(self, loadtestservice_endpoint):
+    async def test_smoke_test_runs_search(self, loadtestservice_endpoint):
         client = self.create_client(endpoint=loadtestservice_endpoint)
         result=client.test_run.list_test_runs_search()
         assert result is not None
