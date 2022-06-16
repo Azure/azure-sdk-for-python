a47f7988eeeaeebea59d5b113c277616736c8265
l0lawrence <llawrence@microsoft.com>
2022-06-16 14:15:42 -0700

    moving around format - unittest and live test

--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/synctests/test_websocket.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/synctests/test_websocket.py
@@ -5,8 +5,30 @@
 
 import pytest
 
-from azure.eventhub._pyamqp import authentication, ReceiveClient
+from azure.eventhub._pyamqp import authentication, ReceiveClient, SendClient
 from azure.eventhub._pyamqp.constants import TransportType
+from azure.eventhub._pyamqp.message import Message
+
+
+def send_message(live_eventhub):
+    uri = "sb://{}/{}".format(live_eventhub['hostname'], live_eventhub['event_hub'])
+    sas_auth = authentication.SASTokenAuth(
+        uri=uri,
+        audience=uri,
+        username=live_eventhub['key_name'],
+        password=live_eventhub['access_key']
+    )
+
+    target = "amqps://{}/{}/Partitions/{}".format(
+        live_eventhub['hostname'],
+        live_eventhub['event_hub'],
+        live_eventhub['partition'])
+    
+    message = Message(value="Single Message")
+
+    with SendClient(live_eventhub['hostname'], target, auth=sas_auth, debug=True, transport_type=TransportType.Amqp) as send_client:
+        send_client.send_message(message)
+
 
 def test_event_hubs_client_web_socket(live_eventhub):
     uri = "sb://{}/{}".format(live_eventhub['hostname'], live_eventhub['event_hub'])
@@ -22,6 +44,8 @@ def test_event_hubs_client_web_socket(live_eventhub):
         live_eventhub['event_hub'],
         live_eventhub['consumer_group'],
         live_eventhub['partition'])
+
+    send_message()
 
     with ReceiveClient(live_eventhub['hostname'] + '/$servicebus/websocket/', source, auth=sas_auth, debug=False, timeout=10, prefetch=1, transport_type=TransportType.AmqpOverWebsocket) as receive_client:
         begin = receive_client._received_messages.qsize()
---
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_client_creation_pyamqp.py
@@ -0,0 +1,24 @@
+import pytest
+
+from azure.eventhub._pyamqp import SendClient, ReceiveClient
+
+def test_send_client_creation():
+
+    sender = SendClient(
+        "fake.host.com",
+        "fake_eh",
+        "my_fake_auth"
+    )
+    assert sender.target == "fake_eh"
+    assert sender._auth == "my_fake_auth"
+
+
+def test_receive_client_creation():
+
+    receiver = ReceiveClient(
+        "fake.host.com",
+        "fake_eh",
+        "my_fake_auth"
+    )
+    assert receiver.source == "fake_eh"
+    assert receiver._auth == "my_fake_auth"
