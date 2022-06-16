2fc00d418877c24dfc9ad6fa7cb0f646c35901a6
Jiefeng Chen (WICRESOFT NORTH AMERICA LTD) <v-jiechen@microsoft.com>
2022-06-16 11:39:26 +0800

    fix

--- scripts/release_issue_status/main.py
+++ scripts/release_issue_status/main.py
@@ -16,10 +16,9 @@ _NULL = ' '
 _FILE_OUT = 'release_issue_status.csv'
 _FILE_OUT_PYTHON = 'release_python_status.md'
 _PYTHON_SDK_ADMINISTRATORS = ['msyyc', 'BigCat20196', 'azure-sdk', 'Wzb123456789']
-_PYTHON_SDK_ASSIGNEES = ['BigCat20196', 'Wzb123456789', 'msyyc']
+_PYTHON_SDK_ASSIGNEES = ['BigCat20196', 'msyyc']
 _ASSIGNER_DICT = {'BigCat20196': os.getenv('JF_TOKEN'),
-                  'msyyc': os.getenv('Token'),
-                  'Wzb123456789': os.getenv('AZURESDK_BOT_TOKEN')}
+                  'msyyc': os.getenv('Token')}
 logging.basicConfig(level=logging.INFO,
                     format='[auto-reply  log] - %(funcName)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
