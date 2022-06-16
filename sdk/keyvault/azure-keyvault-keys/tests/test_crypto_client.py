cecf80ba8ab5c0402647c724462977bd87057038
McCoy Patiño <39780829+mccoyp@users.noreply.github.com>
2022-06-16 09:43:50 -0700

    Account for CBCPAD

--- sdk/keyvault/azure-keyvault-keys/tests/test_crypto_client.py
+++ sdk/keyvault/azure-keyvault-keys/tests/test_crypto_client.py
@@ -266,7 +266,7 @@ class TestCryptoClient(KeyVaultTestCase, KeysTestCase):
                         encrypt_result.algorithm,
                         encrypt_result.ciphertext,
                         iv=encrypt_result.iv,
-                        additional_authenticated_data=None if algorithm.endswith("CBC") else self.aad
+                        additional_authenticated_data=None if "CBC" in algorithm else self.aad
                     )
 
                 assert decrypt_result.key_id == imported_key.id
