e95c686f404999a68ac09cc0ef9d8e4c97c92b7a
Krista Pratico <krpratic@microsoft.com>
2022-06-20 17:55:35 -0700

    add dependency on isodate since we removed msrest (#24914)

--- sdk/cognitivelanguage/azure-ai-language-conversations/setup.py
+++ sdk/cognitivelanguage/azure-ai-language-conversations/setup.py
@@ -74,6 +74,7 @@ setup(
     },
     install_requires=[
         "azure-core<2.0.0,>=1.24.0",
+        "isodate>=0.6.0",
     ],
     project_urls={
         'Bug Reports': 'https://github.com/Azure/azure-sdk-for-python/issues',
