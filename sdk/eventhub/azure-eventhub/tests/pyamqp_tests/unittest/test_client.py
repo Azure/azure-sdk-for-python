51e8a88471d0bd4580c29809a92a85f75cc17681
l0lawrence <llawrence@microsoft.com>
2022-06-21 13:53:29 -0700

    keep_alive_thread

---
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_client.py
@@ -0,0 +1,34 @@
+import types
+from azure.eventhub._pyamqp.client import AMQPClient
+from azure.eventhub._pyamqp.authentication import SASTokenAuth
+
+
+def test_keep_alive_thread_fail_to_start():
+
+    class MockThread:
+        def __init__(self):
+            pass
+
+        def start(self):
+            raise RuntimeError("Fail to start")
+
+        def join(self):
+            raise RuntimeError("Fail to join")
+
+    def hack_open(ins):
+        ins._keep_alive_thread = MockThread()
+        ins._keep_alive_thread.start()
+
+    sas_auth = SASTokenAuth(
+        "sb://fake/fake", "fake", "fake", "fake")
+    target = "amqps://{}/{}".format("fake", "fake")
+    client = AMQPClient(target, auth=sas_auth, keep_alive_interval=10)
+    client.open = types.MethodType(hack_open, client)
+    try:
+        client.open()
+    except Exception as exc:
+        assert type(exc) == RuntimeError
+        client.close()
+        assert not client._keep_alive_thread
+
+#Keep alive thread still exists after client is closed 
