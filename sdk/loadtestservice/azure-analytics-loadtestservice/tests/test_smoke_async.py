62eb9f163bad9d36fa6b739f32eb8c93d9318abf
Yashika Jain <t-yjain@microsoft.com>
2022-06-21 19:22:40 +0530

    fix2

--- sdk/loadtestservice/azure-analytics-loadtestservice/tests/test_smoke_async.py
+++ sdk/loadtestservice/azure-analytics-loadtestservice/tests/test_smoke_async.py
@@ -10,9 +10,20 @@ from testcase_async import LoadtestserviceAsyncTest
 
 class LoadtestserviceSmokeAsyncTest(LoadtestserviceAsyncTest):
 
-    @LoadtestservicePowerShellPreparer()  
+    @LoadtestservicePowerShellPreparer()
+    async def test_smoke_create_or_update_test(self, loadtestservice_endpoint):
+        client = self.create_client(loadtestservice_endpoint)
+        test_id="000002" 
+        body_test={"resourceId":"/subscriptions/123456/resourceGroups/fake/providers/Microsoft.LoadTestService/loadtests/fake",
+                "testId":"000002","description":"","displayName":"test",
+                "loadTestConfig":{"engineSize":"m","engineInstances":1,"splitAllCSVs":False},"secrets":{},"environmentVariables":{},
+                "passFailCriteria":{"passFailMetrics":{}},"keyvaultReferenceIdentityType":"SystemAssigned","keyvaultReferenceIdentityId":None}
+        result = await client.test.create_or_update_test(test_id, body_test)
+        assert result is not None
+
+
+    @LoadtestservicePowerShellPreparer()
     async def test_smoke_list_search(self, loadtestservice_endpoint):
-        client = self.create_client(endpoint=loadtestservice_endpoint)
-        result=client.test.list_load_test_search()
+        client = self.create_client(loadtestservice_endpoint)
         result = await client.test.list_load_test_search()
         assert result is not None
