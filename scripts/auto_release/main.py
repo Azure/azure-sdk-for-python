4d8045e0bcb88f4a316a9f43e0d193faba9fcb8c
BigCat20196 <1095260342@qq.com>
2022-06-22 14:14:16 +0800

    for test

--- scripts/auto_release/main.py
+++ scripts/auto_release/main.py
@@ -82,7 +82,7 @@ def all_files(path: str, files: List[str]):
 
 def checkout_azure_default_branch():
     usr = 'Azure'
-    branch = 'main'
+    branch = 'jftest-generate-servicebus-ci'
     print_exec(f'git remote add {usr} https://github.com/{usr}/azure-sdk-for-python.git')
     print_check(f'git fetch {usr} {branch}')
     print_check(f'git checkout {usr}/{branch}')
