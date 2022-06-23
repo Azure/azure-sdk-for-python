248980f2e6344da05b796b1c60de40ab5c4e7cbc
Yogesh Mohanraj <ymohanraj@microsoft.com>
2022-06-23 13:19:50 -0700

    Updating sdk tests

--- sdk/communication/azure-communication-email/tests/test_email_client_e2e_async.py
+++ sdk/communication/azure-communication-email/tests/test_email_client_e2e_async.py
@@ -1,19 +1,22 @@
+import pytest
+
+from azure.communication.email.aio import EmailClient
 from azure.communication.email import (
-    EmailClient,
     EmailMessage,
     EmailContent,
     EmailRecipients,
     EmailAddress,
     EmailAttachment
 )
-from devtools_testutils import AzureRecordedTestCase, recorded_by_proxy
-from preparers import email_decorator
+from devtools_testutils import AzureRecordedTestCase
+from devtools_testutils.aio import recorded_by_proxy_async
+from async_preparers import email_decorator_async
 
 class TestEmailClient(AzureRecordedTestCase):
     # TODO: Change the assert statements once x-ms-request-id change is merged in
-    @email_decorator
-    @recorded_by_proxy
-    def test_send_email_single_recipient(self):
+    @email_decorator_async
+    @recorded_by_proxy_async
+    async def test_send_email_single_recipient(self):
         email_client = EmailClient(self.communication_connection_string)
 
         message = EmailMessage(
@@ -24,12 +27,13 @@ class TestEmailClient(AzureRecordedTestCase):
             )
         )
 
-        response = email_client.send(message)
-        assert response is not None
+        async with email_client:
+            response = await email_client.send(message)
+            assert response is not None
 
-    @email_decorator
-    @recorded_by_proxy
-    def test_send_email_multiple_recipients(self):
+    @email_decorator_async
+    @recorded_by_proxy_async
+    async def test_send_email_multiple_recipients(self):
         email_client = EmailClient(self.communication_connection_string)
 
         message = EmailMessage(
@@ -43,12 +47,13 @@ class TestEmailClient(AzureRecordedTestCase):
             )
         )
 
-        response = email_client.send(message)
-        assert response is not None
+        async with email_client:
+            response = await email_client.send(message)
+            assert response is not None
 
-    @email_decorator
-    @recorded_by_proxy
-    def test_send_email_attachment(self):
+    @email_decorator_async
+    @recorded_by_proxy_async
+    async def test_send_email_attachment(self):
         email_client = EmailClient(self.communication_connection_string)
 
         message = EmailMessage(
@@ -66,13 +71,14 @@ class TestEmailClient(AzureRecordedTestCase):
             ]
         )
 
-        response = email_client.send(message)
-        assert response is not None
+        async with email_client:
+            response = await email_client.send(message)
+            assert response is not None
 
     # TODO: Comment back in once the x-ms-request-id change is merged in
-    # @email_decorator
-    # @recorded_by_proxy
-    # def test_check_message_status(self):
+    # @email_decorator_async
+    # @recorded_by_proxy_async
+    # async def test_check_message_status(self):
     #     email_client = EmailClient(self.communication_connection_string)
 
     #     message = EmailMessage(
@@ -83,10 +89,11 @@ class TestEmailClient(AzureRecordedTestCase):
     #         )
     #     )
 
-    #     response = email_client.send(message)
-    #     message_id = response.message_id
-    #     if message_id is not None:
-    #         message_status_response = email_client.get_send_status(message_id)
-    #         assert message_status_response.status is not None
-    #     else:
-    #         assert False
+    #     async with email_client:
+    #         response = await email_client.send(message)
+    #         message_id = response.message_id
+    #         if message_id is not None:
+    #             message_status_response = await email_client.get_send_status(message_id)
+    #             assert message_status_response.status is not None
+    #         else:
+    #             assert False
