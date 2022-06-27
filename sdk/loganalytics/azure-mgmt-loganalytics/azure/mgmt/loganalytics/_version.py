a94794761681c8ce8680e7dea4a2a307f46d0058
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2022-06-27 09:28:58 +0800

    Update _version.py

--- sdk/loganalytics/azure-mgmt-loganalytics/azure/mgmt/loganalytics/_version.py
+++ sdk/loganalytics/azure-mgmt-loganalytics/azure/mgmt/loganalytics/_version.py
@@ -6,4 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "0.0.0"
+VERSION = "13.0.0"
