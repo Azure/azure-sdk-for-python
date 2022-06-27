f40940589216714074979b8570b0b99cda67b98c
Lyndon Shi <lyshi@microsoft.com>
2022-06-27 16:40:53 -0700

    write to ledger

--- sdk/confidentialledger/azure-confidentialledger/samples/write_to_ledger_async.py
+++ sdk/confidentialledger/azure-confidentialledger/samples/write_to_ledger_async.py
@@ -1,29 +1,19 @@
-# coding=utf-8
-# --------------------------------------------------------------------------
-#
+# -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-#
-# The MIT License (MIT)
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the ""Software""), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-#
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
 # --------------------------------------------------------------------------
+
+"""
+FILE: write_to_ledger_async.py
+DESCRIPTION:
+    This sample demonstrates how to write to a Confidential Ledger. In this sample, we write some
+    ledger entries and perform common retrieval operations.
+USAGE:
+    python write_to_ledger_async.py
+    Set the environment variables with your own values before running the sample:
+    1) CONFIDENTIALLEDGER_ENDPOINT - the endpoint of the Confidential Ledger.
+"""
 
 import asyncio
 import logging
@@ -39,7 +29,7 @@ from azure.core.exceptions import HttpResponseError
 from azure.identity.aio import DefaultAzureCredential
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
 LOG = logging.getLogger()
 
 
@@ -66,6 +56,7 @@ async def main():
             ledger_id
         )
 
+
     # The Confidential Ledger's TLS certificate must be written to a file to be used by the
     # ConfidentialLedgerClient. Here, we write it to a temporary file so that is is cleaned up
     # automatically when the program exits.
@@ -91,21 +82,26 @@ async def main():
                     )
                     transaction_id = post_entry_result["transactionId"]
                     print(
-                        f"Successfully sent a ledger entry with transaction id {transaction_id}"
+                        f"Successfully sent a ledger entry to be written. It will become durable "
+                        f"at transaction id {transaction_id}"
                     )
                 except HttpResponseError as e:
                     print("Request failed: {}".format(e.response.json()))
                     raise
 
-                # For some scenarios, users may want to eventually ensure the written entry is durably
-                # committed.
+                # For some scenarios, users may want to eventually ensure the written entry is
+                # durably committed.
                 try:
-                    wait_poller = await ledger_client.begin_wait_for_commit(
-                        transaction_id
+                    print(
+                        f"Waiting for {transaction_id} to become durable. This may be skipped for "
+                        "when writing less important entries where client throughput is "
+                        "prioritized."
                     )
+                    wait_poller = await ledger_client.begin_wait_for_commit(transaction_id)
                     await wait_poller.wait()
                     print(
-                        f"Ledger entry at transaction id {transaction_id} has been committed successfully"
+                        f"Ledger entry at transaction id {transaction_id} has been committed "
+                        "successfully"
                     )
                 except HttpResponseError as e:
                     print("Request failed: {}".format(e.response.json()))
@@ -113,19 +109,20 @@ async def main():
 
                 # Get the latest ledger entry.
                 try:
-                    current_ledger_entry = (
-                        await ledger_client.get_current_ledger_entry()
-                    )
-                    print(
-                        f'The current ledger entry is {current_ledger_entry["contents"]}'
-                    )
+                    current_ledger_entry = await ledger_client.get_current_ledger_entry()
+                    current_ledger_entry = current_ledger_entry["contents"]
+                    print(f"The current ledger entry is {current_ledger_entry}")
                 except HttpResponseError as e:
                     print("Request failed: {}".format(e.response.json()))
                     raise
 
-                # Users may wait for a durable commit when writing a ledger entry though this will reduce
-                # client throughput.
+                # Users may wait for a durable commit when writing a ledger entry though this will
+                # reduce client throughput.
                 try:
+                    print(
+                        f"Writing another entry. This time, we'll have the client method wait for "
+                        "commit."
+                    )
                     post_poller = await ledger_client.begin_post_ledger_entry(
                         {"contents": "Hello world again!"}
                     )
@@ -140,68 +137,21 @@ async def main():
 
                 # Get the latest ledger entry.
                 try:
-                    current_ledger_entry = (
-                        await ledger_client.get_current_ledger_entry()
-                    )
-                    print(
-                        f'The current ledger entry is {current_ledger_entry["contents"]}'
-                    )
+                    current_ledger_entry = await ledger_client.get_current_ledger_entry()
+                    current_ledger_entry = current_ledger_entry["contents"]
+                    print(f"The current ledger entry is {current_ledger_entry}")
                 except HttpResponseError as e:
                     print("Request failed: {}".format(e.response.json()))
                     raise
 
-                # Make a query for a prior ledger entry. The service may take some time to load the result, so a
-                # poller is provided.
+                # Make a query for a prior ledger entry. The service may take some time to load the result,
+                # so a poller is provided.
                 try:
-                    get_entry_poller = await ledger_client.begin_get_ledger_entry(
-                        transaction_id
-                    )
+                    get_entry_poller = await ledger_client.begin_get_ledger_entry(transaction_id)
                     get_entry_result = await get_entry_poller.result()
                     print(
-                        f'At transaction id {get_entry_result["transactionId"]}, the ledger entry contains '
-                        f'\'{get_entry_result["contents"]}\''
-                    )
-                except HttpResponseError as e:
-                    print("Request failed: {}".format(e.response.json()))
-                    raise
-
-                # Get a receipt for a ledger entry.
-                # A receipt can be retrieved for any transaction id to provide cryptographic proof of the contents of the transaction.
-                try:
-                    get_receipt_poller = await ledger_client.begin_get_receipt(
-                        transaction_id
-                    )
-                    get_receipt_result = await get_receipt_poller.result()
-                    print(
-                        f'Receipt for transaction id {get_entry_result["transactionId"]}: {get_receipt_result}'
-                    )
-                except HttpResponseError as e:
-                    print("Request failed: {}".format(e.response.json()))
-                    raise
-
-                # Users may specify a collectionId to group different sets of writes.
-                collection_id = "myCollection"
-                try:
-                    post_poller = await ledger_client.begin_post_ledger_entry(
-                        {"contents": "Hello world again!"},
-                        collection_id=collection_id,
-                    )
-                    new_post_result = await post_poller.result()
-                    print(
-                        f"The ledger entry for {collection_id} has been committed successfully at "
-                        f'transaction id {new_post_result["transactionId"]}'
-                    )
-                except HttpResponseError as e:
-                    print("Request failed: {}".format(e.response.json()))
-                    raise
-
-                # Get the latest ledger entry in the collection.
-                try:
-                    current_ledger_entry = await ledger_client.get_current_ledger_entry(
-                        collection_id=collection_id
-                    )
-                    print(
-                        f'The current ledger entry in {collection_id} is {current_ledger_entry["contents"]}'
+                        f'At transaction id {get_entry_result["entry"]["transactionId"]}, the'
+                        f'ledger entry contains \'{get_entry_result["entry"]["contents"]}\''
                     )
                 except HttpResponseError as e:
                     print("Request failed: {}".format(e.response.json()))
