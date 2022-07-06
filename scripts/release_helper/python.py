055c2e72f28eac038cf45d8cfefb69955cf22fb5
BigCat20196 <1095260342@qq.com>
2022-07-06 11:18:25 +0800

    debug

--- scripts/release_helper/python.py
+++ scripts/release_helper/python.py
@@ -69,6 +69,8 @@ class IssueProcessPython(IssueProcess):
         contents = str(self.issue_package.rest_repo.get_contents(readme_path).decoded_content)
         pattern_tag = re.compile(r'tag: package-[\w+-.]+')
         package_tags = pattern_tag.findall(contents)
+        print(f"*** self.target_readme_tag: {self.target_readme_tag}")
+        print(f"*** package_tags: {package_tags}")
         whether_same_tag = self.target_readme_tag in package_tags
         whether_change_readme = not whether_same_tag or self.is_multiapi
         return whether_change_readme
