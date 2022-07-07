9f6bde503ddb2e8ba3c69f386f1fbbcac723fe79
David Pokluda <46501915+dpokluda@users.noreply.github.com>
2022-07-06 21:08:55 -0700

    Device Update GA preview for Python (#24752)
    * Device Update GA preview for Python
    
    * Update setup.py
    
    * Update shared_requirements.txt
    
    * Update sdk/deviceupdate/azure-iot-deviceupdate/CHANGELOG.md
    
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>
    
    * Update CHANGELOG.md
    
    * Updating tests
    
    * Fixing unit tests
    
    * Update CHANGELOG.md
    
    * Addressing SDK review feedback
    
    * Updated to use autorest/python@6.0.0
    
    * fix ci
    
    * fix ci
    
    * Update CHANGELOG.md
    
    * fix ci
    
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>

--- sdk/deviceupdate/azure-iot-deviceupdate/azure/iot/deviceupdate/aio/_client.py
+++ sdk/deviceupdate/azure-iot-deviceupdate/azure/iot/deviceupdate/aio/_client.py
@@ -7,12 +7,12 @@
 # --------------------------------------------------------------------------
 
 from copy import deepcopy
-from typing import Any, Awaitable, Optional, TYPE_CHECKING
+from typing import Any, Awaitable, TYPE_CHECKING
 
 from azure.core import AsyncPipelineClient
 from azure.core.rest import AsyncHttpResponse, HttpRequest
-from msrest import Deserializer, Serializer
 
+from .._serialization import Deserializer, Serializer
 from ._configuration import DeviceUpdateClientConfiguration
 from .operations import DeviceManagementOperations, DeviceUpdateOperations
 
@@ -22,49 +22,48 @@ if TYPE_CHECKING:
 
     from azure.core.credentials_async import AsyncTokenCredential
 
-class DeviceUpdateClient:
-    """Device Update for IoT Hub is an Azure service that enables customers to publish update for their IoT devices to the cloud, and then deploy that update to their devices (approve updates to groups of devices managed and provisioned in IoT Hub). It leverages the proven security and reliability of the Windows Update platform, optimized for IoT devices. It works globally and knows when and how to update devices, enabling customers to focus on their business goals and let Device Update for IoT Hub handle the updates.
+
+class DeviceUpdateClient:  # pylint: disable=client-accepts-api-version-keyword
+    """Device Update for IoT Hub is an Azure service that enables customers to publish update for
+    their IoT devices to the cloud, and then deploy that update to their devices (approve updates
+    to groups of devices managed and provisioned in IoT Hub). It leverages the proven security and
+    reliability of the Windows Update platform, optimized for IoT devices. It works globally and
+    knows when and how to update devices, enabling customers to focus on their business goals and
+    let Device Update for IoT Hub handle the updates.
 
     :ivar device_update: DeviceUpdateOperations operations
     :vartype device_update: azure.iot.deviceupdate.aio.operations.DeviceUpdateOperations
     :ivar device_management: DeviceManagementOperations operations
     :vartype device_management: azure.iot.deviceupdate.aio.operations.DeviceManagementOperations
-    :param endpoint: Account endpoint.
+    :param endpoint: Account endpoint. Required.
     :type endpoint: str
-    :param instance_id: Account instance identifier.
+    :param instance_id: Account instance identifier. Required.
     :type instance_id: str
-    :param credential: Credential needed for the client to connect to Azure.
+    :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
-    :keyword api_version: Api Version. The default value is "2021-06-01-preview". Note that
-     overriding this default value may result in unsupported behavior.
+    :keyword api_version: Api Version. Default value is "2022-07-01-preview". Note that overriding
+     this default value may result in unsupported behavior.
     :paramtype api_version: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
      Retry-After header is present.
     """
 
-    def __init__(
-        self,
-        endpoint: str,
-        instance_id: str,
-        credential: "AsyncTokenCredential",
-        **kwargs: Any
-    ) -> None:
-        _endpoint = 'https://{endpoint}'
-        self._config = DeviceUpdateClientConfiguration(endpoint=endpoint, instance_id=instance_id, credential=credential, **kwargs)
+    def __init__(self, endpoint: str, instance_id: str, credential: "AsyncTokenCredential", **kwargs: Any) -> None:
+        _endpoint = "https://{endpoint}"
+        self._config = DeviceUpdateClientConfiguration(
+            endpoint=endpoint, instance_id=instance_id, credential=credential, **kwargs
+        )
         self._client = AsyncPipelineClient(base_url=_endpoint, config=self._config, **kwargs)
 
         self._serialize = Serializer()
         self._deserialize = Deserializer()
         self._serialize.client_side_validation = False
         self.device_update = DeviceUpdateOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.device_management = DeviceManagementOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.device_management = DeviceManagementOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
 
-
-    def send_request(
-        self,
-        request: HttpRequest,
-        **kwargs: Any
-    ) -> Awaitable[AsyncHttpResponse]:
+    def send_request(self, request: HttpRequest, **kwargs: Any) -> Awaitable[AsyncHttpResponse]:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -73,7 +72,7 @@ class DeviceUpdateClient:
         >>> response = await client.send_request(request)
         <AsyncHttpResponse: 200 OK>
 
-        For more information on this code flow, see https://aka.ms/azsdk/python/protocol/quickstart
+        For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
 
         :param request: The network request you want to make. Required.
         :type request: ~azure.core.rest.HttpRequest
@@ -84,7 +83,7 @@ class DeviceUpdateClient:
 
         request_copy = deepcopy(request)
         path_format_arguments = {
-            "endpoint": self._serialize.url("self._config.endpoint", self._config.endpoint, 'str', skip_quote=True),
+            "endpoint": self._serialize.url("self._config.endpoint", self._config.endpoint, "str", skip_quote=True),
         }
 
         request_copy.url = self._client.format_url(request_copy.url, **path_format_arguments)
