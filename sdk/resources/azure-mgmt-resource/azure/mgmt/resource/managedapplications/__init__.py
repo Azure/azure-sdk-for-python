ee9d610b6a858bd09f3a2cc25ed998ca95542dc3
Zhenbiao Wei (WICRESOFT NORTH AMERICA LTD) <v-zhenbwei@microsoft.com>
2022-07-07 17:31:33 +0800

    lost _serialization,py rerun

--- sdk/resources/azure-mgmt-resource/azure/mgmt/resource/managedapplications/__init__.py
+++ sdk/resources/azure-mgmt-resource/azure/mgmt/resource/managedapplications/__init__.py
@@ -7,18 +7,10 @@
 # --------------------------------------------------------------------------
 
 from ._application_client import ApplicationClient
-from ._version import VERSION
-
-__version__ = VERSION
+__all__ = ['ApplicationClient']
 
 try:
-    from ._patch import __all__ as _patch_all
-    from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+    from ._patch import patch_sdk  # type: ignore
+    patch_sdk()
 except ImportError:
-    _patch_all = []
-from ._patch import patch_sdk as _patch_sdk
-
-__all__ = ["ApplicationClient"]
-__all__.extend([p for p in _patch_all if p not in __all__])
-
-_patch_sdk()
+    pass
