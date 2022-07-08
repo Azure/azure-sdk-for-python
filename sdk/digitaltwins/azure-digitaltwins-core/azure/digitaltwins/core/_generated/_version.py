79024449c6861193ba2de2a63bc0dd7118d943c3
jeffyou543 <104814503+jeffyou543@users.noreply.github.com>
2022-07-08 13:03:54 -0500

    Release/digitaltwins/1.2.0 (#24603)
    * azure-digitaltwins-core prepare new release targeting api-version=2021-06-30-preview (#23646)
    
    * initial changes
    
    * retargeted correct swagger and fix generation issue
    
    * regenerate sdk and re-record with latest autorest
    
    * remove bad test changes and tune tests to expect wus2
    
    * attempt to fix new requirements
    
    * fix analyze errors and update changelog
    
    * remove unneeded sections from changelog
    
    * Increment package version after release of azure-digitaltwins-core (#23770)
    
    * Adding changes to support api-version 2022-05-31 for Digital Twins Data Plane SDK
    
    Adding changes to support api-version 2022-05-31 for Digital Twins Data Plane SDK
    
    Adding changes to support api-version 2022-05-31 for Digital Twins Data Plane SDK
    
    Adding changes to support api-version 2022-05-31 for Digital Twins Data Plane SDK
    
    Adding changes to support api-version 2022-05-31 for Digital Twins Data Plane SDK
    
    Fixing prefix name
    
    Correcting recordings
    
    Updated Changelog with short description under 1.2.0
    
    * Adding westus2 location for testing azuredigitaltwins
    
    Co-authored-by: Nate Lehman <njlehman1@gmail.com>
    Co-authored-by: Azure SDK Bot <53356347+azure-sdk@users.noreply.github.com>

--- sdk/digitaltwins/azure-digitaltwins-core/azure/digitaltwins/core/_generated/_version.py
+++ sdk/digitaltwins/azure-digitaltwins-core/azure/digitaltwins/core/_generated/_version.py
@@ -6,13 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._azure_digital_twins_api import AzureDigitalTwinsAPI
-from ._version import VERSION
-
-__version__ = VERSION
-__all__ = ['AzureDigitalTwinsAPI']
-
-# `._patch.py` is used for handwritten extensions to the generated code
-# Example: https://github.com/Azure/azure-sdk-for-python/blob/main/doc/dev/customize_code/how-to-patch-sdk-code.md
-from ._patch import patch_sdk
-patch_sdk()
+VERSION = "1.2.0"
