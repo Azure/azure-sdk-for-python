314c32f2c7830b42fe205c8cd5027b150cb4f334
l0lawrence <llawrence@microsoft.com>
2022-07-12 14:18:44 -0700

    testing mgmt calls like _start_producer

---
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/synctests/test_mgmt_pyamqp.py
@@ -0,0 +1,87 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import pytest
+import time
+import threading 
+from azure.identity import EnvironmentCredential, DefaultAzureCredential
+from azure.eventhub._pyamqp import SendClient, authentication
+from azure.eventhub._pyamqp.message import Message
+from azure.eventhub._pyamqp.authentication import AccessToken
+from azure.eventhub._pyamqp import utils as pyamqp_utils
+from azure.eventhub import EventHubProducerClient, EventHubSharedKeyCredential
+from azure.eventhub._client_base import EventHubSASTokenCredential
+from azure.eventhub._constants import (
+    MGMT_PARTITION_OPERATION,
+    MGMT_STATUS_CODE,
+    MGMT_STATUS_DESC,
+    READ_OPERATION
+)
+
+from azure.core.credentials import AzureSasCredential, AzureNamedKeyCredential
+
+@pytest.mark.livetest
+def test_mgmt_call_conn_str(connstr_receivers):
+    connection_str, receivers = connstr_receivers
+    client = EventHubProducerClient.from_connection_string(connection_str)
+    client._start_producer("0",60)
+
+@pytest.mark.livetest
+def test_mgmt_call_default_azure_credential(live_eventhub):
+    credential = DefaultAzureCredential()
+    client = EventHubProducerClient(fully_qualified_namespace=live_eventhub['hostname'],
+                                             eventhub_name=live_eventhub['event_hub'],
+                                             credential=credential,
+                                             user_agent='customized information')
+    client._start_producer("0",60)
+
+@pytest.mark.livetest
+def test_mgmt_call_credential(live_eventhub):
+    credential = EnvironmentCredential()
+    client = EventHubProducerClient(fully_qualified_namespace=live_eventhub['hostname'],
+                                             eventhub_name=live_eventhub['event_hub'],
+                                             credential=credential,
+                                             user_agent='customized information')
+    client._start_producer("0",60)
+
+@pytest.mark.livetest
+def test_mgmt_call_sas(live_eventhub):
+    hostname = live_eventhub["hostname"]
+    credential = EventHubSharedKeyCredential(live_eventhub['key_name'], live_eventhub['access_key'])
+    auth_uri = "sb://{}/{}".format(hostname, live_eventhub['event_hub'])
+    token = credential.get_token(auth_uri).token
+    client = EventHubProducerClient(fully_qualified_namespace=hostname,
+                                             eventhub_name=live_eventhub['event_hub'],
+                                             credential=EventHubSASTokenCredential(token, time.time() + 3000))
+    client._start_producer("0",60)
+    assert True
+
+@pytest.mark.livetest
+def test_mgmt_call_sas_credential(live_eventhub):
+    hostname = live_eventhub["hostname"]
+    credential = EventHubSharedKeyCredential(live_eventhub['key_name'], live_eventhub['access_key'])
+    auth_uri = "sb://{}/{}".format(hostname, live_eventhub['event_hub'])
+    token = credential.get_token(auth_uri).token.decode()
+    client = EventHubProducerClient(fully_qualified_namespace=hostname,
+                                             eventhub_name=live_eventhub['event_hub'],
+                                             credential=AzureSasCredential(token))
+    client._start_producer("0",60)
+    assert True
+
+@pytest.mark.livetest
+def test_mgmt_call_azure_named_key_credential(live_eventhub):
+    credential = AzureNamedKeyCredential(live_eventhub['key_name'], live_eventhub['access_key'])
+    client = EventHubProducerClient(fully_qualified_namespace=live_eventhub['hostname'],
+                                             eventhub_name=live_eventhub['event_hub'],
+                                             credential=credential)
+
+    client._start_producer("0",60)
+    assert True
+
+def gen_sas_token(uri, policy, key, abs_expiry):
+    token = pyamqp_utils.generate_sas_token(uri, policy, key, abs_expiry).encode()
+    return AccessToken(token=token, expires_on=abs_expiry)
+    
+
