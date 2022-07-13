7f983c8c6235e5e74676d908d4b462396e316e43
JericHunter <jerichunter@microsoft.com>
2022-07-13 15:19:58 -0700

    Update offer.py

--- sdk/cosmos/azure-cosmos/azure/cosmos/offer.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/offer.py
@@ -25,15 +25,25 @@ from typing import Dict, Any
 class ThroughputProperties(object):
     """Represents the throughput properties in an Azure Cosmos DB SQL API container.
     To read and update throughput properties use the associated methods on the :class:`Container`.
+
+    :param offer_throughput: The provisioned throughput in request units per second as a number.
+
+    :param auto_scale_max_throughput: is the max autoscale throughput, it should have valid throughput
+   values between 1000 and 1000000 inclusive in increments of 1000.
+
+    :param auto_scale_increment_percent: is the  % from the base selected RU it increases at a given time,
+    up to a maximum default value is 0 the increment percent should be greater than or equal to zero.
+
+    Both auto_scale_max_throughput and auto_scale_increment_percent have to be used simultaneously, you cannot
+    set the offer_throughput when setting the auto_scale properties.
     """
 
-    def __init__(self, offer_throughput=None, properties=None, auto_scale_max_throughput=None,
-                 auto_scale_increment_percentage=None):  # pylint: disable=super-init-not-called
-        # type: (int, Dict[str, Any], int, int) -> None
+    def __init__(self, offer_throughput=None, properties=None, **kwargs):  # pylint: disable=super-init-not-called
+        # type: (int, Dict[str, Any], int) -> None
         self.offer_throughput = offer_throughput
         self.properties = properties
-        self.auto_scale_max_throughput = auto_scale_max_throughput
-        self.auto_scale_increment_percentage = auto_scale_increment_percentage
+        self.auto_scale_max_throughput = kwargs.pop('auto_scale_max_throughput', None)
+        self.auto_scale_increment_percent = kwargs.pop('auto_scale_increment_percent', None)
 
 
 
