0134744b8c234a43185d2b1efeaeaf77b07c4d4f
scbedd <45376673+scbedd@users.noreply.github.com>
2022-07-14 17:22:02 -0700

    adding requirements file. fleshing out the python script. time for a test invocation

--- scripts/devops_tasks/generate_ml_sample_update.py
+++ scripts/devops_tasks/generate_ml_sample_update.py
@@ -1,4 +1,7 @@
+# pip install -r eng/ml_sample_tools.txt before invoking
 import argparse
+import glob
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -23,5 +26,24 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    target_glob = os.path.join(os.path.abspath(args.wheel_folder), "*.whl")
+
     print("Input repo {}".format(args.ml_root))
     print("Folder for search {}".format(args.wheel_folder))
+
+    # find all *.whl in the search folder
+    whls = glob.glob(target_glob)
+
+    print(whls)
+
+    
+    # with open(os.path.join(os.path.abspath(args.wheel_folder), 'sdk/setup.sh'), "r", encoding="utf-8") as f:
+    #     lines = f.readlines()    
+    
+    # between <az_ml_sdk_install>
+    # comment each line
+    # and </az_ml_sdk_install>
+
+    # between <az_ml_sdk_test_install>
+    # insert line of form pip install <blob-location>
+    # </az_ml_sdk_test_install>
