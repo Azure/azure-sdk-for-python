dd06cc4599cdbf3e509bb2caba6ab264ce30cc67
scbedd <45376673+scbedd@users.noreply.github.com>
2022-07-15 11:37:39 -0700

    make regexes multiline to enable them. ready for integration testing.

--- scripts/devops_tasks/generate_ml_sample_update.py
+++ scripts/devops_tasks/generate_ml_sample_update.py
@@ -21,17 +21,17 @@ DISABLE_PREVIEW_INSTALL_TEMPLATE = """# <az_ml_sdk_install>
 
 # I am fully aware that regex isn't fully compatible with traditional tagging. That being said, it is not necessary here.
 def replace_preview_install(content):
-    regex = r"^# \<az_ml_sdk_install\>[\s\S]*\<\/az_ml_sdk_install\>$"
-
-    return re.sub(regex, DISABLE_PREVIEW_INSTALL_TEMPLATE, content)
+    regex = r"^# \<az_ml_sdk_install\>[\s\S]*\<\/az_ml_sdk_install\>"
+    return re.sub(regex, DISABLE_PREVIEW_INSTALL_TEMPLATE, content, flags=re.MULTILINE)
 
 
 def replace_test_install_command(content, targeted_urls):
-    regex = r"^# \<az_ml_sdk_test_install\>[\s\S]*\<\/az_ml_sdk_test_install\>$"
+    regex = r"^# \<az_ml_sdk_test_install\>[\s\S]*\<\/az_ml_sdk_test_install\>"
 
-    install_lines = "\n".join(["pip install {}".format(url) for url in targeted_urls])
+    install_commands = ["pip install {}".format(url) for url in targeted_urls]
+    install_lines = "\n".join(install_commands)
 
-    return re.sub(regex, TEST_INSTALL_TEMPLATE.format(install_command=install_lines), content)
+    return re.sub(regex, TEST_INSTALL_TEMPLATE.format(install_command=install_lines), content, flags=re.MULTILINE)
 
 
 if __name__ == "__main__":
@@ -62,35 +62,36 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    target_glob = os.path.join(os.path.abspath(args.wheel_folder), "*.whl")
-
     print("Input repo {}".format(args.ml_root))
     print("Folder for search {}".format(args.wheel_folder))
 
+    target_glob = os.path.join(os.path.abspath(args.wheel_folder), "*.whl")
+    sdk_setup_sh = os.path.join(os.path.abspath(args.ml_root), "sdk", "setup.sh")
     blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
     container_client = blob_service_client.get_container_client(DEFAULT_CONTAINER)
+    to_be_installed = []
 
     whls = glob.glob(target_glob)
-    to_be_installed = []
 
     for whl in whls:
         if os.path.isfile(whl):
-            whl_file_name = os.path.basename(whl)
-            upload_path = UPLOAD_PATTERN.format(build_id=args.build_id, filename=whl_file_name)
+            upload_path = UPLOAD_PATTERN.format(build_id=args.build_id, filename=os.path.basename(whl))
 
             blob_client = container_client.get_blob_client(blob=upload_path)
 
             with open(whl, "rb") as data:
                 result = blob_client.upload_blob(data=data, overwrite=True)
-                to_be_installed = blob_client.primary_endpoint
+                to_be_installed.append(blob_client.primary_endpoint)
         else:
             print("Operated on non-whl file or folder {}".format(whl))
             exit(1)
 
-    with open(os.path.join(os.path.abspath(args.ml_root), "sdk", "setup.sh"), "r", encoding="utf-8") as f:
-        lines = f.read()
+    with open(sdk_setup_sh, "r", encoding="utf-8") as f:
+        original_content = f.read()
 
-    lines = replace_preview_install(lines)
-    lines = replace_test_install_command(lines, to_be_installed)
+    content = replace_preview_install(original_content)
+    content = replace_test_install_command(content, to_be_installed)
 
-    print(lines)
+    if content != original_content:
+        with open(sdk_setup_sh, "w", encoding="utf-8") as f:
+            f.write(content)
