623d4bfa4aa4b0a53081ba838ba550792b997627
Kashif Khan <kashifkhan@microsoft.com>
2022-07-18 09:18:05 -0500

    stuff

--- sdk/eventhub/azure-eventhub/samples/sync_samples/send.py
+++ sdk/eventhub/azure-eventhub/samples/sync_samples/send.py
@@ -9,19 +9,23 @@
 Examples to show sending events with different options to an Event Hub partition.
 """
 
+import logging
+import sys
 import time
 import os
+from tkinter.tix import Tree
 from azure.eventhub import EventHubProducerClient, EventData
 from azure.eventhub.exceptions import EventHubError
+from azure.identity import DefaultAzureCredential
 
-CONNECTION_STR = os.environ['EVENT_HUB_CONN_STR']
-EVENTHUB_NAME = os.environ['EVENT_HUB_NAME']
-
+# CONNECTION_STR = os.environ['EVENT_HUB_CONN_STR']
+# EVENTHUB_NAME = os.environ['EVENT_HUB_NAME']
 
 def send_event_data_batch(producer):
     # Without specifying partition_id or partition_key
     # the events will be distributed to available partitions via round-robin.
     event_data_batch = producer.create_batch()
+    producer.close()
     event_data_batch.add(EventData('Single message'))
     producer.send_batch(event_data_batch)
 
@@ -82,18 +86,24 @@ def send_event_data_list(producer):
         print("Sending error: ", eh_err)
 
 
-producer = EventHubProducerClient.from_connection_string(
-    conn_str=CONNECTION_STR,
-    eventhub_name=EVENTHUB_NAME
-)
+logger = logging.getLogger('azure.eventhub')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+uamqp_logger = logging.getLogger('pyamqp')
+uamqp_logger.setLevel(logging.DEBUG)
+uamqp_logger.addHandler(handler)
+
+producer = EventHubProducerClient('eh-kashifk.servicebus.windows.net', 'eh-kashifk-hub', credential=DefaultAzureCredential(logging_enable = True), logging_enable=True)
+
 
 start_time = time.time()
 with producer:
     send_event_data_batch(producer)
-    send_event_data_batch_with_limited_size(producer)
-    send_event_data_batch_with_partition_key(producer)
-    send_event_data_batch_with_partition_id(producer)
-    send_event_data_batch_with_properties(producer)
-    send_event_data_list(producer)
+    # send_event_data_batch_with_limited_size(producer)
+    # send_event_data_batch_with_partition_key(producer)
+    # send_event_data_batch_with_partition_id(producer)
+    # send_event_data_batch_with_properties(producer)
+    # send_event_data_list(producer)
 
 print("Send messages in {} seconds.".format(time.time() - start_time))
