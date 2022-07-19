109e02b1d62d5ec7e84def7263c4a22e3495e5b0
PythonSdkPipelines <PythonSdkPipelines>
2022-07-19 09:51:05 +0000

    code and test

--- sdk/servicebus/azure-mgmt-servicebus/generate_sample/foo_example/demo1.py
+++ sdk/servicebus/azure-mgmt-servicebus/generate_sample/foo_example/demo1.py
@@ -4,20 +4,16 @@ import json
 import re
 
 
-
-
-
 def main():
-    a=1
-    c,d=3,4
+    a = 1
+    c, d = 3, 4
     print("aaaaaaaaaaaaaaaaaaaaaaa")
     print("aaaaaaaaaaaaaaaaaaaaaaa")
     print("aaaaaaaaaaaaaaaaaaaaaaa")
     print("aaaaaaaaaaaaaaaaaaaaaaa")
 
+    print("hhhhhhhhhhhhhhh")
 
-
-    print('hhhhhhhhhhhhhhh')
 
 if __name__ == "__main__":
     main()
--- sdk/servicebus/azure-mgmt-servicebus/generate_sample/foo_example/demo2/demo2.py
+++ sdk/servicebus/azure-mgmt-servicebus/generate_sample/foo_example/demo2/demo2.py
@@ -4,20 +4,16 @@ import json
 import re
 
 
-
-
-
 def main():
-    a=2
-    c,d=2,2
+    a = 2
+    c, d = 2, 2
     print("aaaaaaaaaaaaaaaaaaaaaaa")
     print("aaaaaaaaaaaaaaaaaaaaaaa")
     print("aaaaaaaaaaaaaaaaaaaaaaa")
     print("aaaaaaaaaaaaaaaaaaaaaaa")
 
+    print("hhhhhhhhhhhhhhh")
 
-
-    print('hhhhhhhhhhhhhhh')
 
 if __name__ == "__main__":
     main()
