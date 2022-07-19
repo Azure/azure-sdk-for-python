f1350a09ad6f5b2d2601ae52386e1dc5340f77e1
Ron <tikicoder@users.noreply.github.com>
2022-07-19 12:16:49 -0700

    Update azure_cli.py

--- sdk/identity/azure-identity/azure/identity/aio/_credentials/azure_cli.py
+++ sdk/identity/azure-identity/azure/identity/aio/_credentials/azure_cli.py
@@ -32,7 +32,7 @@ class AzureCliCredential(AsyncContextManager):
 
     This requires previously logging in to Azure via "az login", and will use the CLI's currently logged in identity.
     """
-    def __init__(self, tenant_id: str = None):
+    def __init__(self, tenant_id: str = ""):
         AsyncContextManager.__init__(self)
 
         self.tenant_id = tenant_id
@@ -59,7 +59,7 @@ class AzureCliCredential(AsyncContextManager):
 
         resource = _scopes_to_resource(*scopes)
         command = COMMAND_LINE.format(resource)
-        tenant = resolve_tenant(default_tenant= "", tenant_id= self.tenant_id, **kwargs)
+        tenant = resolve_tenant(default_tenant= self.tenant_id, **kwargs)
         
         if tenant:
             command += " --tenant " + tenant
