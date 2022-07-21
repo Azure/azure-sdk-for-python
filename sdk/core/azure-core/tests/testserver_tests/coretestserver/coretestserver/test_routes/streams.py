325c09b5a9f30481a8bf2d6d77c9595da60d83eb
Krista Pratico <krpratic@microsoft.com>
2022-07-21 15:20:18 -0700

    Revert "Added string() back, deleted debugging code"
    This reverts commit fd207f2c1c0197495cbb5dcdff7a074c58cc3287.

--- sdk/core/azure-core/tests/testserver_tests/coretestserver/coretestserver/test_routes/streams.py
+++ sdk/core/azure-core/tests/testserver_tests/coretestserver/coretestserver/test_routes/streams.py
@@ -7,88 +7,75 @@
 import os
 import gzip
 from flask import (
-  Response,
-  Blueprint,
+    Response,
+    Blueprint,
 )
 
 streams_api = Blueprint('streams_api', __name__)
 
-
 class StreamingBody:
-  def __iter__(self):
+    def __iter__(self):
+        yield b"Hello, "
+        yield b"world!"
+
+def streaming_body():
     yield b"Hello, "
     yield b"world!"
 
-
-def streaming_body():
-  yield b"Hello, "
-  yield b"world!"
-
-
 def stream_json_error():
-  yield '{"error": {"code": "BadRequest", '
-  yield ' "message": "You made a bad request"}}'
-
+    yield '{"error": {"code": "BadRequest", '
+    yield' "message": "You made a bad request"}}'
 
 def streaming_test():
-  yield b"test"
-
+    yield b"test"
 
 def stream_compressed_header_error():
-  yield b'test'
-
+    yield b'test'
 
 def stream_compressed_no_header():
-  with gzip.open('test.tar.gz', 'wb') as f:
-    f.write(b"test")
-
-  with open(os.path.join(os.path.abspath('test.tar.gz')), "rb") as fd:
-    yield fd.read()
-
-  os.remove("test.tar.gz")
-
-
-def stream_decompress_header():
-  with gzip.open('test.tar.gz', 'wb') as f:
-    f.write(b"test")
-
-  with open(os.path.join(os.path.abspath('test.tar.gz')), "rb") as fd:
-    yield fd.read()
-
-  os.remove("test.tar.gz")
-
-
+    with gzip.open('test.tar.gz', 'wb') as f:
+        f.write(b"test")
+    
+    with open(os.path.join(os.path.abspath('test.tar.gz')), "rb") as fd:
+        yield fd.read()
+    
+    os.remove("test.tar.gz")
+    
 @streams_api.route('/basic', methods=['GET'])
 def basic():
-  return Response(streaming_body(), status=200)
-
+    return Response(streaming_body(), status=200)
 
 @streams_api.route('/iterable', methods=['GET'])
 def iterable():
-  return Response(StreamingBody(), status=200)
-
+    return Response(StreamingBody(), status=200)
 
 @streams_api.route('/error', methods=['GET'])
 def error():
-  return Response(stream_json_error(), status=400)
-
+    return Response(stream_json_error(), status=400)
 
 @streams_api.route('/string', methods=['GET'])
 def string():
-  return Response(
-    streaming_test(), status=200, mimetype="text/plain"
-  )
+    return Response(
+        streaming_test(), status=200, mimetype="text/plain"
+    )
 
 @streams_api.route('/compressed_no_header', methods=['GET'])
 def compressed_no_header():
-  return Response(stream_compressed_no_header(), status=300)
-
+    return Response(stream_compressed_no_header(), status=300)
 
 @streams_api.route('/compressed', methods=['GET'])
 def compressed():
-  return Response(stream_compressed_header_error(), status=300, headers={"Content-Encoding": "gzip"})
+    return Response(stream_compressed_header_error(), status=300, headers={"Content-Encoding": "gzip"})
 
+def stream_decompress_header():
+    with gzip.open('test.tar.gz', 'wb') as f:
+        f.write(b"test")
 
+    with open(os.path.join(os.path.abspath('test.tar.gz')), "rb") as fd:
+        yield fd.read()
+
+    os.remove("test.tar.gz")
+    
 @streams_api.route('/decompress_header', methods=['GET'])
 def decompress_header():
-  return Response(stream_decompress_header(), status=200, headers={"Content-Encoding": "gzip"})
+    return Response(stream_decompress_header(), status=200, headers={"Content-Encoding": "gzip"})
