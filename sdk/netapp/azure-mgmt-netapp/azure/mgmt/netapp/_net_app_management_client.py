cfa7086fa06167826590397f6ac64bd4d185af95
PythonSdkPipelines <PythonSdkPipelines>
2022-07-21 01:11:43 +0000

    code and test

--- sdk/netapp/azure-mgmt-netapp/azure/mgmt/netapp/_net_app_management_client.py
+++ sdk/netapp/azure-mgmt-netapp/azure/mgmt/netapp/_net_app_management_client.py
@@ -63,7 +63,7 @@ class NetAppManagementClient:    # pylint: disable=too-many-instance-attributes
     :type subscription_id: str
     :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
-    :keyword api_version: Api Version. Default value is "2022-01-01". Note that overriding this
+    :keyword api_version: Api Version. Default value is "2022-03-01". Note that overriding this
      default value may result in unsupported behavior.
     :paramtype api_version: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
@@ -84,21 +84,51 @@ class NetAppManagementClient:    # pylint: disable=too-many-instance-attributes
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
-        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
-        self.net_app_resource = NetAppResourceOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.net_app_resource_quota_limits = NetAppResourceQuotaLimitsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.accounts = AccountsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.pools = PoolsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.volumes = VolumesOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.snapshots = SnapshotsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.snapshot_policies = SnapshotPoliciesOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.backups = BackupsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.account_backups = AccountBackupsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.backup_policies = BackupPoliciesOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.volume_quota_rules = VolumeQuotaRulesOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.vaults = VaultsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.volume_groups = VolumeGroupsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.subvolumes = SubvolumesOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.operations = Operations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.net_app_resource = NetAppResourceOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.net_app_resource_quota_limits = NetAppResourceQuotaLimitsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.accounts = AccountsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.pools = PoolsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.volumes = VolumesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.snapshots = SnapshotsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.snapshot_policies = SnapshotPoliciesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.backups = BackupsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.account_backups = AccountBackupsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.backup_policies = BackupPoliciesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.volume_quota_rules = VolumeQuotaRulesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.vaults = VaultsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.volume_groups = VolumeGroupsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.subvolumes = SubvolumesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
 
 
     def _send_request(
