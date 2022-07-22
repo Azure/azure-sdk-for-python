fe9a681f137e405a5770c478949419395df0dba0
Nivedit Jain <jain.22@iitj.ac.in>
2022-07-22 12:03:33 +0530

    removed os.environ from tests

--- sdk/loadtestservice/azure-developer-loadtesting/tests/test_smoke_async.py
+++ sdk/loadtestservice/azure-developer-loadtesting/tests/test_smoke_async.py
@@ -6,16 +6,13 @@
 # -------------------------------------------------------------------------
 from testcase import LoadtestingPowerShellPreparer
 from testcase_async import LoadtestingAsyncTest
-import os
-import pytest
 
-endpoint = os.environ["LOADTESTING_ENDPOINT"]
 TEST_ID = "a011890b-0201-004d-010d"  # ID to be assigned to a test
 FILE_ID = "a012b234-1230-ab00-0040"  # ID to be assigned to file uploaded
 TEST_RUN_ID = "08673e89-3285-46a1-9c6b"  # ID to be assigned to a test run
 APP_COMPONENT = "01730263-6671-4216-b283"  # ID of the APP Component
 DISPLAY_NAME = "new_namespace-new-namespace"  # display name
-SUBSCRIPTION_ID = os.environ["SUBSCRIPTION_ID"]
+SUBSCRIPTION_ID = "fake-subs-id"
 
 
 class LoadtestingSmokeAsyncTest(LoadtestingAsyncTest):
@@ -24,24 +21,10 @@ class LoadtestingSmokeAsyncTest(LoadtestingAsyncTest):
     @LoadtestingPowerShellPreparer()
     async def test_smoke_async(self, loadtesting_endpoint):
         client = self.create_client(endpoint=loadtesting_endpoint)
-        # result = await client.load_test_administration.create_or_update_test(
-        #     TEST_ID,
-        #     {
-        #         "resourceId": f"/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/yashika-rg/providers/Microsoft.LoadTestService/loadtests/loadtestsdk",
-        #         "description": "",
-        #         "displayName": DISPLAY_NAME,
-        #         "loadTestConfig": {
-        #             "engineSize": "m",
-        #             "engineInstances": 1,
-        #             "splitAllCSVs": False,
-        #         },
-        #         "secrets": {},
-        #         "environmentVariables": {},
-        #         "passFailCriteria": {"passFailMetrics": {}},
-        #         "keyvaultReferenceIdentityType": "SystemAssigned",
-        #         "keyvaultReferenceIdentityId": None,
-        #     }
-        # )
-        # test your code here, for example:
-        # result = await client.xxx.xx(...)
-        # assert result is not None
+        # result = await client.load_test_administration.create_or_update_test( TEST_ID, { "resourceId":
+        # f"/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/yashika-rg/providers/Microsoft.LoadTestService/loadtests
+        # /loadtestsdk", "description": "", "displayName": DISPLAY_NAME, "loadTestConfig": { "engineSize": "m",
+        # "engineInstances": 1, "splitAllCSVs": False, }, "secrets": {}, "environmentVariables": {},
+        # "passFailCriteria": {"passFailMetrics": {}}, "keyvaultReferenceIdentityType": "SystemAssigned",
+        # "keyvaultReferenceIdentityId": None, } ) test your code here, for example: result = await client.xxx.xx(
+        # ...) assert result is not None
