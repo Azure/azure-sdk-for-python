8d150d2a3b3f9ebe9275c99b63065ff32f08582e
JericHunter <jerichunter@microsoft.com>
2022-07-26 14:43:14 -0700

    Update offer.py

--- sdk/cosmos/azure-cosmos/azure/cosmos/offer.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/offer.py
@@ -24,26 +24,23 @@ from typing import Dict, Any
 
 class ThroughputProperties(object):
     """Represents the throughput properties in an Azure Cosmos DB SQL API container.
-    To read and update throughput properties use the associated methods on the :class:`Container`.
 
-    :param offer_throughput: The provisioned throughput in request units per second as a number.
+    To read and update throughput properties, use the associated methods on the :class:`Container`.
+    If configuring auto-scale, both `auto_scale_max_throughput` and `auto_scale_increment_percent` must be set and
+    the value of `offer_throughput` will be ignored.
 
-    :param auto_scale_max_throughput: is the max autoscale throughput, it should have valid throughput
-   values between 1000 and 1000000 inclusive in increments of 1000.
-
-    :param auto_scale_increment_percent: is the  % from the base selected RU it increases at a given time,
-    up to a maximum default value is 0 the increment percent should be greater than or equal to zero.
-
-    Both auto_scale_max_throughput and auto_scale_increment_percent have to be used simultaneously, you cannot
-    set the offer_throughput when setting the auto_scale properties.
+    :keyword int offer_throughput: The provisioned throughput in request units per second as a number.
+    :keyword int auto_scale_max_throughput: The max auto-scale throughput. It should have a valid throughput
+     value between 1000 and 1000000 inclusive, in increments of 1000.
+    :keyword int auto_scale_increment_percent: is the % from the base selected RU it increases at a given time,
+     up to a maximum default value of 0. The increment percent should be greater than or equal to zero.
     """
 
-    def __init__(self, offer_throughput=None, properties=None, **kwargs):  # pylint: disable=super-init-not-called
-        # type: (int, Dict[str, Any], int) -> None
-        self.offer_throughput = offer_throughput
-        self.properties = properties
-        self.auto_scale_max_throughput = kwargs.pop('auto_scale_max_throughput', None)
-        self.auto_scale_increment_percent = kwargs.pop('auto_scale_increment_percent', None)
+    def __init__(self, *args, **kwargs): # pylint: disable=super-init-not-called
+        self.offer_throughput = args[0] if args else kwargs.get('offer_througput')
+        self.properties = args[1] if len(args) > 1 else kwargs.get('properties')
+        self.auto_scale_max_throughput = kwargs.get('auto_scale_max_throughput')
+        self.auto_scale_increment_percent = kwargs.get('auto_scale_increment_percent')
 
 
 
