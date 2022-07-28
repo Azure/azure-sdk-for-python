b6220e032cd3cedbb074b565b6e8c3ffc38d8d6b
Wonhyeong Seo <29195190+wonhyeongseo@users.noreply.github.com>
2022-07-28 17:33:22 +0900

    [keyvault] Drop Python 3.6 support in packages
    [keyvault] Drop Python 3.6 support in packages
    
    chore: fix CHANGELOG.md typo

--- sdk/keyvault/azure-keyvault/setup.py
+++ sdk/keyvault/azure-keyvault/setup.py
@@ -29,7 +29,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
