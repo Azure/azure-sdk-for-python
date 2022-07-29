85c676eec32529afd45915027402708549093e0e
Yujin Cho <sysojxj@gmail.com>
2022-07-29 23:48:48 +0900

    Improve the readability of source code
    Change str.format() formatting of the source code to f-string formatting.

--- sdk/keyvault/azure-keyvault-keys/samples/hello_world.py
+++ sdk/keyvault/azure-keyvault-keys/samples/hello_world.py
@@ -48,7 +48,7 @@ key_size = 2048
 key_ops = ["encrypt", "decrypt", "sign", "verify", "wrapKey", "unwrapKey"]
 key_name = "rsaKeyName"
 rsa_key = client.create_rsa_key(key_name, size=key_size, key_operations=key_ops)
-print("RSA Key with name '{0}' created of type '{1}'.".format(rsa_key.name, rsa_key.key_type))
+print(f"RSA Key with name '{rsa_key.name}' created of type '{rsa_key.key_type}'")
 
 # Let's create an Elliptic Curve key with algorithm curve type P-256.
 # if the key already exists in the Key Vault, then a new version of the key is created.
@@ -56,12 +56,12 @@ print("\n.. Create an EC Key")
 key_curve = "P-256"
 key_name = "ECKeyName"
 ec_key = client.create_ec_key(key_name, curve=key_curve)
-print("EC Key with name '{0}' created of type '{1}'.".format(ec_key.name, ec_key.key_type))
+print(f"EC Key with name '{ec_key.name}' created of type '{ec_key.key_type}'")
 
 # Let's get the rsa key details using its name
 print("\n.. Get a Key by its name")
 rsa_key = client.get_key(rsa_key.name)
-print("Key with name '{0}' was found.".format(rsa_key.name))
+print(f"Key with name '{rsa_key.name}' was found.")
 
 # Let's say we want to update the expiration time for the EC key and disable the key to be usable
 # for cryptographic operations. The update method allows the user to modify the metadata (key attributes)
@@ -72,17 +72,15 @@ updated_ec_key = client.update_key_properties(
     ec_key.name, ec_key.properties.version, expires_on=expires, enabled=False
 )
 print(
-    "Key with name '{0}' was updated on date '{1}'".format(updated_ec_key.name, updated_ec_key.properties.updated_on)
+    f"Key with name '{updated_ec_key.name}' was updated on date '{updated_ec_key.properties.updated_on}'"
 )
 print(
-    "Key with name '{0}' was updated to expire on '{1}'".format(
-        updated_ec_key.name, updated_ec_key.properties.expires_on
-    )
+    f"Key with name '{updated_ec_key.name}' was updated to expire on '{updated_ec_key.properties.expires_on}'"
 )
 
 # The RSA key is no longer used, need to delete it from the Key Vault.
 print("\n.. Delete Keys")
 client.begin_delete_key(ec_key.name)
 client.begin_delete_key(rsa_key.name)
-print("Deleted key '{0}'".format(ec_key.name))
-print("Deleted key '{0}'".format(rsa_key.name))
+print(f"Deleted key '{ec_key.name}'")
+print(f"Deleted key '{rsa_key.name}'")
