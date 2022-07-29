946a3fb9e29aa7f7d3727a3855699dfeb8a4228d
Scott Beddall <sbeddall@gmail.com>
2022-07-28 17:09:57 -0700

    resolving issue with Verify-Changelog

--- eng/scripts/get_package_properties.py
+++ eng/scripts/get_package_properties.py
@@ -6,9 +6,9 @@ import re
 
 from ci_tools.parsing import ParsedSetup
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Get package version details from the repo')
-    parser.add_argument('-s', '--search_path', required=True, help='The scope of the search')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get package version details from the repo")
+    parser.add_argument("-s", "--search_path", required=True, help="The scope of the search")
     args = parser.parse_args()
 
     # Use abspath for the os.walk because if setup parsing fails it often changes cwd which throws off the relative walk
@@ -17,7 +17,11 @@ if __name__ == '__main__':
             if "setup.py" in files:
                 try:
                     parsed = ParsedSetup.from_path(root)
-                    print("{0} {1} {2} {3}".format(parsed.name, parsed.version, parsed.is_new_sdk, parsed.setup_filename))
+                    print(
+                        "{0} {1} {2} {3}".format(
+                            parsed.name, parsed.version, parsed.is_new_sdk, os.path.dirname(parsed.setup_filename)
+                        )
+                    )
                 except:
                     # Skip setup.py if the package cannot be parsed
                     pass
