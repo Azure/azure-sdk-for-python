26b0ffb635a62d1d65e98da1c16852973708c043
YalinLi0312 <yall@microsoft.com>
2022-07-29 17:14:14 -0700

    Address comments

--- sdk/containerregistry/azure-containerregistry/tests/conftest.py
+++ sdk/containerregistry/azure-containerregistry/tests/conftest.py
@@ -5,7 +5,6 @@
 # license information.
 # --------------------------------------------------------------------------
 
-import sys
 import os
 
 import pytest
@@ -14,29 +13,12 @@ from devtools_testutils import add_general_regex_sanitizer, add_body_key_sanitiz
 # Fixture
 from testcase import load_registry
 
-# Ignore async tests for Python < 3.5
-collect_ignore_glob = []
-if sys.version_info < (3, 5):
-    collect_ignore_glob.append("*_async.py")
-
 
 def pytest_configure(config):
     config.addinivalue_line("usefixtures", "load_registry")
 
 @pytest.fixture(scope="session", autouse=True)
 def add_sanitizers(test_proxy):
-    # sanitizes table/cosmos account names in URLs
-    add_general_regex_sanitizer(
-        value="fakeendpoint",
-        regex="(?<=\\/\\/)[a-z]+(?=(?:|-secondary)\\.(?:table|blob|queue)\\.(?:cosmos|core)\\."
-              "(?:azure|windows)\\.(?:com|net))",
-    )
-    # sanitizes random UUIDs that are sent in batch request headers and bodies
-    add_general_regex_sanitizer(
-        value="00000000-0000-0000-0000-000000000000",
-        regex="batch[a-z]*_([0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b)",
-        group_for_replace="1",
-    )
     # sanitizes access and refresh tokens that are present in single-string request or response bodies
     # we expect tokens to either be at the end of this body string, or followed by "\u0026" and more content
     add_general_regex_sanitizer(value="access_token", regex="(?<=access_token=).*?(?=(?:\\u0026|$))")
@@ -44,6 +26,7 @@ def add_sanitizers(test_proxy):
     # sanitizes access and refresh tokens in JSON-formatted request or response bodies
     add_body_key_sanitizer(json_path="$..access_token", value="access_token")
     add_body_key_sanitizer(json_path="$..refresh_token", value="refresh_token")
+
     add_uri_regex_sanitizer(regex="sha256%3A(.*)", value="sha256_encoded_stream")
 
     client_id = os.environ.get("CONTAINERREGISTRY_CLIENT_ID", "client-id")
--- sdk/containerregistry/azure-containerregistry/tests/testcase.py
+++ sdk/containerregistry/azure-containerregistry/tests/testcase.py
@@ -3,137 +3,25 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-import copy
-import json
 import logging
 import os
 import pytest
-import six
 import time
 
 from azure.containerregistry import ContainerRegistryClient
 from azure.containerregistry._helpers import _is_tag, OCI_MANIFEST_MEDIA_TYPE
 from azure.containerregistry._generated.models import Annotations, Descriptor, OCIManifest
 
-from azure.core.credentials import AccessToken
 from azure.mgmt.containerregistry import ContainerRegistryManagementClient
 from azure.mgmt.containerregistry.models import ImportImageParameters, ImportSource, ImportMode
 from azure.identity import DefaultAzureCredential, AzureAuthorityHosts, ClientSecretCredential
 
-from devtools_testutils import AzureRecordedTestCase, is_live
-from azure_devtools.scenario_tests import (
-    OAuthRequestResponsesFilter,
-    RecordingProcessor,
-)
+from devtools_testutils import AzureRecordedTestCase, is_live, FakeTokenCredential
 from msrestazure.azure_cloud import AZURE_CHINA_CLOUD, AZURE_US_GOV_CLOUD, AZURE_PUBLIC_CLOUD, AZURE_GERMAN_CLOUD
 
 
 REDACTED = "REDACTED"
 logger = logging.getLogger()
-
-
-class OAuthRequestResponsesFilterACR(RecordingProcessor):
-    """Remove oauth authentication requests and responses from recording."""
-
-    def process_request(self, request):
-        # We want to leave Azure Container Registry challenge auth requests alone
-        import re
-
-        if not re.search("/oauth2(?:/v2.0)?/token", request.uri) or "azurecr.io" in request.uri:
-            return request
-        return None
-
-
-class ManagementRequestReplacer(RecordingProcessor):
-    """Remove oauth authentication requests and responses from recording."""
-
-    # Don't need to save the import image requests
-
-    def process_request(self, request):
-        if "management.azure.com" not in request.uri:
-            return request
-        return None
-
-
-class AcrBodyReplacer(RecordingProcessor):
-    """Replace request body for oauth2 exchanges"""
-
-    def __init__(self):
-        self._401_replacement = 'Bearer realm="https://fake_url.azurecr.io/oauth2/token",service="fake_url.azurecr.io",scope="fake_scope",error="invalid_token"'
-
-    def _scrub_body(self, body):
-        # type: (bytes) -> bytes
-        if isinstance(body, dict):
-            return self._scrub_body_dict(body)
-        if not isinstance(body, six.binary_type):
-            return body
-        s = body.decode("utf-8")
-        if "access_token" not in s and "refresh_token" not in s:
-            return body
-        s = s.split("&")
-        for idx, pair in enumerate(s):
-            [k, v] = pair.split("=")
-            if k == "access_token" or k == "refresh_token":
-                v = REDACTED
-            if k == "service":
-                v = "fake_url.azurecr.io"
-            s[idx] = "=".join([k, v])
-        s = "&".join(s)
-        return s.encode("utf-8")
-
-    def _scrub_body_dict(self, body):
-        new_body = copy.deepcopy(body)
-        for k in ["access_token", "refresh_token"]:
-            if k in new_body.keys():
-                new_body[k] = REDACTED
-        return new_body
-
-    def process_request(self, request):
-        if request.body:
-            request.body = self._scrub_body(request.body)
-
-        return request
-
-    def process_response(self, response):
-        try:
-            headers = response["headers"]
-
-            if "www-authenticate" in headers:
-                headers["www-authenticate"] = (
-                    [self._401_replacement] if isinstance(headers["www-authenticate"], list) else self._401_replacement
-                )
-
-            body = response["body"]
-            try:
-                if body["string"] == b"" or body["string"] == "null":
-                    return response
-
-                refresh = json.loads(body["string"])
-                if "refresh_token" in refresh.keys():
-                    refresh["refresh_token"] = REDACTED
-                if "access_token" in refresh.keys():
-                    refresh["access_token"] = REDACTED
-                body["string"] = json.dumps(refresh)
-            except ValueError:
-                pass
-            except json.decoder.JSONDecodeError:
-                pass
-
-            return response
-        except (KeyError, ValueError):
-            return response
-
-
-class FakeTokenCredential(object):
-    """Protocol for classes able to provide OAuth tokens.
-    :param str scopes: Lets you specify the type of access needed.
-    """
-
-    def __init__(self):
-        self.token = AccessToken("YOU SHALL NOT PASS", 0)
-
-    def get_token(self, *args):
-        return self.token
 
 
 class ContainerRegistryTestClass(AzureRecordedTestCase):
