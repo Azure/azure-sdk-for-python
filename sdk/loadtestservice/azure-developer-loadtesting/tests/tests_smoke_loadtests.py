fc3a6561d66147471a0dc80a29e2f7025d466ad8
Nivedit Jain <jain.22@iitj.ac.in>
2022-08-01 17:50:43 +0530

    client.load_test_administration.delete_load_test

--- sdk/loadtestservice/azure-developer-loadtesting/tests/tests_smoke_loadtests.py
+++ sdk/loadtestservice/azure-developer-loadtesting/tests/tests_smoke_loadtests.py
@@ -17,7 +17,6 @@ class TestOperationsSmokeTest(LoadtestingTest):
 
     @LoadtestingPowerShellPreparer()
     def test_create_or_update_loadtest(self, loadtesting_endpoint):
-
         # positive testing
         client = self.create_client(endpoint=loadtesting_endpoint)
         result = client.load_test_administration.create_or_update_test(
@@ -61,3 +60,37 @@ class TestOperationsSmokeTest(LoadtestingTest):
                 },
             )
 
+    @LoadtestingPowerShellPreparer()
+    def test_delete_loadtest(self, loadtesting_endpoint):
+        # creating a mock test to delete
+        client = self.create_client(endpoint=loadtesting_endpoint)
+        result = client.load_test_administration.create_or_update_test(
+            "to-be-deleted-test-id",
+            {
+                "resourceId": f"/subscriptions/{self.subscription_id}/resourceGroups/yashika-rg/providers/Microsoft.LoadTestService/loadtests/loadtestsdk",
+                "description": "",
+                "displayName": DISPLAY_NAME,
+                "loadTestConfig": {
+                    "engineSize": "m",
+                    "engineInstances": 1,
+                    "splitAllCSVs": False,
+                },
+                "secrets": {},
+                "environmentVariables": {},
+                "passFailCriteria": {"passFailMetrics": {}},
+                "keyvaultReferenceIdentityType": "SystemAssigned",
+                "keyvaultReferenceIdentityId": None,
+            },
+        )
+
+        # positive testing
+        result = client.load_test_administration.delete_load_test(
+            test_id="to-be-deleted-test-id"
+        )
+        assert result is None
+
+        # negative testing
+        with pytest.raises(ResourceNotFoundError):
+            client.load_test_administration.delete_load_test(
+                test_id="non-existing-test-id"
+            )
