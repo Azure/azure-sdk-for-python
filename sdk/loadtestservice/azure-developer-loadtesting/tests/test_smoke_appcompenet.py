771707940983e88386395e9e442051f06cf2eead
Nivedit Jain <jain.22@iitj.ac.in>
2022-08-01 16:21:04 +0530

    complete testing for AppComponent Class

--- sdk/loadtestservice/azure-developer-loadtesting/tests/test_smoke_appcompenet.py
+++ sdk/loadtestservice/azure-developer-loadtesting/tests/test_smoke_appcompenet.py
@@ -8,6 +8,7 @@ import pytest
 
 from testcase import LoadtestingTest, LoadtestingPowerShellPreparer
 from azure.core.exceptions import HttpResponseError
+from azure.core.exceptions import ResourceNotFoundError
 
 DISPLAY_NAME = "TestingResource"
 
@@ -82,3 +83,59 @@ class AppComponentTestingSmokeTest(LoadtestingTest):
                 self.app_component
             )
         assert result is None
+
+
+    @LoadtestingPowerShellPreparer()
+    def test_get_app_components(self, loadtesting_endpoint):
+
+        # creating an app component array to help in testing
+        client = self.create_client(endpoint=loadtesting_endpoint)
+        client.load_test_administration.create_or_update_app_components(
+            self.app_component,
+            {
+                "name": "app_component",
+                "testId": self.test_id,
+                "value": {
+                    f"/subscriptions/{self.subscription_id}/resourceGroups/App-Service-Sample-Demo-rg/providers/Microsoft.Web/sites/App-Service-Sample-Demo": {
+                        "resourceId": f"/subscriptions/{self.subscription_id}/resourceGroups/App-Service-Sample-Demo-rg/providers/Microsoft.Web/sites/App-Service-Sample-Demo",
+                        "resourceName": "App-Service-Sample-Demo",
+                        "resourceType": "Microsoft.Web/sites",
+                        "subscriptionId": self.subscription_id,
+                    },
+                    f"subscriptions/{self.subscription_id}/resourceGroups/Demo-App-Service-Sample-rg/providers/Microsoft.Web/sites/Demo-App-Service-Sample": {
+                        "resourceId": f"/subscriptions/{self.subscription_id}/resourceGroups/Demo-App-Service-Sample-rg/providers/Microsoft.Web/sites/Demo-App-Service-Sample",
+                        "resourceName": "Demo-App-Service-Sample",
+                        "resourceType": "Microsoft.Web/sites",
+                        "subscriptionId": self.subscription_id,
+                    },
+                    f"/subscriptions/{self.subscription_id}/resourceGroups/App-Service-Sample-Demo-rg/providers/Microsoft.Web/sites/App-Service-Sample": {
+                        "resourceId": f"/subscriptions/{self.subscription_id}/resourceGroups/App-Service-Sample-Demo-rg/providers/Microsoft.Web/sites/App-Service-Sample",
+                        "resourceName": "App-Service-Sample-Demo",
+                        "resourceType": "Microsoft.Web/sites",
+                        "subscriptionId": self.subscription_id,
+                    }
+                },
+            },
+        )
+
+        # positive testing
+        result = client.load_test_administration.get_app_components(
+            test_id=self.test_id
+        )
+        assert result is not None
+
+        result = client.load_test_administration.get_app_components(
+            name=self.app_component
+        )
+        assert result is not None
+
+        # negative testing
+        with pytest.raises(ResourceNotFoundError):
+            client.load_test_administration.get_app_components(
+                test_id="abcdefghijklmnopqrstuvwxyz"
+            )
+
+        with pytest.raises(HttpResponseError):
+            client.load_test_administration.get_app_components(
+                name="123"
+            )
