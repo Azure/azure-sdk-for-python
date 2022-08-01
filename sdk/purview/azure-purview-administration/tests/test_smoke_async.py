ed90f6182dea80f080d5fadd3e045347c2de7cc3
BigCat20196 <1095260342@qq.com>
2022-08-01 17:54:19 +0800

    migrate

--- sdk/purview/azure-purview-administration/tests/test_smoke_async.py
+++ sdk/purview/azure-purview-administration/tests/test_smoke_async.py
@@ -7,20 +7,21 @@
 from testcase import PurviewAccountPowerShellPreparer, PurviewMetaPolicyPowerShellPreparer
 from testcase_async import PurviewAccountTestAsync, PurviewMetaPolicyTestAsync
 from _util import PurviewAccountRecordingProcessor, PurviewAccountCollectionsRecordingProcessor
+from devtools_testutils import recorded_by_proxy
 
 
-class PurviewAccountSmokeTestAsync(PurviewAccountTestAsync):
+class TestPurviewAccountSmokeAsync(PurviewAccountTestAsync):
 
     @PurviewAccountPowerShellPreparer()
+    @recorded_by_proxy
     async def test_basic_smoke_test(self, purviewaccount_endpoint):
-        self.recording_processors.append(PurviewAccountRecordingProcessor())
-        client = self.create_async_client(endpoint=purviewaccount_endpoint)
-        response = await client.accounts.get_access_keys()
+        client = self.create_aio_client(endpoint=purviewaccount_endpoint)
+        response = await self.to_list(client.accounts.get_access_keys())
         assert set(response.keys()) == set(['atlasKafkaPrimaryEndpoint', 'atlasKafkaSecondaryEndpoint'])
 
     @PurviewAccountPowerShellPreparer()
+    @recorded_by_proxy
     async def test_collections_list(self, purviewaccount_endpoint):
-        self.recording_processors.append(PurviewAccountCollectionsRecordingProcessor())
         client = self.create_async_client(endpoint=purviewaccount_endpoint)
         response = client.collections.list_collections()
         result = [item async for item in response]
@@ -28,9 +29,10 @@ class PurviewAccountSmokeTestAsync(PurviewAccountTestAsync):
             assert set(item.keys()) == set(['name', 'friendlyName', 'description', 'systemData', 'collectionProvisioningState'])
 
 
-class PurviewMetaDataPolicySmokeTestAsync(PurviewMetaPolicyTestAsync):
+class TestPurviewMetaDataPolicySmokeAsync(PurviewMetaPolicyTestAsync):
 
     @PurviewMetaPolicyPowerShellPreparer()
+    @recorded_by_proxy
     async def test_metadata_policy_smoke_async(self, purviewmetapolicy_endpoint):
         client = self.create_async_client(endpoint=purviewmetapolicy_endpoint)
         response = client.metadata_policy.list_all()
@@ -38,6 +40,7 @@ class PurviewMetaDataPolicySmokeTestAsync(PurviewMetaPolicyTestAsync):
         assert len(result) >= 1
 
     @PurviewMetaPolicyPowerShellPreparer()
+    @recorded_by_proxy
     async def test_metadata_role_smoke_async(self, purviewmetapolicy_endpoint):
         client = self.create_async_client(endpoint=purviewmetapolicy_endpoint)
         response = client.metadata_roles.list()
--- sdk/purview/azure-purview-administration/tests/testcase_async.py
+++ sdk/purview/azure-purview-administration/tests/testcase_async.py
@@ -4,31 +4,25 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from devtools_testutils import AzureTestCase
+from _aio_testcase import AzureRecordedAsyncTestCase
 from azure.purview.administration.account.aio import PurviewAccountClient as AsyncPurviewAccountClient
 from azure.purview.administration.metadatapolicies.aio import PurviewMetadataPoliciesClient as AsyncPurviewMetadataPoliciesClient
 
 
-class PurviewAccountTestAsync(AzureTestCase):
-    def __init__(self, method_name, **kwargs):
-        super(PurviewAccountTestAsync, self).__init__(method_name, **kwargs)
-
+class PurviewAccountTestAsync(AzureRecordedAsyncTestCase):
     def create_async_client(self, endpoint):
         credential = self.get_credential(AsyncPurviewAccountClient, is_async=True)
-        return self.create_client_from_credential(
+        return self.create_aio_client(
             AsyncPurviewAccountClient,
             credential=credential,
             endpoint=endpoint,
         )
 
 
-class PurviewMetaPolicyTestAsync(AzureTestCase):
-    def __init__(self, method_name, **kwargs):
-        super(PurviewMetaPolicyTestAsync, self).__init__(method_name, **kwargs)
-
+class PurviewMetaPolicyTestAsync(AzureRecordedAsyncTestCase):
     def create_async_client(self, endpoint):
         credential = self.get_credential(AsyncPurviewMetadataPoliciesClient, is_async=True)
-        return self.create_client_from_credential(
+        return self.create_aio_client(
             AsyncPurviewMetadataPoliciesClient,
             credential=credential,
             endpoint=endpoint,
