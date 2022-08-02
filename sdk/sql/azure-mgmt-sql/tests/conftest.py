1474ee32e54ee751bda0136d50614bf2a05363f9
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2022-08-02 14:39:06 +0800

    Update conftest.py

--- sdk/sql/azure-mgmt-sql/tests/conftest.py
+++ sdk/sql/azure-mgmt-sql/tests/conftest.py
@@ -50,7 +50,6 @@ def add_sanitizers(test_proxy):
     add_general_regex_sanitizer(regex=tenant_id, value="00000000-0000-0000-0000-000000000000")
     add_general_regex_sanitizer(regex=client_id, value="00000000-0000-0000-0000-000000000000")
     add_general_regex_sanitizer(regex=client_secret, value="00000000-0000-0000-0000-000000000000")
-    add_general_regex_sanitizer(regex=tenant_id, value="00000000-0000-0000-0000-000000000000")
     add_header_regex_sanitizer(key="Set-Cookie", value="[set-cookie;]")
     add_header_regex_sanitizer(key="Cookie", value="cookie;")
     add_body_key_sanitizer(json_path="$..access_token", value="access_token")
