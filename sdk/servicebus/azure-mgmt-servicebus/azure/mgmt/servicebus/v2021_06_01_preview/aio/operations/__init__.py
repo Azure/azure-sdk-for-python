c9e90a4cc2ce28bd84f96093a3d10e913b147419
PythonSdkPipelines <PythonSdkPipelines>
2022-08-05 03:38:04 +0000

    code and test

--- sdk/servicebus/azure-mgmt-servicebus/azure/mgmt/servicebus/v2021_06_01_preview/aio/operations/__init__.py
+++ sdk/servicebus/azure-mgmt-servicebus/azure/mgmt/servicebus/v2021_06_01_preview/aio/operations/__init__.py
@@ -17,6 +17,9 @@ from ._topics_operations import TopicsOperations
 from ._rules_operations import RulesOperations
 from ._subscriptions_operations import SubscriptionsOperations
 
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     'NamespacesOperations',
     'PrivateEndpointConnectionsOperations',
@@ -29,3 +32,5 @@ __all__ = [
     'RulesOperations',
     'SubscriptionsOperations',
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
