babf938803024f938ede5b090465df100b3709b8
BigCat20196 <1095260342@qq.com>
2022-08-08 15:23:24 +0800

    debug

--- scripts/release_sdk_status/util.py
+++ scripts/release_sdk_status/util.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 
 def _find_certificate():
-    print("*** path:", os.getcwd())
-    devcert_path = Path('eng/common/testproxy/dotnet-devcert.crt')
+    devcert_path = Path(os.getenv('SDK_REPO') + '/eng/common/testproxy/dotnet-devcert.crt')
     with open(devcert_path, 'r') as fr:
         return fr.read()
 
