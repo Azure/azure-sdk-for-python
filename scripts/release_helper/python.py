2c0db8003b7b2726af767936800022cd79b06a1d
Yuchao Yan <yuchaoyan@microsoft.com>
2022-08-09 15:46:37 +0800

    [Release helper] update assignee (#25613)
    * update for Go
    
    * check tag consistency and optize reply
    
    * add write.md
    
    * update go js readme
    
    * update
    
    * Update common.py
    
    * Update common.py
    
    * update assignee for JS
    
    * update
    
    * update
    
    * Update common.py
    
    * Update common.py
    
    * Update release_helper.yml for Azure Pipelines
    
    * Update common.py
    
    * update
    
    * Update common.py
    
    * Update common.py
    
    * Update main.py
    
    * update
    
    * update bot advice
    
    * update
    
    * update
    
    * update excel
    
    * update output function
    
    * add exception handle for bad credential
    
    * fix static varaible in class
    
    * update auto_assignee algorithm
    
    * update bot token to have a try
    
    * update bot token
    
    * force to single process
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fix bug
    
    * fix
    
    * update release_helper/js
    
    * update release_helper/js
    
    * fix bug
    
    * update release helper
    
    * Update js.py
    
    * Update js.py
    
    * Update js.py
    
    * urldecode
    
    * format
    
    * add python
    
    * fix
    
    * add package name
    
    * update package name
    
    * update package name
    
    * fix bug
    
    * update get edit_content
    
    * auto-close
    
    * record release
    
    * debug
    
    * debug
    
    * debug
    
    * override run
    
    * init readme link
    
    * fix
    
    * fix
    
    * fix
    
    * update assignee_token and auto-close
    
    * fix
    
    * debug
    
    * fix
    
    * auto-reply
    
    * fix
    
    * debug
    
    * fix
    
    * debug
    
    * debug
    
    * fix
    
    * debugg
    
    * update yaml
    
    * change to add_label
    
    * format
    
    * add attention policy and remind policy
    
    * add duplicated policy
    
    * delete uesless code
    
    * Update scripts/release_helper/utils.py
    
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>
    
    * update
    
    * fix dependence version
    
    * fix dependence version
    
    * fix env
    
    * update js assignee
    
    * update js assignee
    
    * fix find pr number bug
    
    * update assignee logic
    
    * debug
    
    * debug
    
    * update python assignee
    
    * Update python.py
    
    * debug
    
    * debug
    
    * delete useless code
    
    * Update scripts/release_helper/python.py
    
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>
    
    * add multi api policy
    
    * add tag inconsystency
    
    * add specified tag
    
    * Update scripts/release_helper/python.py
    
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>
    
    * update
    
    * update multiapi policy
    
    * update on time
    
    * Update python.py
    
    * add version and is_stable
    
    * update assignee
    
    Co-authored-by: Zed <601306339@qq.com>
    Co-authored-by: Yiming Lei <59104634+RAY-316@users.noreply.github.com>
    Co-authored-by: Jiefeng Chen (WICRESOFT NORTH AMERICA LTD) <v-jiechen@microsoft.com>
    Co-authored-by: Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
    Co-authored-by: BigCat20196 <1095260342@qq.com>

--- scripts/release_helper/python.py
+++ scripts/release_helper/python.py
@@ -9,8 +9,9 @@ from common import IssueProcess, Common, get_origin_link_and_tag, IssuePackage
 from utils import AUTO_CLOSE_LABEL, get_last_released_date, record_release, get_python_release_pipeline, run_pipeline
 
 # assignee dict which will be assigned to handle issues
-_PYTHON_OWNER = {'azure-sdk', 'msyyc'}
-_PYTHON_ASSIGNEE = {'BigCat20196', 'Wzb123456789'}
+_PYTHON_OWNER = {'azure-sdk', 'Wzb123456789', 'BigCat20196'}
+_PYTHON_ASSIGNEE = {'msyyc'}
+
 # labels
 _CONFIGURED = 'Configured'
 _AUTO_ASK_FOR_CHECK = 'auto-ask-check'
