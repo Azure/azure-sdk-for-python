19cde94a800e508a700f2ee0eb705f34d4643b1a
Jacob Lauzon <jalauzon@microsoft.com>
2022-08-09 13:16:27 -0700

    Setup datalake and migrate `test_cpk`

---
+++ sdk/storage/azure-storage-file-datalake/tests/conftest.py
@@ -0,0 +1,29 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import os
+
+import pytest
+
+from devtools_testutils import (
+    add_general_regex_sanitizer,
+    add_header_regex_sanitizer,
+    add_oauth_response_sanitizer,
+    add_uri_string_sanitizer,
+    test_proxy
+)
+
+@pytest.fixture(scope="session", autouse=True)
+def add_sanitizers(test_proxy):
+    subscription_id = os.environ.get("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
+    tenant_id = os.environ.get("STORAGE_TENANT_ID", "00000000-0000-0000-0000-000000000000")
+    add_general_regex_sanitizer(regex=subscription_id, value="00000000-0000-0000-0000-000000000000")
+    add_general_regex_sanitizer(regex=tenant_id, value="00000000-0000-0000-0000-000000000000")
+    add_header_regex_sanitizer(key="Set-Cookie", value="[set-cookie;]")
+    add_header_regex_sanitizer(key="Cookie", value="cookie;")
+    add_oauth_response_sanitizer()
+
+    add_uri_string_sanitizer(target=".preprod.", value=".")
