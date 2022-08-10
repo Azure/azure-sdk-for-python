7094c15d4a64dafade1485402db88f03e61d036d
JericHunter <jerichunter@microsoft.com>
2022-08-10 13:11:39 -0700

    Update offer.py

--- sdk/cosmos/azure-cosmos/azure/cosmos/offer.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/offer.py
@@ -26,14 +26,15 @@ class ThroughputProperties(object):
     """Represents the throughput properties in an Azure Cosmos DB SQL API container.
 
     To read and update throughput properties, use the associated methods on the :class:`Container`.
-    If configuring auto-scale, both `auto_scale_max_throughput` and `auto_scale_increment_percent` must be set and
-    the value of `offer_throughput` will not be allowed to be set in conjunction with auto-scale.
+    If configuring auto-scale, `auto_scale_max_throughput` needs to be set and
+    `auto_scale_increment_percent` can also be set in conjunction with it.
+    The value of `offer_throughput` will not be allowed to be set in conjunction with the auto-scale settings.
 
     :keyword int offer_throughput: The provisioned throughput in request units per second as a number.
     :keyword int auto_scale_max_throughput: The max auto-scale throughput. It should have a valid throughput
      value between 1000 and 1000000 inclusive, in increments of 1000.
     :keyword int auto_scale_increment_percent: is the % from the base selected RU it increases at a given time,
-     up to a maximum default value of 0. The increment percent should be greater than or equal to zero.
+     the increment percent should be greater than or equal to zero.
     """
 
     def __init__(self, *args, **kwargs): # pylint: disable=super-init-not-called
