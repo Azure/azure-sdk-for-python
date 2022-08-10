2214464719e29706738765cf2a8f430ba016834d
JericHunter <jerichunter@microsoft.com>
2022-08-10 09:49:10 -0700

    Update database.py

--- sdk/cosmos/azure-cosmos/azure/cosmos/database.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/database.py
@@ -781,11 +781,16 @@ class DatabaseProxy(object):
             response_hook(self.client_connection.last_response_headers, throughput_properties)
 
         return ThroughputProperties(offer_throughput=throughput_properties[0]["content"]["offerThroughput"],
-                                    properties=throughput_properties[0])
+                                    properties=throughput_properties[0],
+                                    auto_scale_max_throughput=throughput_properties[0]['content']
+                                    ['offerAutopilotSettings']['maxThroughput'],
+                                    auto_scale_increment_percent=throughput_properties[0]['content']
+                                    ['offerAutopilotSettings']['autoUpgradePolicy']['throughputPolicy'][
+                                        'incrementPercent'])
 
     @distributed_trace
-    def replace_throughput(self, throughput, **kwargs):
-        # type: (Optional[int], Any) -> ThroughputProperties
+    def replace_throughput(self, throughput, max_throughput, increment_percent, **kwargs):
+        # type: (Optional[int], Optional[int], Optional[int], Any) -> ThroughputProperties
         """Replace the database-level throughput.
 
         :param throughput: The throughput to be set (an integer).
@@ -816,4 +821,9 @@ class DatabaseProxy(object):
                                                    offer=throughput_properties[0], **kwargs)
         if response_hook:
             response_hook(self.client_connection.last_response_headers, data)
-        return ThroughputProperties(offer_throughput=data["content"]["offerThroughput"], properties=data)
+        return ThroughputProperties(offer_throughput=data["content"]["offerThroughput"], properties=data,
+                                    auto_scale_max_throughput=data['content']['offerAutopilotSettings'][
+                                        'maxThroughput'],
+                                    auto_scale_increment_percent=data['content']['offerAutopilotSettings']
+                                    ['autoUpgradePolicy']['throughputPolicy']['incrementPercent'])
+
