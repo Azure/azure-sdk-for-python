ddd16331e4846e1192f05bb16d961ea9b20baaf6
PythonSdkPipelines <PythonSdkPipelines>
2022-08-10 07:43:41 +0000

    code and test

--- sdk/servicebus/azure-mgmt-servicebus/azure/mgmt/servicebus/v2022_01_01_preview/aio/__init__.py
+++ sdk/servicebus/azure-mgmt-servicebus/azure/mgmt/servicebus/v2022_01_01_preview/aio/__init__.py
@@ -6,4 +6,15 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "8.1.0"
+from ._service_bus_management_client import ServiceBusManagementClient
+
+try:
+    from ._patch import __all__ as _patch_all
+    from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+except ImportError:
+    _patch_all = []
+from ._patch import patch_sdk as _patch_sdk
+__all__ = ['ServiceBusManagementClient']
+__all__.extend([p for p in _patch_all if p not in __all__])
+
+_patch_sdk()
