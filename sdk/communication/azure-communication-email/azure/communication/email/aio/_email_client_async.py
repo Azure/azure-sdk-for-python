afa06adb81d98651910206d9d55e29eebba61751
Yogesh Mohanraj <ymohanraj@microsoft.com>
2022-08-15 09:58:04 -0700

    Adding AAD auth to async client

--- sdk/communication/azure-communication-email/azure/communication/email/aio/_email_client_async.py
+++ sdk/communication/azure-communication-email/azure/communication/email/aio/_email_client_async.py
@@ -9,8 +9,7 @@ from uuid import uuid4
 from azure.core.credentials import AzureKeyCredential
 from azure.core.credentials_async import AsyncTokenCredential
 from azure.core.tracing.decorator_async import distributed_trace_async
-from .._shared.utils import parse_connection_str, get_current_utc_time
-from .._shared.policy import HMACCredentialsPolicy
+from .._shared.utils import parse_connection_str, get_authentication_policy, get_current_utc_time
 from .._generated.aio._azure_communication_email_service import AzureCommunicationEmailService
 from .._version import SDK_MONIKER
 from .._generated.models import SendEmailResult, SendStatusResult, EmailMessage
@@ -34,7 +33,7 @@ class EmailClient(object): # pylint: disable=client-accepts-api-version-keyword
         if endpoint.endswith("/"):
             endpoint = endpoint[:-1]
 
-        authentication_policy = HMACCredentialsPolicy(endpoint, credential)
+        authentication_policy = get_authentication_policy(endpoint, credential, decode_url=True, is_async=True)
 
         self._generated_client = AzureCommunicationEmailService(
             endpoint,
