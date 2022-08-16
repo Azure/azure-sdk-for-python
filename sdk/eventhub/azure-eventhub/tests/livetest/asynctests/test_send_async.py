fc9bff8e4ebf36555e64b5259fe3d31ac1c94f15
l0lawrence <llawrence@microsoft.com>
2022-08-15 18:15:57 -0700

    remove send_event

--- sdk/eventhub/azure-eventhub/tests/livetest/asynctests/test_send_async.py
+++ sdk/eventhub/azure-eventhub/tests/livetest/asynctests/test_send_async.py
@@ -194,36 +194,32 @@ async def test_send_partition_async(connstr_receivers):
         batch = await client.create_batch()
         batch.add(EventData(b"Data"))
         await client.send_batch(batch)
-        await client.send_event(EventData(b"Data"))
 
     async with client:
         batch = await client.create_batch(partition_id="1")
         batch.add(EventData(b"Data"))
         await client.send_batch(batch)
-        await client.send_event(EventData(b"Data"), partition_id="1")
 
     partition_0 = receivers[0].receive_message_batch(timeout=5000)
     partition_1 = receivers[1].receive_message_batch(timeout=5000)
     assert len(partition_1) >= 2
-    assert len(partition_0) + len(partition_1) == 4
+    assert len(partition_0) + len(partition_1) == 2
 
     async with client:
         batch = await client.create_batch()
         batch.add(EventData(b"Data"))
         await client.send_batch(batch)
-        await client.send_event(EventData(b"Data"))
 
     async with client:
         batch = await client.create_batch(partition_id="0")
         batch.add(EventData(b"Data"))
         await client.send_batch(batch)
-        await client.send_event(EventData(b"Data"), partition_id="0")
 
     time.sleep(5)
     partition_0 = receivers[0].receive_message_batch(timeout=5000)
     partition_1 = receivers[1].receive_message_batch(timeout=5000)
     assert len(partition_0) >= 2
-    assert len(partition_0) + len(partition_1) == 4
+    assert len(partition_0) + len(partition_1) == 2
 
 
 @pytest.mark.liveTest
