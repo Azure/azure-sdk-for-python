d41b5fd545351da788e7abd6fa93b5a27994d42e
l0lawrence <llawrence@microsoft.com>
2022-08-16 10:46:55 -0700

    remove args

--- sdk/eventhub/azure-eventhub/stress/scripts/script_commands.sh
+++ sdk/eventhub/azure-eventhub/stress/scripts/script_commands.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python azure_eventhub_producer_stress.py -m stress_send_sync --azure_identity True --hostname 'eh-llawstress.servicebus.windows.net' --eventhub 'eh-llawstress-hub' --duration 7200 &
+python azure_eventhub_producer_stress.py -m stress_send_sync --duration 7200 &
 wait
