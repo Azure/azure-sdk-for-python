71a3f1ef174b7685a7646791f179bb029fc5e6d1
PythonSdkPipelines <PythonSdkPipelines>
2022-08-18 02:23:29 +0000

    code and test

--- sdk/eventgrid/azure-mgmt-eventgrid/azure/mgmt/eventgrid/aio/_event_grid_management_client.py
+++ sdk/eventgrid/azure-mgmt-eventgrid/azure/mgmt/eventgrid/aio/_event_grid_management_client.py
@@ -9,20 +9,42 @@
 from copy import deepcopy
 from typing import Any, Awaitable, TYPE_CHECKING
 
-from msrest import Deserializer, Serializer
-
 from azure.core.rest import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core import AsyncARMPipelineClient
 
 from .. import models
+from .._serialization import Deserializer, Serializer
 from ._configuration import EventGridManagementClientConfiguration
-from .operations import ChannelsOperations, DomainEventSubscriptionsOperations, DomainTopicEventSubscriptionsOperations, DomainTopicsOperations, DomainsOperations, EventSubscriptionsOperations, ExtensionTopicsOperations, Operations, PartnerConfigurationsOperations, PartnerNamespacesOperations, PartnerRegistrationsOperations, PartnerTopicEventSubscriptionsOperations, PartnerTopicsOperations, PrivateEndpointConnectionsOperations, PrivateLinkResourcesOperations, SystemTopicEventSubscriptionsOperations, SystemTopicsOperations, TopicEventSubscriptionsOperations, TopicTypesOperations, TopicsOperations, VerifiedPartnersOperations
+from .operations import (
+    ChannelsOperations,
+    DomainEventSubscriptionsOperations,
+    DomainTopicEventSubscriptionsOperations,
+    DomainTopicsOperations,
+    DomainsOperations,
+    EventSubscriptionsOperations,
+    ExtensionTopicsOperations,
+    Operations,
+    PartnerConfigurationsOperations,
+    PartnerNamespacesOperations,
+    PartnerRegistrationsOperations,
+    PartnerTopicEventSubscriptionsOperations,
+    PartnerTopicsOperations,
+    PrivateEndpointConnectionsOperations,
+    PrivateLinkResourcesOperations,
+    SystemTopicEventSubscriptionsOperations,
+    SystemTopicsOperations,
+    TopicEventSubscriptionsOperations,
+    TopicTypesOperations,
+    TopicsOperations,
+    VerifiedPartnersOperations,
+)
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials_async import AsyncTokenCredential
 
-class EventGridManagementClient:    # pylint: disable=too-many-instance-attributes
+
+class EventGridManagementClient:  # pylint: disable=client-accepts-api-version-keyword,too-many-instance-attributes
     """Azure EventGrid Management Client.
 
     :ivar channels: ChannelsOperations operations
@@ -76,10 +98,10 @@ class EventGridManagementClient:    # pylint: disable=too-many-instance-attribut
     :vartype topic_types: azure.mgmt.eventgrid.aio.operations.TopicTypesOperations
     :ivar verified_partners: VerifiedPartnersOperations operations
     :vartype verified_partners: azure.mgmt.eventgrid.aio.operations.VerifiedPartnersOperations
-    :param credential: Credential needed for the client to connect to Azure.
+    :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: Subscription credentials that uniquely identify a Microsoft Azure
-     subscription. The subscription ID forms part of the URI for every service call.
+     subscription. The subscription ID forms part of the URI for every service call. Required.
     :type subscription_id: str
     :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
@@ -97,7 +119,9 @@ class EventGridManagementClient:    # pylint: disable=too-many-instance-attribut
         base_url: str = "https://management.azure.com",
         **kwargs: Any
     ) -> None:
-        self._config = EventGridManagementClientConfiguration(credential=credential, subscription_id=subscription_id, **kwargs)
+        self._config = EventGridManagementClientConfiguration(
+            credential=credential, subscription_id=subscription_id, **kwargs
+        )
         self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
@@ -107,31 +131,52 @@ class EventGridManagementClient:    # pylint: disable=too-many-instance-attribut
         self.channels = ChannelsOperations(self._client, self._config, self._serialize, self._deserialize)
         self.domains = DomainsOperations(self._client, self._config, self._serialize, self._deserialize)
         self.domain_topics = DomainTopicsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.topic_event_subscriptions = TopicEventSubscriptionsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.domain_event_subscriptions = DomainEventSubscriptionsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.event_subscriptions = EventSubscriptionsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.domain_topic_event_subscriptions = DomainTopicEventSubscriptionsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.system_topic_event_subscriptions = SystemTopicEventSubscriptionsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.partner_topic_event_subscriptions = PartnerTopicEventSubscriptionsOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.topic_event_subscriptions = TopicEventSubscriptionsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.domain_event_subscriptions = DomainEventSubscriptionsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.event_subscriptions = EventSubscriptionsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.domain_topic_event_subscriptions = DomainTopicEventSubscriptionsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.system_topic_event_subscriptions = SystemTopicEventSubscriptionsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.partner_topic_event_subscriptions = PartnerTopicEventSubscriptionsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
         self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
         self.topics = TopicsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.partner_configurations = PartnerConfigurationsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.partner_namespaces = PartnerNamespacesOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.partner_registrations = PartnerRegistrationsOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.partner_configurations = PartnerConfigurationsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.partner_namespaces = PartnerNamespacesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.partner_registrations = PartnerRegistrationsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
         self.partner_topics = PartnerTopicsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.private_endpoint_connections = PrivateEndpointConnectionsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.private_link_resources = PrivateLinkResourcesOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.private_endpoint_connections = PrivateEndpointConnectionsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.private_link_resources = PrivateLinkResourcesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
         self.system_topics = SystemTopicsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.extension_topics = ExtensionTopicsOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.extension_topics = ExtensionTopicsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
         self.topic_types = TopicTypesOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.verified_partners = VerifiedPartnersOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.verified_partners = VerifiedPartnersOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
 
-
-    def _send_request(
-        self,
-        request: HttpRequest,
-        **kwargs: Any
-    ) -> Awaitable[AsyncHttpResponse]:
+    def _send_request(self, request: HttpRequest, **kwargs: Any) -> Awaitable[AsyncHttpResponse]:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -140,7 +185,7 @@ class EventGridManagementClient:    # pylint: disable=too-many-instance-attribut
         >>> response = await client._send_request(request)
         <AsyncHttpResponse: 200 OK>
 
-        For more information on this code flow, see https://aka.ms/azsdk/python/protocol/quickstart
+        For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
 
         :param request: The network request you want to make. Required.
         :type request: ~azure.core.rest.HttpRequest
