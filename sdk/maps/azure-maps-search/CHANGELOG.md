71070adba46c6be20cd0efc4304378801129df82
Alex Tsao <alextts627@gmail.com>
2022-08-19 14:29:51 +0800

    Update expected release date&removed Python2.7 packages

--- sdk/maps/azure-maps-search/CHANGELOG.md
+++ sdk/maps/azure-maps-search/CHANGELOG.md
@@ -1,5 +1,5 @@
 # Release History
 
-## 1.0.0b1 (Unreleased)
+## 1.0.0b1 (2022-09-02)
 
 * Initial Release
--- sdk/maps/azure-maps-search/setup.py
+++ sdk/maps/azure-maps-search/setup.py
@@ -82,8 +82,5 @@ setup(
         'msrest>=0.6.21',
         'azure-common~=1.1',
         'azure-mgmt-core>=1.3.0,<2.0.0',
-    ],
-    extras_require={
-        ":python_version<'3.0'": ['azure-maps-nspkg~=3.0.2'],
-    }
+    ]
 )
