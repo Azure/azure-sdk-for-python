f73df7e66a933e0f428c4014f89461925731b460
Leighton Chen <lechen@microsoft.com>
2022-08-22 16:47:49 -0700

    Implement network statsbeat - success count (#25752)

---
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/statsbeat/_exporter.py
@@ -0,0 +1,34 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+import logging
+
+from typing import Optional
+from opentelemetry.sdk.metrics.export import DataPointT
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.util.instrumentation import InstrumentationScope
+
+from azure.monitor.opentelemetry.exporter._generated.models import TelemetryItem
+from azure.monitor.opentelemetry.exporter import AzureMonitorMetricExporter
+from azure.monitor.opentelemetry.exporter.statsbeat._state import _STATSBEAT_METRIC_NAME_MAPPINGS
+
+_logger = logging.getLogger(__name__)
+
+
+class _StatsBeatExporter(AzureMonitorMetricExporter):
+
+    # pylint: disable=protected-access
+    def _point_to_envelope(
+        self,
+        point: DataPointT,
+        name: str,
+        resource: Optional[Resource] = None,
+        scope: Optional[InstrumentationScope] = None
+    ) -> TelemetryItem:
+        # map statsbeat name from OpenTelemetry name
+        name = _STATSBEAT_METRIC_NAME_MAPPINGS.get(name, "")
+        return super()._point_to_envelope(
+            point,
+            name,
+            resource,
+            scope,
+        )
