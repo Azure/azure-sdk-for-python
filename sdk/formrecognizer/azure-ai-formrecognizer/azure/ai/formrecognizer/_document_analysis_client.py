7d603874c95d7925e3fd95584fa159cc3dcc5938
Catalina Peralta <catalinaperaltah@hotmail.com>
2022-08-23 14:25:35 -0700

    fix value checks in dac

--- sdk/formrecognizer/azure-ai-formrecognizer/azure/ai/formrecognizer/_document_analysis_client.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/azure/ai/formrecognizer/_document_analysis_client.py
@@ -105,18 +105,28 @@ class DocumentAnalysisClient(FormRecognizerClientBase):
                 :caption: Analyze a custom document. For more samples see the `samples` folder.
         """
 
-        if not model_id:
-            raise ValueError("model_id cannot be None or empty.")
-
         cls = kwargs.pop("cls", self._analyze_document_callback)
         continuation_token = kwargs.pop("continuation_token", None)
+
+        if continuation_token is not None:
+            return self._client.begin_analyze_document(  # type: ignore
+                model_id=model_id,
+                analyze_request=document,  # type: ignore
+                content_type="application/octet-stream",
+                string_index_type="unicodeCodePoint",
+                continuation_token=continuation_token,
+                cls=cls,
+                **kwargs
+            )
+
+        if not model_id:
+            raise ValueError("model_id cannot be None or empty.")
 
         return self._client.begin_analyze_document(  # type: ignore
             model_id=model_id,
             analyze_request=document,  # type: ignore
             content_type="application/octet-stream",
             string_index_type="unicodeCodePoint",
-            continuation_token=continuation_token,
             cls=cls,
             **kwargs
         )
@@ -154,6 +164,19 @@ class DocumentAnalysisClient(FormRecognizerClientBase):
                 :caption: Analyze a receipt. For more samples see the `samples` folder.
         """
 
+        cls = kwargs.pop("cls", self._analyze_document_callback)
+        continuation_token = kwargs.pop("continuation_token", None)
+
+        if continuation_token is not None:
+            return self._client.begin_analyze_document(  # type: ignore
+            model_id=model_id,
+            analyze_request={"urlSource": document_url},  # type: ignore
+            string_index_type="unicodeCodePoint",
+            continuation_token=continuation_token,
+            cls=cls,
+            **kwargs
+        )
+
         if not model_id:
             raise ValueError("model_id cannot be None or empty.")
 
@@ -162,14 +185,10 @@ class DocumentAnalysisClient(FormRecognizerClientBase):
                 "'document_url' needs to be of type 'str'."
                 "Please see `begin_analyze_document()` to pass a byte stream.")
 
-        cls = kwargs.pop("cls", self._analyze_document_callback)
-        continuation_token = kwargs.pop("continuation_token", None)
-
         return self._client.begin_analyze_document(  # type: ignore
             model_id=model_id,
             analyze_request={"urlSource": document_url},  # type: ignore
             string_index_type="unicodeCodePoint",
-            continuation_token=continuation_token,
             cls=cls,
             **kwargs
         )
