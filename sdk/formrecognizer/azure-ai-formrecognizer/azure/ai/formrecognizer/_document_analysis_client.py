563050107599ad9d947b58e13fde66972dfeb600
catalinaperalta <catalinaperaltah@hotmail.com>
2022-08-24 09:50:16 -0700

    [formrecognizer] Code updates (#25825)
    * fix value checks in dac
    
    * update test
    
    * remove repeat import
    
    * fix url stream test, remove recordings
    
    * update test assertion, add code comments for future maintenance

--- sdk/formrecognizer/azure-ai-formrecognizer/azure/ai/formrecognizer/_document_analysis_client.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/azure/ai/formrecognizer/_document_analysis_client.py
@@ -105,18 +105,28 @@ class DocumentAnalysisClient(FormRecognizerClientBase):
                 :caption: Analyze a custom document. For more samples see the `samples` folder.
         """
 
-        if not model_id:
-            raise ValueError("model_id cannot be None or empty.")
-
         cls = kwargs.pop("cls", self._analyze_document_callback)
         continuation_token = kwargs.pop("continuation_token", None)
+
+        if continuation_token is not None:
+            return self._client.begin_analyze_document(  # type: ignore
+                model_id=model_id,
+                analyze_request=document,  # type: ignore
+                content_type="application/octet-stream",
+                string_index_type="unicodeCodePoint",
+                continuation_token=continuation_token,
+                cls=cls,
+                **kwargs
+            )
+
+        if not model_id:
+            raise ValueError("model_id cannot be None or empty.")
 
         return self._client.begin_analyze_document(  # type: ignore
             model_id=model_id,
             analyze_request=document,  # type: ignore
             content_type="application/octet-stream",
             string_index_type="unicodeCodePoint",
-            continuation_token=continuation_token,
             cls=cls,
             **kwargs
         )
@@ -154,22 +164,33 @@ class DocumentAnalysisClient(FormRecognizerClientBase):
                 :caption: Analyze a receipt. For more samples see the `samples` folder.
         """
 
+        cls = kwargs.pop("cls", self._analyze_document_callback)
+        continuation_token = kwargs.pop("continuation_token", None)
+
+        # continuation token requests do not perform the same value checks as
+        # regular analysis requests
+        if continuation_token is not None:
+            return self._client.begin_analyze_document(  # type: ignore
+            model_id=model_id,
+            analyze_request={"urlSource": document_url},  # type: ignore
+            string_index_type="unicodeCodePoint",
+            continuation_token=continuation_token,
+            cls=cls,
+            **kwargs
+        )
+
         if not model_id:
             raise ValueError("model_id cannot be None or empty.")
 
         if not isinstance(document_url, str):
             raise ValueError(
-                "'document_url' needs to be of type 'str'."
+                "'document_url' needs to be of type 'str'. "
                 "Please see `begin_analyze_document()` to pass a byte stream.")
-
-        cls = kwargs.pop("cls", self._analyze_document_callback)
-        continuation_token = kwargs.pop("continuation_token", None)
 
         return self._client.begin_analyze_document(  # type: ignore
             model_id=model_id,
             analyze_request={"urlSource": document_url},  # type: ignore
             string_index_type="unicodeCodePoint",
-            continuation_token=continuation_token,
             cls=cls,
             **kwargs
         )
