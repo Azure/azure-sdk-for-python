06d879b2e04ad7c99867b81c83e7d84ef415247a
Jiefeng Chen <51037443+BigCat20196@users.noreply.github.com>
2022-08-29 14:09:58 +0800

    Update change_log.py

--- tools/azure-sdk-tools/packaging_tools/change_log.py
+++ tools/azure-sdk-tools/packaging_tools/change_log.py
@@ -201,7 +201,6 @@ def build_change_log(old_report, new_report):
 
     # when diff result is large,  compare_lengths=True may cause wrong result
     result = diff(old_report, new_report, compare_lengths=False)
-    print(f'result: {result}')
     for diff_line in result:
         # Operations
         if diff_line[0][0] == "operations":
