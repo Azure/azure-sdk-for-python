945e8cd147c8c239dee7f79d41cb833128a90d96
anujissarMS <anujissar@microsoft.com>
2022-08-30 13:20:25 -0700

    Add validations for invalid datestrings
    Add client side validations for valid_from and valid_until as invalid
    datestings are silently removed by the serializer
    ( https://github.com/Azure/autorest.python/issues/1413 )
    Can be removed once issue is resolved

--- sdk/communication/azure-communication-rooms/azure/communication/rooms/_shared/utils.py
+++ sdk/communication/azure-communication-rooms/azure/communication/rooms/_shared/utils.py
@@ -5,6 +5,7 @@
 # -------------------------------------------------------------------------
 
 import base64
+import isodate
 import json
 from typing import (  # pylint: disable=unused-import
     cast,
@@ -14,6 +15,7 @@ from datetime import datetime
 import calendar
 from msrest.serialization import TZ_UTC
 from azure.core.credentials import AccessToken
+from azure.core.exceptions import raise_with_traceback
 
 def _convert_datetime_to_utc_int(input_datetime):
     """
@@ -62,6 +64,19 @@ def get_current_utc_as_int():
     # type: () -> int
     current_utc_datetime = datetime.utcnow()
     return _convert_datetime_to_utc_int(current_utc_datetime)
+
+def verify_datetime_format(input_datetime):
+    #type: (datetime) -> bool
+    if input_datetime is None:
+        return True
+    try:
+        if isinstance(input_datetime, str):
+            input_datetime = isodate.parse_datetime(input_datetime)
+        if type(input_datetime) is datetime:
+            return True
+    except:
+            raise ValueError("{} is not a valid ISO-8601 datetime format".format(input_datetime)) from None
+    return True
 
 def create_access_token(token):
     # type: (str) -> azure.core.credentials.AccessToken
