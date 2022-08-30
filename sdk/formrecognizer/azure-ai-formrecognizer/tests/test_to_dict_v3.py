25eb90708782a63f88a0670348d969a0181eb476
catalinaperalta <catalinaperaltah@hotmail.com>
2022-08-29 17:37:46 -0700

    [formrecognizer] Renames and documentation updates (#25941)
    * OperationDetails rename
    
    * add image/heif to supported content types
    
    * rename OperationSummary
    
    * pylint, cspell fixes
    
    * docs correction

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_to_dict_v3.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_to_dict_v3.py
@@ -908,7 +908,7 @@ class TestToDict(FormRecognizerTest):
         assert d == final
 
     def test_model_operation_info_to_dict(self):
-        model = _models.DocumentModelOperationSummary(
+        model = _models.OperationSummary(
             operation_id="id123",
             status="succeeded",
             percent_completed=100,
@@ -1499,7 +1499,7 @@ class TestToDict(FormRecognizerTest):
         assert d == final
 
     def test_model_operation_to_dict(self):
-        model = _models.DocumentModelOperationDetails(
+        model = _models.OperationDetails(
             api_version="2022-08-31",
             tags={},
             operation_id="id123",
