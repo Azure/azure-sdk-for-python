2a18b2bd245e15de8955deb37bcefcb66b1a6ddc
Xiang Yan <xiangsjtu@gmail.com>
2022-09-06 11:08:51 -0700

    Update setup.py

--- sdk/identity/azure-identity/setup.py
+++ sdk/identity/azure-identity/setup.py
@@ -74,7 +74,7 @@ setup(
     install_requires=[
         "azure-core<2.0.0,>=1.11.0",
         "cryptography>=2.5",
-        "msal<2.0.0,>=1.19.0",
+        "msal<2.0.0,>=1.20.0b1",
         "msal-extensions<2.0.0,>=0.3.0",
         "six>=1.12.0",
     ],
