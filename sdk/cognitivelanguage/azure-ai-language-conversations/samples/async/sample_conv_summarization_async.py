793ecdecce36327e0a354e001027f8e03bb690b4
Wonhyeong Seo <wonhseo@kakao.com>
2022-09-09 04:57:00 +0900

    [CognitiveLanguage] update samples to use asyncio.run (#26090)
    * feat: use asyncio.run instead of loop variable
    
    * fix: typo in code
    
    * no clue why cspell isn't catching it on my local machine
    
    * pylint: last newline on conversations/async/authoring sample

--- sdk/cognitivelanguage/azure-ai-language-conversations/samples/async/sample_conv_summarization_async.py
+++ sdk/cognitivelanguage/azure-ai-language-conversations/samples/async/sample_conv_summarization_async.py
@@ -110,5 +110,4 @@ async def main():
     await sample_conv_summarization_async()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
