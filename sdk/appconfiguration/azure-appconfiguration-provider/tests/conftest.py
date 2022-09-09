b097b0ff9d0c41b058fdf3964eeb7ac2f4f4bfae
Matthew Metcalf <mrm9084@gmail.com>
2022-09-09 13:54:02 -0700

    Adding Test Case, currently doesn't work

---
+++ sdk/appconfiguration/azure-appconfiguration-provider/tests/conftest.py
@@ -0,0 +1,7 @@
+from devtools_testutils import test_proxy
+import pytest
+
+# autouse=True will trigger this fixture on each pytest run, even if it's not explicitly used by a test method
+@pytest.fixture(scope="session", autouse=True)
+def start_proxy(test_proxy):
+    return
