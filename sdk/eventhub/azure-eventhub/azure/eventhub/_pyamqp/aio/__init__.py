a239d6a36afc601c3a57c6317a9d0dee01a13708
Kashif Khan <kashifkhan@microsoft.com>
2022-09-09 14:19:30 -0500

    fix __init__

--- sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/aio/__init__.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/aio/__init__.py
@@ -1,8 +1,8 @@
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 
 from ._connection_async import Connection, ConnectionState
 from ._link_async import Link, LinkDeliverySettleReason, LinkState
@@ -13,3 +13,22 @@ from ._session_async import Session, SessionState
 from ._transport_async import AsyncTransport
 from ._client_async import AMQPClientAsync, ReceiveClientAsync, SendClientAsync
 from ._authentication_async import SASTokenAuthAsync
+
+__all__ = [
+    "Connection",
+    "ConnectionState",
+    "Link",
+    "LinkDeliverySettleReason",
+    "LinkState",
+    "ReceiverLink",
+    "SASLPlainCredential",
+    "SASLTransport",
+    "SenderLink",
+    "Session",
+    "SessionState",
+    "AsyncTransport",
+    "AMQPClientAsync",
+    "ReceiveClientAsync",
+    "SendClientAsync",
+    "SASTokenAuthAsync",
+]
