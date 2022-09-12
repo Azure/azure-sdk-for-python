c17e3f9557a30a7f01feb165b3be6fea2443b9c0
Jacob Lauzon <96087589+jalauzon-msft@users.noreply.github.com>
2022-09-11 19:45:31 -0700

    Address feedback from STG84 GA API View (#26057)

--- sdk/storage/azure-storage-blob/azure/storage/blob/_download.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/_download.py
@@ -9,7 +9,7 @@ import threading
 import time
 import warnings
 from io import BytesIO
-from typing import Generic, Iterator, Optional, TypeVar
+from typing import Generic, IO, Iterator, Optional, TypeVar
 
 from azure.core.exceptions import DecodeError, HttpResponseError, IncompleteReadError
 from azure.core.tracing.common import with_current_context
@@ -646,8 +646,7 @@ class StorageStreamDownloader(Generic[T]):  # pylint: disable=too-many-instance-
             return data.decode(self._encoding)
         return data
 
-    def readall(self):
-        # type: () -> T
+    def readall(self) -> T:
         """
         Read the entire contents of this blob.
         This operation is blocking until all data is downloaded.
@@ -697,7 +696,7 @@ class StorageStreamDownloader(Generic[T]):  # pylint: disable=too-many-instance-
         self._encoding = encoding
         return self.readall()
 
-    def readinto(self, stream):
+    def readinto(self, stream: IO[T]) -> int:
         """Download the contents of this file to a stream.
 
         :param stream:
