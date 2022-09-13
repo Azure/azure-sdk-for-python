eed9806f68383670ba21aafe1b28c27edce5d0bc
Wonhyeong Seo <wonhseo@kakao.com>
2022-09-14 05:12:01 +0900

    [Search] update samples to use asyncio.run (#26145)
    * fix: use asyncio.run instead of loop variable
    
    Co-authored-by: kyc5644 <kyc5644@naver.com>
    Co-authored-by: SaidbySolo <saidbysolo@gmail.com>
    
    Co-authored-by: kyc5644 <kyc5644@naver.com>
    Co-authored-by: SaidbySolo <saidbysolo@gmail.com>
    
    * pylint: added last line to missing files
    
    Co-authored-by: kyc5644 <kyc5644@naver.com>
    Co-authored-by: SaidbySolo <saidbysolo@gmail.com>

--- sdk/search/azure-search-documents/samples/async_samples/sample_index_alias_crud_operations_async.py
+++ sdk/search/azure-search-documents/samples/async_samples/sample_index_alias_crud_operations_async.py
@@ -102,6 +102,4 @@ async def main():
     await client.close()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    asyncio.run(main())
