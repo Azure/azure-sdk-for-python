8b4e6f37461ab182b4df9c57e6329017adadb2eb
PythonSdkPipelines <PythonSdkPipelines>
2022-09-19 02:06:03 +0000

    code and test

---
+++ sdk/quota/azure-mgmt-quota/tests/test_mgmt_quota.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import azure.mgmt.quota
+from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGroupPreparer, recorded_by_proxy
+
+AZURE_LOCATION = "eastus"
+
+
+class TestMgmtquota(AzureMgmtRecordedTestCase):
+    def setup_method(self, method):
+        self.mgmt_client = self.create_mgmt_client(azure.mgmt.quota.AzureQuotaExtensionAPI)
+
+    @recorded_by_proxy
+    def test_quota_quota_operation_list(self):
+        result = self.mgmt_client.quota_operation.list()
+        assert list(result) is not None
