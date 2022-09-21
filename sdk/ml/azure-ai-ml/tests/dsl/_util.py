66f5a1cf18dcc2460b9b73ceb30f3188a8570225
Zhengfei Wang <wangzhengfei.chn@outlook.com>
2022-09-21 10:46:03 +0800

    WIP on DSL E2E tests migration

--- sdk/ml/azure-ai-ml/tests/dsl/_util.py
+++ sdk/ml/azure-ai-ml/tests/dsl/_util.py
@@ -1,1 +1,14 @@
+from azure.ai.ml import MLClient
+from azure.core.exceptions import HttpResponseError
+
+
+def cancel_pipeline_job(pipeline, client: MLClient):
+    job = client.jobs.create_or_update(pipeline)
+    try:
+        client.jobs.cancel(job.name)
+    except HttpResponseError:
+        pass
+    return job
+
+
 _DSL_TIMEOUT_SECOND = 20 * 60  # timeout for dsl's tests, unit in second.
