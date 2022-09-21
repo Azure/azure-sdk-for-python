c06c49ecf1d24fc32bb79ebca1e037088664d84a
Jacob Lauzon <jalauzon@microsoft.com>
2022-09-21 15:05:23 -0700

    Fix encryption recorded tests

--- sdk/storage/azure-storage-blob/tests/conftest.py
+++ sdk/storage/azure-storage-blob/tests/conftest.py
@@ -28,5 +28,6 @@ def add_sanitizers(test_proxy):
 
     add_header_regex_sanitizer(key="x-ms-copy-source-authorization", value="Sanitized")
     add_header_regex_sanitizer(key="x-ms-encryption-key", value="Sanitized")
+    add_general_regex_sanitizer(regex=r'"EncryptionLibrary": "Python .*"', value='"EncryptionLibrary": "Python x.x.x"')
 
     add_uri_regex_sanitizer(regex=r"\.preprod\.", value=".")
