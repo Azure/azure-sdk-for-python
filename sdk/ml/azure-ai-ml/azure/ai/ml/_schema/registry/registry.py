ebd03426459c63f262d16d36d9e547c1536f920b
MilesHolland <108901744+MilesHolland@users.noreply.github.com>
2022-09-22 15:41:57 -0400

    copy closed source changes (#26389)

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/registry/registry.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/registry/registry.py
@@ -4,12 +4,15 @@
 
 from marshmallow import fields
 
-from azure.ai.ml._schema.core.fields import NestedField, StringTransformedEnum
+from azure.ai.ml._schema.core.fields import NestedField, StringTransformedEnum, UnionField
 from azure.ai.ml._schema.core.resource import ResourceSchema
 from azure.ai.ml._utils.utils import snake_to_pascal
 from azure.ai.ml.constants._common import PublicNetworkAccess
+from azure.ai.ml.constants._registry import AcrAccountSku
+from azure.ai.ml.entities._registry.registry_support_classes import SystemCreatedAcrAccount
 
 from .registry_region_arm_details import RegistryRegionArmDetailsSchema
+from .system_created_acr_account import SystemCreatedAcrAccountSchema
 from .util import acr_format_validator
 
 
@@ -34,7 +37,12 @@ class RegistrySchema(ResourceSchema):
     # in replication_locations. This is different from the internal swagger
     # definition, which has a per-region list of acr accounts.
     # Per-region acr account configuration is NOT possible through yaml configs for now.
-    container_registry = fields.Str(validate=acr_format_validator)
+    container_registry = UnionField(
+        [fields.Str(validate=acr_format_validator), NestedField(SystemCreatedAcrAccountSchema)],
+        required=False,
+        is_strict=True,
+        load_default=SystemCreatedAcrAccount(acr_account_sku=AcrAccountSku.PREMIUM),
+    )
     # managed_resource_group = ignored - output only
     # mlflow_registry_uri = ignored - output only
     # discovery_url = ignored - output only
