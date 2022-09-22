390aa78c2ee765beb5c519841745007df080ea1d
Georgia Fitzgerald <gefitzgerald@microsoft.com>
2022-09-22 16:34:00 -0400

    Replace missing files from ADO migration

---
+++ sdk/ml/azure-ai-ml/azure/ai/ml/constants/__init__.py
@@ -0,0 +1,37 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+from ._common import AssetTypes, GitProperties, InputOutputModes, ModelType, TimeZone
+from ._component import ParallelTaskType
+from ._deployment import BatchDeploymentOutputAction
+from ._job import (
+    DistributionType,
+    ImageClassificationModelNames,
+    ImageInstanceSegmentationModelNames,
+    ImageObjectDetectionModelNames,
+    ImportSourceType,
+    JobType,
+)
+from ._registry import ManagedServiceIdentityType as RegistryManagedServiceIdentityType
+from ._workspace import ManagedServiceIdentityType
+
+__all__ = [
+    "ImportSourceType",
+    "JobType",
+    "ParallelTaskType",
+    "AssetTypes",
+    "InputOutputModes",
+    "GitProperties",
+    "DistributionType",
+    "TimeZone",
+    "BatchDeploymentOutputAction",
+    "ModelType",
+    "ManagedServiceIdentityType",
+    "ImageClassificationModelNames",
+    "ImageObjectDetectionModelNames",
+    "ImageInstanceSegmentationModelNames",
+    "RegistryManagedServiceIdentityType",
+]
