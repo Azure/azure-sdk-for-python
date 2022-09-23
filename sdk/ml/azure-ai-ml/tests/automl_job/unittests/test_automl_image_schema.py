7de98265e37616ae2ec5f89a7a43ee1e2b9d4fc9
shubhamiit <41925087+shubhamiit@users.noreply.github.com>
2022-09-23 21:16:30 +0530

    remove limits from sweep section and update in global limit section (#26386)
    * remove limits from sweep section and update in global limit section
    
    * Adding pytest.fixture
    
    * Update Recordings
    
    * fix up recordings
    
    Co-authored-by: shubham soni <sonishubham@microsoft.com>
    Co-authored-by: Neehar Duvvuri <neduvvur@microsoft.com>

--- sdk/ml/azure-ai-ml/tests/automl_job/unittests/test_automl_image_schema.py
+++ sdk/ml/azure-ai-ml/tests/automl_job/unittests/test_automl_image_schema.py
@@ -87,10 +87,18 @@ def compute_binding_expected(mock_workspace_scope: OperationScope) -> str:
 
 @pytest.fixture
 def expected_image_limits(run_type: str) -> RestImageLimitSettings:
+    maxTrials = 1
+    maxConcurrentTrials = 1
+    if run_type == "sweep":
+        maxTrials = 20
+        maxConcurrentTrials = 4
+    elif run_type == "automode":
+        maxTrials = 2
+
     return RestImageLimitSettings(
         timeout=to_iso_duration_format_mins(60),
-        max_concurrent_trials=1,
-        max_trials=2 if run_type == "automode" else 1,
+        max_concurrent_trials=maxConcurrentTrials,
+        max_trials=maxTrials,
     )
 
 
@@ -116,10 +124,7 @@ def expected_image_target_column_name() -> str:
 @pytest.fixture
 def expected_image_sweep_settings() -> RestImageSweepSettings:
     return RestImageSweepSettings(
-        limits=RestImageSweepLimitSettings(
-            max_concurrent_trials=4,
-            max_trials=20,
-        ),
+        limits=RestImageSweepLimitSettings(),
         sampling_algorithm="grid",
         early_termination=RestBanditPolicy(
             slack_factor=0.2,
@@ -343,6 +348,10 @@ def loaded_image_classification_job(
     test_config = load_yaml(test_schema_path)
     if run_type == "automode":
         test_config["limits"]["max_trials"] = 2
+        test_config["limits"]["max_concurrent_trials"] = 1
+    elif run_type == "single":
+        test_config["limits"]["max_trials"] = 1
+        test_config["limits"]["max_concurrent_trials"] = 1
     if run_type != "sweep":
         # Remove search_space and sweep sections from the yaml
         del test_config["search_space"]
@@ -367,6 +376,10 @@ def loaded_image_classification_multilabel_job(
     test_config = load_yaml(test_schema_path)
     if run_type == "automode":
         test_config["limits"]["max_trials"] = 2
+        test_config["limits"]["max_concurrent_trials"] = 1
+    elif run_type == "single":
+        test_config["limits"]["max_trials"] = 1
+        test_config["limits"]["max_concurrent_trials"] = 1
     if run_type != "sweep":
         # Remove search_space and sweep sections from the yaml
         del test_config["search_space"]
@@ -389,6 +402,10 @@ def loaded_image_object_detection_job(
     test_config = load_yaml(test_schema_path)
     if run_type == "automode":
         test_config["limits"]["max_trials"] = 2
+        test_config["limits"]["max_concurrent_trials"] = 1
+    elif run_type == "single":
+        test_config["limits"]["max_trials"] = 1
+        test_config["limits"]["max_concurrent_trials"] = 1
     if run_type != "sweep":
         # Remove search_space and sweep sections from the yaml
         del test_config["search_space"]
@@ -411,6 +428,10 @@ def loaded_image_instance_segmentation_job(
     test_config = load_yaml(test_schema_path)
     if run_type == "automode":
         test_config["limits"]["max_trials"] = 2
+        test_config["limits"]["max_concurrent_trials"] = 1
+    elif run_type == "single":
+        test_config["limits"]["max_trials"] = 1
+        test_config["limits"]["max_concurrent_trials"] = 1
     if run_type != "sweep":
         # Remove search_space and sweep sections from the yaml
         del test_config["search_space"]
