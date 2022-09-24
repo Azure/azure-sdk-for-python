39eacba2142841e382a08d5b665718f37c984589
Matt Metcalf <mrm9084@gmail.com>
2022-09-23 17:18:14 -0700

    Setting defaults for test

--- sdk/appconfiguration/azure-appconfigurationprovider/tests/conftest.py
+++ sdk/appconfiguration/azure-appconfigurationprovider/tests/conftest.py
@@ -10,13 +10,13 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def add_sanitizers(test_proxy):
-    add_general_regex_sanitizer(value="https://fake-endpoint.azconfig.io",
-                                regex=os.environ.get('APPCONFIGURATION_ENDPOINT_STRING'))
+    add_general_regex_sanitizer(value="https://fake-endpoint.azconfig.io", 
+        regex=os.environ.get('APPCONFIGURATION_ENDPOINT_STRING', "https://fake-endpoint.azconfig.io"))
     add_general_regex_sanitizer(value="fake-connection-string",
-                                regex=os.environ.get('APPCONFIGURATION_CONNECTION_STRING'))
+                                regex=os.environ.get('APPCONFIGURATION_CONNECTION_STRING', "fake-connection-string"))
     add_general_regex_sanitizer(
-        value="fake-client-id", regex=os.environ.get('APPCONFIGURATION_CLIENT_ID'))
+        value="fake-client-id", regex=os.environ.get('APPCONFIGURATION_CLIENT_ID', "fake-client-id"))
     add_general_regex_sanitizer(
-        value="fake-client-secret", regex=os.environ.get('APPCONFIGURATION_CLIENT_SECRET'))
+        value="fake-client-secret", regex=os.environ.get('APPCONFIGURATION_CLIENT_SECRET', "fake-client-secret"))
     add_general_regex_sanitizer(
-        value="fake-tenant-id", regex=os.environ.get('APPCONFIGURATION_TENANT_ID'))
+        value="fake-tenant-id", regex=os.environ.get('APPCONFIGURATION_TENANT_ID', "fake-tenant-id"))
