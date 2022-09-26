f8ba82c25608baf249a017fce1c57cbb3cb3a0a4
Leighton Chen <lechen@microsoft.com>
2022-09-26 11:42:41 -0700

    Add instrumentation samples (#26409)

---
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/samples/traces/django/sample/example/apps.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+from django.apps import AppConfig
+
+
+class ExampleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'example'
