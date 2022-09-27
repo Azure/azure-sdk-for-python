752ff7fdc35f096c79c44d57fe63590408643fc0
Xiang Yan <xiangyan@microsoft.com>
2022-09-27 14:43:30 -0700

    update

--- sdk/core/azure-core-experimental/setup.py
+++ sdk/core/azure-core-experimental/setup.py
@@ -45,7 +45,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -60,7 +59,7 @@ setup(
     package_data={
         'pytyped': ['py.typed'],
     },
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
         'azure-core<2.0.0,>=1.25.0',
     ],
