2f1c7fa3b3ba54b223a571ea8d6c9dc70a398f54
YalinLi0312 <yall@microsoft.com>
2022-09-27 16:03:00 -0700

    Disable a cspell check

--- sdk/tables/azure-data-tables/samples/sample_copy_table.py
+++ sdk/tables/azure-data-tables/samples/sample_copy_table.py
@@ -59,7 +59,7 @@ class CopyTableSamples(object):
             "last_updated": datetime.today(),
             "product_id": uuid4(),
             "inventory_count": 42,
-            "barcode": b"135aefg8oj0ld58"
+            "barcode": b"135aefg8oj0ld58" # cspell:disable-line
         }
 
     def copy_table_from_table_to_blob(self):
