06d15fb0ab142f85e15117fca2be3fb0fc896f17
msyyc <70930885+msyyc@users.noreply.github.com>
2022-09-28 11:44:38 +0800

    optimize ci.yml

--- scripts/quickstart_tooling_dpg/main.py
+++ scripts/quickstart_tooling_dpg/main.py
@@ -29,13 +29,12 @@ def check_parameters(
 
 def generate_ci(template_path: Path, folder_path: Path, package_name: str) -> None:
     ci = Path(folder_path, "ci.yml")
-    ci_template_path = template_path / 'ci.yml'
     service_name = folder_path.name
-    name = package_name.split('-')[-1]
+    safe_name = package_name.replace("-", "")
     if not ci.exists():
-        with open(ci_template_path, "r") as file_in:
-            content = file_in.readlines()
-        content = [line.replace("ServiceName", service_name).replace('PackageName', name) for line in content]
+        env = Environment(loader=FileSystemLoader(template_path), keep_trailing_newline=True)
+        template = env.get_template('ci.yml')
+        content = template.render(package_name=package_name, service_name=service_name, safe_name=safe_name)
     else:
         with open(ci, "r") as file_in:
             content = file_in.readlines()
@@ -43,7 +42,7 @@ def generate_ci(template_path: Path, folder_path: Path, package_name: str) -> No
                 if f'{package_name}' in line:
                     return
             content.append(f'    - name: {package_name}\n')
-            content.append(f'      safeName: {package_name.replace("-", "")}\n')
+            content.append(f'      safeName: {safe_name}\n')
     with open(ci, "w") as file_out:
         file_out.writelines(content)
 
