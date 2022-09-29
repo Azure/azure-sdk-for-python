a4730e4cd265bbd09d551d122b3d1f65999f2c36
PythonSdkPipelines <PythonSdkPipelines>
2022-09-29 07:09:54 +0000

    code and test

--- sdk/appcontainers/azure-mgmt-appcontainers/azure/mgmt/appcontainers/models/__init__.py
+++ sdk/appcontainers/azure-mgmt-appcontainers/azure/mgmt/appcontainers/models/__init__.py
@@ -16,6 +16,9 @@ from ._models_py3 import AuthConfig
 from ._models_py3 import AuthConfigCollection
 from ._models_py3 import AuthPlatform
 from ._models_py3 import AvailableOperations
+from ._models_py3 import AvailableWorkloadProfile
+from ._models_py3 import AvailableWorkloadProfileProperties
+from ._models_py3 import AvailableWorkloadProfilesCollection
 from ._models_py3 import AzureActiveDirectory
 from ._models_py3 import AzureActiveDirectoryLogin
 from ._models_py3 import AzureActiveDirectoryRegistration
@@ -24,6 +27,10 @@ from ._models_py3 import AzureCredentials
 from ._models_py3 import AzureFileProperties
 from ._models_py3 import AzureStaticWebApps
 from ._models_py3 import AzureStaticWebAppsRegistration
+from ._models_py3 import BaseContainer
+from ._models_py3 import BillingMeter
+from ._models_py3 import BillingMeterCollection
+from ._models_py3 import BillingMeterProperties
 from ._models_py3 import Certificate
 from ._models_py3 import CertificateCollection
 from ._models_py3 import CertificatePatch
@@ -32,8 +39,14 @@ from ._models_py3 import CheckNameAvailabilityRequest
 from ._models_py3 import CheckNameAvailabilityResponse
 from ._models_py3 import ClientRegistration
 from ._models_py3 import Configuration
+from ._models_py3 import ConnectedEnvironment
+from ._models_py3 import ConnectedEnvironmentCollection
+from ._models_py3 import ConnectedEnvironmentStorage
+from ._models_py3 import ConnectedEnvironmentStorageProperties
+from ._models_py3 import ConnectedEnvironmentStoragesCollection
 from ._models_py3 import Container
 from ._models_py3 import ContainerApp
+from ._models_py3 import ContainerAppAuthToken
 from ._models_py3 import ContainerAppCollection
 from ._models_py3 import ContainerAppProbe
 from ._models_py3 import ContainerAppProbeHttpGet
@@ -43,7 +56,10 @@ from ._models_py3 import ContainerAppSecret
 from ._models_py3 import ContainerResources
 from ._models_py3 import CookieExpiration
 from ._models_py3 import CustomDomain
+from ._models_py3 import CustomDomainConfiguration
 from ._models_py3 import CustomHostnameAnalysisResult
+from ._models_py3 import CustomHostnameAnalysisResultCustomDomainVerificationFailureInfo
+from ._models_py3 import CustomHostnameAnalysisResultCustomDomainVerificationFailureInfoDetailsItem
 from ._models_py3 import CustomOpenIdConnectProvider
 from ._models_py3 import CustomScaleRule
 from ._models_py3 import Dapr
@@ -55,7 +71,25 @@ from ._models_py3 import DefaultAuthorizationPolicy
 from ._models_py3 import DefaultErrorResponse
 from ._models_py3 import DefaultErrorResponseError
 from ._models_py3 import DefaultErrorResponseErrorDetailsItem
+from ._models_py3 import DiagnosticDataProviderMetadata
+from ._models_py3 import DiagnosticDataProviderMetadataPropertyBagItem
+from ._models_py3 import DiagnosticDataTableResponseColumn
+from ._models_py3 import DiagnosticDataTableResponseObject
+from ._models_py3 import DiagnosticRendering
+from ._models_py3 import DiagnosticSupportTopic
+from ._models_py3 import Diagnostics
+from ._models_py3 import DiagnosticsCollection
+from ._models_py3 import DiagnosticsDataApiResponse
+from ._models_py3 import DiagnosticsDefinition
+from ._models_py3 import DiagnosticsProperties
+from ._models_py3 import DiagnosticsStatus
+from ._models_py3 import EnvironmentAuthToken
+from ._models_py3 import EnvironmentSkuProperties
 from ._models_py3 import EnvironmentVar
+from ._models_py3 import ErrorAdditionalInfo
+from ._models_py3 import ErrorDetail
+from ._models_py3 import ErrorResponse
+from ._models_py3 import ExtendedLocation
 from ._models_py3 import Facebook
 from ._models_py3 import ForwardProxy
 from ._models_py3 import GitHub
@@ -67,12 +101,15 @@ from ._models_py3 import HttpSettings
 from ._models_py3 import HttpSettingsRoutes
 from ._models_py3 import IdentityProviders
 from ._models_py3 import Ingress
+from ._models_py3 import InitContainer
+from ._models_py3 import IpSecurityRestrictionRule
 from ._models_py3 import JwtClaimChecks
 from ._models_py3 import LogAnalyticsConfiguration
 from ._models_py3 import Login
 from ._models_py3 import LoginRoutes
 from ._models_py3 import LoginScopes
 from ._models_py3 import ManagedEnvironment
+from ._models_py3 import ManagedEnvironmentOutboundSettings
 from ._models_py3 import ManagedEnvironmentStorage
 from ._models_py3 import ManagedEnvironmentStorageProperties
 from ._models_py3 import ManagedEnvironmentStoragesCollection
@@ -103,6 +140,7 @@ from ._models_py3 import SecretsCollection
 from ._models_py3 import SourceControl
 from ._models_py3 import SourceControlCollection
 from ._models_py3 import SystemData
+from ._models_py3 import TcpScaleRule
 from ._models_py3 import Template
 from ._models_py3 import TrackedResource
 from ._models_py3 import TrafficWeight
@@ -112,158 +150,222 @@ from ._models_py3 import UserAssignedIdentity
 from ._models_py3 import VnetConfiguration
 from ._models_py3 import Volume
 from ._models_py3 import VolumeMount
+from ._models_py3 import WorkloadProfile
+from ._models_py3 import WorkloadProfileStates
+from ._models_py3 import WorkloadProfileStatesCollection
+from ._models_py3 import WorkloadProfileStatesProperties
 
-
-from ._container_apps_api_client_enums import (
-    AccessMode,
-    ActiveRevisionsMode,
-    AppProtocol,
-    BindingType,
-    CertificateProvisioningState,
-    CheckNameAvailabilityReason,
-    ContainerAppProvisioningState,
-    CookieExpirationConvention,
-    CreatedByType,
-    DnsVerificationTestResult,
-    EnvironmentProvisioningState,
-    ForwardProxyConvention,
-    IngressTransportMethod,
-    ManagedServiceIdentityType,
-    RevisionHealthState,
-    RevisionProvisioningState,
-    Scheme,
-    SourceControlOperationState,
-    StorageType,
-    Type,
-    UnauthenticatedClientActionV2,
-)
+from ._container_apps_api_client_enums import AccessMode
+from ._container_apps_api_client_enums import Action
+from ._container_apps_api_client_enums import ActiveRevisionsMode
+from ._container_apps_api_client_enums import AppProtocol
+from ._container_apps_api_client_enums import Applicability
+from ._container_apps_api_client_enums import BindingType
+from ._container_apps_api_client_enums import Category
+from ._container_apps_api_client_enums import CertificateProvisioningState
+from ._container_apps_api_client_enums import CheckNameAvailabilityReason
+from ._container_apps_api_client_enums import ConnectedEnvironmentProvisioningState
+from ._container_apps_api_client_enums import ContainerAppProvisioningState
+from ._container_apps_api_client_enums import CookieExpirationConvention
+from ._container_apps_api_client_enums import CreatedByType
+from ._container_apps_api_client_enums import DnsVerificationTestResult
+from ._container_apps_api_client_enums import EnvironmentProvisioningState
+from ._container_apps_api_client_enums import ExtendedLocationTypes
+from ._container_apps_api_client_enums import ForwardProxyConvention
+from ._container_apps_api_client_enums import IngressTransportMethod
+from ._container_apps_api_client_enums import LogLevel
+from ._container_apps_api_client_enums import ManagedEnvironmentOutBoundType
+from ._container_apps_api_client_enums import ManagedServiceIdentityType
+from ._container_apps_api_client_enums import RevisionHealthState
+from ._container_apps_api_client_enums import RevisionProvisioningState
+from ._container_apps_api_client_enums import Scheme
+from ._container_apps_api_client_enums import SkuName
+from ._container_apps_api_client_enums import SourceControlOperationState
+from ._container_apps_api_client_enums import StorageType
+from ._container_apps_api_client_enums import Type
+from ._container_apps_api_client_enums import UnauthenticatedClientActionV2
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    'AllowedAudiencesValidation',
-    'AllowedPrincipals',
-    'AppLogsConfiguration',
-    'AppRegistration',
-    'Apple',
-    'AppleRegistration',
-    'AuthConfig',
-    'AuthConfigCollection',
-    'AuthPlatform',
-    'AvailableOperations',
-    'AzureActiveDirectory',
-    'AzureActiveDirectoryLogin',
-    'AzureActiveDirectoryRegistration',
-    'AzureActiveDirectoryValidation',
-    'AzureCredentials',
-    'AzureFileProperties',
-    'AzureStaticWebApps',
-    'AzureStaticWebAppsRegistration',
-    'Certificate',
-    'CertificateCollection',
-    'CertificatePatch',
-    'CertificateProperties',
-    'CheckNameAvailabilityRequest',
-    'CheckNameAvailabilityResponse',
-    'ClientRegistration',
-    'Configuration',
-    'Container',
-    'ContainerApp',
-    'ContainerAppCollection',
-    'ContainerAppProbe',
-    'ContainerAppProbeHttpGet',
-    'ContainerAppProbeHttpGetHttpHeadersItem',
-    'ContainerAppProbeTcpSocket',
-    'ContainerAppSecret',
-    'ContainerResources',
-    'CookieExpiration',
-    'CustomDomain',
-    'CustomHostnameAnalysisResult',
-    'CustomOpenIdConnectProvider',
-    'CustomScaleRule',
-    'Dapr',
-    'DaprComponent',
-    'DaprComponentsCollection',
-    'DaprMetadata',
-    'DaprSecretsCollection',
-    'DefaultAuthorizationPolicy',
-    'DefaultErrorResponse',
-    'DefaultErrorResponseError',
-    'DefaultErrorResponseErrorDetailsItem',
-    'EnvironmentVar',
-    'Facebook',
-    'ForwardProxy',
-    'GitHub',
-    'GithubActionConfiguration',
-    'GlobalValidation',
-    'Google',
-    'HttpScaleRule',
-    'HttpSettings',
-    'HttpSettingsRoutes',
-    'IdentityProviders',
-    'Ingress',
-    'JwtClaimChecks',
-    'LogAnalyticsConfiguration',
-    'Login',
-    'LoginRoutes',
-    'LoginScopes',
-    'ManagedEnvironment',
-    'ManagedEnvironmentStorage',
-    'ManagedEnvironmentStorageProperties',
-    'ManagedEnvironmentStoragesCollection',
-    'ManagedEnvironmentsCollection',
-    'ManagedServiceIdentity',
-    'Nonce',
-    'OpenIdConnectClientCredential',
-    'OpenIdConnectConfig',
-    'OpenIdConnectLogin',
-    'OpenIdConnectRegistration',
-    'OperationDetail',
-    'OperationDisplay',
-    'ProxyResource',
-    'QueueScaleRule',
-    'RegistryCredentials',
-    'RegistryInfo',
-    'Replica',
-    'ReplicaCollection',
-    'ReplicaContainer',
-    'Resource',
-    'Revision',
-    'RevisionCollection',
-    'Scale',
-    'ScaleRule',
-    'ScaleRuleAuth',
-    'Secret',
-    'SecretsCollection',
-    'SourceControl',
-    'SourceControlCollection',
-    'SystemData',
-    'Template',
-    'TrackedResource',
-    'TrafficWeight',
-    'Twitter',
-    'TwitterRegistration',
-    'UserAssignedIdentity',
-    'VnetConfiguration',
-    'Volume',
-    'VolumeMount',
-    'AccessMode',
-    'ActiveRevisionsMode',
-    'AppProtocol',
-    'BindingType',
-    'CertificateProvisioningState',
-    'CheckNameAvailabilityReason',
-    'ContainerAppProvisioningState',
-    'CookieExpirationConvention',
-    'CreatedByType',
-    'DnsVerificationTestResult',
-    'EnvironmentProvisioningState',
-    'ForwardProxyConvention',
-    'IngressTransportMethod',
-    'ManagedServiceIdentityType',
-    'RevisionHealthState',
-    'RevisionProvisioningState',
-    'Scheme',
-    'SourceControlOperationState',
-    'StorageType',
-    'Type',
-    'UnauthenticatedClientActionV2',
+    "AllowedAudiencesValidation",
+    "AllowedPrincipals",
+    "AppLogsConfiguration",
+    "AppRegistration",
+    "Apple",
+    "AppleRegistration",
+    "AuthConfig",
+    "AuthConfigCollection",
+    "AuthPlatform",
+    "AvailableOperations",
+    "AvailableWorkloadProfile",
+    "AvailableWorkloadProfileProperties",
+    "AvailableWorkloadProfilesCollection",
+    "AzureActiveDirectory",
+    "AzureActiveDirectoryLogin",
+    "AzureActiveDirectoryRegistration",
+    "AzureActiveDirectoryValidation",
+    "AzureCredentials",
+    "AzureFileProperties",
+    "AzureStaticWebApps",
+    "AzureStaticWebAppsRegistration",
+    "BaseContainer",
+    "BillingMeter",
+    "BillingMeterCollection",
+    "BillingMeterProperties",
+    "Certificate",
+    "CertificateCollection",
+    "CertificatePatch",
+    "CertificateProperties",
+    "CheckNameAvailabilityRequest",
+    "CheckNameAvailabilityResponse",
+    "ClientRegistration",
+    "Configuration",
+    "ConnectedEnvironment",
+    "ConnectedEnvironmentCollection",
+    "ConnectedEnvironmentStorage",
+    "ConnectedEnvironmentStorageProperties",
+    "ConnectedEnvironmentStoragesCollection",
+    "Container",
+    "ContainerApp",
+    "ContainerAppAuthToken",
+    "ContainerAppCollection",
+    "ContainerAppProbe",
+    "ContainerAppProbeHttpGet",
+    "ContainerAppProbeHttpGetHttpHeadersItem",
+    "ContainerAppProbeTcpSocket",
+    "ContainerAppSecret",
+    "ContainerResources",
+    "CookieExpiration",
+    "CustomDomain",
+    "CustomDomainConfiguration",
+    "CustomHostnameAnalysisResult",
+    "CustomHostnameAnalysisResultCustomDomainVerificationFailureInfo",
+    "CustomHostnameAnalysisResultCustomDomainVerificationFailureInfoDetailsItem",
+    "CustomOpenIdConnectProvider",
+    "CustomScaleRule",
+    "Dapr",
+    "DaprComponent",
+    "DaprComponentsCollection",
+    "DaprMetadata",
+    "DaprSecretsCollection",
+    "DefaultAuthorizationPolicy",
+    "DefaultErrorResponse",
+    "DefaultErrorResponseError",
+    "DefaultErrorResponseErrorDetailsItem",
+    "DiagnosticDataProviderMetadata",
+    "DiagnosticDataProviderMetadataPropertyBagItem",
+    "DiagnosticDataTableResponseColumn",
+    "DiagnosticDataTableResponseObject",
+    "DiagnosticRendering",
+    "DiagnosticSupportTopic",
+    "Diagnostics",
+    "DiagnosticsCollection",
+    "DiagnosticsDataApiResponse",
+    "DiagnosticsDefinition",
+    "DiagnosticsProperties",
+    "DiagnosticsStatus",
+    "EnvironmentAuthToken",
+    "EnvironmentSkuProperties",
+    "EnvironmentVar",
+    "ErrorAdditionalInfo",
+    "ErrorDetail",
+    "ErrorResponse",
+    "ExtendedLocation",
+    "Facebook",
+    "ForwardProxy",
+    "GitHub",
+    "GithubActionConfiguration",
+    "GlobalValidation",
+    "Google",
+    "HttpScaleRule",
+    "HttpSettings",
+    "HttpSettingsRoutes",
+    "IdentityProviders",
+    "Ingress",
+    "InitContainer",
+    "IpSecurityRestrictionRule",
+    "JwtClaimChecks",
+    "LogAnalyticsConfiguration",
+    "Login",
+    "LoginRoutes",
+    "LoginScopes",
+    "ManagedEnvironment",
+    "ManagedEnvironmentOutboundSettings",
+    "ManagedEnvironmentStorage",
+    "ManagedEnvironmentStorageProperties",
+    "ManagedEnvironmentStoragesCollection",
+    "ManagedEnvironmentsCollection",
+    "ManagedServiceIdentity",
+    "Nonce",
+    "OpenIdConnectClientCredential",
+    "OpenIdConnectConfig",
+    "OpenIdConnectLogin",
+    "OpenIdConnectRegistration",
+    "OperationDetail",
+    "OperationDisplay",
+    "ProxyResource",
+    "QueueScaleRule",
+    "RegistryCredentials",
+    "RegistryInfo",
+    "Replica",
+    "ReplicaCollection",
+    "ReplicaContainer",
+    "Resource",
+    "Revision",
+    "RevisionCollection",
+    "Scale",
+    "ScaleRule",
+    "ScaleRuleAuth",
+    "Secret",
+    "SecretsCollection",
+    "SourceControl",
+    "SourceControlCollection",
+    "SystemData",
+    "TcpScaleRule",
+    "Template",
+    "TrackedResource",
+    "TrafficWeight",
+    "Twitter",
+    "TwitterRegistration",
+    "UserAssignedIdentity",
+    "VnetConfiguration",
+    "Volume",
+    "VolumeMount",
+    "WorkloadProfile",
+    "WorkloadProfileStates",
+    "WorkloadProfileStatesCollection",
+    "WorkloadProfileStatesProperties",
+    "AccessMode",
+    "Action",
+    "ActiveRevisionsMode",
+    "AppProtocol",
+    "Applicability",
+    "BindingType",
+    "Category",
+    "CertificateProvisioningState",
+    "CheckNameAvailabilityReason",
+    "ConnectedEnvironmentProvisioningState",
+    "ContainerAppProvisioningState",
+    "CookieExpirationConvention",
+    "CreatedByType",
+    "DnsVerificationTestResult",
+    "EnvironmentProvisioningState",
+    "ExtendedLocationTypes",
+    "ForwardProxyConvention",
+    "IngressTransportMethod",
+    "LogLevel",
+    "ManagedEnvironmentOutBoundType",
+    "ManagedServiceIdentityType",
+    "RevisionHealthState",
+    "RevisionProvisioningState",
+    "Scheme",
+    "SkuName",
+    "SourceControlOperationState",
+    "StorageType",
+    "Type",
+    "UnauthenticatedClientActionV2",
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
---
+++ sdk/appcontainers/azure-mgmt-appcontainers/tests/test_operations.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+from contextlib import suppress
+
+from azure.mgmt.appcontainers import ContainerAppsAPIClient
+import azure.mgmt.appcontainers.models
+from azure.core.exceptions import HttpResponseError
+from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGroupPreparer, recorded_by_proxy
+
+AZURE_LOCATION = "eastus"
+
+
+class TestMgmtAPPCONTAINERSOPERATIONS(AzureMgmtRecordedTestCase):
+    def setup_method(self, method):
+        self.mgmt_client = self.create_mgmt_client(ContainerAppsAPIClient)
+
+    @recorded_by_proxy
+    def test_operations_list(self):
+        # it proves that we can normally send request but maybe needs additional parameters
+        with suppress(HttpResponseError):
+            result = self.mgmt_client.operations.list()
+            assert list(result) is not None
