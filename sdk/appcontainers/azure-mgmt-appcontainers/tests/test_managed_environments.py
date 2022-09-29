971b6548838c6389065ea2a3a3b736ce64387f19
PythonSdkPipelines <PythonSdkPipelines>
2022-09-29 07:30:50 +0000

    code and test

---
+++ sdk/appcontainers/azure-mgmt-appcontainers/tests/test_managed_environments.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+from contextlib import suppress
+
+from azure.mgmt.appcontainers import ContainerAppsAPIClient
+import azure.mgmt.appcontainers.models
+from azure.core.exceptions import HttpResponseError
+from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGroupPreparer, recorded_by_proxy
+
+AZURE_LOCATION = "eastus"
+
+
+class TestMgmtAppcontainers(AzureMgmtRecordedTestCase):
+    def setup_method(self, method):
+        self.mgmt_client = self.create_mgmt_client(ContainerAppsAPIClient)
+
+    @recorded_by_proxy
+    def test_managed_environments_list_by_subscription(self):
+        # it proves that we can normally send request but maybe needs additional parameters
+        with suppress(HttpResponseError):
+            result = self.mgmt_client.managed_environments.list_by_subscription()
+            assert list(result) is not None
