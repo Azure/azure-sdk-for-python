6b5a984181a89132113cacb09128831c16bf9991
TonyJ1 <41344586+TonyJ1@users.noreply.github.com>
2022-10-03 22:21:26 -0700

    Fix Unittests: ml_client, job_operations

--- sdk/ml/azure-ai-ml/tests/job_common/unittests/test_job_operations.py
+++ sdk/ml/azure-ai-ml/tests/job_common/unittests/test_job_operations.py
@@ -128,26 +128,26 @@ class TestJobOperations:
     def test_list(self, mock_job_operation: JobOperations) -> None:
         mock_job_operation.list()
         expected = (mock_job_operation._resource_group_name, mock_job_operation._workspace_name)
-        assert expected in mock_job_operation._operation_2022_06_preview.list.call_args
+        assert expected in mock_job_operation._operation_2022_10_preview.list.call_args
 
     @patch.dict(os.environ, {AZUREML_PRIVATE_FEATURES_ENV_VAR: "True"})
     def test_list_private_preview(self, mock_job_operation: JobOperations) -> None:
         mock_job_operation.list()
         expected = (mock_job_operation._resource_group_name, mock_job_operation._workspace_name)
-        assert expected in mock_job_operation._operation_2022_06_preview.list.call_args
+        assert expected in mock_job_operation._operation_2022_10_preview.list.call_args
 
     @patch.object(Job, "_from_rest_object")
     def test_get(self, mock_method, mock_job_operation: JobOperations) -> None:
         mock_method.return_value = Command(component=None)
         mock_job_operation.get("randon_name")
-        mock_job_operation._operation_2022_06_preview.get.assert_called_once()
+        mock_job_operation._operation_2022_10_preview.get.assert_called_once()
 
     @patch.object(Job, "_from_rest_object")
     @patch.dict(os.environ, {AZUREML_PRIVATE_FEATURES_ENV_VAR: "True"})
     def test_get_private_preview_flag_returns_latest(self, mock_method, mock_job_operation: JobOperations) -> None:
         mock_method.return_value = Command(component=None)
         mock_job_operation.get("random_name")
-        mock_job_operation._operation_2022_06_preview.get.assert_called_once()
+        mock_job_operation._operation_2022_10_preview.get.assert_called_once()
 
     def test_stream_command_job(self, mock_job_operation: JobOperations) -> None:
         # setup
@@ -158,7 +158,7 @@ class TestJobOperations:
         mock_job_operation.stream("random_name")
 
         # check
-        mock_job_operation._operation_2022_06_preview.get.assert_called_once()
+        mock_job_operation._operation_2022_10_preview.get.assert_called_once()
         mock_job_operation._get_workspace_url.assert_called_once()
         mock_job_operation._stream_logs_until_completion.assert_called_once()
         assert mock_job_operation._runs_operations_client._operation._client._base_url == "TheWorkSpaceUrl"
@@ -171,7 +171,7 @@ class TestJobOperations:
         mock_job_operation.create_or_update(job=job)
         git_props = get_git_properties()
         assert git_props.items() <= job.properties.items()
-        mock_job_operation._operation_2022_06_preview.create_or_update.assert_called_once()
+        mock_job_operation._operation_2022_10_preview.create_or_update.assert_called_once()
         mock_job_operation._credential.get_token.assert_called_once_with("https://ml.azure.com/.default")
 
     @pytest.mark.skip(reason="Function under test no longer returns Job as output")
@@ -189,15 +189,15 @@ class TestJobOperations:
     def test_archive(self, mock_method, mock_job_operation: JobOperations) -> None:
         mock_method.return_value = Command(component=None)
         mock_job_operation.archive(name="random_name")
-        mock_job_operation._operation_2022_06_preview.get.assert_called_once()
-        mock_job_operation._operation_2022_06_preview.create_or_update.assert_called_once()
+        mock_job_operation._operation_2022_10_preview.get.assert_called_once()
+        mock_job_operation._operation_2022_10_preview.create_or_update.assert_called_once()
 
     @patch.object(Job, "_from_rest_object")
     def test_restore(self, mock_method, mock_job_operation: JobOperations) -> None:
         mock_method.return_value = Command(component=None)
         mock_job_operation.restore(name="random_name")
-        mock_job_operation._operation_2022_06_preview.get.assert_called_once()
-        mock_job_operation._operation_2022_06_preview.create_or_update.assert_called_once()
+        mock_job_operation._operation_2022_10_preview.get.assert_called_once()
+        mock_job_operation._operation_2022_10_preview.create_or_update.assert_called_once()
 
     @pytest.mark.parametrize(
         "corrupt_job_data",
