0f53331db4c95e14fcb1f93cb239f7110e7338ec
Diondra <16376603+diondrapeck@users.noreply.github.com>
2022-10-04 09:06:53 -0700

    Remove job_errors, move all custom exceptions to exceptions.py (#26582)
    * Move custom exceptions to exceptions, fix circular import
    
    * fix import path
    
    * Move import into method to prevent future circular imports
    
    * Fix pylint line too long errors
    
    * Fix pylint errors

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/job.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/job.py
@@ -22,11 +22,18 @@ from azure.ai.ml._utils.utils import dump_yaml_to_file
 from azure.ai.ml.constants._common import BASE_PATH_CONTEXT_KEY, PARAMS_OVERRIDE_KEY, CommonYamlFields
 from azure.ai.ml.constants._compute import ComputeType
 from azure.ai.ml.constants._job.job import JobServices, JobType
-from azure.ai.ml.entities.job_errors import JobParsingError, PipelineChildJobError
 from azure.ai.ml.entities._mixins import TelemetryMixin
 from azure.ai.ml.entities._resource import Resource
 from azure.ai.ml.entities._util import find_type_in_override
-from azure.ai.ml.exceptions import ErrorCategory, ErrorTarget, JobException, ValidationErrorType, ValidationException
+from azure.ai.ml.exceptions import(
+    ErrorCategory,
+    ErrorTarget,
+    JobException,
+    ValidationErrorType,
+    ValidationException,
+    JobParsingError,
+    PipelineChildJobError,
+)
 
 from ._studio_url_from_job_id import studio_url_from_job_id
 from .pipeline._component_translatable import ComponentTranslatableMixin
