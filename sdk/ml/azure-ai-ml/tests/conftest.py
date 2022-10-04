2b21c94aaa53d0b45dec1cd43d1d10a0938f7ff5
Hanchi Wang <hancwang@microsoft.com>
2022-10-03 23:02:41 -0700

    e2e tests passing

--- sdk/ml/azure-ai-ml/tests/conftest.py
+++ sdk/ml/azure-ai-ml/tests/conftest.py
@@ -62,7 +62,7 @@ def fake_datastore_key() -> str:
 def add_sanitizers(test_proxy, fake_datastore_key):
     add_remove_header_sanitizer(headers="x-azureml-token,Log-URL")
     set_custom_default_matcher(
-        excluded_headers="x-ms-meta-name,x-ms-meta-version")
+        excluded_headers="x-ms-meta-name,x-ms-meta-version", ignored_query_parameters="api-version")
     add_body_key_sanitizer(json_path="$.key", value=fake_datastore_key)
     add_body_key_sanitizer(json_path="$....key", value=fake_datastore_key)
     add_body_key_sanitizer(
