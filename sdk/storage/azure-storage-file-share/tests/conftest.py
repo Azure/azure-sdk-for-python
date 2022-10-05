6545f469353c27c71ddc4ee4769202135f87a0c8
Vincent Tran <vincenttran@microsoft.com>
2022-10-05 14:41:20 -0700

    Forgot to save when resolving merge conflict

--- sdk/storage/azure-storage-file-share/tests/conftest.py
+++ sdk/storage/azure-storage-file-share/tests/conftest.py
@@ -27,10 +27,7 @@ def add_sanitizers(test_proxy):
     add_header_regex_sanitizer(key="Cookie", value="cookie;")
     add_oauth_response_sanitizer()
 
-<<<<<<< HEAD
     add_header_regex_sanitizer(key="x-ms-copy-source-authorization", value="Sanitized")
-=======
->>>>>>> main
     add_header_regex_sanitizer(key="x-ms-encryption-key", value="Sanitized")
 
     add_uri_string_sanitizer(target=".preprod.", value=".")
