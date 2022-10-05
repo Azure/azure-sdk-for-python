4169d76d9eb39091781fb8d89feb7d33ca29b3b7
Kashif Khan <361477+kashifkhan@users.noreply.github.com>
2022-10-05 11:01:21 -0700

    [Pyamqp] Fix Async Invalid Host Error Test (#26595)
    * resert transport to original state
    
    * add in missing continue in except
    
    * Update sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/aio/_transport_async.py
    
    Co-authored-by: swathipil <76007337+swathipil@users.noreply.github.com>
    
    Co-authored-by: swathipil <76007337+swathipil@users.noreply.github.com>

--- sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/_transport.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/_transport.py
@@ -289,28 +289,6 @@ class _AbstractTransport(object):  # pylint: disable=too-many-instance-attribute
                     host, port, family, socket.SOCK_STREAM, SOL_TCP
                 )
                 entries_num = len(entries)
-                # now that we have address(es) for the hostname, connect to broker
-                for i, res in enumerate(entries):
-                    af, socktype, proto, _, sa = res
-                    try:
-                        self.sock = socket.socket(af, socktype, proto)
-                        try:
-                            set_cloexec(self.sock, True)
-                        except NotImplementedError:
-                            pass
-                        self.sock.settimeout(timeout)
-                        self.sock.connect(sa)
-                    except socket.error as ex:
-                        e = ex
-                        if self.sock is not None:
-                            self.sock.close()
-                            self.sock = None
-                        # we may have depleted all our options
-                        if i + 1 >= entries_num and n + 1 >= addr_types_num:
-                            raise
-                    else:
-                        # hurray, we established connection
-                        return
             except socket.gaierror:
                 # we may have depleted all our options
                 if n + 1 >= addr_types_num:
@@ -321,6 +299,29 @@ class _AbstractTransport(object):  # pylint: disable=too-many-instance-attribute
                         "failed to resolve broker hostname"
                     )
                 continue  # pragma: no cover
+            
+            # now that we have address(es) for the hostname, connect to broker
+            for i, res in enumerate(entries):
+                af, socktype, proto, _, sa = res
+                try:
+                    self.sock = socket.socket(af, socktype, proto)
+                    try:
+                        set_cloexec(self.sock, True)
+                    except NotImplementedError:
+                        pass
+                    self.sock.settimeout(timeout)
+                    self.sock.connect(sa)
+                except socket.error as ex:
+                    e = ex
+                    if self.sock is not None:
+                        self.sock.close()
+                        self.sock = None
+                    # we may have depleted all our options
+                    if i + 1 >= entries_num and n + 1 >= addr_types_num:
+                        raise
+                else:
+                    # hurray, we established connection
+                    return
 
     def _init_socket(self, socket_settings, read_timeout):
         self.sock.settimeout(None)  # set socket back to blocking mode
