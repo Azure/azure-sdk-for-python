f422bae265315ac087e44a32606ae8d593302004
Alex Tsao <alextts627@gmail.com>
2022-10-06 10:18:02 +0800

    replaced with test proxy

--- sdk/maps/azure-maps-search/CHANGELOG.md
+++ sdk/maps/azure-maps-search/CHANGELOG.md
@@ -1,14 +1,6 @@
 # Release History
 
-## 1.0.0b2 (Unreleased)
-
-### Features Added
-
-### Breaking Changes
-
-### Bugs Fixed
-
-### Other Changes
+## 1.0.0b2 (2022-10-11)
 
 ## 1.0.0b1 (2022-09-06)
 
--- sdk/maps/azure-maps-search/tests/test_maps_search_client.py
+++ sdk/maps/azure-maps-search/tests/test_maps_search_client.py
@@ -1,49 +1,25 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
 import os
-import sys
-import pytest
-
-from devtools_testutils import AzureTestCase
-from azure_devtools.scenario_tests import RecordingProcessor
-from azure.core.credentials import AzureKeyCredential
+from azure.core.credentials import AccessToken, AzureKeyCredential
 from azure.maps.search import MapsSearchClient
 from azure.maps.search.models import StructuredAddress
-
-
-# cSpell:disable
-class HeaderReplacer(RecordingProcessor):
-    def __init__(self):
-        self.headers = []
-
-    def register_header(self, header_name, new_val):
-        self.headers.append((header_name, new_val))
-
-    def process_request(self, request):
-        for header_name, new_val in self.headers:
-            for key in request.headers.keys():
-                if key.lower() == header_name.lower():
-                    request.headers[key] = new_val
-                    break
-        return request
-
+from devtools_testutils import AzureRecordedTestCase, recorded_by_proxy
+from search_preparer import MapsSearchPreparer
 
 # cSpell:disable
-class AzureMapsSearchClientE2ETest(AzureTestCase):
-    def __init__(self, *args, **kwargs):
-        super(AzureMapsSearchClientE2ETest, self).__init__(*args, **kwargs)
-        header_replacer = HeaderReplacer()
-        header_replacer.register_header("subscription-key", "<RealSubscriptionKey>")
-        header_replacer.register_header("x-ms-client-id", "<RealClientId>")
-        self.recording_processors.append(header_replacer)
-
-    def setUp(self):
-        super(AzureMapsSearchClientE2ETest, self).setUp()
+class TestMapsSearchClient(AzureRecordedTestCase):
+    def setup_method(self, method):
         self.client = MapsSearchClient(
-            client_id=self.get_settings_value('CLIENT_ID'),
-            credential=AzureKeyCredential(self.get_settings_value('SUBSCRIPTION_KEY')),
+            credential=AzureKeyCredential(os.environ.get('AZURE_SUBSCRIPTION_KEY', "AzureMapsSubscriptionKey"))
         )
         assert self.client is not None
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_fuzzy_search_poi_coordinates(self):
         result = self.client.fuzzy_search("Taipei 101", coordinates=(25.0338053, 121.5640089))
         assert len(result.results) > 0
@@ -55,14 +31,16 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.address.country_code_iso3 == "TWN"
         assert top_answer.position.lat > 25 and top_answer.position.lon > 121
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_fuzzy_search_poi_country_set(self):
         result = self.client.fuzzy_search("Taipei 101", country_filter=["TW"])
         assert len(result.results) > 0
         for item in result.results:
             assert item.address.country_code_iso3 == "TWN"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_fuzzy_search_address(self):
         result = self.client.fuzzy_search("No. 221, Sec. 2, Zhishan Rd., Shilin Dist., Taipei City 111, Taiwan (R.O.C.)")
         assert len(result.results) > 0
@@ -73,12 +51,14 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.address.postal_code == "111"
         assert top_answer.address.country_code_iso3 == "TWN"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_fuzzy_search_top(self):
         result = self.client.fuzzy_search("Taiwan High Speed Rail", top=5)
         assert len(result.results) == 5
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_fuzzy_search_skip(self):
         result = self.client.fuzzy_search("Taiwan High Speed Rail")
         assert len(result.results) > 0
@@ -87,7 +67,8 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         result2 = self.client.fuzzy_search("Taiwan High Speed Rail", skip=result.num_results)
         assert len(result2.results) > 0 and result2.results[0] != result.results[0]
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_fuzzy_search_multiple_results(self):
         result = self.client.fuzzy_search("Taiwan High Speed Rail")
         assert len(result.results) > 0
@@ -96,28 +77,32 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         result2 = self.client.fuzzy_search("Taiwan High Speed Rail", skip=result.num_results)
         assert len(result2.results) > 0 and result2.results[0] != result.results[0]
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_search_point_of_interest(self):
         result = self.client.search_point_of_interest("Taipei")
         assert len(result.results) > 0
         for item in result.results:
             assert item.type == "POI"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_search_address(self):
         result = self.client.search_address("Taipei")
         assert len(result.results) > 0
         for item in result.results:
             assert item.type != "POI"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_search_nearby_point_of_interest(self):
         result = self.client.search_nearby_point_of_interest(coordinates=(25.0338053, 121.5640089))
         assert len(result.results) > 0
         for item in result.results:
             assert item.type == "POI"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_search_point_of_interest_category(self):
         result = self.client.search_point_of_interest_category(
             "RESTAURANT", coordinates=(25.0338053, 121.5640089)
@@ -127,7 +112,8 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
             assert item.type == "POI"
             assert "RESTAURANT" in [category.code for category in item.point_of_interest.classifications]
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_search_structured_address(self):
         addr = StructuredAddress(street_number="221",
                                  street_name="Sec. 2, Zhishan Rd.",
@@ -145,7 +131,8 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.address.postal_code == "111"
         assert top_answer.address.country_code_iso3 == "TWN"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_get_geometries(self):
         result = self.client.get_geometries(geometry_ids=["8bceafe8-3d98-4445-b29b-fd81d3e9adf5"])
         assert len(result) > 0
@@ -154,14 +141,16 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.geometry_data.type == "FeatureCollection"
         assert len(top_answer.geometry_data.features) > 0
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_get_point_of_interest_categories(self):
         result = self.client.get_point_of_interest_categories()
         assert len(result) > 0
         assert result[0].name == 'Sports Center'
         assert result[0].id == 7320
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_reverse_search_address(self):
         result = self.client.reverse_search_address(coordinates=(25.0338053, 121.5640089))
         assert len(result.results) > 0
@@ -170,7 +159,8 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.address.postal_code == "110"
         assert top_answer.address.country_code_iso3 == "TWN"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_reverse_search_cross_street_address(self):
         result = self.client.reverse_search_cross_street_address(coordinates=(25.0338053, 121.5640089))
         assert len(result.addresses) > 0
@@ -178,7 +168,8 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.address.postal_code == "110"
         assert top_answer.address.country_code_iso3 == "TWN"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_search_inside_geometry(self):
         geo_json_obj = {
             'type': 'Polygon',
@@ -199,7 +190,8 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.address.country_subdivision == "CA"
         assert top_answer.address.local_name == "San Francisco"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_search_along_route(self):
         route_obj = {
             "route": {
@@ -223,7 +215,8 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.address.country_subdivision == "WA"
         assert top_answer.address.country_code == "US"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def test_search_address(self):
         result = self.client.search_address(query="15127 NE 24th Street, Redmond, WA 98052")
         assert len(result.results) > 0
@@ -232,7 +225,8 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.address.country_subdivision == "WA"
         assert top_answer.address.country_code == "US"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def fuzzy_search_batch(self):
         result = self.client.fuzzy_search_batch(
             search_queries=[
@@ -245,7 +239,8 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         assert top_answer.response.results[0].address.country_subdivision == "NY"
         assert top_answer.response.results[0].address.country_code == "US"
 
-    @pytest.mark.live_test_only
+    @MapsSearchPreparer()
+    @recorded_by_proxy
     def search_address_batch(self):
         result = self.client.search_address_batch(
             search_queries=[
@@ -257,10 +252,3 @@ class AzureMapsSearchClientE2ETest(AzureTestCase):
         top_answer = result.items[0]
         assert top_answer.response.results[0].address.country_subdivision == "NY"
         assert top_answer.response.results[0].address.country_code == "US"
-
-if __name__ == "__main__" :
-    testArgs = [ "-v" , "-s" ] if len(sys.argv) == 1 else sys.argv[1:]
-
-    pytest.main(args=testArgs)
-
-    print("main() Leave")
