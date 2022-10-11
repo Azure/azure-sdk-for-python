4872496ee54f846183b54c0301ef05ea49f12e3a
PythonSdkPipelines <PythonSdkPipelines>
2022-10-11 02:41:45 +0000

    code and test

--- sdk/authorization/azure-mgmt-authorization/azure/mgmt/authorization/models.py
+++ sdk/authorization/azure-mgmt-authorization/azure/mgmt/authorization/models.py
@@ -5,3 +5,8 @@
 # license information.
 # --------------------------------------------------------------------------
 from .v2015_07_01.models import *
+from .v2019_08_01_preview.models import *
+from .v2020_10_01.models import *
+from .v2021_01_01_preview.models import *
+from .v2021_12_01_preview.models import *
+from .v2022_04_01.models import *
--- sdk/authorization/azure-mgmt-authorization/azure/mgmt/authorization/v2018_05_01_preview/models/__init__.py
+++ sdk/authorization/azure-mgmt-authorization/azure/mgmt/authorization/v2018_05_01_preview/models/__init__.py
@@ -6,94 +6,76 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-try:
-    from ._models_py3 import AccessReviewDecision
-    from ._models_py3 import AccessReviewDecisionListResult
-    from ._models_py3 import AccessReviewDecisionProperties
-    from ._models_py3 import AccessReviewDecisionTarget
-    from ._models_py3 import AccessReviewDefaultSettings
-    from ._models_py3 import AccessReviewInstance
-    from ._models_py3 import AccessReviewInstanceListResult
-    from ._models_py3 import AccessReviewReviewer
-    from ._models_py3 import AccessReviewScheduleDefinition
-    from ._models_py3 import AccessReviewScheduleDefinitionListResult
-    from ._models_py3 import AccessReviewScheduleDefinitionProperties
-    from ._models_py3 import AccessReviewScheduleSettings
-    from ._models_py3 import ErrorDefinition
-    from ._models_py3 import ErrorDefinitionProperties
-    from ._models_py3 import Operation
-    from ._models_py3 import OperationDisplay
-    from ._models_py3 import OperationListResult
-    from ._models_py3 import ServicePrincipalDecisionTarget
-    from ._models_py3 import UserDecisionTarget
-except (SyntaxError, ImportError):
-    from ._models import AccessReviewDecision  # type: ignore
-    from ._models import AccessReviewDecisionListResult  # type: ignore
-    from ._models import AccessReviewDecisionProperties  # type: ignore
-    from ._models import AccessReviewDecisionTarget  # type: ignore
-    from ._models import AccessReviewDefaultSettings  # type: ignore
-    from ._models import AccessReviewInstance  # type: ignore
-    from ._models import AccessReviewInstanceListResult  # type: ignore
-    from ._models import AccessReviewReviewer  # type: ignore
-    from ._models import AccessReviewScheduleDefinition  # type: ignore
-    from ._models import AccessReviewScheduleDefinitionListResult  # type: ignore
-    from ._models import AccessReviewScheduleDefinitionProperties  # type: ignore
-    from ._models import AccessReviewScheduleSettings  # type: ignore
-    from ._models import ErrorDefinition  # type: ignore
-    from ._models import ErrorDefinitionProperties  # type: ignore
-    from ._models import Operation  # type: ignore
-    from ._models import OperationDisplay  # type: ignore
-    from ._models import OperationListResult  # type: ignore
-    from ._models import ServicePrincipalDecisionTarget  # type: ignore
-    from ._models import UserDecisionTarget  # type: ignore
+from ._models_py3 import AccessReviewDecision
+from ._models_py3 import AccessReviewDecisionListResult
+from ._models_py3 import AccessReviewDecisionProperties
+from ._models_py3 import AccessReviewDecisionTarget
+from ._models_py3 import AccessReviewDefaultSettings
+from ._models_py3 import AccessReviewInstance
+from ._models_py3 import AccessReviewInstanceListResult
+from ._models_py3 import AccessReviewReviewer
+from ._models_py3 import AccessReviewScheduleDefinition
+from ._models_py3 import AccessReviewScheduleDefinitionListResult
+from ._models_py3 import AccessReviewScheduleDefinitionProperties
+from ._models_py3 import AccessReviewScheduleSettings
+from ._models_py3 import ErrorDefinition
+from ._models_py3 import ErrorDefinitionProperties
+from ._models_py3 import Operation
+from ._models_py3 import OperationDisplay
+from ._models_py3 import OperationListResult
+from ._models_py3 import ServicePrincipalDecisionTarget
+from ._models_py3 import UserDecisionTarget
 
-from ._authorization_management_client_enums import (
-    AccessRecommendationType,
-    AccessReviewActorIdentityType,
-    AccessReviewApplyResult,
-    AccessReviewInstanceStatus,
-    AccessReviewRecurrencePatternType,
-    AccessReviewRecurrenceRangeType,
-    AccessReviewResult,
-    AccessReviewReviewerType,
-    AccessReviewScheduleDefinitionReviewersType,
-    AccessReviewScheduleDefinitionStatus,
-    AccessReviewScopePrincipalType,
-    DecisionTargetType,
-    DefaultDecisionType,
-)
+from ._authorization_management_client_enums import AccessRecommendationType
+from ._authorization_management_client_enums import AccessReviewActorIdentityType
+from ._authorization_management_client_enums import AccessReviewApplyResult
+from ._authorization_management_client_enums import AccessReviewInstanceStatus
+from ._authorization_management_client_enums import AccessReviewRecurrencePatternType
+from ._authorization_management_client_enums import AccessReviewRecurrenceRangeType
+from ._authorization_management_client_enums import AccessReviewResult
+from ._authorization_management_client_enums import AccessReviewReviewerType
+from ._authorization_management_client_enums import AccessReviewScheduleDefinitionReviewersType
+from ._authorization_management_client_enums import AccessReviewScheduleDefinitionStatus
+from ._authorization_management_client_enums import AccessReviewScopePrincipalType
+from ._authorization_management_client_enums import DecisionTargetType
+from ._authorization_management_client_enums import DefaultDecisionType
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    'AccessReviewDecision',
-    'AccessReviewDecisionListResult',
-    'AccessReviewDecisionProperties',
-    'AccessReviewDecisionTarget',
-    'AccessReviewDefaultSettings',
-    'AccessReviewInstance',
-    'AccessReviewInstanceListResult',
-    'AccessReviewReviewer',
-    'AccessReviewScheduleDefinition',
-    'AccessReviewScheduleDefinitionListResult',
-    'AccessReviewScheduleDefinitionProperties',
-    'AccessReviewScheduleSettings',
-    'ErrorDefinition',
-    'ErrorDefinitionProperties',
-    'Operation',
-    'OperationDisplay',
-    'OperationListResult',
-    'ServicePrincipalDecisionTarget',
-    'UserDecisionTarget',
-    'AccessRecommendationType',
-    'AccessReviewActorIdentityType',
-    'AccessReviewApplyResult',
-    'AccessReviewInstanceStatus',
-    'AccessReviewRecurrencePatternType',
-    'AccessReviewRecurrenceRangeType',
-    'AccessReviewResult',
-    'AccessReviewReviewerType',
-    'AccessReviewScheduleDefinitionReviewersType',
-    'AccessReviewScheduleDefinitionStatus',
-    'AccessReviewScopePrincipalType',
-    'DecisionTargetType',
-    'DefaultDecisionType',
+    "AccessReviewDecision",
+    "AccessReviewDecisionListResult",
+    "AccessReviewDecisionProperties",
+    "AccessReviewDecisionTarget",
+    "AccessReviewDefaultSettings",
+    "AccessReviewInstance",
+    "AccessReviewInstanceListResult",
+    "AccessReviewReviewer",
+    "AccessReviewScheduleDefinition",
+    "AccessReviewScheduleDefinitionListResult",
+    "AccessReviewScheduleDefinitionProperties",
+    "AccessReviewScheduleSettings",
+    "ErrorDefinition",
+    "ErrorDefinitionProperties",
+    "Operation",
+    "OperationDisplay",
+    "OperationListResult",
+    "ServicePrincipalDecisionTarget",
+    "UserDecisionTarget",
+    "AccessRecommendationType",
+    "AccessReviewActorIdentityType",
+    "AccessReviewApplyResult",
+    "AccessReviewInstanceStatus",
+    "AccessReviewRecurrencePatternType",
+    "AccessReviewRecurrenceRangeType",
+    "AccessReviewResult",
+    "AccessReviewReviewerType",
+    "AccessReviewScheduleDefinitionReviewersType",
+    "AccessReviewScheduleDefinitionStatus",
+    "AccessReviewScopePrincipalType",
+    "DecisionTargetType",
+    "DefaultDecisionType",
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
