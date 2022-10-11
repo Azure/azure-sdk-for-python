25940293269094fcdcc44a494e4296348f7d1fc6
Vincent Tran <vincenttran@microsoft.com>
2022-10-11 16:09:03 -0700

    Delete and initial script run

--- sdk/storage/azure-storage-file-share/tests/test_file_service_properties_async.py
+++ sdk/storage/azure-storage-file-share/tests/test_file_service_properties_async.py
@@ -53,39 +53,41 @@ class FileServicePropertiesTest(AsyncStorageTestCase):
     # --Helpers-----------------------------------------------------------------
     def _assert_metrics_equal(self, metrics1, metrics2):
         if metrics1 is None or metrics2 is None:
-            self.assertEqual(metrics1, metrics2)
+            assert metrics1 == metrics2
             return
 
-        self.assertEqual(metrics1.version, metrics2.version)
-        self.assertEqual(metrics1.enabled, metrics2.enabled)
-        self.assertEqual(metrics1.include_apis, metrics2.include_apis)
+        assert metrics1.version == metrics2.version
+        assert metrics1.enabled == metrics2.enabled
+        assert metrics1.include_apis == metrics2.include_apis
         self._assert_retention_equal(metrics1.retention_policy, metrics2.retention_policy)
 
     def _assert_cors_equal(self, cors1, cors2):
         if cors1 is None or cors2 is None:
-            self.assertEqual(cors1, cors2)
+            assert cors1 == cors2
             return
 
-        self.assertEqual(len(cors1), len(cors2))
+        assert len(cors1) == len(cors2)
 
         for i in range(0, len(cors1)):
             rule1 = cors1[i]
             rule2 = cors2[i]
-            self.assertEqual(len(rule1.allowed_origins), len(rule2.allowed_origins))
-            self.assertEqual(len(rule1.allowed_methods), len(rule2.allowed_methods))
-            self.assertEqual(rule1.max_age_in_seconds, rule2.max_age_in_seconds)
-            self.assertEqual(len(rule1.exposed_headers), len(rule2.exposed_headers))
-            self.assertEqual(len(rule1.allowed_headers), len(rule2.allowed_headers))
+            assert len(rule1.allowed_origins) == len(rule2.allowed_origins)
+            assert len(rule1.allowed_methods) == len(rule2.allowed_methods)
+            assert rule1.max_age_in_seconds == rule2.max_age_in_seconds
+            assert len(rule1.exposed_headers) == len(rule2.exposed_headers)
+            assert len(rule1.allowed_headers) == len(rule2.allowed_headers)
 
     def _assert_retention_equal(self, ret1, ret2):
-        self.assertEqual(ret1.enabled, ret2.enabled)
-        self.assertEqual(ret1.days, ret2.days)
+        assert ret1.enabled == ret2.enabled
+        assert ret1.days == ret2.days
 
     # --Test cases per service ---------------------------------------
     @pytest.mark.playback_test_only
     @FileSharePreparer()
-    @AsyncStorageTestCase.await_prepared_test
-    async def test_file_service_properties_async(self, storage_account_name, storage_account_key):
+    async def test_file_service_properties(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+
         self._setup(storage_account_name, storage_account_key)
         protocol_properties1 = ShareProtocolSettings(smb=ShareSmbSettings(multichannel=SmbMultichannel(enabled=False)))
         protocol_properties2 = ShareProtocolSettings(smb=ShareSmbSettings(multichannel=SmbMultichannel(enabled=True)))
@@ -95,30 +97,32 @@ class FileServicePropertiesTest(AsyncStorageTestCase):
             hour_metrics=Metrics(), minute_metrics=Metrics(), cors=list(), protocol=protocol_properties1)
 
         # Assert
-        self.assertIsNone(resp)
+        assert resp is None
         props = await self.fsc.get_service_properties()
         self._assert_metrics_equal(props['hour_metrics'], Metrics())
         self._assert_metrics_equal(props['minute_metrics'], Metrics())
         self._assert_cors_equal(props['cors'], list())
-        self.assertEqual(props['protocol'].smb.multichannel.enabled, False)
+        assert props['protocol'].smb.multichannel.enabled == False
         # Assert
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ShareProtocolSettings(smb=ShareSmbSettings(multichannel=SmbMultichannel()))
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ShareProtocolSettings(smb=ShareSmbSettings())
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ShareProtocolSettings()
 
         # Act
         await self.fsc.set_service_properties(
             hour_metrics=Metrics(), minute_metrics=Metrics(), cors=list(), protocol=protocol_properties2)
         props = await self.fsc.get_service_properties()
-        self.assertEqual(props['protocol'].smb.multichannel.enabled, True)
+        assert props['protocol'].smb.multichannel.enabled == True
 
     # --Test cases per feature ---------------------------------------
     @FileSharePreparer()
-    @AsyncStorageTestCase.await_prepared_test
-    async def test_set_hour_metrics_async(self, storage_account_name, storage_account_key):
+    async def test_set_hour_metrics(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+
         self._setup(storage_account_name, storage_account_key)
         hour_metrics = Metrics(enabled=True, include_apis=True, retention_policy=RetentionPolicy(enabled=True, days=5))
 
@@ -130,8 +134,10 @@ class FileServicePropertiesTest(AsyncStorageTestCase):
         self._assert_metrics_equal(received_props['hour_metrics'], hour_metrics)
 
     @FileSharePreparer()
-    @AsyncStorageTestCase.await_prepared_test
-    async def test_set_minute_metrics_async(self, storage_account_name, storage_account_key):
+    async def test_set_minute_metrics(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+
         self._setup(storage_account_name, storage_account_key)
         minute_metrics = Metrics(enabled=True, include_apis=True,
                                  retention_policy=RetentionPolicy(enabled=True, days=5))
@@ -144,8 +150,10 @@ class FileServicePropertiesTest(AsyncStorageTestCase):
         self._assert_metrics_equal(received_props['minute_metrics'], minute_metrics)
 
     @FileSharePreparer()
-    @AsyncStorageTestCase.await_prepared_test
-    async def test_set_cors_async(self, storage_account_name, storage_account_key):
+    async def test_set_cors(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+
         self._setup(storage_account_name, storage_account_key)
         cors_rule1 = CorsRule(['www.xyz.com'], ['GET'])
 
@@ -174,13 +182,15 @@ class FileServicePropertiesTest(AsyncStorageTestCase):
 
 
     @FileSharePreparer()
-    @AsyncStorageTestCase.await_prepared_test
-    async def test_too_many_cors_rules_async(self, storage_account_name, storage_account_key):
+    async def test_too_many_cors_rules(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+
         self._setup(storage_account_name, storage_account_key)
         cors = []
         for i in range(0, 6):
             cors.append(CorsRule(['www.xyz.com'], ['GET']))
 
         # Assert
-        with self.assertRaises(HttpResponseError):
+        with pytest.raises(HttpResponseError):
             await self.fsc.set_service_properties(None, None, cors)
