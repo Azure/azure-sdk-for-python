691228cc50cb5c934219eabc90514c966435eb9e
Olena Stoliarova <ostoliarova@microsoft.com>
2022-10-17 17:01:45 +0300

    pylint warnings fix for sms

--- sdk/communication/azure-communication-sms/azure/communication/sms/_sms_client.py
+++ sdk/communication/azure-communication-sms/azure/communication/sms/_sms_client.py
@@ -16,8 +16,9 @@ from azure.communication.sms._models import SmsSendResult
 from ._generated._azure_communication_sms_service import AzureCommunicationSMSService
 from ._shared.utils import parse_connection_str, get_authentication_policy, get_current_utc_time
 from ._version import SDK_MONIKER
+from ._api_versions import DEFAULT_VERSION
 
-class SmsClient(object): # pylint: disable=client-accepts-api-version-keyword
+class SmsClient(object):
     """A client to interact with the AzureCommunicationService Sms gateway.
 
     This client provides operations to send an SMS via a phone number.
@@ -26,6 +27,9 @@ class SmsClient(object): # pylint: disable=client-accepts-api-version-keyword
         The endpoint url for Azure Communication Service resource.
     :param TokenCredential credential:
         The TokenCredential we use to authenticate against the service.
+    :keyword api_version: Azure Communication SMS API version.
+        The default value is "2021-03-07". Note that overriding this default value may result in unsupported behavior.
+    :paramtype api_version: str
     """
     def __init__(
             self, endpoint, # type: str
@@ -44,9 +48,11 @@ class SmsClient(object): # pylint: disable=client-accepts-api-version-keyword
                 "invalid credential from connection string.")
 
         self._endpoint = endpoint
+        self._api_version = kwargs.pop("api_version", DEFAULT_VERSION)
         self._authentication_policy = get_authentication_policy(endpoint, credential)
         self._sms_service_client = AzureCommunicationSMSService(
             self._endpoint,
+            api_version=self._api_version,
             authentication_policy=self._authentication_policy,
             sdk_moniker=SDK_MONIKER,
             **kwargs)
