b74955bb377d5a2d4963c45d6d08112088dade1e
Hanchi Wang <hancwang@microsoft.com>
2022-10-17 15:15:11 -0700

    Pull from main

--- sdk/ml/azure-ai-ml/azure/ai/ml/_restclient/v2022_10_01_preview/aio/operations/_registries_operations.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_restclient/v2022_10_01_preview/aio/operations/_registries_operations.py
@@ -441,7 +441,7 @@ class RegistriesOperations:
         )
         response = pipeline_response.http_response
 
-        if response.status_code not in [200, 201]:
+        if response.status_code not in [200, 201, 202]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
             raise HttpResponseError(response=response, error_format=ARMErrorFormat)
 
@@ -449,6 +449,9 @@ class RegistriesOperations:
             deserialized = self._deserialize('Registry', pipeline_response)
 
         if response.status_code == 201:
+            deserialized = self._deserialize('Registry', pipeline_response)
+
+        if response.status_code == 202:
             deserialized = self._deserialize('Registry', pipeline_response)
 
         if cls:
--- sdk/ml/azure-ai-ml/azure/ai/ml/_restclient/v2022_10_01_preview/operations/_registries_operations.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_restclient/v2022_10_01_preview/operations/_registries_operations.py
@@ -683,7 +683,7 @@ class RegistriesOperations(object):
         )
         response = pipeline_response.http_response
 
-        if response.status_code not in [200, 201]:
+        if response.status_code not in [200, 201, 202]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
             raise HttpResponseError(response=response, error_format=ARMErrorFormat)
 
@@ -691,6 +691,9 @@ class RegistriesOperations(object):
             deserialized = self._deserialize('Registry', pipeline_response)
 
         if response.status_code == 201:
+            deserialized = self._deserialize('Registry', pipeline_response)
+
+        if response.status_code == 202:
             deserialized = self._deserialize('Registry', pipeline_response)
 
         if cls:
