bd47f7a087c80bb33325bacbd7790b7957542301
Chris Miller <chrismiller@microsoft.com>
2022-10-17 13:30:43 -0400

    Add extra sanitizers, fix readme casing

--- sdk/devcenter/azure-developer-devcenter/tests/conftest.py
+++ sdk/devcenter/azure-developer-devcenter/tests/conftest.py
@@ -4,10 +4,12 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from devtools_testutils import test_proxy
+from devtools_testutils import test_proxy, add_body_key_sanitizer
 import pytest
 
 # autouse=True will trigger this fixture on each pytest run, even if it's not explicitly used by a test method
 @pytest.fixture(scope="session", autouse=True)
 def start_proxy(test_proxy):
+    add_body_key_sanitizer(json_path="$..id_token", value="Sanitized")
+    add_body_key_sanitizer(json_path="$..client_info", value="Sanitized")
     return
