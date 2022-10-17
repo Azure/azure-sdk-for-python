25703ec6513014d8065b87f64f11f441a4d39310
vincenttran-msft <101599632+vincenttran-msft@users.noreply.github.com>
2022-10-17 15:38:59 -0700

    [Storage] Fix failing pipeline tests (`test_share.py`) + `name_starts_with` async bug fix (#26793)
    * Add playback only tag to failing live tests
    
    * Refactored to hopefully get rid of conflicts
    
    * Tests should pass now, fix prefix bug
    
    * Re-record test to fix CI

--- sdk/storage/azure-storage-file-share/tests/test_share.py
+++ sdk/storage/azure-storage-file-share/tests/test_share.py
@@ -798,12 +798,12 @@ class TestStorageShare(StorageRecordedTestCase):
         storage_account_key = kwargs.pop("storage_account_key")
 
         self._setup(storage_account_name, storage_account_key)
-        share = self._create_share('random1')
+        share = self._create_share('random2')
         snapshot1 = share.create_snapshot()
         snapshot2 = share.create_snapshot()
 
         # Act
-        shares = self.fsc.list_shares(include_snapshots=True)
+        shares = self.fsc.list_shares(name_starts_with=share.share_name, include_snapshots=True)
         # Assert
         assert shares is not None
         all_shares = list(shares)
@@ -821,8 +821,8 @@ class TestStorageShare(StorageRecordedTestCase):
         storage_account_key = kwargs.pop("storage_account_key")
 
         self._setup(storage_account_name, storage_account_key)
-        share = self._get_share_reference()
-        share.create_share()
+        self._create_share('othershare')
+        share = self._create_share('random2')
 
         # Act
         shares = list(self.fsc.list_shares(name_starts_with=share.share_name))
@@ -1052,6 +1052,7 @@ class TestStorageShare(StorageRecordedTestCase):
         assert share2_props.root_squash == ShareRootSquash.root_squash
         assert share2_props.protocols == ['NFS']
 
+    @pytest.mark.playback_test_only
     @FileSharePreparer()
     @recorded_by_proxy
     def test_list_shares_with_root_squash_and_protocols(self, **kwargs):
