d96a017f219dc7a8838d2f86d55bc98d8fe65fe9
Xiang Yan <xiangsjtu@gmail.com>
2022-10-19 16:35:01 -0700

    Get latest main (#26934)
    * Change default local storage location (#26891)
    
    * code and test (#26908)
    
    Co-authored-by: PythonSdkPipelines <PythonSdkPipelines>
    
    * Update swagger_to_sdk_config_dpg.json (#26913)
    
    * feat: enable internal components in pipeline yaml (#26800)
    
    * feat: enable internal components in pipeline yaml
    
    * refactor: add some gate logic
    
    * fix: fix test_pipeline_job_create_with_registries
    
    * fix: fix pylint
    
    * Cadl automation pipeline (#26912)
    
    * cdoe
    
    * fix
    
    * code
    
    * fix
    
    * code
    
    * meta.json
    
    * meta.json
    
    * compatible
    
    * refactor: divide pipeline related tests (#26886)
    
    * fix: sdk.ml.azure-ai-ml.tests.datastore.e2etests.test_datastore
    
    * feat: component with default label
    
    * refactor: divide test_dsl_pipeline.py
    
    * feat: load labelled arm id
    
    * feat: support name@default
    use name@label for node component in pipeline yaml
    
    * [TA ] improve per-document error message when all documents fail per action (#26902)
    
    * fix bad Document error message when all actions fail
    
    * update changelog
    
    * update wording
    
    * skip cosmos emulator (#26930)
    
    * try 3.7
    
    * skip cosmos emulator tests until fixed
    
    * revert change
    
    * comment out emulator
    
    * fix ci for core (#26923)
    
    * fix ci for core
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * adding additional version override support
    
    * update
    
    * update
    
    * update
    
    * update conda test-requirements to include azure-mgmt-storage that was previously coming dependency tools/azure-sdk-tools
    
    Co-authored-by: scbedd <45376673+scbedd@users.noreply.github.com>
    
    Co-authored-by: Leighton Chen <lechen@microsoft.com>
    Co-authored-by: Azure SDK Bot <53356347+azure-sdk@users.noreply.github.com>
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>
    Co-authored-by: Xingzhi Zhang <37076709+elliotzh@users.noreply.github.com>
    Co-authored-by: Krista Pratico <krpratic@microsoft.com>
    Co-authored-by: scbedd <45376673+scbedd@users.noreply.github.com>

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/pipeline/_load_component.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/pipeline/_load_component.py
@@ -21,6 +21,7 @@ from azure.ai.ml.entities._builders.condition_node import ConditionNode
 from azure.ai.ml.entities._builders.do_while import DoWhile
 from azure.ai.ml.entities._builders.pipeline import Pipeline
 from azure.ai.ml.entities._component.component import Component
+from azure.ai.ml.entities._component.component_factory import component_factory
 from azure.ai.ml.entities._job.automl.automl_job import AutoMLJob
 from azure.ai.ml.entities._util import extract_label
 from azure.ai.ml.exceptions import ErrorCategory, ErrorTarget, ValidationException
@@ -179,7 +180,19 @@ class _PipelineNodeFactory:
         else:
             data[CommonYamlFields.TYPE] = _type
 
-        new_instance = self.get_create_instance_func(_type)()
+        new_instance: Union[BaseNode, AutoMLJob] = self.get_create_instance_func(_type)()
+
+        if isinstance(new_instance, BaseNode):
+            # parse component
+            component_key = new_instance._get_component_attr_name()
+            if component_key in data and isinstance(data[component_key], dict):
+                data[component_key] = component_factory.load_from_dict(
+                    data=data[component_key],
+                    context={
+                        BASE_PATH_CONTEXT_KEY: data[component_key].get(BASE_PATH_CONTEXT_KEY, None),
+                    }
+                )
+
         new_instance.__init__(**data)
         return new_instance
 
--- sdk/ml/azure-ai-ml/tests/pipeline_job/e2etests/test_pipeline_job.py
+++ sdk/ml/azure-ai-ml/tests/pipeline_job/e2etests/test_pipeline_job.py
@@ -1542,6 +1542,17 @@ class TestPipelineJob(AzureRecordedTestCase):
         # assert pipeline_dict["outputs"] == {"output_path": {"mode": "ReadWriteMount", "job_output_type": "uri_folder"}}
         assert pipeline_dict["settings"] == {"default_compute": "cpu-cluster", "_source": "REMOTE.WORKSPACE.COMPONENT"}
 
+    def test_pipeline_node_with_default_component(self, client: MLClient, randstr: Callable[[str], str]):
+        params_override = [{"name": randstr("job_name")}]
+        pipeline_job = load_job(
+            "./tests/test_configs/pipeline_jobs/helloworld_pipeline_job_with_default_component.yml",
+            params_override=params_override,
+        )
+
+        created_pipeline_job = client.jobs.create_or_update(pipeline_job)
+        assert created_pipeline_job.jobs["hello_world_component"].component == \
+               "microsoftsamples_command_component_basic@default"
+
 
 @pytest.mark.usefixtures(
     "recorded_test",
