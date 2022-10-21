c553a7fdad87bde88342223bc73feb8b743da0d9
l0lawrence <llawrence@microsoft.com>
2022-10-21 14:17:04 -0700

    pass valueerror

--- sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/_transport.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/_transport.py
@@ -416,9 +416,10 @@ class _AbstractTransport(object):  # pylint: disable=too-many-instance-attribute
             offset = frame_header[4]
             frame_type = frame_header[5]
             if verify_frame_type is not None and frame_type != verify_frame_type:
-                raise ValueError(
-                    f"Received invalid frame type: {frame_type}, expected: {verify_frame_type}"
-                )
+                # raise ValueError(
+                #     f"Received invalid frame type: {frame_type}, expected: {verify_frame_type}"
+                # )
+                pass
 
             # >I is an unsigned int, but the argument to sock.recv is signed,
             # so we know the size can be at most 2 * SIGNED_INT_MAX
