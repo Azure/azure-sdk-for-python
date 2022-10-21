657238222c754009ad8b38d3027596828e74ff72
l0lawrence <llawrence@microsoft.com>
2022-10-21 10:32:15 -0700

    try this websocketapp

--- sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/_transport.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/_transport.py
@@ -705,13 +705,13 @@ class WebSocketTransport(_AbstractTransport):
             # )
             self.ws = WebSocketApp(
                 url="wss://{}".format(self._custom_endpoint or self._host),
-                subprotocols=[AMQP_WS_SUBPROTOCOL],
-                timeout=self._connect_timeout,
-                skip_utf8_validation=True,
-                sslopt=self.sslopts,
-                http_proxy_host=http_proxy_host,
-                http_proxy_port=http_proxy_port,
-                http_proxy_auth=http_proxy_auth,
+                # subprotocols=[AMQP_WS_SUBPROTOCOL],
+                # timeout=self._connect_timeout,
+                # skip_utf8_validation=True,
+                # sslopt=self.sslopts,
+                # http_proxy_host=http_proxy_host,
+                # http_proxy_port=http_proxy_port,
+                # http_proxy_auth=http_proxy_auth,
                 on_close=self.on_close
                 )
         except ImportError:
