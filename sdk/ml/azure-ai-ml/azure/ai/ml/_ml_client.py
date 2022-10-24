7cde41ea315d104d7df4d477aa15a1a20f476a56
Joshua Loeffler <jloeffler@microsoft.com>
2022-10-24 09:47:16 -0400

    Fix syntax issue

--- sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
@@ -312,7 +312,7 @@ class MLClient(object):
 
         self._workspace_connections = WorkspaceConnectionsOperations(
             self._operation_scope,
-            self._operation_config,_rp_service_client
+            self._operation_config,
             self._rp_service_client_2022_01_01_preview,
             self._operation_container,
             self._credential,
