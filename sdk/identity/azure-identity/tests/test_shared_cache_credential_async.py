d4f1f11dd8a2a0ef2bc6ef4748af95eac2ff68e8
Paul Van Eck <paulvaneck@microsoft.com>
2022-10-25 10:19:59 -0700

    [Identity] Fix issue with cache option usage
    If a user supplies `TokenCachePersistenceOptions` to
    a `SharedTokenCacheCredential`, these options are not
    used when the cache is loaded. This can lead to issues
    when users are trying to use caches with custom names
    since the default name is used instead.
    
    This commit ensures that user-provided cache options
    are propagated when the cache is loaded.
    
    Ref: #26982
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/identity/azure-identity/tests/test_shared_cache_credential_async.py
+++ sdk/identity/azure-identity/tests/test_shared_cache_credential_async.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 
 from azure.core.exceptions import ClientAuthenticationError
 from azure.core.pipeline.policies import SansIOHTTPPolicy
-from azure.identity import CredentialUnavailableError
+from azure.identity import CredentialUnavailableError, TokenCachePersistenceOptions
 from azure.identity.aio import SharedTokenCacheCredential
 from azure.identity._constants import EnvironmentVariables
 from azure.identity._internal.shared_token_cache import (
@@ -619,6 +619,19 @@ async def test_initialization():
             with pytest.raises(CredentialUnavailableError, match="Shared token cache unavailable"):
                 await credential.get_token("scope")
             assert mock_cache_loader.call_count == 1
+
+
+@pytest.mark.asyncio
+async def test_initialization_with_cache_options():
+    """the credential should use user-supplied persistence options"""
+
+    with patch("azure.identity._internal.shared_token_cache._load_persistent_cache") as mock_cache_loader:
+        options = TokenCachePersistenceOptions(name="foo.cache")
+        credential = SharedTokenCacheCredential(cache_persistence_options=options)
+
+        with pytest.raises(CredentialUnavailableError):
+            await credential.get_token("scope")
+        mock_cache_loader.assert_called_once_with(options)
 
 
 @pytest.mark.asyncio
