392c0bb45766e1dcd121e365d8fc2155ad31daeb
scbedd <45376673+scbedd@users.noreply.github.com>
2022-10-24 18:31:48 -0700

    bump the partition count by quadruple

--- eng/common/scripts/Cosmos-Emulator.ps1
+++ eng/common/scripts/Cosmos-Emulator.ps1
@@ -94,7 +94,7 @@ if ($Stage -eq "Launch")
       $argumentList += , $StartParameters
   } else {
     # Use the default params if none provided
-    $argumentList = "/noexplorer /noui /enablepreview /disableratelimiting /enableaadauthentication"
+    $argumentList = "/noexplorer /noui /enablepreview /disableratelimiting /enableaadauthentication /partitioncount=100"
   }
 
   Write-Host "Starting emulator process: $emulator $argumentList"
