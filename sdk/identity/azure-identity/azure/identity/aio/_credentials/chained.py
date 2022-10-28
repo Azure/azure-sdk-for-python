5e346fc24a6a5ba8466e808e32a230359870cf60
Xiang Yan <xiangyan@microsoft.com>
2022-10-28 13:43:38 -0700

    update

--- sdk/identity/azure-identity/azure/identity/aio/_credentials/chained.py
+++ sdk/identity/azure-identity/azure/identity/aio/_credentials/chained.py
@@ -4,15 +4,17 @@
 # ------------------------------------
 import asyncio
 import logging
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
 from azure.core.exceptions import ClientAuthenticationError
-from azure.core.credentials_async import AsyncTokenCredential
 from azure.core.credentials import AccessToken
 from .._internal import AsyncContextManager
 from ... import CredentialUnavailableError
 from ..._credentials.chained import _get_error_message
 from ..._internal import within_credential_chain
+
+if TYPE_CHECKING:
+    from azure.core.credentials_async import AsyncTokenCredential
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,7 +29,7 @@ class ChainedTokenCredential(AsyncContextManager):
     :type credentials: :class:`azure.core.credentials.AsyncTokenCredential`
     """
 
-    def __init__(self, *credentials: AsyncTokenCredential) -> None:
+    def __init__(self, *credentials: "AsyncTokenCredential") -> None:
         if not credentials:
             raise ValueError("at least one credential is required")
 
