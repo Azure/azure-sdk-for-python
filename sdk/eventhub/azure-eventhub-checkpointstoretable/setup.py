5b59bb1532020dc136da1008333963cc86d6a875
Kashif Khan <kashifkhan@microsoft.com>
2022-10-28 10:23:54 -0500

    py311 support

--- sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
@@ -51,6 +51,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         "License :: OSI Approved :: MIT License",
     ],
     packages=find_packages(
