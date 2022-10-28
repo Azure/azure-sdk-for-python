05380d9b348f63858ceeb56b2231ac42f07b3f5d
Laurent Mazuel <lmazuel@microsoft.com>
2022-10-28 11:30:30 -0700

    Fix type-extensions troubles

--- sdk/anomalydetector/azure-ai-anomalydetector/setup.py
+++ sdk/anomalydetector/azure-ai-anomalydetector/setup.py
@@ -64,7 +64,7 @@ setup(
     install_requires=[
         "isodate<1.0.0,>=0.6.1",
         "azure-core<2.0.0,>=1.24.0",
-        "typing_extensions>=4.3.0; python_version<'3.8.0'",
+        "typing-extensions>=4.3.0; python_version<'3.8.0'",
     ],
     python_requires=">=3.7",
 )
