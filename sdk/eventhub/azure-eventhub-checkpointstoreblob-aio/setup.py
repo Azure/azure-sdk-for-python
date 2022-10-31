0d2266fe21cf2b4e23d17d50866f6c454185fa46
Kashif Khan <kashifkhan@microsoft.com>
2022-10-31 14:11:36 -0500

    undo EH changes

--- sdk/eventhub/azure-eventhub-checkpointstoreblob-aio/setup.py
+++ sdk/eventhub/azure-eventhub-checkpointstoreblob-aio/setup.py
@@ -60,7 +60,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
     ],
     zip_safe=False,
--- sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
+++ sdk/eventhub/azure-eventhub-checkpointstoretable/setup.py
@@ -51,7 +51,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.10',
         "License :: OSI Approved :: MIT License",
     ],
     packages=find_packages(
