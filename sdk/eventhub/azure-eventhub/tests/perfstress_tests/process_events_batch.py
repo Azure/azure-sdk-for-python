443caca354bc2f80a91f698c9853463919b2c6c4
antisch <antisch@microsoft.com>
2022-11-02 20:20:41 +1300

    Removed print statements

--- sdk/eventhub/azure-eventhub/tests/perfstress_tests/process_events_batch.py
+++ sdk/eventhub/azure-eventhub/tests/perfstress_tests/process_events_batch.py
@@ -28,7 +28,7 @@ class ProcessEventsBatchTest(_EventHubProcessorTest):
                             pass
                 
                 # Consume properties and body.
-                _ = [print(e.properties, e.body) for e in events]
+                _ = [(e.properties, e.body) for e in events]
 
                 if self.args.checkpoint_interval:
                     self._partition_event_count[partition_context.partition_id] += len(events)
@@ -53,7 +53,7 @@ class ProcessEventsBatchTest(_EventHubProcessorTest):
                             pass
 
                 # Consume properties and body.
-                _ = [print(e.properties, e.body) for e in events]
+                _ = [(e.properties, e.body) for e in events]
 
                 if self.args.checkpoint_interval:
                     self._partition_event_count[partition_context.partition_id] += len(events)
