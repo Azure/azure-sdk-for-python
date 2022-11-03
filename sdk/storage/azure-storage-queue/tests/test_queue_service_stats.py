d64d59dd5649e2ee89046417d9ae248815b92bc4
Vincent Tran <vincenttran@microsoft.com>
2022-11-03 16:21:15 -0700

    Done

--- sdk/storage/azure-storage-queue/tests/test_queue_service_stats.py
+++ sdk/storage/azure-storage-queue/tests/test_queue_service_stats.py
@@ -6,23 +6,15 @@
 import unittest
 
 import pytest
-
 from azure.storage.queue import QueueServiceClient
 
 from devtools_testutils import recorded_by_proxy
 from devtools_testutils.storage import StorageRecordedTestCase
 from settings.testcase import QueuePreparer
 
-SERVICE_UNAVAILABLE_RESP_BODY = '<?xml version="1.0" encoding="utf-8"?><StorageServiceStats><GeoReplication><Status' \
-                                '>unavailable</Status><LastSyncTime></LastSyncTime></GeoReplication' \
-                                '></StorageServiceStats> '
-
-SERVICE_LIVE_RESP_BODY = '<?xml version="1.0" encoding="utf-8"?><StorageServiceStats><GeoReplication><Status' \
-                                '>live</Status><LastSyncTime>Wed, 19 Jan 2021 22:28:43 GMT</LastSyncTime></GeoReplication' \
-                                '></StorageServiceStats> '
-
 # --Test Class -----------------------------------------------------------------
 class TestQueueServiceStats(StorageRecordedTestCase):
+
     # --Helpers-----------------------------------------------------------------
     def _assert_stats_default(self, stats):
         assert stats is not None
@@ -38,17 +30,8 @@ class TestQueueServiceStats(StorageRecordedTestCase):
         assert stats['geo_replication']['status'] == 'unavailable'
         assert stats['geo_replication']['last_sync_time'] is None
 
-    @staticmethod
-    def override_response_body_with_unavailable_status(response):
-        response.http_response.text = lambda encoding=None: SERVICE_UNAVAILABLE_RESP_BODY
-
-    @staticmethod
-    def override_response_body_with_live_status(response):
-        response.http_response.text = lambda encoding=None: SERVICE_LIVE_RESP_BODY
-
     # --Test cases per service ---------------------------------------
-
-    @pytest.mark.live_test_only
+    @pytest.mark.playback_test_only
     @QueuePreparer()
     @recorded_by_proxy
     def test_queue_service_stats(self, **kwargs):
@@ -63,7 +46,7 @@ class TestQueueServiceStats(StorageRecordedTestCase):
         # Assert
         self._assert_stats_default(stats)
 
-    @pytest.mark.live_test_only
+    @pytest.mark.playback_test_only
     @QueuePreparer()
     @recorded_by_proxy
     def test_queue_service_stats_when_unavailable(self, **kwargs):
