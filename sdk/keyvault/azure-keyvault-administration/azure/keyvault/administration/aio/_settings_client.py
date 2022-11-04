515089b4d2035c7a4f64e0ac48afb73d58203b5d
mccoyp <mcpatino@microsoft.com>
2022-11-04 11:44:27 -0700

    We can't create settings

--- sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/aio/_settings_client.py
+++ sdk/keyvault/azure-keyvault-administration/azure/keyvault/administration/aio/_settings_client.py
@@ -7,7 +7,7 @@ from .._internal import AsyncKeyVaultClientBase
 from .._models import GetSettingsResult, KeyVaultSetting
 
 class KeyVaultSettingsClient(AsyncKeyVaultClientBase):
-    """Provides methods to create, update, get, and list settings for an Azure Key Vault.
+    """Provides methods to update, get, and list settings for an Azure Key Vault.
 
     :param str vault_url: URL of the vault on which the client will operate. This is also called the vault's "DNS Name".
         You should validate that this URL references a valid Key Vault or Managed HSM resource.
@@ -21,25 +21,6 @@ class KeyVaultSettingsClient(AsyncKeyVaultClientBase):
     :keyword bool verify_challenge_resource: Whether to verify the authentication challenge resource matches the Key
         Vault or Managed HSM domain. Defaults to True.
     """
-
-    async def set_setting(self, name: str, value: str, **kwargs) -> KeyVaultSetting:
-        """Creates or updates a given account setting with the provided value.
-
-        :param str name: The name of the account setting to create or update.
-        :param str value: The value to set.
-
-        :returns: The created or updated account setting, as a :class:`~azure.keyvault.administration.KeyVaultSetting`.
-        :rtype: ~azure.keyvault.administration.KeyVaultSetting
-        :raises: :class:`~azure.core.exceptions.HttpResponseError`
-        """
-        parameters = UpdateSettingsRequest(value=value)
-        result = await self._client.update_settings(
-            vault_base_url=self._vault_url,
-            setting_name=name,
-            parameters=parameters,
-            **kwargs
-        )
-        return KeyVaultSetting._from_generated(result)
 
     async def get_setting(self, name: str, **kwargs) -> KeyVaultSetting:
         """Gets the setting with the specified name.
@@ -62,3 +43,22 @@ class KeyVaultSettingsClient(AsyncKeyVaultClientBase):
         """
         result = await self._client.get_settings(vault_base_url=self._vault_url, *kwargs)
         return GetSettingsResult._from_generated(result)
+
+    async def update_setting(self, name: str, value: str, **kwargs) -> KeyVaultSetting:
+        """Updates a given account setting with the provided value.
+
+        :param str name: The name of the account setting to update.
+        :param str value: The value to set.
+
+        :returns: The updated account setting, as a :class:`~azure.keyvault.administration.KeyVaultSetting`.
+        :rtype: ~azure.keyvault.administration.KeyVaultSetting
+        :raises: :class:`~azure.core.exceptions.HttpResponseError`
+        """
+        parameters = UpdateSettingsRequest(value=value)
+        result = await self._client.update_settings(
+            vault_base_url=self._vault_url,
+            setting_name=name,
+            parameters=parameters,
+            **kwargs
+        )
+        return KeyVaultSetting._from_generated(result)
