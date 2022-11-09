9f0ac1e2e9105e0f5fbd5286bc8709ad62adbbf3
swathipil <swathip@microsoft.com>
2022-11-09 14:27:26 -0800

    update sync receive client ready flow

--- sdk/eventhub/azure-eventhub/azure/eventhub/_consumer.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_consumer.py
@@ -198,10 +198,12 @@ class EventHubConsumer(
             )
             self._handler = cast("ReceiveClient", self._handler)
             self._handler.open(connection=conn)
-            while not self._handler.client_ready():
-                time.sleep(0.05)
-            self.handler_ready = True
+            self.handler_ready = False
             self.running = True
+        
+        if not self.handler_ready:
+            if self._handler.client_ready():
+                self.handler_ready = True
 
         return self.handler_ready
 
