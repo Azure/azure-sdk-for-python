7f09863e79bdd346ca04ac99cf7f715f14f9820c
PythonSdkPipelines <PythonSdkPipelines>
2022-11-09 07:37:19 +0000

    code and test

--- sdk/desktopvirtualization/azure-mgmt-desktopvirtualization/azure/mgmt/desktopvirtualization/aio/__init__.py
+++ sdk/desktopvirtualization/azure-mgmt-desktopvirtualization/azure/mgmt/desktopvirtualization/aio/__init__.py
@@ -7,4 +7,17 @@
 # --------------------------------------------------------------------------
 
 from ._desktop_virtualization_api_client import DesktopVirtualizationAPIClient
-__all__ = ['DesktopVirtualizationAPIClient']
+
+try:
+    from ._patch import __all__ as _patch_all
+    from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+except ImportError:
+    _patch_all = []
+from ._patch import patch_sdk as _patch_sdk
+
+__all__ = [
+    "DesktopVirtualizationAPIClient",
+]
+__all__.extend([p for p in _patch_all if p not in __all__])
+
+_patch_sdk()
