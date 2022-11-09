260dabaa49f699fb863ed3a8dd856cec788c9ca1
Zhengfei Wang <wangzhengfei.chn@outlook.com>
2022-11-09 16:53:21 +0800

    fix: pylint error

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_component/component.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_component/component.py
@@ -1,7 +1,6 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
-import os
 import tempfile
 from contextlib import contextmanager
 from os import PathLike
