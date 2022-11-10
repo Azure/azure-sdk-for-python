d8ab3871cadb738cde16504cf8bf5f4a96765144
l0lawrence <llawrence@microsoft.com>
2022-11-10 13:45:09 -0800

    mock try 2

--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_websocket_exception.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_websocket_exception.py
@@ -1,12 +1,17 @@
 import unittest
+import pytest
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
 import aiohttp
-from mock import patch
 from azure.eventhub._pyamqp.aio._transport_async import WebSocketTransportAsync
 
 
 class WebsocketException(unittest.TestCase):
 
-    @patch('aiohttp.client_ws.ClientWebSocketResponse.receive_bytes')
+    @mock.patch('azure.eventhub._pyamqp.aio.transport_async.WebSocketTransportAsync.ws._receive_bytes')
     async def test_websocket_exception_async(self, receive_bytes):
         receive_bytes.raises(aiohttp.ClientOSError)
         web_transport = WebSocketTransportAsync("my_host")
