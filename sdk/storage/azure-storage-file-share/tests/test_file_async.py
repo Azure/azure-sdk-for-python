8181fd5bca74e25fd2aa2a5b02fde412886b26d0
Jacob Lauzon <jalauzon@microsoft.com>
2022-11-16 17:39:30 -0800

    CSpell fix

--- sdk/storage/azure-storage-file-share/tests/test_file_async.py
+++ sdk/storage/azure-storage-file-share/tests/test_file_async.py
@@ -231,6 +231,7 @@ class TestStorageFileAsync(AsyncStorageRecordedTestCase):
         storage_account_key = kwargs.pop("storage_account_key")
 
         self._setup(storage_account_name, storage_account_key)
+        # cspell:disable-next-line
         sas = '?sv=2015-04-05&st=2015-04-29T22%3A18%3A26Z&se=2015-04-30T02%3A23%3A26Z&sr=b&sp=rw&sip=168.1.5.60-168.1.5.70&spr=https&sig=Z%2FRHIX5Xcg0Mq2rqI3OlWTjEg2tYkboXr1P9ZUXDtkk%3D'
         file_client = ShareFileClient(
             self.account_url(storage_account_name, "file"),
