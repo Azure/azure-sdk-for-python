c7e9323138cd8724d56492b30634246f9da49dc4
scbedd <45376673+scbedd@users.noreply.github.com>
2022-11-18 13:14:01 -0800

    unzip working

--- eng/tox/tox_helper_tasks.py
+++ eng/tox/tox_helper_tasks.py
@@ -10,16 +10,14 @@
 import shutil
 import sys
 import logging
-import ast
 import os
-import textwrap
-import io
 import glob
 import zipfile
 import tarfile
 import fnmatch
 import subprocess
 import re
+import pdb
 
 from packaging.specifiers import SpecifierSet
 from pkg_resources import Requirement, parse_version
@@ -58,6 +56,7 @@ def unzip_sdist_to_directory(containing_folder: str) -> str:
         return unzip_file_to_directory(zips[0], containing_folder)
     else:
         tars = glob.glob(os.path.join(containing_folder, "*.tar.gz"))
+        pdb.set_trace()
         return unzip_file_to_directory(tars[0], containing_folder)
 
 
@@ -68,10 +67,13 @@ def unzip_file_to_directory(path_to_zip_file: str, extract_location: str) -> str
             extracted_dir = os.path.basename(os.path.splitext(path_to_zip_file)[0])
             return os.path.join(extract_location, extracted_dir)
     else:
-        with tarfile.open(path_to_zip_file, "r:gz") as tar_ref:
+        # .tar.gz -> .tar
+        with tarfile.open(path_to_zip_file) as tar_ref:
             tar_ref.extractall(extract_location)
-            extracted_dir = os.path.basename(os.path.splitext(path_to_zip_file)[0])
-            return os.path.join(extract_location, extracted_dir)
+            extracted_dir = os.path.basename(path_to_zip_file).replace(".tar.gz", "")
+            
+        
+        return os.path.join(extract_location, extracted_dir)
 
 
 def move_and_rename(source_location):
