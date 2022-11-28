6c26fd28e4164fc5e42d621c1c30cddabd3e433f
Xiang Yan <xiangyan@microsoft.com>
2022-11-28 13:04:40 -0800

    updates

--- sdk/search/azure-search-documents/setup.py
+++ sdk/search/azure-search-documents/setup.py
@@ -63,7 +63,6 @@ setup(
     python_requires=">=3.7",
     install_requires=[
         "azure-core<2.0.0,>=1.24.0",
-        "msrest>=0.6.21",
         "azure-common~=1.1",
         "isodate>=0.6.0",
     ],
