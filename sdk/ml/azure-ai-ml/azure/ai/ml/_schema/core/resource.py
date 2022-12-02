f7c628adb4ff1054722b69200f435c6f9b829f60
Xingzhi Zhang <37076709+elliotzh@users.noreply.github.com>
2022-12-02 13:58:36 +0800

    feat: validate component for node (#27660)
    * fix: validate component for node
    
    * refactor: do a general processing for all Resource
    
    * refactor: rebase local path field in _dump_for_validation
    
    * refactor: dump LocalPathField to absolute path
    
    * fix: parse fixed relative path in tests
    
    * fix: ci
    
    * fix: ci
    fix: test_load_pipeline_job_with_internal_components_as_node
    fix: test_spark_job_with_builder
    
    * refactor LocalPathField and make code load only

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/core/resource.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/core/resource.py
@@ -5,10 +5,12 @@
 # pylint: disable=unused-argument,no-self-use,protected-access
 
 import logging
+from pathlib import Path
 
-from marshmallow import fields, post_load
+from marshmallow import fields, post_load, pre_dump, post_dump
 
 from .schema import YamlFileSchema
+from ...constants._common import BASE_PATH_CONTEXT_KEY
 
 module_logger = logging.getLogger(__name__)
 
@@ -21,7 +23,7 @@ class ResourceSchema(YamlFileSchema):
 
     @post_load(pass_original=True)
     def pass_source_path(self, data, original, **kwargs):
-        path = self.resolve_path(original, base_path=self._previous_base_path)
+        path = self._resolve_path(original, base_path=self._previous_base_path)
         if path is not None:
             from ...entities import Resource
 
@@ -32,4 +34,21 @@ class ResourceSchema(YamlFileSchema):
                 # some resource will make dict into object in their post_load
                 # not sure if it's a better way to unify them
                 data._source_path = path
+        return data
+
+    @pre_dump
+    def update_base_path_pre_dump(self, data, **kwargs):
+        # inherit from parent if base_path is not set
+        if data.base_path:
+            cur_base_path: Path = Path(data.base_path).resolve()
+            if not cur_base_path.samefile(self.context[BASE_PATH_CONTEXT_KEY]):
+                self._previous_base_path = Path(self.context[BASE_PATH_CONTEXT_KEY])
+                self.context[BASE_PATH_CONTEXT_KEY] = cur_base_path
+        return data
+
+    @post_dump
+    def reset_base_path_post_dump(self, data, **kwargs):
+        if self._previous_base_path is not None:
+            # pop state
+            self.context[BASE_PATH_CONTEXT_KEY] = self._previous_base_path
         return data
