2853e537ef795d9fea0beb21499f5c14f6358258
Xiang Yan <xiangsjtu@gmail.com>
2022-12-05 09:45:09 -0800

    add wam support (#25944)
    * add wam support
    
    * Update sdk/identity/azure-identity/CHANGELOG.md
    
    Co-authored-by: McCoy Patiño <39780829+mccoyp@users.noreply.github.com>
    
    * Update sdk/identity/azure-identity/azure/identity/_credentials/certificate.py
    
    Co-authored-by: McCoy Patiño <39780829+mccoyp@users.noreply.github.com>
    
    * update
    
    * Update setup.py
    
    * Update shared_requirements.txt
    
    * Update client_secret.py
    
    * Update environment.py
    
    * update
    
    * update
    
    * Updates
    
    * update
    
    * Update sdk/identity/azure-identity/azure/identity/_credentials/browser.py
    
    Co-authored-by: Paul Van Eck <paulvaneck@microsoft.com>
    
    * Update sdk/identity/azure-identity/azure/identity/_credentials/user_password.py
    
    Co-authored-by: Paul Van Eck <paulvaneck@microsoft.com>
    
    * Update sdk/identity/azure-identity/setup.py
    
    * Update shared_requirements.txt
    
    Co-authored-by: McCoy Patiño <39780829+mccoyp@users.noreply.github.com>
    Co-authored-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/identity/azure-identity/azure/identity/_internal/msal_credentials.py
+++ sdk/identity/azure-identity/azure/identity/_internal/msal_credentials.py
@@ -22,6 +22,7 @@ class MsalCredential(object):
             client_credential: Union[str, Dict] = None,
             *,
             additionally_allowed_tenants: List[str] = None,
+            allow_broker: bool = None,
             **kwargs
     ) -> None:
         authority = kwargs.pop("authority", None)
@@ -34,6 +35,7 @@ class MsalCredential(object):
         self._client_applications = {}  # type: Dict[str, msal.ClientApplication]
         self._client_credential = client_credential
         self._client_id = client_id
+        self._allow_broker = allow_broker
         self._additionally_allowed_tenants = additionally_allowed_tenants or []
 
         self._cache = kwargs.pop("_cache", None)
@@ -76,7 +78,8 @@ class MsalCredential(object):
                 azure_region=self._regional_authority,
                 token_cache=self._cache,
                 http_client=self._client,
-                # validate_authority=self._validate_authority
+                # validate_authority=self._validate_authority,
+                allow_broker=self._allow_broker
             )
 
         return self._client_applications[tenant_id]
