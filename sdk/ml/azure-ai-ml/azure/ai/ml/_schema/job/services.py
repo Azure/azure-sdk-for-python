2fbbca845a4c46f69f456f26377ed0217d0a22a4
Njuguna Thande <nthande@microsoft.com>
2022-12-08 17:08:59 -0500

    Changed Job Services to an Enum

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/job/services.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/job/services.py
@@ -7,14 +7,28 @@ import logging
 from marshmallow import fields, post_load
 
 from azure.ai.ml.entities._job.job_service import JobService
+from azure.ai.ml.constants._job.job import JobServiceTypeNames
+from azure.ai.ml._schema.core.fields import StringTransformedEnum
 
 from ..core.schema import PathAwareSchema
 
 module_logger = logging.getLogger(__name__)
 
+EntityNames = JobServiceTypeNames.EntityNames()
+
 
 class JobServiceSchema(PathAwareSchema):
-    job_service_type = fields.Str()
+    job_service_type = StringTransformedEnum(
+        allowed_values=[
+            EntityNames.CUSTOM,
+            EntityNames.TRACKING,
+            EntityNames.STUDIO,
+            EntityNames.JUPYTER_LAB,
+            EntityNames.SSH,
+            EntityNames.TENSOR_BOARD,
+            EntityNames.VS_CODE,
+        ],
+    )
     port = fields.Int()
     endpoint = fields.Str(dump_only=True)
     status = fields.Str(dump_only=True)
