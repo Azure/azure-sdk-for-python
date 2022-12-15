3ec13bb77aacb5b8392d5d3c7594fca3b98c85c8
swathipil <swathip@microsoft.com>
2022-12-14 16:34:39 -0800

    rename websocket_exception test to websocket_exception_async

--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/asynctests/test_websocket_exception_async.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/asynctests/test_websocket_exception_async.py
@@ -8,7 +8,7 @@ from azure.eventhub._pyamqp.aio._transport_async import WebSocketTransportAsync
 
 # class WebsocketException(unittest.TestCase):
 @pytest.mark.asyncio
-async def test_websocket_aiohttp_exception():
+async def test_websocket_aiohttp_exception_async():
     with patch.object(aiohttp.ClientSession,'ws_connect', side_effect=aiohttp.ClientOSError):
         transport = WebSocketTransportAsync(host="my_host")
         with pytest.raises(ConnectionError):
