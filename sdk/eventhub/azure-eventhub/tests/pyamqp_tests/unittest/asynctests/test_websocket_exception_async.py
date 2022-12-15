77c5cbb24d25d1a728791f5f6fb81c2040a12b78
swathipil <76007337+swathipil@users.noreply.github.com>
2022-12-15 14:27:35 -0500

    [EventHubs] add asyncio marker to pyamqp test (#27954)
    * add asyncio marker to pyamqp test
    
    * rename websocket_exception test to websocket_exception_async
    
    * expect aio error

--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/asynctests/test_websocket_exception_async.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/asynctests/test_websocket_exception_async.py
@@ -7,8 +7,9 @@ from azure.eventhub._pyamqp.aio._transport_async import WebSocketTransportAsync
 
 
 # class WebsocketException(unittest.TestCase):
-async def test_websocket_aiohttp_exception():
+@pytest.mark.asyncio
+async def test_websocket_aiohttp_exception_async():
     with patch.object(aiohttp.ClientSession,'ws_connect', side_effect=aiohttp.ClientOSError):
         transport = WebSocketTransportAsync(host="my_host")
-        with pytest.raises(ConnectionError):
+        with pytest.raises(aiohttp.ClientOSError):
             await transport.connect()
