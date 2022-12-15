669eb99c82729ebacedc35d99b7c6f6fd1369251
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2022-12-15 10:52:32 -0800

    Update tests to use construct_local_and_remote_paths

--- sdk/ml/azure-ai-ml/tests/internal_utils/unittests/test_asset_utils.py
+++ sdk/ml/azure-ai-ml/tests/internal_utils/unittests/test_asset_utils.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from typing import Callable
 
 import pytest
 
@@ -10,7 +9,8 @@ from azure.ai.ml._utils._asset_utils import (
     IgnoreFile,
     get_ignore_file,
     get_object_hash,
-    traverse_directory,
+    get_local_paths,
+    construct_local_and_remote_paths,
 )
 from azure.ai.ml._utils.utils import convert_windows_path_to_unix
 
@@ -41,7 +41,7 @@ def gitignore_file(storage_test_directory: str) -> GitIgnoreFile:
 
 
 @pytest.fixture
-def no_ignore_file(no_ignore_file_directory: str) -> IgnoreFile:
+def no_ignore_file() -> IgnoreFile:
     return IgnoreFile(None)
 
 
@@ -75,6 +75,10 @@ def link_file_path(
 
     if os.path.exists(link_file):
         os.remove(link_file)
+
+@pytest.fixture
+def fake_remote_prefix() -> str:
+    return "LocalUpload/1234"
 
 
 @pytest.mark.unittest
@@ -114,34 +118,22 @@ class TestAssetUtils:
         no_ignore_file: IgnoreFile,
     ) -> None:
         source_path = Path(storage_test_directory).resolve()
-        prefix = source_path.name + "/"
 
         amlignore_upload_paths = []
         gitignore_upload_paths = []
         no_ignore_upload_paths = []
 
-        for root, _, files in os.walk(source_path, followlinks=True):
-            amlignore_upload_paths += list(
-                traverse_directory(root, files, source_path, prefix, ignore_file=amlignore_file)
-            )
-            gitignore_upload_paths += list(
-                traverse_directory(root, files, source_path, prefix, ignore_file=gitignore_file)
-            )
-            no_ignore_upload_paths += list(
-                traverse_directory(root, files, source_path, prefix, ignore_file=no_ignore_file)
-            )
+        amlignore_upload_paths, _ = get_local_paths(source_path, ignore_file=amlignore_file)
+        gitignore_upload_paths, _ = get_local_paths(source_path, ignore_file=gitignore_file)
+        no_ignore_upload_paths, _ = get_local_paths(source_path, ignore_file=no_ignore_file)
 
         assert len(no_ignore_upload_paths) == 7
         assert len(gitignore_upload_paths) == 6
         assert len(amlignore_upload_paths) == 4
 
-    def test_upload_paths_match(self, storage_test_directory: str) -> None:
+    def test_upload_paths_match(self, storage_test_directory: str, fake_remote_prefix: str) -> None:
         source_path = Path(storage_test_directory).resolve()
-        prefix = source_path.name + "/"
-        upload_paths = []
-
-        for root, _, files in os.walk(source_path, followlinks=True):
-            upload_paths += list(traverse_directory(root, files, source_path, prefix))
+        upload_paths = construct_local_and_remote_paths(source_path, dest=fake_remote_prefix)
 
         for local_path, remote_path in upload_paths:
             remote_path = remote_path.split("/", 2)[-1]  # strip LocalUpload/<asset id> prefix
@@ -159,21 +151,18 @@ class TestAssetUtils:
         assert original_hash != updated_hash
 
     def test_symlink_upload_paths(
-        self, storage_test_directory: str, target_file_path: os.PathLike, link_file_path: os.PathLike
+        self, storage_test_directory: str, target_file_path: os.PathLike, link_file_path: os.PathLike, fake_remote_prefix: str
     ) -> None:
-        """Confirm that symlink name is preserved for upload to storage, but that target file's path is uploaded
+        """Confirm that symlink name is preserved for upload to storage, but that target file's path is used for upload
 
-        e.g given a file ./dir/foo/bar.txt with a symlink ./other_dir/bar_link.txt, we want to upload the contents of ./dir/food/bar.txt at path ./other_dir/bar_link.txt in the remote storage.
+        e.g given a symlink "./other_dir/bar_link.txt" with target file "./dir/foo/bar.txt", we want to upload the
+        contents of "./dir/food/bar.txt" at path "LocalUpload/<artifact hash>/other_dir/bar_link.txt" in the remote storage.
         """
         source_path = Path(storage_test_directory).resolve()
-        prefix = source_path.name + "/"
-        upload_paths_list = []
+        upload_pairs = construct_local_and_remote_paths(source_path, dest=fake_remote_prefix)
 
-        for root, _, files in os.walk(source_path, followlinks=True):
-            upload_paths_list += list(traverse_directory(root, files, source_path, prefix))
-
-        local_paths = [i for i, _ in upload_paths_list]
-        remote_paths = [j for _, j in upload_paths_list]
+        local_paths = [i for i, _ in upload_pairs]
+        remote_paths = [j for _, j in upload_pairs]
 
         assert target_file_path in local_paths
-        assert any([rp in link_file_path for rp in remote_paths])  # remote file names are relative
+        assert any([rp.replace(fake_remote_prefix, "")  in link_file_path for rp in remote_paths])  # remote file names are relative so only what's after the prefix should match
