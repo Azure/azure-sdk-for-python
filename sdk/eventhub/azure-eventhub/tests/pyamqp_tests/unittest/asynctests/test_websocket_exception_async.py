ae55ea914f4bea6ca68951fffb8b797c5226fb0d
swathipil <swathip@microsoft.com>
2022-12-15 10:34:11 -0800

    expect aio error

--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/asynctests/test_websocket_exception_async.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/asynctests/test_websocket_exception_async.py
@@ -11,5 +11,5 @@ from azure.eventhub._pyamqp.aio._transport_async import WebSocketTransportAsync
 async def test_websocket_aiohttp_exception_async():
     with patch.object(aiohttp.ClientSession,'ws_connect', side_effect=aiohttp.ClientOSError):
         transport = WebSocketTransportAsync(host="my_host")
-        with pytest.raises(ConnectionError):
+        with pytest.raises(aiohttp.ClientOSError):
             await transport.connect()
