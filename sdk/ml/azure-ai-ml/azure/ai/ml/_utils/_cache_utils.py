afac105468b222827e24fc4d3113caa7699ff7b5
zhangxingzhi <zhangxingzhi@microsoft.com>
2022-12-27 13:31:19 +0800

    refactor: put off component hash calculation

--- sdk/ml/azure-ai-ml/azure/ai/ml/_utils/_cache_utils.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_utils/_cache_utils.py
@@ -4,7 +4,9 @@
 import hashlib
 import logging
 from collections import defaultdict
-from typing import Union, List, Dict
+from dataclasses import dataclass
+from functools import partial
+from typing import List, Dict, Optional
 
 from azure.ai.ml.constants._common import AzureMLResourceType
 from azure.ai.ml.entities import Component
@@ -13,35 +15,42 @@ from azure.ai.ml.entities._builders import BaseNode
 
 logger = logging.getLogger(__name__)
 
+_ANONYMOUS_HASH_PREFIX = "anonymous_component:"
+_YAML_SOURCE_PREFIX = "yaml_source:"
+
+
+@dataclass
+class _CacheContent:
+    component_ref: Component
+    in_memory_hash: Optional[str]
+    arm_id: Optional[str] = None
+
 
 class CachedNodeResolver(object):
     """Class to resolve component in nodes with cached component resolution results.
 
     This class is thread-safe if:
-    1) self._resolve_nodes and self._register_node_to_resolve are not called concurrently in the same thread
+    1) self._resolve_nodes is not called concurrently in the same main thread
     2) self._resolve_component is only called concurrently on independent components
-        a) we have used a component hash to deduplicate components to resolve;
+        a) we have used an in-memory component hash to deduplicate components to resolve first;
         b) nodes are registered & resolved layer by layer, so all child components are already resolved
           when we register a pipeline node;
-        c) potential shared dependencies like compute and data have been resolved before calling self._resolve_nodes.
+        c) potential shared dependencies like compute and data have been resolved before
+          calling self._resolve_component.
     """
-    _ANONYMOUS_HASH_PREFIX = "anonymous_component:"
-    _YAML_SOURCE_PREFIX = "yaml_source:"
 
     def __init__(self, resolver):
         self._resolver = resolver
-        self._component_resolution_cache: Dict[str, str] = {}
-        self._component_cache: Dict[str, Component] = {}
-        self._hash_of_components_to_resolve: List[str] = []
-        self._nodes_to_apply: Dict[str, List[BaseNode]] = defaultdict(list)
+        self._cache: Dict[str, _CacheContent] = {}
+        self._nodes_to_resolve: List[BaseNode] = []
 
-    def _resolve_component(self, component: Union[str, Component]) -> str:
-        """Resolve a component with self._resolver."""
-        return self._resolver(component, azureml_type=AzureMLResourceType.COMPONENT)
-
-    @classmethod
-    def _get_component_hash(cls, component: Component) -> str:
+    @staticmethod
+    def _get_in_memory_hash_for_component(component: Component) -> str:
         """Get a hash for a component."""
+        if not isinstance(component, Component):
+            # this shouldn't happen; handle it in case invalid call is made outside this class
+            raise ValueError(f"Component {component} is not a Component object.")
+
         # For components with code, its code will be an absolute path before uploaded to blob,
         # so we can use a mixture of its anonymous hash and its source path as its hash, in case
         # there are 2 components with same code but different ignore files
@@ -52,10 +61,95 @@ class CachedNodeResolver(object):
             object_hash = hashlib.sha256()
             object_hash.update(component._get_anonymous_hash().encode("utf-8"))  # pylint: disable=protected-access
             object_hash.update(component._source_path.encode("utf-8"))  # pylint: disable=protected-access
-            return cls._YAML_SOURCE_PREFIX + object_hash.hexdigest()
+            return _YAML_SOURCE_PREFIX + object_hash.hexdigest()
         # For components without code, like pipeline component, their dependencies have already
         # been resolved before calling this function, so we can use their anonymous hash directly
-        return cls._ANONYMOUS_HASH_PREFIX + component._get_anonymous_hash()  # pylint: disable=protected-access
+        return _ANONYMOUS_HASH_PREFIX + component._get_anonymous_hash()  # pylint: disable=protected-access
+
+    @staticmethod
+    def _resolve_cache_contents(cache_content_to_resolve: List[_CacheContent], resolver):
+        """Resolve all components to resolve and save the results in cache.
+        """
+        _components = list(map(lambda x: x.component_ref, cache_content_to_resolve))
+
+        # TODO: do concurrent resolution controlled by an environment variable here
+        # given deduplication has already been done, we can safely assume that there is no
+        # conflict in concurrent local cache access
+        # multiprocessing need to dump input objects before starting a new process, which will fail
+        # on _AttrDict for now, so put off the concurrent resolution
+        _arm_ids = map(partial(resolver, azureml_type=AzureMLResourceType.COMPONENT), _components)
+
+        for _cache_content, _arm_id in zip(cache_content_to_resolve, _arm_ids):
+            _cache_content.arm_id = _arm_id
+
+    def _prepare_items_to_resolve(self):
+        """Pop all nodes in self._nodes_to_resolve to prepare cache contents to resolve and nodes to resolve.
+        Nodes in self._nodes_to_resolve will be grouped by component hash and saved to a dict of list.
+        Distinct dependent components not in current cache will be saved to a list.
+
+        :return: a tuple of (dict of nodes to resolve, list of cache contents to resolve)
+        """
+        _components = list(map(lambda x: x._component, self._nodes_to_resolve))  # pylint: disable=protected-access
+        # we can do concurrent component in-memory hash calculation here
+        in_memory_component_hashes = map(self._get_in_memory_hash_for_component, _components)
+
+        dict_of_nodes_to_resolve = defaultdict(list)
+        cache_contents_to_resolve: List[_CacheContent] = []
+        for node, component_hash in zip(self._nodes_to_resolve, in_memory_component_hashes):
+            dict_of_nodes_to_resolve[component_hash].append(node)
+            if component_hash not in self._cache:
+                cache_content = _CacheContent(
+                    component_ref=node._component,  # pylint: disable=protected-access
+                    in_memory_hash=component_hash,
+                )
+                self._cache[component_hash] = cache_content
+                cache_contents_to_resolve.append(cache_content)
+        self._nodes_to_resolve.clear()
+        return dict_of_nodes_to_resolve, cache_contents_to_resolve
+
+    @classmethod
+    def _resolve_cache_contents_from_disk(
+        cls,
+        cache_contents_to_resolve: List[_CacheContent]
+    ) -> List[_CacheContent]:
+        """Check on-disk cache to resolve cache contents in cache_contents_to_resolve and return
+        unresolved cache contents.
+        """
+        # TODO: apply local cache controlled by an environment variable here
+        # Note that we should recalculate the hash based on code for local cache, as
+        # we can't assume that the code folder won't change among dependency resolution
+        return cache_contents_to_resolve[:]
+
+    def _fill_back_component_to_nodes(self, dict_of_nodes_to_resolve: Dict[str, List[BaseNode]]):
+        """Fill back resolved component to nodes."""
+        for component_hash, nodes in dict_of_nodes_to_resolve.items():
+            cache_content = self._cache[component_hash]
+            for node in nodes:
+                # hack: parallel.task.code won't be resolved for now, but parallel.task can be a reference
+                # to parallel._component.task, then it will change to an arm id after resolving parallel._component
+                # However, if we have avoided duplicate resolution with in-memory cache, such change won't happen,
+                # so we need to do it manually here
+                # The ideal solution should be done after PRS team decides how to handle parallel.task.code
+                from azure.ai.ml.entities import ParallelComponent, Parallel
+                resolved_component = cache_content.component_ref
+                if isinstance(node, Parallel) and isinstance(resolved_component, ParallelComponent):
+                    origin_component: ParallelComponent = node._component  # pylint: disable=protected-access
+                    if id(node.task) == id(origin_component.task):
+                        node.task.code = resolved_component.task.code
+
+                node._component = cache_content.arm_id  # pylint: disable=protected-access
+
+    def _resolve_nodes(self):
+        """Processing logic of self.resolve_nodes.
+        Should not be called in subgraph creation.
+        """
+        dict_of_nodes_to_resolve, cache_contents_to_resolve = self._prepare_items_to_resolve()
+
+        cache_contents_to_resolve = self._resolve_cache_contents_from_disk(cache_contents_to_resolve)
+
+        self._resolve_cache_contents(cache_contents_to_resolve, resolver=self._resolver)
+
+        self._fill_back_component_to_nodes(dict_of_nodes_to_resolve)
 
     def register_node_to_resolve(self, node: BaseNode):
         """Register a node with its component to resolve.
@@ -65,63 +159,22 @@ class CachedNodeResolver(object):
         # directly resolve node if the resolution involves no remote call
         # so that no node will be registered when create_or_update a subgraph
         if isinstance(component, str):
-            node._component = self._resolve_component(component)  # pylint: disable=protected-access
+            node._component = self._resolver(  # pylint: disable=protected-access
+                component,
+                azureml_type=AzureMLResourceType.COMPONENT
+            )
             return
         if component.id is not None:
             node._component = component.id  # pylint: disable=protected-access
             return
 
-        # 1 possible optimization is to save an id(component) -> component hash map here to
-        # avoid duplicate hash calculation. The risk is that we haven't implicitly forbidden component
-        # modification after it's been used in a node, and we can't guarantee that the hash is still
-        # valid after modification.
-        component_hash = self._get_component_hash(component)
-        if component_hash not in self._component_cache:
-            self._hash_of_components_to_resolve.append(component_hash)
-            self._component_cache[component_hash] = component
-
-        self._nodes_to_apply[component_hash].append(node)
-
-    def _resolve_components_without_in_memory_cache(self):
-        """Resolve all components to resolve and save the results in cache.
-        """
-        # TODO: apply local cache controlled by an environment variable here
-        # Note that we should recalculate the hash based on code for local cache, as
-        # we can't assume that the code folder won't change among dependency resolution
-        # TODO: do concurrent resolution controlled by an environment variable here
-        # given deduplication has already been done, we can safely assume that there is no
-        # conflict in concurrent local cache access
-        # multiprocessing need to dump input objects before starting a new process, which will fail
-        # on _AttrDict for now, so put off the concurrent resolution
-        for component_hash in self._hash_of_components_to_resolve:
-            self._component_resolution_cache[component_hash] = self._resolve_component(
-                self._component_cache[component_hash]
-            )
-
-        self._hash_of_components_to_resolve.clear()
+        self._nodes_to_resolve.append(node)
 
     def resolve_nodes(self):
         """Resolve all dependent components with resolver and set resolved component arm id back to newly
         registered nodes. Registered nodes will be cleared after resolution.
         """
-        if self._hash_of_components_to_resolve:
-            self._resolve_components_without_in_memory_cache()
+        if not self._nodes_to_resolve:
+            return
 
-        for component_hash, nodes in self._nodes_to_apply.items():
-            for node in nodes:
-                resolution_result = self._component_resolution_cache[component_hash]
-
-                # hack: parallel.task.code won't be resolved for now, but parallel.task can be a reference
-                # to parallel._component.task, then it will change to an arm id after resolving parallel._component
-                # However, if we have avoided duplicate resolution with in-memory cache, such change won't happen,
-                # so we need to do it manually here
-                # The ideal solution should be done after PRS team decides how to handle parallel.task.code
-                from azure.ai.ml.entities import ParallelComponent, Parallel
-                resolved_component = self._component_cache[component_hash]
-                if isinstance(node, Parallel) and isinstance(resolved_component, ParallelComponent):
-                    origin_component: ParallelComponent = node._component  # pylint: disable=protected-access
-                    if id(node.task) == id(origin_component.task):
-                        node.task.code = resolved_component.task.code
-
-                node._component = resolution_result  # pylint: disable=protected-access
-        self._nodes_to_apply.clear()
+        self._resolve_nodes()
