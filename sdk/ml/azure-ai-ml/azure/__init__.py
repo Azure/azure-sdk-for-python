422a3063607107fe4b128b8cabf0d3b54814a3a7
Harneet Virk <harnvir@microsoft.com>
2022-12-28 13:39:26 -0800

    Address the lint error

--- sdk/ml/azure-ai-ml/azure/__init__.py
+++ sdk/ml/azure-ai-ml/azure/__init__.py
@@ -5,7 +5,7 @@
 
 Main area includes the following:
 * SDK objects/entities are data classes describing AzureML concepts and configurations.
-* Operations are classes contain logic to interact with backend services, usually operations call auto generated with simple prep or error handling logic.
+* Operations are classes contain logic to interact with backend services, usually auto generated operations call.
 * Autorest objects.
 * SDK v2 support the new YAML file format.
 """
