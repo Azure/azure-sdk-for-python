a41d26a64626deafc07aaef1d2c00e820894ec00
PythonSdkPipelines <PythonSdkPipelines>
2022-12-29 03:38:13 +0000

    code and test

--- sdk/cognitiveservices/azure-mgmt-cognitiveservices/azure/mgmt/cognitiveservices/_version.py
+++ sdk/cognitiveservices/azure-mgmt-cognitiveservices/azure/mgmt/cognitiveservices/_version.py
@@ -6,4 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "13.3.0"
+VERSION = "13.4.0"
