4630bf9486d0a4951a92f5b507b4f0918e5de335
Laurent Mazuel <laurent.mazuel@gmail.com>
2022-12-30 17:19:56 -0800

    Blackening of core

--- sdk/core/azure-core/azure/core/pipeline/_tools.py
+++ sdk/core/azure-core/azure/core/pipeline/_tools.py
@@ -29,6 +29,7 @@ if TYPE_CHECKING:
     from typing import Any
     from azure.core.rest import HttpResponse as RestHttpResponse
 
+
 def await_result(func, *args, **kwargs):
     """If func returns an awaitable, raise that this runner can't handle it."""
     result = func(*args, **kwargs)
@@ -37,6 +38,7 @@ def await_result(func, *args, **kwargs):
             "Policy {} returned awaitable object in non-async pipeline.".format(func)
         )
     return result
+
 
 def is_rest(obj):
     # type: (Any) -> bool
@@ -48,6 +50,7 @@ def is_rest(obj):
     a check for is_stream_consumed, which is an exclusive property on our new responses.
     """
     return hasattr(obj, "is_stream_consumed") or hasattr(obj, "content")
+
 
 def handle_non_stream_rest_response(response):
     # type: (RestHttpResponse) -> None
