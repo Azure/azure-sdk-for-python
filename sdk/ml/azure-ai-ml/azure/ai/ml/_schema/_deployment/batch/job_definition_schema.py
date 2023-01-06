ad63f44e07304a02a276aa35e99fd7c3f98b0e74
Taji Harris <tajiharris@microsoft.com>
2023-01-05 16:27:37 -0800

    removed unused import in job schema

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_deployment/batch/job_definition_schema.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_deployment/batch/job_definition_schema.py
@@ -9,7 +9,7 @@ from typing import Any
 
 from marshmallow import fields, post_load
 
-from azure.ai.ml._schema import ArmVersionedStr, PatchedSchemaMeta, RegistryStr, StringTransformedEnum, UnionField, ArmStr
+from azure.ai.ml._schema import ArmVersionedStr, PatchedSchemaMeta, StringTransformedEnum, UnionField, ArmStr
 from azure.ai.ml._schema.pipeline.pipeline_component import PipelineComponentFileRefField
 from azure.ai.ml.constants._common import AzureMLResourceType
 from azure.ai.ml.constants._job.job import JobType
