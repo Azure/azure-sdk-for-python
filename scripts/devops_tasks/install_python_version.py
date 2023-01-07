ae5461f355585465a718926493bdbcca21bc00e1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 07:31:52 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/devops_tasks/install_python_version.py
+++ scripts/devops_tasks/install_python_version.py
@@ -71,7 +71,26 @@ def install_selected_python_version(installer_url, installer_folder):
 
     else:
         with tarfile.open(local_installer_ref) as tar_file:
-            tar_file.extractall(installer_folder)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar_file, installer_folder)
         try:
             check_call(UNIX_INSTALL_ARRAY, cwd=installer_folder)
         except CalledProcessError as err:
