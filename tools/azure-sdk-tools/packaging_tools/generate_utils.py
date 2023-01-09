275228f6a6ff6a585a511df73ebc8a7115f8ac0a
Yuchao Yan <yuchaoyan@microsoft.com>
2023-01-09 13:48:45 +0800

    update init new service

--- tools/azure-sdk-tools/packaging_tools/generate_utils.py
+++ tools/azure-sdk-tools/packaging_tools/generate_utils.py
@@ -35,21 +35,34 @@ def get_package_names(sdk_folder):
     return package_names
 
 
-def init_new_service(package_name, folder_name):
-    setup = Path(folder_name, package_name, "setup.py")
-    if not setup.exists():
-        check_call(
-            f"python -m packaging_tools --build-conf {package_name} -o {folder_name}",
-            shell=True,
+def init_new_service(package_name, folder_name, is_cadl = False):
+    if is_cadl:
+        output_path = Path(folder_name) / package_name
+        if not (output_path / "sdk_packaging.toml").exists():
+            with open(output_path / "sdk_packaging.toml", "w") as file_out:
+                file_out.write("[packaging]\nauto_update = false")
+
+        # add ci.yaml
+        generate_ci(
+            template_path=Path("scripts/quickstart_tooling_dpg/template_ci"),
+            folder_path=Path(folder_name),
+            package_name=package_name
         )
-        ci = Path(folder_name, "ci.yml")
-        if not ci.exists():
-            with open("ci_template.yml", "r") as file_in:
-                content = file_in.readlines()
-            name = package_name.replace("azure-", "").replace("mgmt-", "")
-            content = [line.replace("MyService", name) for line in content]
-            with open(str(ci), "w") as file_out:
-                file_out.writelines(content)
+    else:
+        setup = Path(folder_name, package_name, "setup.py")
+        if not setup.exists():
+            check_call(
+                f"python -m packaging_tools --build-conf {package_name} -o {folder_name}",
+                shell=True,
+            )
+            ci = Path(folder_name, "ci.yml")
+            if not ci.exists():
+                with open("ci_template.yml", "r") as file_in:
+                    content = file_in.readlines()
+                name = package_name.replace("azure-", "").replace("mgmt-", "")
+                content = [line.replace("MyService", name) for line in content]
+                with open(str(ci), "w") as file_out:
+                    file_out.writelines(content)
 
 
 def update_servicemetadata(sdk_folder, data, config, folder_name, package_name, spec_folder, input_readme):
@@ -348,23 +361,8 @@ def generate_ci(template_path: Path, folder_path: Path, package_name: str) -> No
         file_out.writelines(content)
 
 def gen_cadl(cadl_relative_path: str, spec_folder: str) -> Dict[str, Any]:
-    # update config file
     cadl_python = "@azure-tools/cadl-python"
     autorest_python = "@autorest/python"
-    # project_yaml_path = Path(spec_folder) / cadl_relative_path / "cadl-project.yaml"
-    # with open(project_yaml_path, "r") as file_in:
-    #     project_yaml = yaml.safe_load(file_in)
-    # if not project_yaml.get("emitters", {}).get(cadl_python):
-    #     return
-    # if not project_yaml["emitters"][cadl_python].get("sdk-folder"):
-    #     raise Exception("no sdk-folder is defined")
-    # output_path = Path(os.getcwd()) / project_yaml["emitters"][cadl_python]["sdk-folder"]
-    # if not output_path.exists():
-    #     os.makedirs(output_path)
-
-    # project_yaml["emitters"][cadl_python].pop("sdk-folder")
-    # with open(project_yaml_path, "w") as file_out:
-    #     yaml.safe_dump(project_yaml, file_out)
 
     # npm install tool
     origin_path = os.getcwd()
@@ -384,21 +382,11 @@ def gen_cadl(cadl_relative_path: str, spec_folder: str) -> Dict[str, Any]:
     # generate code
     cadl_file = "client.cadl" if Path("client.cadl").exists() else "."
     check_call(f"npx cadl compile {cadl_file} --emit {cadl_python} --arg \"python-sdk-folder={origin_path}\" ", shell=True)
-    # if not (output_path / "sdk_packaging.toml").exists():
-    #     with open(output_path / "sdk_packaging.toml", "w") as file_out:
-    #         file_out.write("[packaging]\nauto_update = false")
 
     # get version of codegen used in generation
     npm_package_verstion = get_npm_package_version(autorest_python)
 
     # return to original folder
     os.chdir(origin_path)
-
-    # add ci.yaml
-    generate_ci(
-        template_path=Path("scripts/quickstart_tooling_dpg/template_ci"),
-        folder_path=output_path.parent,
-        package_name=project_yaml["emitters"][cadl_python]["package-name"]
-    )
 
     return npm_package_verstion
