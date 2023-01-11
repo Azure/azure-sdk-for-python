921a7fd2df491a9088096b31085598ba38052c66
Zhengfei Wang <wangzhengfei.chn@outlook.com>
2023-01-11 14:15:09 +0800

    Revert "change test order to make sure "test_dsl_pipeline_component_cache_in_resolver" runs first"
    This reverts commit fbaac2ac41e3e9b234905a24ba5c0300754d7809.

--- sdk/ml/azure-ai-ml/tests/dsl/e2etests/test_dsl_pipeline_with_specific_nodes.py
+++ sdk/ml/azure-ai-ml/tests/dsl/e2etests/test_dsl_pipeline_with_specific_nodes.py
@@ -153,58 +153,6 @@ class TestDSLPipelineWithSpecificNodes(AzureRecordedTestCase):
 
         return pipeline_leaf
 
-    # this test should run first, or at least before those tests using same component(s);
-    # otherwise, call count assertion will break.
-    def test_dsl_pipeline_component_cache_in_resolver(self, client: MLClient) -> None:
-        input_data_path = "./tests/test_configs/data/"
-        pipeline_root = self._generate_multi_layer_pipeline_func()
-
-        _submit_and_cancel = partial(
-            submit_and_cancel_new_dsl_pipeline,
-            client=client,
-            job_in_path=Input(path=input_data_path)
-        )
-
-        def _mock_get_component_arm_id(_component: Component) -> str:
-            # the logic has no diff comparing to original function other than always using show_progress=False
-            # just to mock the function and check call information
-            if not _component.id:
-                _component._id = client.components.create_or_update(
-                    _component, is_anonymous=True, show_progress=False
-                ).id
-            return _component.id
-
-        with mock.patch.object(
-            OperationOrchestrator,
-            "_get_component_arm_id",
-            side_effect=_mock_get_component_arm_id
-        ) as mock_resolve:
-            _submit_and_cancel(pipeline_root)
-            # pipeline_leaf, pipeline_mid and 3 command components will be resolved
-            assert mock_resolve.call_count == 5
-
-        with mock.patch.object(
-            OperationOrchestrator,
-            "_get_component_arm_id",
-            side_effect=_mock_get_component_arm_id
-        ) as mock_resolve:
-            _submit_and_cancel(pipeline_root)
-            # no more requests to resolve components as local cache is hit
-            assert mock_resolve.call_count == 0
-
-        pipeline_job = pipeline_root(job_in_path=Input(path=input_data_path))
-        pipeline_job.settings.default_compute = "cpu-cluster"
-        leaf_subgraph = pipeline_job.jobs["pipeline_mid"].component.jobs["pipeline_leaf"].component
-        leaf_subgraph.jobs["another_component_name"].component.command += " & echo updated2"
-        with mock.patch.object(
-            OperationOrchestrator,
-            "_get_component_arm_id",
-            side_effect=_mock_get_component_arm_id
-        ) as mock_resolve:
-            assert_job_cancel(pipeline_job, client)
-            # updated command component and its parents (pipeline_leaf and pipeline_mid) will be resolved
-            assert mock_resolve.call_count == 3
-
     def test_dsl_pipeline_sweep_node(self, client: MLClient, randstr: Callable[[str], str]) -> None:
         yaml_file = "./tests/test_configs/components/helloworld_component.yml"
 
@@ -266,6 +214,56 @@ class TestDSLPipelineWithSpecificNodes(AzureRecordedTestCase):
         name, version = created_pipeline.jobs["sweep_job1"].trial.split(":")
         created_component = client.components.get(name, version)
         assert created_component.display_name == "sweep_job1"
+
+    def test_dsl_pipeline_component_cache_in_resolver(self, client: MLClient) -> None:
+        input_data_path = "./tests/test_configs/data/"
+        pipeline_root = self._generate_multi_layer_pipeline_func()
+
+        _submit_and_cancel = partial(
+            submit_and_cancel_new_dsl_pipeline,
+            client=client,
+            job_in_path=Input(path=input_data_path)
+        )
+
+        def _mock_get_component_arm_id(_component: Component) -> str:
+            # the logic has no diff comparing to original function other than always using show_progress=False
+            # just to mock the function and check call information
+            if not _component.id:
+                _component._id = client.components.create_or_update(
+                    _component, is_anonymous=True, show_progress=False
+                ).id
+            return _component.id
+
+        with mock.patch.object(
+            OperationOrchestrator,
+            "_get_component_arm_id",
+            side_effect=_mock_get_component_arm_id
+        ) as mock_resolve:
+            _submit_and_cancel(pipeline_root)
+            # pipeline_leaf, pipeline_mid and 3 command components will be resolved
+            assert mock_resolve.call_count == 5
+
+        with mock.patch.object(
+            OperationOrchestrator,
+            "_get_component_arm_id",
+            side_effect=_mock_get_component_arm_id
+        ) as mock_resolve:
+            _submit_and_cancel(pipeline_root)
+            # no more requests to resolve components as local cache is hit
+            assert mock_resolve.call_count == 0
+
+        pipeline_job = pipeline_root(job_in_path=Input(path=input_data_path))
+        pipeline_job.settings.default_compute = "cpu-cluster"
+        leaf_subgraph = pipeline_job.jobs["pipeline_mid"].component.jobs["pipeline_leaf"].component
+        leaf_subgraph.jobs["another_component_name"].component.command += " & echo updated2"
+        with mock.patch.object(
+            OperationOrchestrator,
+            "_get_component_arm_id",
+            side_effect=_mock_get_component_arm_id
+        ) as mock_resolve:
+            assert_job_cancel(pipeline_job, client)
+            # updated command component and its parents (pipeline_leaf and pipeline_mid) will be resolved
+            assert mock_resolve.call_count == 3
 
     def test_dsl_pipeline_concurrent_component_registration(self, client: MLClient, mocker: MockFixture) -> None:
         # disable on-disk cache to test concurrent component registration
