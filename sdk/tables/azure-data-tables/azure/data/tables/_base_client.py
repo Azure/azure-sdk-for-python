8b65781906bda9b3848f5a86c7c5f21687cc8888
YalinLi0312 <yall@microsoft.com>
2023-01-11 15:05:21 -0800

    WIP

--- sdk/tables/azure-data-tables/azure/data/tables/_base_client.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_base_client.py
@@ -62,7 +62,7 @@ _SUPPORTED_API_VERSIONS = ["2019-02-02", "2019-07-07", "2020-12-06"]
 _DEV_CONN_STRING = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1" # pylint: disable=line-too-long
 
 
-def get_api_version(kwargs: Dict[str, Any], default: Literal["2019-02-02"]) -> str:
+def get_api_version(kwargs: Dict[str, Any], default: str) -> str:
     api_version = kwargs.pop("api_version", None)
     if api_version and api_version not in _SUPPORTED_API_VERSIONS:
         versions = "\n".join(_SUPPORTED_API_VERSIONS)
