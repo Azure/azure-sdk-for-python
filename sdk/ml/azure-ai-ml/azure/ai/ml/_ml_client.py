b584e2c2e503e11152c7a682551b5bd79085d31d
Valerie Pham <valeriepham@microsoft.com>
2023-01-13 14:08:57 -0600

    Support list/show and create data asset in a registry

--- sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
@@ -385,7 +385,8 @@ class MLClient(object):
         self._data = DataOperations(
             self._operation_scope,
             self._operation_config,
-            self._service_client_10_2022,
+            self._service_client_10_2021_dataplanepreview
+            if registry_name else self._service_client_05_2022,
             self._datastores,
             requests_pipeline=self._requests_pipeline,
             **ops_kwargs,
