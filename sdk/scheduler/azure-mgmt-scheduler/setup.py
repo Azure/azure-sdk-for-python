08dbc26dcadc09d9785bc37c5e9dc63ceeaf6cba
scbedd <45376673+scbedd@users.noreply.github.com>
2023-01-18 11:43:32 -0800

    Revert "Packaging update of azure-mgmt-scheduler"
    This reverts commit 19d8d22760f4960e1e29a505bcd73025dca49f26.

--- sdk/scheduler/azure-mgmt-scheduler/setup.py
+++ sdk/scheduler/azure-mgmt-scheduler/setup.py
@@ -47,7 +47,7 @@ setup(
     url='https://github.com/Azure/azure-sdk-for-python',
     keywords="azure, azure sdk",  # update with search keywords relevant to the azure service / product
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 7 - Inactive',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
@@ -55,7 +55,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
     ],
     zip_safe=False,
@@ -70,10 +69,9 @@ setup(
         'pytyped': ['py.typed'],
     },
     install_requires=[
-        "msrest>=0.7.1",
-        "azure-common~=1.1",
-        "azure-mgmt-core>=1.3.2,<2.0.0",
-        "typing-extensions>=4.3.0; python_version<'3.8.0'",
+        'msrest>=0.7.1',
+        'azure-common~=1.1',
+        'azure-mgmt-core>=1.3.2,<2.0.0',
     ],
     python_requires=">=3.7"
 )
