153e8d57b0f6ce20230b8d5fd9455dcdd2cd73c0
Andrea Piccione <apiccione@microsoft.com>
2023-01-18 09:29:25 +0000

    Modify model classes

--- sdk/confidentialledger/azure-confidentialledger/azure/confidentialledger/receiptverification/_models.py
+++ sdk/confidentialledger/azure-confidentialledger/azure/confidentialledger/receiptverification/_models.py
@@ -4,13 +4,10 @@
 # ------------------------------------
 """Models for receipt verification."""
 
-from typing import List, Optional
-from azure.confidentialledger.receiptverification._serialization import (
-    Model,
-)
+from typing import Any, Dict, List, Optional, Union
 
 
-class ProofElement(Model):
+class ProofElement:
     """ProofElement represents the object contained in the `proof` list field
     of an Azure Confidential Ledger write transaction receipt. The `proof` list
     contains the hashes of the nodes in the Merkle Tree and their relative
@@ -20,85 +17,83 @@ class ProofElement(Model):
 
     Each ProofElement should contain either the `left` or the `right` field,
     but not both or none of them.
-
-    :ivar left: Hash of a left node in the Merkle Tree, as an hexadecimal string.
-    :vartype left: str
-    :ivar right: Hash of a right node in the Merkle Tree, as an hexadecimal string.
-    :vartype right: str
     """
 
-    _attribute_map = {
-        "left": {"key": "left", "type": "str"},
-        "right": {"key": "right", "type": "str"},
-    }
-
-    def __init__(
-        self, *, left: Optional[str] = None, right: Optional[str] = None, **kwargs
-    ):
+    def __init__(self, left: Optional[str] = None, right: Optional[str] = None):
         """
         :keyword left: Hash of a left node in the Merkle Tree, as an hexadecimal string.
-        :paramtype left: str
+        :paramtype left: Optional[str]
+
         :keyword right: Hash of a right node in the Merkle Tree, as an hexadecimal string.
-        :paramtype right: str
+        :paramtype right: Optional[str]
         """
-        super().__init__(**kwargs)
         self.left = left
         self.right = right
 
+    def __eq__(self, other):
+        if not isinstance(other, ProofElement):
+            return False
 
-class LeafComponents(Model):
+        return self.left == other.left and self.right == other.right
+
+    @classmethod
+    def from_dict(cls, proof_dict: Dict[str, Any]):
+        """Create a new instance of this class from a dictionary."""
+
+        return cls(
+            proof_dict.get("left", None),
+            proof_dict.get("right", None),
+        )
+
+
+class LeafComponents:
     """LeafComponents represents the object contained in the `leafComponents`
     field of an Azure Confidential Ledger write transaction receipt. The
     `leafComponents` field contains the elements that are hashed to compute
     the leaf node corresponding to the transaction associated to the given
     receipt.
-
-    :ivar claimsDigest: Hexadecimal string representing the digest of
-        the application claim attached by the Azure Confidential Ledger application.
-    :vartype claimsDigest: str
-    :ivar commitEvidence: A unique string that identifies a transaction / commit,
-        derived from the transaction ID and the secrets used by the Azure Confidential Ledger.
-    :vartype commitEvidence: str
-    :ivar writeSetDigest: Hexadecimal string representing the digest of the keys
-        and values written during a transaction, that captures the state of the
-        ledger at the time the transaction was committed.
-    :vartype writeSetDigest: str
     """
 
-    _validation = {
-        "claimsDigest": {"required": True},
-        "commitEvidence": {"required": True},
-        "writeSetDigest": {"required": True},
-    }
-
-    _attribute_map = {
-        "claimsDigest": {"key": "claimsDigest", "type": "str"},
-        "commitEvidence": {"key": "commitEvidence", "type": "str"},
-        "writeSetDigest": {"key": "writeSetDigest", "type": "str"},
-    }
-
-    def __init__(
-        self, *, claimsDigest: str, commitEvidence: str, writeSetDigest: str, **kwargs
-    ):
+    def __init__(self, claimsDigest: str, commitEvidence: str, writeSetDigest: str):
         """
         :keyword claimsDigest: Hexadecimal string representing the digest of
             the application claim attached by the Azure Confidential Ledger application.
         :paramtype claimsDigest: str
+
         :keyword commitEvidence: A unique string that identifies a transaction / commit,
             derived from the transaction ID and the secrets used by the Azure Confidential Ledger.
         :paramtype commitEvidence: str
+
         :keyword writeSetDigest: Hexadecimal string representing the digest of the keys
             and values written during a transaction, that captures the state of the
             ledger at the time the transaction was committed.
         :paramtype writeSetDigest: str
         """
-        super().__init__(**kwargs)
         self.claimsDigest = claimsDigest
         self.commitEvidence = commitEvidence
         self.writeSetDigest = writeSetDigest
 
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, LeafComponents):
+            return NotImplemented
+        return (
+            self.claimsDigest == other.claimsDigest
+            and self.commitEvidence == other.commitEvidence
+            and self.writeSetDigest == other.writeSetDigest
+        )
 
-class Receipt(Model):
+    @classmethod
+    def from_dict(cls, leaf_components_dict: Dict[str, Any]):
+        """Create a new instance of this class from a dictionary."""
+
+        return cls(
+            leaf_components_dict["claimsDigest"],
+            leaf_components_dict["commitEvidence"],
+            leaf_components_dict["writeSetDigest"],
+        )
+
+
+class Receipt:
     """Receipt represents the object contained in the `receipt` field of an
     Azure Confidential Ledger `get_receipt` response. A
     receipt is a cryptographic proof that a transaction has been committed to
@@ -108,81 +103,91 @@ class Receipt(Model):
     proofing). A receipt contains all the information needed to verify
     transaction inclusion and the verification can be done by applying an ad-
     hoc algorithm.
-
-    :ivar cert: PEM-encoded certificate string of the CCF node that signed the transaction.
-    :vartype cert: str
-    :ivar leafComponents: Components of the leaf node in the Merkle Tree associated to
-        the committed transaction.
-    :vartype leafComponents: ~azure.confidentialledgertools.receiptverification.models.LeafComponents
-    :ivar nodeId: Hexadecimal string representing the digest of the public key of the
-        CCF node that signed the transaction.
-    :vartype nodeId: str
-    :ivar proof: List of nodes' hashes to be used to re-compute the root of the Merkle Tree,
-        together with the leaf node hash, by iteratively concatenating and hashing the given values.
-    :vartype proof: list[~azure.confidentialledgertools.receiptverification.models.ProofElement]
-    :ivar serviceEndorsements: List of PEM-encoded certificates strings representing
-        previous service identities.
-    :vartype serviceEndorsements: list[str]
-    :ivar signature: Base64 string representing the signature of the root of the Merkle Tree at
-        the given transaction.
-    :vartype signature: str
     """
-
-    _validation = {
-        "cert": {"required": True},
-        "leafComponents": {"required": True},
-        "nodeId": {"required": True},
-        "proof": {"required": True},
-        "serviceEndorsements": {"required": False},
-        "signature": {"required": True},
-    }
-
-    _attribute_map = {
-        "cert": {"key": "cert", "type": "str"},
-        "leafComponents": {"key": "leafComponents", "type": "LeafComponents"},
-        "nodeId": {"key": "nodeId", "type": "str"},
-        "proof": {"key": "proof", "type": "[ProofElement]"},
-        "serviceEndorsements": {
-            "key": "serviceEndorsements",
-            "type": "[str]",
-        },
-        "signature": {"key": "signature", "type": "str"},
-    }
 
     def __init__(  # pylint: disable=dangerous-default-value
         self,
-        *,
         cert: str,
-        leafComponents: LeafComponents,
-        nodeId: str,
-        proof: List[ProofElement],
-        serviceEndorsements: List[str] = [],
+        leafComponents: Union[Dict[str, Any], LeafComponents],
+        proof: List[Union[Dict[str, Any], ProofElement]],
         signature: str,
-        **kwargs
+        nodeId: Optional[str] = None,
+        serviceEndorsements: Optional[List[str]] = [],
     ):
         """
         :keyword cert: PEM-encoded certificate string of the CCF node that signed the transaction.
         :paramtype cert: str
+
         :keyword leafComponents: Components of the leaf node in the Merkle Tree associated to
             the committed transaction.
-        :paramtype leafComponents: ~azure.confidentialledgertools.receiptverification.models.LeafComponents
-        :keyword nodeId: Hexadecimal string representing the digest of the public key of the
-            CCF node that signed the transaction.
-        :paramtype nodeId: str
+        :paramtype leafComponents: Dict[str, Any]
+
         :keyword proof: List of nodes' hashes to be used to re-compute the root of the Merkle Tree,
             together with the leaf node hash, by iteratively concatenating and hashing the given values.
-        :paramtype proof: list[~azure.confidentialledgertools.receiptverification.models.ProofElement]
-        :keyword serviceEndorsements: List of PEM-encoded certificates strings representing
-            previous service identities.
-        :paramtype serviceEndorsements: list[str]
+        :paramtype proof: List[Dict[str, Any]]
+
         :keyword signature: Base64 string representing the signature of the root of the Merkle Tree at
             the given transaction.
         :paramtype signature: str
+
+        :keyword nodeId: Hexadecimal string representing the digest of the public key of the
+            CCF node that signed the transaction.
+        :paramtype nodeId: Optional[str]
+
+        :keyword serviceEndorsements: List of PEM-encoded certificates strings representing
+            previous service identities.
+        :paramtype serviceEndorsements: Optional[List[str]]
         """
-        super().__init__(**kwargs)
         self.cert = cert
-        self.leafComponents = leafComponents
         self.nodeId = nodeId
-        self.proof = proof
         self.serviceEndorsements = serviceEndorsements
         self.signature = signature
+
+        if isinstance(leafComponents, LeafComponents):
+            self.leafComponents = leafComponents
+        else:
+            self.leafComponents = LeafComponents.from_dict(leafComponents)
+
+        self.proof = [
+            elem if isinstance(elem, ProofElement) else ProofElement.from_dict(elem)
+            for elem in proof
+        ]
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Receipt):
+            return NotImplemented
+
+        # print(self.cert == other.cert)
+        # print( self.nodeId == other.nodeId)
+        # print( self.serviceEndorsements == other.serviceEndorsements)
+        # print( self.signature == other.signature)
+        # print(self.leafComponents == other.leafComponents)
+
+        # for elem_self, elem_other in zip(self.proof, other.proof):
+        #     print(elem_self.left, elem_self.right)
+        #     print(elem_other.left, elem_other.right)
+
+        return (
+            self.cert == other.cert
+            and self.nodeId == other.nodeId
+            and self.serviceEndorsements == other.serviceEndorsements
+            and self.signature == other.signature
+            and self.leafComponents == other.leafComponents
+            and all(
+                elem_self == elem_other
+                for elem_self, elem_other in zip(self.proof, other.proof)
+            )
+        )
+
+    @classmethod
+    def from_dict(cls, receipt_dict: Dict[str, Any]):
+        """Create a new instance of this class from a dictionary."""
+
+        return cls(
+            cert=receipt_dict["cert"],
+            leafComponents=receipt_dict["leafComponents"],
+            proof=receipt_dict["proof"],
+            signature=receipt_dict["signature"],
+            nodeId=receipt_dict.get("nodeId", None),
+            serviceEndorsements=receipt_dict.get("serviceEndorsements", []),
+        )
