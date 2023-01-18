359e978a9cb06c006d40b44431fd4a4f1e954d5c
scbedd <45376673+scbedd@users.noreply.github.com>
2023-01-18 11:43:47 -0800

    Revert "Packaging update of azure-mgmt-regionmove"
    This reverts commit a65804eca8bead15109a3fe404ccb4f1faf73c0d.

--- sdk/regionmove/azure-mgmt-regionmove/setup.py
+++ sdk/regionmove/azure-mgmt-regionmove/setup.py
@@ -47,15 +47,15 @@ setup(
     url='https://github.com/Azure/azure-sdk-for-python',
     keywords="azure, azure sdk",  # update with search keywords relevant to the azure service / product
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 7 - Inactive',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
     ],
     zip_safe=False,
@@ -65,15 +65,10 @@ setup(
         'azure',
         'azure.mgmt',
     ]),
-    include_package_data=True,
-    package_data={
-        'pytyped': ['py.typed'],
-    },
     install_requires=[
-        "msrest>=0.7.1",
-        "azure-common~=1.1",
-        "azure-mgmt-core>=1.3.2,<2.0.0",
-        "typing-extensions>=4.3.0; python_version<'3.8.0'",
+        'msrest>=0.6.21',
+        'azure-common~=1.1',
+        'azure-mgmt-core>=1.3.0,<2.0.0',
     ],
-    python_requires=">=3.7"
+    python_requires=">=3.6"
 )
