206560d06cd9abdac305246ae8619ea6dd88da41
Andrea Piccione <apiccione@microsoft.com>
2023-01-18 09:31:52 +0000

    Adjust receipt verification tests based on latest changes

--- sdk/confidentialledger/azure-confidentialledger/tests/receiptverification/test_receipt_verification.py
+++ sdk/confidentialledger/azure-confidentialledger/tests/receiptverification/test_receipt_verification.py
@@ -7,27 +7,28 @@
 
 import pytest
 
-from azure.confidentialledger.receiptverification import (
+from azure.confidentialledger.receiptverification._receipt_verification import (
     verify_receipt,
+)
+
+from azure.confidentialledger.receiptverification._models import (
     LeafComponents,
     ProofElement,
 )
 
 from ._shared.constants import (
-    get_test_valid_receipt_2,
-    get_test_valid_service_certificate_2,
-    get_test_valid_service_certificate_1,
-    get_test_valid_receipt_1,
     get_test_valid_receipt_1_dict,
     get_test_valid_receipt_2_dict,
+    get_test_valid_service_certificate_2,
+    get_test_valid_service_certificate_1,
 )
 
 
 @pytest.mark.parametrize(
     "input_receipt,input_service_cert",
     [
-        [get_test_valid_receipt_1(), get_test_valid_service_certificate_1()],
-        [get_test_valid_receipt_2(), get_test_valid_service_certificate_2()],
+        [get_test_valid_receipt_1_dict(), get_test_valid_service_certificate_1()],
+        [get_test_valid_receipt_2_dict(), get_test_valid_service_certificate_2()],
     ],
 )
 def test_receipt_verification_with_valid_receipt_returns_successfully(
@@ -45,27 +46,6 @@ def test_receipt_verification_with_valid_receipt_returns_successfully(
 
 
 @pytest.mark.parametrize(
-    "input_receipt_dict,input_service_cert",
-    [
-        [get_test_valid_receipt_1_dict(), get_test_valid_service_certificate_1()],
-        [get_test_valid_receipt_2_dict(), get_test_valid_service_certificate_2()],
-    ],
-)
-def test_receipt_verification_from_dict_returns_successfully(
-    input_receipt_dict, input_service_cert
-):
-
-    # Check that verify_receipt does not throw any exception
-    # with a valid receipt and service certificate
-    try:
-        verify_receipt(input_receipt_dict, input_service_cert)
-    except Exception as e:
-        pytest.fail(
-            f"verify_receipt threw an exception with a valid receipt dictionary and service certificate {e}"
-        )
-
-
-@pytest.mark.parametrize(
     "input_leaf_components",
     [
         None,
@@ -75,14 +55,18 @@ def test_receipt_verification_from_dict_returns_successfully(
             writeSetDigest="invalid_write_set_digest",
         ),
         LeafComponents(
-            claimsDigest=get_test_valid_receipt_1().leafComponents.claimsDigest,
+            claimsDigest=get_test_valid_receipt_1_dict()["leaf_components"][
+                "claims_digest"
+            ],
             commitEvidence="invalid_commit_evidence",
             writeSetDigest="invalid_write_set_digest",
         ),
         LeafComponents(
             claimsDigest="invalid_claims_digest",
             commitEvidence="invalid_commit_evidence",
-            writeSetDigest=get_test_valid_receipt_1().leafComponents.writeSetDigest,
+            writeSetDigest=get_test_valid_receipt_1_dict()["leaf_components"][
+                "write_set_digest"
+            ],
         ),
     ],
 )
@@ -91,13 +75,12 @@ def test_receipt_verification_with_invalid_leaf_components_throws_exception(
 ):
 
     # Create a receipt with invalid leaf components
-    receipt = get_test_valid_receipt_1()
-    receipt.leafComponents = input_leaf_components
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["leafComponents"] = input_leaf_components
 
     # Check that verify_receipt throws ValueError
     with pytest.raises(
         ValueError,
-        match="Encountered exception when computing leaf node hash from leaf components",
     ):
         verify_receipt(receipt, get_test_valid_service_certificate_1())
 
@@ -106,32 +89,46 @@ def test_receipt_verification_with_invalid_leaf_components_throws_exception(
 def test_receipt_verification_with_invalid_node_cert_throws_exception(input_node_cert):
 
     # Create a receipt with an invalid node certificate
-    receipt = get_test_valid_receipt_1()
-    receipt.cert = input_node_cert
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["cert"] = input_node_cert
 
     # Check that verify_receipt throws ValueError
     with pytest.raises(
         ValueError,
-        match=f"PEM certificate {input_node_cert} is not valid.",
     ):
         verify_receipt(receipt, get_test_valid_service_certificate_1())
 
 
 @pytest.mark.parametrize(
-    "input_node_id", [None, "invalid_node_id", get_test_valid_receipt_2().nodeId]
+    "input_node_id",
+    [None, "invalid_node_id", get_test_valid_receipt_2_dict()["nodeId"]],
 )
 def test_receipt_verification_with_invalid_node_id_throws_exception(input_node_id):
 
     # Create a receipt with an invalid node id
-    receipt = get_test_valid_receipt_1()
-    receipt.nodeId = input_node_id
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["node_id"] = input_node_id
 
     # Check that verify_receipt throws ValueError
     with pytest.raises(
         ValueError,
-        match="Encountered exception when verifying signature",
     ):
         verify_receipt(receipt, get_test_valid_service_certificate_1())
+
+
+def test_receipt_verification_with_missing_node_id_does_not_throw_exception():
+
+    # Create a receipt with an invalid node id
+    receipt = get_test_valid_receipt_1_dict()
+    del receipt["node_id"]
+
+    # Check that verify_receipt does throw any exception
+    try:
+        verify_receipt(receipt, get_test_valid_service_certificate_1())
+    except Exception as e:
+        pytest.fail(
+            f"verify_receipt threw an exception with a valid receipt and service certificate {e}"
+        )
 
 
 @pytest.mark.parametrize(
@@ -149,27 +146,39 @@ def test_receipt_verification_with_invalid_proof_list_throws_exception(
 ):
 
     # Create a receipt with an invalid proof list
-    receipt = get_test_valid_receipt_1()
-    receipt.proof = input_proof_list
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["proof"] = input_proof_list
 
     # Check that verify_receipt throws ValueError
     with pytest.raises(
         ValueError,
-        match="Encountered exception when computing root node hash from proof list",
     ):
         verify_receipt(receipt, get_test_valid_service_certificate_1())
 
 
-@pytest.mark.parametrize("input_endorsements_list", [None])
-def test_receipt_verification_with_invalid_service_endorsements_does_not_throw_exception(
+@pytest.mark.parametrize("input_endorsements_list", [1, 2])
+def test_receipt_verification_with_invalid_service_endorsements_throws_exception(
     input_endorsements_list,
 ):
 
     # Create a receipt with a null endorsements list
-    receipt = get_test_valid_receipt_1()
-    receipt.serviceEndorsements = input_endorsements_list
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["service_endorsements"] = input_endorsements_list
 
     # Check that verify_receipt throws ValueError
+    with pytest.raises(
+        ValueError,
+    ):
+        verify_receipt(receipt, get_test_valid_service_certificate_1())
+
+
+def test_receipt_verification_with_missing_service_endorsements_does_not_throw_exception():
+
+    # Create a receipt and remove the endorsements list
+    receipt = get_test_valid_receipt_1_dict()
+    del receipt["service_endorsements"]
+
+    # Check that verify_receipt does throw any exception
     try:
         verify_receipt(receipt, get_test_valid_service_certificate_1())
     except Exception as e:
@@ -179,19 +188,16 @@ def test_receipt_verification_with_invalid_service_endorsements_does_not_throw_e
 
 
 @pytest.mark.parametrize(
-    "input_signature", [None, get_test_valid_receipt_2().signature]
+    "input_signature", [None, get_test_valid_receipt_2_dict()["signature"]]
 )
 def test_receipt_verification_with_invalid_signature_throws_exception(input_signature):
 
     # Create a receipt with an invalid signature
-    receipt = get_test_valid_receipt_1()
-    receipt.signature = input_signature
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["signature"] = input_signature
 
     # Check that verify_receipt throws ValueError
-    with pytest.raises(
-        ValueError,
-        match="Encountered exception when verifying signature",
-    ):
+    with pytest.raises(ValueError):
         verify_receipt(receipt, get_test_valid_service_certificate_1())
 
 
@@ -203,9 +209,8 @@ def test_receipt_verification_with_invalid_service_cert_throws_exception(
     # Check that verify_receipt throws ValueError with invalid service certificate
     with pytest.raises(
         ValueError,
-        match=f"PEM certificate {input_service_cert} is not valid.",
     ):
-        verify_receipt(get_test_valid_receipt_1(), input_service_cert)
+        verify_receipt(get_test_valid_receipt_1_dict(), input_service_cert)
 
 
 @pytest.mark.parametrize("input_service_cert", [get_test_valid_service_certificate_2()])
@@ -216,22 +221,20 @@ def test_receipt_verification_with_unknown_service_cert_throws_exception(
     # Check that verify_receipt throws ValueError with unknown service certificate
     with pytest.raises(
         ValueError,
-        match="Encountered exception when verifying endorsement of certificate",
     ):
-        verify_receipt(get_test_valid_receipt_1(), input_service_cert)
+        verify_receipt(get_test_valid_receipt_1_dict(), input_service_cert)
 
 
-@pytest.mark.parametrize("input_node_cert", [get_test_valid_receipt_2().cert])
+@pytest.mark.parametrize("input_node_cert", [get_test_valid_receipt_2_dict()["cert"]])
 def test_receipt_verification_with_unknown_node_cert_throws_exception(input_node_cert):
 
     # Create a receipt with unknown node certificate
-    receipt = get_test_valid_receipt_1()
-    receipt.cert = input_node_cert
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["cert"] = input_node_cert
 
     # Check that verify_receipt throws ValueError
     with pytest.raises(
         ValueError,
-        match="Encountered exception when verifying endorsement of certificate",
     ):
         verify_receipt(receipt, get_test_valid_service_certificate_1())
 
@@ -244,48 +247,47 @@ def test_receipt_verification_with_unknown_service_endorsements_throws_exception
 ):
 
     # Create a receipt with unknown endorsements list
-    receipt = get_test_valid_receipt_1()
-    receipt.serviceEndorsements = input_endorsements_list
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["service_endorsements"] = input_endorsements_list
 
     # Check that verify_receipt throws ValueError
     with pytest.raises(
         ValueError,
-        match="Encountered exception when verifying endorsement of certificate",
     ):
         verify_receipt(receipt, get_test_valid_service_certificate_1())
 
 
 @pytest.mark.parametrize(
-    "input_leaf_components", [get_test_valid_receipt_2().leafComponents]
+    "input_leaf_components", [get_test_valid_receipt_2_dict()["leafComponents"]]
 )
 def test_receipt_verification_with_unknown_leaf_components_throws_exception(
     input_leaf_components,
 ):
 
     # Create a receipt with unknown leaf components
-    receipt = get_test_valid_receipt_1()
-    receipt.leafComponents = input_leaf_components
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["leafComponents"] = input_leaf_components
 
     # Check that verify_receipt throws ValueError
     with pytest.raises(
         ValueError,
-        match="Encountered exception when verifying signature",
     ):
         verify_receipt(receipt, get_test_valid_service_certificate_1())
 
 
-@pytest.mark.parametrize("input_proof_list", [[], get_test_valid_receipt_2().proof])
+@pytest.mark.parametrize(
+    "input_proof_list", [[], get_test_valid_receipt_2_dict()["proof"]]
+)
 def test_receipt_verification_with_unknown_proof_list_throws_exception(
     input_proof_list,
 ):
 
     # Create a receipt with unknown proof list
-    receipt = get_test_valid_receipt_1()
-    receipt.proof = input_proof_list
+    receipt = get_test_valid_receipt_1_dict()
+    receipt["proof"] = input_proof_list
 
     # Check that verify_receipt throws ValueError
     with pytest.raises(
         ValueError,
-        match="Encountered exception when verifying signature",
     ):
         verify_receipt(receipt, get_test_valid_service_certificate_1())
