ed43fa5cded9d80bebc5e6c047a4d798ee65dca0
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2023-01-19 10:21:41 -0800

    Expose requests_pipeline on EnvironmentOperations

--- sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
@@ -335,6 +335,7 @@ class MLClient(object):
             self._operation_config,
             self._service_client_10_2021_dataplanepreview if registry_name else self._service_client_05_2022,
             self._operation_container,
+            requests_pipeline=self._requests_pipeline,
             **ops_kwargs,
         )
         self._operation_container.add(AzureMLResourceType.ENVIRONMENT, self._environments)
