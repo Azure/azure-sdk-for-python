0112df2c3a2748320c74e58467b53ff866bd2e1a
Valerie Pham <valeriepham@microsoft.com>
2023-01-23 17:26:09 -0600

    Update service client version for data operations

--- sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
@@ -394,7 +394,7 @@ class MLClient(object):
             self._operation_scope,
             self._operation_config,
             self._service_client_10_2021_dataplanepreview
-            if registry_name else self._service_client_05_2022,
+            if registry_name else self._service_client_10_2022,
             self._datastores,
             requests_pipeline=self._requests_pipeline,
             **ops_kwargs,
