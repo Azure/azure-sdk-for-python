2dbf865a70f10e45eb389e9c2cc0a1d5d6b60035
Kashif Khan <kashifkhan@microsoft.com>
2023-01-27 09:54:28 -0600

    restructure classes

--- sdk/servicebus/azure-servicebus/tests/perf_tests/_test_base.py
+++ sdk/servicebus/azure-servicebus/tests/perf_tests/_test_base.py
@@ -11,7 +11,7 @@ from azure.servicebus import ServiceBusClient, ServiceBusReceiveMode, ServiceBus
 from azure.servicebus.aio import ServiceBusClient as AsyncServiceBusClient
 from azure.servicebus.aio.management import ServiceBusAdministrationClient
 
-class _ReceiveTest(PerfStressTest):
+class _ReceiveTest():
     def __init__(self, arguments) -> None:
         super().__init__(arguments)
         self.connection_string=self.get_from_env("AZURE_SERVICEBUS_CONNECTION_STRING")
@@ -33,77 +33,10 @@ class _ReceiveTest(PerfStressTest):
             transport_type=transport_type,
             uamqp_transport=arguments.uamqp_transport,
         )
-    async def close(self) -> None:
+    async def close_clients(self) -> None:
         self.servicebus_client.close
         await self.async_servicebus_client.close()
         await super().close()
-
-    @staticmethod
-    def add_arguments(parser) -> None:
-        super(_ReceiveTest, _ReceiveTest).add_arguments(parser)
-        parser.add_argument('--message-size', nargs='?', type=int, help='Size of a single message. Defaults to 100 bytes', default=100)
-        parser.add_argument('--num-messages', nargs='?', type=int, help='Number of messages to send or receive. Defaults to 100', default=100)
-        parser.add_argument('--peeklock', action='store_true', help='Receive using PeekLock mode and message settlement.', default=False)
-        parser.add_argument('--uamqp-transport', action="store_true", help="Switch to use uamqp transport. Default is False (pyamqp).", default=False)
-        parser.add_argument('--transport-type', nargs='?', type=int, help="Use Amqp (0) or Websocket (1) transport type. Default is Amqp.", default=0)
-        parser.add_argument('--max-wait-time', nargs='?', type=int, help='Max time to wait for messages before closing. Defaults to 0.', default=0)
-        parser.add_argument('--preload', nargs='?', type=int, help='Number of messages to preload. Default is 10000.', default=10000)
-        
-
-class _QueueReceiveTest(_ReceiveTest):
-    def __init__(self, arguments) -> None:
-        super().__init__(arguments)
-        self.queue_name=self.get_from_env('AZURE_SERVICEBUS_QUEUE_NAME')
-        
-        self.receiver=self.servicebus_client.get_queue_receiver(self.queue_name)
-        self.async_receiver=self.async_servicebus_client.get_queue_receiver(self.queue_name)
-
-    async def _preload_queue(self) -> None:
-        data=get_random_bytes(self.args.message_size)
-
-        current_queue_message_count = 0
-
-        async with ServiceBusAdministrationClient.from_connection_string(self.connection_string) as admin_client:
-            queue_properties = await admin_client.get_queue_runtime_properties(self.queue_name)
-            current_queue_message_count = queue_properties.active_message_count
-    
-
-        print(f"The current queue {self.queue_name} has {current_queue_message_count} already")
-
-        async with self.async_servicebus_client.get_queue_sender(self.queue_name) as sender:
-            batch = await sender.create_message_batch()
-
-            for i in range(current_queue_message_count, self.args.preload):
-                try:
-                    batch.add_message(ServiceBusMessage(data))
-                except ValueError:
-                    await sender.send_messages(batch)
-                    print(f"Loaded {i} messages")
-                    batch = await sender.create_message_batch()
-                    batch.add_message(ServiceBusMessage(data))
-
-            if len(batch):
-                await sender.send_messages(batch)
-    
-    async def global_setup(self) -> None:
-        await super().global_setup()
-        await self._preload_queue()
-    
-    async def close(self) -> None:
-        self.receiver.close()
-        await self.async_receiver.close()
-        await super().close()
-    
-   
-
-class _SubscriptionReceiveTest(_ReceiveTest):
-    def __init__(self, arguments) -> None:
-        super().__init__(arguments)
-        self.topic_name=self.get_from_env('AZURE_SERVICEBUS_TOPIC_NAME')
-        self.subscription_name=self.get_from_env('AZURE_SERVICE_BUS_SUBSCRIPTION_NAME')
-
-        self.receiver=self.servicebus_client.get_subscription_receiver(topic_name=self.topic_name, subscription_name=self.subscription_name)
-        self.async_receiver=self.async_servicebus_client.get_subscription_receiver(topic_name=self.topic_name, subscription_name=self.subscription_name)
 
     async def _preload_topic(self) -> None:
         data=get_random_bytes(self.args.message_size)
@@ -131,64 +64,7 @@ class _SubscriptionReceiveTest(_ReceiveTest):
 
             if len(batch):
                 await sender.send_messages(batch)
-    
-    async def global_setup(self) -> None:
-        await super().global_setup()
-        await self._preload_topic()
-    
-    async def close(self) -> None:
-        self.receiver.close()
-        await self.async_receiver.close()
-        await super().close()
-
-class _ReceiveBatchTest(BatchPerfTest):
-    def __init__(self, arguments) -> None:
-        super().__init__(arguments)
-        self.connection_string=self.get_from_env("AZURE_SERVICEBUS_CONNECTION_STRING")
-        transport_type=TransportType.AmqpOverWebsocket if arguments.transport_type == 1 else TransportType.Amqp
-        mode=ServiceBusReceiveMode.PEEK_LOCK if arguments.peeklock else ServiceBusReceiveMode.RECEIVE_AND_DELETE
-        self.servicebus_client=ServiceBusClient.from_connection_string(
-            self.connection_string,
-            receive_mode=mode,
-            prefetch_count=self.args.num_messages,
-            max_wait_time=self.args.max_wait_time or None, 
-            transport_type=transport_type,
-            uamqp_transport=arguments.uamqp_transport,
-        )
-        self.async_servicebus_client=AsyncServiceBusClient.from_connection_string(
-            self.connection_string,
-            receive_mode=mode,
-            prefetch_count=self.args.num_messages,
-            max_wait_time=self.args.max_wait_time or None,
-            transport_type=transport_type,
-            uamqp_transport=arguments.uamqp_transport,
-        )
-        self.max_message_count = 1000
-    async def close(self) -> None:
-        self.servicebus_client.close
-        await self.async_servicebus_client.close()
-        await super().close()
-
-    @staticmethod
-    def add_arguments(parser) -> None:
-        super(_ReceiveBatchTest, _ReceiveBatchTest).add_arguments(parser)
-        parser.add_argument('--message-size', nargs='?', type=int, help='Size of a single message. Defaults to 100 bytes', default=100)
-        parser.add_argument('--num-messages', nargs='?', type=int, help='Number of messages to send or receive. Defaults to 100', default=100)
-        parser.add_argument('--peeklock', action='store_true', help='Receive using PeekLock mode and message settlement.', default=False)
-        parser.add_argument('--uamqp-transport', action="store_true", help="Switch to use uamqp transport. Default is False (pyamqp).", default=False)
-        parser.add_argument('--transport-type', nargs='?', type=int, help="Use Amqp (0) or Websocket (1) transport type. Default is Amqp.", default=0)
-        parser.add_argument('--max-wait-time', nargs='?', type=int, help='Max time to wait for messages before closing. Defaults to 0.', default=0)
-        parser.add_argument('--preload', nargs='?', type=int, help='Number of messages to preload. Default is 10000.', default=10000)
         
-
-class _QueueReceiveBatchTest(_ReceiveBatchTest):
-    def __init__(self, arguments) -> None:
-        super().__init__(arguments)
-        self.queue_name=self.get_from_env('AZURE_SERVICEBUS_QUEUE_NAME')
-        
-        self.receiver=self.servicebus_client.get_queue_receiver(self.queue_name)
-        self.async_receiver=self.async_servicebus_client.get_queue_receiver(self.queue_name)
-
     async def _preload_queue(self) -> None:
         data=get_random_bytes(self.args.message_size)
 
@@ -215,6 +91,14 @@ class _QueueReceiveBatchTest(_ReceiveBatchTest):
 
             if len(batch):
                 await sender.send_messages(batch)
+
+class _QueueReceiveTest(_ReceiveTest, PerfStressTest):
+    def __init__(self, arguments) -> None:
+        super().__init__(arguments)
+        self.queue_name=self.get_from_env('AZURE_SERVICEBUS_QUEUE_NAME')
+        
+        self.receiver=self.servicebus_client.get_queue_receiver(self.queue_name)
+        self.async_receiver=self.async_servicebus_client.get_queue_receiver(self.queue_name)
     
     async def global_setup(self) -> None:
         await super().global_setup()
@@ -223,11 +107,23 @@ class _QueueReceiveBatchTest(_ReceiveBatchTest):
     async def close(self) -> None:
         self.receiver.close()
         await self.async_receiver.close()
+        await self.close_clients()
         await super().close()
+    
+    @staticmethod
+    def add_arguments(parser) -> None:
+        super(_QueueReceiveTest, _QueueReceiveTest).add_arguments(parser)
+        parser.add_argument('--message-size', nargs='?', type=int, help='Size of a single message. Defaults to 100 bytes', default=100)
+        parser.add_argument('--num-messages', nargs='?', type=int, help='Number of messages to send or receive. Defaults to 100', default=100)
+        parser.add_argument('--peeklock', action='store_true', help='Receive using PeekLock mode and message settlement.', default=False)
+        parser.add_argument('--uamqp-transport', action="store_true", help="Switch to use uamqp transport. Default is False (pyamqp).", default=False)
+        parser.add_argument('--transport-type', nargs='?', type=int, help="Use Amqp (0) or Websocket (1) transport type. Default is Amqp.", default=0)
+        parser.add_argument('--max-wait-time', nargs='?', type=int, help='Max time to wait for messages before closing. Defaults to 0.', default=0)
+        parser.add_argument('--preload', nargs='?', type=int, help='Number of messages to preload. Default is 10000.', default=10000)
     
    
 
-class _SubscriptionReceiveBatchTest(_ReceiveBatchTest):
+class _SubscriptionReceiveTest(_ReceiveTest, PerfStressTest):
     def __init__(self, arguments) -> None:
         super().__init__(arguments)
         self.topic_name=self.get_from_env('AZURE_SERVICEBUS_TOPIC_NAME')
@@ -235,33 +131,6 @@ class _SubscriptionReceiveBatchTest(_ReceiveBatchTest):
 
         self.receiver=self.servicebus_client.get_subscription_receiver(topic_name=self.topic_name, subscription_name=self.subscription_name)
         self.async_receiver=self.async_servicebus_client.get_subscription_receiver(topic_name=self.topic_name, subscription_name=self.subscription_name)
-
-    async def _preload_topic(self) -> None:
-        data=get_random_bytes(self.args.message_size)
-
-        current_topic_message_count = 0
-
-        async with ServiceBusAdministrationClient.from_connection_string(self.connection_string) as admin_client:
-            topic_properties = await admin_client.get_topic_runtime_properties(self.topic_name)
-            current_topic_message_count = topic_properties.scheduled_message_count
-    
-
-        print(f"The current queue {self.topic_name} has {current_topic_message_count} already")
-
-        async with self.async_servicebus_client.get_topic_sender(self.topic_name) as sender:
-            batch = await sender.create_message_batch()
-
-            for i in range(current_topic_message_count, self.args.preload):
-                try:
-                    batch.add_message(ServiceBusMessage(data))
-                except ValueError:
-                    await sender.send_messages(batch)
-                    print(f"Loaded {i} messages")
-                    batch = await sender.create_message_batch()
-                    batch.add_message(ServiceBusMessage(data))
-
-            if len(batch):
-                await sender.send_messages(batch)
     
     async def global_setup(self) -> None:
         await super().global_setup()
@@ -270,7 +139,80 @@ class _SubscriptionReceiveBatchTest(_ReceiveBatchTest):
     async def close(self) -> None:
         self.receiver.close()
         await self.async_receiver.close()
+        await self.close_clients()
         await super().close()
+    
+    @staticmethod
+    def add_arguments(parser) -> None:
+        super(_SubscriptionReceiveTest, _SubscriptionReceiveTest).add_arguments(parser)
+        parser.add_argument('--message-size', nargs='?', type=int, help='Size of a single message. Defaults to 100 bytes', default=100)
+        parser.add_argument('--num-messages', nargs='?', type=int, help='Number of messages to send or receive. Defaults to 100', default=100)
+        parser.add_argument('--peeklock', action='store_true', help='Receive using PeekLock mode and message settlement.', default=False)
+        parser.add_argument('--uamqp-transport', action="store_true", help="Switch to use uamqp transport. Default is False (pyamqp).", default=False)
+        parser.add_argument('--transport-type', nargs='?', type=int, help="Use Amqp (0) or Websocket (1) transport type. Default is Amqp.", default=0)
+        parser.add_argument('--max-wait-time', nargs='?', type=int, help='Max time to wait for messages before closing. Defaults to 0.', default=0)
+        parser.add_argument('--preload', nargs='?', type=int, help='Number of messages to preload. Default is 10000.', default=10000)
+
+
+class _QueueReceiveBatchTest(_ReceiveTest, BatchPerfTest):
+    def __init__(self, arguments) -> None:
+        super().__init__(arguments)
+        self.queue_name=self.get_from_env('AZURE_SERVICEBUS_QUEUE_NAME')
+        
+        self.receiver=self.servicebus_client.get_queue_receiver(self.queue_name)
+        self.async_receiver=self.async_servicebus_client.get_queue_receiver(self.queue_name)
+    
+    async def global_setup(self) -> None:
+        await super().global_setup()
+        await self._preload_queue()
+    
+    async def close(self) -> None:
+        self.receiver.close()
+        await self.async_receiver.close()
+        await self.close_clients()
+        await super().close()
+    
+    @staticmethod
+    def add_arguments(parser) -> None:
+        super(_QueueReceiveBatchTest, _QueueReceiveBatchTest).add_arguments(parser)
+        parser.add_argument('--message-size', nargs='?', type=int, help='Size of a single message. Defaults to 100 bytes', default=100)
+        parser.add_argument('--num-messages', nargs='?', type=int, help='Number of messages to send or receive. Defaults to 100', default=100)
+        parser.add_argument('--peeklock', action='store_true', help='Receive using PeekLock mode and message settlement.', default=False)
+        parser.add_argument('--uamqp-transport', action="store_true", help="Switch to use uamqp transport. Default is False (pyamqp).", default=False)
+        parser.add_argument('--transport-type', nargs='?', type=int, help="Use Amqp (0) or Websocket (1) transport type. Default is Amqp.", default=0)
+        parser.add_argument('--max-wait-time', nargs='?', type=int, help='Max time to wait for messages before closing. Defaults to 0.', default=0)
+        parser.add_argument('--preload', nargs='?', type=int, help='Number of messages to preload. Default is 10000.', default=10000)
+   
+
+class _SubscriptionReceiveBatchTest(_ReceiveTest, BatchPerfTest):
+    def __init__(self, arguments) -> None:
+        super().__init__(arguments)
+        self.topic_name=self.get_from_env('AZURE_SERVICEBUS_TOPIC_NAME')
+        self.subscription_name=self.get_from_env('AZURE_SERVICE_BUS_SUBSCRIPTION_NAME')
+
+        self.receiver=self.servicebus_client.get_subscription_receiver(topic_name=self.topic_name, subscription_name=self.subscription_name)
+        self.async_receiver=self.async_servicebus_client.get_subscription_receiver(topic_name=self.topic_name, subscription_name=self.subscription_name)
+    
+    async def global_setup(self) -> None:
+        await super().global_setup()
+        await self._preload_topic()
+    
+    async def close(self) -> None:
+        self.receiver.close()
+        await self.async_receiver.close()
+        await self.close_clients()
+        await super().close()
+    
+    @staticmethod
+    def add_arguments(parser) -> None:
+        super(_SubscriptionReceiveBatchTest, _SubscriptionReceiveBatchTest).add_arguments(parser)
+        parser.add_argument('--message-size', nargs='?', type=int, help='Size of a single message. Defaults to 100 bytes', default=100)
+        parser.add_argument('--num-messages', nargs='?', type=int, help='Number of messages to send or receive. Defaults to 100', default=100)
+        parser.add_argument('--peeklock', action='store_true', help='Receive using PeekLock mode and message settlement.', default=False)
+        parser.add_argument('--uamqp-transport', action="store_true", help="Switch to use uamqp transport. Default is False (pyamqp).", default=False)
+        parser.add_argument('--transport-type', nargs='?', type=int, help="Use Amqp (0) or Websocket (1) transport type. Default is Amqp.", default=0)
+        parser.add_argument('--max-wait-time', nargs='?', type=int, help='Max time to wait for messages before closing. Defaults to 0.', default=0)
+        parser.add_argument('--preload', nargs='?', type=int, help='Number of messages to preload. Default is 10000.', default=10000)
 
 class _SendTest(BatchPerfTest):
     def __init__(self, arguments) -> None:
