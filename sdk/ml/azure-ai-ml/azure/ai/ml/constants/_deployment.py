1d789dbd0e73b4eb9344f43b67db3248c572c8d0
nancy-mejia <106106141+nancy-mejia@users.noreply.github.com>
2023-01-27 10:07:09 -0800

    Add warning for online deployment create operation to suggest the use of bigger SKU (#28490)

--- sdk/ml/azure-ai-ml/azure/ai/ml/constants/_deployment.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/constants/_deployment.py
@@ -23,3 +23,5 @@ class EndpointDeploymentLogContainerType(object):
     INFERENCE_SERVER_REST = "InferenceServer"
     INFERENCE_SERVER = "inference-server"
     STORAGE_INITIALIZER = "storage-initializer"
+
+SmallSKUs = ["standard_ds1_v2", "standard_ds2_v2"]
