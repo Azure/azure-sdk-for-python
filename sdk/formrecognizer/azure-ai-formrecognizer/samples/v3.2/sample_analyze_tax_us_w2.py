9d2d8bca44519ea84d21c7ecd9d797be23c5e5fb
catalinaperalta <catalinaperaltah@hotmail.com>
2023-01-27 09:38:15 -0800

    [formrecognizer] Add error handling for samples (#28376)
    * add error handling for sync samples
    
    * add error handling for async samples
    
    * update sync samples with comments and more checks
    
    * update async samples with comments and more checks

--- sdk/formrecognizer/azure-ai-formrecognizer/samples/v3.2/sample_analyze_tax_us_w2.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/samples/v3.2/sample_analyze_tax_us_w2.py
@@ -326,4 +326,24 @@ def analyze_tax_us_w2():
 
 
 if __name__ == "__main__":
-    analyze_tax_us_w2()
+    import sys
+    from azure.core.exceptions import HttpResponseError
+    try:
+        analyze_tax_us_w2()
+    except HttpResponseError as error:
+        # Examples of how to check an HttpResponseError
+        # Check by error code:
+        if error.error is not None:
+            if error.error.code == "InvalidRequest":
+                print(f"Received an invalid request error: {error.error}")
+                sys.exit(1)
+            if error.error.code == "InvalidImage":
+                print(f"Received an invalid image error: {error.error}")
+                sys.exit(1)
+        # If the inner error is None and then it is possible to check the message to get more information:
+        filter_msg = ["Generic error", "Timeout", "Invalid request", "InvalidImage"]
+        if any(example_error.casefold() in error.message.casefold() for example_error in filter_msg):
+            print(f"Uh-oh! Something unexpected happened: {error}")
+            sys.exit(1)
+        # Print the full error content:
+        print(f"Full HttpResponseError: {error}")
