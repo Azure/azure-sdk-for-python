9c0e6483d1dd0f63899460f68ffe1a730f8303dd
Sean Brennan <sebrenna@microsoft.com>
2023-01-30 12:17:03 -0800

    hopefully this fixes test recordings?

--- sdk/devcenter/azure-developer-devcenter/tests/testcase.py
+++ sdk/devcenter/azure-developer-devcenter/tests/testcase.py
@@ -18,7 +18,7 @@ class DevcenterTest(AzureRecordedTestCase):
 DevcenterPowerShellPreparer = functools.partial(
     PowerShellPreparer,
     "devcenter", 
-    devcenter_endpoint="https://8ab2df1c-ed88-4946-a8a9-e1bbb3e4d1fd-sdk-dc-na4b3zkj5hmeo.eastus.devcenter.azure.com/",
+    devcenter_endpoint="https://8ab2df1c-ed88-4946-a8a9-e1bbb3e4d1fd-sdk-dc-na4b3zkj5hmeo.eastus.devcenter.azure.com",
     devcenter_project_name="sdk-default-project",
     devcenter_pool_name="sdk-default-pool",
     devcenter_test_user_id="11111111-1111-1111-1111-111111111111",
