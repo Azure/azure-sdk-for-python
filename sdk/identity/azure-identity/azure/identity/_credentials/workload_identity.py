0fe40b16636fa6140e765a150fa1f8e7880aa56c
Xiang Yan <xiangyan@microsoft.com>
2023-01-31 13:56:28 -0800

    updates

--- sdk/identity/azure-identity/azure/identity/_credentials/workload_identity.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/workload_identity.py
@@ -30,7 +30,8 @@ class TokenFileMixin:
 
 class WorkloadIdentityCredential(ClientAssertionCredential, TokenFileMixin):
     """WorkloadIdentityCredential supports Azure workload identity on Kubernetes.
-    See the `workload identity overview <https://learn.microsoft.com/azure/aks/workload-identity-overview>`_ for more information.
+    See the `workload identity overview <https://learn.microsoft.com/azure/aks/workload-identity-overview>`_
+    for more information.
 
     :param str tenant_id: ID of the application's Azure Active Directory tenant. Also called its "directory" ID.
     :param str client_id: The client ID of an Azure AD app registration.
