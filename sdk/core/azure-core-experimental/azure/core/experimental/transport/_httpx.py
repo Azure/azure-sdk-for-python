58baa777df77f69471d767bd65ec6fc7a3c48026
Kashif Khan <kashifkhan@microsoft.com>
2023-02-01 17:32:05 -0600

    initial commit to get httpx in

--- sdk/core/azure-core-experimental/azure/core/experimental/transport/_httpx.py
+++ sdk/core/azure-core-experimental/azure/core/experimental/transport/_httpx.py
@@ -23,3 +23,78 @@
 # IN THE SOFTWARE.
 #
 # --------------------------------------------------------------------------
+from httpx import Response, Client
+from typing import Any, ContextManager, Iterator, Optional
+from azure.core.pipeline.transport import HttpResponse, HttpRequest, HttpTransport
+
+
+class HttpXTransportResponse(HttpResponse):
+    def __init__(self, request: HttpRequest, httpx_response: Response, stream_contextmanager: Optional[ContextManager]) -> None:
+        super().__init__(request, httpx_response)
+        self.status_code = httpx_response.status_code
+        self.headers = httpx_response.headers
+        self.reason = httpx_response.reason_phrase
+        self.content_type = httpx_response.headers.get('content-type')
+        self.stream_contextmanager = stream_contextmanager
+    
+    def body(self) -> bytes:
+        return self.internal_response.content
+
+    def stream_download(self, _) -> Iterator[bytes]:
+        return HttpXStreamDownloadGenerator(_, self)
+
+
+class HttpXStreamDownloadGenerator():
+    def __init__(self, _, response) -> None:
+        self.response = response
+        self.iter_bytes_func = self.response.internal_response.iter_bytes()
+
+    def __iter__(self) -> 'HttpXStreamDownloadGenerator':
+        return self
+
+    def __next__(self):
+        try:
+            return next(self.iter_bytes_func)
+        except StopIteration:
+            self.response.stream_contextmanager.__exit__()
+            raise
+
+class HttpXTransport(HttpTransport):
+    def __init__(self) -> None:
+        self.client: Optional[Client] = None
+
+    def open(self) -> None:
+        self.client = Client()
+
+    def close(self) -> None:
+        if self.client:
+            self.client.close()
+            self.client = None
+    
+    def __enter__(self) -> "HttpXTransport":
+        self.open()
+        return self
+    
+    def __exit__(self, *args) -> None:
+        self.close()
+
+    def send(self, request: HttpRequest, **kwargs) -> HttpXTransportResponse:
+        stream_response = kwargs.pop("stream", False)
+        parameters = {
+            "method": request.method,
+            "url": request.url,
+            "headers": request.headers.items(),
+            "data": request.data,
+            "files": request.files,
+            **kwargs,
+        }
+
+        stream_ctx: Optional[ContextManager] = None
+
+        if stream_response:
+            stream_ctx = self.client.stream(**parameters)
+            response = stream_ctx.__enter__()
+        else:
+            response = self.client.request(**parameters)
+
+        return HttpXTransportResponse(request, response, stream_contextmanager=stream_ctx)
