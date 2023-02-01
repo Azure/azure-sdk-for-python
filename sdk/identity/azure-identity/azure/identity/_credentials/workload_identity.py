2d61792d140ad895cfbf8f945454ca947cf638f2
Xiang Yan <xiangsjtu@gmail.com>
2023-02-01 15:32:42 -0800

    Added `WorkloadIdentityCredential` (#28536)
    * Added `WorkloadIdentityCredential`
    
    * update
    
    * updates
    
    * updates
    
    * update
    
    * update
    
    * updates
    
    * update
    
    * Update sdk/identity/azure-identity/azure/identity/_credentials/workload_identity.py
    
    Co-authored-by: Paul Van Eck <paulvaneck@microsoft.com>
    
    * Update sdk/identity/azure-identity/CHANGELOG.md
    
    Co-authored-by: Paul Van Eck <paulvaneck@microsoft.com>
    
    * Update sdk/identity/azure-identity/azure/identity/_credentials/workload_identity.py
    
    Co-authored-by: Paul Van Eck <paulvaneck@microsoft.com>
    
    * updates
    
    * updates
    
    * update
    
    * Updates
    
    * update
    
    * Update sdk/identity/azure-identity/azure/identity/_credentials/default.py
    
    Co-authored-by: Charles Lowell <10964656+chlowell@users.noreply.github.com>
    
    * Update sdk/identity/azure-identity/azure/identity/aio/_credentials/default.py
    
    Co-authored-by: Charles Lowell <10964656+chlowell@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Paul Van Eck <paulvaneck@microsoft.com>
    Co-authored-by: Charles Lowell <10964656+chlowell@users.noreply.github.com>

--- sdk/identity/azure-identity/azure/identity/_credentials/workload_identity.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/workload_identity.py
@@ -11,35 +11,44 @@ from .client_assertion import ClientAssertionCredential
 class TokenFileMixin:
     def __init__(
             self,
-            token_file_path: str,
+            file: str,
             **_: Any
     ) -> None:
         super(TokenFileMixin, self).__init__()
         self._jwt = ""
         self._last_read_time = 0
-        self._token_file_path = token_file_path
+        self._file = file
 
     def get_service_account_token(self) -> str:
         now = int(time.time())
-        if now - self._last_read_time > 300:
-            with open(self._token_file_path) as f:
+        if now - self._last_read_time > 600:
+            with open(self._file) as f:
                 self._jwt = f.read()
             self._last_read_time = now
         return self._jwt
 
 
-class TokenExchangeCredential(ClientAssertionCredential, TokenFileMixin):
+class WorkloadIdentityCredential(ClientAssertionCredential, TokenFileMixin):
+    """WorkloadIdentityCredential supports Azure workload identity on Kubernetes.
+    See the `workload identity overview <https://learn.microsoft.com/azure/aks/workload-identity-overview>`_
+    for more information.
+
+    :param str tenant_id: ID of the application's Azure Active Directory tenant. Also called its "directory" ID.
+    :param str client_id: The client ID of an Azure AD app registration.
+    :param str file: The path to a file containing a Kubernetes service account token that authenticates the identity.
+    """
+
     def __init__(
             self,
             tenant_id: str,
             client_id: str,
-            token_file_path: str,
+            file: str,
             **kwargs: Any
     ) -> None:
-        super(TokenExchangeCredential, self).__init__(
+        super(WorkloadIdentityCredential, self).__init__(
             tenant_id=tenant_id,
             client_id=client_id,
             func=self.get_service_account_token,
-            token_file_path=token_file_path,
+            file=file,
             **kwargs
         )
