9e812cae6a8826e69e6359f71f820ab6b6337eff
Adam Tazi <adamtazi@microsoft.com>
2023-02-02 15:48:27 -0800

    add media client

---
+++ sdk/communication/azure-communication-callautomation/azure/communication/callautomation/call_media_client.py
@@ -0,0 +1,26 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+class CallMediaClient(object):
+    def __init__(self):
+        # TODO
+        return True
+
+    def play(PlaySource play):
+        # TODO
+        return True
+
+    def play_to_all():
+        # TODO
+        return True
+
+    def start_recognizing():
+        # TODO
+        return True
+        
+    def cancel_all_media_operations():
+        # TODO
+        return True
