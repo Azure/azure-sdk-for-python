f8315ad64051acfa99b949f400cb671b05590d08
Krista Pratico <krpratic@microsoft.com>
2023-02-02 12:46:49 -0800

    [translation] regen+typing (#27786)
    * some typing improvements
    
    * regen and rerecord now that we don't gen with maxpagesize anymore
    
    * fix typing errs and dependencies
    
    * shared reqs
    
    * enable samples for pyright/mypy
    
    * cspell
    
    * fix cspell again
    
    * update
    
    * update recordings tag
    
    * pep526 type hints
    
    * regen

--- sdk/translation/azure-ai-translation-document/samples/sample_begin_translation.py
+++ sdk/translation/azure-ai-translation-document/samples/sample_begin_translation.py
@@ -58,7 +58,7 @@ def sample_translation():
             print(f"Source document location: {document.source_document_url}")
             print(f"Translated document location: {document.translated_document_url}")
             print(f"Translated to language: {document.translated_to}\n")
-        else:
+        elif document.error:
             print(f"Error Code: {document.error.code}, Message: {document.error.message}\n")
     # [END begin_translation]
 
