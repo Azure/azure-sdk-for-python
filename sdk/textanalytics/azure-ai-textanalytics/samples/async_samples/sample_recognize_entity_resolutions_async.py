32dfe15a08bfd0c4f79d78a6751191d997f724c7
Krista Pratico <krpratic@microsoft.com>
2023-02-03 14:04:10 -0800

    fix format of samples and update readmes

--- sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_recognize_entity_resolutions_async.py
+++ sdk/textanalytics/azure-ai-textanalytics/samples/async_samples/sample_recognize_entity_resolutions_async.py
@@ -23,6 +23,7 @@ USAGE:
 
 import asyncio
 
+
 async def sample_recognize_entity_resolutions_async() -> None:
     import os
     from azure.core.credentials import AzureKeyCredential
@@ -38,14 +39,14 @@ async def sample_recognize_entity_resolutions_async() -> None:
             "The cat is 3 years old and weighs ten pounds.",
 
             "The bill for $25.45 is for service between January 1, 2023 and February 2, 2023. "
-            "A discount is applied since the internet dropped to 32 mB/s.",
+            "A discount is applied since the internet dropped below 50 mB/s.",
 
-            "He was the first man to walk on the moon on January 31, 1945 at 1:00pm.",
+            "He was the first man to walk on the moon on July 21, 1969 at 2:56pm.",
 
             "The property spans 25 acres and has a 3 mile round-trip hiking trail.",
 
-            "This aircraft carries 200 liters of fuel and can carry 65-80 passengers. "
-            "Cruising speed is 100 knots at standard 32°F temperature and pressure",
+            "This aircraft carries 200 liters of fuel and can fly 65-80 passengers. "
+            "Cruising speed is 150 knots at standard 32°F temperature and pressure",
         ]
 
         results = await text_analytics_client.recognize_entities(documents, model_version="2022-10-01-preview")
@@ -57,16 +58,19 @@ async def sample_recognize_entity_resolutions_async() -> None:
             continue
 
         for entity in result.entities:
-            print(f"...Entity is '{entity.text}' and categorized as '{entity.category}' with subcategory '{entity.subcategory}'.")
+            print(f"...Entity is '{entity.text}' and categorized as '{entity.category}' "
+                  f"with subcategory '{entity.subcategory}'.")
             for res in entity.resolutions:
                 if res.resolution_kind == ResolutionKind.AGE_RESOLUTION:
                     print(f"......Age resolution has a value of '{res.value}' and unit of '{res.unit}'.")
                 elif res.resolution_kind == ResolutionKind.AREA_RESOLUTION:
                     print(f"......Area resolution has a value of '{res.value}' and unit of '{res.unit}'.")
                 elif res.resolution_kind == ResolutionKind.CURRENCY_RESOLUTION:
-                    print(f"......Currency resolution has a value of '{res.value}', unit of '{res.unit}', and ISO4217 code of '{res.iso4217}'.")
+                    print(f"......Currency resolution has a value of '{res.value}', unit of '{res.unit}', "
+                          f"and ISO4217 code of '{res.iso4217}'.")
                 elif res.resolution_kind == ResolutionKind.DATE_TIME_RESOLUTION:
-                    print(f"......DateTime resolution has a value of '{res.value}', a subkind of '{res.datetime_subkind}', and a timex of '{res.timex}'.")
+                    print(f"......DateTime resolution has a value of '{res.value}', a subkind of "
+                          f"'{res.datetime_subkind}', and a timex of '{res.timex}'.")
                 elif res.resolution_kind == ResolutionKind.INFORMATION_RESOLUTION:
                     print(f"......Information resolution has a value of '{res.value}' and unit of '{res.unit}'.")
                 elif res.resolution_kind == ResolutionKind.LENGTH_RESOLUTION:
@@ -74,15 +78,18 @@ async def sample_recognize_entity_resolutions_async() -> None:
                 elif res.resolution_kind == ResolutionKind.NUMBER_RESOLUTION:
                     print(f"......Number resolution has a kind of '{res.number_kind}' and a value of '{res.value}.")
                 elif res.resolution_kind == ResolutionKind.NUMERIC_RANGE_RESOLUTION:
-                    print(f"......Numeric range resolution has a kind of '{res.range_kind}', a minimum of '{res.minimum}' and a maximum of '{res.maximum}'.")
+                    print(f"......Numeric range resolution has a kind of '{res.range_kind}', a minimum of "
+                          f"'{res.minimum}' and a maximum of '{res.maximum}'.")
                 elif res.resolution_kind == ResolutionKind.ORDINAL_RESOLUTION:
-                    print(f"......Ordinal resolution has a value of '{res.value}', an offset of '{res.offset}, and is relative to '{res.relative_to}'.")
+                    print(f"......Ordinal resolution has a value of '{res.value}', an offset of '{res.offset}', "
+                          f"and is relative to '{res.relative_to}'.")
                 elif res.resolution_kind == ResolutionKind.SPEED_RESOLUTION:
                     print(f"......Speed resolution has a value of '{res.value}' and unit of '{res.unit}'.")
                 elif res.resolution_kind == ResolutionKind.TEMPERATURE_RESOLUTION:
                     print(f"......Temperature resolution has a value of '{res.value}' and unit of '{res.unit}'.")
                 elif res.resolution_kind == ResolutionKind.TEMPORAL_SPAN_RESOLUTION:
-                    print(f"......Temporal span resolution begins on '{res.begin}' and ends on '{res.end}' with duration '{res.duration}'.")
+                    print(f"......Temporal span resolution begins on '{res.begin}' and ends on '{res.end}' "
+                          f"with duration '{res.duration}'.")
                 elif res.resolution_kind == ResolutionKind.VOLUME_RESOLUTION:
                     print(f"......Volume resolution has a value of '{res.value}' and unit of '{res.unit}'.")
                 elif res.resolution_kind == ResolutionKind.WEIGHT_RESOLUTION:
