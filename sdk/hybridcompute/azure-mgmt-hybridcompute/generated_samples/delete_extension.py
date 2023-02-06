b84be92a0811034362f8159655feaabae20d852d
PythonSdkPipelines <PythonSdkPipelines>
2023-02-06 02:33:31 +0000

    code and test

--- sdk/hybridcompute/azure-mgmt-hybridcompute/generated_samples/delete_extension.py
+++ sdk/hybridcompute/azure-mgmt-hybridcompute/generated_samples/delete_extension.py
@@ -37,6 +37,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2022-08-11-preview/examples/DELETEExtension.json
+# x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/stable/2022-03-10/examples/DELETEExtension.json
 if __name__ == "__main__":
     main()
