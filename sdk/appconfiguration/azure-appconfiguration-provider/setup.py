844318e8066a926d0648077d2102cbdbfb353829
Matt Metcalf <mrm9084@gmail.com>
2023-02-08 15:41:35 -0800

    Update setup.py

--- sdk/appconfiguration/azure-appconfiguration-provider/setup.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/setup.py
@@ -75,7 +75,7 @@ setup(
     install_requires=[
         "msrest>=0.6.21",
         "azure-core<2.0.0,>=1.2.2",
-        "azure-appconfiguration<2.0.0,>=1.3.0",
+        "azure-appconfiguration<2.0.0,>=1.4.0b1",
         "azure-keyvault-secrets<5.0.0,>=4.3.0",
     ],
 )
