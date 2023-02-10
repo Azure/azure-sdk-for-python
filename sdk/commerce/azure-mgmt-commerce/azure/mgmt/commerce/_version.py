255de4d0d3f2a2ca0bfd1d2e0c996d740a8230ba
zhenbiao wei <424401670@qq.com>
2023-02-10 14:10:41 +0800

    Update _version.py

--- sdk/commerce/azure-mgmt-commerce/azure/mgmt/commerce/_version.py
+++ sdk/commerce/azure-mgmt-commerce/azure/mgmt/commerce/_version.py
@@ -6,4 +6,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "0.0.0 (it should be perview)"
+VERSION = "6.1.0b1"
