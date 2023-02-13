86fa1350e426802bf74ef0760f08fa73315ef19a
iscai-msft <iscai@microsoft.com>
2023-02-13 15:50:20 -0800

    fix typing and add aio folder

--- sdk/loadtestservice/azure-developer-loadtesting/azure/developer/loadtesting/aio/_client.py
+++ sdk/loadtestservice/azure-developer-loadtesting/azure/developer/loadtesting/aio/_client.py
@@ -3,39 +3,52 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+
 from copy import deepcopy
-from typing import Any, TYPE_CHECKING
+from typing import Any, Awaitable
 
-from azure.core import PipelineClient
-from azure.core.rest import HttpRequest, HttpResponse
+from azure.core.credentials_async import AsyncTokenCredential
+from azure.core import AsyncPipelineClient
+from azure.core.rest import HttpRequest, AsyncHttpResponse
 
-from ._generated._configuration import LoadTestingClientConfiguration
-from ._generated._serialization import Deserializer, Serializer
-from ._generated.operations import AdministrationOperations, TestRunOperations
+from .._generated.aio._configuration import LoadTestingClientConfiguration
+from .._generated._serialization import Deserializer, Serializer
+from .._generated.aio.operations import AdministrationOperations, TestRunOperations
 
-if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
-    from azure.core.credentials import TokenCredential
+class _BaseClient:  # pylint: disable=client-accepts-api-version-keyword
+    """These APIs allow end users to create, view and run load tests using Azure Load Test Service.
 
-class _BaseClient:
-    def __init__(self, endpoint: str, credential: "TokenCredential", **kwargs: Any) -> None:
+    :ivar administration: AdministrationOperations operations
+    :vartype administration: azure.developer.loadtesting.aio.operations.AdministrationOperations
+    :ivar test_run: TestRunOperations operations
+    :vartype test_run: azure.developer.loadtesting.aio.operations.TestRunOperations
+    :param endpoint: URL to perform data plane API operations on the resource. Required.
+    :type endpoint: str
+    :param credential: Credential needed for the client to connect to Azure. Required.
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
+    :keyword api_version: Api Version. Default value is "2022-11-01". Note that overriding this
+     default value may result in unsupported behavior.
+    :paramtype api_version: str
+    """
+
+    def __init__(self, endpoint: str, credential: "AsyncTokenCredential", **kwargs: Any) -> None:
         _endpoint = "https://{Endpoint}"
         self._config = LoadTestingClientConfiguration(endpoint=endpoint, credential=credential, **kwargs)
-        self._client = PipelineClient(base_url=_endpoint, config=self._config, **kwargs)
+        self._client = AsyncPipelineClient(base_url=_endpoint, config=self._config, **kwargs)
 
         self._serialize = Serializer()
         self._deserialize = Deserializer()
         self._serialize.client_side_validation = False
         super().__init__(self._client, self._config, self._serialize, self._deserialize)
 
-    def send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
+    def send_request(self, request: HttpRequest, **kwargs: Any) -> Awaitable[AsyncHttpResponse]:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
         >>> request = HttpRequest("GET", "https://www.example.org/")
         <HttpRequest [GET], url: 'https://www.example.org/'>
-        >>> response = client.send_request(request)
-        <HttpResponse: 200 OK>
+        >>> response = await client.send_request(request)
+        <AsyncHttpResponse: 200 OK>
 
         For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
 
@@ -43,7 +56,7 @@ class _BaseClient:
         :type request: ~azure.core.rest.HttpRequest
         :keyword bool stream: Whether the response payload will be streamed. Defaults to False.
         :return: The response of your network call. Does not do error handling on your response.
-        :rtype: ~azure.core.rest.HttpResponse
+        :rtype: ~azure.core.rest.AsyncHttpResponse
         """
 
         request_copy = deepcopy(request)
@@ -54,13 +67,11 @@ class _BaseClient:
         request_copy.url = self._client.format_url(request_copy.url, **path_format_arguments)
         return self._client.send_request(request_copy, **kwargs)
 
-    def close(self):
-        # type: () -> None
-        self._client.close()
+    async def close(self) -> None:
+        await self._client.close()
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
-        self._client.__exit__(*exc_details)
+    async def __aexit__(self, *exc_details) -> None:
+        await self._client.__aexit__(*exc_details)
 
 
 class AdministrationClient(_BaseClient, AdministrationOperations):  # pylint: disable=client-accepts-api-version-keyword
@@ -69,14 +80,14 @@ class AdministrationClient(_BaseClient, AdministrationOperations):  # pylint: di
     :param endpoint: URL to perform data plane API operations on the resource. Required.
     :type endpoint: str
     :param credential: Credential needed for the client to connect to Azure. Required.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :keyword api_version: Api Version. Default value is "2022-11-01". Note that overriding this
      default value may result in unsupported behavior.
     :paramtype api_version: str
     """
 
-    def __enter__(self) -> "AdministrationClient":
-        self._client.__enter__()
+    async def __aenter__(self) -> "AdministrationClient":
+        await self._client.__aenter__()
         return self
 
 class TestRunClient(_BaseClient, TestRunOperations):  # pylint: disable=client-accepts-api-version-keyword
@@ -85,13 +96,12 @@ class TestRunClient(_BaseClient, TestRunOperations):  # pylint: disable=client-a
     :param endpoint: URL to perform data plane API operations on the resource. Required.
     :type endpoint: str
     :param credential: Credential needed for the client to connect to Azure. Required.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :keyword api_version: Api Version. Default value is "2022-11-01". Note that overriding this
      default value may result in unsupported behavior.
     :paramtype api_version: str
     """
 
-    def __enter__(self) -> "TestRunClient":
-        self._client.__enter__()
+    async def __aenter__(self) -> "TestRunClient":
+        await self._client.__aenter__()
         return self
-
