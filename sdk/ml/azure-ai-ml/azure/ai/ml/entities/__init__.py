1c4d35d8ca1b11eef63e9e66746fcde6c670f530
Banibrata De <banibrata@outlook.com>
2023-02-15 14:25:17 -0800

    don't break public API

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/__init__.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/__init__.py
@@ -15,7 +15,7 @@ from ._assets._artifacts.data import Data
 from ._assets._artifacts.model import Model
 from ._assets.asset import Asset
 from ._assets.environment import BuildContext, Environment
-from ._assets.workspace_asset_reference import WorkspaceAssetReference
+from ._assets.workspace_asset_reference import WorkspaceAssetReference as WorkspaceModelReference
 from ._builders import Command, Parallel, Pipeline, Spark, Sweep
 from ._component.command_component import CommandComponent
 from ._component.component import Component
@@ -254,7 +254,7 @@ __all__ = [
     "SynapseSparkCompute",
     "AutoScaleSettings",
     "AutoPauseSettings",
-    "WorkspaceAssetReference",
+    "WorkspaceModelReference",
     # builders
     "Command",
     "Parallel",
