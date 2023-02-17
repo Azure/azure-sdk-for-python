6f1f3fcfa71783882729e3c5d740c26f35271573
nate <nathanfalke@microsoft.com>
2023-02-17 14:02:37 -0600

    updated requirements to match frozen

--- sdk/easm/azure-defender-easm/setup.py
+++ sdk/easm/azure-defender-easm/setup.py
@@ -63,9 +63,9 @@ setup(
         "pytyped": ["py.typed"],
     },
     install_requires=[
-        "isodate<1.0.0,>=0.6.1",
-        "azure-core<2.0.0,>=1.24.0",
-        "typing-extensions>=4.3.0; python_version<'3.8.0'",
+        "isodate>=0.6.0",
+        "azure-core<2.0.0,>=1.2.2",
+        "typing-extensions; python_version<'3.8.0'",
     ],
     python_requires=">=3.7",
 )
