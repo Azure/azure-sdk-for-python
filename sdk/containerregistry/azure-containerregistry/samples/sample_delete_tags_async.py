216d15c598d7a736357704a7ab63af210b7ea463
Yalin Li <yall@microsoft.com>
2023-02-19 15:34:35 -0800

    [ACR] Fix failing pipelines (#28875)

--- sdk/containerregistry/azure-containerregistry/samples/sample_delete_tags_async.py
+++ sdk/containerregistry/azure-containerregistry/samples/sample_delete_tags_async.py
@@ -7,20 +7,19 @@
 # --------------------------------------------------------------------------
 
 """
-FILE: sample_list_tags_async.py
+FILE: sample_delete_tags_async.py
 
 DESCRIPTION:
-    This sample demonstrates listing the tags for an image in a repository with anonymous pull access.
-    Anonymous access allows a user to list all the collections there, but they wouldn't have permissions to
-    modify or delete any of the images in the registry.
+    This sample demonstrates deleting all but the most recent three tags for each repository.
 
 USAGE:
-    python sample_list_tags_async.py
+    python sample_delete_tags_async.py
 
     Set the environment variables with your own values before running the sample:
     1) CONTAINERREGISTRY_ENDPOINT - The URL of you Container Registry account
 
-    This sample assumes your registry has a repository "library/hello-world", run load_registry() if you don't have.
+    This sample assumes your registry has at least one repository with more than three tags,
+    run load_registry() if you don't have.
     Set the environment variables with your own values before running load_registry():
     1) CONTAINERREGISTRY_ENDPOINT - The URL of you Container Registry account
     2) CONTAINERREGISTRY_TENANT_ID - The service principal's tenant ID
@@ -32,34 +31,41 @@ USAGE:
 import asyncio
 import os
 from dotenv import find_dotenv, load_dotenv
+from azure.containerregistry import ArtifactTagOrder
 from azure.containerregistry.aio import ContainerRegistryClient
-from samples.sample_utilities import load_registry, get_authority, get_audience, get_credential
+from utilities import load_registry, get_authority, get_audience, get_credential
 
 
-class ListTagsAsync(object):
+class DeleteTagsAsync(object):
     def __init__(self):
         load_dotenv(find_dotenv())
         self.endpoint = os.environ.get("CONTAINERREGISTRY_ENDPOINT")
         self.authority = get_authority(self.endpoint)
         self.audience = get_audience(self.authority)
-        self.credential = get_credential(
-            self.authority, exclude_environment_credential=True, is_async=True
-        )
+        self.credential = get_credential(self.authority, is_async=True)
 
-    async def list_tags(self):
+    async def delete_tags(self):
         load_registry()
-        # Instantiate an instance of ContainerRegistryClient    
         async with ContainerRegistryClient(self.endpoint, self.credential, audience=self.audience) as client:
-            manifest = await client.get_manifest_properties("library/hello-world", "latest")
-            print("Tags of " + manifest.repository_name + ": ")
-            # Iterate through all the tags
-            for tag in manifest.tags:
-                print(tag)
+            # [START list_repository_names]
+            async for repository in client.list_repository_names():
+                print(repository)
+                # [END list_repository_names]
+
+                # Keep the three most recent tags, delete everything else
+                tag_count = 0
+                async for tag in client.list_tag_properties(
+                    repository, order_by=ArtifactTagOrder.LAST_UPDATED_ON_DESCENDING
+                ):
+                    tag_count += 1
+                    if tag_count > 3:                        
+                        print(f"Deleting {repository}:{tag.name}")
+                        await client.delete_tag(repository, tag.name)
 
 
 async def main():
-    sample = ListTagsAsync()
-    await sample.list_tags()
+    sample = DeleteTagsAsync()
+    await sample.delete_tags()
 
 
 if __name__ == "__main__":
