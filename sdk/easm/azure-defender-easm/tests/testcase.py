aaab40ec7c8c475e0c93cf31a6f91ece16166863
nathanfalke <113559247+nathanfalke@users.noreply.github.com>
2023-02-22 13:41:38 -0600

    Azure Defender EASM Public Preview (#28450)
    * azure-defender-easm client initial commit
    
    * added tests
    
    * added samples
    
    * added swagger readme
    
    * updated tests to use test-proxy
    
    * updated swagger readme name/output folder
    
    * Out-of-repo recordings
    
    * updated changelog to current year
    
    * updated sdk to reflect most recent swagger json
    
    * disco_groups -> discovery_groups
    
    fixed missed disco_group in readme
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>
    
    * remove old language
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>
    
    * updated samples to use metadata and os environment variables
    
    * added table of contents to readme
    
    * added test proxy to ci
    
    * added easm to cspell
    
    * updated tests to use most recent sdk version
    
    * updated requirements to match frozen
    
    * updated asset tag
    
    * moved requirements changes to overrides
    
    * updated package name in ci
    
    * split shared requirements into separate lines
    
    * added pip link to ignore-links txt
    
    * added pylint ignore
    
    * updated pylint disable
    
    * updated readme links
    
    * added missing headers
    
    ---------
    
    Co-authored-by: mccoyp <mcpatino@microsoft.com>
    Co-authored-by: Anna Tisch <antisch@microsoft.com>

---
+++ sdk/easm/azure-defender-easm/tests/testcase.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import functools
+from devtools_testutils import AzureTestCase, AzureRecordedTestCase, EnvironmentVariableLoader
+from azure.defender.easm import EasmClient
+import datetime
+
+
+class EasmTest(AzureRecordedTestCase):
+    def create_client(self, endpoint, resource_group, subscription_id, workspace):
+        credential = self.get_credential(EasmClient)
+        return self.create_client_from_credential(
+            EasmClient,
+            credential=credential,
+            endpoint=endpoint,
+            resource_group_name=resource_group,
+            subscription_id=subscription_id,
+            workspace_name=workspace
+        )
+
+    def check_timestamp_format(self, time_format, timestamp):
+        try:
+            datetime.datetime.strptime(timestamp, time_format)
+        except ValueError as e:
+            assert False, 'invalid timestamp format'
+
+    def check_guid_format(self, guid):
+        parts = guid.split('-')
+        lengths = [8, 4, 4, 4, 12]
+        assert all(len(seg) == l for seg, l in zip(parts, lengths)), 'invalid guid'
+
+
+EasmParameterProvider = functools.partial(
+    EnvironmentVariableLoader,
+    "easm",
+    easm_endpoint="fake_endpoint.easm.defender.microsoft.com",
+    easm_resource_group="fake_resource_group",
+    easm_subscription_id="fake-sub-id",
+    easm_workspace="fakename"
+)
