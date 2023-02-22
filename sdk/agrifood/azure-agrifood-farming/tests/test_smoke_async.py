7bf4e22294033d9230029e4776c7372fae953041
Bhargav Kansagara <kansagara.bhargav1997@gmail.com>
2023-02-23 01:56:56 +0530

    [AgriFood] Updating clients for 2022-11-01-preview API version (#26161)

--- sdk/agrifood/azure-agrifood-farming/tests/test_smoke_async.py
+++ sdk/agrifood/azure-agrifood-farming/tests/test_smoke_async.py
@@ -3,52 +3,116 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-import datetime
-from azure.core.exceptions import HttpResponseError
-from azure.agrifood.farming.models import Farmer, SatelliteDataIngestionJob, SatelliteData
-from testcase_async import FarmBeatsTestAsync
+from datetime import datetime
+from dateutil.parser import parse
+from testcase_async import FarmBeatsAsyncTestCase
 from testcase import FarmBeatsPowerShellPreparer
-from isodate.tzinfo import Utc
-from random import randint
+from devtools_testutils.aio import recorded_by_proxy_async
 
-class FarmBeatsSmokeTestCaseAsync(FarmBeatsTestAsync):
 
+class TestFarmBeatsSmokeAsync(FarmBeatsAsyncTestCase):
     @FarmBeatsPowerShellPreparer()
-    async def test_farmer(self, agrifood_endpoint):
+    @recorded_by_proxy_async
+    async def test_party(self, **kwargs):
+        agrifood_endpoint = kwargs.pop("agrifood_endpoint")
         client = self.create_client(agrifood_endpoint=agrifood_endpoint)
 
-        farmer_id = self.generate_random_name("async-test-farmer")
+        party_id = "test-party-3957"
 
-        farmer = await client.farmers.create_or_update(
-            farmer_id=farmer_id,
-            farmer=Farmer()
+        party_request = {
+            "name": "Test Party",
+            "description": "Party created during testing.",
+            "status": "Sample Status",
+            "properties": {
+                "foo": "bar",
+                "numeric one": 1,
+                1: "numeric key"
+            }
+        }
+
+        # Setup client
+        client = self.create_client(agrifood_endpoint=agrifood_endpoint)
+
+        # Create
+        party_response = await client.parties.create_or_update(
+            party_id=party_id,
+            party=party_request
         )
 
-        assert farmer.id == farmer_id
-        assert farmer.e_tag
-        assert farmer.created_date_time
-        assert farmer.modified_date_time
+        # Assert on immediate response
+        assert party_response["id"] == party_id
+        assert party_response["name"] == party_response["name"]
+        assert party_response["description"] == party_response["description"]
+        assert party_response["status"] == party_response["status"]
 
-        retrieved_farmer = await client.farmers.get(farmer_id=farmer_id)
-        assert farmer.id == retrieved_farmer.id
-        assert farmer.e_tag == retrieved_farmer.e_tag
-        assert farmer.created_date_time == retrieved_farmer.created_date_time
-        assert farmer.modified_date_time == retrieved_farmer.modified_date_time
+        assert len(party_response["properties"]) == 3
+        assert party_response["properties"]["foo"] == "bar"
+        assert party_response["properties"]["numeric one"] == 1
+        assert party_response["properties"]["1"] == "numeric key"
 
-        await client.farmers.delete(farmer_id=farmer_id)
+        assert party_response["eTag"]
+        assert type(parse(party_response["createdDateTime"])) is datetime
+        assert type(parse(party_response["modifiedDateTime"])) is datetime
+
+        await client.parties.delete(party_id=party_id)
+        await self.close_client()
+
 
     @FarmBeatsPowerShellPreparer()
-    async def test_boundary(self, agrifood_endpoint):
+    @recorded_by_proxy_async
+    async def test_boundary(self, **kwargs):
+        agrifood_endpoint = kwargs.pop("agrifood_endpoint")
         client = self.create_client(agrifood_endpoint=agrifood_endpoint)
+
+        party_id = "smoke-test-party"
+        boundary_id = "smoke-test-boundary"
+
+        party_request = {
+            "name": "Test Party",
+            "description": "Party created during testing.",
+            "status": "Sample Status",
+            "properties": {
+                "foo": "bar",
+                "numeric one": 1,
+                1: "numeric key"
+            }
+        }
+        party = await client.parties.create_or_update(
+            party_id=party_id,
+            party=party_request
+        )
         
-        boundary_id = self.generate_random_name("async-test-boundary")
-        farmer_id = self.generate_random_name("async-test-boundary-farmer")
-        
-        farmer = await client.farmers.create_or_update(farmer_id=farmer_id, farmer=Farmer())
-        boundary = await self.create_boundary_if_not_exist(client, farmer_id, boundary_id)
+        boundary = await client.boundaries.create_or_update(
+            party_id=party_id,
+            boundary_id=boundary_id,
+            boundary={
+                "geometry":
+                {
+                    "type": "Polygon",
+                    "coordinates":
+                        [
+                            [
+                                [73.70457172393799, 20.545385304358106],
+                                [73.70457172393799, 20.545385304358106],
+                                [73.70448589324951, 20.542411534243367],
+                                [73.70877742767334, 20.541688176010233],
+                                [73.71023654937744, 20.545083911372505],
+                                [73.70663166046143, 20.546992723579137],
+                                [73.70457172393799, 20.545385304358106],
+                            ]
+                        ]
+                },
+                "status": "<string>",
+                "name": "<string>",
+                "description": "<string>"
+            }
+        )
+
         assert boundary == await client.boundaries.get(
-            farmer_id=farmer_id,
+            party_id=party_id,
             boundary_id=boundary_id
         )
-        await self.delete_boundary(client, farmer_id, boundary_id)
-        await client.farmers.delete(farmer_id=farmer_id)
+        await client.boundaries.delete(party_id=party_id, boundary_id=boundary_id)
+        await client.parties.delete(party_id=party_id)
+
+        await self.close_client()
