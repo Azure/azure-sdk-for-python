158b3d0239adfb76e4293efcbaa1768b55267c01
scbedd <45376673+scbedd@users.noreply.github.com>
2023-02-22 12:52:58 -0800

    cleaning up test_proxy_startup a bit

--- tools/azure-sdk-tools/tests/integration/test_proxy_startup.py
+++ tools/azure-sdk-tools/tests/integration/test_proxy_startup.py
@@ -1,21 +1,21 @@
 from devtools_testutils import test_proxy
 from urllib3 import PoolManager, Retry
+import os
+from devtools_testutils.config import PROXY_URL
 
-
-# if os.getenv("REQUESTS_CA_BUNDLE"):
-#     http_client = PoolManager(
-#         retries=Retry(total=3, raise_on_status=False),
-#         cert_reqs="CERT_REQUIRED",
-#         ca_certs=os.getenv("REQUESTS_CA_BUNDLE"),
-#     )
-# else:
-http_client = PoolManager(retries=Retry(total=1, raise_on_status=False))
+if os.getenv("REQUESTS_CA_BUNDLE"):
+    http_client = PoolManager(
+        retries=Retry(total=3, raise_on_status=False),
+        cert_reqs="CERT_REQUIRED",
+        ca_certs=os.getenv("REQUESTS_CA_BUNDLE"),
+    )
+else:
+    http_client = PoolManager(retries=Retry(total=1, raise_on_status=False))
 
 
 class TestProxyIntegration:
     # These tests are checking spinup of the proxy, not automatic redirect.
     # Therefor we are not using recorded_by_proxy decorator or recorded_test fixture
     def test_tool_spinup_http(self):
-        result = http_client.request("GET", "http://localhost:5000/Info/Available")
-
-        assert result.status == 200
+        result = http_client.request("GET", f"{PROXY_URL}/Info/Available")
+        assert(result.status == 200)
