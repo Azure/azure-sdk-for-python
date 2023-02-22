85d674bb63af1dc1338b1f5c77778a1ca2bb62ec
Renyi Xiong <renyix@microsoft.com>
2023-02-21 22:11:39 -0800

    data import - fix DataImportSchema import build error

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_data_import/data_import.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_data_import/data_import.py
@@ -7,7 +7,7 @@
 from os import PathLike
 from pathlib import Path
 from typing import Dict, Optional, Union
-from azure.ai.ml._schema._data_import.data_import import DataImportSchema
+from azure.ai.ml._schema import DataImportSchema
 from azure.ai.ml.constants._common import BASE_PATH_CONTEXT_KEY, PARAMS_OVERRIDE_KEY, AssetTypes
 from azure.ai.ml.entities._assets import Data
 from azure.ai.ml.entities._inputs_outputs.external_data import Database, FileSystem
