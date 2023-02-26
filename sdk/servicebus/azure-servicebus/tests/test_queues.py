f0246db71f5fb119c4d1b9fe6f19e0430266f59d
l0lawrence <llawrence@microsoft.com>
2023-02-25 18:09:51 -0800

    missing "_"

--- sdk/servicebus/azure-servicebus/tests/test_queues.py
+++ sdk/servicebus/azure-servicebus/tests/test_queues.py
@@ -328,7 +328,7 @@ class ServiceBusQueueTests(AzureMgmtTestCase):
                 with sender, receiver:
                     # send 5 msgs to queue first
                     sender.send_messages([ServiceBusMessage('test') for _ in range(5)])
-                    receiver._handler._link.on_transfer = types.MethodType(
+                    receiver._handler._link._on_transfer = types.MethodType(
                         _hack_disable_receive_context_message_received, receiver)
                     received_msgs = []
                     while len(received_msgs) < 5:
