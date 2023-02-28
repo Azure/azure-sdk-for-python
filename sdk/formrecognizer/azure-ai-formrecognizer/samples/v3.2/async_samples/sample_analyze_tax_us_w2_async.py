e2a4b2d9a4fae92d5a84d8db8d28beef17b92fe1
Catalina Peralta <catalinaperaltah@hotmail.com>
2023-02-27 16:33:03 -0800

    update error handling in samples

--- sdk/formrecognizer/azure-ai-formrecognizer/samples/v3.2/async_samples/sample_analyze_tax_us_w2_async.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/samples/v3.2/async_samples/sample_analyze_tax_us_w2_async.py
@@ -339,19 +339,17 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except HttpResponseError as error:
+        print("For more information about troubleshooting errors, see the following guide: "
+              "https://aka.ms/azsdk/python/formrecognizer/troubleshooting")
         # Examples of how to check an HttpResponseError
         # Check by error code:
         if error.error is not None:
-            if error.error.code == "InvalidRequest":
-                print(f"Received an invalid request error: {error.error}")
-                sys.exit(1)
             if error.error.code == "InvalidImage":
                 print(f"Received an invalid image error: {error.error}")
                 sys.exit(1)
         # If the inner error is None and then it is possible to check the message to get more information:
-        filter_msg = ["Generic error", "Timeout", "Invalid request", "InvalidImage"]
-        if any(example_error.casefold() in error.message.casefold() for example_error in filter_msg):
+        if "Invalid request".casefold() in error.message.casefold():
             print(f"Uh-oh! Something unexpected happened: {error}")
             sys.exit(1)
         # Print the full error content:
-        print(f"Full HttpResponseError: {error}")
+        print(f"Received a different error: {error}")
