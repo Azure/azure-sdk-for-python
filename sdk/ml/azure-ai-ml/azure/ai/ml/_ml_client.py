6fe7202366299f95eede6f94716eae9a71a0b6e3
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2023-02-28 11:11:35 -0800

    Define user_agent

--- sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
@@ -247,6 +247,10 @@ class MLClient:
         if registry_name:
             properties.update({"registry_name": registry_name})
 
+        user_agent = None
+        if "user_agent" in kwargs:
+            user_agent = kwargs.get("user_agent")
+
         app_insights_handler = get_appinsights_log_handler(user_agent, **{"properties": properties})
         app_insights_handler_kwargs = {"app_insights_handler": app_insights_handler}
 
