192723ffbd6fafcf4a79bf4bd9a039f067cca390
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2023-02-27 16:24:44 -0800

    Re-enable unittests

--- sdk/ml/azure-ai-ml/tests/internal_utils/unittests/test_logger_utils.py
+++ sdk/ml/azure-ai-ml/tests/internal_utils/unittests/test_logger_utils.py
@@ -1,64 +1,71 @@
-# import logging
-# from mock import patch
-# import pytest
+import logging
+from mock import patch
+import pytest
 
-# from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureMLSDKLogHandler
 
-# from azure.ai.ml._telemetry import AML_INTERNAL_LOGGER_NAMESPACE, get_appinsights_log_handler
-# from azure.ai.ml._utils._logger_utils import OpsLogger, initialize_logger_info
-# from azure.ai.ml._user_agent import USER_AGENT
-
-
-# @pytest.mark.unittest
-# class TestLoggerUtils:
-#     def test_initialize_logger_info(self) -> None:
-#         test_name = "test"
-#         test_terminator = "\n\n"
-#         module_logger = logging.getLogger(test_name)
-
-#         initialize_logger_info(module_logger, terminator=test_terminator)
-
-#         assert module_logger.level == logging.INFO
-#         assert not module_logger.propagate
-#         assert module_logger.hasHandlers()
-#         assert module_logger.handlers[0].terminator == test_terminator
+from azure.ai.ml._telemetry import AML_INTERNAL_LOGGER_NAMESPACE, get_appinsights_log_handler
+from azure.ai.ml._utils._logger_utils import OpsLogger, initialize_logger_info
+from azure.ai.ml._user_agent import USER_AGENT
 
 
-# @pytest.mark.unittest
-# class TestOpsLogger:
-#     def test_init(self) -> None:
-#         test_name = "test"
-#         test_logger = OpsLogger(name=test_name)
+@pytest.mark.unittest
+class TestLoggerUtils:
+    def test_initialize_logger_info(self) -> None:
+        test_name = "test"
+        test_terminator = "\n\n"
+        module_logger = logging.getLogger(test_name)
 
-#         assert test_logger is not None
-#         assert test_logger.package_logger.name == AML_INTERNAL_LOGGER_NAMESPACE + test_name
-#         assert not test_logger.package_logger.propagate
-#         assert test_logger.module_logger.name == test_name
-#         assert len(test_logger.custom_dimensions) == 0
+        initialize_logger_info(module_logger, terminator=test_terminator)
 
-#     def test_update_info(self) -> None:
-#         test_name = "test"
-#         test_handler = logging.NullHandler()
-#         test_data = {"app_insights_handler": test_handler}
+        assert module_logger.level == logging.INFO
+        assert not module_logger.propagate
+        assert module_logger.hasHandlers()
+        assert module_logger.handlers[0].terminator == test_terminator
 
-#         test_logger = OpsLogger(name=test_name)
-#         test_logger.update_info(test_data)
 
-#         assert len(test_data) == 0
-#         assert test_logger.package_logger.hasHandlers()
-#         assert test_logger.package_logger.handlers[0] == test_handler
+@pytest.mark.unittest
+class TestOpsLogger:
+    def test_init(self) -> None:
+        test_name = "test"
+        test_logger = OpsLogger(name=test_name)
 
-#     def test_disabled_logging(self) -> None:
-#         with patch(
-#             "azure.ai.ml._telemetry.logging_handler.is_telemetry_collection_disabled",
-#             return_value=True
-#         ):
-#             handler = get_appinsights_log_handler(user_agent=USER_AGENT)
-#             assert isinstance(handler, logging.NullHandler)
+        assert test_logger is not None
+        assert test_logger.package_logger.name == AML_INTERNAL_LOGGER_NAMESPACE + test_name
+        assert not test_logger.package_logger.propagate
+        assert test_logger.module_logger.name == test_name
+        assert len(test_logger.custom_dimensions) == 0
 
-#         with patch(
-#             "azure.ai.ml._telemetry.logging_handler.is_telemetry_collection_disabled",
-#             return_value=False
-#         ):
-#             handler = get_appinsights_log_handler(user_agent=USER_AGENT)
-#             assert isinstance(handler, AzureLogHandler)
+    def test_update_info(self) -> None:
+        test_name = "test"
+        test_handler = logging.NullHandler()
+        test_data = {"app_insights_handler": test_handler}
+
+        test_logger = OpsLogger(name=test_name)
+        test_logger.update_info(test_data)
+
+        assert len(test_data) == 0
+        assert test_logger.package_logger.hasHandlers()
+        assert test_logger.package_logger.handlers[0] == test_handler
+
+    def test_disabled_logging(self) -> None:
+        with patch(
+            "azure.ai.ml._telemetry.logging_handler.is_telemetry_collection_disabled",
+            return_value=True
+        ):
+            handler = get_appinsights_log_handler(user_agent=USER_AGENT)
+            assert isinstance(handler, logging.NullHandler)
+
+        with patch(
+            "azure.ai.ml._telemetry.logging_handler.in_jupyter_notebook",
+            return_value=True
+        ):
+            handler = get_appinsights_log_handler(user_agent=USER_AGENT)
+            assert isinstance(handler, logging.NullHandler)
+
+        with patch(
+            "azure.ai.ml._telemetry.logging_handler.is_telemetry_collection_disabled",
+            return_value=False
+        ):
+            handler = get_appinsights_log_handler(user_agent=USER_AGENT)
+            assert isinstance(handler, AzureMLSDKLogHandler)
