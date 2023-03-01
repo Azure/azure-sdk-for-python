aad7ce2786cd0d6ddae43278f5cc9647f2dd04eb
zhrua <zhrua@microsoft.com>
2023-03-01 11:09:30 +0800

    fix code style

--- sdk/ml/azure-ai-ml/azure/ai/ml/_internal/_schema/environment.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_internal/_schema/environment.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from marshmallow import fields, pre_load
+from marshmallow import fields
 
 from azure.ai.ml._schema import PathAwareSchema
 from azure.ai.ml._schema.core.fields import DumpableEnumField, VersionField
