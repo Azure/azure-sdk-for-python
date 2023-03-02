a9ea1159adea67b5c4479761312410a17db7e8dc
Paul Van Eck <paulvaneck@microsoft.com>
2023-03-01 17:08:18 -0800

    [Core][OTel] Tracing typing updates
    * Also updated the README to point to the right package
      for the OTel exporter for Azure Monitor.
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/core/azure-core-tracing-opentelemetry/setup.py
+++ sdk/core/azure-core-tracing-opentelemetry/setup.py
@@ -44,7 +44,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -59,7 +58,7 @@ setup(
     package_data={
         "pytyped": ["py.typed"],
     },
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         "opentelemetry-api<2.0.0,>=1.0.0",
         "azure-core<2.0.0,>=1.13.0",
