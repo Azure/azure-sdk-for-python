38dd457043076cebc35e8b39e410cd8cc2ce0281
Jacob Lauzon <jalauzon@microsoft.com>
2023-03-01 17:02:13 -0800

    Fix lint

--- sdk/storage/azure-storage-file-share/azure/storage/fileshare/_share_client.py
+++ sdk/storage/azure-storage-file-share/azure/storage/fileshare/_share_client.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+# pylint: disable=too-many-lines
 
 import sys
 from typing import (
