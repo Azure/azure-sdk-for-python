047580623deb0c2691541dd197f4f39a40bb18fe
TonyJ1 <41344586+TonyJ1@users.noreply.github.com>
2023-03-02 02:52:37 -0800

    ComputeRuntimeDTO and FeatureStoreSettings depends on v2022_12_01_preview

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_workspace/compute_runtime.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_workspace/compute_runtime.py
@@ -4,7 +4,7 @@
 
 from typing import Optional
 
-from azure.ai.ml._restclient.v2023_02_01_preview.models import ComputeRuntimeDto as RestComputeRuntimeDto
+from azure.ai.ml._restclient.v2022_12_01_preview.models import ComputeRuntimeDto as RestComputeRuntimeDto
 from azure.ai.ml.entities._mixins import RestTranslatableMixin
 from azure.ai.ml._utils._experimental import experimental
 
