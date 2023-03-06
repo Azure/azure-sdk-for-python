20d6ad12412d77632fd055b5c58f2f19bd1fe396
Yaman Wahsheh <yamanwahsheh@microsoft.com>
2023-03-06 15:31:33 -0800

    Update azure core dependency min version

--- sdk/purview/azure-purview-sharing/setup.py
+++ sdk/purview/azure-purview-sharing/setup.py
@@ -84,7 +84,7 @@ setup(
     },
     python_requires=">=3.6",
     install_requires=[
-        "azure-core<2.0.0,>=1.24.0",
+        "azure-core<2.0.0,>=1.23.0",
         "msrest>=0.6.21",
         'six>=1.11.0',
     ],
