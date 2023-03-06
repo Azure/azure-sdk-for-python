49bc9221f9e98f543e3f6a3340bfbfd89eb17983
Njuguna Thande <nthande@microsoft.com>
2023-03-06 12:50:16 -0500

    Formatting fixes

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_compute/_image_metadata.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_compute/_image_metadata.py
@@ -1,7 +1,6 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
-from azure.ai.ml._utils._experimental import experimental
 
 
 class ImageMetadata:
