05772aee6804d8bf93debad2c17b09dafbb24005
Mayank Kumar <maykumar@microsoft.com>
2023-03-07 10:26:13 -0800

    Updating init file for schema

---
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_feature_store/__init__.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+from .compute_runtime_schema import ComputeRuntimeSchema
+from .feature_store_schema import FeatureStoreSchema
+from .managed_identity_schema import ManagedIdentityConfigurationSchema
+from .materialization_store_schema import MaterializationStoreSchema
+
+__all__ = [
+    "ComputeRuntimeSchema",
+    "FeatureStoreSchema",
+    "ManagedIdentityConfigurationSchema",
+    "MaterializationStoreSchema",
+]
