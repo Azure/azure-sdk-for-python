064fa6aae24e91df6b06b194dfc355bfdf70d0f3
PythonSdkPipelines <PythonSdkPipelines>
2023-03-07 02:09:33 +0000

    code and test

--- sdk/storagemover/azure-mgmt-storagemover/CHANGELOG.md
+++ sdk/storagemover/azure-mgmt-storagemover/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release History
 
+## 0.0.0 (it should be stable) (2023-03-07)
+
+
+
 ## 1.0.0b1 (2023-02-20)
 
 * Initial Release
--- sdk/storagemover/azure-mgmt-storagemover/generated_samples/storage_movers_list_by_subscription.py
+++ sdk/storagemover/azure-mgmt-storagemover/generated_samples/storage_movers_list_by_subscription.py
@@ -34,6 +34,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/preview/2022-07-01-preview/examples/StorageMovers_ListBySubscription.json
+# x-ms-original-file: specification/storagemover/resource-manager/Microsoft.StorageMover/stable/2023-03-01/examples/StorageMovers_ListBySubscription.json
 if __name__ == "__main__":
     main()
