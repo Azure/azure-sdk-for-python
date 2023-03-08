ab2e0ea91778b2e43e04c17267dbd1cc4e921c3d
Hanchi Wang <hancwang@microsoft.com>
2023-03-08 15:59:26 -0800

    Support get VC by ARM ID

--- sdk/ml/azure-ai-ml/azure/ai/ml/operations/_virtual_cluster_operations.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/operations/_virtual_cluster_operations.py
@@ -8,11 +8,13 @@ from azure.ai.ml._scope_dependent_operations import OperationScope
 
 from azure.ai.ml._telemetry import ActivityType, monitor_with_activity
 from azure.ai.ml._utils._logger_utils import OpsLogger
-from azure.ai.ml._utils.azure_resource_utils import get_virtual_clusters_from_subscriptions, get_virtual_cluster_by_id
+from azure.ai.ml.exceptions import ValidationException
+from azure.ai.ml._utils.azure_resource_utils import get_virtual_clusters_from_subscriptions, get_virtual_cluster_by_id, get_generic_resource_by_id
 from azure.ai.ml.constants._common import Scope
 from azure.ai.ml.exceptions import UserErrorException
 from azure.core.tracing.decorator import distributed_trace
 from azure.core.credentials import TokenCredential
+from azure.ai.ml._utils._arm_id_utils import AzureResourceId
 
 ops_logger = OpsLogger(__name__)
 logger, module_logger = ops_logger.package_logger, ops_logger.module_logger
@@ -62,17 +64,26 @@ class VirtualClusterOperations:
     @distributed_trace
     @monitor_with_activity(logger, "VirtualCluster.Get", ActivityType.PUBLICAPI)
     def get(self, name: str) -> Dict:
-        """Get a virtual cluster resource.
+        """
+        Get a virtual cluster resource. If name is provided, the virtual cluster
+        with the name in the subscription and resource group of the MLClient object
+        will be returned. If an ARM id is provided, a virtual cluster with the id will be returned.
 
-        :param name: Name of the virtual cluster.
+        :param name: Name or ARM ID of the virtual cluster.
         :type name: str
         :return: Virtual cluster object
         :rtype: Dict
         """
 
-        return get_virtual_cluster_by_id(
-            name=name,
-            resource_group=self._resource_group_name,
-            subscription_id=self._subscription_id,
-            credential=self._credentials,
-        )
+        try :
+            arm_id = AzureResourceId(name)
+            sub_id = arm_id.subscription_id
+
+            return get_generic_resource_by_id(name, self._credentials, sub_id)
+        except ValidationException:
+            return get_virtual_cluster_by_id(
+                name=name,
+                resource_group=self._resource_group_name,
+                subscription_id=self._subscription_id,
+                credential=self._credentials,
+            )
