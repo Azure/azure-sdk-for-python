681f76d04dd0259dee41679014bf4d52b58e7c64
Banibrata De <banibrata@outlook.com>
2023-03-08 12:43:31 -0800

    fix

--- sdk/ml/azure-ai-ml/azure/ai/ml/_utils/_http_utils.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_utils/_http_utils.py
@@ -58,6 +58,7 @@ def _request_function(f: Callable[["HttpPipeline"], None]):
                 content=kwargs.pop("content", None),
                 data=kwargs.pop("data", None),
                 files=kwargs.pop("files", None),
+                url=kwargs.pop("url", None),
             )
 
             return self.run(request, **kwargs).http_response
