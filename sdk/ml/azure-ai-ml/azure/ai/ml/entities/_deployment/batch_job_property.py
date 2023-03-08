26b49caac338a34c9e0e37922c64ecc5af2f5c6c
Taji Harris <tajiharris@microsoft.com>
2023-03-08 13:17:11 -0800

    expose batch job resource

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_deployment/batch_job_property.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_deployment/batch_job_property.py
@@ -3,8 +3,7 @@
 # ---------------------------------------------------------
 
 from azure.ai.ml._restclient.v2020_09_01_dataplanepreview.models import BatchJobResource
-from typing import Any, Dict, Optional, Union
-from azure.ai.ml.entities import ComputeConfiguration
+
 
 class BatchJob(object):
     """Batch jobs that are created with batch deployments/endpoints invocation.
@@ -13,27 +12,7 @@ class BatchJob(object):
     invocation and job listing.
     """
 
-    def __init__(
-        self,
-        *,
-        compute: Optional[ComputeConfiguration] = None,
-        dataset: Optional["InferenceDataInputBase"] = None,
-        description: Optional[str] = None,
-        error_threshold: Optional[int] = None,
-        input_data: Optional[Dict[str, "JobInput"]] = None,
-        logging_level: Optional[Union[str, "BatchLoggingLevel"]] = None,
-        max_concurrency_per_instance: Optional[int] = None,
-        mini_batch_size: Optional[int] = None,
-        name: Optional[str] = None,
-        output_data: Optional[Dict[str, "JobOutputV2"]] = None,
-        output_dataset: Optional["DataVersion"] = None,
-        output_file_name: Optional[str] = None,
-        partition_keys: Optional[List[str]] = None,
-        properties: Optional[Dict[str, str]] = None,
-        retry_settings: Optional["BatchRetrySettings"] = None,
-        tags: Optional[Dict[str, str]] = None,
-        **kwargs       
-        ):
+    def __init__(self, **kwargs):
         self.id = kwargs.get("id", None)
         self.name = kwargs.get("name", None)
         self.type = kwargs.get("type", None)
