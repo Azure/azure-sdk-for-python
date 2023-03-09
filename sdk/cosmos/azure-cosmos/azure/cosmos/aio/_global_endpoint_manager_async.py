136fbfebf4462173652fa45bbdabb94c03444401
simorenoh <simonmorenohe@gmail.com>
2023-03-09 16:22:52 -0500

    Update _global_endpoint_manager_async.py

--- sdk/cosmos/azure-cosmos/azure/cosmos/aio/_global_endpoint_manager_async.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/aio/_global_endpoint_manager_async.py
@@ -132,7 +132,7 @@ class _GlobalEndpointManager(object):
                 except exceptions.CosmosHttpResponseError:
                     pass
 
-            return None
+            raise
 
     async def _GetDatabaseAccountStub(self, endpoint, **kwargs):
         """Stub for getting database account from the client.
