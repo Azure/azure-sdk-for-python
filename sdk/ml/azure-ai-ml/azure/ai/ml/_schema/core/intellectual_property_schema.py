3592b812add54890b0d31830bdc29b07b67e2ee9
Nancy Mejia Juarez <namejia@microsoft.com>
2023-03-09 12:07:54 -0800

    Fix pylint

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/core/intellectual_property_schema.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/core/intellectual_property_schema.py
@@ -1,4 +1,8 @@
-from marshmallow import fields, post_load
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+from marshmallow import fields
 from azure.ai.ml._schema.core.schema import PatchedSchemaMeta
 from azure.ai.ml._utils._experimental import experimental
 
