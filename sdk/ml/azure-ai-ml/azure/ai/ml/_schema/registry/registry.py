699151366bdf184eb97131139b63c6b8536fdf74
Man <43016276+Man-MSFT@users.noreply.github.com>
2023-03-09 15:09:29 -0800

    Revert "Remove experimental decorators for registry  (#29010)" (#29258)
    This reverts commit cb6f2415b110f9b65e2a346fcb937bcb2f99d46f.

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/registry/registry.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/registry/registry.py
@@ -8,6 +8,7 @@ from azure.ai.ml._schema.core.fields import DumpableStringField, NestedField, St
 from azure.ai.ml._schema.core.intellectual_property_schema import IntellectualPropertySchema
 from azure.ai.ml._schema.core.resource import ResourceSchema
 from azure.ai.ml._schema.workspace.identity import IdentitySchema
+from azure.ai.ml._utils._experimental import experimental
 from azure.ai.ml._utils.utils import snake_to_pascal
 from azure.ai.ml.constants._common import PublicNetworkAccess
 from azure.ai.ml.constants._registry import AcrAccountSku
@@ -19,6 +20,7 @@ from .util import acr_format_validator
 
 
 # Based on 10-01-preview api
+@experimental
 class RegistrySchema(ResourceSchema):
     # Inherits name, id, tags, and description fields from ResourceSchema
 
