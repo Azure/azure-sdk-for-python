a71c7bab718ab59f4b4d8ff8970e14241b28d43b
Isura Kumarapeli <86977367+ikumarapeli-msft@users.noreply.github.com>
2023-03-10 16:43:12 -0800

    pr comments for recording (#29294)
    * pr comments for recording
    
    * pr comments for recording

--- sdk/communication/azure-communication-callautomation/azure/communication/callautomation/_models.py
+++ sdk/communication/azure-communication-callautomation/azure/communication/callautomation/_models.py
@@ -33,6 +33,12 @@ from ._shared.models import (
 )
 
 class ServerCallLocator(object):
+    """
+    The locator used for joining or taking action on a server call.
+
+    :ivar locator_id: The server call id.
+    :vartype locator_id: str
+    """
     def __init__(
         self,
         *,
@@ -52,6 +58,12 @@ class ServerCallLocator(object):
 
 
 class GroupCallLocator(object):
+    """
+    The locator used for joining or taking action on a group call.
+
+    :ivar locator_id: The group call id.
+    :vartype locator_id: str
+    """
     def __init__(
         self,
         *,
