de278339e88b524d7d0f488d6825ca5951d9789e
Arun Rajora <arunrajora@microsoft.com>
2023-03-13 15:25:22 +0530

    Add unittests for classification jobs.

--- sdk/ml/azure-ai-ml/tests/automl_job/unittests/test_classification_job.py
+++ sdk/ml/azure-ai-ml/tests/automl_job/unittests/test_classification_job.py
@@ -9,6 +9,7 @@ from azure.ai.ml import UserIdentityConfiguration
 from azure.ai.ml._restclient.v2022_12_01_preview.models import CustomNCrossValidations, MLTableJobInput
 from azure.ai.ml._restclient.v2022_12_01_preview.models import UserIdentity as RestUserIdentity
 from azure.ai.ml.automl import ClassificationModels, ClassificationPrimaryMetrics, classification
+from azure.ai.ml.constants import TabularTrainingMode
 from azure.ai.ml.constants._common import AssetTypes
 from azure.ai.ml.entities._inputs_outputs import Input
 from azure.ai.ml.entities._job.automl.tabular import ClassificationJob
@@ -71,6 +72,119 @@ class TestAutoMLClassification:
         assert original_obj.training.blocked_training_algorithms == [
             ClassificationModels.LOGISTIC_REGRESSION
         ], "Blocked models not set correctly"
+        assert original_obj.training.training_mode == None, "Training mode not set correctly"
+        assert original_obj.limits.max_nodes == None, "Max nodes not set correctly"
+
+    def test_classification_task_distributed_mode(self):
+        # Create AutoML Classification Task
+        identity = UserIdentityConfiguration()
+        classification_job = classification(
+            training_data=Input(type=AssetTypes.MLTABLE, path="https://foo/bar/train.csv"),
+            target_column_name="target",
+            positive_label="label",
+            enable_model_explainability=True,
+            test_data=Input(type=AssetTypes.MLTABLE, path="https://foo/bar/test.csv"),
+            validation_data_size=0.2,
+            # Job attributes
+            compute="cpucluster",
+            name="classifier_job",
+            experiment_name="foo_exp",
+            tags={"foo_tag": "bar"},
+            properties={"_automl_internal_some_flag": True},
+            identity=identity,
+        )  # type: ClassificationJob
+        # classification_task.set_limits(timeout=60, max_trials=100, max_concurrent_trials=4)
+        classification_job.limits = {"timeout_minutes": 60, "max_trials": 100, "max_concurrent_trials": 4, "max_nodes": 4}
+        classification_job.set_training(
+            allowed_training_algorithms=[ClassificationModels.LIGHT_GBM],
+            training_mode=TabularTrainingMode.DISTRIBUTED,
+        )
+        classification_job.set_featurization(enable_dnn_featurization=True)
+
+        rest_obj = classification_job._to_rest_object()
+        assert isinstance(rest_obj.properties.identity, RestUserIdentity)
+
+        original_obj = ClassificationJob._from_rest_object(rest_obj)
+        assert classification_job == original_obj, "Conversion to/from rest object failed"
+        assert original_obj.training.allowed_training_algorithms == [
+            ClassificationModels.LIGHT_GBM
+        ], "Allowed models not set correctly"
+        assert original_obj.training.training_mode == TabularTrainingMode.DISTRIBUTED, "Training mode not set correctly"
+        assert original_obj.limits.max_nodes == 4, "Max nodes not set correctly"
+
+    def test_classification_task_non_distributed_mode(self):
+        # Create AutoML Classification Task
+        identity = UserIdentityConfiguration()
+        classification_job = classification(
+            training_data=Input(type=AssetTypes.MLTABLE, path="https://foo/bar/train.csv"),
+            target_column_name="target",
+            positive_label="label",
+            enable_model_explainability=True,
+            test_data=Input(type=AssetTypes.MLTABLE, path="https://foo/bar/test.csv"),
+            validation_data_size=0.2,
+            # Job attributes
+            compute="cpucluster",
+            name="classifier_job",
+            experiment_name="foo_exp",
+            tags={"foo_tag": "bar"},
+            properties={"_automl_internal_some_flag": True},
+            identity=identity,
+        )  # type: ClassificationJob
+        # classification_task.set_limits(timeout=60, max_trials=100, max_concurrent_trials=4)
+        classification_job.limits = {"timeout_minutes": 60, "max_trials": 100, "max_concurrent_trials": 4}
+        classification_job.set_training(
+            allowed_training_algorithms=[ClassificationModels.LIGHT_GBM],
+            training_mode=TabularTrainingMode.NON_DISTRIBUTED,
+        )
+        classification_job.set_featurization(enable_dnn_featurization=True)
+
+        rest_obj = classification_job._to_rest_object()
+        assert isinstance(rest_obj.properties.identity, RestUserIdentity)
+
+        original_obj = ClassificationJob._from_rest_object(rest_obj)
+        assert classification_job == original_obj, "Conversion to/from rest object failed"
+        assert original_obj.training.allowed_training_algorithms == [
+            ClassificationModels.LIGHT_GBM
+        ], "Allowed models not set correctly"
+        assert original_obj.training.training_mode == TabularTrainingMode.NON_DISTRIBUTED, "Training mode not set correctly"
+        assert original_obj.limits.max_nodes == None, "Max nodes not set correctly"
+
+    def test_classification_task_auto_mode(self):
+        # Create AutoML Classification Task
+        identity = UserIdentityConfiguration()
+        classification_job = classification(
+            training_data=Input(type=AssetTypes.MLTABLE, path="https://foo/bar/train.csv"),
+            target_column_name="target",
+            positive_label="label",
+            enable_model_explainability=True,
+            test_data=Input(type=AssetTypes.MLTABLE, path="https://foo/bar/test.csv"),
+            validation_data_size=0.2,
+            # Job attributes
+            compute="cpucluster",
+            name="classifier_job",
+            experiment_name="foo_exp",
+            tags={"foo_tag": "bar"},
+            properties={"_automl_internal_some_flag": True},
+            identity=identity,
+        )  # type: ClassificationJob
+        # classification_task.set_limits(timeout=60, max_trials=100, max_concurrent_trials=4)
+        classification_job.limits = {"timeout_minutes": 60, "max_trials": 100, "max_concurrent_trials": 4, "max_nodes": 4}
+        classification_job.set_training(
+            allowed_training_algorithms=[ClassificationModels.LIGHT_GBM],
+            training_mode=TabularTrainingMode.AUTO,
+        )
+        classification_job.set_featurization(enable_dnn_featurization=True)
+
+        rest_obj = classification_job._to_rest_object()
+        assert isinstance(rest_obj.properties.identity, RestUserIdentity)
+
+        original_obj = ClassificationJob._from_rest_object(rest_obj)
+        assert classification_job == original_obj, "Conversion to/from rest object failed"
+        assert original_obj.training.allowed_training_algorithms == [
+            ClassificationModels.LIGHT_GBM
+        ], "Allowed models not set correctly"
+        assert original_obj.training.training_mode == TabularTrainingMode.AUTO, "Training mode not set correctly"
+        assert original_obj.limits.max_nodes == 4, "Max nodes not set correctly"
 
     @pytest.mark.parametrize(
         "n_cross_validations",
