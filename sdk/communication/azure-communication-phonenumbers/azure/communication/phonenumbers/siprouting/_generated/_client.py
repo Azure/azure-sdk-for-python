5662c9fd27ac396a53dc7da46b42af88ffd6cd81
Jiri Burant <83883278+jiriburant@users.noreply.github.com>
2023-03-14 10:56:27 +0100

    Jiriburant/ga version of sip routing (#29217)
    * Regenerated the SDKs.
    
    * Updated siprouting to GA version.
    
    * Added ability to skip tests.
    
    * Split SipTrunkRoute public interface and api side.
    
    * Fixing lint issues.
    
    * Updated SipTrunkRoute model to match the previous one.
    
    * Recording new tests.
    
    * Reverting changes on skipping the sip routing tests.
    
    * Updated CHANGELOG.md
    
    * Fixing PR comments.
    
    * Fixing comment from apiview. get_trunk throws on not found instead of returning None.

--- sdk/communication/azure-communication-phonenumbers/azure/communication/phonenumbers/siprouting/_generated/_client.py
+++ sdk/communication/azure-communication-phonenumbers/azure/communication/phonenumbers/siprouting/_generated/_client.py
@@ -7,64 +7,54 @@
 # --------------------------------------------------------------------------
 
 from copy import deepcopy
-from typing import TYPE_CHECKING
-
-from msrest import Deserializer, Serializer
+from typing import Any
 
 from azure.core import PipelineClient
+from azure.core.rest import HttpRequest, HttpResponse
 
-from . import models
+from . import models as _models
 from ._configuration import SIPRoutingServiceConfiguration
-from .operations import SIPRoutingServiceOperationsMixin
+from ._serialization import Deserializer, Serializer
+from .operations import SipRoutingOperations
 
-if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
-    from typing import Any
 
-    from azure.core.rest import HttpRequest, HttpResponse
-
-class SIPRoutingService(SIPRoutingServiceOperationsMixin):
+class SIPRoutingService:  # pylint: disable=client-accepts-api-version-keyword
     """SipRouting Service.
 
+    :ivar sip_routing: SipRoutingOperations operations
+    :vartype sip_routing:
+     azure.communication.phonenumbers.siprouting.operations.SipRoutingOperations
     :param endpoint: The communication resource, for example
-     https://resourcename.communication.azure.com.
+     https://resourcename.communication.azure.com. Required.
     :type endpoint: str
-    :keyword api_version: Api Version. Default value is "2021-05-01-preview". Note that overriding
-     this default value may result in unsupported behavior.
+    :keyword api_version: Api Version. Default value is "2023-03-01". Note that overriding this
+     default value may result in unsupported behavior.
     :paramtype api_version: str
     """
 
-    def __init__(
-        self,
-        endpoint,  # type: str
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> None
-        _base_url = '{endpoint}'
+    def __init__(  # pylint: disable=missing-client-constructor-parameter-credential
+        self, endpoint: str, **kwargs: Any
+    ) -> None:
+        _endpoint = "{endpoint}"
         self._config = SIPRoutingServiceConfiguration(endpoint=endpoint, **kwargs)
-        self._client = PipelineClient(base_url=_base_url, config=self._config, **kwargs)
+        self._client: PipelineClient = PipelineClient(base_url=_endpoint, config=self._config, **kwargs)
 
-        client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
+        client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
+        self.sip_routing = SipRoutingOperations(self._client, self._config, self._serialize, self._deserialize)
 
-
-    def _send_request(
-        self,
-        request,  # type: HttpRequest
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> HttpResponse
+    def send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
         >>> request = HttpRequest("GET", "https://www.example.org/")
         <HttpRequest [GET], url: 'https://www.example.org/'>
-        >>> response = client._send_request(request)
+        >>> response = client.send_request(request)
         <HttpResponse: 200 OK>
 
-        For more information on this code flow, see https://aka.ms/azsdk/python/protocol/quickstart
+        For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
 
         :param request: The network request you want to make. Required.
         :type request: ~azure.core.rest.HttpRequest
@@ -75,21 +65,18 @@ class SIPRoutingService(SIPRoutingServiceOperationsMixin):
 
         request_copy = deepcopy(request)
         path_format_arguments = {
-            "endpoint": self._serialize.url("self._config.endpoint", self._config.endpoint, 'str', skip_quote=True),
+            "endpoint": self._serialize.url("self._config.endpoint", self._config.endpoint, "str", skip_quote=True),
         }
 
         request_copy.url = self._client.format_url(request_copy.url, **path_format_arguments)
         return self._client.send_request(request_copy, **kwargs)
 
-    def close(self):
-        # type: () -> None
+    def close(self) -> None:
         self._client.close()
 
-    def __enter__(self):
-        # type: () -> SIPRoutingService
+    def __enter__(self) -> "SIPRoutingService":
         self._client.__enter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
+    def __exit__(self, *exc_details: Any) -> None:
         self._client.__exit__(*exc_details)
