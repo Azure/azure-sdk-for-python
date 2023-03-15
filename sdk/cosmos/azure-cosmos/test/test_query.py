bfc00a2c99c104c7602dbd4fdb8c22b7053504cb
simorenoh <simonmorenohe@gmail.com>
2023-03-15 15:06:25 -0400

    Update test_query.py

--- sdk/cosmos/azure-cosmos/test/test_query.py
+++ sdk/cosmos/azure-cosmos/test/test_query.py
@@ -52,11 +52,222 @@ class QueryTest(unittest.TestCase):
         self.assertEqual(iter_list[0]['id'], doc_id)
 
     def test_query_change_feed_with_pk(self):
-        self.query_change_feed(True)
+        created_collection = self.created_db.create_container_if_not_exists("change_feed_test_" + str(uuid.uuid4()),
+                                                                            PartitionKey(path="/pk"))
+        # The test targets partition #3
+        partition_key = "pk"
+        partitionParam = {"partition_key": partition_key}
+
+        # Read change feed without passing any options
+        query_iterable = created_collection.query_items_change_feed()
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 0)
+
+        # Read change feed from current should return an empty list
+        query_iterable = created_collection.query_items_change_feed(**partitionParam)
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 0)
+        self.assertTrue('etag' in created_collection.client_connection.last_response_headers)
+        self.assertNotEqual(created_collection.client_connection.last_response_headers['etag'], '')
+
+        # Read change feed from beginning should return an empty list
+        query_iterable = created_collection.query_items_change_feed(
+            is_start_from_beginning=True,
+            **partitionParam
+        )
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 0)
+        self.assertTrue('etag' in created_collection.client_connection.last_response_headers)
+        continuation1 = created_collection.client_connection.last_response_headers['etag']
+        self.assertNotEqual(continuation1, '')
+
+        # Create a document. Read change feed should return be able to read that document
+        document_definition = {'pk': 'pk', 'id': 'doc1'}
+        created_collection.create_item(body=document_definition)
+        query_iterable = created_collection.query_items_change_feed(
+            is_start_from_beginning=True,
+            **partitionParam
+        )
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 1)
+        self.assertEqual(iter_list[0]['id'], 'doc1')
+        self.assertTrue('etag' in created_collection.client_connection.last_response_headers)
+        continuation2 = created_collection.client_connection.last_response_headers['etag']
+        self.assertNotEqual(continuation2, '')
+        self.assertNotEqual(continuation2, continuation1)
+
+        # Create two new documents. Verify that change feed contains the 2 new documents
+        # with page size 1 and page size 100
+        document_definition = {'pk': 'pk', 'id': 'doc2'}
+        created_collection.create_item(body=document_definition)
+        document_definition = {'pk': 'pk', 'id': 'doc3'}
+        created_collection.create_item(body=document_definition)
+
+        for pageSize in [1, 100]:
+            # verify iterator
+            query_iterable = created_collection.query_items_change_feed(
+                continuation=continuation2,
+                max_item_count=pageSize,
+                **partitionParam
+            )
+            it = query_iterable.__iter__()
+            expected_ids = 'doc2.doc3.'
+            actual_ids = ''
+            for item in it:
+                actual_ids += item['id'] + '.'
+            self.assertEqual(actual_ids, expected_ids)
+
+            # verify by_page
+            # the options is not copied, therefore it need to be restored
+            query_iterable = created_collection.query_items_change_feed(
+                continuation=continuation2,
+                max_item_count=pageSize,
+                **partitionParam
+            )
+            count = 0
+            expected_count = 2
+            all_fetched_res = []
+            for page in query_iterable.by_page():
+                fetched_res = list(page)
+                self.assertEqual(len(fetched_res), min(pageSize, expected_count - count))
+                count += len(fetched_res)
+                all_fetched_res.extend(fetched_res)
+
+            actual_ids = ''
+            for item in all_fetched_res:
+                actual_ids += item['id'] + '.'
+            self.assertEqual(actual_ids, expected_ids)
+
+        # verify reading change feed from the beginning
+        query_iterable = created_collection.query_items_change_feed(
+            is_start_from_beginning=True,
+            **partitionParam
+        )
+        expected_ids = ['doc1', 'doc2', 'doc3']
+        it = query_iterable.__iter__()
+        for i in range(0, len(expected_ids)):
+            doc = next(it)
+            self.assertEqual(doc['id'], expected_ids[i])
+        self.assertTrue('etag' in created_collection.client_connection.last_response_headers)
+        continuation3 = created_collection.client_connection.last_response_headers['etag']
+
+        # verify reading empty change feed
+        query_iterable = created_collection.query_items_change_feed(
+            continuation=continuation3,
+            is_start_from_beginning=True,
+            **partitionParam
+        )
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 0)
 
     def test_query_change_feed_with_pk_range_id(self):
-        self.query_change_feed(False)
+        created_collection = self.created_db.create_container_if_not_exists("change_feed_test_" + str(uuid.uuid4()),
+                                                                            PartitionKey(path="/pk"))
+        # The test targets partition #3
+        partition_key_range_id = 0
+        partitionParam = {"partition_key_range_id": partition_key_range_id}
 
+        # Read change feed without passing any options
+        query_iterable = created_collection.query_items_change_feed()
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 0)
+
+        # Read change feed from current should return an empty list
+        query_iterable = created_collection.query_items_change_feed(**partitionParam)
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 0)
+        self.assertTrue('etag' in created_collection.client_connection.last_response_headers)
+        self.assertNotEqual(created_collection.client_connection.last_response_headers['etag'], '')
+
+        # Read change feed from beginning should return an empty list
+        query_iterable = created_collection.query_items_change_feed(
+            is_start_from_beginning=True,
+            **partitionParam
+        )
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 0)
+        self.assertTrue('etag' in created_collection.client_connection.last_response_headers)
+        continuation1 = created_collection.client_connection.last_response_headers['etag']
+        self.assertNotEqual(continuation1, '')
+
+        # Create a document. Read change feed should return be able to read that document
+        document_definition = {'pk': 'pk', 'id': 'doc1'}
+        created_collection.create_item(body=document_definition)
+        query_iterable = created_collection.query_items_change_feed(
+            is_start_from_beginning=True,
+            **partitionParam
+        )
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 1)
+        self.assertEqual(iter_list[0]['id'], 'doc1')
+        self.assertTrue('etag' in created_collection.client_connection.last_response_headers)
+        continuation2 = created_collection.client_connection.last_response_headers['etag']
+        self.assertNotEqual(continuation2, '')
+        self.assertNotEqual(continuation2, continuation1)
+
+        # Create two new documents. Verify that change feed contains the 2 new documents
+        # with page size 1 and page size 100
+        document_definition = {'pk': 'pk', 'id': 'doc2'}
+        created_collection.create_item(body=document_definition)
+        document_definition = {'pk': 'pk', 'id': 'doc3'}
+        created_collection.create_item(body=document_definition)
+
+        for pageSize in [1, 100]:
+            # verify iterator
+            query_iterable = created_collection.query_items_change_feed(
+                continuation=continuation2,
+                max_item_count=pageSize,
+                **partitionParam
+            )
+            it = query_iterable.__iter__()
+            expected_ids = 'doc2.doc3.'
+            actual_ids = ''
+            for item in it:
+                actual_ids += item['id'] + '.'
+            self.assertEqual(actual_ids, expected_ids)
+
+            # verify by_page
+            # the options is not copied, therefore it need to be restored
+            query_iterable = created_collection.query_items_change_feed(
+                continuation=continuation2,
+                max_item_count=pageSize,
+                **partitionParam
+            )
+            count = 0
+            expected_count = 2
+            all_fetched_res = []
+            for page in query_iterable.by_page():
+                fetched_res = list(page)
+                self.assertEqual(len(fetched_res), min(pageSize, expected_count - count))
+                count += len(fetched_res)
+                all_fetched_res.extend(fetched_res)
+
+            actual_ids = ''
+            for item in all_fetched_res:
+                actual_ids += item['id'] + '.'
+            self.assertEqual(actual_ids, expected_ids)
+
+        # verify reading change feed from the beginning
+        query_iterable = created_collection.query_items_change_feed(
+            is_start_from_beginning=True,
+            **partitionParam
+        )
+        expected_ids = ['doc1', 'doc2', 'doc3']
+        it = query_iterable.__iter__()
+        for i in range(0, len(expected_ids)):
+            doc = next(it)
+            self.assertEqual(doc['id'], expected_ids[i])
+        self.assertTrue('etag' in created_collection.client_connection.last_response_headers)
+        continuation3 = created_collection.client_connection.last_response_headers['etag']
+
+        # verify reading empty change feed
+        query_iterable = created_collection.query_items_change_feed(
+            continuation=continuation3,
+            is_start_from_beginning=True,
+            **partitionParam
+        )
+        iter_list = list(query_iterable)
+        self.assertEqual(len(iter_list), 0)
     def query_change_feed(self, use_partition_key):
         created_collection = self.created_db.create_container_if_not_exists("change_feed_test_" + str(uuid.uuid4()),
                                                                             PartitionKey(path="/pk"))
