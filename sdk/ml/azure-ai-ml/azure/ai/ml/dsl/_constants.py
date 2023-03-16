50ba1e092e6442cc22e4bdf994de368007c5b3c3
Miles Holland <milesholland@microsoft.com>
2023-03-16 13:45:51 -0400

    regenerate test recording

--- sdk/ml/azure-ai-ml/azure/ai/ml/dsl/_constants.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/dsl/_constants.py
@@ -4,4 +4,4 @@
 VALID_NAME_CHARS = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-")
 
 FL_TESTING_COMPONENTS_FOLDER = "./tests/test_configs/components/fl_test_components"
-FL_TESTING_LOCAL_DATA_FOLDER = "./tests/test_configs/data/fl-e2e-test-data"
+FL_TESTING_LOCAL_DATA_FOLDER = "./tests/test_configs/fl-e2e-test-data"
