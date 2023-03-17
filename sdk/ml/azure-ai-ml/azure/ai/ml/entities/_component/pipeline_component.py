9cbe6cd78ced75b4c32dc6833721a658a913fbd7
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-03-17 16:58:45 -0400

    add serialization logic to pipeline component

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_component/pipeline_component.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_component/pipeline_component.py
@@ -451,7 +451,9 @@ class PipelineComponent(Component):
         component["jobs"] = self._build_rest_component_jobs()
         component["sourceJobId"] = self._source_job_id
         if self._intellectual_property:
-            component["intellectualProperty"] = component.pop("intellectual_property")
+            # hack while full pass through supported is worked on for IPP fields
+            component.pop("intellectual_property")
+            component["intellectualProperty"] = self._intellectual_property._to_rest_object().serialize()
         properties = ComponentVersionProperties(
             component_spec=component,
             description=self.description,
