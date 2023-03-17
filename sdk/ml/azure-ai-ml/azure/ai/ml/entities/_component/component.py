94a519377787cb642fdc587a96f9037154b08868
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-03-17 15:32:44 -0400

    fix an issue with serialization

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_component/component.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_component/component.py
@@ -490,7 +490,9 @@ class Component(
         # add source type to component rest object
         component["_source"] = self._source
         if self._intellectual_property:
-            component["intellectualProperty"] = component.pop("intellectual_property")
+            # hack while pass through is completely supported for IPP
+            component.pop("intellectual_property")
+            component["intellectualProperty"] = self._intellectual_property._to_rest_object().serialize()
         properties = ComponentVersionProperties(
             component_spec=component,
             description=self.description,
