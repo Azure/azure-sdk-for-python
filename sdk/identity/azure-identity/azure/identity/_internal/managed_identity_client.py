bc586227865ec480f07fa3bd61c0715f7a3c9eaa
Xiang Yan <xiangsjtu@gmail.com>
2023-03-22 14:19:53 -0700

    Update sdk/identity/azure-identity/azure/identity/_internal/managed_identity_client.py
    Co-authored-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/identity/azure-identity/azure/identity/_internal/managed_identity_client.py
+++ sdk/identity/azure-identity/azure/identity/_internal/managed_identity_client.py
@@ -66,7 +66,7 @@ class ManagedIdentityClientBase(abc.ABC):
         content["expires_on"] = expires_on
         if "refresh_in" not in content:
             refresh_in = expires_on - request_time
-            if refresh_in > 60 * 60 * 2:  # 2 hours
+            if refresh_in >= 60 * 60 * 2:  # 2 hours
                 refresh_in = int(refresh_in / 2)
             content["refresh_in"] = refresh_in
 
