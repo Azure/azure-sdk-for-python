d17063b37d34883192f6f4efb1e78fe6b104fe83
Paul Van Eck <paulvaneck@microsoft.com>
2023-03-23 17:49:01 -0700

    [Monitor] Regen query and ingestion (#29504)
    Regenerated using the latest autorest.
    
    Added the package names to the swagger files so that the user agent
    sdk_moniker is generated as intended.
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/monitor/azure-monitor-query/azure/monitor/query/_generated/_client.py
+++ sdk/monitor/azure-monitor-query/azure/monitor/query/_generated/_client.py
@@ -32,7 +32,7 @@ class MonitorQueryClient:  # pylint: disable=client-accepts-api-version-keyword
         self, *, endpoint: str = "https://api.loganalytics.io/v1", **kwargs: Any
     ) -> None:
         self._config = MonitorQueryClientConfiguration(**kwargs)
-        self._client = PipelineClient(base_url=endpoint, config=self._config, **kwargs)
+        self._client: PipelineClient = PipelineClient(base_url=endpoint, config=self._config, **kwargs)
 
         self._serialize = Serializer()
         self._deserialize = Deserializer()
@@ -69,5 +69,5 @@ class MonitorQueryClient:  # pylint: disable=client-accepts-api-version-keyword
         self._client.__enter__()
         return self
 
-    def __exit__(self, *exc_details) -> None:
+    def __exit__(self, *exc_details: Any) -> None:
         self._client.__exit__(*exc_details)
