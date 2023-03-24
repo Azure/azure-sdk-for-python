f9c4c2480fa3f20cecac0b0e8f727f205423b0ea
swathipil <76007337+swathipil@users.noreply.github.com>
2023-03-24 16:34:07 -0500

    [ServiceBus] remove msrest (#29324)
    - Removing msrest + re-generating ServiceBusAdministrationClient
    - Updated based on swagger at https://github.com/Azure/azure-rest-api-specs/blob/main/specification/servicebus/data-plane/Microsoft.ServiceBus/stable/2021-05/servicebus.json#L519
    - `azure.core.exceptions.HttpResponseError`s are raised instead of `msrest.ValidationError`s.
    
    - related to #26571

--- sdk/servicebus/azure-servicebus/azure/servicebus/management/_generated/aio/_configuration.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/management/_generated/aio/_configuration.py
@@ -19,7 +19,7 @@ else:
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
-    from azure.core.credentials import TokenCredential
+    from azure.core.credentials_async import AsyncTokenCredential
 
 VERSION = "unknown"
 
@@ -33,13 +33,13 @@ class ServiceBusManagementClientConfiguration(Configuration):  # pylint: disable
     :param endpoint: The Service Bus fully qualified domain name. Required.
     :type endpoint: str
     :param credential: Credential needed for the client to connect to Azure. Required.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :keyword api_version: Api Version. Default value is "2021-05". Note that overriding this
      default value may result in unsupported behavior.
     :paramtype api_version: str
     """
 
-    def __init__(self, endpoint: str, credential: "TokenCredential", **kwargs: Any) -> None:
+    def __init__(self, endpoint: str, credential: "AsyncTokenCredential", **kwargs: Any) -> None:
         super(ServiceBusManagementClientConfiguration, self).__init__(**kwargs)
         api_version: Literal["2021-05"] = kwargs.pop("api_version", "2021-05")
 
@@ -61,11 +61,11 @@ class ServiceBusManagementClientConfiguration(Configuration):  # pylint: disable
         self.proxy_policy = kwargs.get("proxy_policy") or policies.ProxyPolicy(**kwargs)
         self.logging_policy = kwargs.get("logging_policy") or policies.NetworkTraceLoggingPolicy(**kwargs)
         self.http_logging_policy = kwargs.get("http_logging_policy") or policies.HttpLoggingPolicy(**kwargs)
-        self.retry_policy = kwargs.get("retry_policy") or policies.RetryPolicy(**kwargs)
+        self.retry_policy = kwargs.get("retry_policy") or policies.AsyncRetryPolicy(**kwargs)
         self.custom_hook_policy = kwargs.get("custom_hook_policy") or policies.CustomHookPolicy(**kwargs)
-        self.redirect_policy = kwargs.get("redirect_policy") or policies.RedirectPolicy(**kwargs)
+        self.redirect_policy = kwargs.get("redirect_policy") or policies.AsyncRedirectPolicy(**kwargs)
         self.authentication_policy = kwargs.get("authentication_policy")
         if self.credential and not self.authentication_policy:
-            self.authentication_policy = policies.BearerTokenCredentialPolicy(
+            self.authentication_policy = policies.AsyncBearerTokenCredentialPolicy(
                 self.credential, *self.credential_scopes, **kwargs
             )
