241315c8bb1472ca102ba7b4066fb8405293cdfa
Zhengfei Wang <wangzhengfei.chn@outlook.com>
2023-03-27 18:29:23 +0800

    fix: consider internal flag for reload schema

--- sdk/ml/azure-ai-ml/tests/conftest.py
+++ sdk/ml/azure-ai-ml/tests/conftest.py
@@ -780,6 +780,7 @@ def enable_pipeline_private_preview_features(mocker: MockFixture):
 @pytest.fixture()
 def enable_private_preview_schema_features():
     """Schemas will be imported at the very beginning, so need to reload related classes."""
+    from azure.ai.ml._internal._setup import _registered, enable_internal_components_in_pipeline
     from azure.ai.ml._schema.component import command_component as command_component_schema
     from azure.ai.ml._schema.component import component as component_schema
     from azure.ai.ml._schema.component import input_output
@@ -800,7 +801,12 @@ def enable_private_preview_schema_features():
         pipeline_component_entity.PipelineComponentSchema = pipeline_component_schema.PipelineComponentSchema
         pipeline_job_entity.PipelineJobSchema = pipeline_job_schema.PipelineJobSchema
 
+        # check internal flag after reload, force register if it is set as True
+        if _registered:
+            enable_internal_components_in_pipeline(force=True)
+
     with patch.dict(os.environ, {AZUREML_PRIVATE_FEATURES_ENV_VAR: "True"}):
+
         _reload_related_classes()
         yield
     _reload_related_classes()
