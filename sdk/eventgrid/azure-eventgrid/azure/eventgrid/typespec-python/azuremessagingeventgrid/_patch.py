363dea83af45936b196df4b3c986d1e346b0ba81
l0lawrence <llawrence@microsoft.com>
2023-03-30 15:52:19 -0700

    patch updates -- wip

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/typespec-python/azuremessagingeventgrid/_patch.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/typespec-python/azuremessagingeventgrid/_patch.py
@@ -6,12 +6,40 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
-from typing import List
-from azure.core.pipeline import PipelineRequest
+import base64
+from typing import List, overload, Union, Any, Optional
+from azure.core.messaging import CloudEvent
+from .models._models import CloudEventEvent as InternalCloudEvent
+from .models._patch import ReceiveResponse
 from azure.core.pipeline.policies import SansIOHTTPPolicy
 from azure.core.credentials import AzureKeyCredential
-
+from azure.core.tracing.decorator import distributed_trace
 from ._client import AzureMessagingEventGridClient as ServiceClientGenerated
+
+def _cloud_event_to_generated(cloud_event, **kwargs):
+    data_kwargs = {}
+
+    if isinstance(cloud_event.data, bytes):
+        data_kwargs["data_base64"] = base64.b64encode(cloud_event.data) # base64 encode double check 
+    else:
+        data_kwargs["data"] = cloud_event.data
+
+    internal_event = InternalCloudEvent(
+        id=cloud_event.id,
+        source=cloud_event.source,
+        type=cloud_event.type,
+        specversion=cloud_event.specversion,
+        # data_base64=data_base64,
+        # time=cloud_event.time,
+        # dataschema=cloud_event.dataschema,
+        # datacontenttype=cloud_event.datacontenttype,
+        # subject=cloud_event.subject,
+        **data_kwargs,
+        **kwargs
+    )
+    if cloud_event.extensions:
+        internal_event.update(cloud_event.extensions)
+    return internal_event
 
 
 class EventGridSharedAccessKeyPolicy(SansIOHTTPPolicy):
@@ -39,6 +67,48 @@ class AzureMessagingEventGridClient(ServiceClientGenerated):
             credential=credential,
             **kwargs
         )
+
+
+    @overload
+    def publish(self, topic_name: str, body: List[CloudEvent], *, content_type: str = "application/cloudevents-batch+json; charset=utf-8", **kwargs: Any):
+        """Publish Batch of Cloud Events to namespace topic."""
+
+    @overload
+    def publish(self, topic_name: str, body: CloudEvent, *, content_type: str = "application/cloudevents+json; charset=utf-8", **kwargs: Any):
+        """Publish Single Cloud Event to namespace topic."""
+
+    @distributed_trace
+    def publish(self, topic_name: str, body: Union[List[CloudEvent], CloudEvent], **kwargs):
+        """Publish Cloud Events to namespace topic."""
+        if isinstance(body, CloudEvent):
+            kwargs["content_type"] = "application/cloudevents+json; charset=utf-8"
+            internal_body = _cloud_event_to_generated(body)
+            # if None 
+            self.publish_cloud_event(topic_name, internal_body, **kwargs)
+        else:
+            kwargs["content_type"] = "application/cloudevents-batch+json; charset=utf-8"
+            internal_body_list = []
+            for item in body:
+                internal_body_list.append(_cloud_event_to_generated(item))
+            self.publish_batch_of_cloud_events(topic_name, internal_body_list, **kwargs)
+    
+    @distributed_trace
+    def receive(self,
+        topic_name: str,
+        event_subscription_name: str,
+        *,
+        max_events: Optional[int] = None,
+        timeout: Optional[int] = None,
+        **kwargs:Any
+        ) -> List[ReceiveResponse]:
+        """Receive Cloud Events from namespace topic."""
+        deserialized_response = []
+        received_response = self.receive_batch_of_cloud_events(topic_name, event_subscription_name, max_events=max_events, timeout=timeout, **kwargs)
+        for detail_item in received_response.get("value"):
+            deserialized_cloud_event = CloudEvent.from_dict(detail_item.get("event"))
+            detail_item["event"] = deserialized_cloud_event
+            deserialized_response.append(detail_item)
+        return deserialized_response
 
 
 def patch_sdk():
--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/typespec-python/azuremessagingeventgrid/models/_patch.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/typespec-python/azuremessagingeventgrid/models/_patch.py
@@ -6,9 +6,72 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
-from typing import List
+from typing import List, Mapping, Any, overload
+from ._models import ReceiveResponse, ReceiveDetails, BrokerProperties
+from azure.core.messaging import CloudEvent
 
-__all__: List[str] = []  # Add all objects you want publicly available to users at this package level
+
+class ReceiveDetails(ReceiveDetails):
+    """Receive operation details per Cloud Event.
+
+    All required parameters must be populated in order to send to Azure.
+
+    :ivar broker_properties: The Event Broker details. Required.
+    :vartype broker_properties: ~azuremessagingeventgrid.models.BrokerProperties
+    :ivar event: Cloud Event details. Required.
+    :vartype event: ~azuremessagingeventgrid.models.CloudEventEvent
+    """
+
+    @overload
+    def __init__(
+        self,
+        *,
+        broker_properties: "BrokerProperties",
+        event: "CloudEvent",
+    ):
+        ...
+
+    @overload
+    def __init__(self, mapping: Mapping[str, Any]):
+        """
+        :param mapping: raw JSON to initialize the model.
+        :type mapping: Mapping[str, Any]
+        """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # pylint: disable=useless-super-delegation
+        super().__init__(*args, **kwargs)
+
+
+class ReceiveResponse(ReceiveResponse):
+    """Details of the Receive operation response.
+
+    All required parameters must be populated in order to send to Azure.
+
+    :ivar value: Array of receive responses, one per cloud event. Required.
+    :vartype value: list[~azuremessagingeventgrid.models.ReceiveDetails]
+    """
+
+    @overload
+    def __init__(
+        self,
+        *,
+        value: List["ReceiveDetails"],
+    ):
+        ...
+
+    @overload
+    def __init__(self, mapping: Mapping[str, Any]):
+        """
+        :param mapping: raw JSON to initialize the model.
+        :type mapping: Mapping[str, Any]
+        """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # pylint: disable=useless-super-delegation
+        super().__init__(*args, **kwargs)
+
+
+
+__all__: List[str] = ["ReceiveDetails","ReceiveResponse"]  # Add all objects you want publicly available to users at this package level
 
 
 def patch_sdk():
