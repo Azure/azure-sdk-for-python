c4bb7bbb00b29f32d867e93cb7f154273269ed08
unknown <Mitgarg17495@gmail.com>
2023-04-04 09:52:28 -0700

    change flag name

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_builders/fl_scatter_gather.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_builders/fl_scatter_gather.py
@@ -109,7 +109,8 @@ class FLScatterGather(ControlFlowNode, NodeIOMixin):
         self.scatter_gather_graph = self.scatter_gather()
 
         # set SG node flag for telemetry
-        self.scatter_gather_graph.properties["azureml.telemetry.attribution"] = "scatter-gather"
+        self.scatter_gather_graph.properties["azureml.telemetry.attribution"] = "FederatedLearningSGJobFlag"
+        self.scatter_gather_graph._to_rest_object()
 
         # set output to final aggregation step's output
         self._outputs = self.scatter_gather_graph.outputs
