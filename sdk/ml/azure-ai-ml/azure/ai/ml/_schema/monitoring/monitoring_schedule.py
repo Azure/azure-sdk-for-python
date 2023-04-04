3ed61de0bd6f499388e3c39fe5e2bcec67de48b0
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-04-04 13:32:42 -0400

    clean up

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/monitoring/monitoring_schedule.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/monitoring/monitoring_schedule.py
@@ -2,13 +2,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from typing import Dict, Optional, Union
-
-
 from azure.ai.ml._schema.core.fields import NestedField
 from azure.ai.ml._schema.monitoring.monitor_definition import MonitorDefinitionSchema
 from azure.ai.ml._schema.schedule.schedule import ScheduleSchema
-from azure.ai.ml._utils._experimental import experimental
 
 
 class MonitorScheduleSchema(ScheduleSchema):
