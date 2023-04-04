ede67d422cd473d1f408d52642d7e5a6bff4df99
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-04-04 13:32:19 -0400

    add MonitorScheduleSchema

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/monitoring/monitoring_schedule.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/monitoring/monitoring_schedule.py
@@ -4,34 +4,12 @@
 
 from typing import Dict, Optional, Union
 
-from azure.ai.ml.entities._monitoring.monitor_definition import MonitorDefinition
-from azure.ai.ml.entities._schedule.schedule import Schedule
-from azure.ai.ml.entities._schedule.trigger import CronTrigger, RecurrenceTrigger
+
+from azure.ai.ml._schema.core.fields import NestedField
+from azure.ai.ml._schema.monitoring.monitor_definition import MonitorDefinitionSchema
+from azure.ai.ml._schema.schedule.schedule import ScheduleSchema
 from azure.ai.ml._utils._experimental import experimental
 
 
-@experimental
-class MonitorSchedule(Schedule):
-    def __init__(
-        self,
-        *,
-        name: str,
-        trigger: Union[CronTrigger, RecurrenceTrigger],
-        create_job: MonitorDefinition = None,
-        display_name: Optional[str] = None,
-        description: Optional[str] = None,
-        tags: Optional[Dict] = None,
-        properties: Optional[Dict] = None,
-        **kwargs,
-    ):
-        super().__init__(
-            name=name,
-            trigger=trigger,
-            display_name=display_name,
-            description=description,
-            tags=tags,
-            properties=properties,
-            description=description,
-            **kwargs,
-        )
-        self.create_job = create_job
+class MonitorScheduleSchema(ScheduleSchema):
+    create_monitor = NestedField(MonitorDefinitionSchema)
