33e925a6b6056a189028b555d6bf7137b8332fca
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-04-05 15:35:11 -0400

    remove experimental from schema

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/monitoring/alert_notification.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/monitoring/alert_notification.py
@@ -7,10 +7,8 @@
 from marshmallow import fields, post_load
 
 from azure.ai.ml._schema.core.schema import PatchedSchemaMeta
-from azure.ai.ml._utils._experimental import experimental
 
 
-@experimental
 class AlertNotificationSchema(metaclass=PatchedSchemaMeta):
     monitoring_signals_enabled = fields.List(fields.Str)
     azure_monitoring_signals = fields.Dict(keys=fields.Str, values=fields.Str)
--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/monitoring/input_data.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/monitoring/input_data.py
@@ -9,10 +9,8 @@ from marshmallow import fields, post_load
 from azure.ai.ml.constants._common import AzureMLResourceType
 from azure.ai.ml._schema.core.schema import PatchedSchemaMeta
 from azure.ai.ml._schema.core.fields import ArmVersionedStr
-from azure.ai.ml._utils._experimental import experimental
 
 
-@experimental
 class MonitorInputDataSchema(metaclass=PatchedSchemaMeta):
     input_dataset = ArmVersionedStr(azureml_type=AzureMLResourceType.DATA)
     dataset_context = fields.Str()
