34aa0c788fcba545139bf77e07e4e1509bd6f4a5
YalinLi0312 <yall@microsoft.com>
2023-04-05 17:48:52 -0700

    Set connection timeout to 100

--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/aio/_async_base_client.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/aio/_async_base_client.py
@@ -42,6 +42,7 @@ class ContainerRegistryBaseClient(object):
             url=endpoint,
             sdk_moniker=USER_AGENT,
             authentication_policy=self._auth_policy,
+            connection_timeout=100,
             **kwargs
         )
 
