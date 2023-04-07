3a4981647332d2ec36063d9e792990094a9883a5
PythonSdkPipelines <PythonSdkPipelines>
2023-04-07 09:11:38 +0000

    code and test

--- sdk/rdbms/azure-mgmt-rdbms/CHANGELOG.md
+++ sdk/rdbms/azure-mgmt-rdbms/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release History
 
+## 0.0.0 (it should be perview) (2023-04-07)
+
+
+
 ## 10.2.0b6 (2023-01-04)
 
 ### Features Added
--- sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/_version.py
+++ sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/_version.py
@@ -5,4 +5,4 @@
 # license information.
 # --------------------------------------------------------------------------
 
-VERSION = "10.2.0b6"
+VERSION = "0.0.0 (it should be perview)"
