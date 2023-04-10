f39282e90c29bc5bbb1f64dca5cc8565024653dd
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-04-10 18:36:56 -0400

    input data to/from rest

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_monitoring/input_data.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_monitoring/input_data.py
@@ -4,7 +4,7 @@
 
 from azure.ai.ml.entities._mixins import RestTranslatableMixin
 from azure.ai.ml._restclient.v2023_04_01_preview.models import MonitoringInputData as RestMonitoringInputData
-from azure.ai.ml._utils.utils import camel_to_snake
+from azure.ai.ml._utils.utils import camel_to_snake, snake_to_camel
 from azure.ai.ml._utils._experimental import experimental
 
 from azure.ai.ml.constants._monitoring import MonitorDatasetContext
@@ -27,4 +27,17 @@ class MonitorInputData(RestTranslatableMixin):
         self.pre_processing_component = pre_processing_component
 
     def _to_rest_object(self) -> RestMonitoringInputData:
-        return super()._to_rest_object()
+        return RestMonitoringInputData(
+            data_context=snake_to_camel(self.dataset_context),
+            asset=self.input_dataset._to_rest_object(),
+            preprocessing_component_id=self.pre_processing_component,
+            target_column_name=self.target_column_name
+        )
+
+    @classmethod
+    def _from_rest_object(cls, obj: RestMonitoringInputData) -> "MonitorInputData":
+        return cls(
+            dataset_context=camel_to_snake(obj.data_context),
+            target_column_name=obj.target_column_name,
+            pre_processing_component=obj.preprocessing_component_id
+        )
