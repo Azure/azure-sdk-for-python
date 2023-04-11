205c0e59e1cc4b8092c57a5d92fee63fdb056448
msyyc <70930885+msyyc@users.noreply.github.com>
2023-04-11 15:15:16 +0800

    comment header

--- sdk/webpubsub/azure-webpubsub-client/azure/webpubsub/client/_model_base.py
+++ sdk/webpubsub/azure-webpubsub-client/azure/webpubsub/client/_model_base.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for
-# license information.
+# Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------
 # pylint: disable=protected-access, arguments-differ, signature-differs, broad-except
 # pyright: reportGeneralTypeIssues=false
