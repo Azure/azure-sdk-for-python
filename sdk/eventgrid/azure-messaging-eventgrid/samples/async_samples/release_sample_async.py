9eff3ecb86a749069a8c8fe0583d7dc8d5ac2887
l0lawrence <llawrence@microsoft.com>
2023-04-11 10:20:47 -0700

    code samples

---
+++ sdk/eventgrid/azure-messaging-eventgrid/samples/async_samples/release_sample_async.py
@@ -0,0 +1,29 @@
+import os
+import asyncio
+from azure.messaging.eventgridmessaging.aio import EventGridMessagingClient
+from azure.messaging.eventgridmessaging import EventGridSharedAccessKeyCredential
+from azure.messaging.eventgridmessaging.models import *
+from azure.core.messaging import CloudEvent
+
+
+
+# Create a client
+EG_KEY = os.environ.get("EG_KEY")
+EG_ENDPOINT = os.environ.get("EG_ENDPOINT")
+TOPIC_NAME = os.environ.get("TOPIC_NAME")
+ES_NAME = os.environ.get("ES_NAME")
+
+client = EventGridMessagingClient(EG_ENDPOINT, EventGridSharedAccessKeyCredential(EG_KEY))
+
+
+async def run():
+    # Release a LockToken
+    try: 
+        tokens = [LockToken({'lockToken': 'token'})]
+        release = await client.release_batch_of_cloud_events(topic_name=TOPIC_NAME, event_subscription_name=ES_NAME, tokens=tokens)
+        print(release)
+    except Exception as e:
+        print(e)
+
+
+asyncio.run(run())
---
+++ sdk/eventgrid/azure-messaging-eventgrid/samples/sync_samples/publish_sample.py
@@ -0,0 +1,27 @@
+import os
+from azure.messaging.eventgridmessaging import EventGridMessagingClient, EventGridSharedAccessKeyCredential
+from azure.messaging.eventgridmessaging.models import *
+from azure.core.messaging import CloudEvent
+
+
+
+# Create a client
+EG_KEY = os.environ.get("EG_KEY")
+EG_ENDPOINT = os.environ.get("EG_ENDPOINT")
+
+client = EventGridMessagingClient(EG_ENDPOINT, EventGridSharedAccessKeyCredential(EG_KEY))
+
+
+# Publish a CloudEvent
+try:
+    cloud_event = CloudEvent(data={"hello": "world"}, source="https://example.com", type="example")
+    client.publish(cloud_event)
+except Exception as e:
+    print(e)
+
+# Publish a list of CloudEvents
+try:
+    list_of_cloud_events = [cloud_event, cloud_event]
+    client.publish(list_of_cloud_events)
+except Exception as e:
+    print(e)
