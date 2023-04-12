d7f81d20e2deda4d28338d6cff3711fd119f09c7
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-04-12 17:08:54 -0400

    to rest for alert settings

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_monitoring/alert_notification.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_monitoring/alert_notification.py
@@ -2,12 +2,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from azure.ai.ml.entities._mixins import RestTranslatableMixin
 from azure.ai.ml._utils._experimental import experimental
-from azure.ai.ml._restclient.v2023_04_01_preview.models import NotificationSetting
-
+from azure.ai.ml._restclient.v2023_04_01_preview.models import (
+    NotificationSetting,
+    MonitoringAlertNotificationSettingsBase,
+    AzMonMonitoringAlertNotificationSettings,
+    EmailMonitoringAlertNotificationSettings
+)
 
 @experimental
 class AlertNotification(RestTranslatableMixin):
@@ -22,11 +26,20 @@ class AlertNotification(RestTranslatableMixin):
         self.azure_monitoring_signals = azure_monitor_signals
         self.emails = emails
 
-    def _to_rest_object(self) -> NotificationSetting:
-        return NotificationSetting(
-            emails=self.emails,
-        )
+    def _to_rest_object(self) -> Union[EmailMonitoringAlertNotificationSettings, AzMonMonitoringAlertNotificationSettings]:
+        if self.emails:
+            return EmailMonitoringAlertNotificationSettings(
+                email_notification_setting=NotificationSetting(emails=self.emails)
+            )
+        elif self.azure_monitoring_signals:
+            return AzMonMonitoringAlertNotificationSettings()
 
     @classmethod
-    def _from_rest_object(cls, obj: NotificationSetting) -> "AlertNotification":
-        return cls(emails=obj.emails)
+    def _from_rest_object(cls, obj: MonitoringAlertNotificationSettingsBase) -> "AlertNotification":
+        if isinstance(obj, EmailMonitoringAlertNotificationSettings):
+            return cls(
+                emails=obj.email_notification_setting.emails
+            )
+        return cls(
+            azure_monitoring_signals={}
+        )
