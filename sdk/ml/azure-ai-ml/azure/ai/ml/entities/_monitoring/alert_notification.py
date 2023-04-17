12a7defe8084efe8af5815fd4f3042b3703ec4eb
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-04-17 14:36:49 -0400

    lint

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_monitoring/alert_notification.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_monitoring/alert_notification.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from typing import Dict, List
+from typing import List
 
 from azure.ai.ml._utils._experimental import experimental
 
