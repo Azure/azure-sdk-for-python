2a25cfacfdfff15706e6eb9d55aaf135495c905e
Lester Cheng <LesterCheng@users.noreply.github.com>
2023-04-18 09:06:21 -0500

    Apply suggestions from code review
    Co-authored-by: Anna Tisch <antisch@microsoft.com>

--- sdk/attestation/azure-security-attestation/azure/security/attestation/_client.py
+++ sdk/attestation/azure-security-attestation/azure/security/attestation/_client.py
@@ -358,7 +358,7 @@ class AttestationClient(object):
         """Performs TPM attestation.
 
         :param bytes content: Attestation request for Trusted Platform Module (TPM) attestation.
-        :returns: attestation response for Trusted Platform Module (TPM) attestation.
+        :returns: Attestation response for Trusted Platform Module (TPM) attestation.
         :rtype: ~azure.security.attestation.TpmAttestationResult
         """
 
