5233eaaad7c87d7f9e1fbe592d1be8896926abcf
PythonSdkPipelines <PythonSdkPipelines>
2023-04-20 06:16:13 +0000

    code and test

--- sdk/newrelicobservability/azure-mgmt-newrelicobservability/CHANGELOG.md
+++ sdk/newrelicobservability/azure-mgmt-newrelicobservability/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release History
 
+## 0.0.0 (it should be stable) (2023-04-20)
+
+
+
 ## 1.0.0b1 (2023-03-24)
 
 * Initial Release
--- sdk/newrelicobservability/azure-mgmt-newrelicobservability/generated_samples/monitors_list_by_resource_group_maximum_set_gen.py
+++ sdk/newrelicobservability/azure-mgmt-newrelicobservability/generated_samples/monitors_list_by_resource_group_maximum_set_gen.py
@@ -36,6 +36,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/newrelic/resource-manager/NewRelic.Observability/preview/2022-07-01-preview/examples/Monitors_ListByResourceGroup_MaximumSet_Gen.json
+# x-ms-original-file: specification/newrelic/resource-manager/NewRelic.Observability/stable/2022-07-01/examples/Monitors_ListByResourceGroup_MaximumSet_Gen.json
 if __name__ == "__main__":
     main()
