995d55f58d12148b9359c5575c03db647d5754d7
Gaurav Singh <gasi@microsoft.com>
2023-04-24 00:06:42 -0700

    undo / update svc client

--- sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
@@ -405,7 +405,7 @@ class MLClient:
         self._batch_endpoints = BatchEndpointOperations(
             self._operation_scope,
             self._operation_config,
-            self._service_client_04_2023_preview,
+            self._service_client_05_2022,
             self._operation_container,
             self._credential,
             requests_pipeline=self._requests_pipeline,
@@ -426,7 +426,7 @@ class MLClient:
         self._batch_deployments = BatchDeploymentOperations(
             self._operation_scope,
             self._operation_config,
-            self._service_client_05_2022,
+            self._service_client_04_2023_preview,
             self._operation_container,
             credentials=self._credential,
             requests_pipeline=self._requests_pipeline,
