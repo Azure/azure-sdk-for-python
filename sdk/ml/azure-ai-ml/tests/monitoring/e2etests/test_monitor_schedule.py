6165926e5bc224445c148574610be06ee22788cd
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-04-24 19:38:28 -0400

    add basic e2e test for data drift

--- sdk/ml/azure-ai-ml/tests/monitoring/e2etests/test_monitor_schedule.py
+++ sdk/ml/azure-ai-ml/tests/monitoring/e2etests/test_monitor_schedule.py
@@ -11,21 +11,25 @@ from azure.ai.ml._utils._arm_id_utils import is_ARM_id_for_resource
 
 
 @pytest.mark.timeout(600)
-@pytest.mark.usefixtures(
-    "recorded_test"
-)
+@pytest.mark.usefixtures("recorded_test")
 @pytest.mark.core_sdk_test
 class TestCommandJob(AzureRecordedTestCase):
-    def test_data_drift_schedule_create(self, client: MLClient, data_with_2_versions: str, randstr: Callable[[str], str]):
+    def test_data_drift_schedule_create(
+        self, client: MLClient, data_with_2_versions: str, randstr: Callable[[str], str]
+    ):
         test_path = "tests/test_configs/monitoring/yaml_configs/data_drift.yaml"
 
         schedule_name = randstr("schedule_name")
 
         params_override = [
             {"name": schedule_name},
-            {"create_monitor.monitoring_signals.testSignal.target_dataset.dataset.input_dataset.path": f"azureml:{data_with_2_versions}:1"},
+            {
+                "create_monitor.monitoring_signals.testSignal.target_dataset.dataset.input_dataset.path": f"azureml:{data_with_2_versions}:1"
+            },
             {"create_monitor.monitoring_signals.testSignal.target_dataset.dataset.input_dataset.type": "uri_folder"},
-            {"create_monitor.monitoring_signals.testSignal.baseline_dataset.input_dataset.path": f"azureml:{data_with_2_versions}:2"},
+            {
+                "create_monitor.monitoring_signals.testSignal.baseline_dataset.input_dataset.path": f"azureml:{data_with_2_versions}:2"
+            },
             {"create_monitor.monitoring_signals.testSignal.baseline_dataset.input_dataset.type": "uri_folder"},
         ]
 
@@ -43,7 +47,9 @@ class TestCommandJob(AzureRecordedTestCase):
 
         data_drift_signal = created_schedule.create_monitor.monitoring_signals["testSignal"]
         assert data_drift_signal.target_dataset
-        assert is_ARM_id_for_resource(data_drift_signal.target_dataset.dataset.input_dataset.path, AzureMLResourceType.DATA)
+        assert is_ARM_id_for_resource(
+            data_drift_signal.target_dataset.dataset.input_dataset.path, AzureMLResourceType.DATA
+        )
         assert data_drift_signal.baseline_dataset
         assert is_ARM_id_for_resource(data_drift_signal.baseline_dataset.input_dataset.path, AzureMLResourceType.DATA)
 
