eee80149d36841b0fdc839b43ca9cf2aa9c75008
antisch <antisch@microsoft.com>
2023-04-28 10:36:14 +1200

    Fix pylint

--- sdk/communication/azure-communication-rooms/azure/communication/rooms/_rooms_client.py
+++ sdk/communication/azure-communication-rooms/azure/communication/rooms/_rooms_client.py
@@ -247,8 +247,9 @@ class RoomsClient(object):
         """Remove participants from a room
         :param room_id: Required. Id of room to be updated
         :type room_id: str
-        :param participant: Required. Collection of identities to be removed from the room.
-        :type participant: List[Union[~azure.communication.rooms.RoomParticipant, ~azure.communication.rooms.CommunicationIdentifier]]
+        :param participants: Required. Collection of identities to be removed from the room.
+        :type participants:
+         List[Union[~azure.communication.rooms.RoomParticipant, ~azure.communication.rooms.CommunicationIdentifier]]
         :returns: None.
         :rtype: None
         :raises: ~azure.core.exceptions.HttpResponseError, ValueError
