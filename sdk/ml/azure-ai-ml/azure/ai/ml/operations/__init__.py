4e7189f85fce21ece273045a4e7b19002700ff1f
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-04-27 11:20:00 -0700

    Update __init__.py

--- sdk/ml/azure-ai-ml/azure/ai/ml/operations/__init__.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/operations/__init__.py
@@ -1,14 +1,10 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
-
 """Contains supported operations for Azure Machine Learning SDKv2.
-
 Operations are classes contain logic to interact with backend services, usually auto generated operations call.
 """
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-
-
 from ._batch_deployment_operations import BatchDeploymentOperations
 from ._batch_endpoint_operations import BatchEndpointOperations
 from ._component_operations import ComponentOperations
@@ -16,6 +12,9 @@ from ._compute_operations import ComputeOperations
 from ._data_operations import DataOperations
 from ._datastore_operations import DatastoreOperations
 from ._environment_operations import EnvironmentOperations
+from ._feature_set_operations import FeatureSetOperations
+from ._feature_store_entity_operations import FeatureStoreEntityOperations
+from ._feature_store_operations import FeatureStoreOperations
 from ._job_operations import JobOperations
 from ._model_operations import ModelOperations
 from ._online_deployment_operations import OnlineDeploymentOperations
@@ -24,7 +23,6 @@ from ._registry_operations import RegistryOperations
 from ._schedule_operations import ScheduleOperations
 from ._workspace_connections_operations import WorkspaceConnectionsOperations
 from ._workspace_operations import WorkspaceOperations
-
 __all__ = [
     "ComputeOperations",
     "DatastoreOperations",
@@ -42,4 +40,7 @@ __all__ = [
     "WorkspaceConnectionsOperations",
     "RegistryOperations",
     "ScheduleOperations",
+    "FeatureSetOperations",
+    "FeatureStoreEntityOperations",
+    "FeatureStoreOperations",
 ]
