d94df8a7be14cac2ef9d46450ce5876a5982a91e
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-04-27 17:06:15 -0700

    Update _schedule_operations.py

--- sdk/ml/azure-ai-ml/azure/ai/ml/operations/_schedule_operations.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/operations/_schedule_operations.py
@@ -14,24 +14,14 @@ from azure.ai.ml._scope_dependent_operations import (
 
 from azure.ai.ml._telemetry import ActivityType, monitor_with_activity, monitor_with_telemetry_mixin
 from azure.ai.ml._utils._logger_utils import OpsLogger
-from azure.ai.ml.entities import Job, JobSchedule, Schedule
-from azure.ai.ml.entities._monitoring.schedule import MonitorSchedule
+from azure.ai.ml.entities import Job, Schedule
 from azure.core.credentials import TokenCredential
 from azure.core.polling import LROPoller
 from azure.core.tracing.decorator import distributed_trace
 
 from .._restclient.v2022_10_01.models import ScheduleListViewType
-from .._utils._arm_id_utils import is_ARM_id_for_parented_resource
-from .._utils.utils import snake_to_camel
 from .._utils._azureml_polling import AzureMLPolling
-from ..constants._common import (
-    ARM_ID_PREFIX,
-    AzureMLResourceType,
-    LROConfigurations,
-    NAMED_RESOURCE_ID_FORMAT_WITH_PARENT,
-    AZUREML_RESOURCE_PROVIDER,
-)
-from ..constants._monitoring import MonitorSignalType
+from ..constants._common import AzureMLResourceType, LROConfigurations
 from . import JobOperations
 from ._job_ops_helper import stream_logs_until_completion
 from ._operation_orchestrator import OperationOrchestrator
@@ -85,7 +75,7 @@ class ScheduleOperations(_ScopeDependentOperations):
     def list(
         self,
         *,
-        list_view_type: ScheduleListViewType = ScheduleListViewType.ENABLED_ONLY,  # pylint: disable=unused-argument
+        list_view_type: ScheduleListViewType = ScheduleListViewType.ENABLED_ONLY,  # pylint: disable=unused-argument,
         **kwargs,
     ) -> Iterable[Schedule]:
         """List schedules in specified workspace.
@@ -188,14 +178,10 @@ class ScheduleOperations(_ScopeDependentOperations):
         :rtype: Union[LROPoller, ~azure.ai.ml.entities.Schedule]
         """
 
-        if isinstance(schedule, JobSchedule):
-            schedule._validate(raise_error=True)
-            if isinstance(schedule.create_job, Job):
-                # Create all dependent resources for job inside schedule
-                self._job_operations._resolve_arm_id_or_upload_dependencies(schedule.create_job)
-        elif isinstance(schedule, MonitorSchedule):
-            # resolve ARM id for target, compute, and input datasets for each signal
-            self._resolve_monitor_schedule_arm_id(schedule)
+        schedule._validate(raise_error=True)
+        if isinstance(schedule.create_job, Job):
+            # Create all dependent resources for job inside schedule
+            self._job_operations._resolve_arm_id_or_upload_dependencies(schedule.create_job)
         # Create schedule
         schedule_data = schedule._to_rest_object()
         poller = self.service_client.begin_create_or_update(
@@ -214,7 +200,7 @@ class ScheduleOperations(_ScopeDependentOperations):
     @monitor_with_activity(logger, "Schedule.Enable", ActivityType.PUBLICAPI)
     def begin_enable(
         self,
-        name: str,
+        name: str
         **kwargs,
     ) -> LROPoller[Schedule]:
         """Enable a schedule.
@@ -245,64 +231,3 @@ class ScheduleOperations(_ScopeDependentOperations):
         schedule = self.get(name=name)
         schedule._is_enabled = False
         return self.begin_create_or_update(schedule, **kwargs)
-
-    def _resolve_monitor_schedule_arm_id(self, schedule: MonitorSchedule) -> None:
-        # resolve compute ID
-        schedule.create_monitor.compute = self._orchestrators.get_asset_arm_id(
-            schedule.create_monitor.compute, AzureMLResourceType.COMPUTE, register_asset=False
-        )
-
-        # resolve target ARM ID
-        target = schedule.create_monitor.monitoring_target
-        if target and target.endpoint_deployment_id:
-            target.endpoint_deployment_id = (
-                target.endpoint_deployment_id[len(ARM_ID_PREFIX) :]
-                if target.endpoint_deployment_id.startswith(ARM_ID_PREFIX)
-                else target.endpoint_deployment_id
-            )
-
-            # if it is an ARM ID, don't process it
-            if not is_ARM_id_for_parented_resource(
-                target.endpoint_deployment_id,
-                snake_to_camel(AzureMLResourceType.ONLINE_ENDPOINT),
-                AzureMLResourceType.DEPLOYMENT,
-            ):
-                endpoint_name, deployment_name = target.endpoint_deployment_id.split(":")
-                target.endpoint_deployment_id = NAMED_RESOURCE_ID_FORMAT_WITH_PARENT.format(
-                    self._subscription_id,
-                    self._resource_group_name,
-                    AZUREML_RESOURCE_PROVIDER,
-                    self._workspace_name,
-                    snake_to_camel(AzureMLResourceType.ONLINE_ENDPOINT),
-                    endpoint_name,
-                    AzureMLResourceType.DEPLOYMENT,
-                    deployment_name,
-                )
-
-        elif target and target.model_id:
-            target.model_id = self._orchestrators.get_asset_arm_id(
-                target.model_id,
-                AzureMLResourceType.MODEL,
-                register_asset=False,
-            )
-
-        # resolve input paths and preprocessing component ids
-        for signal in schedule.create_monitor.monitoring_signals.values():
-            if signal.type == MonitorSignalType.CUSTOM:
-                for input_value in signal.input_datasets.values():
-                    self._job_operations._resolve_job_input(input_value.input_dataset, schedule._base_path)
-                    input_value.pre_processing_component = self._orchestrators.get_asset_arm_id(
-                        asset=input_value.pre_processing_component, azureml_type=AzureMLResourceType.COMPONENT
-                    )
-            else:
-                self._job_operations._resolve_job_inputs(
-                    [signal.target_dataset.dataset.input_dataset, signal.baseline_dataset.input_dataset],
-                    schedule._base_path,
-                )
-                signal.target_dataset.dataset.pre_processing_component = self._orchestrators.get_asset_arm_id(
-                    asset=signal.target_dataset.dataset.pre_processing_component,
-                    azureml_type=AzureMLResourceType.COMPONENT,
-                )
-                signal.baseline_dataset.pre_processing_component = self._orchestrators.get_asset_arm_id(
-                    asset=signal.baseline_dataset.pre_processing_component, azureml_type=AzureMLResourceType.COMPONENT
-                )
