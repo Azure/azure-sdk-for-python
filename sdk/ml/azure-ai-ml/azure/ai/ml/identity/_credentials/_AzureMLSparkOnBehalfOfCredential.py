ba8a63f41af849257f00c4acebd869a6949bb047
Mickey Zhang <ruizh@microsoft.com>
2023-04-28 02:39:47 -0700

    wip

--- sdk/ml/azure-ai-ml/azure/ai/ml/identity/_credentials/_AzureMLSparkOnBehalfOfCredential.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/identity/_credentials/_AzureMLSparkOnBehalfOfCredential.py
@@ -5,6 +5,7 @@
 
 import functools
 import os
+from typing import Optional
 
 from azure.core.pipeline.transport import HttpRequest
 
@@ -13,21 +14,35 @@ from .._internal.managed_identity_client import ManagedIdentityClient
 
 
 class _AzureMLSparkOnBehalfOfCredential(ManagedIdentityBase):
-    def get_client(self, **kwargs):
-        # type: (**Any) -> Optional[ManagedIdentityClient]
+    def get_client(self, **kwargs) -> Optional[ManagedIdentityClient]:
         client_args = _get_client_args(**kwargs)
         if client_args:
             return ManagedIdentityClient(**client_args)
         return None
 
-    def get_unavailable_message(self):
-        # type: () -> str
+    def get_unavailable_message(self) -> str:
         return "AzureML Spark On Behalf of credentials not available in this environment"
 
 
-def _get_client_args(**kwargs):
-    # type: (dict) -> Optional[dict]
+def _get_client_args(**kwargs) -> Optional[dict]:
+    from pyspark.sql import SparkSession
 
+    try:
+        spark = SparkSession.builder.getOrCreate()
+    except Exception:
+        raise Exception("Fail to get spark session, please check if spark environment is set up.")
+
+    spark_conf = spark.sparkContext.getConf()
+    spark_conf_vars = {
+        "AZUREML_SYNAPSE_CLUSTER_IDENTIFIER": "spark.synapse.clusteridentifier",
+        "AZUREML_SYNAPSE_TOKEN_SERVICE_ENDPOINT": "spark.tokenServiceEndpoint",
+    }
+    for env_key, conf_key in spark_conf_vars.items():
+        value = spark_conf.get(conf_key)
+        if value:
+            os.environ[env_key] = value
+
+    # Override default settings if provided via arguments
     if len(kwargs) > 0:
         env_key_from_kwargs = [
             "AZUREML_SYNAPSE_CLUSTER_IDENTIFIER",
@@ -36,28 +51,8 @@ def _get_client_args(**kwargs):
             "AZUREML_RUN_TOKEN_EXPIRY",
         ]
         for env_key in env_key_from_kwargs:
-            if env_key in kwargs.keys():
+            if env_key in kwargs:
                 os.environ[env_key] = kwargs[env_key]
-            else:
-                raise Exception("Unable to initialize AzureMSparkOBOCredential due to invalid arguments")
-    else:
-        from pyspark.sql import SparkSession
-
-        try:
-            spark = SparkSession.builder.getOrCreate()
-        except Exception:
-            raise Exception("Fail to get spark session, please check if spark environment is set up.")
-
-        spark_conf = spark.sparkContext.getConf()
-        spark_conf_vars = {
-            "AZUREML_SYNAPSE_CLUSTER_IDENTIFIER": "spark.synapse.clusteridentifier",
-            "AZUREML_SYNAPSE_TOKEN_SERVICE_ENDPOINT": "spark.tokenServiceEndpoint",
-        }
-        for env_key, conf_key in spark_conf_vars.items():
-            value = spark_conf.get(conf_key)
-            if value:
-                os.environ[env_key] = value
-
 
     token_service_endpoint = os.environ.get("AZUREML_SYNAPSE_TOKEN_SERVICE_ENDPOINT")
     obo_access_token = os.environ.get("AZUREML_OBO_CANARY_TOKEN")
@@ -85,9 +80,7 @@ def _get_client_args(**kwargs):
     )
 
 
-def _get_request(url, resource):
-    # type: (str, str, dict) -> HttpRequest
-
+def _get_request(url, resource) -> HttpRequest:
     obo_access_token = os.environ.get("AZUREML_OBO_CANARY_TOKEN")
     experiment_name = os.environ.get("AZUREML_ARM_PROJECT_NAME")
     run_id = os.environ.get("AZUREML_RUN_ID")
@@ -95,8 +88,6 @@ def _get_request(url, resource):
     tid = os.environ.get("TID")
     obo_service_endpoint = os.environ.get("AZUREML_OBO_SERVICE_ENDPOINT")
     cluster_identifier = os.environ.get("AZUREML_SYNAPSE_CLUSTER_IDENTIFIER")
-
-    request = HttpRequest("GET", url)
 
     request_body = {
         "oboToken": obo_access_token,
@@ -112,6 +103,5 @@ def _get_request(url, resource):
         "obo-access-token": obo_access_token,
         "x-ms-cluster-identifier": cluster_identifier,
     }
-
-    request.format_parameters(dict({"resource": resource}))
+    request = HttpRequest(method="POST", url=url, headers=headers, data=request_body)
     return request
