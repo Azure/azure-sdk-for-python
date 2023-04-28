ed2564743f74ff2073d300e2219a717691538e6a
nemanjarajic <nrajic30@gmail.com>
2023-04-28 13:10:12 -0700

    fix

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/job/parameterized_spark.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/job/parameterized_spark.py
@@ -132,27 +132,29 @@ class ParameterizedSparkSchema(PathAwareSchema):
     @pre_load
     def deserialize_field_names(self, data, **kwargs):
         conf = data["conf"] if "conf" in data else None
-        for field_key, dict_key in CONF_KEY_MAP.items():
-            value = conf.get(dict_key, None)
-            if dict_key in conf and value is not None:
-                del conf[dict_key]
-                conf[field_key] = value
-        data["conf"] = conf
+        if conf is not None:
+            for field_key, dict_key in CONF_KEY_MAP.items():
+                value = conf.get(dict_key, None)
+                if dict_key in conf and value is not None:
+                    del conf[dict_key]
+                    conf[field_key] = value
+            data["conf"] = conf
         return data
 
     @post_dump(pass_original=True)
     def serialize_field_names(self, data: Dict[str, Any], original_data: Dict[str, Any], **kwargs):
         # pass original data here to dump conf fields which are not defined in SparkConfSchema
         conf = data["conf"] if "conf" in data else None
-        for field_name, _ in original_data.conf.items():
-            if field_name not in conf:
-                conf[field_name] = original_data[field_name]
-        for field_name, dict_name in CONF_KEY_MAP.items():
-            val = conf.get(field_name, None)
-            if field_name in conf and val is not None:
-                del conf[field_name]
-                conf[dict_name] = val
-        data["conf"] = conf
+        if conf is not None and original_data.conf is not None:
+            for field_name, _ in original_data.conf.items():
+                if field_name not in conf:
+                    conf[field_name] = original_data[field_name]
+            for field_name, dict_name in CONF_KEY_MAP.items():
+                val = conf.get(field_name, None)
+                if field_name in conf and val is not None:
+                    del conf[field_name]
+                    conf[dict_name] = val
+            data["conf"] = conf
         return data
     
     @post_load
