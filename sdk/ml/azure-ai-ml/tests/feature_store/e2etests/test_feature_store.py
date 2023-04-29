812e8187511ed8a6bf721e1b0561831917a60cc8
Mayank Kumar <maykumar@microsoft.com>
2023-04-28 20:37:49 -0700

    Fix feature store and feature set tests

--- sdk/ml/azure-ai-ml/tests/feature_store/e2etests/test_feature_store.py
+++ sdk/ml/azure-ai-ml/tests/feature_store/e2etests/test_feature_store.py
@@ -8,7 +8,7 @@ import pytest
 from devtools_testutils import AzureRecordedTestCase, is_live
 
 from azure.ai.ml import MLClient
-from azure.ai.ml.entities._feature_store.feature_store import _FeatureStore
+from azure.ai.ml.entities._feature_store.feature_store import FeatureStore
 from azure.core.polling import LROPoller
 
 
@@ -32,31 +32,31 @@ class TestFeatureStore(AzureRecordedTestCase):
         fs_updated_description = f"{fs_name} updated description"
         fs_display_name = f"{fs_name} display name"
 
-        fs = _FeatureStore(name=fs_name, description=fs_description, display_name=fs_display_name)
-        fs_poller = client._feature_stores.begin_create(feature_store=fs)
+        fs = FeatureStore(name=fs_name, description=fs_description, display_name=fs_display_name)
+        fs_poller = client.feature_stores.begin_create(feature_store=fs)
         assert isinstance(fs_poller, LROPoller)
 
         create_fs = fs_poller.result()
-        assert isinstance(create_fs, _FeatureStore)
+        assert isinstance(create_fs, FeatureStore)
 
-        fs_result = client._feature_stores.get(fs_name)
-        assert isinstance(fs_result, _FeatureStore)
+        fs_result = client.feature_stores.get(fs_name)
+        assert isinstance(fs_result, FeatureStore)
         assert fs_result.name == fs_name
 
-        fs_list = client._feature_stores.list()
+        fs_list = client.feature_stores.list()
         assert any(fs_name == fs.name for fs in fs_list)
 
-        fs = _FeatureStore(name=fs_name, description=fs_updated_description, display_name=fs_display_name)
-        fs_poller = client._feature_stores.begin_update(feature_store=fs)
+        fs = FeatureStore(name=fs_name, description=fs_updated_description, display_name=fs_display_name)
+        fs_poller = client.feature_stores.begin_update(feature_store=fs)
         assert isinstance(fs_poller, LROPoller)
 
         updated_fs = fs_poller.result()
-        assert isinstance(updated_fs, _FeatureStore)
+        assert isinstance(updated_fs, FeatureStore)
         assert updated_fs.description == fs_updated_description
 
-        fs_poller = client._feature_stores.begin_delete(name=fs_name, delete_dependent_resources=True)
+        fs_poller = client.feature_stores.begin_delete(name=fs_name, delete_dependent_resources=True)
         assert isinstance(fs_poller, LROPoller)
         fs_poller.result()
 
         with pytest.raises(Exception):
-            client._feature_stores.get(fs_name)
+            client.feature_stores.get(fs_name)
