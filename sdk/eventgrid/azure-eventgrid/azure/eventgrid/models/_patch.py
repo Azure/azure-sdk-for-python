69a61e12c10d5dd7de08a451b88bc3eb6f5e877a
l0lawrence <llawrence@microsoft.com>
2023-05-01 14:52:18 -0700

    update patch models

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/models/_patch.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/models/_patch.py
@@ -7,8 +7,7 @@
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
 from typing import List, overload, Mapping, Any
-from ._models import LockToken
-from ._models import ReceiveDetails as InternalReceiveDetails, ReceiveResult as InternalReceiveResult, BrokerProperties as InternalBrokerProperties
+from ._models import ReceiveDetails as InternalReceiveDetails, ReceiveResult, BrokerProperties
 from azure.core.messaging import CloudEvent
 
 class ReceiveDetails(InternalReceiveDetails):
@@ -28,63 +27,6 @@ class ReceiveDetails(InternalReceiveDetails):
         *,
         broker_properties: "BrokerProperties",
         event: CloudEvent,
-    ):
-        ...
-
-    @overload
-    def __init__(self, mapping: Mapping[str, Any]):
-        """
-        :param mapping: raw JSON to initialize the model.
-        :type mapping: Mapping[str, Any]
-        """
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:  # pylint: disable=useless-super-delegation
-        super().__init__(*args, **kwargs)
-
-class ReceiveResult(InternalReceiveResult):
-    """Details of the Receive operation response.
-
-    All required parameters must be populated in order to send to Azure.
-
-    :ivar value: Array of receive responses, one per cloud event. Required.
-    :vartype value: list[~azure.eventgrid.models.ReceiveDetails]
-    """
-
-    @overload
-    def __init__(
-        self,
-        *,
-        value: List["ReceiveDetails"],
-    ):
-        ...
-
-    @overload
-    def __init__(self, mapping: Mapping[str, Any]):
-        """
-        :param mapping: raw JSON to initialize the model.
-        :type mapping: Mapping[str, Any]
-        """
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:  # pylint: disable=useless-super-delegation
-        super().__init__(*args, **kwargs)
-
-class BrokerProperties(InternalBrokerProperties):
-    """Properties of the Event Broker operation.
-
-    All required parameters must be populated in order to send to Azure.
-
-    :ivar lock_token: The token used to lock the event. Required.
-    :vartype lock_token: ~azure.eventgrid.models.LockToken
-    :ivar delivery_attempt_count: The attempt count for deliverying the event. Required.
-    :vartype delivery_attempt_count: int
-    """
-
-    @overload
-    def __init__(
-        self,
-        *,
-        lock_token: "LockToken",
-        delivery_attempt_count: int,
     ):
         ...
 
