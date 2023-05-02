04a54f18554be1683d1c28f010d66ce4fb790484
ZhidaLiu <86350902+ZhidaLiu@users.noreply.github.com>
2023-05-02 14:18:53 -0700

    Update YAMLRefDocLinks for workspaceHub

--- sdk/ml/azure-ai-ml/azure/ai/ml/constants/_common.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/constants/_common.py
@@ -455,7 +455,7 @@ class YAMLRefDocLinks:
     FEATURE_STORE = "https://aka.ms/ml-cli-v2-featurestore-yaml-reference"
     FEATURE_SET = "https://aka.ms/ml-cli-v2-featureset-yaml-reference"
     FEATURE_STORE_ENTITY = "https://aka.ms/ml-cli-v2-featurestore-entity-yaml-reference"
-
+    WORKSPACEHUB = "https://aka.ms/ml-cli-v2-workspace-hub-entity-yaml-reference"
 
 class YAMLRefDocSchemaNames:
     """YAML reference document schema names.
