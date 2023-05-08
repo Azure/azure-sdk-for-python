01975f5fcaf90bd623f5ab1f37a7813b470a6d75
Jun Min <mijun@microsoft.com>
2023-05-08 12:49:07 +0800

    format

--- sdk/ml/azure-ai-ml/azure/ai/ml/_local_endpoints/mdc_config_resolver.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_local_endpoints/mdc_config_resolver.py
@@ -9,8 +9,8 @@ from azure.ai.ml.entities._deployment.data_collector import DataCollector
 
 class MdcConfigResolver(object):
     def __init__(
-            self,
-            data_collector: DataCollector,
+        self,
+        data_collector: DataCollector,
     ):
         self.environment_variables = {}
         self.volumes = {}
@@ -28,24 +28,28 @@ class MdcConfigResolver(object):
         if data_collector.sampling_rate:
             sampling_percentage = int(data_collector.sampling_rate * 100)
 
-        self.mdc_config = {
-            "collections": {},
-            "runMode": "local"
-        }
+        self.mdc_config = {"collections": {}, "runMode": "local"}
         custom_logging_enabled = False
         for k, v in data_collector.collections:
             if v.enabled:
                 lower_k = k.lower()
-                if lower_k not in ('request', 'response'):
+                if lower_k not in ("request", "response"):
                     custom_logging_enabled = True
                     self.mdc_config["collections"][lower_k] = {}
                     self.mdc_config["collections"][lower_k]["enabled"] = True
-                    self.mdc_config["collections"][lower_k]["sampling_percentage"] = sampling_percentage
+                    self.mdc_config["collections"][lower_k][
+                        "sampling_percentage"
+                    ] = sampling_percentage
 
         self.custom_logging_enabled = custom_logging_enabled
 
-        if data_collector.request_logging and data_collector.request_logging.capture_headers:
-            self.mdc_config["captureHeaders"] = data_collector.request_logging.capture_headers
+        if (
+            data_collector.request_logging
+            and data_collector.request_logging.capture_headers
+        ):
+            self.mdc_config[
+                "captureHeaders"
+            ] = data_collector.request_logging.capture_headers
 
     def write_file(self, directory_path: str) -> None:
         mdc_setting_path = str(Path(directory_path, "mdc_config.json").resolve())
