fe20caee5f599ac14793e7b7a6dc4e436e1fb7a1
Paul Van Eck <paulvaneck@microsoft.com>
2023-05-08 16:06:09 -0700

    [Template][Docs] Update template documentation
    These are updated to more accurately reflect the current state of
    things.
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/template/azure-template/azure/template/template_code.py
+++ sdk/template/azure-template/azure/template/template_code.py
@@ -1,3 +1,10 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+
 def template_main():
     print("Package code.")
     return True
