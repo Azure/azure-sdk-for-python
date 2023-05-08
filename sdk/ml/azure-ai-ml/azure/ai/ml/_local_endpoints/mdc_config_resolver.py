8c7e0fb4b63bcffb21af9414dc2fdaef391b30dc
Jun Min <mijun@microsoft.com>
2023-05-08 14:55:34 +0800

    black run

--- sdk/ml/azure-ai-ml/azure/ai/ml/_local_endpoints/mdc_config_resolver.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_local_endpoints/mdc_config_resolver.py
@@ -37,19 +37,12 @@ class MdcConfigResolver(object):
                     custom_logging_enabled = True
                     self.mdc_config["collections"][lower_k] = {}
                     self.mdc_config["collections"][lower_k]["enabled"] = True
-                    self.mdc_config["collections"][lower_k][
-                        "sampling_percentage"
-                    ] = sampling_percentage
+                    self.mdc_config["collections"][lower_k]["sampling_percentage"] = sampling_percentage
 
         self.custom_logging_enabled = custom_logging_enabled
 
-        if (
-            data_collector.request_logging
-            and data_collector.request_logging.capture_headers
-        ):
-            self.mdc_config[
-                "captureHeaders"
-            ] = data_collector.request_logging.capture_headers
+        if data_collector.request_logging and data_collector.request_logging.capture_headers:
+            self.mdc_config["captureHeaders"] = data_collector.request_logging.capture_headers
 
     def write_file(self, directory_path: str) -> None:
         mdc_setting_path = str(Path(directory_path, "mdc_config.json").resolve())
