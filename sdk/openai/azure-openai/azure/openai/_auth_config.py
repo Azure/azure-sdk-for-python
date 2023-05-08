68a8d7b45f8c904fd8c5a71953b4424f4608abb1
Krista Pratico <krpratic@microsoft.com>
2023-05-08 10:03:19 -0700

    update login to take optional kw args

--- sdk/openai/azure-openai/azure/openai/_auth_config.py
+++ sdk/openai/azure-openai/azure/openai/_auth_config.py
@@ -4,11 +4,12 @@
 # ------------------------------------
 
 import os
-from typing import Optional, Union
+from typing import Optional, Union, List
 import time
 import logging
 import requests
 import openai
+from typing_extensions import Literal
 from azure.core.credentials import TokenCredential
 from azure.identity import DefaultAzureCredential
 
@@ -25,30 +26,71 @@ class CredentialRefresh:
 
     def __call__(self, req):
         if isinstance(self.credential, str):
-            req.headers["api-key"] = openai.api_key
+            if openai.api_type in ["azure", "openai", "open_ai"]:
+                req.headers["api-key"] = openai.api_key
+            elif openai.api_type in ["azuread", "azure_ad"]:
+                req.headers["Authorization"] = f"Bearer {openai.api_key}"
             return req
-        if not self.cached_token or self.cached_token.expires_on - time.time() < 300:
+        elif not self.cached_token or self.cached_token.expires_on - time.time() < 300:
             self.cached_token = self.credential.get_token(*self.scopes)
         req.headers["Authorization"] = f"Bearer {self.cached_token.token}"
         return req
 
 
-def login():
+def login(
+    *,
+    api_key: Optional[Union[str, TokenCredential]] = None,
+    api_key_path: Optional[str] = None,
+    api_base: Optional[str] = None,
+    api_type: Optional[Literal["azuread", "azure_ad", "azure", "openai", "open_ai"]] = None,
+    api_version: Optional[str] = None,
+    organization: Optional[str] = None,
+    scopes: Optional[Union[str, List[str]]] = None,
+):
 
     session = requests.Session()  # match openai config?
 
-    # API version is not configured by env var in openai yet
-    openai.api_version = os.getenv("OPENAI_API_VERSION", "2022-12-01")
+    if api_base:
+        openai.api_base = api_base
 
-    api_key = os.getenv("OPENAI_API_KEY")
-    if api_key in["azuread", "azure_ad"]:
+    if api_key:
+        openai.api_key = api_key
+
+    if api_key_path:
+        openai.api_key_path = api_key_path
+
+    if organization:
+        openai.organization = organization
+
+    if api_type:
+        openai.api_type = api_type
+
+    if api_version:
+        openai.api_version = api_version
+
+    if openai.api_version is None:
+        # API version is not configured by env var in openai yet
+        # PR: https://github.com/openai/openai-python/pull/438
+        openai.api_version = os.getenv("OPENAI_API_VERSION", "2022-12-01")
+
+    if hasattr(api_key, "get_token"):
         openai.api_type = "azuread"
         openai.api_key = "API_KEY"
+        scopes = [scopes] if isinstance(scopes, str) else scopes
+        if scopes is None:
+            scopes = ["https://cognitiveservices.azure.com/.default"]
+        session.auth = CredentialRefresh(api_key, *scopes)
+    elif openai.api_type in ["azuread", "azure_ad"] and openai.api_key is None:
+        openai.api_key = "API_KEY"
         credential = DefaultAzureCredential()
-        scopes = ["https://cognitiveservices.azure.com/.default"]
+        scopes = [scopes] if isinstance(scopes, str) else scopes
+        if scopes is None:
+            scopes = ["https://cognitiveservices.azure.com/.default"]
         session.auth = CredentialRefresh(credential, *scopes)
     else:
-        session.auth = CredentialRefresh(api_key)
+        if openai.api_type is None:
+            raise ValueError("Must pass api_type or set OPENAI_API_TYPE environment variable with api_key.")
+        session.auth = CredentialRefresh(openai.api_key)
 
     # probably should not set session if using OAI...
     openai.requestssession = session
--- sdk/openai/azure-openai/azure/openai/aio/_auth_config_async.py
+++ sdk/openai/azure-openai/azure/openai/aio/_auth_config_async.py
@@ -4,12 +4,14 @@
 # ------------------------------------
 
 import os
-import typing
+from typing import Optional, Union, List, Dict, Any
 import time
 import logging
 import asyncio
 import openai
 import aiohttp
+from typing_extensions import Literal
+from azure.core.credentials_async import AsyncTokenCredential
 from azure.identity.aio import DefaultAzureCredential
 
 
@@ -17,13 +19,15 @@ log = logging.getLogger(__name__)
 
 
 class AsyncCredentialRefresh(aiohttp.ClientRequest):
-    props: typing.Dict[str, typing.Any]  # this is not ideal...
+    props: Dict[str, Any]  # this is not ideal...
 
     async def _refresh_credential(self):
         if isinstance(self.props["credential"], str):
-            self.headers["api-key"] = openai.api_key
-            return
-        if self.props.get("cached_token") is None or self.props.get("cached_token").expires_on - time.time() < 300:
+            if openai.api_type in ["azure", "openai", "open_ai"]:
+                self.headers["api-key"] = openai.api_key
+            elif openai.api_type in ["azuread", "azure_ad"]:
+                self.headers["Authorization"] = f"Bearer {openai.api_key}"
+        elif self.props.get("cached_token") is None or self.props.get("cached_token").expires_on - time.time() < 300:
             async with self.props.get("lock"):
                 if self.props.get("cached_token") is None or self.props.get("cached_token").expires_on - time.time() < 300:
                     self.props["cached_token"] = await self.props["credential"].get_token(*self.props.get("scopes"))
@@ -34,21 +38,59 @@ class AsyncCredentialRefresh(aiohttp.ClientRequest):
         return await super().send(conn)
 
 
-def login():
+def login(
+    *,
+    api_key: Optional[Union[str, AsyncTokenCredential]] = None,
+    api_key_path: Optional[str] = None,
+    api_base: Optional[str] = None,
+    api_type: Optional[Literal["azuread", "azure_ad", "azure", "openai", "open_ai"]] = None,
+    api_version: Optional[str] = None,
+    organization: Optional[str] = None,
+    scopes: Optional[Union[str, List[str]]] = None,
+):
 
     request_class = AsyncCredentialRefresh
 
-    # API version is not configured by env var in openai yet
-    openai.api_version = os.getenv("OPENAI_API_VERSION", "2022-12-01")
+    if api_base:
+        openai.api_base = api_base
 
-    api_key = os.getenv("OPENAI_API_KEY")
-    if api_key in["azuread", "azure_ad"]:
+    if api_key:
+        openai.api_key = api_key
+
+    if api_key_path:
+        openai.api_key_path = api_key_path
+
+    if organization:
+        openai.organization = organization
+
+    if api_type:
+        openai.api_type = api_type
+
+    if api_version:
+        openai.api_version = api_version
+
+    if openai.api_version is None:
+        # API version is not configured by env var in openai yet
+        # PR: https://github.com/openai/openai-python/pull/438
+        openai.api_version = os.getenv("OPENAI_API_VERSION", "2022-12-01")
+
+    if hasattr(api_key, "get_token"):
         openai.api_type = "azuread"
         openai.api_key = "API_KEY"
+        scopes = [scopes] if isinstance(scopes, str) else scopes
+        if scopes is None:
+            scopes = ["https://cognitiveservices.azure.com/.default"]
+        request_class.props = {"credential": api_key, "scopes": scopes, "cached_token": None, "lock": asyncio.Lock()}
+    elif openai.api_type in ["azuread", "azure_ad"] and openai.api_key is None:
+        openai.api_key = "API_KEY"
         credential = DefaultAzureCredential()
-        scopes = ["https://cognitiveservices.azure.com/.default"]
+        scopes = [scopes] if isinstance(scopes, str) else scopes
+        if scopes is None:
+            scopes = ["https://cognitiveservices.azure.com/.default"]
         request_class.props = {"credential": credential, "scopes": scopes, "cached_token": None, "lock": asyncio.Lock()}
     else:
+        if openai.api_type is None:
+            raise ValueError("Must pass api_type or set OPENAI_API_TYPE environment variable with api_key.")
         request_class.props = {"credential": api_key}
 
     # probably should not set session if using OAI...
