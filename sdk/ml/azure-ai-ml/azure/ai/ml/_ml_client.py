6f3d05be712755a0da113056058c18a83cc29c59
nemanjarajic <nrajic30@gmail.com>
2023-05-09 10:03:33 -0700

    april stable api

--- sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
@@ -348,7 +348,7 @@ class MLClient:
         self._workspace_connections = WorkspaceConnectionsOperations(
             self._operation_scope,
             self._operation_config,
-            self._service_client_04_2023_preview,
+            self._service_client_04_2023,
             self._operation_container,
             self._credential,
         )
