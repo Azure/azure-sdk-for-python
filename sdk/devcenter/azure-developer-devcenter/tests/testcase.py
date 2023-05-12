28f767c3c02c22ab5f8b43d04c5e8d05f7400688
Oleg Demchenko <oldemche@microsoft.com>
2023-05-12 03:26:49 -0700

    Moving tests to the azure repo from my fork (#30377)
    * Updated tests.
    
    * Updated API version.
    
    * Update environments samples
    
    ---------
    
    Co-authored-by: Jared Rewerts <jarewert@microsoft.com>

--- sdk/devcenter/azure-developer-devcenter/tests/testcase.py
+++ sdk/devcenter/azure-developer-devcenter/tests/testcase.py
@@ -18,10 +18,12 @@ class DevcenterTest(AzureRecordedTestCase):
 DevcenterPowerShellPreparer = functools.partial(
     PowerShellPreparer,
     "devcenter", 
-    devcenter_endpoint="https://8ab2df1c-ed88-4946-a8a9-e1bbb3e4d1fd-sdk-dc-na4b3zkj5hmeo.eastus.devcenter.azure.com",
-    devcenter_project_name="sdk-default-project",
+    devcenter_endpoint="https://8ab2df1c-ed88-4946-a8a9-e1bbb3e4d1fd-dc-sdk-tests.centraluseuap.devcenter.azure.com",
+    devcenter_project_name="proj-sdk-tests",
     devcenter_pool_name="sdk-default-pool",
-    devcenter_test_user_id="11111111-1111-1111-1111-111111111111",
+    devcenter_test_user_id="df428e89-1bc2-4e72-b736-032c31a6cd97",
     devcenter_environment_type_name="sdk-default-environment-type",
     devcenter_catalog_name="sdk-default-catalog",
-    devcenter_catalog_item_name="Empty")
+    devcenter_environment_definition_name="Sandbox",
+    devcenter_devbox_name="PythonDevBox",
+    devcenter_environment_name="pythonenvironment")
