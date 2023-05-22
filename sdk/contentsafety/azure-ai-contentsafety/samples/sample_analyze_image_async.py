ea06ca1e7be905ece2526e326f002296f00c1ceb
mengaims <50131889+mengaims@users.noreply.github.com>
2023-05-22 15:20:50 +0800

    Update samples and readme for Content Safety (#30444)
    * Add initial projectcarnegie python sdk
    
    * rename from projectcarnegie to contentsafety
    
    * update sdk
    
    * update tests and samples
    
    * change the release date in changelog
    
    * Fix changelog and add list managment sample
    
    * Add list management sample
    
    * update samples
    
    * Improve sample response processing
    
    * update tests
    
    * update setup.py
    
    * Fix PACKAGE_PPRINT_NAME
    
    * Fix sanitizer
    
    * Fix test recording not match error
    
    * Fix spell check
    
    * add tsp-location.yaml
    
    * generate code powershell script
    
    * Change package print name
    
    * Mitigate readme issue and fix error processing
    
    * Fix remaining Pyright errors and change path to absolute path
    
    * Fix usage of create_or_update_text_blocklist
    
    * regen with latest emitter
    
    * Add readme file
    
    * Remove locale in url links in readme
    
    * Refine tests
    
    * Refine samples
    
    * Resolve readme comments
    
    * Replace sample with code snippet
    
    * update samples
    
    * missing header
    
    * Add async samples and improve readme
    
    * run snippet updater
    
    * improve readme
    
    * Fix typo in readme
    
    * Replace relative link with absolute link
    
    * Fix problem in snippet
    
    * improve blockItem related sample to reduce confusion
    
    * Replace PowerShellPreparer with EnvironmentVariableLoader
    
    * Replace format with f-string
    
    * Remove is not None
    
    * Try remove some None check
    
    * Revert "Try remove some None check"
    
    This reverts commit d08d55e99816f560a833df21110b63cb23d3bd37.
    
    ---------
    
    Co-authored-by: Catalina Peralta <catalinaperaltah@hotmail.com>

--- sdk/contentsafety/azure-ai-contentsafety/samples/sample_analyze_image_async.py
+++ sdk/contentsafety/azure-ai-contentsafety/samples/sample_analyze_image_async.py
@@ -5,12 +5,13 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+import asyncio
 
-def analyze_image():
-    # [START analyze_image]
-    
+async def analyze_image_async():
+    # [START analyze_image_async]
+
     import os
-    from azure.ai.contentsafety import ContentSafetyClient
+    from azure.ai.contentsafety.aio import ContentSafetyClient
     from azure.core.credentials import AzureKeyCredential
     from azure.core.exceptions import HttpResponseError
     from azure.ai.contentsafety.models import AnalyzeImageOptions, ImageData
@@ -22,21 +23,22 @@ def analyze_image():
     # Create an Content Safety client
     client = ContentSafetyClient(endpoint, AzureKeyCredential(key))
 
-    # Build request
-    with open(image_path, "rb") as file:
-        request = AnalyzeImageOptions(image=ImageData(content=file.read()))
+    async with client:
+        # Build request
+        with open(image_path, "rb") as file:
+            request = AnalyzeImageOptions(image=ImageData(content=file.read()))
 
-    # Analyze image
-    try:
-        response = client.analyze_image(request)
-    except HttpResponseError as e:
-        print("Analyze image failed.")
-        if e.error:
-            print(f"Error code: {e.error.code}")
-            print(f"Error message: {e.error.message}")
+        # Analyze image
+        try:
+            response = await client.analyze_image(request)
+        except HttpResponseError as e:
+            print("Analyze image failed.")
+            if e.error:
+                print(f"Error code: {e.error.code}")
+                print(f"Error message: {e.error.message}")
+                raise
+            print(e)
             raise
-        print(e)
-        raise
 
     if response.hate_result:
         print(f"Hate severity: {response.hate_result.severity}")
@@ -47,8 +49,11 @@ def analyze_image():
     if response.violence_result:
         print(f"Violence severity: {response.violence_result.severity}")
 
-    # [END analyze_image]
+    # [END analyze_image_async]
 
+async def main():
+    await analyze_image_async()
 
 if __name__ == "__main__":
-    analyze_image()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
--- sdk/contentsafety/azure-ai-contentsafety/samples/sample_analyze_text_async.py
+++ sdk/contentsafety/azure-ai-contentsafety/samples/sample_analyze_text_async.py
@@ -5,12 +5,13 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+import asyncio
 
-def analyze_text():
-    # [START analyze_text]
+async def analyze_text_async():
+    # [START analyze_text_async]
 
     import os
-    from azure.ai.contentsafety import ContentSafetyClient
+    from azure.ai.contentsafety.aio import ContentSafetyClient
     from azure.core.credentials import AzureKeyCredential
     from azure.core.exceptions import HttpResponseError
     from azure.ai.contentsafety.models import AnalyzeTextOptions, TextCategory
@@ -22,29 +23,34 @@ def analyze_text():
     # Create an Content Safety client
     client = ContentSafetyClient(endpoint, AzureKeyCredential(key))
 
-    # Read sample data
-    with open(text_path) as f:
-        # Build request
-        request = AnalyzeTextOptions(text=f.readline(), categories=[TextCategory.HATE, TextCategory.SELF_HARM])
+    async with client:
+        # Read sample data
+        with open(text_path) as f:
+            # Build request
+            request = AnalyzeTextOptions(text=f.readline(), categories=[TextCategory.HATE, TextCategory.SELF_HARM])
 
-    # Analyze text
-    try:
-        response = client.analyze_text(request)
-    except HttpResponseError as e:
-        print("Analyze text failed.")
-        if e.error:
-            print(f"Error code: {e.error.code}")
-            print(f"Error message: {e.error.message}")
+        # Analyze text
+        try:
+            response = await client.analyze_text(request)
+        except HttpResponseError as e:
+            print("Analyze text failed.")
+            if e.error:
+                print(f"Error code: {e.error.code}")
+                print(f"Error message: {e.error.message}")
+                raise
+            print(e)
             raise
-        print(e)
-        raise
 
     if response.hate_result:
         print(f"Hate severity: {response.hate_result.severity}")
     if response.self_harm_result:
         print(f"SelfHarm severity: {response.self_harm_result.severity}")
 
-    # [END analyze_text]
+    # [END analyze_text_async]
+
+async def main():
+    await analyze_text_async()
 
 if __name__ == "__main__":
-    analyze_text()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
