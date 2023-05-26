b3e62b64dacbba14f72e8d36fdab7214463e2124
williamzhao87 <williamzhao87@users.noreply.github.com>
2023-05-26 13:41:16 -0400

    Update with reofferTimeUtc for Decline (#30545)

--- sdk/communication/azure-communication-jobrouter/tests/test_assignment_scenario.py
+++ sdk/communication/azure-communication-jobrouter/tests/test_assignment_scenario.py
@@ -6,6 +6,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
+from datetime import datetime
 import pytest
 from devtools_testutils import recorded_by_proxy
 from azure.core.exceptions import (
@@ -239,7 +240,8 @@ class TestAssignmentScenario(RouterRecordedTestCase):
         with pytest.raises(HttpResponseError) as sre:
             router_client.decline_job_offer(
                 worker_id = self.get_router_worker_id(),
-                offer_id = offer_id
+                offer_id = offer_id,
+                reoffer_time_utc = datetime.min
             )
         assert sre is not None
         
