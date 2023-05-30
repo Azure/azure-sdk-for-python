5d40ba659ab180bda1244bd70f60a850235c1faa
Diondra Peck <dipeck@microsoft.com>
2023-05-30 11:03:25 -0700

    Update MonitorDefinition docstring

---
+++ sdk/ml/azure-ai-ml/samples/ml_samples_spark_configurations.py
@@ -0,0 +1,79 @@
+# coding: utf-8
+
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+"""
+FILE: ml_samples_spark_configurations.py
+DESCRIPTION:
+    These samples demonstrate different ways to configure Spark jobs and components.
+USAGE:
+    python ml_samples_spark_configurations.py
+
+"""
+
+import os
+
+
+class SparkConfigurationOptions(object):
+    def ml_spark_config(self):
+        from azure.identity import DefaultAzureCredential
+
+        from azure.ai.ml import MLClient
+
+        subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
+        resource_group = os.environ["RESOURCE_GROUP_NAME"]
+        credential = DefaultAzureCredential()
+        ml_client = MLClient(credential, subscription_id, resource_group, workspace_name="test-ws1")
+
+        # [START spark_monitor_definition]
+        from azure.ai.ml.entities import (
+            AlertNotification,
+            MonitorDefinition,
+            MonitoringTarget,
+            SparkResourceConfiguration,
+        )
+
+        monitor_definition = MonitorDefinition(
+            compute=SparkResourceConfiguration(instance_type="standard_e4s_v3", runtime_version="3.2"),
+            monitoring_target=MonitoringTarget(
+                endpoint_deployment_id="azureml:fraud_detection_endpoint:fraud_detection_deployment"
+            ),
+            alert_notification=AlertNotification(emails=["abc@example.com", "def@example.com"]),
+        )
+
+        # [END spark_monitor_definition]
+
+        # [START spark_component_definition]
+        from azure.ai.ml.entities import SparkComponent
+
+        component = SparkComponent(
+            name="add_greeting_column_spark_component",
+            display_name="Aml Spark add greeting column test module",
+            description="Aml Spark add greeting column test module",
+            version="1",
+            inputs={
+                "file_input": {"type": "uri_file", "mode": "direct"},
+            },
+            driver_cores=2,
+            driver_memory="1g",
+            executor_cores=1,
+            executor_memory="1g",
+            executor_instances=1,
+            code="./src",
+            entry={"file": "add_greeting_column.py"},
+            py_files=["utils.zip"],
+            files=["my_files.txt"],
+            args="--file_input ${{inputs.file_input}}",
+            base_path="./tests/test_configs/dsl_pipeline/spark_job_in_pipeline",
+        )
+
+        # [END spark_component_definition]
+
+
+if __name__ == "__main__":
+    sample = SparkConfigurationOptions()
+    sample.ml_spark_config()
