cac00a4297f480534a9cd3cec053641a70bc1b92
minnieliu <minnieliu96@hotmail.com>
2023-06-06 13:17:55 -0700

    Introduce new Python SDK version for Rooms GA (#30629)
    * Introduce new Python SDK version for Rooms GA
    
    * Autogenerate the changes with new GA version and record new tests
    
    * Update swagger readme link
    
    * Update changelog release date
    
    * Update changelog
    
    ---------
    
    Co-authored-by: Minnie Liu <peiliu@microsoft.com>

--- sdk/communication/azure-communication-rooms/setup.py
+++ sdk/communication/azure-communication-rooms/setup.py
@@ -43,7 +43,7 @@ setup(
     author_email="azpysdkhelp@microsoft.com",
     url="https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/communication/azure-communication-rooms",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
