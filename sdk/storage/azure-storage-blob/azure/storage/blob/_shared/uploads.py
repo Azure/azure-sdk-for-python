39f4f45e20d2602449ca911e2cda68f7489a3ca5
Vincent Tran <vincenttran@microsoft.com>
2023-06-08 11:08:30 -0700

    Pop back in no-self-use since current pipeline uses it

--- sdk/storage/azure-storage-blob/azure/storage/blob/_shared/uploads.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/_shared/uploads.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+# pylint: disable=no-self-use
 
 from concurrent import futures
 from io import BytesIO, IOBase, SEEK_CUR, SEEK_END, SEEK_SET, UnsupportedOperation
