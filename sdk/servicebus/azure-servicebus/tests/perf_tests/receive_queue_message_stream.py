74e91b4d2a88352d9d45a09eaf80412b4b81632f
Paul Van Eck <paulvaneck@microsoft.com>
2023-06-12 11:25:37 -0700

    [SB][Perf] Update perf documentation (#30666)
    Update the perf tests README for ServiceBus to reflect the current
    state of the tests.
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/servicebus/azure-servicebus/tests/perf_tests/receive_queue_message_stream.py
+++ sdk/servicebus/azure-servicebus/tests/perf_tests/receive_queue_message_stream.py
@@ -3,8 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import asyncio
-
 from ._test_base import _QueueReceiveTest
 
 
