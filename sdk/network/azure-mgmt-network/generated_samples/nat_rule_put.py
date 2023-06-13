438e25a2a7430203a200bd9b675d9c00b41cb60c
Yuchao Yan <yuchaoyan@microsoft.com>
2023-06-13 11:02:39 +0800

    [network] test for api-version (#30721)
    * Update swagger_to_sdk_config_autorest.json
    
    * test
    
    * Packaging update of azure-mgmt-network
    
    * code and test
    
    * update version
    
    ---------
    
    Co-authored-by: Azure SDK Bot <adxpysdk@microsoft.com>
    Co-authored-by: PythonSdkPipelines <PythonSdkPipelines>

--- sdk/network/azure-mgmt-network/generated_samples/nat_rule_put.py
+++ sdk/network/azure-mgmt-network/generated_samples/nat_rule_put.py
@@ -14,7 +14,7 @@ from azure.mgmt.network import NetworkManagementClient
     pip install azure-identity
     pip install azure-mgmt-network
 # USAGE
-    python p2s_vpn_gateways_disconnect_p2s_vpn_connections.py
+    python nat_rule_put.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,14 +29,23 @@ def main():
         subscription_id="subid",
     )
 
-    response = client.p2_svpn_gateways.begin_disconnect_p2_s_vpn_connections(
-        resource_group_name="p2s-vpn-gateway-test",
-        p2_s_vpn_gateway_name="p2svpngateway",
-        request={"vpnConnectionIds": ["vpnconnId1", "vpnconnId2"]},
+    response = client.nat_rules.begin_create_or_update(
+        resource_group_name="rg1",
+        gateway_name="gateway1",
+        nat_rule_name="natRule1",
+        nat_rule_parameters={
+            "properties": {
+                "externalMappings": [{"addressSpace": "192.168.21.0/24"}],
+                "internalMappings": [{"addressSpace": "10.4.0.0/24"}],
+                "ipConfigurationId": "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworkGateways/cloudnet1-VNG/ipConfigurations/default",
+                "mode": "EgressSnat",
+                "type": "Static",
+            }
+        },
     ).result()
     print(response)
 
 
-# x-ms-original-file: specification/network/resource-manager/Microsoft.Network/stable/2022-11-01/examples/P2sVpnGatewaysDisconnectP2sVpnConnections.json
+# x-ms-original-file: specification/network/resource-manager/Microsoft.Network/stable/2022-11-01/examples/NatRulePut.json
 if __name__ == "__main__":
     main()
