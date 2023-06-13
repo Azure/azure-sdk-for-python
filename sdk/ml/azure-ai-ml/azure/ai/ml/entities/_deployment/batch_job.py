d2d713d98321e301b909083617636105302d79c2
Taji Harris <tajiharris@microsoft.com>
2023-06-13 12:14:18 -0700

    add parameter descriptions

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_deployment/batch_job.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_deployment/batch_job.py
@@ -14,10 +14,66 @@ from azure.ai.ml._restclient.v2020_09_01_dataplanepreview.models import (
 )
 from azure.ai.ml.entities._job.compute_configuration import ComputeConfiguration
 from azure.ai.ml.entities._deployment.deployment_settings import BatchRetrySettings
+from azure.ai.ml.entities._system_data import SystemData
 
 # pylint: disable=too-many-instance-attributes
 class BatchJob(object):
     """Batch jobs that are created with batch deployments/endpoints invocation.
+
+    :param id: The id of the batch job.
+    :type id: str
+    :param name: The name of the batch job resource.
+    :type name: str
+    :param type: Type of the batch job.
+    :type type: str
+    :param status: Status of the job. Possible values include: "NotStarted", "Starting",
+     "Provisioning", "Preparing", "Queued", "Running", "Finalizing", "CancelRequested", "Completed",
+     "Failed", "Canceled", "NotResponding", "Paused", "Unknown".
+    :type status: str
+    :param compute: Compute configuration used to set instance count.
+    :type compute: ComputeConfiguration
+    :param dataset: Input dataset.
+    :type dataset: InferenceDataInputBase
+    :param description: The asset description text.
+    :type description: str
+    :param error_threshold: Error threshold, if the error count for the entire input goes above
+        this value,
+        the batch inference will be aborted. Range is [-1, int.MaxValue]
+        -1 value indicates, ignore all failures during batch inference
+        For FileDataset count of file failures
+        For TabularDataset, this is the count of record failures, defaults to -1
+    :type error_thershold: int
+    :param input_data: Input data for the job.
+    :type input_data: Dict[str, JobInput]
+    :param logging_level: Logging level for batch inference operation. Possible values include:
+     "Info", "Warning", "Debug".
+    :type logging_level: Union[str, BatchLoggingLevel]
+    :param max_concurrency_per_instance: Indicates maximum number of parallelism per instance.
+    :type max_concurrency_per_instance: int
+    :param mini_batch_size: Size of the mini-batch passed to each batch invocation.
+    :type mini_batch_size: int
+    :param job_name: Name of the batch job.
+    :type job_name: str
+    :param ouptut_data: Job output data location.
+    :type output_data: Dict[str, JobOutputV2]
+    :param output_dataset: Output dataset location
+    :type output_dataset: DataVersion
+    :param output_file_name: Output file name.
+    :type output_file_name: str
+    :param partition_keys: Partition keys list used for Named partitioning.
+    :type partition_keys: List[str]
+    :param properties: The asset property dictionary.
+    :type properties: dict[str,str]
+    :param retry_settings: Retry Settings for the batch inference operation.
+    :type retry_settings: BatchRetrySettings
+    :param system_data: System data associated with resource provider.
+    :type system_data: SystemData
+    :param tags: A set of tags. Tag dictionary. Tags can be added, removed, and updated.
+    :type tags: Dict[str,str]
+
+
+
+
 
     This class shouldn't be instantiated directly. Instead, it is used as the return type of batch deployment/endpoint
     invocation and job listing.
@@ -45,6 +101,7 @@ class BatchJob(object):
         partition_keys: Optional[List[str]] = None,
         properties: Optional[Dict[str, str]] = None,
         retry_settings: Optional[BatchRetrySettings] = None,
+        system_data: Optional[SystemData] = None,
         tags: Optional[Dict[str, str]] = None,
         **kwargs  # pylint:disable=unused-argument
     ):
@@ -66,6 +123,7 @@ class BatchJob(object):
         self.output_file_name = output_file_name
         self.partition_keys = partition_keys
         self.properties = properties
+        self.system_data = system_data
         self.retry_settings = retry_settings
         self.tags = tags
 
@@ -92,6 +150,7 @@ class BatchJob(object):
             partition_keys=batch_job_property.partition_keys,
             properties=batch_job_property.properties,
             retry_settings=batch_job_property.retry_settings,
+            system_data=obj.system_data,
             tags=batch_job_property.tags,
         )
 
