56fbb3a05fac510d70b4c20caf794750ac750737
Hope Welch <hopewelch@microsoft.com>
2023-06-15 14:25:39 -0700

    new get sent share invitation test

--- sdk/purview/azure-purview-sharing/tests/test_sent_shares.py
+++ sdk/purview/azure-purview-sharing/tests/test_sent_shares.py
@@ -5,6 +5,7 @@
 # license information.
 # -------------------------------------------------------------------------
 import json
+from uuid import uuid4
 
 from testcase import TestPurviewSharing, PurviewSharingPowerShellPreparer
 from devtools_testutils import recorded_by_proxy
@@ -15,7 +16,8 @@ from azure.purview.sharing.operations._operations import (
     build_sent_shares_get_request,
     build_sent_shares_list_request,
     build_sent_shares_delete_request,
-    build_sent_shares_list_invitations_request
+    build_sent_shares_list_invitations_request,
+    build_sent_shares_get_invitation_request
 )
 
 class TestSentShares(TestPurviewSharing):
@@ -307,3 +309,63 @@ class TestSentShares(TestPurviewSharing):
         except HttpResponseError as e:
             print("Exception " + str(e))
             print("Response " + delete_response.text())
+    ###     ###
+    ### NEW ###
+    ###     ###
+    @PurviewSharingPowerShellPreparer()
+    @recorded_by_proxy
+    def test_get_sent_share_invitation(self, purviewsharing_endpoint):
+        client = self.create_client(endpoint=purviewsharing_endpoint)
+        sent_share_id = uuid4()
+        sent_share_invitation_id = uuid4()
+        sent_share = self.prepare_sent_share()
+
+        request = build_sent_shares_create_or_replace_request(
+            sent_share_id,
+            content_type="application/json",
+            content=json.dumps(sent_share))
+        
+        response = client.send_request(request)
+
+        assert response is not None
+        assert response.status_code == 201, "Invalid Status Code " + str(response.status_code)
+
+        consumerEmail = "consumer@contoso.com"
+
+        invitation = {
+            "invitationKind": "User",
+            "properties": {
+                "targetEmail": consumerEmail,
+                "notify": "true",
+                "expirationDate": "2024-03-02 00:00:00"
+            }
+        }
+
+        invitation_request = build_sent_shares_create_invitation_request(
+            sent_share_id=sent_share_id,
+            sent_share_invitation_id=sent_share_invitation_id,
+            content_type="application/json",
+            content=json.dumps(invitation))
+        
+        invitation_response = client.send_request(invitation_request)
+
+        assert invitation_response is not None
+        assert invitation_response.status_code == 201, "Invalid Status Code " + str(invitation_response.status_code)
+        assert invitation_response.content is not None
+
+        created_invitation = json.loads(invitation_response.content)
+
+        assert created_invitation['id'] == str(sent_share_invitation_id)
+        assert created_invitation['properties']['targetEmail'] == consumerEmail
+
+        get_request = build_sent_shares_get_invitation_request(sent_share_id=sent_share_id, sent_share_invitation_id=sent_share_invitation_id)
+        get_response = client.send_request(get_request)
+
+        assert get_response is not None
+        assert get_response.content is not None
+         
+        retrieved_sent_share_invitation = json.loads(get_response.content)
+
+        assert retrieved_sent_share_invitation['id'] == str(sent_share_invitation_id)
+
+        
