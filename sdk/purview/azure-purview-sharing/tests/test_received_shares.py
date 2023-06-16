63ff252703664ee5539cac5c3c67871b9054b89e
Hope Welch <hopewelch@microsoft.com>
2023-06-15 17:15:53 -0700

    +get all attached shares test&record

--- sdk/purview/azure-purview-sharing/tests/test_received_shares.py
+++ sdk/purview/azure-purview-sharing/tests/test_received_shares.py
@@ -20,7 +20,8 @@ from azure.purview.sharing.operations._operations import (
     build_received_shares_list_detached_request,
     build_received_shares_get_request,
     build_received_shares_delete_request,
-    build_received_shares_create_or_replace_request
+    build_received_shares_create_or_replace_request,
+    build_received_shares_list_attached_request
 )
 
 class TestReceivedShares(TestPurviewSharing):
@@ -82,9 +83,6 @@ class TestReceivedShares(TestPurviewSharing):
         assert len(list_detached['value']) > 0
         # is the number of items that has shareStatus "Detached" equal to the number of all results
         assert len([x for x in list_detached['value'] if x['properties']['shareStatus'] == "Detached"]) == len(list_detached['value'])
-    
-
-
 
     @PurviewSharingPowerShellPreparer()
     @recorded_by_proxy
@@ -149,10 +147,6 @@ class TestReceivedShares(TestPurviewSharing):
         print(retrieved_share)
         assert retrieved_share is not None
         assert retrieved_share['id'] == received_share['id']
-
-
-
-
 
     @PurviewSharingPowerShellPreparer()
     @recorded_by_proxy
@@ -309,3 +303,106 @@ class TestReceivedShares(TestPurviewSharing):
         except HttpResponseError as e:
             print("Exception " + str(e))
             print("Response " + update_response.text())
+
+    ###     ###
+    ### NEW ###
+    ###     ###
+    @PurviewSharingPowerShellPreparer()
+    @recorded_by_proxy
+    def test_get_all_attached_shares(self, purviewsharing_endpoint):
+        client = self.create_client(endpoint=purviewsharing_endpoint)
+        sent_share_id = uuid4()
+        sent_share_invitation_id = uuid4()
+        sent_share = self.prepare_sent_share()
+
+        # cspell:disable-next-line
+        consumer_storage_account_resource_id = "/subscriptions/0f3dcfc3-18f8-4099-b381-8353e19d43a7/resourceGroups/faisalaltell/providers/Microsoft.Storage/storageAccounts/bbfaisalr1"
+
+        request = build_sent_shares_create_or_replace_request(
+            sent_share_id,
+            content_type="application/json",
+            content=json.dumps(sent_share))
+        
+        response = client.send_request(request)
+
+        assert response is not None
+        assert response.status_code == 201, "Invalid Status Code " + str(response.status_code)
+
+        consumerEmail = "consumer@contoso.com"
+
+        invitation = {
+            "invitationKind": "User",
+            "properties": {
+                "targetEmail": consumerEmail,
+                "notify": "true",
+                "expirationDate": "2024-01-01 00:00:00"
+            }
+        }
+
+        invitation_request = build_sent_shares_create_invitation_request(
+            sent_share_id=sent_share_id,
+            sent_share_invitation_id=sent_share_invitation_id,
+            content_type="application/json",
+            content=json.dumps(invitation))
+        
+        invitation_response = client.send_request(invitation_request)
+
+        assert invitation_response is not None
+        assert invitation_response.status_code == 201, "Invalid Status Code " + str(invitation_response.status_code)
+        assert invitation_response.content is not None
+
+        created_invitation = json.loads(invitation_response.content)
+
+        assert created_invitation['id'] == str(sent_share_invitation_id)
+        assert created_invitation['properties']['targetEmail'] == consumerEmail
+
+        list_detached_request = build_received_shares_list_detached_request(orderby="properties/createdAt desc")
+        list_detached_response = client.send_request(list_detached_request)
+
+        assert list_detached_response is not None
+
+        list_detached = json.loads(list_detached_response.content)
+        received_share = list_detached['value'][0]
+
+        store_reference = {
+            "referenceName": consumer_storage_account_resource_id,
+            "type": "ArmResourceReference"
+        }
+
+        # cspell:disable
+        sink = {
+            "properties": {
+                "containerName": "containerellxvxonnlukvfzbwhlexfzqfs",
+                "folder": "folderpubhxhmiibnxcvqchnbi",
+                "mountPath": "mountPathciynxouybsqvfgmcfwtt",
+            },
+            "storeKind": "AdlsGen2Account",
+            "storeReference": store_reference
+        }
+        # cspell:enable
+
+        received_share['properties']['sink'] = sink
+
+        update_request = build_received_shares_create_or_replace_request(
+            received_share['id'],
+            content_type="application/json",
+            content=json.dumps(received_share))
+        
+        update_response = client.send_request(update_request)
+
+        assert update_response is not None
+        assert update_response.status_code == 201, "Invalid Status Code " + str(update_response.status_code)
+
+        list_attached_request = build_received_shares_list_attached_request(
+            reference_name=consumer_storage_account_resource_id,
+            orderby="properties/createdAt desc")
+        list_attached_response = client.send_request(list_attached_request)
+
+        assert list_attached_response is not None
+
+        list_attached = json.loads(list_attached_response.content)
+
+        assert list_attached is not None
+        assert len(list_attached['value']) > 0
+        # is the number of items that has shareStatus "Attached" equal to the number of all results
+        assert len([x for x in list_attached['value'] if x['properties']['shareStatus'] == "Attached"]) == len(list_attached['value'])
