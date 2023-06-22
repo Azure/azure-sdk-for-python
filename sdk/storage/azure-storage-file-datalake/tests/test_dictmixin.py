7b8d21ec2375086cccee3f526574438af1c13e63
Pamela Fox <pamela.fox@gmail.com>
2023-06-22 12:11:36 +0000

    Adding contains and tests

---
+++ sdk/storage/azure-storage-file-datalake/tests/test_dictmixin.py
@@ -0,0 +1,19 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+
+from azure.storage.filedatalake import _models
+
+
+class TestDictMixin:
+
+    def test_contains(self):
+        model = _models.DictMixin()
+        key = "name"
+        value = "steve"
+        model.__setitem__(key, value)
+        assert model.__contains__(key)
+        assert key in model
