f82ad8554a1857bb49f351aa6f17ff12b7c3da2a
Kashif Khan <kashifkhan@microsoft.com>
2023-06-26 13:54:34 -0500

    fix tests for py3.7

--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/asynctests/test_connection_async.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/asynctests/test_connection_async.py
@@ -1,12 +1,18 @@
+import sys
+import asyncio
+from unittest.mock import AsyncMock, Mock
 import pytest
-from unittest.mock import AsyncMock
 from azure.eventhub._pyamqp.aio import Connection
 
 @pytest.mark.asyncio
 async def test_connection_end_session_on_timeout():
     connection = Connection("fake.host.com")
-    connection._process_outgoing_frame = AsyncMock(return_value=None)
-    connection._network_trace_params = {'amqpSession':'test_name'}
+    if sys.version_info >= (3, 8):
+        connection._process_outgoing_frame = AsyncMock(return_value=None)
+    else:
+        future = asyncio.Future()
+        future.set_result(None)
+        connection._process_outgoing_frame = Mock(return_value=future)
     # create session on the Connection
     session = connection.create_session(network_trace=False)
     outgoing_channel = session.channel
@@ -14,7 +20,7 @@ async def test_connection_end_session_on_timeout():
     incoming_channel = 0
     connection._incoming_endpoints[incoming_channel] = session
     assert outgoing_channel == connection._incoming_endpoints[incoming_channel].channel
-    # typically after some inactivity(60,000 ms), the server will force a connection close
+    # typically after some inactivity(60,000 ms) on the connection, the server will force a connection close
     # and will send across an END frame
     await connection._incoming_end(incoming_channel, None)
     # end points dont have the channels anymore and Session link is gone.
--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_connection.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_connection.py
@@ -5,7 +5,6 @@ from azure.eventhub._pyamqp import Connection
 def test_connection_end_session_on_timeout():
     connection = Connection("fake.host.com")
     connection._process_outgoing_frame = Mock(return_value=None)
-    connection._network_trace_params = {'amqpSession':'test_name'}
     # create session on the Connection
     session = connection.create_session(network_trace=False)
     outgoing_channel = session.channel
