1f6e6e0bcc6b9ba22388b863be50f318a3e35814
Runhan Li <runhli@microsoft.com>
2023-06-28 11:06:17 -0700

    initial commit

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_feature_set/source_metadata_schema.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_feature_set/source_metadata_schema.py
@@ -8,17 +8,20 @@ from typing import Dict
 
 from marshmallow import fields, post_load
 
+from azure.ai.ml._schema.core.fields import UnionField
 from azure.ai.ml._schema.core.schema import PatchedSchemaMeta
 
 from .delay_metadata_schema import DelayMetadataSchema
+from .source_process_code_metadata_schema import SourceProcessCodeSchema
 from .timestamp_column_metadata_schema import TimestampColumnMetadataSchema
 
 
 class SourceMetadataSchema(metaclass=PatchedSchemaMeta):
     type = fields.Str(required=True)
-    path = fields.Str(required=True)
+    path = UnionField([fields.Str(), fields.Dict()], required=True)
     timestamp_column = fields.Nested(TimestampColumnMetadataSchema, required=True)
     source_delay = fields.Nested(DelayMetadataSchema, required=False)
+    source_process_code = fields.Nested(SourceProcessCodeSchema, required=False)
 
     @post_load
     def make(self, data: Dict, **kwargs):
