208d6026148c61806d702acb14eb4634f521251d
Runhan Li <runhli@microsoft.com>
2023-06-28 11:27:42 -0700

    add latest version

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_feature_set/feature_set_schema.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_feature_set/feature_set_schema.py
@@ -16,6 +16,7 @@ from .materialization_settings_schema import MaterializationSettingsSchema
 class FeatureSetSchema(YamlFileSchema):
     name = fields.Str(required=True, allow_none=False)
     version = fields.Str(required=True, allow_none=False)
+    latest_version = fields.Str(dump_only=True)
     specification = NestedField(FeatureSetSpecificationSchema, required=True, allow_none=False)
     entities = fields.List(fields.Str, required=True, allow_none=False)
     stage = fields.Str(validate=validate.OneOf(["Development", "Production", "Archived"]), dump_default="Development")
