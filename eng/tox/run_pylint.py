c2cde76dd471af948cc87b6907365845a2f89bc3
l0lawrence <llawrence@microsoft.com>
2023-06-29 13:29:45 -0700

    pin python version

--- eng/tox/run_pylint.py
+++ eng/tox/run_pylint.py
@@ -63,6 +63,8 @@ if __name__ == "__main__":
                 sys.executable,
                 "-m",
                 "pylint",
+                "--python-version",
+                "3.7",
                 "--rcfile={}".format(rcFileLocation),
                 "--output-format=parseable",
                 os.path.join(args.target_package, top_level_module),
