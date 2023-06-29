eac280ef14e69744d8018915e85c37c99c1e0186
l0lawrence <llawrence@microsoft.com>
2023-06-29 08:10:42 -0700

    update doc type

--- sdk/eventhub/azure-eventhub-checkpointstoreblob-aio/azure/eventhub/extensions/checkpointstoreblobaio/_blobstoragecsaio.py
+++ sdk/eventhub/azure-eventhub-checkpointstoreblob-aio/azure/eventhub/extensions/checkpointstoreblobaio/_blobstoragecsaio.py
@@ -171,7 +171,7 @@ class BlobCheckpointStore(CheckpointStore):
         :param str eventhub_name: The name of the specific Event Hub the partition ownerships are associated with,
          relative to the Event Hubs namespace that contains it.
         :param str consumer_group: The name of the consumer group the ownerships are associated with.
-        :rtype: Iterable[Dict[str, Any]]
+        :rtype: iterable[dict[str, any]]
         :return: Iterable of dictionaries containing partition ownership information:
 
                 - `fully_qualified_namespace` (str): The fully qualified namespace that the Event Hub belongs to.
@@ -224,8 +224,8 @@ class BlobCheckpointStore(CheckpointStore):
     ) -> Iterable[Dict[str, Any]]:
         """Tries to claim ownership for a list of specified partitions.
 
-        :param Iterable[Dict[str,Any]] ownership_list: Iterable of dictionaries containing all the ownerships to claim.
-        :rtype: Iterable[Dict[str,Any]]
+        :param iterable[dict[str,any]] ownership_list: Iterable of dictionaries containing all the ownerships to claim.
+        :rtype: iterable[dict[str,any]]
         :return: Iterable of dictionaries containing partition ownership information:
 
                 - `fully_qualified_namespace` (str): The fully qualified namespace that the Event Hub belongs to.
@@ -254,7 +254,7 @@ class BlobCheckpointStore(CheckpointStore):
         Note: If you plan to implement a custom checkpoint store with the intention of running between
         cross-language EventHubs SDKs, it is recommended to persist the offset value as an integer.
 
-        :param Dict[str,Any] checkpoint: A dict containing checkpoint information:
+        :param dict[str,any] checkpoint: A dict containing checkpoint information:
 
                 - `fully_qualified_namespace` (str): The fully qualified namespace that the Event Hub belongs to.
                   The format is like "<namespace>.servicebus.windows.net".
@@ -299,7 +299,7 @@ class BlobCheckpointStore(CheckpointStore):
         :param str eventhub_name: The name of the specific Event Hub the checkpoints are associated with, relative to
          the Event Hubs namespace that contains it.
         :param str consumer_group: The name of the consumer group the checkpoints are associated with.
-        :rtype: Iterable[Dict[str,Any]]
+        :rtype: iterable[dict[str,any]]
         :return: Iterable of dictionaries containing partition checkpoint information:
 
                 - `fully_qualified_namespace` (str): The fully qualified namespace that the Event Hub belongs to.
