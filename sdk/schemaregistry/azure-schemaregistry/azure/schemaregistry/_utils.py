3cbfeb83373eee79961215257bb228f7fda04d69
swathipil <swathip@microsoft.com>
2023-06-30 12:14:28 -0700

    pylint next for avroencoder

--- sdk/schemaregistry/azure-schemaregistry/azure/schemaregistry/_utils.py
+++ sdk/schemaregistry/azure-schemaregistry/azure/schemaregistry/_utils.py
@@ -89,11 +89,11 @@ def build_get_schema_request(args: Iterable, kwargs: Any) -> HttpRequest:
             group_name = kwargs.pop("group_name")
             name = kwargs.pop("name")
             version = kwargs.pop("version")
-        except KeyError as exc:
-            raise TypeError(
+        except KeyError:
+            raise TypeError(    # pylint:disable=raise-missing-from
                 """Missing required argument(s). Specify either `schema_id` """
                 """or `group_name`, `name`, `version."""
-            ) from exc
+            )
         return schema_rest.build_get_schema_version_request(
             group_name=group_name,
             schema_name=name,
