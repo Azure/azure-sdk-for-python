4d45ca7c7b6482e900583d096f351b1233792e5b
Matt Metcalf <mrm9084@gmail.com>
2023-07-05 16:05:36 -0700

    Updating sample

--- sdk/appconfiguration/feature-management/samples/test.py
+++ sdk/appconfiguration/feature-management/samples/test.py
@@ -1,36 +1,13 @@
 from feature.management import FeatureManager, RandomFilter, TimeWindowFilter, TargetingFilter
 import json
+import os
+import sys
 
-def build_feature_flag(name, enabled, client_filters):
-	return "{{\"id\": \"{}\",\"description\": \"\",\"enabled\": \"{}\",\"conditions\": {{\"client_filters\": {}}}}}".format(name, enabled, client_filters)
+script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-def build_filter(name, parameters):
-	return "{{\"name\": \"{}\",\"parameters\": {}}}".format(name, parameters)
+f = open(script_directory + "/feature-flags.json", "r")
 
-alpha_string = build_feature_flag("Alpha", "true", "[]")
-beta_string = build_feature_flag("Beta", "false", "[]")
-
-gamma_string = build_feature_flag("Gamma", "true", "[" + build_filter("Microsoft.Random", "{\"Value\": 50}") + "]")
-delta_string = build_feature_flag("Delta", "true", "[{\"name\": \"Microsoft.TimeWindowFilter\",\"parameters\": {\"Start\": \"Thu, 29 Jun 2023 07:00:00 GMT\",\"End\": \"Sat, 30 Jun 2023 07:00:00 GMT\"}}]")
-sigma_string = build_feature_flag("Sigma", "true", "[{\"name\": \"Microsoft.TimeWindowFilter\",\"parameters\": {\"Start\": \"Tue, 27 Jun 2023 06:00:00 GMT\",\"End\": \"Wen, 28 Jun 2023 06:05:00 GMT\"}}]")
-epsilon_string = build_feature_flag("Epsilon", "true", "[{\"name\": \"Microsoft.TimeWindowFilter\",\"parameters\": {\"Start\": \"Tue, 27 Jun 2023 06:00:00 GMT\"}}]")
-zeta_string = build_feature_flag("Zeta", "true", "[{\"name\": \"Microsoft.TimeWindowFilter\",\"parameters\": {\"End\": \"Tue, 28 Jun 2024 06:00:00 GMT\"}}]")
-target_string = build_feature_flag("Target", "true", "[{\"name\": \"Microsoft.Targeting\",\"parameters\": {\"Audience\": {\"Users\": [\"Adam\"],\"Groups\": [{\"Name\": \"Stage1\",\"RolloutPercentage\": 100}],\"DefaultRolloutPercentage\": 50,\"Exclusion\": {\"Users\": [],\"Groups\": []}}}}]")
-
-feature_flags = {}
-
-def build_feature_flags(name, string):
-	json_object = json.loads(string)
-	feature_flags[name] = json_object
-
-build_feature_flags("Alpha", alpha_string)
-build_feature_flags("Beta", beta_string)
-build_feature_flags("Gamma", gamma_string)
-build_feature_flags("Delta", delta_string)
-build_feature_flags("Sigma", sigma_string)
-build_feature_flags("Epsilon", epsilon_string)
-build_feature_flags("Zeta", zeta_string)
-build_feature_flags("Target", target_string)
+feature_flags = json.load(f)
 
 feature_filters = {}
 feature_filters["Microsoft.Random"] = RandomFilter()
@@ -38,15 +15,21 @@ feature_filters["Microsoft.TimeWindowFilter"] = TimeWindowFilter()
 feature_filters["Microsoft.Targeting"] = TargetingFilter()
 
 
-
 feature_manager = FeatureManager(feature_flags, filters=feature_filters)
 
+# Is always true
 print("Alpha is ", feature_manager.is_enabled("Alpha"))
+# Is always false
 print("Beta is ", feature_manager.is_enabled("Beta"))
+# Is false 50% of the time
 print("Gamma is ", feature_manager.is_enabled("Gamma"))
+# Is true between 06-29-2023 to 07-17-2023
 print("Delta is ", feature_manager.is_enabled("Delta"))
+# Is true After 06-27-2023
 print("Sigma is ", feature_manager.is_enabled("Sigma"))
+# Is true Before 06-28-2023
 print("Epsilon is ", feature_manager.is_enabled("Epsilon"))
 print("Zeta is ", feature_manager.is_enabled("Zeta"))
+# Target is true for Adam, group Stage 1, and 50% of users
 print("Target is ", feature_manager.is_enabled("Target", user="Adam"))
 print("Target is ", feature_manager.is_enabled("Target", user="Brian"))
