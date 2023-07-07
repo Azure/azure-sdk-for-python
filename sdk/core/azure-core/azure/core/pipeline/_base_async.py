5bf502888cfcad263fb79808374ada8ce4f9dbc0
xiangyan99 <xiangsjtu@gmail.com>
2023-07-07 11:30:41 -0700

    update

--- sdk/core/azure-core/azure/core/pipeline/_base_async.py
+++ sdk/core/azure-core/azure/core/pipeline/_base_async.py
@@ -29,7 +29,11 @@ from contextlib import AbstractAsyncContextManager
 from azure.core.pipeline import PipelineRequest, PipelineResponse, PipelineContext
 from azure.core.pipeline.policies import AsyncHTTPPolicy, SansIOHTTPPolicy
 from ._tools_async import await_result as _await_result
-from .transport import AsyncHttpTransport, AsyncHttpResponse as LegacyAsyncHttpResponse, HttpRequest as LegacyHttpRequest
+from .transport import (
+    AsyncHttpTransport,
+    AsyncHttpResponse as LegacyAsyncHttpResponse,
+    HttpRequest as LegacyHttpRequest,
+)
 from azure.core.rest import AsyncHttpResponse, HttpRequest
 
 AsyncHTTPResponseType = TypeVar("AsyncHTTPResponseType", LegacyAsyncHttpResponse, AsyncHttpResponse)
