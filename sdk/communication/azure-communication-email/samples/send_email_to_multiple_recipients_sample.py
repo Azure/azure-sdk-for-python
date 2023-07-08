06168ed70621dec0a9ff5e2694af7fc901e41e5c
Vigneshwaran <svigneshwaran1073@gmail.com>
2023-07-08 22:47:28 +0530

    Update send_email_to_multiple_recipients_sample.py

--- sdk/communication/azure-communication-email/samples/send_email_to_multiple_recipients_sample.py
+++ sdk/communication/azure-communication-email/samples/send_email_to_multiple_recipients_sample.py
@@ -43,7 +43,7 @@ class EmailMultipleRecipientSample(object):
             "content": {
                 "subject": "This is the subject",
                 "plainText": "This is the body",
-                "html": "html><h1>This is the body</h1></html>"
+                "html": "<html><h1>This is the body</h1></html>"
             },
             "recipients": {
                 "to": [
