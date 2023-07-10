fbc12758b2ad316f49dad32412cfe51bc7823977
iscai-msft <iscai@microsoft.com>
2023-07-10 16:20:03 -0700

    fix queue name

--- sdk/storage/azure-storage-queue/azure/storage/queue/_generated/aio/_azure_queue_storage.py
+++ sdk/storage/azure-storage-queue/azure/storage/queue/_generated/aio/_azure_queue_storage.py
@@ -12,7 +12,7 @@ from typing import Any, Awaitable
 from azure.core import AsyncPipelineClient
 from azure.core.rest import AsyncHttpResponse, HttpRequest
 
-from .. import models
+from .. import models as _models
 from .._serialization import Deserializer, Serializer
 from ._configuration import AzureQueueStorageConfiguration
 from .operations import MessageIdOperations, MessagesOperations, QueueOperations, ServiceOperations
@@ -43,9 +43,9 @@ class AzureQueueStorage:  # pylint: disable=client-accepts-api-version-keyword
         self, url: str, base_url: str = "", **kwargs: Any
     ) -> None:
         self._config = AzureQueueStorageConfiguration(url=url, **kwargs)
-        self._client = AsyncPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client: AsyncPipelineClient = AsyncPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
-        client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
+        client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
@@ -83,5 +83,5 @@ class AzureQueueStorage:  # pylint: disable=client-accepts-api-version-keyword
         await self._client.__aenter__()
         return self
 
-    async def __aexit__(self, *exc_details) -> None:
+    async def __aexit__(self, *exc_details: Any) -> None:
         await self._client.__aexit__(*exc_details)
