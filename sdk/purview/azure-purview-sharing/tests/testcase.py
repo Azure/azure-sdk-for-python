eb603df7f6dd02ab5abd5c6c759bebc19bf49c95
Hope Welch <hopewelch@microsoft.com>
2023-07-12 16:54:28 -0700

    one more auth test for git

--- sdk/purview/azure-purview-sharing/tests/testcase.py
+++ sdk/purview/azure-purview-sharing/tests/testcase.py
@@ -3,7 +3,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
-# -------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 import functools
 from devtools_testutils import AzureRecordedTestCase, PowerShellPreparer
 from azure.purview.sharing import PurviewSharingClient
