1ece9514c521d78ab5f823c539b63a11e217bc42
azure-sdk <azuresdk@microsoft.com>
2023-07-12 03:55:27 +0000

    Increment package version after release of azure-maps-render

--- sdk/maps/azure-maps-render/azure/maps/render/_version.py
+++ sdk/maps/azure-maps-render/azure/maps/render/_version.py
@@ -6,5 +6,5 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "1.0.0b2"
+VERSION = "1.0.0b3"
 API_VERSION = "2022-08-01"
