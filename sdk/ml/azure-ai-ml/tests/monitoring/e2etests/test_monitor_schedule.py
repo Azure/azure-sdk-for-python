7960c9167da2226e61048a97467070c660d0d82f
nemanjarajic <nrajic30@gmail.com>
2023-07-18 14:42:33 -0700

    test cases fixed

--- sdk/ml/azure-ai-ml/tests/monitoring/e2etests/test_monitor_schedule.py
+++ sdk/ml/azure-ai-ml/tests/monitoring/e2etests/test_monitor_schedule.py
@@ -204,27 +204,13 @@ class TestMonitorSchedule(AzureRecordedTestCase):
 
 def get_model_inputs_outputs_from_deployment(client: MLClient, endpoint_name: str, deployment_name: str):
     online_deployment = client.online_deployments.get(deployment_name, endpoint_name)
-    deployment_data_collector = online_deployment.data_collector
-
-    if deployment_data_collector:
-        assert deployment_data_collector.collections.get("model_inputs") != None
-        assert deployment_data_collector.collections.get("model_outputs") != None
-
-        in_reg = AMLVersionedArmId(deployment_data_collector.collections.get("model_inputs").data)
-        out_reg = AMLVersionedArmId(deployment_data_collector.collections.get("model_outputs").data)
-        model_inputs_name = in_reg.asset_name
-        model_inputs_version = in_reg.asset_version
-        model_outputs_name = out_reg.asset_name
-        model_outputs_version = out_reg.asset_version
-        model_inputs_type = client.data.get(model_inputs_name, model_inputs_version).type
-        model_outputs_type = client.data.get(model_outputs_name, model_outputs_version).type
-    else:
-        model_inputs_name = online_deployment.tags.get(DEPLOYMENT_MODEL_INPUTS_NAME_KEY)
-        model_inputs_version = online_deployment.tags.get(DEPLOYMENT_MODEL_INPUTS_VERSION_KEY)
-        model_outputs_name = online_deployment.tags.get(DEPLOYMENT_MODEL_OUTPUTS_NAME_KEY)
-        model_outputs_version = online_deployment.tags.get(DEPLOYMENT_MODEL_OUTPUTS_VERSION_KEY)
-        model_inputs_type = client.data.get(model_inputs_name, model_inputs_version).type
-        model_outputs_type = client.data.get(model_outputs_name, model_outputs_version).type
+ 
+    model_inputs_name = online_deployment.tags.get(DEPLOYMENT_MODEL_INPUTS_NAME_KEY)
+    model_inputs_version = online_deployment.tags.get(DEPLOYMENT_MODEL_INPUTS_VERSION_KEY)
+    model_outputs_name = online_deployment.tags.get(DEPLOYMENT_MODEL_OUTPUTS_NAME_KEY)
+    model_outputs_version = online_deployment.tags.get(DEPLOYMENT_MODEL_OUTPUTS_VERSION_KEY)
+    model_inputs_type = client.data.get(model_inputs_name, model_inputs_version).type
+    model_outputs_type = client.data.get(model_outputs_name, model_outputs_version).type
 
     return (
         model_inputs_name,
