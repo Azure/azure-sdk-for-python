8cda1e0bf33e7239c190369589a6726747fd8165
Leighton Chen <lechen@microsoft.com>
2023-07-24 15:41:19 -0700

    docs

--- sdk/iotfirmwaredefense/azure-mgmt-iotfirmwaredefense/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Release History
-
-## 1.0.0b1 (2023-07-24)
-
-* Initial Release
--- sdk/loadtesting/azure-developer-loadtesting/azure/developer/loadtesting/aio/__init__.py
+++
@@ -1,9 +0,0 @@
-
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for
-# license information.
-# --------------------------------------------------------------------------
-from ._client import LoadTestRunClient, LoadTestAdministrationClient
-
-__all__ = ["LoadTestAdministrationClient", "LoadTestRunClient"]
