dd43b13c44cd88f9b2f3c8eebb29947dc447d6c6
ZhidaLiu <zhili@microsoft.com>
2023-07-25 14:22:48 -0700

    Initial WorkspaceHub change for ML SDK (#29842)
    * template and base operation
    
    contract temp change
    
    Update schema and entities
    
    Update template1
    
    * update operation
    
    * finished most operations
    
    * template fixs
    
    * Update Hub name to wprkspaceHub
    
    update
    
    update restclient
    
    add kind support for 2023-02-01
    
    update 2023-02-01-preview swagger and regenerate rest
    
    update on parameter name, still need move change to febpreview and regenerate client
    
    * remove lean workspace type and schema
    
    remove lean operation
    
    update template
    
    small fix
    
    update name and remove lean type
    
    * revert workspace operation
    
    Revert "revert workspace operation"
    
    This reverts commit 10a74ab365de8356831f250130e2c5179ffd045e.
    
    Revert "update name and remove lean type"
    
    This reverts commit 598a1fd2f71416ede63a4c4b3e190cab34acf589.
    
    Revert "Revert "update name and remove lean type""
    
    This reverts commit f09898cdd542a0ac47940b10184111c83b090c2c.
    
    Revert "Revert "revert workspace operation""
    
    This reverts commit e654d146886b590b76468a83c39123b8a268c2a0.
    
    revert change related to 02-01-preview
    
    update to user 04-01-preview client
    
    user 04-01-preview client for hub operation
    
    fix lint error
    
    lint error fix
    
     diagnose related package to 04-01-preview
    
    * reformat code
    
    update test client
    
    small fix
    
    fix ut
    
    format fix
    
    * prevent CMK update for lean workspace
    
    * resolve rebase issue
    
    Resolve merge conflict issue
    
    * Update Hub related test code
    
    * Update E2E test and configs
    
    * Update model version for workspace
    
    * rename workspacehub path
    
    fix
    
    * Update names
    
    Update format
    
    * Remove WorkspaceHub properties from workspace
    
    * remove workspaceHub properties from workspace
    
    remove workspacehub properties from workspace
    
    * Fix import issue and tests
    
    * Fix tests
    
    * move dependent delete to workspacehub operation
    
    * reformat code and fix feature store tests
    
    fix
    
    * Update YAMLRefDocLinks for workspaceHub
    
    * format code
    
    * rename workspace_hub and update creation flow
    
    * fix format
    
    fix
    
    * associated_workspace and add support operation.
    
    * Update template for project workspace
    
    * reformat code
    
    * fix format and code
    
    * use different template for project workspace
    
    * fix template bug
    
    * Stop create LA workspace for workspacehub
    
    * Update using new schema
    
    * Update tests
    
    Update tests
    
    fix format
    
    update format
    
    Update more entity to 06-01 preview
    
    fix tests
    
    Fixed some tests
    
    bug fixing
    
    * Update docstring
    
    * reflect comments
    
    * Add missing changes

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_workspace_hub/workspace_hub.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_workspace_hub/workspace_hub.py
@@ -2,43 +2,41 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from marshmallow import fields, EXCLUDE
+from marshmallow import fields
 
-from azure.ai.ml._schema._utils.utils import validate_arm_str
 from azure.ai.ml._schema.core.fields import NestedField, StringTransformedEnum
+from azure.ai.ml._schema._utils.utils import validate_arm_str
 from azure.ai.ml._schema.core.schema import PathAwareSchema
 from azure.ai.ml._schema.workspace.customer_managed_key import CustomerManagedKeySchema
 from azure.ai.ml._schema.workspace.identity import IdentitySchema
 from azure.ai.ml._utils.utils import snake_to_pascal
 from azure.ai.ml.constants._common import PublicNetworkAccess
 from azure.ai.ml._schema.workspace.networking import ManagedNetworkSchema
+from azure.ai.ml._schema._workspace_hub.workspace_hub_config import WorkspaceHubConfigSchema
 
 from azure.ai.ml._schema import ExperimentalField
+from azure.ai.ml._utils._experimental import experimental
 
 
-class WorkspaceSchema(PathAwareSchema):
+@experimental
+class WorkspaceHubSchema(PathAwareSchema):
     name = fields.Str(required=True)
     location = fields.Str()
     id = fields.Str(dump_only=True)
     resource_group = fields.Str()
     description = fields.Str()
-    discovery_url = fields.Str()
     display_name = fields.Str()
-    hbi_workspace = fields.Bool()
+    customer_managed_key = NestedField(CustomerManagedKeySchema)
     storage_account = fields.Str(validate=validate_arm_str)
     container_registry = fields.Str(validate=validate_arm_str)
     key_vault = fields.Str(validate=validate_arm_str)
-    application_insights = fields.Str(validate=validate_arm_str)
-    customer_managed_key = NestedField(CustomerManagedKeySchema)
     tags = fields.Dict(keys=fields.Str(), values=fields.Str())
-    mlflow_tracking_uri = fields.Str(dump_only=True)
-    image_build_compute = fields.Str()
     public_network_access = StringTransformedEnum(
         allowed_values=[PublicNetworkAccess.DISABLED, PublicNetworkAccess.ENABLED],
         casing_transform=snake_to_pascal,
     )
     identity = NestedField(IdentitySchema)
     primary_user_assigned_identity = fields.Str()
-    workspace_hub = fields.Str(validate=validate_arm_str)
-    managed_network = ExperimentalField(NestedField(ManagedNetworkSchema, unknown=EXCLUDE))
-    enable_data_isolation = fields.Bool()
+    managed_network = ExperimentalField(NestedField(ManagedNetworkSchema))
+    existing_workspaces = fields.List(fields.Str())
+    workspace_hub_config = ExperimentalField(NestedField(WorkspaceHubConfigSchema))
