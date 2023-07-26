819831dfa58ddbcbb44911e0142420d961adf48e
YalinLi0312 <yall@microsoft.com>
2023-07-26 15:34:58 -0700

    Fix merge conflict

--- sdk/tables/azure-data-tables/azure/data/tables/aio/_table_client_async.py
+++ sdk/tables/azure-data-tables/azure/data/tables/aio/_table_client_async.py
@@ -40,7 +40,6 @@ from .._error import (
     _validate_key_values,
 )
 from .._deserialize import deserialize_iso, _return_headers_and_deserialized, _convert_to_entity, _trim_service_metadata
-from .._error import _decode_error, _process_table_error, _reprocess_error, _reraise_error, _validate_tablename_error
 from .._table_client import EntityType, TransactionOperationType
 from ._base_client_async import AsyncTablesBaseClient
 from ._models import TableEntityPropertiesPaged
