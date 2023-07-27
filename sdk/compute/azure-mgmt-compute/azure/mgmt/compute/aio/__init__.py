92aa881f90b084992a5facfd9e5c82220c5130f1
msyyc <70930885+msyyc@users.noreply.github.com>
2023-07-27 17:11:26 +0800

    combiner

--- sdk/compute/azure-mgmt-compute/azure/mgmt/compute/aio/__init__.py
+++ sdk/compute/azure-mgmt-compute/azure/mgmt/compute/aio/__init__.py
@@ -6,5 +6,18 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._compute_management_client import ComputeManagementClient
-__all__ = ['ComputeManagementClient']
+from ._client import ComputeManagementClient
+
+try:
+    from ._patch import __all__ as _patch_all
+    from ._patch import *  # pylint: disable=unused-wildcard-import
+except ImportError:
+    _patch_all = []
+from ._patch import patch_sdk as _patch_sdk
+
+__all__ = [
+    "ComputeManagementClient",
+]
+__all__.extend([p for p in _patch_all if p not in __all__])
+
+_patch_sdk()
