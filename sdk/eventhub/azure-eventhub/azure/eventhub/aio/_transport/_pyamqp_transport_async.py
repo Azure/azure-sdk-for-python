8c731abd930647b51217a5b4d5baea28a5ebb698
l0lawrence <llawrence@microsoft.com>
2023-07-27 08:25:03 -0700

    UPDATE2

--- sdk/eventhub/azure-eventhub/azure/eventhub/aio/_transport/_pyamqp_transport_async.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/aio/_transport/_pyamqp_transport_async.py
@@ -190,7 +190,8 @@ class PyamqpTransportAsync(PyamqpTransport, AmqpTransportAsync):
                     _LOGGER.info("Current Thread running %r.", threading.current_thread())
                     if consumer._last_received_event:
                         _LOGGER.info("Last received event: %r.", consumer._last_received_event)
-                    _LOGGER.info("Current consumer offset: %r.", consumer._offset)
+                    if consumer._offset:
+                        _LOGGER.info("Current consumer offset: %r.", consumer._offset)
                     # If optional dependency is not installed, do not retry.
                     if isinstance(exception, ImportError):
                         raise exception
