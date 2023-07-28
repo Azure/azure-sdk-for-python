7a3313642db71864f20bd2223da3e9182c7675de
msyyc <70930885+msyyc@users.noreply.github.com>
2023-07-28 10:06:25 +0800

    skip file under v20* when build .whl

--- sdk/compute/azure-mgmt-compute/setup.py
+++ sdk/compute/azure-mgmt-compute/setup.py
@@ -67,6 +67,7 @@ setup(
             # Exclude packages that will be covered by PEP420 or nspkg
             "azure",
             "azure.mgmt",
+            "azure.mgmt.compute.v20*",
         ]
     ),
     include_package_data=True,
