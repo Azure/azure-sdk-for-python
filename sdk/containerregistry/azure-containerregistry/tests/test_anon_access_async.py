d14cc745cf59e713577fcdc43592109b5ae303a6
Yalin Li <yall@microsoft.com>
2023-07-28 17:36:13 -0700

    [ACR] Enable to run black in ci (#31307)
    * Update ci.yml
    
    * Run black
    
    * Fix pylint
    
    * Update snippet
    
    * black mgmt sdk
    
    * Run black on mgmt sdk
    
    ---------
    
    Co-authored-by: msyyc <70930885+msyyc@users.noreply.github.com>

--- sdk/containerregistry/azure-containerregistry/tests/test_anon_access_async.py
+++ sdk/containerregistry/azure-containerregistry/tests/test_anon_access_async.py
@@ -205,18 +205,18 @@ class TestContainerRegistryClientAsync(AsyncContainerRegistryTestClass):
 
 @pytest.mark.asyncio
 async def test_set_api_version():
-    containerregistry_endpoint="https://fake_url.azurecr.io"
-    
+    containerregistry_endpoint = "https://fake_url.azurecr.io"
+
     async with ContainerRegistryClient(endpoint=containerregistry_endpoint, audience="https://microsoft.com") as client:
         assert client._client._config.api_version == "2021-07-01"
-    
+
     async with ContainerRegistryClient(
-        endpoint=containerregistry_endpoint, audience="https://microsoft.com", api_version = "2019-08-15-preview"
+        endpoint=containerregistry_endpoint, audience="https://microsoft.com", api_version="2019-08-15-preview"
     ) as client:
         assert client._client._config.api_version == "2019-08-15-preview"
-    
+
     with pytest.raises(ValueError):
         async with ContainerRegistryClient(
-            endpoint=containerregistry_endpoint, audience="https://microsoft.com", api_version = "2019-08-15"
+            endpoint=containerregistry_endpoint, audience="https://microsoft.com", api_version="2019-08-15"
         ) as client:
             pass
