0e68aaef695e4894b356a5d83dec9547f8f5fd74
Matt Metcalf <mrm9084@gmail.com>
2023-07-31 12:02:32 -0700

    App Config Fixing Live Tests (#31277)
    * Fixing Live Tests
    
    * Fixing mapping
    
    * Fixed formatting
    
    * Update test-resources.json
    
    * Update test-resources.json
    
    * Update test-resources.json
    
    * Using original store
    
    * Updating live tests
    
    * Fixing startup
    
    * Fixing Test Cases
    
    * Fixing Samples
    
    * Formatting
    
    * Adding KVR
    
    * Update testcase.py
    
    * Fixing Samples
    
    * Update test-resources.json
    
    * Update test-resources.json
    
    * Update testcase.py
    
    * Updating JSONs
    
    * Updating Tests
    
    * formatting
    
    * fixing sha playback issue
    
    * Update test-resources.json
    
    * Update tests.yml

--- sdk/appconfiguration/azure-appconfiguration-provider/samples/async_key_vault_reference_sample.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/samples/async_key_vault_reference_sample.py
@@ -12,7 +12,7 @@ from sample_utilities import get_authority, get_audience, get_credential
 
 
 async def main():
-    endpoint = os.environ.get("AZURE_APPCONFIG_ENDPOINT")
+    endpoint = os.environ.get("APPCONFIGURATION_ENDPOINT_STRING")
     authority = get_authority(endpoint)
     audience = get_audience(authority)
     credential = get_credential(authority, is_async=True)
@@ -30,5 +30,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
