f2307da91258812afce3a09190f104f772c980fa
SDKAuto <sdkautomation@microsoft.com>
2023-08-02 22:50:35 +0000

    CodeGen from PR 23862 in Azure/azure-rest-api-specs [Hub Generated] Review request for Microsoft.Maps to add version stable/2023-06-01 (#23862)
    * Adds base for updating Microsoft.Maps from version preview/2021-12-01-preview to version 2023-06-01
    
    * Updates readme
    
    * Updates API version in new specs and examples
    
    * updates to schema
    
    * move to v4 as managedidentity is broken in v5
    
    * fixes from build
    
    * fix example
    
    * reuse trackedresource v2
    
    * adding suppression for move
    
    * trying to suppress with from
    
    * attempt 3 for supression based on cogsearch

--- sdk/maps/azure-mgmt-maps/generated_samples/get_maps_creator.py
+++ sdk/maps/azure-mgmt-maps/generated_samples/get_maps_creator.py
@@ -14,7 +14,7 @@ from azure.mgmt.maps import AzureMapsManagementClient
     pip install azure-identity
     pip install azure-mgmt-maps
 # USAGE
-    python get_operations_by_subscription.py
+    python get_maps_creator.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,11 +29,14 @@ def main():
         subscription_id="21a9967a-e8a9-4656-a70b-96ff1c4d05a0",
     )
 
-    response = client.maps.list_subscription_operations()
-    for item in response:
-        print(item)
+    response = client.creators.get(
+        resource_group_name="myResourceGroup",
+        account_name="myMapsAccount",
+        creator_name="myCreator",
+    )
+    print(response)
 
 
-# x-ms-original-file: specification/maps/resource-manager/Microsoft.Maps/preview/2021-12-01-preview/examples/GetOperationsSubscription.json
+# x-ms-original-file: specification/maps/resource-manager/Microsoft.Maps/stable/2023-06-01/examples/GetMapsCreator.json
 if __name__ == "__main__":
     main()
