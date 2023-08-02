5542633a92048e3f2a7d2be350007202e5fffef6
l0lawrence <llawrence@microsoft.com>
2023-08-02 11:39:16 -0700

    move doc return

--- sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/_connection.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/_connection.py
@@ -49,8 +49,8 @@ def get_local_timeout(now, idle_timeout, last_frame_received_time):
     :param float now: The current time to check against.
     :param float idle_timeout: The idle timeout value.
     :param float last_frame_received_time: The time the last frame was received.
-    :rtype: bool
     :returns: Whether to shutdown the connection due to timeout.
+    :rtype: bool
     """
     if idle_timeout and last_frame_received_time:
         time_since_last_received = now - last_frame_received_time
