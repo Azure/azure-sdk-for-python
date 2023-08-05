cdc1c5c612df0ec89a27f26b941fff4c8d48c698
Catalina Peralta <catalinaperaltah@hotmail.com>
2023-08-05 14:39:27 -0700

    add repr tests

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_repr.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_repr.py
@@ -439,6 +439,71 @@ def doc_type_info():
     assert repr(model) == model_repr
     return model, model_repr
 
+
+@pytest.fixture
+def blob_file_list_source():
+    model = _models.BlobFileListSource(
+            container_url="https://test.blob.core.windows.net/blob-sas-url",
+            file_list="filelist.jsonl",
+    )
+    model_repr = f"BlobFileListSource(container_url={'https://test.blob.core.windows.net/blob-sas-url'}, file_list={'filelist.jsonl'})"
+
+    assert repr(model) == model_repr
+    return model, model_repr
+
+
+@pytest.fixture
+def blob_source():
+    model = _models.BlobSource(
+            container_url="https://test.blob.core.windows.net/blob-sas-url",
+            prefix="prefix",
+    )
+    model_repr = f"BlobSource(container_url={'https://test.blob.core.windows.net/blob-sas-url'}, prefix={'prefix'})"
+
+    assert repr(model) == model_repr
+    return model, model_repr
+
+
+@pytest.fixture
+def classifier_document_type_details(blob_source):
+    model = _models.ClassifierDocumentTypeDetails(
+            source=blob_source[0],
+    )
+    model_repr = f"ClassifierDocumentTypeDetails(source={blob_source[1]})"
+
+    assert repr(model) == model_repr
+    return model, model_repr
+
+
+@pytest.fixture
+def classifier_document_type_details_file_list(blob_file_list_source):
+    model = _models.ClassifierDocumentTypeDetails(
+            source=blob_file_list_source[0],
+    )
+    model_repr = f"ClassifierDocumentTypeDetails(source={blob_file_list_source[1]})"
+
+    assert repr(model) == model_repr
+    return model, model_repr
+
+
+@pytest.fixture
+def document_classifier_details(classifier_document_type_details, classifier_document_type_details_file_list):
+    model = _models.DocumentClassifierDetails(
+            api_version="2023-07-31",
+            description="my description",
+            created_on=datetime.datetime(2021, 9, 16, 10, 10, 59, 342380),
+            expires_on=datetime.datetime(2024, 9, 16, 10, 10, 59, 342380),
+            classifier_id="custom-classifier",
+            doc_types={
+                "form-A": classifier_document_type_details[0],
+                "form-B": classifier_document_type_details_file_list[0],
+            }
+    )
+    model_repr = f"DocumentClassifierDetails(classifier_id={'custom-classifier'}, description={'my description'}, created_on={datetime.datetime(2021, 9, 16, 10, 10, 59, 342380)}, api_version={'2023-07-31'}, doc_types={{'form-A': {classifier_document_type_details[1]}, 'form-B': {classifier_document_type_details_file_list[1]}}}, expires_on={datetime.datetime(2024, 9, 16, 10, 10, 59, 342380)})"
+    assert repr(model) == model_repr
+    return model, model_repr
+
+
 @pytest.fixture
 def document_model(doc_type_info):
     model = _models.DocumentModelDetails(
@@ -572,6 +637,23 @@ class TestRepr():
                 analyzed_document[1],
             )
         assert repr(model) == model_repr
+
+
+    def test_document_classifier_details(self, classifier_document_type_details, classifier_document_type_details_file_list):
+        model = _models.DocumentClassifierDetails(
+                api_version="2023-07-31",
+                description="my description",
+                created_on=datetime.datetime(2021, 9, 16, 10, 10, 59, 342380),
+                expires_on=datetime.datetime(2024, 9, 16, 10, 10, 59, 342380),
+                classifier_id="custom-classifier",
+                doc_types={
+                    "form-A": classifier_document_type_details[0],
+                    "form-B": classifier_document_type_details_file_list[0],
+                }
+        )
+        model_repr = f"DocumentClassifierDetails(classifier_id={'custom-classifier'}, description={'my description'}, created_on={datetime.datetime(2021, 9, 16, 10, 10, 59, 342380)}, api_version={'2023-07-31'}, doc_types={{'form-A': {classifier_document_type_details[1]}, 'form-B': {classifier_document_type_details_file_list[1]}}}, expires_on={datetime.datetime(2024, 9, 16, 10, 10, 59, 342380)})"
+        assert repr(model) == model_repr
+
 
     def test_model_operation(self, document_analysis_error, document_model):
         model = _models.OperationDetails(
