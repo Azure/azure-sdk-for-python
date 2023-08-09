298a7d0c4b29b90b4620ce79d0f9571905ac7002
zhangxingzhi <zhangxingzhi@microsoft.com>
2023-08-09 12:11:46 +0800

    generate rest_client for content service

--- sdk/ml/azure-ai-ml/scripts/regenerate_restclient.py
+++ sdk/ml/azure-ai-ml/scripts/regenerate_restclient.py
@@ -1,5 +1,8 @@
+import contextlib
+import json
 import logging
 import os
+import re
 import subprocess
 import sys
 import time
@@ -99,6 +102,92 @@ def download_file(from_url: str, to_path: Path, with_file_name: str) -> None:
         f.write(response.read().decode("utf-8"))
 
 
+def snake_case_to_camel_case(snake_case: str) -> str:
+    return "".join(word[0].upper() + word[1:] if len(word) > 1 else word.capitalize() for word in snake_case.split("/"))
+
+
+@contextlib.contextmanager
+def insert_operation_id_in_swagger(swagger_path: str) -> None:
+    """For some unknown reason, it will fail to generate the restclient for content-service and saying that there are
+    duplicate operations although no operation id is specified. This function will insert distinct operation id for
+    those operations that do not have one.
+
+    Reference:
+    https://stackoverflow.com/questions/69279224/autorest-error-duplicate-operation-detected-this-is-most-likely-due-to-2-ope
+
+    Origin path:
+    "/content/{apiVersion}/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/
+    Microsoft.MachineLearningServices/workspaces/{workspaceName}/{originEntityName}/{originActionName}"
+
+    Sample 1:
+    originEntityName: snapshots
+    originActionName: {snapshotId}/metadataWithCredentials
+    method: get
+    operationId = Snapshot_GetMetadataWithCredentials
+
+    Sample 2:
+    originEntityName: endpoints
+    originActionName: {endpointName}/versions/{version}/listKeys
+    method: get
+    operationId = Endpoint_ListKeysByNameAndVersion
+    """
+    with open(swagger_path, "r") as f:
+        origin_swagger_content = f.read()
+        swagger_obj = json.loads(origin_swagger_content)
+
+    for request_url, method_dict in swagger_obj["paths"].items():
+        for method, operation_dict in method_dict.items():
+            if "operationId" in operation_dict:
+                continue
+
+            m = re.match(r".*/workspaces/\{workspaceName}/([^/]+)/(.*)", request_url)
+            if not m:
+                continue
+            entity_name, action_name = m.groups()
+            # simple stemming
+            if entity_name.endswith("s"):
+                entity_name = entity_name[:-1]
+
+            action_name = action_name.replace("{" + entity_name + "Id}", "")
+            if "{name}/versions/{version}" in action_name:
+                action_name = action_name.replace("{name}/versions/{version}", "")
+                action_name += "ByNameAndVersion"
+
+            with_action = False
+            for action in ["get", "delete", "list", "createOrUpdate", "update", "create"]:
+                if action_name.startswith(action):
+                    action_name = action_name.replace(action, action + "/", 1)
+                    with_action = True
+                    break
+                if action_name.endswith(action):
+                    action_name = action_name.replace(action, "/" + action, 1)
+                    with_action = True
+                    break
+
+            if not with_action:
+                if method == "put":
+                    action_prefix = "createOrUpdate"
+                elif method == "post":
+                    action_prefix = "create"
+                else:
+                    action_prefix = method
+                action_name = f"{action_prefix}/{action_name}"
+
+            operation_dict["operationId"] = (
+                f"{snake_case_to_camel_case(entity_name)}_" f"{snake_case_to_camel_case(action_name)}"
+            )
+
+            print(f"Inserting operation id {operation_dict['operationId']} for {request_url} {method}")
+
+    with open(swagger_path, "w") as f:
+        json.dump(swagger_obj, f, indent=2)
+
+    yield
+
+    with open(swagger_path, "w") as f:
+        f.write(origin_swagger_content)
+
+
 def regenerate_restclient(api_tag, verbose):
     readme_path = Path("./swagger/machinelearningservices/resource-manager/readme.md")
     restclient_path = Path("./azure/ai/ml/_restclient/")
@@ -110,25 +199,29 @@ def regenerate_restclient(api_tag, verbose):
     else:
         tag_arg = f"--tag={api_tag_arg}"
 
-    commands = [
-        "autorest",
-        "--python",
-        "--track2",
-        "--version=3.6.2",
-        "--use=@autorest/python@5.12.6",
-        f"--python-sdks-folder={restclient_path.absolute()}",
-        "--package-version=0.1.0",
-        tag_arg,
-        str(readme_path.absolute()),
-        "--modelerfour.lenient-model-deduplication",
-        '--title="Azure Machine Learning Workspaces"',
-    ]
-    print_blue(f"- Running autorest command: {' '.join(commands)}")
-    run_command(
-        commands,
-        throw_on_retcode=True,
-        **command_args,
-    )
+    with insert_operation_id_in_swagger(
+        "./swagger/machinelearningservices/resource-manager/Microsoft.MachineLearn"
+        "ingServices/stable/content-service/swagger.json"
+    ):
+        commands = [
+            "autorest",
+            "--python",
+            "--track2",
+            "--version=3.6.2",
+            "--use=@autorest/python@5.12.6",
+            f"--python-sdks-folder={restclient_path.absolute()}",
+            "--package-version=0.1.0",
+            tag_arg,
+            str(readme_path.absolute()),
+            "--modelerfour.lenient-model-deduplication",
+            '--title="Azure Machine Learning Workspaces"',
+        ]
+        print_blue(f"- Running autorest command: {' '.join(commands)}")
+        run_command(
+            commands,
+            throw_on_retcode=True,
+            **command_args,
+        )
 
 
 if __name__ == "__main__":
