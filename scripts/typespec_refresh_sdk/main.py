d6b8cb93804bc2cf5a87ad3f432f0045d6e1cee0
Patrick Hallisey <pahallis@microsoft.com>
2023-08-10 15:42:12 -0700

    Add npmrc arg to typespec_refresh_sdk/main.py

--- scripts/typespec_refresh_sdk/main.py
+++ scripts/typespec_refresh_sdk/main.py
@@ -4,16 +4,26 @@ from pathlib import Path
 from subprocess import check_call
 
 
+def call(cmd: str):
+    print(f"Calling: {cmd}")
+    return check_call(cmd, shell=True)
+
+
 def main(sdk_folder: str):
+    repoRoot = Path(os.path.dirname(os.path.abspath(__file__))) / "../.."
+    npmrcPath = repoRoot / ".npmrc"
+    npmrcPathArg = f"-NpmrcPath {npmrcPath}" if npmrcPath.exists() else ""
+
     # install package.json
-    script = Path("eng/common/scripts/TypeSpec-Project-Sync.ps1")
-    check_call(f"pwsh {script} {Path(sdk_folder)}", shell=True)
+    call(f"pwsh eng/common/scripts/TypeSpec-Project-Sync.ps1 {Path(sdk_folder)}")
 
     # generate SDK
-    cmd = Path("eng/common/scripts/TypeSpec-Project-Generate.ps1")
-    check_call(f"pwsh {cmd} {Path(sdk_folder)}", shell=True)
+    call(
+        f"pwsh eng/common/scripts/TypeSpec-Project-Generate.ps1 {Path(sdk_folder)} {npmrcPathArg}"
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv[1:]) != 1:
         print("Please input sdk folder like: sdk/datadog/azure-mgmt-datadog")
     else:
