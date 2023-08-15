82bd19e344a822c2d991fc5c7f50bbaafa4ba108
azure-sdk <PythonSdkPipelines>
2023-08-15 04:19:51 +0000

    code and test

--- sdk/compute/azure-mgmt-compute/generated_samples/run_command_examples/virtual_machine_run_command_delete.py
+++ sdk/compute/azure-mgmt-compute/generated_samples/run_command_examples/virtual_machine_run_command_delete.py
@@ -36,6 +36,6 @@ def main():
     ).result()
 
 
-# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-03-01/examples/runCommandExamples/VirtualMachineRunCommand_Delete.json
+# x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-07-01/examples/runCommandExamples/VirtualMachineRunCommand_Delete.json
 if __name__ == "__main__":
     main()
