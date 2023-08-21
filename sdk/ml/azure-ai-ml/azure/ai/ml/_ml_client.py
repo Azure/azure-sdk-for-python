29f300cf90a06d84533e4896ca95be0039d037dd
MilesHolland <108901744+MilesHolland@users.noreply.github.com>
2023-08-21 13:55:15 -0400

    1.10.0 cherry picks 8 18 (#31720)
    * Enable feature store to provision network and flip public network access (#31645)
    
    * Enable feature store to provision network and flip public network access
    
    * Format the code
    
    * Update provision network to include spark (#31685)
    
    * [FeatureStore] simple feature store setup (#31468)
    
    * simple feature store initial commit
    
    * add unit tests
    
    * add e2e tests
    
    * update
    
    * test
    
    * test
    
    * update
    
    * remove print
    
    * update
    
    * update
    
    * update
    
    * u
    
    * update template
    
    * update template
    
    * update
    
    * add dependency
    
    * [ml] Address apiview docstring comments for 1.10.0 release (#31709)
    
    * Remove extra/incorrect load_component methods and fix example
    
    * Fix 'integer' typo to 'int'
    
    * Fix 'integer' typo to 'int', change keyword args from param
    
    * Remove parent class args from docstring
    
    * Fix load_component example
    
    * Update project template (#31704)
    
    * minor fix (#31706)
    
    ---------
    
    Co-authored-by: Fred Li <51424245+fredms@users.noreply.github.com>
    Co-authored-by: runhli <87873226+runhli@users.noreply.github.com>
    Co-authored-by: Diondra <16376603+diondrapeck@users.noreply.github.com>
    Co-authored-by: ZhidaLiu <zhili@microsoft.com>

--- sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
@@ -495,7 +495,7 @@ class MLClient:
 
         self._featurestores = FeatureStoreOperations(
             self._operation_scope,
-            self._service_client_04_2023_preview,
+            self._service_client_06_2023_preview,
             self._operation_container,
             self._credential,
             **app_insights_handler_kwargs,
@@ -510,7 +510,7 @@ class MLClient:
         )
 
         self._featurestoreentities = FeatureStoreEntityOperations(
-            self._operation_scope, self._operation_config, self._service_client_04_2023_preview, **ops_kwargs
+            self._operation_scope, self._operation_config, self._service_client_06_2023_preview, **ops_kwargs
         )
 
         self._workspace_hubs = WorkspaceHubOperations(
