1ee065ea6fa1a5379b1b68fa0b678e6a98be532b
xiangyan99 <xiangsjtu@gmail.com>
2023-08-21 14:04:42 -0700

    update

--- sdk/core/azure-core/tests/test_pipeline.py
+++ sdk/core/azure-core/tests/test_pipeline.py
@@ -210,6 +210,12 @@ def test_format_url_braces_with_dot():
     assert url
 
 
+def test_format_url_single_brace():
+    base_url = "https://bing.com/{aaa.bbb"
+    with pytest.raises(ValueError):
+        url = _format_url_section(base_url)
+
+
 def test_format_incorrect_endpoint():
     # https://github.com/Azure/azure-sdk-for-python/pull/12106
     client = PipelineClientBase("{Endpoint}/text/analytics/v3.0")
