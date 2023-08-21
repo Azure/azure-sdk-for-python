9fa249ed9a93782bf7c3547ded5792cd217de5ab
Nemanja <55772835+nemanjarajic@users.noreply.github.com>
2023-08-21 14:29:33 -0700

    Existing signal update (#31428)
    * Existing signal update
    
    * reference data
    
    * properties
    
    * params
    
    * Fixes and unit tests
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * fix
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * pylint and black
    
    * pylint
    
    * pylint
    
    * recordings and e2e tests
    
    * e2e tests mark to production
    
    * pylint and black
    
    * pylint

--- sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_ml_client.py
@@ -472,7 +472,7 @@ class MLClient:
         self._schedules = ScheduleOperations(
             self._operation_scope,
             self._operation_config,
-            self._service_client_04_2023_preview,
+            self._service_client_06_2023_preview,
             self._operation_container,
             self._credential,
             _service_client_kwargs=kwargs,
