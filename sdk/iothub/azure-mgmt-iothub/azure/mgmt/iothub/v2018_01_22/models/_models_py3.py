601d6c91e4728272d6b0c180c1ef4516fe47959b
azure-sdk <PythonSdkPipelines>
2023-08-23 09:45:44 +0000

    code and test

--- sdk/iothub/azure-mgmt-iothub/azure/mgmt/iothub/v2018_01_22/models/_models_py3.py
+++ sdk/iothub/azure-mgmt-iothub/azure/mgmt/iothub/v2018_01_22/models/_models_py3.py
@@ -1303,7 +1303,7 @@ class JobResponse(_serialization.Model):
     :vartype end_time_utc: ~datetime.datetime
     :ivar type: The type of the job. Known values are: "unknown", "export", "import", "backup",
      "readDeviceProperties", "writeDeviceProperties", "updateDeviceConfiguration", "rebootDevice",
-     "factoryResetDevice", and "firmwareUpdate".
+     "factoryResetDevice", "firmwareUpdate", and "import".
     :vartype type: str or ~azure.mgmt.iothub.v2018_01_22.models.JobType
     :ivar status: The status of the job. Known values are: "unknown", "enqueued", "running",
      "completed", "failed", and "cancelled".
