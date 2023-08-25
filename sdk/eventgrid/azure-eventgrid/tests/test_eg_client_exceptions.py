8e5f0dc7eeb0cafcf23d7b1ecb505136b504efc3
Libba Lawrence <llawrence@microsoft.com>
2023-08-25 08:45:08 -0700

    Beta LiveTests (#30728)
    * add bicep file for tests
    
    * update output
    
    * update test
    
    * secret sanitization
    
    * refactor failing test
    
    * update conftest
    
    * update assets and sanitizers
    
    * update preparer loc
    
    * update conftest
    
    * conftest
    
    * update conftest
    
    * remove variables for now
    
    * update assets
    
    * update tests
    
    * try to update regex
    
    * update recordings
    
    * update conftest
    
    * update preparer
    
    * update test
    
    * update exception test
    
    * update tests
    
    * update asset
    
    * update conftest
    
    * pr comments
    
    * default needs to be eastus
    
    * import

--- sdk/eventgrid/azure-eventgrid/tests/test_eg_client_exceptions.py
+++ sdk/eventgrid/azure-eventgrid/tests/test_eg_client_exceptions.py
@@ -6,27 +6,27 @@
 import pytest
 import os
 import time
+from devtools_testutils import AzureRecordedTestCase, recorded_by_proxy
 from azure.eventgrid import EventGridClient
 from azure.eventgrid.models import *
 from azure.core.messaging import CloudEvent
 from azure.core.credentials import AzureKeyCredential
 from azure.core.exceptions import HttpResponseError, ResourceNotFoundError
 
+from eventgrid_preparer import EventGridPreparer
 
-class TestEGClientExceptions:
-    def create_eg_client(self, endpoint):
-        eventgrid_key = os.environ["EVENTGRID_KEY"]
+
+class TestEGClientExceptions(AzureRecordedTestCase):
+    def create_eg_client(self, endpoint, key):
         client = EventGridClient(
-            endpoint=endpoint, credential=AzureKeyCredential(eventgrid_key)
+            endpoint=endpoint, credential=AzureKeyCredential(key)
         )
         return client
 
-    @pytest.mark.live_test_only
-    def test_publish_cloud_event_bad_request(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        topic_name = os.environ["TOPIC_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_publish_cloud_event_bad_request(self, eventgrid_endpoint, eventgrid_key):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
         event = CloudEvent(
             type="Contoso.Items.ItemReceived",
             source=None,
@@ -35,13 +35,12 @@ class TestEGClientExceptions:
         )
 
         with pytest.raises(HttpResponseError):
-            client.publish_cloud_events(topic_name, [event])
+            client.publish_cloud_events("testtopic1", [event])
 
-    @pytest.mark.live_test_only
-    def test_publish_cloud_event_not_found(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy    
+    def test_publish_cloud_event_not_found(self, eventgrid_endpoint, eventgrid_key):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
         event = CloudEvent(
             type="Contoso.Items.ItemReceived",
             source=None,
@@ -52,152 +51,123 @@ class TestEGClientExceptions:
         with pytest.raises(ResourceNotFoundError):
             client.publish_cloud_events("faketopic", [event])
 
-    @pytest.mark.live_test_only
-    def test_receive_cloud_event_not_found(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_receive_cloud_event_not_found(self, eventgrid_endpoint, eventgrid_key, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
 
         with pytest.raises(ResourceNotFoundError):
-            client.receive_cloud_events("faketopic", event_subscription_name)
+            client.receive_cloud_events("faketopic", eventgrid_event_subscription_name)
 
-    @pytest.mark.live_test_only
-    def test_receive_cloud_event_max_events_negative(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        topic_name = os.environ["TOPIC_NAME"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_receive_cloud_event_max_events_negative(self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
 
         with pytest.raises(HttpResponseError):
             client.receive_cloud_events(
-                topic_name, event_subscription_name, max_events=-20
+                eventgrid_topic_name, eventgrid_event_subscription_name, max_events=-20
             )
 
-    @pytest.mark.live_test_only
-    def test_receive_cloud_event_timeout_negative(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        topic_name = os.environ["TOPIC_NAME"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_receive_cloud_event_timeout_negative(self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
 
         with pytest.raises(HttpResponseError):
             client.receive_cloud_events(
-                topic_name, event_subscription_name, max_wait_time=-20
+                eventgrid_topic_name, eventgrid_event_subscription_name, max_wait_time=-20
             )
 
-    @pytest.mark.live_test_only
-    def test_receive_cloud_event_timeout_max_value(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        topic_name = os.environ["TOPIC_NAME"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_receive_cloud_event_timeout_max_value(self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
 
         with pytest.raises(HttpResponseError):
             client.receive_cloud_events(
-                topic_name, event_subscription_name, max_wait_time=121
+                eventgrid_topic_name, eventgrid_event_subscription_name, max_wait_time=121
             )
 
-    @pytest.mark.live_test_only
-    def test_receive_cloud_event_timeout_min_value(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        topic_name = os.environ["TOPIC_NAME"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_receive_cloud_event_timeout_min_value(self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
 
         with pytest.raises(HttpResponseError):
             client.receive_cloud_events(
-                topic_name, event_subscription_name, max_wait_time=9
+                eventgrid_topic_name, eventgrid_event_subscription_name, max_wait_time=9
             )
 
-    @pytest.mark.live_test_only
-    def test_acknowledge_cloud_event_not_found(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_acknowledge_cloud_event_not_found(self, eventgrid_endpoint, eventgrid_key, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
 
         with pytest.raises(ResourceNotFoundError):
             lock_tokens = AcknowledgeOptions(lock_tokens=["faketoken"])
             client.acknowledge_cloud_events(
-                "faketopic", event_subscription_name, lock_tokens=lock_tokens
+                "faketopic", eventgrid_event_subscription_name, lock_tokens=lock_tokens
             )
 
-    @pytest.mark.live_test_only
-    def test_release_cloud_event_not_found(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_release_cloud_event_not_found(self, eventgrid_endpoint, eventgrid_key, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
 
         with pytest.raises(ResourceNotFoundError):
             lock_tokens = ReleaseOptions(lock_tokens=["faketoken"])
             client.release_cloud_events(
-                "faketopic", event_subscription_name, lock_tokens=lock_tokens
+                "faketopic", eventgrid_event_subscription_name, lock_tokens=lock_tokens
             )
 
-    @pytest.mark.live_test_only
-    def test_reject_cloud_event_not_found(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_reject_cloud_event_not_found(self, eventgrid_endpoint, eventgrid_key, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
         lock_tokens = RejectOptions(lock_tokens=["faketoken"])
 
         with pytest.raises(ResourceNotFoundError):
             client.reject_cloud_events(
-                "faketopic", event_subscription_name, lock_tokens=lock_tokens
+                "faketopic", eventgrid_event_subscription_name, lock_tokens=lock_tokens
             )
 
-    @pytest.mark.live_test_only
-    def test_acknowledge_cloud_event_invalid_token(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        topic_name = os.environ["TOPIC_NAME"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_acknowledge_cloud_event_invalid_token(self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
 
         lock_tokens = AcknowledgeOptions(lock_tokens=["faketoken"])
         ack = client.acknowledge_cloud_events(
-            topic_name, event_subscription_name, lock_tokens=lock_tokens
+            eventgrid_topic_name, eventgrid_event_subscription_name, lock_tokens=lock_tokens
         )
         assert type(ack) == AcknowledgeResult
         assert ack.succeeded_lock_tokens == []
         assert type(ack.failed_lock_tokens[0]) == FailedLockToken
         assert ack.failed_lock_tokens[0].lock_token == "faketoken"
 
-    @pytest.mark.live_test_only
-    def test_release_cloud_event_invalid_token(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        topic_name = os.environ["TOPIC_NAME"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_release_cloud_event_invalid_token(self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
 
         lock_tokens = ReleaseOptions(lock_tokens=["faketoken"])
         release = client.release_cloud_events(
-            topic_name, event_subscription_name, lock_tokens=lock_tokens
+            eventgrid_topic_name, eventgrid_event_subscription_name, lock_tokens=lock_tokens
         )
         assert type(release) == ReleaseResult
         assert release.succeeded_lock_tokens == []
         assert type(release.failed_lock_tokens[0]) == FailedLockToken
         assert release.failed_lock_tokens[0].lock_token == "faketoken"
 
-    @pytest.mark.live_test_only
-    def test_reject_cloud_event_invalid_token(self):
-        eventgrid_endpoint = os.environ["EVENTGRID_ENDPOINT"]
-        topic_name = os.environ["TOPIC_NAME"]
-        event_subscription_name = os.environ["EVENT_SUBSCRIPTION_NAME"]
-
-        client = self.create_eg_client(eventgrid_endpoint)
+    @EventGridPreparer()
+    @recorded_by_proxy
+    def test_reject_cloud_event_invalid_token(self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name):
+        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
         lock_tokens = RejectOptions(lock_tokens=["faketoken"])
 
         reject = client.reject_cloud_events(
-            topic_name, event_subscription_name, lock_tokens=lock_tokens
+            eventgrid_topic_name, eventgrid_event_subscription_name, lock_tokens=lock_tokens
         )
         assert type(reject) == RejectResult
         assert reject.succeeded_lock_tokens == []
