9ced6e87fa6f3103832123224af6eed13f7e08dd
Yalin Li <yall@microsoft.com>
2023-08-29 18:29:01 -0700

    [Tables] Fix a bug in submit_transaction() with empty operation list (#31482)

--- sdk/tables/azure-data-tables/tests/test_table_batch_async.py
+++ sdk/tables/azure-data-tables/tests/test_table_batch_async.py
@@ -893,6 +893,19 @@ class TestTableBatchAsync(AzureRecordedTestCase, AsyncTableTestCase):
     @pytest.mark.live_test_only
     @tables_decorator_async
     @recorded_by_proxy_async
+    async def test_empty_batch(self, tables_storage_account_name, tables_primary_storage_account_key):
+        url = self.account_url(tables_storage_account_name, "table")
+        table_name = self.get_resource_name("mytable")
+        async with TableClient(url, table_name, credential=tables_primary_storage_account_key) as client:
+            await client.create_table()
+            result = await client.submit_transaction([])
+            assert result == []
+            await client.delete_table()
+
+    # Playback doesn't work as test proxy issue: https://github.com/Azure/azure-sdk-tools/issues/2900
+    @pytest.mark.live_test_only
+    @tables_decorator_async
+    @recorded_by_proxy_async
     async def test_client_with_url_ends_with_table_name(
         self, tables_storage_account_name, tables_primary_storage_account_key
     ):
