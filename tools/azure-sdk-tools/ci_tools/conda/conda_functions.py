a01e0d790cdbadc7a1a0e434b7fa0c778a974a3e
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2023-08-30 17:54:21 -0700

    adding print of build command as well as removing azure-ai-ml for lack of successful build

--- tools/azure-sdk-tools/ci_tools/conda/conda_functions.py
+++ tools/azure-sdk-tools/ci_tools/conda/conda_functions.py
@@ -622,7 +622,7 @@ def invoke_conda_build(conda_output_dir: str, conda_env_dir: str, conda_build_fo
     if optional_py_version:
         command += f" --py {optional_py_version}"
 
-    print(command)
+    print("Calling \'{command}\' in folder {conda_build_folder}.")
     get_output(
         command,
         conda_build_folder,
