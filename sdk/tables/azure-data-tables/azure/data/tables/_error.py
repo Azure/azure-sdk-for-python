174f54eac4970acd2c3375ddff33f6bc092e8c49
Yalin Li <yall@microsoft.com>
2023-08-31 19:06:00 -0700

    Update sdk/tables/azure-data-tables/azure/data/tables/_error.py
    Co-authored-by: Anna Tisch <antisch@microsoft.com>

--- sdk/tables/azure-data-tables/azure/data/tables/_error.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_error.py
@@ -139,7 +139,7 @@ def _decode_error(response, error_message=None, error_type=None, **kwargs):  # p
                     additional_data[info.tag] = info.text
         else:
             if error_body:
-                # Attemp to parse from XML element
+                # Attempt to parse from XML element
                 for info in error_body.iter():
                     if info.tag.lower().find("code") != -1:
                         error_code = info.text
