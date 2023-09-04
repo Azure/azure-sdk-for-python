c9527900b73bbaea63e4b92439aa5693edaf29e8
azure-sdk <PythonSdkPipelines>
2023-09-04 03:15:39 +0000

    code and test

--- sdk/network/azure-mgmt-network/generated_samples/route_filter_rule_create.py
+++ sdk/network/azure-mgmt-network/generated_samples/route_filter_rule_create.py
@@ -44,6 +44,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/network/resource-manager/Microsoft.Network/stable/2023-04-01/examples/RouteFilterRuleCreate.json
+# x-ms-original-file: specification/network/resource-manager/Microsoft.Network/stable/2023-05-01/examples/RouteFilterRuleCreate.json
 if __name__ == "__main__":
     main()
