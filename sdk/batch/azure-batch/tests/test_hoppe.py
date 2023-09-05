ea790ddd6d52ee93791147bbed75f6762146d0e5
hoppe <hoppewang@microsoft.com>
2023-09-05 21:56:17 +0800

    really temp...

---
+++ sdk/batch/azure-batch/tests/test_hoppe.py
@@ -0,0 +1,17 @@
+
+# class A:
+#     def __init__(self, x):
+#         self.x = x
+
+#     def logX(self):
+#         print(self.x)
+
+
+# a1 = A("100")
+
+
+# A.logX = lambda self: print("Hello: " + self.x)
+
+# a1.logX = lambda: print("Hello 2:" )
+
+# a1.logX()
