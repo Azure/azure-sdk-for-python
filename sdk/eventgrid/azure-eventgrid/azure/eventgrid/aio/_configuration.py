9e95ed9f6ea2e7cfff84582a635de07130e764b7
l0lawrence <llawrence@microsoft.com>
2023-09-06 12:53:33 -0700

    generate with newer emitter

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/_configuration.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/_configuration.py
@@ -15,7 +15,9 @@ from azure.core.pipeline import policies
 from .._version import VERSION
 
 
-class EventGridClientConfiguration(Configuration):  # pylint: disable=too-many-instance-attributes
+class EventGridClientConfiguration(    # pylint: disable=too-many-instance-attributes,name-too-long
+    Configuration
+):
     """Configuration for EventGridClient.
 
     Note that all parameters used to create this instance are saved as instance
