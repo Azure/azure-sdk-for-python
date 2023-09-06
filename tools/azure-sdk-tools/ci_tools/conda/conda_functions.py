2714463407311ac5ad28100467b391db174de98f
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2023-09-06 16:57:56 -0700

    adjustments recommended by McCoy

--- tools/azure-sdk-tools/ci_tools/conda/conda_functions.py
+++ tools/azure-sdk-tools/ci_tools/conda/conda_functions.py
@@ -79,8 +79,6 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
