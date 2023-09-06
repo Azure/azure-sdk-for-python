4aa04fa0b008a998e0b1a74978482a128733ffe8
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2023-09-05 18:34:53 -0700

    update to consume sdk_verify_keywords

--- tools/azure-sdk-tools/ci_tools/keywords_verify.py
+++ tools/azure-sdk-tools/ci_tools/keywords_verify.py
@@ -22,9 +22,9 @@ def entrypoint() -> None:
     pkg_details = ParsedSetup.from_path(args.target)
 
     if in_ci():
-        if not is_check_enabled(args.target_package, "pylint"):
+        if not is_check_enabled(args.target_package, "verify_keywords"):
             logging.info(
-                f"Package {pkg_details.name} opts-out of pylint check."
+                f"Package {pkg_details.name} opts-out of keyword verification check."
             )
             exit(0)
 
