5c789c61d85d04bdf010464b5de5c457de543d36
mccoyp <mcpatino@microsoft.com>
2023-09-07 19:13:29 -0700

    Initial implementation

--- sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/crypto/_models.py
+++ sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/crypto/_models.py
@@ -2,11 +2,143 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-from typing import TYPE_CHECKING
+from typing import cast, Optional, Union, TYPE_CHECKING
+
+from cryptography.hazmat.primitives import _serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import utils as asym_utils
+from cryptography.hazmat.primitives.asymmetric.padding import AsymmetricPadding, OAEP, PKCS1v15, PSS
+from cryptography.hazmat.primitives.asymmetric.rsa import (
+    RSAPrivateKey,
+    RSAPrivateNumbers,
+    RSAPublicKey,
+    RSAPublicNumbers,
+)
+
+from ._enums import EncryptionAlgorithm, KeyWrapAlgorithm, SignatureAlgorithm
 
 if TYPE_CHECKING:
-    from . import EncryptionAlgorithm, KeyWrapAlgorithm, SignatureAlgorithm
-    from typing import Any, Optional
+    from ._client import CryptographyClient
+    from .._client import KeyClient
+
+
+SIGN_ALGORITHM_MAP = {
+    hashes.SHA256: SignatureAlgorithm.rs256,
+    hashes.SHA384: SignatureAlgorithm.rs384,
+    hashes.SHA512: SignatureAlgorithm.rs512,
+}
+PSS_MAP = {
+    SignatureAlgorithm.rs256: SignatureAlgorithm.ps256,
+    SignatureAlgorithm.rs384: SignatureAlgorithm.ps384,
+    SignatureAlgorithm.rs512: SignatureAlgorithm.ps512,
+}
+
+
+class KeyVaultPrivateKey(RSAPrivateKey):
+    """An `RSAPrivateKey` implementation based on a key managed by Key Vault.
+
+    :param str key_name: The name of the key vault key to use.
+    :param key_client: The client that will be used to communicate with Key Vault.
+    :type key_client: KeyClient
+    """
+
+    def __init__(self, key_name: str, key_client: "KeyClient", **kwargs) -> None:  # pylint:disable=unused-argument
+        self._key_name: str = key_name
+        self._key_client: "KeyClient" = key_client
+        self._crypto_client: "CryptographyClient" = key_client.get_cryptography_client(key_name)
+
+    def decrypt(self, ciphertext: bytes, padding: AsymmetricPadding) -> bytes:
+        """Decrypts the provided ciphertext.
+
+        :param bytes ciphertext: Encrypted bytes to decrypt.
+        :param padding: The padding to use. Supported paddings are `OAEP` and `PKCS1v15`. For `OAEP` padding, `SHA256`
+            will be used as the encryption algorithm and MGF1 will be used as the mask generation function.
+            See https://learn.microsoft.com/azure/key-vault/keys/about-keys-details for details.
+
+        :returns: The decrypted plaintext, as bytes.
+        :rtype: bytes
+        """
+        if isinstance(padding, OAEP):
+            # There's no public algorithm attribute attached to an OAEP padding instance, so we default to SHA256
+            algorithm = EncryptionAlgorithm.rsa_oaep_256
+        if isinstance(padding, PKCS1v15):
+            algorithm = EncryptionAlgorithm.rsa1_5
+        else:
+            raise ValueError(f"Unsupported padding: {padding.name}")
+        result = self._crypto_client.decrypt(algorithm, ciphertext)
+        return result.plaintext
+
+    @property
+    def key_size(self) -> int:
+        """The bit length of the public modulus.
+
+        :returns: The key's size.
+        :rtype: int
+        """
+        key = self._key_client.get_key(self._key_name)
+        return int.from_bytes(key.key.n, "big").bit_length()  # type: ignore[attr-defined]
+
+    def public_key(self) -> RSAPublicKey:
+        """The `RSAPublicKey` associated with this private key.
+
+        :returns: The `RSAPublicKey` associated with the key.
+        :rtype: RSAPublicKey
+        """
+        key = self._key_client.get_key(self._key_name)
+        e = int.from_bytes(key.key.e, "big")  # type: ignore[attr-defined]
+        n = int.from_bytes(key.key.n, "big")  # type: ignore[attr-defined]
+        public_numbers = RSAPublicNumbers(e, n)
+        return public_numbers.public_key()
+
+    def sign(
+        self,
+        data: bytes,
+        padding: AsymmetricPadding,
+        algorithm: Union[asym_utils.Prehashed, hashes.HashAlgorithm],
+    ) -> bytes:
+        """Signs the data.
+
+        :param bytes data: The data to sign, as bytes.
+        :param padding: The padding to use. Supported paddings are `PKCS1v15` and `PSS`. If `PSS` is given, the
+            operation will use RSASSA-PSS using SHA-x and MGF1 with SHA-x, where "x" is determined by the `algorithm`
+            provided. See https://learn.microsoft.com/azure/key-vault/keys/about-keys-details for details.
+        :type padding: AsymmetricPadding
+        :param algorithm: The algorithm to sign with. Only `HashAlgorithm`s are supported -- specifically, `SHA256`,
+            `SHA384`, and `SHA512`.
+        :type algorithm: asym_utils.Prehashed or hashes.HashAlgorithm
+
+        :returns: The signature, as bytes.
+        :rtype: bytes
+        """
+        if isinstance(algorithm, asym_utils.Prehashed):
+            raise ValueError("`Prehashed` algorithms are unsupported. Please provide a `HashAlgorithm` instead.")
+
+        mapped_algorithm = SIGN_ALGORITHM_MAP.get(type(algorithm))
+        if mapped_algorithm is None:
+            raise ValueError(f"Unsupported algorithm: {algorithm.name}")
+        # If PSS padding is requested, use the PSS equivalent algorithm
+        if isinstance(padding, PSS):
+            mapped_algorithm = PSS_MAP.get(mapped_algorithm)
+        # The only other padding accepted is PKCS1v15
+        elif not isinstance(padding, PKCS1v15):
+            raise ValueError(f"Unsupported padding: {padding.name}")
+
+        digest = hashes.Hash(algorithm)
+        digest.update(data)
+        result = self._crypto_client.sign(cast(SignatureAlgorithm, mapped_algorithm), digest.finalize())
+        return result.signature
+
+    def private_numbers(self) -> RSAPrivateNumbers:
+        """Returns an RSAPrivateNumbers. Not implemented, as the private key is managed by Key Vault."""
+        raise NotImplementedError()
+
+    def private_bytes(
+        self,
+        encoding: _serialization.Encoding,
+        format: _serialization.PrivateFormat,
+        encryption_algorithm: _serialization.KeySerializationEncryption,
+    ) -> bytes:
+        """Returns the key serialized as bytes. Not implemented, as the private key is managed by Key Vault."""
+        raise NotImplementedError()
 
 
 class DecryptResult:
@@ -18,7 +150,7 @@ class DecryptResult:
     :param bytes plaintext: The decrypted bytes
     """
 
-    def __init__(self, key_id: "Optional[str]", algorithm: "EncryptionAlgorithm", plaintext: bytes) -> None:
+    def __init__(self, key_id: Optional[str], algorithm: EncryptionAlgorithm, plaintext: bytes) -> None:
         self.key_id = key_id
         self.algorithm = algorithm
         self.plaintext = plaintext
@@ -39,9 +171,7 @@ class EncryptResult:
         authenticated algorithm
     """
 
-    def __init__(
-        self, key_id: "Optional[str]", algorithm: "EncryptionAlgorithm", ciphertext: bytes, **kwargs
-    ) -> None:
+    def __init__(self, key_id: Optional[str], algorithm: EncryptionAlgorithm, ciphertext: bytes, **kwargs) -> None:
         self.key_id = key_id
         self.algorithm = algorithm
         self.ciphertext = ciphertext
@@ -59,7 +189,7 @@ class SignResult:
     :param bytes signature:
     """
 
-    def __init__(self, key_id: "Optional[str]", algorithm: "SignatureAlgorithm", signature: bytes) -> None:
+    def __init__(self, key_id: Optional[str], algorithm: SignatureAlgorithm, signature: bytes) -> None:
         self.key_id = key_id
         self.algorithm = algorithm
         self.signature = signature
@@ -74,7 +204,7 @@ class VerifyResult:
     :type algorithm: ~azure.keyvault.keys.crypto.SignatureAlgorithm
     """
 
-    def __init__(self, key_id: "Optional[str]", is_valid: bool, algorithm: "SignatureAlgorithm") -> None:
+    def __init__(self, key_id: Optional[str], is_valid: bool, algorithm: SignatureAlgorithm) -> None:
         self.key_id = key_id
         self.is_valid = is_valid
         self.algorithm = algorithm
@@ -89,7 +219,7 @@ class UnwrapResult:
     :param bytes key: The unwrapped key
     """
 
-    def __init__(self, key_id: "Optional[str]", algorithm: "KeyWrapAlgorithm", key: bytes) -> None:
+    def __init__(self, key_id: Optional[str], algorithm: KeyWrapAlgorithm, key: bytes) -> None:
         self.key_id = key_id
         self.algorithm = algorithm
         self.key = key
@@ -104,7 +234,7 @@ class WrapResult:
     :param bytes encrypted_key: The encrypted key bytes
     """
 
-    def __init__(self, key_id: "Optional[str]", algorithm: "KeyWrapAlgorithm", encrypted_key: bytes) -> None:
+    def __init__(self, key_id: Optional[str], algorithm: KeyWrapAlgorithm, encrypted_key: bytes) -> None:
         self.key_id = key_id
         self.algorithm = algorithm
         self.encrypted_key = encrypted_key
