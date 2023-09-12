2edb7e070f14f0d07d09ad3a72896485cdb7ae2a
Fred Li <feli1@microsoft.com>
2023-09-12 12:27:54 -0700

    Enable feature set backfill with yml file

---
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_feature_set/feature_set_backfill_schema.py
@@ -0,0 +1,21 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+from marshmallow import fields
+from azure.ai.ml._schema.core.fields import NestedField
+from azure.ai.ml._schema.core.schema import YamlFileSchema
+from azure.ai.ml._schema._feature_set.materialization_settings_schema import (
+    MaterializationComputeResourceSchema,
+)
+from azure.ai.ml._schema._feature_set.feature_window_schema import FeatureWindowSchema
+
+
+class FeatureSetBackfillSchema(YamlFileSchema):
+    name = fields.Str()
+    version = fields.Str()
+    feature_window = NestedField(FeatureWindowSchema)
+    description = fields.Str()
+    tags = fields.Dict()
+    resource = NestedField(MaterializationComputeResourceSchema)
+    spark_conf = fields.Dict()
