9087a264ceb52ce3af9cfae62c45140529531682
Kevin Zhang <iaminwc@163.com>
2023-09-15 16:15:24 -0700

    minor fix for subcomponents.py

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_builders/subcomponents.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_builders/subcomponents.py
@@ -43,7 +43,7 @@ def save_mltable_yaml(path: str, mltable_paths: List[str]) -> None:
 
 # TODO 2293610: add support for more types of outputs besides uri_folder and mltable
 @command_component()
-def create_scatter_output_table(aggregated_output: Output, **kwargs: Any) -> None:
+def create_scatter_output_table(aggregated_output: Output, **kwargs: Any) -> Output:
     """Create scatter output table.
 
     This function is used by the FL scatter gather node to reduce a dynamic number of silo outputs
