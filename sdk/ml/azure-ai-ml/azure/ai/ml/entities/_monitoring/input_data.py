81a76fbc02c5c61efeeac251728b23a9242670e5
nemanjarajic <nrajic30@gmail.com>
2023-09-15 19:26:57 -0700

    pylint

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_monitoring/input_data.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_monitoring/input_data.py
@@ -2,6 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+# pylint: disable=protected-access
+
 import datetime
 from typing import Dict, Optional
 import isodate
