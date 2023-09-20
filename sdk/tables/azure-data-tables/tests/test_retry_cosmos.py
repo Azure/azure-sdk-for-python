f2c41dd1ad66bf1bb952aa460dda874a84512d23
YalinLi0312 <yall@microsoft.com>
2023-09-20 16:00:06 -0700

    Test get_entity() without entity in cosmos

--- sdk/tables/azure-data-tables/tests/test_retry_cosmos.py
+++ sdk/tables/azure-data-tables/tests/test_retry_cosmos.py
@@ -4,14 +4,15 @@
 # license information.
 # --------------------------------------------------------------------------
 import pytest
+import time
 
 from devtools_testutils import AzureRecordedTestCase, recorded_by_proxy
 
-from azure.core.exceptions import ServiceRequestError
+from azure.core.exceptions import ResourceNotFoundError, ServiceRequestError
 from azure.data.tables import TableClient
 from azure.data.tables._models import LocationMode
 
-from test_retry import FailoverRetryTransport, SecondaryFailoverRetryTransport, SecondpageFailoverRetryTransport
+from test_retry import FailoverRetryTransport, SecondpageFailoverRetryTransport
 from preparers import cosmos_decorator
 from _shared.testcase import TableTestCase
 
@@ -38,10 +39,28 @@ class TestStorageRetry(AzureRecordedTestCase, TableTestCase):
                 client.create_table(failover=ServiceRequestError("Attempting to force failover"))  # POST, not retry
             assert "Attempting to force failover" in str(ex.value)
 
-        # prepare an entity
+        # prepare a table
         with TableClient(url, table_name, credential=tables_primary_cosmos_account_key) as client:
             client.create_table()
+        time.sleep(10)
+
+        # test get_entity() without the entity ready
+        with TableClient(
+            url,
+            table_name,
+            credential=tables_primary_cosmos_account_key,
+            retry_total=5,
+            retry_to_secondary=True,
+            transport=FailoverRetryTransport(),
+        ) as client:
+            with pytest.raises(ResourceNotFoundError) as ex:
+                client.get_entity("foo", "bar", failover=ServiceRequestError("Attempting to force failover"))
+            assert "The specified resource does not exist." in str(ex.value)
+
+        # prepare an entity
+        with TableClient(url, table_name, credential=tables_primary_cosmos_account_key) as client:
             client.create_entity(entity)
+        time.sleep(10)
 
         with TableClient(
             url,
@@ -97,10 +116,28 @@ class TestStorageRetry(AzureRecordedTestCase, TableTestCase):
         table_name = self.get_resource_name("mytable")
         entity = {"PartitionKey": "foo", "RowKey": "bar"}
 
-        # prepare an entity
+        # prepare a table
         with TableClient(url, table_name, credential=tables_primary_cosmos_account_key) as client:
             client.create_table()
+        time.sleep(10)
+
+        # test get_entity() without the entity ready
+        with TableClient(
+            url,
+            table_name,
+            credential=tables_primary_cosmos_account_key,
+            retry_total=5,
+            location_mode=LocationMode.SECONDARY,
+            transport=FailoverRetryTransport(location_mode=LocationMode.SECONDARY),
+        ) as client:
+            with pytest.raises(ResourceNotFoundError) as ex:
+                client.get_entity("foo", "bar", failover=ServiceRequestError("Attempting to force failover"))
+            assert "The specified resource does not exist." in str(ex.value)
+
+        # prepare an entity
+        with TableClient(url, table_name, credential=tables_primary_cosmos_account_key) as client:
             client.create_entity(entity)
+        time.sleep(10)
 
         with TableClient(
             url,
@@ -108,7 +145,7 @@ class TestStorageRetry(AzureRecordedTestCase, TableTestCase):
             credential=tables_primary_cosmos_account_key,
             retry_total=5,
             location_mode=LocationMode.SECONDARY,
-            transport=SecondaryFailoverRetryTransport(),
+            transport=FailoverRetryTransport(location_mode=LocationMode.SECONDARY),
         ) as client:
             client.get_entity(
                 "foo", "bar", failover=ServiceRequestError("Attempting to force failover")
