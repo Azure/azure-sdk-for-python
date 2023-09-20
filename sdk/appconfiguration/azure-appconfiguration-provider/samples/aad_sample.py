aa5a233e6665ce29abc949375afcbbae78ba3ce2
Matt Metcalf <mrm9084@gmail.com>
2023-09-20 13:58:26 -0700

    Fixing User Agent usage in samples (#32161)

--- sdk/appconfiguration/azure-appconfiguration-provider/samples/aad_sample.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/samples/aad_sample.py
@@ -6,27 +6,28 @@
 
 from azure.appconfiguration.provider import load, SettingSelector
 import os
-from sample_utilities import get_authority, get_audience, get_credential
+from sample_utilities import get_authority, get_audience, get_credential, get_client_modifications
 
 endpoint = os.environ.get("APPCONFIGURATION_ENDPOINT_STRING")
 authority = get_authority(endpoint)
 audience = get_audience(authority)
 credential = get_credential(authority)
+kwargs = get_client_modifications()
 
 # Connecting to Azure App Configuration using AAD
-config = load(endpoint=endpoint, credential=credential)
+config = load(endpoint=endpoint, credential=credential, **kwargs)
 
 print(config["message"])
 
 # Connecting to Azure App Configuration using AAD and trim key prefixes
 trimmed = {"test."}
-config = load(endpoint=endpoint, credential=credential, trim_prefixes=trimmed)
+config = load(endpoint=endpoint, credential=credential, trim_prefixes=trimmed, **kwargs)
 
 print(config["message"])
 
 # Connection to Azure App Configuration using SettingSelector
 selects = {SettingSelector(key_filter="message*")}
-config = load(endpoint=endpoint, credential=credential, selects=selects)
+config = load(endpoint=endpoint, credential=credential, selects=selects, **kwargs)
 
 print("message found: " + str("message" in config))
 print("test.message found: " + str("test.message" in config))
