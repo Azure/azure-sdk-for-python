e3dbf51b9af97123c00c0505ad887e60ad289302
Diondra Peck <dipeck@microsoft.com>
2023-09-20 12:39:00 -0700

    Fix circular import/mypy issue

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/pipeline/__init__.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/pipeline/__init__.py
@@ -2,4 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+from __future__ import annotations
+
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
