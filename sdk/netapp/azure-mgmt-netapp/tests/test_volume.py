4d0697bb181edfca7d97f3d2c64d75a16c9cd583
Einar <einar@netapp.com>
2023-09-20 11:11:51 +0000

    Remove backups since they have been depricated Add a get call after updating pool Log account deletion is done Add endoint tests for new operations

--- sdk/netapp/azure-mgmt-netapp/tests/test_volume.py
+++ sdk/netapp/azure-mgmt-netapp/tests/test_volume.py
@@ -494,3 +494,14 @@ class TestNetAppVolume(AzureMgmtRecordedTestCase):
             delete_volume(self.client, setup.TEST_RG, setup.PERMA_ACCOUNT, setup.TEST_POOL_2, volumeName1)
             delete_pool(self.client, setup.TEST_RG, setup.PERMA_ACCOUNT, setup.TEST_POOL_2)
         print("Finished with test_pool_change")
+        
+    @recorded_by_proxy
+    def test_begin_populate_availability_zone(self):
+        set_bodiless_matcher()
+        print("Starting begin_populate_availability_zone")
+        try:
+            self.client.volumes.begin_populate_availability_zone(setup.TEST_RG, setup.PERMA_ACCOUNT, setup.PERMA_POOL, setup.PERMA_VOLUME)
+        except Exception as e:
+            #AFEC still in 2023.5. Fix this in 2023.7 
+            assert str(e).startswith("(")
+        print("Finished with begin_populate_availability_zone")
