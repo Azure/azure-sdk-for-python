69c60d7fe138f72f17e08c4f0116c1f4d6520b26
Einar <einar@netapp.com>
2023-09-21 12:08:21 +0000

    fix recording for network sibling

--- sdk/netapp/azure-mgmt-netapp/tests/test_net_app_resource_operations.py
+++ sdk/netapp/azure-mgmt-netapp/tests/test_net_app_resource_operations.py
@@ -2,8 +2,6 @@ from devtools_testutils import AzureMgmtRecordedTestCase, recorded_by_proxy
 import setup
 import azure.mgmt.netapp.models
 
-SUBSID = '69a75bda-882e-44d5-8431-63421204132a'
-
 class TestNetAppResourceOperations(AzureMgmtRecordedTestCase):
 
     def setup_method(self, method):
@@ -16,7 +14,7 @@ class TestNetAppResourceOperations(AzureMgmtRecordedTestCase):
         
         print("Starting test_query_network_sibling_set")
         try:
-           self.client.net_app_resource.query_network_sibling_set(setup.LOCATION, "9760acf5-4638-11e7-9bdb-020073ca3333", "/subscriptions/" + SUBSID + "/resourceGroups/sdk-python-tests-rg-tmp/providers/Microsoft.Network/virtualNetworks/testVnet/subnets/sdk-python-tests-subnet")
+           self.client.net_app_resource.query_network_sibling_set(setup.LOCATION, "9760acf5-4638-11e7-9bdb-020073ca3333", "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/sdk-python-tests-rg-tmp/providers/Microsoft.Network/virtualNetworks/testVnet/subnets/sdk-python-tests-subnet")
         except Exception as e:
             assert str(e).startswith("(NoNicsFoundForSubnet)")
         print("Finished with test_query_network_sibling_set")
@@ -26,7 +24,7 @@ class TestNetAppResourceOperations(AzureMgmtRecordedTestCase):
         
         print("Starting test_begin_update_network_sibling_set")
         try:
-           self.client.net_app_resource.begin_update_network_sibling_set(setup.LOCATION, "9760acf5-4638-11e7-9bdb-020073ca3333", "/subscriptions/" + SUBSID + "/resourceGroups/sdk-python-tests-rg-tmp/providers/Microsoft.Network/virtualNetworks/testVnet/subnets/sdk-python-tests-subnet", "herpiderp", "Basic")
+           self.client.net_app_resource.begin_update_network_sibling_set(setup.LOCATION, "9760acf5-4638-11e7-9bdb-020073ca3333", "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/sdk-python-tests-rg-tmp/providers/Microsoft.Network/virtualNetworks/testVnet/subnets/sdk-python-tests-subnet", "herpiderp", "Basic")
         except Exception as e:
             assert str(e).startswith("(NetworkSiblingSetChanged)")
         print("Finished with test_begin_update_network_sibling_set")
