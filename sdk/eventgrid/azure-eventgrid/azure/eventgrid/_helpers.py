c6f3698576fecb4ccf6e990882e87e3cd4173d9f
l0lawrence <llawrence@microsoft.com>
2023-09-22 13:13:49 -0700

    use key

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/_helpers.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/_helpers.py
@@ -200,7 +200,7 @@ def _to_json_http_request(events):
         if isinstance(event, EventGridEvent):
             for attr in event._attribute_map:
                 if getattr(event, attr) is not None:
-                    data[attr] = _SERIALIZER.body(getattr(event, attr), event._attribute_map[attr]["type"])
+                    data[event._attribute_map[attr]["key"]] = _SERIALIZER.body(getattr(event, attr), event._attribute_map[attr]["type"])
         else:
             data["type"] =  _SERIALIZER.body(event.type, "str")
             data["specversion"] = _SERIALIZER.body(event.specversion, "str")
