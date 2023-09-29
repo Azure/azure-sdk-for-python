9e3f7d469c2e742b93075aaf2288179c91d30f1b
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2023-09-29 15:24:49 -0700

    repair specific get functionality

--- tools/azure-sdk-tools/tests/test_optional_functionality.py
+++ tools/azure-sdk-tools/tests/test_optional_functionality.py
@@ -41,29 +41,21 @@ def test_toml_result():
 def test_optional_specific_get():
     package_with_toml = os.path.join(integration_folder, 'scenarios', 'optional_environment_two_options')
     actual = get_config_setting(package_with_toml, 'optional')
-    expected = {
-        'mypy': True,
-        'type_check_samples': True,
-        'verifytypes': True,
-        'pyright': True,
-        'pylint': True,
-        'black': True,
-        'optional':[
-            {
-                'name': 'no_requests',
-                'install': [],
-                'uninstall': ['requests'],
-                'additional_pytest_args': ['-k', '*_async.py']
-            },
-            {
-                'name': 'no_aiohttp', 
-                'install': [], 
-                'uninstall': ['aiohttp'], 
-                'additional_pytest_args': ['-k', 'not *_async.py']
-            }
-        ]
-    }
-
+    expected = [
+        {
+            'name': 'no_requests',
+            'install': [],
+            'uninstall': ['requests'],
+            'additional_pytest_args': ['-k', '*_async.py']
+        },
+        {
+            'name': 'no_aiohttp', 
+            'install': [], 
+            'uninstall': ['aiohttp'], 
+            'additional_pytest_args': ['-k', 'not *_async.py']
+        }
+    ]
+    
     assert expected == actual
 
 
