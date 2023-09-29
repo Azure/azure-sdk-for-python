4b4062dc9628ba2ba02ab1061ca6466e7082d7c8
Matt Metcalf <mrm9084@gmail.com>
2023-09-29 00:00:57 -0700

    Update sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_azureappconfigurationprovider.py
    Co-authored-by: Anna Tisch <antisch@microsoft.com>

--- sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_azureappconfigurationprovider.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_azureappconfigurationprovider.py
@@ -81,7 +81,7 @@ def load(
     :keyword keyvault_client_configs: A Mapping of SecretClient endpoints to client configurations from
      azure-keyvault-secrets. This is optional if keyvault_credential is provided. If a credential isn't provided a credential
      will need to be in each set for each.
-    :paramtype keyvault_client_configs: Mapping[Url, Mapping]
+    :paramtype keyvault_client_configs: Mapping[str, Mapping]
     :keyword secret_resolver: A function that takes a URI and returns a value.
     :paramtype secret_resolver: Callable[[str], str]
     :keyword refresh_on: One or more settings whose modification will trigger a full refresh after a fixed interval.
