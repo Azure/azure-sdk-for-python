f0d84ecb32c47678c7fcd988c724e72a4cbf530d
Rango Meadows <v-rameadows@microsoft.com>
2023-10-02 17:03:28 -0700

    Updating tests with simpler template & assets.json

--- sdk/translation/azure-ai-translation-text/tests/test_break_sentence_async.py
+++ sdk/translation/azure-ai-translation-text/tests/test_break_sentence_async.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License.
 # ------------------------------------
 
-from devtools_testutils import recorded_by_proxy
+from devtools_testutils.aio import recorded_by_proxy_async
 from azure.ai.translation.text.models import InputTextItem
 from preparer import TextTranslationPreparer
 from testcase import TextTranslationTest
@@ -12,75 +12,71 @@ import asyncio
 class TestBreakSentenceAsync(TextTranslationTest):
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_autodetect(self, **kwargs):
-        async def test_autodetect_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            apikey = kwargs.get("text_translation_apikey")
-            region = kwargs.get("text_translation_region")
-            client = self.create_async_client(endpoint, apikey, region)
-            input_text_elements = [InputTextItem(text="Hello world")]
+    @recorded_by_proxy_async
+    async def test_autodetect(self, **kwargs):
+        endpoint = kwargs.get("text_translation_endpoint")
+        apikey = kwargs.get("text_translation_apikey")
+        region = kwargs.get("text_translation_region")
+        client = self.create_async_client(endpoint, apikey, region)
+        input_text_elements = [InputTextItem(text="Hello world")]
 
+        async with client:
             response = await client.find_sentence_boundaries(content=input_text_elements)
-            assert response is not None
-            assert response[0].detected_language.language == "en"
-            assert response[0].detected_language.score == 1
-            assert response[0].sent_len[0] == 11
-        asyncio.run(test_autodetect_async(**kwargs))
+        assert response is not None
+        assert response[0].detected_language.language == "en"
+        assert response[0].detected_language.score == 1
+        assert response[0].sent_len[0] == 11
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_with_language(self, **kwargs):
-        async def test_with_language_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            apikey = kwargs.get("text_translation_apikey")
-            region = kwargs.get("text_translation_region")
-            client = self.create_async_client(endpoint, apikey, region)
+    @recorded_by_proxy_async
+    async def test_with_language(self, **kwargs):    
+        endpoint = kwargs.get("text_translation_endpoint")
+        apikey = kwargs.get("text_translation_apikey")
+        region = kwargs.get("text_translation_region")
+        client = self.create_async_client(endpoint, apikey, region)
 
-            input_text_elements = [InputTextItem(
-                text="รวบรวมแผ่นคำตอบ ระยะเวลาของโครงการ วิธีเลือกชายในฝัน หมายเลขซีเรียลของระเบียน วันที่สิ้นสุดของโครงการเมื่อเสร็จสมบูรณ์ ปีที่มีการรวบรวม ทุกคนมีวัฒนธรรมและวิธีคิดเหมือนกัน ได้รับโทษจำคุกตลอดชีวิตใน ฉันลดได้ถึง 55 ปอนด์ได้อย่างไร  ฉันคิดว่าใครๆ ก็ต้องการกำหนดเมนูอาหารส่วนบุคคล")]
+        input_text_elements = [InputTextItem(
+            text="รวบรวมแผ่นคำตอบ ระยะเวลาของโครงการ วิธีเลือกชายในฝัน หมายเลขซีเรียลของระเบียน วันที่สิ้นสุดของโครงการเมื่อเสร็จสมบูรณ์ ปีที่มีการรวบรวม ทุกคนมีวัฒนธรรมและวิธีคิดเหมือนกัน ได้รับโทษจำคุกตลอดชีวิตใน ฉันลดได้ถึง 55 ปอนด์ได้อย่างไร  ฉันคิดว่าใครๆ ก็ต้องการกำหนดเมนูอาหารส่วนบุคคล")]
 
+        async with client:
             response = await client.find_sentence_boundaries(
-            content = input_text_elements, language="th")
-            assert response is not None
-            expected_lengths = [78, 41, 110, 46]
-            for i, expected_length in enumerate(expected_lengths):
-                assert expected_length == response[0].sent_len[i]
-        asyncio.run(test_with_language_async(**kwargs))        
+                content = input_text_elements, language="th")
+        assert response is not None
+        expected_lengths = [78, 41, 110, 46]
+        for i, expected_length in enumerate(expected_lengths):
+            assert expected_length == response[0].sent_len[i]          
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_with_language_script(self, **kwargs):
-        async def test_with_language_script_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            apikey = kwargs.get("text_translation_apikey")
-            region = kwargs.get("text_translation_region")
-            client = self.create_async_client(endpoint, apikey, region)
+    @recorded_by_proxy_async
+    async def test_with_language_script(self, **kwargs):        
+        endpoint = kwargs.get("text_translation_endpoint")
+        apikey = kwargs.get("text_translation_apikey")
+        region = kwargs.get("text_translation_region")
+        client = self.create_async_client(endpoint, apikey, region)
 
-            input_text_elements = [InputTextItem(text="zhè shì gè cè shì。")]
+        input_text_elements = [InputTextItem(text="zhè shì gè cè shì。")]
 
+        async with client:
             response = await client.find_sentence_boundaries(
                 content=input_text_elements, language="zh-Hans", script="Latn")
-            assert response is not None
-            assert response[0].sent_len[0] == 18
-        asyncio.run(test_with_language_script_async(**kwargs))
+        assert response is not None
+        assert response[0].sent_len[0] == 18        
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_with_multiple_languages(self, **kwargs):
-        async def test_with_multiple_languages_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            apikey = kwargs.get("text_translation_apikey")
-            region = kwargs.get("text_translation_region")
-            client = self.create_async_client(endpoint, apikey, region)
+    @recorded_by_proxy_async
+    async def test_with_multiple_languages(self, **kwargs):
+        endpoint = kwargs.get("text_translation_endpoint")
+        apikey = kwargs.get("text_translation_apikey")
+        region = kwargs.get("text_translation_region")
+        client = self.create_async_client(endpoint, apikey, region)
 
-            input_text_elements = [InputTextItem(text="hello world"), InputTextItem(
-                text="العالم هو مكان مثير جدا للاهتمام")]
+        input_text_elements = [InputTextItem(text="hello world"), InputTextItem(
+            text="العالم هو مكان مثير جدا للاهتمام")]
 
+        async with client:
             response = await client.find_sentence_boundaries(content=input_text_elements)
-            assert response is not None
-            assert response[0].detected_language.language == "en"
-            assert response[1].detected_language.language == "ar"
-            assert response[0].sent_len[0] == 11
-            assert response[1].sent_len[0] == 32
-        asyncio.run(test_with_multiple_languages_async(**kwargs))
+        assert response is not None
+        assert response[0].detected_language.language == "en"
+        assert response[1].detected_language.language == "ar"
+        assert response[0].sent_len[0] == 11
+        assert response[1].sent_len[0] == 32
--- sdk/translation/azure-ai-translation-text/tests/test_get_languages_async.py
+++ sdk/translation/azure-ai-translation-text/tests/test_get_languages_async.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License.
 # ------------------------------------
 
-from devtools_testutils import recorded_by_proxy
+from devtools_testutils.aio import recorded_by_proxy_async
 from preparer import TextTranslationPreparer
 from testcase import TextTranslationTest
 import asyncio
@@ -11,133 +11,126 @@ import asyncio
 class TestGetLanguagesAsync(TextTranslationTest):
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_all_scopes(self, **kwargs):
-        async def test_all_scopes_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            client = self.create_async_getlanguage_client(endpoint)
+    @recorded_by_proxy_async
+    async def test_all_scopes(self, **kwargs):        
+        endpoint = kwargs.get("text_translation_endpoint")
+        client = self.create_async_getlanguage_client(endpoint)
+        async with client:
             response = await client.get_languages()
 
-            assert len(response.translation) > 0
-            assert len(response.transliteration) > 0
-            assert len(response.dictionary) > 0
-        asyncio.run(test_all_scopes_async(**kwargs))
+        assert len(response.translation) > 0
+        assert len(response.transliteration) > 0
+        assert len(response.dictionary) > 0        
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_translation_scope(self, **kwargs):
-        async def test_translation_scope_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            client = self.create_async_getlanguage_client(endpoint)
+    @recorded_by_proxy_async
+    async def test_translation_scope(self, **kwargs):        
+        endpoint = kwargs.get("text_translation_endpoint")
+        client = self.create_async_getlanguage_client(endpoint)
+        async with client:
             response = await client.get_languages(scope="translation")
 
-            assert len(response.translation) > 0
-            translations = response.translation["af"]
-            assert translations.dir is not None
-            assert translations.name is not None
-            assert translations.native_name is not None
-        asyncio.run(test_translation_scope_async(**kwargs))
+        assert len(response.translation) > 0
+        translations = response.translation["af"]
+        assert translations.dir is not None
+        assert translations.name is not None
+        assert translations.native_name is not None        
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_transliteration_scope(self, **kwargs):
-        async def test_transliteration_scope_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            client = self.create_async_getlanguage_client(endpoint)
+    @recorded_by_proxy_async
+    async def test_transliteration_scope(self, **kwargs):        
+        endpoint = kwargs.get("text_translation_endpoint")
+        client = self.create_async_getlanguage_client(endpoint)
+        async with client:
             response = await client.get_languages(scope="transliteration")
 
-            assert len(response.transliteration) > 0
-            transliterations = response.transliteration["be"]
-            assert transliterations.name is not None
-            assert transliterations.native_name is not None
-            assert transliterations.scripts is not None
+        assert len(response.transliteration) > 0
+        transliterations = response.transliteration["be"]
+        assert transliterations.name is not None
+        assert transliterations.native_name is not None
+        assert transliterations.scripts is not None
 
-            assert len(transliterations.scripts) > 0
-            assert transliterations.scripts[0].name is not None
-            assert transliterations.scripts[0].native_name is not None
-            assert transliterations.scripts[0].code is not None
-            assert transliterations.scripts[0].dir is not None
-            assert transliterations.scripts[0].to_scripts is not None
+        assert len(transliterations.scripts) > 0
+        assert transliterations.scripts[0].name is not None
+        assert transliterations.scripts[0].native_name is not None
+        assert transliterations.scripts[0].code is not None
+        assert transliterations.scripts[0].dir is not None
+        assert transliterations.scripts[0].to_scripts is not None
 
-            assert len(transliterations.scripts[0].to_scripts) > 0
-            assert transliterations.scripts[0].to_scripts[0].name is not None
-            assert transliterations.scripts[0].to_scripts[0].native_name is not None
-            assert transliterations.scripts[0].to_scripts[0].code is not None
-            assert transliterations.scripts[0].to_scripts[0].dir is not None
-        asyncio.run(test_transliteration_scope_async(**kwargs))
+        assert len(transliterations.scripts[0].to_scripts) > 0
+        assert transliterations.scripts[0].to_scripts[0].name is not None
+        assert transliterations.scripts[0].to_scripts[0].native_name is not None
+        assert transliterations.scripts[0].to_scripts[0].code is not None
+        assert transliterations.scripts[0].to_scripts[0].dir is not None
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_transliteration_multiple_scripts(self, **kwargs):
-        async def test_transliteration_multiple_scripts_async(**kwargs):            
-            endpoint = kwargs.get("text_translation_endpoint")
-            client = self.create_async_getlanguage_client(endpoint)
+    @recorded_by_proxy_async
+    async def test_transliteration_multiple_scripts(self, **kwargs):        
+        endpoint = kwargs.get("text_translation_endpoint")
+        client = self.create_async_getlanguage_client(endpoint)
+        async with client:
             response = await client.get_languages(scope="transliteration")
 
-            assert len(response.transliteration) > 0
-            transliterations = response.transliteration["zh-Hant"]
-            assert transliterations.name is not None
-            assert transliterations.native_name is not None
-            assert transliterations.scripts is not None
+        assert len(response.transliteration) > 0
+        transliterations = response.transliteration["zh-Hant"]
+        assert transliterations.name is not None
+        assert transliterations.native_name is not None
+        assert transliterations.scripts is not None
 
-            assert len(transliterations.scripts) > 1
-            assert len(transliterations.scripts[0].to_scripts) > 1
-            assert len(transliterations.scripts[1].to_scripts) > 1
-        asyncio.run(test_transliteration_multiple_scripts_async(**kwargs))
+        assert len(transliterations.scripts) > 1
+        assert len(transliterations.scripts[0].to_scripts) > 1
+        assert len(transliterations.scripts[1].to_scripts) > 1        
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_dictionary_scope(self, **kwargs):    
-        async def test_dictionary_scope_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            client = self.create_async_getlanguage_client(endpoint)
+    @recorded_by_proxy_async
+    async def test_dictionary_scope(self, **kwargs):            
+        endpoint = kwargs.get("text_translation_endpoint")
+        client = self.create_async_getlanguage_client(endpoint)
+        async with client:
             response = await client.get_languages(scope="dictionary")
 
-            assert len(response.dictionary) > 0
-            dictionaries = response.dictionary["de"]
-            assert dictionaries.name is not None
-            assert dictionaries.native_name is not None
+        assert len(response.dictionary) > 0
+        dictionaries = response.dictionary["de"]
+        assert dictionaries.name is not None
+        assert dictionaries.native_name is not None
 
-            assert len(dictionaries.translations) > 0
-            assert dictionaries.translations[0].code is not None
-            assert dictionaries.translations[0].dir is not None
-            assert dictionaries.translations[0].name is not None
-            assert dictionaries.translations[0].native_name is not None
-        asyncio.run(test_dictionary_scope_async(**kwargs))
+        assert len(dictionaries.translations) > 0
+        assert dictionaries.translations[0].code is not None
+        assert dictionaries.translations[0].dir is not None
+        assert dictionaries.translations[0].name is not None
+        assert dictionaries.translations[0].native_name is not None        
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_dictionary_multiple_translations(self, **kwargs):
-        async def test_dictionary_multiple_translations_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            client = self.create_async_getlanguage_client(endpoint)
+    @recorded_by_proxy_async
+    async def test_dictionary_multiple_translations(self, **kwargs):        
+        endpoint = kwargs.get("text_translation_endpoint")
+        client = self.create_async_getlanguage_client(endpoint)
+        async with client:
             response = await client.get_languages(scope="dictionary")
 
-            assert len(response.dictionary) > 0
-            dictionaries = response.dictionary["en"]
-            assert dictionaries.name is not None
-            assert dictionaries.native_name is not None
+        assert len(response.dictionary) > 0
+        dictionaries = response.dictionary["en"]
+        assert dictionaries.name is not None
+        assert dictionaries.native_name is not None
 
-            assert len(dictionaries.translations) > 1
-            assert dictionaries.translations[0].code is not None
-            assert dictionaries.translations[0].dir is not None
-            assert dictionaries.translations[0].name is not None
-            assert dictionaries.translations[0].native_name is not None
-        asyncio.run(test_dictionary_multiple_translations_async(**kwargs))
+        assert len(dictionaries.translations) > 1
+        assert dictionaries.translations[0].code is not None
+        assert dictionaries.translations[0].dir is not None
+        assert dictionaries.translations[0].name is not None
+        assert dictionaries.translations[0].native_name is not None        
 
     @TextTranslationPreparer()
-    @recorded_by_proxy
-    def test_with_culture(self, **kwargs):
-        async def test_with_culture_async(**kwargs):
-            endpoint = kwargs.get("text_translation_endpoint")
-            client = self.create_async_getlanguage_client(endpoint)
+    @recorded_by_proxy_async
+    async def test_with_culture(self, **kwargs):        
+        endpoint = kwargs.get("text_translation_endpoint")
+        client = self.create_async_getlanguage_client(endpoint)
+        async with client:
             response = await client.get_languages(accept_language="es")
 
-            assert len(response.translation.items()) > 0
-            assert len(response.transliteration.items()) > 0
-            assert len(response.dictionary.items()) > 0
-            translations = response.translation["en"]
-            assert translations.dir is not None
-            assert translations.name is not None
-            assert translations.native_name is not None
-        asyncio.run(test_with_culture_async(**kwargs))
+        assert len(response.translation.items()) > 0
+        assert len(response.transliteration.items()) > 0
+        assert len(response.dictionary.items()) > 0
+        translations = response.translation["en"]
+        assert translations.dir is not None
+        assert translations.name is not None
+        assert translations.native_name is not None        
