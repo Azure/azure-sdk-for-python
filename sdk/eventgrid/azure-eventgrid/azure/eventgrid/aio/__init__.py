53fb34b007690842da43468783f34f0659b29e1a
l0lawrence <llawrence@microsoft.com>
2023-10-04 16:07:57 -0700

    update mypy

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/__init__.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/__init__.py
@@ -6,7 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._client import EventGridClient
+from ._patch import EventGridClient
 
 try:
     from ._patch import __all__ as _patch_all
