c74420c86d899fa3c7814d370d16a3cba3ce8619
l0lawrence <llawrence@microsoft.com>
2023-10-05 08:49:16 -0700

    remove patched EG - no longer needed

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/__init__.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/__init__.py
@@ -6,7 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._patch import EventGridClient
+from ._client import EventGridClient
 
 try:
     from ._patch import __all__ as _patch_all
