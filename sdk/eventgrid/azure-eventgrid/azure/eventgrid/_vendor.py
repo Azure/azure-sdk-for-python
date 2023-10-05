2422407d4f8135b7a98e704d63bda40b369c6ea1
Libba Lawrence <llawrence@microsoft.com>
2023-10-05 13:08:42 -0700

    [EGv2] generate with newer emitter (#31962)
    * [EGv2] Build Release (#30325)
    
    * move old sdk under legacy
    
    * gen typespec code
    
    * naming changes from archboard
    
    * samples
    
    * update patch naming
    
    * update imports with new gen
    
    * update samples
    
    * update client naming on aio
    
    * update receive op
    
    * update async to close client
    
    * update receive()
    
    * update gen code
    
    * moving around samples
    
    * updating samples
    
    * update samples
    
    * update patch and samples
    
    * patch internalmodels
    
    * spacing
    
    * updating model patch
    
    * update patch models
    
    * add both models back
    
    * update docstring
    
    * update docs
    
    * updating patch for receive
    
    * old EG models
    
    * add reject samples
    
    * patch
    
    * update format
    
    * update patch
    
    * eventgrid_client exceptions
    
    * update test imports
    
    * update total sample
    
    * receive patch fix
    
    * add in more tests
    
    * update test file
    
    * remove locktoken model
    
    * remove LockToken in patch
    
    * remove event delivery delay
    
    * eg client exceptions
    
    * .8.5 generation, and deliveryCount
    
    * rename sample
    
    * update version for beta
    
    * changelog
    
    * updating for gen
    
    * regen
    
    * generate via commit
    
    * publish result
    
    * fix docstring
    
    * publish docstring
    
    * return type
    
    * publish result
    
    * return publish result -- is none
    
    * format
    
    * update Publish result model
    
    * deliverycount patch
    
    * update from main
    
    * add copyright
    
    * added to readme
    
    * remove from readme
    
    * force publish_result response
    
    * update patch tp unindent
    
    * cspell
    
    * update mypy.ini
    
    * import order
    
    * mark livetest
    
    * update operations init
    
    * rename async
    
    * mypy
    
    * ignore mypy
    
    * pylint
    
    * pylint
    
    * ignore pylint for now to avoid gen code errors
    
    * ignore samples until ARM setup
    
    * update patches
    
    * remove publish result
    
    * remove PublishResult
    
    * remove publishresult
    
    * comma
    
    Co-authored-by: swathipil <76007337+swathipil@users.noreply.github.com>
    
    * update publishResult
    
    * change to .value
    
    * gen code " to '
    
    * remove comment
    
    * ran black
    
    * update changelog
    
    * update sample readme
    
    * gen code without query name
    
    * gen code
    
    * update tsp commit
    
    * remove publishresult
    
    * readme disclaimer
    
    * update changelog
    
    ---------
    
    Co-authored-by: swathipil <76007337+swathipil@users.noreply.github.com>
    
    * generate with newer emitter
    
    * update tsp
    
    * regen
    
    * update tests
    
    * update tspconfig
    
    * cspell
    
    * version
    
    * update serialization
    
    * update assets
    
    * update mypy
    
    ---------
    
    Co-authored-by: swathipil <76007337+swathipil@users.noreply.github.com>

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/_vendor.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/_vendor.py
@@ -6,7 +6,7 @@
 # --------------------------------------------------------------------------
 
 from abc import ABC
-from typing import List, TYPE_CHECKING, cast
+from typing import TYPE_CHECKING
 
 from ._configuration import EventGridClientConfiguration
 
@@ -16,23 +16,12 @@ if TYPE_CHECKING:
 
     from ._serialization import Deserializer, Serializer
 
-
-def _format_url_section(template, **kwargs):
-    components = template.split("/")
-    while components:
-        try:
-            return template.format(**kwargs)
-        except KeyError as key:
-            # Need the cast, as for some reasons "split" is typed as list[str | Any]
-            formatted_components = cast(List[str], template.split("/"))
-            components = [
-                c for c in formatted_components if "{}".format(key.args[0]) not in c
-            ]
-            template = "/".join(components)
-
-class EventGridClientMixinABC(ABC):
+class EventGridClientMixinABC(  
+    ABC
+):
     """DO NOT use this class. It is for internal typing use only."""
     _client: "PipelineClient"
     _config: EventGridClientConfiguration
     _serialize: "Serializer"
     _deserialize: "Deserializer"
+
