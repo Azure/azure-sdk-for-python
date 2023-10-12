92df6758d2c21d036e624fde7e3a3dea6130aa1f
l0lawrence <llawrence@microsoft.com>
2023-10-12 14:47:26 -0700

    update test

--- sdk/eventgrid/azure-eventgrid/tests/unittests/test_binary_mode.py
+++ sdk/eventgrid/azure-eventgrid/tests/unittests/test_binary_mode.py
@@ -4,9 +4,10 @@
 # license information.
 # --------------------------------------------------------------------------
 import pytest
+import json
 import base64
 from azure.eventgrid._operations._patch import _to_http_request
-from azure.eventgrid._serialization import Deserializer
+from azure.eventgrid._model_base import AzureJSONEncoder
 from azure.eventgrid.models import *
 from azure.core.messaging import CloudEvent
 
@@ -23,9 +24,10 @@ class TestEGClientExceptions():
 
         request = _to_http_request("https://eg-topic.westus2-1.eventgrid.azure.net/api/events", event=event)
 
-        deserialize = Deserializer()
-        assert deserialize('str', request.data) == base64.b64encode(b'this is binary data')
+        assert request.data == json.dumps(base64.b64encode(event.data), cls=AzureJSONEncoder, exclude_readonly=True)
         assert request.headers.get("ce-source") == "source"
+        assert request.headers.get("ce-subject") == "MySubject"
+        assert request.headers.get("ce-type") == "Contoso.Items.ItemReceived"
 
         
 
