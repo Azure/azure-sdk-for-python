1125bab611ae46450e62527e745da95a9881ab51
Rajarshi Sarkar <73562869+sarkar-rajarshi@users.noreply.github.com>
2023-10-17 10:41:56 -0700

    [ACS JobRouter] Dpg models (#32532)
    * python dpg models
    
    * fix tests
    
    * dpg models with tests
    
    * minor fix
    
    * minor fixes
    
    * fix lint error
    
    * pylint error fixes

--- sdk/communication/azure-communication-jobrouter/samples/distribution_policy_crud_ops.py
+++ sdk/communication/azure-communication-jobrouter/samples/distribution_policy_crud_ops.py
@@ -37,18 +37,15 @@ class DistributionPolicySamples(object):
         )
 
         # set `connection_string` to an existing ACS endpoint
-        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str = connection_string)
+        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str=connection_string)
         print("JobRouterAdministrationClient created successfully!")
 
         distribution_policy: DistributionPolicy = router_admin_client.create_distribution_policy(
-            distribution_policy_id = policy_id,
-            distribution_policy = DistributionPolicy(
-                offer_expires_after_seconds = 1 * 60,
-                mode = LongestIdleMode(
-                    min_concurrent_offers = 1,
-                    max_concurrent_offers = 1
-                )
-            )
+            distribution_policy_id=policy_id,
+            distribution_policy=DistributionPolicy(
+                offer_expires_after_seconds=1 * 60,
+                mode=LongestIdleMode(min_concurrent_offers=1, max_concurrent_offers=1),
+            ),
         )
 
         print(f"Distribution Policy successfully created with id: {distribution_policy.id}")
@@ -66,15 +63,11 @@ class DistributionPolicySamples(object):
         )
 
         # set `connection_string` to an existing ACS endpoint
-        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str = connection_string)
+        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str=connection_string)
         print("JobRouterAdministrationClient created successfully!")
 
         updated_distribution_policy: DistributionPolicy = router_admin_client.update_distribution_policy(
-            distribution_policy_id = policy_id,
-            mode = RoundRobinMode(
-                min_concurrent_offers = 1,
-                max_concurrent_offers = 1
-            )
+            distribution_policy_id=policy_id, mode=RoundRobinMode(min_concurrent_offers=1, max_concurrent_offers=1)
         )
 
         print(f"Distribution policy successfully update with new distribution mode")
@@ -86,9 +79,9 @@ class DistributionPolicySamples(object):
         # [START get_distribution_policy]
         from azure.communication.jobrouter import JobRouterAdministrationClient
 
-        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str = connection_string)
+        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str=connection_string)
 
-        distribution_policy = router_admin_client.get_distribution_policy(distribution_policy_id = policy_id)
+        distribution_policy = router_admin_client.get_distribution_policy(distribution_policy_id=policy_id)
 
         print(f"Successfully fetched distribution policy with id: {distribution_policy.id}")
         # [END get_distribution_policy]
@@ -98,7 +91,7 @@ class DistributionPolicySamples(object):
         # [START list_distribution_policies]
         from azure.communication.jobrouter import JobRouterAdministrationClient
 
-        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str = connection_string)
+        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str=connection_string)
 
         distribution_policy_iterator = router_admin_client.list_distribution_policies()
 
@@ -113,9 +106,9 @@ class DistributionPolicySamples(object):
         # [START list_distribution_policies_batched]
         from azure.communication.jobrouter import JobRouterAdministrationClient
 
-        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str = connection_string)
+        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str=connection_string)
 
-        distribution_policy_iterator = router_admin_client.list_distribution_policies(results_per_page = 10)
+        distribution_policy_iterator = router_admin_client.list_distribution_policies(results_per_page=10)
 
         for policy_page in distribution_policy_iterator.by_page():
             policies_in_page = list(policy_page)
@@ -134,14 +127,14 @@ class DistributionPolicySamples(object):
         # [START delete_distribution_policy]
         from azure.communication.jobrouter import JobRouterAdministrationClient
 
-        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str = connection_string)
+        router_admin_client = JobRouterAdministrationClient.from_connection_string(conn_str=connection_string)
 
-        router_admin_client.delete_distribution_policy(distribution_policy_id = policy_id)
+        router_admin_client.delete_distribution_policy(distribution_policy_id=policy_id)
 
         # [END delete_distribution_policy]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sample = DistributionPolicySamples()
     sample.create_distribution_policy()
     sample.update_distribution_policy()
--- sdk/communication/azure-communication-jobrouter/tests/test_model_router_job.py
+++ sdk/communication/azure-communication-jobrouter/tests/test_model_router_job.py
@@ -11,21 +11,15 @@ from azure.communication.jobrouter import RouterJob
 
 
 class TestRouterJob(object):
-
     def test_job_router_accepts_notes_with_datetime_and_str(self):
 
         current_timestamp = datetime.utcnow()
         current_timestamp_as_str = _datetime_as_isostr(current_timestamp)
         second_timestamp = "2022-05-13T16:59:04.531199Z"
 
-        notes = {
-            second_timestamp: "Fake notes 1",
-            current_timestamp: "Fake notes 2"
-        }
+        notes = {second_timestamp: "Fake notes 1", current_timestamp: "Fake notes 2"}
 
-        router_job: RouterJob = RouterJob(
-            notes = notes
-        )
+        router_job: RouterJob = RouterJob(notes=notes)
 
         assert (second_timestamp in router_job.notes) is True
         assert (current_timestamp_as_str in router_job.notes) is True
@@ -33,5 +27,5 @@ class TestRouterJob(object):
         assert router_job is not None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
