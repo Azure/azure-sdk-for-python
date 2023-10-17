e83ac708acad15c954b922db2b69da40cc1ac1b7
l0lawrence <llawrence@microsoft.com>
2023-10-17 13:37:43 -0700

    add Todo and start adding more tests

--- sdk/eventgrid/azure-eventgrid/tests/unittests/test_binary_mode.py
+++ sdk/eventgrid/azure-eventgrid/tests/unittests/test_binary_mode.py
@@ -11,7 +11,12 @@ from azure.eventgrid._model_base import AzureJSONEncoder
 from azure.eventgrid.models import *
 from azure.core.messaging import CloudEvent
 
-
+class MyTestClass(object):
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return self.name
+    
 class TestEGClientExceptions():
     
     def test_binary_request_format(self):
@@ -29,6 +34,58 @@ class TestEGClientExceptions():
         assert request.headers.get("ce-subject") == "MySubject"
         assert request.headers.get("ce-type") == "Contoso.Items.ItemReceived"
 
-        
+    
+    def test_binary_request_format_with_extensions(self):
+        event = CloudEvent(
+            type="Contoso.Items.ItemReceived",
+            source="source",
+            subject="MySubject",
+            data=b'this is binary data',
+            extensions={"extension1": "value1", "extension2": "value2"}
+        )
 
+        request = _to_http_request("https://eg-topic.westus2-1.eventgrid.azure.net/api/events", event=event)
+
+        assert request.data == json.dumps(base64.b64encode(event.data), cls=AzureJSONEncoder, exclude_readonly=True)
+        assert request.headers.get("ce-source") == "source"
+        assert request.headers.get("ce-subject") == "MySubject"
+        assert request.headers.get("ce-type") == "Contoso.Items.ItemReceived"
+        assert request.headers.get("ce-extension1") == "value1"
+
+    def test_binary_request_format_with_extensions_and_datacontenttype(self):
+        event = CloudEvent(
+            type="Contoso.Items.ItemReceived",
+            source="source",
+            subject="MySubject",
+            data='this is my data',
+            datacontenttype="application/json",
+            extensions={"extension1": "value1", "extension2": "value2"}
+        )
+
+        request = _to_http_request("https://eg-topic.westus2-1.eventgrid.azure.net/api/events", event=event)
+
+        assert request.data == json.dumps(base64.b64encode(event.data), cls=AzureJSONEncoder, exclude_readonly=True)
+        assert request.headers.get("ce-source") == "source"
+        assert request.headers.get("ce-subject") == "MySubject"
+        assert request.headers.get("ce-type") == "Contoso.Items.ItemReceived"
+        assert request.headers.get("ce-extension1") == "value1"
+
+    def test_complex_binary_request_format(self):
+        test_class = MyTestClass("test")
+        event = CloudEvent(
+            type="Contoso.Items.ItemReceived",
+            source="source",
+            subject="MySubject",
+            data=test_class,
+            datacontenttype="application/json",
+            extensions={"extension1": "value1", "extension2": "value2"}
+        )
+
+        request = _to_http_request("https://eg-topic.westus2-1.eventgrid.azure.net/api/events", event=event)
+
+        assert request.data == json.dumps(base64.b64encode(event.data), cls=AzureJSONEncoder, exclude_readonly=True)
+        assert request.headers.get("ce-source") == "source"
+        assert request.headers.get("ce-subject") == "MySubject"
+        assert request.headers.get("ce-type") == "Contoso.Items.ItemReceived"
+        assert request.headers.get("ce-extension1") == "value1"
         
