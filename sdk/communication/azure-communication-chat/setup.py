5e7243ad8eaf70d189264391de004ec32a4ec33f
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2023-10-17 16:44:48 -0700

    bump azure-core version and remove msrest from setup

--- sdk/communication/azure-communication-chat/setup.py
+++ sdk/communication/azure-communication-chat/setup.py
@@ -61,8 +61,7 @@ setup(
         'pytyped': ['py.typed'],
     },
     install_requires=[
-        "msrest>=0.7.1",
-        "azure-core<2.0.0,>=1.24.0",
+        "azure-core<2.0.0,>=1.28.0",
     ],
     extras_require={
         ":python_version<'3.8'": ["typing-extensions"]
