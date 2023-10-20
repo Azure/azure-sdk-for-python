6316b3fcb79f1684d47c8641d2f36c93835daa31
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-10-20 12:17:16 -0400

    remove logger thing for now

--- sdk/ai/azure-ai-generative/azure/ai/generative/__init__.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/__init__.py
@@ -2,17 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from ._version import VERSION
-
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-
-import logging
-
 from ._ai_client import AIClient
-from ._telemetry import initialize_logger_info
-
-module_logger = logging.getLogger(__name__)
-initialize_logger_info(module_logger, terminator="\n")
+from ._version import VERSION
 
 __all__ = [
     "AIClient",
