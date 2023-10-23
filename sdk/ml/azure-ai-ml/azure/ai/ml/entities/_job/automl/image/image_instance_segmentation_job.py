ef48096bd3d89208b674d3dc53c81f75928ca288
nvijayrania <107195344+nvijayrania@users.noreply.github.com>
2023-10-23 12:31:37 +0530

    Update sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/automl/image/image_instance_segmentation_job.py
    Co-authored-by: Rupal jain <rupaljain@microsoft.com>

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/automl/image/image_instance_segmentation_job.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/automl/image/image_instance_segmentation_job.py
@@ -28,7 +28,7 @@ class ImageInstanceSegmentationJob(AutoMLImageObjectDetectionBase):
 
     :keyword primary_metric: The primary metric to use for optimization.
     :paramtype primary_metric: Optional[str, azure.ai.ml.automl.InstanceSegmentationPrimaryMetrics]
-    :keyword kwargs: Job-specific arguments
+    :keyword kwargs: Job-specific arguments.
     :paramtype kwargs: Dict[str, Any]
 
     .. admonition:: Example:
