0205427e01e1e5952dee16096dc075046fb00ae4
nvijayrania <107195344+nvijayrania@users.noreply.github.com>
2023-10-23 12:30:40 +0530

    Update sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/automl/image/image_classification_multilabel_job.py
    Co-authored-by: Rupal jain <rupaljain@microsoft.com>

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/automl/image/image_classification_multilabel_job.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/automl/image/image_classification_multilabel_job.py
@@ -27,7 +27,7 @@ from azure.ai.ml.entities._util import load_from_dict
 class ImageClassificationMultilabelJob(AutoMLImageClassificationBase):
     """Configuration for AutoML multi-label Image Classification job.
 
-    :param primary_metric: The primary metric to use for optimization
+    :param primary_metric: The primary metric to use for optimization.
     :type primary_metric: Optional[str, ~azure.ai.ml.automl.ClassificationMultilabelPrimaryMetrics]
     :param kwargs: Job-specific arguments
     :type kwargs: Dict[str, Any]
