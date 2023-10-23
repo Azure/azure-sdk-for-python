ea5d66d23e3e4105e2632fba9aaceaa003b84880
nvijayrania <107195344+nvijayrania@users.noreply.github.com>
2023-10-23 12:31:46 +0530

    Update sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/automl/image/image_object_detection_job.py
    Co-authored-by: Rupal jain <rupaljain@microsoft.com>

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/automl/image/image_object_detection_job.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_job/automl/image/image_object_detection_job.py
@@ -24,7 +24,7 @@ from azure.ai.ml.entities._util import load_from_dict
 class ImageObjectDetectionJob(AutoMLImageObjectDetectionBase):
     """Configuration for AutoML Image Object Detection job.
 
-    :keyword primary_metric: The primary metric to use for optimization
+    :keyword primary_metric: The primary metric to use for optimization.
     :paramtype primary_metric: Optional[str, azure.ai.ml.ObjectDetectionPrimaryMetrics]
     :keyword kwargs: Job-specific arguments
     :paramtype kwargs: Dict[str, Any]
