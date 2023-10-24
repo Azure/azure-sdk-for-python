03ad628f7a209e3adbbd86614ea9bdc2be3d3d82
Ankit Singhal <30610298+singankit@users.noreply.github.com>
2023-10-24 10:23:44 -0700

    Evalute support task_type evaluate (#32667)
    * Evalute support task_type evaluate
    
    * Fixing constants import
    
    * Update sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_metric_handler.py
    
    Co-authored-by: Hanchi Wang <luigiking307@gmail.com>
    
    ---------
    
    Co-authored-by: Hanchi Wang <luigiking307@gmail.com>

--- sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_constants.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_constants.py
@@ -4,6 +4,9 @@
 
 from azureml.metrics import constants
 
+CHAT = "chat"
+
 TYPE_TO_KWARGS_MAPPING = {
-    constants.QUESTION_ANSWERING: ["questions", "contexts", "y_pred", "y_test"]
+    constants.QUESTION_ANSWERING: ["questions", "contexts", "y_pred", "y_test"],
+    CHAT: ["y_pred"]
 }
