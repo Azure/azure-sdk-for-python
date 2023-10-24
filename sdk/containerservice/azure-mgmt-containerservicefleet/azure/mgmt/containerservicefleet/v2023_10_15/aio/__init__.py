efa1bda3891d367cb2783c340a6cce2f968ad1bf
azure-sdk <PythonSdkPipelines>
2023-10-24 02:40:22 +0000

    code and test

--- sdk/containerservice/azure-mgmt-containerservicefleet/azure/mgmt/containerservicefleet/v2023_10_15/aio/__init__.py
+++ sdk/containerservice/azure-mgmt-containerservicefleet/azure/mgmt/containerservicefleet/v2023_10_15/aio/__init__.py
@@ -6,4 +6,18 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-VERSION = "1.0.0"
+from ._container_service_fleet_mgmt_client import ContainerServiceFleetMgmtClient
+
+try:
+    from ._patch import __all__ as _patch_all
+    from ._patch import *  # pylint: disable=unused-wildcard-import
+except ImportError:
+    _patch_all = []
+from ._patch import patch_sdk as _patch_sdk
+
+__all__ = [
+    "ContainerServiceFleetMgmtClient",
+]
+__all__.extend([p for p in _patch_all if p not in __all__])
+
+_patch_sdk()
