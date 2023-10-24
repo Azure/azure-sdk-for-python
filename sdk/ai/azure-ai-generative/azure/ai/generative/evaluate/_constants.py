1e4d2cc8262da984addc0f41202e0875b2956fa5
Ankit Singhal <anksing@microsoft.com>
2023-10-24 14:45:55 -0700

    Evaluate updated task types

--- sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_constants.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_constants.py
@@ -6,7 +6,15 @@ from azureml.metrics import constants
 
 CHAT = "chat"
 
+SUPPORTED_TASK_TYPE = ["qa", "qa-rag", "chat-rag"]
+
+SUPPORTED_TO_METRICS_TASK_TYPE_MAPPING = {
+    "qa": constants.QUESTION_ANSWERING,
+    "qa-rag": constants.RAG_EVALUATION,
+    "chat-rag": constants.RAG_EVALUATION,
+}
+
 TYPE_TO_KWARGS_MAPPING = {
     constants.QUESTION_ANSWERING: ["questions", "contexts", "y_pred", "y_test"],
-    CHAT: ["y_pred"]
+    constants.RAG_EVALUATION: ["y_pred"]
 }
