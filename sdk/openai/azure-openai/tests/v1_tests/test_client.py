01da3cdc2eaa80077f3e25f2a882e361ed799a93
Krista Pratico <krpratic@microsoft.com>
2023-10-24 13:17:13 -0700

    reorganize client tests

--- sdk/openai/azure-openai/tests/v1_tests/test_client.py
+++ sdk/openai/azure-openai/tests/v1_tests/test_client.py
@@ -11,12 +11,93 @@ from azure.identity import DefaultAzureCredential
 from conftest import (
     AZURE,
     ENV_AZURE_OPENAI_ENDPOINT,
+    ENV_AZURE_OPENAI_KEY,
     ENV_AZURE_OPENAI_API_VERSION,
+    ENV_AZURE_OPENAI_CHAT_COMPLETIONS_NAME,
     configure
 )
 
 
 class TestClient(AzureRecordedTestCase):
+    """Azure AD with token provider is missing here because it is tested per feature"""
+
+    @configure
+    @pytest.mark.parametrize("api_type", [AZURE])
+    def test_chat_completion_bad_deployment(self, client, azure_openai_creds, api_type, **kwargs):
+
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"}
+        ]
+
+        with pytest.raises(openai.NotFoundError) as e:
+            client.chat.completions.create(messages=messages, model="bad_deployment")
+        assert e.value.status_code == 404
+        assert "The API deployment for this resource does not exist" in e.value.message
+
+    @configure
+    @pytest.mark.parametrize("api_type", [AZURE])
+    def test_chat_completion_endpoint_deployment(self, client, azure_openai_creds, api_type, **kwargs):
+
+        client = openai.AzureOpenAI(
+            endpoint=os.getenv(ENV_AZURE_OPENAI_ENDPOINT),
+            deployment=ENV_AZURE_OPENAI_CHAT_COMPLETIONS_NAME,
+            api_key=os.getenv(ENV_AZURE_OPENAI_KEY),
+            api_version=ENV_AZURE_OPENAI_API_VERSION,
+        )
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"}
+        ]
+
+        completion = client.chat.completions.create(messages=messages, model="placeholder")
+        assert completion.id
+        assert completion.object == "chat.completion"
+        assert completion.model
+        assert completion.created
+        assert completion.usage.completion_tokens is not None
+        assert completion.usage.prompt_tokens is not None
+        assert completion.usage.total_tokens == completion.usage.completion_tokens + completion.usage.prompt_tokens
+        assert len(completion.choices) == 1
+        assert completion.choices[0].finish_reason
+        assert completion.choices[0].index is not None
+        assert completion.choices[0].message.content
+        assert completion.choices[0].message.role
+
+        # try to call some other feature not under the same deployment name
+        with pytest.raises(openai.BadRequestError) as e:
+            client.embeddings.create(input=["Hello world!"], model="placeholder")
+        assert e.value.status_code == 400
+        assert "The embeddings operation does not work with the specified model, " \
+        f"{ENV_AZURE_OPENAI_CHAT_COMPLETIONS_NAME}. Please choose different model and try again" in e.value.message 
+
+    @configure
+    @pytest.mark.parametrize("api_type", [AZURE])
+    def test_chat_completion_base_url(self, client, azure_openai_creds, api_type, **kwargs):
+
+        client = openai.AzureOpenAI(
+            base_url=f"{os.getenv(ENV_AZURE_OPENAI_ENDPOINT)}/openai/deployments/{ENV_AZURE_OPENAI_CHAT_COMPLETIONS_NAME}",
+            api_key=os.getenv(ENV_AZURE_OPENAI_KEY),
+            api_version=ENV_AZURE_OPENAI_API_VERSION,
+        )
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"}
+        ]
+
+        completion = client.chat.completions.create(messages=messages, **kwargs)
+        assert completion.id
+        assert completion.object == "chat.completion"
+        assert completion.model
+        assert completion.created
+        assert completion.usage.completion_tokens is not None
+        assert completion.usage.prompt_tokens is not None
+        assert completion.usage.total_tokens == completion.usage.completion_tokens + completion.usage.prompt_tokens
+        assert len(completion.choices) == 1
+        assert completion.choices[0].finish_reason
+        assert completion.choices[0].index is not None
+        assert completion.choices[0].message.content
+        assert completion.choices[0].message.role
 
     @configure
     @pytest.mark.parametrize("api_type", [AZURE])
--- sdk/openai/azure-openai/tests/v1_tests/test_client_async.py
+++ sdk/openai/azure-openai/tests/v1_tests/test_client_async.py
@@ -11,12 +11,96 @@ from azure.identity.aio import DefaultAzureCredential
 from conftest import (
     AZURE,
     ENV_AZURE_OPENAI_ENDPOINT,
+    ENV_AZURE_OPENAI_KEY,
     ENV_AZURE_OPENAI_API_VERSION,
+    ENV_AZURE_OPENAI_CHAT_COMPLETIONS_NAME,
     configure_async
 )
 
 
 class TestClientAsync(AzureRecordedTestCase):
+    """Azure AD with token provider is missing here because it is tested per feature"""
+
+    @configure_async
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("api_type", [AZURE])
+    async def test_chat_completion_bad_deployment(self, client_async, azure_openai_creds, api_type, **kwargs):
+
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"}
+        ]
+
+        with pytest.raises(openai.NotFoundError) as e:
+            await client_async.chat.completions.create(messages=messages, model="bad_deployment")
+        assert e.value.status_code == 404
+        assert "The API deployment for this resource does not exist" in e.value.message
+
+    @configure_async
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("api_type", [AZURE])
+    async def test_chat_completion_endpoint_deployment(self, client_async, azure_openai_creds, api_type, **kwargs):
+
+        client = openai.AsyncAzureOpenAI(
+            endpoint=os.getenv(ENV_AZURE_OPENAI_ENDPOINT),
+            deployment=ENV_AZURE_OPENAI_CHAT_COMPLETIONS_NAME,
+            api_key=os.getenv(ENV_AZURE_OPENAI_KEY),
+            api_version=ENV_AZURE_OPENAI_API_VERSION,
+        )
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"}
+        ]
+
+        completion = await client.chat.completions.create(messages=messages, model="placeholder")
+        assert completion.id
+        assert completion.object == "chat.completion"
+        assert completion.model
+        assert completion.created
+        assert completion.usage.completion_tokens is not None
+        assert completion.usage.prompt_tokens is not None
+        assert completion.usage.total_tokens == completion.usage.completion_tokens + completion.usage.prompt_tokens
+        assert len(completion.choices) == 1
+        assert completion.choices[0].finish_reason
+        assert completion.choices[0].index is not None
+        assert completion.choices[0].message.content
+        assert completion.choices[0].message.role
+
+        # try to call some other feature not under the same deployment name
+        with pytest.raises(openai.BadRequestError) as e:
+            await client.embeddings.create(input=["Hello world!"], model="placeholder")
+        assert e.value.status_code == 400
+        assert "The embeddings operation does not work with the specified model, " \
+        f"{ENV_AZURE_OPENAI_CHAT_COMPLETIONS_NAME}. Please choose different model and try again" in e.value.message
+
+    @configure_async
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("api_type", [AZURE])
+    async def test_chat_completion_base_url(self, client_async, azure_openai_creds, api_type, **kwargs):
+
+        client = openai.AsyncAzureOpenAI(
+            base_url=f"{os.getenv(ENV_AZURE_OPENAI_ENDPOINT)}/openai/deployments/{ENV_AZURE_OPENAI_CHAT_COMPLETIONS_NAME}",
+            api_key=os.getenv(ENV_AZURE_OPENAI_KEY),
+            api_version=ENV_AZURE_OPENAI_API_VERSION,
+        )
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"}
+        ]
+
+        completion = await client.chat.completions.create(messages=messages, **kwargs)
+        assert completion.id
+        assert completion.object == "chat.completion"
+        assert completion.model
+        assert completion.created
+        assert completion.usage.completion_tokens is not None
+        assert completion.usage.prompt_tokens is not None
+        assert completion.usage.total_tokens == completion.usage.completion_tokens + completion.usage.prompt_tokens
+        assert len(completion.choices) == 1
+        assert completion.choices[0].finish_reason
+        assert completion.choices[0].index is not None
+        assert completion.choices[0].message.content
+        assert completion.choices[0].message.role
 
     @configure_async
     @pytest.mark.asyncio
