e428b9e0be2e40c7d434bd831969b829fb86f793
Krista Pratico <krpratic@microsoft.com>
2023-10-24 15:05:26 -0700

    [content-safety] regenerate to fix next-pyright errors (#32513)
    * regenerate to pass next-pyright check
    
    * Packaging update of azure-ai-contentsafety
    
    * Revert "Packaging update of azure-ai-contentsafety"
    
    This reverts commit 7d329764e5c10b989476f6890e21fba2e0727dcd.
    
    * no packaging update
    
    * temp fix cspell, needs fix in generator
    
    * regen after more typing fixes in generator
    
    * revert changes in setup by generator
    
    ---------
    
    Co-authored-by: Azure SDK Bot <adxpysdk@microsoft.com>

--- sdk/contentsafety/azure-ai-contentsafety/setup.py
+++ sdk/contentsafety/azure-ai-contentsafety/setup.py
@@ -64,7 +64,7 @@ setup(
     },
     install_requires=[
         "isodate<1.0.0,>=0.6.1",
-        "azure-core<2.0.0,>=1.24.0",
+        "azure-core<2.0.0,>=1.28.0",
         "typing-extensions>=4.3.0; python_version<'3.8.0'",
     ],
     python_requires=">=3.7",
