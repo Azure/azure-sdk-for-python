6823a1aee28096bdf9ee8c397b72cd748482387b
QianqianNie <44004519+QianqianNie@users.noreply.github.com>
2023-10-24 23:37:49 -0700

    rename mlindex to index and data type to file/folder/table (#32722)

--- sdk/ai/azure-ai-generative/azure/ai/generative/index/__init__.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/index/__init__.py
@@ -1,22 +1,22 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
-"""MLIndex Creation and Operation Functions."""
+"""Index Creation and Operation Functions."""
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
-from ._build_mlindex import build_mlindex
+from ._build_mlindex import build_index
 from ._functions import (
-    get_langchain_embeddings_from_mlindex,
-    get_langchain_retriever_from_mlindex,
-    get_langchain_vectorstore_from_mlindex,
-    get_native_index_client_from_mlindex
+    get_langchain_embeddings_from_index,
+    get_langchain_retriever_from_index,
+    get_langchain_vectorstore_from_index,
+    get_native_index_client_from_index
 )
 
 __all__ = [
-    "build_mlindex",
-    "get_langchain_embeddings_from_mlindex",
-    "get_langchain_retriever_from_mlindex",
-    "get_langchain_vectorstore_from_mlindex",
-    "get_native_index_client_from_mlindex"
+    "build_index",
+    "get_langchain_embeddings_from_index",
+    "get_langchain_retriever_from_index",
+    "get_langchain_vectorstore_from_index",
+    "get_native_index_client_from_index"
 ]
