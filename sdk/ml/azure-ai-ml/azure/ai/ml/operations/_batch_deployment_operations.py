23200ae7f12029d942f2df332f8293ec8ef157aa
Taji Harris <tajiharris@microsoft.com>
2023-10-26 14:18:49 -0700

    black reformatting

--- sdk/ml/azure-ai-ml/azure/ai/ml/operations/_batch_deployment_operations.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/operations/_batch_deployment_operations.py
@@ -338,7 +338,7 @@ class BatchDeploymentOperations(_ScopeDependentOperations):
                     name=deployment.component.name, version=deployment.component.version
                 )
                 deployment.component = registered_component.id
-            except Exception as err: # pylint: disable=broad-except
+            except Exception as err:  # pylint: disable=broad-except
                 if isinstance(err, ResourceNotFoundError) or isinstance(err, HttpResponseError):
                     deployment.component = self._all_operations.all_operations[
                         AzureMLResourceType.COMPONENT
