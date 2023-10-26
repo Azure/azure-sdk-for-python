4300ec727b1fa512f81d4f353035007e677533ea
l0lawrence <llawrence@microsoft.com>
2023-10-26 13:54:21 -0700

    unittest transfer frame

--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_link.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_link.py
@@ -1,5 +1,6 @@
 from unittest.mock import Mock
 from azure.eventhub._pyamqp.link import Link
+from azure.eventhub._pyamqp.receiver import ReceiverLink
 from azure.eventhub._pyamqp.constants import LinkState
 import pytest
 
@@ -57,3 +58,26 @@ def test_link_should_not_detach(state):
     link._outgoing_detach = Mock(return_value=None)
     link.detach()
     link._outgoing_detach.assert_not_called()
+
+# Test to receive transfer frames from the server
+def test_receive_transfer_frame_multiple():
+    session = None
+    link = ReceiverLink(
+        session,
+        3,
+        source_address="test_source",
+        target_address="test_target",
+        network_trace=False,
+        network_trace_params={},
+    )
+
+    link.current_link_credit = 2 # Set the link credit to 2
+
+    # frame: handle, delivery_id, delivery_tag, messge_format, settled, more, rcv_settle_mode, state, resume, aborted, bathable, payload
+    transfer_frame_one = [3, 0, b'/blah', 0, None, True, None, None, None, False, b"test1"]
+    transfer_frame_two = [3, None, b'/blah', 0, None, False, None, None, None, False, b"test2"]
+
+    link._incoming_transfer(transfer_frame_one)
+    assert link.current_link_credit == 2
+    link._incoming_transfer(transfer_frame_two)
+    assert link.current_link_credit == 1
