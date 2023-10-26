1517da46b1f50f4ebe2d0fc1d19a9cc6f679feba
Krista Pratico <krpratic@microsoft.com>
2023-10-26 08:51:58 -0700

    get get_bearer_token_provider from azure-identity release

--- sdk/openai/azure-openai/tests/conftest.py
+++ sdk/openai/azure-openai/tests/conftest.py
@@ -14,74 +14,11 @@ import yarl
 import functools
 import openai
 from devtools_testutils.sanitizers import add_header_regex_sanitizer, add_oauth_response_sanitizer
-from azure.identity import DefaultAzureCredential
-from azure.identity.aio import DefaultAzureCredential as AsyncDefaultAzureCredential
-
-
-# ---- copy/pasted in for now ---------------------------------------------------------------------------
-from typing import Callable, Coroutine, Any
-
-from azure.core.credentials import TokenCredential
-from azure.core.pipeline.policies import BearerTokenCredentialPolicy
-from azure.core.pipeline import PipelineRequest, PipelineContext
-from azure.core.rest import HttpRequest
-
-from azure.core.credentials_async import AsyncTokenCredential
-from azure.core.pipeline.policies import AsyncBearerTokenCredentialPolicy
-
-
-def _make_request() -> PipelineRequest[HttpRequest]:
-    return PipelineRequest(HttpRequest("CredentialWrapper", "https://fakeurl"), PipelineContext(None))
-
-
-def get_bearer_token_provider(credential: TokenCredential, *scopes: str) -> Callable[[], str]:
-    """Returns a callable that provides a bearer token.
-    It can be used for instance to write code like:
-    .. code-block:: python
-        from azure.identity import DefaultAzureCredential, get_bearer_token_provider
-        credential = DefaultAzureCredential()
-        bearer_token_provider = get_bearer_token_provider(credential, "https://storage.azure.com/.default")
-        # Usage
-        request.headers["Authorization"] = "Bearer " + bearer_token_provider()
-    :param credential: The credential used to authenticate the request.
-    :type credential: ~azure.core.credentials.TokenCredential
-    :param str scopes: The scopes required for the bearer token.
-    :rtype: callable
-    :return: A callable that returns a bearer token.
-    """
-    policy = BearerTokenCredentialPolicy(credential, *scopes)
-    def wrapper() -> str:
-        request = _make_request()
-        policy.on_request(request)
-        return request.http_request.headers["Authorization"][len("Bearer ") :]
-
-    return wrapper
-
-
-
-def get_bearer_token_provider_async(credential: AsyncTokenCredential, *scopes: str) -> Callable[[], Coroutine[Any, Any, str]]:
-    """Returns a callable that provides a bearer token.
-    It can be used for instance to write code like:
-    .. code-block:: python
-        from azure.identity.aio import DefaultAzureCredential, get_bearer_token_provider
-        credential = DefaultAzureCredential()
-        bearer_token_provider = get_bearer_token_provider(credential, "https://storage.azure.com/.default")
-        # Usage
-        request.headers["Authorization"] = "Bearer " + await bearer_token_provider()
-    :param credential: The credential used to authenticate the request.
-    :type credential: ~azure.core.credentials.TokenCredential
-    :param str scopes: The scopes required for the bearer token.
-    :rtype: coroutine
-    :return: A coroutine that returns a bearer token.
-    """
-    policy = AsyncBearerTokenCredentialPolicy(credential, *scopes)
-    async def wrapper() -> str:
-        request = _make_request()
-        await policy.on_request(request)
-        return request.http_request.headers["Authorization"][len("Bearer ") :]
-
-    return wrapper
-# ---- copy/pasted in for now ---------------------------------------------------------------------------
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from azure.identity.aio import (
+    DefaultAzureCredential as AsyncDefaultAzureCredential,
+    get_bearer_token_provider as get_bearer_token_provider_async,
+)
 
 # controls whether we run tests against v0 or v1. Options: v0 or v1. Default: v1
 ENV_OPENAI_TEST_MODE = "OPENAI_TEST_MODE"
