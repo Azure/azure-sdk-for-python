25e29f280f59974e8b862e650bce1884b9b81dd7
prakharg <prakharg@microsoft.com>
2023-10-27 11:39:20 -0700

    add utils

--- sdk/ai/azure-ai-generative/tests/synthetic_qa/e2etests/utils.py
+++ sdk/ai/azure-ai-generative/tests/synthetic_qa/e2etests/utils.py
@@ -1,3 +1,7 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
 import logging
 import re
 import string
