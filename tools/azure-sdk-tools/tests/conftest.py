2d2822debdf4942784973c95742a84748db70264
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2023-10-31 13:19:42 -0700

    Update tools/azure-sdk-tools/tests/conftest.py
    Co-authored-by: kdestin <101366538+kdestin@users.noreply.github.com>

--- tools/azure-sdk-tools/tests/conftest.py
+++ tools/azure-sdk-tools/tests/conftest.py
@@ -5,21 +5,19 @@ import os
 from typing import List
 from tempfile import TemporaryDirectory
 
-def create_temp_directory(fake_creation_paths: List[str]) -> TemporaryDirectory:
-    tmp_dir = TemporaryDirectory()
-
-    for file in fake_creation_paths:
-        target_path = os.path.join(tmp_dir.name, file)
-        dirname = os.path.join(tmp_dir.name, os.path.dirname(file))
-
-        if not os.path.exists(dirname):
-            os.mkdir(dirname)
-
-        with open(target_path, "w"):
-            pass
-
-    return tmp_dir
-
 @pytest.fixture()
 def tmp_directory_create():
-    return create_temp_directory
+    with TemporaryDirectory() as tmp_dir:
+        def create_temp_directory(fake_creation_paths: List[str]) -> TemporaryDirectory:
+            for file in fake_creation_paths:
+                target_path = os.path.join(tmp_dir.name, file)
+                dirname = os.path.join(tmp_dir.name, os.path.dirname(file))
+
+                if not os.path.exists(dirname):
+                    os.mkdir(dirname)
+
+                with open(target_path, "w"):
+                    pass
+            return tmp_dir
+
+        yield create_temp_directory
