851d112e97d45269671301ea9fb8d212e5e17514
l0lawrence <llawrence@microsoft.com>
2023-10-31 20:06:30 -0700

    add samples

---
+++ sdk/eventgrid/azure-eventgrid/samples/sync_samples/eventgrid_client_samples/sample_publish_release_receive.py
@@ -0,0 +1,57 @@
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import os
+from azure.core.credentials import AzureKeyCredential
+from azure.eventgrid import EventGridClient
+from azure.eventgrid.models import *
+from azure.core.messaging import CloudEvent
+from azure.core.exceptions import HttpResponseError
+
+
+EVENTGRID_KEY: str = os.environ["EVENTGRID_KEY"]
+EVENTGRID_ENDPOINT: str = os.environ["EVENTGRID_ENDPOINT"]
+TOPIC_NAME: str = os.environ["EVENTGRID_TOPIC_NAME"]
+EVENT_SUBSCRIPTION_NAME: str = os.environ["EVENTGRID_EVENT_SUBSCRIPTION_NAME"]
+
+# Create a client
+client = EventGridClient(EVENTGRID_ENDPOINT, AzureKeyCredential(EVENTGRID_KEY))
+
+
+# Publish a CloudEvent
+try:
+    cloud_event = CloudEvent(data="hello", source="https://example.com", type="example")
+    client.publish_cloud_events(topic_name=TOPIC_NAME, body=cloud_event)
+
+    receive_result = client.receive_cloud_events(topic_name=TOPIC_NAME, event_subscription_name=EVENT_SUBSCRIPTION_NAME, max_events=10, max_wait_time=10)
+    lock_tokens_to_release = []
+    for item in receive_result.value:
+        lock_tokens_to_release.append(item.broker_properties.lock_token)
+
+
+    # Release a LockToken
+    release_token = ReleaseOptions(lock_tokens=lock_tokens_to_release)
+    release_events = client.release_cloud_events(
+        topic_name=TOPIC_NAME,
+        event_subscription_name=EVENT_SUBSCRIPTION_NAME,
+        release_delay_in_seconds=3600,
+        release_options=release_token,
+    )
+    print("Released Event:", release_events)
+
+
+    receive_result = client.receive_cloud_events(topic_name=TOPIC_NAME, event_subscription_name=EVENT_SUBSCRIPTION_NAME, max_events=10, max_wait_time=10)
+    print("Received events after release:", receive_result.value)
+
+    # Acknowledge a LockToken
+    acknowledge_token = AcknowledgeOptions(lock_tokens=lock_tokens_to_release)
+    acknowledge_events = client.acknowledge_cloud_events(
+        topic_name=TOPIC_NAME,
+        event_subscription_name=EVENT_SUBSCRIPTION_NAME,
+        acknowledge_options=acknowledge_token,
+    )
+    print("Acknowledged events after release:", acknowledge_events)
+except HttpResponseError:
+    raise
--- sdk/eventgrid/azure-eventgrid/samples/sync_samples/eventgrid_client_samples/sample_release_operation.py
+++ sdk/eventgrid/azure-eventgrid/samples/sync_samples/eventgrid_client_samples/sample_release_operation.py
@@ -11,8 +11,8 @@ from azure.core.exceptions import HttpResponseError
 
 EVENTGRID_KEY: str = os.environ["EVENTGRID_KEY"]
 EVENTGRID_ENDPOINT: str = os.environ["EVENTGRID_ENDPOINT"]
-TOPIC_NAME: str = os.environ["TOPIC_NAME"]
-EVENT_SUBSCRIPTION_NAME: str = os.environ["EVENT_SUBSCRIPTION_NAME"]
+TOPIC_NAME: str = os.environ["EVENTGRID_TOPIC_NAME"]
+EVENT_SUBSCRIPTION_NAME: str = os.environ["EVENTGRID_EVENT_SUBSCRIPTION_NAME"]
 
 # Create a client
 client = EventGridClient(EVENTGRID_ENDPOINT, AzureKeyCredential(EVENTGRID_KEY))
@@ -23,7 +23,7 @@ try:
     release_events = client.release_cloud_events(
         topic_name=TOPIC_NAME,
         event_subscription_name=EVENT_SUBSCRIPTION_NAME,
-        release_delay_in_seconds=10,
+        release_delay_in_seconds=3600,
         release_options=lock_tokens,
     )
     print(release_events)
