0455c789818c9f01ff4ac27d621dca1efa7eec05
Xiang Yan <xiangsjtu@gmail.com>
2023-11-02 10:15:44 -0700

    pylint next fixes for core

--- sdk/core/azure-core/azure/core/pipeline/transport/_base.py
+++ sdk/core/azure-core/azure/core/pipeline/transport/_base.py
@@ -55,6 +55,7 @@ from typing import (
 from http.client import HTTPResponse as _HTTPResponse
 
 from azure.core.exceptions import HttpResponseError
+from azure.core.pipeline.policies import SansIOHTTPPolicy
 from ...utils._utils import case_insensitive_dict
 from ...utils._pipeline_transport_rest_shared import (
     _format_parameters_helper,
@@ -81,7 +82,7 @@ _LOGGER = logging.getLogger(__name__)
 binary_type = str
 
 
-def _format_url_section(template, **kwargs):
+def _format_url_section(template, **kwargs: Dict[str, str]) -> str:
     """String format the template with the kwargs, auto-skip sections of the template that are NOT in the kwargs.
 
     By default in Python, "format" will raise a KeyError if a template element is not found. Here the section between
@@ -90,7 +91,6 @@ def _format_url_section(template, **kwargs):
     This is used for API like Storage, where when Swagger has template section not defined as parameter.
 
     :param str template: a string template to fill
-    :keyword dict[str,str] kwargs: Template values as string
     :rtype: str
     :returns: Template completed
     """
@@ -126,7 +126,7 @@ def _urljoin(base_url: str, stub_url: str) -> str:
     stub_url_path = split_url.pop(0)
     stub_url_query = split_url.pop() if split_url else None
 
-    # Note that _replace is a public API named that way to avoid to avoid conflicts in namedtuple
+    # Note that _replace is a public API named that way to avoid conflicts in namedtuple
     # https://docs.python.org/3/library/collections.html?highlight=namedtuple#collections.namedtuple
     parsed_base_url = parsed_base_url._replace(
         path=parsed_base_url.path.rstrip("/") + "/" + stub_url_path,
@@ -351,7 +351,9 @@ class HttpRequest:
         self.data = data
         self.files = None
 
-    def set_multipart_mixed(self, *requests: "HttpRequest", **kwargs: Any) -> None:
+    def set_multipart_mixed(
+        self, *requests: "HttpRequest", policies: List[SansIOHTTPPolicy] = None, boundary: str = None, **kwargs: Any
+    ) -> None:
         """Set the part of a multipart/mixed.
 
         Only supported args for now are HttpRequest objects.
@@ -368,10 +370,11 @@ class HttpRequest:
         :keyword list[SansIOHTTPPolicy] policies: SansIOPolicy to apply at preparation time
         :keyword str boundary: Optional boundary
         """
+        policies = policies or []
         self.multipart_mixed_info = (
             requests,
-            kwargs.pop("policies", []),
-            kwargs.pop("boundary", None),
+            policies,
+            boundary,
             kwargs,
         )
 
@@ -830,7 +833,14 @@ class PipelineClientBase:
         return request
 
     def options(
-        self, url: str, params: Optional[Dict[str, str]] = None, headers: Optional[Dict[str, str]] = None, **kwargs: Any
+        self,  # pylint: disable=unused-argument
+        url: str,
+        params: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+        *,
+        content: Optional[Union[bytes, str, Dict]] = None,
+        form_content: Optional[Dict] = None,
+        **kwargs: Any,
     ) -> HttpRequest:
         """Create a OPTIONS request object.
 
@@ -843,7 +853,5 @@ class PipelineClientBase:
         :return: An HttpRequest object
         :rtype: ~azure.core.pipeline.transport.HttpRequest
         """
-        content = kwargs.get("content")
-        form_content = kwargs.get("form_content")
         request = self._request("OPTIONS", url, params, headers, content, form_content, None)
         return request
