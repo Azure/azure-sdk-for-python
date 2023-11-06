a4c4e5c48ba71fa16cff05026dbab13e14c2a88a
Miles Holland <milesholland@microsoft.com>
2023-11-06 13:19:16 -0500

    revert conn subtypes

--- sdk/ai/azure-ai-resources/azure/ai/resources/entities/connection_subtypes.py
+++ sdk/ai/azure-ai-resources/azure/ai/resources/entities/connection_subtypes.py
@@ -1,7 +1,3 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
 
 from typing import Optional
 
@@ -27,7 +23,7 @@ class AzureOpenAIConnection(BaseConnection):
     :param credentials: The credentials for authenticating to the external resource.
     :type credentials: ~azure.ai.ml.entities.ApiKeyConfiguration
     :param api_version: The api version that this connection was created for.
-    :type api_version: Optional[str]
+    :type api_version: str
     :param api_type: The api type that this connection was created for. Defaults to "Azure" and currently rarely changes.
     :type api_type: str
     """
@@ -37,7 +33,7 @@ class AzureOpenAIConnection(BaseConnection):
         *,
         target: str,
         credentials: ApiKeyConfiguration,
-        api_version: Optional[str] = None,
+        api_version: str = "unset",
         api_type: str = "Azure",
         **kwargs,
     ):
@@ -137,7 +133,7 @@ class AzureAISearchConnection(BaseConnection):
     :param credentials: The credentials for authenticating to the external resource.
     :type credentials: ~azure.ai.ml.entities.ApiKeyConfiguration
     :param api_version: The api version that this connection was created for. Only applies to certain connection types.
-    :type api_version: Optional[str]
+    :type api_version: str
     """
 
     def __init__(
@@ -145,7 +141,7 @@ class AzureAISearchConnection(BaseConnection):
         *,
         target: str,
         credentials: ApiKeyConfiguration,
-        api_version: Optional[str] = None,
+        api_version: str = "unset",
         **kwargs,
     ):
         kwargs.pop("type", None)  # make sure we never somehow use wrong type
@@ -202,7 +198,7 @@ class AzureAIServiceConnection(BaseConnection):
     :param credentials: The credentials for authenticating to the external resource.
     :type credentials: ~azure.ai.ml.entities.ApiKeyConfiguration
     :param api_version: The api version that this connection was created for.
-    :type api_version: Optional[str]
+    :type api_version: str
     :param kind: The kind of ai service that this connection points to. Valid inputs include:
         "AzureOpenAI", "ContentSafety", and "Speech"
     :type kind: str
@@ -213,7 +209,7 @@ class AzureAIServiceConnection(BaseConnection):
         *,
         target: str,
         credentials: ApiKeyConfiguration,
-        api_version: Optional[str] = None,
+        api_version: str = "unset",
         kind: str,
         **kwargs,
     ):
