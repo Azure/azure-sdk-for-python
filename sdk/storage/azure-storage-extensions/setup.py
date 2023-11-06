1bc84c8d5bc01a11dee12341cde555e852253f66
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2023-11-06 10:47:28 -0800

    Revert "Packaging update of azure-storage-extensions"
    This reverts commit e63fb36b04b016220dba1606b8b4b57f8abe53b4.

--- sdk/storage/azure-storage-extensions/setup.py
+++ sdk/storage/azure-storage-extensions/setup.py
@@ -1,83 +1,44 @@
-#!/usr/bin/env python
-
 # -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
 
-import re
-import os.path
-from io import open
-from setuptools import find_packages, setup
+import os
+import glob
+from setuptools import setup, Extension
 
-# Change the PACKAGE_NAME only to change folder and different name
 PACKAGE_NAME = "azure-storage-extensions"
-PACKAGE_PPRINT_NAME = "MyService Management"
+PACKAGE_PPRINT_NAME = "Azure Storage Extensions"
 
-# a-b-c => a/b/c
-package_folder_path = PACKAGE_NAME.replace("-", "/")
-# a-b-c => a.b.c
-namespace_name = PACKAGE_NAME.replace("-", ".")
-
-# Version extraction inspired from 'requests'
-with open(
-    os.path.join(package_folder_path, "version.py")
-    if os.path.exists(os.path.join(package_folder_path, "version.py"))
-    else os.path.join(package_folder_path, "_version.py"),
-    "r",
-) as fd:
-    version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError("Cannot find version information")
-
-with open("README.md", encoding="utf-8") as f:
-    readme = f.read()
-with open("CHANGELOG.md", encoding="utf-8") as f:
-    changelog = f.read()
+package_folder_path = PACKAGE_NAME.replace('-', '/')
 
 setup(
     name=PACKAGE_NAME,
-    version=version,
-    description="Microsoft Azure {} Client Library for Python".format(PACKAGE_PPRINT_NAME),
-    long_description=readme + "\n\n" + changelog,
-    long_description_content_type="text/markdown",
-    license="MIT License",
-    author="Microsoft Corporation",
-    author_email="azpysdkhelp@microsoft.com",
-    url="https://github.com/Azure/azure-sdk-for-python",
-    keywords="azure, azure sdk",  # update with search keywords relevant to the azure service / product
+    version="1.0.0b1",
+    include_package_data=True,
+    description=PACKAGE_PPRINT_NAME,
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    author='Microsoft Corporation',
+    author_email='ascl@microsoft.com',
+    url='https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/storage/azure-storage-extensions',
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        'Programming Language :: Python',
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
     ],
     zip_safe=False,
-    packages=find_packages(
-        exclude=[
-            "tests",
-            # Exclude packages that will be covered by PEP420 or nspkg
-            "azure",
-            "azure.storage",
-        ]
-    ),
-    include_package_data=True,
-    package_data={
-        "pytyped": ["py.typed"],
-    },
-    install_requires=[
-        "isodate<1.0.0,>=0.6.1",
-        "azure-common~=1.1",
-        "azure-mgmt-core>=1.3.2,<2.0.0",
-        "typing-extensions>=4.3.0; python_version<'3.8.0'",
-    ],
     python_requires=">=3.7",
+    ext_package='azure.storage.extensions',
+    ext_modules=[
+        Extension('crc64', glob.glob(os.path.join(package_folder_path, 'crc64', '*.c')))
+    ],
 )
