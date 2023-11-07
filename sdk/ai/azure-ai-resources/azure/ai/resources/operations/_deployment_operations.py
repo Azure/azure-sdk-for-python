b9bd8046e23e0b2156c4d7615dbfc48ccf08eab1
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-11-06 19:27:03 -0500

    fix sku selection azureml registry

--- sdk/ai/azure-ai-resources/azure/ai/resources/operations/_deployment_operations.py
+++ sdk/ai/azure-ai-resources/azure/ai/resources/operations/_deployment_operations.py
@@ -160,6 +160,10 @@ class DeploymentOperations:
 
                 if "registries/azureml/" in model_details.id:
                     default_instance_type = model_details.properties["inference-recommended-sku"]
+                    allowed_instance_types = []
+                    if "," in default_instance_type:
+                        allowed_instance_types = model_details.properties["inference-recommended-sku"].split(",")
+                        default_instance_type = allowed_instance_types[0]
                     min_sku_spec = model_details.properties["inference-min-sku-spec"].split("|")
                     self._check_default_instance_type_and_populate(
                         default_instance_type, deployment, min_sku_spec=min_sku_spec
