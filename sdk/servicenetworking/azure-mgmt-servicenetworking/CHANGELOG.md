33f9584164201ba1f673cabac7c6b13839175ada
azure-sdk <PythonSdkPipelines>
2023-11-07 01:11:38 +0000

    code and test

--- sdk/servicenetworking/azure-mgmt-servicenetworking/CHANGELOG.md
+++ sdk/servicenetworking/azure-mgmt-servicenetworking/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release History
 
+## 0.0.0 (it should be stable) (2023-11-20)
+
+
+
 ## 1.0.0b2 (2023-05-20)
 
 ### Features Added
--- sdk/servicenetworking/azure-mgmt-servicenetworking/generated_samples/traffic_controllers_get_list.py
+++ sdk/servicenetworking/azure-mgmt-servicenetworking/generated_samples/traffic_controllers_get_list.py
@@ -34,6 +34,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/cadl/examples/TrafficControllersGetList.json
+# x-ms-original-file: specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/stable/2023-11-01/examples/TrafficControllersGetList.json
 if __name__ == "__main__":
     main()
