0c3a5b2eb059a8394761f0252c8693d5f3818d13
Kashif Khan <kashifkhan@microsoft.com>
2023-11-07 12:50:37 -0600

    modified to add any value

--- sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_encode.py
+++ sdk/eventhub/azure-eventhub/tests/pyamqp_tests/unittest/test_encode.py
@@ -7,14 +7,11 @@ from azure.eventhub._pyamqp._encode import encode_filter_set
     ({b'com.microsoft:session-filter': 'aba'}, 'aba'),
     ({b'com.microsoft:session-filter': 'ab'}, 'ab'),
     ({b'com.microsoft:session-filter': 'a'}, 'a'),
+    ({b'com.microsoft:session-filter': 1}, 1),
 ])
 def test_valid_filter_encode(value, expected):
     fields = encode_filter_set(value)
     assert len(fields) == 2
     assert fields['VALUE'][0][1] == expected
-
-def test_invalid_filter_encode():
-    value = {b'com.microsoft:session-filter': 1}
-    pytest.raises(TypeError, encode_filter_set, value)
 
 
