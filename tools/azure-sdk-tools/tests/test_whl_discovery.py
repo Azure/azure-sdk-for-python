7d6f74c11096cf3adbbf93a71a2be5359671698d
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2023-11-08 14:23:46 -0800

    resolve analyze spelling errors. add additional test case for find_sdist, update test case for find_whl. disable sdist env for azure-storage-extensions (which will never work)

--- tools/azure-sdk-tools/tests/test_whl_discovery.py
+++ tools/azure-sdk-tools/tests/test_whl_discovery.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 
 from tempfile import TemporaryDirectory
 
-from ci_tools.functions import find_whl
+from ci_tools.functions import find_whl, find_sdist
 
 
 integration_folder = os.path.join(os.path.dirname(__file__), "integration")
@@ -101,4 +101,52 @@ def test_find_whl_discovers_specific_wheels(test_patch, tmp_directory_create):
 
     test_patch.return_value = compatible_tags
     found = find_whl(tmp_dir, "azure-storage-extensions", "1.0.0b1")
+    assert isinstance(found, str)
+
+
+@patch("ci_tools.functions.get_interpreter_compatible_tags")
+def test_find_sdist_discovers_specific_sdist(test_patch, tmp_directory_create):
+    tmp_dir = tmp_directory_create(
+        [
+            "azure_storage_extensions-1.0.0b1-cp310-cp310-macosx_10_9_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl",
+            "azure_storage_extensions-1.0.0b1-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp310-cp310-win_amd64.whl",
+            "azure_storage_extensions-1.0.0b1-cp310-cp310-win32.whl",
+            "azure_storage_extensions-1.0.0b1-cp311-cp311-macosx_10_9_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp311-cp311-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl",
+            "azure_storage_extensions-1.0.0b1-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp311-cp311-win_amd64.whl",
+            "azure_storage_extensions-1.0.0b1-cp311-cp311-win32.whl",
+            "azure_storage_extensions-1.0.0b1-cp312-cp312-macosx_10_9_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp312-cp312-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl",
+            "azure_storage_extensions-1.0.0b1-cp312-cp312-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp312-cp312-win_amd64.whl",
+            "azure_storage_extensions-1.0.0b1-cp312-cp312-win32.whl",
+            "azure_storage_extensions-1.0.0b1-cp37-cp37m-macosx_10_9_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp37-cp37m-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl",
+            "azure_storage_extensions-1.0.0b1-cp37-cp37m-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp37-cp37m-win_amd64.whl",
+            "azure_storage_extensions-1.0.0b1-cp37-cp37m-win32.whl",
+            "azure_storage_extensions-1.0.0b1-cp38-cp38-macosx_10_9_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp38-cp38-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl",
+            "azure_storage_extensions-1.0.0b1-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp38-cp38-win_amd64.whl",
+            "azure_storage_extensions-1.0.0b1-cp38-cp38-win32.whl",
+            "azure_storage_extensions-1.0.0b1-cp39-cp39-linux_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp39-cp39-macosx_10_9_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp39-cp39-macosx_11_0_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp39-cp39-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl",
+            "azure_storage_extensions-1.0.0b1-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+            "azure_storage_extensions-1.0.0b1-cp39-cp39-win_amd64.whl",
+            "azure_storage_extensions-1.0.0b1-cp39-cp39-win32.whl",
+            "azure-storage-extensions-1.0.0b1.tar.gz"
+        ]
+    )
+
+    with open(os.path.join(tags_folder, 'from_WSL_310.txt'), 'r', encoding='utf-8') as f:
+        compatible_tags = [line.strip() for line in f.readlines()]
+
+    test_patch.return_value = compatible_tags
+    found = find_sdist(tmp_dir, "azure-storage-extensions", "1.0.0b1")
     assert isinstance(found, str)
