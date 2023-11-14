6a57993ac6e37451873a098c32b5ee6932a35a38
Rajarshi Sarkar <73562869+sarkar-rajarshi@users.noreply.github.com>
2023-11-13 20:42:09 -0800

    [ACS JobRouter] Enum discriminators for polymorphic base classes (#33149)
    * change discriminator type for polymorphic base classes + fix mypy issues
    
    * regen code with 'stream' kwarg removed
    
    * update assets.json
    
    * more mypy fixes
    - suppress mypy in samples
    
    * manually update generated operations to return the (Async)ItemPaged[TEntity]

--- sdk/communication/azure-communication-jobrouter/azure/communication/jobrouter/aio/_operations/__init__.py
+++ sdk/communication/azure-communication-jobrouter/azure/communication/jobrouter/aio/_operations/__init__.py
@@ -6,8 +6,8 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._operations import JobRouterAdministrationClientOperationsMixin
-from ._operations import JobRouterClientOperationsMixin
+from ._patch import JobRouterAdministrationClientOperationsMixin
+from ._patch import JobRouterClientOperationsMixin
 
 from ._patch import __all__ as _patch_all
 from ._patch import *  # pylint: disable=unused-wildcard-import
