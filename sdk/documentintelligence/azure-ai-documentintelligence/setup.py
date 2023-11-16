d86169e4b45231665300997cada32f09e6c7df71
YalinLi0312 <yall@microsoft.com>
2023-11-16 09:50:04 -0800

    Add python 3.12

--- sdk/documentintelligence/azure-ai-documentintelligence/setup.py
+++ sdk/documentintelligence/azure-ai-documentintelligence/setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
     ],
     zip_safe=False,
