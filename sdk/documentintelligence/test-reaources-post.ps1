d712108371318a504f5ed03e73468ada8dea7aed
YalinLi0312 <yall@microsoft.com>
2023-11-17 16:36:25 -0800

    Delete test-reaources-post.ps1

--- sdk/documentintelligence/test-reaources-post.ps1
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-# Current ask from service team to wait after creating resource
-
-Start-Sleep -s 600
