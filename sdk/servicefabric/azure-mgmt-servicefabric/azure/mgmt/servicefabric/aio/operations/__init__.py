e9c82550ae081896a47cb2e6ff1599d3b68006d4
azure-sdk <PythonSdkPipelines>
2023-11-21 06:12:32 +0000

    code and test

--- sdk/servicefabric/azure-mgmt-servicefabric/azure/mgmt/servicefabric/aio/operations/__init__.py
+++ sdk/servicefabric/azure-mgmt-servicefabric/azure/mgmt/servicefabric/aio/operations/__init__.py
@@ -15,7 +15,7 @@ from ._applications_operations import ApplicationsOperations
 from ._services_operations import ServicesOperations
 
 from ._patch import __all__ as _patch_all
-from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import *  # pylint: disable=unused-wildcard-import
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
