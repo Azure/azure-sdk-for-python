68a43c6ac48b3b7931a2e242688e84d22d9f5a55
azure-sdk <PythonSdkPipelines>
2023-11-29 01:14:52 +0000

    code and test

--- sdk/healthcareapis/azure-mgmt-healthcareapis/azure/mgmt/healthcareapis/aio/_healthcare_apis_management_client.py
+++ sdk/healthcareapis/azure-mgmt-healthcareapis/azure/mgmt/healthcareapis/aio/_healthcare_apis_management_client.py
@@ -73,11 +73,11 @@ class HealthcareApisManagementClient:  # pylint: disable=client-accepts-api-vers
     :vartype operation_results: azure.mgmt.healthcareapis.aio.operations.OperationResultsOperations
     :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
-    :param subscription_id: The subscription identifier. Required.
+    :param subscription_id: The ID of the target subscription. Required.
     :type subscription_id: str
     :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
-    :keyword api_version: Api Version. Default value is "2022-06-01". Note that overriding this
+    :keyword api_version: Api Version. Default value is "2023-11-01". Note that overriding this
      default value may result in unsupported behavior.
     :paramtype api_version: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
@@ -94,7 +94,7 @@ class HealthcareApisManagementClient:  # pylint: disable=client-accepts-api-vers
         self._config = HealthcareApisManagementClientConfiguration(
             credential=credential, subscription_id=subscription_id, **kwargs
         )
-        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client: AsyncARMPipelineClient = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -157,5 +157,5 @@ class HealthcareApisManagementClient:  # pylint: disable=client-accepts-api-vers
         await self._client.__aenter__()
         return self
 
-    async def __aexit__(self, *exc_details) -> None:
+    async def __aexit__(self, *exc_details: Any) -> None:
         await self._client.__aexit__(*exc_details)
--- sdk/healthcareapis/azure-mgmt-healthcareapis/generated_samples/operations_list.py
+++ sdk/healthcareapis/azure-mgmt-healthcareapis/generated_samples/operations_list.py
@@ -34,6 +34,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2022-06-01/examples/OperationsList.json
+# x-ms-original-file: specification/healthcareapis/resource-manager/Microsoft.HealthcareApis/stable/2023-11-01/examples/OperationsList.json
 if __name__ == "__main__":
     main()
