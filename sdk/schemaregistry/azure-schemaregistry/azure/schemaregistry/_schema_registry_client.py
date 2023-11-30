610b24fa5648881d3709d95c96b99320d427bad5
swathipil <76007337+swathipil@users.noreply.github.com>
2023-11-30 15:19:55 -0800

    [SchemaRegistry] verify types/type completeness (#33335)
    * try out typeddict for mypy
    
    * change casted dict name for spread operator use
    
    * avroencoder type complete
    
    * add back exception
    
    * remove unused import

--- sdk/schemaregistry/azure-schemaregistry/azure/schemaregistry/_schema_registry_client.py
+++ sdk/schemaregistry/azure-schemaregistry/azure/schemaregistry/_schema_registry_client.py
@@ -27,6 +27,7 @@ from __future__ import annotations
 from typing import Any, TYPE_CHECKING, Union, overload, cast, IO, Dict
 from functools import partial
 
+from typing_extensions import TypedDict
 from azure.core.tracing.decorator import distributed_trace
 
 from ._utils import (
@@ -46,6 +47,16 @@ from ._generated._client import AzureSchemaRegistry
 if TYPE_CHECKING:
     from azure.core.credentials import TokenCredential
     from azure.core.rest import HttpResponse
+
+    class SchemaPropertiesDict(TypedDict):  # needed for use with spread operator
+        """
+        Typing for SchemaProperties dict.
+        """
+        id: str
+        format: "SchemaFormat"
+        group_name: str
+        name: str
+        version: int
 
 
 class SchemaRegistryClient(object):
@@ -139,7 +150,8 @@ class SchemaRegistryClient(object):
             cls=partial(prepare_schema_properties_result, format),
             **http_request_kwargs,
         )
-        return SchemaProperties(**schema_properties)
+        properties = cast("SchemaPropertiesDict", schema_properties)
+        return SchemaProperties(**properties)
 
     @overload
     def get_schema(self, schema_id: str, **kwargs: Any) -> Schema:
@@ -223,9 +235,10 @@ class SchemaRegistryClient(object):
                 **http_request_kwargs,
             )
         http_response.read()
+        properties = cast("SchemaPropertiesDict", schema_properties)
         return Schema(
             definition=http_response.text(),
-            properties=SchemaProperties(**schema_properties),
+            properties=SchemaProperties(**properties),
         )
 
     @distributed_trace
@@ -273,4 +286,5 @@ class SchemaRegistryClient(object):
                 **http_request_kwargs,
             )
         )
-        return SchemaProperties(**schema_properties)
+        properties = cast("SchemaPropertiesDict", schema_properties)
+        return SchemaProperties(**properties)
