3b71d3dd2eab86981dd69aa010afdfb04c09a82a
nemanjarajic <nrajic30@gmail.com>
2023-11-30 19:51:47 -0800

    fix

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_endpoint/batch_endpoint.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_endpoint/batch_endpoint.py
@@ -78,7 +78,7 @@ class BatchEndpoint(Endpoint):
             self.defaults = {}
             self.defaults["deployment_name"] = default_deployment_name
 
-    def _to_rest_batch_endpoint(self, location: str) -> BatchEndpointData:
+    def _to_rest_batch_endpoint(self, location: str) -> BatchEndpoint:
         validate_endpoint_or_deployment_name(self.name)
         batch_endpoint = RestBatchEndpoint(
             description=self.description,
@@ -86,10 +86,10 @@ class BatchEndpoint(Endpoint):
             properties=self.properties,
             defaults=self.defaults,
         )
-        return BatchEndpointData(location=location, tags=self.tags, properties=batch_endpoint)
+        return BatchEndpoint(location=location, tags=self.tags, properties=batch_endpoint)
 
     @classmethod
-    def _from_rest_object(cls, obj: BatchEndpointData) -> "BatchEndpoint":
+    def _from_rest_object(cls, obj: BatchEndpoint) -> "BatchEndpoint":
         return BatchEndpoint(
             id=obj.id,
             name=obj.name,
