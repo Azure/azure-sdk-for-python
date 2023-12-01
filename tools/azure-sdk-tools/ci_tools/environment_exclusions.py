09f23b5f2bda94cac32380b5d63dbe6e43a9a66a
Krista Pratico <krpratic@microsoft.com>
2023-12-01 15:18:26 -0800

    test set PULLREQUEST

--- tools/azure-sdk-tools/ci_tools/environment_exclusions.py
+++ tools/azure-sdk-tools/ci_tools/environment_exclusions.py
@@ -52,11 +52,11 @@ def is_check_enabled(package_path: str, check: str, default: Any = True) -> bool
     if package_path == ".":
         package_path = os.getcwd()
 
-    ci_setting = get_config_setting(package_path, "ci_enabled", default)
-    print(f"CI setting: {ci_setting}")
+    ci_enabled = get_config_setting(package_path, "ci_enabled", default)
+    print(f"CI setting: {ci_enabled}")
     print(f"in_public: {in_public()}")
     # if not in_public() and ci_setting is False:
-    if not in_public() and ci_setting is False:
+    if not in_public() and ci_enabled is False:
         return False
 
     # now pull the new pyproject.toml configuration
