b37643da9047647ccea1702f3b2cab908db25641
YalinLi0312 <yall@microsoft.com>
2023-12-13 15:13:01 -0800

    Remove unused imports

--- sdk/documentintelligence/azure-ai-documentintelligence/azure/ai/documentintelligence/_patch.py
+++ sdk/documentintelligence/azure-ai-documentintelligence/azure/ai/documentintelligence/_patch.py
@@ -6,7 +6,7 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
-from typing import Any, List, Optional, Union
+from typing import Any, List, Union
 
 from azure.core.credentials import AzureKeyCredential, TokenCredential
 
