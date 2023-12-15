b06b0deecf5878d1dd56419eb558cbcb858bbd93
YalinLi0312 <yall@microsoft.com>
2023-12-15 14:59:01 -0800

    Remove polling_interval from docstring

--- sdk/documentintelligence/azure-ai-documentintelligence/azure/ai/documentintelligence/_patch.py
+++ sdk/documentintelligence/azure-ai-documentintelligence/azure/ai/documentintelligence/_patch.py
@@ -29,8 +29,6 @@ class DocumentIntelligenceClient(DIClientGenerated):  # pylint: disable=client-a
      "2023-10-31-preview". Note that overriding this default value may result in unsupported
      behavior.
     :paramtype api_version: str
-    :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
-     Retry-After header is present.
     """
 
     def __init__(
@@ -62,8 +60,6 @@ class DocumentIntelligenceAdministrationClient(DIAClientGenerated):  # pylint: d
      "2023-10-31-preview". Note that overriding this default value may result in unsupported
      behavior.
     :paramtype api_version: str
-    :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
-     Retry-After header is present.
     """
 
     def __init__(
