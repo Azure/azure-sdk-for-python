a804fb433a3170bebfc9358efc6c1ba9dea16bb3
Matt Metcalf <mrm9084@gmail.com>
2023-12-15 09:28:42 -0800

    ): # pylint: disable= consider-using-with

--- sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/aio/_azureappconfigurationproviderasync.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/aio/_azureappconfigurationproviderasync.py
@@ -329,7 +329,7 @@ class AzureAppConfigurationProvider(Mapping[str, Union[str, JSON]]):  # pylint: 
         if not self._refresh_timer.needs_refresh():
             logging.debug("Refresh called but refresh interval not elapsed.")
             return
-        if not self._refresh_lock.acquire(blocking=False):
+        if not self._refresh_lock.acquire(blocking=False): # pylint: disable= consider-using-with
             logging.debug("Refresh called but refresh already in progress.")
             return
         success = False
