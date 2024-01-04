0b8fe04e11cd7d84d69e20596a254af2d89a2edd
Mickey Zhang <ruizh@microsoft.com>
2024-01-04 11:18:58 -0800

    update doc string

--- sdk/ml/azure-ai-ml/azure/ai/ml/identity/_credentials/aml_on_behalf_of.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/identity/_credentials/aml_on_behalf_of.py
@@ -19,7 +19,7 @@ class AzureMLOnBehalfOfCredential(object):
     This credential can only be used on `Azure Machine Learning Compute
     <https://docs.microsoft.com/azure/machine-learning/concept-compute-target#azure-machine-learning-compute-managed>`_ or `Azure Machine Learning Serverless Spark Compute
     <https://learn.microsoft.com/en-us/azure/machine-learning/apache-spark-azure-ml-concepts#serverless-spark-compute>`_
-    during job execution when user request to run job during its identity.
+    during job execution when user request to run job using its identity.
     """
     # pylint: enable=line-too-long
 
