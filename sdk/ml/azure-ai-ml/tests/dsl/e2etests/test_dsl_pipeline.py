afcf6fb8683854ec4020f241819c69e49d9c7b09
bupt-wenxiaole <506539136@qq.com>
2024-01-05 19:34:19 +0800

    skip test cases

--- sdk/ml/azure-ai-ml/tests/dsl/e2etests/test_dsl_pipeline.py
+++ sdk/ml/azure-ai-ml/tests/dsl/e2etests/test_dsl_pipeline.py
@@ -2662,8 +2662,8 @@ class TestDSLPipeline(AzureRecordedTestCase):
         pipeline_job.settings.default_compute = "cpu-cluster"
         assert_job_cancel(pipeline_job, client)
 
+    @pytest.mark.skip("Will renable when parallel e2e recording issue is fixed")
     @pytest.mark.disable_mock_code_hash
-    @pytest.skip("Will renable when parallel e2e recording issue is fixed")
     def test_register_output_sdk(self, client: MLClient):
         from azure.ai.ml.sweep import (
             BanditPolicy,
