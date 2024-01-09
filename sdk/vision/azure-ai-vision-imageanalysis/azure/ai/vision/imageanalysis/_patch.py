de1080747a65790d0c30d8306a1c1b8def6149a1
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-01-09 10:18:17 -0800

    Make sure analyze overload ref-docs appear

--- sdk/vision/azure-ai-vision-imageanalysis/azure/ai/vision/imageanalysis/_patch.py
+++ sdk/vision/azure-ai-vision-imageanalysis/azure/ai/vision/imageanalysis/_patch.py
@@ -6,11 +6,14 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 
-Note: the analyze overloads here should have been implemented in the `_patch.py` file in the `_operations` folder
+Note 1: the analyze overloads here should have been implemented in the `_patch.py` file in the `_operations` folder
 instead of here. That would have worked fine, except there is an issue with the generated Python
 ref-docs. The overloads do not show up. See this GitHub issue: https://github.com/Azure/autorest.python/issues/1315.
 To overcome this, the overloads are defined here. Consider moving them to the right place once the
 above issue is fixed.
+
+Note 2: Don't bother documenting the two overload methods below. The doc tool (sphinx) will not pick them up. Instead,
+document the 3rd method.
 """
 from typing import List, overload, Any, Optional, Union
 from azure.core.tracing.decorator import distributed_trace
@@ -19,6 +22,7 @@ from ._operations._operations import ImageAnalysisClientOperationsMixin
 from ._client import ImageAnalysisClient as ImageAnalysisClientGenerated
 
 class ImageAnalysisClient(ImageAnalysisClientGenerated):
+
     @overload
     def analyze(
         self,
@@ -31,40 +35,7 @@ class ImageAnalysisClient(ImageAnalysisClientGenerated):
         model_version: Optional[str] = None,
         **kwargs: Any
     ) -> _models.ImageAnalysisResult:
-        """Performs a single Image Analysis operation.
-
-        :keyword image_url: The publicly accessible URL of the image to analyze.
-        :paramtype image_url: str
-        :keyword visual_features: A list of visual features to analyze. Required. Seven visual features
-         are supported: Caption, DenseCaptions, Read (OCR), Tags, Objects, SmartCrops, and People. At
-         least one visual feature must be specified.
-        :paramtype visual_features: list[~azure.ai.vision.imageanalysis.models.VisualFeatures]
-        :keyword language: The desired language for result generation (a two-letter language code).
-         Defaults to 'en' (English). See https://aka.ms/cv-languages for a list of supported languages.
-        :paramtype language: str
-        :keyword gender_neutral_caption: Boolean flag for enabling gender-neutral captioning for
-         Caption and Dense Captions features. Defaults to 'false'.
-         Captions may contain gender terms (for example: 'man', 'woman', or 'boy', 'girl').
-         If you set this to 'true', those will be replaced with gender-neutral terms (for example:
-         'person' or 'child').
-        :paramtype gender_neutral_caption: bool
-        :keyword smart_crops_aspect_ratios: A list of aspect ratios to use for smart cropping.
-         Defaults to one crop region with an aspect ratio the service sees fit between
-         0.5 and 2.0 (inclusive). Aspect ratios are calculated by dividing the target crop
-         width in pixels by the height in pixels. When set, supported values are
-         between 0.75 and 1.8 (inclusive).
-        :paramtype smart_crops_aspect_ratios: list[float]
-        :keyword model_version: The version of cloud AI-model used for analysis. Defaults to 'latest',
-         for the latest AI model with recent improvements.
-         The format is the following: 'latest' or 'YYYY-MM-DD' or 'YYYY-MM-DD-preview',
-         where 'YYYY', 'MM', 'DD' are the year, month and day associated with the model.
-         If you would like to make sure analysis results do not change over time, set this
-         value to a specific model version.
-        :paramtype model_version: str
-        :return: ImageAnalysisResult. The ImageAnalysisResult is compatible with MutableMapping
-        :rtype: ~azure.ai.vision.imageanalysis.models.ImageAnalysisResult
-        :raises: ~azure.core.exceptions.HttpResponseError
-        """
+        ...
 
     @overload
     def analyze(
@@ -78,8 +49,25 @@ class ImageAnalysisClient(ImageAnalysisClientGenerated):
         model_version: Optional[str] = None,
         **kwargs: Any
     ) -> _models.ImageAnalysisResult:
+        ...
+
+    @distributed_trace
+    def analyze(
+        self,
+        *,
+        visual_features: List[_models.VisualFeatures],
+        image_data: Optional[bytes] = None,
+        image_url: Optional[str] = None,
+        language: Optional[str] = None,
+        gender_neutral_caption: Optional[bool] = None,
+        smart_crops_aspect_ratios: Optional[List[float]] = None,
+        model_version: Optional[str] = None,
+        **kwargs: Any
+    ) -> _models.ImageAnalysisResult:
         """Performs a single Image Analysis operation.
 
+        :keyword image_url: The publicly accessible URL of the image to analyze.
+        :paramtype image_url: str
         :keyword image_data: A buffer containing the whole image to be analyzed.
         :paramtype image_data: bytes
         :keyword visual_features: A list of visual features to analyze. Required. Seven visual features
@@ -112,19 +100,6 @@ class ImageAnalysisClient(ImageAnalysisClientGenerated):
         :rtype: ~azure.ai.vision.imageanalysis.models.ImageAnalysisResult
         :raises: ~azure.core.exceptions.HttpResponseError
         """
-    @distributed_trace
-    def analyze(
-        self,
-        *,
-        visual_features: List[_models.VisualFeatures],
-        image_data: Optional[bytes] = None,
-        image_url: Optional[str] = None,
-        language: Optional[str] = None,
-        gender_neutral_caption: Optional[bool] = None,
-        smart_crops_aspect_ratios: Optional[List[float]] = None,
-        model_version: Optional[str] = None,
-        **kwargs: Any
-    ) -> _models.ImageAnalysisResult:
 
         visual_features_impl: List[Union[str, _models.VisualFeatures]] = list(visual_features)
 
