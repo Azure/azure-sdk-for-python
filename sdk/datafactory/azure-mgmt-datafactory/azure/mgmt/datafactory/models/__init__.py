cea15f7c116c7f6ce34e9c382c1267d051c393b7
azure-sdk <PythonSdkPipelines>
2024-01-09 07:07:00 +0000

    code and test

--- sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/models/__init__.py
+++ sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/models/__init__.py
@@ -587,8 +587,16 @@ from ._models_py3 import SalesforceServiceCloudLinkedService
 from ._models_py3 import SalesforceServiceCloudObjectDataset
 from ._models_py3 import SalesforceServiceCloudSink
 from ._models_py3 import SalesforceServiceCloudSource
+from ._models_py3 import SalesforceServiceCloudV2LinkedService
+from ._models_py3 import SalesforceServiceCloudV2ObjectDataset
+from ._models_py3 import SalesforceServiceCloudV2Sink
+from ._models_py3 import SalesforceServiceCloudV2Source
 from ._models_py3 import SalesforceSink
 from ._models_py3 import SalesforceSource
+from ._models_py3 import SalesforceV2LinkedService
+from ._models_py3 import SalesforceV2ObjectDataset
+from ._models_py3 import SalesforceV2Sink
+from ._models_py3 import SalesforceV2Source
 from ._models_py3 import SapBWLinkedService
 from ._models_py3 import SapBwCubeDataset
 from ._models_py3 import SapBwSource
@@ -734,6 +742,10 @@ from ._models_py3 import VerticaLinkedService
 from ._models_py3 import VerticaSource
 from ._models_py3 import VerticaTableDataset
 from ._models_py3 import WaitActivity
+from ._models_py3 import WarehouseLinkedService
+from ._models_py3 import WarehouseSink
+from ._models_py3 import WarehouseSource
+from ._models_py3 import WarehouseTableDataset
 from ._models_py3 import WebActivity
 from ._models_py3 import WebActivityAuthentication
 from ._models_py3 import WebAnonymousAuthentication
@@ -839,6 +851,8 @@ from ._data_factory_management_client_enums import RunQueryOrder
 from ._data_factory_management_client_enums import RunQueryOrderByField
 from ._data_factory_management_client_enums import SalesforceSinkWriteBehavior
 from ._data_factory_management_client_enums import SalesforceSourceReadBehavior
+from ._data_factory_management_client_enums import SalesforceV2SinkWriteBehavior
+from ._data_factory_management_client_enums import SalesforceV2SourceReadBehavior
 from ._data_factory_management_client_enums import SapCloudForCustomerSinkWriteBehavior
 from ._data_factory_management_client_enums import SapHanaAuthenticationType
 from ._data_factory_management_client_enums import SapHanaPartitionOption
@@ -1464,8 +1478,16 @@ __all__ = [
     "SalesforceServiceCloudObjectDataset",
     "SalesforceServiceCloudSink",
     "SalesforceServiceCloudSource",
+    "SalesforceServiceCloudV2LinkedService",
+    "SalesforceServiceCloudV2ObjectDataset",
+    "SalesforceServiceCloudV2Sink",
+    "SalesforceServiceCloudV2Source",
     "SalesforceSink",
     "SalesforceSource",
+    "SalesforceV2LinkedService",
+    "SalesforceV2ObjectDataset",
+    "SalesforceV2Sink",
+    "SalesforceV2Source",
     "SapBWLinkedService",
     "SapBwCubeDataset",
     "SapBwSource",
@@ -1611,6 +1633,10 @@ __all__ = [
     "VerticaSource",
     "VerticaTableDataset",
     "WaitActivity",
+    "WarehouseLinkedService",
+    "WarehouseSink",
+    "WarehouseSource",
+    "WarehouseTableDataset",
     "WebActivity",
     "WebActivityAuthentication",
     "WebAnonymousAuthentication",
@@ -1715,6 +1741,8 @@ __all__ = [
     "RunQueryOrderByField",
     "SalesforceSinkWriteBehavior",
     "SalesforceSourceReadBehavior",
+    "SalesforceV2SinkWriteBehavior",
+    "SalesforceV2SourceReadBehavior",
     "SapCloudForCustomerSinkWriteBehavior",
     "SapHanaAuthenticationType",
     "SapHanaPartitionOption",
