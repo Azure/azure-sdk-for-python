0e1c1b522a9871a3c9ff304d065b9d63de9de9e0
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-01-10 11:42:09 -0800

    Fix pylint issues

--- sdk/vision/azure-ai-vision-imageanalysis/azure/ai/vision/imageanalysis/models/_enums.py
+++ sdk/vision/azure-ai-vision-imageanalysis/azure/ai/vision/imageanalysis/models/_enums.py
@@ -14,16 +14,21 @@ class VisualFeatures(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """The visual features supported by the Image Analysis service."""
 
     TAGS = "tags"
-    """Extract content tags for thousands of recognizable objects, living beings, scenery, and actions that appear in the image."""
+    """Extract content tags for thousands of recognizable objects, living beings, scenery, and actions
+    that appear in the image."""
     CAPTION = "caption"
     """Generate a human-readable caption sentence that describes the content of the image."""
     DENSE_CAPTIONS = "denseCaptions"
-    """Generate human-readable caption sentences for up to 10 different regions in the image, including one for the whole image."""
+    """Generate human-readable caption sentences for up to 10 different regions in the image,
+    including one for the whole image."""
     OBJECTS = "objects"
-    """Object detection. This is similar to tags, but focused on detecting physical objects in the image and returning their location."""
+    """Object detection. This is similar to tags, but focused on detecting physical objects in the
+    image and returning their location."""
     READ = "read"
-    """Extract printed or handwritten text from the image. Also known as Optical Character Recognition (OCR)."""
+    """Extract printed or handwritten text from the image. Also known as Optical Character Recognition
+    (OCR)."""
     SMART_CROPS = "smartCrops"
-    """Find representative sub-regions of the image for thumbnail generation, at desired aspect ratios, with priority given to detected faces."""
+    """Find representative sub-regions of the image for thumbnail generation, at desired aspect
+    ratios, with priority given to detected faces."""
     PEOPLE = "people"
     """Detect people in the image and return their location."""
