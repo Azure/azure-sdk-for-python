81d398b163474475272881d4feb7e91759f4b968
Cristian <clomeli@microsoft.com>
2024-01-11 01:53:48 -0800

    revert pipeline decorator

--- sdk/ai/azure-ai-generative/azure/ai/generative/index/_dataindex/dsl/_pipeline_decorator.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/index/_dataindex/dsl/_pipeline_decorator.py
@@ -58,7 +58,7 @@ P = ParamSpec("P")
 # Overload the returns a decorator when func is None
 @overload
 def pipeline(
-    func: Optional[[P, T]] = None,
+    func: None = None,
     *,
     name: Optional[str] = None,
     version: Optional[str] = None,
