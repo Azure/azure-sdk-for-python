a1885cb3fda9f1d709b95999898c8c611f0ecd3d
Libba Lawrence <llawrence@microsoft.com>
2024-01-16 15:34:43 -0800

    add classifiers (#33872)

--- sdk/eventgrid/azure-eventgrid/setup.py
+++ sdk/eventgrid/azure-eventgrid/setup.py
@@ -54,6 +54,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
     ],
     python_requires=">=3.8",
