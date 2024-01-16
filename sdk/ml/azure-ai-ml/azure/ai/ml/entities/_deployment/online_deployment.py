a428ecb761dc5723e8d0a4dfae4eb14b6e7fde85
Kevin Zhang <iaminwc@163.com>
2024-01-15 18:09:48 -0800

    update online_deployment.py

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_deployment/online_deployment.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_deployment/online_deployment.py
@@ -831,10 +831,7 @@ class ManagedOnlineDeployment(OnlineDeployment):
             code_configuration=code_config,
             environment=deployment.environment_id,
             app_insights_enabled=deployment.app_insights_enabled,
-            scale_settings=cast(
-                Optional[Union[DefaultScaleSettings, TargetUtilizationScaleSettings]],
-                OnlineScaleSettings._from_rest_object(deployment.scale_settings),
-            ),
+            scale_settings=OnlineScaleSettings._from_rest_object(deployment.scale_settings),  # type: ignore
             liveness_probe=ProbeSettings._from_rest_object(deployment.liveness_probe),
             environment_variables=deployment.environment_variables,
             readiness_probe=ProbeSettings._from_rest_object(deployment.readiness_probe),
