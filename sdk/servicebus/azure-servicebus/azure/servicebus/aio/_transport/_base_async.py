82b4cdf431694071c3f03d8f29c09650762337a7
Libba Lawrence <llawrence@microsoft.com>
2024-01-23 09:50:57 -0800

    uamqp conflict remove

--- sdk/servicebus/azure-servicebus/azure/servicebus/aio/_transport/_base_async.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/aio/_transport/_base_async.py
@@ -37,7 +37,6 @@ class AmqpTransportAsync(ABC):  # pylint: disable=too-many-public-methods
     AMQP_LONG_VALUE: Callable
     AMQP_ARRAY_VALUE: Callable
     AMQP_UINT_VALUE: Callable
-    AMQP_TIMESTAMP_VALUE: Callable
 
     @staticmethod
     @abstractmethod
