db1f057431212685d9eeb3e10acac415d1e2e259
mccoyp <mcpatino@microsoft.com>
2024-01-24 00:09:07 -0800

    Support user auth in live tests

--- tools/azure-sdk-tools/devtools_testutils/azure_recorded_testcase.py
+++ tools/azure-sdk-tools/devtools_testutils/azure_recorded_testcase.py
@@ -4,6 +4,7 @@
 # license information.
 # --------------------------------------------------------------------------
 import functools
+import logging
 import os
 import os.path
 import six
@@ -23,6 +24,8 @@ from .fake_credentials_async import AsyncFakeCredential
 from .helpers import is_live, trim_kwargs_from_test_function
 from .sanitizers import add_general_string_sanitizer
 
+
+_LOGGER = logging.getLogger()
 
 load_dotenv(find_dotenv())
 
@@ -87,10 +90,37 @@ class AzureRecordedTestCase(object):
         tenant_id = os.environ.get("AZURE_TENANT_ID", getattr(os.environ, "TENANT_ID", None))
         client_id = os.environ.get("AZURE_CLIENT_ID", getattr(os.environ, "CLIENT_ID", None))
         secret = os.environ.get("AZURE_CLIENT_SECRET", getattr(os.environ, "CLIENT_SECRET", None))
+
+        use_pwsh = os.environ.get("AZURE_TEST_USE_PWSH_AUTH")
+        use_cli = os.environ.get("AZURE_TEST_USE_CLI_AUTH")
         is_async = kwargs.pop("is_async", False)
 
+        # User-based authentication through Azure PowerShell, if requested
+        if use_pwsh.lower() == "true" and self.is_live:
+            _LOGGER.info(
+                "Environment variable AZURE_TEST_USE_PWSH_AUTH set to 'true'. Using AzurePowerShellCredential."
+            )
+            from azure.identity import AzurePowerShellCredential
+
+            if is_async:
+                from azure.identity.aio import AzurePowerShellCredential
+            return AzurePowerShellCredential()
+        # User-based authentication through Azure CLI, if requested
+        if use_cli.lower() == "true" and self.is_live:
+            _LOGGER.info("Environment variable AZURE_TEST_USE_CLI_AUTH set to 'true'. Using AzureCliCredential.")
+            from azure.identity import AzureCliCredential
+
+            if is_async:
+                from azure.identity.aio import AzureCliCredential
+            return AzureCliCredential()
+
+        # Service principal authentication
         if tenant_id and client_id and secret and self.is_live:
             if _is_autorest_v3(client_class):
+                _LOGGER.info(
+                    "Service principal client ID, secret, and tenant ID detected. Using ClientSecretCredential.\n"
+                    "For user-based authentication, set AZURE_TEST_USE_PWSH_AUTH or AZURE_TEST_USE_CLI_AUTH to 'true'."
+                )
                 # Create azure-identity class
                 from azure.identity import ClientSecretCredential
 
