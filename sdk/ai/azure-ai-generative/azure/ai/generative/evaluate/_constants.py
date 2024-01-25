658b79cabd44584269b0a0949a9a28ed4fdaecb3
Hanchi Wang <hancwang@microsoft.com>
2024-01-25 13:48:31 -0800

    mypy

--- sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_constants.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_constants.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from typing import Dict, Union, Type
+from typing import Dict, Union
 
 QA = "qa"
 CHAT = "chat"
@@ -68,9 +68,9 @@ class ChatMetrics:
     ]
 
 
-TASK_TYPE_TO_METRICS_MAPPING: Dict[str, Union[Type[QaMetrics], [ChatMetrics]]] = {
-    "qa": QaMetrics,
-    "rag-evaluation": ChatMetrics
+TASK_TYPE_TO_METRICS_MAPPING: Dict[str, Union[QaMetrics, ChatMetrics]] = {
+    "qa": QaMetrics(),
+    "rag-evaluation": ChatMetrics()
 }
 
 SUPPORTED_TASK_TYPE_TO_METRICS_MAPPING = {
