cedd6d5c32db537564220c464b84e7c51cd4f1d6
iscai-msft <isabellavcai@gmail.com>
2024-01-29 14:16:08 -0500

    add comment

--- sdk/core/azure-core/azure/core/utils/_pipeline_transport_rest_shared.py
+++ sdk/core/azure-core/azure/core/utils/_pipeline_transport_rest_shared.py
@@ -414,5 +414,7 @@ def _aiohttp_body_helper(
 
 def get_file_items(files: "FilesType") -> Sequence[Tuple[str, "FileType"]]:
     if isinstance(files, Mapping):
+        # casting because ItemsView technically isn't a Sequence, even
+        # though realistically it is ordered python 3.7 and after
         return cast(Sequence[Tuple[str, "FileType"]], files.items())
     return files
