d5c6c073fc355acbb4d88d4524ad9f841027be2f
Ankit Singhal <30610298+singankit@users.noreply.github.com>
2024-01-30 10:15:14 -0800

    Spec Review feedback (#34032)
    * Spec Review feedback incorporated
    
    * Updating signature for code metric handler
    
    * Spec Review feedback incorporated
    
    * Updating signature for code metric handler
    
    * Rebased from main
    
    * Adding tests and user agent

--- sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_user_agent.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_user_agent.py
@@ -1,11 +1,6 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+from azure.ai.generative._version import VERSION
 
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-
-from ._custom_metric import PromptMetric
-
-__all__ = [
-    "PromptMetric"
-]
+USER_AGENT = "{}/{} {}/{}".format("azure-ai-generative", VERSION, "evaluate", VERSION)
