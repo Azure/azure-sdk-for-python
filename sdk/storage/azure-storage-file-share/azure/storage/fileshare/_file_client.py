0177f2fde66b1726f3a7e27f24ea83e68f0d4e68
Vincent Phat Tran <vincenttran@microsoft.com>
2024-01-31 11:17:29 -0800

    Target right place

--- sdk/storage/azure-storage-file-share/azure/storage/fileshare/_file_client.py
+++ sdk/storage/azure-storage-file-share/azure/storage/fileshare/_file_client.py
@@ -1480,7 +1480,7 @@ class ShareFileClient(StorageAccountHostsMixin):
         offset: Optional[int] = None,
         length: Optional[int] = None,
         *,
-        support_rename: Optional[bool] = None,
+        include_renames: Optional[bool] = None,
         **kwargs: Any
     ) -> Tuple[List[Dict[str, int]], List[Dict[str, int]]]:
         """Returns the list of valid page ranges for a file or snapshot
@@ -1496,7 +1496,7 @@ class ShareFileClient(StorageAccountHostsMixin):
             The snapshot diff parameter that contains an opaque DateTime value that
             specifies a previous file snapshot to be compared
             against a more recent snapshot or the current file.
-        :keyword Optional[bool] support_rename:
+        :keyword Optional[bool] include_renames:
             Only valid if previous_sharesnapshot parameter is provided. Specifies whether the changed ranges for
             a file that has been renamed or moved between the target snapshot (or live file) and the previous
             snapshot should be listed. If set to True, the valid changed ranges for the file will be returned.
@@ -1520,7 +1520,7 @@ class ShareFileClient(StorageAccountHostsMixin):
             offset=offset,
             length=length,
             previous_sharesnapshot=previous_sharesnapshot,
-            support_rename=support_rename,
+            support_rename=include_renames,
             **kwargs)
         try:
             ranges = self._client.file.get_range_list(**options)
