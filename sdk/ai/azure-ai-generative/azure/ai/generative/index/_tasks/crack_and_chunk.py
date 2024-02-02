40fc38a1bb62bacd633f769a11b5401143f9ebb3
jingyizhu99 <83610845+jingyizhu99@users.noreply.github.com>
2024-02-01 16:50:11 -0800

    cleanup duplicated code gen ai sdk (#34048)
    * remove duplicated
    
    * fix references
    
    * remove extra line/file used for testing
    
    * from_metadata()
    
    * as_langchain_embeddings
    
    * ignore llm in spelling checks
    
    * ignore llms
    
    * type checking
    
    * fix type checking
    
    * ignore index
    
    * ignore type check
    
    * ignore[no-redef]
    
    * revoke deletion of _functions.py
    
    * resolve unexpected change from merge
    
    * remove extra whitespaces

--- sdk/ai/azure-ai-generative/azure/ai/generative/index/_tasks/crack_and_chunk.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/index/_tasks/crack_and_chunk.py
@@ -16,7 +16,6 @@ import pandas as pd
 from azure.ai.generative.index._documents import (
     SUPPORTED_EXTENSIONS,
     ChunkedDocument,
-    Document,
     DocumentChunksIterator,
     DocumentSource,
 )
@@ -30,6 +29,7 @@ from azure.ai.generative.index._utils.logging import (
     safe_mlflow_start_run,
     track_activity,
 )
+from azure.ai.resources._index._documents import Document
 
 logger = get_logger("crack_and_chunk")
 
