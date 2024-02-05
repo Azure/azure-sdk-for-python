6f462a3f82fa1bb9cbf9ec2ce22082665d643f4c
Xiang Yan <xiangyan@microsoft.com>
2024-02-05 14:15:07 -0800

    update

--- sdk/identity/azure-identity/tests/test_pickling.py
+++ sdk/identity/azure-identity/tests/test_pickling.py
@@ -5,9 +5,10 @@
 import pickle
 from azure.identity import DefaultAzureCredential
 
+
 def test_pickle_dac():
     cred = DefaultAzureCredential()
-    with open('data.pkl', 'wb') as outfile:
+    with open("data.pkl", "wb") as outfile:
         pickle.dump(cred, outfile)
-    with open('data.pkl', 'rb') as infile:
+    with open("data.pkl", "rb") as infile:
         data_loaded = pickle.load(infile)
--- sdk/identity/azure-identity/tests/test_pickling_async.py
+++ sdk/identity/azure-identity/tests/test_pickling_async.py
@@ -5,9 +5,10 @@
 import pickle
 from azure.identity.aio import DefaultAzureCredential
 
+
 def test_pickle_dac():
     cred = DefaultAzureCredential()
-    with open('data_aio.pkl', 'wb') as outfile:
+    with open("data_aio.pkl", "wb") as outfile:
         pickle.dump(cred, outfile)
-    with open('data_aio.pkl', 'rb') as infile:
+    with open("data_aio.pkl", "rb") as infile:
         data_loaded = pickle.load(infile)
