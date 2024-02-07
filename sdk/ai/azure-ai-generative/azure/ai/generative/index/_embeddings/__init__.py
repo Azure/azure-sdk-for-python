fa52ccc9a498dca613f06f344abdd101a01007b7
Hanchi Wang <hancwang@microsoft.com>
2024-02-07 13:38:26 -0800

    Revert "cleanup duplicated code gen ai sdk (#34048)"
    This reverts commit bb974379b240740359b1a74ce9536f3b2ec55c4e.

--- sdk/ai/azure-ai-generative/azure/ai/generative/index/_embeddings/__init__.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/index/_embeddings/__init__.py
@@ -21,15 +21,14 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 import yaml  # type: ignore[import]
 from azure.core.credentials import TokenCredential
-from azure.ai.generative.index._documents import DocumentChunksIterator, DocumentSource
+from azure.ai.generative.index._documents import Document, DocumentChunksIterator, DocumentSource, StaticDocument
+from azure.ai.generative.index._embeddings.openai import OpenAIEmbedder
 from azure.ai.generative.index._langchain.vendor.document_loaders.base import BaseLoader
+from azure.ai.generative.index._langchain.vendor.embeddings.base import Embeddings as Embedder
+from azure.ai.generative.index._langchain.vendor.schema.document import Document as LangChainDocument
+from azure.ai.generative.index._models import init_open_ai_from_config, parse_model_uri
 from azure.ai.generative.index._utils.logging import get_logger, track_activity
 from azure.ai.generative.index._utils.tokens import tiktoken_cache_dir
-from azure.ai.resources._index._documents import Document, StaticDocument
-from azure.ai.resources._index._embeddings.openai import OpenAIEmbedder
-from azure.ai.resources._index._langchain.vendor.embeddings.base import Embeddings as Embedder
-from azure.ai.resources._index._langchain.vendor.schema.document import Document as LangChainDocument
-from azure.ai.resources._index._models import init_open_ai_from_config, parse_model_uri
 
 logger = get_logger(__name__)
 
@@ -55,7 +54,7 @@ def get_langchain_embeddings(embedding_kind: str, arguments: dict, credential: O
         )
         return embedder
     elif embedding_kind == "hugging_face":
-        from azure.ai.resources._index._langchain.vendor.embeddings.huggingface import HuggingFaceEmbeddings
+        from azure.ai.generative.index._langchain.vendor.embeddings.huggingface import HuggingFaceEmbeddings
 
         args = copy.deepcopy(arguments)
 
@@ -1005,7 +1004,7 @@ class EmbeddingsContainer:
             import_faiss_or_so_help_me = dependable_faiss_import
         elif engine.endswith("indexes.faiss.FaissAndDocStore"):
             from azure.ai.generative.index._docstore import FileBasedDocstore
-            from azure.ai.generative.index._indexes.faiss import FaissAndDocStore, import_faiss_or_so_help_me  # type: ignore[no-redef]
+            from azure.ai.generative.index._indexes.faiss import FaissAndDocStore, import_faiss_or_so_help_me
 
             def add_doc(doc_id, emb_doc, documents):
                 documents.append(
