cf747e654b916d1a8670f9e99bb56db6cfb6a312
Kushagra Thapar <kuthapar@microsoft.com>
2024-02-07 16:37:30 -0800

    Added Emulator CI and additional tests for Azure Cosmos SDK (#33363)
    * Added Cross partition query tests for sync and async APIs
    
    * Added license header for new files
    
    * Updated database and container creation
    
    * Updated python tests with setup and tear down
    
    * Fixed some tests cases
    
    * Fixed spelling
    
    * Added emulator CI
    
    * Removed python 3.7 emulator tests config
    
    * Removed collection crud tests from emulator
    
    * Updated some collection heavy tests to skip for emulator
    
    * Fixing python 3.10
    
    * Marking test_computed_properties async test to run only in live tests
    
    * Fixed emulator tests with pytest fixtures
    
    * Fixed more emulator tests with pytest fixtures
    
    * Fixed more emulator tests with pytest fixtures
    
    * Fixed more emulator tests with pytest fixtures and updated azure core min version
    
    * Fixed flaky tests
    
    * Updated stored procedure create calls
    
    * Fixing sproc test
    
    * Fixing trigger test
    
    * Fixing flaky live ci tests
    
    * Fixing max parallel and removed python 3.11 for cosmos emulator windows as there is a python open issue
    
    * Added MIT license header
    
    * Update sdk/cosmos/azure-cosmos/test/test_partition_split_query.py
    
    ---------
    
    Co-authored-by: Simon Moreno <30335873+simorenoh@users.noreply.github.com>

--- sdk/cosmos/azure-cosmos/test/test_routing_map.py
+++ sdk/cosmos/azure-cosmos/test/test_routing_map.py
@@ -1,53 +1,31 @@
-#The MIT License (MIT)
-#Copyright (c) 2014 Microsoft Corporation
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
+# The MIT License (MIT)
+# Copyright (c) Microsoft Corporation. All rights reserved.
 
 import unittest
-import uuid
 
 import pytest
-import azure.cosmos.documents as documents
+
 import azure.cosmos.cosmos_client as cosmos_client
-from azure.cosmos import PartitionKey
-from azure.cosmos._routing.routing_map_provider import PartitionKeyRangeCache
-from azure.cosmos._routing import routing_range as routing_range
 import test_config
+from azure.cosmos import DatabaseProxy, ContainerProxy
+from azure.cosmos._routing import routing_range as routing_range
+from azure.cosmos._routing.routing_map_provider import PartitionKeyRangeCache
 
-pytestmark = pytest.mark.cosmosEmulator
 
-#IMPORTANT NOTES:
-  
-#      Most test cases in this file create collections in your Azure Cosmos account.
-#      Collections are billing entities.  By running these test cases, you may incur monetary costs on your account.
-  
-#      To Run the test, replace the two member fields (masterKey and host) with values 
-#   associated with your Azure Cosmos account.
-
-@pytest.mark.usefixtures("teardown")
-class RoutingMapEndToEndTests(unittest.TestCase):
-    """Routing Map Functionalities end to end Tests.
+@pytest.mark.cosmosEmulator
+class TestRoutingMapEndToEnd(unittest.TestCase):
+    """Routing Map Functionalities end-to-end Tests.
     """
 
-    host = test_config._test_config.host
-    masterKey = test_config._test_config.masterKey
-    connectionPolicy = test_config._test_config.connectionPolicy
+    host = test_config.TestConfig.host
+    masterKey = test_config.TestConfig.masterKey
+    connectionPolicy = test_config.TestConfig.connectionPolicy
+    configs = test_config.TestConfig
+    client: cosmos_client.CosmosClient = None
+    created_database: DatabaseProxy = None
+    created_container: ContainerProxy = None
+    TEST_DATABASE_ID = configs.TEST_DATABASE_ID
+    TEST_COLLECTION_ID = configs.TEST_SINGLE_PARTITION_CONTAINER_ID
 
     @classmethod
     def setUpClass(cls):
@@ -57,25 +35,22 @@ class RoutingMapEndToEndTests(unittest.TestCase):
                 "You must specify your Azure Cosmos account values for "
                 "'masterKey' and 'host' at the top of this class to run the "
                 "tests.")
-        
-        cls.client = cosmos_client.CosmosClient(cls.host, cls.masterKey, consistency_level="Session", connection_policy=cls.connectionPolicy)
-        cls.created_database = cls.client.create_database_if_not_exists(test_config._test_config.TEST_DATABASE_ID)
-        cls.created_container = cls.created_database.create_container("routing_map_tests_"+str(uuid.uuid4()), PartitionKey(path="/pk"))
+
+        cls.client = cosmos_client.CosmosClient(cls.host, cls.masterKey)
+        cls.created_database = cls.client.get_database_client(cls.TEST_DATABASE_ID)
+        cls.created_container = cls.created_database.get_container_client(cls.TEST_COLLECTION_ID)
         cls.collection_link = cls.created_container.container_link
 
     def test_read_partition_key_ranges(self):
         partition_key_ranges = list(self.client.client_connection._ReadPartitionKeyRanges(self.collection_link))
-        #"the number of expected partition ranges returned from the emulator is 5."
-        if self.host == 'https://localhost:8081/':
-            self.assertEqual(5, len(partition_key_ranges))
-        else:
-            self.assertEqual(1, len(partition_key_ranges))
-        
+        self.assertEqual(1, len(partition_key_ranges))
+
     def test_routing_map_provider(self):
         partition_key_ranges = list(self.client.client_connection._ReadPartitionKeyRanges(self.collection_link))
 
         routing_mp = PartitionKeyRangeCache(self.client.client_connection)
-        overlapping_partition_key_ranges = routing_mp.get_overlapping_ranges(self.collection_link, routing_range.Range("", "FF", True, False))
+        overlapping_partition_key_ranges = routing_mp.get_overlapping_ranges(
+            self.collection_link, routing_range.Range("", "FF", True, False))
         self.assertEqual(len(overlapping_partition_key_ranges), len(partition_key_ranges))
         self.assertEqual(overlapping_partition_key_ranges, partition_key_ranges)
 
