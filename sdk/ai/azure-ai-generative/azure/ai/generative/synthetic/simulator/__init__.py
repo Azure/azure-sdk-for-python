1c5c6ee7d6edd4366d95d73d66c0c41117c28e57
Kevin Zhang <iaminwc@163.com>
2024-02-07 16:05:30 -0800

    fix import pylint errors - 2

--- sdk/ai/azure-ai-generative/azure/ai/generative/synthetic/simulator/__init__.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/synthetic/simulator/__init__.py
@@ -2,9 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 import os
+
+_template_dir = os.path.join(os.path.dirname(__file__), "templates")
+
 from .simulator.simulator import Simulator
 from .templates.simulator_templates import SimulatorTemplates
-
-_template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 
 __all__ = ["Simulator", "SimulatorTemplates"]
