24709f625b5139267f47e4dbaa180dc4dff75a91
Matt Metcalf <mrm9084@gmail.com>
2024-02-12 11:55:53 -0800

    Updating Min SDK version to bug in 1.4.0 with feature flags

--- sdk/appconfiguration/azure-appconfiguration-provider/setup.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/setup.py
@@ -74,7 +74,7 @@ setup(
     install_requires=[
         "msrest>=0.6.21",
         "azure-core<2.0.0,>=1.25.0",
-        "azure-appconfiguration<2.0.0,>=1.4.0",
+        "azure-appconfiguration<2.0.0,>=1.5.0",
         "azure-keyvault-secrets<5.0.0,>=4.3.0",
     ],
 )
