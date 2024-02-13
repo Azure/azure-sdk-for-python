23cef0afd55b6961f1922c9e27f30775352425eb
Vinothini Dharmaraj <146493756+v-vdharmaraj@users.noreply.github.com>
2024-02-13 13:30:37 -0800

    Adding source caller id to the answer call option (#34268)
    * Adding source caller id to the answer call option
    
    * Fixing the test failure on the mute participant scenario
    
    * skipping mute participant test to unblock the PR checkin Will enable it back once we have the latest test data
    
    * changing the ordering of the skip test attributes
    
    * updating the API spec endpoint

--- sdk/communication/azure-communication-callautomation/azure/communication/callautomation/_generated/aio/operations/_operations.py
+++ sdk/communication/azure-communication-callautomation/azure/communication/callautomation/_generated/aio/operations/_operations.py
@@ -1284,7 +1284,7 @@ class CallConnectionOperations:
 
         response = pipeline_response.http_response
 
-        if response.status_code not in [202]:
+        if response.status_code not in [200]:
             if _stream:
                 await response.read()  # Load the body in memory and close the socket
             map_error(status_code=response.status_code, response=response, error_map=error_map)
