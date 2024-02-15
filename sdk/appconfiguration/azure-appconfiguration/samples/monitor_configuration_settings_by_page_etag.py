9b93576e61af01470597940218fac0591d69e4a5
YalinLi0312 <yall@microsoft.com>
2024-02-15 12:16:30 -0800

    Update sample

--- sdk/appconfiguration/azure-appconfiguration/samples/monitor_configuration_settings_by_page_etag.py
+++ sdk/appconfiguration/azure-appconfiguration/samples/monitor_configuration_settings_by_page_etag.py
@@ -1,7 +1,5 @@
 import os
-from uuid import uuid4
 from azure.appconfiguration import AzureAppConfigurationClient, ConfigurationSetting
-from azure.core.exceptions import ResourceExistsError
 from dotenv import find_dotenv, load_dotenv
 
 
@@ -15,46 +13,52 @@ def main():
     for i in range(400):
         client.add_configuration_setting(
             ConfigurationSetting(
-                key="sample_key_" + str(i),
-                label="sample_label_" + str(i),
+                key=f"sample_key_{str(i)}",
+                label=f"sample_label_{str(i)}",
             )
         )
-    # there will have 4 pages in list result, there are 100 configuration settings per page.
+    # there will have 4 pages while listing, there are 100 configuration settings per page.
     
-    # print page etags
-    print("**********************print page etags*****************************")
+    # get page etags
+    print("**********************get page etags*****************************")
     page_etags = []
     items = client.list_configuration_settings(key_filter="sample_key_*", label_filter="sample_label_*")
-    iterator = items.by_page() # azure.core.paging.PageIterator obj
+    iterator = items.by_page()
     for page in iterator:
         etag = iterator._response.http_response.headers['Etag']
         page_etags.append(etag)
         print(f"ETag: {etag}")
     
+    # ETag: "P3Oae4jiSypN6U1OApprRj2k7548_x3IYkn6NHp-wAU" next_link: /kv?key=sample_key_*&label=sample_label_*&api-version=2023-10-01&after=c2FtcGxlX2tleV8xODgKc2FtcGxlX2xhYmVsXzE4OA%3D%3D
+    # ETag: "x1HUZXjADggqDwAZfSArFfFgZKhLz439uDIpNY80hqc" next_link: /kv?key=sample_key_*&label=sample_label_*&api-version=2023-10-01&after=c2FtcGxlX2tleV8yNzQKc2FtcGxlX2xhYmVsXzI3NA%3D%3D
+    # ETag: "B37CWEtuQhXSOWv2f1T0QBlEMGFUbC1W0UVkKvSAWMM" next_link: /kv?key=sample_key_*&label=sample_label_*&api-version=2023-10-01&after=c2FtcGxlX2tleV8zNjQKc2FtcGxlX2xhYmVsXzM2NA%3D%3D
+    # ETag: "X-rxvugJpqrNoZwapRGx5oHI0wDnQRPiBo_MdWXZzoc" next_link: /kv?key=sample_key_*&label=sample_label_*&api-version=2023-10-01&after=c2FtcGxlX2tleV85NQpzYW1wbGVfbGFiZWxfOTU%3D
+    # ETag: "GoIDT8F8w0Jko6tIF3FgZz5hrPQgCN-WLTxiwK71vhw" next_link: None
+    
     # monitor page updates
-    print("**********************monitor page updates*****************************")
+    print("**********************monitor page before updates*****************************")
     response = client.list_configuration_settings(page_etags=page_etags, key_filter="sample_key_*", label_filter="sample_label_*")
     for page in response:
         if page:
+            print("This page has changes.")
             for item in page:
                 print(f"Key: {item.key}, Label: {item.label}")
         else:
-            print(page)
-    # expected output: [None, None, None, None, None]
+            print("No change found.")
     
     # add a configuration setting
+    print("**********************add a configuration setting*****************************")
     client.add_configuration_setting(
         ConfigurationSetting(
-            key="sample_key_201",
-            label="sample_label_202",
+            key="sample_key_401",
+            label="sample_label_402",
         )
     )
-    # there will have 5 pages in list result, first 4 pages with 100 items and the last page with one item
     
     # print page etags after updates
     print("*****************print page etags after updates**********************************")
     items = client.list_configuration_settings(key_filter="sample_key_*", label_filter="sample_label_*")
-    iterator = items.by_page() # azure.core.paging.PageIterator obj
+    iterator = items.by_page()
     for page in iterator:
         etag = iterator._response.http_response.headers['Etag']
         print(f"ETag: {etag}")
@@ -62,20 +66,14 @@ def main():
     # monitor page updates
     print("**********************monitor page updates*****************************")
     response = client.list_configuration_settings(page_etags=page_etags, key_filter="sample_key_*", label_filter="sample_label_*")
+    # List[ItemPages[ConfigurationSetting]]
     for page in response:
         if page:
+            print("This page has changes.")
             for item in page:
                 print(f"Key: {item.key}, Label: {item.label}")
         else:
-            print(page)
-    # expected output: [<first page items>, <second page items>, <third page items>, <fourth page items>, <fifth page items>]
-    
-    # for page_etag, continuation_token in zip(page_etags, continuation_tokens):
-    #     result = client.monitor_single_page_configuration_settings(page_etag=page_etag, continuation_token=continuation_token, key_filter="sample_key_*", label_filter="sample_label_*")
-    #     if result:
-    #         for configuration_setting in result:
-    #             print(f"Key: {configuration_setting.key}, Label: {configuration_setting.label}")
-    #     print(result)               
+            print("No change found.")          
 
     # clean up
     print("*************************clean up**************************")
@@ -84,6 +82,9 @@ def main():
         client.delete_configuration_setting(item.key, label=item.label)
         count += 1
     print(count)
+    
+    items = client.list_configuration_settings(key_filter="sample_key_*", label_filter="sample_label_*")
+    print(items is None)
 
 
 if __name__ == "__main__":
