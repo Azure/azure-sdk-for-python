9324e6c7dde4293f27ad9c76e2868259fc5631fa
Krista Pratico <krpratic@microsoft.com>
2024-02-19 05:51:58 -0800

    update copyright year

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -87,7 +87,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Azure SDK for Python'
-copyright = u'2019, Microsoft'
+copyright = u'2024, Microsoft'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
