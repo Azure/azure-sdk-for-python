ae0925e89c292f7ecea531fb22aa0ea64f2856c4
azure-sdk <PythonSdkPipelines>
2024-02-22 03:42:26 +0000

    code and test

--- sdk/apicenter/azure-mgmt-apicenter/generated_samples/workspaces_list.py
+++ sdk/apicenter/azure-mgmt-apicenter/generated_samples/workspaces_list.py
@@ -14,7 +14,7 @@ from azure.mgmt.apicenter import ApiCenterMgmtClient
     pip install azure-identity
     pip install azure-mgmt-apicenter
 # USAGE
-    python services_update.py
+    python workspaces_list.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,13 +29,14 @@ def main():
         subscription_id="00000000-0000-0000-0000-000000000000",
     )
 
-    response = client.services.update(
+    response = client.workspaces.list(
         resource_group_name="contoso-resources",
         service_name="contoso",
     )
-    print(response)
+    for item in response:
+        print(item)
 
 
-# x-ms-original-file: specification/apicenter/resource-manager/Microsoft.ApiCenter/preview/2023-07-01-preview/examples/Services_Update.json
+# x-ms-original-file: specification/apicenter/resource-manager/Microsoft.ApiCenter/stable/2024-03-01/examples/Workspaces_List.json
 if __name__ == "__main__":
     main()
