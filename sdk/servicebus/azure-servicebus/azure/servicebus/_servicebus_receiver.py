7c12567e1affce2d17f6c434f8dcf5c30e958418
Libba Lawrence <llawrence@microsoft.com>
2024-02-22 14:24:20 -0800

    move over link credit

--- sdk/servicebus/azure-servicebus/azure/servicebus/_servicebus_receiver.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_servicebus_receiver.py
@@ -167,6 +167,7 @@ class ServiceBusReceiver(
         prefetch_count: int = 0,
         **kwargs: Any,
     ) -> None:
+        self._message_count = 1
         self._session_id = None
         self._message_iter: Optional[Iterator[ServiceBusReceivedMessage]] = None
         self._amqp_transport: "AmqpTransport"
@@ -365,7 +366,7 @@ class ServiceBusReceiver(
             if self._max_wait_time
             else 0,
             # set link_credit to at least 1 so that messages can be received
-            link_credit=self._prefetch_count + 1,
+            link_credit=self._prefetch_count + 1 if self._prefetch_count else self._message_count,
             # If prefetch is "off", then keep_alive coroutine frequently listens on the connection for messages and
             # releases right away, since no "prefetched" messages should be in the internal buffer.
             keep_alive_interval=self._config.keep_alive
@@ -412,6 +413,7 @@ class ServiceBusReceiver(
     ) -> List[ServiceBusReceivedMessage]:
         # pylint: disable=protected-access
         try:
+            self._message_count = max_message_count or 1
             self._receive_context.set()
             self._open()
 
