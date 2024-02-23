dd303683bcd91ad15dbaf184e21e0fa1485326de
Leighton Chen <lechen@microsoft.com>
2024-02-23 12:14:46 -0800

    tests

--- sdk/monitor/azure-monitor-opentelemetry-exporter/tests/quickpulse/test_live_metrics.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/tests/quickpulse/test_live_metrics.py
@@ -38,23 +38,9 @@ class TestLiveMetrics(unittest.TestCase):
 
 class TestQuickpulseManager(unittest.TestCase):
 
-    @mock.patch("opentelemetry.sdk.metrics.MeterProvider.__new__")
-    @mock.patch("azure.monitor.opentelemetry.exporter._quickpulse._exporter._QuickpulseMetricReader.__new__")
-    @mock.patch("opentelemetry.sdk.trace.id_generator.RandomIdGenerator.__new__")
-    @mock.patch("azure.monitor.opentelemetry.exporter._quickpulse._exporter._QuickpulseExporter.__new__")
-    @mock.patch("azure.monitor.opentelemetry.exporter._quickpulse._generated.models.MonitoringDataPoint.__new__")
-    def test_init(self, point_mock, exporter_mock, generator_mock, reader_mock, provider_mock):
-        point_inst_mock = mock.Mock()
-        point_mock.return_value = point_inst_mock
-        exporter_inst_mock = mock.Mock()
-        exporter_mock.return_value = exporter_inst_mock
-        reader_inst_mock = mock.Mock()
-        reader_mock.return_value = reader_inst_mock
-        provider_inst_mock = mock.Mock()
-        provider_mock.return_value = provider_inst_mock
-        generator_inst_mock = mock.Mock()
-        generator_mock.return_value = generator_inst_mock
-        generator_inst_mock.generate_trace_id.return_value = "test_trace_id"
+    @mock.patch("opentelemetry.sdk.trace.id_generator.RandomIdGenerator.generate_trace_id")
+    def test_init(self, generator_mock):
+        generator_mock.return_value = "test_trace_id"
         resource = Resource.create(
             {
                 ResourceAttributes.SERVICE_INSTANCE_ID: "test_instance",
@@ -63,22 +49,32 @@ class TestQuickpulseManager(unittest.TestCase):
         )
         part_a_fields = _populate_part_a_fields(resource)
         qpm = _QuickpulseManager(
-            connection_string="test_cs",
+            connection_string="InstrumentationKey=4321abcd-5678-4efa-8abc-1234567890ac;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/",
             resource=resource,
         )
-        self.assertEqual(qpm._base_monitoring_data_point, point_inst_mock)
-        self.assertEqual(qpm._exporter, exporter_inst_mock)
-        self.assertEqual(qpm._reader, reader_inst_mock)
-        self.assertEqual(qpm._meter_provider, provider_inst_mock)
-        point_mock.assert_called_with(
-            MonitoringDataPoint,
-            version=_get_sdk_version(),
-            invariant_version=1,
-            instance=part_a_fields.get(ContextTagKeys.AI_CLOUD_ROLE_INSTANCE, ""),
-            role_name=part_a_fields.get(ContextTagKeys.AI_CLOUD_ROLE, ""),
-            machine_name=platform.node(),
-            stream_id="test_trace_id",
+        self.assertTrue(isinstance(qpm._exporter, _QuickpulseExporter))
+        self.assertEqual(
+            qpm._exporter._live_endpoint,
+            "https://eastus.livediagnostics.monitor.azure.com/",
         )
-        exporter_mock.assert_called_with(_QuickpulseExporter, "test_cs")
-        reader_mock.assert_called_with(_QuickpulseMetricReader, exporter_inst_mock, point_inst_mock)
-        provider_mock.assert_called_with(MeterProvider, [reader_inst_mock])
+        self.assertEqual(
+            qpm._exporter._instrumentation_key,
+            "4321abcd-5678-4efa-8abc-1234567890ac",
+        )
+        self.assertEqual(qpm._base_monitoring_data_point.version, _get_sdk_version())
+        self.assertEqual(qpm._base_monitoring_data_point.invariant_version, 1)
+        self.assertEqual(
+            qpm._base_monitoring_data_point.instance,
+            part_a_fields.get(ContextTagKeys.AI_CLOUD_ROLE_INSTANCE, "")
+        )
+        self.assertEqual(
+            qpm._base_monitoring_data_point.role_name,
+            part_a_fields.get(ContextTagKeys.AI_CLOUD_ROLE, "")
+        )
+        self.assertEqual(qpm._base_monitoring_data_point.machine_name, platform.node())
+        self.assertEqual(qpm._base_monitoring_data_point.stream_id, "test_trace_id")
+        self.assertTrue(isinstance(qpm._reader, _QuickpulseMetricReader))
+        self.assertEqual(qpm._reader._exporter, qpm._exporter)
+        self.assertEqual(qpm._reader._base_monitoring_data_point, qpm._base_monitoring_data_point)
+        self.assertTrue(isinstance(qpm._meter_provider, MeterProvider))
+        self.assertEqual(qpm._meter_provider._sdk_config.metric_readers, [qpm._reader])
