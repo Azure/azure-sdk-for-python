f7fd254916849b4c6145284280b20169bdec46d8
Sagar Sumant <sasum@microsoft.com>
2024-02-23 07:01:12 -0800

    aoai schema parsing test.

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_finetuning/azure_openai_finetuning.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/_finetuning/azure_openai_finetuning.py
@@ -8,10 +8,13 @@ from azure.ai.ml._schema.core.fields import StringTransformedEnum
 from azure.ai.ml._restclient.v2024_01_01_preview.models import ModelProvider
 from azure.ai.ml._schema._finetuning.azure_openai_hyperparameters import AzureOpenAiHyperparametersSchema
 from azure.ai.ml._schema._finetuning.finetuning_vertical import FineTuningVerticalSchema
+from azure.ai.ml._utils.utils import camel_to_snake
 
 # This is meant to match the yaml definition NOT the models defined in _restclient
 
 
 class AzureOpenAiFineTuningSchema(FineTuningVerticalSchema):
-    model_provider = StringTransformedEnum(required=True, allowed_values=ModelProvider.AZURE_OPEN_AI)
+    model_provider = StringTransformedEnum(
+        required=True, allowed_values=ModelProvider.AZURE_OPEN_AI, casing_transform=camel_to_snake
+    )
     hyperparameters = AzureOpenAiHyperparametersSchema()
--- sdk/ml/azure-ai-ml/tests/finetuning_job/unittests/test_azure_openai_finetuning_job_schema.py
+++ sdk/ml/azure-ai-ml/tests/finetuning_job/unittests/test_azure_openai_finetuning_job_schema.py
@@ -4,7 +4,8 @@ from typing import Dict, cast
 from azure.ai.ml import load_job
 from azure.ai.ml._scope_dependent_operations import OperationScope
 from azure.ai.ml.entities._job.finetuning.finetuning_job import FineTuningJob
-from azure.ai.ml.entities._job.finetuning.custom_model_finetuning_job import CustomModelFineTuningJob
+from azure.ai.ml.entities._job.finetuning.azure_openai_finetuning_job import AzureOpenAIFineTuningJob
+from azure.ai.ml.entities._job.finetuning.azure_openai_hyperparameters import AzureOpenAIHyperparameters
 from azure.ai.ml.entities._inputs_outputs import Input, Output
 from azure.ai.ml._restclient.v2024_01_01_preview.models import (
     FineTuningJob as RestFineTuningJob,
@@ -12,9 +13,9 @@ from azure.ai.ml._restclient.v2024_01_01_preview.models import (
 
 
 @pytest.fixture
-def loaded_custom_model_finetuning_job_full(mock_machinelearning_client: OperationScope) -> FineTuningJob:
+def loaded_azure_openai_model_finetuning_job_full(mock_machinelearning_client: OperationScope) -> FineTuningJob:
     test_schema_path = Path(
-        "./tests/test_configs/finetuning_job/e2e_configs/custom_model_finetuning_job/mock_custom_model_finetuning_job_full.yaml"
+        "./tests/test_configs/finetuning_job/e2e_configs/custom_model_finetuning_job/mock_azure_openai_finetuning_job_full.yaml"
     )
     job = load_job(test_schema_path)
     mock_machinelearning_client.jobs._resolve_arm_id_or_upload_dependencies(job)
@@ -32,33 +33,37 @@ def validation_dataset() -> Input:
 
 
 @pytest.fixture
-def mlflow_model_llama() -> Input:
-    return Input(type="mlflow_model", path="azureml://registries/azureml-meta/models/Llama-2-7b/versions/9")
+def mlflow_model_gpt4() -> Input:
+    return Input(type="mlflow_model", path="azureml://registries/azure-openai-v2/models/gpt4/versions/9")
 
 
 @pytest.fixture
-def hyperparameters() -> Dict[str, str]:
-    return {"per_device_train_batch_size": "1", "learning_rate": "0.00002", "num_train_epochs": "1"}
+def hyperparameters() -> AzureOpenAIHyperparameters:
+    return AzureOpenAIHyperparameters(
+        batch_size=1,
+        learning_rate_multiplier=0.00002,
+        n_epochs=1,
+    )
 
 
 @pytest.fixture
-def expected_custom_model_finetuning_job_full(
-    train_dataset, validation_dataset, mlflow_model_llama, hyperparameters
+def expected_azure_openai_finetuning_job_full(
+    train_dataset, validation_dataset, mlflow_model_gpt4, hyperparameters
 ) -> RestFineTuningJob:
-    custom_model_finetuning_job = CustomModelFineTuningJob(
-        task="text_completion",
-        model=mlflow_model_llama,
+    custom_model_finetuning_job = AzureOpenAIFineTuningJob(
+        task="chat_completion",
+        model=mlflow_model_gpt4,
         training_data=train_dataset,
         validation_data=validation_dataset,
-        name="simple_custom_model_finetuning_job",
+        name="simple_azure_openai_finetuning_job",
         hyperparameters=hyperparameters,
         tags={"foo_tag": "bar"},
         properties={"my_property": "my_value"},
-        experiment_name="llama-finetuning-experiment",
-        display_name="llama-display-name-1234",
-        outputs={"registered_model": Output(type="mlflow_model", name="llama-finetune-registered-1234")},
+        experiment_name="gpt4-finetuning-experiment",
+        display_name="gpt4-display-name-1234",
+        outputs={"registered_model": Output(type="mlflow_model", name="gpt4-finetune-registered-1234")},
     )
-    return CustomModelFineTuningJob._to_rest_object(custom_model_finetuning_job)
+    return AzureOpenAIFineTuningJob._to_rest_object(custom_model_finetuning_job)
 
 
 class TestCustomModelFineTuningJobSchema:
@@ -72,11 +77,11 @@ class TestCustomModelFineTuningJobSchema:
         assert mlflow_model.type == "mlflow_model"
 
     def test_custom_model_finetuning_job_full(
-        self, expected_custom_model_finetuning_job_full, loaded_custom_model_finetuning_job_full
+        self, expected_azure_openai_finetuning_job_full, loaded_azure_openai_model_finetuning_job_full
     ):
-        self._validate_finetuning_job(loaded_custom_model_finetuning_job_full)
+        self._validate_finetuning_job(loaded_azure_openai_model_finetuning_job_full)
 
         assert (
-            loaded_custom_model_finetuning_job_full._to_rest_object().as_dict()
-            == expected_custom_model_finetuning_job_full.as_dict()
+            loaded_azure_openai_model_finetuning_job_full._to_rest_object().as_dict()
+            == expected_azure_openai_finetuning_job_full.as_dict()
         )
