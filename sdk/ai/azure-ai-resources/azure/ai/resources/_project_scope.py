7114df4931740ea5cab52dbda4f3d43161ae3e06
Miles Holland <milesholland@microsoft.com>
2024-02-26 14:46:34 -0500

    rerun tests

--- sdk/ai/azure-ai-resources/azure/ai/resources/_project_scope.py
+++ sdk/ai/azure-ai-resources/azure/ai/resources/_project_scope.py
@@ -11,6 +11,6 @@ class OperationScope(object):
 
     subscription_id: str
     resource_group_name: str
-    ai_resource_name: Optional[str] = None
+    ai_hub_name: Optional[str] = None
     project_name: Optional[str] = None
     registry_name: Optional[str] = None
