50e902ee5e834c50409c7e440f3faf9b00a5fae5
qunsong <qunsong@microsoft.com>
2024-02-29 11:14:59 -0800

    fix bug in validate_conversation of chat eval flow

--- sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/pf_templates/built_in_metrics/chat/validate_conversation.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/pf_templates/built_in_metrics/chat/validate_conversation.py
@@ -7,11 +7,12 @@ from utils import filter_metrics, is_conversation_valid, is_conversation_valid_w
 def is_metric_group_selected(selected_metrics: dict) -> dict:
     group_selected = {}
     for metric_group in selected_metrics:
+        group_selected[metric_group] = False
         for metric in selected_metrics[metric_group]:
             if selected_metrics[metric_group][metric]:
                 group_selected[metric_group] = True
                 break
-    return group_selected 
+    return group_selected
 
                 
 # The inputs section will change based on the arguments of the tool function, after you save the code
