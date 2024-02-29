2472f3aa58503ff0d5ec60ec89d76be18112d41b
jerevoss <jerevoss@gmail.com>
2024-02-29 13:30:01 -0800

    Update exporter version

--- sdk/monitor/azure-monitor-opentelemetry/setup.py
+++ sdk/monitor/azure-monitor-opentelemetry/setup.py
@@ -87,7 +87,7 @@ setup(
     install_requires=[
         "azure-core<2.0.0,>=1.24.0",
         "azure-core-tracing-opentelemetry~=1.0.0b10",
-        "azure-monitor-opentelemetry-exporter~=1.0.0b22",
+        "azure-monitor-opentelemetry-exporter~=1.0.0b23",
         "opentelemetry-instrumentation-django~=0.42b0",
         "opentelemetry-instrumentation-fastapi~=0.42b0",
         "opentelemetry-instrumentation-flask~=0.42b0",
