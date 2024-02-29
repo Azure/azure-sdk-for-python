51424655a21401309678fe3b3c30fce6ccab35c7
l0lawrence <llawrence@microsoft.com>
2024-02-29 13:42:58 -0800

    sample

--- sdk/eventgrid/azure-eventgrid/samples/sync_samples/sample_one_client.py
+++ sdk/eventgrid/azure-eventgrid/samples/sync_samples/sample_one_client.py
@@ -12,13 +12,42 @@ from azure.core.messaging import CloudEvent
 from azure.core.exceptions import HttpResponseError
 
 
+EVENTGRID_KEY_GA: str = os.environ["EVENTGRID_KEY_GA"]
+EVENTGRID_ENDPOINT_GA: str = os.environ["EVENTGRID_ENDPOINT_GA"]
 EVENTGRID_KEY: str = os.environ["EVENTGRID_KEY"]
 EVENTGRID_ENDPOINT: str = os.environ["EVENTGRID_ENDPOINT"]
 TOPIC_NAME: str = os.environ["EVENTGRID_TOPIC_NAME"]
 EVENT_SUBSCRIPTION_NAME: str = os.environ["EVENTGRID_EVENT_SUBSCRIPTION_NAME"]
 
-# Create a client
+# Create a NameSpace client
 client = EventGridClient(EVENTGRID_ENDPOINT, AzureKeyCredential(EVENTGRID_KEY))
 
-# Create a publisherClient
+# Create a client using publisherClient naming, will create a NamespaceClient
 client = EventGridPublisherClient(EVENTGRID_ENDPOINT, AzureKeyCredential(EVENTGRID_KEY))
+
+# Try to create a basic client with the wrong endpoint
+try:
+    client_basic = EventGridClient(EVENTGRID_ENDPOINT, AzureKeyCredential(EVENTGRID_KEY), level="Basic")
+except ValueError as e:
+    print(e)
+
+# Create a Basic Client with the correct endpoint
+client_basic = EventGridClient(EVENTGRID_ENDPOINT_GA, AzureKeyCredential(EVENTGRID_KEY_GA), level="Basic")
+
+
+# Publish an event to a topic using basic client
+event = CloudEvent(data={"key": "value"}, type="Contoso.Items.ItemReceived", source="https://contoso.com/items")
+client_basic.publish(TOPIC_NAME, event)
+
+# Publish an event to a topic using standard client
+client.publish(TOPIC_NAME, event)
+
+
+# Receive events from EventGrid with a basic client - raise error
+try:
+    client_basic.receive_cloud_events(TOPIC_NAME, EVENT_SUBSCRIPTION_NAME)
+except ValueError as e:
+    print(e)
+
+# Receive events from EventGrid standard client
+    client.receive_cloud_events(TOPIC_NAME, EVENT_SUBSCRIPTION_NAME)
