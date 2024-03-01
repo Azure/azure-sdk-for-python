387b565146b304455301c9734c4aa04f06e97015
megheshpatil <mepatil@microsoft.com>
2024-03-01 10:55:29 -0800

    updated code to fix error

--- sdk/communication/azure-communication-phonenumbers/azure/communication/phonenumbers/_phone_numbers_client.py
+++ sdk/communication/azure-communication-phonenumbers/azure/communication/phonenumbers/_phone_numbers_client.py
@@ -438,24 +438,25 @@ class PhoneNumbersClient(object):
         )
 
     @distributed_trace
-    def search_operator_information( #type: OperatorInformationResult
+    def search_operator_information(
         self,
         phone_numbers,  # type: PhoneNumberSearchType
-        options:Optional[OperatorInformationOptions]=None, #type: OperatorInformationOptions
+        options: Optional[OperatorInformationOptions] = None,
         **kwargs  # type: Any
     ) -> OperatorInformationResult:
-        """Searches for operator information for a given list of phone numbers.
+        """
+        Searches for operator information for a given list of phone numbers.
 
         :param phone_numbers: The phone number(s) whose operator information should be searched
-        :type phone_numbers: str or list[str]
+        :type phone_numbers: str or List[str]
         :return: A search result containing operator information associated with the requested phone numbers
         :rtype: ~azure.communication.phonenumbers.models.OperatorInformationResult
         """
         if not isinstance(phone_numbers, list):
-            phone_numbers = cast(PhoneNumberSearchType, [ phone_numbers ])
+            phone_numbers = cast(PhoneNumberSearchType, [phone_numbers])
         if options is None:
             options = OperatorInformationOptions(include_additional_phone_and_operator_details=False)
-        request = OperatorInformationRequest(phone_numbers = phone_numbers, options=options)
+        request = OperatorInformationRequest(phone_numbers=phone_numbers, options=options)
         return self._phone_number_client.phone_numbers.operator_information_search(
             request,
             **kwargs
