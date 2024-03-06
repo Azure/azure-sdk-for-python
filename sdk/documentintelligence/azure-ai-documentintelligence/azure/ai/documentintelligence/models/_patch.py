21cdf29a0c6c7ac3ab8b640a2af2ab5607d72f72
YalinLi0312 <yall@microsoft.com>
2024-03-05 16:55:13 -0800

    Update _patch.py

--- sdk/documentintelligence/azure-ai-documentintelligence/azure/ai/documentintelligence/models/_patch.py
+++ sdk/documentintelligence/azure-ai-documentintelligence/azure/ai/documentintelligence/models/_patch.py
@@ -6,73 +6,41 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
-from typing import Any, List, Optional, overload
+from typing import List, Optional
 from ._models import (
     AnalyzeDocumentRequest as GeneratedAnalyzeDocumentRequest,
     ClassifyDocumentRequest as GeneratedClassifyDocumentRequest,
 )
-from .._model_base import rest_discriminator, rest_field
+from .._model_base import rest_field
 
 
 class AnalyzeDocumentRequest(GeneratedAnalyzeDocumentRequest):
     """Document analysis parameters.
 
-    :ivar url_source: Document URL to analyze.  Either urlSource or bytesSource must be specified.
+    :ivar url_source: Document URL to analyze.  Either url_source or bytes_source must be specified.
     :vartype url_source: str
-    :ivar bytes_source: Document bytes to analyze.  Either urlSource or bytesSource must be specified.
+    :ivar bytes_source: Document bytes to analyze.  Either url_source or bytes_source must be specified.
     :vartype bytes_source: bytes
     """
-
-    url_source: Optional[str] = rest_field(name="urlSource")
-    """Document URL to analyze.  Either urlSource or bytesSource must be specified."""
+    
     bytes_source: Optional[bytes] = rest_field(name="base64Source", format="base64")
-    """Document bytes to analyze.  Either urlSource or bytesSource must be specified."""
-
-    @overload
-    def __init__(
-        self,
-        *,
-        url_source: Optional[str] = None,
-        bytes_source: Optional[bytes] = None,
-    ):
-        ...
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:  # pylint: disable=useless-super-delegation
-        super().__init__(*args, **kwargs)
+    """Document bytes to analyze.  Either url_source or bytes_source must be specified."""
 
 
 class ClassifyDocumentRequest(GeneratedClassifyDocumentRequest):
     """Document classification parameters.
 
-    :ivar url_source: Document URL to classify.  Either urlSource or bytesSource must be
+    :ivar url_source: Document URL to classify.  Either url_source or bytes_source must be
      specified.
     :vartype url_source: str
-    :ivar bytes_source: Document bytes to classify.  Either urlSource or bytesSource must be specified.
+    :ivar bytes_source: Document bytes to classify.  Either url_source or bytes_source must be specified.
     :vartype bytes_source: bytes
     """
 
-    url_source: Optional[str] = rest_field(name="urlSource")
-    """Document URL to classify.  Either urlSource or bytesSource must be specified."""
     bytes_source: Optional[bytes] = rest_field(name="base64Source", format="base64")
-    """Document bytes to classify.  Either urlSource or bytesSource must be specified."""
+    """Document bytes to classify.  Either url_source or bytes_source must be specified."""
 
-    @overload
-    def __init__(
-        self,
-        *,
-        url_source: Optional[str] = None,
-        bytes_source: Optional[bytes] = None,
-    ):
-        ...
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:  # pylint: disable=useless-super-delegation
-        super().__init__(*args, **kwargs)
-
-
-__all__: List[str] = [
-    "AnalyzeDocumentRequest",
-    "ClassifyDocumentRequest",
-]  # Add all objects you want publicly available to users at this package level
+__all__: List[str] = ["AnalyzeDocumentRequest", "ClassifyDocumentRequest"]  # Add all objects you want publicly available to users at this package level
 
 
 def patch_sdk():
