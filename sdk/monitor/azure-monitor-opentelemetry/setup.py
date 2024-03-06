9b7e1b0c534c0aff76438c1625e10a2deba1892b
Xiang Yan <xiangyan@microsoft.com>
2024-03-06 14:48:59 -0800

    update

--- sdk/monitor/azure-monitor-opentelemetry/setup.py
+++ sdk/monitor/azure-monitor-opentelemetry/setup.py
@@ -62,7 +62,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
