040a67341de24a55ddc3047c2b9fc73605b18678
guoqing2023 <124081795+guoqing2023@users.noreply.github.com>
2024-03-05 18:36:37 -0600

    Update setup.py
    upgrade the azure.core package

--- sdk/communication/azure-communication-phonenumbers/setup.py
+++ sdk/communication/azure-communication-phonenumbers/setup.py
@@ -66,7 +66,7 @@ setup(
     python_requires=">=3.7",
     install_requires=[
         "msrest>=0.7.1",
-        'azure-core<2.0.0,>=1.24.0',
+        'azure-core<2.0.0,>=1.28.0',
     ],
     extras_require={
         ":python_version<'3.8'": ["typing-extensions"]
