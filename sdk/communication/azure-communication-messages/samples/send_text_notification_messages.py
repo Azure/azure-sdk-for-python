0bbf9d3c479bf497da751ccf58f03fc33e01fd1b
Shamsha Khan <shamkh@microsoft.com>
2024-03-07 14:19:48 -0800

    Update sdk/communication/azure-communication-messages/samples/send_text_notification_messages.py
    Co-authored-by: Anna Tisch <antisch@microsoft.com>

--- sdk/communication/azure-communication-messages/samples/send_text_notification_messages.py
+++ sdk/communication/azure-communication-messages/samples/send_text_notification_messages.py
@@ -41,7 +41,7 @@ class SendWhatsAppMessageSample(object):
 
         text_options = TextNotificationContent(
             channel_registration_id=self.channel_id,
-            to= [self.phone_number], 
+            to=[self.phone_number], 
             content="Hello World via Notification Messaging SDK.",
         )
         
