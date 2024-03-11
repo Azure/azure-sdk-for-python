755666b397c5bbd8d43afb58e8ceba1a5a0c3385
simorenoh <simonmorenohe@gmail.com>
2024-03-11 11:08:44 -0700

    Update exceptions.py

--- sdk/cosmos/azure-cosmos/azure/cosmos/exceptions.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/exceptions.py
@@ -76,8 +76,7 @@ class CosmosBatchOperationError(HttpResponseError):
     :ivar message: Error message.
     :vartype message: str
     :ivar operation_responses: List of failed operations' responses.
-    :vartype operation_responses:
-        Sequence[Union[Tuple[str, Tuple[Any, ...]], Tuple[str, Tuple[Any, ...], Dict[str, Any]]]]
+    :vartype operation_responses: List[dict[str, Any]]
     .. admonition:: Example:
 
         .. literalinclude:: ../samples/document_management.py
