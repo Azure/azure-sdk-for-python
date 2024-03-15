cbece6444f2b71a49fef001024bab8ec424427e6
ChenxiJiang333 <119990644+ChenxiJiang333@users.noreply.github.com>
2024-03-15 10:24:49 +0800

    Update setup.py

--- sdk/communication/azure-mgmt-communication/setup.py
+++ sdk/communication/azure-mgmt-communication/setup.py
@@ -77,7 +77,6 @@ setup(
         "isodate<1.0.0,>=0.6.1",
         "azure-common~=1.1",
         "azure-mgmt-core>=1.3.2",
-        "typing-extensions>=4.3.0; python_version<'3.8.0'",
     ],
     python_requires=">=3.8",
 )
