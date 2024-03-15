066cab425eeb98d48290f9a404813640ddce636e
Julia Wang <juwang@microsoft.com>
2024-03-15 12:13:02 -0700

    add comments

--- sdk/ml/azure-ai-ml/azure/ai/ml/_utils/_download_model.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_utils/_download_model.py
@@ -55,6 +55,10 @@ def download_blob_to_local(blob_sas_url, local_folder):
         print(f"Error copying blob to local folder: {e}")
         return None
 
+# sample parameters
+# modelAssetId = "azureml://registries/testFeed/models/testModelb67845316abe49839cec18f3ab61fd8f/versions/13"
+# localDestinationPath = "C:\\PythonVirtualEnv\\venv1\\BlobDownLoad"
+# gitHubAuthToken = "ghu_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 def download_model(model_asset_id, github_auth_token, local_destination_path):
     try:
         model_entity, blob_sas_uri = get_registry_model_details_for_non_azure_accounts(model_asset_id, github_auth_token)
