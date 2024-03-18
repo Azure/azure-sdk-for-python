587fa9933daafbb42997f0e395efafd2234cd894
kdestin <101366538+kdestin@users.noreply.github.com>
2024-03-18 19:19:35 -0400

    refactor: Make DataAsset params keyword only

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_deployment/data_asset.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_deployment/data_asset.py
@@ -13,14 +13,15 @@ from azure.ai.ml.constants._common import BASE_PATH_CONTEXT_KEY
 class DataAsset:
     """Data Asset entity
 
-    :ivar str data_id: Arm id of registered data asset
-    :ivar str name: Name of data asset
-    :ivar str path: Path where the data asset is stored.
-    :ivar int version: Version of data asset.
+    :keyword str data_id: Arm id of registered data asset
+    :keyword str name: Name of data asset
+    :keyword str path: Path where the data asset is stored.
+    :keyword int version: Version of data asset.
     """
 
     def __init__(
         self,
+        *,
         data_id: Optional[str] = None,
         name: Optional[str] = None,
         path: Optional[str] = None,
