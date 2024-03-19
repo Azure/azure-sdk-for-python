53ba5aa79a3128578359526bfc8be6105677a112
qusongms <142928570+qusongms@users.noreply.github.com>
2024-03-19 14:45:38 -0700

    modify built-in chat eval flow: (#34511)
    * modify built-in chat eval flow:
    
    * modify rai service request format
    * fix aggregation logic
    * add input validation for rai service
    
    * fix bug in validate_conversation of chat eval flow

--- sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/pf_templates/built_in_metrics/chat/utils.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/pf_templates/built_in_metrics/chat/utils.py
@@ -1,6 +1,9 @@
 from promptflow.connections import AzureOpenAIConnection
 import constants
 import numpy as np
+from azureml.metrics.common import _validation
+from azureml.metrics.common.contract import Contract
+from azureml.metrics.common.exceptions import InvalidOperationException
 
 def get_openai_parameters(connection: AzureOpenAIConnection, deployment_name: str) -> dict:
     openai_params = {
@@ -55,3 +58,42 @@ def get_harm_severity_level(harm_score: int) -> str:
         if harm_score >= harm_score_range[0] and harm_score <= harm_score_range[1]:
             return harm_level.name
     return np.nan
+
+def is_conversation_valid(chat: []) -> bool:
+    reference_code = "validate_conversation"
+    name = "chat_format"
+    # check if role and content keys exist in every turn
+    _validation._check_chat_conversation([chat], name, reference_code=reference_code)
+    return True
+
+def is_conversation_valid_with_context(chat: []) -> bool:
+    reference_code = "validate_conversation"
+    name = "chat_context_format"
+
+    # check if context/documents keys exist for rag evaluation
+    for turn_num, each_turn in enumerate(chat):
+        # to accept legacy rag_evaluation format:
+        # [{"user": {"content": "<user_content>"}, 
+        #  "assistant": {"content": "<assistang_content>"}, 
+        #  "retrieved_documents": "<retrieved_documents>"}]
+        if "user" in each_turn and "assistant" in each_turn: # legancy rag_evaluation format
+            Contract.assert_true("retrieved_documents" in each_turn, 
+                message = "Please ensure to have retrieved_documents key in each turn for rag_evaluation."
+                    + " Please check turn_number: {}".format(turn_num),
+                target=name, log_safe=True, 
+                reference_code = reference_code)
+        elif "role" in each_turn and each_turn["role"] == "assistant":
+            #if "context" not in each_turn:
+            Contract.assert_true("context" in each_turn, 
+                message = "Please ensure to have context key in assistant turn for rag_evaluation."
+                    + " Please check turn_number: {}".format(turn_num),
+                target=name, log_safe=True, 
+                reference_code = reference_code)
+            if "context" in each_turn: #and "citations" not in each_turn["context"]:
+                Contract.assert_true("citations" in each_turn["context"], 
+                message = "Please ensure to have citations key in assistant turn context for rag_evaluation."
+                    + " Please check turn_number: {}".format(turn_num),
+                target=name, log_safe=True, 
+                reference_code = reference_code)
+
+    return True
