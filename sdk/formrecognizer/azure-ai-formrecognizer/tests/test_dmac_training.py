d6d6c1e829be72060713b178d40b1c828e143496
mccoyp <mcpatino@microsoft.com>
2024-03-18 18:12:12 -0700

    [skip ci] Delete azure-devtools and deps/references

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/test_dmac_training.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/test_dmac_training.py
@@ -270,7 +270,7 @@ class TestDMACTraining(FormRecognizerTest):
         poller = client.begin_build_document_model("template", blob_container_url=None, continuation_token=cont_token)
         result = poller.result()
         assert result
-        initial_poller.wait()  # necessary so azure-devtools doesn't throw assertion error
+        initial_poller.wait()  # necessary so devtools_testutils doesn't throw assertion error
 
     @skip_flaky_test
     @FormRecognizerPreparer()
