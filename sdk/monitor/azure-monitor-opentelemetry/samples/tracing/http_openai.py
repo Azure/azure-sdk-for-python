d6a40635d36b8e3b4da7daf5ca7253a926487ff2
jerevoss <jerevoss@gmail.com>
2024-03-20 15:17:13 -0700

    Add openai sample

---
+++ sdk/monitor/azure-monitor-opentelemetry/samples/tracing/http_openai.py
@@ -0,0 +1,30 @@
+from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
+
+# The HTTPX instrumentation captures http requests made in the OpenAI library.
+# For openai>=1.4.0, it is important to instrument httpx before importing openai.
+HTTPXClientInstrumentor().instrument()
+
+# import openai # For openai==0.x
+from openai import OpenAI # For openai==1.x
+from opentelemetry.instrumentation.openai import OpenAIInstrumentor
+
+from azure.monitor.opentelemetry import configure_azure_monitor
+
+configure_azure_monitor()
+
+# The OpenAI Instrumation is not officially part of the OpenTelemetry community.
+# Therefore, we cannot atest to its stability. Use with appropiate caution.
+OpenAIInstrumentor().instrument()
+
+client = OpenAI() # For openai==1.x
+completion = client.chat.completions.create( # For openai==1.x
+# completion = openai.ChatCompletion.create( # For openai==0.x
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+print(completion.choices[0].message)
+
+input()
