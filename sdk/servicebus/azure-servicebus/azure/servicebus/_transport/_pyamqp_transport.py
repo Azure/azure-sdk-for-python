5186689ce8a281655265678875078135289d443d
Libba Lawrence <llawrence@microsoft.com>
2024-03-20 08:42:16 -0700

    pylint

--- sdk/servicebus/azure-servicebus/azure/servicebus/_transport/_pyamqp_transport.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_transport/_pyamqp_transport.py
@@ -767,7 +767,7 @@ class PyamqpTransport(AmqpTransport):   # pylint: disable=too-many-public-method
         :param int link_credit: Total link credit wanted.
         :rtype: None
         """
-        link_credit_needed = link_credit - handler._link.current_link_credit
+        link_credit_needed = link_credit - handler._link.current_link_credit # pylint: disable=protected-access
         if link_credit_needed > 0:
             handler._link.flow(link_credit=link_credit_needed) # pylint: disable=protected-access
 
