ea5b59a765fa0d8c1383a6c1bd4e9867ec3fbe19
Michal Materna <michal.materna@hotmail.com>
2024-03-25 10:56:49 -0700

    [Text Translation] Add support for AAD authentication (#34883)
    * [Text Translation] Add support for AAD authentication
    
    * Fixing build
    
    * Fixing PR comments
    
    * Update sdk/translation/azure-ai-translation-text/azure/ai/translation/text/_patch.py
    
    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>
    
    * Update sdk/translation/azure-ai-translation-text/azure/ai/translation/text/_patch.py
    
    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>
    
    * Update sdk/translation/azure-ai-translation-text/azure/ai/translation/text/_patch.py
    
    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>
    
    * Fix PR comments
    
    * Update sdk/translation/azure-ai-translation-text/azure/ai/translation/text/_patch.py
    
    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>
    
    * Update sdk/translation/azure-ai-translation-text/azure/ai/translation/text/aio/_patch.py
    
    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>
    
    * fix types
    
    * Update sdk/translation/azure-ai-translation-text/azure/ai/translation/text/_patch.py
    
    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>
    
    * Fix build
    
    ---------
    
    Co-authored-by: Michal Materna <mimat@microsoft.com>
    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>

--- sdk/translation/azure-ai-translation-text/azure/ai/translation/text/aio/__init__.py
+++ sdk/translation/azure-ai-translation-text/azure/ai/translation/text/aio/__init__.py
@@ -6,13 +6,14 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._patch import TextTranslationClient
+from ._patch import TextTranslationClient, AsyncTranslatorAADCredential
 
 
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
     "TextTranslationClient",
+    "AsyncTranslatorAADCredential"
 ]
 
 
