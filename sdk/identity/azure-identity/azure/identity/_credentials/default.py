69a2e356af6288bdf5626c7db5566361a83fbbc7
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2024-03-25 11:36:50 -0500

    Update Azure AD and docs.microsoft.com links (#34913)

--- sdk/identity/azure-identity/azure/identity/_credentials/default.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/default.py
@@ -204,7 +204,7 @@ class DefaultAzureCredential(ChainedTokenCredential):
 
         :param str scopes: desired scopes for the access token. This method requires at least one scope.
             For more information about scopes, see
-            https://learn.microsoft.com/azure/active-directory/develop/scopes-oidc.
+            https://learn.microsoft.com/entra/identity-platform/scopes-oidc.
         :keyword str claims: additional claims required in the token, such as those returned in a resource provider's
             claims challenge following an authorization failure.
         :keyword str tenant_id: optional tenant to include in the token request.
