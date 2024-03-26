f3b3d3bf313a34c4d91a66c29cdb1483ce036379
Nagkumar Arkalgud <nagkumar91@users.noreply.github.com>
2024-03-25 21:51:03 -0700

    adding max_simulation_results for sync call (#34923)
    * adding max_simulation_results for sync call
    
    * # pylint: skip-file
    
    * # pylint: disable=E0611
    
    * # pylint: disable=E0611

--- sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_local_flow_handler.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/_local_flow_handler.py
@@ -1,6 +1,8 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+# pylint: disable=E0611
+
 import logging
 import pandas as pd
 
