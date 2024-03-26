9973a202caea3ea7a8da85e3d6dc7d0305b7fb2e
qusongms <142928570+qusongms@users.noreply.github.com>
2024-03-25 23:57:21 -0700

    Qunsong/qa eval groundedness service (#34797)
    * modify built-in qa evaluation flow
    
    * move groundedness evaluation to groundedness evaluation service
    * change content harm level "Safe" to "Very low"
    
    * add default groundedness results to qa eval flow
    
    * modify built-in qa evaluation flow:
    
    * add logic to check service availability in a region
    * change hate_fairness to hate_unfairness
    
    * add gpt_groundedness to qa node list
    
    * update built-in qa evaluation flow
    
    * add flight control to flow input
    * code flake8 cleaning
    
    * round f1_score in built_in qa eval flow
    
    * metric name update
    
    * update e2e test of qa built-in evaluation flow
    
    * update built-in qa evaluation flow
    
    * fix fallback check logic in validate_groundedness_service
    * add e2e test of fallback groundedness

--- sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/pf_templates/built_in_metrics/qa/call_groundedness_service.py
+++ sdk/ai/azure-ai-generative/azure/ai/generative/evaluate/pf_templates/built_in_metrics/qa/call_groundedness_service.py
@@ -3,7 +3,7 @@ from rai_client import RAIServiceHandler
 
 
 @tool
-def call_rai_service(request_body: dict) -> dict:
+def call_groundedness_service(request_body: dict) -> [dict]:
     service_handler = RAIServiceHandler()
     annotation_results = service_handler.get_annotation(request_body)
     return annotation_results
