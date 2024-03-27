08c23ba875be9ec91dac2c3bcb14b3913da9e6ae
Libba Lawrence <llawrence@microsoft.com>
2024-03-27 14:27:48 -0700

    kwargs.pop

--- sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/aio/_client_async.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/aio/_client_async.py
@@ -736,7 +736,7 @@ class ReceiveClientAsync(ReceiveClientSync, AMQPClientAsync):
         :rtype: bool
         """
         try:
-            if self._link.current_link_credit <= 0 and kwargs.get("flow", True):
+            if self._link.current_link_credit <= 0 and kwargs.pop("flow", True):
                 await self._link.flow(link_credit=self._link_credit)
             await self._connection.listen(wait=self._socket_timeout, **kwargs)
         except ValueError:
