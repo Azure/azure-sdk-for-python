bbb1f0b42cb2e3131f81462456fe31e971299807
Xiang Yan <xiangyan@microsoft.com>
2024-03-29 13:40:13 -0700

    update

--- tools/azure-sdk-tools/ci_tools/dependency_analysis.py
+++ tools/azure-sdk-tools/ci_tools/dependency_analysis.py
@@ -57,6 +57,7 @@ def get_lib_deps(base_dir: str) -> Tuple[Dict[str, Dict[str, Any]], Dict[str, Di
     packages = {}
     dependencies = {}
     for lib_dir in discover_targeted_packages("azure*", base_dir):
+        print(f"Processing {lib_dir}")
         try:
             setup_path = os.path.join(lib_dir, "setup.py")
             parsed = ParsedSetup.from_path(setup_path)
@@ -65,6 +66,7 @@ def get_lib_deps(base_dir: str) -> Tuple[Dict[str, Dict[str, Any]], Dict[str, Di
             packages[lib_name] = {"version": version, "source": lib_dir, "deps": []}
 
             for req in requires:
+                print(f"  Processing {lib_name} requires {req}")
                 req_name, spec = parse_require(req)
                 if spec is None:
                     spec = ""
@@ -87,11 +89,13 @@ def get_wheel_deps(wheel_dir: str) -> Tuple[Dict[str, Dict[str, Any]], Dict[str,
             with WheelFile(whl_path) as whl:
                 pkg_info = read_pkg_info_bytes(whl.read(whl.dist_info_path + "/METADATA"))
                 lib_name = pkg_info.get("Name")
+                print(f"Processing whl {lib_name}")
 
                 packages[lib_name] = {"version": pkg_info.get("Version"), "source": whl_path, "deps": []}
 
                 requires = pkg_info.get_all("Requires-Dist")
                 for req in requires:
+                    print(f"  Processing whl {lib_name} requires {req}")
                     req = req.split(";")[0]  # Extras conditions appear after a semicolon
                     req = re.sub(r"[\s\(\)]", "", req)  # Version specifiers appear in parentheses
                     req_name, spec = parse_require(req)
@@ -312,15 +316,15 @@ def analyze_dependencies() -> None:
     upper_bound = False
     for requirement in sorted(dependencies.keys()):
         if requirement in approved_upper_bound_constraints:
-            print(f"Skipping approved upper bound constraint for {requirement}")
+            print(f"  Skipping approved upper bound constraint for {requirement}")
             continue
-        print(f"Checking {requirement}")
+        print(f"  Checking {requirement}")
         specs = dependencies[requirement]
         for spec in specs.keys():
-            print(f"Checking {requirement} {spec}")
+            print(f"    Checking {requirement} {spec}")
             if any(s in spec for s in upper_bound_operators):
                 # There is a upper bound on the version, so we can't guarantee compatibility
-                print(f"Upper bound constraint found in {requirement} {spec}")
+                print(f"    Upper bound constraint found in {requirement} {spec}")
                 upper_bounds.append(requirement)
                 upper_bound = True
     if upper_bound:        
