06eda909bb2e65fb93dfc2c9c3266f565024661f
Libba Lawrence <llawrence@microsoft.com>
2024-04-02 09:25:44 -0700

    uncomment

--- sdk/servicebus/azure-servicebus/samples/sync_samples/batch_delete.py
+++ sdk/servicebus/azure-servicebus/samples/sync_samples/batch_delete.py
@@ -55,8 +55,8 @@ with servicebus_client:
         new_time = datetime.now(timezone.utc) + timedelta(hours=10)
         print(f"Deleting messages that are older than {new_time}")
         deleted_msgs = receiver.delete_messages(
-            # max_message_count=200,
-            # before_enqueued_time_utc=new_time
+            max_message_count=200,
+            before_enqueued_time_utc=new_time
         )
         print(f"{deleted_msgs} messages deleted.")
 
