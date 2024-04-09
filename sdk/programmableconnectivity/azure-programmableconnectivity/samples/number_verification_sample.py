6c416ee1cc93287c7a999f76d37c5035b106424d
Aron Hegedus <aronhegedus@microsoft.com>
2024-04-09 14:32:25 +0100

    add copyright headers and rename snippets

--- sdk/programmableconnectivity/azure-programmableconnectivity/samples/number_verification_sample.py
+++ sdk/programmableconnectivity/azure-programmableconnectivity/samples/number_verification_sample.py
@@ -1,3 +1,8 @@
+## ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
 from azure.programmableconnectivity import ProgrammableConnectivityClient
 from azure.identity import DefaultAzureCredential
 from azure.core.exceptions import HttpResponseError
