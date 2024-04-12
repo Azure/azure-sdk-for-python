c5f299226bcbb282de08485d1b04f7259f1b8a9e
azure-sdk <PythonSdkPipelines>
2024-04-12 01:12:15 +0000

    code and test

--- sdk/quota/azure-mgmt-quota/generated_samples/get_network_one_sku_quota_limit.py
+++ sdk/quota/azure-mgmt-quota/generated_samples/get_network_one_sku_quota_limit.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.quota import QuotaMgmtClient
 
 """
@@ -26,6 +27,7 @@ from azure.mgmt.quota import QuotaMgmtClient
 def main():
     client = QuotaMgmtClient(
         credential=DefaultAzureCredential(),
+        subscription_id="SUBSCRIPTION_ID",
     )
 
     response = client.quota.get(
@@ -35,6 +37,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/quota/resource-manager/Microsoft.Quota/stable/2023-02-01/examples/getNetworkOneSkuQuotaLimit.json
+# x-ms-original-file: specification/quota/resource-manager/Microsoft.Quota/preview/2023-06-01-preview/examples/getNetworkOneSkuQuotaLimit.json
 if __name__ == "__main__":
     main()
