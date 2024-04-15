778ce4b8e4d4d09ed97582d2c17dbc455fcd2d91
Krista Pratico <krpratic@microsoft.com>
2024-04-15 15:05:41 -0700

    line too long

--- sdk/cognitivelanguage/azure-ai-language-questionanswering/azure/ai/language/questionanswering/_operations/_patch.py
+++ sdk/cognitivelanguage/azure-ai-language-questionanswering/azure/ai/language/questionanswering/_operations/_patch.py
@@ -216,7 +216,11 @@ class QuestionAnsweringClientOperationsMixin(QuestionAnsweringClientOperationsMi
     # pylint ignore b/c with overloads we need to doc ALL the params in the impl for them to show up in docs
     # pylint: disable=docstring-keyword-should-match-keyword-only,docstring-missing-param,docstring-should-be-keyword
     @distributed_trace
-    def get_answers(self, *args: AnswersOptions, **kwargs: Any) -> AnswersResult:  # pyright: ignore[reportIncompatibleMethodOverride]
+    def get_answers(  # pyright: ignore[reportIncompatibleMethodOverride]
+        self,
+        *args: AnswersOptions,
+        **kwargs: Any
+    ) -> AnswersResult:
         """Answers the specified question using your knowledge base.
 
         :param options: Positional only. POST body of the request. Provide either `options`, OR
@@ -302,7 +306,11 @@ class QuestionAnsweringClientOperationsMixin(QuestionAnsweringClientOperationsMi
         """
 
     @distributed_trace
-    def get_answers_from_text(self, *args: AnswersFromTextOptions, **kwargs: Any) -> AnswersFromTextResult:  # pyright: ignore[reportIncompatibleMethodOverride]
+    def get_answers_from_text(  # pyright: ignore[reportIncompatibleMethodOverride]
+        self,
+        *args: AnswersFromTextOptions,
+        **kwargs: Any
+    ) -> AnswersFromTextResult:
         """Answers the specified question using the provided text in the body.
 
         :param options: Positional only. POST body of the request. Provide either `options`, OR
