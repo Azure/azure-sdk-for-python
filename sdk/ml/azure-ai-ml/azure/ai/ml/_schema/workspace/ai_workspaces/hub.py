f0c70795f7664a57601972ff2e28db05c2911be7
Miles Holland <milesholland@microsoft.com>
2024-04-17 12:16:31 -0400

    show associated workspaces in yaml view

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/workspace/ai_workspaces/hub.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/workspace/ai_workspaces/hub.py
@@ -15,3 +15,4 @@ class HubSchema(WorkspaceSchema):
     # additional_workspace_storage_accounts This field exists in the API, but is unused, and thus not surfaced yet.
     kind = StringTransformedEnum(required=True, allowed_values=WorkspaceKind.HUB)
     default_project_resource_group = fields.Str(required=False)
+    associated_workspaces = fields.List(fields.Str(), required=False, dump_only=True)
