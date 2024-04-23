dbc1f383100480813ba6f9ccaf86ae1163d94558
bambriz <beambriz2017@gmail.com>
2024-04-23 12:06:13 -0700

    tests updates

--- sdk/cosmos/azure-cosmos/test/test_crud_async.py
+++ sdk/cosmos/azure-cosmos/test/test_crud_async.py
@@ -292,7 +292,7 @@ class TestCRUDOperationsAsync(unittest.IsolatedAsyncioTestCase):
         # Create document with partitionkey not present as a leaf level property but a dict
         await created_collection1.create_item(document_definition)
         _retry_utility_async.ExecuteFunctionAsync = self.OriginalExecuteFunction
-        assert self.last_headers[1] == [{}]
+        assert self.last_headers[0] == [{}]
         del self.last_headers[:]
 
         collection_id = 'test_partitioned_collection_partition_key_extraction2 ' + str(uuid.uuid4())
@@ -330,7 +330,7 @@ class TestCRUDOperationsAsync(unittest.IsolatedAsyncioTestCase):
         _retry_utility_async.ExecuteFunctionAsync = self._mock_execute_function
         await created_collection1.create_item(body=document_definition)
         _retry_utility_async.ExecuteFunctionAsync = self.OriginalExecuteFunction
-        assert self.last_headers[1] == '["val1"]'
+        assert self.last_headers[0] == '["val1"]'
         del self.last_headers[:]
 
         collection_id = 'test_partitioned_collection_partition_key_extraction_special_chars2 ' + str(uuid.uuid4())
@@ -349,7 +349,7 @@ class TestCRUDOperationsAsync(unittest.IsolatedAsyncioTestCase):
         # create document without partition key being specified
         await created_collection2.create_item(body=document_definition)
         _retry_utility_async.ExecuteFunctionAsync = self.OriginalExecuteFunction
-        assert self.last_headers[1] == '["val2"]'
+        assert self.last_headers[0] == '["val2"]'
         del self.last_headers[:]
 
         await created_db.delete_container(created_collection1.id)
