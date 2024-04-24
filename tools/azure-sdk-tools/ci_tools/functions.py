0f027c5675a09fd5e00b2b1c385d5533b9bda04b
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2024-04-24 14:20:24 -0700

    update find_sdist to honor legacy and canonicalized formats

--- tools/azure-sdk-tools/ci_tools/functions.py
+++ tools/azure-sdk-tools/ci_tools/functions.py
@@ -395,17 +395,20 @@ def find_sdist(dist_dir: str, pkg_name: str, pkg_version: str) -> str:
         logging.error("Package name cannot be empty to find sdist")
         return
 
-    pkg_name_format = f"{pkg_name}-{pkg_version}.tar.gz"
+    pkg_name_legacy_format = f"{pkg_name}-{pkg_version}.tar.gz"
+    pkg_name_normalized_format = f"{pkg_name}_{pkg_version}.tar.gz"
 
     packages = []
     for root, dirnames, filenames in os.walk(dist_dir):
-        for filename in fnmatch.filter(filenames, pkg_name_format):
+        for filename in fnmatch.filter(filenames, [pkg_name_legacy_format, pkg_name_normalized_format]):
             packages.append(os.path.join(root, filename))
 
     packages = [os.path.relpath(w, dist_dir) for w in packages]
 
     if not packages:
-        logging.error("No sdist is found in directory %s with package name format %s", dist_dir, pkg_name_format)
+        logging.error(
+            f"No sdist is found in directory {dist_dir} with package name format {[pkg_name_legacy_format, pkg_name_normalized_format]}"
+        )
         return
     return packages[0]
 
