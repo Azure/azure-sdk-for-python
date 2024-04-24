4c25098ff464433442a220b7861c63a572f1f3a9
Diondra Peck <dipeck@microsoft.com>
2024-04-24 13:09:12 -0700

    Fix exception disable comments

--- sdk/ml/azure-ai-ml/azure/ai/ml/_schema/core/fields.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/_schema/core/fields.py
@@ -648,7 +648,7 @@ class TypeSensitiveUnionField(UnionField):
                 self.context[BASE_PATH_CONTEXT_KEY] = target_path.parent
                 with target_path.open(encoding=DefaultOpenEncoding.READ) as f:
                     return yaml.safe_load(f)
-        except Exception:  # pylint: disable=broad-exception
+        except Exception:  # pylint: disable=broad-except
             pass
         return value
 
@@ -796,7 +796,7 @@ class VersionField(Field):
             return value
         if isinstance(value, (int, float)):
             return str(value)
-        raise Exception(f"Type {type(value)} is not supported for version.")  # pylint: disable=broad-exception
+        raise Exception(f"Type {type(value)} is not supported for version.")  # pylint: disable=broad-except
 
 
 class NumberVersionField(VersionField):
