1c45995916a7be41f46735e6f1f144345996b704
Peter Wu <weirongwu@microsoft.com>
2024-04-24 23:52:37 -0400

    surfaced copy source status and error code for upload blob from url (sync)

--- sdk/storage/azure-storage-blob/tests/test_block_blob_async.py
+++ sdk/storage/azure-storage-blob/tests/test_block_blob_async.py
@@ -511,6 +511,28 @@ class TestStorageBlockBlobAsync(AsyncStorageRecordedTestCase):
 
     @BlobPreparer()
     @recorded_by_proxy_async
+    async def test_upload_blob_from_url_sync_copy_source_error_and_status_code(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+
+        # Arrange
+        await self._setup(storage_account_name, storage_account_key)
+        source_blob_name = "sourceblob"
+        source_blob = self.bsc.get_blob_client(self.container_name, source_blob_name)
+        target_blob_name = "targetblob"
+        target_blob = self.bsc.get_blob_client(self.container_name, target_blob_name)
+
+        # Act
+        with pytest.raises(HttpResponseError) as e:
+            await target_blob.upload_blob_from_url(source_blob.url)
+
+        # Assert
+        assert e.value.response.headers["x-ms-copy-source-status-code"] == "409"
+        assert e.value.response.headers["x-ms-copy-source-error-code"] == "PublicAccessNotPermitted"
+        assert "copysourceerrormessage:Public access is not permitted on this storage account." in e.value.message
+
+    @BlobPreparer()
+    @recorded_by_proxy_async
     async def test_put_block(self, **kwargs):
         storage_account_name = kwargs.pop("storage_account_name")
         storage_account_key = kwargs.pop("storage_account_key")
