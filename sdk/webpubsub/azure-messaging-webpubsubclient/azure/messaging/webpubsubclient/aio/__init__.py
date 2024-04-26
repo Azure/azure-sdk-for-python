a49cab022f843c50037727750bdb90f536351de5
Yuchao Yan <yuchaoyan@microsoft.com>
2024-04-26 10:57:31 +0800

    [Webpubsub] async API (#34764)
    * init
    
    * basic function and perf
    
    * update
    
    * async test
    
    * black
    
    * optimize retry logic
    
    * update for sample
    
    * set policy for async test in windows
    
    * remove async lock
    
    * fix cspell
    
    * fix mypy
    
    * fix pylint
    
    * update dependency
    
    * aiohttp>=3.8.0 in setup.py
    
    * fix for Linux
    
    * optimize for sync api
    
    * fix sample
    
    * optiize for async api
    
    * review
    
    * update test
    
    * update dependency
    
    * update
    
    * fix recovery and reconnect logic
    
    * update test case
    
    * update
    
    * optimize
    
    * async test for open client error
    
    * typing fix
    
    * typing fix
    
    * typing fix
    
    * review for on_error
    
    * stress test
    
    * fix cspell
    
    * fix cspell
    
    * update logging for stress
    
    * rename stress readme
    
    * update
    
    * Update sdk/webpubsub/azure-messaging-webpubsubclient/azure/messaging/webpubsubclient/_util.py
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>
    
    * Update sdk/webpubsub/azure-messaging-webpubsubclient/azure/messaging/webpubsubclient/_client.py
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>
    
    * Update sdk/webpubsub/azure-messaging-webpubsubclient/azure/messaging/webpubsubclient/_client.py
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>
    
    * Update sdk/webpubsub/azure-messaging-webpubsubclient/azure/messaging/webpubsubclient/_client.py
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>
    
    * Update sdk/webpubsub/azure-messaging-webpubsubclient/azure/messaging/webpubsubclient/_client.py
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>
    
    * Update sdk/webpubsub/azure-messaging-webpubsubclient/azure/messaging/webpubsubclient/_client.py
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>
    
    * Update sdk/webpubsub/azure-messaging-webpubsubclient/azure/messaging/webpubsubclient/aio/_client.py
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>
    
    * review
    
    * review
    
    * review
    
    * update changelog
    
    * update test case name
    
    * fix test
    
    * optimize close logic
    
    * mypy fix
    
    * optimize client.close logic for async api
    
    * udpate changelog
    
    * expose is_connected
    
    * review
    
    * remove nested function
    
    * set all created thread as daemon
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Anna Tisch <antisch@microsoft.com>

--- sdk/webpubsub/azure-messaging-webpubsubclient/azure/messaging/webpubsubclient/aio/__init__.py
+++ sdk/webpubsub/azure-messaging-webpubsubclient/azure/messaging/webpubsubclient/aio/__init__.py
@@ -3,5 +3,12 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------
+try:
+    from ._client import WebPubSubClient, WebPubSubClientCredential
+except ImportError as e:
+    raise ImportError("package aiohttp is not installed.") from e
 
-VERSION = "1.1.0"
+__all__ = [
+    "WebPubSubClient",
+    "WebPubSubClientCredential",
+]
