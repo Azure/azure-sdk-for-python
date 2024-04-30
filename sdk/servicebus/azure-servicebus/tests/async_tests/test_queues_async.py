47a7b1fe673eb1053ed43cc7c5e0becae9c9bca7
swathipil <swathip@microsoft.com>
2024-04-30 10:37:20 -0700

    update cached to new queue for client_session_fail test

--- sdk/servicebus/azure-servicebus/tests/async_tests/test_queues_async.py
+++ sdk/servicebus/azure-servicebus/tests/async_tests/test_queues_async.py
@@ -1036,7 +1036,7 @@ class TestServiceBusQueueAsync(AzureMgmtRecordedTestCase):
     @pytest.mark.live_test_only
     @CachedServiceBusResourceGroupPreparer(name_prefix='servicebustest')
     @CachedServiceBusNamespacePreparer(name_prefix='servicebustest')
-    @CachedServiceBusQueuePreparer(name_prefix='servicebustest', dead_lettering_on_message_expiration=True)
+    @ServiceBusQueuePreparer(name_prefix='servicebustest', dead_lettering_on_message_expiration=True)
     @pytest.mark.parametrize("uamqp_transport", uamqp_transport_params, ids=uamqp_transport_ids)
     @ArgPasserAsync()
     async def test_async_queue_by_servicebus_client_session_fail(self, uamqp_transport, *, servicebus_namespace_connection_string=None, servicebus_queue=None, **kwargs):
