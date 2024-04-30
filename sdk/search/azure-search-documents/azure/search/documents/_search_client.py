5aa10c689d8a747542b9e5b78c4068b8e265666d
Xiang Yan <xiangsjtu@gmail.com>
2024-04-30 15:13:41 -0700

    Update sdk/search/azure-search-documents/azure/search/documents/_search_client.py
    Co-authored-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/search/azure-search-documents/azure/search/documents/_search_client.py
+++ sdk/search/azure-search-documents/azure/search/documents/_search_client.py
@@ -294,8 +294,8 @@ class SearchClient(HeadersMixin):
         :keyword vector_filter_mode: Determines whether or not filters are applied before or after the
              vector search is performed. Default is 'preFilter'. Known values are: "postFilter" and "preFilter".
         :paramtype vector_filter_mode: str or VectorFilterMode
-        :ivar hybrid_search: The query parameters to configure hybrid search behaviors.
-        :vartype hybrid_search: ~azure.search.documents.models.HybridSearch
+        :keyword hybrid_search: The query parameters to configure hybrid search behaviors.
+        :paramtype hybrid_search: ~azure.search.documents.models.HybridSearch
         :return: List of search results.
         :rtype:  SearchItemPaged[dict]
 
