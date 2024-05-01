72ccc75c4b7a760a5f975aa93d069ea0991af72e
Krista Pratico <krpratic@microsoft.com>
2024-05-01 13:39:44 -0700

    add aoai assistants streaming tests

--- sdk/openai/azure-openai/tests/test_assistants.py
+++ sdk/openai/azure-openai/tests/test_assistants.py
@@ -10,8 +10,176 @@ import pathlib
 import uuid
 from devtools_testutils import AzureRecordedTestCase
 from conftest import ASST_AZURE, ASST_AZUREAD, PREVIEW, GPT_4_OPENAI, configure
+from openai import AssistantEventHandler
+from openai.types.beta.threads import (
+    Text,
+    Message,
+    ImageFile,
+    TextDelta,
+    MessageDelta,
+)
+from openai.types.beta.threads.runs import RunStep, ToolCall, RunStepDelta, ToolCallDelta
 
 TIMEOUT = 300
+
+
+class EventHandler(AssistantEventHandler):
+    def on_text_delta(self, delta: TextDelta, snapshot: Text) -> None:
+        if delta.value:
+            assert delta.value is not None
+        if delta.annotations:
+            for annotation in delta.annotations:
+                if annotation.type == "file_citation":
+                    assert annotation.index is not None
+                    assert annotation.file_citation.file_id
+                    assert annotation.file_citation.quote
+                elif annotation.type == "file_path":
+                    assert annotation.index is not None
+                    assert annotation.file_path.file_id
+
+    def on_run_step_done(self, run_step: RunStep) -> None:
+        details = run_step.step_details
+        if details.type == "tool_calls":
+            for tool in details.tool_calls:
+                if tool.type == "code_interpreter":
+                    assert tool.id
+                    assert tool.code_interpreter.outputs
+                    assert tool.code_interpreter.input is not None
+                elif tool.type == "function":
+                    assert tool.id
+                    assert tool.function.arguments is not None
+                    assert tool.function.name is not None
+
+    def on_run_step_delta(self, delta: RunStepDelta, snapshot: RunStep) -> None:
+        details = delta.step_details
+        if details is not None:
+            if details.type == "tool_calls":
+                for tool in details.tool_calls or []:
+                    if tool.type == "code_interpreter" and tool.code_interpreter and tool.code_interpreter.input:
+                        assert tool.index is not None
+                        assert tool.code_interpreter.input is not None
+            elif details.type == "message_creation":
+                assert details.message_creation.message_id
+
+    def on_run_step_created(self, run_step: RunStep):
+        assert run_step.object == "thread.run.step"
+        assert run_step.id
+        assert run_step.type
+        assert run_step.created_at
+        assert run_step.assistant_id
+        assert run_step.thread_id
+        assert run_step.run_id
+        assert run_step.status
+        assert run_step.step_details
+
+    def on_message_created(self, message: Message):
+        assert message.object == "thread.message"
+        assert message.id
+        assert message.created_at
+        assert message.file_ids is not None
+        assert message.status
+        assert message.thread_id
+
+    def on_message_delta(self, delta: MessageDelta, snapshot: Message):
+        if delta.content:
+            for content in delta.content:
+                if content.type == "text":
+                    assert content.index is not None
+                    if content.text:
+                        if content.text.value:
+                            assert content.text.value is not None
+                        if content.text.annotations:
+                            for annot in content.text.annotations:
+                                if annot.type == "file_citation":
+                                    assert annot.end_index is not None
+                                    assert annot.file_citation.file_id
+                                    assert annot.file_citation.quote
+                                    assert annot.start_index is not None
+                                elif annot.type == "file_path":
+                                    assert annot.end_index is not None
+                                    assert annot.file_path.file_id
+                                    assert annot.start_index is not None
+                elif content.type == "image_file":
+                    assert content.index is not None
+                    assert content.image_file.file_id
+
+
+    def on_message_done(self, message: Message):
+        for msg in message.content:
+            if msg.type == "image_file":
+                assert msg.image_file.file_id
+            if msg.type == "text":
+                assert msg.text.value
+                if msg.text.annotations:
+                    for annot in msg.text.annotations:
+                        if annot.type == "file_citation":
+                            assert annot.end_index is not None
+                            assert annot.file_citation.file_id
+                            assert annot.file_citation.quote
+                            assert annot.start_index is not None
+                            assert annot.text is not None
+                        elif annot.type == "file_path":
+                            assert annot.end_index is not None
+                            assert annot.file_path.file_id
+                            assert annot.start_index is not None
+                            assert annot.text is not None
+
+    def on_text_created(self, text: Text):
+        assert text.value is not None
+
+    def on_text_done(self, text: Text):
+        assert text.value is not None
+        for annot in text.annotations:
+            if annot.type == "file_citation":
+                assert annot.end_index is not None
+                assert annot.file_citation.file_id
+                assert annot.file_citation.quote
+                assert annot.start_index is not None
+                assert annot.text is not None
+            elif annot.type == "file_path":
+                assert annot.end_index is not None
+                assert annot.file_path.file_id
+                assert annot.start_index is not None
+                assert annot.text is not None
+
+    def on_image_file_done(self, image_file: ImageFile):
+        assert image_file.file_id
+
+    def on_tool_call_created(self, tool_call: ToolCall):
+        assert tool_call.id
+
+    def on_tool_call_delta(self, delta: ToolCallDelta, snapshot: ToolCall):
+        if delta.type == "code_interpreter":
+            assert delta.index is not None
+            if delta.code_interpreter:
+                if delta.code_interpreter.input:
+                    assert delta.code_interpreter.input is not None
+            if delta.code_interpreter.outputs:
+                for output in delta.code_interpreter.outputs:
+                    if output.type == "image":
+                        assert output.image.file_id
+                    elif output.type == "logs":
+                        assert output.logs
+        if delta.type == "function":
+            assert delta.id
+            if delta.function:
+                assert delta.function.arguments is not None
+                assert delta.function.name is not None
+
+    def on_tool_call_done(self, tool_call: ToolCall):
+        if tool_call.type == "code_interpreter":
+            assert tool_call.id
+            assert tool_call.code_interpreter.input is not None
+            for output in tool_call.code_interpreter.outputs:
+                if output.type == "image":
+                    assert output.image.file_id
+                elif output.type == "logs":
+                    assert output.logs
+        if tool_call.type == "function":
+            assert tool_call.id
+            assert tool_call.function.arguments is not None
+            assert tool_call.function.name is not None
+
 
 class TestAssistants(AzureRecordedTestCase):
 
@@ -406,3 +574,63 @@ class TestAssistants(AzureRecordedTestCase):
             )
             assert delete_thread.id
             assert delete_thread.deleted is True
+
+    @configure
+    @pytest.mark.parametrize("api_type, api_version", [(ASST_AZURE, PREVIEW), (GPT_4_OPENAI, "v1")])
+    def test_assistants_streaming(self, client, api_type, api_version, **kwargs):
+        assistant = client.beta.assistants.create(
+            name="Math Tutor",
+            instructions="You are a personal math tutor. Write and run code to answer math questions.",
+            tools=[{"type": "code_interpreter"}],
+            **kwargs,
+        )
+        try:
+            thread = client.beta.threads.create()
+            client.beta.threads.messages.create(
+                thread_id=thread.id,
+                role="user",
+                content="I need to solve the equation `3x + 11 = 14`. Can you help me?",
+            )
+            stream = client.beta.threads.runs.create(
+                thread_id=thread.id,
+                assistant_id=assistant.id,
+                instructions="Please address the user as Jane Doe. The user has a premium account.",
+                stream=True,
+            )
+
+            for event in stream:
+                assert event
+        finally:
+            client.beta.assistants.delete(assistant.id)
+
+    @configure
+    @pytest.mark.parametrize("api_type, api_version", [(ASST_AZURE, PREVIEW), (GPT_4_OPENAI, "v1")])
+    def test_assistants_stream_event_handler(self, client, api_type, api_version, **kwargs):
+        assistant = client.beta.assistants.create(
+            name="Math Tutor",
+            instructions="You are a personal math tutor. Write and run code to answer math questions.",
+            tools=[{"type": "code_interpreter"}],
+            **kwargs
+        )
+
+        try:
+            question = "I need to solve the equation `3x + 11 = 14`. Can you help me and then generate an image with the answer?"
+
+            thread = client.beta.threads.create(
+                messages=[
+                    {
+                        "role": "user",
+                        "content": question,
+                    },
+                ]
+            )
+
+            with client.beta.threads.runs.stream(
+                thread_id=thread.id,
+                assistant_id=assistant.id,
+                instructions="Please address the user as Jane Doe. The user has a premium account.",
+                event_handler=EventHandler(),
+            ) as stream:
+                stream.until_done()
+        finally:
+            client.beta.assistants.delete(assistant.id)
