b64cfdd289b89c8b9280df4a82b04847437897fd
Xiang Yan <xiangsjtu@gmail.com>
2024-05-03 16:35:54 -0700

    We don't support sni cert for aio. Remove the doc. (#35475)
    * We don't support sni cert for aio. Remove the doc.
    
    * update readme

--- sdk/identity/azure-identity/azure/identity/aio/_credentials/environment.py
+++ sdk/identity/azure-identity/azure/identity/aio/_credentials/environment.py
@@ -36,9 +36,6 @@ class EnvironmentCredential(AsyncContextManager):
       - **AZURE_CLIENT_ID**: the service principal's client ID
       - **AZURE_CLIENT_CERTIFICATE_PATH**: path to a PEM or PKCS12 certificate file including the private key.
       - **AZURE_CLIENT_CERTIFICATE_PASSWORD**: (optional) password of the certificate file, if any.
-      - **AZURE_CLIENT_SEND_CERTIFICATE_CHAIN**: (optional) If True, the credential will send the public certificate
-        chain in the x5c header of each token request's JWT. This is required for Subject Name/Issuer (SNI)
-        authentication. Defaults to False.
       - **AZURE_AUTHORITY_HOST**: authority of a Microsoft Entra endpoint, for example
         "login.microsoftonline.com", the authority for Azure Public Cloud, which is the default
         when no value is given.
