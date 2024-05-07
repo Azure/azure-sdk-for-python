9ed68117bddfaaad6a264a642b51ecb1c6ab418f
YalinLi0312 <yall@microsoft.com>
2024-05-06 19:29:37 -0700

    Add license header

--- sdk/tables/azure-data-tables/azure/data/tables/_encoder.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_encoder.py
@@ -1,3 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
 import abc, enum
 from typing import Any, Optional, Tuple, Union, Mapping, TypeVar, Generic, Dict
 from uuid import UUID
