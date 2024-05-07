8b6840762083e0b794c33340e4d527887b7cee60
Peter Wu <weirongwu@microsoft.com>
2024-05-07 18:47:56 -0400

    fixed authentication error detail message capitalization check in share tests

--- sdk/storage/azure-storage-file-share/tests/test_share_async.py
+++ sdk/storage/azure-storage-file-share/tests/test_share_async.py
@@ -995,7 +995,7 @@ class TestStorageShareAsync(AsyncStorageRecordedTestCase):
 
         # Assert
         assert e.value.error_code == StorageErrorCode.AUTHENTICATION_FAILED
-        assert "authenticationerrordetail" in e.value.message
+        assert "AuthenticationErrorDetail" in e.value.message
 
 
     @FileSharePreparer()
