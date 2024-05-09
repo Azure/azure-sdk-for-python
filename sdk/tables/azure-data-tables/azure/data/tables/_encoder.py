0f27cc3c7fe79dfb516e133587b43edfe09bbedb
YalinLi0312 <yall@microsoft.com>
2024-05-08 19:56:09 -0700

    Fix pylint

--- sdk/tables/azure-data-tables/azure/data/tables/_encoder.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_encoder.py
@@ -3,7 +3,8 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-import abc, enum
+import abc
+import enum
 from typing import Any, Optional, Tuple, Union, TypeVar, Generic, Dict
 from uuid import UUID
 from datetime import datetime
@@ -20,16 +21,29 @@ T = TypeVar("T")
 
 class TableEntityEncoderABC(abc.ABC, Generic[T]):
     def prepare_key(self, key: str) -> str:
-        """Duplicate the single quote char to escape."""
+        """Duplicate the single quote char to escape.
+
+        :param str key: The entity PartitionKey or RowKey value in table entity.
+        :return: The entity PartitionKey or RowKey value in table entity.
+        :rtype: str
+        """
         try:
             return key.replace("'", "''")
-        except (AttributeError, TypeError):
-            raise TypeError("PartitionKey or RowKey must be of type string.")
+        except (AttributeError, TypeError) as exc:
+            raise TypeError("PartitionKey or RowKey must be of type string.") from exc
 
-    def prepare_value(
+    def prepare_value( # pylint: disable=too-many-return-statements
         self, name: Optional[str], value: Any
     ) -> Tuple[Optional[EdmType], Optional[Union[str, int, float, bool]]]:
-        """This is a utility to allow customers to reproduce our internal encoding for types like datetime, bytes, float and int64."""
+        """Prepare the encoded value and its edm type.
+
+        :param name: The entity property name.
+        :type name: str or None
+        :param value: The entity property value.
+        :type value: Any
+        :return: The value edm type and encoded value.
+        :rtype: A tuple of ~azure.data.tables.EdmType or None, and str, int, float, bool or None
+        """
         if isinstance(value, bool):
             return None, value
         if isinstance(value, enum.Enum):
@@ -65,23 +79,26 @@ class TableEntityEncoderABC(abc.ABC, Generic[T]):
             raise TypeError(f"Unsupported data type '{type(value)}' for entity property '{name}'.")
         raise TypeError(f"Unsupported data type '{type(value)}'.")
 
-    def _prepare_value_in_tuple(self, value: Tuple[Any, Optional[Union[str, EdmType]]]) -> Tuple[Optional[EdmType], Optional[Union[str, int, float, bool]]]:
+    def _prepare_value_in_tuple( # pylint: disable=too-many-return-statements
+            self, value: Tuple[Any, Optional[Union[str, EdmType]]]
+        ) -> Tuple[Optional[EdmType], Optional[Union[str, int, float, bool]]]:
         unencoded_value = value[0]
         edm_type = value[1]
         if unencoded_value is None:
-            return edm_type, unencoded_value
+            return EdmType(edm_type), unencoded_value
         if edm_type == EdmType.STRING:
             return EdmType.STRING, str(unencoded_value)
         if edm_type == EdmType.INT64:
-            return EdmType.INT64, str(unencoded_value) 
+            return EdmType.INT64, str(unencoded_value)
         if edm_type == EdmType.INT32:
             return EdmType.INT32, int(unencoded_value)
         if edm_type == EdmType.BOOLEAN:
-            return EdmType.BOOLEAN, unencoded_value  # Leaving unaltered as per original implementation.
+            return EdmType.BOOLEAN, unencoded_value
         if edm_type == EdmType.GUID:
             return EdmType.GUID, str(unencoded_value)
         if edm_type == EdmType.BINARY:
-            return EdmType.BINARY, _encode_base64(unencoded_value)  # Leaving this with the double-encoding bug for now, as per original implementation
+            # Leaving this with the double-encoding bug for now, as per original implementation
+            return EdmType.BINARY, _encode_base64(unencoded_value)
         if edm_type == EdmType.DOUBLE:
             if isinstance(unencoded_value, str):
                 # Pass a serialized value straight through
@@ -106,22 +123,44 @@ class TableEntityEncoderABC(abc.ABC, Generic[T]):
                 pass
             return EdmType.DATETIME, _to_utc_datetime(unencoded_value)
         raise TypeError(f"Unsupported edm type '{edm_type}'.")
-    
+
     @abc.abstractmethod
     def encode_entity(self, entity: T) -> Dict[str, Union[str, int, float, bool]]:
-        """Encode an entity object into JSON format to send out."""
+        """Encode an entity object into JSON format to send out.
+
+        :param entity: A table entity.
+        :type entity: Custom entity type
+        :return: An entity with property's metadata in JSON format.
+        :rtype: dict
+        """
 
     @abc.abstractmethod
     def decode_entity(self, entity: Dict[str, Union[str, int, float, bool]]) -> T: ...
 
 
 class TableEntityEncoder(TableEntityEncoderABC[EntityType]):
-
     def encode_entity(self, entity: EntityType) -> Dict[str, Union[str, int, float, bool]]:
-        """This method currently behaves the same as the existing serialization function in that
-        there is no special treatment for the system keys - PartitionKey, RowKey, Timestamp, Etag.
-
-        We could investigate whether, where and how it would be most effective to check for these.
+        """Encode an entity object into JSON format to send out.
+        The entity format is:
+        {
+            "Address":"Mountain View",
+            "Age":23,
+            "AmountDue":200.23,
+            "CustomerCode@odata.type":"Edm.Guid",
+            "CustomerCode":"c9da6455-213d-42c9-9a79-3e9149a57833",
+            "CustomerSince@odata.type":"Edm.DateTime",
+            "CustomerSince":"2008-07-10T00:00:00",
+            "IsActive":true,
+            "NumberOfOrders@odata.type":"Edm.Int64",
+            "NumberOfOrders":"255",
+            "PartitionKey":"my_partition_key",
+            "RowKey":"my_row_key"
+        }
+        
+        :param entity: A table entity.
+        :type entity: ~azure.data.tables.TableEntity or Mapping[str, Any]
+        :return: An entity with property's metadata in JSON format.
+        :rtype: dict
         """
         encoded = {}
         for key, value in entity.items():
