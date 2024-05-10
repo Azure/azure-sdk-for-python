d8b34c11f36faad0f5aac0dba1259070c0feb807
MilesHolland <108901744+MilesHolland@users.noreply.github.com>
2024-05-10 17:37:11 -0400

    revert connection name (#35578)
    * revert connection name
    
    * rename files and schema class
    
    * fake
    
    * undo fake
    
    * rename create field
    
    * run black

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/__init__.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/__init__.py
@@ -255,7 +255,7 @@ from ._schedule.trigger import CronTrigger, RecurrencePattern, RecurrenceTrigger
 from ._system_data import SystemData
 from ._validation import ValidationResult
 from ._workspace.compute_runtime import ComputeRuntime
-from ._workspace.connections.connection import Connection
+from ._workspace.connections.workspace_connection import WorkspaceConnection
 from ._workspace.connections.connection_subtypes import (
     AzureBlobStoreConnection,
     MicrosoftOneLakeConnection,
@@ -352,7 +352,7 @@ __all__ = [
     "ModelBatchDeploymentSettings",
     "Workspace",
     "WorkspaceKeys",
-    "Connection",
+    "WorkspaceConnection",
     "AzureBlobStoreConnection",
     "MicrosoftOneLakeConnection",
     "AzureOpenAIConnection",
