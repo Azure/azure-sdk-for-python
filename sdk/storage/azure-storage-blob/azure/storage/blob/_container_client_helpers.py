a4e89656c5db1b7f509fd96492897d68c43053bb
Vincent Tran <vincenttran@microsoft.com>
2024-05-14 13:34:54 -0700

    indents

--- sdk/storage/azure-storage-blob/azure/storage/blob/_container_client_helpers.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/_container_client_helpers.py
@@ -224,38 +224,38 @@ def _generate_delete_blobs_options(
 
     reqs = []
     for blob in blobs:
-            blob_name = _get_blob_name(blob)
-            if not isinstance(blob, str):
-                options = _generic_delete_blob_options(  # pylint: disable=protected-access
-                    snapshot=blob.get('snapshot'),
-                    version_id=blob.get('version_id'),
-                    delete_snapshots=delete_snapshots or blob.get('delete_snapshots'),
-                    lease=blob.get('lease_id'),
-                    if_modified_since=if_modified_since or blob.get('if_modified_since'),
-                    if_unmodified_since=if_unmodified_since or blob.get('if_unmodified_since'),
-                    etag=blob.get('etag'),
-                    if_tags_match_condition=if_tags_match_condition or blob.get('if_tags_match_condition'),
-                    match_condition=blob.get('match_condition') or MatchConditions.IfNotModified if blob.get('etag')
-                    else None,
-                    timeout=blob.get('timeout'),
-                )
-            else:
-                options = _generic_delete_blob_options(  # pylint: disable=protected-access
-                    delete_snapshots=delete_snapshots,
-                    if_modified_since=if_modified_since,
-                    if_unmodified_since=if_unmodified_since,
-                    if_tags_match_condition=if_tags_match_condition
-                )
-
-            query_parameters, header_parameters = _generate_delete_blobs_subrequest_options(client, **options)
-
-            req = HttpRequest(
-                "DELETE",
-                f"/{quote(container_name)}/{quote(blob_name, safe='/~')}{query_str}",
-                headers=header_parameters
+        blob_name = _get_blob_name(blob)
+        if not isinstance(blob, str):
+            options = _generic_delete_blob_options(  # pylint: disable=protected-access
+                snapshot=blob.get('snapshot'),
+                version_id=blob.get('version_id'),
+                delete_snapshots=delete_snapshots or blob.get('delete_snapshots'),
+                lease=blob.get('lease_id'),
+                if_modified_since=if_modified_since or blob.get('if_modified_since'),
+                if_unmodified_since=if_unmodified_since or blob.get('if_unmodified_since'),
+                etag=blob.get('etag'),
+                if_tags_match_condition=if_tags_match_condition or blob.get('if_tags_match_condition'),
+                match_condition=blob.get('match_condition') or MatchConditions.IfNotModified if blob.get('etag')
+                else None,
+                timeout=blob.get('timeout'),
             )
-            req.format_parameters(query_parameters)
-            reqs.append(req)
+        else:
+            options = _generic_delete_blob_options(  # pylint: disable=protected-access
+                delete_snapshots=delete_snapshots,
+                if_modified_since=if_modified_since,
+                if_unmodified_since=if_unmodified_since,
+                if_tags_match_condition=if_tags_match_condition
+            )
+
+        query_parameters, header_parameters = _generate_delete_blobs_subrequest_options(client, **options)
+
+        req = HttpRequest(
+            "DELETE",
+            f"/{quote(container_name)}/{quote(blob_name, safe='/~')}{query_str}",
+            headers=header_parameters
+        )
+        req.format_parameters(query_parameters)
+        reqs.append(req)
 
     return reqs, kwargs
 
