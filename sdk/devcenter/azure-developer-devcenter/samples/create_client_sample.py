6e91e6daee84fca3026e47d4f0415ada01878a9a
Driele Neves Ribeiro <drielene@microsoft.com>
2024-05-16 18:06:38 -0400

    DevCenter DataPlane v2023-04-01 stable SDK from TypeSpec (#35257)
    * Generate with models
    
    * Updates after arch board meetting. Missing enum rename.
    
    * regen after merge main
    
    * regen after tsp updates
    
    * Add script for custom tsp update
    
    * Update tsp commit in main and regen
    
    * Update tests to use model
    
    * Break down tests
    
    * Add samples
    
    * Add snippets and update readme
    
    * clean up imports
    
    * Update changelog
    
    * Update version
    
    * Fix samples
    
    * Fix formatting
    
    * Regen after merge main
    
    * update latest tsp commit
    
    * Push recordings to assets repo and update tag reference
    
    * Add words to be ignored by cspell
    
    * Revert removal of skip_quote
    
    * Re-record failing tests and fix create dev box test
    
    * fix vcpus cspell file path
    
    * Regen latest main
    
    * Revert removal of skip_quote
    
    * Regen using latest tsp
    
    * temp fix api version
    
    * Fix readme section reference
    
    * Revert "temp fix api version"
    
    This reverts commit f476e43e270244d2600b8a1bd513ff640c597328.
    
    * Regen latest main
    
    * Rename to Operation Status
    
    * Use the lro model in test assertion
    
    * Update changelong
    
    * Update readme and code snippets
    
    * Rename samples
    
    * add async tests and update recordings
    
    * add aync samples
    
    * fix issue in async samples
    
    * Update tests recordings
    
    * Update tests and re-record tests
    
    * patch waiting in playback mode
    
    ---------
    
    Co-authored-by: Lei Tian <leti@microsoft.com>

--- sdk/devcenter/azure-developer-devcenter/samples/create_client_sample.py
+++ sdk/devcenter/azure-developer-devcenter/samples/create_client_sample.py
@@ -24,38 +24,30 @@
 # IN THE SOFTWARE.
 #
 # --------------------------------------------------------------------------
-import logging
-import os
-
-from azure.developer.devcenter import DevCenterClient
-from azure.identity import DefaultAzureCredential
-from azure.core.exceptions import HttpResponseError
-
 """
-FILE: create_devbox_sample.py
+FILE: create_client_sample.py
 
 DESCRIPTION:
-    This sample demonstrates how to create, connect and delete a dev box using python DevCenterClient. For this sample,
-    you must have previously configured DevCenter, Project, Network Connection, Dev Box Definition, and Pool.More details 
-    on how to configure those requirements at https://learn.microsoft.com/azure/dev-box/quickstart-configure-dev-box-service
-
+    This sample demonstrates how to create a Dev Center client. For this sample, you must 
+    have previously configured a Dev Center in Azure. More details on how to configure it at 
+    https://learn.microsoft.com/azure/deployment-environments/quickstart-create-and-configure-devcenter#create-a-dev-center
 
 USAGE:
-    python create_devbox_sample.py
+    python create_client_sample.py
 
     Set the environment variables with your own values before running the sample:
     1) DEVCENTER_ENDPOINT - the endpoint for your devcenter
 """
 
-def get_project_name(LOG, client):
-    projects = list(client.projects.list_by_dev_center(top=1))
-    return projects[0].name
+def create_dev_center_client():
+    # [START create_dev_center_client]
+    import os
 
-
-def main():
+    from azure.developer.devcenter import DevCenterClient
+    from azure.identity import DefaultAzureCredential
 
     # Set the values of the dev center endpoint, client ID, and client secret of the AAD application as environment variables:
-    # DEVCENTER_ENDPOINT, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET
+    # DEVCENTER_ENDPOINT, AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET
     try:
         endpoint = os.environ["DEVCENTER_ENDPOINT"]
     except KeyError:
@@ -63,31 +55,7 @@ def main():
 
     # Build a client through AAD
     client = DevCenterClient(endpoint, credential=DefaultAzureCredential())
-
-    # Fetch control plane resource dependencies
-    projects = list(client.list_projects(top=1))
-    target_project_name = projects[0]["name"]
-
-    pools = list(client.list_pools(target_project_name, top=1))
-    target_pool_name = pools[0]["name"]
-
-    # Stand up a new dev box
-    create_response = client.begin_create_dev_box(
-        target_project_name, "me", "Test_DevBox", {"poolName": target_pool_name}
-    )
-    devbox_result = create_response.result()
-
-    print(f"Provisioned dev box with status {devbox_result['provisioningState']}.")
-
-    # Connect to the provisioned dev box
-    remote_connection_response = client.get_remote_connection(target_project_name, "me", "Test_DevBox")
-    print(f"Connect to the dev box using web URL {remote_connection_response['webUrl']}")
-
-    # Tear down the dev box when finished
-    delete_response = client.begin_delete_dev_box(target_project_name, "me", "Test_DevBox")
-    delete_response.wait()
-    print("Deleted dev box successfully.")
-
+    # [END create_dev_center_client]
 
 if __name__ == "__main__":
-    main()
+    create_dev_center_client()
