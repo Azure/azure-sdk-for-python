397af6e9fd62cfc179ca963615b8e1763af6a267
Libba Lawrence <llawrence@microsoft.com>
2024-05-20 14:23:47 -0700

    use patch version

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/__init__.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/aio/__init__.py
@@ -6,7 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._client import EventGridPublisherClient
+from ._patch import EventGridPublisherClient
 from ._client import EventGridConsumerClient
 
 try:
