5ed6e0874ddd4567e8a2112b534193131366c293
Libba Lawrence <llawrence@microsoft.com>
2024-05-21 09:41:38 -0700

    updates

--- sdk/eventgrid/azure-eventgrid/tests/test_eg_client.py
+++ sdk/eventgrid/azure-eventgrid/tests/test_eg_client.py
@@ -7,7 +7,7 @@ import pytest
 import os
 import time
 from devtools_testutils import AzureRecordedTestCase, recorded_by_proxy
-from azure.eventgrid import EventGridClient
+from azure.eventgrid import EventGridPublisherClient, EventGridConsumerClient
 from azure.eventgrid.models import *
 from azure.core.messaging import CloudEvent
 from azure.core.credentials import AzureKeyCredential
@@ -25,9 +25,10 @@ def _clean_up(client, eventgrid_topic_name, eventgrid_event_subscription_name):
 
 
 class TestEGClientExceptions(AzureRecordedTestCase):
-    def create_eg_client(self, endpoint, key):
-        client = EventGridClient(endpoint=endpoint, credential=AzureKeyCredential(key))
-        return client
+    def create_eg_client(self, endpoint, key, topic):
+        publisher = EventGridPublisherClient(endpoint=endpoint, credential=AzureKeyCredential(key), namespace_topic=topic)
+        consumer = EventGridConsumerClient(endpoint=endpoint, credential=AzureKeyCredential(key))
+        return publisher, consumer
 
     @pytest.mark.live_test_only()
     @EventGridPreparer()
@@ -35,7 +36,7 @@ class TestEGClientExceptions(AzureRecordedTestCase):
     def test_publish_receive_cloud_event(
         self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name
     ):
-        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
+        publisher, consumer = self.create_eg_client(eventgrid_endpoint, eventgrid_key, eventgrid_topic_name)
 
         event = CloudEvent(
             type="Contoso.Items.ItemReceived",
@@ -44,14 +45,14 @@ class TestEGClientExceptions(AzureRecordedTestCase):
             data=b"this is binary data",
         )
 
-        client.send(topic_name=eventgrid_topic_name, events=[event])
+        publisher.send(events=[event])
 
         time.sleep(5)
 
-        events = client.receive_cloud_events(eventgrid_topic_name, eventgrid_event_subscription_name, max_events=1)
+        events = consumer.receive_cloud_events(eventgrid_topic_name, eventgrid_event_subscription_name, max_events=1)
         lock_token = events.value[0].broker_properties.lock_token
 
-        ack = client.acknowledge_cloud_events(
+        ack = consumer.acknowledge_cloud_events(
             eventgrid_topic_name, eventgrid_event_subscription_name, lock_tokens=[lock_token]
         )
         assert len(ack.succeeded_lock_tokens) == 1
@@ -63,7 +64,7 @@ class TestEGClientExceptions(AzureRecordedTestCase):
     def test_publish_release_cloud_event(
         self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name
     ):
-        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
+        publisher, consumer = self.create_eg_client(eventgrid_endpoint, eventgrid_key, eventgrid_topic_name)
 
         event = CloudEvent(
             type="Contoso.Items.ItemReceived",
@@ -72,27 +73,27 @@ class TestEGClientExceptions(AzureRecordedTestCase):
             data=b"this is binary data",
         )
 
-        client.send(topic_name=eventgrid_topic_name, events=[event])
+        publisher.send(events=[event])
 
-        events = client.receive_cloud_events(eventgrid_topic_name, eventgrid_event_subscription_name, max_events=1)
+        events = consumer.receive_cloud_events(eventgrid_topic_name, eventgrid_event_subscription_name, max_events=1)
         lock_token = events.value[0].broker_properties.lock_token
 
-        ack = client.release_cloud_events(
+        ack = consumer.release_cloud_events(
             eventgrid_topic_name, eventgrid_event_subscription_name, lock_tokens=[lock_token]
         )
         assert len(ack.succeeded_lock_tokens) == 1
         assert len(ack.failed_lock_tokens) == 0
 
-        events = client.receive_cloud_events(eventgrid_topic_name, eventgrid_event_subscription_name, max_events=1)
+        events = consumer.receive_cloud_events(eventgrid_topic_name, eventgrid_event_subscription_name, max_events=1)
         assert events.value[0].broker_properties.delivery_count > 1
 
-        client.reject_cloud_events(
+        consumer.reject_cloud_events(
             eventgrid_topic_name,
             eventgrid_event_subscription_name,
             lock_tokens=[events.value[0].broker_properties.lock_token],
         )
 
-        _clean_up(client, eventgrid_topic_name, eventgrid_event_subscription_name)
+        _clean_up(consumer, eventgrid_topic_name, eventgrid_event_subscription_name)
 
     @pytest.mark.live_test_only()
     @EventGridPreparer()
@@ -100,7 +101,7 @@ class TestEGClientExceptions(AzureRecordedTestCase):
     def test_receive_type(
         self, eventgrid_endpoint, eventgrid_key, eventgrid_topic_name, eventgrid_event_subscription_name
     ):
-        client = self.create_eg_client(eventgrid_endpoint, eventgrid_key)
+        publisher, consumer = self.create_eg_client(eventgrid_endpoint, eventgrid_key, eventgrid_topic_name)
 
         event = CloudEvent(
             type="Contoso.Items.ItemReceived",
@@ -109,13 +110,13 @@ class TestEGClientExceptions(AzureRecordedTestCase):
             data={"key": "value"},
         )
 
-        client.send(topic_name=eventgrid_topic_name, events=event)
+        publisher.send(events=event)
 
         time.sleep(5)
 
-        events = client.receive_cloud_events(eventgrid_topic_name, eventgrid_event_subscription_name, max_events=1)
+        events = consumer.receive_cloud_events(eventgrid_topic_name, eventgrid_event_subscription_name, max_events=1)
         data = events.value[0].event.data
 
         assert isinstance(data, dict)
 
-        _clean_up(client, eventgrid_topic_name, eventgrid_event_subscription_name)
+        _clean_up(consumer, eventgrid_topic_name, eventgrid_event_subscription_name)
