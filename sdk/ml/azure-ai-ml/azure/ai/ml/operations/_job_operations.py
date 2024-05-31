ca32c2c127efb7c7a63a4dba56e51cb80a903618
Amit Chauhan <achauhan@microsoft.com>
2024-05-31 09:51:08 +0530

    fix for job lower case

--- sdk/ml/azure-ai-ml/azure/ai/ml/operations/_job_operations.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/operations/_job_operations.py
@@ -1473,11 +1473,7 @@ class JobOperations(_ScopeDependentOperations):
                 Code(base_path=job._base_path, path=job.trial.code),
                 azureml_type=AzureMLResourceType.CODE,
             )
-        if (
-            job.trial is not None
-            and job.trial.environment is not None
-            and not is_ARM_id_for_resource(job.trial.environment, AzureMLResourceType.ENVIRONMENT)
-        ):
+        if job.trial is not None:
             job.trial.environment = resolver(  # type: ignore[assignment]
                 job.trial.environment, azureml_type=AzureMLResourceType.ENVIRONMENT
             )
