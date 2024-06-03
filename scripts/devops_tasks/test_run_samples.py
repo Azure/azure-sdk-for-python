6a821f35b34c85c5e02621b34a9ee1317cd5b7b8
Libba Lawrence <llawrence@microsoft.com>
2024-06-03 14:35:28 -0700

    Update scripts/devops_tasks/test_run_samples.py

--- scripts/devops_tasks/test_run_samples.py
+++ scripts/devops_tasks/test_run_samples.py
@@ -92,7 +92,7 @@ IGNORED_SAMPLES = {
         "consume_cloud_events_from_eventhub.py",
         "consume_eventgrid_events_from_service_bus_queue.py",
         "sample_publish_events_to_a_topic_using_sas_credential.py",
-        "sample_publish_events_to_a_topic_using_sas_credential_async.py",
+        "sample_publish_events_to_a_topic_using_sas_credential_async.py"
     ],
     "azure-eventhub": [
         "client_identity_authentication.py",    # TODO: remove after fixing issue #29177
