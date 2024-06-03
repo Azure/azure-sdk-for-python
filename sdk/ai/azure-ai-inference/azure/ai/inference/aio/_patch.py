c32740314cba2b5b255c3ac27d550422d06ffd35
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-06-03 12:39:02 -0700

    Fix missing ranges in ref-doc comments

--- sdk/ai/azure-ai-inference/azure/ai/inference/aio/_patch.py
+++ sdk/ai/azure-ai-inference/azure/ai/inference/aio/_patch.py
@@ -187,7 +187,9 @@ class ChatCompletionsClient(ChatCompletionsClientGenerated):
          value of 0.15 will cause only the tokens comprising the top 15% of probability mass to be
          considered.
          It is not recommended to modify temperature and top_p for the same completions request as the
-         interaction of these two settings is difficult to predict. Default value is None.
+         interaction of these two settings is difficult to predict.
+         Supported range is [0, 1].
+         Default value is None.
         :paramtype top_p: float
         :keyword max_tokens: The maximum number of tokens to generate. Default value is None.
         :paramtype max_tokens: int
@@ -320,25 +322,28 @@ class ChatCompletionsClient(ChatCompletionsClientGenerated):
          HTTP request header. Default value is None.
         :paramtype extras: dict[str, str]
         :keyword frequency_penalty: A value that influences the probability of generated tokens
-         appearing based on their cumulative
-         frequency in generated text.
+         appearing based on their cumulative frequency in generated text.
          Positive values will make tokens less likely to appear as their frequency increases and
-         decrease the likelihood of the model repeating the same statements verbatim. Default value is
-         None.
+         decrease the likelihood of the model repeating the same statements verbatim.
+         Supported range is [-2, 2].
+         Default value is None.
         :paramtype frequency_penalty: float
         :keyword presence_penalty: A value that influences the probability of generated tokens
          appearing based on their existing
          presence in generated text.
          Positive values will make tokens less likely to appear when they already exist and increase
-         the
-         model's likelihood to output new topics. Default value is None.
+         the model's likelihood to output new topics.
+         Supported range is [-2, 2].
+         Default value is None.
         :paramtype presence_penalty: float
         :keyword temperature: The sampling temperature to use that controls the apparent creativity of
          generated completions.
          Higher values will make output more random while lower values will make results more focused
          and deterministic.
          It is not recommended to modify temperature and top_p for the same completions request as the
-         interaction of these two settings is difficult to predict. Default value is None.
+         interaction of these two settings is difficult to predict.
+         Supported range is [0, 1].
+         Default value is None.
         :paramtype temperature: float
         :keyword top_p: An alternative to sampling with temperature called nucleus sampling. This value
          causes the
