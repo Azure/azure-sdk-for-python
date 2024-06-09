22a1c31a59c04576362c7f148dcb2bf586af3c48
antisch <antisch@microsoft.com>
2024-06-10 09:04:21 +1200

    Fix test

--- sdk/core/azure-core-experimental/tests/test_urllib3_transport.py
+++ sdk/core/azure-core-experimental/tests/test_urllib3_transport.py
@@ -1,6 +1,7 @@
 
 import urllib3
 import pytest
+import io
 
 from azure.core.exceptions import ServiceRequestError
 from azure.core.experimental.transport import Urllib3Transport
@@ -12,18 +13,14 @@ PLACEHOLDER_ENDPOINT = "https://my-resource-group.cognitiveservices.azure.com/"
 
 
 def mock_successful_post() -> urllib3.response.HTTPResponse:
-    return urllib3.response.HTTPResponse(status=200, body=b"Hello, world!")
+    return urllib3.response.HTTPResponse(status=200, body=io.BytesIO(b"Hello, world!"))
 
 class TestUrllib3Transport:
 
     def test_send_success(self) -> None:
-
         class MockPoolManager(urllib3.PoolManager):
             def urlopen(*args, **kwargs):
-                try:
-                    super().urlopen(*args, **kwargs)
-                except urllib3.exceptions.HTTPError:
-                    return mock_successful_post()
+                return mock_successful_post()
 
         transport = Urllib3Transport(pool=MockPoolManager())
         pipeline = Pipeline(transport, [RetryPolicy()])
@@ -33,7 +30,7 @@ class TestUrllib3Transport:
         request = HttpRequest(method=method, url=PLACEHOLDER_ENDPOINT, headers=headers, content=content)
         response = (pipeline.run(request=request)).http_response
         expected = mock_successful_post()
-        assert response.body() == expected.body
+        assert response.body() == expected.data
         assert response.status_code == expected.status
         assert response.reason == expected.reason
 
