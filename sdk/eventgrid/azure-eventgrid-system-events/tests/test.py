3e630d66db985b9db2b36e33650f9949b2f8fd33
Libba Lawrence <llawrence@microsoft.com>
2024-06-12 07:39:18 -0700

    add test

--- sdk/eventgrid/azure-eventgrid-system-events/tests/test.py
+++ sdk/eventgrid/azure-eventgrid-system-events/tests/test.py
@@ -4,7 +4,8 @@ from devtools_testutils import AzureRecordedTestCase
 
 
 class SystemEventTest(AzureRecordedTestCase):
-    
+
+    @pytest.mark.live_test_only
     def test_acs_chat_event_base_properties():
         event = AcsChatEventBaseProperties()
         assert event is not None
