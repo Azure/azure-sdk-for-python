4c9217005d38fc67370b0f78b95ffe968d5472bb
Libba Lawrence <llawrence@microsoft.com>
2024-06-12 13:19:28 -0700

    renmae test file

--- sdk/eventgrid/azure-eventgrid-system-events/tests/test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from azure.eventgrid.system.events.models import AcsChatEventBaseProperties
-
-class SystemEventTest():
-
-    @pytest.mark.skip("Test is not implemented")
-    def test_acs_chat_event_base_properties():
-        event = AcsChatEventBaseProperties()
