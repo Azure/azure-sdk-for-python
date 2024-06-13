17f7a267813cb946513c2dd1c1fc5c35542dcbc9
Driele Neves Ribeiro <drielene@microsoft.com>
2024-06-13 11:23:18 -0700

    Customize LRO to return LROPoller[None]

--- sdk/devcenter/azure-developer-devcenter/azure/developer/devcenter/_operations/_patch.py
+++ sdk/devcenter/azure-developer-devcenter/azure/developer/devcenter/_operations/_patch.py
@@ -6,10 +6,45 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
-from typing import List
 
-__all__: List[str] = []  # Add all objects you want publicly available to users at this package level
+from ._operations import DevCenterClientOperationsMixin as DevCenterClientOperationsMixinGenerated
+from azure.core.polling import LROPoller
+from azure.core.tracing.decorator import distributed_trace
+from typing import Any, List, Optional
 
+class DevCenterClientOperationsMixin(DevCenterClientOperationsMixinGenerated):
+
+    @distributed_trace
+    def begin_delete_dev_box(
+        self, project_name: str, user_id: str, dev_box_name: str, **kwargs: Any
+    ) -> LROPoller[None]:
+        return super.begin_delete_dev_box(project_name, user_id, dev_box_name, **kwargs)
+
+    @distributed_trace
+    def begin_start_dev_box(
+        self, project_name: str, user_id: str, dev_box_name: str, **kwargs: Any
+    ) -> LROPoller[None]:
+        return super.begin_start_dev_box(project_name, user_id, dev_box_name, **kwargs)
+
+    @distributed_trace
+    def begin_stop_dev_box(
+        self, project_name: str, user_id: str, dev_box_name: str, *, hibernate: Optional[bool] = None, **kwargs: Any
+    ) -> LROPoller[None]:
+        return super.begin_stop_dev_box(project_name, user_id, dev_box_name, hibernate, **kwargs)
+
+    @distributed_trace
+    def begin_restart_dev_box(
+        self, project_name: str, user_id: str, dev_box_name: str, **kwargs: Any
+    ) -> LROPoller[None]:
+        return super.begin_restart_dev_box(project_name, user_id, dev_box_name, **kwargs)
+
+    @distributed_trace
+    def begin_delete_environment(
+        self, project_name: str, user_id: str, environment_name: str, **kwargs: Any
+    ) -> LROPoller[None]:
+        return super().begin_delete_environment(project_name, user_id, environment_name, **kwargs)
+
+__all__: List[str] = ["DevCenterClientOperationsMixin"]  # Add all objects you want publicly available to users at this package level
 
 def patch_sdk():
     """Do not remove from this file.
--- sdk/devcenter/azure-developer-devcenter/azure/developer/devcenter/aio/_operations/_patch.py
+++ sdk/devcenter/azure-developer-devcenter/azure/developer/devcenter/aio/_operations/_patch.py
@@ -6,9 +6,44 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
-from typing import List
+from ._operations import DevCenterClientOperationsMixin as DevCenterClientOperationsMixinGenerated
+from azure.core.polling import AsyncLROPoller
+from azure.core.tracing.decorator_async import distributed_trace_async
+from typing import Any, List, Optional
 
-__all__: List[str] = []  # Add all objects you want publicly available to users at this package level
+class DevCenterClientOperationsMixin(DevCenterClientOperationsMixinGenerated):
+
+    @distributed_trace_async
+    async def begin_delete_dev_box(
+        self, project_name: str, user_id: str, dev_box_name: str, **kwargs: Any
+    ) -> AsyncLROPoller[None]:
+        return super.begin_delete_dev_box(project_name, user_id, dev_box_name, **kwargs)
+
+    @distributed_trace_async
+    async def begin_start_dev_box(
+        self, project_name: str, user_id: str, dev_box_name: str, **kwargs: Any
+    ) -> AsyncLROPoller[None]:
+        return super.begin_start_dev_box(project_name, user_id, dev_box_name, **kwargs)
+
+    @distributed_trace_async
+    async def begin_stop_dev_box(
+        self, project_name: str, user_id: str, dev_box_name: str, *, hibernate: Optional[bool] = None, **kwargs: Any
+    ) -> AsyncLROPoller[None]:
+        return super.begin_stop_dev_box(project_name, user_id, dev_box_name, hibernate, **kwargs)
+
+    @distributed_trace_async
+    async def begin_restart_dev_box(
+        self, project_name: str, user_id: str, dev_box_name: str, **kwargs: Any
+    ) -> AsyncLROPoller[None]:
+        return super.begin_restart_dev_box(project_name, user_id, dev_box_name, **kwargs)
+
+    @distributed_trace_async
+    async def begin_delete_environment(
+        self, project_name: str, user_id: str, environment_name: str, **kwargs: Any
+    ) -> AsyncLROPoller[None]:
+        return super().begin_delete_environment(project_name, user_id, environment_name, **kwargs)
+
+__all__: List[str] = ["DevCenterClientOperationsMixin"]  # Add all objects you want publicly available to users at this package level
 
 
 def patch_sdk():
