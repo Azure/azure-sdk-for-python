b2a22279db4ea8c8024ba712a73fd8848e8fa7f8
Kashif Khan <kashifkhan@microsoft.com>
2024-06-18 17:38:38 +0000

    move the set at the right location

--- sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/receiver.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/receiver.py
@@ -56,7 +56,7 @@ class ReceiverLink(Link):
     def _incoming_transfer(self, frame):
         if self.network_trace:
             _LOGGER.debug("<- %r", TransferFrame(payload=b"***", *frame[:-1]), extra=self.network_trace_params)
-        self._received_messages.add(frame[2])
+
         # If more is false --> this is the last frame of the message
         if not frame[5]:
             self.current_link_credit -= 1
@@ -69,6 +69,7 @@ class ReceiverLink(Link):
         if self._received_payload or frame[5]:  # more
             self._received_payload.extend(frame[11])
         if not frame[5]:
+            self._received_messages.add(frame[2])
             if self._received_payload:
                 message = decode_payload(memoryview(self._received_payload))
                 self._received_payload = bytearray()
