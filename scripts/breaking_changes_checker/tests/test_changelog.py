3749d2617039c86575094e8f1f4b6796d79a2a95
catalinaperalta <catalinaperaltah@hotmail.com>
2024-06-19 16:02:35 -0700

    [bct] Support class level properties (#36007)
    * wip - support non-breaking changes
    
    * refactor to support changelog functionality
    
    * print changelog or breaking changes
    
    * support changelog flag
    
    * detect and register class level attributes
    
    * diff check for class properties and method params
    
    * revert temp changes
    
    * fix property checking logic
    
    * add some init handling
    
    * cleanup
    
    * add comment
    
    * add tests
    
    * rename added class method parameter
    
    * cleanup
    
    * revert init exception
    
    * update tests
    
    * comment
    
    ---------
    
    Co-authored-by: Catalina Peralta <caperal@microsoft.com>

--- scripts/breaking_changes_checker/tests/test_changelog.py
+++ scripts/breaking_changes_checker/tests/test_changelog.py
@@ -25,3 +25,281 @@ def test_changelog_flag():
     msg, _, *args = bc.features_added[0]
     assert msg == BreakingChangesTracker.ADDED_CLIENT_METHOD_MSG
     assert args == ['azure.ai.contentsafety', 'BlocklistClient', 'new_blocklist_client_method']
+
+def test_new_class_property_added():
+    # Testing reporting on class level property added
+    current = {
+        "azure.ai.contentsafety": {
+            "class_nodes": {
+                "AnalyzeTextResult": {
+                    "methods": {},
+                    "properties": {
+                        "blocklists_match": "Optional",
+                        "categories_analysis": "List[_models.TextCategoriesAnalysis]",
+                        "new_class_att": "str"
+                    }
+                },
+            }
+        }
+    }
+
+    stable = {
+        "azure.ai.contentsafety": {
+            "class_nodes": {
+                "AnalyzeTextResult": {
+                    "methods": {},
+                    "properties": {
+                        "blocklists_match": "Optional",
+                        "categories_analysis": "List[_models.TextCategoriesAnalysis]",
+                    }
+                },
+            }
+        }
+    }
+
+    diff = jsondiff.diff(stable, current)
+    bc = BreakingChangesTracker(stable, current, diff, "azure-ai-contentsafety", changelog=True)
+    bc.run_checks()
+
+    assert len(bc.features_added) == 1
+    msg, _, *args = bc.features_added[0]
+    assert msg == BreakingChangesTracker.ADDED_CLASS_PROPERTY_MSG
+    assert args == ['azure.ai.contentsafety', 'AnalyzeTextResult', 'new_class_att']
+
+
+def test_new_class_property_added_init():
+    # Testing if a property is added both in the init and at the class level that we only get 1 report for it
+    current = {
+        "azure.ai.contentsafety": {
+            "class_nodes": {
+                "AnalyzeTextResult": {
+                    "methods": {
+                        "__init__": {
+                            "parameters": {
+                                "self": {
+                                    "default": None,
+                                    "param_type": "positional_or_keyword"
+                                },
+                                "new_class_att": {
+                                    "default": None,
+                                    "param_type": "keyword_only"
+                                },
+                                "kwargs": {
+                                    "default": None,
+                                    "param_type": "var_keyword"
+                                }
+                            },
+                            "is_async": False
+                        },
+                    },
+                    "properties": {
+                        "blocklists_match": "Optional",
+                        "categories_analysis": "List[_models.TextCategoriesAnalysis]",
+                        "new_class_att": "str"
+                    }
+                },
+            }
+        }
+    }
+
+    stable = {
+        "azure.ai.contentsafety": {
+            "class_nodes": {
+                "AnalyzeTextResult": {
+                    "methods": {
+                        "__init__": {
+                            "parameters": {
+                                "self": {
+                                    "default": None,
+                                    "param_type": "positional_or_keyword"
+                                },
+                                "new_class_att": {
+                                    "default": None,
+                                    "param_type": "keyword_only"
+                                },
+                                "kwargs": {
+                                    "default": None,
+                                    "param_type": "var_keyword"
+                                }
+                            },
+                            "is_async": False
+                        },
+                    },
+                    "properties": {
+                        "blocklists_match": "Optional",
+                        "categories_analysis": "List[_models.TextCategoriesAnalysis]",
+                    }
+                },
+            }
+        }
+    }
+
+    diff = jsondiff.diff(stable, current)
+    bc = BreakingChangesTracker(stable, current, diff, "azure-ai-contentsafety", changelog=True)
+    bc.run_checks()
+
+    assert len(bc.features_added) == 1
+    msg, _, *args = bc.features_added[0]
+    assert msg == BreakingChangesTracker.ADDED_CLASS_PROPERTY_MSG
+    assert args == ['azure.ai.contentsafety', 'AnalyzeTextResult', 'new_class_att']
+
+
+def test_new_class_property_added_init_only():
+    # Testing if we get a report on a new class property added only in the init
+    current = {
+        "azure.ai.contentsafety": {
+            "class_nodes": {
+                "AnalyzeTextResult": {
+                    "methods": {
+                        "__init__": {
+                            "parameters": {
+                                "self": {
+                                    "default": None,
+                                    "param_type": "positional_or_keyword"
+                                },
+                                "new_class_att": {
+                                    "default": None,
+                                    "param_type": "keyword_only"
+                                },
+                                "kwargs": {
+                                    "default": None,
+                                    "param_type": "var_keyword"
+                                }
+                            },
+                            "is_async": False
+                        },
+                    },
+                    "properties": {
+                        "blocklists_match": "Optional",
+                        "categories_analysis": "List[_models.TextCategoriesAnalysis]",
+                    }
+                },
+            }
+        }
+    }
+
+    stable = {
+        "azure.ai.contentsafety": {
+            "class_nodes": {
+                "AnalyzeTextResult": {
+                    "methods": {
+                        "__init__": {
+                            "parameters": {
+                                "self": {
+                                    "default": None,
+                                    "param_type": "positional_or_keyword"
+                                },
+                                "kwargs": {
+                                    "default": None,
+                                    "param_type": "var_keyword"
+                                }
+                            },
+                            "is_async": False
+                        },
+                    },
+                    "properties": {
+                        "blocklists_match": "Optional",
+                        "categories_analysis": "List[_models.TextCategoriesAnalysis]",
+                    }
+                },
+            }
+        }
+    }
+
+    diff = jsondiff.diff(stable, current)
+    bc = BreakingChangesTracker(stable, current, diff, "azure-ai-contentsafety", changelog=True)
+    bc.run_checks()
+
+    assert len(bc.features_added) == 1
+    msg, _, *args = bc.features_added[0]
+    assert msg == BreakingChangesTracker.ADDED_CLASS_METHOD_PARAMETER_MSG
+    assert args == ['azure.ai.contentsafety', 'AnalyzeTextResult', 'new_class_att', '__init__']
+
+
+def test_new_class_method_parameter_added():
+    # Testing if we get a report on a new class method parameter added
+    current = {
+        "azure.ai.contentsafety": {
+            "class_nodes": {
+                "AnalyzeTextResult": {
+                    "methods": {
+                        "__init__": {
+                            "parameters": {
+                                "self": {
+                                    "default": None,
+                                    "param_type": "positional_or_keyword"
+                                },
+                                "kwargs": {
+                                    "default": None,
+                                    "param_type": "var_keyword"
+                                }
+                            },
+                            "is_async": False
+                        },
+                        "foo": {
+                            "parameters": {
+                                "self": {
+                                    "default": None,
+                                    "param_type": "positional_or_keyword"
+                                },
+                                "bar": {
+                                    "default": None,
+                                    "param_type": "var_keyword"
+                                }
+                            }
+                        }
+                    },
+                    "properties": {
+                        "blocklists_match": "Optional",
+                        "categories_analysis": "List[_models.TextCategoriesAnalysis]",
+                    }
+                },
+            }
+        }
+    }
+
+    stable = {
+        "azure.ai.contentsafety": {
+            "class_nodes": {
+                "AnalyzeTextResult": {
+                    "methods": {
+                        "__init__": {
+                            "parameters": {
+                                "self": {
+                                    "default": None,
+                                    "param_type": "positional_or_keyword"
+                                },
+                                "kwargs": {
+                                    "default": None,
+                                    "param_type": "var_keyword"
+                                }
+                            },
+                            "is_async": False
+                        },
+                        "foo": {
+                            "parameters": {
+                                "self": {
+                                    "default": None,
+                                    "param_type": "positional_or_keyword"
+                                },
+                            },
+                            "is_async": False
+                        }
+                    },
+                    "properties": {
+                        "blocklists_match": "Optional",
+                        "categories_analysis": "List[_models.TextCategoriesAnalysis]",
+                    }
+                },
+            }
+        }
+    }
+
+    diff = jsondiff.diff(stable, current)
+    bc = BreakingChangesTracker(stable, current, diff, "azure-ai-contentsafety", changelog=True)
+    bc.run_checks()
+
+    assert len(bc.features_added) == 1
+    msg, _, *args = bc.features_added[0]
+    assert msg == BreakingChangesTracker.ADDED_CLASS_METHOD_PARAMETER_MSG
+    assert args == ['azure.ai.contentsafety', 'AnalyzeTextResult', 'bar', 'foo']
