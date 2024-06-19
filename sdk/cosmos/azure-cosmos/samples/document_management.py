ee8cb4ca2e35e2775a67bfec03c45ff2d1948c8b
simorenoh <simonmorenohe@gmail.com>
2024-06-18 23:59:59 -0700

    samples using last_response_headers

--- sdk/cosmos/azure-cosmos/samples/document_management.py
+++ sdk/cosmos/azure-cosmos/samples/document_management.py
@@ -203,9 +203,9 @@ def execute_item_batch(database):
     container = database.create_container_if_not_exists(id="batch_container",
                                                         partition_key=PartitionKey(path='/account_number'))
     # We create three items to use for the sample.
-    container.create_item(get_sales_order("read_item"))
-    container.create_item(get_sales_order("delete_item"))
-    container.create_item(get_sales_order("replace_item"))
+    create_response1 = container.create_item(get_sales_order("read_item"))
+    create_response2 = container.create_item(get_sales_order("delete_item"))
+    create_response3 = container.create_item(get_sales_order("replace_item"))
 
     # We create our batch operations
     create_item_operation = ("create", (get_sales_order("create_item"),))
@@ -215,11 +215,10 @@ def execute_item_batch(database):
     replace_item_operation = ("replace", ("replace_item", {"id": "replace_item", "message": "item was replaced"}))
     replace_item_if_match_operation = ("replace",
                                        ("replace_item", {"id": "replace_item", "message": "item was replaced"}),
-                                       {"if_match_etag": container.client_connection.last_response_headers.get("etag")})
+                                       {"if_match_etag": create_response3.response_headers.get("etag")})
     replace_item_if_none_match_operation = ("replace",
-                                           ("replace_item", {"id": "replace_item", "message": "item was replaced"}),
-                                           {"if_none_match_etag":
-                                                container.client_connection.last_response_headers.get("etag")})
+                                            ("replace_item", {"id": "replace_item", "message": "item was replaced"}),
+                                            {"if_none_match_etag": create_response3.response_headers.get("etag")})
 
     # Put our operations into a list
     batch_operations = [
