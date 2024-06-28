69f84c024453f6ecc91c29cdba1cc5dfa2d5330c
mccoyp <mcpatino@microsoft.com>
2024-06-27 17:34:46 -0700

    Handle missing SP credentials

--- sdk/keyvault/azure-keyvault-administration/tests/_async_test_case.py
+++ sdk/keyvault/azure-keyvault-administration/tests/_async_test_case.py
@@ -48,9 +48,9 @@ class BaseClientPreparer(AzureRecordedTestCase):
 
     def _set_mgmt_settings_real_values(self):
         if self.is_live:
-            os.environ["AZURE_TENANT_ID"] = os.environ["KEYVAULT_TENANT_ID"]
-            os.environ["AZURE_CLIENT_ID"] = os.environ["KEYVAULT_CLIENT_ID"]
-            os.environ["AZURE_CLIENT_SECRET"] = os.environ["KEYVAULT_CLIENT_SECRET"]
+            os.environ["AZURE_TENANT_ID"] = os.getenv("KEYVAULT_TENANT_ID", "")  # empty in pipelines
+            os.environ["AZURE_CLIENT_ID"] = os.getenv("KEYVAULT_CLIENT_ID", "")  # empty in pipelines
+            os.environ["AZURE_CLIENT_SECRET"] = os.getenv("KEYVAULT_CLIENT_SECRET", "")  # empty for user-based auth
 
 
 class KeyVaultBackupClientPreparer(BaseClientPreparer):
