14d4905cd073b0e98521d2f60437e1267e335b8b
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2024-07-02 18:16:25 -0700

    attempt using a venv in general crossplat. lets sus out the issues

---
+++ eng/pipelines/templates/steps/create-venv.ps1
@@ -0,0 +1,19 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $VenvName,
+    # The root of the repository should be $(BUild.SourcesDirectory) passed in from template
+    [Parameter(Mandatory = $true)]
+    [string] $RepoRoot
+)
+
+$venvPath = Join-Path $RepoRoot $VenvName)
+if (-not (Test-Path ($venvPath)) {
+    Write-Host "Creating virtual environment $VenvName"
+    python -m venv "$RepoRoot\$VenvName"
+    Write-Host "Virtual environment $VenvName created."
+}
+else {
+    Write-Host "Virtual environment $VenvName already exists. Skipping creation."
+}
+
+Write-Host "##vso[task.setvariable variable=$($VenvName)_LOCATION;]${{ parameters.RepoRoot }}/$(VenvName)"
