76d71ab25485014e3d500d0c4e9643a873855a1e
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2024-07-03 11:40:06 -0700

    call into the virtual environment and prepend the path. does this all work?

---
+++ eng/scripts/activate-ci-venv.ps1
@@ -0,0 +1,47 @@
+<#! 
+.SYNOPSIS
+Activates a virtual environment for a CI machine. Any further usages of "python" will utilize this virtual environment.
+
+.DESCRIPTION
+When activating a virtual environment, only a few things are actually functionally changed on the machine.
+
+# when activating a virtual environment, only 3 variables are actually affected
+# 1. PATH = path to the bin directory of the virtual env. "Scripts" on windows machines
+# 2. VIRTUAL_ENV = path to root of the virtual env
+# 3. VIRTUAL_ENV_PROMPT = the prompt that is displayed next to the CLI cursor when the virtual env is active
+# within a CI machine, we only need the PATH and VIRTUAL_ENV variables to be set.
+
+.PARAMETER VenvName
+The name of the virtual environment to activate.
+
+.PARAMETER RepoRoot
+The root of the repository.
+#>
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$VenvName,
+    [Parameter(Mandatory=$true)]
+    [string]$RepoRoot # mandatory here, but $(Build.SourcesDirectory) will be passed in the template yaml
+)
+
+Set-StrictMode -Version 4
+$ErrorActionPreference = "Stop"
+
+$venvPath = Join-Path $RepoRoot $VenvName
+$venvBinPath = Join-Path $venvPath "bin"
+
+if (-not (Test-Path $venvPath)) {
+    Write-Error "Virtual environment '$venvPath' does not exist at $venvPath"
+    exit 1
+}
+
+if ($IsWindows) {
+    $venvBinPath = Join-Path $venvPath "Scripts"
+}
+
+$env:VIRTUAL_ENV = $venvPath
+$env:PATH = "$venvBinPath;$($env:PATH)"
+
+Write-Host "Activating virtual environment '$VenvName' at $venvPath via AzDO."
+Write-Host "##vso[task.setvariable variable=PATH;]$($env:PATH)"
+Write-Host "##vso[task.setvariable variable=VIRTUAL_ENV;]$($env:VIRTUAL_ENV)"
