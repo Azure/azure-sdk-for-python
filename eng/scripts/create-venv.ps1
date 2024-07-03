582e7bb9268a9e02a6315dfc8564f1770a7934db
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2024-07-03 13:05:19 -0700

    whenever we UsePythonVersion in a test or build task, use a venv. This will totally eliminate any possibility of wheel file permission changes breaking us

--- eng/scripts/create-venv.ps1
+++ eng/scripts/create-venv.ps1
@@ -1,19 +1,28 @@
+<#! 
+.SYNOPSIS
+Creates a virtual environment for a CI machine.
+
+.PARAMETER VenvName
+The name of the virtual environment to activate.
+
+.PARAMETER RepoRoot
+The root of the repository.
+#>
 param(
     [Parameter(Mandatory = $true)]
     [string] $VenvName,
-    # The root of the repository should be $(BUild.SourcesDirectory) passed in from template
+    # The root of the repository should be $(Build.SourcesDirectory) passed in from template
     [Parameter(Mandatory = $true)]
     [string] $RepoRoot
 )
 
 $venvPath = Join-Path $RepoRoot $VenvName
 if (!(Test-Path $venvPath)) {
-    Write-Host "Creating virtual environment $VenvName"
+    Write-Host "Creating virtual environment '$VenvName'."
     python -m venv "$venvPath"
-    Write-Host "Virtual environment $VenvName created."
+    Write-Host "Virtual environment '$VenvName' created."
+    Write-Host "##vso[task.setvariable variable=$($VenvName)_LOCATION]$venvPath"
 }
 else {
-    Write-Host "Virtual environment $VenvName already exists. Skipping creation."
+    Write-Host "Virtual environment '$VenvName' already exists. Skipping creation."
 }
-
-Write-Host "##vso[task.setvariable variable=$($VenvName)_LOCATION]$venvPath"
