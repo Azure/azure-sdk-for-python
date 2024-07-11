896b5af436d0361f2b10bbcadc03275e38377cff
ChenxiJiang333 <v-chenjiang@microsoft.com>
2024-07-11 15:12:30 +0800

    update aio testcase

--- sdk/sql/azure-mgmt-sql/tests/test_cli_mgmt_sql.py
+++ sdk/sql/azure-mgmt-sql/tests/test_cli_mgmt_sql.py
@@ -6,31 +6,36 @@
 # license information.
 #--------------------------------------------------------------------------
 
-from azure.mgmt.sql import SqlManagementClient
-from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGroupPreparer, recorded_by_proxy
+from azure.mgmt.sql.aio import SqlManagementClient
+from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGroupPreparer
+from devtools_testutils.aio import recorded_by_proxy_async
 
 AZURE_LOCATION = 'eastus'
 
-class TestMgmtSql(AzureMgmtRecordedTestCase):
+class TestMgmtSqlAsync(AzureMgmtRecordedTestCase):
 
     def setup_method(self, method):
         self.client = self.create_mgmt_client(SqlManagementClient)
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
-    @recorded_by_proxy
-    def test_server_list_by_resource_group(self, resource_group):
-        assert list(self.client.servers.list_by_resource_group(resource_group.name)) == []
+    @recorded_by_proxy_async
+    async def test_server_list_by_resource_group(self, resource_group):
+        response = self.client.servers.list_by_resource_group(resource_group.name)
+        assert [r async for r in response] == []
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
-    @recorded_by_proxy
-    def test_managed_instances_list_by_resource_group(self, resource_group):
-        assert list(self.client.managed_instances.list_by_resource_group(resource_group.name)) == []
+    @recorded_by_proxy_async
+    async def test_managed_instances_list_by_resource_group(self, resource_group):
+        response = self.client.managed_instances.list_by_resource_group(resource_group.name)
+        assert [r async for r in response] == []
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
-    @recorded_by_proxy
-    def test_instance_pools_list_by_resource_group(self, resource_group):
-        assert list(self.client.instance_pools.list_by_resource_group(resource_group.name)) == []
+    @recorded_by_proxy_async
+    async def test_instance_pools_list_by_resource_group(self, resource_group):
+        response = self.client.instance_pools.list_by_resource_group(resource_group.name)
+        assert [r async for r in response] == []
 
-    @recorded_by_proxy
-    def test_list_operations(self):
-        assert list(self.client.operations.list())
+    @recorded_by_proxy_async
+    async def test_list_operations(self):
+        response = self.client.operations.list()
+        assert response
