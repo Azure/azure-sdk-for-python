84848dd430ab43ffaf73dc06e4a656b55edb665f
YalinLi0312 <yall@microsoft.com>
2024-07-12 17:32:04 -0700

    Update page length in tests

--- sdk/documentintelligence/azure-ai-documentintelligence/tests/test_dac_classify_document_async.py
+++ sdk/documentintelligence/azure-ai-documentintelligence/tests/test_dac_classify_document_async.py
@@ -82,7 +82,7 @@ class TestDACClassifyDocumentAsync(AsyncDocumentIntelligenceTest):
             )
             document = await poller.result()
             assert document.model_id == classifier.classifier_id
-            assert document.pages is not None
+            assert len(document.pages) == 4
             assert document.tables is None
             assert document.paragraphs is None
             assert document.styles is None
