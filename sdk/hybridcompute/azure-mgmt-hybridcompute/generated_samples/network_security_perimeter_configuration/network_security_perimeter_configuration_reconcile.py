6ef01034f4e5b428876271edc98a98f97c48f099
azure-sdk <PythonSdkPipelines>
2024-07-18 07:41:36 +0000

    code and test

--- sdk/hybridcompute/azure-mgmt-hybridcompute/generated_samples/network_security_perimeter_configuration/network_security_perimeter_configuration_reconcile.py
+++ sdk/hybridcompute/azure-mgmt-hybridcompute/generated_samples/network_security_perimeter_configuration/network_security_perimeter_configuration_reconcile.py
@@ -6,8 +6,6 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import Any, IO, Union
-
 from azure.identity import DefaultAzureCredential
 
 from azure.mgmt.hybridcompute import HybridComputeManagementClient
@@ -17,7 +15,7 @@ from azure.mgmt.hybridcompute import HybridComputeManagementClient
     pip install azure-identity
     pip install azure-mgmt-hybridcompute
 # USAGE
-    python run_commands_update.py
+    python network_security_perimeter_configuration_reconcile.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,18 +27,16 @@ from azure.mgmt.hybridcompute import HybridComputeManagementClient
 def main():
     client = HybridComputeManagementClient(
         credential=DefaultAzureCredential(),
-        subscription_id="{subscriptionId}",
+        subscription_id="00000000-1111-2222-3333-444444444444",
     )
 
-    response = client.machine_run_commands.begin_update(
-        resource_group_name="myResourceGroup",
-        machine_name="myMachine",
-        run_command_name="myRunCommand",
-        run_command_properties={"tags": {"tag1": "value1", "tag2": "value2"}},
+    client.network_security_perimeter_configurations.begin_reconcile_for_private_link_scope(
+        resource_group_name="my-resource-group",
+        scope_name="my-privatelinkscope",
+        perimeter_name="aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee.myAssociation",
     ).result()
-    print(response)
 
 
-# x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2024-03-31-preview/examples/runCommand/RunCommands_Update.json
+# x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2024-05-20-preview/examples/networkSecurityPerimeterConfiguration/NetworkSecurityPerimeterConfigurationReconcile.json
 if __name__ == "__main__":
     main()
