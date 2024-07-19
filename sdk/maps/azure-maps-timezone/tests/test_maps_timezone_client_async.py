6a5b15085702f03ad8f7398bcd859c25b1de9f5e
Lazar Pejic <lazar@janeasystems.com>
2024-07-19 23:34:43 +0200

    Enrich tests for timezone-sdk

--- sdk/maps/azure-maps-timezone/tests/test_maps_timezone_client_async.py
+++ sdk/maps/azure-maps-timezone/tests/test_maps_timezone_client_async.py
@@ -26,39 +26,45 @@ class TestMapsTimezoneClient(AzureRecordedTestCase):
     async def test_get_timezone_by_coordinates(self):
         async with self.client:
             result = await self.client.get_timezone_by_coordinates(coordinates=[25.0338053, 121.5640089])
-            assert result is not None
+            assert result is not None and "TimeZones" in result
 
     @MapsTimezonePreparer()
     @recorded_by_proxy_async
     async def test_get_timezone_by_id(self):
         async with self.client:
             result = await self.client.get_timezone_by_id(timezone_id="sr-Latn-RS")
-            assert result is not None
+            assert result is not None and "TimeZones" in result
 
     @MapsTimezonePreparer()
     @recorded_by_proxy_async
     async def test_get_iana_version(self):
         async with self.client:
+            expected_result = {'Version': '2024a'}
             result = await self.client.get_iana_version()
-            assert result is not None
+            assert result == expected_result
 
     @MapsTimezonePreparer()
     @recorded_by_proxy_async
     async def test_get_iana_timezone_ids(self):
         async with self.client:
-            result = self.client.get_iana_timezone_ids()
-            assert result is not None
+            result = await self.client.get_iana_timezone_ids()
+            assert result is not None and len(result) > 0
 
     @MapsTimezonePreparer()
     @recorded_by_proxy_async
     async def test_get_windows_timezone_ids(self):
         async with self.client:
             result = await self.client.get_windows_timezone_ids()
-            assert result is not None
+            assert result is not None and len(result) > 0
 
     @MapsTimezonePreparer()
     @recorded_by_proxy_async
     async def test_convert_windows_timezone_to_iana(self):
         async with self.client:
+            expected_result = [
+                {'HasZone1970Location': True, 'Id': 'America/Vancouver', 'IsAlias': False},
+                {'HasZone1970Location': True, 'Id': 'America/Los_Angeles', 'IsAlias': False},
+                {'HasZone1970Location': False, 'Id': 'PST8PDT', 'IsAlias': False}
+            ]
             result = await self.client.convert_windows_timezone_to_iana(windows_timezone_id="Pacific Standard Time")
-            assert result is not None
+            assert result == expected_result
