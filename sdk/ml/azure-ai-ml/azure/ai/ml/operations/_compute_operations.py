f29cb6297e297bccc4ea55918b1b8ad4ad4456e8
Amit Chauhan <70937115+achauhan-scc@users.noreply.github.com>
2024-07-26 09:37:03 +0530

    addressing review comment

--- sdk/ml/azure-ai-ml/azure/ai/ml/operations/_compute_operations.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/operations/_compute_operations.py
@@ -424,7 +424,7 @@ class ComputeOperations(_ScopeDependentOperations):
     @distributed_trace
     @monitor_with_activity(ops_logger, "Compute.enablesso", ActivityType.PUBLICAPI)
     @experimental
-    def enable_sso(self, *, name: str, enable_sso: Optional[bool] = True):
+    def enable_sso(self, *, name: str, enable_sso: bool = True):
         """enable sso for a compute instance.
 
         :keyword name: Name of the compute instance.
