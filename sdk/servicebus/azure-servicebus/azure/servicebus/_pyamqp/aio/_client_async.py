e69380ea7229fecbd431fb8d6bfcb52005594e74
Libba Lawrence <llawrence@microsoft.com>
2024-07-31 14:39:05 -0700

    fix state error

--- sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/aio/_client_async.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/aio/_client_async.py
@@ -932,8 +932,7 @@ class ReceiveClientAsync(ReceiveClientSync, AMQPClientAsync):
                 await self.close_async()
 
     async def _on_disposition_received_async(self, message_delivery, reason, state):
-        for state_key in state:
-            state_error = state[state_key]
+        state_error = list(state.values())[0]
         message_delivery.reason = reason
         if reason == LinkDeliverySettleReason.DISPOSITION_RECEIVED:
             if state and (len(state_error) == 0 or state_error[0] is None):
@@ -1069,7 +1068,7 @@ class ReceiveClientAsync(ReceiveClientSync, AMQPClientAsync):
             first_delivery_id=first,
             last_delivery_id=last,
             delivery_tag=delivery_tag,
-            settled=True,
+            settled=False,
             delivery_state=state,
             batchable=batchable,
             wait=True,
