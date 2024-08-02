0bb355a18e44482fa34e8e05ccb3fdec8f8c4920
Paul Van Eck <paulvaneck@microsoft.com>
2024-08-02 15:46:24 -0700

    [Monitor][Query] Adjust server timeout live tests (#36719)
    Reduces flakiness
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/monitor/azure-monitor-query/tests/test_logs_client_async.py
+++ sdk/monitor/azure-monitor-query/tests/test_logs_client_async.py
@@ -56,7 +56,8 @@ class TestLogsClientAsync(AzureMonitorQueryLogsTestCase):
                     monitor_info['workspace_id'],
                     "range x from 1 to 1000000000000000 step 1 | count",
                     timespan=None,
-                    server_timeout=1,
+                    server_timeout=2,
+                    retry_total=0,
                 )
 
         assert 'Gateway timeout' in e.value.message
