ab96aa14fabbb60e65c1cd05fe9d994d9e1d62d0
simorenoh <simonmorenohe@gmail.com>
2024-08-06 11:15:03 -0700

    Revert "update typehint for mypy"
    This reverts commit adbab3ab91f2acd44327d443abba9f2417e8b49d.

--- sdk/cosmos/azure-cosmos/azure/cosmos/_synchronized_request.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/_synchronized_request.py
@@ -79,7 +79,7 @@ def _Request(global_endpoint_manager, request_params, connection_policy, pipelin
     :param azure.core.pipeline.transport.HttpRequest request:
         The request object to send through the pipeline
     :return: tuple of (result, headers)
-    :rtype: Tuple[Dict[str, Any], ~azure.core.CaseInsensitiveDict]
+    :rtype: tuple of (dict, dict)
 
     """
     # pylint: disable=protected-access
