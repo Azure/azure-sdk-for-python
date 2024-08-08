db08e44af35f01ee915fd3f4cbafe831b4a687bf
bambriz <beambriz2017@gmail.com>
2024-08-08 16:48:18 -0700

    update for testing

--- sdk/cosmos/azure-cosmos/test/test_config.py
+++ sdk/cosmos/azure-cosmos/test/test_config.py
@@ -29,12 +29,13 @@ class TestConfig(object):
     # [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Cosmos DB Emulator Key")]
     masterKey = os.getenv('ACCOUNT_KEY',
                           'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==')
-    host = os.getenv('ACCOUNT_HOST', 'https://bambriz-test.documents.azure.com:443/')
+    host = os.getenv('ACCOUNT_HOST', 'https://localhost:8081/')
     connection_str = os.getenv('ACCOUNT_CONNECTION_STR', 'AccountEndpoint={};AccountKey={};'.format(host, masterKey))
-    is_live._cache = True
+    is_emulator = host == 'https://localhost:8081/'
+    is_live._cache = True if not is_emulator else False
     credential = get_credential()
     credential_async = get_credential(is_async=True)
-    is_emulator = host == 'https://localhost:8081/'
+
     connectionPolicy = documents.ConnectionPolicy()
     connectionPolicy.DisableSSLVerification = True
 
