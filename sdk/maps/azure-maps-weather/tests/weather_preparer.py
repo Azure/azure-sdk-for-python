f08f65b5aa16e075c5ea1723d1d4b054f94499c4
zhz0704 <eatcoc10@gmail.com>
2024-08-14 09:52:52 +0800

    Finish tests

--- sdk/maps/azure-maps-weather/tests/weather_preparer.py
+++ sdk/maps/azure-maps-weather/tests/weather_preparer.py
@@ -6,7 +6,7 @@
 import functools
 from devtools_testutils import EnvironmentVariableLoader
 
-MapsTimezonePreparer = functools.partial(
+MapsWeatherPreparer = functools.partial(
     EnvironmentVariableLoader, "maps",
     subscription_key="<maps-subscription-key>",
     maps_client_id="fake_client_id",
