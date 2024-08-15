16bbe90a014588b1852471f6cdd4b383f71cdd5e
jerevoss <jerevoss@gmail.com>
2024-08-15 11:57:39 -0700

    Clean up

--- sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/statsbeat/_exporter.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/statsbeat/_exporter.py
@@ -15,7 +15,6 @@ class _StatsBeatExporter(AzureMonitorMetricExporter):
     def __init__(self, **kwargs: Any) -> None:
         AzureMonitorMetricExporter.__init__(self, **kwargs)
         self._custom_metrics_kusto_enabled = True
-        print("Statsbeat custom_metrics_kusto_enabled = %s" % self._custom_metrics_kusto_enabled)
 
     # pylint: disable=protected-access
     def _point_to_envelope(
@@ -25,7 +24,6 @@ class _StatsBeatExporter(AzureMonitorMetricExporter):
         resource: Optional[Resource] = None,
         scope: Optional[InstrumentationScope] = None
     ) -> Optional[TelemetryItem]:
-        print("_StatsBeatExporter._point_to_envelope: name = %s" % name)
         # map statsbeat name from OpenTelemetry name
         name = _STATSBEAT_METRIC_NAME_MAPPINGS[name]
         return super()._point_to_envelope(
