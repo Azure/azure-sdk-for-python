51a22b0399298b00ac2da11e659c97a11178649f
Yalin Li <yall@microsoft.com>
2024-08-15 15:25:55 -0700

    [Document Intelligence] Generate for API 2024-07-31-preview (#36823)

--- sdk/documentintelligence/azure-ai-documentintelligence/tests/test_dac_analyze_custom_model.py
+++ sdk/documentintelligence/azure-ai-documentintelligence/tests/test_dac_analyze_custom_model.py
@@ -23,9 +23,7 @@ class TestDACAnalyzeCustomModel(DocumentIntelligenceTest):
     @DocumentIntelligencePreparer()
     def test_analyze_document_none_model_id(self, **kwargs):
         documentintelligence_endpoint = kwargs.pop("documentintelligence_endpoint")
-        client = DocumentIntelligenceClient(
-            documentintelligence_endpoint, get_credential()
-        )
+        client = DocumentIntelligenceClient(documentintelligence_endpoint, get_credential())
         with pytest.raises(ValueError) as e:
             client.begin_analyze_document(model_id=None, analyze_request=b"xx")
         assert "No value for given attribute" in str(e.value)
@@ -33,9 +31,7 @@ class TestDACAnalyzeCustomModel(DocumentIntelligenceTest):
     @DocumentIntelligencePreparer()
     def test_analyze_document_none_model_id_from_url(self, **kwargs):
         documentintelligence_endpoint = kwargs.pop("documentintelligence_endpoint")
-        client = DocumentIntelligenceClient(
-            documentintelligence_endpoint, get_credential()
-        )
+        client = DocumentIntelligenceClient(documentintelligence_endpoint, get_credential())
         with pytest.raises(ValueError) as e:
             client.begin_analyze_document(
                 model_id=None, analyze_request=AnalyzeDocumentRequest(url_source="https://badurl.jpg")
@@ -46,9 +42,7 @@ class TestDACAnalyzeCustomModel(DocumentIntelligenceTest):
     @recorded_by_proxy
     def test_analyze_document_empty_model_id(self, **kwargs):
         documentintelligence_endpoint = kwargs.pop("documentintelligence_endpoint")
-        client = DocumentIntelligenceClient(
-            documentintelligence_endpoint, get_credential()
-        )
+        client = DocumentIntelligenceClient(documentintelligence_endpoint, get_credential())
         with pytest.raises(ResourceNotFoundError) as e:
             client.begin_analyze_document(model_id="", analyze_request=b"xx")
         assert "Resource not found" in str(e.value)
@@ -58,9 +52,7 @@ class TestDACAnalyzeCustomModel(DocumentIntelligenceTest):
     @recorded_by_proxy
     def test_analyze_document_empty_model_id_from_url(self, **kwargs):
         documentintelligence_endpoint = kwargs.pop("documentintelligence_endpoint")
-        client = DocumentIntelligenceClient(
-            documentintelligence_endpoint, get_credential()
-        )
+        client = DocumentIntelligenceClient(documentintelligence_endpoint, get_credential())
         with pytest.raises(ResourceNotFoundError) as e:
             client.begin_analyze_document(
                 model_id="", analyze_request=AnalyzeDocumentRequest(url_source="https://badurl.jpg")
@@ -73,12 +65,8 @@ class TestDACAnalyzeCustomModel(DocumentIntelligenceTest):
     def test_custom_document_transform(self, documentintelligence_storage_container_sas_url, **kwargs):
         set_bodiless_matcher()
         documentintelligence_endpoint = kwargs.pop("documentintelligence_endpoint")
-        di_admin_client = DocumentIntelligenceAdministrationClient(
-            documentintelligence_endpoint, get_credential()
-        )
-        di_client = DocumentIntelligenceClient(
-            documentintelligence_endpoint, get_credential()
-        )
+        di_admin_client = DocumentIntelligenceAdministrationClient(documentintelligence_endpoint, get_credential())
+        di_client = DocumentIntelligenceClient(documentintelligence_endpoint, get_credential())
 
         recorded_variables = kwargs.pop("variables", {})
         recorded_variables.setdefault("model_id", str(uuid.uuid4()))
@@ -98,7 +86,7 @@ class TestDACAnalyzeCustomModel(DocumentIntelligenceTest):
         assert document.model_id == model.model_id
         assert len(document.pages) == 1
         assert len(document.tables) == 2
-        assert document.paragraphs is None
+        assert len(document.paragraphs) == 52
         assert len(document.styles) == 1
         assert document.string_index_type == "textElements"
         assert document.content_format == "text"
