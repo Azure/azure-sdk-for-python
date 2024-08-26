532539f7b64098a183fa2c926db0c363961abcdc
Scott Beddall <scbedd@microsoft.com>
2024-08-26 16:15:55 -0700

    logging update

--- tools/azure-sdk-tools/ci_tools/functions.py
+++ tools/azure-sdk-tools/ci_tools/functions.py
@@ -201,21 +201,24 @@ def discover_targeted_packages(
 
     # glob the starting package set
     collected_packages = glob_packages(glob_string, target_root_dir)
+    logging.info(f"Results for glob_string \"{glob_string}\" and root directory \"{target_root_dir}\" are: {collected_packages}")
 
     # apply the additional contains filter
     collected_packages = [pkg for pkg in collected_packages if additional_contains_filter in pkg]
+    logging.info(f"Results after additional contains filter: \"{additional_contains_filter}\" {collected_packages}")
 
     # filter for compatibility, this means excluding a package that doesn't support py36 when we are running a py36 executable
     if compatibility_filter:
         collected_packages = apply_compatibility_filter(collected_packages)
+        logging.info(f"Results after compatibility filter: {collected_packages}")
 
-    # apply package-specific exclusions only if we have gotten more than one
-    if len(collected_packages) > 1:
-        if not include_inactive:
-            collected_packages = apply_inactive_filter(collected_packages)
+    if not include_inactive:
+        collected_packages = apply_inactive_filter(collected_packages)
+        logging.info(f"Results after inactive filter: {collected_packages}")
 
     # Apply filter based on filter type. for e.g. Docs, Regression, Management
     collected_packages = apply_business_filter(collected_packages, filter_type)
+    logging.info(f"Results after business filter: {collected_packages}")
 
     return sorted(collected_packages)
 
