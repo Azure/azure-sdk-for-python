9ba2270f0312b67c045f76665aa825dba00ac467
ChenxiJiang333 <v-chenjiang@microsoft.com>
2024-08-27 16:52:44 +0800

    update-testcases

--- sdk/mysqlflexibleservers/azure-mgmt-mysqlflexibleservers/tests/test_mysql_flexible_servers_operations_async_test.py
+++ sdk/mysqlflexibleservers/azure-mgmt-mysqlflexibleservers/tests/test_mysql_flexible_servers_operations_async_test.py
@@ -22,8 +22,6 @@ class TestMySQLManagementOperationsAsync(AzureMgmtRecordedTestCase):
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy_async
     async def test_list(self, resource_group):
-        response = self.client.operations.list(
-            api_version="2024-02-01-preview",
-        )
+        response = self.client.operations.list()
         result = [r async for r in response]
         assert result
