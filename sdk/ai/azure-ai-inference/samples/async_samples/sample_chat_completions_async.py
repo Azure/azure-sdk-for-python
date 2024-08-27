3f8dd7b58b213cc5975f398d9c4d2f91c28cf559
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-08-27 16:29:01 -0700

    Azure AI Inference SDK - Changes for beta 4 release (#36669)

--- sdk/ai/azure-ai-inference/samples/async_samples/sample_chat_completions_async.py
+++ sdk/ai/azure-ai-inference/samples/async_samples/sample_chat_completions_async.py
@@ -7,12 +7,17 @@ DESCRIPTION:
     This sample demonstrates how to get a chat completion response
     from the service using an asynchronous client.
 
+    This sample assumes the AI model is hosted on a Serverless API or
+    Managed Compute endpoint. For GitHub Models or Azure OpenAI endpoints,
+    the client constructor needs to be modified. See package documentation:
+    https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/ai/azure-ai-inference/README.md#key-concepts
+
 USAGE:
     python sample_chat_completion_async.py
 
     Set these two environment variables before running the sample:
     1) AZURE_AI_CHAT_ENDPOINT - Your endpoint URL, in the form
-        https://<your-deployment-name>.<your-azure-region>.inference.ai.azure.com
+        https://<your-deployment-name>.<your-azure-region>.models.ai.azure.com
         where `your-deployment-name` is your unique AI Model deployment name, and
         `your-azure-region` is the Azure region where your model is deployed.
     2) AZURE_AI_CHAT_KEY - Your model key (a 32-character string). Keep it secret.
