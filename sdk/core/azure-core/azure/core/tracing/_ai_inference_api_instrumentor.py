7cbbc0b7e450afc43c9323dee2fccbf977e39087
Marko Hietala <markhiet@microsoft.com>
2024-09-06 16:19:16 -0500

    file name change

--- sdk/core/azure-core/azure/core/tracing/_ai_inference_api_instrumentor.py
+++ sdk/core/azure-core/azure/core/tracing/_ai_inference_api_instrumentor.py
@@ -20,16 +20,16 @@ class AiInferenceApiInstrumentor(AzureTelemetryInstrumentor):
         
         var_value = os.environ.get("AZURE_TRACING_GEN_AI_CONTENT_RECORDING_ENABLED")
         enable_content_tracing = self.str_to_bool(var_value)
-        from ._inference_api_instrumentor_impl import _inject_inference_api
+        from ._ai_inference_api_instrumentor_impl import _inject_inference_api
         _inject_inference_api(enable_content_tracing)
 
     def uninstrument(self):
         if not self.is_instrumented():
             raise RuntimeError("Not instrumented")
 
-        from ._inference_api_instrumentor_impl import _restore_inference_api
+        from ._ai_inference_api_instrumentor_impl import _restore_inference_api
         _restore_inference_api()
 
     def is_instrumented(self):
-        from ._inference_api_instrumentor_impl import _is_instrumented
+        from ._ai_inference_api_instrumentor_impl import _is_instrumented
         return _is_instrumented()
