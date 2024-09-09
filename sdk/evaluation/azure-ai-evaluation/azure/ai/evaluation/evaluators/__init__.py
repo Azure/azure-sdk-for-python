b9e800e4f9b9b63ee104540dc753de411f5358c3
Neehar Duvvuri <neduvvur@microsoft.com>
2024-09-09 13:19:44 -0400

    sync up with xpia/upia simulator

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/evaluators/__init__.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/evaluators/__init__.py
@@ -39,6 +39,7 @@ __all__ = [
     "HateUnfairnessEvaluator",
     "ContentSafetyEvaluator",
     "ContentSafetyChatEvaluator",
+    "IndirectAttackEvaluator",
     "BleuScoreEvaluator",
     "GleuScoreEvaluator",
     "MeteorScoreEvaluator",
