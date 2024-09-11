7aeef6f433e98571b0b7624f57d3d89570b94949
Billy Hu <ninhu@microsoft.com>
2024-09-10 17:41:30 -0700

    math evaluator update (#37283)

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_common/utils.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_common/utils.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     import constants
 
-from typing import List, cast
+from typing import List
 
 import nltk
 import numpy as np
@@ -45,14 +45,11 @@ def get_harm_severity_level(harm_score: int) -> str:
 def nltk_tokenize(text: str) -> List[str]:
     """Tokenize the input text using the NLTK tokenizer."""
 
-    is_latin_or_numeric = all(
-        ("\u0020" <= c <= "\u007E")  # Basic Latin
-        or ("\u00A0" <= c <= "\u00FF")  # Latin-1 Supplement
-        or ("0" <= c <= "9")  # Digits
-        for c in text
-    )
+    if not text.isascii():
+        # Use NISTTokenizer for international tokenization
+        tokens = NISTTokenizer().international_tokenize(text)
+    else:
+        # By default, use NLTK word tokenizer
+        tokens = nltk.word_tokenize(text)
 
-    if is_latin_or_numeric:
-        return cast(List[str], nltk.word_tokenize(text))
-
-    return list(NISTTokenizer().international_tokenize(text))
+    return list(tokens)
