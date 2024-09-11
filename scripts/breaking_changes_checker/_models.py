dfa5d280099c8ed76b3758f488372ba259dc16d6
Catalina Peralta <caperal@microsoft.com>
2024-09-10 19:45:34 -0700

    update protocol model

--- scripts/breaking_changes_checker/_models.py
+++ scripts/breaking_changes_checker/_models.py
@@ -22,11 +22,17 @@ class Suppression(NamedTuple):
     function_name: Optional[str] = None
     parameter_or_property_name: Optional[str] = None
 
+class CheckerType:
+    MODULE = "module"
+    CLASS = "class"
+    FUNCTION_OR_METHOD = "function_or_method"
+
 @runtime_checkable
 class ChangesChecker(Protocol):
+    node_type: CheckerType
     name: str
     is_breaking: bool
     message: Union[str, dict]
 
-    def run_check(self, diff: dict, stable_nodes: dict, current_nodes: dict) -> List[BreakingChange]:
+    def run_check(self, diff: dict, stable_nodes: dict, current_nodes: dict, **kwargs) -> List[BreakingChange]:
         ...
