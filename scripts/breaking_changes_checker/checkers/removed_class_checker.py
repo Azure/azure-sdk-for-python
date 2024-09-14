4c1c848c37fd6891cc71fa67971d043323bb919b
Catalina Peralta <caperal@microsoft.com>
2024-09-14 00:10:51 -0700

    fix breaking class checker

--- scripts/breaking_changes_checker/checkers/removed_class_checker.py
+++ scripts/breaking_changes_checker/checkers/removed_class_checker.py
@@ -12,47 +12,29 @@ import jsondiff
 from typing import Dict, List
 
 class RemovedClassChecker:
-    node_type = CheckerType.FUNCTION_OR_METHOD
+    node_type = CheckerType.CLASS
     name = "RemovedOrRenamedClass"
     is_breaking = True
     message = {
         "removed_renamed_client": "Deleted or renamed client `{}`",
-        "removed_renamed_client_method": "Deleted or renamed client method `{}.{}`",
         "removed_renamed_class": "Deleted or renamed model `{}`",
-        "removed_renamed_class_method": "Deleted or renamed method `{}.{}`",
-        "removed_renamed_module_function": "Deleted or renamed function `{}`",
-        "removed_renamed_positional_method_param": "Method `{}.{}` deleted or renamed its parameter `{}` of kind `{}`",
-        "removed_renamed_positional_function_param": "Function `{}` deleted or renamed its parameter `{}` of kind `{}`",
-        "added_positional_param_method": "Method `{}.{}` inserted a `{}` parameter `{}`",
-        "added_positional_param_function": "Function `{}` inserted a `{}` parameter `{}`",
         "removed_renamed_client_instance_attribute": "Client `{}` deleted or renamed instance variable `{}`",
         "removed_renamed_class_instance_attribute": "Model `{}` deleted or renamed its instance variable `{}`",
         "removed_renamed_enum_value": "Deleted or renamed enum value `{}.{}`",
-        "changed_default_param_value_method": "Method `{}.{}` parameter `{}` changed default value from `{}` to `{}`",
-        "changed_default_param_value_function": "Function `{}` parameter `{}` changed default value from `{}` to `{}`",
-        "removed_default_param_value_method": "Method `{}.{}` removed default value `{}` from its parameter `{}`",
-        "removed_default_param_value_function": "Function `{}` removed default value `{}` from its parameter `{}`",
-        "changed_parameter_ordering_method": "Method `{}.{}` re-ordered its parameters from `{}` to `{}`",
-        "changed_parameter_ordering_function": "Function `{}` re-ordered its parameters from `{}` to `{}`",
-        "changed_parameter_kind_method": "Method `{}.{}` changed its parameter `{}` from `{}` to `{}`",
-        "changed_parameter_kind_function": "Function `{}` changed its parameter `{}` from `{}` to `{}`",
-        "changed_class_method_kind": "Method `{}.{}` changed from `{}` to `{}`",
-        "changed_function_kind": "Changed function `{}` from `{}` to `{}`",
-        "removed_module": "Deleted or renamed module `{}`",
-        "removed_class_method_kwargs": "Method `{}.{}` changed from accepting keyword arguments to not accepting them",
-        "removed_function_kwargs": "Function `{}` changed from accepting keyword arguments to not accepting them",
         "removed_renamed_operation_group": "Deleted or renamed client operation group `{}.{}`"
     }
 
     def run_check(self, diff, stable_nodes, current_nodes, **kwargs):
         breaking_changes = []
         module_name = kwargs.get("module_name", None)
+        if module_name not in stable_nodes:
+            # This is a new module, so we won't check for removed classes
+            return breaking_changes
         for class_name, class_components in diff.items():
             stable_class_nodes = stable_nodes[module_name]["class_nodes"]
             if class_name not in stable_class_nodes and not isinstance(class_name, jsondiff.Symbol):
                 continue  # this is a new model/additive change in current_nodes version so skip checks
-            
-            
+
             class_deleted = False
             if isinstance(class_name, jsondiff.Symbol):
                 deleted_classes = []
@@ -71,7 +53,7 @@ class RemovedClassChecker:
                         class_deleted = True
                     else:
                         bc = (
-                            self.message["default"],
+                            self.message["removed_renamed_class"],
                             "RemovedOrRenamedClass",
                             module_name, name
                         )
