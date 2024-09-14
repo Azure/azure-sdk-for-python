e0503090cb7f6124b256d16c508fe777b2017c34
Catalina Peralta <caperal@microsoft.com>
2024-09-14 00:11:04 -0700

    add checkers to supported list

--- scripts/breaking_changes_checker/supported_checkers.py
+++ scripts/breaking_changes_checker/supported_checkers.py
@@ -5,10 +5,14 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+from checkers.removed_class_checker import RemovedClassChecker
+from checkers.removed_class_method_checker import RemovedClassMethodChecker
 from checkers.removed_method_overloads_checker import RemovedMethodOverloadChecker
 from checkers.added_method_overloads_checker import AddedMethodOverloadChecker
 
 CHECKERS = [
+    RemovedClassChecker(),
+    RemovedClassMethodChecker(),
     RemovedMethodOverloadChecker(),
     AddedMethodOverloadChecker(),
 ]
