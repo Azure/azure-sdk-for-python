cde768bec6ad1756e4b60156a83799dd2b53798a
Miles Holland <milesholland@microsoft.com>
2024-09-20 13:04:18 -0400

    more base eval stuff

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_common/_base_eval.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_common/_base_eval.py
@@ -6,15 +6,25 @@ from typing import Optional, List, Dict
 
 from abc import ABC
 
+from typing import Dict, List
+
+import numpy as np
+
 class BaseQRCEval(ABC):
     """Base class for all evaluators that are capable of accepting either a query and response as input,
     or a conversation. The only thing that most child classes will need to implement is the _eval_qr method
-    to handle a single query/response pair.
+    to handle a single query/response pair. Although some might need to implement the _aggregate_results method.
+
+    param eval_last_turn: If True, only the last turn of the conversation will be evaluated, and no
+        aggregation will be performed. If False, all turns will be evaluated and the numeric results will be,
+        aggregated. Per-turn results are still be available in the output via the "evaluation_per_turn" key
+        when this occurs. Default is False, resulting full conversation evaluation and aggregation.
+    type eval_last_turn: bool
     """
 
 
-    def __init__(self, eval_):
-        pass
+    def __init__(self, eval_last_turn: bool = False):
+        self._eval_last_turn = eval_last_turn
 
     def __call__(self, query: Optional[str] = None, response: Optional[str] = None, conversation: Optional[List[Dict]] = None):
         """_summary_
@@ -30,8 +40,7 @@ class BaseQRCEval(ABC):
             if conversation is not None:
                 pass # TODO another exception
             return self._eval_qr(query, response)
-        elif conversation is not None: # Technically not needed.
-                
+        elif conversation is not None:
             # Extract queries, responses from conversation
             queries = []
             responses = []
@@ -42,6 +51,8 @@ class BaseQRCEval(ABC):
             else:
                 conversation_slice = conversation
 
+            # Convert conversation slice into queries and responses.
+            # Assume that 'user' role is asking queries and 'assistant' role is responding.
             for each_turn in conversation_slice:
                 role = each_turn["role"]
                 if role == "user":
@@ -52,31 +63,18 @@ class BaseQRCEval(ABC):
             # Evaluate each turn
             per_turn_results = []
             for turn_num in range(len(queries)):
-                current_turn_result = {}
+                per_turn_results.append(self._eval_qr(queries[turn_num], responses[turn_num]))
 
-                if self._parallel:
-                    # Parallel execution
-                    # Use a thread pool for parallel execution in the composite evaluator,
-                    # as it's ~20% faster than asyncio tasks based on tests.
-                    with ThreadPoolExecutor() as executor:
-                        future_to_evaluator = {
-                            executor.submit(self._evaluate_turn, turn_num, queries, responses, evaluator): evaluator
-                            for evaluator in self._evaluators
-                        }
-
-                        for future in as_completed(future_to_evaluator):
-                            result = future.result()
-                            current_turn_result.update(result)
-                else:
-                    # Sequential execution
-                    for evaluator in self._evaluators:
-                        result = self._evaluate_turn(turn_num, queries, responses, evaluator)
-                        current_turn_result.update(result)
-
-                per_turn_results.append(current_turn_result)
-
-            aggregated = self._aggregate_results(per_turn_results)
-            return aggregated
+            # Aggregate results if there was more than 1 conversation turn, otherwise
+            # Return the results directly.
+            if len(per_turn_results) > 1:
+                return self._aggregate_results(per_turn_results)
+            elif len(per_turn_results) == 1:
+                return per_turn_results[0]
+            else:
+                return {} # TODO should this be an exception instead
+        else:
+            raise NotImplementedError("Input type unrecognized by generic evaluator input parser.")
 
     def _eval_qr(self, query: str, response: str) -> Dict:
         """_summary_
@@ -88,4 +86,33 @@ class BaseQRCEval(ABC):
         Returns:
             Dict: _description_
         """
-        raise NotImplementedError("Evaluators need to define ") # Raise not implemented error
+        raise NotImplementedError("Evaluators need to define their own query/response evaluation") # Raise not implemented error
+    
+    def _aggregate_results(self, per_turn_results: List[Dict]) -> Dict:
+        """Aggregate the evaluation results of each conversation turn into a single result.
+
+        Exact implementation might need to vary slightly depending on the results produced.
+        Default behavior is to average the all number-based outputs, and 
+        raise NotImplementedError("Evaluators need to define their own aggregation method")
+        
+        param per_turn_results: List of evaluation results for each turn in the conversation.
+        type per_turn_results: List[Dict]
+        """
+
+        aggregated = {}
+        evaluation_per_turn = {}
+
+        for turn in per_turn_results:
+            for metric, value in turn.items():
+                if metric not in evaluation_per_turn.keys():
+                    evaluation_per_turn[metric] = []
+                evaluation_per_turn[metric].append(value)
+
+
+        for metric, values in evaluation_per_turn.items():
+            if all(isinstance(value, (int, float)) for value in values):
+                aggregated[metric] = np.mean(values)
+
+        aggregated["evaluation_per_turn"] = evaluation_per_turn
+
+        return aggregated
---
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_common/_base_rai_svc_eval.py
@@ -0,0 +1,54 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+from typing import Optional, List, Dict, override
+
+from abc import ABC
+
+from typing import Dict
+from azure.core.credentials import TokenCredential
+from azure.ai.evaluation._common.constants import EvaluationMetrics
+from azure.ai.evaluation._common.rai_service import evaluate_with_rai_service
+from ._base_eval import BaseQRCEval
+
+class BaseRaiServiceEval(BaseQRCEval):
+    """Base class for all evaluators that require the use of the Azure AI RAI service for evaluation.
+    This includes content safety evaluators, protected material evaluators, and others.
+
+    param azure_ai_project: The scope of the Azure AI project.
+    type azure_ai_project: dict
+    param credential: The credential for connecting to the Azure AI project.
+    type credential: ~azure.core.credentials.TokenCredential
+    param eval_last_turn: If True, only the last turn of the conversation will be evaluated, and no
+        aggregation will be performed. If False, all turns will be evaluated and the numeric results will be,
+        aggregated. Per-turn results are still be available in the output via the "evaluation_per_turn" key
+        when this occurs. Default is False, resulting full conversation evaluation and aggregation.
+    type eval_last_turn: bool
+    """
+
+
+    def __init__(self, eval_metric: EvaluationMetrics, azure_ai_project: Dict, credential: TokenCredential, eval_last_turn: bool = False):
+        self._eval_metric = eval_metric
+        self._eval_last_turn = eval_last_turn
+        self._azure_ai_project = azure_ai_project
+        self._credential = credential
+
+    @override
+    async def _eval_qr(self, query: str, response: str) -> Dict:
+        """_summary_
+
+        Args:
+            query (str): _description_
+            response (str): _description_
+
+        Returns:
+            Dict: _description_
+        """
+        return await evaluate_with_rai_service(
+            metric_name=self._eval_metric,
+            query=query,
+            response=response,
+            project_scope=self._azure_ai_project,
+            credential=self._credential,
+        )
