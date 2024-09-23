bdd03c374c89c56832bb1655157096ab482dd58c
azure-sdk <PythonSdkPipelines>
2024-09-23 10:26:16 +0000

    code and test

--- sdk/mongocluster/azure-mgmt-mongocluster/CHANGELOG.md
+++ sdk/mongocluster/azure-mgmt-mongocluster/CHANGELOG.md
@@ -1,5 +1,46 @@
 # Release History
 
+## 1.0.0b2 (2024-09-23)
+
+### Features Added
+
+  - Client `MongoClusterMgmtClient` added operation group `replicas`
+  - Enum `CreateMode` added member `GEO_REPLICA`
+  - Enum `CreateMode` added member `REPLICA`
+  - Model `MongoClusterProperties` added method `infrastructure_version`
+  - Model `MongoClusterProperties` added method `preview_features`
+  - Model `MongoClusterProperties` added method `replica`
+  - Model `MongoClusterProperties` added method `replica_parameters`
+  - Model `MongoClusterProperties` added property `replica_parameters`
+  - Model `MongoClusterProperties` added property `preview_features`
+  - Model `MongoClusterProperties` added property `replica`
+  - Model `MongoClusterProperties` added property `infrastructure_version`
+  - Model `MongoClusterRestoreParameters` added method `point_in_time_utc`
+  - Model `MongoClusterRestoreParameters` added property `point_in_time_utc`
+  - Model `MongoClusterUpdateProperties` added method `preview_features`
+  - Model `MongoClusterUpdateProperties` added property `preview_features`
+  - Model `NodeGroupSpec` added method `disk_size_gb`
+  - Model `NodeGroupSpec` added property `disk_size_gb`
+  - Added model `MongoClusterReplicaParameters`
+  - Added enum `PreviewFeature`
+  - Added enum `PromoteMode`
+  - Added enum `PromoteOption`
+  - Added model `PromoteReplicaRequest`
+  - Added model `Replica`
+  - Added model `ReplicationProperties`
+  - Added enum `ReplicationRole`
+  - Added enum `ReplicationState`
+  - Model `MongoClustersOperations` added method `begin_promote`
+  - Added model `ReplicasOperations`
+
+### Breaking Changes
+
+  - Model `MongoClusterRestoreParameters` deleted or renamed its instance variable `point_in_time_u_t_c`
+  - Deleted or renamed method `MongoClusterRestoreParameters.point_in_time_u_t_c`
+  - Model `NodeGroupSpec` deleted or renamed its instance variable `disk_size_g_b`
+  - Deleted or renamed method `NodeGroupSpec.disk_size_g_b`
+  - OperationDisplay.__init__ had all overloads removed
+
 ## 1.0.0b1 (2024-07-01)
 
 ### Other Changes
--- sdk/mongocluster/azure-mgmt-mongocluster/tests/test_cli_mgmt_mongocluster_lro.py
+++ sdk/mongocluster/azure-mgmt-mongocluster/tests/test_cli_mgmt_mongocluster_lro.py
@@ -1,8 +1,9 @@
 from azure.mgmt.mongocluster import MongoClusterMgmtClient
 from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGroupPreparer, recorded_by_proxy
 
-AZURE_LOCATION = 'westus2'
+AZURE_LOCATION = "westus2"
 Mongo_Cluster_Name = "pythontestmongocluster"
+
 
 class TestMgmtMongoCluster(AzureMgmtRecordedTestCase):
 
@@ -13,64 +14,52 @@ class TestMgmtMongoCluster(AzureMgmtRecordedTestCase):
     @recorded_by_proxy
     def test_mongo_clusters_mgmt(self, resource_group):
         response = self.client.mongo_clusters.begin_create_or_update(
-            resource_group_name = resource_group.name,
-            mongo_cluster_name = Mongo_Cluster_Name,
-            resource = {
+            resource_group_name=resource_group.name,
+            mongo_cluster_name=Mongo_Cluster_Name,
+            resource={
                 "location": AZURE_LOCATION,
                 "properties": {
                     "administratorLogin": "myMongoCluster",
                     "administratorLoginPassword": "myMongoCluster333",
                     "serverVersion": "5.0",
                     "nodeGroupSpecs": [
-                        {
-                            "diskSizeGB": 128,
-                            "enableHa": True,
-                            "kind": "Shard",
-                            "nodeCount": 1,
-                            "sku": "M30"
-                        }
+                        {"diskSizeGB": 128, "enableHa": True, "kind": "Shard", "nodeCount": 1, "sku": "M30"}
                     ],
                 },
-            }
+            },
         ).result()
         assert response
 
         response = self.client.mongo_clusters.begin_update(
-            resource_group_name = resource_group.name,
-            mongo_cluster_name = Mongo_Cluster_Name,
-            properties = {
+            resource_group_name=resource_group.name,
+            mongo_cluster_name=Mongo_Cluster_Name,
+            properties={
                 "location": AZURE_LOCATION,
                 "properties": {
                     "administratorLogin": "myMongoCluster",
                     "administratorLoginPassword": "myMongoCluster333",
                     "serverVersion": "5.0",
                     "nodeGroupSpecs": [
-                        {
-                            "kind": "Shard",
-                            "sku": "M50",
-                            "diskSizeGB": 256,
-                            "enableHa": True,
-                            "nodeCount": 1
-                        }
+                        {"kind": "Shard", "sku": "M50", "diskSizeGB": 256, "enableHa": True, "nodeCount": 1}
                     ],
-                    "publicNetworkAccess": "Enabled"
+                    "publicNetworkAccess": "Enabled",
                 },
-            }
+            },
         ).result()
         assert response
 
         response = self.client.mongo_clusters.get(
-            resource_group_name = resource_group.name,
-            mongo_cluster_name = Mongo_Cluster_Name,
+            resource_group_name=resource_group.name,
+            mongo_cluster_name=Mongo_Cluster_Name,
         )
         assert response
 
         response = self.client.mongo_clusters.list_by_resource_group(
-            resource_group_name = resource_group.name,
+            resource_group_name=resource_group.name,
         )
         assert len(list(response)) == 1
 
         response = self.client.mongo_clusters.begin_delete(
-            resource_group_name = resource_group.name,
-            mongo_cluster_name = Mongo_Cluster_Name,
+            resource_group_name=resource_group.name,
+            mongo_cluster_name=Mongo_Cluster_Name,
         ).result()
