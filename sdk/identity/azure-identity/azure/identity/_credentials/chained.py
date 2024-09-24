d0205dc9edeb18e672c784df1a7c740118d856f5
Scott Addie <scott.addie@microsoft.com>
2024-09-24 09:05:18 -0500

    Drive awareness of credential chains doc

--- sdk/identity/azure-identity/azure/identity/_credentials/chained.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/chained.py
@@ -39,6 +39,8 @@ class ChainedTokenCredential:
     Its :func:`get_token` method calls ``get_token`` on each credential in the sequence, in order, returning the first
     valid token received.
 
+    For more information, see [ChainedTokenCredential overview](https://aka.ms/azsdk/python/identity/credential-chains#chainedtokencredential-overview).
+
     :param credentials: credential instances to form the chain
     :type credentials: ~azure.core.credentials.TokenCredential
 
