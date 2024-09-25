da4820ad6d69ec51dcc7a4b9217c4a3b70822f81
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2024-09-25 13:43:57 -0700

    Pipeline V3 (#37428)
    * refactor package selection methodology to utilize Save-Package-Properties for package targeting
    * introduce the python - pullrequest build definition that is triggered against all sdk/ directories, and builds/tests the packages which were changed
      * this also includes abstractions to reduce runtime by distributing sets of packages and duplicating matrix definition
    * refactor all analyze checks which used to operate on ServiceDirectory to instead operate upon the packages present within a PackageInfoFolder from Save-Package-Properties
    * miscellaneous tooling changes to account for new matrix possibilities
    
    ---------
    
    Co-authored-by: Wes Haggard <weshaggard@users.noreply.github.com>
    Co-authored-by: Patrick Hallisey <pahallis@microsoft.com>

--- eng/scripts/get_package_properties.py
+++ eng/scripts/get_package_properties.py
@@ -1,10 +1,43 @@
 import argparse
-import sys
-import glob
 import os
 import re
 
+from typing import Dict, List
+
 from ci_tools.parsing import ParsedSetup
+
+additional_pr_triggers: Dict[str, List[str]] = {
+    "azure-core":[
+        os.path.join("/sdk", "servicebus", "azure-servicebus"),
+        os.path.join("/sdk", "eventhub", "azure-eventhub"),
+        os.path.join("/sdk", "tables", "azure-data-table"),
+        os.path.join("/sdk", "appconfig", "azure-appconfig"),
+        os.path.join("/sdk", "keyvault", "azure-keyvault-keys"),
+        os.path.join("/sdk", "identity", "azure-identity"),
+        os.path.join("/sdk", "core", "azure-mgmt-core"),
+        os.path.join("/sdk", "core", "azure-core-experimental"),
+        os.path.join("/sdk", "core", "azure-core-tracing-opentelemetry"),
+        os.path.join("/sdk", "core", "azure-core-tracing-opencensus"),
+        # todo: this currently won't be included, as azure-cosmos needs some special construction
+        # related to windows only + emulator
+        # os.path.join("/sdk", "cosmos", "azure-cosmos"),
+        os.path.join("/sdk", "ml", "azure-ai-ml"),
+        os.path.join("/sdk", "ai", "azure-ai-documentintelligence"),
+        os.path.join("/sdk", "ai", "azure-ai-inference"),
+        os.path.join("/sdk", "ai", "azure-ai-textanalytics"),
+        os.path.join("/sdk", "ai", "azure-ai-doctranslation"),
+        os.path.join("/sdk", "compute", "azure-mgmt-compute"),
+        os.path.join("/sdk", "communication", "azure-communication-chat"),
+        os.path.join("/sdk", "communication", "azure-communication-identity"),
+        os.path.join("/sdk", "storage", "azure-storage-blob")
+    ],
+    "azure-mgmt-core": [
+        os.path.join("/sdk", "compute", "azure-mgmt-compute"),
+        os.path.join("/sdk", "network", "azure-mgmt-network"),
+        os.path.join("/sdk", "resource", "azure-mgmt-resource"),
+        os.path.join("/sdk", "keyvault", "azure-mgmt-keyvault")
+    ]
+}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get package version details from the repo")
@@ -18,9 +51,17 @@ if __name__ == "__main__":
                 try:
                     parsed = ParsedSetup.from_path(root)
 
+                    dependent_packages = ""
+                    if parsed.name in additional_pr_triggers:
+                        dependent_packages = ",".join(additional_pr_triggers[parsed.name])
+
                     print(
-                        "{0} {1} {2} {3}".format(
-                            parsed.name, parsed.version, parsed.is_new_sdk, os.path.dirname(parsed.setup_filename)
+                        "{0} {1} {2} {3} {4}".format(
+                            parsed.name,
+                            parsed.version,
+                            parsed.is_new_sdk,
+                            os.path.dirname(parsed.setup_filename),
+                            dependent_packages
                         )
                     )
                 except:
