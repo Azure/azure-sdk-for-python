896a27553268f91a39851ce744c467a7db63508a
Peter Wu <weirongwu@microsoft.com>
2024-09-25 00:06:20 -0700

    ShareProperties and added tests for provisioned v2 billing

--- sdk/storage/azure-storage-file-share/tests/test_share.py
+++ sdk/storage/azure-storage-file-share/tests/test_share.py
@@ -1818,6 +1818,49 @@ class TestStorageShare(StorageRecordedTestCase):
         finally:
             self._delete_shares()
 
+    @FileSharePreparer()
+    @recorded_by_proxy
+    def test_provisioned_billing_v2(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+
+        try:
+            self._setup(storage_account_name, storage_account_key)
+
+            share_name = self.get_resource_name(TEST_SHARE_PREFIX)
+            share = self.fsc.get_share_client(share_name)
+            self.test_shares.append(share_name)
+
+            share.create_share(share_provisioned_iops=500, share_provisioned_bandwidth_mibps=150)
+            props = share.get_share_properties()
+            assert props is not None
+            assert props.provisioned_iops == 500
+            assert props.provisioned_bandwidth == 150
+            assert props.included_burst_iops is not None
+            assert props.max_burst_credits_for_iops is not None
+            assert props.next_allowed_provisioned_iops_downgrade_time is not None
+            assert props.next_allowed_provisioned_bandwidth_downgrade_time is not None
+
+            share.set_share_properties(
+                access_tier="Hot",
+                share_provisioned_iops=3000,
+                share_provisioned_bandwidth_mibps=125
+            )
+
+            shares = list(self.fsc.list_shares())
+
+            assert shares is not None
+            assert len(shares) >= 1
+            assert shares[0].name == share_name
+            assert shares[0].provisioned_iops == 3000
+            assert shares[0].provisioned_bandwidth == 125
+            assert shares[0].included_burst_iops is not None
+            assert shares[0].max_burst_credits_for_iops is not None
+            assert shares[0].next_allowed_provisioned_iops_downgrade_time is not None
+            assert shares[0].next_allowed_provisioned_bandwidth_downgrade_time is not None
+        finally:
+            self._delete_shares()
+
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
     unittest.main()
