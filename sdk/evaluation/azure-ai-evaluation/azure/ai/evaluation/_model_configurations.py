17afc7cb0c6e9c00f61cbafc48a859230d54a9b6
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2024-09-25 16:34:38 -0400

    fix docstrings

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_model_configurations.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_model_configurations.py
@@ -10,39 +10,39 @@ from typing_extensions import NotRequired
 class AzureOpenAIModelConfiguration(TypedDict, total=False):
     """Model Configuration for Azure OpenAI Model"""
 
-    """The type of the model configuration. Should be 'azure_openai' for AzureOpenAIModelConfiguration"""
     type: Literal["azure_openai"]
-    """Name of Azure OpenAI deployment to make request to"""
+    """The type of the model configuration. Should be 'azure_openai' for AzureOpenAIModelConfiguration"""
     azure_deployment: str
-    """Endpoint of Azure OpenAI resource to make request to"""
+    """Name of Azure OpenAI deployment to make request to"""
     azure_endpoint: str
-    "API key of Azure OpenAI resource"
+    """Endpoint of Azure OpenAI resource to make request to"""
     api_key: str
-    """(Optional) API version to use in request to Azure OpenAI deployment"""
+    """API key of Azure OpenAI resource"""
     api_version: NotRequired[str]
+    """(Optional) API version to use in request to Azure OpenAI deployment"""
 
 
 class OpenAIModelConfiguration(TypedDict, total=False):
     """Model Configuration for OpenAI Model"""
 
-    """The type of the model configuration. Should be 'openai' for OpenAIModelConfiguration"""
     type: Literal["openai"]
-    "API key needed to make request to model"
+    """The type of the model configuration. Should be 'openai' for OpenAIModelConfiguration"""
     api_key: str
-    """Name of model to be used in OpenAI request"""
+    "API key needed to make request to model"
     model: str
-    """(Optional) Base URL to be used in OpenAI request"""
+    """Name of model to be used in OpenAI request"""
     base_url: NotRequired[str]
-    """(Optional) OpenAI organization"""
+    """(Optional) Base URL to be used in OpenAI request"""
     organization: NotRequired[str]
+    """(Optional) OpenAI organization"""
 
 
 class AzureAIProject(TypedDict):
     """Azure AI Project Information"""
 
-    """Azure subscription id of the project"""
     subscription_id: str
-    """Azure resource group name of the project"""
+    """Azure subscription id of the project"""
     resource_group_name: str
-    """Azure project name"""
+    """Azure resource group name of the project"""
     project_name: str
+    """Azure project name"""
