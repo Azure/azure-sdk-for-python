efdc18a404839aaaf6594a50160d2d963f86cff9
Nagkumar Arkalgud <nagkumar91@users.noreply.github.com>
2024-10-03 15:03:15 -0700

    Update _simulator.py

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/simulator/_simulator.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/simulator/_simulator.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-# pylint: disable=W0102,W0613,R0914,C0301,E0401,E0611
+# pylint: disable=W0102,W0613,R0914,C0301,E0401,E0611,C0411
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
