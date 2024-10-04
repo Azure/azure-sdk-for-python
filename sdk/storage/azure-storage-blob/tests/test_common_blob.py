1175ec8d8103e37f7db873d52018413bdf5ebc9a
Vincent Tran <vincenttran@microsoft.com>
2024-10-04 13:48:31 -0700

    Debugged

--- sdk/storage/azure-storage-blob/tests/test_common_blob.py
+++ sdk/storage/azure-storage-blob/tests/test_common_blob.py
@@ -3230,7 +3230,123 @@ class TestStorageCommonBlob(StorageRecordedTestCase):
             mgmt_client.blob_containers.delete(storage_resource_group_name, versioned_storage_account_name, container_name)
 
         return variables
+    
+    @BlobPreparer()
+    @recorded_by_proxy
+    def test_snapshot_immutability_policy_and_legal_hold(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+        variables = kwargs.pop("variables", {})
 
+        self._setup(storage_account_name, storage_account_key)
+
+        blob_name = self._create_block_blob()
+        blob = self.bsc.get_blob_client(self.container_name, blob_name)
+        snapshot_blob = self.bsc.get_blob_client(self.container_name, blob_name, snapshot=blob.create_snapshot())
+
+        try:
+            expiry_time = self.get_datetime_variable(variables, 'expiry_time', datetime.utcnow() + timedelta(seconds=5))
+            immutability_policy = ImmutabilityPolicy(
+                expiry_time=expiry_time,
+                policy_mode=BlobImmutabilityPolicyMode.Unlocked
+            )
+
+            snapshot_blob.set_immutability_policy(immutability_policy=immutability_policy)
+            props = snapshot_blob.get_blob_properties()
+            assert props['immutability_policy']['expiry_time'] is not None
+            assert props['immutability_policy']['policy_mode'] == "unlocked"
+
+            snapshot_blob.delete_immutability_policy()
+            props = snapshot_blob.get_blob_properties()
+            assert props['immutability_policy']['expiry_time'] is None
+            assert props['immutability_policy']['policy_mode'] is None
+
+            snapshot_blob.set_legal_hold(True)
+            props = snapshot_blob.get_blob_properties()
+            assert props['has_legal_hold']
+        finally:
+            snapshot_blob.set_legal_hold(False)
+            blob.delete_blob(delete_snapshots="include")
+
+    @BlobPreparer()
+    @recorded_by_proxy
+    def test_snapshot_immutability_policy_and_legal_hold(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+        variables = kwargs.pop("variables", {})
+
+        self._setup(storage_account_name, storage_account_key)
+
+        blob_name = self._create_block_blob()
+        blob = self.bsc.get_blob_client(self.container_name, blob_name)
+        snapshot_blob = self.bsc.get_blob_client(self.container_name, blob_name, snapshot=blob.create_snapshot())
+
+        try:
+            expiry_time = self.get_datetime_variable(variables, 'expiry_time', datetime.utcnow() + timedelta(seconds=5))
+            immutability_policy = ImmutabilityPolicy(
+                expiry_time=expiry_time,
+                policy_mode=BlobImmutabilityPolicyMode.Unlocked
+            )
+
+            snapshot_blob.set_immutability_policy(immutability_policy=immutability_policy)
+            props = snapshot_blob.get_blob_properties()
+            assert props['immutability_policy']['expiry_time'] is not None
+            assert props['immutability_policy']['policy_mode'] == "unlocked"
+
+            snapshot_blob.delete_immutability_policy()
+            props = snapshot_blob.get_blob_properties()
+            assert props['immutability_policy']['expiry_time'] is None
+            assert props['immutability_policy']['policy_mode'] is None
+
+            snapshot_blob.set_legal_hold(True)
+            props = snapshot_blob.get_blob_properties()
+            assert props['has_legal_hold']
+        finally:
+            snapshot_blob.set_legal_hold(False)
+            blob.delete_blob(delete_snapshots="include")
+
+    @BlobPreparer()
+    @recorded_by_proxy
+    def test_versioning_immutability_policy_and_legal_hold_new_new_new_new_new(self, **kwargs):
+        versioned_storage_account_name = kwargs.pop("versioned_storage_account_name")
+        versioned_storage_account_key = kwargs.pop("versioned_storage_account_key")
+        variables = kwargs.pop("variables", {})
+
+        self._setup(versioned_storage_account_name, versioned_storage_account_key)
+
+        blob_name = self.get_resource_name('blob')
+        root_blob = self.bsc.get_blob_client(self.container_name, blob_name) # This is the BlobClient that will point to latest version / root
+        old_version_dict = root_blob.upload_blob(b"abc", overwrite=True) # Capture the old dict values here so we have a non-root version_id
+        root_blob.upload_blob(b"abcdef", overwrite=True) # This will create a new version, latest version -> abcdef | old version -> abc
+
+        try:
+            expiry_time = self.get_datetime_variable(variables, 'expiry_time', datetime.utcnow() + timedelta(seconds=5))
+            immutability_policy = ImmutabilityPolicy(
+                expiry_time=expiry_time,
+                policy_mode=BlobImmutabilityPolicyMode.Unlocked
+            )
+            old_version_blob = self.bsc.get_blob_client( # Spin up a BlobClient that is pointed to the non-root version_id
+                self.container_name, blob_name,
+                version_id=old_version_dict['version_id']
+            )
+
+            old_version_blob.set_immutability_policy(immutability_policy=immutability_policy) # Set immutability on not-root (as notebook states)
+            props = root_blob.get_blob_properties() # Get properties of the root blob -- the setting does not show on old_version_blob props
+            assert props['immutability_policy']['expiry_time'] is not None
+            assert props['immutability_policy']['policy_mode'] == "unlocked"
+
+            old_version_blob.delete_immutability_policy()
+            props = root_blob.get_blob_properties()
+            assert props['immutability_policy']['expiry_time'] is None
+            assert props['immutability_policy']['policy_mode'] is None
+
+            old_version_blob.set_legal_hold(True) # Set legal hold not on root blob (as notebook states)
+            props = root_blob.get_blob_properties() # Get properties of the root blob -- the setting does not show on old_version_blob props
+            assert props['has_legal_hold']
+        finally:
+            root_blob.set_legal_hold(False)
+            root_blob.delete_blob(delete_snapshots="include")
+    
     @BlobPreparer()
     @recorded_by_proxy
     def test_validate_empty_blob(self, **kwargs):
