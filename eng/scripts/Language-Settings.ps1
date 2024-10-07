a7c11476be82eabd217f298c493c50c7ec0f7e01
Scott Beddall <scbedd@microsoft.com>
2024-10-07 15:39:23 -0700

    fix language-settings

--- eng/scripts/Language-Settings.ps1
+++ eng/scripts/Language-Settings.ps1
@@ -39,7 +39,7 @@ function Get-python-AdditionalValidationPackagesFromPackageSet {
 
   if (-not $diffObj.ChangedFiles) {
     Write-Host "No files positively changed. Returning azure-template as fallback."
-    $additionalPackage = $AllPkgProps | Where-Object { $_.Name -eq "azure-template" } | Select-Object -First 1 }
+    $additionalPackage = $AllPkgProps | Where-Object { $_.Name -eq "azure-template" } | Select-Object -First 1
     $additionalValidationPackages += $additionalPackage
   }
 
