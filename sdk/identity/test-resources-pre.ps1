15fc7440cbc9833557b1175213b763b45623f863
Ben Broderick Phillips <bebroder@microsoft.com>
2024-10-08 17:16:40 -0400

    Fix aks region version lookup

--- sdk/identity/test-resources-pre.ps1
+++ sdk/identity/test-resources-pre.ps1
@@ -8,9 +8,6 @@ param (
     [Parameter(ValueFromRemainingArguments = $true)]
     $RemainingArguments,
 
-    [Parameter()]
-    [string] $Location = '',
-
     [Parameter(Mandatory = $true)]
     [ValidatePattern('^[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$')]
     [string] $TestApplicationId,
@@ -54,7 +51,8 @@ Write-Host "Azure CLI version: $az_version"
 az cloud set --name $Environment
 az login --service-principal -u $TestApplicationId --tenant $TenantId --allow-no-subscriptions --federated-token $env:ARM_OIDC_TOKEN
 az account set --subscription $SubscriptionId
-$versions = az aks get-versions -l westus -o json | ConvertFrom-Json
+$region = if ($Environment -eq 'AzureUSGovernment') { 'usgovvirginia' } elseif ($Environment -eq 'AzureChinaCloud') { 'chinanorth3' } else { 'westus' }
+$versions = az aks get-versions -l $region -o json | ConvertFrom-Json
 Write-Host "AKS versions: $($versions | ConvertTo-Json -Depth 100)"
 $patchVersions = $versions.values | Where-Object { $_.isPreview -eq $null } | Select-Object -ExpandProperty patchVersions
 Write-Host "AKS patch versions: $($patchVersions | ConvertTo-Json -Depth 100)"
