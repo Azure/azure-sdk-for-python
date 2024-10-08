3fff0bfdeea53681e244807357f5f5b6f16d7a3e
Nagkumar Arkalgud <nagkumar@naarkalg-work-mac.local>
2024-10-08 13:37:16 -0700

    Trying a fix for mypy

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/simulator/_model_tools/_identity_manager.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/simulator/_model_tools/_identity_manager.py
@@ -11,7 +11,7 @@ from abc import ABC, abstractmethod
 from enum import Enum
 from typing import Optional, Union
 
-from azure.core.credentials import TokenCredential
+from azure.core.credentials import TokenCredential, AccessToken
 from azure.identity import DefaultAzureCredential, ManagedIdentityCredential
 
 AZURE_TOKEN_REFRESH_INTERVAL = 600  # seconds
