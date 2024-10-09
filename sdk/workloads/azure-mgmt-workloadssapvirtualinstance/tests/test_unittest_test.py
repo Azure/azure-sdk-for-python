bb3514ea320f0eeecf70fc18cc16923c1906f5d4
ChenxiJiang333 <v-chenjiang@microsoft.com>
2024-10-09 18:03:14 +0800

    update-testcase

--- sdk/workloads/azure-mgmt-workloadssapvirtualinstance/tests/test_unittest_test.py
+++ sdk/workloads/azure-mgmt-workloadssapvirtualinstance/tests/test_unittest_test.py
@@ -6,13 +6,14 @@
 # license information.
 # --------------------------------------------------------------------------
 from azure.mgmt.workloadssapvirtualinstance import WorkloadsSapVirtualInstanceMgmtClient
+from azure.mgmt.workloadssapvirtualinstance.models import SAPConfiguration
 from azure.identity import DefaultAzureCredential
 
 
 def test_unittest_model():
     client = WorkloadsSapVirtualInstanceMgmtClient(DefaultAzureCredential(), "12345")
     body = {"configurationType": "DeploymentWithOSConfig"}
-    result1 = client._deserialize("SAPConfiguration", body)
-    result2 = client._deserialize("SAPConfiguration", body)
+    result1 = client._deserialize(SAPConfiguration, body)
+    result2 = client._deserialize(SAPConfiguration, body)
     assert result1.configuration_type == result2.configuration_type
     assert body["configurationType"] == "DeploymentWithOSConfig"
