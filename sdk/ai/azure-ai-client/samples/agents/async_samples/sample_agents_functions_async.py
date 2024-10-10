5c5031567aee73a7e967bfe5118317eda5613dba
howieleung <howieleung@microsoft.com>
2024-10-10 11:01:41 -0700

    moved ai_client to main function

--- sdk/ai/azure-ai-client/samples/agents/async_samples/sample_agents_functions_async.py
+++ sdk/ai/azure-ai-client/samples/agents/async_samples/sample_agents_functions_async.py
@@ -31,30 +31,30 @@ import os
 
 from user_async_functions import user_async_functions
 
-# Create an Azure AI Client from a connection string, copied from your AI Studio project.
-# At the moment, it should be in the format "<HostName>;<AzureSubscriptionId>;<ResourceGroup>;<HubName>"
-# Customer needs to login to Azure subscription via Azure CLI and set the environment variables
-
-connection_string = os.environ["AI_CLIENT_CONNECTION_STRING"]
-
-ai_client = AzureAIClient.from_connection_string(
-    credential=DefaultAzureCredential(),
-    connection=connection_string,
-)
-
-# Or, you can create the Azure AI Client by giving all required parameters directly
-"""
-ai_client = AzureAIClient(
-    credential=DefaultAzureCredential(),
-    host_name=os.environ["AI_CLIENT_HOST_NAME"],
-    subscription_id=os.environ["AI_CLIENT_SUBSCRIPTION_ID"],
-    resource_group_name=os.environ["AI_CLIENT_RESOURCE_GROUP_NAME"],
-    workspace_name=os.environ["AI_CLIENT_WORKSPACE_NAME"],
-    logging_enable=True, # Optional. Remove this line if you don't want to show how to enable logging
-)
-"""
-
 async def main():
+    # Create an Azure AI Client from a connection string, copied from your AI Studio project.
+    # At the moment, it should be in the format "<HostName>;<AzureSubscriptionId>;<ResourceGroup>;<HubName>"
+    # Customer needs to login to Azure subscription via Azure CLI and set the environment variables
+
+    connection_string = os.environ["AI_CLIENT_CONNECTION_STRING"]
+
+    ai_client = AzureAIClient.from_connection_string(
+        credential=DefaultAzureCredential(),
+        connection=connection_string,
+    )
+
+    # Or, you can create the Azure AI Client by giving all required parameters directly
+    """
+    ai_client = AzureAIClient(
+        credential=DefaultAzureCredential(),
+        host_name=os.environ["AI_CLIENT_HOST_NAME"],
+        subscription_id=os.environ["AI_CLIENT_SUBSCRIPTION_ID"],
+        resource_group_name=os.environ["AI_CLIENT_RESOURCE_GROUP_NAME"],
+        workspace_name=os.environ["AI_CLIENT_WORKSPACE_NAME"],
+        logging_enable=True, # Optional. Remove this line if you don't want to show how to enable logging
+    )
+    """    
+    
     async with ai_client:
         # Initialize assistant functions
         functions = AsyncFunctionTool(functions=user_async_functions)
--- sdk/ai/azure-ai-client/samples/agents/async_samples/sample_agents_stream_iteration_async.py
+++ sdk/ai/azure-ai-client/samples/agents/async_samples/sample_agents_stream_iteration_async.py
@@ -29,30 +29,30 @@ from azure.identity import DefaultAzureCredential
 
 import os
 
-# Create an Azure AI Client from a connection string, copied from your AI Studio project.
-# At the moment, it should be in the format "<HostName>;<AzureSubscriptionId>;<ResourceGroup>;<HubName>"
-# Customer needs to login to Azure subscription via Azure CLI and set the environment variables
-
-connection_string = os.environ["AI_CLIENT_CONNECTION_STRING"]
-
-ai_client = AzureAIClient.from_connection_string(
-    credential=DefaultAzureCredential(),
-    connection=connection_string,
-)
-
-# Or, you can create the Azure AI Client by giving all required parameters directly
-"""
-ai_client = AzureAIClient(
-    credential=DefaultAzureCredential(),
-    host_name=os.environ["AI_CLIENT_HOST_NAME"],
-    subscription_id=os.environ["AI_CLIENT_SUBSCRIPTION_ID"],
-    resource_group_name=os.environ["AI_CLIENT_RESOURCE_GROUP_NAME"],
-    workspace_name=os.environ["AI_CLIENT_WORKSPACE_NAME"],
-    logging_enable=True, # Optional. Remove this line if you don't want to show how to enable logging
-)
-"""
-
 async def main():
+    # Create an Azure AI Client from a connection string, copied from your AI Studio project.
+    # At the moment, it should be in the format "<HostName>;<AzureSubscriptionId>;<ResourceGroup>;<HubName>"
+    # Customer needs to login to Azure subscription via Azure CLI and set the environment variables
+
+    connection_string = os.environ["AI_CLIENT_CONNECTION_STRING"]
+
+    ai_client = AzureAIClient.from_connection_string(
+        credential=DefaultAzureCredential(),
+        connection=connection_string,
+    )
+
+    # Or, you can create the Azure AI Client by giving all required parameters directly
+    """
+    ai_client = AzureAIClient(
+        credential=DefaultAzureCredential(),
+        host_name=os.environ["AI_CLIENT_HOST_NAME"],
+        subscription_id=os.environ["AI_CLIENT_SUBSCRIPTION_ID"],
+        resource_group_name=os.environ["AI_CLIENT_RESOURCE_GROUP_NAME"],
+        workspace_name=os.environ["AI_CLIENT_WORKSPACE_NAME"],
+        logging_enable=True, # Optional. Remove this line if you don't want to show how to enable logging
+    )
+    """    
+        
     async with ai_client:         
         agent = await ai_client.agents.create_agent(
             model="gpt-4-1106-preview", name="my-assistant", instructions="You are helpful assistant"
