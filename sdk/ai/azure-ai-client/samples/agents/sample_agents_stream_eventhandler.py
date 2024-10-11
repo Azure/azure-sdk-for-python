bdd7f34d9e5496b7d786732366c64cf33708e00d
howieleung <howieleung@microsoft.com>
2024-10-11 09:55:03 -0700

    fixed samples

--- sdk/ai/azure-ai-client/samples/agents/sample_agents_stream_eventhandler.py
+++ sdk/ai/azure-ai-client/samples/agents/sample_agents_stream_eventhandler.py
@@ -99,7 +99,7 @@ with ai_client:
     message = ai_client.agents.create_message(thread_id=thread.id, role="user", content="Hello, tell me a joke")
     print(f"Created message, message ID {message.id}")
 
-    with ai_client.agents.create_and_process_stream(
+    with ai_client.agents.create_stream(
         thread_id=thread.id, assistant_id=agent.id, event_handler=MyEventHandler()
     ) as stream:
         stream.until_done()
