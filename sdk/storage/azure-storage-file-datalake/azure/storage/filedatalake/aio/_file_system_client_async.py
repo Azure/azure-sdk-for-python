05008f5997d97bc5edd07b37a4cf79374cc35038
Peter Wu <weirongwu@microsoft.com>
2024-10-10 20:49:29 -0400

    fixed next-pylint errors

--- sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/aio/_file_system_client_async.py
+++ sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/aio/_file_system_client_async.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-# pylint: disable=invalid-overridden-method, too-many-lines, docstring-keyword-should-match-keyword-only
+# pylint: disable=too-many-lines, docstring-keyword-should-match-keyword-only
 
 import functools
 from typing import (
