b20b448c2c3022cda1d109a5d8f4ff3071e8de2a
Libba Lawrence <llawrence@microsoft.com>
2024-10-16 10:13:30 -0700

    eh fix to remove cbs auth error when there is a port attached

--- sdk/eventhub/azure-eventhub/azure/eventhub/_producer_client.py
+++ sdk/eventhub/azure-eventhub/azure/eventhub/_producer_client.py
@@ -201,7 +201,9 @@ class EventHubProducerClient(
             network_tracing=kwargs.get("logging_enable"),
             **kwargs
         )
-        self._auth_uri = f"sb://{self._address.hostname}{self._address.path}"
+        # remove port from auth_uri (if present)
+        auth_uri_hostname = self._address.hostname.split(":")[0]
+        self._auth_uri = f"sb://{auth_uri_hostname}{self._address.path}"
         self._keep_alive = kwargs.get("keep_alive", None)
 
         self._producers: Dict[str, Optional[EventHubProducer]] = {
