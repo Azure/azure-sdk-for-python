01f8ca7abf99fa62cc5025652b636ad37b269335
Catalina Peralta <caperal@microsoft.com>
2024-10-15 19:18:33 -0700

    shared method

--- scripts/breaking_changes_checker/checkers/added_method_overloads_checker.py
+++ scripts/breaking_changes_checker/checkers/added_method_overloads_checker.py
@@ -10,6 +10,12 @@ sys.path.append(os.path.abspath("../../scripts/breaking_changes_checker"))
 from _models import CheckerType
 import jsondiff
 
+def parse_overload_signature(method_name, overload) -> str:
+    parsed_overload_signature = f"def {method_name}(" + ", ".join([f"{name}: {data['type']}" for name,  data in overload["parameters"].items()]) + ")"
+    if overload["return_type"] is not None:
+        parsed_overload_signature += f" -> {overload['return_type']}"
+    return parsed_overload_signature
+    
 class AddedMethodOverloadChecker:
     node_type = CheckerType.FUNCTION_OR_METHOD
     name = "AddedMethodOverload"
@@ -30,9 +36,7 @@ class AddedMethodOverloadChecker:
                 if isinstance(overload, jsondiff.Symbol):
                     if overload.label == "insert":
                         for _, added_overload in method_components["overloads"][overload]:
-                            parsed_overload_signature = f"def {method_name}(" + ", ".join([f"{name}: {data['type']}" for name,  data in added_overload["parameters"].items()]) + ")"
-                            if added_overload["return_type"] is not None:
-                                parsed_overload_signature += f" -> {added_overload['return_type']}"
+                            parsed_overload_signature = parse_overload_signature(method_name, added_overload)
                             changes_list.append((self.message["default"], self.name, module_name, class_name, method_name, parsed_overload_signature))
                 elif isinstance(overload, int):
                     current_node_overload = current_nodes[module_name]["class_nodes"][class_name]["methods"][method_name]["overloads"][overload]
@@ -42,8 +46,6 @@ class AddedMethodOverloadChecker:
                     changes_list.append((self.message["default"], self.name, module_name, class_name, method_name, parsed_overload_signature))
                 else:
                     # this case is for when the overload is not a symbol and simply shows as a new overload in the diff
-                    parsed_overload_signature = f"def {method_name}(" + ", ".join([f"{name}: {data['type']}" for name,  data in overload["parameters"].items()]) + ")"
-                    if overload["return_type"] is not None:
-                        parsed_overload_signature += f" -> {overload['return_type']}"
+                    parsed_overload_signature = parse_overload_signature(method_name, overload)
                     changes_list.append((self.message["default"], self.name, module_name, class_name, method_name, parsed_overload_signature))
         return changes_list
