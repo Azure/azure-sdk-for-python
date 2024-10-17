a12065b6458d70c63332653b06963e6f7f539552
Alex Sanchez <alexsanchez@microsoft.com>
2024-10-16 19:15:53 -0700

    Remove synthetic data generation constant

--- sdk/ml/azure-ai-ml/azure/ai/ml/constants/_job/synthetic_data_generation.py
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-
-class SyntheticDataGenerationConstants:
-    DataGenerationType = "data_generation_type"
