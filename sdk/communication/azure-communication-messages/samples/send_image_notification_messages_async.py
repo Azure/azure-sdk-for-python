6a687e33d571bf5576d80cff8ef015b92dcea56a
Shamsha Khan <shamkh@microsoft.com>
2024-10-18 10:43:13 -0700

    Add support for more media message types: Video, Document and Audio (#37690)
    * updating w.r.t. new api chnages
    
    * updating change log
    
    * fixing Whats_App kind issue
    
    * test fixes
    
    * adding new sample
    
    * update asset tag
    
    * updated w.r.t. to latest API spec
    
    * remove MediaContent
    
    * updating tests
    
    * fixing build
    
    * release date update
    
    * release date update
    
    * release date update
    
    * release date update
    
    * updated samples
    
    * update version
    
    * updating latest rest commit

--- sdk/communication/azure-communication-messages/samples/send_image_notification_messages_async.py
+++ sdk/communication/azure-communication-messages/samples/send_image_notification_messages_async.py
@@ -28,12 +28,13 @@ import sys
 
 sys.path.append("..")
 
+
 class SendWhatsAppMessageSampleAsync(object):
 
     connection_string = os.getenv("COMMUNICATION_SAMPLES_CONNECTION_STRING")
     phone_number = os.getenv("RECIPIENT_PHONE_NUMBER")
     channel_id = os.getenv("WHATSAPP_CHANNEL_ID")
-    
+
     async def send_image_send_message_async(self):
         from azure.communication.messages.aio import NotificationMessagesClient
         from azure.communication.messages.models import ImageNotificationContent
@@ -43,22 +44,22 @@ class SendWhatsAppMessageSampleAsync(object):
 
         image_options = ImageNotificationContent(
             channel_registration_id=self.channel_id,
-            to= [self.phone_number],
+            to=[self.phone_number],
             content="Hello World via Notification Messaging SDK.",
-            media_uri="https://aka.ms/acsicon1"
+            media_uri="https://aka.ms/acsicon1",
         )
-        
+
         # calling send() with whatsapp message details
         async with messaging_client:
             message_responses = await messaging_client.send(image_options)
             response = message_responses.receipts[0]
-            print("Message with message id {} was successful sent to {}"
-                 .format(response.message_id, response.to))
+            print("Message with message id {} was successful sent to {}".format(response.message_id, response.to))
+
 
 async def main():
     sample = SendWhatsAppMessageSampleAsync()
     await sample.send_image_send_message_async()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
