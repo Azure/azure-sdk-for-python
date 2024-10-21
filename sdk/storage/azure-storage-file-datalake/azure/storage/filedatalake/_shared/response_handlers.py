51fc2c88fc9022160f808a1912ae5bb78f7443e6
Peter Wu <weirongwu@microsoft.com>
2024-10-21 11:50:28 -0700

    simpler solution

--- sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_shared/response_handlers.py
+++ sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_shared/response_handlers.py
@@ -4,7 +4,6 @@
 # license information.
 # --------------------------------------------------------------------------
 import logging
-import re
 from typing import NoReturn
 from xml.etree.ElementTree import Element
 
@@ -60,11 +59,10 @@ def normalize_headers(headers):
 
 
 def deserialize_metadata(response, obj, headers):  # pylint: disable=unused-argument
-    pattern = re.compile(r'x-ms-meta-', re.IGNORECASE)
     try:
-        raw_metadata = {k: v for k, v in response.http_response.headers.items() if pattern.match(k)}
+        raw_metadata = {k: v for k, v in response.http_response.headers.items() if k.lower().startswith('x-ms-meta-')}
     except AttributeError:
-        raw_metadata = {k: v for k, v in response.headers.items() if pattern.match(k)}
+        raw_metadata = {k: v for k, v in response.headers.items() if k.lower().startswith('x-ms-meta-')}
     return {k[10:]: v for k, v in raw_metadata.items()}
 
 
