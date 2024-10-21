96ab929f15880a02432a4898ab980320726e698c
Ankit Singhal <anksing@microsoft.com>
2024-10-21 12:04:58 -0700

    Sample upload file method on AI Client

--- sdk/ai/azure-ai-client/azure/ai/client/_patch.py
+++ sdk/ai/azure-ai-client/azure/ai/client/_patch.py
@@ -6,7 +6,10 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
-from typing import List, Any
+import uuid
+from os import PathLike
+from pathlib import Path
+from typing import List, Any, Union
 from typing_extensions import Self
 from azure.core.credentials import TokenCredential
 from azure.core import PipelineClient
@@ -184,6 +187,41 @@ class AzureAIClient(ClientGenerated):
         resource_group_name = parts[2]
         project_name = parts[3]
         return cls(endpoint, subscription_id, resource_group_name, project_name, credential, **kwargs)
+
+    def upload_file(self, file_path: Union[Path, str, PathLike]) -> str:
+        """Upload a file to the Azure AI Studio project.
+           This method required *azure-ai-ml* to be installed.
+
+        :param file_path: The path to the file to upload.
+        :type file_path: Union[str, Path, PathLike]
+        :return: The asset id of uploaded file.
+        :rtype: str
+        """
+        try:
+            from azure.ai.ml import MLClient
+            from azure.ai.ml.entities import Data
+            from azure.ai.ml.constants import AssetTypes
+        except ImportError:
+            raise ImportError(
+                "azure-ai-ml must be installed to use this function. Please install it using `pip install azure-ai-ml`")
+
+        data = Data(
+            path=file_path,
+            type=AssetTypes.URI_FILE,
+            name=str(uuid.uuid4()),  # generating random name
+            is_anonymous=True,
+        )
+
+        ml_client = MLClient(
+            self._config.credential,
+            self._config.subscription_id,
+            self._config.resource_group_name,
+            self._config.project_name,
+        )
+
+        data_asset = ml_client.data.create_or_update(data)
+
+        return data_asset.id
 
 
 __all__: List[str] = [
