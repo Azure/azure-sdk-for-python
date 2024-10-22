b557ef844d3800cf81cee434d765897f3e49bdc0
Alex Sanchez <alexsanchez@microsoft.com>
2024-10-22 13:29:49 -0700

    Add __path__

--- sdk/ml/azure-ai-ml/azure/ai/ml/distillation/__init__.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/distillation/__init__.py
@@ -4,6 +4,8 @@
 
 """Contains distillation classes for Azure Machine Learning SDKv2."""
 
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
 from azure.ai.ml.entities._job.distillation import EndpointRequestSettings, PromptSettings, TeacherModelSettings
 
 __all__ = ["PromptSettings", "EndpointRequestSettings", "TeacherModelSettings"]
