78e68947587af10f338d04467db528e32bdeddee
M-Hietala <78813398+M-Hietala@users.noreply.github.com>
2024-10-22 16:02:22 -0500

    M hietala/agent tools updates (#38031)
    * no need for function name separately in function definitions
    
    * more changes related to not needing to specify function names separately
    
    * adding capability to remove vector stores from file search tool
    
    * adding remove file to code interpreter
    
    * review comment related change
    
    ---------
    
    Co-authored-by: Marko Hietala <markhiet@microsoft.com>

--- sdk/ai/azure-ai-client/samples/agents/async_samples/sample_agents_vector_store_batch_file_search_async.py
+++ sdk/ai/azure-ai-client/samples/agents/async_samples/sample_agents_vector_store_batch_file_search_async.py
@@ -77,6 +77,21 @@ async def main():
         run = await ai_client.agents.create_and_process_run(thread_id=thread.id, assistant_id=agent.id)
         print(f"Created run, run ID: {run.id}")
 
+        file_search_tool.remove_vector_store(vector_store.id)
+        print(f"Removed vector store from file search, vector store ID: {vector_store.id}")
+
+        await ai_client.agents.update_agent(assistant_id=agent.id, tools=file_search_tool.definitions, tool_resources=file_search_tool.resources)
+        print(f"Updated agent, agent ID: {agent.id}")
+
+        thread = await ai_client.agents.create_thread()
+        print(f"Created thread, thread ID: {thread.id}")
+
+        message = await ai_client.agents.create_message(thread_id=thread.id, role="user", content="What feature does Smart Eyewear offer?")
+        print(f"Created message, message ID: {message.id}")
+
+        run = await ai_client.agents.create_and_process_run(thread_id=thread.id, assistant_id=agent.id)
+        print(f"Created run, run ID: {run.id}")
+
         await ai_client.agents.delete_file(file.id)
         print("Deleted file")
 
@@ -87,7 +102,7 @@ async def main():
         print("Deleted agent")
 
         messages = await ai_client.agents.list_messages(thread_id=thread.id)
-        print(f"Messages: {messages}")
+        print(f"Messages: {messages}")        
 
 
 if __name__ == "__main__":
