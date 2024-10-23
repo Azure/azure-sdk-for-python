dfbaaf5f2a25b9e63c559475ff4093473f05a1b0
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-10-23 14:25:10 -0700

    Create azure-ai-projects folder

---
+++ sdk/ai/azure-ai-projects/tests/connections/connection_test_base.py
@@ -0,0 +1,40 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import sys
+import logging
+import functools
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from devtools_testutils import AzureRecordedTestCase, EnvironmentVariableLoader
+
+ServicePreparerChatCompletions = functools.partial(
+    EnvironmentVariableLoader,
+    "project_connection_string",
+    project_connection_string_connections_tests="endpoint;azure-subscription-id;azure-rg-name;ai-studio-hub-name",
+)
+
+# Set to True to enable SDK logging
+LOGGING_ENABLED = True
+
+if LOGGING_ENABLED:
+    # Create a logger for the 'azure' SDK
+    # See https://docs.python.org/3/library/logging.html
+    logger = logging.getLogger("azure")
+    logger.setLevel(logging.DEBUG)  # INFO or DEBUG
+
+    # Configure a console output
+    handler = logging.StreamHandler(stream=sys.stdout)
+    logger.addHandler(handler)
+
+class ConnectionsTestBase:
+
+    def get_sync_client(self, **kwargs) -> AIProjectClient:
+        conn_str = kwargs.pop("project_connection_string_connections_tests")
+        project_client = AIProjectClient.from_connection_string(
+            credential=DefaultAzureCredential(),
+            conn_str=conn_str,
+        )
+        return project_client
+
