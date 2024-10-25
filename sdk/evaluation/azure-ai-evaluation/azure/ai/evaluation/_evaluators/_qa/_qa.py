c2d85adce758679f63bcb53661d2a372c2ee2f2c
Miles Holland <milesholland@microsoft.com>
2024-10-25 16:57:36 -0400

    remove unneeded todo

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_qa/_qa.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_qa/_qa.py
@@ -55,7 +55,6 @@ class QAEvaluator:
         }
     """
 
-    # TODO re-enable parallelism once a fix is in place
     def __init__(self, model_config, parallel: bool = True):
         self._parallel = parallel
 
