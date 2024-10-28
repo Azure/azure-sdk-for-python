cdc3ffb5e2a5e6043905207cae617f5c053607ee
nick863 <30440255+nick863@users.noreply.github.com>
2024-10-28 12:49:28 -0700

    Fix sphinx docstring and cSpell (#38125)
    * Fix sphinx
    
    * Fix spelling
    
    * Fix
    
    * Fix II
    
    * Generate new code
    
    * Fix test
    
    * Fix

--- sdk/ai/azure-ai-projects/tests/conftest.py
+++ sdk/ai/azure-ai-projects/tests/conftest.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv, find_dotenv
 if not load_dotenv(find_dotenv(filename="azure_ai_projects_tests.env"), override=True):
     print("Failed to apply environment variables for azure-ai-projects tests.")
 
+
 # autouse=True will trigger this fixture on each pytest run, even if it's not explicitly used by a test method
 @pytest.fixture(scope="session", autouse=True)
 def start_proxy(test_proxy):
