147e7fa2ebefb0055392d7b7e34b73b26615fb97
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-10-30 10:11:27 -0700

    Enable tracing (#38185)

---
+++ sdk/ai/azure-ai-projects/samples/inference/sample_chat_completions_with_azure_ai_inference_client_and_azure_monitor_tracing.py
@@ -0,0 +1,49 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+"""
+DESCRIPTION:
+    Given an AIProjectClient, this sample demonstrates how to get an authenticated 
+    ChatCompletionsClient from the azure.ai.inference package. The client
+    is already instrumented to upload traces to Azure Monitor. View the results
+    in the "Tracing" tab in your Azure AI Studio project page.
+
+USAGE:
+    python sample_chat_completions_with_azure_ai_inference_client_and_azure_monitor_tracing.py
+
+    Before running the sample:
+
+    pip install azure-ai-projects azure-ai-inference azure-identity azure.monitor.opentelemetry
+
+    Set these environment variables with your own values:
+    * PROJECT_CONNECTION_STRING - the Azure AI Project connection string, as found in your AI Studio Project.
+    * AZURE_TRACING_GEN_AI_CONTENT_RECORDING_ENABLED=true - Optional. For detailed traces, including chat request and response messages.
+"""
+import os
+from azure.ai.projects import AIProjectClient
+from azure.ai.inference.models import UserMessage
+from azure.identity import DefaultAzureCredential
+from azure.monitor.opentelemetry import configure_azure_monitor
+
+with AIProjectClient.from_connection_string(
+    credential=DefaultAzureCredential(),
+    conn_str=os.environ["PROJECT_CONNECTION_STRING"],
+) as project_client:
+
+    # Enable Azure Monitor tracing. Set environment variable `AZURE_TRACING_GEN_AI_CONTENT_RECORDING_ENABLED=true`
+    # for detailed logs, including chat request and response messages.
+    application_insights_connection_string = project_client.diagnostics.get_connection_string()
+    if not application_insights_connection_string:
+        print("Application Insights was not enabled for this project.")
+        print("Enable it via the 'Tracing' tab in your AI Studio project page.")
+        exit()
+    configure_azure_monitor(connection_string=application_insights_connection_string)
+
+    # Get an authenticated azure.ai.inference ChatCompletionsClient for your default Serverless connection:
+    with project_client.inference.get_chat_completions_client() as client:
+
+        response = client.complete(messages=[UserMessage(content="How many feet are in a mile?")])
+
+        print(response.choices[0].message.content)
--- sdk/ai/azure-ai-projects/tests/diagnostics/test_diagnostics.py
+++ sdk/ai/azure-ai-projects/tests/diagnostics/test_diagnostics.py
@@ -2,8 +2,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-
+import sys
 from devtools_testutils import recorded_by_proxy
+from azure.ai.projects import AIProjectClient
 from diagnostics_test_base import DiagnosticsTestBase, servicePreparerDiagnosticsTests
 
 
@@ -12,14 +13,22 @@ class TestDiagnostics(DiagnosticsTestBase):
 
     @servicePreparerDiagnosticsTests()
     @recorded_by_proxy
-    def test_diagnostics(self, **kwargs):
+    def test_diagnostics_get_connection_string(self, **kwargs):
         with self.get_sync_client(**kwargs) as project_client:
-            assert project_client.diagnostics.connection_string == None
-            assert project_client.diagnostics.enable() == True
-            assert project_client.diagnostics.connection_string is not None
-            assert bool(
-                DiagnosticsTestBase.REGEX_APPINSIGHTS_CONNECTION_STRING.match(
-                    project_client.diagnostics.connection_string
-                )
-            )
-            assert project_client.diagnostics.enable() == True
+            connection_string = project_client.diagnostics.get_connection_string()
+            print(connection_string)
+            assert connection_string
+            assert bool(DiagnosticsTestBase.REGEX_APPINSIGHTS_CONNECTION_STRING.match(connection_string))
+            assert connection_string == project_client.diagnostics.get_connection_string()
+
+    @servicePreparerDiagnosticsTests()
+    def test_diagnostics_enable_console_tracing(self, **kwargs):
+        with self.get_sync_client(**kwargs) as project_client:
+            project_client.diagnostics.enable(destination=sys.stdout)
+            #TODO: Create inference client and do chat completions. How do I know if traces were emitted?
+
+    @servicePreparerDiagnosticsTests()
+    def test_diagnostics_enable_otlp_tracing(self, **kwargs):
+        with self.get_sync_client(**kwargs) as project_client:
+            project_client.diagnostics.enable(destination="https://some.otlp.collector.endpoint")
+            #TODO: Create inference client and do chat completions. Test proxy will log attempt at telemetry call.
