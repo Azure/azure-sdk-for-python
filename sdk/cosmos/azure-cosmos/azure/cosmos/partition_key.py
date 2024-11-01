3f4a73e621db6446c50fe6725f73efdeb75698e9
bambriz <beambriz2017@gmail.com>
2024-11-01 09:39:15 -0700

    Update partition_key.py

--- sdk/cosmos/azure-cosmos/azure/cosmos/partition_key.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/partition_key.py
@@ -284,7 +284,8 @@ class PartitionKey(dict):
             partition_key: Union[str, int, float, bool, Sequence[Union[str, int, float, bool, None]], Type[NonePartitionKeyValue]]) -> bool: # pylint: disable=line-too-long
         if self.kind != "MultiHash":
             return False
-        return isinstance(partition_key, (list, tuple)) and len(self['paths']) != len(partition_key)
+        ret = isinstance(partition_key, (list, tuple)) and len(self['paths']) != len(partition_key)  # pylint: disable=C0303, line-too-long
+        return ret
 
 
 def _return_undefined_or_empty_partition_key(is_system_key: bool) -> Union[_Empty, _Undefined]:
