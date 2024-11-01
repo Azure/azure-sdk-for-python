c130cf4ac23599402943eefd312b92c8ae6f6fe8
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2024-11-01 16:15:07 -0400

    Fix __call__ Overload Types (#38238)
    * Fix __call__ overload issues
    
    * fix typing issue
    
    * make query required for groundednesspro
    
    * fix a malformatted docstring
    
    * fix some type hints and remove eval_last_turn from evaluators
    
    * fix optional import
    
    * comment out eval last turn section

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/simulator/_adversarial_simulator.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/simulator/_adversarial_simulator.py
@@ -15,6 +15,7 @@ from azure.ai.evaluation._common._experimental import experimental
 from azure.ai.evaluation._common.utils import validate_azure_ai_project
 from azure.ai.evaluation._exceptions import ErrorBlame, ErrorCategory, ErrorTarget, EvaluationException
 from azure.ai.evaluation._http_utils import get_async_http_client
+from azure.ai.evaluation._model_configurations import AzureAIProject
 from azure.ai.evaluation.simulator import AdversarialScenario
 from azure.ai.evaluation.simulator._adversarial_scenario import _UnstableAdversarialScenario
 from azure.core.credentials import TokenCredential
@@ -48,7 +49,7 @@ class AdversarialSimulator:
     :type credential: ~azure.core.credentials.TokenCredential
     """
 
-    def __init__(self, *, azure_ai_project: dict, credential):
+    def __init__(self, *, azure_ai_project: AzureAIProject, credential: TokenCredential):
         """Constructor."""
 
         try:
