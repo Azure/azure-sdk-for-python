d9ea7a8ca0d65b761de57759fe1fb49d5dbf9ef8
Billy Hu <ninhu@microsoft.com>
2024-11-01 09:20:57 -0700

    Evaluation: Fix the `output_path` parameter of `evaluate` API doesn't support relative path (#38241)
    * Fix output_path parameter doesn't support relative path
    
    * add comments
    
    * fix the test
    
    * update
    
    * minor update
    
    * update

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluate/_batch_run/target_run_context.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluate/_batch_run/target_run_context.py
@@ -17,8 +17,12 @@ class TargetRunContext:
 
     def __init__(self, upload_snapshot: bool) -> None:
         self._upload_snapshot = upload_snapshot
+        self._original_cwd = os.getcwd()
 
     def __enter__(self) -> None:
+        # Preserve current working directory, as PF may change it without restoring it afterward
+        self._original_cwd = os.getcwd()
+
         # Address "[WinError 32] The process cannot access the file" error,
         # caused by conflicts when the venv and target function are in the same directory.
         # Setting PF_FLOW_ENTRY_IN_TMP to true uploads only the flex entry file (flow.flex.yaml).
@@ -31,5 +35,7 @@ class TargetRunContext:
         exc_value: Optional[BaseException],
         exc_tb: Optional[types.TracebackType],
     ) -> None:
+        os.chdir(self._original_cwd)
+
         if not self._upload_snapshot:
             os.environ.pop(PF_FLOW_ENTRY_IN_TMP, None)
