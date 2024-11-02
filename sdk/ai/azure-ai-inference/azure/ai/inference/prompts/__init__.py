056f371c2e706c644ac3ec2b86fba62332fb9485
David Wu <KyosukeNo1@gmail.com>
2024-11-02 14:13:21 -0700

    Fix unused import linter from __init__.py

--- sdk/ai/azure-ai-inference/azure/ai/inference/prompts/__init__.py
+++ sdk/ai/azure-ai-inference/azure/ai/inference/prompts/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-
-from ._patch import patch_sdk as _patch_sdk
+# pylint: disable=unused-import
+from ._patch import patch_sdk as _patch_sdk, PromptTemplate
 _patch_sdk()
