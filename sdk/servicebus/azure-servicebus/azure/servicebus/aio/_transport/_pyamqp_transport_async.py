114a7ef3612aefd4fe886d2054a27784e376b24c
swathipil <76007337+swathipil@users.noreply.github.com>
2024-11-04 11:19:59 -0600

    [EventHub] add ssl_context kwarg to clients (#37702)
    * remove verify from pyamqp JWTToken
    
    * add ssl_context kwarg
    
    * add tests
    
    * fix merge
    
    * fix failing/lint/mypy
    
    * lint
    
    * separate ssl_context property from conn verify
    
    * make sb changes
    
    * add tests
    
    * changelog
    
    * black

--- sdk/servicebus/azure-servicebus/azure/servicebus/aio/_transport/_pyamqp_transport_async.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/aio/_transport/_pyamqp_transport_async.py
@@ -112,6 +112,7 @@ class PyamqpTransportAsync(PyamqpTransport, AmqpTransportAsync):
             keep_alive_interval=config.keep_alive,
             custom_endpoint_address=config.custom_endpoint_address,
             connection_verify=config.connection_verify,
+            ssl_context=config.ssl_context,
             transport_type=config.transport_type,
             http_proxy=config.http_proxy,
             socket_timeout=config.socket_timeout,
@@ -209,6 +210,7 @@ class PyamqpTransportAsync(PyamqpTransport, AmqpTransportAsync):
             transport_type=config.transport_type,
             custom_endpoint_address=config.custom_endpoint_address,
             connection_verify=config.connection_verify,
+            ssl_context=config.ssl_context,
             receive_settle_mode=PyamqpTransportAsync.ServiceBusToAMQPReceiveModeMap[receive_mode],
             send_settle_mode=(
                 constants.SenderSettleMode.Settled
@@ -398,7 +400,6 @@ class PyamqpTransportAsync(PyamqpTransport, AmqpTransportAsync):
             timeout=config.auth_timeout,
             custom_endpoint_hostname=config.custom_endpoint_hostname,
             port=config.connection_port,
-            verify=config.connection_verify,
         )
 
     @staticmethod
