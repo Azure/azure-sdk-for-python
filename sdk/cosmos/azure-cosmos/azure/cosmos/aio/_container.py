607752668159c1d9341977b9e6a0b4fbc9b53856
James Suplizio <jasupliz@microsoft.com>
2024-11-05 12:12:10 -0800

    Remove a defunct variable from docindex.yml (#38342)

--- sdk/cosmos/azure-cosmos/azure/cosmos/aio/_container.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/aio/_container.py
@@ -22,19 +22,17 @@
 """Create, read, update and delete items in the Azure Cosmos DB SQL API service.
 """
 from datetime import datetime
-from typing import Any, Dict, Mapping, Optional, Sequence, Type, Union, List, Tuple, cast, overload, Iterable
+from typing import Any, Dict, Mapping, Optional, Sequence, Type, Union, List, Tuple, cast, overload, AsyncIterable
 from typing_extensions import Literal
 
 from azure.core import MatchConditions
-from azure.core.async_paging import AsyncItemPaged
+from azure.core.async_paging import AsyncItemPaged, AsyncList
 from azure.core.tracing.decorator import distributed_trace
 from azure.core.tracing.decorator_async import distributed_trace_async  # type: ignore
 from azure.cosmos._change_feed.change_feed_state import ChangeFeedMode
 from azure.cosmos._change_feed.change_feed_utils import validate_kwargs
 
 from ._cosmos_client_connection_async import CosmosClientConnection
-from .._change_feed.feed_range_internal import FeedRangeInternalEpk
-from .._cosmos_responses import CosmosDict, CosmosList
 from ._scripts import ScriptsProxy
 from .._base import (
     build_options as _build_options,
@@ -44,6 +42,8 @@ from .._base import (
     GenerateGuidId,
     _set_properties_cache
 )
+from .._change_feed.feed_range_internal import FeedRangeInternalEpk
+from .._cosmos_responses import CosmosDict, CosmosList
 from .._routing.routing_range import Range
 from .._session_token_helpers import get_latest_session_token
 from ..offer import ThroughputProperties
@@ -681,8 +681,8 @@ class ContainerProxy:
         :keyword response_hook: A callable invoked with the response metadata.
         :paramtype response_hook: Optional[Callable[[Mapping[str, Any], Mapping[str, Any]], None]]
         :param Any args: args
-        :returns: An Iterable of items (dicts).
-        :rtype: Iterable[Dict[str, Any]]
+        :returns: An AsyncItemPaged of items (dicts).
+        :rtype: AsyncItemPaged[Dict[str, Any]]
         """
         # pylint: disable=too-many-statements
         validate_kwargs(kwargs)
@@ -1310,13 +1310,13 @@ class ContainerProxy:
             *,
             force_refresh: Optional[bool] = False,
             **kwargs: Any
-    ) -> Iterable[Dict[str, Any]]:
+    ) -> AsyncIterable[Dict[str, Any]]:
         """ Obtains a list of feed ranges that can be used to parallelize feed operations.
 
         :keyword bool force_refresh:
             Flag to indicate whether obtain the list of feed ranges directly from cache or refresh the cache.
-        :returns: A list representing the feed ranges in base64 encoded string
-        :rtype: Iterable[Dict[str, Any]]
+        :returns: AsyncIterable representing the feed ranges in base64 encoded string
+        :rtype: AsyncIterable[Dict[str, Any]]
 
         .. warning::
           The structure of the dict representation of a feed range may vary, including which keys
@@ -1335,7 +1335,8 @@ class ContainerProxy:
 
         feed_ranges = [FeedRangeInternalEpk(Range.PartitionKeyRangeToRange(partitionKeyRange)).to_dict()
                 for partitionKeyRange in partition_key_ranges]
-        return (feed_range for feed_range in feed_ranges)
+
+        return AsyncList(feed_ranges)
 
     async def get_latest_session_token(
             self,
