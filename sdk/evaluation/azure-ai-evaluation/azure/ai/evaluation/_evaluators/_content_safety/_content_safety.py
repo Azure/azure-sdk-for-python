3f57b92a3fe145f0f8f9e7f26edd78e4dffb022e
Ralph El Hage <ralphe@microsoft.com>
2024-11-04 20:02:29 -0800

    Remove promptflow.tracing dependencies from code

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_content_safety/_content_safety.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_content_safety/_content_safety.py
@@ -4,7 +4,7 @@
 from concurrent.futures import as_completed
 from typing import Callable, Dict, List, Union
 
-from promptflow.tracing import ThreadPoolExecutorWithContext as ThreadPoolExecutor
+from ..._external._context_utils import ThreadPoolExecutorWithContext as ThreadPoolExecutor
 from typing_extensions import overload, override
 
 from azure.ai.evaluation._evaluators._common import EvaluatorBase
