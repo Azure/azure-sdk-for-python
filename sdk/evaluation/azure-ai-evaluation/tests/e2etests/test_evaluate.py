cfc9b853fa68074234a7b301480dc6e82fabd66b
Waqas Javed <7674577+w-javed@users.noreply.github.com>
2024-11-08 13:41:34 -0800

    Protected material results fix (#38414)
    * docstring-update
    
    * doc string updates
    
    * doc string updates
    
    * Add-assistant-validation
    
    * Fix for exp declaration - multi-turn - docstring-content-with-text
    
    * registering image artifact, multi-turn test added, text in content allowed
    
    * lint fixes
    
    * lint fixes, inference test fix
    
    * lint fixes
    
    * Addressing afer review comments
    
    * lint-fix
    
    * protected_material
    
    * lint fix
    
    * adding e2e test pro-material
    
    * asset for new test
    
    * lint issue

--- sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_evaluate.py
+++ sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_evaluate.py
@@ -13,6 +13,7 @@ from azure.ai.evaluation import (
     ContentSafetyEvaluator,
     ContentSafetyMultimodalEvaluator,
     SexualMultimodalEvaluator,
+    ProtectedMaterialMultimodalEvaluator,
     F1ScoreEvaluator,
     FluencyEvaluator,
     GroundednessEvaluator,
@@ -413,6 +414,44 @@ class TestEvaluate:
         # Check that label is renamed to passing rate in metrics
         assert "groundedness_pro.groundedness_pro_passing_rate" in convo_metrics.keys()
         assert 0 <= convo_metrics.get("groundedness_pro.groundedness_pro_passing_rate") <= 1
+
+    def test_evaluate_with_protected_material_multimodal_evaluator(
+        self, project_scope, azure_cred, multimodal_file_with_imageurls
+    ):
+        os.environ["PF_EVALS_BATCH_USE_ASYNC"] = "false"
+        input_data = pd.read_json(multimodal_file_with_imageurls, lines=True)
+        eval = ProtectedMaterialMultimodalEvaluator(azure_ai_project=project_scope, credential=azure_cred)
+        result = evaluate(
+            evaluation_name=f"test-multimodal-protected-material-eval-dataset-{str(uuid.uuid4())}",
+            azure_ai_project=project_scope,
+            data=multimodal_file_with_imageurls,
+            evaluators={"protected_material": eval},
+            evaluator_config={
+                "protected_material": {"conversation": "${data.conversation}"},
+            },
+        )
+
+        row_result_df = pd.DataFrame(result["rows"])
+        metrics = result["metrics"]
+        # validate the results
+        assert result is not None
+        assert result["rows"] is not None
+        assert row_result_df.shape[0] == len(input_data)
+
+        assert "outputs.protected_material.artwork_label" in row_result_df.columns.to_list()
+        assert "outputs.protected_material.artwork_reason" in row_result_df.columns.to_list()
+        assert "outputs.protected_material.fictional_characters_label" in row_result_df.columns.to_list()
+        assert "outputs.protected_material.fictional_characters_reason" in row_result_df.columns.to_list()
+        assert "outputs.protected_material.logos_and_brands_label" in row_result_df.columns.to_list()
+        assert "outputs.protected_material.logos_and_brands_reason" in row_result_df.columns.to_list()
+
+        assert "protected_material.fictional_characters_label" in metrics.keys()
+        assert "protected_material.logos_and_brands_label" in metrics.keys()
+        assert "protected_material.artwork_label" in metrics.keys()
+
+        assert 0 <= metrics.get("protected_material.fictional_characters_label") <= 1
+        assert 0 <= metrics.get("protected_material.logos_and_brands_label") <= 1
+        assert 0 <= metrics.get("protected_material.artwork_label") <= 1
 
     # @pytest.mark.performance_test
     @pytest.mark.skip(reason="Temporary skip to merge 37201, will re-enable in subsequent pr")
