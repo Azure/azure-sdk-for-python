4da7e28581055c1013a5ba47e594e52dae80f88c
kdestin <101366538+kdestin@users.noreply.github.com>
2024-11-11 14:57:52 -0500

    docs(sample): Remove reference to non-existent attribute (provisioning_status)

--- sdk/ai/azure-ai-projects/samples/evaluations/sample_evaluations_schedules.py
+++ sdk/ai/azure-ai-projects/samples/evaluations/sample_evaluations_schedules.py
@@ -61,10 +61,7 @@ def main():
         tags=tags,
     )
 
-    evaluation_schedule = ai_client.evaluations.create_or_replace_schedule(
-        name, evaluation_schedule
-    )
-    print(evaluation_schedule.provisioning_state)
+    evaluation_schedule = ai_client.evaluations.create_or_replace_schedule(name, evaluation_schedule)
     print(evaluation_schedule)
 
     # Sample for get an evaluation schedule with name
