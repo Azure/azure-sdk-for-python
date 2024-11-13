64fadfe06dfae4ed60882c5759b045af6238acf1
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-11-12 17:27:56 -0800

    Disable all agents tests. Push recordings for all other tests to the assents folder (#38481)

--- sdk/ai/azure-ai-projects/tests/conftest.py
+++ sdk/ai/azure-ai-projects/tests/conftest.py
@@ -18,6 +18,10 @@ from azure.ai.projects import AIProjectClient
 if not load_dotenv(find_dotenv(filename="azure_ai_projects_tests.env"), override=True):
     print("Failed to apply environment variables for azure-ai-projects tests.")
 
+def pytest_collection_modifyitems(items):
+    for item in items: 
+        if 'tests\\agents' in item.fspath.strpath or 'tests/agents' in item.fspath.strpath:
+            item.add_marker(pytest.mark.skip(reason="Skip running Agents tests in PR pipeline until test recordings are available"))
 
 class SanitizedValues:
     SUBSCRIPTION_ID = "00000000-0000-0000-0000-000000000000"
