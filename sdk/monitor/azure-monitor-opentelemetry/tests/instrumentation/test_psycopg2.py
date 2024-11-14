b3e69d727664cced449f6ed7b22da49d3cbe25e2
Leighton Chen <lechen@microsoft.com>
2024-11-14 12:11:16 -0800

    Update test_psycopg2.py

--- sdk/monitor/azure-monitor-opentelemetry/tests/instrumentation/test_psycopg2.py
+++ sdk/monitor/azure-monitor-opentelemetry/tests/instrumentation/test_psycopg2.py
@@ -10,7 +10,7 @@ import unittest
 
 # Skip for Python v3.13 until https://github.com/psycopg/psycopg2/pull/1729 is resolved
 # Skip for Python v3.8 on windows due to https://github.com/psycopg/psycopg/issues/936
-if not (platform.system() == "Windows" and sys.version_info < (3, 9)) and sys.implementation.name != "pypy":
+if (not (platform.system() == "Windows" and sys.version_info < (3, 9))) and sys.implementation.name != "pypy":
     from opentelemetry.instrumentation.psycopg2 import (
         Psycopg2Instrumentor,
     )
