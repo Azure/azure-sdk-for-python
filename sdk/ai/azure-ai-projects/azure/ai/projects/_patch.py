d2ba58ecef222104c2c98ead0ee76df186e8c774
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-11-14 14:49:04 -0800

    Fix scopes to use auth2.0 instea of auth1.0 (#38552)

--- sdk/ai/azure-ai-projects/azure/ai/projects/_patch.py
+++ sdk/ai/azure-ai-projects/azure/ai/projects/_patch.py
@@ -68,7 +68,7 @@ class AIProjectClient(
             project_name=project_name,
             credential=credential,
             api_version="2020-02-02",
-            credential_scopes=["https://management.azure.com"],
+            credential_scopes=["https://management.azure.com/.default"],
             **kwargs0,
         )
 
@@ -106,7 +106,7 @@ class AIProjectClient(
             project_name=project_name,
             credential=credential,
             api_version="2024-07-01-preview",
-            credential_scopes=["https://management.azure.com"],
+            credential_scopes=["https://management.azure.com/.default"],
             **kwargs1,
         )
         _policies1 = kwargs1.pop("policies", None)
@@ -137,7 +137,7 @@ class AIProjectClient(
             project_name=project_name,
             credential=credential,
             api_version="2024-07-01-preview",  # TODO: Update me
-            credential_scopes=["https://ml.azure.com"],
+            credential_scopes=["https://ml.azure.com/.default"],
             **kwargs2,
         )
         _policies2 = kwargs2.pop("policies", None)
@@ -169,7 +169,7 @@ class AIProjectClient(
             project_name=project_name,
             credential=credential,
             api_version="2024-07-01-preview",  # TODO: Update me
-            credential_scopes=["https://ml.azure.com"],  # TODO: Update once service changes are ready
+            credential_scopes=["https://ml.azure.com/.default"],  # TODO: Update once service changes are ready
             **kwargs3,
         )
         _policies3 = kwargs3.pop("policies", None)
