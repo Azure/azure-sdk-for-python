28ab9d7c7fd0575cf1524114c2cf3503ec77cb8b
mohitsinghnegi2 <176272913+mohitsinghnegi2@users.noreply.github.com>
2024-11-18 17:07:01 +0530

    add doc string

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_workspace/network_acls.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_workspace/network_acls.py
@@ -44,6 +44,13 @@ class NetworkAcls(RestTranslatableMixin):
     :type default_action: str
     :param ip_rules: Rules governing the accessibility of a resource from a specific IP address or IP range.
     :type ip_rules: Optional[List[IPRule]]
+
+    .. literalinclude:: ../samples/ml_samples_workspace.py
+            :start-after: [START workspace_network_access_settings]
+            :end-before: [END workspace_network_access_settings]
+            :language: python
+            :dedent: 8
+            :caption: Examples to choose one of three Public network access settings.
     """
 
     def __init__(
