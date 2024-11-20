19d1dc232fbf1d472393a4675452c068aa8ae8bb
Sophia Ramsey <sophiaramsey@microsoft.com>
2024-11-20 15:43:17 -0800

    update b2

--- sdk/ai/azure-ai-projects/tests/conftest.py
+++ sdk/ai/azure-ai-projects/tests/conftest.py
@@ -19,6 +19,14 @@ if not load_dotenv(find_dotenv(filename="azure_ai_projects_tests.env"), override
     print("Failed to apply environment variables for azure-ai-projects tests.")
 
 
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if "tests\\agents" in item.fspath.strpath or "tests/agents" in item.fspath.strpath:
+            item.add_marker(
+                pytest.mark.skip(reason="Skip running Agents tests in PR pipeline until test recordings are available")
+            )
+
+
 class SanitizedValues:
     SUBSCRIPTION_ID = "00000000-0000-0000-0000-000000000000"
     RESOURCE_GROUP_NAME = "00000"
@@ -107,5 +115,5 @@ def add_sanitizers(test_proxy, mock_project_scope, mock_dataset_name):
     # Remove the following sanitizers since certain fields are needed in tests and are non-sensitive:
     #  - AZSDK3493: $..name
     #  - AZSDK3430: $..id
-    remove_batch_sanitizers(["AZSDK3493"])
+    remove_batch_sanitizers(["AZSDK3493"]) 
     remove_batch_sanitizers(["AZSDK3430"])
