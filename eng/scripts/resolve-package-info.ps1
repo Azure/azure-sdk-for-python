a28659eb159cd8e790e70126fa86d53b9b56f40f
Scott Beddall <scbedd@microsoft.com>
2024-11-19 17:28:45 -0800

    attempting to understand why the default isn't working as we expect

--- eng/scripts/resolve-package-info.ps1
+++ eng/scripts/resolve-package-info.ps1
@@ -1,16 +1,13 @@
 param (
     [string] $ParameterTargetingStringSetting,
     [string] $PackagePropertiesFolder,
-    [string] $IncludeIndirect = "true"
+    [bool] $IncludeIndirect = $true
 )
+
+Write-Host "Type of IncludeIndirect: $($IncludeIndirect.GetType().FullName)"
 
 $setting = $ParameterTargetingStringSetting
 $targetingString = $ParameterTargetingStringSetting
-
-Write-Host "I see the following for env:BuildTargetingString"
-Write-Host $env:BuildTargetingString
-Write-Host "I see the following for env:BUILDTARGETINGSTRING"
-Write-Host $env:BUILDTARGETINGSTRING
 
 # users of internal service builds will set variable "BuildTargetingString" at runtime
 # if they want to artificially filter the discovered packages for the build.
@@ -19,13 +16,17 @@ if ($env:BUILDTARGETINGSTRING) {
     $targetingString = $env:BUILDTARGETINGSTRING
 }
 
+Write-Host "The resolved targeting string within the script is `"$targetingString`""
+
 if (Test-Path "$PackagePropertiesFolder") {
-    if ($IncludeIndirect -eq "true") {
+    if ($IncludeIndirect) {
+        Write-Host "Including indirect"
         $packageProperties = Get-ChildItem -Recurse -Force "$PackagePropertiesFolder/*.json" `
             | Where-Object { if ($_.Name.Replace(".json", "") -like "$targetingString") { return $true } else { Remove-Item $_.FullName; return $false; } }
             | ForEach-Object { $_.Name.Replace(".json", "") }
     }
     else {
+        Write-Host "Not including indirect"
         $packageProperties = Get-ChildItem -Recurse -Force "$PackagePropertiesFolder/*.json" `
         | Where-Object { (Get-Content -Raw $_ | ConvertFrom-Json).IncludedForValidation -eq $false } `
         | Where-Object { if ($_.Name.Replace(".json", "") -like "$targetingString") { return $true } else { Remove-Item $_.FullName; return $false; } }
