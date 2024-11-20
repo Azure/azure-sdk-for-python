bc25a8a58982f0350aff04b2a761f4dada8a36f7
Amit Chauhan <70937115+achauhan-scc@users.noreply.github.com>
2024-11-20 11:29:23 +0530

    improvig the sample

--- sdk/ml/azure-ai-ml/samples/ml_sample_compliant_logger.py
+++ sdk/ml/azure-ai-ml/samples/ml_sample_compliant_logger.py
@@ -19,20 +19,12 @@ USAGE:
 
 import logging
 
+# from shrike.compliant_logging import (
+from azure.ai.ml._logging.compliant_logger import DataCategory, enable_compliant_logging
+
 
 class CompliantLoggerSamples(object):
-
-    def get_shrike_logger(self):
-        from shrike.compliant_logging import enable_compliant_logging
-
-        enable_compliant_logging(
-            format="%(prefix)s %(asctime)s %(levelname)s:%(name)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-        )
-        return logging.getLogger(__name__)
-
-    def get_sdkv2_logger(self):
-        from azure.ai.ml._logging.compliant_logger import enable_compliant_logging
-
+    def get_logger(self):
         enable_compliant_logging(
             format="%(prefix)s %(asctime)s %(levelname)s:%(name)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S"
         )
@@ -41,20 +33,8 @@ class CompliantLoggerSamples(object):
 
 if __name__ == "__main__":
     sample = CompliantLoggerSamples()
-    # Shrike logger
-    # OUTPUT WHEN line 48 & 49 are uncomment + 55 & 57 commented
-    # SystemLog: 2024-11-20 10:41:44 INFO:__main__:Testing log message.
-    #  2024-11-20 10:41:44 INFO:__main__:Testing log message.
-    # from shrike.compliant_logging import DataCategory
-    # logger = sample.get_shrike_logger()
 
-    # SDK Logger
-    # OUTPUT WHEN line 48 & 49 are comment + 55 & 57 uncommented
-    # SystemLog: 2024-11-20 10:47:31 INFO:__main__:Testing log message.
-    #  2024-11-20 10:41:44 INFO:__main__:Testing log message.
-    from azure.ai.ml._logging.compliant_logger import DataCategory
-
-    logger = sample.get_sdkv2_logger()
+    logger = sample.get_logger()
     message = "Testing log message."
     logger.info(message, category=DataCategory.PUBLIC)
     logger.info(message, category=DataCategory.PRIVATE)
