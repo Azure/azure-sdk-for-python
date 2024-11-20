0538567ae46c2e2b01fa6f00f177dfff27b77a04
Scott Beddall <scbedd@microsoft.com>
2024-11-19 16:58:27 -0800

    this can be simpler, but I want to just maintain the same logic while investigating this thing

---
+++ eng/scripts/resolve-package-info.ps1
@@ -0,0 +1,39 @@
+param (
+    [string] $ParameterTargetingStringSetting,
+    [string] $PackagePropertiesFolder,
+    [string] $IncludeIndirect = "true"
+)
+
+$setting = $ParameterTargetingStringSetting
+$targetingString = $ParameterTargetingStringSetting
+
+# users of internal service builds will set variable "BuildTargetingString" at runtime
+# if they want to artificially filter the discovered packages for the build.
+# when azure devops sets the variable, it will make it all capital letters, so we should check that
+if ($env:BUILDTARGETINGSTRING) {
+    $targetingString = $env:BUILDTARGETINGSTRING
+}
+
+if (Test-Path "$PackagePropertiesFolder") {
+    if ($IncludeIndirect -eq "true") {
+        $packageProperties = Get-ChildItem -Recurse -Force "$PackagePropertiesFolder/*.json" `
+            | Where-Object { if ($_.Name.Replace(".json", "") -like "$targetingString") { return $true } else { Remove-Item $_.FullName; return $false; } }
+            | ForEach-Object { $_.Name.Replace(".json", "") }
+    }
+    else {
+        $packageProperties = Get-ChildItem -Recurse -Force "$PackagePropertiesFolder/*.json" `
+        | Where-Object { (Get-Content -Raw $_ | ConvertFrom-Json).IncludedForValidation -eq $false } `
+        | Where-Object { if ($_.Name.Replace(".json", "") -like "$targetingString") { return $true } else { Remove-Item $_.FullName; return $false; } }
+        | ForEach-Object { $_.Name.Replace(".json", ""); }
+    }
+
+    $setting = $packageProperties -join ","
+
+    # in case we don't expect any packages, we should set the variable to null, which will match NO packages and cause whatever the check
+    # is to skip with exit 0 (which is what we want!)
+    if (-not $setting) {
+        $setting = "null"
+    }
+}
+
+Write-Host "##vso[task.setvariable variable=TargetingString;]$setting"
