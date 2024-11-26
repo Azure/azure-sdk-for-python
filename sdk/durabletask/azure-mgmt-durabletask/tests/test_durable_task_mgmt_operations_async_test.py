bb528a4dcaef2c8293e78d3e29da58d223b4ff1b
Chenxi Jiang (WICRESOFT NORTH AMERICA LTD) <v-chenjiang@microsoft.com>
2024-11-26 13:54:33 +0800

    update testcases

--- sdk/durabletask/azure-mgmt-durabletask/tests/test_durable_task_mgmt_operations_async_test.py
+++ sdk/durabletask/azure-mgmt-durabletask/tests/test_durable_task_mgmt_operations_async_test.py
@@ -24,4 +24,4 @@ class TestDurableTaskMgmtOperationsAsync(AzureMgmtRecordedTestCase):
     async def test_operations_list(self, resource_group):
         response = self.client.operations.list()
         result = [r async for r in response]
-        assert result == []
+        assert result
--- sdk/durabletask/azure-mgmt-durabletask/tests/test_durable_task_mgmt_operations_test.py
+++ sdk/durabletask/azure-mgmt-durabletask/tests/test_durable_task_mgmt_operations_test.py
@@ -23,4 +23,4 @@ class TestDurableTaskMgmtOperations(AzureMgmtRecordedTestCase):
     def test_operations_list(self, resource_group):
         response = self.client.operations.list()
         result = [r for r in response]
-        assert result == []
+        assert result
