c4e7f16b830708736287278e2d44e3b61ecb7681
Libba Lawrence <llawrence@microsoft.com>
2024-11-26 10:46:57 -0800

    [sb] update conn str (#38655)
    * emulator
    
    * nit
    
    * fixing
    
    * changelog
    
    * parse_conn_string tests
    
    * mgmt client not supported yet -- don't want to have customers see a diff in behavior if they add support suddenly
    
    * changelog
    
    * pylint

--- sdk/servicebus/azure-servicebus/azure/servicebus/management/_management_client.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/management/_management_client.py
@@ -244,14 +244,10 @@ class ServiceBusAdministrationClient:  # pylint:disable=too-many-public-methods
         :returns: Returns a ServiceBusAdministrationClient.
         :rtype: ~azure.servicebus.management.ServiceBusAdministrationClient
         """
-        (
-            endpoint,
-            shared_access_key_name,
-            shared_access_key,
-            _,
-            token,
-            token_expiry,
-        ) = _parse_conn_str(conn_str)
+        # pylint: disable=unused-variable
+        (endpoint, shared_access_key_name, shared_access_key, _, token, token_expiry, emulator) = _parse_conn_str(
+            conn_str
+        )
         if token and token_expiry:
             credential = ServiceBusSASTokenCredential(token, token_expiry)
         elif shared_access_key_name and shared_access_key:
