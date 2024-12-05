0c494ba9e9bc13be9514d8b24ed9bf53581dd77a
Glenn Harper <glharper@microsoft.com>
2024-12-05 13:07:09 -0800

    added genned code for OpenAPIToolDefinition related classes

--- sdk/ai/azure-ai-projects/azure/ai/projects/models/_enums.py
+++ sdk/ai/azure-ai-projects/azure/ai/projects/models/_enums.py
@@ -303,6 +303,14 @@ class MessageStreamEvent(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """Event sent before a message is completed. The data of this event is of type ThreadMessage"""
 
 
+class OpenApiAuthType(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """Type of chunking strategy."""
+
+    ANONYMOUS = "anonymous"
+    CONNECTION = "connection"
+    MANAGED_IDENTITY = "managed_identity"
+
+
 class RunStatus(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """Possible values for the status of an agent thread run."""
 
