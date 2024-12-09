aab7abd7fabc6d2b45fa4e0968b41e68c38b3cff
Matthew Metcalf <mrm9084@gmail.com>
2024-12-09 15:51:22 -0800

    Update sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_client_manager.py
    Co-authored-by: Avani Gupta <avanigupta@users.noreply.github.com>

--- sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_client_manager.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_client_manager.py
@@ -350,8 +350,7 @@ class ConfigurationClientManager(ConfigurationClientManagerBase):  # pylint:disa
 
     def find_active_clients(self):
         """
-        Figures out the current active clients, if _load_balancing_enabled is enabled the start of the list will be the
-        one after the last active client used.
+        Return a list of clients that are not in backoff state. If load balancing is enabled, the most recently used client is moved to the end of the list. 
         """
         active_clients = [client for client in self._replica_clients if client.is_active()]
 
