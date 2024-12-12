14b27de2bfc4dcd1ec2c3a2031d91784b1da9b7e
howieleung <howieleung@microsoft.com>
2024-12-11 16:48:19 -0800

    readme regenerate

--- sdk/ai/azure-ai-projects/samples/agents/sample_agents_azure_ai_search.py
+++ sdk/ai/azure-ai-projects/samples/agents/sample_agents_azure_ai_search.py
@@ -31,6 +31,7 @@ project_client = AIProjectClient.from_connection_string(
     conn_str=os.environ["PROJECT_CONNECTION_STRING"],
 )
 
+# [START create_agent_with_azure_ai_search_tool]
 conn_list = project_client.connections.list()
 conn_id = ""
 for conn in conn_list:
@@ -53,6 +54,7 @@ with project_client:
         tool_resources=ai_search.resources,
         headers={"x-ms-enable-preview": "true"},
     )
+    # [END create_agent_with_azure_ai_search_tool]
     print(f"Created agent, ID: {agent.id}")
 
     # Create thread for communication
