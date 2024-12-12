95e8cff5f319531a5e1d6a35d02f5a435f14cda4
Catalina Peralta <caperal@microsoft.com>
2024-12-11 19:08:57 -0800

    long line

--- sdk/documentintelligence/azure-ai-documentintelligence/azure/ai/documentintelligence/_operations/_patch.py
+++ sdk/documentintelligence/azure-ai-documentintelligence/azure/ai/documentintelligence/_operations/_patch.py
@@ -701,7 +701,8 @@ class DocumentIntelligenceClientOperationsMixin(GeneratedDIClientOps):  # pylint
         """Gets the result of batch document analysis.
 
         :param str continuation_token: An opaque continuation token. Required.
-        :return: An instance of LROPoller that returns AnalyzeBatchResult. The AnalyzeBatchResult is compatible with MutableMapping
+        :return: An instance of LROPoller that returns AnalyzeBatchResult. The AnalyzeBatchResult is
+         compatible with MutableMapping
         :rtype: ~azure.core.polling.LROPoller[~azure.ai.documentintelligence.models.AnalyzeBatchResult]
         :raises ~azure.core.exceptions.HttpResponseError:
         """
