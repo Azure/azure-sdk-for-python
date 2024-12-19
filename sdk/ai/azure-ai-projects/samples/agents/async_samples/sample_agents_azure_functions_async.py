074ad581135672acd4f9e374959ec187242e01b4
Rohit Ganguly <rohitganguly88@gmail.com>
2024-12-19 14:08:52 -0500

    restore AI projects files

--- sdk/ai/azure-ai-projects/samples/agents/async_samples/sample_agents_azure_functions_async.py
+++ sdk/ai/azure-ai-projects/samples/agents/async_samples/sample_agents_azure_functions_async.py
@@ -17,7 +17,7 @@ USAGE:
     pip install azure-ai-projects azure-identity
  
     Set this environment variables with your own values:
-    PROJECT_CONNECTION_STRING - the Azure AI Project connection string, as found in your AI Foundry Project.
+    PROJECT_CONNECTION_STRING - the Azure AI Project connection string, as found in your AI Studio Project.
     STORAGE_SERVICE_ENDPONT - the storage service queue endpoint, triggering Azure function.
     Please see Getting Started with Azure Functions page for more information on Azure Functions:
     https://learn.microsoft.com/azure/azure-functions/functions-get-started
