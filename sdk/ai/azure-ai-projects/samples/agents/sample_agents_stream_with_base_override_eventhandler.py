75ec075df44f6d15a4af9eaf9f1edf04c6da71b6
howieleung <howieleung@microsoft.com>
2025-01-02 09:24:28 -0800

    fixed cspelling and mypy

--- sdk/ai/azure-ai-projects/samples/agents/sample_agents_stream_with_base_override_eventhandler.py
+++ sdk/ai/azure-ai-projects/samples/agents/sample_agents_stream_with_base_override_eventhandler.py
@@ -37,9 +37,9 @@ import os
 # Because we want the iteration to be a string, we define str as the generic type for BaseAsyncAgentEventHandler
 # and override the _process_event method to return a string.
 # The get_stream_chunks method is defined to return the chunks as strings because the iteration is a string.
-class MyEventHandler(BaseAgentEventHandler[str]):
+class MyEventHandler(BaseAgentEventHandler[Optional[str]]):
 
-    def _process_event(self, event_data_str: str) -> str:  # type: ignore[return]
+    def _process_event(self, event_data_str: str) -> Optional[str]:  # type: ignore[return]
         event_lines = event_data_str.strip().split("\n")
         event_type: Optional[str] = None
         event_data = ""
@@ -57,11 +57,12 @@ class MyEventHandler(BaseAgentEventHandler[str]):
             event_obj: MessageDeltaChunk = MessageDeltaChunk(**json.loads(event_data))
 
             return event_obj.text
-        return ""
+        return None
 
     def get_stream_chunks(self) -> Generator[str, None, None]:
         for chunk in self:
-            yield chunk
+            if chunk:
+                yield chunk
 
 
 project_client = AIProjectClient.from_connection_string(
