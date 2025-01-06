643f17c341a68ce10362030c4cfbfd623da42ce3
howieleung <howieleung@microsoft.com>
2025-01-05 21:11:59 -0800

    added integrated tests

--- sdk/ai/azure-ai-projects/tests/agents/test_agent_models_async.py
+++ sdk/ai/azure-ai-projects/tests/agents/test_agent_models_async.py
@@ -1,4 +1,4 @@
-from typing import AsyncIterator, List
+from typing import Any, AsyncIterator, List
 from unittest.mock import AsyncMock, patch
 import pytest
 import os
@@ -19,7 +19,8 @@ def read_file(file_name: str) -> str:
 
 
 main_stream_response = read_file("main_stream_response")
-fetch_current_datetime_stream_response = read_file("fetch_current_datetime_stream_response")
+fetch_current_datetime_and_weather_stream_response = read_file("fetch_current_datetime_and_weather_stream_response")
+send_email_stream_response = read_file("send_email_stream_response")
 
 
 async def convert_to_byte_iterator(main_stream_response: str) -> AsyncIterator[bytes]:
@@ -102,7 +103,9 @@ class TestBaseAsyncAgentEventHandler:
     async def test_event_handler_chain_responses(self):
         handler = self.MyAgentEventhHandler()
         handler.initialize(convert_to_byte_iterator(main_stream_response), self.mock_callable)
-        handler.initialize(convert_to_byte_iterator(fetch_current_datetime_stream_response), self.mock_callable)
+        handler.initialize(
+            convert_to_byte_iterator(fetch_current_datetime_and_weather_stream_response), self.mock_callable
+        )
         count = 0
         all_event_str: List[str] = []
         async for event_str in handler:
@@ -110,7 +113,9 @@ class TestBaseAsyncAgentEventHandler:
             all_event_str.append(event_str)
             count += 1
 
-        assert count == main_stream_response.count("event:") + fetch_current_datetime_stream_response.count("event:")
+        assert count == main_stream_response.count("event:") + fetch_current_datetime_and_weather_stream_response.count(
+            "event:"
+        )
         assert all_event_str[-1].startswith("event: done")
 
     @pytest.mark.asyncio
@@ -122,14 +127,16 @@ class TestBaseAsyncAgentEventHandler:
         async for event_str in handler:
             assert event_str.startswith("event:")
 
-        handler.initialize(convert_to_byte_iterator(fetch_current_datetime_stream_response), self.mock_callable)
+        handler.initialize(
+            convert_to_byte_iterator(fetch_current_datetime_and_weather_stream_response), self.mock_callable
+        )
 
         async for event_str in handler:
             assert event_str.startswith("event:")
             all_event_str.append(event_str)
             count += 1
 
-        assert fetch_current_datetime_stream_response.count("event:")
+        assert fetch_current_datetime_and_weather_stream_response.count("event:")
         assert all_event_str[-1].startswith("event: done")
 
 
@@ -158,58 +165,3 @@ class TestAsyncAgentEventHandler:
         assert mock_parse_event.call_args[0][0] == "event"
         assert submit_tool_outputs.call_count == 1
         assert submit_tool_outputs.call_args[0] == (event_obj, handler)
-
-
-class TestParseEvent:
-
-    def test_parse_event_thread_run_created(self):
-        event_data_str = 'event: thread.run.created\ndata: {"id": "123"}'
-        event_type, event_obj = _parse_event(event_data_str)
-        assert event_type == AgentStreamEvent.THREAD_RUN_CREATED.value
-        assert isinstance(event_obj, ThreadRun)
-        assert event_obj.id == "123"
-
-    def test_parse_event_thread_run_step_created(self):
-        event_data_str = 'event: thread.run.step.created\ndata: {"id": "456"}'
-        event_type, event_obj = _parse_event(event_data_str)
-        assert event_type == AgentStreamEvent.THREAD_RUN_STEP_CREATED.value
-        assert isinstance(event_obj, RunStep)
-        assert event_obj.id == "456"
-
-    def test_parse_event_thread_message_created(self):
-        event_data_str = 'event: thread.message.created\ndata: {"id": "789"}'
-        event_type, event_obj = _parse_event(event_data_str)
-        assert event_type == AgentStreamEvent.THREAD_MESSAGE_CREATED.value
-        assert isinstance(event_obj, ThreadMessage)
-        assert event_obj.id == "789"
-
-    def test_parse_event_thread_message_delta(self):
-        event_data_str = 'event: thread.message.delta\ndata: {"id": "101"}'
-        event_type, event_obj = _parse_event(event_data_str)
-        assert event_type == AgentStreamEvent.THREAD_MESSAGE_DELTA.value
-        assert isinstance(event_obj, MessageDeltaChunk)
-        assert event_obj.id == "101"
-
-    def test_parse_event_thread_run_step_delta(self):
-        event_data_str = 'event: thread.run.step.delta\ndata: {"id": "202"}'
-        event_type, event_obj = _parse_event(event_data_str)
-        assert event_type == AgentStreamEvent.THREAD_RUN_STEP_DELTA.value
-        assert isinstance(event_obj, RunStepDeltaChunk)
-        assert event_obj.id == "202"
-
-    def test_parse_event_invalid_event_type(self):
-        event_data_str = 'event: invalid.event\ndata: {"id": "303"}'
-        event_type, event_obj = _parse_event(event_data_str)
-        assert event_type == "invalid.event"
-        assert event_obj == "{'id': '303'}"
-
-    def test_parse_event_no_event_type(self):
-        event_data_str = 'data: {"id": "404"}'
-        with pytest.raises(ValueError):
-            _parse_event(event_data_str)
-
-    def test_parse_event_invalid_json(self):
-        event_data_str = "event: thread.run.created\ndata: invalid_json"
-        event_type, event_obj = _parse_event(event_data_str)
-        assert event_type == AgentStreamEvent.THREAD_RUN_CREATED.value
-        assert event_obj == "invalid_json"
