ee9494459fc5964a1f35f5c37b291db78237afd5
Scott Beddall <scbedd@microsoft.com>
2025-01-06 16:12:16 -0800

    apply black

--- tools/azure-sdk-tools/tests/integration/scenarios/setup_py_metapackage/setup.py
+++ tools/azure-sdk-tools/tests/integration/scenarios/setup_py_metapackage/setup.py
@@ -8,17 +8,10 @@
 
 from setuptools import setup
 from io import open
-
-with open("README.md", encoding="utf-8") as f:
-    README = f.read()
-with open("CHANGELOG.md", encoding="utf-8") as f:
-    CHANGELOG = f.read()
-
 setup(
     name='azure-keyvault',
     version='4.2.1b1',
     description='Microsoft Azure Key Vault Client Libraries for Python',
-    long_description=README + "\n\n" + CHANGELOG,
     long_description_content_type="text/markdown",
     license='MIT License',
     author='Microsoft Corporation',
