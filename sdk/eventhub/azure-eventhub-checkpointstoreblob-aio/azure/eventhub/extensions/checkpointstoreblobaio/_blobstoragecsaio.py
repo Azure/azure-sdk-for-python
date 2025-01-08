e172c45d1ea87a296944e2369ddf2b94ce10c065
Libba Lawrence <llawrence@microsoft.com>
2025-01-08 11:03:07 -0800

    fix

--- sdk/eventhub/azure-eventhub-checkpointstoreblob-aio/azure/eventhub/extensions/checkpointstoreblobaio/_blobstoragecsaio.py
+++ sdk/eventhub/azure-eventhub-checkpointstoreblob-aio/azure/eventhub/extensions/checkpointstoreblobaio/_blobstoragecsaio.py
@@ -96,8 +96,8 @@ class BlobCheckpointStore(CheckpointStore):
         :returns: A blob checkpoint store.
         :rtype: ~azure.eventhub.extensions.checkpointstoreblobaio.BlobCheckpointStore
         """
-        account_url, secondary, credential = parse_connection_str( # type: ignore[arg-type, assignment]
-            conn_str, credential, 'blob')
+        account_url, secondary, credential = parse_connection_str( # type: ignore[assignment]
+            conn_str, credential, 'blob') # type: ignore[arg-type]
         if 'secondary_hostname' not in kwargs:
             kwargs['secondary_hostname'] = secondary
 
