aa888be7fc7e1a5d1035f36eae024784cf40513f
azure-sdk <PythonSdkPipelines>
2025-01-10 03:44:37 +0000

    code and test

--- sdk/computeschedule/azure-mgmt-computeschedule/generated_samples/scheduled_actions_virtual_machines_get_operation_status.py
+++ sdk/computeschedule/azure-mgmt-computeschedule/generated_samples/scheduled_actions_virtual_machines_get_operation_status.py
@@ -15,7 +15,7 @@ from azure.mgmt.computeschedule import ComputeScheduleMgmtClient
     pip install azure-identity
     pip install azure-mgmt-computeschedule
 # USAGE
-    python operations_list_minimum_set_gen.py
+    python scheduled_actions_virtual_machines_get_operation_status.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -30,11 +30,16 @@ def main():
         subscription_id="SUBSCRIPTION_ID",
     )
 
-    response = client.operations.list()
-    for item in response:
-        print(item)
+    response = client.scheduled_actions.virtual_machines_get_operation_status(
+        locationparameter="eastus2euap",
+        request_body={
+            "correlationid": "35780d2f-1dca-4610-afb4-dd25eec1f34r",
+            "operationIds": ["23480d2f-1dca-4610-afb4-dd25eec1f34r"],
+        },
+    )
+    print(response)
 
 
-# x-ms-original-file: 2024-08-15-preview/Operations_List_MinimumSet_Gen.json
+# x-ms-original-file: 2024-10-01/ScheduledActions_VirtualMachinesGetOperationStatus.json
 if __name__ == "__main__":
     main()
