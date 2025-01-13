5e612d0b395ef238a4afc050016552a7ed3e613a
Venkat Ragavan S <vens@microsoft.com>
2025-01-13 12:55:10 +0530

    skipping failing test due to proxy

--- sdk/loadtesting/azure-developer-loadtesting/tests/test_run_ops.py
+++ sdk/loadtesting/azure-developer-loadtesting/tests/test_run_ops.py
@@ -170,7 +170,7 @@ class TestRunOps(LoadtestingTest):
         with pytest.raises(ResourceNotFoundError):
             run_client.stop_test_run(NON_EXISTING_RESOURCE)
 
-
+    @pytest.mark.live_test_only
     @LoadtestingPowerShellPreparer()
     @recorded_by_proxy
     def test_get_metrics(self, loadtesting_endpoint, loadtesting_test_id, loadtesting_test_run_id):
