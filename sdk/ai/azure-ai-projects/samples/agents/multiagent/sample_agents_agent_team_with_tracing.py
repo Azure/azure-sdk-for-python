e8ba86e3a23198eeff3ee13de591c7e959dfd600
Marko Hietala <markhiet@microsoft.com>
2025-01-14 16:15:35 -0600

    adding multiagent traces

---
+++ sdk/ai/azure-ai-projects/samples/agents/multiagent/sample_agents_agent_team_with_tracing.py
@@ -0,0 +1,62 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+"""
+DESCRIPTION:
+    This sample demonstrates how to multiple agents using AgentTeam with traces.
+
+USAGE:
+    python sample_agents_agent_team.py
+
+    Before running the sample:
+
+    pip install azure-ai-projects azure-identity
+
+    Set this environment variables with your own values:
+    PROJECT_CONNECTION_STRING - the Azure AI Project connection string, as found in your AI Foundry project.
+"""
+
+import os, sys
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor, ConsoleSpanExporter
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from agent_team_with_traces import AgentTeam
+
+project_client = AIProjectClient.from_connection_string(
+    credential=DefaultAzureCredential(),
+    conn_str=os.environ["PROJECT_CONNECTION_STRING"],
+)
+
+# Enable console tracing
+# or, if you have local OTLP endpoint running, change it to
+exporter = ConsoleSpanExporter()
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+trace.get_tracer_provider().add_span_processor(SimpleSpanProcessor(exporter))
+
+with project_client:
+    agent_team = AgentTeam("test_team", project_client=project_client)
+    agent_team.add_agent(
+        model="gpt-4-1106-preview",
+        name="Coder",
+        instructions="You are software engineer who writes great code. Your name is Coder.",
+    )
+    agent_team.add_agent(
+        model="gpt-4-1106-preview",
+        name="Reviewer",
+        instructions="You are software engineer who reviews code. Your name is Reviewer.",
+    )
+    agent_team.assemble_team()
+
+    print("A team of agents specialized in software engineering is available for requests.")
+    while True:
+        user_input = input("Input (type 'quit' to exit): ")
+        if user_input.lower() == "quit":
+            break
+        agent_team.process_request(request=user_input)
+
+    agent_team.dismantle_team()
