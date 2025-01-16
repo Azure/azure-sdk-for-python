2219468aca5c0ad20f3c8e5827b5de0a342c8010
ChenxiJiang333 <v-chenjiang@microsoft.com>
2025-01-16 10:34:59 +0800

    update testcases

--- sdk/redis/azure-mgmt-redis/tests/test_redis_management_operations_async_test.py
+++ sdk/redis/azure-mgmt-redis/tests/test_redis_management_operations_async_test.py
@@ -14,7 +14,6 @@ from devtools_testutils.aio import recorded_by_proxy_async
 AZURE_LOCATION = "eastus"
 
 
-
 class TestRedisManagementOperationsAsync(AzureMgmtRecordedTestCase):
     def setup_method(self, method):
         self.client = self.create_mgmt_client(RedisManagementClient, is_async=True)
--- sdk/redis/azure-mgmt-redis/tests/test_redis_management_redis_operations_test.py
+++ sdk/redis/azure-mgmt-redis/tests/test_redis_management_redis_operations_test.py
@@ -13,7 +13,6 @@ from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGrou
 AZURE_LOCATION = "eastus"
 
 
-
 class TestredisManagementRedisOperations(AzureMgmtRecordedTestCase):
     def setup_method(self, method):
         self.client = self.create_mgmt_client(RedisManagementClient)
@@ -28,7 +27,6 @@ class TestredisManagementRedisOperations(AzureMgmtRecordedTestCase):
         result = [r for r in response]
         assert result == []
 
-
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy
     def test_list_by_subscription(self, resource_group):
@@ -37,4 +35,3 @@ class TestredisManagementRedisOperations(AzureMgmtRecordedTestCase):
         )
         result = [r for r in response]
         assert response
-        
