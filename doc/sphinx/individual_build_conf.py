02b1832e58ab2d33838b5cad0fa3d44e5a51a758
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2025-01-21 13:40:01 -0800

    fix the intersphinx references for a new reference methodology (#39332)

--- doc/sphinx/individual_build_conf.py
+++ doc/sphinx/individual_build_conf.py
@@ -64,8 +64,8 @@ intersphinx_mapping = {
     'trio': ('https://trio.readthedocs.io/en/stable/', None),
     'msal': ('https://msal-python.readthedocs.io/en/latest/', None),
     # Azure packages
-    'azure-core': ('https://azuresdkdocs.blob.core.windows.net/$web/python/azure-core/1.0.0/', None),
-    'azure-identity': ('https://azuresdkdocs.blob.core.windows.net/$web/python/azure-identity/1.0.0/', None),
+    'azure-core': ('https://azuresdkdocs.z19.web.core.windows.net/python/azure-core/1.0.0/', None),
+    'azure-identity': ('https://azuresdkdocs.z19.web.core.windows.net/python/azure-identity/1.0.0/', None),
 }
 
 autodoc_member_order = 'groupwise'
