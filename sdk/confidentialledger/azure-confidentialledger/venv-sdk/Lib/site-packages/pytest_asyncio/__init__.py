8bad155b5093e23938fa5232a5830597d643b3d3
amruthashree <aanekondamat@microsoft.com>
2025-01-23 14:55:35 -0600

    Pushing only recorded tests

--- sdk/confidentialledger/azure-confidentialledger/venv-sdk/Lib/site-packages/pytest_asyncio/__init__.py
+++
@@ -1,8 +0,0 @@
-"""The main point for importing pytest-asyncio items."""
-
-from __future__ import annotations
-
-from ._version import version as __version__  # noqa: F401
-from .plugin import fixture, is_async_test
-
-__all__ = ("fixture", "is_async_test")
