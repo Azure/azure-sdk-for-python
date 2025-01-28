5febfbf2770d6b7e6d16358ccbfe2316b84b2309
Prashant Dhote <pdhote@microsoft.com>
2025-01-28 11:29:51 +0530

    code formatting

--- sdk/ml/azure-ai-ml/tests/batch_online_common/unittests/test_deployment_entity.py
+++ sdk/ml/azure-ai-ml/tests/batch_online_common/unittests/test_deployment_entity.py
@@ -134,7 +134,7 @@ class TestOnlineDeploymentFromYAML:
         with pytest.raises(Exception) as exc:
             blue._merge_with(blue_copy)
         assert "are not matched when merging" in str(exc.value)
-    
+
     def test_k8s_deployment(self) -> None:
         with open(TestOnlineDeploymentFromYAML.BLUE_K8S_ONLINE_DEPLOYMENT, "r") as f:
             target = yaml.safe_load(f)
@@ -155,17 +155,23 @@ class TestOnlineDeploymentFromYAML:
         assert blue.resources.requests.gpu == target["resources"]["requests"]["nvidia.com/gpu"]
         assert blue.description == target["description"]
         assert blue.instance_type == target["instance_type"]
-    
+
     def test_get_arm_resource(self) -> None:
         blue = load_online_deployment(TestOnlineDeploymentFromYAML.MINIMAL_DEPLOYMENT)
         arm_resource = blue._get_arm_resource()
         assert arm_resource[ArmConstants.DEPENDSON_PARAMETER_NAME][0] == f"{blue.environment._arm_type}Deployment"
         assert arm_resource[ArmConstants.DEPENDSON_PARAMETER_NAME][1] == f"{blue.model._arm_type}Deployment"
-    
+
     def test_set_scale_settings(self) -> None:
-        with open(TestOnlineDeploymentFromYAML.MINIMAL_DEPLOYMENT, 'r') as f:
+        with open(TestOnlineDeploymentFromYAML.MINIMAL_DEPLOYMENT, "r") as f:
             blue = yaml.safe_load(f)
-            blue["scale_settings"] = {"type":"default", "min_instances": 1, "max_instances": 2, "polling_interval": 3, "target_utilization_percentage": 4}
+            blue["scale_settings"] = {
+                "type": "default",
+                "min_instances": 1,
+                "max_instances": 2,
+                "polling_interval": 3,
+                "target_utilization_percentage": 4,
+            }
 
             class OnlineDeploymentDict(dict):
                 def __init__(self, *args, **kwargs):
@@ -178,23 +184,24 @@ class TestOnlineDeploymentFromYAML:
             assert blue["scale_settings"]["type"] == "default"
 
     def test_kubenetes_deployment_to_dict(self) -> None:
-         with open(TestOnlineDeploymentFromYAML.BLUE_ONLINE_DEPLOYMENT, 'r') as f:
+        with open(TestOnlineDeploymentFromYAML.BLUE_ONLINE_DEPLOYMENT, "r") as f:
             minimal_deployment = yaml.safe_load(f)
-            online_deployment_dict = load_online_deployment(TestOnlineDeploymentFromYAML.BLUE_ONLINE_DEPLOYMENT)._to_dict()
-            assert online_deployment_dict["name"]== minimal_deployment["name"]
-            assert online_deployment_dict['endpoint_name']== minimal_deployment['endpoint_name']
-            assert online_deployment_dict['model']['name']== minimal_deployment['model']['name']
-            assert online_deployment_dict['type']== 'kubernetes'
+            online_deployment_dict = load_online_deployment(
+                TestOnlineDeploymentFromYAML.BLUE_ONLINE_DEPLOYMENT
+            )._to_dict()
+            assert online_deployment_dict["name"] == minimal_deployment["name"]
+            assert online_deployment_dict["endpoint_name"] == minimal_deployment["endpoint_name"]
+            assert online_deployment_dict["model"]["name"] == minimal_deployment["model"]["name"]
+            assert online_deployment_dict["type"] == "kubernetes"
 
     def test_managed_deployment_to_dict(self) -> None:
-         with open(TestOnlineDeploymentFromYAML.MINIMAL_DEPLOYMENT, 'r') as f:
+        with open(TestOnlineDeploymentFromYAML.MINIMAL_DEPLOYMENT, "r") as f:
             minimal_deployment = yaml.safe_load(f)
             online_deployment_dict = load_online_deployment(TestOnlineDeploymentFromYAML.MINIMAL_DEPLOYMENT)._to_dict()
-            assert online_deployment_dict["name"]== minimal_deployment["name"]
-            assert online_deployment_dict['endpoint_name']== minimal_deployment['endpoint_name']
-            assert online_deployment_dict['model']['name']== minimal_deployment['model']['name']
-            assert online_deployment_dict['type']== 'managed'
-
+            assert online_deployment_dict["name"] == minimal_deployment["name"]
+            assert online_deployment_dict["endpoint_name"] == minimal_deployment["endpoint_name"]
+            assert online_deployment_dict["model"]["name"] == minimal_deployment["model"]["name"]
+            assert online_deployment_dict["type"] == "managed"
 
     def test_generate_dependencies(self) -> None:
         blue = load_online_deployment(TestOnlineDeploymentFromYAML.MINIMAL_DEPLOYMENT)
@@ -203,32 +210,31 @@ class TestOnlineDeploymentFromYAML:
         assert code.scoring_script == blue.code_configuration.scoring_script
         assert environment_id == blue.environment.id
         assert model_id == blue.model.id
-        
+
     def test_get_arm_resource_and_params(self) -> None:
         blue = load_online_deployment(TestOnlineDeploymentFromYAML.MINIMAL_DEPLOYMENT)
         blue.egress_public_network_access = "Enabled"
         blue.private_network_connection = True
         arm_resource = blue._get_arm_resource_and_params(location="westus2")
-        
+
         assert arm_resource[0][0][ArmConstants.DEPENDSON_PARAMETER_NAME][0] == f"{blue.environment._arm_type}Deployment"
         assert arm_resource[0][0][ArmConstants.DEPENDSON_PARAMETER_NAME][1] == f"{blue.model._arm_type}Deployment"
         assert arm_resource[0][1]["online_deployment"]["name"] == blue.name
         assert arm_resource[1][0]["type"] == "Microsoft.MachineLearningServices/workspaces/environments/versions"
         assert arm_resource[1][1]["environment_version"]["name"] == blue.environment.name
-        assert arm_resource[2][0]["type"] == 'Microsoft.MachineLearningServices/workspaces/models/versions'
+        assert arm_resource[2][0]["type"] == "Microsoft.MachineLearningServices/workspaces/models/versions"
         assert arm_resource[2][1]["model_version"]["name"] == blue.model.name
-    
-    
+
     def test_get_arm_resource_and_params_for_kubenets_deployment(self) -> None:
         blue = load_online_deployment(TestOnlineDeploymentFromYAML.BLUE_ONLINE_DEPLOYMENT)
         arm_resource = blue._get_arm_resource_and_params(location="westus2")
-        
+
         assert arm_resource[0][0][ArmConstants.DEPENDSON_PARAMETER_NAME][0] == f"{blue.environment._arm_type}Deployment"
         assert arm_resource[0][0][ArmConstants.DEPENDSON_PARAMETER_NAME][1] == f"{blue.model._arm_type}Deployment"
         assert arm_resource[0][1]["online_deployment"]["name"] == blue.name
         assert arm_resource[1][0]["type"] == "Microsoft.MachineLearningServices/workspaces/environments/versions"
         assert arm_resource[1][1]["environment_version"]["name"] == blue.environment.name
-        assert arm_resource[2][0]["type"] == 'Microsoft.MachineLearningServices/workspaces/models/versions'
+        assert arm_resource[2][0]["type"] == "Microsoft.MachineLearningServices/workspaces/models/versions"
         assert arm_resource[2][1]["model_version"]["name"] == blue.model.name
 
     def test_preview_mir_deployment(self) -> None:
@@ -368,8 +374,7 @@ class TestOnlineDeploymentSDK:
     def test_target_utilization_scale_settings_normal(self) -> None:
         deployment = ManagedOnlineDeployment(name="test1", scale_settings=DefaultScaleSettings())
         deployment._validate_scale_settings()
-    
-    
+
     def test_managed_online_deployment_from_rest_object(self) -> None:
         with open("./tests/test_configs/deployments/online/online_deployment_managed_rest.json", "r") as f:
             rest_object = RestOnlineDeploymentData.deserialize(json.load(f))
@@ -384,19 +389,28 @@ class TestOnlineDeploymentSDK:
             assert blue_deployment.properties == rest_object.properties.properties
             assert blue_deployment.model == rest_object.properties.model
             assert blue_deployment.code_configuration.code == rest_object.properties.code_configuration.code_id
-            assert blue_deployment.request_settings.request_timeout_ms == rest_object.properties.request_settings.request_timeout.seconds * 1000
+            assert (
+                blue_deployment.request_settings.request_timeout_ms
+                == rest_object.properties.request_settings.request_timeout.seconds * 1000
+            )
             assert blue_deployment.environment == rest_object.properties.environment_id
             assert blue_deployment.endpoint_name == "some-endpoint"
             assert blue_deployment.app_insights_enabled == rest_object.properties.app_insights_enabled
             assert blue_deployment.scale_settings.type == rest_object.properties.scale_settings.scale_type.lower()
-            assert blue_deployment.liveness_probe.failure_threshold == rest_object.properties.liveness_probe.failure_threshold
+            assert (
+                blue_deployment.liveness_probe.failure_threshold
+                == rest_object.properties.liveness_probe.failure_threshold
+            )
             assert blue_deployment.instance_type == rest_object.properties.instance_type
             assert blue_deployment.instance_count == rest_object.sku.capacity
             assert blue_deployment.private_network_connection is None
             assert blue_deployment.egress_public_network_access == rest_object.properties.egress_public_network_access
-            assert blue_deployment.data_collector.collections["some-collection"].client_id == rest_object.properties.data_collector.collections["some-collection"].client_id
+            assert (
+                blue_deployment.data_collector.collections["some-collection"].client_id
+                == rest_object.properties.data_collector.collections["some-collection"].client_id
+            )
             assert blue_deployment.provisioning_state == rest_object.properties.provisioning_state
-    
+
     def test_kubenets_online_deployment_from_rest_object_(self) -> None:
         with open("./tests/test_configs/deployments/online/online_deployment_kubernetes_rest.json", "r") as f:
             rest_object = RestOnlineDeploymentData.deserialize(json.load(f))
@@ -410,30 +424,53 @@ class TestOnlineDeploymentSDK:
             assert blue_deployment.environment_variables == rest_object.properties.environment_variables
             assert blue_deployment.properties == rest_object.properties.properties
             assert blue_deployment.model == rest_object.properties.model
-            assert blue_deployment.resources.limits.cpu == rest_object.properties.container_resource_requirements.container_resource_limits.cpu
-            assert blue_deployment.resources.requests.cpu == rest_object.properties.container_resource_requirements.container_resource_requests.cpu
+            assert (
+                blue_deployment.resources.limits.cpu
+                == rest_object.properties.container_resource_requirements.container_resource_limits.cpu
+            )
+            assert (
+                blue_deployment.resources.requests.cpu
+                == rest_object.properties.container_resource_requirements.container_resource_requests.cpu
+            )
             assert blue_deployment.code_configuration.code == rest_object.properties.code_configuration.code_id
-            assert blue_deployment.code_configuration.scoring_script == rest_object.properties.code_configuration.scoring_script
-            assert blue_deployment.request_settings.request_timeout_ms == rest_object.properties.request_settings.request_timeout.seconds * 1000
+            assert (
+                blue_deployment.code_configuration.scoring_script
+                == rest_object.properties.code_configuration.scoring_script
+            )
+            assert (
+                blue_deployment.request_settings.request_timeout_ms
+                == rest_object.properties.request_settings.request_timeout.seconds * 1000
+            )
             assert blue_deployment.environment == rest_object.properties.environment_id
             assert blue_deployment.endpoint_name == "some-endpoint"
             assert blue_deployment.app_insights_enabled == rest_object.properties.app_insights_enabled
             assert blue_deployment.scale_settings.type == "target_utilization"
             assert blue_deployment.scale_settings.min_instances == rest_object.properties.scale_settings.min_instances
             assert blue_deployment.scale_settings.max_instances == rest_object.properties.scale_settings.max_instances
-            assert blue_deployment.scale_settings.polling_interval == rest_object.properties.scale_settings.polling_interval.seconds
-            assert blue_deployment.scale_settings.target_utilization_percentage == rest_object.properties.scale_settings.target_utilization_percentage
-            assert blue_deployment.liveness_probe.success_threshold == rest_object.properties.liveness_probe.success_threshold
+            assert (
+                blue_deployment.scale_settings.polling_interval
+                == rest_object.properties.scale_settings.polling_interval.seconds
+            )
+            assert (
+                blue_deployment.scale_settings.target_utilization_percentage
+                == rest_object.properties.scale_settings.target_utilization_percentage
+            )
+            assert (
+                blue_deployment.liveness_probe.success_threshold
+                == rest_object.properties.liveness_probe.success_threshold
+            )
             assert blue_deployment.instance_type == rest_object.properties.instance_type
             assert blue_deployment.instance_count == rest_object.sku.capacity
-            assert blue_deployment.data_collector.collections["some-collection"].client_id == rest_object.properties.data_collector.collections["some-collection"].client_id
+            assert (
+                blue_deployment.data_collector.collections["some-collection"].client_id
+                == rest_object.properties.data_collector.collections["some-collection"].client_id
+            )
             assert blue_deployment.provisioning_state == rest_object.properties.provisioning_state
 
     def test_online_deployment_from_rest_object_unsupported_type(self) -> None:
-                with open("./tests/test_configs/deployments/online/online_deployment_kubernetes_rest.json", "r") as f:
-                    rest_object = RestOnlineDeploymentData.deserialize(json.load(f))
-                    rest_object.properties.endpoint_compute_type = "Other"
-                    with pytest.raises(DeploymentException) as exc:
-                        OnlineDeployment._from_rest_object(rest_object)
-                    assert str(exc.value) == "Unsupported online endpoint type Other."
-
+        with open("./tests/test_configs/deployments/online/online_deployment_kubernetes_rest.json", "r") as f:
+            rest_object = RestOnlineDeploymentData.deserialize(json.load(f))
+            rest_object.properties.endpoint_compute_type = "Other"
+            with pytest.raises(DeploymentException) as exc:
+                OnlineDeployment._from_rest_object(rest_object)
+            assert str(exc.value) == "Unsupported online endpoint type Other."
