73e7d13c5916d5d7b81347f3d8b64fce874f44b3
Prashant Dhote <pdhote@microsoft.com>
2025-01-28 12:37:06 +0530

    add unit tests for to_dict method

--- sdk/ml/azure-ai-ml/tests/workspace/unittests/test_workspace_entity.py
+++ sdk/ml/azure-ai-ml/tests/workspace/unittests/test_workspace_entity.py
@@ -48,7 +48,7 @@ class TestWorkspaceEntity:
             ServerlessComputeSettings(
                 custom_subnet="/subscriptions/b17253fa-f327-42d6-9686-f3e553e24763/resourcegroups/static_resources_cli_v2_e2e_tests_resources/providers/Microsoft.Network/virtualNetworks/testwsvnet/subnets/default",
                 no_public_ip=True,
-            ),  # Override but using same value
+            ),  # Override but using same valuehg
             ServerlessComputeSettings(
                 custom_subnet="/subscriptions/b17253fa-f327-42d6-9686-f3e553e24763/resourcegroups/static_resources_cli_v2_e2e_tests_resources/providers/Microsoft.Network/virtualNetworks/testwsvnet/subnets/testsubnet",
                 no_public_ip=True,
