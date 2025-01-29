17e75170a06012876c93a00d19fee3e7bb2eae64
Allen Kim <allekim@microsoft.com>
2025-01-29 11:13:34 -0800

    Removing unused imports

--- sdk/cosmos/azure-cosmos/azure/cosmos/aio/_cosmos_client.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/aio/_cosmos_client.py
@@ -26,7 +26,6 @@ from typing import Any, Dict, List, Optional, Union, cast, Mapping, Iterable, Ca
 from azure.core.async_paging import AsyncItemPaged
 from azure.core.credentials import TokenCredential
 from azure.core.credentials_async import AsyncTokenCredential
-from azure.core import MatchConditions
 
 from azure.core.tracing.decorator_async import distributed_trace_async
 from azure.core.tracing.decorator import distributed_trace
