8ea6b9fe2556cf3f32e786cdded22d688967a77a
Simon Moreno <30335873+simorenoh@users.noreply.github.com>
2025-01-29 00:09:10 -0500

    more sync mock tests - missing async copies

--- sdk/cosmos/azure-cosmos/azure/cosmos/documents.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/documents.py
@@ -74,8 +74,8 @@ class DatabaseAccount:  # pylint: disable=too-many-instance-attributes
         self.ReservedDocumentStorageInMB: int = 0
         self.ProvisionedDocumentStorageInMB: int = 0
         self.ConsistencyPolicy: Optional[UserConsistencyPolicy] = None
-        self._WritableLocations: List[str] = []
-        self._ReadableLocations: List[str] = []
+        self._WritableLocations: List[dict] = []
+        self._ReadableLocations: List[dict] = []
         self._EnableMultipleWritableLocations = False
 
     @property
