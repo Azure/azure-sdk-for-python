61c4bc4ac3551f71d8a222bd4841dc7247aeff86
Miles Holland <milesholland@microsoft.com>
2025-01-30 13:44:20 -0500

    update user agent to align with azure standards

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_user_agent.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_user_agent.py
@@ -1,6 +1,11 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+from azure.core.pipeline.policies import UserAgentPolicy
 from azure.ai.evaluation._version import VERSION
+import platform
 
-USER_AGENT = "{}/{}".format("azure-ai-evaluation", VERSION)
+# Format is "azsdk-python-this-package/version language/version (platform info)"
+
+#USER_AGENT = "azsdk-python-evaluation/{} Python/{} ({})".format(VERSION, platform.python_version(), platform.platform())
+USER_AGENT = UserAgentPolicy(sdk_moniker="{}/{}".format("azure-ai-evaluation", VERSION)).user_agent
