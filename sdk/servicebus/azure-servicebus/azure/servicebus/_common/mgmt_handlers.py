0447ecf20210273995bbd1d71b8ead0bf6e97c72
Kashif Khan <kashifkhan@microsoft.com>
2025-01-31 20:35:00 +0000

    set last sequence number for next call

--- sdk/servicebus/azure-servicebus/azure/servicebus/_common/mgmt_handlers.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_common/mgmt_handlers.py
@@ -52,9 +52,10 @@ def peek_op(  # pylint: disable=inconsistent-return-statements
 ):
     condition = message.application_properties.get(MGMT_RESPONSE_MESSAGE_ERROR_CONDITION)
     if status_code == 200:
-        return amqp_transport.parse_received_message(
+        parsed_messages = amqp_transport.parse_received_message(
             message, message_type=ServiceBusReceivedMessage, receiver=receiver, is_peeked_message=True
         )
+        receiver._last_received_sequenced_number = parsed_messages[-1].sequence_number # pylint: disable=protected-access
     if status_code in [202, 204]:
         return []
 
