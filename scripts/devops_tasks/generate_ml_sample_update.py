0bd8a16455169047291b6ac753d391fd24f92181
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2025-01-30 19:12:20 -0800

    Update ml samples pipeline to retarget `azuresdkartifacts` (#39491)
    * update the default storage account being used to share packages to the ml samples repo
    * rewrite the blob uri from newly uploaded distribution to reference the public web access uri

--- scripts/devops_tasks/generate_ml_sample_update.py
+++ scripts/devops_tasks/generate_ml_sample_update.py
@@ -7,9 +7,8 @@ import re
 from azure.storage.blob import BlobServiceClient
 from azure.identity import AzurePowerShellCredential, ChainedTokenCredential, AzureCliCredential
 
-UPLOAD_PATTERN = "{build_id}/{filename}"
-DEFAULT_CONTAINER = os.getenv("BLOB_CONTAINER", "ml-sample-submissions")
-CONNECTION_STRING = os.getenv("BLOB_CONNECTION_STRING", None)
+UPLOAD_PATTERN = "python/distributions/ml-sample/{build_id}/{filename}"
+BLOB_CONTAINER = "$web"
 
 TEST_INSTALL_TEMPLATE = """# <az_ml_sdk_test_install>
 {install_command}
@@ -38,7 +37,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="This python script modifies build.sh files for the azure ml samples repository. Inputs are a "
         + "folder containing the azure-ml whl, the root of a cloned azureml-samples repo, and an optional build id. "
-        + "Retrieves the necessary connection string from BLOB_CONNECTION_STRING."
     )
 
     parser.add_argument(
@@ -77,7 +75,7 @@ if __name__ == "__main__":
     credential_chain = ChainedTokenCredential(AzureCliCredential(), AzurePowerShellCredential())
 
     blob_service_client = BlobServiceClient(account_url=f"https://{args.storage_account_name}.blob.core.windows.net", credential=credential_chain)
-    container_client = blob_service_client.get_container_client(DEFAULT_CONTAINER)
+    container_client = blob_service_client.get_container_client(BLOB_CONTAINER)
     to_be_installed = []
 
     whls = glob.glob(target_glob)
@@ -90,7 +88,12 @@ if __name__ == "__main__":
 
             with open(whl, "rb") as data:
                 result = blob_client.upload_blob(data=data, overwrite=True)
-                to_be_installed.append(blob_client.primary_endpoint)
+                url = blob_client.primary_endpoint
+
+                # replace the original blob uri with the static website style uri
+                # this is such that the install command can be invoked directly without needing to authenticate to the storage account
+                url = url.replace(f"https://{args.storage_account_name}.blob.core.windows.net/%24web/", f"https://{args.storage_account_name}.z5.web.core.windows.net/")
+                to_be_installed.append(url)
         else:
             print("Operated on non-whl file or folder {}".format(whl))
             exit(1)
