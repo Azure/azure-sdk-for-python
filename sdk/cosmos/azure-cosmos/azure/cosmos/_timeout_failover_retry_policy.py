539e9cdde073562db1173b7b528ebc4489e2d195
Kushagra Thapar <kuthapar@microsoft.com>
2025-01-31 20:13:37 -0800

    Fixed issues with swapping and retry policies

--- sdk/cosmos/azure-cosmos/azure/cosmos/_timeout_failover_retry_policy.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/_timeout_failover_retry_policy.py
@@ -21,8 +21,6 @@ class _TimeoutFailoverRetryPolicy(object):
         self.location_index = 0
         self.connection_policy = connection_policy
         self.request = args[0] if args else None
-        if self.request:
-            self.location_endpoint = self.global_endpoint_manager.resolve_service_endpoint(self.request)
 
     def ShouldRetry(self, _exception):
         """Returns true if the request should retry based on the passed-in exception.
@@ -53,28 +51,25 @@ class _TimeoutFailoverRetryPolicy(object):
             # So that we can check in location cache if we need to return the current or previous
             # based on where the request was routed previously.
             self.request.last_routed_location_endpoint_within_region = self.request.location_endpoint_to_route
-            print("In Timeout retry policy: last routed location endpoint within region: ",
-                  self.request.last_routed_location_endpoint_within_region)
 
             if _OperationType.IsReadOnlyOperation(self.request.operation_type):
                 # We just directly got to the next location in case of read requests
                 # We don't retry again on the same region for regional endpoint
-                self.location_endpoint = self.resolve_next_region_service_endpoint()
+                location_endpoint = self.resolve_next_region_service_endpoint()
             else:
-                self.location_endpoint = self.resolve_current_region_service_endpoint()
+                self.global_endpoint_manager.swap_regional_endpoint_values(self.request)
+                location_endpoint = self.resolve_current_region_service_endpoint()
                 # This is the case where both current and previous point to the same writable endpoint
                 # In this case we don't want to retry again, rather failover to the next region
-                if self.request.last_routed_location_endpoint_within_region == self.location_endpoint:
-                    self.location_endpoint = self.resolve_next_region_service_endpoint()
+                if self.request.last_routed_location_endpoint_within_region == location_endpoint:
+                    location_endpoint = self.resolve_next_region_service_endpoint()
 
-            print("Timeout retry policy Location endpoint: ", self.location_endpoint)
-            self.request.route_to_location(self.location_endpoint)
+            self.request.route_to_location(location_endpoint)
         return True
 
 
     # This function prepares the request to go to the second endpoint in the same region
     def resolve_current_region_service_endpoint(self):
-        print("Timeout retry policy - Resolving current region endpoint")
         # clear previous location-based routing directive
         self.request.clear_route_to_location()
         # resolve the next service endpoint in the same region
@@ -85,7 +80,6 @@ class _TimeoutFailoverRetryPolicy(object):
     # This function prepares the request to go to the next region
     def resolve_next_region_service_endpoint(self):
         self.location_index += 1
-        print("Timeout retry policy - Resolving next service endpoint - location index: {}".format(self.location_index))
         # clear previous location-based routing directive
         self.request.clear_route_to_location()
         # clear the last routed endpoint within same region since we are going to a new region now
