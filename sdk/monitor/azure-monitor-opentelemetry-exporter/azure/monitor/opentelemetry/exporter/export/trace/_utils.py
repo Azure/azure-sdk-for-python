819466bff9d43ccee4769e9dc35356ebfcd2d38d
Leighton Chen <lechen@microsoft.com>
2025-02-06 13:24:22 -0800

    revert

--- sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/export/trace/_utils.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/export/trace/_utils.py
@@ -1,6 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-import logging
 
 from typing import no_type_check, Optional, Tuple
 from urllib.parse import urlparse
@@ -13,9 +12,6 @@ from opentelemetry.semconv.attributes import (
 )
 from opentelemetry.semconv.trace import DbSystemValues, SpanAttributes
 from opentelemetry.util.types import Attributes
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
 
 
 # pylint:disable=too-many-return-statements
@@ -260,14 +256,11 @@ def _get_user_agent(attributes: Attributes) -> Optional[str]:
 @no_type_check
 def _get_url_for_http_request(attributes: Attributes) -> Optional[str]:
     url = ""
-    logger.debug("here")
     if attributes:
         # Url
         if url_attributes.URL_FULL in attributes:
-            logger.debug("here2")
             return attributes[url_attributes.URL_FULL]
         if SpanAttributes.HTTP_URL in attributes:
-            logger.debug("here3")
             return attributes[SpanAttributes.HTTP_URL]
         # Scheme
         scheme = _get_http_scheme(attributes)
@@ -275,37 +268,31 @@ def _get_url_for_http_request(attributes: Attributes) -> Optional[str]:
         http_target = ""
         if url_attributes.URL_PATH in attributes:
             http_target = attributes.get(url_attributes.URL_PATH, "")
-            if url_attributes.URL_QUERY in attributes:
-                http_target = "{}?{}".format(
-                    http_target,
-                    attributes.get(url_attributes.URL_QUERY, "")
-                )
-            logger.debug("here4")
-        elif SpanAttributes.HTTP_TARGET in attributes:
-            http_target = attributes.get(SpanAttributes.HTTP_TARGET, "")
-            logger.debug("here5")
+        if url_attributes.URL_QUERY in attributes:
+            http_target = "{}?{}".format(
+                http_target,
+                attributes.get(url_attributes.URL_QUERY, "")
+            )
+        if not http_target:
+            http_target = attributes.get(SpanAttributes.HTTP_TARGET)
         if scheme and http_target:
             # Host
             http_host = ""
             if server_attributes.SERVER_ADDRESS in attributes:
                 http_host = attributes.get(server_attributes.SERVER_ADDRESS, "")
-                if server_attributes.SERVER_PORT in attributes:
-                    http_host = "{}:{}".format(
-                        http_host,
-                        attributes.get(server_attributes.SERVER_PORT, "")
-                    )
-                logger.debug("here6")  
-            elif SpanAttributes.HTTP_HOST in attributes:
+            if server_attributes.SERVER_PORT in attributes:
+                http_host = "{}:{}".format(
+                    http_host,
+                    attributes.get(server_attributes.SERVER_PORT, "")
+                )
+            if not http_host:
                 http_host = attributes.get(SpanAttributes.HTTP_HOST, "")
-                logger.debug("here7")  
-            # Url
             if http_host:
                 url = "{}://{}{}".format(
                     scheme,
                     http_host,
                     http_target,
                 )
-                logger.debug("here8")  
             elif SpanAttributes.HTTP_SERVER_NAME in attributes:
                 server_name = attributes[SpanAttributes.HTTP_SERVER_NAME]
                 host_port = attributes.get(SpanAttributes.NET_HOST_PORT, "")
@@ -315,7 +302,6 @@ def _get_url_for_http_request(attributes: Attributes) -> Optional[str]:
                     host_port,
                     http_target,
                 )
-                logger.debug("here9")  
             elif SpanAttributes.NET_HOST_NAME in attributes:
                 host_name = attributes[SpanAttributes.NET_HOST_NAME]
                 host_port = attributes.get(SpanAttributes.NET_HOST_PORT, "")
@@ -325,6 +311,4 @@ def _get_url_for_http_request(attributes: Attributes) -> Optional[str]:
                     host_port,
                     http_target,
                 )
-                logger.debug("here10")
-        logger.debug(url)  
     return url
