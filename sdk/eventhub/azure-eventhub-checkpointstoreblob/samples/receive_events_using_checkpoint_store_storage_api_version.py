20c34818b6ec913565370d322a3427f84a951d91
Libba Lawrence <llawrence@microsoft.com>
2025-02-07 09:17:34 -0800

    [EH] update checkpointstore tests/samples (#39498)
    * try
    
    * tests
    
    * skip
    
    * update
    
    * update
    
    * req
    
    * bump test
    
    * bump
    
    * git
    
    * try this
    
    * add
    
    * await
    
    * do we need all permissions here?
    
    * bump
    
    * fix
    
    * api version
    
    * update
    
    * remove
    
    * Revert "remove"
    
    This reverts commit 503d44a90a48a29a7ae0e6c3b982590cce8001a6.
    
    * add
    
    * Revert "add"
    
    This reverts commit ef03d41f7a9f5a454a38b7eed520d41c72ba8dee.
    
    * try
    
    * getcred
    
    * try
    
    * async
    
    * dev
    
    * try
    
    * try
    
    * bump
    
    * cahnge
    
    * tests
    
    * Update README.md with new documentation link
    
    * why
    
    * space
    
    * space2
    
    * r

--- sdk/eventhub/azure-eventhub-checkpointstoreblob/samples/receive_events_using_checkpoint_store_storage_api_version.py
+++ sdk/eventhub/azure-eventhub-checkpointstoreblob/samples/receive_events_using_checkpoint_store_storage_api_version.py
@@ -15,14 +15,16 @@ service versions supported on Azure Stack Hub, please refer to
 """
 
 import os
+from azure.identity import DefaultAzureCredential
 from azure.eventhub import EventHubConsumerClient # type: ignore[attr-defined]
 from azure.eventhub.extensions.checkpointstoreblob import BlobCheckpointStore
 
-CONNECTION_STR = os.environ["EVENT_HUB_CONN_STR"]
+FULLY_QUALIFIED_NAMESPACE = os.environ["EVENT_HUB_HOSTNAME"]
 EVENTHUB_NAME = os.environ['EVENT_HUB_NAME']
-STORAGE_CONNECTION_STR = os.environ["AZURE_STORAGE_CONN_STR"]
+STORAGE_ACCOUNT = "https://{}.blob.core.windows.net".format(
+        os.environ["AZURE_STORAGE_ACCOUNT"])
 BLOB_CONTAINER_NAME = "your-blob-container-name"  # Please make sure the blob container resource exists.
-STORAGE_SERVICE_API_VERSION = "2017-11-09"
+STORAGE_SERVICE_API_VERSION = "2019-02-02"
 
 
 def on_event(partition_context, event):
@@ -33,13 +35,15 @@ def on_event(partition_context, event):
 
 
 if __name__ == '__main__':
-    checkpoint_store = BlobCheckpointStore.from_connection_string(
-        STORAGE_CONNECTION_STR,
+    checkpoint_store = BlobCheckpointStore(
+        STORAGE_ACCOUNT,
         container_name=BLOB_CONTAINER_NAME,
-        api_version=STORAGE_SERVICE_API_VERSION
+        api_version=STORAGE_SERVICE_API_VERSION,
+        credential=DefaultAzureCredential()
     )
-    client = EventHubConsumerClient.from_connection_string(
-        CONNECTION_STR,
+    client = EventHubConsumerClient(
+        FULLY_QUALIFIED_NAMESPACE,
+        credential=DefaultAzureCredential(),
         consumer_group='$Default',
         eventhub_name=EVENTHUB_NAME,
         checkpoint_store=checkpoint_store
