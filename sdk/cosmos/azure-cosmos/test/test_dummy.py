5d72848fbf91c295007acbff2a7e110572ca07c2
Fabian Meiswinkel <fabianm@microsoft.com>
2025-02-07 16:31:23 +0000

    Refactoring to have FaultInjectionTransport in its own file

--- sdk/cosmos/azure-cosmos/test/test_dummy.py
+++ sdk/cosmos/azure-cosmos/test/test_dummy.py
@@ -1,16 +1,12 @@
 # The MIT License (MIT)
 # Copyright (c) Microsoft Corporation. All rights reserved.
 
-from collections.abc import MutableMapping
 import logging
 import time
-from tokenize import String
-from typing import Any, Callable
 import unittest
 import uuid
 
 import pytest
-import pytest_asyncio
 
 from azure.cosmos.aio._container import ContainerProxy
 from azure.cosmos.aio._database import DatabaseProxy
@@ -18,11 +14,12 @@ from azure.cosmos.exceptions import CosmosHttpResponseError
 import test_config
 from azure.cosmos import PartitionKey
 from azure.cosmos.aio import CosmosClient, _retry_utility_async
-from azure.core.rest import HttpRequest, AsyncHttpResponse
+from azure.core.rest import HttpRequest
 import asyncio
-import aiohttp
 import sys
 from azure.core.pipeline.transport import AioHttpTransport
+from typing import Any, Callable
+import _fault_injection_transport
 
 COLLECTION = "created_collection"
 logger = logging.getLogger('azure.cosmos')
@@ -108,7 +105,7 @@ class TestDummyAsync:
                                'name': 'sample document',
                                'key': 'value'}
 
-        custom_transport = FaulInjectionTransport(logger)
+        custom_transport =  _fault_injection_transport.FaulInjectionTransport(logger)
         predicate : Callable[[HttpRequest], bool] = lambda r: self.predicate_req_payload_contains_id(r, idValue) 
         custom_transport.addFault(predicate, lambda: self.throw_after_delay(
             500,
@@ -128,7 +125,7 @@ class TestDummyAsync:
             await self.cleanup(initializedObjects)
 
     async def test_succeeds_with_multiple_endpoints(self, setup):
-        custom_transport = FaulInjectionTransport(logger)
+        custom_transport = _fault_injection_transport.FaulInjectionTransport(logger)
         idValue: str = str(uuid.uuid4())
         document_definition = {'id': idValue,
                                'pk': idValue,
@@ -148,62 +145,6 @@ class TestDummyAsync:
 
         finally:
             await self.cleanup(initializedObjects)        
-
-
-class FaulInjectionTransport(AioHttpTransport):
-    def __init__(self, logger: logging.Logger, *, session: aiohttp.ClientSession | None = None, loop=None, session_owner: bool = True, **config):
-        self.logger = logger
-        self.faults = []
-        self.requestTransformations = []
-        self.responseTransformations = []
-        super().__init__(session=session, loop=loop, session_owner=session_owner, **config)
-
-    def addFault(self, predicate: Callable[[HttpRequest], bool], faultFactory: Callable[[HttpRequest], asyncio.Task[Exception]]):
-        self.faults.append({"predicate": predicate, "apply": faultFactory})
-
-    def addRequestTransformation(self, predicate: Callable[[HttpRequest], bool], requestTransformation: Callable[[HttpRequest], asyncio.Task[HttpRequest]]):
-        self.requestTransformations.append({"predicate": predicate, "apply": requestTransformation})
-
-    def addResponseTransformation(self, predicate: Callable[[HttpRequest], bool], responseTransformation: Callable[[HttpRequest, Callable[[HttpRequest], asyncio.Task[AsyncHttpResponse]]], AsyncHttpResponse]):
-        self.responseTransformations.append({
-            "predicate": predicate, 
-            "apply": responseTransformation})    
-
-    def firstItem(self, iterable, condition=lambda x: True):
-        """
-        Returns the first item in the `iterable` that satisfies the `condition`.
-        
-        If no item satisfies the condition, it returns None.
-        """
-        return next((x for x in iterable if condition(x)), None)
-
-    async def send(self, request: HttpRequest, *, stream: bool = False, proxies: MutableMapping[str, str] | None = None, **config):
-        # find the first fault Factory with matching predicate if any
-        firstFaultFactory = self.firstItem(iter(self.faults), lambda f: f["predicate"](request))
-        if (firstFaultFactory != None):
-            injectedError = await firstFaultFactory["apply"]()
-            self.logger.info("Found to-be-injected error {}".format(injectedError))
-            raise injectedError
-
-        # apply the chain of request transformations with matching predicates if any
-        matchingRequestTransformations = filter(lambda f: f["predicate"](f["predicate"]), iter(self.requestTransformations))
-        for currentTransformation in matchingRequestTransformations:
-            request = await currentTransformation["apply"](request)
-
-        firstResonseTransformation = self.firstItem(iter(self.responseTransformations), lambda f: f["predicate"](request))
-        
-        getResponseTask = super().send(request, stream=stream, proxies=proxies, **config)
-        
-        if (firstResonseTransformation != None):
-            self.logger.info(f"Invoking response transformation")
-            response = await firstResonseTransformation["apply"](request, lambda: getResponseTask)        
-            self.logger.info(f"Received response transformation result with status code {response.status_code}")
-            return response
-        else:
-            self.logger.info(f"Sending request to {request.url}")
-            response = await getResponseTask
-            self.logger.info(f"Received response with status code {response.status_code}")
-            return response
 
 if __name__ == '__main__':
     unittest.main()
