0847bf07f7897816bb66fbda3bfa37ef2bae1450
Sydney Lister <sydneylister@microsoft.com>
2025-02-10 11:53:22 -0800

    minor updates

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/__init__.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/__init__.py
@@ -2,7 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-from ._dsb_evaluation._dsb_evaluation import _DSBEvaluator, _DSBEvaluation
 from ._evaluate._evaluate import evaluate
 from ._evaluators._bleu import BleuScoreEvaluator
 from ._evaluators._coherence import CoherenceEvaluator
