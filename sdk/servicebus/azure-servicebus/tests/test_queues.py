37933cae3f6626080fc8fbde91a5908b43f706c5
swathipil <swathip@microsoft.com>
2025-02-11 14:55:42 -0800

    add alr renew 300 msgs test for sync

--- sdk/servicebus/azure-servicebus/tests/test_queues.py
+++ sdk/servicebus/azure-servicebus/tests/test_queues.py
@@ -1683,44 +1683,38 @@ class TestServiceBusQueue(AzureMgmtRecordedTestCase):
     @CachedServiceBusResourceGroupPreparer(name_prefix="servicebustest")
     @CachedServiceBusNamespacePreparer(name_prefix="servicebustest")
     @ServiceBusQueuePreparer(
-        name_prefix="servicebustest", dead_lettering_on_message_expiration=True, lock_duration="PT1M"
+        name_prefix="servicebustest", dead_lettering_on_message_expiration=True, lock_duration="PT55S"
     )
     @pytest.mark.parametrize("uamqp_transport", uamqp_transport_params, ids=uamqp_transport_ids)
     @ArgPasser()
     def test_queue_receive_handler_with_autolockrenew_many_msgs(
         self, uamqp_transport, *, servicebus_namespace=None, servicebus_queue=None, **kwargs
     ):
-
+        # only tested on sync since async can concurrently send renew-lock requests without waiting for the transfer frame from the service
         fully_qualified_namespace = f"{servicebus_namespace.name}{SERVICEBUS_ENDPOINT_SUFFIX}"
         credential = get_credential()
         with ServiceBusClient(
-            fully_qualified_namespace, credential, logging_enable=False, uamqp_transport=uamqp_transport
+            fully_qualified_namespace, credential, logging_enable=True, uamqp_transport=uamqp_transport
         ) as sb_client:
             with sb_client.get_queue_sender(servicebus_queue.name) as sender:
                 msgs_to_send = [ServiceBusMessage("message: {}".format(i)) for i in range(400)]
                 sender.send_messages(msgs_to_send)
-                print("Send messages to non-sessionful queue.")
 
             # Can also be called via "with AutoLockRenewer() as renewer" to automate shutdown.
-            renewer = AutoLockRenewer()
+            renewer = AutoLockRenewer(max_lock_renewal_duration=60*5)
 
-            with sb_client.get_queue_receiver(queue_name=servicebus_queue.name, renewer=renewer) as receiver:
+            with sb_client.get_queue_receiver(queue_name=servicebus_queue.name, auto_lock_renewer=renewer) as receiver:
                 received_msgs = receiver.receive_messages(max_message_count=300)
                 # At least 300 messages should be renewed
+                # TODO: This should be bumped up once sync alr perf is improved
                 while len(received_msgs) < 300:
                     count = 300 - len(received_msgs)
                     received_msgs.extend(receiver.receive_messages(max_message_count=count))
 
                 assert len(received_msgs) == 300, "Did not receive all messages"
-                print("Register messages into AutoLockRenewer done.")
-                # Sleep for 50 secs, then allow thread to renew locks 10 secs before expiry
-                time.sleep(50)
-
-                time.sleep(70)  # message handling for long period (E.g. application logic)
-
+                time.sleep(100)
                 for msg in received_msgs:
                     receiver.complete_message(msg)  # Settling the message deregisters it from the AutoLockRenewer
-                print("Complete messages.")
 
             renewer.close()
 
