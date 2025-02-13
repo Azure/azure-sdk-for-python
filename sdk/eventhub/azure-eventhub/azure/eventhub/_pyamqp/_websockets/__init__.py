f3590246fca76214a1f3bcda64289224670b36ad
Kashif Khan <kashifkhan@microsoft.com>
2025-02-13 18:17:48 +0000

    move websocket files up one level

--- sdk/eventhub/azure-eventhub/azure/eventhub/_pyamqp/_websockets/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._exceptions import WebSocketConnectionClosed, WebSocketConnectionError
-from .websockets import WebSocket, AsyncWebSocket
-
-__all__ = ["WebSocket", "AsyncWebSocket", "WebSocketConnectionClosed", "WebSocketConnectionError"]
