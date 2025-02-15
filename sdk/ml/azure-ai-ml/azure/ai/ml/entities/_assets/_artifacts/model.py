0c22f0c710b647d45a7e0a33f8d246e03e3a88d6
Chao Yu <chaoyu@microsoft.com>
2025-02-15 11:05:28 -0500

    Add system_meatadata property to azure ml model class

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_assets/_artifacts/model.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_assets/_artifacts/model.py
@@ -85,6 +85,7 @@ class Model(Artifact):  # pylint: disable=too-many-instance-attributes
     ) -> None:
         self.job_name = kwargs.pop("job_name", None)
         self._intellectual_property = kwargs.pop("intellectual_property", None)
+        self._system_metadata = kwargs.pop("system_metatdata", None)
         super().__init__(
             name=name,
             version=version,
@@ -182,6 +183,10 @@ class Model(Artifact):  # pylint: disable=too-many-instance-attributes
             stage=self.stage,
             is_anonymous=self._is_anonymous,
         )
+        
+        if hasattr(self, '_system_metadata'):
+            model_version.system_metadata = self._system_metadata
+
         model_version_resource = ModelVersion(properties=model_version)
 
         return model_version_resource
