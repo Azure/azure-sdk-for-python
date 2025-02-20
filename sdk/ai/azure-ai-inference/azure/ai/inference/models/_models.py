a961376958f9b8f5800cf264620794a5c210fd24
trangevi <trangevi@microsoft.com>
2025-02-20 10:00:34 -0800

    pylint fix
    Signed-off-by: trangevi <trangevi@microsoft.com>

--- sdk/ai/azure-ai-inference/azure/ai/inference/models/_models.py
+++ sdk/ai/azure-ai-inference/azure/ai/inference/models/_models.py
@@ -397,7 +397,7 @@ class ChatCompletionsOptions(_model_base.Model):
      yourself via a system or user message. Without this, the model may generate an unending stream
      of whitespace until the generation reaches the token limit, resulting in a long-running and
      seemingly \"stuck\" request. Also note that the message content may be partially cut off if
-     ``finish_reason=\"length\"``\ , which indicates the generation exceeded ``max_tokens`` or the
+     ``finish_reason=\"length\"``\\ , which indicates the generation exceeded ``max_tokens`` or the
      conversation exceeded the max context length."""
     stop: Optional[List[str]] = rest_field(visibility=["read", "create", "update", "delete", "query"])
     """A collection of textual sequences that will end completions generation."""
