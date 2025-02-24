e15a4e75d9315eb79aa54eaee6354b66f993867d
Sydney Lister <sydneylister@microsoft.com>
2025-02-24 11:40:18 -0800

    sample update

--- sdk/evaluation/azure-ai-evaluation/samples/pyrit_sim.py
+++ sdk/evaluation/azure-ai-evaluation/samples/pyrit_sim.py
@@ -9,6 +9,7 @@ pip install -e ".[pyrit]"
 from azure.ai.evaluation._safety_evaluation._safety_evaluation import _SafetyEvaluation
 import os
 from azure.identity import DefaultAzureCredential
+from azure.ai.evaluation.simulator import AdversarialScenario
 
 
 async def main():
@@ -16,18 +17,34 @@ async def main():
         "azure_endpoint": os.environ.get("AZURE_ENDPOINT"),
         "azure_deployment": os.environ.get("AZURE_DEPLOYMENT_NAME"),
     }
+
+    def test_target_fn(query: str) -> str:
+        return "mock response"
+
     azure_ai_project = {
         "subscription_id": os.environ.get("AZURE_SUBSCRIPTION_ID"),
         "resource_group_name": os.environ.get("AZURE_RESOURCE_GROUP"),
         "project_name": os.environ.get("AZURE_PROJECT_NAME"),
     }
-    safety_eval = _SafetyEvaluation(
+
+    safety_eval_callback_target = _SafetyEvaluation(
         azure_ai_project=azure_ai_project,
-        model_config=model_config,
         credential=DefaultAzureCredential(),
     )
 
-    outputs = await safety_eval(
+    outputs = await safety_eval_callback_target(
+        target=test_target_fn,
+        num_rows=8,
+    )
+
+    print(outputs)
+
+    safety_eval_model_target = _SafetyEvaluation(
+        azure_ai_project=azure_ai_project,
+        credential=DefaultAzureCredential(),
+    )
+
+    outputs = await safety_eval_model_target(
         target=model_config,
         num_rows=8,
     )
