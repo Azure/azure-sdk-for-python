802be278515e155f5974f305c2fdd46b4dc24034
Prashant Dhote <pdhote@microsoft.com>
2025-02-27 00:28:02 +0530

    add deployment update test

--- sdk/ml/azure-ai-ml/tests/online_services/e2etests/test_online_deployment.py
+++ sdk/ml/azure-ai-ml/tests/online_services/e2etests/test_online_deployment.py
@@ -6,7 +6,7 @@ from devtools_testutils import AzureRecordedTestCase
 from azure.ai.ml import MLClient, load_online_deployment, load_online_endpoint
 from azure.ai.ml.constants import AssetTypes
 from azure.ai.ml.entities import ManagedOnlineDeployment, ManagedOnlineEndpoint, Model, CodeConfiguration, Environment
-
+from azure.ai.ml.entities import TargetUtilizationScaleSettings
 
 @pytest.mark.e2etest
 @pytest.mark.usefixtures(
@@ -16,12 +16,12 @@ from azure.ai.ml.entities import ManagedOnlineDeployment, ManagedOnlineEndpoint,
 )
 @pytest.mark.production_experiences_test
 class TestOnlineDeployment(AzureRecordedTestCase):
-    def test_online_deployment(
+    def test_online_deployment_create(
         self, client: MLClient, rand_online_name: Callable[[], str], rand_online_deployment_name: Callable[[], str]
     ) -> None:
         endpoint_yaml = "tests/test_configs/deployments/online/simple_online_endpoint_mir.yaml"
         deployment_yaml = "tests/test_configs/deployments/online/online_deployment_1.yaml"
-        name = rand_online_name("name")
+        name = rand_online_name("endpoint_name")
         endpoint = load_online_endpoint(endpoint_yaml)
         endpoint.name = name
 
@@ -50,6 +50,42 @@ class TestOnlineDeployment(AzureRecordedTestCase):
                 endpoint_name=endpoint.name,
                 request_file="tests/test_configs/deployments/model-1/sample-request.json",
             )
+        except Exception as ex:
+            raise ex
+        finally:
+            client.online_endpoints.begin_delete(name=endpoint.name)
+    
+    def test_online_deployment_update(
+        self, client: MLClient, rand_online_name: Callable[[], str], rand_online_deployment_name: Callable[[], str]
+    ) -> None:
+        endpoint_yaml = "tests/test_configs/deployments/online/simple_online_endpoint_mir.yaml"
+        deployment_yaml = "tests/test_configs/deployments/online/online_deployment_1.yaml"
+        endpoint = load_online_endpoint(endpoint_yaml)
+        endpoint_name = rand_online_name("endpoint-name")
+        endpoint.name = endpoint_name
+        deployment = load_online_deployment(deployment_yaml)
+        deployment_name = rand_online_deployment_name("deployment_name")
+        deployment.name = deployment_name
+        deployment.endpoint_name = endpoint_name
+
+        # create a endpoint
+        client.online_endpoints.begin_create_or_update(endpoint).result()
+
+        try:
+            # create a deployment
+            client.online_deployments.begin_create_or_update(deployment).result()
+            dep = client.online_deployments.get(name=deployment.name, endpoint_name=endpoint.name)
+            assert dep.name == deployment.name
+
+            deps = client.online_deployments.list(endpoint_name=endpoint.name)
+            deps = list(deps)
+            assert len(deps) == 1
+            assert deps[0].name == deployment.name
+
+            dep.instance_count = 3
+            updated_deploymwnt = client.online_deployments.begin_create_or_update(dep).result()
+            assert updated_deploymwnt.instance_count == 3
+
         except Exception as ex:
             raise ex
         finally:
