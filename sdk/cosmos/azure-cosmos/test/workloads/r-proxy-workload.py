e0e8cb5a4c1a108a49b0812958fc50a57e008fa4
tvaron3 <tomas.varon1802@gmail.com>
2025-02-27 15:18:06 -0800

    update proxy

--- sdk/cosmos/azure-cosmos/test/workloads/r-proxy-workload.py
+++ sdk/cosmos/azure-cosmos/test/workloads/r-proxy-workload.py
@@ -56,7 +56,8 @@ async def run_workload(client_id):
         async with AsyncClient(COSMOS_URI, COSMOS_KEY, preferred_locations=PREFERRED_LOCATIONS,
                                enable_diagnostics_logging=True, logger=logger, transport=transport,
                                user_agent=str(client_id) + "-" + datetime.now().strftime(
-                                   "%Y%m%d-%H%M%S")) as client:
+                                   "%Y%m%d-%H%M%S"),
+                               proxy={"http": "http://0.0.0.0:5100"}) as client:
             db = client.get_database_client("SimonDB")
             cont = db.get_container_client("SimonContainer")
             time.sleep(1)
