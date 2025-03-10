cbabbe9400d7cbdedf0e53ee381dfe5a5fef96f0
Libba Lawrence <llawrence@microsoft.com>
2025-03-10 13:40:51 -0700

    add

---
+++ tools/azure-sdk-tools/linting_tools/lint_test_bench/test_files/test_client_constructor_takes_credential.py
@@ -0,0 +1,13 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+# This code violates missing-client-constructor-parameter-credential and missing-client-constructor-parameter-kwargs
+class ClassNameClient():
+    def __init__(self, *, api_version:str = "2018") -> None:
+        """
+        :keyword eight: The eighth parameter.
+        :paramtype eight: str
+        :keyword api_version: The API version to use.
+        :paramtype api_version: str
+        """
+        self.api_version: str = api_version
---
+++ tools/azure-sdk-tools/linting_tools/lint_test_bench/test_files_without_comment/test_client_constructor_takes_credential.py
@@ -0,0 +1,12 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+class ClassNameClient():
+    def __init__(self, *, api_version:str = "2018") -> None:
+        """
+        :keyword eight: The eighth parameter.
+        :paramtype eight: str
+        :keyword api_version: The API version to use.
+        :paramtype api_version: str
+        """
+        self.api_version: str = api_version
