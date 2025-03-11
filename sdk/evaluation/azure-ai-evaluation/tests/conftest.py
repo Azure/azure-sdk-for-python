0f6098bbfa9df0a81fafef27d2308b209d59724f
Waqas Javed <7674577+w-javed@users.noreply.github.com>
2025-03-10 20:46:46 -0700

    revert operation

--- sdk/evaluation/azure-ai-evaluation/tests/conftest.py
+++ sdk/evaluation/azure-ai-evaluation/tests/conftest.py
@@ -62,7 +62,6 @@ class SanitizedValues:
     TENANT_ID = ZERO_GUID
     USER_OBJECT_ID = ZERO_GUID
     IMAGE_NAME = "00000000.png"
-    OPERATIONS = ZERO_GUID
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -113,9 +112,6 @@ def add_sanitizers(
         )
         add_general_regex_sanitizer(
             regex=r"image_understanding/([-\w\._\(\)/]+)", value=mock_project_scope["image_name"], group_for_replace="1"
-        )
-        add_general_regex_sanitizer(
-            regex=r"/operations/([-\w\._\(\)]+)", value=mock_project_scope["operations"], group_for_replace="1"
         )
 
     def openai_stainless_default_headers():
@@ -344,7 +340,6 @@ def mock_project_scope() -> Dict[str, str]:
         "resource_group_name": f"{SanitizedValues.RESOURCE_GROUP_NAME}",
         "project_name": f"{SanitizedValues.WORKSPACE_NAME}",
         "image_name": f"{SanitizedValues.IMAGE_NAME}",
-        "operations": f"{SanitizedValues.OPERATIONS}",
     }
 
 
