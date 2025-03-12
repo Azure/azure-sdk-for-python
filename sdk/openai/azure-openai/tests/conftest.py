33cc1415b1d81436eeef7658205dd2fbc46c9612
Krista Pratico <krpratic@microsoft.com>
2025-03-11 17:52:58 -0700

    Update api_version to feb preview

--- sdk/openai/azure-openai/tests/conftest.py
+++ sdk/openai/azure-openai/tests/conftest.py
@@ -21,7 +21,7 @@ from devtools_testutils import get_credential
 
 # for pytest.parametrize
 GA = "2024-10-21"
-PREVIEW = "2025-01-01-preview"
+PREVIEW = "2025-02-01-preview"
 LATEST = PREVIEW
 
 AZURE = "azure"
