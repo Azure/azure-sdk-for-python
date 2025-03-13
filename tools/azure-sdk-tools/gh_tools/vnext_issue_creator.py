f8e2515088b9b0b73073e9feef685a698bf90dca
Libba Lawrence <llawrence@microsoft.com>
2025-03-13 09:33:50 -0700

    try

--- tools/azure-sdk-tools/gh_tools/vnext_issue_creator.py
+++ tools/azure-sdk-tools/gh_tools/vnext_issue_creator.py
@@ -88,7 +88,7 @@ def get_build_info(build_link: str, check_type: CHECK_TYPE, service_directory: s
             # Get the build info from the build link
             logs_output = requests.get(logs_link, headers=AUTH_HEADERS)
             build_output = logs_output.text
-            return type(build_output)
+            return build_output
             build_output = build_output.split(f"next-pylint: commands[3]> python /mnt/vss/_work/1/s/eng/tox/run_pylint.py -t /mnt/vss/_work/1/s/sdk/{service_directory}/{package_name} --next=True")[1]
             build_output = build_output.split(f"ERROR:root:{package_name} exited with linting error")
 
