8b512d847874c8e853439c8db4b16aef00cdeef1
Libba Lawrence <llawrence@microsoft.com>
2025-03-13 09:49:24 -0700

    json

--- tools/azure-sdk-tools/gh_tools/vnext_issue_creator.py
+++ tools/azure-sdk-tools/gh_tools/vnext_issue_creator.py
@@ -87,12 +87,12 @@ def get_build_info(build_link: str, check_type: CHECK_TYPE, service_directory: s
             logs_link = f"https://dev.azure.com/azure-sdk/internal/_apis/build/builds/{build_id}/logs/{log_id}?api-version=6.0"
             # Get the build info from the build link
             logs_output = requests.get(logs_link, headers=AUTH_HEADERS)
-            build_output = logs_output.text
-            return logs_output
-            build_output = build_output.split(f"next-pylint: commands[3]> python /mnt/vss/_work/1/s/eng/tox/run_pylint.py -t /mnt/vss/_work/1/s/sdk/{service_directory}/{package_name} --next=True")[1]
-            build_output = build_output.split(f"ERROR:root:{package_name} exited with linting error")
-
+            build_output = json.loads(logs_output.text)
             return build_output
+            # build_output = build_output.split(f"next-pylint: commands[3]> python /mnt/vss/_work/1/s/eng/tox/run_pylint.py -t /mnt/vss/_work/1/s/sdk/{service_directory}/{package_name} --next=True")[1]
+            # build_output = build_output.split(f"ERROR:root:{package_name} exited with linting error")
+
+            # return build_output
 
     except:
         return "Error getting build info"
