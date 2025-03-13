94be8320587ef878f4b7ab5b60535c6849a4807a
Jose Santos <jcsantos@microsoft.com>
2025-03-13 18:10:16 +0000

    updated intent_resolution evaluator logic

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_intent_resolution/_intent_resolution.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_intent_resolution/_intent_resolution.py
@@ -2,13 +2,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 import os
-from typing import Dict, Union, List
+import math
+import re
+import json
+from typing import Dict, Union, List, Optional
 
 from typing_extensions import overload, override
 
+from azure.ai.evaluation._exceptions import EvaluationException, ErrorBlame, ErrorCategory, ErrorTarget
 from azure.ai.evaluation._evaluators._common import PromptyEvaluatorBase
-from azure.ai.evaluation._model_configurations import Conversation
-
+from azure.ai.evaluation._model_configurations import Conversation, Message
 
 class IntentResolutionEvaluator(PromptyEvaluatorBase[Union[str, float]]):
     """
@@ -38,6 +41,7 @@ class IntentResolutionEvaluator(PromptyEvaluatorBase[Union[str, float]]):
 
     _PROMPTY_FILE = "intent_resolution.prompty"
     _RESULT_KEY = "intent_resolution"
+    _OPTIONAL_PARAMS = ["tool_definitions"]
 
     id = "azureml://registries/azureml/models/IntentResolution-Evaluator/versions/4"
     """Evaluator identifier, experimental and to be used only with evaluation in cloud."""
@@ -46,40 +50,27 @@ class IntentResolutionEvaluator(PromptyEvaluatorBase[Union[str, float]]):
     def __init__(self, model_config):
         current_dir = os.path.dirname(__file__)
         prompty_path = os.path.join(current_dir, self._PROMPTY_FILE)
-        print("Initializing IntentResolutionEvaluator")
+        print("Initializing IntentResolutionEvaluator2")
         super().__init__(model_config=model_config, prompty_file=prompty_path, result_key=self._RESULT_KEY)
 
     @overload
     def __call__(
         self,
         *,
-        query: str,
-        response: str,
+        query: Union[str, List[Message]],
+        response: Union[str, List[Message]],
+        tool_definitions : Optional[Union[str, List[Message]]] = None,
     ) -> Dict[str, Union[str, float]]:
         """Evaluate intent resolution for given input of query, response
 
         :keyword query: The query to be evaluated.
-        :paramtype query: str
+        :paramtype query: Union[str, List[Message]]
         :keyword response: The response to be evaluated.
-        :paramtype response: str
+        :paramtype response: Union[str, List[Message]]
+        :keyword tool_definitions: An optional list of messages containing the tools the agent is to be aware of.
+        :paramtype tool_definitions: Optional[Union[str, List[Message]]]
         :return: The intent resolution score.
         :rtype: Dict[str, float]
-        """
-
-    @overload
-    def __call__(
-        self,
-        *,
-        conversation: Conversation,
-    ) -> Dict[str, Union[float, Dict[str, List[Union[str, float]]]]]:
-        """Evaluate intent resolution for a conversation
-
-        :keyword conversation: The conversation to evaluate. Expected to contain a list of conversation turns under the
-            key "messages", and potentially a global context under the key "context". Conversation turns are expected
-            to be dictionaries with keys "content", "role", and possibly "context".
-        :paramtype conversation: Optional[~azure.ai.evaluation.Conversation]
-        :return: The intent resolution score.
-        :rtype: Dict[str, Union[float, Dict[str, List[float]]]]
         """
 
     @override
@@ -104,3 +95,56 @@ class IntentResolutionEvaluator(PromptyEvaluatorBase[Union[str, float]]):
         :rtype: Union[Dict[str, float], Dict[str, Union[float, Dict[str, List[float]]]]]
         """
         return super().__call__(*args, **kwargs)
+
+    @override
+    async def _do_eval(self, eval_input: Dict) -> Dict[str, Union[float, str]]:  # type: ignore[override]
+        """Do a relevance evaluation.
+
+        :param eval_input: The input to the evaluator. Expected to contain
+        whatever inputs are needed for the _flow method, including context
+        and other fields depending on the child class.
+        :type eval_input: Dict
+        :return: The evaluation result.
+        :rtype: Dict
+        """
+        # we override the _do_eval method as we want the output to be a dictionary, which is a different schema than _base_prompty_eval.py
+        if "query" not in eval_input and "response" not in eval_input:
+            raise EvaluationException(
+                message="Only text conversation inputs are supported.",
+                internal_message="Only text conversation inputs are supported.",
+                blame=ErrorBlame.USER_ERROR,
+                category=ErrorCategory.INVALID_VALUE,
+                target=ErrorTarget.CONVERSATION,
+            )
+        llm_output = await self._flow(timeout=self._LLM_CALL_TIMEOUT, **eval_input)
+        #print(f"Raw llm output: {llm_output}")
+        score = math.nan
+        if llm_output:
+            parsed_llm_output = {}
+            reason = ""
+            try:
+                #_chain_of_thought = r"<S0>(.*?)</S0>"
+                evaluation_pattern = r"<S1>(.*?)</S1>"
+                evaluation_match = re.findall(evaluation_pattern, llm_output, re.DOTALL)
+                if evaluation_match:
+                    parsed_llm_output = json.loads(evaluation_match[0].strip())
+                    if 'resolution_score' in parsed_llm_output:
+                        score = parsed_llm_output.get("resolution_score", math.nan)
+                    if 'explanation' in parsed_llm_output:
+                        reason = parsed_llm_output.get("explanation", "")
+            except ValueError as exc:
+                raise EvaluationException(
+                    message=f"Failed to parse model output: \n{llm_output}",
+                    internal_message="Failed to parse model output.",
+                    category=ErrorCategory.FAILED_EXECUTION,
+                    blame=ErrorBlame.SYSTEM_ERROR,
+                ) from exc
+            response_dict = {
+                            self._result_key: float(score),
+                            f"gpt_{self._result_key}": float(score),
+                            f"{self._result_key}_reason": reason,
+                            f"additional_details" : parsed_llm_output
+                        }
+            return response_dict
+        return {self._result_key: float(score), f"gpt_{self._result_key}": float(score)}
+
