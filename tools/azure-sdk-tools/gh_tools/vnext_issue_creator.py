6693f3d6852802048e195fc7cf09175013969cbf
Libba Lawrence <llawrence@microsoft.com>
2025-03-13 08:40:12 -0700

    this

--- tools/azure-sdk-tools/gh_tools/vnext_issue_creator.py
+++ tools/azure-sdk-tools/gh_tools/vnext_issue_creator.py
@@ -81,11 +81,14 @@ def get_build_info(build_link: str, check_type: CHECK_TYPE) -> str:
     add = False
     # Iterate through the timeline records to find the log ID
     for record in response_json['records']:
-        if record['parentId'] == job_id:
-            if record['log']:
-                log_id = record['log']['id'] # prep env log
-        
-                return f"https://dev.azure.com/azure-sdk/internal/_apis/build/builds/{build_id}/logs/{log_id}?api-version=6.0"
+        if record['Name'] == "Run Pylint Next":
+            add = True
+        if add:
+            if record['parentId'] == job_id:
+                if record['log']:
+                    log_id = record['log']['id'] + 1 # prep env log
+            
+                    return f"https://dev.azure.com/azure-sdk/internal/_apis/build/builds/{build_id}/logs/{log_id}?api-version=6.0"
         # if record['id'] == job_id:
         #     return record
             # try:
