91c82c9ee5e93053c73b7d00d593d782a1faf4f8
Kushagra Thapar <kuthapar@microsoft.com>
2025-03-14 16:52:07 -0700

    Added more workloads

---
+++ sdk/cosmos/azure-cosmos/test/workloads/r-w-q-workload-4.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+from r_w_q_workload import run_workload
+
+sys.path.append(r"./")
+
+import asyncio
+
+from datetime import datetime
+
+import logging
+
+if __name__ == "__main__":
+    logger = logging.getLogger('azure.cosmos')
+    file_name = os.path.basename(__file__)
+    first_name = file_name.split(".")[0]
+    file_handler = logging.FileHandler("log-" + first_name + "-" + datetime.now().strftime("%Y%m%d-%H%M%S") + '.log')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    asyncio.run(run_workload(first_name, logger))
