ac9c6c5210835e533a5b5216a7f5530e933090b9
Ankit Singhal <30610298+singankit@users.noreply.github.com>
2025-03-14 16:56:55 -0700

    Tool Call Accuracy Evaluator (#40068)
    * Tool Call Accuracy Evaluator
    
    * Review comments
    
    * Updating score key and output structure
    
    * Tool Call Accuracy Evaluator
    
    * Review comments
    
    * Updating score key and output structure
    
    * Updating prompt
    
    * Renaming parameter

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/__init__.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/__init__.py
@@ -27,6 +27,8 @@ from ._evaluators._similarity import SimilarityEvaluator
 from ._evaluators._xpia import IndirectAttackEvaluator
 from ._evaluators._code_vulnerability import CodeVulnerabilityEvaluator
 from ._evaluators._ungrounded_attributes import UngroundedAttributesEvaluator
+from ._evaluators._isa import ISAEvaluator
+from ._evaluators._tool_call_accuracy import ToolCallAccuracyEvaluator
 from ._model_configurations import (
     AzureAIProject,
     AzureOpenAIModelConfiguration,
@@ -69,4 +71,6 @@ __all__ = [
     "EvaluationResult",
     "CodeVulnerabilityEvaluator",
     "UngroundedAttributesEvaluator",
+    "ISAEvaluator",
+    "ToolCallAccuracyEvaluator",
 ]
