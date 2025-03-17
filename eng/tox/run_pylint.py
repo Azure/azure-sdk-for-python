cffb4e0d4be06c1c974a925bf61c780937f951c4
Libba Lawrence <llawrence@microsoft.com>
2025-03-17 16:25:49 -0700

    this

--- eng/tox/run_pylint.py
+++ eng/tox/run_pylint.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -16,7 +15,7 @@ import sys
 
 from ci_tools.environment_exclusions import is_check_enabled
 from ci_tools.parsing import ParsedSetup
-from ci_tools.variables import in_ci
+from ci_tools.variables import in_ci, in_analyze_weekly
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -39,6 +38,13 @@ if __name__ == "__main__":
         "--next",
         default=False,
         help="Next version of pylint is being tested.",
+        required=False,      
+    )
+
+    parser.add_argument(
+        "--vnext",
+        default=False,
+        help="Make vnext issue.",
         required=False,      
     )
 
@@ -72,5 +78,12 @@ if __name__ == "__main__":
         logging.error(
             "{} exited with linting error {}. Please see this link for more information https://aka.ms/azsdk/python/pylint-guide".format(pkg_details.name, e.returncode)
         )
+        if args.next and in_ci() and args.vnext:
+            from gh_tools.vnext_issue_creator import create_vnext_issue
+            create_vnext_issue(pkg_dir, "pylint")
+
         exit(1)
 
+    if args.next and in_ci() and args.vnext:
+        from gh_tools.vnext_issue_creator import close_vnext_issue
+        close_vnext_issue(pkg_details.name, "pylint")
