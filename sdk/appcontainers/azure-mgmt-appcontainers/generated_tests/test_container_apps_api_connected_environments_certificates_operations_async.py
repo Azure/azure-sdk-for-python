2d1f1ef4264e9072d8cb9adafd23c63cc1aef095
azure-sdk <PythonSdkPipelines>
2025-03-27 02:50:53 +0000

    code and test

--- sdk/appcontainers/azure-mgmt-appcontainers/generated_tests/test_container_apps_api_connected_environments_certificates_operations_async.py
+++ sdk/appcontainers/azure-mgmt-appcontainers/generated_tests/test_container_apps_api_connected_environments_certificates_operations_async.py
@@ -21,11 +21,11 @@ class TestContainerAppsAPIConnectedEnvironmentsCertificatesOperationsAsync(Azure
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy_async
-    async def test_list(self, resource_group):
+    async def test_connected_environments_certificates_list(self, resource_group):
         response = self.client.connected_environments_certificates.list(
             resource_group_name=resource_group.name,
             connected_environment_name="str",
-            api_version="2024-08-02-preview",
+            api_version="2025-01-01",
         )
         result = [r async for r in response]
         # please add some check logic here by yourself
@@ -33,12 +33,12 @@ class TestContainerAppsAPIConnectedEnvironmentsCertificatesOperationsAsync(Azure
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy_async
-    async def test_get(self, resource_group):
+    async def test_connected_environments_certificates_get(self, resource_group):
         response = await self.client.connected_environments_certificates.get(
             resource_group_name=resource_group.name,
             connected_environment_name="str",
             certificate_name="str",
-            api_version="2024-08-02-preview",
+            api_version="2025-01-01",
         )
 
         # please add some check logic here by yourself
@@ -46,12 +46,12 @@ class TestContainerAppsAPIConnectedEnvironmentsCertificatesOperationsAsync(Azure
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy_async
-    async def test_create_or_update(self, resource_group):
+    async def test_connected_environments_certificates_create_or_update(self, resource_group):
         response = await self.client.connected_environments_certificates.create_or_update(
             resource_group_name=resource_group.name,
             connected_environment_name="str",
             certificate_name="str",
-            api_version="2024-08-02-preview",
+            api_version="2025-01-01",
         )
 
         # please add some check logic here by yourself
@@ -59,12 +59,12 @@ class TestContainerAppsAPIConnectedEnvironmentsCertificatesOperationsAsync(Azure
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy_async
-    async def test_delete(self, resource_group):
+    async def test_connected_environments_certificates_delete(self, resource_group):
         response = await self.client.connected_environments_certificates.delete(
             resource_group_name=resource_group.name,
             connected_environment_name="str",
             certificate_name="str",
-            api_version="2024-08-02-preview",
+            api_version="2025-01-01",
         )
 
         # please add some check logic here by yourself
@@ -72,13 +72,13 @@ class TestContainerAppsAPIConnectedEnvironmentsCertificatesOperationsAsync(Azure
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy_async
-    async def test_update(self, resource_group):
+    async def test_connected_environments_certificates_update(self, resource_group):
         response = await self.client.connected_environments_certificates.update(
             resource_group_name=resource_group.name,
             connected_environment_name="str",
             certificate_name="str",
             certificate_envelope={"tags": {"str": "str"}},
-            api_version="2024-08-02-preview",
+            api_version="2025-01-01",
         )
 
         # please add some check logic here by yourself
