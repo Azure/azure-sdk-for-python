d53c2c7a9e394acfe0cb161cd91581ca80675a43
Libba Lawrence <llawrence@microsoft.com>
2025-03-28 15:28:08 -0700

    more tests

---
+++ tools/azure-sdk-tools/linting_tools/lint_test_bench/test_files/test_exception_raise_log.py
@@ -0,0 +1,17 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+import logging
+
+# This code violates do-not-log-raised-errors
+
+try:
+    a = "this is doing something here"
+except TypeError as e:
+    logging.debug(
+        "This is a TypeError: %s",
+        e,
+    )
+    raise e
+
