2ffce4031f867e1d8f84790c0739bf273022c2b1
Libba Lawrence <llawrence@microsoft.com>
2025-04-01 09:15:04 -0700

    this

--- tools/azure-sdk-tools/linting_tools/lint_test_bench/test_files/test_do_not_hardcode_conn_verify.py
+++ tools/azure-sdk-tools/linting_tools/lint_test_bench/test_files/test_do_not_hardcode_conn_verify.py
@@ -3,9 +3,14 @@
 # --------------------------------------------------------------------------------------------
 # This code violates do-not-harcode-connection-verify
 
+from azure.core import PipelineClient
+from azure.core.pipeline.transport import HttpRequest
 
-from azure.core.pipeline.transport import HttpTransport
-
-def create_client():
-    transport = HttpTransport(connection_verify=False)  # Hardcoded to False
-    return transport
+def test_do_not_hardcode_conn_verify(**kwargs):
+    client: PipelineClient = PipelineClient(base_url="endpoint", policies={}, **kwargs)
+    client._pipeline._transport.connection_verify = False
+    response = client._pipeline.run(
+        HttpRequest("GET", "https://example.com"),
+        **kwargs
+    )
+    return response
