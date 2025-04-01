33792d5a94597ccc3be45dd14ff2525bdab872b4
Libba Lawrence <llawrence@microsoft.com>
2025-04-01 09:16:43 -0700

    protected access

--- tools/azure-sdk-tools/linting_tools/lint_test_bench/test_files/test_do_not_hardcode_conn_verify.py
+++ tools/azure-sdk-tools/linting_tools/lint_test_bench/test_files/test_do_not_hardcode_conn_verify.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+# pylint: disable=protected-access
 # This code violates do-not-harcode-connection-verify
 
 from azure.core import PipelineClient
