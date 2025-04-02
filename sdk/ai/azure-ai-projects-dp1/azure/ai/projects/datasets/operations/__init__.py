a32dfc6c16b703da058239bae1ba032bd5571a23
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2025-04-02 14:58:47 -0700

    First

--- sdk/ai/azure-ai-projects-dp1/azure/ai/projects/datasets/operations/__init__.py
+++ sdk/ai/azure-ai-projects-dp1/azure/ai/projects/datasets/operations/__init__.py
@@ -12,21 +12,14 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ._patch import *  # pylint: disable=unused-wildcard-import
 
-from ._client import AIProjectClient  # type: ignore
-from ._version import VERSION
+from ._operations import DatasetsOperations  # type: ignore
 
-__version__ = VERSION
-
-try:
-    from ._patch import __all__ as _patch_all
-    from ._patch import *
-except ImportError:
-    _patch_all = []
+from ._patch import __all__ as _patch_all
+from ._patch import *
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    "AIProjectClient",
+    "DatasetsOperations",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])  # pyright: ignore
-
 _patch_sdk()
