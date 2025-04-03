53cd84085114f49e01a034fd732ab65e22acbe75
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2025-04-03 15:58:48 -0700

    Re-emit from TypeSpec. Fix deployment samples

---
+++ sdk/ai/azure-ai-projects-onedp/samples/connections/sample_connections.py
@@ -0,0 +1,56 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+"""
+DESCRIPTION:
+    Given an AIProjectClient, this sample demonstrates how to use the synchronous
+    `.connections` methods to enumerate the properties of all connections
+    and get the properties of a connection by its name.
+
+USAGE:
+    python sample_connections.py
+
+    Before running the sample:
+
+    pip install azure-ai-projects azure-identity
+
+    Set these environment variables with your own values:
+    1) PROJECT_ENDPOINT - Required. The Azure AI Project endpoint, as found in the overview page of your
+       Azure AI Foundry project.
+    2) CONNECTION_NAME - The name of a connection, as found in the "Connected resources" tab
+       in the Management Center of your AI Foundry project.
+"""
+
+import os
+from azure.identity import DefaultAzureCredential
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.projects.onedp import AIProjectClient
+from azure.ai.projects.onedp.models import ConnectionType
+
+endpoint = os.environ["PROJECT_ENDPOINT"]
+#connection_name = os.environ["CONNECTION_NAME"]
+
+with AIProjectClient(
+    endpoint=endpoint,
+    #credential=DefaultAzureCredential(),
+    credential=AzureKeyCredential(os.environ["PROJECT_API_KEY"]),
+) as project_client:
+
+    print("List the properties of all connections")
+    connections = project_client.connections.list()
+    for connection in connections:
+        print(connection)
+    exit()
+
+    print("List the properties of all connections of a particular type (in this case, Azure OpenAI connections)")
+    connections = project_client.connections.list(
+        connection_type=ConnectionType.AZURE_OPEN_AI,
+    )
+    for connection in connections:
+        print(connection)
+
+    print(f"Get the properties of a connection named {connection_name}")
+    connection = project_client.connections.get(connection_name)
+    print(connection)
