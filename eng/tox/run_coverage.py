4a03d14420b16ac3b17d349e99e5e9bd07390f84
Prashant Dhote <168401122+pdhotems@users.noreply.github.com>
2025-04-03 14:56:50 +0530

    Remove redundant logging statement in run_coverage.py

--- eng/tox/run_coverage.py
+++ eng/tox/run_coverage.py
@@ -73,8 +73,4 @@ if __name__ == "__main__":
                         f"Coverage for {pkg_details.name} is below the threshold of {cov_threshold:.2f}% (actual: {total_coverage:.2f}%)"
                     )
                     exit(1)
-                logging.info(
-                    f"Coverage for {pkg_details.name} is {total_coverage:.2f}%)"
-                )
-
 
