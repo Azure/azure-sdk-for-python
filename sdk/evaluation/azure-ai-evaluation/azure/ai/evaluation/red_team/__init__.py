cc099e745cff99e991c999cd837b6658c8f6ae93
Nagkumar Arkalgud <nagkumar@naarkalg-work-mac.local>
2025-04-04 14:20:38 -0700

    fix result structure

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/red_team/__init__.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/red_team/__init__.py
@@ -2,6 +2,11 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+try:
+    import pyrit
+except ImportError:
+    print("[INFO] Could not import Pyrit. Please install the dependency with `pip install azure-ai-evaluation[redteam]`.")
+
 from ._red_team import RedTeam
 from ._attack_strategy import AttackStrategy
 from ._attack_objective_generator import RiskCategory
