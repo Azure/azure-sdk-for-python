ce2b7f0f4dc4270fb35f78d06112fc5698e14011
Nagkumar Arkalgud <nagkumar@naarkalg-work-mac.local>
2025-04-04 14:34:00 -0700

    update test and init for red team

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/red_team/__init__.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/red_team/__init__.py
@@ -3,14 +3,13 @@
 # ---------------------------------------------------------
 
 try:
-    import pyrit
+    from ._red_team import RedTeam
+    from ._attack_strategy import AttackStrategy
+    from ._attack_objective_generator import RiskCategory
+    from ._red_team_result import RedTeamResult
 except ImportError:
     print("[INFO] Could not import Pyrit. Please install the dependency with `pip install azure-ai-evaluation[redteam]`.")
 
-from ._red_team import RedTeam
-from ._attack_strategy import AttackStrategy
-from ._attack_objective_generator import RiskCategory
-from ._red_team_result import RedTeamResult
 
 __all__ = [
     "RedTeam",
