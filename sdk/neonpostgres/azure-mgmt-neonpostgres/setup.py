f6fa1e2cc2ccf7e27da33693a18ba4a2a0a56715
azure-sdk <PythonSdkPipelines>
2025-04-07 07:55:47 +0000

    code and test

--- sdk/neonpostgres/azure-mgmt-neonpostgres/setup.py
+++ sdk/neonpostgres/azure-mgmt-neonpostgres/setup.py
@@ -22,11 +22,9 @@ namespace_name = PACKAGE_NAME.replace("-", ".")
 
 # Version extraction inspired from 'requests'
 with open(
-    (
-        os.path.join(package_folder_path, "version.py")
-        if os.path.exists(os.path.join(package_folder_path, "version.py"))
-        else os.path.join(package_folder_path, "_version.py")
-    ),
+    os.path.join(package_folder_path, "version.py")
+    if os.path.exists(os.path.join(package_folder_path, "version.py"))
+    else os.path.join(package_folder_path, "_version.py"),
     "r",
 ) as fd:
     version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
@@ -51,11 +49,10 @@ setup(
     url="https://github.com/Azure/azure-sdk-for-python",
     keywords="azure, azure sdk",  # update with search keywords relevant to the azure service / product
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -81,5 +78,5 @@ setup(
         "azure-common>=1.1",
         "azure-mgmt-core>=1.3.2",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 )
