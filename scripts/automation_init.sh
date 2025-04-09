aa5ecc439cd1dc3cb1a03c9a9401a6483626f218
Yuchao Yan <yuchaoyan@microsoft.com>
2025-04-09 13:55:36 +0800

    [SDK generation pipeline] fix pipeline failure (#40437)
    * debug
    
    * update

--- scripts/automation_init.sh
+++ scripts/automation_init.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-# install python3.10
-sudo add-apt-repository ppa:deadsnakes/ppa > /dev/null
-sudo apt-get install python3.10 > /dev/null
-sudo apt-get install python3.10-venv > /dev/null
-
 # init env
-rm -rf $TMPDIR/venv-sdk > /dev/null
-python3.10 -m venv $TMPDIR/venv-sdk > /dev/null
-VIRTUAL_ENV=$TMPDIR/venv-sdk
-export VIRTUAL_ENV
-PATH="$VIRTUAL_ENV/bin:$PATH"
-export PATH
 python -m pip install -U pip > /dev/null
 python scripts/dev_setup.py -p azure-core > /dev/null
 pip install tox==4.15.0 > /dev/null
-pip install wheel > /dev/null
+pip install wheel==0.46.1 > /dev/null
+pip install setuptools==78.1.0 > /dev/null
 
 # install tsp-client globally (local install may interfere with tooling)
 echo Install tsp-client
