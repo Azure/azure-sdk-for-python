63cd33550b53e56ccc7bbfc2f8d8f98078e5602e
Libba Lawrence <llawrence@microsoft.com>
2025-04-11 10:37:17 -0700

    this

--- tools/azure-sdk-tools/linting_tools/lint_test_bench/test_files/test_client_method_missing_tracing.py
+++ tools/azure-sdk-tools/linting_tools/lint_test_bench/test_files/test_client_method_missing_tracing.py
@@ -3,7 +3,10 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 from typing import List
+from azure.core.tracing.decorator_async import distributed_trace_async
 # This code violates client-method-has-tracing-decorator
 class Some2Client():
+
+    @distributed_trace_async
     def get_thing(self) -> List[str]:
         return []
