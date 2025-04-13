acb52add11c317aa1aaa0a90cc68c6fbf6ac5fa2
Peter Wu <weirongwu@microsoft.com>
2025-04-12 22:02:27 -0400

    Copyright header

--- sdk/storage/azure-storage-blob/tests/test_transports.py
+++ sdk/storage/azure-storage-blob/tests/test_transports.py
@@ -1,3 +1,9 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
 from azure.storage.blob import BlobClient, BlobServiceClient
 from azure.core.exceptions import ResourceExistsError
 
