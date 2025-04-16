82ffc8b7c3fe85a8cb2b812814c9c68fe33aabb9
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2025-04-16 11:59:12 -0700

    Beta 9 release of Python Azure AI Projects SDK (#40280)

--- sdk/ai/azure-ai-projects/samples/agents/sample_agents_bing_custom_search.py
+++ sdk/ai/azure-ai-projects/samples/agents/sample_agents_bing_custom_search.py
@@ -5,11 +5,12 @@
 
 """
 DESCRIPTION:
-    This sample demonstrates how to use agent operations with toolset from
+    This sample demonstrates how to use agent operations with the Bing Custom Search tool from
     the Azure Agents service using a synchronous client.
+    For more information on the Bing Custom Search tool, see: https://aka.ms/AgentCustomSearchDoc
 
 USAGE:
-    python sample_agents_run_with_toolset.py
+    python sample_agents_bing_custom_search.py
 
     Before running the sample:
 
@@ -20,40 +21,41 @@ USAGE:
        Azure AI Foundry project.
     2) MODEL_DEPLOYMENT_NAME - The deployment name of the AI model, as found under the "Name" column in 
        the "Models + endpoints" tab in your Azure AI Foundry project.
+    3) BING_CUSTOM_CONNECTION_NAME - The connection name of the Bing Custom Search connection, as found in the 
+       "Connected resources" tab in your Azure AI Foundry project.
 """
 
 import os
 from azure.ai.projects import AIProjectClient
+from azure.ai.projects.models import MessageRole, BingCustomSearchTool
 from azure.identity import DefaultAzureCredential
-from azure.ai.projects.models import FunctionTool, ToolSet, CodeInterpreterTool
-from user_functions import user_functions
+
 
 project_client = AIProjectClient.from_connection_string(
     credential=DefaultAzureCredential(),
     conn_str=os.environ["PROJECT_CONNECTION_STRING"],
 )
 
-# Create agent with toolset and process assistant run
+# [START create_agent_with_bing_custom_search_tool]
+bing_custom_connection = project_client.connections.get(connection_name=os.environ["BING_CUSTOM_CONNECTION_NAME"])
+conn_id = bing_custom_connection.id
+
+print(conn_id)
+
+# Initialize agent bing custom search tool and add the connection id
+bing_custom_tool = BingCustomSearchTool(connection_id=conn_id, instance_name="<config_instance_name>")
+
+# Create agent with the bing custom search tool and process assistant run
 with project_client:
-    # Initialize agent toolset with user functions and code interpreter
-    # [START create_agent_toolset]
-    functions = FunctionTool(user_functions)
-    code_interpreter = CodeInterpreterTool()
-
-    toolset = ToolSet()
-    toolset.add(functions)
-    toolset.add(code_interpreter)
-
-    # To enable tool calls executed automatically
-    project_client.agents.enable_auto_function_calls(toolset=toolset)
-
     agent = project_client.agents.create_agent(
         model=os.environ["MODEL_DEPLOYMENT_NAME"],
         name="my-assistant",
         instructions="You are a helpful assistant",
-        toolset=toolset,
+        tools=bing_custom_tool.definitions,
+        headers={"x-ms-enable-preview": "true"},
     )
-    # [END create_agent_toolset]
+    # [END create_agent_with_bing_custom_search_tool]
+
     print(f"Created agent, ID: {agent.id}")
 
     # Create thread for communication
@@ -63,15 +65,13 @@ with project_client:
     # Create message to thread
     message = project_client.agents.create_message(
         thread_id=thread.id,
-        role="user",
-        content="Hello, send an email with the datetime and weather information in New York?",
+        role=MessageRole.USER,
+        content="How many medals did the USA win in the 2024 summer olympics?",
     )
     print(f"Created message, ID: {message.id}")
 
     # Create and process agent run in thread with tools
-    # [START create_and_process_run]
     run = project_client.agents.create_and_process_run(thread_id=thread.id, agent_id=agent.id)
-    # [END create_and_process_run]
     print(f"Run finished with status: {run.status}")
 
     if run.status == "failed":
@@ -81,6 +81,12 @@ with project_client:
     project_client.agents.delete_agent(agent.id)
     print("Deleted agent")
 
-    # Fetch and log all messages
-    messages = project_client.agents.list_messages(thread_id=thread.id)
-    print(f"Messages: {messages}")
+    # Print the Agent's response message with optional citation
+    response_message = project_client.agents.list_messages(thread_id=thread.id).get_last_message_by_role(
+        MessageRole.AGENT
+    )
+    if response_message:
+        for text_message in response_message.text_messages:
+            print(f"Agent response: {text_message.text.value}")
+        for annotation in response_message.url_citation_annotations:
+            print(f"URL Citation: [{annotation.url_citation.title}]({annotation.url_citation.url})")
--- sdk/ai/azure-ai-projects/samples/agents/sample_agents_image_input_base64.py
+++ sdk/ai/azure-ai-projects/samples/agents/sample_agents_image_input_base64.py
@@ -5,11 +5,11 @@
 
 """
 DESCRIPTION:
-    This sample demonstrates how to use basic agent operations from
+    This sample demonstrates how to use basic agent operations using image file input for the
     the Azure Agents service using a synchronous client.
 
 USAGE:
-    python sample_agents_basics.py
+    python sample_agents_image_input_base64.py
 
     Before running the sample:
 
@@ -22,45 +22,67 @@ USAGE:
        the "Models + endpoints" tab in your Azure AI Foundry project.
 """
 
-import os, time
+import os, time, base64
+from typing import List
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
-from azure.ai.projects.models import MessageTextContent
+from azure.ai.projects.models import (
+    MessageTextContent,
+    MessageInputContentBlock,
+    MessageImageUrlParam,
+    MessageInputTextBlock,
+    MessageInputImageUrlBlock,
+)
 
-# [START create_project_client]
+
+def image_to_base64(image_path: str) -> str:
+    """
+    Convert an image file to a Base64-encoded string.
+
+    :param image_path: The path to the image file (e.g. 'image_file.png')
+    :return: A Base64-encoded string representing the image.
+    :raises FileNotFoundError: If the provided file path does not exist.
+    :raises OSError: If there's an error reading the file.
+    """
+    if not os.path.isfile(image_path):
+        raise FileNotFoundError(f"File not found at: {image_path}")
+
+    try:
+        with open(image_path, "rb") as image_file:
+            file_data = image_file.read()
+        return base64.b64encode(file_data).decode("utf-8")
+    except Exception as exc:
+        raise OSError(f"Error reading file '{image_path}'") from exc
+
+
 project_client = AIProjectClient.from_connection_string(
     credential=DefaultAzureCredential(),
     conn_str=os.environ["PROJECT_CONNECTION_STRING"],
 )
-# [END create_project_client]
 
 with project_client:
 
-    # [START create_agent]
     agent = project_client.agents.create_agent(
         model=os.environ["MODEL_DEPLOYMENT_NAME"],
         name="my-assistant",
         instructions="You are helpful assistant",
     )
-    # [END create_agent]
     print(f"Created agent, agent ID: {agent.id}")
 
-    # [START create_thread]
     thread = project_client.agents.create_thread()
-    # [END create_thread]
     print(f"Created thread, thread ID: {thread.id}")
 
-    # List all threads for the agent
-    # [START list_threads]
-    threads = project_client.agents.list_threads()
-    # [END list_threads]
-
-    # [START create_message]
-    message = project_client.agents.create_message(thread_id=thread.id, role="user", content="Hello, tell me a joke")
-    # [END create_message]
+    input_message = "Hello, what is in the image ?"
+    image_base64 = image_to_base64("image_file.png")
+    img_url = f"data:image/png;base64,{image_base64}"
+    url_param = MessageImageUrlParam(url=img_url, detail="high")
+    content_blocks: List[MessageInputContentBlock] = [
+        MessageInputTextBlock(text=input_message),
+        MessageInputImageUrlBlock(image_url=url_param),
+    ]
+    message = project_client.agents.create_message(thread_id=thread.id, role="user", content=content_blocks)
     print(f"Created message, message ID: {message.id}")
 
-    # [START create_run]
     run = project_client.agents.create_run(thread_id=thread.id, agent_id=agent.id)
 
     # Poll the run as long as run status is queued or in progress
@@ -68,13 +90,14 @@ with project_client:
         # Wait for a second
         time.sleep(1)
         run = project_client.agents.get_run(thread_id=thread.id, run_id=run.id)
-        # [END create_run]
         print(f"Run status: {run.status}")
+
+    if run.status == "failed":
+        print(f"Run failed: {run.last_error}")
 
     project_client.agents.delete_agent(agent.id)
     print("Deleted agent")
 
-    # [START list_messages]
     messages = project_client.agents.list_messages(thread_id=thread.id)
 
     # The messages are following in the reverse order,
@@ -84,4 +107,4 @@ with project_client:
         if isinstance(last_message_content, MessageTextContent):
             print(f"{data_point.role}: {last_message_content.text.value}")
 
-    # [END list_messages]
+    print(f"Messages: {messages}")
