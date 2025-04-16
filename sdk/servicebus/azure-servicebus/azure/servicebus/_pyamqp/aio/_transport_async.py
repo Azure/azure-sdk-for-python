b494ac717bb5ac97fd84c69a92308c414b2e38e7
Libba Lawrence <llawrence@microsoft.com>
2025-04-16 10:31:33 -0700

    [EH/SB/EG] next* (#40483)
    * next*
    
    * shouldnt return
    
    * pylint
    
    * this
    
    * this
    
    * tis

--- sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/aio/_transport_async.py
+++ sdk/servicebus/azure-servicebus/azure/servicebus/_pyamqp/aio/_transport_async.py
@@ -32,7 +32,7 @@
 # THE POSSIBILITY OF SUCH DAMAGE.
 # -------------------------------------------------------------------------
 
-import asyncio
+import asyncio # pylint:disable=do-not-import-asyncio
 import errno
 import socket
 import ssl
