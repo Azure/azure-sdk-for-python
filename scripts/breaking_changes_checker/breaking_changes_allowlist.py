1a073c1ee43a47fc3957af1f4db7ba47d6a0032f
Yuchao Yan <yuchaoyan@microsoft.com>
2025-04-18 10:39:32 +0800

    add suppression

--- scripts/breaking_changes_checker/breaking_changes_allowlist.py
+++ scripts/breaking_changes_checker/breaking_changes_allowlist.py
@@ -54,5 +54,7 @@ IGNORE_BREAKING_CHANGES = {
         ("AddedClassMethod", "*", "*", "values"),
         ("AddedClassMethodParameter", "*", "*", "args", "__init__"),
         ("AddedClassMethodParameter", "*", "*", "exclude_readonly", "as_dict"),
+        # changes to support multi cloud and actually it is not breaking change
+        ("ChangedParameterDefaultValue", "*", "base_url", "str", "none"),
     ]
 }
