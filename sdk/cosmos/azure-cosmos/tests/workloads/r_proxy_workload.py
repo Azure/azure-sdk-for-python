26c5c0077bc0bcbdeef4e0d49e9eda2c5d7a44d7
Allen Kim <allenkim0129@gmail.com>
2025-04-24 11:18:05 -0700

    Add excluded location configs to workload testing (#40704)
    * Added Excluded Locations
    
    * Added envoy config generator
    
    * Add template
    
    * Updated Setup Document

--- sdk/cosmos/azure-cosmos/tests/workloads/r_proxy_workload.py
+++ sdk/cosmos/azure-cosmos/tests/workloads/r_proxy_workload.py
@@ -6,10 +6,8 @@ import sys
 import aiohttp
 
 from azure.cosmos import documents
-from workload_utils import create_logger, read_item_concurrently, query_items_concurrently
-from workload_configs import COSMOS_KEY, PREFERRED_LOCATIONS, CONCURRENT_REQUESTS, COSMOS_PROXY_URI, COSMOS_CONTAINER, \
-    COSMOS_DATABASE, CONCURRENT_QUERIES
-from workload_configs import USE_MULTIPLE_WRITABLE_LOCATIONS
+from workload_utils import *
+from workload_configs import *
 
 sys.path.append(r"/")
 
@@ -17,26 +15,23 @@ from azure.cosmos.aio import CosmosClient as AsyncClient
 from azure.core.pipeline.transport import AioHttpTransport
 import asyncio
 
-from datetime import datetime
-
 async def run_workload(client_id, client_logger):
     async with aiohttp.ClientSession(trust_env=True) as proxied_aio_http_session:
         connectionPolicy = documents.ConnectionPolicy()
         connectionPolicy.UseMultipleWriteLocations = USE_MULTIPLE_WRITABLE_LOCATIONS
-
         transport = AioHttpTransport(session=proxied_aio_http_session, session_owner=False)
-        async with AsyncClient(COSMOS_PROXY_URI, COSMOS_KEY, preferred_locations=PREFERRED_LOCATIONS,
+        async with AsyncClient(COSMOS_PROXY_URI, COSMOS_KEY, connection_policy=connectionPolicy,
+                               preferred_locations=PREFERRED_LOCATIONS, excluded_locations=CLIENT_EXCLUDED_LOCATIONS,
                                enable_diagnostics_logging=True, logger=client_logger, transport=transport,
-                               user_agent=str(client_id) + "-" + datetime.now().strftime(
-                                   "%Y%m%d-%H%M%S"), connection_policy=connectionPolicy) as client:
+                               user_agent=get_user_agent(client_id)) as client:
             db = client.get_database_client(COSMOS_DATABASE)
             cont = db.get_container_client(COSMOS_CONTAINER)
             await asyncio.sleep(1)
 
             while True:
                 try:
-                    await read_item_concurrently(cont, CONCURRENT_REQUESTS)
-                    await query_items_concurrently(cont, CONCURRENT_QUERIES)
+                    await read_item_concurrently(cont, REQUEST_EXCLUDED_LOCATIONS, CONCURRENT_REQUESTS)
+                    await query_items_concurrently(cont, REQUEST_EXCLUDED_LOCATIONS, CONCURRENT_QUERIES)
                 except Exception as e:
                     client_logger.info("Exception in application layer")
                     client_logger.error(e)
