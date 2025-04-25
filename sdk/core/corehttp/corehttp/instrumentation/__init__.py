d102f363899d48ad2a72d09972b912392b94072b
Paul Van Eck <paulvaneck@microsoft.com>
2025-04-25 13:31:53 -0700

    [Corehttp] Implement tracing capabilities (#39172)
    Added native tracing capabilities with OpenTelemetry.
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>

---
+++ sdk/core/corehttp/corehttp/instrumentation/__init__.py
@@ -0,0 +1,9 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+from .tracing._tracer import get_tracer
+
+__all__ = [
+    "get_tracer",
+]
