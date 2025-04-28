5fd7695194f16891a55b1015d1ec6954223933c1
Allen Kim <allekim@microsoft.com>
2025-04-28 09:33:10 -0700

    Fixed Client Available Regions in logging policy

--- sdk/cosmos/azure-cosmos/azure/cosmos/_cosmos_http_logging_policy.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/_cosmos_http_logging_policy.py
@@ -34,9 +34,10 @@ import types
 from azure.core.pipeline import PipelineRequest, PipelineResponse
 from azure.core.pipeline.policies import HttpLoggingPolicy
 
+from ._location_cache import LocationCache
 from .http_constants import HttpHeaders
 from ._global_endpoint_manager import _GlobalEndpointManager
-from .documents import DatabaseAccount
+from .documents import DatabaseAccount, ConnectionPolicy
 
 if TYPE_CHECKING:
     from azure.core.rest import HttpRequest, HttpResponse, AsyncHttpResponse
@@ -305,19 +306,26 @@ class CosmosHttpLoggingPolicy(HttpLoggingPolicy):
 
     def __get_client_settings(self) -> Optional[Dict[str, Any]]:
         # Place any client settings we want to log here
-        client_info: Dict[str, Any] = {"Client Preferred Regions": [], "Client Available Read Regions": [],
-                                       "Client Available Write Regions": []}
+        client_preferred_regions = []
+        client_excluded_regions = []
+        client_account_read_regions = []
+        client_account_write_regions = []
+
         if self.__global_endpoint_manager:
-            client_info['Client Preferred Regions'] = self.__global_endpoint_manager.PreferredLocations \
-                if hasattr(self.__global_endpoint_manager, "PreferredLocations") else []
-            try:
-                location_cache = self.__global_endpoint_manager.location_cache
-                client_info["Client Available Read Regions"] = location_cache.available_read_locations
-                client_info["Client Available Write Regions"] = location_cache.available_write_locations
-            except AttributeError:
-                client_info["Client Available Read Regions"] = []
-                client_info["Client Available Write Regions"] = []
-        return client_info
+            if self.__global_endpoint_manager.Client and self.__global_endpoint_manager.Client.connection_policy:
+                connection_policy: ConnectionPolicy = self.__global_endpoint_manager.Client.connection_policy
+                client_preferred_regions = connection_policy.PreferredLocations
+                client_excluded_regions = connection_policy.ExcludedLocations
+
+            if self.__global_endpoint_manager.location_cache:
+                location_cache: LocationCache = self.__global_endpoint_manager.location_cache
+                client_account_read_regions = location_cache.account_read_locations
+                client_account_write_regions = location_cache.account_write_locations
+
+        return {"Client Preferred Regions": client_preferred_regions,
+                "Client Excluded Regions": client_excluded_regions,
+                "Client Account Read Regions": client_account_read_regions,
+                "Client Account Write Regions": client_account_write_regions}
 
     def __get_database_account_settings(self) -> Optional[DatabaseAccount]:
         if self.__global_endpoint_manager and hasattr(self.__global_endpoint_manager, '_database_account_cache'):
