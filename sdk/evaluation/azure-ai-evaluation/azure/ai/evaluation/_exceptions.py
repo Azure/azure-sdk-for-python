765f8805b67f834d3d19535055e6b21b556ef4b8
MilesHolland <108901744+MilesHolland@users.noreply.github.com>
2025-04-28 15:18:51 -0400

    Mar25/evals/aoai integration (#40630)
    * add typespec autogen files
    
    * initial integration
    
    * add sub grader classes
    
    * remove extra print statement
    
    * change polling interval
    
    * add name to id property dictionary
    
    * column mapping logic
    
    * Add a  that maps to pass or fail
    
    * better error handling and timeout logic
    
    * nits
    
    * Update sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_aoai/aoai_grader.py
    
    Co-authored-by: Nagkumar Arkalgud <nagkumar91@users.noreply.github.com>
    
    * default AOAI version and experimental tag
    
    * log nits
    
    * testing
    
    * recordings
    
    * rename graders
    
    * CL
    
    * cspell
    
    * more changes to c spell
    
    * fix tests
    
    * fix tests
    
    * update requirements
    
    * rename aoai to azure open ai externally
    
    ---------
    
    Co-authored-by: Nagkumar Arkalgud <nagkumar@Mac.lan>
    Co-authored-by: Nagkumar Arkalgud <nagkumar91@users.noreply.github.com>

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_exceptions.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_exceptions.py
@@ -86,6 +86,7 @@ class ErrorTarget(Enum):
     CONVERSATION = "Conversation"
     TOOL_CALL_ACCURACY_EVALUATOR = "ToolCallAccuracyEvaluator"
     RED_TEAM = "RedTeam"
+    AOAI_GRADER = "AoaiGrader"
 
 
 class EvaluationException(AzureError):
