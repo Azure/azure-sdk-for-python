e7f32f938d438b2ceaceca373724d010f9490fc0
Xiang Yan <xiangsjtu@gmail.com>
2025-04-29 15:14:40 -0700

    update

--- sdk/search/azure-search-documents/azure/search/documents/indexes/_generated/models/_models_py3.py
+++ sdk/search/azure-search-documents/azure/search/documents/indexes/_generated/models/_models_py3.py
@@ -1785,7 +1785,7 @@ class ChatCompletionSchema(_serialization.Model):
         self.type = type
         self.properties = properties
         self.required = required
-        self.additional_properties = additional_properties
+        self.additional_properties = additional_properties  # type: ignore
 
 
 class WebApiSkill(SearchIndexerSkill):
