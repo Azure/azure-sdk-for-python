3882d827519736b2a0f924fce14d9e0a6ef44565
Waqas Javed <7674577+w-javed@users.noreply.github.com>
2025-04-29 01:17:41 -0700

    CI Fix

--- sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_mass_evaluate.py
+++ sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_mass_evaluate.py
@@ -225,19 +225,7 @@ class TestMassEvaluate:
         assert metrics["qa.similarity"] >= 0
         assert metrics["qa.gpt_similarity"] >= 0
 
-    @pytest.mark.parametrize(
-        ("proj_scope", "cred", "conv", "config"),
-        (
-            ("project_scope", "azure_cred", "data_convo_file", "model_config"),
-            ("project_scope_onedp", "azure_cred_onedp", "data_convo_file", "model_config"),
-        )
-    )
-    def test_evaluate_conversation(self, request, proj_scope, cred, conv, config):
-        project_scope = request.getfixturevalue(proj_scope)
-        azure_cred = request.getfixturevalue(cred)
-        model_config = request.getfixturevalue(config)
-        data_convo_file = request.getfixturevalue(conv)
-        
+    def test_evaluate_conversation(self, model_config, data_convo_file, azure_cred, project_scope):
         evaluators = {
             "grounded": GroundednessEvaluator(model_config),
             "coherence": CoherenceEvaluator(model_config),
