e79bdea823c2f262b9cee1852d29dc19761f2b6f
Ralph <18542438+ralph-msft@users.noreply.github.com>
2025-05-01 16:50:15 -0700

    Add back support for Entra ID auth with Prompty files (#40838)
    - Adds an async credential provider that supports multi-cloud as per the Promptflow Prompty implementation
    - Adds a client to query Azure cloud endpoint metadata which is a streamlined version of the code from Promptflow, and azure-ai-ml
    - Moves TokenScope to be shared and adds entries for Cognitive Services, and AI ML. Updates the rest of the code to use this moved enum
    - Adds support for the `AI_EVALS_USE_PF_PROMPTY` environment variable which when set to `true` will go back to using the legacy Promptflow Prompty implementation

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_legacy/prompty/_connection.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_legacy/prompty/_connection.py
@@ -3,40 +3,16 @@
 # ---------------------------------------------------------
 
 import os
-import re
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any, ClassVar, Mapping, Optional, Set, Union
+from typing import Any, ClassVar, Mapping, Optional
 
 from azure.ai.evaluation._legacy.prompty._exceptions import MissingRequiredInputError
 from azure.ai.evaluation._legacy.prompty._utils import dataclass_from_dict
 
 
-ENV_VAR_PATTERN = re.compile(r"^\$\{env:(.*)\}$")
-
-
-def _parse_environment_variable(value: Union[str, Any]) -> Union[str, Any]:
-    """Get environment variable from ${env:ENV_NAME}. If not found, return original value.
-
-    :param value: The value to parse.
-    :type value: str | Any
-    :return: The parsed value
-    :rtype: str | Any"""
-    if not isinstance(value, str):
-        return value
-
-    result = re.match(ENV_VAR_PATTERN, value)
-    if result:
-        env_name = result.groups()[0]
-        return os.environ.get(env_name, value)
-
-    return value
-
-
 def _is_empty_connection_config(connection_dict: Mapping[str, Any]) -> bool:
-    ignored_fields = set(["azure_deployment", "model"])
-    keys = {k for k, v in connection_dict.items() if v}
-    return len(keys - ignored_fields) == 0
+    return any(key not in {"azure_deployment", "model", "type"} for key in connection_dict.keys())
 
 
 @dataclass
@@ -52,16 +28,6 @@ class Connection(ABC):
         :rtype: str"""
         ...
 
-    @abstractmethod
-    def is_valid(self, missing_fields: Optional[Set[str]] = None) -> bool:
-        """Check if the connection is valid.
-
-        :param missing_fields: If set, this will be populated with the missing required fields.
-        :type missing_fields: Set[str] | None
-        :return: True if the connection is valid, False otherwise.
-        :rtype: bool"""
-        ...
-
     @staticmethod
     def parse_from_config(model_configuration: Mapping[str, Any]) -> "Connection":
         """Parse a connection from a model configuration.
@@ -71,18 +37,18 @@ class Connection(ABC):
         :return: The connection.
         :rtype: Connection
         """
-        connection: Connection
-        connection_dict = {k: _parse_environment_variable(v) for k, v in model_configuration.items()}
+        connection_dict = {**model_configuration}
         connection_type = connection_dict.pop("type", "")
 
+        connection: Connection
         if connection_type in [AzureOpenAIConnection.TYPE, "azure_openai"]:
-            if _is_empty_connection_config(connection_dict):
+            if not _is_empty_connection_config(connection_dict):
                 connection = AzureOpenAIConnection.from_env()
             else:
                 connection = dataclass_from_dict(AzureOpenAIConnection, connection_dict)
 
         elif connection_type in [OpenAIConnection.TYPE, "openai"]:
-            if _is_empty_connection_config(connection_dict):
+            if not _is_empty_connection_config(connection_dict):
                 connection = OpenAIConnection.from_env()
             else:
                 connection = dataclass_from_dict(OpenAIConnection, connection_dict)
@@ -94,13 +60,6 @@ class Connection(ABC):
             )
             raise MissingRequiredInputError(error_message)
 
-        missing_fields: Set[str] = set()
-        if not connection.is_valid(missing_fields):
-            raise MissingRequiredInputError(
-                f"The following required fields are missing for connection {connection.type}: "
-                f"{', '.join(missing_fields)}"
-            )
-
         return connection
 
 
@@ -109,6 +68,7 @@ class OpenAIConnection(Connection):
     """Connection class for OpenAI endpoints."""
 
     base_url: str
+    model: str
     api_key: Optional[str] = None
     organization: Optional[str] = None
 
@@ -122,20 +82,10 @@ class OpenAIConnection(Connection):
     def from_env(cls) -> "OpenAIConnection":
         return cls(
             base_url=os.environ.get("OPENAI_BASE_URL", ""),
+            model=os.environ.get("OPENAI_MODEL", ""),
             api_key=os.environ.get("OPENAI_API_KEY"),
             organization=os.environ.get("OPENAI_ORG_ID"),
         )
-
-    def is_valid(self, missing_fields: Optional[Set[str]] = None) -> bool:
-        if missing_fields is None:
-            missing_fields = set()
-        if not self.base_url:
-            missing_fields.add("base_url")
-        if not self.api_key:
-            missing_fields.add("api_key")
-        if not self.organization:
-            missing_fields.add("organization")
-        return not bool(missing_fields)
 
 
 @dataclass
@@ -143,8 +93,8 @@ class AzureOpenAIConnection(Connection):
     """Connection class for Azure OpenAI endpoints."""
 
     azure_endpoint: str
-    api_key: Optional[str] = None  # TODO ralphe: Replace this TokenCredential to allow for more flexible authentication
-    azure_deployment: Optional[str] = None
+    azure_deployment: str
+    api_key: Optional[str] = None
     api_version: Optional[str] = None
     resource_id: Optional[str] = None
 
@@ -158,8 +108,8 @@ class AzureOpenAIConnection(Connection):
     def from_env(cls) -> "AzureOpenAIConnection":
         return cls(
             azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT", ""),
+            azure_deployment=os.environ.get("AZURE_OPENAI_DEPLOYMENT", ""),
             api_key=os.environ.get("AZURE_OPENAI_API_KEY"),
-            azure_deployment=os.environ.get("AZURE_OPENAI_DEPLOYMENT"),
             api_version=os.environ.get("AZURE_OPENAI_API_VERSION", "2024-02-01"),
         )
 
@@ -167,16 +117,3 @@ class AzureOpenAIConnection(Connection):
         # set default API version
         if not self.api_version:
             self.api_version = "2024-02-01"
-
-    def is_valid(self, missing_fields: Optional[Set[str]] = None) -> bool:
-        if missing_fields is None:
-            missing_fields = set()
-        if not self.azure_endpoint:
-            missing_fields.add("azure_endpoint")
-        if not self.api_key:
-            missing_fields.add("api_key")
-        if not self.azure_deployment:
-            missing_fields.add("azure_deployment")
-        if not self.api_version:
-            missing_fields.add("api_version")
-        return not bool(missing_fields)
