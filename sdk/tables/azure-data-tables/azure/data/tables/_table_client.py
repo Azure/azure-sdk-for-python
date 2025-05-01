1e1ab4ae4d95d8bf4415854a801dccf7b71de6e5
Paul Van Eck <paulvaneck@microsoft.com>
2025-05-01 14:16:32 -0700

    [Tables] Add audience keyword argument support (#40487)
    This enables sovereign cloud support.
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>
    Co-authored-by: Laurent Mazuel <laurent.mazuel@gmail.com>

--- sdk/tables/azure-data-tables/azure/data/tables/_table_client.py
+++ sdk/tables/azure-data-tables/azure/data/tables/_table_client.py
@@ -17,7 +17,7 @@ from azure.core.tracing.decorator import distributed_trace
 from ._common_conversion import _prepare_key, _return_headers_and_deserialized, _trim_service_metadata
 from ._encoder import TableEntityEncoder, EncoderMapType
 from ._decoder import TableEntityDecoder, deserialize_iso, DecoderMapType
-from ._base_client import parse_connection_str, TablesBaseClient
+from ._base_client import parse_connection_str, TablesBaseClient, AudienceType
 from ._entity import TableEntity
 from ._error import (
     _decode_error,
@@ -73,6 +73,7 @@ class TableClient(TablesBaseClient):
         table_name: str,
         *,
         credential: Optional[Union[AzureSasCredential, AzureNamedKeyCredential, TokenCredential]] = None,
+        audience: Optional[AudienceType] = None,
         api_version: Optional[str] = None,
         encoder_map: Optional[EncoderMapType] = None,
         decoder_map: Optional[DecoderMapType] = None,
@@ -91,6 +92,9 @@ class TableClient(TablesBaseClient):
             ~azure.core.credentials.AzureNamedKeyCredential or
             ~azure.core.credentials.AzureSasCredential or
             ~azure.core.credentials.TokenCredential or None
+        :keyword audience: Optional audience to use for Microsoft Entra ID authentication. If not specified,
+            the public cloud audience will be used.
+        :paramtype audience: str or None
         :keyword api_version: Specifies the version of the operation to use for this request. Default value
             is "2019-02-02".
         :paramtype api_version: str or None
@@ -112,7 +116,9 @@ class TableClient(TablesBaseClient):
         self.table_name: str = table_name
         self.encoder = TableEntityEncoder(convert_map=encoder_map)
         self.decoder = TableEntityDecoder(convert_map=decoder_map, flatten_result_entity=flatten_result_entity)
-        super(TableClient, self).__init__(endpoint, credential=credential, api_version=api_version, **kwargs)
+        super(TableClient, self).__init__(
+            endpoint, credential=credential, api_version=api_version, audience=audience, **kwargs
+        )
 
     @classmethod
     def from_connection_string(cls, conn_str: str, table_name: str, **kwargs: Any) -> "TableClient":
