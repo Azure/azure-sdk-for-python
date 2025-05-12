304dc5bf8785eeeeb86b71fadeb2e29f8711948d
Peter Wu <weirongwu@microsoft.com>
2025-05-12 13:37:54 -0400

    [Async] _lease_async.pyi

--- sdk/storage/azure-storage-blob/azure/storage/blob/_lease.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/_lease.py
@@ -20,7 +20,7 @@ if TYPE_CHECKING:
     from datetime import datetime
 
 
-class BlobLeaseClient(): # pylint: disable=client-accepts-api-version-keyword
+class BlobLeaseClient:  # pylint: disable=client-accepts-api-version-keyword
     """Creates a new BlobLeaseClient.
 
     This client provides lease operations on a BlobClient or ContainerClient.
@@ -41,7 +41,7 @@ class BlobLeaseClient(): # pylint: disable=client-accepts-api-version-keyword
     """The last modified timestamp of the lease currently being maintained.
     This will be `None` if no lease has yet been acquired or modified."""
 
-    def __init__( # pylint: disable=missing-client-constructor-parameter-credential, missing-client-constructor-parameter-kwargs
+    def __init__(  # pylint: disable=missing-client-constructor-parameter-credential, missing-client-constructor-parameter-kwargs
         self, client: Union["BlobClient", "ContainerClient"],
         lease_id: Optional[str] = None
     ) -> None:
