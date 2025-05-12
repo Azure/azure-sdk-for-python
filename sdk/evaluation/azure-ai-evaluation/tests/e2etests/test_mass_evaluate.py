2a2a86364730b7ac3c50a22d6d2d95ad57cf2b50
Waqas Javed <7674577+w-javed@users.noreply.github.com>
2025-05-12 15:49:42 -0700

    enable singleton test with asset

--- sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_mass_evaluate.py
+++ sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_mass_evaluate.py
@@ -239,7 +239,7 @@ class TestMassEvaluate:
         ("proj_scope", "cred", "conv", "m_config"),
         (
             ("project_scope", "azure_cred", "data_convo_file", "model_config"),
-            # ("project_scope_onedp", "azure_cred_onedp", "data_convo_file", "model_config_onedp"),
+            ("project_scope_onedp", "azure_cred_onedp", "data_convo_file", "model_config_onedp"),
         )
     )
     def test_evaluate_conversation(self, request, proj_scope, cred, conv, m_config):
