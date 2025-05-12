596d1489c0d1d90b7628a6a3f19b5b6a00e59a58
Peter Wu <weirongwu@microsoft.com>
2025-05-12 12:59:21 -0400

    [Sync] _lease.pyi

---
+++ sdk/storage/azure-storage-blob/azure/storage/blob/_lease.pyi
@@ -0,0 +1,78 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+# pylint: disable=unused-import
+
+from datetime import datetime
+from typing import Any, Optional, Union, TYPE_CHECKING
+
+from azure.core import MatchConditions
+from azure.core.tracing.decorator import distributed_trace
+from ._blob_client import BlobClient
+from ._container_client import ContainerClient
+
+class BlobLeaseClient:  # pylint: disable=client-accepts-api-version-keyword
+    def __init__(self, client: Union[BlobClient, ContainerClient], lease_id: Optional[str] = None) -> None: ...
+    @distributed_trace
+    def acquire(
+        self,
+        lease_duration: int = -1,
+        *,
+        if_modified_since: Optional[datetime] = None,
+        if_unmodified_since: Optional[datetime] = None,
+        etag: Optional[str] = None,
+        match_condition: Optional[MatchConditions] = None,
+        if_tags_match_condition: Optional[str] = None,
+        timeout: Optional[int] = None,
+        **kwargs: Any
+    ) -> None: ...
+    @distributed_trace
+    def renew(
+        self,
+        *,
+        if_modified_since: Optional[datetime] = None,
+        if_unmodified_since: Optional[datetime] = None,
+        etag: Optional[str] = None,
+        match_condition: Optional[MatchConditions] = None,
+        if_tags_match_condition: Optional[str] = None,
+        timeout: Optional[int] = None,
+        **kwargs: Any
+    ) -> None: ...
+    @distributed_trace
+    def release(
+        self,
+        *,
+        if_modified_since: Optional[datetime] = None,
+        if_unmodified_since: Optional[datetime] = None,
+        etag: Optional[str] = None,
+        match_condition: Optional[MatchConditions] = None,
+        if_tags_match_condition: Optional[str] = None,
+        timeout: Optional[int] = None,
+        **kwargs: Any
+    ) -> None: ...
+    @distributed_trace
+    def change(
+        self,
+        proposed_lease_id: str,
+        *,
+        if_modified_since: Optional[datetime] = None,
+        if_unmodified_since: Optional[datetime] = None,
+        etag: Optional[str] = None,
+        match_condition: Optional[MatchConditions] = None,
+        if_tags_match_condition: Optional[str] = None,
+        timeout: Optional[int] = None,
+        **kwargs: Any
+    ) -> None: ...
+    @distributed_trace
+    def break_lease(
+        self,
+        lease_break_period: Optional[int] = None,
+        *,
+        if_modified_since: Optional[datetime] = None,
+        if_unmodified_since: Optional[datetime] = None,
+        if_tags_match_condition: Optional[str] = None,
+        timeout: Optional[int] = None,
+        **kwargs: Any
+    ) -> int: ...
