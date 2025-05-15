69d49df45f3bee944154ab5868c4264e6c128d58
jhakulin <jhakulin@microsoft.com>
2025-05-15 09:42:39 -0700

    update PROJECT_ENDPOINT description to samples

--- sdk/ai/azure-ai-agents/samples/agents_async/sample_agents_json_schema_async.py
+++ sdk/ai/azure-ai-agents/samples/agents_async/sample_agents_json_schema_async.py
@@ -14,8 +14,11 @@ USAGE:
 
     pip install azure-ai-agents azure-identity pydantic
 
-    Set this environment variables with your own values:
-    PROJECT_ENDPOINT - the Azure AI Agents endpoint.
+    Set these environment variables with your own values:
+    1) PROJECT_ENDPOINT - The Azure AI Project endpoint, as found in the Overview 
+                          page of your Azure AI Foundry portal.
+    2) MODEL_DEPLOYMENT_NAME - The deployment name of the AI model, as found under the "Name" column in 
+       the "Models + endpoints" tab in your Azure AI Foundry project.
 """
 
 import asyncio
--- sdk/ai/azure-ai-agents/samples/agents_tools/sample_agents_logic_apps.py
+++ sdk/ai/azure-ai-agents/samples/agents_tools/sample_agents_logic_apps.py
@@ -23,7 +23,8 @@ USAGE:
     pip install azure-ai-agents azure-identity azure-mgmt-logic
 
     Set this environment variables with your own values:
-    1) PROJECT_ENDPOINT - the Azure AI Agents endpoint.
+    1) PROJECT_ENDPOINT - The Azure AI Project endpoint, as found in the Overview 
+                          page of your Azure AI Foundry portal.
     2) MODEL_DEPLOYMENT_NAME - The deployment name of the AI model, as found under the "Name" column in 
        the "Models + endpoints" tab in your Azure AI Foundry project.
 
