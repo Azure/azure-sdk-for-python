995892cba5e5d2e876093efbd5d1dcba633ab634
bambriz <beambriz2017@gmail.com>
2025-05-15 18:39:27 -0700

    Update test_cosmos_http_logging_policy_async.py

--- sdk/cosmos/azure-cosmos/tests/test_cosmos_http_logging_policy_async.py
+++ sdk/cosmos/azure-cosmos/tests/test_cosmos_http_logging_policy_async.py
@@ -157,8 +157,8 @@ class TestCosmosHttpLoggerAsync(unittest.IsolatedAsyncioTestCase):
                                                                      logger=self.logger,
                                                                      enable_diagnostics_logging=True)
         # Test if we can log errors with the filtered diagnostics logger
-        database_id = "database_test-" + str(uuid.uuid4())
-        container_id = "container_test-" + str(uuid.uuid4())
+        database_id = "database_test_" + str(uuid.uuid4())
+        container_id = "diagnostics_container_test_" + str(uuid.uuid4())
         await self.client_filtered_diagnostic.create_database(id=database_id)
         database = self.client_filtered_diagnostic.get_database_client(database_id)
         await database.create_container(id=container_id, partition_key=PartitionKey(path="/pk"))
