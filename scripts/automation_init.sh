fec45d1df495bb40cb8889fb8e23c75db812701a
Yuchao Yan <yuchaoyan@microsoft.com>
2025-05-20 16:14:06 +0800

    update for wheel

--- scripts/automation_init.sh
+++ scripts/automation_init.sh
@@ -4,7 +4,7 @@
 python -m pip install -U pip > /dev/null
 python scripts/dev_setup.py -p azure-core > /dev/null
 pip install tox==4.15.0 > /dev/null
-pip install wheel==0.46.1 > /dev/null
+pip install wheel==0.45.1 > /dev/null
 pip install setuptools==78.1.0 > /dev/null
 
 # install tsp-client globally (local install may interfere with tooling)
