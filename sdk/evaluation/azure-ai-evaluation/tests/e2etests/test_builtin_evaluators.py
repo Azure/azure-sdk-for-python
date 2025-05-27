fa1c426da3a31ff440e8419611c9db4d2656aa42
Ankit Singhal <anksing@microsoft.com>
2025-05-27 14:36:29 -0700

    Adding tests

--- sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_builtin_evaluators.py
+++ sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_builtin_evaluators.py
@@ -83,6 +83,12 @@ class TestBuiltInEvaluators:
             (RougeType.ROUGE_4),
             (RougeType.ROUGE_5),
             (RougeType.ROUGE_L),
+            (RougeType.ROUGE_1.value),
+            (RougeType.ROUGE_2.value),
+            (RougeType.ROUGE_3.value),
+            (RougeType.ROUGE_4.value),
+            (RougeType.ROUGE_5.value),
+            (RougeType.ROUGE_L.value),
         ],
     )
     def test_math_evaluator_rouge_score(self, rouge_type):
