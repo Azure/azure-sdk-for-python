286c918fbb554dd5527cc210e9c2f49e0dd5725c
ripark <ripark@microsoft.com>
2025-05-27 22:21:22 +0000

    - Remove unused imports - Remove unused args, suggested by @swathipil

--- sdk/eventhub/azure-eventhub/tests/livetest/synctests/test_faultinjector.py
+++ sdk/eventhub/azure-eventhub/tests/livetest/synctests/test_faultinjector.py
@@ -1,27 +1,26 @@
-import logging
 import ssl
-import time
-import pytest
 import threading
-import sys
+import pytest
 
 from azure.eventhub import EventData
 from azure.eventhub import EventHubConsumerClient
-from azure.eventhub._eventprocessor.in_memory_checkpoint_store import (
-    InMemoryCheckpointStore,
-)
-from azure.eventhub._constants import ALL_PARTITIONS
 
 
 @pytest.mark.liveTest
-def test_receive_partition_using_fault_injector(auth_credential_senders, uamqp_transport, faultinjector_detach_after_delay, client_args):
+def test_receive_partition_using_fault_injector_detach_after_delay(
+    auth_credential_senders,
+    faultinjector_detach_after_delay,
+):
     fault_injector_config = faultinjector_detach_after_delay
 
     if not fault_injector_config:
         pytest.skip(
-            "Fault injector not enabled. See conftest.py::faultinjector for requirements.")
+            "Fault injector not enabled. See conftest.py::faultinjector for requirements."
+        )
 
-    fully_qualified_namespace, eventhub_name, credential, senders = auth_credential_senders
+    fully_qualified_namespace, eventhub_name, credential, senders = (
+        auth_credential_senders
+    )
 
     senders[0].send(EventData("Test EventData"))
 
@@ -43,7 +42,6 @@ def test_receive_partition_using_fault_injector(auth_credential_senders, uamqp_t
         custom_endpoint_address=fault_injector_config["custom_endpoint_address"],
         ssl_context=context,
         retry_total=0,
-        **client_args,
     )
 
     def on_event(partition_context, _):
