d05f14697897285d5058489c462a63a35170856c
Cezara Mateiasi <CEMATEIA@microsoft.com>
2025-05-28 15:43:11 +0200

    Update entra_token_guard_policy.py

--- sdk/communication/azure-communication-identity/azure/communication/identity/_shared/entra_token_guard_policy.py
+++ sdk/communication/azure-communication-identity/azure/communication/identity/_shared/entra_token_guard_policy.py
@@ -19,8 +19,8 @@ class EntraTokenGuardPolicy(HTTPPolicy):
         self._response_cache = None
 
     def send(self, request: PipelineRequest):
-        cache_valid, token = self._is_entra_token_cache_valid(request)
-        if cache_valid and self._is_acs_token_cache_valid():
+        cache_valid, token = _EntraTokenGuardUtils.is_entra_token_cache_valid(self._entra_token_cache, request)
+        if cache_valid and _EntraTokenGuardUtils.is_acs_token_cache_valid(self._response_cache):
             response = self._response_cache
         else:
             self._entra_token_cache = token
@@ -30,33 +30,6 @@ class EntraTokenGuardPolicy(HTTPPolicy):
             raise RuntimeError("Failed to obtain a valid PipelineResponse in AsyncEntraTokenGuardPolicy.send")
         return response
 
-    def _is_entra_token_cache_valid(self, request):
-        current_entra_token = request.http_request.headers.get("Authorization", "")
-        cache_valid = (
-                self._entra_token_cache is not None and
-                current_entra_token == self._entra_token_cache
-        )
-        return cache_valid, current_entra_token
-
-    def _is_acs_token_cache_valid(self):
-        if self._response_cache is None or self._response_cache.http_response.status_code != 200:
-            return False
-        return self._is_access_token_valid()
-
-    def _is_access_token_valid(self):
-        try:
-            if self._response_cache is None or self._response_cache.http_response is None:
-                return False
-            content = self._response_cache.http_response.text()
-            data = json.loads(content)
-            expires_on = data["accessToken"]["expiresOn"]
-            if isinstance(expires_on, int):
-                expires_on_dt = datetime.fromtimestamp(expires_on, tz=timezone.utc)
-            else:
-                expires_on_dt = datetime.fromisoformat(expires_on)
-            return datetime.now(timezone.utc) < expires_on_dt
-        except Exception:
-            return False
 
 
 class AsyncEntraTokenGuardPolicy(AsyncHTTPPolicy):
@@ -68,8 +41,8 @@ class AsyncEntraTokenGuardPolicy(AsyncHTTPPolicy):
         self._response_cache = None
 
     async def send(self, request: PipelineRequest):
-        cache_valid, token = self._is_entra_token_cache_valid(request)
-        if cache_valid and self._is_acs_token_cache_valid():
+        cache_valid, token = _EntraTokenGuardUtils.is_entra_token_cache_valid(self._entra_token_cache, request)
+        if cache_valid and _EntraTokenGuardUtils.is_acs_token_cache_valid(self._response_cache):
             response = self._response_cache
         else:
             self._entra_token_cache = token
@@ -78,31 +51,39 @@ class AsyncEntraTokenGuardPolicy(AsyncHTTPPolicy):
         if response is None:
             raise RuntimeError("Failed to obtain a valid PipelineResponse in AsyncEntraTokenGuardPolicy.send")
         return response
+    
+    
 
-    def _is_entra_token_cache_valid(self, request):
-        current_entra_token = request.http_request.headers.get("Authorization", "")
-        cache_valid = (
-                self._entra_token_cache is not None and
-                current_entra_token == self._entra_token_cache
-        )
-        return cache_valid, current_entra_token
+class _EntraTokenGuardUtils:
+        @staticmethod
+        def is_entra_token_cache_valid(entra_token_cache, request):
+            current_entra_token = request.http_request.headers.get("Authorization", "")
+            cache_valid = (
+                entra_token_cache is not None and
+                current_entra_token == entra_token_cache
+            )
+            return cache_valid, current_entra_token
 
-    def _is_acs_token_cache_valid(self):
-        if self._response_cache is None or self._response_cache.http_response.status_code != 200:
-            return False
-        return self._is_access_token_valid()
-
-    def _is_access_token_valid(self):
-        try:
-            if self._response_cache is None or self._response_cache.http_response is None:
+        @staticmethod
+        def is_acs_token_cache_valid(response_cache):
+            if response_cache is None or response_cache.http_response.status_code != 200:
                 return False
-            content = self._response_cache.http_response.text()
-            data = json.loads(content)
-            expires_on = data["accessToken"]["expiresOn"]
-            if isinstance(expires_on, int):
-                expires_on_dt = datetime.fromtimestamp(expires_on, tz=timezone.utc)
-            else:
-                expires_on_dt = datetime.fromisoformat(expires_on)
-            return datetime.now(timezone.utc) < expires_on_dt
-        except Exception:
-            return False
+            return _EntraTokenGuardUtils.is_access_token_valid(response_cache)
+
+        @staticmethod
+        def is_access_token_valid(response_cache):
+            try:
+                if response_cache is None or response_cache.http_response is None:
+                    return False
+                content = response_cache.http_response.text()
+                data = json.loads(content)
+                expires_on = data["accessToken"]["expiresOn"]
+                if isinstance(expires_on, int):
+                    expires_on_dt = datetime.fromtimestamp(expires_on, tz=timezone.utc)
+                else:
+                    expires_on_dt = datetime.fromisoformat(expires_on)
+                return datetime.now(timezone.utc) < expires_on_dt
+            except Exception:
+                return False
+
+        
