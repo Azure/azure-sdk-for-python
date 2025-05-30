fdbbd753fe10b2d6a7fa82eb40b6a434091d735b
Josiah Vinson <88204686+jovinson-ms@users.noreply.github.com>
2025-05-30 16:45:07 -0700

    Updating README based on feedback (#41371)
    Co-authored-by: Josiah Vinson <jovinson@microsoft.com>

--- sdk/healthdataaiservices/azure-health-deidentification/samples/examples.py
+++ sdk/healthdataaiservices/azure-health-deidentification/samples/examples.py
@@ -1,18 +1,21 @@
-from azure.core.exceptions import AzureError
+# [START create_client]
 from azure.health.deidentification import DeidentificationClient
-from azure.health.deidentification.models import (
-    DeidentificationContent,
-)
 from azure.identity import DefaultAzureCredential
 import os
 
-# [START create_client]
+
 endpoint = os.environ["AZURE_HEALTH_DEIDENTIFICATION_ENDPOINT"]
 credential = DefaultAzureCredential()
 client = DeidentificationClient(endpoint, credential)
 # [END create_client]
 
 # [START handle_error]
+from azure.core.exceptions import AzureError
+from azure.health.deidentification.models import (
+    DeidentificationContent,
+)
+
+
 error_client = DeidentificationClient("https://contoso.deid.azure.com", credential)
 body = DeidentificationContent(input_text="Hello, I'm Dr. John Smith.")
 
