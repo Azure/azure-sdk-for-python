ddf5b15ee7e7d94983ede4e37f0ead3ea6ccafd1
Cezara Mateiasi <CEMATEIA@microsoft.com>
2025-06-03 01:14:28 +0200

    try to fix conflict in pipeline 2

--- sdk/communication/azure-communication-phonenumbers/setup.py
+++ sdk/communication/azure-communication-phonenumbers/setup.py
@@ -68,6 +68,7 @@ setup(
     install_requires=[
         "msrest>=0.7.1",
         "azure-core<2.0.0,>=1.28.0",
+        "azure-core>=1.23.0"
     ],
     extras_require={":python_version<'3.8'": ["typing-extensions"]},
     project_urls={
