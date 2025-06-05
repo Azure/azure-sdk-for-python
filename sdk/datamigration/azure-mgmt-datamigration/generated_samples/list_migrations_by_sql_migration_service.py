34a9f84a2932beedf538b627be6863ff2345452d
azure-sdk <PythonSdkPipelines>
2025-06-05 08:28:16 +0000

    code and test

--- sdk/datamigration/azure-mgmt-datamigration/generated_samples/list_migrations_by_sql_migration_service.py
+++ sdk/datamigration/azure-mgmt-datamigration/generated_samples/list_migrations_by_sql_migration_service.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -7,6 +8,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.datamigration import DataMigrationManagementClient
 
 """
@@ -14,7 +16,7 @@ from azure.mgmt.datamigration import DataMigrationManagementClient
     pip install azure-identity
     pip install azure-mgmt-datamigration
 # USAGE
-    python get_monitor_data_migration_service.py
+    python list_migrations_by_sql_migration_service.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -29,13 +31,14 @@ def main():
         subscription_id="00000000-1111-2222-3333-444444444444",
     )
 
-    response = client.sql_migration_services.list_monitoring_data(
+    response = client.sql_migration_services.list_migrations(
         resource_group_name="testrg",
         sql_migration_service_name="service1",
     )
-    print(response)
+    for item in response:
+        print(item)
 
 
-# x-ms-original-file: specification/datamigration/resource-manager/Microsoft.DataMigration/preview/2022-03-30-preview/examples/GetMonitorDataMigrationService.json
+# x-ms-original-file: specification/datamigration/resource-manager/Microsoft.DataMigration/preview/2025-03-15-preview/examples/ListMigrationsBySqlMigrationService.json
 if __name__ == "__main__":
     main()
