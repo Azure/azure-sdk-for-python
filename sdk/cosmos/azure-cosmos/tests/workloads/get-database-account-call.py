11d08b9d8466ab40d930ab8ea6ad40e8149f0451
tvaron3 <tomas.varon1802@gmail.com>
2025-06-05 19:39:55 -0700

    revert change

--- sdk/cosmos/azure-cosmos/tests/workloads/get-database-account-call.py
+++ sdk/cosmos/azure-cosmos/tests/workloads/get-database-account-call.py
@@ -15,22 +15,23 @@ import time
 from datetime import datetime
 
 
-async def run_workload(client_id: str, logger):
+async def run_workload(client_id: str):
     async with AsyncClient(COSMOS_URI, COSMOS_KEY, preferred_locations=PREFERRED_LOCATIONS,
                            enable_diagnostics_logging=True, logger=logger,
                            user_agent=client_id + "-" + datetime.now().strftime(
                                "%Y%m%d-%H%M%S")) as client:
         await asyncio.sleep(1)
-        endpoints_to_test = ["https://tomasvaron-full-fidelity.documents.azure.com:443/",
-                             "https://tomasvaron-full-fidelity-westus3.documents.azure.com:443/",
-                             "https://tomasvaron-full-fidelity-westus.documents.azure.com:443/"]
 
         while True:
             try:
-                for endpoint in endpoints_to_test:
-                    database_account = await client._get_database_account(url_connection=endpoint)
-
-                time.sleep(20)
+                database_account = await client._get_database_account()
+                logger.info("%s - Database account - writable locations: %s",
+                            datetime.now().strftime("%Y%m%d-%H%M%S"),
+                            database_account.WritableLocations)
+                logger.info("%s - Database account - readable locations: %s",
+                            datetime.now().strftime("%Y%m%d-%H%M%S"),
+                            database_account.ReadableLocations)
+                time.sleep(1)
             except Exception as e:
                 logger.error(e)
                 raise e
@@ -39,4 +40,4 @@ async def run_workload(client_id: str, logger):
 if __name__ == "__main__":
     file_name = os.path.basename(__file__)
     prefix, logger = create_logger(file_name)
-    asyncio.run(run_workload(prefix, logger))
+    asyncio.run(run_workload(prefix))
