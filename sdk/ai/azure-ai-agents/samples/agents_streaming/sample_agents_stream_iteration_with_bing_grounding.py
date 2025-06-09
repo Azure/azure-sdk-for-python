14801da451149826c64b4b68266a7f577fd7bc3d
Glenn Harper <64209257+glharper@users.noreply.github.com>
2025-06-09 11:42:00 -0400

    [AI] [Agents] v1.1.0b2 (#41325)
    * [AI] [Agents] add is_valid_connection_id check to connection tool init
    
    * Howie/async func b2 (#41349)
    
    * use as* `asyncio.gather` is used to make function tool calls in paral… (#41311)
    
    * use as* `asyncio.gather` is used to make function tool calls in parallel for `async` scenario.
    
    * Fixed version
    
    * update
    
    * update version
    
    * run black
    
    * fix change_context usages (#41362)
    
    * fixing tracing for streaming when not using event handler (#41424)
    
    * fixing tracing for streaming when not using event handler
    
    * fixing mypy warnings
    
    * fixes
    
    * changelog
    
    * Update CHANGELOG.md
    
    * Howie/samples with proj client (#41426)
    
    * Update samples with project client
    
    * more samples
    
    * update samples
    
    * UPDATE
    
    * update
    
    * change tooling samples
    
    * black and readme update
    
    * update pip install
    
    * Update changelog
    
    * change  is_valid_connection_id to _is_valid_connection_id and run black
    
    * fixed lint
    
    * revert dev req
    
    * Update README.md
    
    * update
    
    * fix readme
    
    * added a new sample
    
    * Update CHANGELOG.md with new release date
    
    ---------
    
    Co-authored-by: Howie Leung <howieleung@microsoft.com>
    Co-authored-by: Liudmila Molkova <limolkova@microsoft.com>
    Co-authored-by: M-Hietala <78813398+M-Hietala@users.noreply.github.com>

--- sdk/ai/azure-ai-agents/samples/agents_streaming/sample_agents_stream_iteration_with_bing_grounding.py
+++ sdk/ai/azure-ai-agents/samples/agents_streaming/sample_agents_stream_iteration_with_bing_grounding.py
@@ -14,7 +14,7 @@ USAGE:
 
     Before running the sample:
 
-    pip install azure-ai-agents azure-identity
+    pip install azure-ai-projects azure-ai-agents azure-identity
 
     Set these environment variables with your own values:
     1) PROJECT_ENDPOINT - The Azure AI Project endpoint, as found in the Overview
@@ -26,7 +26,7 @@ USAGE:
 """
 
 import os
-from azure.ai.agents import AgentsClient
+from azure.ai.projects import AIProjectClient
 from azure.ai.agents.models import AgentStreamEvent, RunStepDeltaChunk
 from azure.ai.agents.models import (
     MessageDeltaChunk,
@@ -40,12 +40,14 @@ from azure.ai.agents.models import (
 )
 from azure.identity import DefaultAzureCredential
 
-agents_client = AgentsClient(
+project_client = AIProjectClient(
     endpoint=os.environ["PROJECT_ENDPOINT"],
     credential=DefaultAzureCredential(),
 )
 
-with agents_client:
+with project_client:
+    agents_client = project_client.agents
+
     bing_connection_id = os.environ["AZURE_BING_CONNECTION_ID"]
     bing = BingGroundingTool(connection_id=bing_connection_id)
     print(f"Bing Connection ID: {bing_connection_id}")
