56a0fd267a73b4249ee80e6580e5835840521dfe
Cezara Mateiasi <CEMATEIA@microsoft.com>
2025-06-10 18:43:17 +0200

    fix props in phone number

--- sdk/communication/azure-communication-jobrouter/azure/communication/jobrouter/_shared/models.py
+++ sdk/communication/azure-communication-jobrouter/azure/communication/jobrouter/_shared/models.py
@@ -161,11 +161,7 @@ class PhoneNumberIdentifier:
             asserted_id_index = -1 if is_anonymous else phone_number.rfind("_") + 1
             has_asserted_id = 0 < asserted_id_index < len(phone_number)
             asserted_id = phone_number[asserted_id_index:] if has_asserted_id else None
-            props = {"value": value}
-            if asserted_id is not None:
-                props["asserted_id"] = asserted_id
-            if is_anonymous is not None:
-                props["is_anonymous"] = is_anonymous
+            props = {"value": value, "asserted_id": asserted_id, "is_anonymous": is_anonymous}
             self.properties = PhoneNumberProperties(**props)
         else:    
             self.properties = PhoneNumberProperties(value=value)
