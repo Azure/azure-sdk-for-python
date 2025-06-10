91f66bed845a4ab62a7a30bf0d4f56c5cdf797fd
azure-sdk <PythonSdkPipelines>
2025-06-10 01:12:56 +0000

    code and test

--- sdk/dashboard/azure-mgmt-dashboard/generated_samples/managed_private_endpoints_refresh.py
+++ sdk/dashboard/azure-mgmt-dashboard/generated_samples/managed_private_endpoints_refresh.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -7,6 +8,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.dashboard import DashboardManagementClient
 
 """
@@ -35,6 +37,6 @@ def main():
     ).result()
 
 
-# x-ms-original-file: specification/dashboard/resource-manager/Microsoft.Dashboard/stable/2023-09-01/examples/ManagedPrivateEndpoints_Refresh.json
+# x-ms-original-file: specification/dashboard/resource-manager/Microsoft.Dashboard/preview/2024-11-01-preview/examples/ManagedPrivateEndpoints_Refresh.json
 if __name__ == "__main__":
     main()
