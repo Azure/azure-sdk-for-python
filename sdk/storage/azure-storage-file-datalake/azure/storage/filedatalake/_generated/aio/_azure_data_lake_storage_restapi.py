2c81008bd19c900d7002aaf63bbd8877fef00694
Peter Wu <weirongwu@microsoft.com>
2025-06-10 14:59:42 -0400

    Regenerated code for all packages

--- sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_generated/aio/_azure_data_lake_storage_restapi.py
+++ sdk/storage/azure-storage-file-datalake/azure/storage/filedatalake/_generated/aio/_azure_data_lake_storage_restapi.py
@@ -15,7 +15,7 @@ from azure.core.pipeline import policies
 from azure.core.rest import AsyncHttpResponse, HttpRequest
 
 from .. import models as _models
-from .._serialization import Deserializer, Serializer
+from .._utils.serialization import Deserializer, Serializer
 from ._configuration import AzureDataLakeStorageRESTAPIConfiguration
 from .operations import FileSystemOperations, PathOperations, ServiceOperations
 
@@ -52,6 +52,7 @@ class AzureDataLakeStorageRESTAPI:  # pylint: disable=client-accepts-api-version
         self._config = AzureDataLakeStorageRESTAPIConfiguration(
             url=url, x_ms_lease_duration=x_ms_lease_duration, **kwargs
         )
+
         _policies = kwargs.pop("policies", None)
         if _policies is None:
             _policies = [
