aec5b77b1761d5da47a88d538fdf874660e4bea8
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2025-06-13 07:09:10 -0700

    More fixes

--- sdk/ai/azure-ai-projects/tests/test_indexes_async.py
+++ sdk/ai/azure-ai-projects/tests/test_indexes_async.py
@@ -37,7 +37,7 @@ class TestIndexesAsync(TestBase):
             index = await project_client.indexes.create_or_update(
                 name=index_name,
                 version=index_version,
-                body=AzureAISearchIndex(connection_name=ai_search_connection_name, index_name=ai_search_index_name),
+                index=AzureAISearchIndex(connection_name=ai_search_connection_name, index_name=ai_search_index_name),
             )
             print(index)
             TestBase.validate_index(
