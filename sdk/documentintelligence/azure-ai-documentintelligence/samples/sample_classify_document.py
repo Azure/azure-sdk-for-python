3a797922a0623cd4f9942a388ec6ecd1374e71e1
Abhishek <95518276+Abhishek0075@users.noreply.github.com>
2025-06-17 13:18:29 +0000

    Specifying split argument

--- sdk/documentintelligence/azure-ai-documentintelligence/samples/sample_classify_document.py
+++ sdk/documentintelligence/azure-ai-documentintelligence/samples/sample_classify_document.py
@@ -51,7 +51,7 @@ def classify_document(classifier_id):
 
     document_intelligence_client = DocumentIntelligenceClient(endpoint=endpoint, credential=AzureKeyCredential(key))
     with open(path_to_sample_documents, "rb") as f:
-        poller = document_intelligence_client.begin_classify_document(classifier_id, body=f)
+        poller = document_intelligence_client.begin_classify_document(classifier_id, split="perPage", body=f)
     result: AnalyzeResult = poller.result()
 
     print("----Classified documents----")
