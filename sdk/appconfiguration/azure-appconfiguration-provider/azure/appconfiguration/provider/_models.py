8d1ead8e56a858aece986f2e1629ff3919eeee98
Matt Metcalf <mrm9084@gmail.com>
2025-06-19 09:15:43 -0700

    Update _models.py

--- sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_models.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_models.py
@@ -46,12 +46,12 @@ class SettingSelector:
     :keyword key_filter:A filter to select configuration settings and feature flags based on their keys.
     :type key_filter: str
     :keyword label_filter: A filter to select configuration settings and feature flags based on their labels. Default
-     is value is \\0 i.e. (No Label) as seen in the portal.
+    is value is \0 i.e. (No Label) as seen in the portal.
     :type label_filter: Optional[str]
     :keyword tag_filters: A filter to select configuration settings and feature flags based on their tags. This is a
-     list of strings that will be used to match tags on the configuration settings. Reserved characters (\\*, \\, ,)
-     must be escaped with backslash if they are part of the value. Tag filters must follow the format
-     "tagName=tagValue", for empty values use "tagName=" and for null values use "tagName=\\0".
+    list of strings that will be used to match tags on the configuration settings. Reserved characters (\\*, \\, ,)
+    must be escaped with backslash if they are part of the value. Tag filters must follow the format
+    "tagName=tagValue", for empty values use "tagName=" and for null values use "tagName=\\0".
     :type tag_filters: Optional[List[str]]
     """
 
