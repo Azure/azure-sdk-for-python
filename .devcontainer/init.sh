cbe116f4806d7f46150f2a73f9a127e93ee46137
Laurent Mazuel <laurent.mazuel@gmail.com>
2025-07-01 10:08:56 -0700

    Codespace setup (#41836)
    * Codespace setup
    
    * Cspell the devcontainer folder

---
+++ .devcontainer/init.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Install and use nvm version from .nvmrc
+source $NVM_DIR/nvm.sh
+nvm install
+nvm use
+# Set default node to version we just installed so that new shells start with it
+nvm alias default $(node --version)
+
+# Install utilities
+npm install -g autorest @typespec/compiler @azure-tools/typespec-client-generator-cli
+
+# Install PowerShell. PowerShell is needed for the test proxy asset sync migration scripts,
+# and is also useful for running scripts in eng/common/scripts and eng/common/TestResources.
+# Update the list of packages
+sudo apt-get update
+# Install pre-requisite packages.
+sudo apt-get install -y wget apt-transport-https software-properties-common
+# Download the Microsoft repository GPG keys
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+# Delete the the Microsoft repository GPG keys file
+rm packages-microsoft-prod.deb
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+# Install PowerShell
+sudo apt-get install -y powershell
+
+# Install Python tooling
+# init env
+python -m pip install -U pip > /dev/null
+python scripts/dev_setup.py -p azure-core > /dev/null
+pip install tox==4.15.0 > /dev/null
+pip install wheel==0.43.0 > /dev/null
+pip install setuptools==78.1.0 > /dev/null
+pip install setuptools-scm==8.3.0 > /dev/null
