1e99e0243cc8f6ffb8fa3948a6c1a91cfc0496ea
M-Hietala <78813398+M-Hietala@users.noreply.github.com>
2025-07-08 13:58:09 -0500

    adding response format samples

---
+++ sdk/ai/azure-ai-agents/samples/agents_response_formats/sample_agents_text_response_format.py
@@ -0,0 +1,75 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+"""
+DESCRIPTION:
+    This sample demonstrates how to create an agent with text response format.
+
+USAGE:
+    python sample_agents_text_response_format.py
+
+    Before running the sample:
+
+    pip install azure-ai-projects azure-ai-agents azure-identity
+
+    Set these environment variables with your own values:
+    1) PROJECT_ENDPOINT - The Azure AI Project endpoint, as found in the Overview
+                          page of your Azure AI Foundry portal.
+    2) MODEL_DEPLOYMENT_NAME - The deployment name of the AI model, as found under the "Name" column in
+       the "Models + endpoints" tab in your Azure AI Foundry project.
+"""
+
+import os, time
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from azure.ai.agents.models import ListSortOrder, AgentsResponseFormat
+
+project_client = AIProjectClient(
+    endpoint=os.environ["PROJECT_ENDPOINT"],
+    credential=DefaultAzureCredential(),
+)
+
+with project_client:
+    agents_client = project_client.agents
+
+    # [START create_agent_with_text_response_format]
+    agent = agents_client.create_agent(
+        model=os.environ["MODEL_DEPLOYMENT_NAME"],
+        name="my-agent",
+        instructions="You are helpful agent.",
+        response_format=AgentsResponseFormat(type="text")
+    )
+    # [END create_agent_with_text_response_format]
+    print(f"Created agent, agent ID: {agent.id}")
+
+    thread = agents_client.threads.create()
+    print(f"Created thread, thread ID: {thread.id}")
+
+    # List all threads for the agent
+    threads = agents_client.threads.list()
+
+    message = agents_client.messages.create(thread_id=thread.id, role="user", content="Hello, give me a list of planets in our solar system.")
+    print(f"Created message, message ID: {message.id}")
+
+    run = agents_client.runs.create(thread_id=thread.id, agent_id=agent.id)
+
+    # Poll the run as long as run status is queued or in progress
+    while run.status in ["queued", "in_progress", "requires_action"]:
+        # Wait for a second
+        time.sleep(1)
+        run = agents_client.runs.get(thread_id=thread.id, run_id=run.id)
+        print(f"Run status: {run.status}")
+
+    if run.status == "failed":
+        print(f"Run error: {run.last_error}")
+
+    agents_client.delete_agent(agent.id)
+    print("Deleted agent")
+
+    messages = agents_client.messages.list(thread_id=thread.id, order=ListSortOrder.ASCENDING)
+    for msg in messages:
+        if msg.text_messages:
+            last_text = msg.text_messages[-1]
+            print(f"{msg.role}: {last_text.text.value}")
