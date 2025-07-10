7c76629ce15eb277d6514ef7f074f2ae498c0ef2
M-Hietala <78813398+M-Hietala@users.noreply.github.com>
2025-07-10 16:26:21 -0500

    removing unwanted change

--- sdk/ai/azure-ai-agents/samples/sample_agents_basics.py
+++ sdk/ai/azure-ai-agents/samples/sample_agents_basics.py
@@ -40,7 +40,6 @@ with project_client:
         model=os.environ["MODEL_DEPLOYMENT_NAME"],
         name="my-agent",
         instructions="You are helpful agent",
-        description="This is a sample agent created from the Azure AI Agents SDK",
     )
     # [END create_agent]
     print(f"Created agent, agent ID: {agent.id}")
