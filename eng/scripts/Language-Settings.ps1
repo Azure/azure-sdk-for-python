37ac69042f0e4ab50c8cc85adf7221d75e5c9018
Scott Beddall <scbedd@microsoft.com>
2025-07-11 18:44:33 +0000

    reducing duplication in favor of template. updating create venv usage

--- eng/scripts/Language-Settings.ps1
+++ eng/scripts/Language-Settings.ps1
@@ -162,7 +162,14 @@ function Get-AllPackageInfoFromRepo ($serviceDirectory)
   try
   {
     Push-Location $RepoRoot
-    $null = python -m pip install "./tools/azure-sdk-tools[build]" -q -I
+    # Use ‘uv pip install’ if uv is on PATH, otherwise fall back to python -m pip
+    if (Get-Command uv -ErrorAction SilentlyContinue) {
+      Write-Host "Using uv pip install"
+      $null = uv pip install "./tools/azure-sdk-tools[build]" -q -I
+    } else {
+      Write-Host "Using python -m pip install"
+      $null = python -m pip install "./tools/azure-sdk-tools[build]" -q -I
+    }
     $allPkgPropLines = python (Join-path eng scripts get_package_properties.py) -s $searchPath
   }
   catch
@@ -174,7 +181,6 @@ function Get-AllPackageInfoFromRepo ($serviceDirectory)
   {
     Pop-Location
   }
-
 
   foreach ($line in $allPkgPropLines)
   {
@@ -412,7 +418,12 @@ function SetPackageVersion ($PackageName, $Version, $ServiceDirectory, $ReleaseD
   {
     $ReleaseDate = Get-Date -Format "yyyy-MM-dd"
   }
-  python -m pip install "$RepoRoot/tools/azure-sdk-tools[build]" -q -I
+  if (Get-Command uv -ErrorAction SilentlyContinue) {
+    uv pip install "$RepoRoot/tools/azure-sdk-tools[build]" -q -I
+  } else {
+    python -m pip install "$RepoRoot/tools/azure-sdk-tools[build]" -q -I
+  }
+
   sdk_set_version --package-name $PackageName --new-version $Version `
   --service $ServiceDirectory --release-date $ReleaseDate --replace-latest-entry-title $ReplaceLatestEntryTitle
 }
