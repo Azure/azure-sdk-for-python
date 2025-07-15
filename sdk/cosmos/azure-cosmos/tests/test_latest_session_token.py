1ddbe6ee2647445e7f4f5a8d2ebc562c91b87c2d
bambriz <beambriz2017@gmail.com>
2025-07-15 10:49:30 -0700

    update tests
    Updates tests
    and fixes a key error issue in session.py

--- sdk/cosmos/azure-cosmos/tests/test_latest_session_token.py
+++ sdk/cosmos/azure-cosmos/tests/test_latest_session_token.py
@@ -95,7 +95,7 @@ class TestLatestSessionToken(unittest.TestCase):
         phys_session_token = container.get_latest_session_token(phys_feed_ranges_and_session_tokens, phys_target_feed_range)
         pk_range_id, session_token = parse_session_token(phys_session_token)
 
-        assert session_token.global_lsn >= 350
+        assert 335 <= session_token.global_lsn <= 385
         assert '2' in pk_range_id
         self.database.delete_container(container.id)
 
