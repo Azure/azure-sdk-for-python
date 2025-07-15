f5d52eb315c294dae44c0fa4e98dca231ea91587
Peter Wu <weirongwu@microsoft.com>
2025-07-15 16:44:48 -0400

    Documented results_per_page for list_blobs, list_blob_names

--- sdk/storage/azure-storage-blob/azure/storage/blob/_container_client.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/_container_client.py
@@ -864,6 +864,9 @@ class ContainerClient(StorageAccountHostsMixin, StorageEncryptionMixin):    # py
         :keyword str name_starts_with:
             Filters the results to return only blobs whose names
             begin with the specified prefix.
+        :keyword int results_per_page:
+            Controls the maximum number of Blobs that will be included in each page of results if using
+            :func: `ItemPaged.by_page()`.
         :keyword int timeout:
             Sets the server-side timeout for the operation in seconds. For more details see
             https://learn.microsoft.com/rest/api/storageservices/setting-timeouts-for-blob-service-operations.
