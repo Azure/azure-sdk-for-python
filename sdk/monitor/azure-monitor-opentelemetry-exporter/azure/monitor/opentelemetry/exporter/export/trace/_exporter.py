f3068e342ad8e8960602d51f0a5bac4a1d841f42
Karlie Li <aiweili@microsoft.com>
2025-07-18 15:54:48 -0700

    update

--- sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/export/trace/_exporter.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/export/trace/_exporter.py
@@ -420,7 +420,7 @@ def _convert_span_to_envelope(span: ReadableSpan) -> TelemetryItem:
             # following mappings will override the type
             if gen_ai_attributes_val:
                 data.type = _GEN_AI_ATTRIBUTE_PREFIX.format(gen_ai_attributes_val)
-            # If no fields are available to set target using standard rules, 
+            # If no fields are available to set target using standard rules,
             # set Dependency Target to gen_ai.system if present
             if not target and not data.target and gen_ai_attributes_val:
                 target = gen_ai_attributes_val
