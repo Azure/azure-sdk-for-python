8880a6c9f909e3aa008bbfcae336f967658bb27a
Kashif Khan <kashifkhan@microsoft.com>
2025-07-23 08:33:06 -0500

    reset ns change

--- sdk/webpubsub/azure-messaging-webpubsubservice/azure/messaging/webpubsubservice/aio/_client.py
+++ sdk/webpubsub/azure-messaging-webpubsubservice/azure/messaging/webpubsubservice/aio/_client.py
@@ -16,7 +16,7 @@ from azure.core.rest import AsyncHttpResponse, HttpRequest
 
 from .._utils.serialization import Deserializer, Serializer
 from ._configuration import WebPubSubServiceClientConfiguration
-from ._operations import _WebPubSubServiceClientOperationsMixin
+from ._operations._operations import _WebPubSubServiceClientOperationsMixin
 
 if TYPE_CHECKING:
     from azure.core.credentials_async import AsyncTokenCredential
