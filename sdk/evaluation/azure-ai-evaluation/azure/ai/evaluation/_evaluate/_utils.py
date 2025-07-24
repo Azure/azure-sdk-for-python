9248c38f69510b9b05469713aa2fc958ec6deebc
Nagkumar Arkalgud <nagkumar@naarkalg-work-mac.local>
2025-07-24 12:22:00 -0700

    Add properties to evaluation upload run for FDP

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluate/_utils.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluate/_utils.py
@@ -191,6 +191,7 @@ def _log_metrics_and_instance_results_onedp(
         upload_run_response = client.start_evaluation_run(
             evaluation=EvaluationUpload(
                 display_name=evaluation_name,
+                properties=properties,
             )
         )
 
@@ -205,6 +206,9 @@ def _log_metrics_and_instance_results_onedp(
                 properties=properties,
             ),
         )
+        import pdb
+
+        pdb.set_trace()
 
     return update_run_response.properties.get("AiStudioEvaluationUri")
 
