95c5a0f79a306955c1c64b0df43a73768e08162b
Kashif Khan <kashifkhan@microsoft.com>
2025-07-25 20:48:52 +0000

    new generated files from tsp

--- sdk/attestation/azure-security-attestation/azure/security/attestation/aio/_configuration.py
+++ sdk/attestation/azure-security-attestation/azure/security/attestation/aio/_configuration.py
@@ -10,10 +10,10 @@ from typing import Any, TYPE_CHECKING
 
 from azure.core.pipeline import policies
 
-from ._version import VERSION
+from .._version import VERSION
 
 if TYPE_CHECKING:
-    from azure.core.credentials import TokenCredential
+    from azure.core.credentials_async import AsyncTokenCredential
 
 
 class AttestationClientConfiguration:    # pylint: disable=too-many-instance-attributes
@@ -25,7 +25,7 @@ class AttestationClientConfiguration:    # pylint: disable=too-many-instance-att
     :param endpoint: Required.
     :type endpoint: str
     :param credential: Credential used to authenticate requests to the service. Required.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :keyword api_version: The API version to use for this operation. Default value is "2025-06-01".
      Note that overriding this default value may result in unsupported behavior.
     :paramtype api_version: str
@@ -34,7 +34,7 @@ class AttestationClientConfiguration:    # pylint: disable=too-many-instance-att
     def __init__(
         self,
         endpoint: str,
-        credential: "TokenCredential",
+        credential: "AsyncTokenCredential",
         **kwargs: Any
     ) -> None:
         api_version: str = kwargs.pop('api_version', "2025-06-01")
@@ -63,8 +63,8 @@ class AttestationClientConfiguration:    # pylint: disable=too-many-instance-att
         self.logging_policy = kwargs.get('logging_policy') or policies.NetworkTraceLoggingPolicy(**kwargs)
         self.http_logging_policy = kwargs.get('http_logging_policy') or policies.HttpLoggingPolicy(**kwargs)
         self.custom_hook_policy = kwargs.get('custom_hook_policy') or policies.CustomHookPolicy(**kwargs)
-        self.redirect_policy = kwargs.get('redirect_policy') or policies.RedirectPolicy(**kwargs)
-        self.retry_policy = kwargs.get('retry_policy') or policies.RetryPolicy(**kwargs)
+        self.redirect_policy = kwargs.get('redirect_policy') or policies.AsyncRedirectPolicy(**kwargs)
+        self.retry_policy = kwargs.get('retry_policy') or policies.AsyncRetryPolicy(**kwargs)
         self.authentication_policy = kwargs.get('authentication_policy')
         if self.credential and not self.authentication_policy:
-            self.authentication_policy = policies.BearerTokenCredentialPolicy(self.credential, *self.credential_scopes, **kwargs)
+            self.authentication_policy = policies.AsyncBearerTokenCredentialPolicy(self.credential, *self.credential_scopes, **kwargs)
