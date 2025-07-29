587be203415a8ef3c870a060fb312478f729046e
Kashif Khan <kashifkhan@microsoft.com>
2025-07-29 14:41:22 -0500

    update setup.py

--- sdk/webpubsub/azure-messaging-webpubsubservice/setup.py
+++ sdk/webpubsub/azure-messaging-webpubsubservice/setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
     ],
     zip_safe=False,
@@ -64,7 +65,7 @@ setup(
     },
     install_requires=[
         "isodate>=0.6.1",
-        "azure-core>=1.30.0",
+        "azure-core>=1.35.0",
         "typing-extensions>=4.6.0",
     ],
     python_requires=">=3.9",
