435873939a3411847a3cfe61c00535bd5ffd8e14
Kashif Khan <kashifkhan@microsoft.com>
2025-07-29 14:41:50 -0500

    import from _operation changes

--- sdk/webpubsub/azure-messaging-webpubsubservice/azure/messaging/webpubsubservice/_client.py
+++ sdk/webpubsub/azure-messaging-webpubsubservice/azure/messaging/webpubsubservice/_client.py
@@ -15,7 +15,7 @@ from azure.core.pipeline import policies
 from azure.core.rest import HttpRequest, HttpResponse
 
 from ._configuration import WebPubSubServiceClientConfiguration
-from ._operations._operations import _WebPubSubServiceClientOperationsMixin
+from ._operations import _WebPubSubServiceClientOperationsMixin
 from ._utils.serialization import Deserializer, Serializer
 
 if TYPE_CHECKING:
