e32e9a330a6f92c3d0244b632acf7fde4affa7ed
Paul Van Eck <paulvaneck@microsoft.com>
2025-07-29 16:03:27 -0700

    [Monitor] Update query package to use TypeSpec (#42205)
    MetricsClient and MetricsQueryClient have been removed from the azure-monitor-query package.
    This is part of the migration to split logs and metrics functionality into separate packages and using
    TypeSpec as the generation tooling for this SDK.
    
    - The MetricsClient has been moved to the new azure-monitor-querymetrics package.
    - Functionality provided by MetricsQueryClient can be access through the azure-mgmt-monitor package.
    - All metrics-related content has been removed.
    - The package version has been updated to 2.0.0 due to the breaking changes.
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>

---
+++ sdk/monitor/azure-monitor-query/azure/monitor/query/models/_patch.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------
+"""Customize generated code here.
+
+Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
+"""
+from typing import List
+
+from . import _models  # pylint: disable=unused-import
+
+__all__: List[str] = ["_models"]  # Add all objects you want publicly available to users at this package level
+
+
+def patch_sdk():
+    """Do not remove from this file.
+
+    `patch_sdk` is a last resort escape hatch that allows you to do customizations
+    you can't accomplish using the techniques described in
+    https://aka.ms/azsdk/python/dpcodegen/python/customize
+    """
