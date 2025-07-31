ffdd6cca2d985661f68cf0fdd4b195ee8ef0dd5f
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2025-07-31 14:59:19 -0700

    Fix alignment

--- sdk/ai/azure-ai-projects/azure/ai/projects/aio/_configuration.py
+++ sdk/ai/azure-ai-projects/azure/ai/projects/aio/_configuration.py
@@ -25,10 +25,8 @@ class AIProjectClientConfiguration:  # pylint: disable=too-many-instance-attribu
     :param endpoint: Project endpoint. In the form
      "https://your-ai-services-account-name.services.ai.azure.com/api/projects/_project"
      if your Foundry Hub has only one Project, or to use the default Project in your Hub. Or in the
-     form
-     "https://your-ai-services-account-name.services.ai.azure.com/api/projects/your-project-name"
-     if you want to explicitly
-     specify the Foundry Project name. Required.
+     form "https://your-ai-services-account-name.services.ai.azure.com/api/projects/your-project-name"
+     if you want to explicitly specify the Foundry Project name. Required.
     :type endpoint: str
     :param credential: Credential used to authenticate requests to the service. Required.
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
