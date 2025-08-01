49e537e2c5ef5d3dd0cca3ddd1d6e9bf31d78a56
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2025-08-01 15:17:17 -0700

    Changes for release of azure-ai-projects client library version 1.1.0b1 (#42306)

--- sdk/ai/azure-ai-projects/samples/inference/azure-ai-inference/sample_image_embeddings_with_azure_ai_inference_client_async.py
+++ sdk/ai/azure-ai-projects/samples/inference/azure-ai-inference/sample_image_embeddings_with_azure_ai_inference_client_async.py
@@ -6,13 +6,13 @@
 
 """
 DESCRIPTION:
-    Given an AI Foundry Project endpoint, this sample demonstrates how to get an authenticated 
-    async EmbeddingsClient from the azure.ai.inference package, and perform one text
-    embeddings operation. For more information on the azure.ai.inference package see
-    https://pypi.org/project/azure-ai-inference/.
+    Given an AI Foundry Project endpoint, this sample demonstrates how to get an authenticated
+    async ImageEmbeddingsClient from the azure.ai.inference package, and perform one
+    image embeddings operation. For more information on the azure.ai.inference package
+    see https://pypi.org/project/azure-ai-inference/.
 
 USAGE:
-    python sample_text_embeddings_with_azure_ai_inference_client_async.py
+    python sample_image_embeddings_with_azure_ai_inference_client_async.py
 
     Before running the sample:
 
@@ -28,7 +28,8 @@ import os
 import asyncio
 from urllib.parse import urlparse
 from azure.identity.aio import DefaultAzureCredential
-from azure.ai.inference.aio import EmbeddingsClient
+from azure.ai.inference.aio import ImageEmbeddingsClient
+from azure.ai.inference.models import ImageEmbeddingInput
 
 
 async def main():
@@ -36,21 +37,26 @@ async def main():
     endpoint = os.environ["PROJECT_ENDPOINT"]
     model_deployment_name = os.environ["MODEL_DEPLOYMENT_NAME"]
 
-    # Project endpoint has the form:   https://<your-ai-services-account-name>.services.ai.azure.com/api/projects/<your-project-name>
-    # Inference endpoint has the form: https://<your-ai-services-account-name>.services.ai.azure.com/models
-    # Strip the "/api/projects/<your-project-name>" part and replace with "/models":
+    # Project endpoint has the form:   https://your-ai-services-account-name.services.ai.azure.com/api/projects/your-project-name
+    # Inference endpoint has the form: https://your-ai-services-account-name.services.ai.azure.com/models
+    # Strip the "/api/projects/your-project-name" part and replace with "/models":
     inference_endpoint = f"https://{urlparse(endpoint).netloc}/models"
+
+    # Construct the path to the image file used in this sample
+    data_folder = os.environ.get("DATA_FOLDER", os.path.dirname(os.path.abspath(__file__)))
+    image_file = os.path.join(data_folder, "sample1.png")
 
     async with DefaultAzureCredential() as credential:
 
-        async with EmbeddingsClient(
+        async with ImageEmbeddingsClient(
             endpoint=inference_endpoint,
             credential=credential,
             credential_scopes=["https://ai.azure.com/.default"],
         ) as client:
 
             response = await client.embed(
-                model=model_deployment_name, input=["first phrase", "second phrase", "third phrase"]
+                model=model_deployment_name,
+                input=[ImageEmbeddingInput.load(image_file=image_file, image_format="png")],
             )
 
             for item in response.data:
