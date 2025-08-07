a47b8e588c093cb023cd02a7ac19f5d1b12c641c
Matt Metcalf <mrm9084@gmail.com>
2025-08-07 11:51:59 -0700

    fixed // formatting

--- sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_json.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/azure/appconfiguration/provider/_json.py
@@ -12,32 +12,32 @@ DOUBLE_QUOTE = '"'
 NEW_LINE = "\n"
 
 
-def _find_string_end(text: str, current_index: int) -> int:
+def _find_string_end(text: str, index: int) -> int:
     """
     Helper function that finds the end of a string literal.
 
     :param text: The input text to process.
     :type text: str
-    :param current_index: The current index in the text, should be the position right after the opening quote.
-    :type current_index: int
+    :param index: The current index in the text, should be the position right after the opening quote.
+    :type index: int
     :raises ValueError: If there is an unterminated string literal.
     :return: The index of the closing quote.
     :rtype: int
     """
-    while current_index < len(text):
-        current_char = text[current_index]
+    while index < len(text):
+        current_char = text[index]
         if current_char == DOUBLE_QUOTE:
             # Check for escaped quote
             esc_count = 0
-            j = current_index - 1
+            j = index - 1
             while j >= 0 and text[j] == ESCAPE_CHAR:
                 esc_count += 1
                 j -= 1
             if esc_count % 2 == 0:
                 # Found end of string
-                current_index += 1
-                return current_index
-        current_index += 1
+                index += 1
+                return index
+        index += 1
     raise ValueError("Unterminated string literal")
 
 
@@ -53,41 +53,42 @@ def remove_json_comments(text: str) -> str:
     :rtype: str
     """
     result = []
-    current_index = 0
+    index = 0
     length = len(text)
-    while current_index < length:
-        current_char = text[current_index]
+    while index < length:
+        current_char = text[index]
         if current_char == DOUBLE_QUOTE:
             result.append(current_char)
-            current_index += 1
-            string_end = _find_string_end(text, current_index)
-            result.append(text[current_index:string_end])
-            current_index = string_end
-        elif text[current_index : current_index + 2] == DOUBLE_FORWARD_SLASH_COMMENT:
+            index += 1
+            string_end = _find_string_end(text, index)
+            result.append(text[index:string_end])
+            index = string_end
+        elif text[index : index + 2] == DOUBLE_FORWARD_SLASH_COMMENT:
             # Skip to end of line or end of file
-            current_index += 2
-            while current_index < length and text[current_index] != NEW_LINE:
-                current_index += 1
+            index += 2
+            while index < length and text[index] != NEW_LINE:
+                index += 1
             # If we found a newline, move past it
-            if current_index < length and text[current_index] == NEW_LINE:
-                current_index += 1
-        elif text[current_index : current_index + 2] == MULTI_LINE_COMMENT:
+            if index < length and text[index] == NEW_LINE:
+                result.append(NEW_LINE)
+                index += 1
+        elif text[index : index + 2] == MULTI_LINE_COMMENT:
             # Skip to end of block comment
-            current_index += 2
+            index += 2
 
             # Search for the end of the comment
             found_end = False
-            while current_index < length - 1:
-                if current_index + 1 < length and text[current_index : current_index + 2] == END_MULTI_LINE_COMMENT:
+            while index < length - 1:
+                if index + 1 < length and text[index : index + 2] == END_MULTI_LINE_COMMENT:
                     found_end = True
-                    current_index += 2  # Skip past the end marker
+                    index += 2  # Skip past the end marker
                     break
-                current_index += 1
+                index += 1
 
             # If we reached the end without finding the comment closer, raise an error
             if not found_end:
                 raise ValueError("Unterminated multi-line comment")
         else:
             result.append(current_char)
-            current_index += 1
+            index += 1
     return "".join(result)
