85ad65322d3d4c0a313d2fa9a25bc6488fb76ff0
Amber Chen (Centific Technologies Inc) <v-amberchen@microsoft.com>
2025-08-15 10:49:33 -0700

    add tests for prediction sync and async

--- sdk/cognitivelanguage/azure-ai-language-conversations/tests/test_conversation_prediction_async.py
+++ sdk/cognitivelanguage/azure-ai-language-conversations/tests/test_conversation_prediction_async.py
@@ -2,7 +2,7 @@ import functools
 import pytest
 
 from devtools_testutils import AzureRecordedTestCase, PowerShellPreparer, recorded_by_proxy
-from azure.ai.language.conversations import ConversationAnalysisClient
+from azure.ai.language.conversations.aio import ConversationAnalysisClient
 from azure.ai.language.conversations.models import (
     AnalyzeConversationOperationInput,
     ConversationLanguageUnderstandingActionContent,
@@ -18,6 +18,8 @@ from azure.ai.language.conversations.models import (
     ConversationLanguageUnderstandingInput,
 )
 from typing import cast
+from devtools_testutils.aio import recorded_by_proxy_async
+from azure.core.async_paging import AsyncItemPaged
 
 from azure.core.credentials import AzureKeyCredential
 
@@ -32,19 +34,19 @@ ConversationsPreparer = functools.partial(
 class TestConversations(AzureRecordedTestCase):
 
     # Start with any helper functions you might need, for example a client creation method:
-    def create_client(self, endpoint, key):
+    async def create_client(self, endpoint, key):
         credential = AzureKeyCredential(key)
         client = ConversationAnalysisClient(endpoint, credential)
         return client
 
     ...
 
-
 class TestConversationsCase(TestConversations):
     @ConversationsPreparer()
-    @recorded_by_proxy
-    def test_conversation_prediction(self, conversations_endpoint, conversations_key):
-        client = self.create_client(conversations_endpoint, conversations_key)
+    @recorded_by_proxy_async
+    @pytest.mark.asyncio
+    async def test_create_project(self, conversations_endpoint, conversations_key):
+        client = await self.create_client(conversations_endpoint, conversations_key)
 
         project_name = "EmailApp"
         deployment_name = "production"
@@ -54,22 +56,22 @@ class TestConversationsCase(TestConversations):
                 conversation_item=TextConversationItem(
                     id="1",
                     participant_id="participant1",
-                    text="Send an email to Carol about tomorrow's demo"
+                    text="Send an email to Carol about tomorrow's demo",
                 )
             ),
             action_content=ConversationLanguageUnderstandingActionContent(
                 project_name=project_name,
                 deployment_name=deployment_name,
-                string_index_type=StringIndexType.UTF16_CODE_UNIT
-            )
+                string_index_type=StringIndexType.UTF16_CODE_UNIT,
+            ),
         )
 
-        action_result = client.analyze_conversation(data)
-
+        action_result = await client.analyze_conversation(data)
         action_result = cast(ConversationActionResult, action_result)
 
         prediction = action_result.result.prediction
         prediction = cast(ConversationPrediction, prediction)
+
         print(f"Top intent: {prediction.top_intent}")
 
         print("Intents:")
