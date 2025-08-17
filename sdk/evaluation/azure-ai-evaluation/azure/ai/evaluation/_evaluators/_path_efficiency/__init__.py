73993fb0fd7de44ab54ba10e549740dd6bafa7ad
mohessie <mohessie@microsoft.com>
2025-08-17 20:55:41 +0300

    Implement Path Efficiency Evaluator

---
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_path_efficiency/__init__.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+from ._path_efficiency import PathEfficiencyEvaluator
+
+__all__ = [
+    "PathEfficiencyEvaluator"
+]
