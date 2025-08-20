b1b473dc0fd1329615dbb000496d44f8a35f9472
kdestin <101366538+kdestin@users.noreply.github.com>
2025-08-20 16:28:10 -0400

    tests: Add a test for using credential with prompty based evaluator

--- sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_builtin_evaluators.py
+++ sdk/evaluation/azure-ai-evaluation/tests/e2etests/test_builtin_evaluators.py
@@ -1229,6 +1229,22 @@ class TestBuiltInEvaluators:
         assert score["logos_and_brands_label"] in [True, False]
         assert score["fictional_characters_reason"], "fictional_characters_reason must not be None or empty."
 
+    def test_prompty_based_evaluator_custom_credentials(self, sanitized_model_config, simple_conversation, azure_cred):
+        """Validate that prompty based evaluators support passing custom credtials"""
+        config = {"credential": azure_cred, **sanitized_model_config}
+        # ensure that we aren't using an api_key for auth
+        config.pop("api_key", None)
+
+        eval_fn = GroundednessEvaluator(config)
+        score = eval_fn(
+            response="The capital of Japan is Tokyo.",
+            context="Tokyo is Japan's capital.",
+        )
+
+        assert score is not None
+        assert score["groundedness"] > 1.0
+        assert score["groundedness_reason"]
+
 
 @pytest.mark.usefixtures("recording_injection", "recorded_test")
 class TestUserAgent:
