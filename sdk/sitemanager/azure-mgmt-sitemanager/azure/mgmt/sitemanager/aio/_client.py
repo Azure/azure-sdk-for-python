8c3560633e37760af1f92fe4d6ee407fe8f2a952
azure-sdk <PythonSdkPipelines>
2025-08-29 03:15:10 +0000

    code and test

--- sdk/sitemanager/azure-mgmt-sitemanager/azure/mgmt/sitemanager/aio/_client.py
+++ sdk/sitemanager/azure-mgmt-sitemanager/azure/mgmt/sitemanager/aio/_client.py
@@ -42,9 +42,8 @@ class SiteManagerMgmtClient:
     :type subscription_id: str
     :param base_url: Service host. Default value is None.
     :type base_url: str
-    :keyword api_version: The API version to use for this operation. Default value is
-     "2025-03-01-preview". Note that overriding this default value may result in unsupported
-     behavior.
+    :keyword api_version: The API version to use for this operation. Default value is "2025-06-01".
+     Note that overriding this default value may result in unsupported behavior.
     :paramtype api_version: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
      Retry-After header is present.
