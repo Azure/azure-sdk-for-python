c7549f8bace55af13093d4c2c39502e32b729069
jenny <63012604+JennyPng@users.noreply.github.com>
2025-08-29 09:42:38 -0700

    clean test

--- eng/tools/azure-sdk-tools/tests/test_logging_config.py
+++ eng/tools/azure-sdk-tools/tests/test_logging_config.py
@@ -11,13 +11,11 @@ import os
 	(argparse.Namespace(quiet=False, verbose=True, log_level=None), None, "INFO", logging.DEBUG),  
 	(argparse.Namespace(quiet=False, verbose=False, log_level="ERROR"), "ERROR", "INFO", logging.ERROR),  
 	(argparse.Namespace(quiet=False, verbose=False, log_level=None), None, "WARNING", logging.WARNING),  
-    (argparse.Namespace(quiet=False, verbose=False, log_level=None), None, "DEBUG", logging.DEBUG),  
 ])
 @patch("logging.basicConfig")
 def test_configure_logging_various_levels(mock_basic_config, cli_args, level, level_env, expected_level, monkeypatch):
     monkeypatch.setenv("LOGLEVEL", level_env)
     assert os.environ["LOGLEVEL"] == level_env
-    print(level_env)
     configure_logging(cli_args, level=level)
     assert logger.level == expected_level
     mock_basic_config.assert_called_with(level=expected_level, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
