e7a9fc494e058ae61e26f1c05f31a1f7e6d2d4c7
jenny <63012604+JennyPng@users.noreply.github.com>
2025-08-28 17:20:40 -0700

    add unit test

---
+++ eng/tools/azure-sdk-tools/tests/test_logging_config.py
@@ -0,0 +1,22 @@
+
+import logging
+from ci_tools.logging import configure_logging, logger
+from unittest.mock import patch
+import pytest
+import argparse
+import os
+
+@pytest.mark.parametrize("cli_args,level,level_env,expected_level", [
+	(argparse.Namespace(quiet=True, verbose=False, log_level=None), "INFO", "INFO", logging.ERROR),  
+	(argparse.Namespace(quiet=False, verbose=True, log_level=None), "INFO", "INFO", logging.DEBUG),  
+	(argparse.Namespace(quiet=False, verbose=False, log_level="ERROR"), "ERROR", "INFO", logging.ERROR),  
+	(argparse.Namespace(quiet=False, verbose=False, log_level=None), "INFO", "WARNING", logging.INFO),  
+    (argparse.Namespace(quiet=False, verbose=False, log_level=None), "INFO", "DEBUG", logging.DEBUG),  
+])
+@patch("logging.basicConfig")
+def test_configure_logging_various_levels(mock_basic_config, cli_args, level, level_env, expected_level, monkeypatch):
+    monkeypatch.setenv("LOGLEVEL", level_env)
+    assert os.environ["LOGLEVEL"] == level_env
+    configure_logging(cli_args, level=level)
+    assert logger.level == expected_level
+    mock_basic_config.assert_called_with(level=expected_level, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
