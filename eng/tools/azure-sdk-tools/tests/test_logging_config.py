4e2299a5fba931feb54b43077aa281e1911a3c78
jenny <63012604+JennyPng@users.noreply.github.com>
2025-08-28 17:33:15 -0700

    test fix

--- eng/tools/azure-sdk-tools/tests/test_logging_config.py
+++ eng/tools/azure-sdk-tools/tests/test_logging_config.py
@@ -10,13 +10,14 @@ import os
 	(argparse.Namespace(quiet=True, verbose=False, log_level=None), "INFO", "INFO", logging.ERROR),  
 	(argparse.Namespace(quiet=False, verbose=True, log_level=None), "INFO", "INFO", logging.DEBUG),  
 	(argparse.Namespace(quiet=False, verbose=False, log_level="ERROR"), "ERROR", "INFO", logging.ERROR),  
-	(argparse.Namespace(quiet=False, verbose=False, log_level=None), "INFO", "WARNING", logging.INFO),  
+	(argparse.Namespace(quiet=False, verbose=False, log_level=None), "INFO", "WARNING", logging.WARNING),  
     (argparse.Namespace(quiet=False, verbose=False, log_level=None), "INFO", "DEBUG", logging.DEBUG),  
 ])
 @patch("logging.basicConfig")
 def test_configure_logging_various_levels(mock_basic_config, cli_args, level, level_env, expected_level, monkeypatch):
     monkeypatch.setenv("LOGLEVEL", level_env)
     assert os.environ["LOGLEVEL"] == level_env
+    print(level_env)
     configure_logging(cli_args, level=level)
     assert logger.level == expected_level
     mock_basic_config.assert_called_with(level=expected_level, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
