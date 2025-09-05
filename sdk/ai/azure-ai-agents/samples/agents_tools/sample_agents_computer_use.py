fc68a1409257bdb345b88c3deeec415d2eff71b8
Glenn Harper <glharper@microsoft.com>
2025-09-05 14:49:42 -0700

    Update computer use sample, add fake screenshots

--- sdk/ai/azure-ai-agents/samples/agents_tools/sample_agents_computer_use.py
+++ sdk/ai/azure-ai-agents/samples/agents_tools/sample_agents_computer_use.py
@@ -7,7 +7,9 @@
 """
 DESCRIPTION:
     This sample demonstrates how to use agent operations with the Computer Use tool (preview)
-    using a synchronous client.
+    using a synchronous client. This sample uses fake screenshot to demonstrate how output actions work,
+    but the actual implementation would involve mapping the output action types to their corresponding
+    API calls in the user's preferred managed environment framework (e.g. Playwright or Docker).
 
 USAGE:
     python sample_agents_computer_use.py
@@ -30,17 +32,20 @@ USAGE:
 
 import os, time, base64
 from typing import List
-from azure.ai.agents.models._models import SubmitToolApprovalAction
+from azure.ai.agents.models._models import ComputerScreenshot, TypeAction
 from azure.ai.projects import AIProjectClient
 from azure.ai.agents.models import (
     MessageRole,
     RunStepToolCallDetails,
     RunStepComputerUseToolCall,
     ComputerUseTool,
+    ComputerToolOutput,
     MessageInputContentBlock,
     MessageImageUrlParam,
     MessageInputTextBlock,
     MessageInputImageUrlBlock,
+    RequiredComputerUseToolCall,
+    SubmitToolOutputsAction
 )
 from azure.identity import DefaultAzureCredential
 
@@ -63,13 +68,13 @@ def image_to_base64(image_path: str) -> str:
     except Exception as exc:
         raise OSError(f"Error reading file '{image_path}'") from exc
 
-asset_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "./cua_screenshot.jpg"))
-endpoint = ""
-project_client = AIProjectClient(endpoint=endpoint, credential=DefaultAzureCredential())
+asset_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../assets/cua_screenshot.jpg"))
+action_result_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../assets/cua_screenshot_next.jpg"))
+project_client = AIProjectClient(endpoint=os.environ["PROJECT_ENDPOINT"], credential=DefaultAzureCredential())
 
 # Initialize Computer Use tool with a browser-sized viewport
 environment = os.environ.get("COMPUTER_USE_ENVIRONMENT", "windows")
-computer_use = ComputerUseTool(display_width=1026, display_height=768, environment=environment)
+computer_use = ComputerUseTool(display_width=1026, display_height=769, environment=environment)
 
 with project_client:
 
@@ -77,8 +82,7 @@ with project_client:
 
     # Create a new Agent that has the Computer Use tool attached.
     agent = agents_client.create_agent(
-        #model=os.environ["MODEL_DEPLOYMENT_NAME"],
-        model="computer-use-preview",
+        model=os.environ["MODEL_DEPLOYMENT_NAME"],
         name="my-agent-computer-use",
         instructions="""
             You are an computer automation assistant. 
@@ -114,24 +118,65 @@ with project_client:
     run = agents_client.runs.create(thread_id=thread.id, agent_id=agent.id)
     print(f"Created run, ID: {run.id}")
 
+    #create a fake screenshot showing the text typed in
+    result_image_base64 = image_to_base64(action_result_file_path)
+    result_img_url = f"data:image/jpeg;base64,{result_image_base64}"
+    computer_screenshot = ComputerScreenshot(image_url=result_img_url)
+
     while run.status in ["queued", "in_progress", "requires_action"]:
         time.sleep(1)
         run = agents_client.runs.get(thread_id=thread.id, run_id=run.id)
 
-        if run.status == "requires_action":# and isinstance(run.required_action, SubmitToolApprovalAction):
+        if run.status == "requires_action" and isinstance(run.required_action, SubmitToolOutputsAction):
             print("Run requires action:")
-            print(run)
+            tool_calls = run.required_action.submit_tool_outputs.tool_calls
+            if not tool_calls:
+                print("No tool calls provided - cancelling run")
+                agents_client.runs.cancel(thread_id=thread.id, run_id=run.id)
+                break
+
+            tool_outputs = []
+            for tool_call in tool_calls:
+                if isinstance(tool_call, RequiredComputerUseToolCall):
+                    print(tool_call)
+                    try:
+                        action = tool_call.computer_use_preview.action
+                        print(f"Executing computer use action: {action.type}")
+                        if isinstance(action, TypeAction):
+                            print(f"  Text to type: {action.text}")
+                            #add hook to input text in managed environment API here
+                            
+                            #add a fake screenshot showing the text typed in
+                            tool_outputs.append(
+                                ComputerToolOutput(
+                                    tool_call_id=tool_call.id,
+                                    output=computer_screenshot
+                                )
+                            )
+                        if isinstance(action, ComputerScreenshot):
+                            print(f"  Screenshot requested")
+                            #add hook to take screenshot in managed environment API here
+                            
+                            #add a fake screenshot showing the text typed in
+                            tool_outputs.append(
+                                ComputerToolOutput(
+                                    tool_call_id=tool_call.id,
+                                    output=computer_screenshot
+                                )
+                            )
+                    except Exception as e:
+                        print(f"Error executing tool_call {tool_call.id}: {e}")
+
+            print(f"Tool outputs: {tool_outputs}")
+            if tool_outputs:
+                agents_client.runs.submit_tool_outputs(thread_id=thread.id, run_id=run.id, tool_outputs=tool_outputs)
+
 
         print(f"Current run status: {run.status}")
 
     print(f"Run completed with status: {run.status}")
     if run.status == "failed":
         print(f"Run failed: {run.last_error}")
-    # Create and process agent run in thread with tools
-    #print("Waiting for Agent run to complete. Please wait...")
-    #run = agents_client.runs.create_and_process(thread_id=thread.id, agent_id=agent.id)
-    #print(f"Run finished with status: {run.status}")
-
 
     # Fetch run steps to get the details of the agent run
     run_steps = agents_client.run_steps.list(thread_id=thread.id, run_id=run.id)
@@ -150,9 +195,6 @@ with project_client:
                 if isinstance(call, RunStepComputerUseToolCall):
                     details = call.computer_use_preview
                     print(f"    Computer use action type: {details.action.type}")
-                    # The output is a ComputerScreenshot, which may include a file_id or image_url
-                    print(f"    Screenshot file_id: {call.output.file_id}")
-                    print(f"    Screenshot image_url: {call.output.image_url}")
 
                 print()  # extra newline between tool calls
 
@@ -161,4 +203,3 @@ with project_client:
     # Optional: Delete the agent once the run is finished.
     agents_client.delete_agent(agent.id)
     print("Deleted agent")
-
