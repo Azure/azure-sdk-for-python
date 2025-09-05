c0a85177cefc477405a163a8c8a8397ac311b36a
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2025-09-05 12:00:08 -0700

    Update async sample

--- sdk/ai/azure-ai-agents/samples/agents_async/sample_agents_json_schema_response_format_using_pydantic_async.py
+++ sdk/ai/azure-ai-agents/samples/agents_async/sample_agents_json_schema_response_format_using_pydantic_async.py
@@ -6,13 +6,14 @@
 """
 DESCRIPTION:
     This sample demonstrates how to use agents with JSON schema output format.
+    It uses the Pydantic package to define the output JSON schema from Python classes.
 
 USAGE:
-    python sample_agents_json_schema_async.py
+    python sample_agents_json_schema_response_format_using_pydantic_async.py
 
     Before running the sample:
 
-    pip install azure-ai-projects azure-ai-agents azure-identity pydantic
+    pip install azure-ai-projects azure-ai-agents azure-identity aiohttp pydantic
 
     Set these environment variables with your own values:
     1) PROJECT_ENDPOINT - The Azure AI Project endpoint, as found in the Overview
@@ -20,18 +21,15 @@ USAGE:
     2) MODEL_DEPLOYMENT_NAME - The deployment name of the AI model, as found under the "Name" column in
        the "Models + endpoints" tab in your Azure AI Foundry project.
 """
-
 import asyncio
 import os
 
 from enum import Enum
-from pydantic import BaseModel, TypeAdapter
+from pydantic import BaseModel
 from azure.ai.projects.aio import AIProjectClient
-from azure.identity.aio import DefaultAzureCredential
+from azure.identity.aio  import DefaultAzureCredential
 from azure.ai.agents.models import (
     ListSortOrder,
-    MessageTextContent,
-    MessageRole,
     ResponseFormatJsonSchema,
     ResponseFormatJsonSchemaType,
     RunStatus,
@@ -39,16 +37,22 @@ from azure.ai.agents.models import (
 
 
 # Create the pydantic model to represent the planet names and there masses.
-class Planets(str, Enum):
-    Earth = "Earth"
-    Mars = "Mars"
-    Jupyter = "Jupyter"
-
+class PlanetName(str, Enum):
+    Mercury = "Mercury",
+    Venus= "Venus",
+    Earth = "Earth",
+    Mars = "Mars",
+    Jupiter = "Jupiter",
+    Saturn = "Saturn",
+    Uranus = "Uranus",
+    Neptune = "Neptune"
 
 class Planet(BaseModel):
-    planet: Planets
+    name: PlanetName
     mass: float
 
+class Planets(BaseModel):
+    planets: list[Planet]
 
 async def main():
     project_client = AIProjectClient(
@@ -62,12 +66,12 @@ async def main():
         agent = await agents_client.create_agent(
             model=os.environ["MODEL_DEPLOYMENT_NAME"],
             name="my-agent",
-            instructions="Extract the information about planets.",
+            instructions="You are helpful agent. Your response is JSON formatted.",
             response_format=ResponseFormatJsonSchemaType(
                 json_schema=ResponseFormatJsonSchema(
                     name="planet_mass",
-                    description="Extract planet mass.",
-                    schema=Planet.model_json_schema(),
+                    description="Masses of Solar System planets.",
+                    schema=Planets.model_json_schema(),
                 )
             ),
         )
@@ -79,7 +83,7 @@ async def main():
         message = await agents_client.messages.create(
             thread_id=thread.id,
             role="user",
-            content=("The mass of the Mars is 6.4171E23 kg; the mass of the Earth is 5.972168E24 kg;"),
+            content="Hello, give me a list of planets in our solar system, and their mass in kilograms.",
         )
         print(f"Created message, message ID: {message.id}")
 
@@ -96,13 +100,11 @@ async def main():
             order=ListSortOrder.ASCENDING,
         )
 
+        messages = agents_client.messages.list(thread_id=thread.id, order=ListSortOrder.ASCENDING)
         async for msg in messages:
-            if msg.role == MessageRole.AGENT:
-                last_part = msg.content[-1]
-                if isinstance(last_part, MessageTextContent):
-                    planet = TypeAdapter(Planet).validate_json(last_part.text.value)
-                    print(f"The mass of {planet.planet} is {planet.mass} kg.")
-
+            if msg.text_messages:
+                last_text = msg.text_messages[-1]
+                print(f"{msg.role}: {last_text.text.value}")
 
 if __name__ == "__main__":
     asyncio.run(main())
