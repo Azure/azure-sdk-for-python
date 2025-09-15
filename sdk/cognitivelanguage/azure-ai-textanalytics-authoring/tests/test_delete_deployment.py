8b18ddc255a688337638f845d4fde2d87bd29f06
Amber Chen (Centific Technologies Inc) <v-amberchen@microsoft.com>
2025-09-15 14:39:00 -0700

    update test for delete deployment and update assets.json

--- sdk/cognitivelanguage/azure-ai-textanalytics-authoring/tests/test_delete_deployment.py
+++ sdk/cognitivelanguage/azure-ai-textanalytics-authoring/tests/test_delete_deployment.py
@@ -27,7 +27,7 @@ class TestConversationsDeleteDeploymentSync(TestConversations):
         client = self.create_client(authoring_endpoint, authoring_key)
 
         project_name = "single-class-project"
-        deployment_name = "deniz-model1"
+        deployment_name = "deployment0902"
 
         project_client = client.get_project_client(project_name)
 
