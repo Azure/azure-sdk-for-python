308c91975f97ea4c495d41b76791daeb23ed4be7
Simon Moreno <30335873+simorenoh@users.noreply.github.com>
2025-09-22 11:20:31 -0400

    Update _cosmos_http_logging_policy.py

--- sdk/cosmos/azure-cosmos/azure/cosmos/_cosmos_http_logging_policy.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/_cosmos_http_logging_policy.py
@@ -191,9 +191,8 @@ def _get_client_settings(global_endpoint_manager: Optional[_GlobalEndpointManage
     client_settings = {"Preferred Regions": client_preferred_regions,
                        "Excluded Regions": client_excluded_regions,
                        "Account Read Regions": client_account_read_regions,
-                       "Account Write Regions": client_account_write_regions}
-    if global_endpoint_manager.is_per_partition_automatic_failover_enabled():
-        client_settings["Per Partition Automatic Failover"] = True
+                       "Account Write Regions": client_account_write_regions,
+                       "Per-Partition Automatic Failover Enabled": global_endpoint_manager.is_per_partition_automatic_failover_enabled()}
     if client_settings and isinstance(client_settings, dict):
         logger_str += ''.join([f"\t{k}: {v}\n" for k, v in client_settings.items()])
     return logger_str
