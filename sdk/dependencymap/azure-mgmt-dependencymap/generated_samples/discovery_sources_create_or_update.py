86c0065a62f863d2b4ca419c5a3b8c05916ddd35
azure-sdk <azuresdk@microsoft.com>
2025-09-23 16:57:32 +0000

    Configurations:  'specification/azuredependencymap/DependencyMap.Management/tspconfig.yaml', API Version: 2025-07-01-preview, SDK Release Type: beta, and CommitSHA: '1006dcb616b179a62d6517d55fe44371aff4a575' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5373686 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/dependencymap/azure-mgmt-dependencymap/generated_samples/discovery_sources_create_or_update.py
+++ sdk/dependencymap/azure-mgmt-dependencymap/generated_samples/discovery_sources_create_or_update.py
@@ -8,7 +8,7 @@
 
 from azure.identity import DefaultAzureCredential
 
-from azure.mgmt.dependencymap import DependencyMapMgmtClient
+from azure.mgmt.dependencymap import DependencyMapClient
 
 """
 # PREREQUISITES
@@ -25,7 +25,7 @@ from azure.mgmt.dependencymap import DependencyMapMgmtClient
 
 
 def main():
-    client = DependencyMapMgmtClient(
+    client = DependencyMapClient(
         credential=DefaultAzureCredential(),
         subscription_id="SUBSCRIPTION_ID",
     )
@@ -36,13 +36,13 @@ def main():
         source_name="sourceTest1",
         resource={
             "location": "y",
-            "properties": {"sourceId": "wzlrkzumplzjmixbqv", "sourceType": "DiscoverySourceResourceProperties"},
+            "properties": {"sourceId": "wzlrkzumplzjmixbqv", "sourceType": "OffAzure"},
             "tags": {},
         },
     ).result()
     print(response)
 
 
-# x-ms-original-file: 2025-01-31-preview/DiscoverySources_CreateOrUpdate.json
+# x-ms-original-file: 2025-07-01-preview/DiscoverySources_CreateOrUpdate.json
 if __name__ == "__main__":
     main()
