1e49d4b676341632cf24cb7e1cd87c5490e83db8
Salma Elshafey <selshafey@microsoft.com>
2025-09-28 21:26:47 +0300

    Remove Parameter Correctness Evaluator

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_paramater_correctness/__init__.py
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-from ._parameter_correctness import ParameterCorrectnessEvaluator
-
-__all__ = [
-    "ParameterCorrectnessEvaluator",
-]
