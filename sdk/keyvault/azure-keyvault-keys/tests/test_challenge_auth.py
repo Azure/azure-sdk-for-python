8ea565e589e40b21b49d343bfede07eb6d818262
mccoyp <mcpatino@microsoft.com>
2025-09-30 18:35:35 +0000

    Remove import

--- sdk/keyvault/azure-keyvault-keys/tests/test_challenge_auth.py
+++ sdk/keyvault/azure-keyvault-keys/tests/test_challenge_auth.py
@@ -7,7 +7,6 @@ Tests for the HTTP challenge authentication implementation. These tests aren't p
 the challenge cache is global to the process.
 """
 import base64
-import copy
 import functools
 from itertools import product
 import os
