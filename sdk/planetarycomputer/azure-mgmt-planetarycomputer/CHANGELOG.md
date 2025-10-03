e61b3b6f299a360079c6353a5685105ec52f4563
Youssef Chahibi <106792935+chahibi@users.noreply.github.com>
2025-10-03 14:15:14 +0100

    Address pipeline errors

--- sdk/planetarycomputer/azure-mgmt-planetarycomputer/CHANGELOG.md
+++ sdk/planetarycomputer/azure-mgmt-planetarycomputer/CHANGELOG.md
@@ -1,5 +1,12 @@
 # Release History
 
+## 1.0.0b11 (2025-10-03)
+
+### Other Changes
+
+  - Address SDK architecture review comments
+
+
 ## 1.0.0b1 (2025-05-20)
 
 ### Other Changes
--- sdk/planetarycomputer/azure-planetarycomputer/samples/stac_queryables_create.py
+++ sdk/planetarycomputer/azure-planetarycomputer/samples/stac_queryables_create.py
@@ -9,6 +9,7 @@
 from azure.identity import DefaultAzureCredential
 
 from azure.planetarycomputer import PlanetaryComputerClient
+from azure.planetarycomputer.models import StacQueryable, StacQueryableDefinitionDataType
 
 """
 # PREREQUISITES
@@ -29,14 +30,18 @@ def main():
         credential=DefaultAzureCredential(),
     )
 
+    queryable = StacQueryable(
+        name="eo:cloud_cover",
+        data_type=StacQueryableDefinitionDataType.NUMBER,
+        create_index=False,
+        definition={
+            "data_type": StacQueryableDefinitionDataType.NUMBER,
+        },
+    )
+
     response = client.stac.create_queryables(
         collection_id="36fcb8da-9b15-49e0-b400-0d2e751e2061",
-        body=[
-            {
-                "definition": {"data_type": "number", "description": "Like cloud cover, but with more party tricks"},
-                "name": "eo:clown_cover",
-            }
-        ],
+        body=[queryable],
     )
     print(response)
 
