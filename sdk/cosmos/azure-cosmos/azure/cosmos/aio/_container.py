07b0257c25efd0830e902ed8f3c828e7be093efd
Andrew Mathew <andrewmathew@microsoft.com>
2025-10-03 16:10:19 -0400

    fixes for pylint and pipelines

--- sdk/cosmos/azure-cosmos/azure/cosmos/aio/_container.py
+++ sdk/cosmos/azure-cosmos/azure/cosmos/aio/_container.py
@@ -1411,7 +1411,7 @@ class ContainerProxy:
         self,
         throughput: Union[int, ThroughputProperties],
         *,
-        response_hook: Optional[Callable[[Mapping[str, Any], List[Dict[str, Any]]], None]] = None,
+        response_hook: Optional[Callable[[Mapping[str, Any], List[CosmosDict]], None]] = None,
         **kwargs: Any
     ) -> ThroughputProperties:
         """Replace the container's throughput.
@@ -1421,7 +1421,7 @@ class ContainerProxy:
         :param throughput: The throughput to be set.
         :type throughput: Union[int, ~azure.cosmos.ThroughputProperties]
         :keyword response_hook: A callable invoked with the response metadata.
-        :paramtype response_hook: Callable[[Dict[str, str], Dict[str, Any]], None]
+        :paramtype response_hook: Callable[[Dict[str, str], CosmosDict], None]
         :raises ~azure.cosmos.exceptions.CosmosHttpResponseError: No throughput properties exist for the container
             or the throughput properties could not be updated.
         :returns: ThroughputProperties for the container, updated with new throughput.
@@ -1442,7 +1442,7 @@ class ContainerProxy:
         _replace_throughput(throughput=throughput, new_throughput_properties=new_offer)
         data = await self.client_connection.ReplaceOffer(offer_link=throughput_properties[0]["_self"],
                                                          offer=throughput_properties[0], **kwargs)
-        
+
         if response_hook:
             response_hook(self.client_connection.last_response_headers, data)
 
