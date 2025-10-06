eec0723155461bc5c40090028569002d5d4291e5
Kashif Khan <kashifkhan@microsoft.com>
2025-10-06 18:19:43 +0000

    new generated files

--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/aio/operations/__init__.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/aio/operations/__init__.py
@@ -12,17 +12,17 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ._patch import *  # pylint: disable=unused-wildcard-import
 
-from ._client import ContainerRegistryClient  # type: ignore
+from ._operations import ContainerRegistryOperations # type: ignore
+from ._operations import ContainerRegistryBlobOperations # type: ignore
+from ._operations import AuthenticationOperations # type: ignore
 
-try:
-    from ._patch import __all__ as _patch_all
-    from ._patch import *
-except ImportError:
-    _patch_all = []
+from ._patch import __all__ as _patch_all
+from ._patch import *
 from ._patch import patch_sdk as _patch_sdk
 __all__ = [
-    'ContainerRegistryClient',
+    'ContainerRegistryOperations',
+    'ContainerRegistryBlobOperations',
+    'AuthenticationOperations',
 ]
 __all__.extend([p for p in _patch_all if p not in __all__]) # pyright: ignore
-
 _patch_sdk()
