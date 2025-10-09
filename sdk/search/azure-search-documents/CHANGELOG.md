b1c3bdaddaf01f8f42eee08701185c6fdd3d0356
azure-sdk <azuresdk@microsoft.com>
2025-10-09 22:11:20 +0000

    Increment package version after release of azure-search-documents

--- sdk/search/azure-search-documents/CHANGELOG.md
+++ sdk/search/azure-search-documents/CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release History
 
+## 11.6.1 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
+
 ## 11.6.0 (2025-10-10)
 
 ### Features Added
--- sdk/search/azure-search-documents/azure/search/documents/_version.py
+++ sdk/search/azure-search-documents/azure/search/documents/_version.py
@@ -3,6 +3,6 @@
 # Licensed under the MIT License.
 # ------------------------------------
 
-VERSION = "11.6.0"  # type: str
+VERSION = "11.6.1"  # type: str
 
 SDK_MONIKER = "search-documents/{}".format(VERSION)  # type: str
