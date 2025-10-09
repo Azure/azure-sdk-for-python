98dbd15db50c9b5fa449c134d0cd79e8b20d7e65
ahibrahim <ahibrahim@microsoft.com>
2025-10-09 23:57:44 +0300

    Revert "dummy commit"
    This reverts commit f22ebc0b2e53ba0df29b788c80552ee5cab4e0e7.

--- sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_common/_base_eval.py
+++ sdk/evaluation/azure-ai-evaluation/azure/ai/evaluation/_evaluators/_common/_base_eval.py
@@ -642,7 +642,6 @@ class EvaluatorBase(ABC, Generic[T_EvalValue]):
         # Otherwise, aggregate results.
         return self._aggregate_results(per_turn_results=per_turn_results)
 
-
     # ~~~ METHODS THAT SHOULD NOT BE OVERRIDDEN BY CHILDREN~~~``
 
     @final
