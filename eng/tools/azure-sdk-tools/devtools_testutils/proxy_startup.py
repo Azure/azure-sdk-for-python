c587cef26f822558663177092be549b8f384f213
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2025-10-14 13:36:41 -0700

    Allow certificate bundling to skip if the proxy url isn't https (#43424)

--- eng/tools/azure-sdk-tools/devtools_testutils/proxy_startup.py
+++ eng/tools/azure-sdk-tools/devtools_testutils/proxy_startup.py
@@ -341,7 +341,10 @@ def start_test_proxy(request) -> None:
     """
 
     repo_root = ascend_to_root(request.node.items[0].module.__file__)
-    check_certificate_location(repo_root)
+    requires_https = PROXY_URL.startswith("https://")
+
+    if requires_https:
+        check_certificate_location(repo_root)
 
     if not PROXY_MANUALLY_STARTED:
         if check_availability() == 200:
@@ -367,13 +370,17 @@ def start_test_proxy(request) -> None:
                 _LOGGER.info("Downloading and starting standalone proxy executable...")
                 tool_name = prepare_local_tool(root)
 
-            # Always start the proxy with these two defaults set to allow SSL connection
-            passenv = {
-                "ASPNETCORE_Kestrel__Certificates__Default__Path": os.path.join(
-                    root, "eng", "common", "testproxy", "dotnet-devcert.pfx"
-                ),
-                "ASPNETCORE_Kestrel__Certificates__Default__Password": "password",
-            }
+            if requires_https:
+                # Always start the proxy with these two defaults set to allow SSL connection
+                passenv = {
+                    "ASPNETCORE_Kestrel__Certificates__Default__Path": os.path.join(
+                        root, "eng", "common", "testproxy", "dotnet-devcert.pfx"
+                    ),
+                    "ASPNETCORE_Kestrel__Certificates__Default__Password": "password",
+                }
+            else:
+                passenv = {}
+
             # If they are already set, override what we give the proxy with what is in os.environ
             passenv.update(os.environ)
 
