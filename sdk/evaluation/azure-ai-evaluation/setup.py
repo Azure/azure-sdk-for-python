36e45f659a28f8af51b1c6b07492b93726385adf
Neehar Duvvuri <neeharduvvuri@Neehars-MacBook-Pro.local>
2025-10-20 11:16:06 -0400

    pin otel<1.39.0 since breaking change coming in that version

--- sdk/evaluation/azure-ai-evaluation/setup.py
+++ sdk/evaluation/azure-ai-evaluation/setup.py
@@ -85,7 +85,7 @@ setup(
     ],
     extras_require={
         "redteam": ['pyrit==0.8.1;python_version>="3.10"', 'duckdb==1.3.2;python_version>="3.10"'],
-        "opentelemetry": ["opentelemetry-sdk>=1.17.0", "azure-monitor-opentelemetry-exporter>=1.0.0b17"],
+        "opentelemetry": ["opentelemetry-sdk>=1.17.0,<1.39.0", "azure-monitor-opentelemetry-exporter>=1.0.0b17"],
     },
     project_urls={
         "Bug Reports": "https://github.com/Azure/azure-sdk-for-python/issues",
