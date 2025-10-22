d9bc043cbd917fa32e99a16d0cbf75058dea73b6
Yuchao Yan <yuchaoyan@microsoft.com>
2025-10-22 11:20:34 +0800

    update

--- sdk/keyvault/azure-mgmt-keyvault/azure/mgmt/keyvault/aio/operations/_operations.py
+++ sdk/keyvault/azure-mgmt-keyvault/azure/mgmt/keyvault/aio/operations/_operations.py
@@ -1366,7 +1366,9 @@ class VaultsOperations:
         _headers = kwargs.pop("headers", {}) or {}
         _params = kwargs.pop("params", {}) or {}
 
-        filter: Literal["resourceType eq 'Microsoft.KeyVault/vaults'"] = kwargs.pop("filter")
+        filter: Literal["resourceType eq 'Microsoft.KeyVault/vaults'"] = kwargs.pop(
+            "filter", _params.pop("$filter", "resourceType eq 'Microsoft.KeyVault/vaults'")
+        )
         api_version: Literal["2015-11-01"] = kwargs.pop("api_version", _params.pop("api-version", "2015-11-01"))
         cls: ClsType[List[_models.TrackedResource]] = kwargs.pop("cls", None)
 
@@ -1405,7 +1407,7 @@ class VaultsOperations:
                         for key, value in urllib.parse.parse_qs(_parsed_next_link.query).items()
                     }
                 )
-                _next_request_params["api-version"] = api_version
+                _next_request_params["api-version"] = self._config.api_version
                 _request = HttpRequest(
                     "GET", urllib.parse.urljoin(next_link, _parsed_next_link.path), params=_next_request_params
                 )
