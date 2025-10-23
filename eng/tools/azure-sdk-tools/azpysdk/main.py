29dc40582e27ae089c383ba4c2da52c11f26ba35
Scott Beddall <scbedd@microsoft.com>
2025-10-23 01:34:50 +0000

    remove whl until it's ready for primetime

--- eng/tools/azure-sdk-tools/azpysdk/main.py
+++ eng/tools/azure-sdk-tools/azpysdk/main.py
@@ -25,7 +25,6 @@ from .pyright import pyright
 from .next_pyright import next_pyright
 from .ruff import ruff
 from .verifytypes import verifytypes
-from .verify_whl import verify_whl
 from .verify_sdist import verify_sdist
 from .bandit import bandit
 from .verify_keywords import verify_keywords
@@ -86,7 +85,6 @@ def build_parser() -> argparse.ArgumentParser:
     ruff().register(subparsers, [common])
     verifytypes().register(subparsers, [common])
     verify_sdist().register(subparsers, [common])
-    verify_whl().register(subparsers, [common])
     bandit().register(subparsers, [common])
     verify_keywords().register(subparsers, [common])
 
