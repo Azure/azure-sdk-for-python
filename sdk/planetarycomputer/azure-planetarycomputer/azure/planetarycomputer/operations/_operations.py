fc84cd1a9aaf5224b0a35127e9702d7149072ca6
Youssef Chahibi <106792935+chahibi@users.noreply.github.com>
2025-10-28 23:11:29 +0000

    Formatting

--- sdk/planetarycomputer/azure-planetarycomputer/azure/planetarycomputer/operations/_operations.py
+++ sdk/planetarycomputer/azure-planetarycomputer/azure/planetarycomputer/operations/_operations.py
@@ -33,7 +33,7 @@ from azure.core.utils import case_insensitive_dict
 
 from .. import models as _models
 from .._configuration import PlanetaryComputerClientConfiguration
-from .._utils.model_base import Model as _Model, SdkJSONEncoder, _deserialize, _deserialize_xml
+from .._utils.model_base import Model as _Model, SdkJSONEncoder, _deserialize
 from .._utils.serialization import Deserializer, Serializer
 from .._utils.utils import prepare_multipart_form_data
 
