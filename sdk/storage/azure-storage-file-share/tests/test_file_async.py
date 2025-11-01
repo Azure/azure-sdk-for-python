a45d44514c17eb5fbdd88bd5a49fa9318dd59985
Peter Wu <weirongwu@microsoft.com>
2025-11-01 07:50:39 -0400

    File Share + ADLS download decompression test

--- sdk/storage/azure-storage-file-share/tests/test_file_async.py
+++ sdk/storage/azure-storage-file-share/tests/test_file_async.py
@@ -4050,3 +4050,27 @@ class TestStorageFileAsync(AsyncStorageRecordedTestCase):
             assert e.value.response.headers["x-ms-copy-source-error-code"] == "NoAuthenticationInformation"
         finally:
             await self.fsc.delete_share(self.share_name)
+
+    @pytest.mark.live_test_only
+    @FileSharePreparer()
+    async def test_download_file_decompress(self, **kwargs):
+        storage_account_name = kwargs.pop("storage_account_name")
+        storage_account_key = kwargs.pop("storage_account_key")
+
+        self._setup(storage_account_name, storage_account_key)
+        file_name = self._get_file_reference()
+        file_client = ShareFileClient(
+            self.account_url(storage_account_name, "file"),
+            share_name=self.share_name,
+            file_path=file_name,
+            credential=storage_account_key,
+            max_range_size=4 * 1024
+        )
+        compressed_data = b'\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xcaH\xcd\xc9\xc9WH+\xca\xcfUH\xaf\xca,\x00\x00\x00\x00\xff\xff\x03\x00d\xaa\x8e\xb5\x0f\x00\x00\x00'
+        decompressed_data = b"hello from gzip"
+        content_settings = ContentSettings(content_encoding='gzip')
+
+        # Act / Assert
+        await file_client.upload_file(data=compressed_data, content_settings=content_settings)
+        await self.assertFileEqual(file_client, decompressed_data, decompress=True)
+        await self.assertFileEqual(file_client, compressed_data, decompress=False)
