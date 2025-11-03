c66a3c96c4d2f5e0705224616bf7a4d56acffb58
azure-sdk <PythonSdkPipelines>
2025-11-03 09:18:19 +0000

    code and test

--- sdk/containerregistry/azure-mgmt-containerregistry/generated_samples/managed_identity/tasks_update_with_login_identity.py
+++ sdk/containerregistry/azure-mgmt-containerregistry/generated_samples/managed_identity/tasks_update_with_login_identity.py
@@ -58,6 +58,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2025-03-01-preview/examples/ManagedIdentity/TasksUpdate_WithLoginIdentity.json
+# x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/RegistryTasks/preview/2025-03-01-preview/examples/ManagedIdentity/TasksUpdate_WithLoginIdentity.json
 if __name__ == "__main__":
     main()
