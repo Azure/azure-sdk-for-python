879142f95c664813acef95e1f003d2dfeb6b7a99
Yuchao Yan <yuchaoyan@microsoft.com>
2025-11-03 15:30:58 +0800

    Update serialization.py

--- sdk/core/azure-core/azure/core/serialization.py
+++ sdk/core/azure-core/azure/core/serialization.py
@@ -379,7 +379,7 @@ def attribute_list(obj: Any) -> List[str]:
         if flattened_attribute == attr_name:
             retval.extend(attribute_list(rest_field._class_type))  # pylint: disable=protected-access
         else:
-            rest_name_from_attr_name = ''.join(w.capitalize() if i else w for i, w in enumerate(attr_name.split('_')))
+            rest_name_from_attr_name = convert_to_camel_case(attr_name)
             if f"{rest_field._rest_name}Property" == rest_name_from_attr_name:
                 retval.append(attr_name.removesuffix("_property"))
             else:
