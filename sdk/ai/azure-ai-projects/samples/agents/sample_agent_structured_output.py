2fa8a798d18a71601fb21e424a657a9d0cf20ee9
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2025-11-06 20:10:15 -0800

    First sample

---
+++ sdk/ai/azure-ai-projects/samples/agents/sample_agent_structured_output.py
@@ -0,0 +1,87 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+"""
+DESCRIPTION:
+    This sample demonstrates how to run basic Prompt Agent operations
+    using the synchronous AIProjectClient, while defining a desired
+    JSON schema for the response ("structured output").
+
+    The OpenAI compatible Responses and Conversation calls in this sample are made using
+    the OpenAI client from the `openai` package. See https://platform.openai.com/docs/api-reference
+    for more information.
+
+    This sample is inspired from the OpenAI example here:
+    https://platform.openai.com/docs/guides/structured-outputs/supported-schemas
+
+USAGE:
+    python sample_agent_structured_output.py
+
+    Before running the sample:
+
+    pip install "azure-ai-projects>=2.0.0b1" openai azure-identity python-dotenv pydantic
+
+    Set these environment variables with your own values:
+    1) AZURE_AI_PROJECT_ENDPOINT - The Azure AI Project endpoint, as found in the Overview
+       page of your Azure AI Foundry portal.
+    2) AZURE_AI_MODEL_DEPLOYMENT_NAME - The deployment name of the AI model, as found under the "Name" column in
+       the "Models + endpoints" tab in your Azure AI Foundry project.
+"""
+
+import os
+from dotenv import load_dotenv
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from azure.ai.projects.models import PromptAgentDefinition, PromptAgentDefinitionText, ResponseTextFormatConfigurationJsonSchema
+from pydantic import BaseModel
+
+load_dotenv()
+
+class CalendarEvent(BaseModel):
+    model_config = {"extra": "forbid"}
+    name: str
+    date: str
+    participants: list[str]
+
+project_client = AIProjectClient(
+    endpoint=os.environ["AZURE_AI_PROJECT_ENDPOINT"],
+    credential=DefaultAzureCredential(),
+)
+
+with project_client:
+
+    openai_client = project_client.get_openai_client()
+
+    agent = project_client.agents.create_version(
+        agent_name="MyAgent",
+        definition=PromptAgentDefinition(
+            model=os.environ["AZURE_AI_MODEL_DEPLOYMENT_NAME"],
+            #BUG? text=PromptAgentDefinitionText(format=ResponseTextFormatConfigurationJsonSchema(name="CalendarEvent", schema=CalendarEvent.model_json_schema())),
+            text=PromptAgentDefinitionText(format={"type": "json_schema", "name": "CalendarEvent", "schema": CalendarEvent.model_json_schema()}),
+            instructions="""
+                You are a helpful assistant that extracts calendar event information from the input user messages,
+                and returns it in the desired structured output format.
+            """,
+        ),
+    )
+    print(f"Agent created (id: {agent.id}, name: {agent.name}, version: {agent.version})")
+
+    conversation = openai_client.conversations.create(
+        items=[{"type": "message", "role": "user", "content": "Alice and Bob are going to a science fair on Friday."}],
+    )
+    print(f"Created conversation with initial user message (id: {conversation.id})")
+
+    response = openai_client.responses.create(
+        conversation=conversation.id,
+        extra_body={"agent": {"name": agent.name, "type": "agent_reference"}},
+        input="",
+    )
+    print(f"Response output: {response.output_text}")
+
+    openai_client.conversations.delete(conversation_id=conversation.id)
+    print("Conversation deleted")
+
+    project_client.agents.delete_version(agent_name=agent.name, agent_version=agent.version)
+    print("Agent deleted")
