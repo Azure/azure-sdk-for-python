d11cafee611b32e8f9b9e4b509be2391f6e5a8bb
Kashif Khan <kashifkhan@microsoft.com>
2025-11-10 10:48:46 -0600

    initial generated client

--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/_generated/models/_models.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/_generated/models/_models.py
@@ -1,3 +1,4 @@
+# pylint: disable=too-many-lines
 # coding=utf-8
 # pylint: disable=too-many-lines
 # --------------------------------------------------------------------------
--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/aio/operations/__init__.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/aio/operations/__init__.py
@@ -12,21 +12,18 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ._patch import *  # pylint: disable=unused-wildcard-import
 
-from ._client import ContainerRegistry  # type: ignore
-from ._version import VERSION
+from ._operations import ContainerRegistryOperations  # type: ignore
+from ._operations import ContainerRegistryBlobOperations  # type: ignore
+from ._operations import AuthenticationOperations  # type: ignore
 
-__version__ = VERSION
-
-try:
-    from ._patch import __all__ as _patch_all
-    from ._patch import *
-except ImportError:
-    _patch_all = []
+from ._patch import __all__ as _patch_all
+from ._patch import *
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    "ContainerRegistry",
+    "ContainerRegistryOperations",
+    "ContainerRegistryBlobOperations",
+    "AuthenticationOperations",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])  # pyright: ignore
-
 _patch_sdk()
