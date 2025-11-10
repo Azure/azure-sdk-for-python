35cb39b1e168e867b83d4260ebfbd23dc0446226
Kashif Khan <kashifkhan@microsoft.com>
2025-11-10 14:49:51 -0600

    fix other endpoint

--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/aio/operations/_patch.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/aio/operations/_patch.py
@@ -93,7 +93,7 @@ class AuthenticationOperations(AuthenticationOperationsGenerated):
             params=_params,
         )
         path_format_arguments = {
-            "url": self._serialize.url("self._config.url", self._config.url, "str", skip_quote=True),
+            "url": self._serialize.url("self._config.url", self._config.endpoint, "str", skip_quote=True),
         }
         request.url = self._client.format_url(request.url, **path_format_arguments)  # type: ignore
 
@@ -166,7 +166,7 @@ class AuthenticationOperations(AuthenticationOperationsGenerated):
             params=_params,
         )
         path_format_arguments = {
-            "url": self._serialize.url("self._config.url", self._config.url, "str", skip_quote=True),
+            "url": self._serialize.url("self._config.url", self._config.endpoint, "str", skip_quote=True),
         }
         request.url = self._client.format_url(request.url, **path_format_arguments)  # type: ignore
 
