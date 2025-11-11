bf926e3b074a16d771a1b3b1f8ed32b545b046f2
Saanika Gupta <saanikagupta@microsoft.com>
2025-11-11 13:29:06 +0530

    Fix typo

--- sdk/ml/azure-ai-ml/tests/internal_utils/unittests/test_dev_requirements.py
+++ sdk/ml/azure-ai-ml/tests/internal_utils/unittests/test_dev_requirements.py
@@ -26,7 +26,7 @@ class TestPackageInstallation:
         not (IS_CPYTHON and sys.version_info >= (3, 13)),
         reason="Skipping because environment is not >= cpython 3.13",
     )
-    def test_package_not_installed_in_cpython_3_13():
+    def test_package_not_installed_in_cpython_3_13(self):
         assert not is_package_installed(
             PACKAGE_NAME
         ), f"{PACKAGE_NAME} should not be installed in CPython 3.13 or above environment."
@@ -35,14 +35,14 @@ class TestPackageInstallation:
         not (IS_CPYTHON and sys.version_info < (3, 13)),
         reason="Skipping because environment is not below cpython 3.13",
     )
-    def test_package_installed_below_cpython_3_13():
+    def test_package_installed_below_cpython_3_13(self):
         assert is_package_installed(PACKAGE_NAME), f"{PACKAGE_NAME} should be installed in CPython < 3.13."
 
     @pytest.mark.skipif(
         not (IS_PYPY and sys.version_info >= (3, 10)),
         reason="Skipping because environment is not >= pypy 3.10",
     )
-    def test_package_not_installed_in_pypy_3_10():
+    def test_package_not_installed_in_pypy_3_10(self):
         assert not is_package_installed(
             PACKAGE_NAME
         ), f"{PACKAGE_NAME} should not be installed in PyPy 3.10 or above environment."
@@ -51,5 +51,5 @@ class TestPackageInstallation:
         not (IS_PYPY and sys.version_info < (3, 10)),
         reason="Skipping because environment is not below pypy 3.10",
     )
-    def test_package_installed_below_pypy_3_10():
+    def test_package_installed_below_pypy_3_10(self):
         assert is_package_installed(PACKAGE_NAME), f"{PACKAGE_NAME} should be installed in PyPy < 3.10 environment."
