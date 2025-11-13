1d832b8e9f45d1bccecdf81e6ebe0bca28164fde
Qi Yin <v-qiyin1@microsoft.com>
2025-11-13 23:06:27 +0800

    move down the pylint tag

--- sdk/cognitivelanguage/azure-ai-language-questionanswering-authoring/azure/ai/language/questionanswering/authoring/_operations/_patch.py
+++ sdk/cognitivelanguage/azure-ai-language-questionanswering-authoring/azure/ai/language/questionanswering/authoring/_operations/_patch.py
@@ -1,9 +1,9 @@
-# pylint: disable=line-too-long,useless-suppression,duplicate-code,arguments-renamed
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------
+# pylint: disable=line-too-long,useless-suppression,duplicate-code,arguments-renamed
 """Customize generated code here.
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
