995c9c73d046f074f8474421582ed1a27ef3e7ef
catalinaperalta <caperal@microsoft.com>
2025-11-18 10:29:50 -0800

    update docs

--- sdk/maps/azure-maps-geolocation/azure/maps/geolocation/models/_patch.py
+++ sdk/maps/azure-maps-geolocation/azure/maps/geolocation/models/_patch.py
@@ -13,13 +13,13 @@ from typing import Optional
 class CountryRegionResult(object):
     """Represents coordinate latitude and longitude
 
-    :keyword ip_address:
+    :param ip_address:
         The IP Address of the request.
-    :paramtype ip_address: Optional[str]
-    :keyword iso_code: iso_code:
+    :type ip_address: Optional[str]
+    :param iso_code:
         The IP Address's 2-character code of the country or region.
         Please note, IP address in ranges reserved for special purpose will return Null for country/region.
-    :paramtype iso_code: Optional[str]
+    :type iso_code: Optional[str]
     """
 
     def __init__(self, ip_address: Optional[str] = None, iso_code: Optional[str] = None):
