5099fa4e20d9c1a0eeb096a10f64fc6a91eafffb
jennypng <63012604+JennyPng@users.noreply.github.com>
2025-11-20 17:00:18 -0800

    always use wheel

--- eng/tools/azure-sdk-tools/azpysdk/apistub.py
+++ eng/tools/azure-sdk-tools/azpysdk/apistub.py
@@ -3,21 +3,19 @@ import os
 import sys
 
 from typing import Optional, List
-import subprocess
-from subprocess import CalledProcessError, check_call
+from subprocess import CalledProcessError
 
 from .Check import Check
-from ci_tools.functions import install_into_venv, get_pip_command, find_whl
-from ci_tools.scenario.generation import create_package_and_install, get_pip_command
-from ci_tools.variables import discover_repo_root, in_ci, set_envvar_defaults
-from ci_tools.environment_exclusions import is_check_enabled
-from ci_tools.logging import logger, run_logged
+from ci_tools.functions import install_into_venv, find_whl
+from ci_tools.scenario.generation import create_package_and_install
+from ci_tools.variables import discover_repo_root, set_envvar_defaults
+from ci_tools.logging import logger
 from ci_tools.parsing import ParsedSetup
 
 REPO_ROOT = discover_repo_root()
 
 
-def get_package_wheel_path(pkg_root: str, out_path: str) -> tuple[str, Optional[str]]:
+def get_package_wheel_path(pkg_root: str, out_path: Optional[str]) -> tuple[str, Optional[str]]:
     # parse setup.py to get package name and version
     pkg_details = ParsedSetup.from_path(pkg_root)
 
@@ -37,12 +35,13 @@ def get_package_wheel_path(pkg_root: str, out_path: str) -> tuple[str, Optional[
         if out_path:
             out_token_path = os.path.join(out_path, os.path.basename(os.path.dirname(pkg_path)))
         return pkg_path, out_token_path
+
+    # Otherwise, use wheel created in staging directory, or fall back on source directory
     pkg_path = pkg_root
-    # If the package is not a wheel and out_path is given, the token file output path should be the same as the target package path
-    if out_path:            
-        out_token_path = os.path.join(out_path, os.path.basename(pkg_path))
-        if not os.path.exists(out_token_path):
-            os.makedirs(out_token_path, exist_ok=True)
+    if out_path:
+        pkg_path = find_whl(out_path, pkg_details.name, pkg_details.version) or pkg_root
+        out_token_path = os.path.join(out_path, os.path.basename(pkg_root))
+
     return pkg_path, out_token_path
 
 
@@ -66,6 +65,8 @@ class apistub(Check):
             "apistub", parents=parents, help="Run the apistub check to generate an API stub for a package"
         )
         p.set_defaults(func=self.run)
+
+        p.add_argument("-o", "--out-path", dest="out_path", help="Output directory to generate json token file")
 
     def run(self, args: argparse.Namespace) -> int:
         """Run the apistub check command."""
@@ -99,14 +100,24 @@ class apistub(Check):
                 logger.error(f"Failed to install dependencies: {e}")
                 return e.returncode
 
+            create_package_and_install(
+                distribution_directory=staging_directory,
+                target_setup=package_dir,
+                skip_install=True,
+                cache_dir=None,
+                work_dir=staging_directory,
+                force_create=False,
+                package_type="wheel",
+                pre_download_disabled=False,
+                python_executable=executable,
+            )
+
             self.pip_freeze(executable)
 
-            # Check if a wheel is already built for current package and install from wheel when available
-            # If wheel is not available then install package from source
-            pkg_path, out_token_path = get_package_wheel_path(package_dir, staging_directory)
+            pkg_path, out_token_path = get_package_wheel_path(package_dir, args.out_path)
             cross_language_mapping_path = get_cross_language_mapping_path(package_dir)
 
-            cmds = [executable, "-m", "apistub", "--pkg-path", pkg_path]
+            cmds = ["-m", "apistub", "--pkg-path", pkg_path]
 
             if out_token_path:
                 cmds.extend(["--out-path", out_token_path])
@@ -116,7 +127,7 @@ class apistub(Check):
             logger.info("Running apistubgen {}.".format(cmds))
 
             try:
-                check_call(cmds, cwd=package_dir)
+                self.run_venv_command(executable, cmds, cwd=package_dir, check=True, immediately_dump=True)
             except CalledProcessError as e:
                 logger.error(f"{package_name} exited with error {e.returncode}")
                 results.append(e.returncode)
