a14b5c9af28836a883244d9a4c3728442513f1f8
Libba Lawrence <llawrence@microsoft.com>
2025-11-24 09:59:35 -0800

    testing

--- sdk/eventgrid/azure-eventgrid/setup.py
+++ sdk/eventgrid/azure-eventgrid/setup.py
@@ -64,7 +64,7 @@ setup(
     install_requires=[
         "isodate>=0.6.1",
         "azure-core>=1.30.0",
-        "typing-extensions>=4.6.0",
+        "typing-extensions==4.6.0",
     ],
     python_requires=">=3.8",
 )
