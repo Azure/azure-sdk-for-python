03710845549b939bbca275515cc3ac79d21dd3b1
Radhika Gupta <guptaradhika@microsoft.com>
2025-12-03 16:42:32 -0800

    Unpin versions to let CI/CD tests to pass on exporter PR

--- sdk/monitor/azure-monitor-opentelemetry-exporter/setup.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/setup.py
@@ -85,8 +85,8 @@ setup(
         "azure-core<2.0.0,>=1.28.0",
         "azure-identity~=1.17",
         "msrest>=0.6.10",
-        "opentelemetry-api==1.39",
-        "opentelemetry-sdk==1.39",
+        "opentelemetry-api~=1.38",
+        "opentelemetry-sdk~=1.38",
         "psutil>=5.9,<8",
     ],
     entry_points={
