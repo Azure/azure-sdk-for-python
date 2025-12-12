f5c81cb2d6475831e5b23e379f651b31421907b1
Xiang Yan <xiangyan@microsoft.com>
2025-12-12 09:34:42 -0800

    update

--- sdk/search/azure-search-documents/azure/search/documents/models/_patch.py
+++ sdk/search/azure-search-documents/azure/search/documents/models/_patch.py
@@ -151,12 +151,6 @@ class IndexDocumentsBatch(IndexDocumentsBatchGenerated):
         else:
             self.actions.append(actions)
 
-
-__all__: list[str] = [
-    "IndexDocumentsBatch",
-]  # Add all objects you want publicly available to users at this package level
-
-
 def patch_sdk():
     """Do not remove from this file.
 
