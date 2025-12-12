9b0c944c48defd1f42c0ae75050ebbe09444f873
Xiang Yan <xiangyan@microsoft.com>
2025-12-12 12:42:46 -0800

    updates

--- sdk/search/azure-search-documents/azure/search/documents/models/_patch.py
+++ sdk/search/azure-search-documents/azure/search/documents/models/_patch.py
@@ -152,6 +152,11 @@ class IndexDocumentsBatch(IndexDocumentsBatchGenerated):
             self.actions.append(actions)
 
 
+__all__: list[str] = [
+    "RequestEntityTooLargeError",
+]  # Add all objects you want publicly available to users at this package level
+
+
 def patch_sdk():
     """Do not remove from this file.
 
