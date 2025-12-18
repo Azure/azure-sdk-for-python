eb4819524653c2f84fef1c388df86b6a3b775055
Radhika Gupta <guptaradhika@microsoft.com>
2025-12-18 15:42:48 -0800

    Fix black

--- sdk/monitor/azure-monitor-opentelemetry/tests/instrumentation/test_django.py
+++ sdk/monitor/azure-monitor-opentelemetry/tests/instrumentation/test_django.py
@@ -17,4 +17,6 @@ class TestDjangoInstrumentation(unittest.TestCase):
             DjangoInstrumentor().instrument()
         except Exception as ex:  # pylint: disable=broad-except
             print(ex)
-            self.fail(f"Unexpected exception raised when instrumenting {DjangoInstrumentor.__name__}")
+            self.fail(
+                f"Unexpected exception raised when instrumenting {DjangoInstrumentor.__name__}"
+            )
