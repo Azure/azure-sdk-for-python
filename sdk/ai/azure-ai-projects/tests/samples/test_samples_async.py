ccb7f91d5160f7994421df705b17dfa6d557c40f
Howie Leung <howieleung@microsoft.com>
2025-12-30 15:32:00 -0800

    added feature to test samples with optional env vars

--- sdk/ai/azure-ai-projects/tests/samples/test_samples_async.py
+++ sdk/ai/azure-ai-projects/tests/samples/test_samples_async.py
@@ -8,7 +8,12 @@ import pytest
 from devtools_testutils.aio import recorded_by_proxy_async
 from devtools_testutils import AzureRecordedTestCase, RecordedTransport
 from test_base import servicePreparer
-from sample_executor import AsyncSampleExecutor, get_async_sample_paths, SamplePathPasser
+from sample_executor import (
+    AdditionalTestsWithEnvironmentVariables,
+    AsyncSampleExecutor,
+    SamplePathPasser,
+    get_async_sample_paths,
+)
 from test_samples_helpers import agent_tools_instructions, get_sample_environment_variables_map
 
 
@@ -18,20 +23,31 @@ class TestSamplesAsync(AzureRecordedTestCase):
     # To run this test with a specific sample, use:
     # pytest tests/samples/test_samples_async.py::TestSamplesAsync::test_agent_tools_samples_async[sample_agent_memory_search_async]
     @servicePreparer()
+    @AdditionalTestsWithEnvironmentVariables(
+        [
+            (
+                "sample_agent_computer_use_async.py",
+                {"COMPUTER_USE_MODEL_DEPLOYMENT_NAME": "sanitized_model"},
+            ),
+        ]
+    )
     @pytest.mark.parametrize(
         "sample_path",
         get_async_sample_paths(
             "agents/tools",
-            samples_to_skip=[
-                "sample_agent_mcp_with_project_connection_async.py",
-            ],
+            samples_to_skip=["sample_agent_mcp_with_project_connection_async.py"],
         ),
     )
     @SamplePathPasser()
     @recorded_by_proxy_async(RecordedTransport.AZURE_CORE, RecordedTransport.HTTPX)
     async def test_agent_tools_samples_async(self, sample_path: str, **kwargs) -> None:
         env_var_mapping = get_sample_environment_variables_map(operation_group="agents")
-        executor = AsyncSampleExecutor(self, sample_path, env_var_mapping, **kwargs)
+        executor = AsyncSampleExecutor(
+            self,
+            sample_path,
+            env_var_mapping,
+            **kwargs,
+        )
         await executor.execute_async()
         await executor.validate_print_calls_by_llm_async(
             instructions=agent_tools_instructions,
