377ab562986a3d191d76791b6db89627dbe199b9
Lu Sun <lusu@microsoft.com>
2025-12-30 16:26:10 -0800

    updated reserved function name

--- sdk/agentserver/azure-ai-agentserver-core/azure/ai/agentserver/core/server/common/constants.py
+++ sdk/agentserver/azure-ai-agentserver-core/azure/ai/agentserver/core/server/common/constants.py
@@ -2,5 +2,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
-# Reserved function name for HITL interrupts.
-HUMAN_IN_THE_LOOP_FUNCTION_NAME = "__hosted_agent_adapter_interrupt__"
+# Reserved function name for HITL.
+HUMAN_IN_THE_LOOP_FUNCTION_NAME = "__hosted_agent_adapter_hitl__"
