06aa807fa7c26870715f8b5910c572b4d0c17217
Jeremy Voss <jeremyvoss@microsoft.com>
2026-01-14 14:23:14 -0800

    Initialize performance counters before adding processors

--- sdk/monitor/azure-monitor-opentelemetry/azure/monitor/opentelemetry/_configure.py
+++ sdk/monitor/azure-monitor-opentelemetry/azure/monitor/opentelemetry/_configure.py
@@ -133,6 +133,12 @@ def configure_azure_monitor(**kwargs) -> None:  # pylint: disable=C4758
     disable_metrics = configurations[DISABLE_METRICS_ARG]
     enable_live_metrics_config = configurations[ENABLE_LIVE_METRICS_ARG]
 
+    # Setup metrics pipeline
+    # Setup up metrics with Performance Counters before _PerformanceCountersSpanProcessor and _PerformanceCountersLogRecordProcessor
+    # This avoids a circular dependency in the case that Performance Counter setup produces a log.
+    if not disable_metrics:
+        _setup_metrics(configurations)
+
     # Setup live metrics
     if enable_live_metrics_config:
         _setup_live_metrics(configurations)
@@ -144,10 +150,6 @@ def configure_azure_monitor(**kwargs) -> None:  # pylint: disable=C4758
     # Setup logging pipeline
     if not disable_logging:
         _setup_logging(configurations)
-
-    # Setup metrics pipeline
-    if not disable_metrics:
-        _setup_metrics(configurations)
 
     # Setup instrumentations
     # Instrumentations need to be setup last so to use the global providers
