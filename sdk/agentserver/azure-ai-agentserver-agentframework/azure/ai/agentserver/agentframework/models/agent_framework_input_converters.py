7adbb6639b68d08719dbe9f65ad90dbcf0e3fb80
Lu Sun <lusu@microsoft.com>
2026-01-22 16:08:43 -0800

    fix minors

--- sdk/agentserver/azure-ai-agentserver-agentframework/azure/ai/agentserver/agentframework/models/agent_framework_input_converters.py
+++ sdk/agentserver/azure-ai-agentserver-agentframework/azure/ai/agentserver/agentframework/models/agent_framework_input_converters.py
@@ -50,13 +50,13 @@ class AgentFrameworkInputConverter:
             if agent_thread and agent_thread.message_store:
                 thread_messages = await agent_thread.message_store.list_messages()
             pending_hitl_requests = self._hitl_helper.get_pending_hitl_request(thread_messages, checkpoint)
-            logger.info("Pending HitL requests: %s", list(pending_hitl_requests.keys()))
-            hitl_response = self._hitl_helper.validate_and_convert_hitl_response(
-                input,
-                pending_requests=pending_hitl_requests)
-            logger.info("HitL response validation result: %s", [m.to_dict() for m in hitl_response])
-            if hitl_response:
-                return hitl_response
+            if pending_hitl_requests:
+                logger.info("Pending HitL requests: %s", list(pending_hitl_requests.keys()))
+                hitl_response = self._hitl_helper.validate_and_convert_hitl_response(
+                    input,
+                    pending_requests=pending_hitl_requests)
+                if hitl_response:
+                    return hitl_response
 
         return self._transform_input_internal(input)
 
