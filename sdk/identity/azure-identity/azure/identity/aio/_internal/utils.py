41fd7f0307f17c5433d4399535b925b8a6bf05a8
Paul Van Eck <paulvaneck@microsoft.com>
2026-01-29 20:28:51 +0000

    Adjust utils
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>

--- sdk/identity/azure-identity/azure/identity/aio/_internal/utils.py
+++ sdk/identity/azure-identity/azure/identity/aio/_internal/utils.py
@@ -2,31 +2,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
+import asyncio  # pylint: disable=do-not-import-asyncio
 import sys
-from functools import cache
 from typing import Type
-
-
-@cache
-def get_running_async_library() -> str:
-    """Get the name of the async library that the current context is running under.
-
-    :return: The name of the running async library.
-    :rtype: str
-    :raises RuntimeError: if the current context is not running under an async library.
-    """
-
-    try:
-        import asyncio  # pylint: disable=do-not-import-asyncio
-
-        # Check if we are running in an asyncio event loop.
-        asyncio.get_running_loop()
-        return "asyncio"
-    except RuntimeError as err:
-        # Otherwise, assume we are running in a trio event loop if it has already been imported.
-        if "trio" in sys.modules:
-            return "trio"
-        raise RuntimeError("An asyncio or trio event loop is required.") from err
 
 
 def get_running_async_lock_class() -> Type:
@@ -36,14 +14,18 @@ def get_running_async_lock_class() -> Type:
     :rtype: Type[Lock]
     :raises RuntimeError: if the current context is not running under an async library.
     """
-    if get_running_async_library() == "trio":
-        import trio  # pylint: disable=networking-import-outside-azure-core-transport,import-error
 
-        return trio.Lock
+    try:
+        # Check if we are running in an asyncio event loop.
+        asyncio.get_running_loop()
+        return asyncio.Lock
+    except RuntimeError as err:
+        # Otherwise, assume we are running in a trio event loop if it has already been imported.
+        if "trio" in sys.modules:
+            import trio  # pylint: disable=networking-import-outside-azure-core-transport,import-error
 
-    import asyncio  # pylint: disable=do-not-import-asyncio
-
-    return asyncio.Lock
+            return trio.Lock
+        raise RuntimeError("An asyncio or trio event loop is required.") from err
 
 
 def get_current_loop_id() -> int:
@@ -53,11 +35,17 @@ def get_current_loop_id() -> int:
     :rtype: int
     :raises RuntimeError: if the current context is not running under an async library.
     """
-    if get_running_async_library() == "trio":
-        import trio  # pylint: disable=networking-import-outside-azure-core-transport,import-error
+    try:
+        return id(asyncio.get_running_loop())
+    except RuntimeError:
+        pass
 
-        return id(trio.lowlevel.current_trio_token())
+    try:
+        if "trio" in sys.modules:
+            import trio  # pylint: disable=networking-import-outside-azure-core-transport,import-error
 
-    import asyncio  # pylint: disable=do-not-import-asyncio
+            return id(trio.lowlevel.current_trio_token())
+    except RuntimeError:
+        pass
 
-    return id(asyncio.get_running_loop())
+    raise RuntimeError("An asyncio or trio event loop is required.")
