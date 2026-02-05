e27f6752ff7d6d9659b4754dfde4f35cd1c975c6
Libba Lawrence <llawrence@microsoft.com>
2026-02-05 09:51:35 -0800

    have to use _raw_credential?

--- sdk/storage/azure-storage-blob/azure/storage/blob/_container_client.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/_container_client.py
@@ -167,7 +167,7 @@ class ContainerClient(StorageAccountHostsMixin, StorageEncryptionMixin):    # py
         self._client.close()
 
     def _build_generated_client(self) -> AzureBlobStorage:
-        return AzureBlobStorage(self.url, self.credential, version=self._api_version, pipeline=self._pipeline)
+        return AzureBlobStorage(self.url, credential=self._raw_credential, version=self._api_version, pipeline=self._pipeline)
 
     def _format_url(self, hostname):
         return _format_url(
