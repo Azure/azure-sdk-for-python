851953148fa3d74c7d9af886d18588d752b69282
Xiang Yan <xiangyan@microsoft.com>
2026-02-06 09:42:47 -0800

    fix pylint

--- sdk/search/azure-search-documents/azure/search/documents/models/_enums.py
+++ sdk/search/azure-search-documents/azure/search/documents/models/_enums.py
@@ -1,4 +1,5 @@
-# coding=utf-8
+# pylint: disable=enum-must-be-uppercase
+# # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
