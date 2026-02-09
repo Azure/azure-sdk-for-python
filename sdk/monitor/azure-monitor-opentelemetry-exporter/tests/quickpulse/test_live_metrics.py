6027053c1e6a3cf3ab71e7ba393c3f9d93c38afb
Radhika Gupta <guptaradhika@microsoft.com>
2026-02-09 15:56:44 -0800

    fix format

--- sdk/monitor/azure-monitor-opentelemetry-exporter/tests/quickpulse/test_live_metrics.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/tests/quickpulse/test_live_metrics.py
@@ -79,7 +79,7 @@ class TestLiveMetrics(unittest.TestCase):
     @mock.patch("azure.monitor.opentelemetry.exporter._quickpulse._live_metrics.set_statsbeat_live_metrics_feature_set")
     @mock.patch("azure.monitor.opentelemetry.exporter._quickpulse._live_metrics.get_quickpulse_manager")
     def test_enable_live_metrics_with_all_parameters(
-        self, manager_mock, statsbeat_mock   # pylint: disable=unused-argument
+        self, manager_mock, statsbeat_mock  # pylint: disable=unused-argument
     ):
         """Test enable_live_metrics with all possible parameters."""
         mock_resource = Resource.create(
