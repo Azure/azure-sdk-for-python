db7e9dfb32c6e3a4a71c0fc13b86deb8dc0629ba
Jacob Lauzon <96087589+jalauzon-msft@users.noreply.github.com>
2026-02-19 14:58:16 -0800

    [Storage][102] CRC64 content validation - part 1 (#45096)

--- eng/tools/azure-sdk-tools/devtools_testutils/storage/__init__.py
+++ eng/tools/azure-sdk-tools/devtools_testutils/storage/__init__.py
@@ -1,9 +1,12 @@
 from .api_version_policy import ApiVersionAssertPolicy
+from .decorators import GenericTestProxyParametrize1, GenericTestProxyParametrize2
 from .service_versions import service_version_map, ServiceVersion, is_version_before
 from .testcase import StorageRecordedTestCase, LogCaptured
 
 __all__ = [
     "ApiVersionAssertPolicy",
+    "GenericTestProxyParametrize1",
+    "GenericTestProxyParametrize2",
     "service_version_map",
     "StorageRecordedTestCase",
     "ServiceVersion",
