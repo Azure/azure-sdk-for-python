0efcfa4ecdc8846d1058aa6cf11294b535a1e4cb
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2026-03-03 10:12:20 -0800

    Adapt sample code comments to 2.0.0 release

--- sdk/ai/azure-ai-projects/samples/evaluations/sample_evaluations_score_model_grader_with_image.py
+++ sdk/ai/azure-ai-projects/samples/evaluations/sample_evaluations_score_model_grader_with_image.py
@@ -14,7 +14,7 @@ USAGE:
 
     Before running the sample:
 
-    pip install "azure-ai-projects>=2.0.0b2" azure-identity python-dotenv Pillow
+    pip install "azure-ai-projects>=2.0.0" python-dotenv pillow
 
     Set these environment variables with your own values:
     1) AZURE_AI_PROJECT_ENDPOINT - Required. The Azure AI Project endpoint, as found in the overview page of your
