1c6945740995118ce163dca404fa1c46758bcf33
Matthew Metcalf <mrm9084@gmail.com>
2026-03-03 14:37:29 -0800

    Update sdk/appconfiguration/azure-appconfiguration-provider/tests/test_constants.py
    Co-authored-by: Ross Grambo <rossgrambo@microsoft.com>

--- sdk/appconfiguration/azure-appconfiguration-provider/tests/test_constants.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/tests/test_constants.py
@@ -2,10 +2,10 @@ FEATURE_MANAGEMENT_KEY = "feature_management"
 FEATURE_FLAG_KEY = "feature_flags"
 
 APPCONFIGURATION_CONNECTION_STRING = (
-    "Endpoint=https://Sanitized.azconfig.io;"
+    "Endpoint=https://sanitized.azconfig.io;"
     "Id=0-l4-s0:h5htBaY5Z1LwFz50bIQv;"
     "Secret=lamefakesecretlamefakesecretlamefakesecrett="
 )
-APPCONFIGURATION_ENDPOINT_STRING = "https://Sanitized.azconfig.io"
-APPCONFIGURATION_KEYVAULT_SECRET_URL = "https://Sanitized.vault.azure.net/secrets/fake-secret/"
-APPCONFIGURATION_KEYVAULT_SECRET_URL2 = "https://Sanitized.vault.azure.net/secrets/fake-secret2/"
+APPCONFIGURATION_ENDPOINT_STRING = "https://sanitized.azconfig.io"
+APPCONFIGURATION_KEYVAULT_SECRET_URL = "https://sanitized.vault.azure.net/secrets/fake-secret/"
+APPCONFIGURATION_KEYVAULT_SECRET_URL2 = "https://sanitized.vault.azure.net/secrets/fake-secret2/"
