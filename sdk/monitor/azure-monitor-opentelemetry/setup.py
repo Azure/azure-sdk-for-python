6f4cb1e34e28a2ccae74913bb75ae8f7f026a5b9
Radhika Gupta <guptaradhika@microsoft.com>
2026-03-04 14:16:03 -0800

    Update sdk version

--- sdk/monitor/azure-monitor-opentelemetry/setup.py
+++ sdk/monitor/azure-monitor-opentelemetry/setup.py
@@ -85,7 +85,7 @@ setup(
         "azure-core<2.0.0,>=1.28.0",
         "azure-core-tracing-opentelemetry~=1.0.0b11",
         "azure-monitor-opentelemetry-exporter~=1.0.0b47",
-        "opentelemetry-sdk==1.39",
+        "opentelemetry-sdk==1.40",
         "opentelemetry-instrumentation-django~=0.61b0",
         "opentelemetry-instrumentation-fastapi~=0.61b0",
         "opentelemetry-instrumentation-flask~=0.61b0",
