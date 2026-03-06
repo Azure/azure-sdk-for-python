c7b9ebe93d41cf2c8fd327e514b5c67ff51020ac
Paul Van Eck <paulvaneck@microsoft.com>
2026-03-06 11:22:18 -0800

    [Monitor] Generate exporter with TypeSpec (#44442)
    * [Monitor] Generate exporter with TypeSpec
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>
    
    * Fix resource envelope serialization for OTel resource attributes
    
    * Update tests
    
    * Update log exporter
    
    * Update
    
    * Regenerate exporter typespec models
    
    * Regenerate exporter typespec models and fix renames
    
    * Fix _generated/__init__.py to re-export AzureMonitorClient from exporter subpackage
    
    * Add trailing newline to _generated/__init__.py
    
    * Added tracing false in autogenerated code definition
    
    * Update
    
    * Update
    
    * Remove unused model imports from _base.py and fix lint
    
    * Update tsp-location.yml and regen
    
    Use the merged commit hash from the specs repo and regen using
    an updated Python emitter version.
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>
    
    ---------
    
    Signed-off-by: Paul Van Eck <paulvaneck@microsoft.com>
    Co-authored-by: Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>

--- sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/_constants.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/_constants.py
@@ -12,6 +12,8 @@ from opentelemetry.semconv.metrics.http_metrics import (
 from azure.core import CaseInsensitiveEnumMeta
 
 
+_EXPORTER_DOMAIN_SCHEMA_VERSION = 2
+
 # Environment variables
 
 _APPLICATIONINSIGHTS_STATSBEAT_DISABLED_ALL = "APPLICATIONINSIGHTS_STATSBEAT_DISABLED_ALL"
