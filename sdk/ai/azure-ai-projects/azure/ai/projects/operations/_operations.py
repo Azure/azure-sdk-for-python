81a703e99dfb0fd10aeb22f7a4a0791844561aec
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2026-03-20 15:27:04 -0700

    post processing

--- sdk/ai/azure-ai-projects/azure/ai/projects/operations/_operations.py
+++ sdk/ai/azure-ai-projects/azure/ai/projects/operations/_operations.py
@@ -7050,7 +7050,7 @@ class BetaMemoryStoresOperations:
             if scope is _Unset:
                 raise TypeError("missing required argument: scope")
             body = {
-                "items_property": items,
+                "items": items,
                 "options": options,
                 "previous_search_id": previous_search_id,
                 "scope": scope,
@@ -7136,7 +7136,7 @@ class BetaMemoryStoresOperations:
             if scope is _Unset:
                 raise TypeError("missing required argument: scope")
             body = {
-                "items_property": items,
+                "items": items,
                 "previous_update_id": previous_update_id,
                 "scope": scope,
                 "update_delay": update_delay,
