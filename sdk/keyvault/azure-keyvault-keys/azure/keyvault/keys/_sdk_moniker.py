9182f24bfa370617829c965ec5d3d04d52739fd9
Libba Lawrence <llawrence@microsoft.com>
2026-03-25 15:04:51 -0700

    remove nelwine

--- sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/_sdk_moniker.py
+++ sdk/keyvault/azure-keyvault-keys/azure/keyvault/keys/_sdk_moniker.py
@@ -2,7 +2,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-
 from ._version import VERSION
 
 SDK_MONIKER = f"keyvault-keys/{VERSION}"
