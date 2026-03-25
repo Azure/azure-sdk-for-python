cb3098bd16261de626f0d548c19519027404d36b
Lu Sun <lusu@microsoft.com>
2026-03-24 20:33:17 -0700

    fix sphinx

--- sdk/agentserver/azure-ai-agentserver-responses/azure/ai/agentserver/responses/store/__init__.py
+++ sdk/agentserver/azure-ai-agentserver-responses/azure/ai/agentserver/responses/store/__init__.py
@@ -1,26 +1,2 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-"""Store sub-package: response providers and supporting types."""
-
-from ._base import ResponseProviderProtocol, ResponseStreamProviderProtocol
-from ._foundry_errors import (
-    FoundryApiError,
-    FoundryBadRequestError,
-    FoundryResourceNotFoundError,
-    FoundryStorageError,
-)
-from ._foundry_provider import FoundryStorageProvider
-from ._foundry_settings import FoundryStorageSettings
-from ._memory import InMemoryResponseProvider
-
-__all__ = [
-    "ResponseProviderProtocol",
-    "ResponseStreamProviderProtocol",
-    "InMemoryResponseProvider",
-    "FoundryStorageProvider",
-    "FoundryStorageSettings",
-    "FoundryStorageError",
-    "FoundryResourceNotFoundError",
-    "FoundryBadRequestError",
-    "FoundryApiError",
-]
