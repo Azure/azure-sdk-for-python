86ac43f8567934da67f71e256b15c9e4dfd920f3
azure-sdk <azuresdk@microsoft.com>
2026-03-31 16:52:42 +0000

    Configurations:  'specification/computelimit/resource-manager/Microsoft.ComputeLimit/ComputeLimit/tspconfig.yaml', API Version: 2026-03-20, SDK Release Type: stable, and CommitSHA: 'a83147fe12aece4ded0bd96be212e5242e087cb9' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6087532 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/computelimit/azure-mgmt-computelimit/generated_samples/features_get_shared_limit.py
+++ sdk/computelimit/azure-mgmt-computelimit/generated_samples/features_get_shared_limit.py
@@ -15,7 +15,7 @@ from azure.mgmt.computelimit import ComputeLimitMgmtClient
     pip install azure-identity
     pip install azure-mgmt-computelimit
 # USAGE
-    python shared_limits_list.py
+    python features_get_shared_limit.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -30,13 +30,13 @@ def main():
         subscription_id="SUBSCRIPTION_ID",
     )
 
-    response = client.shared_limits.list_by_subscription_location_resource(
+    response = client.features.get(
         location="eastus",
+        feature_name="SharedLimit",
     )
-    for item in response:
-        print(item)
+    print(response)
 
 
-# x-ms-original-file: 2026-03-20/SharedLimits_List.json
+# x-ms-original-file: 2026-03-20/Features_Get_SharedLimit.json
 if __name__ == "__main__":
     main()
