b6f1057ffc6775cf9245b28b9c6d85cc89b41871
azure-sdk <azuresdk@microsoft.com>
2026-04-01 21:32:35 +0000

    Configurations:  'specification/dataprotection/resource-manager/Microsoft.DataProtection/DataProtection/tspconfig.yaml', API Version: 2026_03_01, SDK Release Type: beta, and CommitSHA: '32ab0a3f798fed9517cb4026e47144251ea9331e' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6095330 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/dataprotection/azure-mgmt-dataprotection/generated_samples/get_default_delete_protected_item_requests.py
+++ sdk/dataprotection/azure-mgmt-dataprotection/generated_samples/get_default_delete_protected_item_requests.py
@@ -38,6 +38,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: 2025-07-01/ResourceGuardCRUD/GetDefaultDeleteProtectedItemRequests.json
+# x-ms-original-file: 2026-03-01/ResourceGuardCRUD/GetDefaultDeleteProtectedItemRequests.json
 if __name__ == "__main__":
     main()
