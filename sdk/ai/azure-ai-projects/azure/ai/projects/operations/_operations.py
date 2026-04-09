907d624de205adf5f4cdfb3bb6f56f9a8ec5d5aa
Pranav Pandit <pranavp@microsoft.com>
2026-04-09 14:15:49 +0000

    delete sessions returns 200

--- sdk/ai/azure-ai-projects/azure/ai/projects/operations/_operations.py
+++ sdk/ai/azure-ai-projects/azure/ai/projects/operations/_operations.py
@@ -5776,7 +5776,7 @@ class BetaAgentsOperations:
 
         response = pipeline_response.http_response
 
-        if response.status_code not in [204]:
+        if response.status_code not in [200, 204]:
             map_error(status_code=response.status_code, response=response, error_map=error_map)
             error = _failsafe_deserialize(
                 _models.ApiErrorResponse,
