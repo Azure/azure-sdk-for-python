1f51ecf821787dc1dfff3ee39c07a5a9bd7ac66b
Libba Lawrence <llawrence@microsoft.com>
2026-04-14 19:18:25 -0700

    pylint

--- sdk/storage/azure-storage-blob/azure/storage/blob/aio/__init__.py
+++ sdk/storage/azure-storage-blob/azure/storage/blob/aio/__init__.py
@@ -78,9 +78,8 @@ async def upload_blob_to_url(
     :return: Blob-updated property dict (Etag and last modified)
     :rtype: dict[str, Any]
     """
-    async with BlobClient.from_blob_url(
-        blob_url, credential=credential
-    ) as client:  # pylint: disable=not-async-context-manager
+    client = BlobClient.from_blob_url(blob_url, credential=credential)
+    async with client:
         return await client.upload_blob(data=data, blob_type=BlobType.BLOCKBLOB, **kwargs)
 
 
@@ -146,9 +145,8 @@ async def download_blob_from_url(
     :rtype: None
     """
     overwrite = kwargs.pop("overwrite", False)
-    async with BlobClient.from_blob_url(
-        blob_url, credential=credential
-    ) as client:  # pylint: disable=not-async-context-manager
+    client = BlobClient.from_blob_url(blob_url, credential=credential)
+    async with client:
         if hasattr(output, "write"):
             await _download_to_stream(client, output, **kwargs)
         else:
