0cc8abf0d1afa4ada4303e12a067e8d49facc325
Jiarui Guo <jiarguo@microsoft.com>
2026-04-20 17:22:25 -0700

    update test setup to reuse preview endpoints

--- sdk/translation/azure-ai-translation-text/tests/conftest.py
+++ sdk/translation/azure-ai-translation-text/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from devtools_testutils import test_proxy, add_remove_header_sanitizer, remove_batch_sanitizers
+from devtools_testutils import test_proxy, add_remove_header_sanitizer, remove_batch_sanitizers, set_custom_default_matcher
 
 # autouse=True will trigger this fixture on each pytest run, even if it's not explicitly used by a test method
 # def start_proxy(test_proxy):
@@ -14,3 +14,7 @@ def add_sanitizers(test_proxy):
     #  - AZSDK3430: $..id
     #  - AZSDK3424: $..to
     remove_batch_sanitizers(["AZSDK3430", "AZSDK3424"])
+
+    # Ignore api-version during request matching so recordings with
+    # 2025-10-01-preview can be reused with 2026-06-06
+    set_custom_default_matcher(ignored_query_parameters="api-version")
