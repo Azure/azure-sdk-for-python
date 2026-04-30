dd1828aa53a8aa27880588c9bfbdbd695f07ca5b
Kashif Khan <kashifkhan@microsoft.com>
2026-04-30 15:32:51 +0000

    remove _FixedOffset and use stdlib

--- sdk/core/corehttp/corehttp/utils/_utils.py
+++ sdk/core/corehttp/corehttp/utils/_utils.py
@@ -26,30 +26,6 @@ if TYPE_CHECKING:
     from corehttp.rest._helpers import FileType, FilesType
 
 
-class _FixedOffset(datetime.tzinfo):
-    """Fixed offset in minutes east from UTC.
-
-    Copy/pasted from Python doc
-
-    :param int offset: offset in minutes
-    """
-
-    def __init__(self, offset):
-        self.__offset = datetime.timedelta(minutes=offset)
-
-    def utcoffset(self, dt):
-        return self.__offset
-
-    def tzname(self, dt):
-        return str(self.__offset.total_seconds() / 3600)
-
-    def __repr__(self):
-        return "<FixedOffset {}>".format(self.tzname(None))
-
-    def dst(self, dt):
-        return datetime.timedelta(0)
-
-
 def case_insensitive_dict(
     *args: Optional[Union[Mapping[str, Any], Iterable[Tuple[str, Any]]]], **kwargs: Any
 ) -> MutableMapping[str, Any]:
