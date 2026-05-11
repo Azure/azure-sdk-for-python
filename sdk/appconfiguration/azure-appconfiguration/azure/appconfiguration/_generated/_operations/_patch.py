560163838f09ab9ff0c4bf423b66564b587dfad6
Matt Metcalf <mrm9084@gmail.com>
2026-05-11 16:43:20 -0700

    getting rid of duplicate code

--- sdk/appconfiguration/azure-appconfiguration/azure/appconfiguration/_generated/_operations/_patch.py
+++ sdk/appconfiguration/azure-appconfiguration/azure/appconfiguration/_generated/_operations/_patch.py
@@ -105,12 +105,7 @@ class AzureAppConfigurationClientOperationsMixin(AzureAppConfigClientOpGenerated
             params=_next_request_params,
             headers=_next_headers,
         )
-
-        path_format_arguments = {
-            "endpoint": self._serialize.url("self._config.endpoint", self._config.endpoint, "str", skip_quote=True),
-        }
-        _request.url = self._client.format_url(_request.url, **path_format_arguments)
-        return _request
+        return self._format_request(_request)
 
     def _format_request(self, _request: HttpRequest) -> HttpRequest:
         """Apply endpoint formatting to a request URL."""
