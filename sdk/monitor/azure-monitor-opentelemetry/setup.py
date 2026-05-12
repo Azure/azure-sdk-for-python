41e51e2011a0cc1ffcf5f0ededfcf3c0f4835232
Radhika Gupta <guptaradhika@microsoft.com>
2026-05-12 10:28:41 -0700

    Update min exporter version

--- sdk/monitor/azure-monitor-opentelemetry/setup.py
+++ sdk/monitor/azure-monitor-opentelemetry/setup.py
@@ -83,7 +83,7 @@ setup(
     install_requires=[
         "azure-core<2.0.0,>=1.28.0",
         "azure-core-tracing-opentelemetry~=1.0.0b11",
-        "azure-monitor-opentelemetry-exporter~=1.0.0b49",
+        "azure-monitor-opentelemetry-exporter~=1.0.0b52",
         "opentelemetry-sdk==1.40",
         "opentelemetry-instrumentation-django==0.61b0",
         "opentelemetry-instrumentation-fastapi==0.61b0",
