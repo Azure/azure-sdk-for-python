26af249acf0aeba78baa577862880841d7db8dac
Radhika Gupta <guptaradhika@microsoft.com>
2026-05-12 09:59:10 -0700

    Fix cspell

--- sdk/monitor/azure-monitor-opentelemetry/tests/test_configure.py
+++ sdk/monitor/azure-monitor-opentelemetry/tests/test_configure.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# cspell:ignore genai
 import unittest
 from unittest.mock import Mock, call, patch
 
