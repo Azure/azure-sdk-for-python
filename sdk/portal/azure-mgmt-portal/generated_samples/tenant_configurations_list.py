ef45dc8e397414b5159d8be7af6c77174ef52963
Yuchao Yan <yuchaoyan@microsoft.com>
2026-05-13 12:55:21 +0800

    regenerate (#46855)

--- sdk/portal/azure-mgmt-portal/generated_samples/tenant_configurations_list.py
+++ sdk/portal/azure-mgmt-portal/generated_samples/tenant_configurations_list.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -7,6 +8,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.portal import Portal
 
 """
@@ -14,7 +16,7 @@ from azure.mgmt.portal import Portal
     pip install azure-identity
     pip install azure-mgmt-portal
 # USAGE
-    python create_or_update_a_dashboard.py
+    python tenant_configurations_list.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -26,33 +28,14 @@ from azure.mgmt.portal import Portal
 def main():
     client = Portal(
         credential=DefaultAzureCredential(),
-        subscription_id="00000000-0000-0000-0000-000000000000",
+        subscription_id="SUBSCRIPTION_ID",
     )
 
-    response = client.dashboards.create_or_update(
-        resource_group_name="testRG",
-        dashboard_name="testDashboard",
-        dashboard={
-            "location": "eastus",
-            "properties": {
-                "lenses": [
-                    {
-                        "order": 1,
-                        "parts": [
-                            {"position": {"colSpan": 3, "rowSpan": 4, "x": 1, "y": 2}},
-                            {"position": {"colSpan": 6, "rowSpan": 6, "x": 5, "y": 5}},
-                        ],
-                    },
-                    {"order": 2, "parts": []},
-                ],
-                "metadata": {"metadata": {"ColSpan": 2, "RowSpan": 1, "X": 4, "Y": 3}},
-            },
-            "tags": {"aKey": "aValue", "anotherKey": "anotherValue"},
-        },
-    )
-    print(response)
+    response = client.tenant_configurations.list()
+    for item in response:
+        print(item)
 
 
-# x-ms-original-file: specification/portal/resource-manager/Microsoft.Portal/preview/2020-09-01-preview/examples/createOrUpdateDashboard.json
+# x-ms-original-file: specification/portal/resource-manager/Microsoft.Portal/preview/2020-09-01-preview/examples/TenantConfigurations_List.json
 if __name__ == "__main__":
     main()
