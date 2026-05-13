577c82b9ff63c805347b1202f2816bde3f56ec5e
azure-sdk <azuresdk@microsoft.com>
2026-05-13 01:18:20 +0000

    Configurations:  'specification/containerservice/resource-manager/Microsoft.ContainerService/aks/tspconfig.yaml', API Version: 2026-03-02-preview, SDK Release Type: beta, and CommitSHA: '96ce8adc0868ca53f8b16234d3a3f5da55be48af' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6288733 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/containerservice/azure-mgmt-containerservice/generated_samples/managed_clusters_create_mcsnapshot.py
+++ sdk/containerservice/azure-mgmt-containerservice/generated_samples/managed_clusters_create_mcsnapshot.py
@@ -64,6 +64,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: 2026-02-02-preview/ManagedClustersCreate_MCSnapshot.json
+# x-ms-original-file: 2026-03-02-preview/ManagedClustersCreate_MCSnapshot.json
 if __name__ == "__main__":
     main()
