87d03dfe667d8120a77b2c350b6850ea110dc7c0
salma-elshafey <selshafey@microsoft.com>
2026-05-14 21:51:11 +0300

    fix pylint issue

--- sdk/ai/azure-ai-projects/samples/evaluations/agentic_evaluators/sample_quality_grader.py
+++ sdk/ai/azure-ai-projects/samples/evaluations/agentic_evaluators/sample_quality_grader.py
@@ -1,4 +1,4 @@
-# pylint: disable=line-too-long,useless-suppression
+# pylint: disable=line-too-long,useless-suppression,missing-function-docstring
 # ------------------------------------
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
