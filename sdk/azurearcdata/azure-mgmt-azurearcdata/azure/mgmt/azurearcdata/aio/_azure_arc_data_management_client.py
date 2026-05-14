d661f597384181723708e374fec92073d220fbce
Yuchao Yan <yuchaoyan@microsoft.com>
2026-05-14 10:30:05 +0800

    Regenerate azure-mgmt-azurearcdata with latest code generator tool (#46876)

--- sdk/azurearcdata/azure-mgmt-azurearcdata/azure/mgmt/azurearcdata/aio/_azure_arc_data_management_client.py
+++ sdk/azurearcdata/azure-mgmt-azurearcdata/azure/mgmt/azurearcdata/aio/_azure_arc_data_management_client.py
@@ -7,29 +7,32 @@
 # --------------------------------------------------------------------------
 
 from copy import deepcopy
-from typing import Any, Awaitable, TYPE_CHECKING
+from typing import Any, Awaitable, Optional, TYPE_CHECKING, cast
+from typing_extensions import Self
 
+from azure.core.pipeline import policies
 from azure.core.rest import AsyncHttpResponse, HttpRequest
+from azure.core.settings import settings
 from azure.mgmt.core import AsyncARMPipelineClient
+from azure.mgmt.core.policies import AsyncARMAutoResourceProviderRegistrationPolicy
+from azure.mgmt.core.tools import get_arm_endpoints
 
 from .. import models as _models
-from .._serialization import Deserializer, Serializer
+from .._utils.serialization import Deserializer, Serializer
 from ._configuration import AzureArcDataManagementClientConfiguration
 from .operations import (
-    ActiveDirectoryConnectorsOperations,
     DataControllersOperations,
     Operations,
-    PostgresInstancesOperations,
     SqlManagedInstancesOperations,
     SqlServerInstancesOperations,
 )
 
 if TYPE_CHECKING:
-    # pylint: disable=unused-import,ungrouped-imports
+    from azure.core import AzureClouds
     from azure.core.credentials_async import AsyncTokenCredential
 
 
-class AzureArcDataManagementClient:  # pylint: disable=client-accepts-api-version-keyword
+class AzureArcDataManagementClient:
     """The AzureArcData management API provides a RESTful set of web APIs to manage Azure Data
     Services on Azure Arc Resources.
 
@@ -43,19 +46,17 @@ class AzureArcDataManagementClient:  # pylint: disable=client-accepts-api-versio
      azure.mgmt.azurearcdata.aio.operations.SqlServerInstancesOperations
     :ivar data_controllers: DataControllersOperations operations
     :vartype data_controllers: azure.mgmt.azurearcdata.aio.operations.DataControllersOperations
-    :ivar active_directory_connectors: ActiveDirectoryConnectorsOperations operations
-    :vartype active_directory_connectors:
-     azure.mgmt.azurearcdata.aio.operations.ActiveDirectoryConnectorsOperations
-    :ivar postgres_instances: PostgresInstancesOperations operations
-    :vartype postgres_instances: azure.mgmt.azurearcdata.aio.operations.PostgresInstancesOperations
     :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: The ID of the Azure subscription. Required.
     :type subscription_id: str
-    :param base_url: Service URL. Default value is "https://management.azure.com".
+    :param base_url: Service URL. Default value is None.
     :type base_url: str
-    :keyword api_version: Api Version. Default value is "2022-03-01-preview". Note that overriding
-     this default value may result in unsupported behavior.
+    :keyword cloud_setting: The cloud setting for which to get the ARM endpoint. Default value is
+     None.
+    :paramtype cloud_setting: ~azure.core.AzureClouds
+    :keyword api_version: Api Version. Default value is "2021-11-01". Note that overriding this
+     default value may result in unsupported behavior.
     :paramtype api_version: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
      Retry-After header is present.
@@ -65,13 +66,45 @@ class AzureArcDataManagementClient:  # pylint: disable=client-accepts-api-versio
         self,
         credential: "AsyncTokenCredential",
         subscription_id: str,
-        base_url: str = "https://management.azure.com",
+        base_url: Optional[str] = None,
+        *,
+        cloud_setting: Optional["AzureClouds"] = None,
         **kwargs: Any
     ) -> None:
+        _cloud = cloud_setting or settings.current.azure_cloud  # type: ignore
+        _endpoints = get_arm_endpoints(_cloud)
+        if not base_url:
+            base_url = _endpoints["resource_manager"]
+        credential_scopes = kwargs.pop("credential_scopes", _endpoints["credential_scopes"])
         self._config = AzureArcDataManagementClientConfiguration(
-            credential=credential, subscription_id=subscription_id, **kwargs
+            credential=credential,
+            subscription_id=subscription_id,
+            cloud_setting=cloud_setting,
+            credential_scopes=credential_scopes,
+            **kwargs
         )
-        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+
+        _policies = kwargs.pop("policies", None)
+        if _policies is None:
+            _policies = [
+                policies.RequestIdPolicy(**kwargs),
+                self._config.headers_policy,
+                self._config.user_agent_policy,
+                self._config.proxy_policy,
+                policies.ContentDecodePolicy(**kwargs),
+                AsyncARMAutoResourceProviderRegistrationPolicy(),
+                self._config.redirect_policy,
+                self._config.retry_policy,
+                self._config.authentication_policy,
+                self._config.custom_hook_policy,
+                self._config.logging_policy,
+                policies.DistributedTracingPolicy(**kwargs),
+                policies.SensitiveHeaderCleanupPolicy(**kwargs) if self._config.redirect_policy else None,
+                self._config.http_logging_policy,
+            ]
+        self._client: AsyncARMPipelineClient = AsyncARMPipelineClient(
+            base_url=cast(str, base_url), policies=_policies, **kwargs
+        )
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -87,14 +120,10 @@ class AzureArcDataManagementClient:  # pylint: disable=client-accepts-api-versio
         self.data_controllers = DataControllersOperations(
             self._client, self._config, self._serialize, self._deserialize
         )
-        self.active_directory_connectors = ActiveDirectoryConnectorsOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.postgres_instances = PostgresInstancesOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
 
-    def _send_request(self, request: HttpRequest, **kwargs: Any) -> Awaitable[AsyncHttpResponse]:
+    def _send_request(
+        self, request: HttpRequest, *, stream: bool = False, **kwargs: Any
+    ) -> Awaitable[AsyncHttpResponse]:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -114,14 +143,14 @@ class AzureArcDataManagementClient:  # pylint: disable=client-accepts-api-versio
 
         request_copy = deepcopy(request)
         request_copy.url = self._client.format_url(request_copy.url)
-        return self._client.send_request(request_copy, **kwargs)
+        return self._client.send_request(request_copy, stream=stream, **kwargs)  # type: ignore
 
     async def close(self) -> None:
         await self._client.close()
 
-    async def __aenter__(self) -> "AzureArcDataManagementClient":
+    async def __aenter__(self) -> Self:
         await self._client.__aenter__()
         return self
 
-    async def __aexit__(self, *exc_details) -> None:
+    async def __aexit__(self, *exc_details: Any) -> None:
         await self._client.__aexit__(*exc_details)
--- sdk/azurearcdata/azure-mgmt-azurearcdata/generated_samples/delete_sql_server_instance.py
+++ sdk/azurearcdata/azure-mgmt-azurearcdata/generated_samples/delete_sql_server_instance.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long,useless-suppression
 # coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
@@ -7,6 +8,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
+
 from azure.mgmt.azurearcdata import AzureArcDataManagementClient
 
 """
@@ -29,13 +31,12 @@ def main():
         subscription_id="00000000-1111-2222-3333-444444444444",
     )
 
-    response = client.sql_server_instances.begin_delete(
+    client.sql_server_instances.begin_delete(
         resource_group_name="testrg",
         sql_server_instance_name="testsqlServerInstance",
     ).result()
-    print(response)
 
 
-# x-ms-original-file: specification/azurearcdata/resource-manager/Microsoft.AzureArcData/preview/2022-03-01-preview/examples/DeleteSqlServerInstance.json
+# x-ms-original-file: specification/azurearcdata/resource-manager/Microsoft.AzureArcData/stable/2021-11-01/examples/DeleteSqlServerInstance.json
 if __name__ == "__main__":
     main()
