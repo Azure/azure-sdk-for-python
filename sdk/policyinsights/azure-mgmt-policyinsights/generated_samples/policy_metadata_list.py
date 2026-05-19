0be734c5fa9b789309f446afe800bde44eb802aa
Yuchao Yan <yuchaoyan@microsoft.com>
2026-05-19 13:59:17 +0800

    Regenerate azure-mgmt-policyinsights with latest code generator tool (#46975)

--- sdk/policyinsights/azure-mgmt-policyinsights/generated_samples/policy_metadata_list.py
+++ sdk/policyinsights/azure-mgmt-policyinsights/generated_samples/policy_metadata_list.py
@@ -36,6 +36,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: specification/policyinsights/resource-manager/Microsoft.PolicyInsights/stable/2024-10-01/examples/PolicyMetadata_List.json
+# x-ms-original-file: specification/policyinsights/resource-manager/Microsoft.PolicyInsights/stable/2019-10-01/examples/PolicyMetadata_List.json
 if __name__ == "__main__":
     main()
