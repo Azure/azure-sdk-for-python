d07cb997e8f5bf6854002557b7a6474903b77b41
Yuchao Yan <yuchaoyan@microsoft.com>
2026-05-19 16:47:33 +0800

    Regenerate azure-mgmt-purview with latest code generator tool (#46980)

--- sdk/purview/azure-mgmt-purview/azure/mgmt/purview/aio/operations/_private_link_resources_operations.py
+++ sdk/purview/azure-mgmt-purview/azure/mgmt/purview/aio/operations/_private_link_resources_operations.py
@@ -22,12 +22,16 @@ from azure.core.exceptions import (
 from azure.core.pipeline import PipelineResponse
 from azure.core.rest import AsyncHttpResponse, HttpRequest
 from azure.core.tracing.decorator import distributed_trace
+from azure.core.tracing.decorator_async import distributed_trace_async
 from azure.core.utils import case_insensitive_dict
 from azure.mgmt.core.exceptions import ARMErrorFormat
 
 from ... import models as _models
 from ..._utils.serialization import Deserializer, Serializer
-from ...operations._operations import build_list_request
+from ...operations._private_link_resources_operations import (
+    build_get_by_group_id_request,
+    build_list_by_account_request,
+)
 from .._configuration import PurviewManagementClientConfiguration
 
 T = TypeVar("T")
@@ -35,14 +39,14 @@ ClsType = Optional[Callable[[PipelineResponse[HttpRequest, AsyncHttpResponse], T
 List = list
 
 
-class Operations:
+class PrivateLinkResourcesOperations:
     """
     .. warning::
         **DO NOT** instantiate this class directly.
 
         Instead, you should access the following operations through
         :class:`~azure.mgmt.purview.aio.PurviewManagementClient`'s
-        :attr:`operations` attribute.
+        :attr:`private_link_resources` attribute.
     """
 
     models = _models
@@ -55,20 +59,26 @@ class Operations:
         self._deserialize: Deserializer = input_args.pop(0) if input_args else kwargs.pop("deserializer")
 
     @distributed_trace
-    def list(self, **kwargs: Any) -> AsyncItemPaged["_models.Operation"]:
-        """Lists the available operations.
+    def list_by_account(
+        self, resource_group_name: str, account_name: str, **kwargs: Any
+    ) -> AsyncItemPaged["_models.PrivateLinkResource"]:
+        """Gets a list of privately linkable resources for an account.
 
-        List of available operations.
+        Gets a list of privately linkable resources for an account.
 
-        :return: An iterator like instance of either Operation or the result of cls(response)
-        :rtype: ~azure.core.async_paging.AsyncItemPaged[~azure.mgmt.purview.models.Operation]
+        :param resource_group_name: The resource group name. Required.
+        :type resource_group_name: str
+        :param account_name: The name of the account. Required.
+        :type account_name: str
+        :return: An iterator like instance of either PrivateLinkResource or the result of cls(response)
+        :rtype: ~azure.core.async_paging.AsyncItemPaged[~azure.mgmt.purview.models.PrivateLinkResource]
         :raises ~azure.core.exceptions.HttpResponseError:
         """
         _headers = kwargs.pop("headers", {}) or {}
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
         api_version: str = kwargs.pop("api_version", _params.pop("api-version", self._config.api_version))
-        cls: ClsType[_models.OperationList] = kwargs.pop("cls", None)
+        cls: ClsType[_models.PrivateLinkResourceList] = kwargs.pop("cls", None)
 
         error_map: MutableMapping = {
             401: ClientAuthenticationError,
@@ -81,7 +91,10 @@ class Operations:
         def prepare_request(next_link=None):
             if not next_link:
 
-                _request = build_list_request(
+                _request = build_list_by_account_request(
+                    resource_group_name=resource_group_name,
+                    account_name=account_name,
+                    subscription_id=self._config.subscription_id,
                     api_version=api_version,
                     headers=_headers,
                     params=_params,
@@ -106,7 +119,7 @@ class Operations:
             return _request
 
         async def extract_data(pipeline_response):
-            deserialized = self._deserialize("OperationList", pipeline_response)
+            deserialized = self._deserialize("PrivateLinkResourceList", pipeline_response)
             list_of_elem = deserialized.value
             if cls:
                 list_of_elem = cls(list_of_elem)  # type: ignore
@@ -132,3 +145,68 @@ class Operations:
             return pipeline_response
 
         return AsyncItemPaged(get_next, extract_data)
+
+    @distributed_trace_async
+    async def get_by_group_id(
+        self, resource_group_name: str, account_name: str, group_id: str, **kwargs: Any
+    ) -> _models.PrivateLinkResource:
+        """Gets a privately linkable resources for an account with given group identifier.
+
+        Gets a privately linkable resources for an account with given group identifier.
+
+        :param resource_group_name: The resource group name. Required.
+        :type resource_group_name: str
+        :param account_name: The name of the account. Required.
+        :type account_name: str
+        :param group_id: The group identifier. Required.
+        :type group_id: str
+        :return: PrivateLinkResource or the result of cls(response)
+        :rtype: ~azure.mgmt.purview.models.PrivateLinkResource
+        :raises ~azure.core.exceptions.HttpResponseError:
+        """
+        error_map: MutableMapping = {
+            401: ClientAuthenticationError,
+            404: ResourceNotFoundError,
+            409: ResourceExistsError,
+            304: ResourceNotModifiedError,
+        }
+        error_map.update(kwargs.pop("error_map", {}) or {})
+
+        _headers = kwargs.pop("headers", {}) or {}
+        _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
+
+        api_version: str = kwargs.pop("api_version", _params.pop("api-version", self._config.api_version))
+        cls: ClsType[_models.PrivateLinkResource] = kwargs.pop("cls", None)
+
+        _request = build_get_by_group_id_request(
+            resource_group_name=resource_group_name,
+            account_name=account_name,
+            group_id=group_id,
+            subscription_id=self._config.subscription_id,
+            api_version=api_version,
+            headers=_headers,
+            params=_params,
+        )
+        _request.url = self._client.format_url(_request.url)
+
+        _stream = False
+        pipeline_response: PipelineResponse = await self._client._pipeline.run(  # pylint: disable=protected-access
+            _request, stream=_stream, **kwargs
+        )
+
+        response = pipeline_response.http_response
+
+        if response.status_code not in [200]:
+            map_error(status_code=response.status_code, response=response, error_map=error_map)
+            error = self._deserialize.failsafe_deserialize(
+                _models.ErrorResponseModel,
+                pipeline_response,
+            )
+            raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
+
+        deserialized = self._deserialize("PrivateLinkResource", pipeline_response.http_response)
+
+        if cls:
+            return cls(pipeline_response, deserialized, {})  # type: ignore
+
+        return deserialized  # type: ignore
