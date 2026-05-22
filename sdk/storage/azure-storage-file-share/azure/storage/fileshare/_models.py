f4decd63055f877c09f3cd4045ca5340d59eb7f0
Peter Wu <weirongwu@microsoft.com>
2026-05-22 18:27:07 -0400

    WIP

--- sdk/storage/azure-storage-file-share/azure/storage/fileshare/_models.py
+++ sdk/storage/azure-storage-file-share/azure/storage/fileshare/_models.py
@@ -115,9 +115,9 @@ class Metrics(GeneratedMetrics):
             version=generated.version,
             enabled=generated.enabled,
             include_apis=generated.include_apis,
-            retention_policy=RetentionPolicy._from_generated(
+            retention_policy=RetentionPolicy._from_generated(  # pylint: disable=protected-access
                 generated.retention_policy
-            ),  # pylint: disable=protected-access
+            ),
         )
 
 
@@ -736,8 +736,8 @@ class SharePropertiesPaged(PageIterator):
         self.marker = self._response.marker
         self.results_per_page = self._response.max_results
         self.current_page = [
-            ShareProperties._from_generated(i) for i in self._response.share_items
-        ]  # pylint: disable=protected-access
+            ShareProperties._from_generated(i) for i in self._response.share_items  # pylint: disable=protected-access
+        ]
         return self._response.next_marker or None, self.current_page
 
 
@@ -853,8 +853,8 @@ class HandlesPaged(PageIterator):
     def _extract_data_cb(self, get_next_return):
         self.location_mode, self._response = get_next_return
         self.current_page = [
-            Handle._from_generated(h) for h in self._response.handle_list
-        ]  # pylint: disable=protected-access
+            Handle._from_generated(h) for h in self._response.handle_list  # pylint: disable=protected-access
+        ]
         return self._response.next_marker or None, self.current_page
 
 
@@ -1107,11 +1107,11 @@ class DirectoryPropertiesPaged(PageIterator):
         self.marker = self._response.marker
         self.results_per_page = self._response.max_results
         self.current_page = [
-            DirectoryProperties._from_generated(i) for i in self._response.segment.directory_items
-        ]  # pylint: disable = protected-access
+            DirectoryProperties._from_generated(i) for i in self._response.segment.directory_items  # pylint: disable = protected-access
+        ]
         self.current_page.extend(
-            [FileProperties._from_generated(i) for i in self._response.segment.file_items]
-        )  # pylint: disable = protected-access
+            [FileProperties._from_generated(i) for i in self._response.segment.file_items]  # pylint: disable = protected-access
+        )
         return self._response.next_marker or None, self.current_page
 
 
