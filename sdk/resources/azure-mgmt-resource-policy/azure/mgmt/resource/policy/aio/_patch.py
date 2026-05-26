4d14da5298c97e1aec44e63fea37913739f4ea77
Yuchao Yan <yuchaoyan@microsoft.com>
2026-05-26 13:57:56 +0800

    generated from typespec:55e7cecb6fb849b2ed064ae68551b31d58c7a904

--- sdk/resources/azure-mgmt-resource-policy/azure/mgmt/resource/policy/aio/_patch.py
+++ sdk/resources/azure-mgmt-resource-policy/azure/mgmt/resource/policy/aio/_patch.py
@@ -3,12 +3,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------
-
 """Customize generated code here.
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
-
 
 __all__: list[str] = []  # Add all objects you want publicly available to users at this package level
 
