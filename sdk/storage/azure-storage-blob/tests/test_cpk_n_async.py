2f4906f3398c86f8eab2f320301cb1a6ee6f9cf4
Peter Wu <weirongwu@microsoft.com>
2026-05-26 15:16:48 -0400

    Context manager

--- sdk/storage/azure-storage-blob/tests/test_cpk_n_async.py
+++ sdk/storage/azure-storage-blob/tests/test_cpk_n_async.py
@@ -59,11 +59,8 @@ class TestStorageCPKAsync(AsyncStorageRecordedTestCase):
         if self.is_live:
             try:
                 sync_credential = self.get_credential(SyncBlobServiceClient, is_async=False)
-                sync_bsc = SyncBlobServiceClient(
-                    account_url=bsc.url,
-                    credential=sync_credential,
-                )
-                sync_bsc.delete_container(self.container_name)
+                with SyncBlobServiceClient(account_url=bsc.url, credential=sync_credential) as sync_bsc:
+                    sync_bsc.delete_container(self.container_name)
             except HttpResponseError:
                 pass
 
