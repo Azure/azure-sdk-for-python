965e06595a7dcecc8e3d6ba163dc11f0e25b44af
Libba Lawrence <llawrence@microsoft.com>
2026-06-09 08:23:39 -0700

    pylint

--- sdk/storage/azure-storage-queue/azure/storage/queue/_queue_service_client.py
+++ sdk/storage/azure-storage-queue/azure/storage/queue/_queue_service_client.py
@@ -381,12 +381,12 @@ class QueueServiceClient(StorageAccountHostsMixin, StorageEncryptionMixin):
         """
         props = StorageServiceProperties(
             logging=(
-                analytics_logging._to_generated() if analytics_logging else None
-            ),  # pylint: disable=protected-access
+                analytics_logging._to_generated() if analytics_logging else None  # pylint: disable=protected-access
+            ),
             hour_metrics=hour_metrics._to_generated() if hour_metrics else None,  # pylint: disable=protected-access
             minute_metrics=(
-                minute_metrics._to_generated() if minute_metrics else None
-            ),  # pylint: disable=protected-access
+                minute_metrics._to_generated() if minute_metrics else None  # pylint: disable=protected-access
+            ),
             cors=CorsRule._to_generated(cors),  # pylint: disable=protected-access
         )
         try:
--- sdk/storage/azure-storage-queue/azure/storage/queue/aio/_queue_service_client_async.py
+++ sdk/storage/azure-storage-queue/azure/storage/queue/aio/_queue_service_client_async.py
@@ -385,12 +385,12 @@ class QueueServiceClient(  # type: ignore [misc]
         """
         props = StorageServiceProperties(
             logging=(
-                analytics_logging._to_generated() if analytics_logging else None
-            ),  # pylint: disable=protected-access
+                analytics_logging._to_generated() if analytics_logging else None  # pylint: disable=protected-access
+            ),
             hour_metrics=hour_metrics._to_generated() if hour_metrics else None,  # pylint: disable=protected-access
             minute_metrics=(
-                minute_metrics._to_generated() if minute_metrics else None
-            ),  # pylint: disable=protected-access
+                minute_metrics._to_generated() if minute_metrics else None  # pylint: disable=protected-access
+            ),
             cors=CorsRule._to_generated(cors),  # pylint: disable=protected-access
         )
         try:
