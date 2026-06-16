04bc74517ed01ecf24f90044d89f8a07375bb1a3
Radhika Gupta <guptaradhika@microsoft.com>
2026-06-16 11:55:10 -0700

    Fix pylint in the distro

--- sdk/monitor/azure-monitor-opentelemetry/azure/monitor/opentelemetry/_browser_sdk_loader/__init__.py
+++ sdk/monitor/azure-monitor-opentelemetry/azure/monitor/opentelemetry/_browser_sdk_loader/__init__.py
@@ -87,7 +87,7 @@ def _register_django_middleware(config: BrowserSDKConfig) -> None:
         # Store configuration globally for the middleware to access
         _store_django_config(config)
     except Exception as ex:  # pylint: disable=broad-exception-caught
-        _logger.warning("Failed to register Django middleware: %s", ex, exc_info=True)
+        _logger.warning("Failed to register Django middleware: %s", ex, exc_info=True) # pylint: disable=do-not-log-exceptions-if-not-debug
 
 
 def _store_django_config(config: BrowserSDKConfig) -> None:
