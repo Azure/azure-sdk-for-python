0c71cb7d614c3ab35015d5d8c1ca488b79f46e0e
azure-sdk <azuresdk@microsoft.com>
2026-07-03 03:00:25 +0000

    Configurations:  'specification/cosmos-db/resource-manager/Microsoft.DocumentDB/DocumentDB/tspconfig.yaml', API Version: 2026-03-15, SDK Release Type: stable, and CommitSHA: '3901ca0e742e846b91f299ea063d92bb23820f30' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6515962 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/cosmos/azure-mgmt-cosmosdb/generated_samples/cosmos_db_database_get_metrics.py
+++ sdk/cosmos/azure-mgmt-cosmosdb/generated_samples/cosmos_db_database_get_metrics.py
@@ -35,12 +35,12 @@ def main():
         resource_group_name="rg1",
         account_name="ddb1",
         database_rid="rid",
-        filter="$filter=(name.value eq 'Total Requests') and timeGrain eq duration'PT5M' and startTime eq '2017-11-19T23:53:55.2780000Z' and endTime eq '2017-11-20T00:13:55.2780000Z",
+        filter="(name.value eq 'Total Requests') and timeGrain eq duration'PT5M' and startTime eq '2017-11-19T23:53:55.2780000Z' and endTime eq '2017-11-20T00:13:55.2780000Z",
     )
     for item in response:
         print(item)
 
 
-# x-ms-original-file: 2025-11-01-preview/CosmosDBDatabaseGetMetrics.json
+# x-ms-original-file: 2026-03-15/CosmosDBDatabaseGetMetrics.json
 if __name__ == "__main__":
     main()
