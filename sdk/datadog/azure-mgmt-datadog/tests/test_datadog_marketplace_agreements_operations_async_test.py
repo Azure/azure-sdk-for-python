d91a50f234b67110545141d6390beed0cc0fd5d9
Chenxi Jiang (WICRESOFT NORTH AMERICA LTD) <v-chenjiang@microsoft.com>
2026-07-06 11:55:50 +0800

    update

--- sdk/datadog/azure-mgmt-datadog/tests/test_datadog_marketplace_agreements_operations_async_test.py
+++ sdk/datadog/azure-mgmt-datadog/tests/test_datadog_marketplace_agreements_operations_async_test.py
@@ -6,7 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 import pytest
-from azure.mgmt.datadog.aio import DatadogClient
+from azure.mgmt.datadog.aio import MicrosoftDatadogClient
 
 from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGroupPreparer
 from devtools_testutils.aio import recorded_by_proxy_async
@@ -17,7 +17,7 @@ AZURE_LOCATION = "eastus"
 @pytest.mark.live_test_only
 class TestDatadogMarketplaceAgreementsOperationsAsync(AzureMgmtRecordedTestCase):
     def setup_method(self, method):
-        self.client = self.create_mgmt_client(DatadogClient, is_async=True)
+        self.client = self.create_mgmt_client(MicrosoftDatadogClient, is_async=True)
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy_async
