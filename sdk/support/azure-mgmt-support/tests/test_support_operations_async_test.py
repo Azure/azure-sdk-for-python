003757bcd2304a222f8614f464f6b986e713a0b4
Chenxi Jiang (WICRESOFT NORTH AMERICA LTD) <v-chenjiang@microsoft.com>
2026-07-07 13:27:25 +0800

    update

--- sdk/support/azure-mgmt-support/tests/test_support_operations_async_test.py
+++ sdk/support/azure-mgmt-support/tests/test_support_operations_async_test.py
@@ -6,7 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 import pytest
-from azure.mgmt.support.aio import SupportClient
+from azure.mgmt.support.aio import SupportMgmtClient
 
 from devtools_testutils import AzureMgmtRecordedTestCase, RandomNameResourceGroupPreparer
 from devtools_testutils.aio import recorded_by_proxy_async
@@ -17,7 +17,7 @@ AZURE_LOCATION = "eastus"
 @pytest.mark.live_test_only
 class TestSupportOperationsAsync(AzureMgmtRecordedTestCase):
     def setup_method(self, method):
-        self.client = self.create_mgmt_client(SupportClient, is_async=True)
+        self.client = self.create_mgmt_client(SupportMgmtClient, is_async=True)
 
     @RandomNameResourceGroupPreparer(location=AZURE_LOCATION)
     @recorded_by_proxy_async
