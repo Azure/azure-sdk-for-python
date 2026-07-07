1eba25a9163c76c6d01fabb1176c3b9c2e6db7b2
azure-sdk <azuresdk@microsoft.com>
2026-07-07 07:28:00 +0000

    Configurations:  'specification/advisor/resource-manager/Microsoft.Advisor/Advisor/tspconfig.yaml', API Version: 2026-03-01-preview, SDK Release Type: beta, and CommitSHA: 'a7bdbe741c0ef1f1b159d8569b173757bd823ae9' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6527980 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/advisor/azure-mgmt-advisor/tests/test_mgmt_advisor.py
+++ sdk/advisor/azure-mgmt-advisor/tests/test_mgmt_advisor.py
@@ -1,34 +1,29 @@
 # coding: utf-8
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 import azure.mgmt.advisor
 import datetime
 import re
 import unittest
 
-from azure.mgmt.advisor.models import (
-    ConfigData
-)
+from azure.mgmt.advisor.models import ConfigData
 
-from devtools_testutils import (
-    AzureMgmtRecordedTestCase, ResourceGroupPreparer, recorded_by_proxy
-)
+from devtools_testutils import AzureMgmtRecordedTestCase, ResourceGroupPreparer, recorded_by_proxy
 
 # the goal of these tests is to validate AutoRest generation of the Python wrapper
 # and NOT to validate the behavior of the API. so the tests will primarily attempt
 # to verify that all operations are possible using the generated client and that
 # the operations can accept valid input and produce valid output.
 
+
 class TestMgmtAdvisor(AzureMgmtRecordedTestCase):
 
     def setup_method(self, method):
-        self.client = self.create_mgmt_client(
-            azure.mgmt.advisor.AdvisorManagementClient
-        )
+        self.client = self.create_mgmt_client(azure.mgmt.advisor.AdvisorManagementClient)
 
     @recorded_by_proxy
     def test_generate_recommendations(self):
@@ -40,8 +35,8 @@ class TestMgmtAdvisor(AzureMgmtRecordedTestCase):
         response = self.client.recommendations.generate(cls=call)
 
         # we should get a valid Location header back
-        assert 'Location' in response.headers
-        location = response.headers['Location']
+        assert "Location" in response.headers
+        location = response.headers["Location"]
 
         # extract the operation ID from the Location header
         operation_id = re.findall("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", location)
@@ -51,10 +46,7 @@ class TestMgmtAdvisor(AzureMgmtRecordedTestCase):
         assert len(operation_id), 1
 
         # we should be able to get generation status for this operation ID
-        response = self.client.recommendations.get_generate_status(
-            cls=call,
-            operation_id = operation_id[0]
-        )
+        response = self.client.recommendations.get_generate_status(cls=call, operation_id=operation_id[0])
         status_code = response.status_code
 
         # and the status should be 202 or 204
@@ -82,20 +74,17 @@ class TestMgmtAdvisor(AzureMgmtRecordedTestCase):
             assert rec.short_description != None
             assert rec.short_description.problem != None
             assert rec.short_description.solution != None
-            if (rec.impacted_value != None):
+            if rec.impacted_value != None:
                 recommendation = rec
 
         # construct the properties needed for further operations
-        resourceUri = recommendation.id[:recommendation.id.find("/providers/Microsoft.Advisor/recommendations")]
+        resourceUri = recommendation.id[: recommendation.id.find("/providers/Microsoft.Advisor/recommendations")]
         recommendationName = recommendation.name
         suppressionName = "Python_SDK_Test"
         timeToLive = "00:01:00:00"
 
         # get the individual recommendation
-        output = self.client.recommendations.get(
-            resource_uri = resourceUri,
-            recommendation_id = recommendationName
-        )
+        output = self.client.recommendations.get(resource_uri=resourceUri, recommendation_id=recommendationName)
 
         # it should be identical to what we got from list
         assert output.id == rec.id
@@ -103,10 +92,7 @@ class TestMgmtAdvisor(AzureMgmtRecordedTestCase):
 
         # create a new suppression
         suppression = self.client.suppressions.create(
-            resource_uri = resourceUri,
-            recommendation_id = recommendationName,
-            name = suppressionName,
-            ttl = timeToLive
+            resource_uri=resourceUri, recommendation_id=recommendationName, name=suppressionName, ttl=timeToLive
         )
 
         # it should get created successfully
@@ -114,25 +100,28 @@ class TestMgmtAdvisor(AzureMgmtRecordedTestCase):
 
         # get the suppression
         sup = self.client.suppressions.get(
-            resource_uri = resourceUri,
-            recommendation_id = recommendationName,
-            name = suppressionName
+            resource_uri=resourceUri, recommendation_id=recommendationName, name=suppressionName
         )
 
         # it should be identical to what we just added
         assert sup.name == suppressionName
-        assert sup.id == resourceUri + "/providers/Microsoft.Advisor/recommendations/" + recommendationName + "/suppressions/" + suppressionName
+        assert (
+            sup.id
+            == resourceUri
+            + "/providers/Microsoft.Advisor/recommendations/"
+            + recommendationName
+            + "/suppressions/"
+            + suppressionName
+        )
 
         # delete the suppression
         self.client.suppressions.delete(
-            resource_uri = resourceUri,
-            recommendation_id = recommendationName,
-            name = suppressionName
+            resource_uri=resourceUri, recommendation_id=recommendationName, name=suppressionName
         )
 
         # the suppression should be gone
-        #response = list(self.client.suppressions.list())
-        #for sup in response:
+        # response = list(self.client.suppressions.list())
+        # for sup in response:
         #    assert sup.Name != suppressionName
 
     @unittest.skip("unavailable")
@@ -141,7 +130,7 @@ class TestMgmtAdvisor(AzureMgmtRecordedTestCase):
 
         # create a new configuration to update low CPU threshold to 20
         input = ConfigData()
-        input.low_cpu_threshold=20
+        input.low_cpu_threshold = 20
 
         # update the configuration
         response = self.client.configurations.create_in_subscription(input)
@@ -153,7 +142,7 @@ class TestMgmtAdvisor(AzureMgmtRecordedTestCase):
         assert output.low_cpu_threshold == "20"
 
         # restore the default configuration
-        input.low_cpu_threshold=5
+        input.low_cpu_threshold = 5
         response = self.client.configurations.create_in_subscription(input)
 
         # retrieve the configurations
@@ -170,35 +159,32 @@ class TestMgmtAdvisor(AzureMgmtRecordedTestCase):
 
         # create a new configuration to update exclude to True
         input = ConfigData()
-        input.exclude=True
+        input.exclude = True
 
         # update the configuration
         self.client.configurations.create_in_resource_group(
-            configuration_name=configurationName,
-            resource_group=resourceGroupName,
-            config_contract=input
+            configuration_name=configurationName, resource_group=resourceGroupName, config_contract=input
         )
 
         # retrieve the configurations
-        output = list(self.client.configurations.list_by_resource_group(resource_group = resourceGroupName))[0]
+        output = list(self.client.configurations.list_by_resource_group(resource_group=resourceGroupName))[0]
 
         # it should be identical to what we just set
         assert output.exclude == True
 
         # restore the default configuration
-        input.exclude=False
+        input.exclude = False
         self.client.configurations.create_in_resource_group(
-            configuration_name=configurationName,
-            resource_group=resourceGroupName,
-            config_contract=input
+            configuration_name=configurationName, resource_group=resourceGroupName, config_contract=input
         )
 
         # retrieve the configurations
-        output = list(self.client.configurations.list_by_resource_group(resource_group = resourceGroupName))[0]
+        output = list(self.client.configurations.list_by_resource_group(resource_group=resourceGroupName))[0]
 
         # it should be identical to what we just set
         assert output.exclude == False
 
-#------------------------------------------------------------------------------
-if __name__ == '__main__':
+
+# ------------------------------------------------------------------------------
+if __name__ == "__main__":
     unittest.main()
