1e14cd53c87bd3d6c98b726a12c338fe1f142f9d
Saanika Gupta <gsaanika@gmail.com>
2026-07-07 11:59:03 +0530

    Migrate _util.py normalize_job_input_output_type imports to arm_ml_service (JobInput/JobOutput annotations + JobInputType enum, identical values)

--- sdk/ml/azure-ai-ml/azure/ai/ml/entities/_util.py
+++ sdk/ml/azure-ai-ml/azure/ai/ml/entities/_util.py
@@ -12,9 +12,9 @@ from unittest import mock
 import msrest
 from marshmallow.exceptions import ValidationError
 
-from .._restclient.v2023_04_01_preview.models import JobInput as RestJobInput
-from .._restclient.v2023_04_01_preview.models import JobInputType as JobInputType10
-from .._restclient.v2023_04_01_preview.models import JobOutput as RestJobOutput
+from .._restclient.arm_ml_service.models import JobInput as RestJobInput
+from .._restclient.arm_ml_service.models import JobInputType as JobInputType10
+from .._restclient.arm_ml_service.models import JobOutput as RestJobOutput
 from .._schema._datastore import AzureBlobSchema, AzureDataLakeGen1Schema, AzureDataLakeGen2Schema, AzureFileSchema
 from .._schema._deployment.batch.batch_deployment import BatchDeploymentSchema
 from .._schema._deployment.online.online_deployment import (
