bd1d8dfc12e1ab83dfcef3fecb6c6e3026879ce2
azure-sdk <azuresdk@microsoft.com>
2026-07-13 16:54:17 +0000

    Configurations:  'specification/hybridcompute/resource-manager/Microsoft.HybridCompute/HybridCompute/tspconfig.yaml', API Version: 2026-06-16-preview, SDK Release Type: beta, and CommitSHA: 'ac0d84ebb3f50b37259806b55d9088e4ea930093' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6553724 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/hybridcompute/azure-mgmt-hybridcompute/generated_samples/machines_list_by_resource_group.py
+++ sdk/hybridcompute/azure-mgmt-hybridcompute/generated_samples/machines_list_by_resource_group.py
@@ -37,6 +37,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: 2025-09-16-preview/machine/Machines_ListByResourceGroup.json
+# x-ms-original-file: 2026-06-16-preview/machine/Machines_ListByResourceGroup.json
 if __name__ == "__main__":
     main()
