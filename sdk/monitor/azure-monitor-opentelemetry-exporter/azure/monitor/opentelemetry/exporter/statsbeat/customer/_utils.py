f06c8fdd9e1910691e1ac465c6d7cb738125c0b9
Radhika Gupta <guptaradhika@microsoft.com>
2026-07-14 14:19:20 -0700

    Add ServiceResponseTimeoutError to the classification categories

--- sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/statsbeat/customer/_utils.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/azure/monitor/opentelemetry/exporter/statsbeat/customer/_utils.py
@@ -5,7 +5,7 @@ from typing import Optional, List, Tuple, Union, Any
 
 # mypy: disable-error-code="import-untyped"
 from requests import ReadTimeout, Timeout  # pylint: disable=networking-import-outside-azure-core-transport
-from azure.core.exceptions import ServiceRequestTimeoutError
+from azure.core.exceptions import ServiceRequestTimeoutError, ServiceResponseTimeoutError
 from azure.monitor.opentelemetry.exporter._constants import (
     _REQUEST,
     RetryCode,
@@ -95,6 +95,7 @@ def _determine_client_retry_code(
     """
     timeout_exception_types = (
         ServiceRequestTimeoutError,
+        ServiceResponseTimeoutError,
         ReadTimeout,
         TimeoutError,
         Timeout,
