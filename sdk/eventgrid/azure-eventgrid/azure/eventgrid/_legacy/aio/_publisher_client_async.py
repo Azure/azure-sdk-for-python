8f60a282b2309aba0aef9532c586367c0baf5762
Nishanth-MS <118794679+Nishanth-MS@users.noreply.github.com>
2026-07-28 09:40:13 +0530

    Fix duplicate blocked_redirect_headers kwarg in async client

--- sdk/eventgrid/azure-eventgrid/azure/eventgrid/_legacy/aio/_publisher_client_async.py
+++ sdk/eventgrid/azure-eventgrid/azure/eventgrid/_legacy/aio/_publisher_client_async.py
@@ -116,6 +116,16 @@ class EventGridPublisherClient:
         credential: Union[AzureKeyCredential, AzureSasCredential, "AsyncTokenCredential"], **kwargs: Any
     ) -> List[Any]:
         auth_policy = _get_authentication_policy(credential, AsyncBearerTokenCredentialPolicy)
+        # Merge any caller-supplied blocked redirect headers with the mandatory
+        # Event Grid credential headers, and remove the key from kwargs so it is
+        # not passed twice to SensitiveHeaderCleanupPolicy.
+        blocked_redirect_headers = [
+            "Authorization",
+            "x-ms-authorization-auxiliary",
+            "aeg-sas-key",
+            "aeg-sas-token",
+            *kwargs.pop("blocked_redirect_headers", []),
+        ]
         sdk_moniker = "eventgridpublisherclient/{}".format(VERSION)
         policies = [
             RequestIdPolicy(**kwargs),
@@ -131,12 +141,7 @@ class EventGridPublisherClient:
             # `Authorization` and `x-ms-authorization-auxiliary` by default; the Event
             # Grid SAS headers (`aeg-sas-key`/`aeg-sas-token`) are added here as well.
             SensitiveHeaderCleanupPolicy(
-                blocked_redirect_headers=[
-                    "Authorization",
-                    "x-ms-authorization-auxiliary",
-                    "aeg-sas-key",
-                    "aeg-sas-token",
-                ],
+                blocked_redirect_headers=blocked_redirect_headers,
                 **kwargs,
             ),
             CustomHookPolicy(**kwargs),
