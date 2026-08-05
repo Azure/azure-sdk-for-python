2d9c33afe4664316ffb6e4da78128616d50a5493
azure-sdk <azuresdk@microsoft.com>
2026-08-05 14:46:09 +0000

    Configurations:  'specification/sql/resource-manager/Microsoft.Sql/SQL/tspconfig.yaml', SDK Release Type: beta, and CommitSHA: 'fae30cdf5799e6c1323b2ecc24ba4e9669571ee4' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6662436 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/sql/azure-mgmt-sql/generated_samples/synapse_link_workspace_list_by_database.py
+++ sdk/sql/azure-mgmt-sql/generated_samples/synapse_link_workspace_list_by_database.py
@@ -39,6 +39,6 @@ def main():
         print(item)
 
 
-# x-ms-original-file: 2025-01-01/SynapseLinkWorkspaceListByDatabase.json
+# x-ms-original-file: 2025-08-01-preview/SynapseLinkWorkspaceListByDatabase.json
 if __name__ == "__main__":
     main()
