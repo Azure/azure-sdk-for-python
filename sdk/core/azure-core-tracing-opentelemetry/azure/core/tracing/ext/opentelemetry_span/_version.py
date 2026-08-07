14a2b23744d536f26b0f8f10496ca7f1418d5a11
Daniel Jurek <djurek@microsoft.com>
2026-08-06 17:08:00 -0700

    Revert "TEMP: touch azure-core-tracing-opentelemetry to force package targeting"
    This reverts commit c3a62f7c38aff401346e572fce1e20c073c4b81a.

--- sdk/core/azure-core-tracing-opentelemetry/azure/core/tracing/ext/opentelemetry_span/_version.py
+++ sdk/core/azure-core-tracing-opentelemetry/azure/core/tracing/ext/opentelemetry_span/_version.py
@@ -4,7 +4,3 @@
 # ------------------------------------
 
 VERSION = "1.0.0b14"
-
-# TEMPORARY (do not merge): touched to force package targeting so the
-# CFS network-isolation validation run exercises apistub/pyright/verifytypes.
-
