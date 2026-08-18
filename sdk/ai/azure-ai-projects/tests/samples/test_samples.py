0e140b5553ae1174d366bf1ab1b6796cf2a2ad79
Howie Leung <howieleung@microsoft.com>
2026-08-18 11:19:38 -0700

    Update sample_agent_to_agent.py skip reason in test_samples.py

--- sdk/ai/azure-ai-projects/tests/samples/test_samples.py
+++ sdk/ai/azure-ai-projects/tests/samples/test_samples.py
@@ -48,6 +48,7 @@ class TestSamples(AzureRecordedTestCase):
                 "sample_agent_browser_automation.py",  # APITimeoutError: request timed out
                 "sample_agent_openapi.py",  # 400 2/28/2026 validation/tool_user_error; failing weather GET curl call in OpenAPI tool
                 "sample_agent_memory_search.py",  # Skipped until re-enabled and recorded on Foundry endpoint that supports the new versioning schema
+                "sample_agent_to_agent.py",  # Skipped not sample should work, but not able to obtain a project endpoint that work with a2a at this moment
             ],
         ),
     )
