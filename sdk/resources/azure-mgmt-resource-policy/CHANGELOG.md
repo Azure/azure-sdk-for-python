c0188fa818eee1a2ad19e8efa2d6ae661adb4d4b
azure-sdk <azuresdk@microsoft.com>
2026-08-25 23:29:27 +0000

    Configurations:  'specification/resources/resource-manager/Microsoft.Authorization/policy/tspconfig.yaml', API Version: 2026-01-01-preview, SDK Release Type: beta, and CommitSHA: 'befd1687368facdf2e5f0878725c66107c2cd8d5' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6745144 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/resources/azure-mgmt-resource-policy/CHANGELOG.md
+++ sdk/resources/azure-mgmt-resource-policy/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release History
 
+## 1.0.0b4 (2026-08-25)
+
+tool can't generate changelog for this release, please update manually.
+
 ## 1.0.0b3 (2026-05-26)
 
 ### Features Added
--- sdk/resources/azure-mgmt-resource-policy/azure/mgmt/resource/policy/models/_enums.py
+++ sdk/resources/azure-mgmt-resource-policy/azure/mgmt/resource/policy/models/_enums.py
@@ -207,7 +207,7 @@ class ResourceIdentityType(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     SYSTEM_ASSIGNED = "SystemAssigned"
     """Indicates that a system assigned identity is associated with the resource."""
     USER_ASSIGNED = "UserAssigned"
-    """Indicates that a system assigned identity is associated with the resource."""
+    """Indicates that a user assigned identity is associated with the resource."""
     NONE = "None"
     """Indicates that no identity is associated with the resource or that the existing identity should
     be removed."""
