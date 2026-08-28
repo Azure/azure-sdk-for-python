b3f90b57fece35e2443e797708dd14a775c6c261
Radhika Gupta <guptaradhika@microsoft.com>
2026-08-28 09:45:18 -0700

    Fix format

--- sdk/monitor/azure-monitor-opentelemetry-exporter/samples/logs/sample_log_custom_measurements.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/samples/logs/sample_log_custom_measurements.py
@@ -36,8 +36,7 @@ custom_measurements = json.dumps({"itemsProcessed": 42.0, "queueDepth": 7})
 logger.info(
     "INFO: Custom event with measurements",
     extra={
-        #"microsoft.custom_event.name": "custom-measurements-event", ## Enable to see the custom measurements attribute for custom events.
+        # "microsoft.custom_event.name": "custom-measurements-event", ## Enable to see the custom measurements attribute for custom events.
         "microsoft.custom_measurements": custom_measurements,
     },
 )
-
