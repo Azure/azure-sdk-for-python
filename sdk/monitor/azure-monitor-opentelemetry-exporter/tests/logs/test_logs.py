df6d6df0e4b4f9585c9c8fc70e8dc007421a055e
Radhika Gupta <guptaradhika@microsoft.com>
2026-08-31 13:53:16 -0700

    Fix format

--- sdk/monitor/azure-monitor-opentelemetry-exporter/tests/logs/test_logs.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/tests/logs/test_logs.py
@@ -688,7 +688,9 @@ class TestAzureLogExporter(unittest.TestCase):
         envelope = exporter._log_to_envelope(log_data)
 
         self.assertEqual(envelope.name, "Microsoft.ApplicationInsights.Availability")
-        self.assertEqual(envelope.time, datetime.datetime(2025, 4, 19, 12, 10, 59, 993000, tzinfo=datetime.timezone.utc))
+        self.assertEqual(
+            envelope.time, datetime.datetime(2025, 4, 19, 12, 10, 59, 993000, tzinfo=datetime.timezone.utc)
+        )
         self.assertEqual(envelope.data.base_type, "AvailabilityData")
         self.assertEqual(envelope.data.base_data.id, "test-id")
         self.assertEqual(envelope.data.base_data.name, "test-name")
