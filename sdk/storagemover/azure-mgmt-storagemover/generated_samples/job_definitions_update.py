f00ebf7fa4e3e670a0de3bd9970e98ae8eaa21f4
azure-sdk <azuresdk@microsoft.com>
2026-09-08 11:04:17 +0000

    Configurations:  'specification/storagemover/resource-manager/Microsoft.StorageMover/StorageMover/tspconfig.yaml', API Version: 2026-05-01, SDK Release Type: stable, and CommitSHA: 'f398d14cf518231c3512b317db36c431794d1a83' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6798570 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/storagemover/azure-mgmt-storagemover/generated_samples/job_definitions_update.py
+++ sdk/storagemover/azure-mgmt-storagemover/generated_samples/job_definitions_update.py
@@ -49,6 +49,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: 2025-12-01/JobDefinitions_Update.json
+# x-ms-original-file: 2026-05-01/JobDefinitions_Update.json
 if __name__ == "__main__":
     main()
