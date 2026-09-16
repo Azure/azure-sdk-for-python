55bfadbb1f0a91de6cdb88ebfb52278d35e9143c
azure-sdk <azuresdk@microsoft.com>
2026-09-16 21:22:23 +0000

    Configurations:  'specification/resources/resource-manager/Microsoft.Authorization/policy/tspconfig.yaml', API Version: 2026-06-01, SDK Release Type: stable, and CommitSHA: '710828f4424a112000ac9a81896a8648f87b7548' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6845337 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/resources/azure-mgmt-resource-policy/generated_samples/create_or_update_policy_definition_version_at_management_group.py
+++ sdk/resources/azure-mgmt-resource-policy/generated_samples/create_or_update_policy_definition_version_at_management_group.py
@@ -63,6 +63,6 @@ def main():
     print(response)
 
 
-# x-ms-original-file: 2026-01-01-preview/createOrUpdatePolicyDefinitionVersionAtManagementGroup.json
+# x-ms-original-file: 2026-06-01/createOrUpdatePolicyDefinitionVersionAtManagementGroup.json
 if __name__ == "__main__":
     main()
