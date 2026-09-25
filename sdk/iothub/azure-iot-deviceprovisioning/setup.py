bba54aeea40c94b197b9c381029e14ff8b50927d
Chidozie Ononiwu <chononiw@microsoft.com>
2026-09-24 18:12:42 -0700

    Upgrade sdk/iothub/azure-iot-deviceprovisioning to python version 3.9+

--- sdk/iothub/azure-iot-deviceprovisioning/setup.py
+++ sdk/iothub/azure-iot-deviceprovisioning/setup.py
@@ -51,8 +51,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
@@ -64,7 +62,7 @@ setup(
         'azure',
         'azure.iot',
     ]),
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     include_package_data=True,
     package_data={
         'pytyped': ['py.typed'],
