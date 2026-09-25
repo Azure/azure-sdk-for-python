1a28f8629a89dbed30580478ab8dc9b9b7d40e80
Chidozie Ononiwu <chononiw@microsoft.com>
2026-09-24 18:37:32 -0700

    Upgrade sdk/synapse to python version 3.9+

--- sdk/synapse/azure-synapse-accesscontrol/CHANGELOG.md
+++ sdk/synapse/azure-synapse-accesscontrol/CHANGELOG.md
@@ -10,6 +10,7 @@
 
 ### Other Changes
 
+- Python 3.6, 3.7 and 3.8 are no longer supported. Please use Python version 3.9 or later.
 - Python 2.7 is no longer supported. Please use Python version 3.6 or later.
 
 ## 0.7.0 (2021-08-10)
--- sdk/synapse/azure-synapse-spark/setup.py
+++ sdk/synapse/azure-synapse-spark/setup.py
@@ -52,9 +52,6 @@ setup(
         'Programming Language :: Python',
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
@@ -66,7 +63,7 @@ setup(
         'azure',
         'azure.synapse',
     ]),
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     install_requires=[
         'msrest>=0.6.21',
         'azure-common~=1.1',
