71a14f8354e26dbbf3f4c0ea1cc682979bf0977b
Chidozie Ononiwu <chononiw@microsoft.com>
2026-09-24 18:16:37 -0700

    Upgrade sdk/monitor/azure-monitor-opentelemetry-exporter to python version 3.9+

--- sdk/monitor/azure-monitor-opentelemetry-exporter/CHANGELOG.md
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/CHANGELOG.md
@@ -14,6 +14,8 @@
 
 ### Other Changes
 
+- Python 3.8 is no longer supported. Please use Python version 3.9 or later.
+
 ## 1.0.0b57 (2026-09-02)
 
 ### Features Added
--- sdk/monitor/azure-monitor-opentelemetry-exporter/setup.py
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/setup.py
@@ -81,7 +81,7 @@ setup(
     package_data={
         "pytyped": ["py.typed"],
     },
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "azure-core<2.0.0,>=1.38.0",
         "azure-identity~=1.17",
